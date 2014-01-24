dbd817f30c4010d86f673f1559e1d5a0cc8bc299
Allen Liu <kamyu104@gmail.com>
2014-01-24 23:25:50 +0800

    update sortColors.cpp

--- C++/sortColors.cpp
+++ C++/sortColors.cpp
@@ -4,16 +4,17 @@
 class Solution {
     public:
         void sortColors(int A[], int n) {
-            int i0 = 0;
-            int i2 = n-1;
-            for(int i = 0; i < n && i0 <= i2 && i <= i2; i++) {
-                while(A[i] != 1 && i0 <= i2 && i >= i0 && i <= i2) {
-                    if(A[i] == 0) {
-                        swap(A[i], A[i0++]);
-                    }
-                    else if(A[i] == 2) {
-                        swap(A[i], A[i2--]);
-                    }
+            int red = 0;
+            int blue = n-1;
+            for(int i = 0; i < blue + 1;) {
+                if(A[i] == 0) {
+                    swap(A[i++], A[red++]);
+                }
+                else if(A[i] == 2) {
+                    swap(A[i], A[blue--]);
+                }
+                else {
+                    i++;
                 }
             }
         }
