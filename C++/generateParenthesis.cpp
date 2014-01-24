0686fbdf3c7a539e2fbe6434ab82270d132736a0
Allen Liu <kamyu104@gmail.com>
2014-01-24 23:16:00 +0800

    add generateParenthesis.cpp

---
+++ C++/generateParenthesis.cpp
@@ -0,0 +1,20 @@
+// Time Complexity O(2^n/n)??
+// Space Complexity O(n)
+
+class Solution {
+    public:
+        void generator(vector<string> &ans, string s, int l, int r,  int n) {
+            if(l==n) {
+                ans.push_back(s.append(n-r, ')'));
+                return;
+            }
+            generator(ans, s+"(", l+1, r, n);
+            if(l>r) generator(ans, s+")", l, r+1, n);
+
+        }
+        vector<string> generateParenthesis(int n) {
+            vector<string> ans;
+            generator(ans, "", 0, 0, n);
+            return ans;
+        }
+};
