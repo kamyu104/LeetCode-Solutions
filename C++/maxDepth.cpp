c18d3fc9b5fb050368577c4cb4e2afc9388198f3
Allen Liu <kamyu104@gmail.com>
2014-01-24 20:56:23 +0800

    refactor maxDepth

--- C++/maxDepth.cpp
+++ C++/maxDepth.cpp
@@ -12,9 +12,6 @@ public:
     int maxDepth(TreeNode *root) {
         if(!root)
             return 0;
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-        if(left > right) return left + 1;
-        else return right + 1;
+        return  max(maxDepth(root->left), maxDepth(root->right))+1;
     }
 };
