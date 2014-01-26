6696d8036d693ac87831b3255b6f48c42cee6b57
Allen Liu <kamyu104@gmail.com>
2014-01-26 22:26:41 +0800

    add searchMatrix2.cpp

---
+++ C++/searchMatrix2.cpp
@@ -0,0 +1,26 @@
+// Time Complexity: O(log(mn))
+// Space Complexity: O(1)
+
+class Solution {
+    public:
+        bool searchMatrix(vector<vector<int> > &matrix, int target) {
+            if(matrix.empty()) return false;
+
+            const size_t m = matrix.size();
+            const size_t n = matrix.front().size();
+
+            int start = 0, end = m * n;
+
+            while(start < end) {
+                int mid = (start + end) / 2;
+                int value = matrix[mid / n][mid % n];
+                if(target == value)
+                    return true;
+                else if (value < target)
+                    start = mid + 1;
+                else
+                    end = mid;
+            }
+            return false;
+        }
+};
