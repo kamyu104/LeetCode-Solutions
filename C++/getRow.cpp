4c123b5c9c7539056f62e2b2c3535eadba09dcc4
Allen Liu <kamyu104@gmail.com>
2014-01-27 02:17:37 +0800

    Update getRow.cpp

--- C++/getRow.cpp
+++ C++/getRow.cpp
@@ -4,10 +4,10 @@
 class Solution {
     public:
         vector<int> getRow(int rowIndex) {
-            vector<int> ans(rowIndex+1, 1);
+            vector<int> ans(rowIndex + 1, 1);
 
             for(int i = 2; i <= rowIndex; ++i) {
-                for(int j = i - 1; j > 0; --j) {;
+                for(int j = i - 1; j > 0; --j) {
                     ans[j] += ans[j - 1];
                 }
             }
