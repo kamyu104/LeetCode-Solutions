4950f023a56016b60e593d7d6f362bcdd34193bf
Allen Liu <kamyu104@gmail.com>
2014-01-29 03:24:36 +0800

    add uniquePathWithObstacles.cpp

---
+++ C++/uniquePathWithObstacles.cpp
@@ -0,0 +1,25 @@
+// Time Complexity: O(m * n)
+// Space Complexity: O(m + n)
+
+class Solution {
+    public:
+        int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
+            const int m = obstacleGrid.size();
+            const int n = obstacleGrid[0].size();
+            vector<int> v(n, 0);
+
+            v[0] = 1;
+            for(int i = 0; i < m; ++i) {
+                if(obstacleGrid[i][0] != 0)
+                    v[0] = 0;
+                for(int j = 1; j < n; ++j) {
+                    if(obstacleGrid[i][j] == 0)
+                        v[j] += v[j - 1];
+                    else
+                        v[j] = 0;
+                }
+            }
+
+            return v[n - 1];
+        }
+};
