289a5108520782e489ba0cd13d46d36f83bdffd1
Allen Liu <kamyu104@gmail.com>
2014-01-31 03:08:10 +0800

    add jump.cpp

---
+++ C++/jump.cpp
@@ -0,0 +1,19 @@
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public:
+        int jump(int A[], int n) {
+            int ans = 0;
+            int last = 0;
+            int cur = 0;         // at most position by further jump
+            for(int i = 0, next; i < n; ++i) {
+                if(i > last) {   // he cannot reach "i" by current jumps
+                    ++ans;       // so he should jump one more time
+                    last = cur;  // to reach at most position
+                }
+                cur = max(cur, i + A[i]); // update at most position by further jump
+            }
+            return ans;
+        }
+};
