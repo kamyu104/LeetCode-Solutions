cb1a5a27ee2546fb3d6f110f627c6694df703be6
Allen Liu <kamyu104@gmail.com>
2014-01-31 15:13:41 +0800

    add minDistance.cpp

---
+++ C++/minDistance.cpp
@@ -0,0 +1,35 @@
+// Time Complexity: O(m * n)
+// Space Complexity: O(m + n)
+
+class Solution {
+    public:
+        int minDistance(string word1, string word2) {
+            const size_t m = word1.size();
+            const size_t n = word2.size();
+
+            if(m < n)
+                return minDistance(word2, word1);
+
+            vector<size_t> f(n + 1, 0);
+            size_t upper_left;
+
+            for(size_t i = 0; i < n + 1; ++i)
+                f[i] = i;
+
+            for(size_t i = 1; i < m + 1; ++i) {
+                upper_left = f[0];
+                f[0] = i;
+                for(size_t j = 1; j < n + 1; ++j) {
+                    size_t upper = f[j];
+                    if(word1[i - 1] == word2[j - 1])
+                        f[j] = upper_left;
+                    else {
+                        f[j] = 1 + min(upper_left, min(f[j - 1], f[j]));
+                    }
+                    upper_left = upper;
+                }
+            }
+
+            return f[n];
+        }
+};
