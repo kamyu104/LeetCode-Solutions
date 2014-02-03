42a793f5fb907cc70d2e00233327eae22466e6a5
Allen Liu <kamyu104@gmail.com>
2014-02-03 23:34:17 +0800

    add convert.cpp

---
+++ C++/convert.cpp
@@ -0,0 +1,31 @@
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public:
+        string convert(string s, int nRows) {
+            vector<string> row(nRows);
+            string ans;
+            int cnt = 0;
+
+            if(nRows == 1)
+                return s;
+
+            for(auto c : s) {
+                if(cnt < nRows) {
+                    row[cnt].push_back(c);
+                }
+                else {
+                    row[2 * nRows - 2 - cnt].push_back(c);
+                }
+
+                cnt = (cnt + 1) % (2 * nRows - 2);
+            }
+
+            for(auto s : row) {
+                ans.append(s);
+            }
+
+            return ans;
+        }
+};
