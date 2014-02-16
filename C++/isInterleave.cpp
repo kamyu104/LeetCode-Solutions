47ac74682ab21f435a118d9ab414753f8fe59ae8
Allen Liu <kamyu104@gmail.com>
2014-02-16 17:23:57 +0800

    add reorderList.cpp

---
+++ C++/isInterleave.cpp
@@ -0,0 +1,27 @@
+// Time Complexity: O(m * n)
+// Space Complexity: O(m + n)
+
+class Solution {
+    public:
+        bool isInterleave(string s1, string s2, string s3) {
+            if(s1.length() + s2.length() != s3.length()) return false;
+
+            if(s1.length() < s2.length()) return isInterleave(s2, s1, s3);
+
+            vector<bool> f(s2.length() + 1, true);
+
+            for(auto j = 1; j <= s2.length(); ++j) {
+                f[j] = f[j - 1] && s2[j - 1] == s3[j - 1];
+            }
+
+            for(auto i = 1; i <= s1.length(); ++i) {
+                f[0] = f[0] && s1[i - 1] == s3[i - 1];
+                for(auto j = 1; j <= s2.length(); ++j) {
+                    f[j] = (f[j] && s1[i - 1] == s3[i + j - 1])
+                        || (f[j - 1] && s2[j - 1] == s3[i + j - 1]);
+                }
+            }
+
+            return f[s2.length()];
+        }
+};
