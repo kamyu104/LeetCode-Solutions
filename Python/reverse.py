f3dda2ebc3be32b4ca23b4427ba84665af040355
Allen Liu <kamyu104@gmail.com>
2014-05-30 00:39:33 +0800

    add python

---
+++ Python/reverse.py
@@ -0,0 +1,8 @@
+class Solution:
+    # @param s, a string
+    # @return a string
+	def reverseWords(self, s):
+		return ' '.join(reversed(s.split()))
+
+if __name__ == '__main__':
+	print Solution().reverseWords('hello world')
