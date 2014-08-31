c6d52342035d48c896e11e8fa43d8a74850a345e
Allen Liu <kamyu104@gmail.com>
2014-08-31 19:55:32 +0800

    add addBinary.py, addTwoNumbers.py

---
+++ Python/addBinary.py
@@ -0,0 +1,31 @@
+# Time: O(n)
+# Space: O(1)
+#
+# Given two binary strings, return their sum (also a binary string).
+#
+# For example,
+# a = "11"
+# b = "1"
+# Return "100".
+
+class Solution:
+    # @param a, a string
+    # @param b, a string
+    # @return a string
+    def addBinary(self, a, b):
+        result, carry, val, len_a, len_b, i = "", 0, 0, len(a), len(b), 0
+        for i in range(max(len_a, len_b)):
+            val = carry
+            if i < len_a:
+                sum += int(a[-(i + 1)])
+            if i < len_b: 
+                sum += int(b[-(i + 1)])
+            carry, val = val / 2, val % 2
+            result = "{0}{1}".format(val, result)
+        if carry == 1:
+            result = "1" + result
+        return result
+
+if __name__ == '__main__':
+    result = Solution().addBinary('11', '1')
+    print result
---
+++ Python/addTwoNumbers.py
@@ -0,0 +1,45 @@
+# Time: O(n)
+# Space: O(1)
+#
+# You are given two linked lists representing two non-negative numbers. 
+# The digits are stored in reverse order and each of their nodes contain a single digit.
+# Add the two numbers and return it as a linked list.
+#
+# Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+# Output: 7 -> 0 -> 8
+#
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    # @return a ListNode
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(0)
+        current, carry = dummy, 0
+
+        while l1 is not None or l2 is not None:
+            val = carry
+            if l1 is not None:
+                val += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                val += l2.val
+                l2 = l2.next
+            carry, val = val / 10, val % 10
+            current.next = ListNode(val)
+            current = current.next
+
+        if carry == 1:
+            current.next = ListNode(1)
+
+        return dummy.next
+
+if __name__ == '__main__':
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    result = Solution().addTwoNumbers(a, b)
+    print "{0} -> {1} -> {2}".format(result.val, result.next.val, result.next.next.val)
+        
