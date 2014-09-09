ac5beebd22b91c05d3ebbf1b2ddb11605c71cfcb
Allen Liu <kamyu104@gmail.com>
2014-09-10 02:34:15 +0800

    update

--- Python/divideTwoIntegers.py
+++ Python/divideTwoIntegers.py
@@ -7,14 +7,14 @@
 class Solution:
     # @return an integer
     def divide(self, dividend, divisor):
-        result, a, b = 0, abs(dividend), abs(divisor)
-        while a >= b:
-            c = b
+        result, dvd, dvs = 0, abs(dividend), abs(divisor)
+        while dvd >= dvs:
+            inc = dvs
             i = 0
-            while a >= c:
-                a -= c
+            while dvd >= inc:
+                dvd -= inc
                 result += 1 << i
-                c <<= 1
+                inc <<= 1
                 i += 1
         if dividend > 0 and divisor < 0 or dividend < 0 and divisor > 0:
             return -result
