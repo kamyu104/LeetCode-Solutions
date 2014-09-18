9095432417b5289c8e9ef4d992fdcf41d462e7a2
Allen Liu <kamyu104@gmail.com>
2014-09-19 00:23:42 +0800

    add more solutions

---
+++ Python/sameTree.py
@@ -0,0 +1,32 @@
+# Time:  O(n)
+# Space: O(logn)
+#
+# Given two binary trees, write a function to check if they are equal or not.
+# 
+# Two binary trees are considered equal if they are structurally identical and the nodes have the same value.
+#
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param p, a tree node
+    # @param q, a tree node
+    # @return a boolean
+    def isSameTree(self, p, q):
+        if p is None and q is None:
+            return True
+        
+        if p is not None and q is not None:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
+    
+if __name__ == "__main__":
+    root1, root1.left, root1.right = TreeNode(1), TreeNode(2), TreeNode(3)
+    root2, root2.left, root2.right = TreeNode(1), TreeNode(2), TreeNode(3)
+    print Solution().isSameTree(root1, root2)
---
+++ Python/uniqueBinarySearchTrees.py
@@ -0,0 +1,28 @@
+# Time:  O(n^2)
+# Space: O(n)
+#
+# Given n, how many structurally unique BST's (binary search trees) that store values 1...n?
+# 
+# For example,
+# Given n = 3, there are a total of 5 unique BST's.
+# 
+#    1         3     3      2      1
+#     \       /     /      / \      \
+#      3     2     1      1   3      2
+#     /     /       \                 \
+#    2     1         2                 3
+#
+
+class Solution:
+    # @return an integer
+    def numTrees(self, n):
+        counts = [1, 1]
+        for i in range(2, n + 1):
+            count = 0
+            for j in xrange(i):
+                count += counts[j] * counts[i - j - 1]
+            counts.append(count)
+        return counts[-1]
+    
+if __name__ == "__main__":
+    print Solution().numTrees(3)
