fa97dc148cd2f151ea8b555c9fa86ece54f30ba4
Allen Liu <kamyu104@gmail.com>
2014-10-19 23:06:07 +0800

    add more solutions

---
+++ Python/textJustification.py
@@ -0,0 +1,74 @@
+# Time:  O(n)
+# SpacE: O(1)
+#
+# Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
+# 
+# You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' 
+# when necessary so that each line has exactly L characters.
+# 
+# Extra spaces between words should be distributed as evenly as possible. 
+# If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+# 
+# For the last line of text, it should be left justified and no extra space is inserted between words.
+# 
+# For example,
+# words: ["This", "is", "an", "example", "of", "text", "justification."]
+# L: 16.
+# 
+# Return the formatted lines as:
+# [
+#    "This    is    an",
+#    "example  of text",
+#    "justification.  "
+# ]
+# Note: Each word is guaranteed not to exceed L in length.
+
+class Solution:
+    # @param words, a list of strings
+    # @param L, an integer
+    # @return a list of strings
+    def fullJustify(self, words, L):
+        result = []
+        
+        i = 0
+        while i < len(words):
+            # count words in one line
+            size, begin = 0, i
+            while i < len(words):
+                if size == 0:
+                    newsize = len(words[i])
+                else:
+                    newsize = size + len(words[i]) + 1
+                if newsize <= L:
+                    size = newsize
+                else:
+                    break
+                i += 1
+            
+            # count space number    
+            spaceCount = L - size
+            if i - begin - 1 > 0 and i < len(words):
+                everyCount = spaceCount / (i - begin - 1)
+                spaceCount %= i - begin - 1
+            else:
+                everyCount = 0
+            
+            # add space
+            j = begin
+            while j < i:
+                if j == begin:
+                    s = words[j]
+                else:
+                    s += ' ' * (everyCount + 1)
+                    if spaceCount > 0 and i < len(words):
+                        s += ' '
+                        spaceCount -= 1
+                    s += words[j]
+                j += 1
+            s += ' ' * spaceCount
+            result.append(s)
+            
+        return result
+
+if __name__ == "__main__":
+    print Solution().fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)
---
+++ Python/wildcardMatching.py
@@ -0,0 +1,123 @@
+# Time:  O(m + n)
+# Space: O(1)
+#
+# Implement wildcard pattern matching with support for '?' and '*'.
+# 
+# '?' Matches any single character.
+# '*' Matches any sequence of characters (including the empty sequence).
+# 
+# The matching should cover the entire input string (not partial).
+# 
+# The function prototype should be:
+# bool isMatch(const char *s, const char *p)
+# 
+# Some examples:
+# isMatch("aa","a") -> false
+# isMatch("aa","aa") -> true
+# isMatch("aaa","aa") -> false
+# isMatch("aa", "*") -> true
+# isMatch("aa", "a*") -> true
+# isMatch("ab", "?*") -> true
+# isMatch("aab", "c*a*b") -> false
+#
+
+# iteration
+class Solution:
+    # @param s, an input string
+    # @param p, a pattern string
+    # @return a boolean
+    def isMatch(self, s, p):
+        p_ptr, s_ptr, last_s_ptr, last_p_ptr = 0, 0, -1, -1
+        while s_ptr < len(s):
+            if p_ptr < len(p) and (s[s_ptr] == p[p_ptr] or p[p_ptr] == '?'):
+                s_ptr += 1
+                p_ptr += 1
+            elif p_ptr < len(p) and p[p_ptr] == '*':
+                p_ptr += 1
+                last_s_ptr = s_ptr
+                last_p_ptr = p_ptr
+            elif last_p_ptr != -1:
+                last_s_ptr += 1
+                s_ptr = last_s_ptr
+                p_ptr = last_p_ptr
+            else:
+                return False
+            
+        while p_ptr < len(p) and p[p_ptr] == '*':
+            p_ptr += 1
+        
+        return p_ptr == len(p)
+    
+# dp
+class Solution2:
+    # @return a boolean
+    def isMatch(self, s, p):
+        k = 2
+        result = [[False for j in xrange(len(p) + 1)] for i in xrange(k)]
+
+        result[0][0] = True
+        for i in xrange(1, len(p) + 1):
+            if p[i-1] == '*':
+                result[0][i] = result[0][i-1]
+        for i in xrange(1,len(s) + 1):
+            result[i % k][0] = False
+            for j in xrange(1, len(p) + 1):
+                if p[j-1] != '*':
+                    result[i % k][j] = result[(i-1) % k][j-1] and (s[i-1] == p[j-1] or p[j-1] == '?')
+                else:
+                    result[i % k][j] = result[i % k][j-1] or result[(i-1) % k][j]
+                    
+        return result[len(s) % k][len(p)]
+
+# dp
+# Time:  O(m * n)
+# Space: O(m * n)
+class Solution3:
+    # @return a boolean
+    def isMatch(self, s, p):
+        result = [[False for j in xrange(len(p) + 1)] for i in xrange(len(s) + 1)]
+
+        result[0][0] = True
+        for i in xrange(1, len(p) + 1):
+            if p[i-1] == '*':
+                result[0][i] = result[0][i-1]
+        for i in xrange(1,len(s) + 1):
+            result[i][0] = False
+            for j in xrange(1, len(p) + 1):
+                if p[j-1] != '*':
+                    result[i][j] = result[i-1][j-1] and (s[i-1] == p[j-1] or p[j-1] == '?')
+                else:
+                    result[i][j] = result[i][j-1] or result[i-1][j]
+                    
+        return result[len(s)][len(p)]
+
+
+# recursive, slowest
+class Solution4:
+    # @return a boolean
+    def isMatch(self, s, p):
+        if len(p) == 0:
+            return len(s) == 0
+        
+        if p[0] != '*':
+            if len(s) == 0 or (p[0] == s[0] or p[0] == '?'):
+                return self.isMatch(s[1:], p[1:])
+            else:
+                return False
+        else:
+            while len(s) > 0:
+                if self.isMatch(s, p[1:]):
+                    return True
+                s = s[1:]
+            return self.isMatch(s, p[1:])
+
+if __name__ == "__main__":
+    print Solution().isMatch("aaaabaaaab","a*b*b")
+#     print Solution().isMatch("aaaaaaaaaaaaab", "a*a*a*a*a*a*a*a*a*a*c")
+#     print Solution().isMatch("aa","a")
+#     print Solution().isMatch("aa","aa")
+#     print Solution().isMatch("aaa","aa")
+#     print Solution().isMatch("aa", "a*")
+#     print Solution().isMatch("aa", "?*")
+#     print Solution().isMatch("ab", "?*")
+    print Solution().isMatch("aab", "c*a*b")
