875a66dbefac3d733a3904d7b49857f1c2ee383f
kamyu <kamyu104@gmail.com>
2014-10-22 00:06:51 +0800

    Update best-time-to-buy-and-sell-stock-ii.py

--- Python/best-time-to-buy-and-sell-stock-ii.py
+++ Python/best-time-to-buy-and-sell-stock-ii.py
@@ -16,7 +16,7 @@ class Solution:
     # @return an integer
     def maxProfit(self, prices):
         profit = 0
-        for i in range(len(prices) - 1):
+        for i in xrange(len(prices) - 1):
             profit += max(0, prices[i + 1] - prices[i])     
         return profit
 
