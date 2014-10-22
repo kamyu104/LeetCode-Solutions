51640d32095848bdba39f7422b2e764af18b890d
Allen Liu <kamyu104@gmail.com>
2014-10-23 00:48:04 +0800

    update

--- Python/balanced-binary-tree.py
+++ Python/balanced-binary-tree.py
@@ -5,6 +5,7 @@
 #
 # For this problem, a height-balanced binary tree is defined as a binary tree 
 # in which the depth of the two subtrees of every node never differ by more than 1.
+#
 
 # Definition for a  binary tree node
 class TreeNode:
