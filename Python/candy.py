610984ac13e4bca35cee8537092e995aeeeea380
Allen Liu <kamyu104@gmail.com>
2014-10-24 00:55:46 +0800

    update

--- Python/candy.py
+++ Python/candy.py
@@ -9,6 +9,7 @@
 # Children with a higher rating get more candies than their neighbors.
 # What is the minimum candies you must give?
 #
+
 import operator
 
 class Solution:
@@ -16,11 +17,11 @@ class Solution:
     # @return an integer
     def candy(self, ratings):
         candies = [1 for i in range(len(ratings))]
-        for i in range(1, len(ratings)):
+        for i in xrange(1, len(ratings)):
             if ratings[i] > ratings[i - 1]:
                 candies[i] = candies[i - 1] + 1
         
-        for i in reversed(range(1, len(ratings))):
+        for i in reversed(xrange(1, len(ratings))):
             if ratings[i - 1] > ratings[i] and candies[i - 1] <= candies[i]:
                 candies[i - 1] = candies[i] + 1
         
@@ -29,3 +30,4 @@ class Solution:
 if __name__ == "__main__":
     result = Solution().candy([1, 2, 3, 2, 3, 5, 2, 5])
     print result
+
