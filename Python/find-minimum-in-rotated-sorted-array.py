28b78a32b020e18b525f11d76b26e3da2fe8a138
Allen Liu <kamyu104@gmail.com>
2014-11-16 22:01:01 +0800

    refine solutions

--- Python/find-minimum-in-rotated-sorted-array.py
+++ Python/find-minimum-in-rotated-sorted-array.py
@@ -16,10 +16,7 @@ class Solution:
     def findMin(self, num):
         low, high = 0, len(num)
            
-        while low < high - 1:
-            if num[low] < num[high - 1]:
-                return num[low]
-            
+        while low < high - 1 and  num[low] >= num[high - 1]:
             mid = low + (high - low) / 2
                 
             if num[mid] > num[low]:
@@ -40,11 +37,9 @@ class Solution2:
     def findMin(self, num):
         low, high = 0, len(num) - 1
            
-        while low < high:
-            if num[low] < num[high]:
-                return num[low]
-            
+        while low < high and num[low] >= num[high]:
             mid = low + (high - low) / 2
+            
             if num[mid] >= num[low]:
                 low = mid + 1
             else:
