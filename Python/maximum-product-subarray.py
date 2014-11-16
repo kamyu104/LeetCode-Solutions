af94ad1f8900dbd0f7e55f2aa752609b19d0fb53
Allen Liu <kamyu104@gmail.com>
2014-11-16 22:13:12 +0800

    refine solutions

--- Python/maximum-product-subarray.py
+++ Python/maximum-product-subarray.py
@@ -12,6 +12,16 @@ class Solution:
     def maxProduct(self, A):
         global_max, local_max, local_min = float("-inf"), 1, 1
         for x in A:
+            local_max, local_min = max(x, local_max * x, local_min * x), min(x, local_max * x, local_min * x)
+            global_max = max(global_max, local_max)
+        return global_max
+
+class Solution2:
+    # @param A, a list of integers
+    # @return an integer
+    def maxProduct(self, A):
+        global_max, local_max, local_min = float("-inf"), 1, 1
+        for x in A:
             local_max = max(1, local_max)
             if x > 0:
                 local_max, local_min = local_max * x, local_min * x
