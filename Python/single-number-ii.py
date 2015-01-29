fc41a875811d0f61ca856492c3d5f1b33d7a7d99
Allen Liu <kamyu104@gmail.com>
2015-01-30 01:04:21 +0800

    add extended solution

--- Python/single-number-ii.py
+++ Python/single-number-ii.py
@@ -11,7 +11,7 @@ class Solution:
     # @param A, a list of integer
     # @return an integer
     def singleNumber(self, A):
-        one, two, carry = 0, 0, 0
+        one, two = 0, 0
         for x in A:
             one, two = (~x & one) | (x & ~one & ~two), (~x & two) | (x & one)
         return one
@@ -28,6 +28,17 @@ class Solution2:
             one &= ~carry
             two &= ~carry
         return one
+
+#  every element appears 4 times except for one with 2 times
+class SolutionEX:
+    # @param A, a list of integer
+    # @return an integer
+    # [1, 1, 1, 1, 2, 2, 2, 2, 3, 3]
+    def singleNumber(self, A):
+        one, two, three = 0, 0, 0
+        for x in A:
+            one, two, three = (~x & one) | (x & ~one & ~two & ~three), (~x & two) | (x & one), (~x & three) | (x & two)
+        return two
 
 if __name__ == "__main__":
     print Solution().singleNumber([1, 1, 1, 2, 2, 2, 3])
