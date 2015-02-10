72d97d2bd9c79864310c7b4e9086766de55d3ed2
kamyu <kamyu104@gmail.com>
2015-02-11 00:12:44 +0800

    Update binary-search-tree-iterator.py

--- Python/binary-search-tree-iterator.py
+++ Python/binary-search-tree-iterator.py
@@ -1,5 +1,5 @@
 # Time:  O(1)
-# Space: O(logn)
+# Space: O(h), h is height of binary tree
 # 
 # Implement an iterator over a binary search tree (BST).
 # Your iterator will be initialized with the root node of a BST.
