20126fbed480e956289de0f7e79f57de09b8fa8c
kamyu <kamyu104@gmail.com>
2015-02-11 00:23:16 +0800

    Update path-sum.py

--- Python/path-sum.py
+++ Python/path-sum.py
@@ -1,5 +1,5 @@
 # Time:  O(n)
-# Space: O(logn)
+# Space: O(h), h is height of binary tree
 #
 # Given a binary tree and a sum, determine if the tree has a root-to-leaf path 
 # such that adding up all the values along the path equals the given sum.
