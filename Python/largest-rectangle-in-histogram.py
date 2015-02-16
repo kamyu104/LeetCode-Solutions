710748c6c14d8d42ae4a862416dd204a2cac0a90
kamyu <kamyu104@users.noreply.github.com>
2015-02-16 23:10:12 +0800

    Update largest-rectangle-in-histogram.py

--- Python/largest-rectangle-in-histogram.py
+++ Python/largest-rectangle-in-histogram.py
@@ -16,12 +16,12 @@ class Solution:
     def largestRectangleArea(self, height):
         increasing, area, i = [], 0, 0
         while i <= len(height):
-            if len(increasing) == 0 or (i < len(height) and height[i] > height[increasing[-1]]):
+            if not increasing or (i < len(height) and height[i] > height[increasing[-1]]):
                 increasing.append(i)
                 i += 1
             else:
                 last = increasing.pop()
-                if len(increasing) == 0:
+                if not increasing:
                     area = max(area, height[last] * i)
                 else:
                     area = max(area, height[last] * (i - increasing[-1] - 1 ))
