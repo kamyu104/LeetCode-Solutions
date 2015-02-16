42a226e955ced9bd35b8de8b221ed1a2d9380df0
kamyu <kamyu104@users.noreply.github.com>
2015-02-16 23:24:24 +0800

    Update regular-expression-matching.py

--- Python/regular-expression-matching.py
+++ Python/regular-expression-matching.py
@@ -70,8 +70,8 @@ class Solution2:
 class Solution3:
     # @return a boolean
     def isMatch(self, s, p):
-        if len(p) == 0:
-            return len(s) == 0
+        if not p:
+            return not s
         
         if len(p) == 1 or p[1] != '*':
             if len(s) > 0 and (p[0] == s[0] or p[0] == '.'):
