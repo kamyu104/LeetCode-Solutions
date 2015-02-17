1106b6d4115fde9c924d0aa4361178688909b15a
Allen Liu <kamyu104@gmail.com>
2015-02-18 01:10:07 +0800

    update

--- Python/best-time-to-buy-and-sell-stock-iv.py
+++ Python/best-time-to-buy-and-sell-stock-iv.py
@@ -13,32 +13,31 @@ class Solution:
     # @return an integer as the maximum profit 
     def maxProfit(self, k, prices):
         if k >= len(prices) / 2:
-            return self.maxUnlimitPairProfit(prices)
+            return self.maxAtMostNPairsProfit(prices)
 
         return self.maxAtMostKPairsProfit(prices, k)
-    
-    def maxUnlimitPairProfit(self, prices):
+
+    def maxAtMostNPairsProfit(self, prices):
         profit = 0
         for i in xrange(len(prices) - 1):
             profit += max(0, prices[i + 1] - prices[i])     
         return profit
-        
+
     def maxAtMostKPairsProfit(self, prices, k):
         if k == 0:
             return 0
-            
-        k_sum = [float("-inf") for _ in xrange(2 * k)]
-        for i in xrange(1, len(k_sum), 2):
-            k_sum[i] = 0
-            
+
+        max_buy = [float("-inf") for _ in xrange(k + 1)]
+        max_sell = [0 for _ in xrange(k + 1)]
+
         for i in xrange(len(prices)):
-            j, sign, pre_k_sum = 0, -1, 0
-            while j < len(k_sum):
-                diff = pre_k_sum + sign * prices[i]
-                pre_k_sum, k_sum[j] = k_sum[j], max(diff, k_sum[j])
-                j, sign = j + 1, sign * -1
-                
-        return k_sum[-1]
+            j = 1
+            while j <= k:
+                max_buy[j] = max(max_buy[j], max_sell[j-1] - prices[i])
+                max_sell[j] = max(max_sell[j], max_buy[j] + prices[i]) 
+                j = j + 1
+
+        return max_sell[k]
 
 if __name__ == "__main__":
     print Solution().maxAtMostKPairsProfit([1, 2, 3, 4], 2)
