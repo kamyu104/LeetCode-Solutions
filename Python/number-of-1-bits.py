930b7e6c6b371fb7bfa92de4f4ea92d750d7ce56
Allen Liu <kamyu104@gmail.com>
2015-03-10 23:00:52 +0800

    update

--- Python/number-of-1-bits.py
+++ Python/number-of-1-bits.py
@@ -2,9 +2,9 @@
 # Space: O(1)
 #
 # Write a function that takes an unsigned integer 
-# and returns the number of ’1' bits it has (also known as the Hamming weight).
+# and returns the number of '1' bits it has (also known as the Hamming weight).
 # 
-# For example, the 32-bit integer ’11' has binary representation 00000000000000000000000000001011, 
+# For example, the 32-bit integer '11' has binary representation 00000000000000000000000000001011, 
 # so the function should return 3.
 #
 # Credits:
