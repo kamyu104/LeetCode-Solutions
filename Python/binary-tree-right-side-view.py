3c48770170015553dc6a51aa8523399c495a7df4
Allen Liu <kamyu104@gmail.com>
2015-04-07 20:24:15 +0800

    update

--- Python/binary-tree-right-side-view.py
+++ Python/binary-tree-right-side-view.py
@@ -15,11 +15,11 @@
 #
 
 # Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
     # @param root, a tree node
