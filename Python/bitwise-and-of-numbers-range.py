43dadaf16a6bdb0557d65bc9c2fff9682e1547be
kamyu <kamyu104@users.noreply.github.com>
2015-04-18 02:19:28 +0800

    Create bitwise-and-of-numbers-range.py

---
+++ Python/bitwise-and-of-numbers-range.py
@@ -0,0 +1,21 @@
+# Time:  O(1)
+# Space: O(1)
+#
+# Given a range [m, n] where 0 <= m <= n <= 2147483647, 
+# return the bitwise AND of all numbers in this range, inclusive.
+#
+# For example, given the range [5, 7], you should return 4.
+#
+
+class Solution:
+    # @param m, an integer
+    # @param n, an integer
+    # @return an integer
+    def rangeBitwiseAnd(self, m, n):
+        i = 0
+        while n-m >> i:
+            i += 1
+        return n&m >> i << i
+
+if __name__ == '__main__':
+    print Solution().rangeBitwiseAnd(5, 7)
