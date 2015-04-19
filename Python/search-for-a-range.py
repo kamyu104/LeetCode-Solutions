a250d32a23a1a65eafdba2346651fe23a812297f
kamyu <kamyu104@users.noreply.github.com>
2015-04-19 16:15:34 +0800

    Update search-for-a-range.py

--- Python/search-for-a-range.py
+++ Python/search-for-a-range.py
@@ -17,10 +17,12 @@ class Solution:
     # @param target, an integer to be searched
     # @return a list of length 2, [index1, index2]
     def searchRange(self, A, target):
-        left = self.binarySearch(lambda x, y: x > y, A, target)
+        # Find the first index where target <= A[idx]
+        left = self.binarySearch(lambda x, y: x <= y, A, target)
         if left >= len(A) or A[left] != target:
             return [-1, -1]
-        right = self.binarySearch(lambda x, y: x >= y, A, target)
+        # Find the first index where target < A[idx]
+        right = self.binarySearch(lambda x, y: x < y, A, target)
         return [left, right - 1]
     
     def binarySearch(self, compare, A, target):
@@ -28,9 +30,19 @@ class Solution:
         while start < end:
             mid = start + (end - start) / 2
             if compare(target, A[mid]):
-                start = mid + 1
-            else:
                 end = mid
+            else:
+                start = mid + 1
+        return start
+    
+    def binarySearch2(self, compare, A, target):
+        start, end = 0, len(A) - 1
+        while start <= end:
+            mid = start + (end - start) / 2
+            if compare(target, A[mid]):
+                end = mid - 1
+            else:
+                start = mid + 1
         return start
 
 if __name__ == "__main__":
