ae57dd90dd54bbf7bde9d5a91ff00da50c91c71d
Allen Liu <kamyu104@gmail.com>
2015-05-02 10:51:14 +0800

    update

--- Python/regular-expression-matching.py
+++ Python/regular-expression-matching.py
@@ -66,8 +66,38 @@ class Solution2:
                     
         return result[len(s)][len(p)]
 
-# recursive
+# iteration
 class Solution3:
+    # @return a boolean
+    def isMatch(self, s, p):
+        p_ptr, s_ptr, last_s_ptr, last_p_ptr = 0, 0, -1, -1
+        last_ptr = []
+        while s_ptr < len(s):
+            if p_ptr < len(p) and (p_ptr == len(p) - 1 or p[p_ptr + 1] != '*') and \
+            (s_ptr < len(s) and (p[p_ptr] == s[s_ptr] or p[p_ptr] == '?')):
+                    s_ptr += 1
+                    p_ptr += 1
+            elif p_ptr < len(p) - 1 and (p_ptr != len(p) - 1 and p[p_ptr + 1] == '*'):
+                p_ptr += 2
+                last_ptr.append([s_ptr, p_ptr])
+            elif  last_ptr:
+                [last_s_ptr, last_p_ptr] = last_ptr.pop()
+                while last_ptr and p[last_p_ptr - 2] != s[last_s_ptr] and p[last_p_ptr - 2] != '.':
+                    [last_s_ptr, last_p_ptr] = last_ptr.pop()
+                last_s_ptr += 1
+                s_ptr = last_s_ptr
+                p_ptr = last_p_ptr
+                last_ptr.append([s_ptr, p_ptr])
+            else:
+                return False
+            
+        while p_ptr < len(p) and p[p_ptr] == '.' and p[p_ptr + 1] == '*':
+            p_ptr += 2
+        
+        return p_ptr == len(p)
+    
+# recursive
+class Solution4:
     # @return a boolean
     def isMatch(self, s, p):
         if not p:
