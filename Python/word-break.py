69570e5f23a2407d221a186f658de48973d6f2f5
kamyu <kamyu104@users.noreply.github.com>
2015-05-04 17:19:22 +0800

    Update word-break.py

--- Python/word-break.py
+++ Python/word-break.py
@@ -12,6 +12,34 @@
 #
 
 class Solution:
+    # @param s: A string s
+    # @param dict: A dictionary of words dict
+    def wordSegmentation(self, s, dict):
+        if not s:
+            return True
+            
+        cnt = {}
+        for w in dict:
+            for c in w:
+                if c not in cnt:
+                    cnt[c] = 0
+                cnt[c] += 1
+        for c in s:
+            if c not in cnt:
+                return False
+                
+        n = len(s)
+        possible = [False for _ in xrange(n)]
+        for i in xrange(n):
+            for j in reversed(xrange(i + 1)):
+                if (j == 0 or possible[j-1]) and s[j:i+1] in dict:
+                    possible[i] = True
+                    break
+                
+        return possible[n-1]
+
+# slower
+class Solution2:
     # @param s, a string
     # @param dict, a set of string
     # @return a boolean
