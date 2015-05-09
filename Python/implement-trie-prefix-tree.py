3d4025b5716d2d16e363d3ef97f299011f7e9b1f
kamyu <kamyu104@users.noreply.github.com>
2015-05-09 12:49:04 +0800

    Create implement-trie-prefix-tree.py

---
+++ Python/implement-trie-prefix-tree.py
@@ -0,0 +1,61 @@
+# Time:  O(n), per operation
+# Space: O(1)
+#
+# Implement a trie with insert, search, and startsWith methods.
+#
+# Note:
+# You may assume that all inputs are consist of lowercase letters a-z.
+#
+
+class TrieNode:
+    # Initialize your data structure here.
+    def __init__(self):
+        self.flag = False
+        self.children = {}
+        
+
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    # @param {string} word
+    # @return {void}
+    # Inserts a word into the trie.
+    def insert(self, word):
+        cur = self.root
+        for c in word:
+            if not c in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.flag = True
+
+    # @param {string} word
+    # @return {boolean}
+    # Returns if the word is in the trie.
+    def search(self, word):
+        res, node = self.childSearch(word)
+        if res:
+            return node.flag
+        return False        
+
+    # @param {string} prefix
+    # @return {boolean}
+    # Returns if there is any word in the trie
+    # that starts with the given prefix.
+    def startsWith(self, prefix):
+        return self.childSearch(prefix)[0]
+
+    def childSearch(self, word):
+        cur = self.root
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+            else:
+                return False, None
+        return True, cur
+
+# Your Trie object will be instantiated and called as such:
+# trie = Trie()
+# trie.insert("somestring")
+# trie.search("key")
