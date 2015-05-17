7635ff273a48b65b41eb6fb186cc29c94e956599
kamyu <kamyu104@users.noreply.github.com>
2015-05-17 13:38:20 +0800

    Update rotate-image.py

--- Python/rotate-image.py
+++ Python/rotate-image.py
@@ -20,12 +20,12 @@ class Solution:
         # anti-diagonal mirror
         for i in xrange(n):
             for j in xrange(n - i):
-                matrix[i][j], matrix[n - 1 - j][n - 1 -i] = matrix[n - 1 - j][n - 1 -i], matrix[i][j]
+                matrix[i][j], matrix[n-1-j][n-1-i] = matrix[n-1-j][n-1-i], matrix[i][j]
         
         # horizontal mirror
         for i in xrange(n / 2):
             for j in xrange(n):
-                matrix[i][j], matrix[n - 1 - i][j] = matrix[n - 1 - i][j], matrix[i][j]
+                matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
                 
         return matrix
                 
