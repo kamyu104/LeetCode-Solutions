7896b3d457d1fb4fa1b9cf2ee04bb81955a6634a
kamyu <kamyu104@users.noreply.github.com>
2015-05-19 16:04:11 +0800

    Update word-search-ii.py

--- Python/word-search-ii.py
+++ Python/word-search-ii.py
@@ -47,12 +47,12 @@ class Solution:
             
         for i in xrange(len(board)):
             for j in xrange(len(board[0])):
-                if self.existRecu(board, trie, 0, i, j, visited, [], result):
+                if self.findWordsRecu(board, trie, 0, i, j, visited, [], result):
                     return True
         
         return result.keys()
     
-    def existRecu(self, board, trie, cur, i, j, visited, cur_word, result):
+    def findWordsRecu(self, board, trie, cur, i, j, visited, cur_word, result):
         if not trie or i < 0 or i >= len(board) or j < 0 or j >= len(board[0]) or visited[i][j]:
             return
         
@@ -65,10 +65,10 @@ class Solution:
             result["".join(cur_word)] = True
         
         visited[i][j] = True
-        self.existRecu(board, next_node, cur + 1, i + 1, j, visited, cur_word, result)
-        self.existRecu(board, next_node, cur + 1, i - 1, j, visited, cur_word, result)
-        self.existRecu(board, next_node, cur + 1, i, j + 1, visited, cur_word, result)
-        self.existRecu(board, next_node, cur + 1, i, j - 1, visited, cur_word, result)     
+        self.findWordsRecu(board, next_node, cur + 1, i + 1, j, visited, cur_word, result)
+        self.findWordsRecu(board, next_node, cur + 1, i - 1, j, visited, cur_word, result)
+        self.findWordsRecu(board, next_node, cur + 1, i, j + 1, visited, cur_word, result)
+        self.findWordsRecu(board, next_node, cur + 1, i, j - 1, visited, cur_word, result)     
         visited[i][j] = False
         cur_word.pop()
         
