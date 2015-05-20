cc1ec09b39a8450fc8c49acba45dbaf86a825d3e
kamyu <kamyu104@users.noreply.github.com>
2015-05-20 11:41:20 +0800

    Create house-robber-ii.cpp

---
+++ C++/house-robber-ii.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        if (nums.size() == 1) {
+            return nums[0];
+        }
+        
+        return max(robRange(nums, 0, nums.size() - 1), robRange(nums, 1, nums.size()));
+    }
+    int robRange(vector<int>& nums, int start, int end) {
+        int num_i = nums[start], num_i_1 = 0, num_i_2 = 0;
+        for (int i = start + 1; i < end; ++i) {
+            num_i_2 = num_i_1;
+            num_i_1 = num_i;
+            num_i = max(nums[i] + num_i_2, num_i_1);
+        }
+        return num_i;
+    }
+};
