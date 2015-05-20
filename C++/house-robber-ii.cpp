4ad5e5031ca4e76664a8235d0f062d797a714e47
kamyu <kamyu104@users.noreply.github.com>
2015-05-20 11:43:50 +0800

    Update house-robber-ii.cpp

--- C++/house-robber-ii.cpp
+++ C++/house-robber-ii.cpp
@@ -14,6 +14,7 @@ public:
         return max(robRange(nums, 0, nums.size() - 1), // Include the first one of nums without the last one.
                    robRange(nums, 1, nums.size()));     // Include the last one of nums without the first one.
     }
+    
     int robRange(vector<int>& nums, int start, int end) {
         int num_i = nums[start], num_i_1 = 0, num_i_2 = 0;
         for (int i = start + 1; i < end; ++i) {
