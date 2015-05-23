a31d8ab1c64b71240777f2398b12024602a476ff
kamyu <kamyu104@users.noreply.github.com>
2015-05-23 21:06:06 +0800

    Create shortest-palindrome.py

---
+++ Python/shortest-palindrome.py
@@ -0,0 +1,40 @@
+# Time:  O(n)
+# Space: O(n)
+
+# TLE
+class Solution:
+    # @param {string} s
+    # @return {string}
+    def shortestPalindrome(self, s):
+        string = self.preProcess(s)
+        palindrome = [0] * len(string) 
+        center, right = 0, 0
+        for i in xrange(1, len(string) - 1):
+            i_mirror = 2 * center - i
+            if right > i:
+                palindrome[i] = min(right - i, palindrome[i_mirror])
+            else:
+                palindrome[i] = 0
+
+            while string[i + 1 + palindrome[i]] == string[i - 1 - palindrome[i]]:
+                palindrome[i] += 1
+
+            if i + palindrome[i] > right:
+                center, right = i, i + palindrome[i]       
+        
+        max_len, max_center = 0, 0
+        for i in xrange(1, len(string) - 1):
+            if i - palindrome[i] == 1:
+                max_len = palindrome[i]
+        return s[len(s)-1:max_len-1:-1] + s
+        
+    def preProcess(self, s):
+        
+        if not s:
+            return "^$"
+        string = "^"
+        for i in s:
+            string +=  "#" + i
+        string += "#$"
+        return string
+        
