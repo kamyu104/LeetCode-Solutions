e4ac5cfcd0647ecc1a215a60dcf00a56f8403e6e
kamyu <kamyu104@users.noreply.github.com>
2015-05-24 09:18:32 +0800

    Update shortest-palindrome.cpp

--- C++/shortest-palindrome.cpp
+++ C++/shortest-palindrome.cpp
@@ -1,7 +1,42 @@
 // Time:  O(n)
 // Space: O(n)
 
+// KMP Algorithm
 class Solution {
+public:
+    string shortestPalindrome(string s) {
+            if (s.empty()) {
+                return s;
+            }
+            string rev_s(s.crbegin(), s.crend());
+            string A = s + rev_s;
+            vector<int> pattern(move(getPrefix(A)));
+            string non_palindrome = s.substr(pattern.back() + 1);
+            reverse(non_palindrome.begin(), non_palindrome.end());
+            return non_palindrome + s;
+        }
+        
+private:
+    vector<int> getPrefix(const string& pattern) {
+        vector<int> prefix(pattern.length(), -1);
+        int j = -1;
+        for (int i = 1; i < pattern.length(); ++i) {
+            while (j > -1 && pattern[j + 1] != pattern[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == pattern[i]) {
+                ++j;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// Manacher's Algorithm
+class Solution2 {
 public:
     string shortestPalindrome(string s) {
             string T = preProcess(s);
@@ -40,17 +75,17 @@ public:
             ans.append(s); // cba(Palindrome)abc.
             return ans;
         }
-    private:
-        string preProcess(string s) {
-            int n = s.length();
-            if (n == 0) {
-                return "^$";
-            }
-            string ret = "^";
-            for (int i = 0; i < n; i++)
-                ret += "#" + s.substr(i, 1);
-
-            ret += "#$";
-            return ret;
+private:
+    string preProcess(string s) {
+        int n = s.length();
+        if (n == 0) {
+            return "^$";
         }
+        string ret = "^";
+        for (int i = 0; i < n; ++i) {
+            ret += "#" + s.substr(i, 1);
+        }
+        ret += "#$";
+        return ret;
+    }
 };
