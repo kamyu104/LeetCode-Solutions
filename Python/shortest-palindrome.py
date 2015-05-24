6b2e890f1798ec5718c06773275f572176c15ffe
kamyu <kamyu104@users.noreply.github.com>
2015-05-24 09:09:15 +0800

    Update shortest-palindrome.py

--- Python/shortest-palindrome.py
+++ Python/shortest-palindrome.py
@@ -1,8 +1,32 @@
 # Time:  O(n)
 # Space: O(n)
 
-# TLE
+# KMP algorithm
 class Solution:
+    # @param {string} s
+    # @return {string}
+    def shortestPalindrome(self, s):
+        if not s:
+            return s
+            
+        A = s + s[::-1]
+        prefix = self.getPrefix(A)
+            
+        return s[prefix[-1]+1:][::-1] + s
+        
+    def getPrefix(self, pattern):
+        prefix = [-1] * len(pattern)
+        j = -1
+        for i in xrange(1, len(pattern)):
+            while j > -1 and pattern[j+1] != pattern[i]:
+                j = prefix[j]
+            if pattern[j+1] == pattern[i]:
+                j += 1
+            prefix[i] = j
+        return prefix
+
+# Manacher's Algorithm
+class Solution_TLE:
     # @param {string} s
     # @return {string}
     def shortestPalindrome(self, s):
