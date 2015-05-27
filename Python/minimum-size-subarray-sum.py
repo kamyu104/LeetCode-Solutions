45ae6da2cb751f7335ea5222e33f21e196a0288e
kamyu <kamyu104@users.noreply.github.com>
2015-05-27 13:27:59 +0800

    Update minimum-size-subarray-sum.py

--- Python/minimum-size-subarray-sum.py
+++ Python/minimum-size-subarray-sum.py
@@ -11,6 +11,7 @@
 # If you have figured out the O(n) solution, try coding another solution of which the time complexity is O(n log n).
 #
 
+# Sliding window solution.
 class Solution:
     # @param {integer} s
     # @param {integer[]} nums
@@ -28,3 +29,34 @@ class Solution:
         if min_len == float("inf"):
             return 0
         return min_len
+
+# Time:  O(nlogn)
+# Space: O(n)
+# Binary search solution.
+class Solution2:
+    # @param {integer} s
+    # @param {integer[]} nums
+    # @return {integer}
+    def minSubArrayLen(self, s, nums):
+        min_size = float("inf")
+        sum_from_start = [n for n in nums]
+        for i in xrange(len(sum_from_start) - 1):
+            sum_from_start[i + 1] += sum_from_start[i]
+        for i in xrange(len(sum_from_start)):
+            end = self.binarySearch(lambda x, y: x <= y, sum_from_start, \
+                                    i, len(sum_from_start), \
+                                    sum_from_start[i] - nums[i] + s)
+            if end < len(sum_from_start):
+                min_size = min(min_size,  end  - i + 1)
+        if min_size == float("inf"):
+            return 0
+        return min_size
+    
+    def binarySearch(self, compare, A, start, end, target):
+        while start < end:
+            mid = start + (end - start) / 2
+            if compare(target, A[mid]):
+                end = mid
+            else:
+                start = mid + 1
+        return start
