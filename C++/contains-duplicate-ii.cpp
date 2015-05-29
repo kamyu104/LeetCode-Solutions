f6e143beca2da62a434e859c5b8338491c234456
kamyu <kamyu104@users.noreply.github.com>
2015-05-29 10:39:15 +0800

    Update contains-duplicate-ii.cpp

--- C++/contains-duplicate-ii.cpp
+++ C++/contains-duplicate-ii.cpp
@@ -4,17 +4,17 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        unordered_map<int, int> hash;
+        unordered_map<int, int> lookup;
         for (int i = 0; i < nums.size(); ++i) {
-            if (hash.find(nums[i]) == hash.end()) {
-                hash[nums[i]] = i;
+            if (lookup.find(nums[i]) == lookup.end()) {
+                lookup[nums[i]] = i;
             } else {
                 // It the value occurs before, check the difference.
-                if (i - hash[nums[i]] <= k) {
+                if (i - lookup[nums[i]] <= k) {
                     return true;
                 }
                 // Update the index of the value.
-                hash[nums[i]] = i;
+                lookup[nums[i]] = i;
             }
         }
         return false;
