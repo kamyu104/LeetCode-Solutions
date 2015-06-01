6c63c880cf8da24f765aee4a52a8cf624f086446
kamyu <kamyu104@users.noreply.github.com>
2015-06-01 14:33:20 +0800

    Update contains-duplicate-iii.cpp

--- C++/contains-duplicate-iii.cpp
+++ C++/contains-duplicate-iii.cpp
@@ -4,20 +4,20 @@
 class Solution {
 public:
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
-        deque<int64_t> window;
+        queue<int64_t> window;
         multiset<int64_t> bst;
         for (int i = 0; i < nums.size(); ++i) {
             // Only keep at most k elements.
             if (bst.size() > k) {
                 int num = window.front();
-                window.pop_front();
+                window.pop();
                 bst.erase(bst.find(num));
             }
             // Every search costs time: O(logn).
             const auto it = bst.lower_bound(nums[i] - t);
             if (it == bst.cend() || (*it - nums[i]) > t) {
                 // Not found.
-                window.emplace_back(nums[i]);
+                window.emplace(nums[i]);
                 bst.emplace(nums[i]);
             } else {
                 return true;
