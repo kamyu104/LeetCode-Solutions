9b552e4dfbab1d874691edd09384ccc35fb176c9
kamyu <kamyu104@users.noreply.github.com>
2015-06-04 00:06:52 +0800

    Update maximal-square.py

--- Python/maximal-square.py
+++ Python/maximal-square.py
@@ -1,5 +1,5 @@
 # Time:  O(n^2)
-# Space: O(n^2)
+# Space: O(n)
 #
 # Given a 2D binary matrix filled with 0's and 1's, 
 # find the largest square containing all 1's and return its area.
@@ -13,7 +13,80 @@
 # Return 4.
 #
 
+# DP with sliding window.
 class Solution:
+    # @param {character[][]} matrix
+    # @return {integer}
+    def maximalSquare(self, matrix):
+        if not matrix:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        size = [[0 for j in xrange(n)] for i in xrange(2)]
+        max_size = 0
+        
+        for j in xrange(n):
+            if matrix[0][j] == '1':
+                size[0][j] = 1
+            max_size = max(max_size, size[0][j])
+            
+        for i in xrange(1, m):
+            if matrix[i][0] == '1':
+                size[i % 2][0] = 1
+            else:
+                size[i % 2][0] = 0
+            for j in xrange(1, n):
+                if matrix[i][j] == '1':
+                    size[i % 2][j] = min(size[i % 2][j - 1], \
+                                         size[(i - 1) % 2][j], \
+                                         size[(i - 1) % 2][j - 1]) + 1
+                    max_size = max(max_size, size[i % 2][j])
+                else:
+                    size[i % 2][j] = 0
+                    
+        return max_size * max_size
+
+
+# Time:  O(n^2)
+# Space: O(n^2)
+# DP.
+class Solution2:
+    # @param {character[][]} matrix
+    # @return {integer}
+    def maximalSquare(self, matrix):
+        if not matrix:
+            return 0
+
+        m, n = len(matrix), len(matrix[0])
+        size = [[0 for j in xrange(n)] for i in xrange(m)]
+        max_size = 0
+        
+        for j in xrange(n):
+            if matrix[0][j] == '1':
+                size[0][j] = 1
+            max_size = max(max_size, size[0][j])
+            
+        for i in xrange(1, m):
+            if matrix[i][0] == '1':
+                size[i][0] = 1
+            else:
+                size[i][0] = 0
+            for j in xrange(1, n):
+                if matrix[i][j] == '1':
+                    size[i][j] = min(size[i][j - 1],  \
+                                     size[i - 1][j],  \
+                                     size[i - 1][j - 1]) + 1
+                    max_size = max(max_size, size[i][j])
+                else:
+                    size[i][j] = 0
+                    
+        return max_size * max_size
+        
+        
+# Time:  O(n^2)
+# Space: O(n^2)
+# DP.
+class Solution3:
     # @param {character[][]} matrix
     # @return {integer}
     def maximalSquare(self, matrix):
