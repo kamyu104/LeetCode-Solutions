7b39af16836265034a89f625d44b2ed8c8211a13
kamyu <kamyu104@users.noreply.github.com>
2015-06-03 22:06:19 +0800

    Create maximal-square.cpp

---
+++ C++/maximal-square.cpp
@@ -0,0 +1,46 @@
+// Time:  O(n^2)
+// Space: O(n^2)
+
+struct MaxHW {
+    int h, w;
+};
+
+class Solution {
+public:
+    int maximalSquare(vector<vector<char>>& A) {
+        if (A.empty()) {
+            return 0;
+        }
+        
+        // DP table stores (h, w) for each (i, j).
+        vector<vector<MaxHW>> table(A.size(), vector<MaxHW>(A.front().size()));
+        for (int i = A.size() - 1; i >= 0; --i) {
+            for (int j = A[i].size() - 1; j >= 0; --j) {
+                // Find the largest h such that (i, j) to (i + h - 1, j) are feasible.
+                // Find the largest w such that (i, j) to (i, j + w - 1) are feasible.
+                table[i][j] = A[i][j] == '1'
+                                  ? MaxHW{i + 1 < A.size() ? table[i + 1][j].h + 1 : 1,
+                                          j + 1 < A[i].size() ? table[i][j + 1].w + 1 : 1}
+                                  : MaxHW{0, 0};
+            }
+        }
+        
+        // A table stores the length of largest square for each (i, j).
+        vector<vector<int>> s(A.size(), vector<int>(A.front().size(), 0));
+        int max_square_area = 0;
+        for (int i = A.size() - 1; i >= 0; --i) {
+            for (int j = A[i].size() - 1; j >= 0; --j) {
+                int side = min(table[i][j].h, table[i][j].w);
+                if (A[i][j]) {
+                    // Get the length of largest square with bottom-left corner (i, j).
+                    if (i + 1 < A.size() && j + 1 < A[i + 1].size()) {
+                        side = min(s[i + 1][j + 1] + 1, side);
+                    }
+                    s[i][j] = side;
+                    max_square_area = max(max_square_area, side * side);
+                }
+            }
+        }
+        return max_square_area;
+    }
+};
