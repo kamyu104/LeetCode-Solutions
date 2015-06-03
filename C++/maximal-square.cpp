1a17f8aea3afdc8d6faa3c9c7d9339749168e832
kamyu <kamyu104@users.noreply.github.com>
2015-06-03 23:54:33 +0800

    Update maximal-square.cpp

--- C++/maximal-square.cpp
+++ C++/maximal-square.cpp
@@ -1,12 +1,81 @@
 // Time:  O(n^2)
-// Space: O(n^2)
+// Space: O(n)
 
-struct MaxHW {
-    int h, w;
-};
-
+// DP with rolling window.
 class Solution {
 public:
+    int maximalSquare(vector<vector<char>>& A) {
+        if (A.empty()) {
+            return 0;
+        }
+        const int m = A.size(), n = A[0].size();
+        vector<vector<int>> size(2, vector<int>(n, 0));
+        int max_size = 0;
+        
+        for (int j = 0; j < n; ++j) {
+            size[0][j] = A[0][j] - '0';
+            max_size = max(max_size, size[0][j]);
+        }
+        for (int i = 1; i < m; ++i) {
+            size[i % 2][0] = A[i][0] - '0';
+            for (int j = 1; j < n; ++j) {
+                if (A[i][j] == '1') {
+                    size[i % 2][j] = min(size[i % 2][j - 1], 
+                                         min(size[(i - 1) % 2][j], 
+                                             size[(i - 1) % 2][j - 1])) + 1;
+                    max_size = max(max_size, size[i % 2][j]);
+                } else {
+                    size[i % 2][j] = 0;
+                }
+            }
+        }
+        return max_size * max_size;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n^2)
+// DP.
+class Solution2 {
+public:
+    int maximalSquare(vector<vector<char>>& A) {
+        if (A.empty()) {
+            return 0;
+        }
+        const int m = A.size(), n = A[0].size();
+        vector<vector<int>> size(m, vector<int>(n, 0));
+        int max_size = 0;
+        
+        for (int j = 0; j < n; ++j) {
+            size[0][j] = A[0][j] - '0';
+            max_size = max(max_size, size[0][j]);
+        }
+        for (int i = 1; i < m; ++i) {
+            size[i][0] = A[i][0] - '0';
+            for (int j = 1; j < n; ++j) {
+                if (A[i][j] == '1') {
+                    size[i][j] = min(size[i][j - 1], 
+                                    min(size[i - 1][j], 
+                                        size[i - 1][j - 1])) + 1;
+                    max_size = max(max_size, size[i][j]);
+                } else {
+                    size[i][j] = 0;
+                }
+            }
+        }
+        return max_size * max_size;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n^2)
+// DP.
+class Solution3 {
+public:
+    struct MaxHW {
+        int h, w;
+    };
+    
     int maximalSquare(vector<vector<char>>& A) {
         if (A.empty()) {
             return 0;
