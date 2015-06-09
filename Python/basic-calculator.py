305a8b9e00baf5ae33dee92c57ce6c45b6b3ba42
kamyu <kamyu104@users.noreply.github.com>
2015-06-09 23:59:19 +0800

    Create basic-calculator.py

---
+++ Python/basic-calculator.py
@@ -0,0 +1,47 @@
+# Time:  O(n)
+# Space: O(n)
+#
+# Implement a basic calculator to evaluate a simple expression string.
+#
+# The expression string may contain open ( and closing parentheses ),
+# the plus + or minus sign -, non-negative integers and empty spaces .
+#
+# You may assume that the given expression is always valid.
+#
+# Some examples:
+# "1 + 1" = 2
+# " 2-1 + 2 " = 3
+# "(1+(4+5+2)-3)+(6+8)" = 23
+#
+
+class Solution:
+    # @param {string} s
+    # @return {integer}
+    def calculate(self, s):
+        operands, operators = [], []
+        operand = ""
+        for i in reversed(xrange(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0  or not s[i-1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ""
+            elif s[i] == ')' or s[i] == '+' or s[i] == '-':
+                operators.append(s[i])
+            elif s[i] == '(':
+                while operators[-1] != ')':
+                    self.compute(operands, operators)
+                operators.pop()
+                
+        while operators:
+            self.compute(operands, operators)
+            
+        return operands[-1]
+
+    def compute(self, operands, operators):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == '+':
+            operands.append(left + right)
+        elif op == '-':
+            operands.append(left - right)
