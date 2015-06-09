8d77e46b65e6f277ead4dbd8ab86669a2073e6c3
kamyu <kamyu104@users.noreply.github.com>
2015-06-09 14:26:19 +0800

    Update next-permutation.py

--- Python/next-permutation.py
+++ Python/next-permutation.py
@@ -14,23 +14,29 @@
 #
 
 class Solution:
+    # @param {integer[]} nums
+    # @return {void} Do not return anything, modify nums in-place instead.
+    def nextPermutation(self, num):
+        num = self.nextPermutation2(num)
+        
     # @param num, a list of integer
     # @return a list of integer
-    def nextPermutation(self, num):
+    def nextPermutation2(self, num):
         k, l = -1, 0
         for i in xrange(len(num) - 1):
             if num[i] < num[i + 1]:
                 k = i
                 
         if k == -1:
-            return num[::-1]
+            num.reverse()
+            return
         
         for i in xrange(len(num)):
             if num[i] > num[k]:
                 l = i
                 
         num[k], num[l] = num[l], num[k]
-        return num[:k + 1] + num[:k:-1]
+        num[k + 1:] = num[:k:-1]
 
 if __name__ == "__main__":
     num = [1, 4, 3, 2]
