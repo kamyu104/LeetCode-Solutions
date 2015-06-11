9d956edc4b7a9252bedd13965fe3996653b69cad
kamyu <kamyu104@users.noreply.github.com>
2015-06-11 21:17:39 +0800

    Update implement-stack-using-queues.cpp

--- C++/implement-stack-using-queues.cpp
+++ C++/implement-stack-using-queues.cpp
@@ -6,7 +6,7 @@ public:
     queue<int> q;
     
     // Push element x onto stack.
-    void push(int x) {
+    void push(int x) {  // O(n)
         int n = q.size();
         q.emplace(x);
         for (; n > 0; --n) {
@@ -16,17 +16,55 @@ public:
     }
 
     // Removes the element on top of the stack.
-    void pop() {
+    void pop() {  // O(1)
         q.pop();
     }
 
     // Get the top element.
-    int top() {
+    int top() {  // O(1)
         return q.front();
     }
 
     // Return whether the stack is empty.
-    bool empty() {
+    bool empty() {  // O(1)
+        return q.empty();
+    }
+};
+
+// Time:  push: O(1), pop: O(n), top: O(n)
+// Space: O(n)
+class Stack2 {
+public:
+    queue<int> q, q2;
+    
+    // Push element x onto stack.
+    void push(int x) {  // O(1)
+        q.emplace(x);
+    }
+
+    // Removes the element on top of the stack.
+    void pop() {  // O(n)
+        for (int i = 0; i < q.size() - 1; ++i) {
+            q.emplace(q.front());
+            q.pop();
+        }
+        q.pop();
+    }
+
+    // Get the top element.
+    int top() {  // O(n)
+        for (int i = 0; i < q.size() - 1; ++i) {
+            q.emplace(q.front());
+            q.pop();
+        }
+        int top = q.front();
+        q.emplace(q.front());
+        q.pop();
+        return top;
+    }
+
+    // Return whether the stack is empty.
+    bool empty() {  // O(1)
         return q.empty();
     }
 };
