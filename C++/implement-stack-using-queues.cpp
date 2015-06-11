513361992b6cd083877ca244b41fae42f8781b0d
kamyu <kamyu104@users.noreply.github.com>
2015-06-11 21:27:28 +0800

    Update implement-stack-using-queues.cpp

--- C++/implement-stack-using-queues.cpp
+++ C++/implement-stack-using-queues.cpp
@@ -3,67 +3,64 @@
 
 class Stack {
 public:
-    queue<int> q;
+    queue<int> q_;
     
     // Push element x onto stack.
     void push(int x) {  // O(n)
-        q.emplace(x);
-        for (int i = 0; i < q.size() - 1; ++i) {
-            q.emplace(q.front());
-            q.pop();
+        q_.emplace(x);
+        for (int i = 0; i < q_.size() - 1; ++i) {
+            q_.emplace(q_.front());
+            q_.pop();
         }
     }
 
     // Removes the element on top of the stack.
     void pop() {  // O(1)
-        q.pop();
+        q_.pop();
     }
 
     // Get the top element.
     int top() {  // O(1)
-        return q.front();
+        return q_.front();
     }
 
     // Return whether the stack is empty.
     bool empty() {  // O(1)
-        return q.empty();
+        return q_.empty();
     }
 };
 
-// Time:  push: O(1), pop: O(n), top: O(n)
+// Time:  push: O(1), pop: O(n), top: O(1)
 // Space: O(n)
 class Stack2 {
 public:
-    queue<int> q, q2;
+    queue<int> q_;
+    int top_;
     
     // Push element x onto stack.
     void push(int x) {  // O(1)
-        q.emplace(x);
+        q_.emplace(x);
+        top_ = x;
     }
 
     // Removes the element on top of the stack.
     void pop() {  // O(n)
-        for (int i = 0; i < q.size() - 1; ++i) {
-            q.emplace(q.front());
-            q.pop();
+        for (int i = 0; i < q_.size() - 1; ++i) {
+            top_ = q_.front();
+            q_.emplace(top_);
+            q_.pop();
         }
-        q.pop();
+        q_.pop();
     }
 
     // Get the top element.
-    int top() {  // O(n)
-        for (int i = 0; i < q.size() - 1; ++i) {
-            q.emplace(q.front());
-            q.pop();
-        }
-        int top = q.front();
-        q.emplace(q.front());
-        q.pop();
-        return top;
+    int top() {  // O(1)
+        return top_;
     }
 
     // Return whether the stack is empty.
     bool empty() {  // O(1)
-        return q.empty();
+        return q_.empty();
     }
 };
+
