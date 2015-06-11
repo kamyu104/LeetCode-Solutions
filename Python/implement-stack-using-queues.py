7d960b8ee32e97db03373b74386e142d6e60afee
kamyu <kamyu104@users.noreply.github.com>
2015-06-11 22:12:00 +0800

    Update implement-stack-using-queues.py

--- Python/implement-stack-using-queues.py
+++ Python/implement-stack-using-queues.py
@@ -1,5 +1,5 @@
 # Time: push: O(n), pop: O(1), top: O(1)
-# Space: O(1)
+# Space: O(n)
 #
 # Implement the following operations of a stack using queues.
 #
@@ -41,24 +41,52 @@ class Queue:
 class Stack:
     # initialize your data structure here.
     def __init__(self):
-        self.q = Queue()
+        self.q_ = Queue()
 
     # @param x, an integer
     # @return nothing
     def push(self, x):
-        q = self.q
-        q.push(x)
-        for _ in xrange(q.size() - 1):
-            q.push(q.pop())
+        self.q_.push(x)
+        for _ in xrange(self.q_.size() - 1):
+            self.q_.push(self.q_.pop())
 
     # @return nothing
     def pop(self):
-        self.q.pop()
+        self.q_.pop()
 
     # @return an integer
     def top(self):
-        return self.q.peek()
+        return self.q_.peek()
 
     # @return an boolean
     def empty(self):
-        return self.q.empty()
+        return self.q_.empty()
+
+# Time: push: O(1), pop: O(n), top: O(1)
+# Space: O(n)
+class Stack2:
+    # initialize your data structure here.
+    def __init__(self):
+        self.q_ = Queue()
+        self.top_ = None
+
+    # @param x, an integer
+    # @return nothing
+    def push(self, x):
+        self.q_.push(x)
+        self.top_ = x
+
+    # @return nothing
+    def pop(self):
+        for _ in xrange(self.q_.size() - 1):
+            self.top_ = self.q_.pop()
+            self.q_.push(self.top_)
+        self.q_.pop()
+
+    # @return an integer
+    def top(self):
+        return self.top_
+
+    # @return an boolean
+    def empty(self):
+        return self.q_.empty()
