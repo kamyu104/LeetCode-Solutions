f9014699ba4fd2d8b8ebe384712ea03c0aa3d419
kamyu <kamyu104@users.noreply.github.com>
2015-06-29 09:46:42 +0800

    Update majority-element-ii.py

--- Python/majority-element-ii.py
+++ Python/majority-element-ii.py
@@ -2,7 +2,7 @@
 # Space: O(1)
 #
 # Given an integer array of size n, 
-# find all elements that appear more than ⌊ n/3 ⌋ times. 
+# find all elements that appear more than [n/3] times. 
 # The algorithm should run in linear time and in O(1) space.
 #
 
