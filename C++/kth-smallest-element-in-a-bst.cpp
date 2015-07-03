c78ac6447011103a3b949a19e79a706d4406300a
kamyu <kamyu104@users.noreply.github.com>
2015-07-03 16:30:31 +0800

    Update kth-smallest-element-in-a-bst.cpp

--- C++/kth-smallest-element-in-a-bst.cpp
+++ C++/kth-smallest-element-in-a-bst.cpp
@@ -1,5 +1,5 @@
 // Time:  O(min(h, k))
-// Space: O(h)
+// Space: O(min(h, k))
 
 /**
  * Definition for a binary tree node.
@@ -10,7 +10,37 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
 class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        deque<TreeNode *> s;
+        TreeNode *cur = root;
+        int rank = 0;
+        while (!s.empty() || cur) {
+            if (cur) {
+                s.emplace_back(cur);
+                if (s.size() > k) {
+                    s.pop_front();
+                }
+                cur = cur->left;
+            } else {
+                cur = s.back();
+                s.pop_back();
+                if (++rank == k) {
+                    return cur->val;
+                }
+                cur = cur->right;
+            }
+        }
+
+        return INT_MIN;
+    }
+};
+ 
+// Time:  O(min(h, k))
+// Space: O(h)
+class Solution2 {
 public:
     int kthSmallest(TreeNode* root, int k) {
         stack<TreeNode *> s;
