f1559d8e7c5335bc6e18a003358fb84f90c8c2e6
kamyu <kamyu104@users.noreply.github.com>
2015-07-07 03:00:01 +0800

    Create power-of-two.py

---
+++ Python/power-of-two.py
@@ -0,0 +1,11 @@
+# Time:  O(1)
+# Space: O(1)
+#
+# Given an integer, write a function to determine if it is a power of two.
+#
+
+class Solution:
+    # @param {integer} n
+    # @return {boolean}
+    def isPowerOfTwo(self, n):
+        return n > 0 and (n & (n - 1)) == 0
