f5fa877a16ca1bdcfcfd042a9dc17428260d2f28
kamyu <kamyu104@users.noreply.github.com>
2015-07-10 11:47:44 +0800

    Create palindrome-linked-list.cpp

---
+++ C++/palindrome-linked-list.cpp
@@ -0,0 +1,43 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        // Reverse the first half list.
+        ListNode *reverse = nullptr, *fast = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            auto head_next = head->next;
+            head->next = reverse;
+            reverse = head;
+            head = head_next;
+        }
+
+        // If the number of the nodes is odd,
+        // set the head of the tail list to the next of the median node.
+        ListNode *tail = fast ? head->next : head;
+
+        // Compare the reversed first half list with the second half list.
+        // And restore the reversed first half list.
+        bool is_palindrome = true;
+        while (reverse) {
+            is_palindrome = is_palindrome && reverse->val == tail->val;
+            auto reverse_next = reverse->next;
+            reverse->next = head;
+            head = reverse;
+            reverse = reverse_next;
+            tail = tail->next;
+        }
+            
+        return is_palindrome;   
+    }
+};
