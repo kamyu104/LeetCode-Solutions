e2a2ccd410dfcf4f8baff970a12c569b95aa497f
kamyu <kamyu104@users.noreply.github.com>
2015-07-13 09:51:49 +0800

    Create lowest-common-ancestor-of-a-binary-tree.cpp

---
+++ C++/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -0,0 +1,29 @@
+// Time:  O(h)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) {
+            return root;
+        }
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+         // 1. If the current subtree contains both p and q,
+         //    then result is their LCA.
+         // 2. If only one of them is in that subtree,
+         //    the result is that one of them.
+         // 3. If neither of them is in that subtree,
+         //    the result is the node of that subtree.
+        return left ? (right ? root : left) : right;
+    }
+};
