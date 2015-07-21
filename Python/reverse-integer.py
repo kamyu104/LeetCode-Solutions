857e80f86ef6c5ed541e9f3781d947b7143deacb
kamyu <kamyu104@users.noreply.github.com>
2015-07-21 13:31:49 +0800

    Update reverse-integer.py

--- Python/reverse-integer.py
+++ Python/reverse-integer.py
@@ -28,7 +28,7 @@ class Solution:
             while x:
                 ans = ans * 10 + x % 10
                 x /= 10
-            return ans
+            return ans if ans <= 2147483647 else 0  # Handle overflow.
         else:
             return -self.reverse(-x)
         
