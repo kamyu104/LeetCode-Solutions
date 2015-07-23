463c34d3e69b64538d41fbc5f70bec3c0dcd2040
kamyu <kamyu104@users.noreply.github.com>
2015-07-23 09:13:25 +0800

    Create search-a-2d-matrix-ii.py

---
+++ Python/search-a-2d-matrix-ii.py
@@ -0,0 +1,47 @@
+# Time:  O(m + n)
+# Space: O(1)
+#
+# Write an efficient algorithm that searches for a value in an m x n matrix.
+# This matrix has the following properties:
+#
+# Integers in each row are sorted in ascending from left to right.
+# Integers in each column are sorted in ascending from top to bottom.
+# For example,
+#
+# Consider the following matrix:
+#
+# [
+#   [1,   4,  7, 11, 15],
+#   [2,   5,  8, 12, 19],
+#   [3,   6,  9, 16, 22],
+#   [10, 13, 14, 17, 24],
+#   [18, 21, 23, 26, 30]
+# ]
+# Given target = 5, return true.
+#
+# Given target = 20, return false.
+#
+
+class Solution:
+    # @param {integer[][]} matrix
+    # @param {integer} target
+    # @return {boolean}
+    def searchMatrix(self, matrix, target):
+        m = len(matrix)
+        if m == 0:
+            return False
+        
+        n = len(matrix[0])
+        if n == 0:
+            return False
+            
+        count, i, j = 0, 0, n - 1
+        while i < m and j >= 0:
+            if matrix[i][j] == target:
+                return True
+            elif matrix[i][j] > target:
+                j -= 1
+            else:
+                i += 1
+                
+        return False
