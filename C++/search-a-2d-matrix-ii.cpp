7f8afbdc7c423cd9d5d76c7ea1fdb602c5b540f4
kamyu <kamyu104@users.noreply.github.com>
2015-07-23 09:02:23 +0800

    Create search-a-2d-matrix-ii.cpp

---
+++ C++/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,30 @@
+// Time:  O(m + n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        const int m = matrix.size();
+        if (m == 0) {
+            return 0;
+        }
+        const int n = matrix[0].size();
+        if  (n == 0) {
+            return 0;
+        }
+        int count = 0;
+
+        int i = 0, j = n - 1;
+        while (i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                --j;
+            } else {
+                ++i;
+            }
+        }
+
+        return false;
+    }
+};
