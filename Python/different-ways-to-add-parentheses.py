06f95680e227e3b9512d8d9cbdbe0cc67ec7eda3
kamyu <kamyu104@users.noreply.github.com>
2015-07-28 00:10:44 +0800

    Update different-ways-to-add-parentheses.py

--- Python/different-ways-to-add-parentheses.py
+++ Python/different-ways-to-add-parentheses.py
@@ -62,7 +62,7 @@ class Solution2:
                 return lookup[left][right]
             result = []
             for i in xrange(left, right):
-                if input[i] in "+-*":
+                if input[i] in ops:
                     for x in diffWaysToComputeRecu(left, i):
                         for y in diffWaysToComputeRecu(i + 1, right):
                             result.append(ops[input[i]](x, y))
