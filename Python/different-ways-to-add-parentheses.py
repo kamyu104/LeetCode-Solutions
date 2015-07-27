19931a8137881a8d44aff1dc710f25664debd6df
kamyu <kamyu104@users.noreply.github.com>
2015-07-27 23:02:43 +0800

    Create different-ways-to-add-parentheses.py

---
+++ Python/different-ways-to-add-parentheses.py
@@ -0,0 +1,48 @@
+# Time:  O(n * 4^n / n^(3/2)) ~= n * (Catalan numbers) = n * (C(2n, n) - C(2n, n - 1))
+# Space: O(n^2 * 4^n / n^(3/2))
+#
+# Given a string of numbers and operators, return all possible
+# results from computing all the different possible ways to
+# group numbers and operators. The valid operators are +, - and *.
+#
+#
+# Example 1
+# Input: "2-1-1".
+#
+# ((2-1)-1) = 0
+# (2-(1-1)) = 2
+# Output: [0, 2]
+#
+#
+# Example 2
+# Input: "2*3-4*5"
+#
+# (2*(3-(4*5))) = -34
+# ((2*3)-(4*5)) = -14
+# ((2*(3-4))*5) = -10
+# (2*((3-4)*5)) = -10
+# (((2*3)-4)*5) = 10
+# Output: [-34, -14, -10, -10, 10]
+#
+
+class Solution:
+    # @param {string} input
+    # @return {integer[]}
+    def diffWaysToCompute(self, input):
+        tokens = re.split('(\D)', input)
+        nums = map(int, tokens[::2])
+        ops = map({'+': operator.add, '-': operator.sub, '*': operator.mul}.get, tokens[1::2])
+        lookup = [[None for _ in xrange(len(nums))] for _ in xrange(len(nums))]
+        
+        def diffWaysToComputeRecu(left, right):
+            if left == right:
+                return [nums[left]]
+            if lookup[left][right]:
+                return lookup[left][right]
+            lookup[left][right] = [ops[i](x, y)
+                                   for i in xrange(left, right)
+                                   for x in diffWaysToComputeRecu(left, i)
+                                   for y in diffWaysToComputeRecu(i + 1, right)]
+            return lookup[left][right]
+            
+        return diffWaysToComputeRecu(0, len(nums) - 1)
