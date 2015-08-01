5aba1f152dfed2024c8a1840ce7fa11dc7953aeb
kamyu <kamyu104@users.noreply.github.com>
2015-08-01 17:49:16 +0800

    Update valid-anagram.cpp

--- C++/valid-anagram.cpp
+++ C++/valid-anagram.cpp
@@ -24,3 +24,19 @@ public:
         return true;
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(n)
+class Solution2 {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+        
+        return s == t;
+    }
+};
