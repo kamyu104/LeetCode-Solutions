18975d4805089d847e88e6b4be11e61b6417acff
kamyu <kamyu104@users.noreply.github.com>
2015-08-06 03:29:05 +0800

    Create count-univalue-subtrees.cpp

---
+++ C++/count-univalue-subtrees.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int countUnivalSubtrees(TreeNode* root) {
+        int count = 0;
+        isUnivalSubtrees(root, &count);
+        return count;
+    }
+    
+    bool isUnivalSubtrees(TreeNode* root, int *count) {
+        if (root == nullptr) {
+            return true;
+        }
+        bool left = isUnivalSubtrees(root->left, count);
+        bool right = isUnivalSubtrees(root->right, count);
+        if (isSame(root, root->left, left) &&
+            isSame(root, root->right, right)) {
+                ++(*count);
+                return true;
+            }
+        return false;
+    }
+    
+    bool isSame(TreeNode* root, TreeNode* child, bool isUni) {
+        return child == nullptr || (isUni && root->val == child->val);
+    }
+};
