2683d8049dfab3edf715b84418c2cd6c0b10552b
kamyu <kamyu104@users.noreply.github.com>
2015-08-06 03:34:53 +0800

    Create shortest-word-distance-iii.cpp

---
+++ C++/shortest-word-distance-iii.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int shortestWordDistance(vector<string>& words, string word1, string word2) {
+        int dist = INT_MAX;
+        for (int i = 0, index1 = -1, index2 = -1; i < words.size(); ++i) {
+            if (words[i] == word1) {
+                if (index1 != -1) {
+                    dist = min(dist, abs(index1 - i));
+                }
+                index1 = i;
+            } else if (words[i] == word2) {
+                index2 = i;
+            }
+            if (index1 != -1 && index2 != -1) {
+                dist = min(dist, abs(index1 - index2));
+            }
+        }
+        return dist;
+    }
+};
