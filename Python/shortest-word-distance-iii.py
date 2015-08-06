829730eb8e0048831c5c17971352011d45e4d760
kamyu <kamyu104@users.noreply.github.com>
2015-08-06 21:39:13 +0800

    Create shortest-word-distance-iii.py

---
+++ Python/shortest-word-distance-iii.py
@@ -0,0 +1,24 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution:
+    # @param {string[]} words
+    # @param {string} word1
+    # @param {string} word2
+    # @return {integer}
+    def shortestWordDistance(self, words, word1, word2):
+        dist = float("inf")
+        i, index1, index2 = 0, None, None
+        while i < len(words):
+            if words[i] == word1:
+                if index1 is not None:
+                    dist = min(dist, abs(index1 - i))
+                index1 = i
+            elif words[i] == word2:
+                index2 = i
+
+            if index1 is not None and index2 is not None:
+                dist = min(dist, abs(index1 - index2))
+            i += 1
+
+        return dist
