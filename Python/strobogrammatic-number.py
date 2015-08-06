be4c202c9ecd1b04ac72325fefce95b01ccffb7e
kamyu <kamyu104@users.noreply.github.com>
2015-08-06 22:50:04 +0800

    Update strobogrammatic-number.py

--- Python/strobogrammatic-number.py
+++ Python/strobogrammatic-number.py
@@ -9,7 +9,7 @@ class Solution:
     def isStrobogrammatic(self, num):
         n = len(num)
         for i in xrange((n+1) / 2):
-            if num[n-1-i] not in self.lookup or\
+            if num[n-1-i] not in self.lookup or \
                num[i] != self.lookup[num[n-1-i]]:
                 return False
             i += 1
