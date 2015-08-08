4c80ab856a5b61242aaaa7c8421ca734a353bd11
kamyu <kamyu104@users.noreply.github.com>
2015-08-08 16:05:00 +0800

    Create meeting-rooms-ii.py

---
+++ Python/meeting-rooms-ii.py
@@ -0,0 +1,34 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# Definition for an interval.
+# class Interval:
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution:
+    # @param {Interval[]} intervals
+    # @return {integer}
+    def minMeetingRooms(self, intervals):
+        starts, ends = [], []
+        for i in intervals:
+            starts.append(i.start)
+            ends.append(i.end)
+
+        starts.sort()
+        ends.sort()
+
+        s, e = 0, 0
+        min_rooms, cnt_rooms = 0, 0
+        while s < len(starts):
+            if starts[s] < ends[e]:
+                cnt_rooms += 1  # Acquire a room.
+                # Update the min number of rooms.
+                min_rooms = max(min_rooms, cnt_rooms)
+                s += 1
+            else:
+                cnt_rooms -= 1  # Release a room.
+                e += 1
+
+        return min_rooms
