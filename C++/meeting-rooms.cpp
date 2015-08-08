a4176a23e3ccb25c93be2d0709d2a2ce64ee6249
kamyu <kamyu104@users.noreply.github.com>
2015-08-08 16:01:06 +0800

    Create meeting-rooms.cpp

---
+++ C++/meeting-rooms.cpp
@@ -0,0 +1,25 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+/**
+ * Definition for an interval.
+ * struct Interval {
+ *     int start;
+ *     int end;
+ *     Interval() : start(0), end(0) {}
+ *     Interval(int s, int e) : start(s), end(e) {}
+ * };
+ */
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+        sort(intervals.begin(), intervals.end(),
+             [](const Interval& x, const Interval& y) { return x.start < y.start; });
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start < intervals[i-1].end) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
