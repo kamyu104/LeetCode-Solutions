e42648375c420506b746a3dfa8ccab4ac135d900
kamyu <kamyu104@users.noreply.github.com>
2015-08-11 19:17:17 +0800

    Create factor-combinations.py

---
+++ Python/factor-combinations.py
@@ -0,0 +1,23 @@
+# Time:  O(nlogn)
+# Space: O(logn)
+
+class Solution:
+    # @param {integer} n
+    # @return {integer[][]}
+    def getFactors(self, n):
+        result = []
+        factors = []
+        self.getResult(n, result, factors)
+        return result
+
+    def getResult(self, n, result, factors):
+        i = 2 if not factors else factors[-1]
+        while i <= n / i:
+            if n % i == 0:
+                factors.append(i);
+                factors.append(n / i);
+                result.append(list(factors));
+                factors.pop();
+                self.getResult(n / i, result, factors);
+                factors.pop()
+            i += 1
