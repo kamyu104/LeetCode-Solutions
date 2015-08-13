12df29858a595c0023a55c43fe6507425c9202be
kamyu <kamyu104@users.noreply.github.com>
2015-08-13 21:53:15 +0800

    Create verify-preorder-sequence-in-binary-search-tree.cpp

---
+++ C++/verify-preorder-sequence-in-binary-search-tree.cpp
@@ -0,0 +1,40 @@
+// Time:  O(n)
+// Soace: O(1)
+
+class Solution {
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        int low = INT_MIN, i = -1;
+        for (auto& p : preorder) {
+            if (p < low) {
+                return false;
+            }
+            while (i >= 0 && p > preorder[i]) {
+                low = preorder[i--];
+            }
+            preorder[++i] = p;
+        }
+        return true;
+    }
+};
+
+// Time:  O(n)
+// Soace: O(n)
+class Solution2 {
+public:
+    bool verifyPreorder(vector<int>& preorder) {
+        int low = INT_MIN, i = -1;
+        stack<int> path;
+        for (auto& p : preorder) {
+            if (p < low) {
+                return false;
+            }
+            while (!path.empty() && p > path.top()) {
+                low = path.top();
+                path.pop();
+            }
+            path.emplace(p);
+        }
+        return true;
+    }
+};
