fcf991e2d5d54c1951c9d3fd43eefa1d3aedc1d7
kamyu <kamyu104@users.noreply.github.com>
2015-08-13 22:05:02 +0800

    Update verify-preorder-sequence-in-binary-search-tree.py

--- Python/verify-preorder-sequence-in-binary-search-tree.py
+++ Python/verify-preorder-sequence-in-binary-search-tree.py
@@ -5,7 +5,7 @@ class Solution:
     # @param {integer[]} preorder
     # @return {boolean}
     def verifyPreorder(self, preorder):
-        low, i = float("-inf"), -1;
+        low, i = float("-inf"), -1
         for p in preorder:
             if p < low:
                 return False
@@ -14,4 +14,21 @@ class Solution:
                 i -= 1
             i += 1
             preorder[i] = p
+        return True
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2:
+    # @param {integer[]} preorder
+    # @return {boolean}
+    def verifyPreorder(self, preorder):
+        low = float("-inf")
+        path = []
+        for p in preorder:
+            if p < low:
+                return False
+            while path and p > path[-1]:
+                low = path[-1]
+                path.pop()
+            path.append(p)
         return True
