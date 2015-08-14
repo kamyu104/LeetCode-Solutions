38f4e8134654921fb21d4940a1508be9cf7631e5
kamyu <kamyu104@users.noreply.github.com>
2015-08-14 19:49:06 +0800

    Create paint-house.py

---
+++ Python/paint-house.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution:
+    # @param {integer[][]} costs
+    # @return {integer}
+    def minCost(self, costs):
+        if not costs:
+            return 0
+        
+        min_cost = [costs[0], [0, 0, 0]]
+        
+        n = len(costs)
+        for i in xrange(1, n):
+            min_cost[i % 2][0] = costs[i][0] + \
+                                 min(min_cost[(i - 1) % 2][1], min_cost[(i - 1) % 2][2])
+            min_cost[i % 2][1] = costs[i][1] + \
+                                 min(min_cost[(i - 1) % 2][0], min_cost[(i - 1) % 2][2])
+            min_cost[i % 2][2] = costs[i][2] + \
+                                 min(min_cost[(i - 1) % 2][0], min_cost[(i - 1) % 2][1])
+
+        return min(min_cost[(n - 1) % 2][0], min_cost[(n - 1) % 2][1], min_cost[(n - 1) % 2][2])
