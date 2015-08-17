8818a7a7659c8e8697c54d42c6f827bd4751ce87
kamyu <kamyu104@users.noreply.github.com>
2015-08-17 23:19:21 +0800

    Update reverse-bits.py

--- Python/reverse-bits.py
+++ Python/reverse-bits.py
@@ -3,15 +3,12 @@
 #
 # Reverse bits of a given 32 bits unsigned integer.
 #
-# For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+# For example, given input 43261596 (represented in binary as
+# 00000010100101000001111010011100), return 964176192 (represented in binary
+# as 00111001011110000010100101000000).
 #
 # Follow up:
 # If this function is called many times, how would you optimize it?
-# 
-# Related problem: Reverse Integer
-#
-# Credits:
-# Special thanks to @ts for adding this problem and creating all test cases.
 #
 
 class Solution:
