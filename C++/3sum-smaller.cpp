1511ea186fe9fa023c8fa67b39c77a0903c4a6e4
kamyu <kamyu104@users.noreply.github.com>
2015-08-17 21:40:17 +0800

    Create 3sum-smaller.cpp

---
+++ C++/3sum-smaller.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+class Solution {
+public:
+    int threeSumSmaller(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        const int n = nums.size();
+
+        int count = 0;
+        for (int k = 2; k < n; ++k) {
+            int i = 0, j = k - 1;
+            while (i < j) {  // Two Pointers, linear time.
+                if (nums[i] + nums[j] >= target - nums[k]) {
+                    --j;
+                } else {
+                    count += j - i;
+                    ++i;
+                }
+            }
+        }
+
+        return count;
+    }
+};
