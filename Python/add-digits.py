ae914dce9f4d74ce958fec74fe09df48529e85ae
kamyu <kamyu104@users.noreply.github.com>
2015-08-17 09:02:09 +0800

    Update add-digits.py

--- Python/add-digits.py
+++ Python/add-digits.py
@@ -1,6 +1,23 @@
 # Time:  O(1)
 # Space: O(1)
-
+#
+# Given a non-negative integer num, repeatedly add
+# all its digits until the result has only one digit.
+#
+# For example:
+#
+# Given num = 38, the process is like: 3 + 8 = 11,
+# 1 + 1 = 2. Since 2 has only one digit, return it.
+#
+# Follow up:
+# Could you do it without any loop/recursion in O(1)
+# runtime?
+#
+# Hint:
+#
+# A naive implementation of the above process is trivial.
+# Could you come up with other methods? 
+#
 class Solution:
     # @param {integer} num
     # @return {integer}
