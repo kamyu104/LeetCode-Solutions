ff777ce3c43d904e7aecde1d4dd325a405806d6c
kamyu <kamyu104@users.noreply.github.com>
2015-08-17 08:59:37 +0800

    Update binary-tree-paths.py

--- Python/binary-tree-paths.py
+++ Python/binary-tree-paths.py
@@ -1,6 +1,20 @@
 # Time:  O(n * h)
 # Space: O(h)
 #
+# Given a binary tree, return all root-to-leaf paths.
+#
+# For example, given the following binary tree:
+#
+#   1
+#  /   \
+# 2     3
+#  \
+#   5
+# All root-to-leaf paths are:
+#
+# ["1->2->5", "1->3"]
+#
+#
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, x):
