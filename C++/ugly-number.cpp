41b5ce9f5dca4b818c3d5542124c45795e634e20
kamyu <kamyu104@users.noreply.github.com>
2015-08-19 09:35:33 +0800

    Create ugly-number.cpp

---
+++ C++/ugly-number.cpp
@@ -0,0 +1,17 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isUgly(int num) {
+        if (num == 0) {
+            return false;
+        }
+        for (const auto& i : {2, 3, 5}) {
+            while (num % i == 0) {
+                num /= i;
+            }
+        }
+        return num == 1;
+    }
+};
