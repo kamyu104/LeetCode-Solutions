92dfd9ee7bd859b7410526cbe3dfc53e42e457e1
kamyu <kamyu104@users.noreply.github.com>
2015-08-20 23:34:13 +0800

    Update paint-house-ii.cpp

--- C++/paint-house-ii.cpp
+++ C++/paint-house-ii.cpp
@@ -13,17 +13,17 @@ public:
         const int n = costs.size();
         const int k = costs[0].size();
         for (int i = 1; i < n; ++i) {
-            int min_1st = numeric_limits<int>::max(), min_2nd = numeric_limits<int>::max();
+            int smallest = numeric_limits<int>::max(), second_smallest = numeric_limits<int>::max();
             for (int j = 0; j < k; ++j) {
-                if (min_1st >= min_cost[(i - 1) % 2][j]) {
-                    min_2nd = min_1st;
-                    min_1st = min_cost[(i - 1) % 2][j];
-                } else {
-                    min_2nd = min(min_2nd, min_cost[(i - 1) % 2][j]);
+                if (min_cost[(i - 1) % 2][j] < smallest) {
+                    second_smallest = smallest;
+                    smallest = min_cost[(i - 1) % 2][j];
+                } else if (min_cost[(i - 1) % 2][j] < second_smallest) {
+                    second_smallest = min_cost[(i - 1) % 2][j];
                 }
             }
             for (int j = 0; j < k; ++j) {
-                const int min_j = (min_cost[(i - 1) % 2][j] != min_1st) ? min_1st : min_2nd;
+                const int min_j = (min_cost[(i - 1) % 2][j] != smallest) ? smallest : second_smallest;
                 min_cost[i % 2][j] = costs[i][j] + min_j;
             }
         }
