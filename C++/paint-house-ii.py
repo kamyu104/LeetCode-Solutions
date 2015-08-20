386cae1c97aed2ad36d34a49621cce3e836455aa
kamyu <kamyu104@users.noreply.github.com>
2015-08-20 23:27:02 +0800

    Create paint-house-ii.py

---
+++ C++/paint-house-ii.py
@@ -0,0 +1,33 @@
+// Time:  O(n * k)
+// Space: O(k)
+
+class Solution {
+public:
+    int minCostII(vector<vector<int>>& costs) {
+        if (costs.empty()) {
+            return 0;
+        }
+        
+        vector<vector<int>> min_cost(2, costs[0]);
+
+        const int n = costs.size();
+        const int k = costs[0].size();
+        for (int i = 1; i < n; ++i) {
+            int min_1st = INT_MAX, min_2nd = INT_MAX;
+            for (int j = 0; j < k; ++j) {
+                if (min_1st >= min_cost[(i - 1) % 2][j]) {
+                    min_2nd = min_1st;
+                    min_1st = min_cost[(i - 1) % 2][j];
+                } else {
+                    min_2nd = min(min_2nd, min_cost[(i - 1) % 2][j]);
+                }
+            }
+            for (int j = 0; j < k; ++j) {
+                const int min_j = (min_cost[(i - 1) % 2][j] != min_1st) ? min_1st : min_2nd;
+                min_cost[i % 2][j] = costs[i][j] + min_j;
+            }
+        }
+
+        return *min_element(min_cost[(n - 1) % 2].begin(), min_cost[(n - 1) % 2].end());
+    }
+};
