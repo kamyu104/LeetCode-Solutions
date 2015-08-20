31b12b6ef699c5c6de0f4b36e03037fb3679c56b
kamyu <kamyu104@users.noreply.github.com>
2015-08-20 23:53:22 +0800

    Update paint-house-ii.py

--- Python/paint-house-ii.py
+++ Python/paint-house-ii.py
@@ -1,7 +1,21 @@
 # Time:  O(n * k)
 # Space: O(k)
 
-class Solution(object):
+class Solution2(object):
+    def minCostII(self, costs):
+        """
+        :type costs: List[List[int]]
+        :rtype: int
+        """
+        return min(reduce(self.combine, costs)) if costs else 0
+
+    def combine(self, tmp, house):
+        smallest, k, i = min(tmp), len(tmp), tmp.index(min(tmp))
+        tmp, tmp[i] = [smallest] * k, min(tmp[:i] + tmp[i+1:])
+        return map(sum, zip(house, tmp))
+
+
+class Solution2(object):
     def minCostII(self, costs):
         """
         :type costs: List[List[int]]
