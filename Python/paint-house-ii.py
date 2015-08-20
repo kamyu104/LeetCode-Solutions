2b0665617f3d66fe7fe91fd256e8034776484b20
kamyu <kamyu104@users.noreply.github.com>
2015-08-20 23:35:41 +0800

    Update paint-house-ii.py

--- Python/paint-house-ii.py
+++ Python/paint-house-ii.py
@@ -14,14 +14,14 @@ class Solution(object):
         k = len(costs[0])
         min_cost = [costs[0], [0] * k]
         for i in xrange(1, n):
-            min_1st, min_2nd = float("inf"), float("inf")
+            smallest, second_smallest = float("inf"), float("inf")
             for j in xrange(k):
-                if min_1st >= min_cost[(i - 1) % 2][j]:
-                    min_1st, min_2nd = min_cost[(i - 1) % 2][j], min_1st
-                elif min_2nd >= min_cost[(i - 1) % 2][j]:
-                    min_2nd = min_cost[(i - 1) % 2][j]
+                if min_cost[(i - 1) % 2][j] < smallest:
+                    smallest, second_smallest = min_cost[(i - 1) % 2][j], smallest
+                elif min_cost[(i - 1) % 2][j] < second_smallest:
+                    second_smallest = min_cost[(i - 1) % 2][j]
             for j in xrange(k):
-                min_j = min_1st if min_cost[(i - 1) % 2][j] != min_1st else min_2nd
+                min_j = smallest if min_cost[(i - 1) % 2][j] != smallest else second_smallest
                 min_cost[i % 2][j] = costs[i][j] + min_j
 
         return min(min_cost[(n - 1) % 2])
