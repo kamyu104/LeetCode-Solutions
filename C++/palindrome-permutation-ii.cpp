1bd01b7d63d2f879fbe48ee326aba3e0804639f3
kamyu <kamyu104@users.noreply.github.com>
2015-08-23 16:20:54 +0800

    Update palindrome-permutation-ii.cpp

--- C++/palindrome-permutation-ii.cpp
+++ C++/palindrome-permutation-ii.cpp
@@ -17,6 +17,40 @@ public:
         for (const auto& kvp : cnt) {
             if (kvp.second % 2) {
                 if (mid.empty()) {
+                    mid.push_back(kvp.first);
+                } else {  // The count of the middle char is at most one. 
+                    return {};
+                }
+            }
+            chars.append(kvp.second / 2, kvp.first);
+        }
+
+        vector<string> result;
+        sort(chars.begin(), chars.end());
+        do {
+            string reverse_chars(chars.crbegin(), chars.crend());
+            result.emplace_back(chars + mid + reverse_chars);
+        } while (next_permutation(chars.begin(), chars.end()));
+        return result;
+    }
+};
+
+class Solution2 {
+public:
+    vector<string> generatePalindromes(string s) {
+        if (s.empty()) {
+            return {};
+        }
+
+        unordered_map<char, int> cnt;
+        for (const auto& c : s) {
+            ++cnt[c];
+        }
+
+        string mid, chars;
+        for (const auto& kvp : cnt) {
+            if (kvp.second % 2) {
+                if (mid.empty()) {
                     mid.append(1, kvp.first);
                 } else {  // The count of the middle char is at most one. 
                     return {};
@@ -55,39 +89,5 @@ public:
                 (*used)[i] = false;
             }
         }
-    }
-};
-
-class Solution2 {
-public:
-    vector<string> generatePalindromes(string s) {
-        if (s.empty()) {
-            return {};
-        }
-
-        unordered_map<char, int> cnt;
-        for (const auto& c : s) {
-            ++cnt[c];
-        }
-
-        string mid, chars;
-        for (const auto& kvp : cnt) {
-            if (kvp.second % 2) {
-                if (mid.empty()) {
-                    mid.push_back(kvp.first);
-                } else {  // The count of the middle char is at most one. 
-                    return {};
-                }
-            }
-            chars.append(kvp.second / 2, kvp.first);
-        }
-
-        vector<string> result;
-        sort(chars.begin(), chars.end());
-        do {
-            string reverse_chars(chars.crbegin(), chars.crend());
-            result.emplace_back(chars + mid + reverse_chars);
-        } while (next_permutation(chars.begin(), chars.end()));
-        return result;
     }
 };
