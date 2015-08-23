3a2ba1904c4a5c2f396e4a3e073db0f802cc8024
kamyu <kamyu104@users.noreply.github.com>
2015-08-23 14:53:56 +0800

    Create palindrome-permutation-ii.py

---
+++ Python/palindrome-permutation-ii.py
@@ -0,0 +1,14 @@
+# Time:  O(n * n!)
+# Space: O(n)
+
+class Solution(object):
+    def generatePalindromes(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        cnt = collections.Counter(s)
+        mid = tuple(k for k, v in cnt.iteritems() if v % 2)
+        chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
+        return [''.join(half_palindrome + mid + half_palindrome[::-1]) \
+                for half_palindrome in set(itertools.permutations(chars))] if len(mid) < 2 else []
