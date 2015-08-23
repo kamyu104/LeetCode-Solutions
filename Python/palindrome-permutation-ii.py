f5d64483a2d459f3400c467b7f343b1148d3f3af
kamyu <kamyu104@users.noreply.github.com>
2015-08-23 16:41:45 +0800

    Update palindrome-permutation-ii.py

--- Python/palindrome-permutation-ii.py
+++ Python/palindrome-permutation-ii.py
@@ -10,24 +10,24 @@ class Solution(object):
         cnt = collections.Counter(s)
         mid = ''.join(k for k, v in cnt.iteritems() if v % 2)
         chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
-        return [half_palindrome + mid + half_palindrome[::-1] \
-                for half_palindrome in self.permuteUnique(chars)] if len(mid) < 2 else []
+        return self.permuteUnique(mid, chars) if len(mid) < 2 else []
     
-    def permuteUnique(self, nums):
+    def permuteUnique(self, mid, nums):
         result = []
         used = [False] * len(nums)
-        self.permuteRecu(result, used, [], nums)
+        self.permuteRecu(mid, result, used, [], nums)
         return result
     
-    def permuteRecu(self, result, used, cur, nums):
+    def permuteRecu(self, mid, result, used, cur, nums):
         if len(cur) == len(nums):
-            result.append(''.join(cur))
+            half_palindrome = ''.join(cur)
+            result.append(half_palindrome + mid + half_palindrome[::-1])
             return
         for i in xrange(len(nums)):
             if not used[i] and not (i > 0 and nums[i-1] == nums[i] and used[i-1]):
                 used[i] = True
                 cur.append(nums[i])
-                self.permuteRecu(result, used, cur, nums)
+                self.permuteRecu(mid, result, used, cur, nums)
                 cur.pop()
                 used[i] = False
 
