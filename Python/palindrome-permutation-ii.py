9568009681662946a2bda36afbe6942f8b0453f1
kamyu <kamyu104@users.noreply.github.com>
2015-08-23 15:25:11 +0800

    Update palindrome-permutation-ii.py

--- Python/palindrome-permutation-ii.py
+++ Python/palindrome-permutation-ii.py
@@ -8,6 +8,36 @@ class Solution(object):
         :rtype: List[str]
         """
         cnt = collections.Counter(s)
+        mid = [k for k, v in cnt.iteritems() if v % 2]
+        chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
+        return [''.join(half_palindrome + mid + half_palindrome[::-1]) \
+                 for half_palindrome in self.permuteUnique(chars)] if len(mid) < 2 else []
+    
+    def permuteUnique(self, nums):
+        result = []
+        used = [False] * len(nums)
+        self.permuteRecu(result, used, [], nums)
+        return result
+    
+    def permuteRecu(self, result, used, cur, nums):
+        if len(cur) == len(nums):
+            result.append(cur + [])
+            return
+        for i in xrange(len(nums)):
+            if not used[i] and not (i > 0 and nums[i-1] == nums[i] and used[i-1]):
+                used[i] = True
+                cur.append(nums[i])
+                self.permuteRecu(result, used, cur, nums)
+                cur.pop()
+                used[i] = False
+
+class Solution2(object):
+    def generatePalindromes(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        cnt = collections.Counter(s)
         mid = tuple(k for k, v in cnt.iteritems() if v % 2)
         chars = ''.join(k * (v / 2) for k, v in cnt.iteritems())
         return [''.join(half_palindrome + mid + half_palindrome[::-1]) \
