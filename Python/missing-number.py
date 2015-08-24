677dba773d0809fb2a6da3aade160e179fa4e1c6
kamyu <kamyu104@users.noreply.github.com>
2015-08-24 09:25:08 +0800

    Update missing-number.py

--- Python/missing-number.py
+++ Python/missing-number.py
@@ -1,5 +1,16 @@
 # Time:  O(n)
 # Space: O(1)
+#
+# Given an array containing n distinct numbers taken from
+# 0, 1, 2, ..., n, find the one that is missing from the array.
+#
+# For example,
+# Given nums = [0, 1, 3] return 2.
+#
+# Note:
+# Your algorithm should run in linear runtime complexity. 
+# Could you implement it using only constant extra space complexity?
+#
 
 class Solution(object):
     def missingNumber(self, nums):
