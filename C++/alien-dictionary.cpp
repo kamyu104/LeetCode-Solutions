5332491cda574afa1405478acf9769802b9c5949
kamyu <kamyu104@users.noreply.github.com>
2015-08-26 01:22:41 +0800

    Update alien-dictionary.cpp

--- C++/alien-dictionary.cpp
+++ C++/alien-dictionary.cpp
@@ -1,7 +1,70 @@
 // Time:  O(|V|+|E|) = O(26 + 26^2) = O(1)
 // Space: O(|V|+|E|) = O(26 + 26^2) = O(1)
 
+// BFS solution.
 class Solution {
+public:
+    string alienOrder(vector<string>& words) {
+        unordered_set<char> nodes;
+        unordered_map<char, unordered_set<char>> in_degree, out_degree;
+        queue<char> zero_in_degree_queue;
+        for (const auto& word : words) {
+            for (char c : word) {
+                nodes.emplace(c);
+            }
+        }
+        for (int i = 1; i < words.size(); ++i) {
+            findEdges(words[i - 1], words[i], &in_degree, &out_degree);
+        }
+        for (const auto& node : nodes) {
+            if (in_degree.find(node) == in_degree.end()) {
+                zero_in_degree_queue.emplace(node);
+            }
+        }
+
+        // BFS
+        string result;
+        while (!zero_in_degree_queue.empty()) {
+            const auto& precedence = zero_in_degree_queue.front();
+            zero_in_degree_queue.pop();
+            result.push_back(precedence);
+            
+            if (out_degree.find(precedence) != out_degree.end()) {
+                for (const auto& c : out_degree[precedence]) {
+                    in_degree[c].erase(precedence);
+                    if (in_degree[c].empty()) {
+                        zero_in_degree_queue.emplace(c);
+                    }
+                }
+                out_degree.erase(precedence);
+            }
+        }
+        
+        if (!out_degree.empty()) {
+            return "";
+        }
+        
+        return result;
+    }
+
+private:
+    // Construct the graph.
+    void findEdges(const string &word1, const string &word2,
+                   unordered_map<char, unordered_set<char>> *in_degree,
+                   unordered_map<char, unordered_set<char>> *out_degree) {
+        int len = min(word1.length(), word2.length());
+        for (int i = 0; i < len; ++i) {
+            if (word1[i] != word2[i]) {
+                (*in_degree)[word2[i]].emplace(word1[i]);
+                (*out_degree)[word1[i]].emplace(word2[i]);
+                break;
+            }
+        }
+    }
+};
+
+// DFS solution.
+class Solution2 {
 public:
     string alienOrder(vector<string>& words) {
         // Find ancestors of each node by DFS.
