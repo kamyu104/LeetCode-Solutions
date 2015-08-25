92efd51b15d244ab50590bee26d67addb4e462f2
kamyu <kamyu104@users.noreply.github.com>
2015-08-26 00:51:27 +0800

    Update alien-dictionary.py

--- Python/alien-dictionary.py
+++ Python/alien-dictionary.py
@@ -1,7 +1,61 @@
 # Time:  O(|V|+|E|) = O(26 + 26^2) = O(1)
 # Space: O(|V|+|E|) = O(26 + 26^2) = O(1)
 
+# BFS solution.
 class Solution(object):
+    def alienOrder(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        # Find ancestors of each node by BFS
+        result, zero_in_degree_queue, in_degree, out_degree = [], [], {}, {}
+        nodes = sets.Set()
+        for i in xrange(len(words)):
+            for c in words[i]:
+                nodes.add(c)
+        
+        for i in xrange(1, len(words)):
+            self.findEdges(words[i - 1], words[i], in_degree, out_degree)
+        
+        for node in nodes:
+            if node not in in_degree:
+                zero_in_degree_queue.append(node)
+        
+        while zero_in_degree_queue:
+            precedence = zero_in_degree_queue.pop()
+            result.append(precedence)
+            
+            if precedence in out_degree:
+                for course in out_degree[precedence]:
+                    in_degree[course].discard(precedence)
+                    if not in_degree[course]:
+                        zero_in_degree_queue.append(course)
+            
+                del out_degree[precedence]
+        
+        if out_degree:
+            return ""
+
+        return "".join(result)
+
+
+    # Construct the graph.
+    def findEdges(self, word1, word2, in_degree, out_degree):
+        str_len = min(len(word1), len(word2))
+        for i in xrange(str_len):
+            if word1[i] != word2[i]:
+                if word2[i] not in in_degree:
+                    in_degree[word2[i]] = sets.Set()
+                if word1[i] not in out_degree:
+                    out_degree[word1[i]] = sets.Set()
+                in_degree[word2[i]].add(word1[i])
+                out_degree[word1[i]].add(word2[i])
+                break
+
+
+# DFS solution.
+class Solution2(object):
     def alienOrder(self, words):
         """
         :type words: List[str]
