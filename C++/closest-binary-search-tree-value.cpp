44a889a23f3b3ea0012958f0f564becd19e9b926
kamyu <kamyu104@users.noreply.github.com>
2015-08-27 09:45:18 +0800

    Create closest-binary-search-tree-value.cpp

---
+++ C++/closest-binary-search-tree-value.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int closestValue(TreeNode* root, double target) {
+        double gap = numeric_limits<double>::max();
+        int closest = numeric_limits<int>::max();
+        
+        while (root) {
+            if (abs(static_cast<double>(root->val) - target) < gap) {
+                gap = abs(root->val - target);
+                closest = root->val;
+            }
+            if (root->val == target) {
+                return root->val;
+            } else if (root->val > target) {
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+        return closest;
+    }
+};
