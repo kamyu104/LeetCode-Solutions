5db6b4ab1431cccceb83ed90b20a7b531d7b97ed
kamyu <kamyu104@users.noreply.github.com>
2015-08-30 14:42:32 +0800

    Create encode-and-decode-strings.py

---
+++ Python/encode-and-decode-strings.py
@@ -0,0 +1,30 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Codec:
+
+    def encode(self, strs):
+        """Encodes a list of strings to a single string.
+        
+        :type strs: List[str]
+        :rtype: str
+        """
+        encoded_str = ""
+        for s in strs:
+            encoded_str += "%0*x" % (8, len(s)) + s
+        return encoded_str
+
+
+    def decode(self, s):
+        """Decodes a single string to a list of strings.
+        
+        :type s: str
+        :rtype: List[str]
+        """
+        i = 0
+        strs = []
+        while i < len(s):
+            l = int(s[i:i+8], 16)
+            strs.append(s[i+8:i+8+l])
+            i += 8+l 
+        return strs
