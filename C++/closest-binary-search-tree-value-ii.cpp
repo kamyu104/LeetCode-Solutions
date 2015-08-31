080d334acab33c5a6dd7e668edcd9f95b1961264
kamyu <kamyu104@users.noreply.github.com>
2015-08-31 22:04:38 +0800

    Create closest-binary-search-tree-value-ii.cpp

---
+++ C++/closest-binary-search-tree-value-ii.cpp
@@ -0,0 +1,71 @@
+// Time:  O(h + k)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> closestKValues(TreeNode* root, double target, int k) {
+        // The forward or backward iterator.
+        const auto backward = [](const vector<TreeNode*>& s) { return s.back()->left; };
+        const auto forward = [](const vector<TreeNode*>& s) { return s.back()->right; };
+        const auto dist = [target](const TreeNode* a, const TreeNode* b) { 
+                              return abs(a->val - target) < abs(b->val - target); 
+                          };
+
+        // Build the stack to the closest node.
+        vector<TreeNode*> s;
+        while (root) {
+            s.emplace_back(root);
+            root = target < root->val ? root->left : root->right;
+        }
+
+        // Get the stack to the next smaller node.
+        vector<TreeNode*> forward_stack(s.begin(), next(min_element(s.begin(), s.end(), dist)));
+        vector<TreeNode*> backward_stack(forward_stack);
+        nextNode(backward_stack, backward, forward);
+    
+        // Get the closest k values by advancing the iterators of the stacks.
+        vector<int> result;
+        for (int i = 0; i < k; ++i) {
+            if (backward_stack.empty() ||
+                !forward_stack.empty() && dist(forward_stack.back(), backward_stack.back())) {
+                result.emplace_back(forward_stack.back()->val);
+                nextNode(forward_stack, forward, backward);
+            } else if (forward_stack.empty() ||
+                       !forward_stack.empty() && !dist(forward_stack.back(), backward_stack.back())) {
+                result.emplace_back(backward_stack.back()->val);
+                nextNode(backward_stack, backward, forward);
+            }
+        }
+        return result;
+    }
+    
+    // Helper to make a stack to the next node.
+    void nextNode(vector<TreeNode*>& s, 
+                  const function<TreeNode*(const vector<TreeNode*>&)>& child1,
+                  const function<TreeNode*(const vector<TreeNode*>&)>& child2) {
+        if (!s.empty()) {
+            if (child2(s)) {
+                s.emplace_back(child2(s));
+                while (child1(s)) {
+                    s.emplace_back(child1(s));
+                }
+            } else {
+                auto child = s.back();
+                s.pop_back();
+                while (!s.empty() && child == child2(s)) {
+                    child = s.back();
+                    s.pop_back();
+                }
+            }
+        }
+    }
+};
