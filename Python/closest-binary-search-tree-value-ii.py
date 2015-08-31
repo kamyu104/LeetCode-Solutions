4b89cf404c5e21421daf79b889cbee91543d3b99
kamyu <kamyu104@users.noreply.github.com>
2015-08-31 10:13:34 +0800

    Update closest-binary-search-tree-value-ii.py

--- Python/closest-binary-search-tree-value-ii.py
+++ Python/closest-binary-search-tree-value-ii.py
@@ -67,71 +67,58 @@ class Solution2(object):
         :type k: int
         :rtype: List[int]
         """
-        # Build the stack to the closest node.
+        # Build the stack to the closet node.
         stack = []
-        node = self.closestValue(root, target, stack)
-        result = [node.val]
+        while root:
+            stack.append(root)
+            root = root.left if target < root.val else root.right
+        dist = lambda node: abs(node.val - target) if node else float("inf")
+        stack = stack[:stack.index(min(stack, key=dist))+1]
         
         # The forward or backward iterator.
         backward = lambda node: node.left
         forward = lambda node: node.right
-        smaller_it, larger_it = BSTIterator(stack, backward, forward), BSTIterator(stack, forward, backward)
+        smaller_it = self.BSTIterator(stack, backward, forward)
+        larger_it = self.BSTIterator(stack, forward, backward)
         smaller_node, larger_node = smaller_it.next(), larger_it.next()
 
         # Get the closest k values by advancing the iterators of the stacks.
+        result = [stack[-1].val]
         for _ in xrange(k - 1):
-            smaller = smaller_node.val if smaller_node else float("-inf")
-            larger = larger_node.val if larger_node else float("inf")
-            if abs(smaller - target) < abs(larger - target):
-                result.append(smaller)
+            if dist(smaller_node) < dist(larger_node):
+                result.append(smaller_node.val)
                 smaller_node = smaller_it.next()
             else:
-                result.append(larger)
+                result.append(larger_node.val)
                 larger_node = larger_it.next()
         return result
+    
+    class BSTIterator:
+        # @param root, a binary search tree's root node
+        def __init__(self, stack, child1, child2):
+            self.stack = list(stack)
+            self.cur = self.stack.pop()
+            self.child1 = child1
+            self.child2 = child2
+    
+        # @return an integer, the next node
+        def next(self):
+            node = None
+            if self.cur and self.child1(self.cur):
+                self.stack.append(self.cur)
+                node = self.child1(self.cur)
+                while self.child2(node):
+                    self.stack.append(node)
+                    node = self.child2(node)
+            elif self.stack:
+                prev = self.cur
+                node = self.stack.pop()
+                while node:
+                    if self.child2(node) is prev:
+                        break
+                    else:
+                        prev = node
+                        node = self.stack.pop() if self.stack else None
+            self.cur = node
+            return node
 
-    def closestValue(self, root, target, stack):
-        gap = float("inf")
-        closest = None
-        while root:
-            stack.append(root)
-            if abs(root.val - target) < gap:
-                gap = abs(root.val - target)
-                closest = root
-            if target == root.val:
-                break
-            else:
-                root = root.left if target < root.val else root.right
-
-        while stack and stack[-1] != closest:
-            stack.pop()
-        return closest
-
-class BSTIterator:
-    # @param root, a binary search tree's root node
-    def __init__(self, stack, child1, child2):
-        self.stack = list(stack)
-        self.cur = self.stack.pop()
-        self.child1 = child1
-        self.child2 = child2
-
-    # @return an integer, the next node
-    def next(self):
-        node = None
-        if self.cur and self.child1(self.cur):
-            self.stack.append(self.cur)
-            node = self.child1(self.cur)
-            while self.child2(node):
-                self.stack.append(node)
-                node = self.child2(node)
-        elif self.stack:
-            prev = self.cur
-            node = self.stack.pop()
-            while node:
-                if self.child2(node) is prev:
-                    break
-                else:
-                    prev = node
-                    node = self.stack.pop() if self.stack else None
-        self.cur = node
-        return node
