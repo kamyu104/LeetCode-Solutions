28525257864e8f554eec70d6366f282e36aacee8
kamyu <kamyu104@users.noreply.github.com>
2015-08-31 20:30:10 +0800

    Update integer-to-english-words.py

--- Python/integer-to-english-words.py
+++ Python/integer-to-english-words.py
@@ -21,7 +21,7 @@ class Solution(object):
 
         lookup = {0: "Zero", 1:"One", 2: "Two", 3: "Three", 4: "Four", \
                   5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine", \
-                  10 : "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", \
+                  10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", \
                   15: "Fifteen", 16: "Sixteen", 17: "Seventeen", 18: "Eighteen", 19: "Nineteen", \
                   20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty", 60: "Sixty", \
                   70: "Seventy", 80: "Eighty", 90: "Ninety"}
