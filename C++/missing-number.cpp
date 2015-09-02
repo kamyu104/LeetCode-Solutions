d34693eea72a988c91d2c08829a8240b1276a275
kamyu <kamyu104@users.noreply.github.com>
2015-09-02 22:26:17 +0800

    Update missing-number.cpp

--- C++/missing-number.cpp
+++ C++/missing-number.cpp
@@ -11,3 +11,15 @@ public:
         return num;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    int missingNumber(vector<int>& nums) {
+        vector<int> expected(nums.size()); 
+        iota(expected.begin(), expected.end(), 1);  // Costs extra space O(n)
+        return accumulate (nums.cbegin(), nums.cend(), 0, bit_xor<int>()) ^
+               accumulate (expected.cbegin(), expected.cend(), 0, bit_xor<int>());
+    }
+};
