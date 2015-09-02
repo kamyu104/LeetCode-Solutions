4d50525bcea36c7a2ed6799e704b0eb76f26d61b
kamyu <kamyu104@users.noreply.github.com>
2015-09-03 01:27:29 +0800

    Update power-of-two.py

--- Python/power-of-two.py
+++ Python/power-of-two.py
@@ -9,3 +9,9 @@ class Solution:
     # @return {boolean}
     def isPowerOfTwo(self, n):
         return n > 0 and (n & (n - 1)) == 0
+
+class Solution2:
+    # @param {integer} n
+    # @return {boolean}
+    def isPowerOfTwo(self, n):
+        return n > 0 and (n & ~-n) == 0
