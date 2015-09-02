d4f679a1f59692a209e70dcc12025a618ff76ad5
kamyu <kamyu104@users.noreply.github.com>
2015-09-03 01:17:07 +0800

    Update single-number-iii.cpp

--- C++/single-number-iii.cpp
+++ C++/single-number-iii.cpp
@@ -8,7 +8,7 @@ public:
         const auto x_xor_y = accumulate(nums.cbegin(), nums.cend(), 0, bit_xor<int>());
 
         // Get the last bit where 1 occurs by "x & ~(x - 1)"
-        // Becease -(x - 1) = ~(x - 1) + 1 <=> -x = ~(x - 1) 
+        // Because -(x - 1) = ~(x - 1) + 1 <=> -x = ~(x - 1) 
         // So we can also get the last bit where 1 occurs by "x & -x"
         const auto bit = x_xor_y & -x_xor_y;
 
