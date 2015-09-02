a9f5e0ba9d0676ccb2393a4e786860261d995586
kamyu <kamyu104@users.noreply.github.com>
2015-09-02 08:48:05 +0800

    Update integer-to-english-words.py

--- Python/integer-to-english-words.py
+++ Python/integer-to-english-words.py
@@ -9,6 +9,18 @@
 # 12345 -> "Twelve Thousand Three Hundred Forty Five"
 # 1234567 -> "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
 #
+# Hint:
+#
+# 1. Did you see a pattern in dividing the number into chunk of words?
+# For example, 123 and 123000.
+#
+# 2. Group the number by thousands (3 digits). You can write a helper
+# function that takes a number less than 1000 and convert just that chunk to words.
+#
+# 3. There are many edge cases. What are some good test cases?
+# Does your code work with input such as 0? Or 1000010?
+# (middle chunk is zero and should not be printed out)
+#
 
 class Solution(object):
     def numberToWords(self, num):
