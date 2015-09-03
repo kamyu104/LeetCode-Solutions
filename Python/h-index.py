82432d95f8dcad0c62a65db0b37c74523b224efe
kamyu <kamyu104@users.noreply.github.com>
2015-09-03 23:22:32 +0800

    Create h-index.py

---
+++ Python/h-index.py
@@ -0,0 +1,47 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+# Given an array of citations (each citation is a non-negative integer)
+# of a researcher, write a function to compute the researcher's h-index.
+#
+# According to the definition of h-index on Wikipedia: 
+# "A scientist has index h if h of his/her N papers have
+# at least h citations each, and the other N − h papers have
+# no more than h citations each."
+#
+# For example, given citations = [3, 0, 6, 1, 5], 
+# which means the researcher has 5 papers in total
+# and each of them had received 3, 0, 6, 1, 5 citations respectively. 
+# Since the researcher has 3 papers with at least 3 citations each and 
+# the remaining two with no more than 3 citations each, his h-index is 3.
+#
+# Note: If there are several possible values for h, the maximum one is taken as the h-index.
+#
+
+class Solution(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        citations.sort(reverse=True)
+        h = 0
+        for i, x in enumerate(citations):
+            if x >= i + 1:
+                h += 1
+            else:
+                break
+        return h
+  
+# Time:  O(nlogn)
+# Space: O(n)
+class Solution2(object):
+    def hIndex(self, citations):
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        sorted(citations, reverse=True)
+        h = 0
+        return sum(1 if x >= i + 1 else 0 for i, x in enumerate(sorted(citations, reverse=True)))
+  
