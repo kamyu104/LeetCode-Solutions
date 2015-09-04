f92ef4b8c143a6679bb78407c409c55ed20609b8
kamyu <kamyu104@users.noreply.github.com>
2015-09-05 00:55:09 +0800

    Create paint-fence.cpp

---
+++ C++/paint-fence.cpp
@@ -0,0 +1,39 @@
+// Time:  O(n)
+// Space: O(1)
+class Solution {
+public:
+    int numWays(int n, int k) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return k;
+        }
+        vector<int> ways(3, 0);
+        ways[0] = k;
+        ways[1] = (k - 1) * ways[0] + k;
+        for (int i = 2; i < n; ++i) {
+            ways[i % 3] = (k - 1) * (ways[(i - 1) % 3] + ways[(i - 2) % 3]);
+        }
+        return ways[(n - 1) % 3];
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    int numWays(int n, int k) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return k;
+        }
+        vector<int> ways(n, 0);
+        ways[0] = k;
+        ways[1] = (k - 1) * ways[0] + k;
+        for (int i = 2; i < n; ++i) {
+            ways[i] = (k - 1) * (ways[i - 1] + ways[i - 2]);
+        }
+        return ways[n - 1];
+    }
+};
