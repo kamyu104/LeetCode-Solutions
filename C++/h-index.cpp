80362111e7798d1fbbf181d96c5be361d4974896
kamyu <kamyu104@users.noreply.github.com>
2015-09-04 09:23:26 +0800

    Update h-index.cpp

--- C++/h-index.cpp
+++ C++/h-index.cpp
@@ -1,7 +1,34 @@
+// Time:  O(n)
+// Space: O(n)
+
+// Counting sort.
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        const auto n = citations.size();
+        vector<int> count(n + 1, 0);
+        for (const auto& x : citations) {
+            if(x >= n) {
+                ++count[n];
+            } else {
+                ++count[x];
+            }
+        }
+
+        int h = 0;
+        for (int i = n; i >= 0; --i) {
+            h += count[i];
+            if (h >= i) {
+                return i;
+            }
+        }
+        return h;
+    }
+};
+
 // Time:  O(nlogn)
 // Space: O(1)
-
-class Solution {
+class Solution2 {
 public:
     int hIndex(vector<int>& citations) {
         sort(citations.begin(), citations.end(), greater<int>());
