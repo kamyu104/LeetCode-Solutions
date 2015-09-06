6ae564a5ad58cbfdcd054d2363907def0bf268a9
kamyu <kamyu104@users.noreply.github.com>
2015-09-06 21:32:26 +0800

    Create find-the-celebrity.py

---
+++ Python/find-the-celebrity.py
@@ -0,0 +1,25 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# The knows API is already defined for you.
+# @param a, person a
+# @param b, person b
+# @return a boolean, whether a knows b
+# def knows(a, b):
+#
+
+class Solution(object):
+    def findCelebrity(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        candidate = 0
+        for i in xrange(1, n):
+            if knows(candidate, i):
+                candidate = i
+        for i in xrange(n):
+            if i != candidate and (knows(candidate, i) \
+                or not knows(i, candidate)):
+                return -1
+        return candidate
