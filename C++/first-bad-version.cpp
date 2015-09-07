ada3c651385ce8250b5aea1b47651cf3df2cec70
kamyu <kamyu104@users.noreply.github.com>
2015-09-07 21:40:10 +0800

    Create first-bad-version.cpp

---
+++ C++/first-bad-version.cpp
@@ -0,0 +1,23 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// Forward declaration of isBadVersion API.
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int left = 1, right = n;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            // Is target
+            if (isBadVersion(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
