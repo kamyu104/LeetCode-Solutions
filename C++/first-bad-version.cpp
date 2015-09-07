cbf9c56bf8e8976c89ca2b9c823a95de34365e91
kamyu <kamyu104@users.noreply.github.com>
2015-09-07 21:49:05 +0800

    Update first-bad-version.cpp

--- C++/first-bad-version.cpp
+++ C++/first-bad-version.cpp
@@ -8,7 +8,6 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         int left = 1, right = n;
-
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (isBadVersion(mid)) {
