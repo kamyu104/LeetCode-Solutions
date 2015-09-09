e174db9bcb4c038bcedd5cd91ff643196a5d227e
kamyu <kamyu104@users.noreply.github.com>
2015-09-10 01:39:19 +0800

    Update perfect-squares.cpp

--- C++/perfect-squares.cpp
+++ C++/perfect-squares.cpp
@@ -4,6 +4,21 @@
 class Solution {
 public:
     int numSquares(int n) {
+        static vector<int> num{0};
+        while (num.size() <= n) {
+            int squares = numeric_limits<int>::max();
+            for (int i = 1; i * i <= num.size(); ++i) {
+                squares = min(squares, num[num.size() - i * i] + 1);
+            }
+            num.emplace_back(squares);
+        }
+        return num[n];
+    }
+};
+
+class Solution2 {
+public:
+    int numSquares(int n) {
         vector<int> num(n + 1, numeric_limits<int>::max());
         num[0] = 0;
         for (int i = 0; i <= n; ++i) {
