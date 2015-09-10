7837db615aac355d5be0df903e94a017029de09e
kamyu <kamyu104@users.noreply.github.com>
2015-09-10 14:02:40 +0800

    Update wiggle-sort.cpp

--- C++/wiggle-sort.cpp
+++ C++/wiggle-sort.cpp
@@ -5,8 +5,8 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         for (int i = 1; i < nums.size(); ++i) {
-            if (((i & 1) && nums[i] < nums[i - 1]) ||
-                (!(i & 1) && nums[i] > nums[i - 1])) {
+            if (((i % 2) && nums[i] < nums[i - 1]) ||
+                (!(i % 2) && nums[i] > nums[i - 1])) {
                 // Swap unordered elements.
                 swap(nums[i], nums[i - 1]);
             }
