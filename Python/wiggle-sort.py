84561425d9a5df7e12473444a19a84660e8bdcbc
kamyu <kamyu104@users.noreply.github.com>
2015-09-10 14:03:01 +0800

    Update wiggle-sort.py

--- Python/wiggle-sort.py
+++ Python/wiggle-sort.py
@@ -8,8 +8,8 @@ class Solution(object):
         :rtype: void Do not return anything, modify nums in-place instead.
         """
         for i in xrange(1, len(nums)):
-            if ((i & 1) and nums[i - 1] > nums[i]) or \
-                (not (i & 1) and nums[i - 1] < nums[i]):
+            if ((i % 2) and nums[i - 1] > nums[i]) or \
+                (not (i % 2) and nums[i - 1] < nums[i]):
                 nums[i - 1], nums[i] = nums[i], nums[i - 1]
 
 
