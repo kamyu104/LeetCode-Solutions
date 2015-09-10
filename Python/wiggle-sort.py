a87b594685e388aaa96d2465ae332bbb844d50b0
kamyu <kamyu104@users.noreply.github.com>
2015-09-10 14:04:44 +0800

    Update wiggle-sort.py

--- Python/wiggle-sort.py
+++ Python/wiggle-sort.py
@@ -11,25 +11,3 @@ class Solution(object):
             if ((i % 2) and nums[i - 1] > nums[i]) or \
                 (not (i % 2) and nums[i - 1] < nums[i]):
                 nums[i - 1], nums[i] = nums[i], nums[i - 1]
-
-
-class Solution2(object):
-    def wiggleSort(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
-        """
-        if not nums:
-            return
-
-        pre = nums[0]
-        inc = True
-        for i in xrange(1, len(nums)):
-            if (inc and pre <= nums[i]) or \
-                (not inc and pre >= nums[i]):
-                nums[i - 1] = pre
-                pre = nums[i]
-            else:
-                nums[i - 1] = nums[i]
-            inc = not inc
-        nums[-1] = pre
