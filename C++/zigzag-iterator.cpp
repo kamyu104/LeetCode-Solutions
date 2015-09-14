fd884fb4437f8e13920ee3d3bc0a358341a1bd61
kamyu <kamyu104@users.noreply.github.com>
2015-09-14 10:15:26 +0800

    Update zigzag-iterator.cpp

--- C++/zigzag-iterator.cpp
+++ C++/zigzag-iterator.cpp
@@ -5,29 +5,29 @@ class ZigzagIterator {
 public:
     ZigzagIterator(vector<int>& v1, vector<int>& v2) {
         if (!v1.empty()) {
-            deq.emplace_back(make_pair(v1.size(), v1.begin()));
+            q.emplace(make_pair(v1.size(), v1.begin()));
         }
         if (!v2.empty()) {
-            deq.emplace_back(make_pair(v2.size(), v2.begin()));
+            q.emplace(make_pair(v2.size(), v2.begin()));
         }
     }
 
     int next() {
-        const auto len = deq.front().first;
-        const auto it = deq.front().second;
-        deq.pop_front();
+        const auto len = q.front().first;
+        const auto it = q.front().second;
+        q.pop();
         if (len > 1) {
-            deq.emplace_back(make_pair(len - 1, it + 1));
+            q.emplace(make_pair(len - 1, it + 1));
         }
         return *it;
     }
 
     bool hasNext() {
-        return !deq.empty();
+        return !q.empty();
     }
 
 private:
-    deque<pair<int, vector<int>::const_iterator>> deq;
+    queue<pair<int, vector<int>::const_iterator>> q;
 };
 
 /**
