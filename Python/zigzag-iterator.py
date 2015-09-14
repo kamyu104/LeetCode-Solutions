63b6acbd3aed8d2403f248e4f21b78bf0bf4b3cc
kamyu <kamyu104@users.noreply.github.com>
2015-09-14 10:26:36 +0800

    Create zigzag-iterator.py

---
+++ Python/zigzag-iterator.py
@@ -0,0 +1,31 @@
+# Time:  O(n)
+# Space: O(1)
+
+class ZigzagIterator(object):
+
+    def __init__(self, v1, v2):
+        """
+        Initialize your q structure here.
+        :type v1: List[int]
+        :type v2: List[int]
+        """
+        self.q = collections.deque([(len(v), iter(v)) for v in (v1, v2) if v])
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        len, iter = self.q.popleft()
+        if len > 1:
+            self.q.append((len-1, iter))
+        return next(iter)
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return bool(self.q)
+
+# Your ZigzagIterator object will be instantiated and called as such:
+# i, v = ZigzagIterator(v1, v2), []
+# while i.hasNext(): v.append(i.next())
