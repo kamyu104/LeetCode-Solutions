ad97c02b8d96a9e2f77b3bf0ff5ed837c80f1440
kamyu <kamyu104@users.noreply.github.com>
2015-09-16 23:04:01 +0800

    Update expression-add-operators.cpp

--- C++/expression-add-operators.cpp
+++ C++/expression-add-operators.cpp
@@ -23,39 +23,36 @@ public:
     }
 
     void addOperatorsDFS(const string& num, const int& target, const int& pos,
-                         const int& operand1, const int& operand2, vector<string> *expr,
-                         vector<string> *result) {
-        if (pos == num.length()) {
-            if (operand1 + operand2 == target) {
-                result->emplace_back(move(join(*expr)));
-            }
-            return;
-        }
+                         const int& operand1, const int& operand2,
+                         vector<string> *expr, vector<string> *result) {
+        if (pos == num.length() && operand1 + operand2 == target) {
+            result->emplace_back(move(join(*expr)));
+        } else {
+            int val = 0;
+            string val_str;
+            for (int i = pos; i < num.length(); ++i) {
+                val = val * 10 + num[i] - '0';
+                val_str.push_back(num[i]);
+                // Avoid overflow and "00...".
+                if (to_string(val) != val_str) {
+                    break;
+                }
     
-        int val = 0;
-        string val_str;
-        for (int i = pos; i < num.length(); ++i) {
-            val = val * 10 + num[i] - '0';
-            val_str.push_back(num[i]);
-            // Avoid overflow and "00...".
-            if (to_string(val) != val_str) {
-                break;
-            }
-
-            // Case '+':
-            expr->emplace_back("+" + val_str);
-            addOperatorsDFS(num, target, i + 1, operand1 + operand2, val, expr, result);
-            expr->pop_back();
-
-            // Case '-':
-            expr->emplace_back("-" + val_str);
-            addOperatorsDFS(num, target, i + 1, operand1 + operand2, -val, expr, result);
-            expr->pop_back();
+                // Case '+':
+                expr->emplace_back("+" + val_str);
+                addOperatorsDFS(num, target, i + 1, operand1 + operand2, val, expr, result);
+                expr->pop_back();
     
-            // Case '*':
-            expr->emplace_back("*" + val_str);
-            addOperatorsDFS(num, target, i + 1, operand1, operand2 * val, expr, result);
-            expr->pop_back();
+                // Case '-':
+                expr->emplace_back("-" + val_str);
+                addOperatorsDFS(num, target, i + 1, operand1 + operand2, -val, expr, result);
+                expr->pop_back();
+        
+                // Case '*':
+                expr->emplace_back("*" + val_str);
+                addOperatorsDFS(num, target, i + 1, operand1, operand2 * val, expr, result);
+                expr->pop_back();
+            }
         }
     }
     
