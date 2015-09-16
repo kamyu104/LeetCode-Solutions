50cd550277e08392eb0a41cf43fd0c47f7a26aa5
kamyu <kamyu104@users.noreply.github.com>
2015-09-16 23:01:53 +0800

    Update expression-add-operators.cpp

--- C++/expression-add-operators.cpp
+++ C++/expression-add-operators.cpp
@@ -6,58 +6,62 @@ public:
     vector<string> addOperators(string num, int target) {
         vector<string> result;
         vector<string> expr;
-        addOperatorsDFS(num, target, 0, 0, 0, &expr, &result);
+        int val = 0;
+        string val_str;
+        for (int i = 0; i < num.length(); ++i) {
+            val = val * 10 + num[i] - '0';
+            val_str.push_back(num[i]);
+            // Avoid overflow and "00...".
+            if (to_string(val) != val_str) {
+                break;
+            }
+            expr.emplace_back(val_str);
+            addOperatorsDFS(num, target, i + 1, 0, val, &expr, &result);
+            expr.pop_back();
+        }
         return result;
     }
 
-    void addOperatorsDFS(const string& s, const int& target, const int& pos,
+    void addOperatorsDFS(const string& num, const int& target, const int& pos,
                          const int& operand1, const int& operand2, vector<string> *expr,
                          vector<string> *result) {
-        if (pos == s.length()) {
+        if (pos == num.length()) {
             if (operand1 + operand2 == target) {
                 result->emplace_back(move(join(*expr)));
             }
             return;
         }
     
-        int num = 0;
-        string num_str;
-        for (int i = pos; i < s.length(); ++i) {
-            num = num * 10 + s[i] - '0';
-            num_str.push_back(s[i]);
+        int val = 0;
+        string val_str;
+        for (int i = pos; i < num.length(); ++i) {
+            val = val * 10 + num[i] - '0';
+            val_str.push_back(num[i]);
             // Avoid overflow and "00...".
-            if (to_string(num) != num_str) {
+            if (to_string(val) != val_str) {
                 break;
             }
 
             // Case '+':
-            expr->emplace_back("+"), expr->emplace_back(num_str);
-            addOperatorsDFS(s, target, i + 1, operand1 + operand2, num, expr, result);
-            expr->pop_back(), expr->pop_back();
+            expr->emplace_back("+" + val_str);
+            addOperatorsDFS(num, target, i + 1, operand1 + operand2, val, expr, result);
+            expr->pop_back();
 
-            // '-' and '*' could be used only if the expression is not empty.
-            if (!expr->empty()) {
-                // Case '-':
-                expr->emplace_back("-"), expr->emplace_back(num_str);
-                addOperatorsDFS(s, target, i + 1, operand1 + operand2, -num, expr, result);
-                expr->pop_back(), expr->pop_back();
-        
-                // Case '*':
-                expr->emplace_back("*"), expr->emplace_back(num_str);
-                addOperatorsDFS(s, target, i + 1, operand1, operand2 * num, expr, result);
-                expr->pop_back(), expr->pop_back();
-            }
+            // Case '-':
+            expr->emplace_back("-" + val_str);
+            addOperatorsDFS(num, target, i + 1, operand1 + operand2, -val, expr, result);
+            expr->pop_back();
+    
+            // Case '*':
+            expr->emplace_back("*" + val_str);
+            addOperatorsDFS(num, target, i + 1, operand1, operand2 * val, expr, result);
+            expr->pop_back();
         }
     }
     
     string join(const vector<string>& expr) {
-        string e;
-        for (int i = 0; i < expr.size(); ++i) {
-            if (i == 0 && expr[i] == "+") {  // Skip '+' at the beginning of the string.
-                continue;
-            }
-            e.append(expr[i]);
-        }
-        return e;
+        ostringstream stream;
+        copy(expr.cbegin(), expr.cend(), ostream_iterator<string>(stream));
+        return stream.str();
     }
 };
