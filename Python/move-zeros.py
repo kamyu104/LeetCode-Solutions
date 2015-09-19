8ae1c115635d4d3d78732522a9212c3d5c6f9863
kamyu <kamyu104@users.noreply.github.com>
2015-09-19 10:48:31 +0800

    Create move-zeros.py

---
+++ Python/move-zeros.py
@@ -0,0 +1,42 @@
+# Time:  O(n)
+# Space: O(1)
+#
+# Given an array nums, write a function to move all 0's
+# to the end of it while maintaining the relative order 
+# of the non-zero elements.
+#
+# For example, given nums = [0, 1, 0, 3, 12], after 
+# calling your function, nums should be [1, 3, 12, 0, 0].
+#
+# Note:
+# You must do this in-place without making a copy of the array.
+# Minimize the total number of operations.
+#
+
+class Solution(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+
+
+class Solution2(object):
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+        for i in xrange(len(nums)):
+            if nums[i]:
+                nums[pos] = nums[i]
+                pos += 1
+        
+        for i in xrange(pos, len(nums)):
+            nums[i] = 0 
