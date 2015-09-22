9b0553faed51757f4ed6f2a546b0a14233bc06f1
kamyu <kamyu104@users.noreply.github.com>
2015-09-22 19:45:47 +0800

    Create inorder-successor-in-bst.py

---
+++ Python/inorder-successor-in-bst.py
@@ -0,0 +1,34 @@
+# Time:  O(logn)
+# Space: O(1)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def inorderSuccessor(self, root, p):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :rtype: TreeNode
+        """
+        # If it has right subtree.
+        if p.right:
+            p = p.right
+            while p.left:
+                p = p.left
+            return p
+
+        # Search from root.
+        successor = None
+        while root and root != p:
+            if root.val > p.val:
+                successor = root
+                root = root.left
+            else:
+                root = root.right
+
+        return successor
