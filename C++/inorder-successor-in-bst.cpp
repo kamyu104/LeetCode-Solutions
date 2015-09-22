3da4c9f85eb4dcd204f672864fe58b87ab980d95
kamyu <kamyu104@users.noreply.github.com>
2015-09-22 19:42:04 +0800

    Create inorder-successor-in-bst.cpp

---
+++ C++/inorder-successor-in-bst.cpp
@@ -0,0 +1,38 @@
+// Time:  O(logn)
+// Space: O(1)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* inorderSuccessor(TreeNode* root, TreeNode* p) {
+        // If it has right subtree.
+        if (p->right) {
+            p = p->right;
+            while (p->left) {
+                p = p->left;
+            }
+            return p;
+        }
+
+        // Search from root.
+        TreeNode *successor = nullptr;
+        while (root && root != p ) { 
+            if (root->val > p->val) {
+                successor = root;
+                root = root->left;
+            } else {
+                root = root->right;
+            }
+        }
+
+        return successor;
+    }
+};
