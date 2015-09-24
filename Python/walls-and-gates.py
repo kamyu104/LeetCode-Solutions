16bf9e031dbb2f381ebb77241b6e97b6a8fef0e6
kamyu <kamyu104@users.noreply.github.com>
2015-09-25 00:54:15 +0800

    Update walls-and-gates.py

--- Python/walls-and-gates.py
+++ Python/walls-and-gates.py
@@ -1,5 +1,6 @@
 # Time:  O(m * n)
 # Space: O(m + n)
+from collections import deque
 
 class Solution(object):
     def wallsAndGates(self, rooms):
@@ -7,9 +8,13 @@ class Solution(object):
         :type rooms: List[List[int]]
         :rtype: void Do not return anything, modify rooms in-place instead.
         """
-        q = [(i, j) for i, row in enumerate(rooms) for j, r in enumerate(row) if not r]
-        for i, j in q:
+        INF = 2147483647
+        q = deque([(i, j) for i, row in enumerate(rooms) for j, r in enumerate(row) if not r])
+        while q:
+            (i, j) = q.popleft()
             for I, J in (i+1, j), (i-1, j), (i, j+1), (i, j-1):
-                if 0 <= I < len(rooms) and 0 <= J < len(rooms[0]) and rooms[I][J] == 2147483647:
+                if 0 <= I < len(rooms) and 0 <= J < len(rooms[0]) \
+                   and rooms[I][J] == INF:
                     rooms[I][J] = rooms[i][j] + 1
-                    q += (I, J),
+                    q.append((I, J))
+        
