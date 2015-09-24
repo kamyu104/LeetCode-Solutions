31445469522f9609cbf3b164c0303859ac28a285
kamyu <kamyu104@users.noreply.github.com>
2015-09-24 20:18:21 +0800

    Update walls-and-gates.py

--- Python/walls-and-gates.py
+++ Python/walls-and-gates.py
@@ -4,20 +4,21 @@
 from collections import deque
 
 class Solution(object):
-    def wallsAndGates(self, a):
+    def wallsAndGates(self, rooms):
         """
         :type rooms: List[List[int]]
         :rtype: void Do not return anything, modify rooms in-place instead.
         """
-        for i in xrange(len(a)):
-            for j in xrange(len(a[0])):
-                if a[i][j] == 0:
+        for i in xrange(len(rooms)):
+            for j in xrange(len(rooms[0])):
+                if rooms[i][j] == 0:
                     q = deque([(i + 1, j, 1), (i - 1, j, 1), (i, j + 1, 1), (i, j - 1, 1)])
                     while q:
                         ii, jj, dist = q.popleft()
-                        if ii < 0 or jj < 0 or ii >= len(a) or jj >= len(a[0]) or a[ii][jj] <= dist:
+                        if ii < 0 or jj < 0 or ii >= len(rooms) or \
+                           jj >= len(rooms[0]) or rooms[ii][jj] <= dist:
                             continue
-                        a[ii][jj] = dist
+                        rooms[ii][jj] = dist
                         q.append((ii + 1, jj, dist + 1))
                         q.append((ii - 1, jj, dist + 1))
                         q.append((ii, jj + 1, dist + 1))
