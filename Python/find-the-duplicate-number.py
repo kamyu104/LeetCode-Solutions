f7d5f6ccf05119ab95826b9a01cfaea7c862b882
kamyu <kamyu104@users.noreply.github.com>
2015-09-28 12:08:17 +0800

    Update find-the-duplicate-number.py

--- Python/find-the-duplicate-number.py
+++ Python/find-the-duplicate-number.py
@@ -1,7 +1,30 @@
-# Time:  O(n)
+# Time:  O(nlogn)
 # Space: O(1)
 
 class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 1, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) / 2
+            # Get count of num <= mid.
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            if count > mid:
+                right = mid - 1 
+            else:
+                left = mid + 1
+        return left
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
     def findDuplicate(self, nums):
         """
         :type nums: List[int]
