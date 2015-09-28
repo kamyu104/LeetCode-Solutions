b223c5e1a41d7f92c0e94de69d8648e10e20b3fa
kamyu <kamyu104@users.noreply.github.com>
2015-09-28 20:15:34 +0800

    Update find-the-duplicate-number.py

--- Python/find-the-duplicate-number.py
+++ Python/find-the-duplicate-number.py
@@ -1,5 +1,16 @@
 # Time:  O(n)
 # Space: O(1)
+#
+# Given an array nums containing n + 1 integers where each integer
+# is between 1 and n (inclusive), prove that at least one duplicate
+# element must exist. Assume that there is only one duplicate number,
+# find the duplicate one.
+#
+# Note:
+# You must not modify the array (assume the array is read only).
+# You must use only constant extra space.
+# Your runtime complexity should be less than O(n2).
+#
 
 # Two pointers method, same as Linked List Cycle II.
 class Solution(object):
