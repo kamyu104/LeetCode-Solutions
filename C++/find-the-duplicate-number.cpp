cfbaff0e3281487b45c319811e98fdeceda94a26
kamyu <kamyu104@users.noreply.github.com>
2015-09-28 12:12:01 +0800

    Update find-the-duplicate-number.cpp

--- C++/find-the-duplicate-number.cpp
+++ C++/find-the-duplicate-number.cpp
@@ -1,7 +1,33 @@
-// Time:  O(n)
+// Time:  O(nlogn)
 // Space: O(1)
 
 class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int left = 1, right = nums.size();
+
+        while (left <= right) {
+            const int mid = left + (right - left) / 2;
+            // Get count of num <= mid.
+            int count = 0;
+            for (const auto& num : nums) {
+                if (num <= mid) {
+                    ++count;
+                }
+            }
+            if (count > mid) {
+                right = mid - 1; 
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
 public:
     int findDuplicate(vector<int>& nums) {
         int duplicate = 0;
