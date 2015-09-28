fe478cb35d216297adf1c7a5a63a3d310d72dd54
kamyu <kamyu104@users.noreply.github.com>
2015-09-28 11:53:12 +0800

    Create find-the-duplicate-number.py

---
+++ Python/find-the-duplicate-number.py
@@ -0,0 +1,24 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        duplicate = 0
+        # Mark the value as visited by negative.
+        for num in nums:
+            if nums[abs(num) - 1] > 0:
+                nums[abs(num) - 1] *= -1
+            else:
+                duplicate = abs(num)
+                break
+        # Rollback the value.
+        for num in nums:
+            if nums[abs(num) - 1] < 0:
+                nums[abs(num) - 1] *= -1
+            else:
+                break
+        return duplicate
