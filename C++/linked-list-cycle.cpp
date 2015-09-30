9424c1693a73ddb76248b49b9bafe823668c7d22
kamyu <kamyu104@users.noreply.github.com>
2015-09-30 21:30:33 +0800

    Update linked-list-cycle.cpp

--- C++/linked-list-cycle.cpp
+++ C++/linked-list-cycle.cpp
@@ -14,7 +14,7 @@ public:
     bool hasCycle(ListNode *head) {
         ListNode *slow = head, *fast = head;
 
-        while(fast && fast->next) {
+        while (fast && fast->next) {
             slow = slow->next, fast = fast->next->next;
             if (slow == fast) {
                 return true;
