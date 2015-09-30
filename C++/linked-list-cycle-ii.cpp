8fb81c101420de3bd38a8e749c1d0b50df2764df
kamyu <kamyu104@users.noreply.github.com>
2015-09-30 21:28:45 +0800

    Update linked-list-cycle-ii.cpp

--- C++/linked-list-cycle-ii.cpp
+++ C++/linked-list-cycle-ii.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
-    public:
-        ListNode *detectCycle(ListNode *head) {
-            ListNode *slow = head, *fast = head;
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
 
-            while (fast && fast->next) {
-                slow = slow->next, fast = fast->next->next;
-                if (slow == fast) {  // There is a cycle.
-                    slow = head;
-                    // Both pointers advance at the same time.
-                    while (slow != fast) {
-                        slow = slow->next, fast = fast->next;
-                    }
-                    return slow;  // slow is the begin of cycle.
-            }
-            return nullptr;  // No cycle.
+        while (fast && fast->next) {
+            slow = slow->next, fast = fast->next->next;
+            if (slow == fast) {  // There is a cycle.
+                slow = head;
+                // Both pointers advance at the same time.
+                while (slow != fast) {
+                    slow = slow->next, fast = fast->next;
+                }
+                return slow;  // slow is the begin of cycle.
         }
+        return nullptr;  // No cycle.
+    }
 };
