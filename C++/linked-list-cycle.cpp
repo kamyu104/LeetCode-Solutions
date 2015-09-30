44f6f007cb8db38805aef76c3514145c9021e35d
kamyu <kamyu104@users.noreply.github.com>
2015-09-30 21:30:09 +0800

    Update and rename hasCycle.cpp to linked-list-cycle.cpp

---
+++ C++/linked-list-cycle.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next, fast = fast->next->next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
