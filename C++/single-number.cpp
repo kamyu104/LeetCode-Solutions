f6345c1796a8b4e9940ae4bc9f6fddc8f70b9f9c
kamyu <kamyu104@users.noreply.github.com>
2015-10-01 21:43:06 +0800

    Create single-number.cpp

---
+++ C++/single-number.cpp
@@ -0,0 +1,10 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        return accumulate(nums.cbegin(), nums.cend(),
+                          0, std::bit_xor<int>());
+    }
+};
