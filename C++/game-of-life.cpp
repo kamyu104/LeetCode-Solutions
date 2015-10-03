3105541c0cfcdbab4591b17bb52eb2b54655af7e
kamyu <kamyu104@users.noreply.github.com>
2015-10-04 03:51:29 +0800

    Create game-of-life.cpp

---
+++ C++/game-of-life.cpp
@@ -0,0 +1,33 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+class Solution {
+public:
+    void gameOfLife(vector<vector<int>>& board) {
+        const int m = board.size(), n = m ? board[0].size() : 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                int count = 0;
+                // Count live cells in 3x3 block.
+                for (int I = max(i - 1, 0); I < min(i + 2, m); ++I) {
+                    for (int J = max(j - 1, 0); J < min(j + 2, n); ++J) {
+                        count += board[I][J] & 1;
+                    }
+                }
+                // if (count == 4 && board[i][j]) means:
+                //     Any live cell with three live neighbors lives.
+                // if (count == 3) means:
+                //     Any live cell with two live neighbors.
+                //     Any dead cell with exactly three live neighbors lives.
+                if ((count == 4 && board[i][j]) || count == 3) {
+                    board[i][j] |= 2;  // Make as live. 
+                }
+            }
+        }
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                board[i][j] >>= 1;  // Update to the next state.
+            }
+        }
+    }
+};
