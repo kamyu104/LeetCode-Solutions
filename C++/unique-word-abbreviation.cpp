4f4eabff43286a8396e7d713eb50f921e717ab23
kamyu <kamyu104@users.noreply.github.com>
2015-10-03 14:04:26 +0800

    Update unique-word-abbreviation.cpp

--- C++/unique-word-abbreviation.cpp
+++ C++/unique-word-abbreviation.cpp
@@ -6,21 +6,21 @@ class ValidWordAbbr {
 public:
     ValidWordAbbr(vector<string> &dictionary) {
         for (string& word : dictionary) {
-            const string hash_word = hash(word);
-            lookup_[hash_word].emplace(word);
+            const string abbr = abbreviation(word);
+            lookup_[abbr].emplace(word);
         }
     } 
 
     bool isUnique(string word) {
-        const string hash_word = hash(word);
-        return lookup_[hash_word].empty() ||
-               (lookup_[hash_word].count(word) == lookup_[hash_word].size());
+        const string abbr = abbreviation(word);
+        return lookup_[abbr].empty() ||
+               (lookup_[abbr].count(word) == lookup_[abbr].size());
     }
 
 private:
     unordered_map<string, unordered_set<string>> lookup_;
 
-    string hash(const string& word) {
+    string abbreviation(const string& word) {
         return word.front() + to_string(word.length()) + word.back();
     }
 };
