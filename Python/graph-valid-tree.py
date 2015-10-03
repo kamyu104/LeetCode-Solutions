b3913fe8c8a4b7aa1fedcd0064bcc661e448da04
kamyu <kamyu104@users.noreply.github.com>
2015-10-03 14:29:02 +0800

    Update graph-valid-tree.py

--- Python/graph-valid-tree.py
+++ Python/graph-valid-tree.py
@@ -1,6 +1,7 @@
 # Time:  O(|V| + |E|)
 # Space: O(|V|)
 
+# BFS solution.
 class Solution:
     # @param {integer} n
     # @param {integer[][]} edges
@@ -9,14 +10,15 @@ class Solution:
         if len(edges) != n - 1:
             return False
 
-        parent, neighbors = 0, 1
-        nodes = {}
+        visited_from, neighbors = 0, 1
+        nodes = {}  # A structure to track each node's [visited_from, neighbors]
         for i in xrange(n):
             nodes[i] = [-1, []]
         for edge in edges:
             nodes[edge[0]][neighbors].append(edge[1])
             nodes[edge[1]][neighbors].append(edge[0])
 
+        # BFS to check whether the graph is valid tree.
         visited = {}
         q = collections.deque()
         q.append(0)
@@ -24,11 +26,11 @@ class Solution:
             i = q.popleft()
             visited[i] = True
             for n in nodes[i][neighbors]:
-                if n != nodes[i][parent]:
+                if n != nodes[i][visited_from]:
                     if n in visited:
                         return False
                     else:
                         visited[n] = True
-                        nodes[n][parent] = i
+                        nodes[n][visited_from] = i
                         q.append(n)
         return True
