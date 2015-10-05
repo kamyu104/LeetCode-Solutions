a62a43f71f17e70e00316a3f022c6a4f8094e681
kamyu <kamyu104@users.noreply.github.com>
2015-10-06 01:22:50 +0800

    Update word-pattern.cpp

--- C++/word-pattern.cpp
+++ C++/word-pattern.cpp
@@ -4,6 +4,7 @@
 class Solution {
 public:
     bool wordPattern(string pattern, string str) {
+        // Count the words.
         int cnt = str.empty() ? 0 : 1;
         for (const auto& c : str) {
             if (c == ' ') {
@@ -18,15 +19,19 @@ public:
         unordered_map<char, string> p2w;
         int i = 0, j = 0;
         for (const auto& p : pattern) {
+            // Get a word at a time without saving all the words.
             j = str.find(" ", i);
             if (j == string::npos) {
                 j = str.length();
             }
             const string w = str.substr(i, j - i);
+    
             if (!w2p.count(w) && !p2w.count(p)) { 
+                // Build mapping. Space: O(c)
                 w2p[w] = p; 
                 p2w[p] = w; 
             } else if (!w2p.count(w) || w2p[w] != p) {
+                // Contradict mapping.
                 return false;
             }
             i = j + 1;
