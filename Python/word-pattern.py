7d98147e836a97900438c8b970fc9ae2c5a3ecbc
kamyu <kamyu104@users.noreply.github.com>
2015-10-06 00:51:01 +0800

    Update word-pattern.py

--- Python/word-pattern.py
+++ Python/word-pattern.py
@@ -59,3 +59,28 @@ class Solution(object):
             if c == ' ':
                 cnt += 1
         return cnt
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def wordPattern(self, pattern, str):
+        """
+        :type pattern: str
+        :type str: str
+        :rtype: bool
+        """
+        words = str.split()
+        if len(pattern) != len(words):
+            return False
+
+        w2p, p2w = {}, {}
+        for p, w in izip(pattern, words):
+            if w not in w2p and p not in p2w:
+                # Build mapping.
+                w2p[w] = p
+                p2w[p] = w 
+            elif w not in w2p or w2p[w] != p:
+                # Contradict mapping.
+                return False
+        return True
