511ab9f3dbd7a34ef4b7223a8b8bf457d925f751
kamyu <kamyu104@users.noreply.github.com>
2015-10-09 20:26:04 +0800

    Update anagrams.py

--- Python/anagrams.py
+++ Python/anagrams.py
@@ -1,4 +1,4 @@
-# Time:  O(n)
+# Time:  O(nlogg) = O(n / g * glogg), g is max size of group
 # Space: O(n)
 #
 # Given an array of strings, return all groups of strings that are anagrams.
@@ -6,20 +6,19 @@
 # Note: All inputs will be in lower-case.
 #
 
-class Solution:
-    # @param strs, a list of strings
-    # @return a list of strings
-    def anagrams(self, strs):
-        anagrams_map, result = {}, []
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+        anagrams_map, result = collections.defaultdict(list), []
         for s in strs:
             sorted_str = ("").join(sorted(s))
-            if sorted_str in anagrams_map:
-                anagrams_map[sorted_str].append(s)
-            else:
-                anagrams_map[sorted_str] = [s]
+            anagrams_map[sorted_str].append(s)
         for anagram in anagrams_map.values():
-            if len(anagram) > 1:
-                result += anagram
+            anagram.sort()
+            result.append(anagram)
         return result
         
 if __name__ == "__main__":
