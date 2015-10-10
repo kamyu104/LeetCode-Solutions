8572b0fe40e1ce4adaf9a3e96d17c4009827f352
kamyu <kamyu104@users.noreply.github.com>
2015-10-10 23:05:38 +0800

    Update word-pattern-ii.cpp

--- C++/word-pattern-ii.cpp
+++ C++/word-pattern-ii.cpp
@@ -1,4 +1,4 @@
-// Time:  O(C(n + p - 1, p - 1)), n is length of str, p is length of pattern
+// Time:  O(n * C(n + p - 1, p - 1)), n is length of str, p is length of pattern
 // Space: O(n + p) 
 
 class Solution {
@@ -13,31 +13,29 @@ public:
                const int i, const int j,
                unordered_map<string, char>* w2p,
                unordered_map<char, string>* p2w) {
-
-        if (i == pattern.size() && j == str.length()) {
-            return true;
-        }
-
-        for (int k = j; k < str.length(); ++k) {
+        bool is_match = false;
+        if (i == pattern.length() && j == str.length()) {
+            is_match = true;
+        } else if (i < pattern.length() && j < str.length()) {
             const char p = pattern[i];
-            const string w = str.substr(j, k - j + 1);
-            bool build_mapping = false;
-            if (!w2p->count(w) && !p2w->count(p)) { 
-                // Build mapping. Space: O(n + p)
-                (*w2p)[w] = p; 
-                (*p2w)[p] = w;
-                build_mapping = true;
-            } else if (!w2p->count(w) || (*w2p)[w] != p) {
-                // Contradict mapping.
-                continue;
-            }
-            if (match(pattern, str, i + 1, k + 1, w2p, p2w)) {
-                return true;
-            } else if (build_mapping) {
-                w2p->erase(w);
-                p2w->erase(p);
+            if (p2w->count(p)) {
+                if ((*p2w)[p] == str.substr(j, (*p2w)[p].length())) {  // Match pattern.
+                    return match(pattern, str, i + 1, j + (*p2w)[p].length(), w2p, p2w);
+                }  // Else return false.
+            } else {
+                for (int k = j; k < str.length() && !is_match; ++k) {
+                    const string w = str.substr(j, k - j + 1);
+                    if (!w2p->count(w)) {
+                        // Build mapping. Space: O(n + p)
+                        (*w2p)[w] = p; 
+                        (*p2w)[p] = w;
+                        is_match = match(pattern, str, i + 1, k + 1, w2p, p2w);
+                        w2p->erase(w);
+                        p2w->erase(p);
+                    }  // Else try longer word.
+                }
             }
         }
-        return false;
+        return is_match;
     }
 };
