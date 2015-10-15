a783b1ec432ef9907837935f9f755dbf806543e3
kamyu <kamyu104@users.noreply.github.com>
2015-10-15 10:38:27 +0800

    Update flip-game.cpp

--- C++/flip-game.cpp
+++ C++/flip-game.cpp
@@ -6,11 +6,11 @@
       vector<string> generatePossibleNextMoves(string s) {
           vector<string> res;
           int n = s.length();
-          for (int i = 0; i < n - 1; ++i) {
+          for (int i = 0; i < n - 1; ++i) {  // n times
               if (s[i] == '+') {
                   for (;i < n - 1 && s[i + 1] == '+'; ++i) {
                       s[i] = s[i + 1] = '-';
-                      res.emplace_back(s);
+                      res.emplace_back(s);  // O(n) to copy a string
                       s[i] = s[i + 1] = '+';
                   }
               }
