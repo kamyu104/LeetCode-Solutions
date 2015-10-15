9bee6cfff0a86aa8e2209edb9f2dd622f6e01f53
kamyu <kamyu104@users.noreply.github.com>
2015-10-15 13:55:47 +0800

    Create flip-game.py

---
+++ Python/flip-game.py
@@ -0,0 +1,32 @@
+# Time:  O(c * n + n)
+# Space: O(n)
+
+# This solution compares only O(1) times for the two consecutive "+"
+class Solution(object):
+    def generatePossibleNextMoves(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        res = []
+        i, n = 0, len(s) - 1
+        while i < n:                                    # O(n) times
+            if s[i] == '+':
+                while i < n and s[i+1] == '+':          # O(c) times
+                    res.append(s[:i] + '--' + s[i+2:])  # O(n) time and space
+                    i += 1
+            i += 1
+        return res
+
+
+# Time:  O(c * m * n + n) = O(c * n + n), where m = 2 in this question
+# Space: O(n)
+# This solution compares O(m) = O(2) times for two consecutive "+", where m is length of the pattern
+class Solution2(object):
+  def generatePossibleNextMoves(self, s):
+      """
+      :type s: str
+      :rtype: List[str]
+      """
+      return [s[:i] + "--" + s[i+2:] for i in xrange(len(s) - 1) if s[i:i+2] == "++"]
+  
