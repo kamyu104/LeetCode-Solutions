fdcb9a78327760209be2c5fdfbb7d433727103cd
kamyu <kamyu104@users.noreply.github.com>
2015-10-15 13:57:28 +0800

    Update flip-game.cpp

--- C++/flip-game.cpp
+++ C++/flip-game.cpp
@@ -1,14 +1,14 @@
  // Time:  O(c * n + n), n is length of string, c is count of "++"
- // Space: O(1), no extra space excluding that result requires at most O(n^2) space
+ // Space: O(1), no extra space excluding the result which requires at most O(n^2) space
  
  class Solution {
   public:
       vector<string> generatePossibleNextMoves(string s) {
           vector<string> res;
           int n = s.length();
-          for (int i = 0; i < n - 1; ++i) {  // O(n) times
+          for (int i = 0; i < n - 1; ++i) {  // O(n) time
               if (s[i] == '+') {
-                  for (;i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) times
+                  for (;i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
                       s[i] = s[i + 1] = '-';
                       res.emplace_back(s);  // O(n) to copy a string
                       s[i] = s[i + 1] = '+';
