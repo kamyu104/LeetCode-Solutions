614c22511f71af95bf4bd38bf5e8d11869192cd5
kamyu <kamyu104@users.noreply.github.com>
2015-10-16 14:58:43 +0800

    Update flip-game-ii.py

--- Python/flip-game-ii.py
+++ Python/flip-game-ii.py
@@ -1,4 +1,4 @@
-# Time:  O(n^(c+2)), n is length of string, c is count of "++"
+# Time:  O(c * n * c!), n is length of string, c is count of "++"
 # Space: O(c * n), recursion would be called at most c in depth.
 #                  Besides, it costs n space for modifying string at each depth.
 
@@ -13,8 +13,9 @@ class Solution(object):
         while not is_win and i < n:                                     # O(n) time
             if s[i] == '+':
                 while not is_win and i < n and s[i+1] == '+':           # O(c) time
-                     # t(n, c) = n * (t(n, c-1) + n) = ... = n^c * t(n, 0) + n^2 * (1 + n + ... + n^(c-1))
-                     # = n^(c+1) + n^2 * (n^c - 1) / (n-1) = O(n^(c+2))
+                     # t(n, c) = c * (t(n, c-1) + n) + n = ...
+                     # = c! * t(n, 0) + n * c! * (c + 1) (1/0! + 1/1! + ... 1/c!)
+                     # = n * c! + n * c! * (c + 1) * O(e) = O(c * n * c!)
                     is_win = not self.canWin(s[:i] + '--' + s[i+2:])    # O(n) space
                     i += 1
             i += 1
