6c56736a08ce522fc461687ea0d3fb81332b5c20
kamyu <kamyu104@users.noreply.github.com>
2015-10-16 14:01:57 +0800

    Update flip-game-ii.py

--- Python/flip-game-ii.py
+++ Python/flip-game-ii.py
@@ -13,8 +13,8 @@ class Solution(object):
         while not is_win and i < n:                                     # O(n) time
             if s[i] == '+':
                 while not is_win and i < n and s[i+1] == '+':           # O(c) time
-                     # F(n, c) = n * (F(n, c-1) + n) = ... = n^c * F(n, 0) + n^2 * (1 + n + ... + n^(c-1))
-                     # = n^(c+1) + n^2 * (n^c - 1) / (n - 1) = O(n^(c+2))
+                     # t(n, c) = n * (t(n, c-1) + n) = ... = n^c * t(n, 0) + n^2 * (1 + n + ... + n^(c-1))
+                     # = n^(c+1) + n^2 * (n^c - 1) / (n-1) = O(n^(c+2))
                     is_win = not self.canWin(s[:i] + '--' + s[i+2:])    # O(n) space
                     i += 1
             i += 1
