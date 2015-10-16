b80a855636e54b93ab50c7e008dbfe299cd91125
kamyu <kamyu104@users.noreply.github.com>
2015-10-16 13:35:16 +0800

    Create flip-game-ii.cpp

---
+++ C++/flip-game-ii.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n^(c+1)), n is length of string, c is count of "++"
+// Space: O(c)
+
+class Solution {
+public:
+    bool canWin(string s) {
+          int n = s.length();
+          bool other_win = true; 
+          for (int i = 0; other_win && i < n - 1; ++i) {  // O(n) time
+              if (s[i] == '+') {
+                  for (; other_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
+                      s[i] = s[i + 1] = '-';
+                      other_win = canWin(s);  // F(n, c) = n * F(n, c - 1) = ... = n^c * F(n, 0) = n^(c+1)
+                      s[i] = s[i + 1] = '+';
+                  }
+              }
+          }
+          return !other_win;  
+    }
+};
