75ec6fa30ec280321940d9e24cc6d8ddb6c5669d
kamyu <kamyu104@users.noreply.github.com>
2015-10-16 23:20:11 +0800

    Update flip-game-ii.cpp

--- C++/flip-game-ii.cpp
+++ C++/flip-game-ii.cpp
@@ -1,23 +1,52 @@
-// Time:  O(n * c!), n is length of string, c is count of "++"
-// Space: O(c), recursion would be called at most c in depth.
-//              Besides, no extra space in each depth for the modified string.
+// Time:  O(n * 2^c)
+// Space: O(n * 2^c)
 
+// hash solution.
 class Solution {
 public:
     bool canWin(string s) {
-          int n = s.length();
-          bool is_win = false; 
-          for (int i = 0; !is_win && i < n - 1; ++i) {  // O(n) time
-              if (s[i] == '+') {
-                  for (; !is_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
-                      s[i] = s[i + 1] = '-';
-                      // t(n, c) = c * t(n, c - 1) + n = ... = c! * t(n, 0) + n * c! * (1/0! + 1/1! + ... 1/c!)
-                      // = n * c! + n * c! * O(e) = O(n * c!)
-                      is_win = !canWin(s); 
-                      s[i] = s[i + 1] = '+';
-                  }
-              }
-          }
-          return is_win;  
+        if (!lookup_.count(s)) {
+            int n = s.length();
+            bool is_win = false; 
+            for (int i = 0; !is_win && i < n - 1; ++i) {  // O(n) time
+                if (s[i] == '+') {
+                    for (; !is_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
+                        s[i] = s[i + 1] = '-';
+                        // t(n, c) = c * t(n, c - 1) + n = ... = c! * t(n, 0) + n * c! * (1/0! + 1/1! + ... 1/c!)
+                        // = n * c! + n * c! * O(e) = O(n * c!)
+                        is_win = !canWin(s); 
+                        s[i] = s[i + 1] = '+';
+                        lookup_[s] = is_win;
+                    }
+                }
+            }
+        }
+        return lookup_[s];  
+    }
+private:
+    unordered_map<string, bool> lookup_;
+};
+
+
+// Time:  O(n * c!), n is length of string, c is count of "++"
+// Space: O(c), recursion would be called at most c in depth.
+//              Besides, no extra space in each depth for the modified string.
+class Solution2 {
+public:
+    bool canWin(string s) {
+        int n = s.length();
+        bool is_win = false; 
+        for (int i = 0; !is_win && i < n - 1; ++i) {  // O(n) time
+            if (s[i] == '+') {
+                for (; !is_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
+                    s[i] = s[i + 1] = '-';
+                    // t(n, c) = c * t(n, c - 1) + n = ... = c! * t(n, 0) + n * c! * (1/0! + 1/1! + ... 1/c!)
+                    // = n * c! + n * c! * O(e) = O(n * c!)
+                    is_win = !canWin(s); 
+                     s[i] = s[i + 1] = '+';
+                }
+            }
+        }
+        return is_win;  
     }
 };
