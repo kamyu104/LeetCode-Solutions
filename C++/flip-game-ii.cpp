e4f4b69f7a7a132cfb3ab8474021e878d3072580
kamyu <kamyu104@users.noreply.github.com>
2015-10-16 13:58:04 +0800

    Update flip-game-ii.cpp

--- C++/flip-game-ii.cpp
+++ C++/flip-game-ii.cpp
@@ -5,16 +5,16 @@ class Solution {
 public:
     bool canWin(string s) {
           int n = s.length();
-          bool other_win = true; 
-          for (int i = 0; other_win && i < n - 1; ++i) {  // O(n) time
+          bool is_win = false; 
+          for (int i = 0; !is_win && i < n - 1; ++i) {  // O(n) time
               if (s[i] == '+') {
-                  for (; other_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
+                  for (; !is_win && i < n - 1 && s[i + 1] == '+'; ++i) {  // O(c) time
                       s[i] = s[i + 1] = '-';
-                      other_win = canWin(s);  // F(n, c) = n * F(n, c - 1) = ... = n^c * F(n, 0) = n^(c+1)
+                      is_win = !canWin(s);  // F(n, c) = n * F(n, c - 1) = ... = n^c * F(n, 0) = n^(c+1)
                       s[i] = s[i + 1] = '+';
                   }
               }
           }
-          return !other_win;  
+          return is_win;  
     }
 };
