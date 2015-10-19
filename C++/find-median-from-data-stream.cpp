60d87c9517b3046365db4eb65fdd2f379a97eb50
kamyu <kamyu104@users.noreply.github.com>
2015-10-19 15:53:13 +0800

    Update find-median-from-data-stream.cpp

--- C++/find-median-from-data-stream.cpp
+++ C++/find-median-from-data-stream.cpp
@@ -1,7 +1,46 @@
 // Time:  O(nlogn) for total n addNums, O(logn) per addNum, O(1) per findMedian.
 // Space: O(n), total space
 
+// Heap solution.
 class MedianFinder {
+public:
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        // Balance smaller half and larger half.
+        if (max_heap_.empty() || num > max_heap_.top()) {
+            min_heap_.emplace(num);
+            if (min_heap_.size() > max_heap_.size() + 1) {
+                max_heap_.emplace(min_heap_.top());
+                min_heap_.pop();
+            }
+        } else {
+            max_heap_.emplace(num);
+            if (max_heap_.size() > min_heap_.size()) {
+                min_heap_.emplace(max_heap_.top());
+                max_heap_.pop();
+            }
+        }
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        return min_heap_.size() == max_heap_.size() ?
+                   (max_heap_.top() + min_heap_.top()) / 2.0 :
+                   min_heap_.top();
+
+    }
+
+private:
+    // min_heap_ stores the larger half seen so far.
+    priority_queue<int, vector<int>, greater<int>> min_heap_;
+    // max_heap_ stores the smaller half seen so far.
+    priority_queue<int, vector<int>, less<int>> max_heap_;
+
+};
+
+// BST solution.
+class MedianFinder2 {
 public:
 
     // Adds a number into the data structure.
