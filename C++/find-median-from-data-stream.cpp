981bd40f15b2d7d9ea10da6cf92476aeb66eeba3
kamyu <kamyu104@users.noreply.github.com>
2015-10-19 15:40:09 +0800

    Create find-median-from-data-stream.cpp

---
+++ C++/find-median-from-data-stream.cpp
@@ -0,0 +1,44 @@
+// Time:  O(logn), per addition
+// Space: O(n), total space
+
+class MedianFinder {
+public:
+
+    // Adds a number into the data structure.
+    void addNum(int num) {
+        // Balance smaller half and larger half.
+        if (max_bst_.empty() || num > *max_bst_.cbegin()) {
+            min_bst_.insert(num);
+            if (min_bst_.size() > max_bst_.size() + 1) {
+                max_bst_.insert(*min_bst_.cbegin());
+                min_bst_.erase(min_bst_.cbegin());
+            }
+        } else {
+            max_bst_.insert(num);
+            if (max_bst_.size() > min_bst_.size()) {
+                min_bst_.insert(*max_bst_.cbegin());
+                max_bst_.erase(max_bst_.cbegin());
+            }
+        }
+    }
+
+    // Returns the median of current data stream
+    double findMedian() {
+        return min_bst_.size() == max_bst_.size() ?
+                   (*max_bst_.cbegin() + *min_bst_.cbegin()) / 2.0 :
+                   *min_bst_.cbegin();
+
+    }
+
+private:
+   // min_num_ stores the larger half seen so far.
+    multiset<int, less<int>> min_bst_;
+    // max_bst_ stores the smaller half seen so far.
+    multiset<int, greater<int>> max_bst_;
+
+};
+
+// Your MedianFinder object will be instantiated and called as such:
+// MedianFinder mf;
+// mf.addNum(1);
+// mf.findMedian();
