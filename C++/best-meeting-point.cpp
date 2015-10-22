d0a97d8bf8b3a0619f55952826b59edc1e98b58d
kamyu <kamyu104@users.noreply.github.com>
2015-10-22 13:30:17 +0800

    Create best-meeting-point.cpp

---
+++ C++/best-meeting-point.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int minTotalDistance(vector<vector<int>>& grid) {
+        vector<int> x, y;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j]) {
+                    x.emplace_back(i);
+                    y.emplace_back(j);
+                }
+            }
+        }
+        nth_element(x.begin(), x.begin() + x.size() / 2, x.end());
+        nth_element(y.begin(), y.begin() + y.size() / 2, y.end());
+        const int mid_x = x[x.size() / 2];
+        const int mid_y = y[y.size() / 2];
+        int sum = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j]) {
+                    sum += abs(mid_x - i) + abs(mid_y - j);
+                }
+            }
+        }
+        return sum;
+    }
+};
