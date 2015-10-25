99ce4a02964a40e4a305821b37ea6ae9b5cdeec0
kamyu <kamyu104@users.noreply.github.com>
2015-10-25 12:36:16 +0800

    Update graph-valid-tree.cpp

--- C++/graph-valid-tree.cpp
+++ C++/graph-valid-tree.cpp
@@ -1,6 +1,7 @@
 // Time:  O(|V| + |E|)
 // Space: O(|V| + |E|)
 
+// Same complexity, but faster version.
 class Solution {
 public:
     struct node {
@@ -23,6 +24,45 @@ public:
 
         if (nodes.size() != n) {
             return false;
+        }
+
+        unordered_set<int> visited;
+        queue<int> q;
+        q.emplace(0);
+        while (!q.empty()) {
+            const int i = q.front();
+            q.pop();
+            visited.emplace(i);
+            for (const auto& node : nodes[i].neighbors) {
+                if (node != nodes[i].parent) {
+                    if (visited.find(node) != visited.end()) {
+                        return false;
+                    } else {
+                        visited.emplace(node);
+                        nodes[node].parent = i;
+                        q.emplace(node);
+                    }
+                }
+            }
+        }
+        return visited.size() == n;
+    }
+};
+
+// Time:  O(|V| + |E|)
+// Space: O(|V| + |E|)
+class Solution2 {
+public:
+    struct node {
+        int parent = -1;
+        vector<int>neighbors;
+    };
+
+    bool validTree(int n, vector<pair<int, int>>& edges) {
+        unordered_map<int, node> nodes;
+        for (const auto& edge : edges) {
+            nodes[edge.first].neighbors.emplace_back(edge.second);
+            nodes[edge.second].neighbors.emplace_back(edge.first);
         }
 
         unordered_set<int> visited;
