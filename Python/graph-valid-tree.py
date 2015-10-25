50e168038400ff19637323c2f7083ccf8c621ab8
kamyu <kamyu104@users.noreply.github.com>
2015-10-25 12:38:48 +0800

    Update graph-valid-tree.py

--- Python/graph-valid-tree.py
+++ Python/graph-valid-tree.py
@@ -1,7 +1,7 @@
 # Time:  O(|V| + |E|)
 # Space: O(|V| + |E|)
 
-# BFS solution.
+# BFS solution. Same complexity but faster version.
 class Solution:
     # @param {integer} n
     # @param {integer[][]} edges
@@ -22,6 +22,40 @@ class Solution:
         
         if len(nodes) != n:  # Check number of nodes.
             return False
+
+        # BFS to check whether the graph is valid tree.
+        visited = {}
+        q = collections.deque()
+        q.append(0)
+        while q:
+            i = q.popleft()
+            visited[i] = True
+            for node in nodes[i][neighbors]:
+                if node != nodes[i][visited_from]:
+                    if node in visited:
+                        return False
+                    else:
+                        visited[node] = True
+                        nodes[node][visited_from] = i
+                        q.append(node)
+        return len(visited) == n
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|V| + |E|)
+# BFS solution.
+class Solution:
+    # @param {integer} n
+    # @param {integer[][]} edges
+    # @return {boolean}
+    def validTree(self, n, edges):
+        visited_from, neighbors = 0, 1
+
+        # A structure to track each node's [visited_from, neighbors]
+        nodes = collections.defaultdict(lambda: [-1, []])
+        for edge in edges:
+            nodes[edge[0]][neighbors].append(edge[1])
+            nodes[edge[1]][neighbors].append(edge[0])
 
         # BFS to check whether the graph is valid tree.
         visited = {}
