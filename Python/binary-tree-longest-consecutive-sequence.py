eb9cf4f6c7c45ad241ad3a8082779f5534c6be06
kamyu <kamyu104@users.noreply.github.com>
2015-10-28 14:23:51 +0800

    Create binary-tree-longest-consecutive-sequence.py

---
+++ Python/binary-tree-longest-consecutive-sequence.py
@@ -0,0 +1,35 @@
+# Time:  O(n)
+# Space: O(h)
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def longestConsecutive(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        self.max_len = 0
+
+        def longestConsecutiveHelper(root):
+            if not root:
+                return 0
+    
+            left_len = longestConsecutiveHelper(root.left)
+            right_len = longestConsecutiveHelper(root.right)
+    
+            cur_len = 1
+            if root.left and root.left.val == root.val + 1:
+                cur_len = max(cur_len, left_len + 1);
+            if root.right and root.right.val == root.val + 1:
+                cur_len = max(cur_len, right_len + 1)
+            self.max_len = max(self.max_len, cur_len, left_len, right_len)
+            return cur_len
+
+        longestConsecutiveHelper(root)
+        return self.max_len
