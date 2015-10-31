b73546fd383f78a3eedd05279dd13f3faf86dc25
kamyu <kamyu104@users.noreply.github.com>
2015-10-31 09:25:20 +0800

    Create bulls-and-cow.cpp

---
+++ C++/bulls-and-cow.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+//OSpace: O(10) = O(1)
+
+class Solution {
+public:
+    string getHint(string secret, string guess) {
+        unordered_map<char, int> lookup;
+        int A = 0, B = 0;
+        for (const auto& s : secret) {
+            ++lookup[s];
+        }
+        for (const auto& g : guess) {
+            if (lookup[g]) {
+                --lookup[g];
+                ++B;
+            }
+        }
+        for (int i = 0; i < secret.length() && i < guess.length(); ++i) {
+            if (secret[i] == guess[i]) {
+                ++A, --B;
+            }
+        }
+        return to_string(A).append("A").append(to_string(B).append("B"));
+    }
+};
