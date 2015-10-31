34ef73dac59bfa43bc5e1e7931abd0682ee1bf10
kamyu <kamyu104@users.noreply.github.com>
2015-10-31 10:09:24 +0800

    Update bulls-and-cow.py

--- Python/bulls-and-cow.py
+++ Python/bulls-and-cow.py
@@ -30,11 +30,42 @@
 # digits, and their lengths are always equal.
 #
 
+# One pass solution.
+from collections import defaultdict
+from itertools import izip
+
+class Solution(object):
+    def getHint(self, secret, guess):
+        """
+        :type secret: str
+        :type guess: str
+        :rtype: str
+        """
+        A, B = 0, 0
+        s_lookup, g_lookup = defaultdict(int), defaultdict(int)
+        for s, g in izip(secret, guess):
+            if s == g:
+                A += 1
+            else:
+                if s_lookup[g]:
+                    s_lookup[g] -= 1
+                    B += 1
+                else:
+                    g_lookup[g] += 1
+                if g_lookup[s]:
+                    g_lookup[s] -= 1
+                    B += 1
+                else:
+                    s_lookup[s] += 1
+                    
+        return "%dA%dB" % (A, B)
+
+
 # Two pass solution.
 from collections import Counter
 from itertools import imap
 
-class Solution(object):
+class Solution2(object):
     def getHint(self, secret, guess):
         """
         :type secret: str
