25f119f21909523825508ca1368a7391d97aa289
kamyu <kamyu104@users.noreply.github.com>
2015-11-03 23:31:44 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -1,7 +1,35 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
+// Binary search solution with STL.
 class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> LIS;
+
+        for (const auto& num : nums) {
+            insert(&LIS, num);
+        }
+
+        return LIS.size();
+    }
+
+private:
+    void insert(vector<int> *LIS, const int target) {
+        // Find the first index "left" which satisfies LIS[left] >= target
+        auto it = lower_bound(LIS->begin(), LIS->end(), target);
+
+        // If not found, append the target.
+        if (it != LIS->end()) {
+            *it = target;
+        } else {
+            LIS->emplace_back(target);
+        }
+    }
+};
+
+// Binary search solution.
+class Solution2 {
 public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> LIS;
