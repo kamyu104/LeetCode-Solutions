bd42990b2f72611597285b72872eaf680b32889f
kamyu <kamyu104@users.noreply.github.com>
2015-11-03 23:51:13 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -64,3 +64,24 @@ private:
         }
     }
 };
+
+// Time:  O(n^2)
+// Space: O(n)
+// Traditional DP solution.
+class Solution3 {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        const int n = nums.size();
+        vector<int> dp(n, 1);
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
+};
