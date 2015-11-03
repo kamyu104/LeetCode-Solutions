94cdc4bdcc94c5b751ba54d259d7fce43b908662
kamyu <kamyu104@users.noreply.github.com>
2015-11-03 23:29:10 +0800

    Update longest-increasing-subsequence.cpp

--- C++/longest-increasing-subsequence.cpp
+++ C++/longest-increasing-subsequence.cpp
@@ -6,22 +6,22 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         vector<int> LIS;
 
-        for (const auto& i : nums) {
-            insert(LIS, i);
+        for (const auto& num : nums) {
+            insert(&LIS, num);
         }
 
         return LIS.size();
     }
 
 private:
-    void insert(vector<int>& LIS, int target) {
-        int left = 0, right = LIS.size() - 1;
+    void insert(vector<int> *LIS, const int target) {
+        int left = 0, right = LIS->size() - 1;
         auto comp = [](int x, int target) { return x >= target; };
 
         // Find the first index "left" which satisfies LIS[left] >= target
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            if (comp(LIS[mid], target)) {
+            if (comp((*LIS)[mid], target)) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
@@ -29,10 +29,10 @@ private:
         }
 
         // If not found, append the target.
-        if (left == LIS.size()) {
-            LIS.emplace_back(target);
+        if (left == LIS->size()) {
+            LIS->emplace_back(target);
         } else {
-            LIS[left] = target;
+            (*LIS)[left] = target;
         }
     }
 };
