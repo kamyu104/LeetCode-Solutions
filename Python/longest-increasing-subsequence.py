8e0d6be00e2c4d9299f132fdddb27efbdda49ee2
kamyu <kamyu104@users.noreply.github.com>
2015-11-03 23:58:55 +0800

    Update longest-increasing-subsequence.py

--- Python/longest-increasing-subsequence.py
+++ Python/longest-increasing-subsequence.py
@@ -42,3 +42,20 @@ class Solution(object):
             insert(num)
 
         return len(LIS)
+
+# Time:  O(n^2)
+# Space: O(n)
+# Traditional DP solution.
+class Solution2(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = []
+        for i in xrange(len(nums)):
+            dp.append(1)
+            for j in xrange(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return max(dp) if dp else 0
