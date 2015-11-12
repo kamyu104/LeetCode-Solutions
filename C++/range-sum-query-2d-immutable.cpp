6797d1795b207bc47b515810a067560d1964ec09
kamyu <kamyu104@users.noreply.github.com>
2015-11-12 15:18:41 +0800

    Update range-sum-query-2d-immutable.cpp

--- C++/range-sum-query-2d-immutable.cpp
+++ C++/range-sum-query-2d-immutable.cpp
@@ -26,8 +26,8 @@ public:
     }
 
     int sumRegion(int row1, int col1, int row2, int col2) {
-        return sums[row2+1][col2+1] - sums[row2+1][col1] - 
-               sums[row1][col2+1] + sums[row1][col1];   
+        return sums[row2 + 1][col2 + 1] - sums[row2 + 1][col1] - 
+               sums[row1][col2 + 1] + sums[row1][col1];   
     }
 
 private:
