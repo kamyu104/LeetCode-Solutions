54ac5c20b23cd0d7bebf46d3e8b8d9e9d37f2480
kamyu <kamyu104@users.noreply.github.com>
2015-11-14 10:58:03 +0800

    Update number-of-islands-ii.cpp

--- C++/number-of-islands-ii.cpp
+++ C++/number-of-islands-ii.cpp
@@ -22,6 +22,9 @@ public:
                 }
             }
 
+            number += 1 - neighbors.size();  // Merge neighbors into one island.
+            numbers.emplace_back(number);
+
             // For each direction, find and union.
             for (const auto& d : directions) {
                 const auto& neighbor = make_pair(position.first + d.first,
@@ -32,9 +35,6 @@ public:
                     union_set(&set, node_id(node, n), node_id(neighbor, n));
                 }
             }
-
-            number += 1 - neighbors.size();
-            numbers.emplace_back(number);
         }
         return numbers;
     }
