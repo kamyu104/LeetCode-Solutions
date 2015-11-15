2b3b01ff26c7755e9c3b6b354648d8177ad37cd7
kamyu <kamyu104@users.noreply.github.com>
2015-11-15 17:09:39 +0800

    Update number-of-islands-ii.cpp

--- C++/number-of-islands-ii.cpp
+++ C++/number-of-islands-ii.cpp
@@ -1,4 +1,4 @@
-// Time:  O(k * log(m * n)), k is the length of the positions
+// Time:  O(k), k is the length of the positions
 // Space: O(k)
 
 // Using unordered_map.
@@ -53,7 +53,7 @@ public:
 };
 
 
-// Time:  O(k * log(m * n)), k is the length of the positions
+// Time:  O(k), k is the length of the positions
 // Space: O(m * n)
 // Using vector.
 class Solution2 {
