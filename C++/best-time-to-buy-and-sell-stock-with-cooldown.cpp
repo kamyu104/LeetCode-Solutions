64e4f0e0106ccfa06f44bbf306469d206df4a301
kamyu <kamyu104@users.noreply.github.com>
2015-11-24 13:59:39 +0800

    Create best-time-to-buy-and-sell-stock-with-cooldown.cpp

---
+++ C++/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -0,0 +1,44 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) {
+            return 0;
+        }
+        vector<int> buy(2), coolDown(2), sell(2);
+        buy[0] = -prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            // Bought before or buy today.
+            buy[i % 2] = max(buy[(i - 1) % 2], coolDown[(i - 1) % 2] - prices[i]);
+            // Sell today.
+            sell[i % 2] = buy[(i - 1) % 2] + prices[i];  
+            // Sold before yesterday or sold yesterday.
+            coolDown[i % 2] = max(coolDown[(i - 1) % 2], sell[(i - 1) % 2]);  
+        }
+        return max(coolDown[(prices.size() - 1) % 2], sell[(prices.size() - 1) % 2]);
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.empty()) {
+            return 0;
+        }
+        vector<int> buy(prices.size()), coolDown(prices.size()), sell(prices.size());
+        buy[0] = -prices[0];
+        for (int i = 1; i < prices.size(); ++i) {
+            // Bought before or buy today.
+            buy[i] = max(buy[i - 1], coolDown[i - 1] - prices[i]);
+            // Sell today.
+            sell[i] = buy[i - 1] + prices[i];  
+            // Sold before yesterday or sold yesterday.
+            coolDown[i] = max(coolDown[i - 1], sell[i - 1]);  
+        }
+        return max(coolDown[prices.size() - 1], sell[prices.size() - 1]);
+    }
+};
