9a018993db61885a95eb1f01914ac19f7c7ef3ca
kamyu <kamyu104@users.noreply.github.com>
2015-11-28 15:33:02 +0800

    Update spiral-matrix-ii.cpp

--- C++/spiral-matrix-ii.cpp
+++ C++/spiral-matrix-ii.cpp
@@ -35,47 +35,47 @@ public:
 // Time:  O(n^2)
 // Space: O(1)
 class Solution2 {
-    public:
-        vector<vector<int> > generateMatrix(int n) {
-            vector<vector<int> > matrix(n, vector<int>(n));
-            enum Action {RIGHT, DOWN, LEFT, UP};
-            Action action = RIGHT;
-            for (int i = 0, j = 0, cnt = 0, total = n * n; cnt < total;) {
-                matrix[i][j] = ++cnt;
+public:
+    vector<vector<int> > generateMatrix(int n) {
+        vector<vector<int> > matrix(n, vector<int>(n));
+        enum Action {RIGHT, DOWN, LEFT, UP};
+        Action action = RIGHT;
+        for (int i = 0, j = 0, cnt = 0, total = n * n; cnt < total;) {
+            matrix[i][j] = ++cnt;
 
-                switch (action) {
-                    case RIGHT:
-                        if (j + 1 < n && matrix[i][j + 1] == 0) {
-                            ++j;
-                        } else {
-                            action = DOWN, ++i;
-                        }
-                        break;
-                    case DOWN:
-                        if (i + 1 < n && matrix[i + 1][j] == 0) {
-                            ++i;
-                        } else {
-                            action = LEFT, --j;
-                        }
-                        break;
-                    case LEFT:
-                        if (j - 1 >= 0 && matrix[i][j - 1] == 0) {
-                            --j;
-                        } else {
-                            action = UP, --i;
-                        }
-                        break;
-                    case UP:
-                        if (i - 1 >= 0 && matrix[i - 1][j] == 0) {
-                            --i;
-                        } else {
-                            action = RIGHT, ++j;
-                        }
-                        break;
-                    default:
-                        break;
-                }
+            switch (action) {
+                case RIGHT:
+                    if (j + 1 < n && matrix[i][j + 1] == 0) {
+                        ++j;
+                    } else {
+                        action = DOWN, ++i;
+                    }
+                    break;
+                case DOWN:
+                    if (i + 1 < n && matrix[i + 1][j] == 0) {
+                        ++i;
+                    } else {
+                        action = LEFT, --j;
+                    }
+                    break;
+                case LEFT:
+                    if (j - 1 >= 0 && matrix[i][j - 1] == 0) {
+                        --j;
+                    } else {
+                        action = UP, --i;
+                    }
+                    break;
+                case UP:
+                    if (i - 1 >= 0 && matrix[i - 1][j] == 0) {
+                        --i;
+                    } else {
+                        action = RIGHT, ++j;
+                    }
+                    break;
+                default:
+                    break;
             }
-            return matrix;
         }
+        return matrix;
+    }
 };
