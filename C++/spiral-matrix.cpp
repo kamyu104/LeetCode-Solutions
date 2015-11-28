2c58589d12bf9b8bf622057141c2f9ee45106c29
kamyu <kamyu104@users.noreply.github.com>
2015-11-28 15:25:11 +0800

    Update and rename spiralOrder.cpp to spiral-matrix.cpp

---
+++ C++/spiral-matrix.cpp
@@ -0,0 +1,89 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> result;
+        if (matrix.empty()) {
+            return result;
+        }
+
+        for (int left = 0, right = matrix[0].size() - 1,
+                 top = 0, bottom = matrix.size() - 1;
+             left <= right && top <= bottom;
+             ++left, --right, ++top, --bottom) {
+
+            for (int j = left; j <= right; ++j) {
+                result.emplace_back(matrix[top][j]);
+            }
+            for (int i = top + 1; i < bottom; ++i) {
+                result.emplace_back(matrix[i][right]);
+            }
+            for (int j = right; top < bottom && j >= left; --j) {
+                result.emplace_back(matrix[bottom][j]);
+            }
+            for (int i = bottom - 1; left < right && i > top; --i) {
+                result.emplace_back(matrix[i][left]);
+            }
+        }
+
+        return result;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(1)
+class Solution2 {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        const int m = matrix.size();
+        vector<int> ans;
+        if (m == 0) {
+            return ans;
+        }
+
+        const int n = matrix.front().size();
+        enum Action {RIGHT, DOWN, LEFT, UP};
+        Action action = RIGHT;
+        for (int i = 0, j = 0, begini = 0, beginj = 0, endi = m,
+             endj = n, cnt = 0, total = m * n; cnt < total; ++cnt) {
+
+            ans.emplace_back(matrix[i][j]);
+
+            switch (action) {
+                case RIGHT:
+                    if (j + 1 < endj) {
+                        ++j;
+                    } else {
+                        action = DOWN, ++begini, ++i;
+                    }
+                    break;
+                case DOWN:
+                    if (i + 1 < endi) {
+                        ++i;
+                    } else {
+                        action = LEFT, --endj, --j;
+                    }
+                    break;
+                case LEFT:
+                    if (j - 1 >= beginj) {
+                        --j;
+                    } else {
+                        action = UP, --endi, --i;
+                    }
+                    break;
+                case UP:
+                    if (i - 1 >= begini) {
+                        --i;
+                    } else {
+                        action = RIGHT, ++beginj, ++j;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        return ans;
+    }
+};
