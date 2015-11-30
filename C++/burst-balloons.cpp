3d67eec01196ebbb88a3d6f5acd8c647f679a37f
kamyu <kamyu104@users.noreply.github.com>
2015-11-30 22:12:04 +0800

    Update burst-balloons.cpp

--- C++/burst-balloons.cpp
+++ C++/burst-balloons.cpp
@@ -16,8 +16,7 @@ public:
         vector<vector<int>> max_coins(coins.size(), vector<int>(coins.size()));
         for (int k = 2; k < coins.size(); ++k) {
             for (int left = 0; left < coins.size() - k; ++left) {
-                const int right = left + k;
-                for (int i = left + 1; i < right; ++i) {
+                for (int i = left + 1, right = left + k; i < right; ++i) {
                     max_coins[left][right] = max(max_coins[left][right],
                          coins[left] * coins[i] * coins[right] +
                          max_coins[left][i] + max_coins[i][right]);
