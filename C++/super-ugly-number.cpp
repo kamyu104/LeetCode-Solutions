f1bbc2c1e0fbd6b66912c9ff61072a4778e5ecec
kamyu <kamyu104@users.noreply.github.com>
2015-12-04 01:01:55 +0800

    Update super-ugly-number.cpp

--- C++/super-ugly-number.cpp
+++ C++/super-ugly-number.cpp
@@ -1,14 +1,16 @@
 // Time:  O(n * k)
 // Space: O(n + k)
 
-// DP solution.
+// DP solution. (596ms)
 class Solution {
 public:
     int nthSuperUglyNumber(int n, vector<int>& primes) {
-        vector<int> uglies{1}, ugly_by_prime(primes), idx(primes.size());
-        while (uglies.size() < n) {
+        vector<int> uglies(n), ugly_by_prime(primes), idx(primes.size());
+        uglies[0] = 1;
+
+        for (int i = 1; i < n; ++i) {
             int min_val = *min_element(ugly_by_prime.begin(), ugly_by_prime.end());
-            uglies.emplace_back(min_val);
+            uglies[i] = min_val;
             for (int k = 0; k < primes.size(); ++k) {
                 if (min_val == ugly_by_prime[k]) {
                     ugly_by_prime[k] = primes[k] * uglies[++idx[k]];
