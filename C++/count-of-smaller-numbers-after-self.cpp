e18794bf79c34575a72bef30586c8418546ff95e
kamyu <kamyu104@users.noreply.github.com>
2015-12-07 09:14:48 +0800

    Update count-of-smaller-numbers-after-self.cpp

--- C++/count-of-smaller-numbers-after-self.cpp
+++ C++/count-of-smaller-numbers-after-self.cpp
@@ -1,8 +1,91 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
-// BIT solution.
+// BST solution. (40ms)
 class Solution {
+public:
+public:
+    class BSTreeNode {
+    public:
+        int val, count;
+        BSTreeNode *left, *right;
+        BSTreeNode(int val, int count) {
+            this->val = val;
+            this->count = count;
+            this->left = this->right = nullptr;
+        }
+    };
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> res(nums.size());
+
+        BSTreeNode *root = nullptr;
+
+        // Insert into BST and get left count.
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            int count = 0;
+            BSTreeNode *node = new BSTreeNode(nums[i], 0);
+            root = insertNode(root, node);
+            count = query(root, nums[i]);
+            res[i] = count;
+        }
+
+        return res;
+    }
+
+    // Insert node into BST.
+    BSTreeNode* insertNode(BSTreeNode* root, BSTreeNode* node) {
+        if (root == nullptr) {
+            return node;
+        }
+        BSTreeNode* curr = root;
+        while (curr) {
+            // Insert left if smaller.
+            if (node->val < curr->val) {
+                ++curr->count; // Increase the number of left children.
+                if (curr->left != nullptr) {
+                    curr = curr->left;
+                } else {
+                    curr->left = node;
+                    break;
+                }
+            } else {  // Insert right if larger or equal.
+                if (curr->right != nullptr) {
+                    curr = curr->right;
+                } else {
+                    curr->right = node;
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+
+    // Query the smaller count of the value.
+    int query(BSTreeNode* root, int val) {
+        if (root == nullptr) {
+            return 0;
+        }
+        int count = 0;
+        BSTreeNode* curr = root;
+        while (curr) {
+            // Insert left.
+            if (val < curr->val) {
+                curr = curr->left;
+            } else if (val > curr->val) {
+                count += 1 + curr->count; // Count the number of the smaller nodes.
+                curr = curr->right;
+            } else {  // Equal.
+                return count + curr->count;
+            }
+        }
+        return 0;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// BIT solution. (56ms)
+class Solution2 {
 public:
     vector<int> countSmaller(vector<int>& nums) {
         // Get the place (position in the ascending order) of each number.
