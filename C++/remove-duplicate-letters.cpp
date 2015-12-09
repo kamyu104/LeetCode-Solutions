665c4f8a0426ee5e117ae7328d359158f009a810
kamyu <kamyu104@users.noreply.github.com>
2015-12-09 22:02:15 +0800

    Update remove-duplicate-letters.cpp

--- C++/remove-duplicate-letters.cpp
+++ C++/remove-duplicate-letters.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
-        const int k = 26;
         unordered_map<char, int> cnts;
-        unordered_set<char> visited;
-        string res;
-        stack<char> stk;
         for (const auto& c : s) {
             ++cnts[c];
         }
+
+        string res;
+        unordered_set<char> visited;
+        stack<char> stk;
         for (int i = 0; i < s.size(); --cnts[s[i]], ++i) {
             if (!visited.count(s[i]) && (stk.empty() || stk.top() != s[i])) {
                 while (!stk.empty() && stk.top() >= s[i] && cnts[stk.top()] > 0) {
