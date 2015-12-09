e9f732def58edae79cbe94bff59e759154821e4f
kamyu <kamyu104@users.noreply.github.com>
2015-12-10 07:43:10 +0800

    Update remove-duplicate-letters.cpp

--- C++/remove-duplicate-letters.cpp
+++ C++/remove-duplicate-letters.cpp
@@ -6,23 +6,23 @@ class Solution {
 public:
     string removeDuplicateLetters(string s) {
         const int k = 26;
-        vector<int> cnts(k);
+        vector<int> remaining(k);
         for (const auto& c : s) {
-            ++cnts[c - 'a'];
+            ++remaining[c - 'a'];
         }
 
         vector<bool> in_stack(k);
         string stk;
         for (const auto& c : s) {
             if (!in_stack[c - 'a']) {
-                while (!stk.empty() && stk.back() > c && cnts[stk.back() - 'a']) {
+                while (!stk.empty() && stk.back() > c && remaining[stk.back() - 'a']) {
                     in_stack[stk.back() - 'a'] = false;
                     stk.pop_back();
                 }
                 stk.push_back(c);
                 in_stack[c - 'a'] = true;
             }
-            --cnts[c - 'a'];
+            --remaining[c - 'a'];
         }
         return stk; 
     }
@@ -34,23 +34,23 @@ public:
 class Solution2 {
 public:
     string removeDuplicateLetters(string s) {
-        unordered_map<char, int> cnts;
+        unordered_map<char, int> remaining;
         for (const auto& c : s) {
-            ++cnts[c];
+            ++remaining[c];
         }
 
         unordered_set<char> in_stack;
         string stk;
         for (const auto& c : s) {
             if (!in_stack.count(c)) {
-                while (!stk.empty() && stk.back() > c && cnts[stk.back()]) {
+                while (!stk.empty() && stk.back() > c && remaining[stk.back()]) {
                     in_stack.erase(stk.back());
                     stk.pop_back();
                 }
                 stk.push_back(c);
                 in_stack.emplace(c);
             }
-            --cnts[c];
+            --remaining[c];
         }
         return stk; 
     }
