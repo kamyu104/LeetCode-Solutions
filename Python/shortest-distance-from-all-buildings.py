9e6db215b781e38d3567815c2f644ac41696d7af
kamyu <kamyu104@users.noreply.github.com>
2015-12-14 23:25:35 +0800

    Update shortest-distance-from-all-buildings.py

--- Python/shortest-distance-from-all-buildings.py
+++ Python/shortest-distance-from-all-buildings.py
@@ -7,7 +7,7 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
-        def bfs(grid, dists, reachable, x, y):
+        def bfs(grid, dists, cnts, x, y):
             dist, m, n = 0, len(grid), len(grid[0])
             visited = [[False for _ in xrange(n)] for _ in xrange(m)]
     
@@ -22,34 +22,28 @@ class Solution(object):
                         I, J = i+dir[0], j+dir[1]
                         if 0 <= I < m and 0 <= J < n and grid[I][J] == 0 and not visited[I][J]:
                             cur_level.append((I, J))
+                            cnts[I][J] += 1
                             visited[I][J] = True
     
                 dist += 1
                 pre_level = cur_level
-    
-            for i in xrange(m):
-                for j in xrange(n):
-                    if not visited[i][j]:
-                        reachable[i][j] = False
 
-        m, n = len(grid),  len(grid[0])
+
+        m, n, cnt = len(grid),  len(grid[0]), 0
         dists = [[0 for _ in xrange(n)] for _ in xrange(m)]
-        reachable = [[True for _ in xrange(n)] for _ in xrange(m)]
-        for i in xrange(m):
-            for j in xrange(n):
-                if grid[i][j] > 0:
-                    reachable[i][j] = False
-                    dists[i][j] = float("inf")
-
+        cnts = [[0 for _ in xrange(n)] for _ in xrange(m)]
         for i in xrange(m):
             for j in xrange(n):
                 if grid[i][j] == 1:
-                    bfs(grid, dists, reachable, i, j)
+                    cnt += 1
+                    bfs(grid, dists, cnts, i, j)
 
         shortest = float("inf")
         for i in xrange(m):
             for j in xrange(n):
-                if dists[i][j] < shortest and reachable[i][j]:
+                if dists[i][j] < shortest and cnts[i][j] == cnt:
                     shortest = dists[i][j]
 
         return shortest if shortest != float("inf") else -1
+
+
