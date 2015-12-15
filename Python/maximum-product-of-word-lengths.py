1900e2e722d9e779027a626cf69e3be55861e2ed
kamyu <kamyu104@users.noreply.github.com>
2015-12-16 07:58:12 +0800

    Create maximum-product-of-word-lengths.py

---
+++ Python/maximum-product-of-word-lengths.py
@@ -0,0 +1,51 @@
+# Time:  O(nlogn) ~ O(n^2)
+# Space: O(n)
+# Sorting + Pruning + Bit Manipulation
+
+# Given a string array words, find the maximum value of
+# length(word[i]) * length(word[j]) where the two words 
+# do not share common letters. You may assume that each
+# word will contain only lower case letters. If no such
+# two words exist, return 0.
+#
+# Example 1:
+# Given ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+# Return 16
+# The two words can be "abcw", "xtfn".
+#
+# Example 2:
+# Given ["a", "ab", "abc", "d", "cd", "bcd", "abcd"]
+# Return 4
+# The two words can be "ab", "cd".
+#
+# Example 3:
+# Given ["a", "aa", "aaa", "aaaa"]
+# Return 0
+# No such pair of words.
+#
+# Follow up:
+# Could you do better than O(n2), where n is the number of words?
+
+class Solution(object):
+    def maxProduct(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        words.sort(key=lambda x: len(x), reverse=True)
+        bits = [0] * len(words)
+        for i, word in enumerate(words):
+            for c in word:
+                bits[i] |= (1 << (ord(c) - ord('a')))
+
+        max_product = 0
+        for i in xrange(len(words) - 1):
+            if len(words[i]) ** 2 <= max_product:
+                break
+            for j in xrange(i + 1, len(words)):
+                if len(words[i]) * len(words[j]) <= max_product:
+                    break
+                if not(bits[i] & bits[j]):
+                    max_product = len(words[i]) * len(words[j])
+
+        return max_product
