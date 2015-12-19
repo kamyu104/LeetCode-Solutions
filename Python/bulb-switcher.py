43cbea660fac13df7d8398e265761674e0a2c9fe
kamyu <kamyu104@users.noreply.github.com>
2015-12-19 20:16:21 +0900

    Create bulb-switcher.py

---
+++ Python/bulb-switcher.py
@@ -0,0 +1,10 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        type n: int
+        rtype: int
+        """
+        return int(math.sqrt(n))
