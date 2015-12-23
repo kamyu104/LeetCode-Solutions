48575a4a69cba88f878c70084231fc82df80805b
kamyu <kamyu104@users.noreply.github.com>
2015-12-23 09:09:00 +0800

    Update generalized-abbreviation.cpp

--- C++/generalized-abbreviation.cpp
+++ C++/generalized-abbreviation.cpp
@@ -6,23 +6,23 @@ public:
     vector<string> generateAbbreviations(string word) {
         vector<string> res;
         string cur;
-        generateAbbreviationsHelper(word, 0, false, &cur, &res);
+        generateAbbreviationsHelper(word, 0, &cur, &res);
         return res;
     }
 
-    void generateAbbreviationsHelper(const string& word, int i, bool is_prev_num, string *cur, vector<string> *res) {
+    void generateAbbreviationsHelper(const string& word, int i, string *cur, vector<string> *res) {
         if (i == word.length()) {
             res->emplace_back(*cur);
             return;
         }
         cur->push_back(word[i]);
-        generateAbbreviationsHelper(word, i + 1, false, cur, res);
+        generateAbbreviationsHelper(word, i + 1, cur, res);
         cur->pop_back();
-        if (!is_prev_num) {
-            for (int l = 1; i + l <= word.length(); ++l) {
-                cur->append(to_string(l));
-                generateAbbreviationsHelper(word, i + l, true, cur, res);
-                cur->resize(cur->length() - to_string(l).length());
+        if (cur->empty() || not isdigit(cur->back())) {
+            for (int len = 1; i + len <= word.length(); ++len) {
+                cur->append(to_string(len));
+                generateAbbreviationsHelper(word, i + len, cur, res);
+                cur->resize(cur->length() - to_string(len).length());
             }
         }
     }
