f5005782eb474fcf929c9c09e725322e16f17168
kamyu <kamyu104@users.noreply.github.com>
2015-12-27 14:33:36 +0800

    Create coin-change.cpp

---
+++ C++/coin-change.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n * k), n is the number of coins, k is the amount of money
+// Space: O(k)
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> amounts(amount + 1, numeric_limits<int>::max());
+        amounts[0] = 0;
+        for (int i = 0; i <= amount; ++i) {
+            if (amounts[i] != numeric_limits<int>::max()) {
+                for (const auto& coin : coins) {
+                    if (i + coin <= amount) {
+                        amounts[i + coin] = min(amounts[i + coin], amounts[i] + 1);
+                    }
+                }
+            }
+        }
+        return amounts[amount] == numeric_limits<int>::max() ? -1 : amounts[amount];
+    }
+};
