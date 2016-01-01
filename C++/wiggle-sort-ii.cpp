bf549cd6bf6151f605e7347c491214d0bdc76cc6
kamyu <kamyu104@users.noreply.github.com>
2016-01-01 17:45:01 +0800

    Update wiggle-sort-ii.cpp

--- C++/wiggle-sort-ii.cpp
+++ C++/wiggle-sort-ii.cpp
@@ -1,7 +1,33 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(1)
 
+// Using virtual index solution.
 class Solution {
+public:
+    void wiggleSort(vector<int>& nums) {
+        const int n = nums.size();
+        int mid = (n - 1) / 2;
+        nth_element(nums.begin(), nums.begin() + mid, nums.end());
+        dutchFlagSort(nums, nums[mid]);
+    }
+
+    void dutchFlagSort(vector<int>& nums, int val) {
+        #define Nums(i) nums[(1 + 2 * (i)) % N]
+        for (int i = 0, j = 0, n = nums.size() - 1, N = nums.size() +  n % 2; j <= n;) {
+            if (Nums(j) > val) {
+                swap(Nums(i++), Nums(j++));
+            } else if (Nums(j) < val) {
+                swap(Nums(j), Nums(n--));
+            } else {
+                j++;
+            }
+        }
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
 public:
     void wiggleSort(vector<int>& nums) {
         const int n = nums.size();
