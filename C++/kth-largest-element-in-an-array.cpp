4824c7d5ecb8b8f8227a6c057962c8f618b29795
kamyu <kamyu104@users.noreply.github.com>
2016-01-01 21:38:07 +0800

    Update kth-largest-element-in-an-array.cpp

--- C++/kth-largest-element-in-an-array.cpp
+++ C++/kth-largest-element-in-an-array.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n)
+// Time:  O(n) ~ O(n^2)
 // Space: O(1)
 
 class Solution {
@@ -36,7 +36,7 @@ public:
     }
 };
 
-// Time:  O(n)
+// Time:  O(n) ~ O(n^2)
 // Space: O(1)
 class Solution2 {
 public:
