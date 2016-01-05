0878e1cf74231a60755b19319dfdc31655c66171
kamyu <kamyu104@users.noreply.github.com>
2016-01-05 19:19:03 +0800

    Create maximum-size-subarray-sum-equals-k.cpp

---
+++ C++/maximum-size-subarray-sum-equals-k.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int maxSubArrayLen(vector<int>& nums, int k) {
+        unordered_map<int, int> sums;
+        int cur_sum = 0, max_len = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            cur_sum += nums[i];
+            if (cur_sum == k) {
+                max_len = i + 1;
+            } else if (sums.find(cur_sum - k) != sums.end()) {
+                max_len = max(max_len, i - sums[cur_sum - k]);
+            }
+            if (sums.find(cur_sum) == sums.end()) {
+                sums[cur_sum] = i;
+            }
+        }
+        return max_len;
+    }
+};
