81e9eec1031e2704977feae11fa638f8d0b95910
kamyu <kamyu104@users.noreply.github.com>
2016-01-05 19:16:30 +0800

    Create maximum-size-subarray-sum-equals-k.py

---
+++ Python/maximum-size-subarray-sum-equals-k.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def maxSubArrayLen(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        sums = {}
+        cur_sum, max_len = 0, 0
+        for i in xrange(len(nums)):
+            cur_sum += nums[i]
+            if cur_sum == k:
+                max_len = i + 1
+            elif cur_sum - k in sums:
+                max_len = max(max_len, i - sums[cur_sum - k])
+            if cur_sum not in sums:
+                sums[cur_sum] = i  # Only keep the smallest index.
+        return max_len
