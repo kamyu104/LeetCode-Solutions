4b56dfc94a52665e1a3403b06ee1f33f418fbdcb
kamyu <kamyu104@users.noreply.github.com>
2016-01-07 22:23:21 +0800

    Update 3sum-closest.py

--- Python/3sum-closest.py
+++ Python/3sum-closest.py
@@ -11,26 +11,29 @@
 # The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 #
 
-class Solution:
-    # @return an integer
+class Solution(object):
     def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
         nums, result, min_diff, i = sorted(nums), float("inf"), float("inf"), 0
         while i < len(nums) - 2:
-            j, k = i + 1, len(nums) - 1
-            while j < k:
-                diff = nums[i] + nums[j] + nums[k] - target
-                if abs(diff) < min_diff:
-                    min_diff = abs(diff)
-                    result = nums[i] + nums[j] + nums[k]
-                if diff < 0:
-                    j += 1
-                elif diff > 0:
-                    k -= 1
-                else:
-                    return target
+            if i == 0 or nums[i] != nums[i - 1]:
+                j, k = i + 1, len(nums) - 1
+                while j < k:
+                    diff = nums[i] + nums[j] + nums[k] - target
+                    if abs(diff) < min_diff:
+                        min_diff = abs(diff)
+                        result = nums[i] + nums[j] + nums[k]
+                    if diff < 0:
+                        j += 1
+                    elif diff > 0:
+                        k -= 1
+                    else:
+                        return target
             i += 1
-            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
-                i += 1
         return result
 
 if __name__ == '__main__':
