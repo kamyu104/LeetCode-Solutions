6956cff3e49f8c0bfb2935f64882f352b8ea27ed
kamyu <kamyu104@users.noreply.github.com>
2016-01-07 22:12:46 +0800

    Update 3sum.py

--- Python/3sum.py
+++ Python/3sum.py
@@ -15,27 +15,29 @@
 #    (-1, -1, 2)
 #
 
-class Solution:
-    # @return a list of lists of length 3, [[val1,val2,val3]]
+class Solution(object):
     def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         nums, result, i = sorted(nums), [], 0
         while i < len(nums) - 2:
-            j, k = i + 1, len(nums) - 1
-            while j < k:
-                if nums[i] + nums[j] + nums[k] < 0:
-                    j += 1
-                elif nums[i] + nums[j] + nums[k] > 0:
-                    k -= 1
-                else:
-                    result.append([nums[i], nums[j], nums[k]])
-                    j, k = j + 1, k - 1
-                    while j < k and nums[j] == nums[j - 1]:
+            if i == 0 or nums[i] != nums[i - 1]:
+                j, k = i + 1, len(nums) - 1
+                while j < k:
+                    if nums[i] + nums[j] + nums[k] < 0:
                         j += 1
-                    while j < k and nums[k] == nums[k + 1]:
+                    elif nums[i] + nums[j] + nums[k] > 0:
                         k -= 1
+                    else:
+                        result.append([nums[i], nums[j], nums[k]])
+                        j, k = j + 1, k - 1
+                        while j < k and nums[j] == nums[j - 1]:
+                            j += 1
+                        while j < k and nums[k] == nums[k + 1]:
+                            k -= 1
             i += 1
-            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
-                i += 1
         return result
 
 if __name__ == '__main__':
