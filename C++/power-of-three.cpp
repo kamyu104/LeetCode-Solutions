475531ddf6852143d7499f68f45d85ab22366958
kamyu <kamyu104@users.noreply.github.com>
2016-01-09 13:41:45 +0800

    Update power-of-three.cpp

--- C++/power-of-three.cpp
+++ C++/power-of-three.cpp
@@ -3,11 +3,10 @@
 
 class Solution {
 public:
-    bool isPowerOfThree(int n) {
-        return n > 0 && max_pow3_ % n == 0;
-    }
+    static const int max_log3 = log(numeric_limits<int>::max()) / log(3);
+    static const int max_pow3 = pow(3, max_log3);
 
-private:
-    const int max_log3_ = log(numeric_limits<int>::max()) / log(3);
-    const int max_pow3_ = pow(3, max_log3_);
+    bool isPowerOfThree(int n) {
+        return n > 0 && max_pow3 % n == 0;
+    }
 };
