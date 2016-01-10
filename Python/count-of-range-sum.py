46bce4582a5a3fc908b0e05811d7cced9fd2dd11
kamyu <kamyu104@users.noreply.github.com>
2016-01-10 16:34:25 +0800

    Update count-of-range-sum.py

--- Python/count-of-range-sum.py
+++ Python/count-of-range-sum.py
@@ -52,4 +52,4 @@ class Solution(object):
         sums = [0] * (len(nums) + 1)
         for i in xrange(len(nums)):
             sums[i + 1] = sums[i] + nums[i]
-        return countAndMergeSort(sums, 0, len(sums), lower, upper);
+        return countAndMergeSort(sums, 0, len(sums), lower, upper)
