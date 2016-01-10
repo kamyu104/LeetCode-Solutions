673820df9eb65c19d941179e8b2711df87454c78
kamyu <kamyu104@users.noreply.github.com>
2016-01-11 07:32:04 +0800

    Update count-of-smaller-numbers-after-self.cpp

--- C++/count-of-smaller-numbers-after-self.cpp
+++ C++/count-of-smaller-numbers-after-self.cpp
@@ -122,3 +122,41 @@ private:
         return i & -i;
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(n)
+// Divide and Conquer solution. (80ms)
+class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> counts(nums.size());
+        vector<pair<int, int>> num_idxs;
+        for (int i = 0; i < nums.size(); ++i) {
+            num_idxs.emplace_back(nums[i], i);
+        }
+        countAndMergeSort(&num_idxs, 0, num_idxs.size() - 1, &counts);
+        return counts;
+    }
+
+    void countAndMergeSort(vector<pair<int, int>> *num_idxs, int start, int end, vector<int> *counts) {
+        if (end - start <= 0) {  // The number of range [start, end] of which size is less than 2 doesn't need sort.
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        countAndMergeSort(num_idxs, start, mid, counts);
+        countAndMergeSort(num_idxs, mid + 1, end, counts);
+
+        int j = mid + 1, k = mid + 1, r = mid + 1;
+        vector<pair<int, int>> tmp;
+        for (int i = start; i <= mid; ++i) {
+            // Merge the two sorted arrays into tmp.
+            while (r <= end && (*num_idxs)[r].first < (*num_idxs)[i].first) {
+                tmp.emplace_back((*num_idxs)[r++]);
+            }
+            tmp.emplace_back((*num_idxs)[i]);
+            (*counts)[(*num_idxs)[i].second] += r - (mid + 1);
+        }
+        // Copy tmp back to num_idxs.
+        copy(tmp.begin(), tmp.end(), num_idxs->begin() + start);
+    }
+};
