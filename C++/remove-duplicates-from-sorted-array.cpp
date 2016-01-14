992e8d4f8fe657901f3d222f02b59e47e5f05e36
kamyu <kamyu104@users.noreply.github.com>
2016-01-14 23:08:24 +0800

    Update and rename removeDuplicates.cpp to remove-duplicates-from-sorted-array.cpp

---
+++ C++/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,16 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int last = -1;
+        for (const auto& num : nums) {
+            if (last == -1 || nums[last] != num) {
+                nums[++last] = num;
+            }
+        }
+
+        return last + 1;
+    }
+};
