5477fa85045954d0a55ed0366787aaafc49ce06e
kamyu <kamyu104@users.noreply.github.com>
2016-01-22 00:33:34 +0800

    Update rotate.cpp

--- C++/rotate.cpp
+++ C++/rotate.cpp
@@ -1,18 +1,37 @@
-// Time Complexity: O(n^2)
-// Space Complexity: O(1)
+// Time:  O(n^2)
+// Space: O(1)
 
 class Solution {
-    public:
-        void rotate(vector<vector<int> > &matrix) {
-            int n = matrix.size();
-            for(int i = 0; i < n / 2; i++) {
-                for(int j = i; j < n - 1 - i; j++) {
-                    int tmp = matrix[i][j];
-                    matrix[i][j] = matrix[n-1-j][i];
-                    matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
-                    matrix[n-1-i][n-1-j]= matrix[j][n-1-i];
-                    matrix[j][n-1-i] = tmp;
-                }
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = i; j < n - 1 - i; ++j) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[n - 1 - j][i];
+                matrix[n - 1- j][i] = matrix[n - 1 - i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = tmp;
             }
         }
+    }
+};
+
+class Solution2 {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        const int n = matrix.size();
+        // Anti-diagonal mirror.
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n - i; ++j) {
+                swap(matrix[i][j], matrix[n - 1 - j][n - 1 - i]);
+            }
+        }
+        // Horizontal mirror.
+        for (int i = 0; i < n / 2; ++i) {
+            for (int j = 0; j < n; ++j) {
+                swap(matrix[i][j], matrix[n - 1 - i][j]);
+            }
+        }  
+    }
 };
