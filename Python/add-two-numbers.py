1e8b4da23a78d571b3c612993ed2bd04fe021eab
kamyu <kamyu104@users.noreply.github.com>
2016-01-31 10:53:39 +0800

    Update add-two-numbers.py

--- Python/add-two-numbers.py
+++ Python/add-two-numbers.py
@@ -14,18 +14,22 @@ class ListNode:
         self.val = x
         self.next = None
 
-class Solution:
-    # @return a ListNode
+class Solution(object):
     def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
         dummy = ListNode(0)
         current, carry = dummy, 0
 
-        while l1 is not None or l2 is not None:
+        while l1 or l2:
             val = carry
-            if l1 is not None:
+            if l1:
                 val += l1.val
                 l1 = l1.next
-            if l2 is not None:
+            if l2:
                 val += l2.val
                 l2 = l2.next
             carry, val = val / 10, val % 10
