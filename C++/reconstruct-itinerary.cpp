68e20292f6c810f0d7d575a00f5e2078aabfde38
kamyu <kamyu104@users.noreply.github.com>
2016-02-04 16:15:04 +0800

    Create reconstruct-itinerary.cpp

---
+++ C++/reconstruct-itinerary.cpp
@@ -0,0 +1,38 @@
+// Time:  O((n-1)!)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<string> findItinerary(vector<pair<string, string>> tickets) {
+        unordered_map<string, map<string, int>> graph;
+        for (const auto& ticket : tickets) {
+            ++graph[ticket.first][ticket.second];
+        }
+        const string start = "JFK";
+        vector<string> ans{start};
+        routeHelper(start, tickets.size(), &graph, &ans);
+        return ans;
+    }
+
+private:
+    bool routeHelper(const string& start, const int size,
+             unordered_map<string, map<string, int>> *graph, vector<string> *ans) {
+
+        if (size == 0) {
+            return true;
+        }
+
+        for (const auto& neighbor : (*graph)[start]) {
+            if ((*graph)[start][neighbor.first]) {
+                --(*graph)[start][neighbor.first];
+                ans->emplace_back(neighbor.first);
+                if (dfs(neighbor.first, size - 1, graph, ans)) {
+                    return true;
+                }
+                ans->pop_back();
+                ++(*graph)[start][neighbor.first];
+            }
+        }
+        return false;
+    }
+};
