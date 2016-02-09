eb3b4800c5935de934ae2cd4f65e0edbc969d61e
kamyu <kamyu104@users.noreply.github.com>
2016-02-09 18:09:20 +0800

    Update and rename atoi.cpp to string-to-integer-atoi.cpp

---
+++ C++/string-to-integer-atoi.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int myAtoi(string str) {
+        if (str.empty()) {
+            return 0;
+        }
+
+        int ans = 0;
+        int sign = 1;
+        int i = 0;
+
+        // Skip ' '.
+        while (str[i] == ' ') {
+            ++i;
+        }
+
+        // Parse sign.
+        if (str[i] == '+') {
+            ++i;
+        } else if (str[i] == '-') {
+            sign = -1;
+            ++i;
+        }
+
+        // Compute integer.
+        for (; i < str.length() && isdigit(str[i]); ++i) {
+            if (ans > (numeric_limits<int>::max() - (str[i] - '0')) / 10) { 
+                return sign > 0 ? numeric_limits<int>::max() : numeric_limits<int>::min();
+            }
+            ans *= 10;
+            ans += str[i] - '0';
+        }
+
+        ans *= sign;
+        return ans;
+    }
+};
