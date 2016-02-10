4b16e649d044693abad877b98b2e0812bfcf2d36
kamyu <kamyu104@users.noreply.github.com>
2016-02-10 16:52:02 +0800

    Update longest-common-prefix.cpp

--- C++/longest-common-prefix.cpp
+++ C++/longest-common-prefix.cpp
@@ -10,7 +10,7 @@ public:
 
         for (int i = 0; i < strs[0].length(); ++i) {
             for (const auto& str : strs) {
-                if (str[i] != strs[0][i]) {
+                if (i >= str.length() || str[i] != strs[0][i]) {
                     return strs[0].substr(0, i);
                 }
             }
