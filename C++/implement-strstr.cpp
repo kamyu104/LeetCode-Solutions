e7651f363d2e635dd3149affa1323a3329a4b396
kamyu <kamyu104@users.noreply.github.com>
2016-02-11 11:54:35 +0800

    Update and rename strStr.cpp to implement-strstr.cpp

---
+++ C++/implement-strstr.cpp
@@ -0,0 +1,66 @@
+// Time:  O(n + k)
+// Space: O(k)
+
+// Wiki of KMP algorithm:
+// http://en.wikipedia.org/wiki/Knuth-Morris-Pratt_algorithm
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) {
+            return 0;
+        }
+
+        return KMP(haystack, needle);
+    }
+
+    int KMP(const string& text, const string& pattern) {
+        const vector<int> prefix = getPrefix(pattern);
+        int j = -1;
+        for (int i = 0; i < text.length(); ++i) {
+            while (j > -1 && pattern[j + 1] != text[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == text[i]) {
+                ++j;
+            }
+            if (j == pattern.length() - 1) {
+                return i - j;
+            }
+        }
+        return -1;
+    }
+
+    vector<int> getPrefix(const string& pattern) {
+        vector<int> prefix(pattern.length(), -1);
+        int j = -1;
+        for (int i = 1; i < pattern.length(); ++i) {
+            while (j > -1 && pattern[j + 1] != pattern[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == pattern[i]) {
+                ++j;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+};
+
+
+// Time:  O(n * k)
+// Space: O(k)
+class Solution2 {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) {
+            return 0;
+        }
+
+        for (int i = 0; i + needle.length() < haystack.length() + 1; ++i) {
+            if (haystack.substr(i, needle.length()) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
