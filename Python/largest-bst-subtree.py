1ecf7b92269eada19e2911cdd4ead75086f574af
kamyu <kamyu104@users.noreply.github.com>
2016-02-12 15:59:21 +0800

    Update largest-bst-subtree.py

--- Python/largest-bst-subtree.py
+++ Python/largest-bst-subtree.py
@@ -14,31 +14,27 @@ class Solution(object):
         :type root: TreeNode
         :rtype: int
         """
-        if not root:
+        if root is None:
             return 0
 
         max_size = [1]
         def largestBSTSubtreeHelper(root):
-            if root.left is None and root.right is None:
-                return 1, root.val, root.val
+            if root is None or (root.left is None and root.right is None):
+                return int(root is not None), root.val, root.val
 
-            if root.left is None:
-                size, min_val, max_val = largestBSTSubtreeHelper(root.right)
-                if size > 0 and root.val < min_val:
-                    max_size[0] = max(max_size[0], 1 + size)
-                    return 1 + size, root.val, max_val
-            elif root.right is None:
-                size, min_val, max_val = largestBSTSubtreeHelper(root.left)
-                if size > 0 and max_val < root.val:
-                    max_size[0] = max(max_size[0], 1 + size)
-                    return 1 + size, min_val, root.val
-            else:
+            left_size, left_min, left_max = 0, root.val, root.val - 1
+            if root.left is not None:
                 left_size, left_min, left_max = largestBSTSubtreeHelper(root.left)
+            
+            right_size, right_min, right_max = 0, root.val + 1, root.val
+            if root.right is not None:
                 right_size, right_min, right_max = largestBSTSubtreeHelper(root.right)
-                if left_max < root.val < right_min and left_size > 0 and right_size > 0:
-                    size = 1 + left_size + right_size
-                    max_size[0] = max(max_size[0], size)
-                    return size, left_min, right_max
+
+            if (root.left is None or left_size > 0) and \
+               (root.right is None or right_size > 0) and \
+               left_max < root.val < right_min:
+                max_size[0] = max(max_size[0], 1 + left_size + right_size)
+                return 1 + left_size + right_size, left_min, right_max
 
             return 0, root.val, root.val
 
