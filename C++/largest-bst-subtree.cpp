a870e686c92261e36768e31cbba24472f17e06df
kamyu <kamyu104@users.noreply.github.com>
2016-02-12 16:05:24 +0800

    Update largest-bst-subtree.cpp

--- C++/largest-bst-subtree.cpp
+++ C++/largest-bst-subtree.cpp
@@ -28,29 +28,21 @@ private:
             return make_tuple(1, root->val, root->val);
         }
 
-        if (!root->left) {
-            int size, min_val, max_val;
-            tie(size, min_val, max_val) = largestBSTSubtreeHelper(root->right, max_size);
-            if (size > 0 && root->val < min_val) {
-                *max_size = max(*max_size, 1 + size);
-                return make_tuple(1 + size, root->val, max_val);
-            }
-        } else if (!root->right) {
-            int size, min_val, max_val;
-            tie(size, min_val, max_val) = largestBSTSubtreeHelper(root->left, max_size);
-            if (size > 0 && max_val < root->val) {
-                *max_size = max(*max_size, 1 + size);
-                return make_tuple(1 + size, min_val, root->val);
-            }
-        } else {
-            int left_size, left_min, left_max, right_size, right_min, right_max;
+        int left_size = 0, left_min = root->val, left_max = root->val - 1;
+        if (root->left) {
             tie(left_size, left_min, left_max) = largestBSTSubtreeHelper(root->left, max_size);
+        }
+
+        int right_size = 0, right_min = root->val + 1, right_max = root->val;
+        if (root->right) {
             tie(right_size, right_min, right_max) = largestBSTSubtreeHelper(root->right, max_size);
-            if (left_size > 0 && right_size > 0 &&
-                left_max < root->val && root->val < right_min) {
-                *max_size = max(*max_size, 1 + left_size + right_size);
-                return make_tuple(1 + left_size + right_size, left_min, right_max);
-            }
+        }
+
+        if ((!root->left || left_size > 0) && 
+            (!root->right || right_size > 0) &&
+            left_max < root->val && root->val < right_min) {
+            *max_size = max(*max_size, 1 + left_size + right_size);
+            return make_tuple(1 + left_size + right_size, left_min, right_max);
         }
 
         return make_tuple(0, root->val, root->val);
