d63714484a4e7373d0fc2f5d5cd2b85bbc8c6473
kamyu <kamyu104@users.noreply.github.com>
2016-02-12 16:14:40 +0800

    Update largest-bst-subtree.cpp

--- C++/largest-bst-subtree.cpp
+++ C++/largest-bst-subtree.cpp
@@ -28,23 +28,24 @@ private:
             return make_tuple(1, root->val, root->val);
         }
 
-        int left_size = 0, left_min = root->val, left_max = root->val - 1;
+        int left_size = 0, left_min = root->val, left_max = root->val;
         if (root->left) {
             tie(left_size, left_min, left_max) = largestBSTSubtreeHelper(root->left, max_size);
         }
 
-        int right_size = 0, right_min = root->val + 1, right_max = root->val;
+        int right_size = 0, right_min = root->val, right_max = root->val;
         if (root->right) {
             tie(right_size, right_min, right_max) = largestBSTSubtreeHelper(root->right, max_size);
         }
 
+        int size = 0;
         if ((!root->left || left_size > 0) && 
             (!root->right || right_size > 0) &&
-            left_max < root->val && root->val < right_min) {
-            *max_size = max(*max_size, 1 + left_size + right_size);
-            return make_tuple(1 + left_size + right_size, left_min, right_max);
+            left_max <= root->val && root->val <= right_min) {
+            size = 1 + left_size + right_size;
+            *max_size = max(*max_size, size);
         }
 
-        return make_tuple(0, root->val, root->val);
+        return make_tuple(size, left_min, right_max);
     }
 };
