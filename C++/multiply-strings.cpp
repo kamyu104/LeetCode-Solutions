2e6b2e048fe9233617d94191a59c43567ec18aad
kamyu <kamyu104@users.noreply.github.com>
2016-02-13 11:33:54 +0800

    Update and rename multiply.cpp to multiply-strings.cpp

---
+++ C++/multiply-strings.cpp
@@ -0,0 +1,59 @@
+// Time:  O(m * n)
+// Space: O(m + n)
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        return BigInt(num1) * BigInt(num2);
+    }
+
+    class BigInt {
+    public:
+        BigInt(const string& s) {
+            transform(s.rbegin(), s.rend(), back_inserter(n_), 
+                      [](const char c) { return c - '0';});
+        }
+    
+        operator string() {
+            string s;
+            transform(find_if(n_.rbegin(), prev(n_.rend()),
+                             [](const int i) { return i != 0; }),
+                      n_.rend(), back_inserter(s), 
+                      [](const int i) { return i + '0'; });
+            return s;
+        }
+    
+        BigInt operator*(const BigInt &rhs) const {
+            BigInt z(n_.size() + rhs.size() + 1, 0);
+            for(auto i = 0; i < n_.size(); ++i) {
+                for(auto j = 0; j < rhs.size(); ++j) {
+                    z[i + j] += n_[i] * rhs[j];
+                    z[i + j + 1] += z[i + j] / 10;
+                    z[i + j] %= 10;
+                }
+            }
+            return z;
+        }
+    
+    private:
+        vector<int> n_;
+    
+        BigInt(int num, int val): n_(num, val) {
+        }
+    
+        // Getter.
+        int operator[] (int i) const {
+            return n_[i];
+        }
+    
+        // Setter.
+        int & operator[] (int i) {
+            return n_[i];
+        }
+    
+        size_t size() const {
+            return n_.size();
+        }
+    };
+
+};
