d5fa40b4d47158f6353ba2a65df02c2465db5731
kamyu <kamyu104@users.noreply.github.com>
2016-02-13 12:13:05 +0800

    Update multiply-strings.py

--- Python/multiply-strings.py
+++ Python/multiply-strings.py
@@ -10,23 +10,30 @@ class Solution:
     # @param num1, a string
     # @param num2, a string
     # @return a string
+class Solution(object):
     def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
         num1, num2 = num1[::-1], num2[::-1]
-        result = [0 for i in xrange(len(num1) + len(num2))]
+        res = [0] * (len(num1) + len(num2))
         for i in xrange(len(num1)):
             for j in xrange(len(num2)):
-                result[i + j] += int(num1[i]) * int(num2[j])
-                
-        carry, num3 = 0, []
-        for digit in result:
-            sum = carry + digit
-            carry = sum / 10
-            num3.insert(0, str(sum % 10))
-            
-        while len(num3) > 1 and num3[0] == "0":
-            del num3[0]
-            
-        return ''.join(num3)
+                res[i + j] += int(num1[i]) * int(num2[j])
+                res[i + j + 1] += res[i + j] / 10
+                res[i + j] %= 10
+
+        res.reverse()
+
+        # Skip leading 0s.
+        i = 0
+        while i < len(res) and res[i] == 0:
+            i += 1
+
+        res = ''.join(map(str, res[i:]))
+        return res if res else "0"
 
 if __name__ == "__main__":
     print Solution().multiply("123", "1000")
