ee226e74ffb8689b00aa98d419477173fd04f696
kamyu <kamyu104@users.noreply.github.com>
2016-02-15 17:31:03 +0800

    Update length-of-last-word.cpp

--- C++/length-of-last-word.cpp
+++ C++/length-of-last-word.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        const auto is_space = [](const char c) { return c == ' '; };
+        const auto is_space = [](const char c) { return isspace(c); };
         const auto it = find_if_not(s.rbegin(), s.rend(), is_space);
         const auto jt = find_if(it, s.rend(), is_space);
         return distance(it, jt);
