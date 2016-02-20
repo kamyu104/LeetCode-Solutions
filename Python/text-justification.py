4c6b46ace452a3842aa372ba12833e13d3500f6f
kamyu <kamyu104@users.noreply.github.com>
2016-02-20 16:54:20 +0800

    Update text-justification.py

--- Python/text-justification.py
+++ Python/text-justification.py
@@ -1,15 +1,19 @@
 # Time:  O(n)
-# Space: O(1)
+# Space: O(k), k is maxWidth.
 #
-# Given an array of words and a length L, format the text such that each line has exactly L characters and is fully (left and right) justified.
+# Given an array of words and a length L, format the text such that
+# each line has exactly L characters and is fully (left and right) justified.
 # 
-# You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' 
+# You should pack your words in a greedy approach; that is, pack 
+# as many words as you can in each line. Pad extra spaces ' ' 
 # when necessary so that each line has exactly L characters.
 # 
 # Extra spaces between words should be distributed as evenly as possible. 
-# If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+# If the number of spaces on a line do not divide evenly between words, 
+# the empty slots on the left will be assigned more spaces than the slots on the right.
 # 
-# For the last line of text, it should be left justified and no extra space is inserted between words.
+# For the last line of text, it should be left justified and no extra space
+# is inserted between words.
 # 
 # For example,
 # words: ["This", "is", "an", "example", "of", "text", "justification."]
@@ -23,52 +27,44 @@
 # ]
 # Note: Each word is guaranteed not to exceed L in length.
 
-class Solution:
-    # @param words, a list of strings
-    # @param L, an integer
-    # @return a list of strings
-    def fullJustify(self, words, L):
-        result = []
-        
-        i = 0
-        while i < len(words):
-            # count words in one line
-            size, begin = 0, i
-            while i < len(words):
-                if size == 0:
-                    newsize = len(words[i])
-                else:
-                    newsize = size + len(words[i]) + 1
-                if newsize <= L:
-                    size = newsize
-                else:
-                    break
-                i += 1
-            
-            # count space number    
-            spaceCount = L - size
-            if i - begin - 1 > 0 and i < len(words):
-                everyCount = spaceCount / (i - begin - 1) + 1
-                spaceCount %= i - begin - 1
-            else:
-                everyCount = 1
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        def addSpaces(i, spaceCnt, maxWidth, is_last):
+            if i < spaceCnt:
+                # For the last line of text, it should be left justified,
+                # and no extra space is inserted between words.
+                return 1 if is_last else (maxWidth // spaceCnt) + int(i < maxWidth % spaceCnt)
+            return 0
 
-            # add space
-            j = begin
-            while j < i:
-                if j == begin:
-                    s = words[j]
-                else:
-                    s += ' ' * everyCount
-                    if spaceCount > 0 and i < len(words):
-                        s += ' '
-                        spaceCount -= 1
-                    s += words[j]
-                j += 1
-            s += ' ' * spaceCount
-            result.append(s)
-            
-        return result
+        def connect(words, maxWidth, begin, end, length, is_last):
+            s = []
+            n = end - begin
+            for i in xrange(n):
+                s += words[begin + i],
+                s += ' ' * addSpaces(i, n - 1, maxWidth - length, is_last),
+            # For only one word in a line.
+            line = "".join(s)
+            if len(line) < maxWidth:
+                line += ' ' * (maxWidth - len(line))
+            return line
+
+        res = []
+        begin, length = 0, 0
+        for i in xrange(len(words)):
+            if length + len(words[i]) + (i - begin) > maxWidth:
+                res += connect(words, maxWidth, begin, i, length, False),
+                begin, length = i, 0
+            length += len(words[i])
+
+        # Last line.
+        res += connect(words, maxWidth, begin, len(words), length, True),
+        return res
+
 
 if __name__ == "__main__":
     print Solution().fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16)
