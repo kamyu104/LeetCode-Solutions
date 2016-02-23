788d9ead8213b348184fa907dcfd61ef515c77a6
kamyu <kamyu104@users.noreply.github.com>
2016-02-24 00:11:43 +0800

    Update self-crossing.cpp

--- C++/self-crossing.cpp
+++ C++/self-crossing.cpp
@@ -4,27 +4,32 @@
 class Solution {
 public:
     bool isSelfCrossing(vector<int>& x) {
+        if (x.size() >= 5 && x[3] == x[1] && x[4] + x[0] >= x[2]) {
+            // Crossing in a loop:
+            //     i-2
+            // i-1┌────┐
+            //    └─══>┘i-3
+            //    i i-4  (overlapped)  
+            return true;
+        }
+
         for (int i = 3; i < x.size(); ++i) {
             if (x[i] >= x[i - 2] && x[i - 3] >= x[i - 1]) {
-//                          i-2
-//              case 1 : i-1┌─┐
-//                          └─┼─>i
-//                           i-3
-                return true;
-            } else if (i >= 4 && x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
-//                          i-2
-//              case 2 : i-1┌────┐
-//                          └─══>┘i-3
-//                          i  i-4  (overlapped)  
+                // Crossing in a shrinking spiral:
+                //    i-2
+                // i-1┌─┐
+                //    └─┼─>i
+                //     i-3
                 return true;
             } else if (i >= 5 && x[i - 4] <= x[i - 2] && x[i] + x[i - 4] >= x[i - 2] &&
                        x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3]) {
-//                          i-4
-//                          ┌──┐ 
-//              case 3 :    │i<┼─┐
-//                       i-3│ i-5│i-1
-//                          └────┘
-//                          i-2
+                // Crossing in a growing spiral:
+                //    i-4
+                //    ┌──┐ 
+                //    │i<┼─┐
+                // i-3│ i-5│i-1
+                //    └────┘
+                // i-2
                 return true;
             }
         }
