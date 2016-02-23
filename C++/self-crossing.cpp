ea103e28fc4217234217e73aa4011903e35b16af
kamyu <kamyu104@users.noreply.github.com>
2016-02-23 19:58:24 +0800

    Update self-crossing.cpp

--- C++/self-crossing.cpp
+++ C++/self-crossing.cpp
@@ -6,25 +6,25 @@ public:
     bool isSelfCrossing(vector<int>& x) {
         for (int i = 3; i < x.size(); ++i) {
             if (x[i] >= x[i - 2] && x[i - 3] >= x[i - 1]) {
-//                         i-2
-//             case 1 : i-1┌─┐
-//                         └─┼─>i
-//                          i-3
+//                          i-2
+//              case 1 : i-1┌─┐
+//                          └─┼─>i
+//                           i-3
                 return true;
             } else if (i >= 4 && x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
-//                         i-2
-//             case 2 : i-1┌────┐
-//                         └─══>┘i-3
-//                         i  i-4  (overlapped)  
+//                          i-2
+//              case 2 : i-1┌────┐
+//                          └─══>┘i-3
+//                          i  i-4  (overlapped)  
                 return true;
             } else if (i >= 5 && x[i - 4] <= x[i - 2] && x[i] + x[i - 4] >= x[i - 2] &&
                        x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3]) {
-//                         i-4
-//                         ┌──┐ 
-//             case 3 :    │i<┼─┐
-//                      i-3│ i-5│i-1
-//                         └────┘
-//                         i-2
+//                          i-4
+//                          ┌──┐ 
+//              case 3 :    │i<┼─┐
+//                       i-3│ i-5│i-1
+//                          └────┘
+//                          i-2
                 return true;
             }
         }
