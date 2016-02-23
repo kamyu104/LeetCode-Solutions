21325459a1b7fc6f662d1cce2d42b11ccd650516
kamyu <kamyu104@users.noreply.github.com>
2016-02-23 19:55:51 +0800

    Update self-crossing.cpp

--- C++/self-crossing.cpp
+++ C++/self-crossing.cpp
@@ -12,18 +12,18 @@ public:
 //                          i-3
                 return true;
             } else if (i >= 4 && x[i - 1] == x[i - 3] && x[i] + x[i - 4] >= x[i - 2]) {
-//                           i-2
-//             case 2 : i-1 ┌────┐
-//                          └─══>┘i-3
-//                          i  i-4  (overlapped)  
+//                         i-2
+//             case 2 : i-1┌────┐
+//                         └─══>┘i-3
+//                         i  i-4  (overlapped)  
                 return true;
             } else if (i >= 5 && x[i - 4] <= x[i - 2] && x[i] + x[i - 4] >= x[i - 2] &&
                        x[i - 1] <= x[i - 3] && x[i - 1] + x[i - 5] >= x[i - 3]) {
-//                        i-4
-//                        ┌──┐ 
-//             case 3 :   │i<┼─┐
-//                     i-3│ i-5│i-1
-//                        └────┘
+//                         i-4
+//                         ┌──┐ 
+//             case 3 :    │i<┼─┐
+//                      i-3│ i-5│i-1
+//                         └────┘
 //                         i-2
                 return true;
             }
