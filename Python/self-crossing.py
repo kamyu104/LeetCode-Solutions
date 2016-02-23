3cd21358bf8bf13d55d78743ec04b6f649f28d3b
kamyu <kamyu104@users.noreply.github.com>
2016-02-24 00:19:35 +0800

    Update self-crossing.py

--- Python/self-crossing.py
+++ Python/self-crossing.py
@@ -26,26 +26,30 @@ class Solution(object):
         :type x: List[int]
         :rtype: bool
         """
+        if len(x) >= 5 and x[3] == x[1] and x[4] + x[0] >= x[2]:
+            # Crossing in a loop:
+            #     2
+            # 3 ┌────┐
+            #   └─══>┘1
+            #   4  0  (overlapped)  
+                return True
+
         for i in xrange(3, len(x)):
             if x[i] >= x[i - 2] and x[i - 3] >= x[i - 1]:
-#                           i-2
-#               case 1 : i-1┌─┐
-#                           └─┼─>i
-#                            i-3
-                return True
-            elif i >= 4 and x[i - 1] == x[i - 3] and x[i] + x[i - 4] >= x[i - 2]:
-#                             i-2
-#               case 2 : i-1┌────┐
-#                           └─══>┘i-3
-#                            i  i-4  (overlapped)  
+                # Case 1:
+                #    i-2
+                # i-1┌─┐
+                #    └─┼─>i
+                #     i-3
                 return True
             elif i >= 5 and x[i - 4] <= x[i - 2] and x[i] + x[i - 4] >= x[i - 2] and \
                        x[i - 1] <= x[i - 3] and x[i - 5] + x[i - 1] >= x[i - 3]:
-#                          i-4
-#                          ┌──┐ 
-#               case 3 :   │i<┼─┐
-#                       i-3│ i-5│i-1
-#                          └────┘
-#                          i-2
+                # Case 2:
+                #    i-4
+                #    ┌──┐ 
+                #    │i<┼─┐
+                # i-3│ i-5│i-1
+                #    └────┘
+                #      i-2
                 return True
         return False
