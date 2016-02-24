3c0cd1c55ff8f3b08e060598b44fefc8d460263f
kamyu <kamyu104@users.noreply.github.com>
2016-02-25 00:02:08 +0800

    Update self-crossing.py

--- Python/self-crossing.py
+++ Python/self-crossing.py
@@ -2,22 +2,37 @@
 # Space: O(1)
 
 # You are given an array x of n positive numbers.
-# You start at point (0,0) and moves x[0] metres to
-# the north, then x[1] metres to the west, x[2] metres
-# to the south, x[3] metres to the east and so on.
-# In other words, after each move your direction changes counter-clockwise.
+# You start at point (0,0) and moves x[0] metres to the north,
+# then x[1] metres to the west, x[2] metres to the south,
+# x[3] metres to the east and so on. In other words,
+# after each move your direction changes counter-clockwise.
 #
-# Write a one-pass algorithm with O(1) extra space to determine, 
+# Write a one-pass algorithm with O(1) extra space to determine,
 # if your path crosses itself, or not.
 #
 # Example 1:
-# Given x = [2, 1, 1, 2]
+# Given x = [2, 1, 1, 2],
+# ┌───┐
+# │   │
+# └───┼──>
+#     │
+#
 # Return true (self crossing)
 # Example 2:
-# Given x = [1, 2, 3, 4]
+# Given x = [1, 2, 3, 4],
+# ┌──────┐
+# │      │
+# │
+# │
+# └────────────>
+#
 # Return false (not self crossing)
 # Example 3:
-# Given x = [1, 1, 1, 1]
+# Given x = [1, 1, 1, 1],
+# ┌───┐
+# │   │
+# └───┼>
+#
 # Return true (self crossing)
 
 class Solution(object):
