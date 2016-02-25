8355889d1598b8a77138acf8d7a8ba68d87e5427
kamyu <kamyu104@users.noreply.github.com>
2016-02-25 22:51:02 +0800

    Update compare-version-numbers.py

--- Python/compare-version-numbers.py
+++ Python/compare-version-numbers.py
@@ -2,11 +2,16 @@
 # Space: O(1)
 
 # Compare two version numbers version1 and version1.
-# If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+# If version1 > version2 return 1, if version1 < version2
+# return -1, otherwise return 0.
 # 
-# You may assume that the version strings are non-empty and contain only digits and the . character.
-# The . character does not represent a decimal point and is used to separate number sequences.
-# For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+# You may assume that the version strings are non-empty and
+# contain only digits and the . character.
+# The . character does not represent a decimal point and 
+# is used to separate number sequences.
+# For instance, 2.5 is not "two and a half" or "half way to
+# version three", it is the fifth second-level revision of
+# the second first-level revision.
 # 
 # Here is an example of version numbers ordering:
 # 
