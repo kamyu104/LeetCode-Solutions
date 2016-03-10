4c9e200c6cf65eed86fa356481da8bac5cdc2f15
kamyu <kamyu104@users.noreply.github.com>
2016-03-11 00:54:17 +0800

    Update longest-palindromic-substring.py

--- Python/longest-palindromic-substring.py
+++ Python/longest-palindromic-substring.py
@@ -8,40 +8,45 @@
 
 # Manacher's Algorithm
 # http://leetcode.com/2011/11/longest-palindromic-substring-part-ii.html
-class Solution:
+
+class Solution(object):
     def longestPalindrome(self, s):
-        string = self.preProcess(s)
-        palindrome = [0] * len(string) 
+        """
+        :type s: str
+        :rtype: str
+        """
+        def preProcess(s):
+            if not s:
+                return "^$"
+            T = "^"
+            for i in s:
+                T +=  "#" + i
+            T += "#$"
+            return T
+
+        T = preProcess(s)
+        P = [0] * len(T) 
         center, right = 0, 0
-        for i in xrange(1, len(string) - 1):
+        for i in xrange(1, len(T) - 1):
             i_mirror = 2 * center - i
             if right > i:
-                palindrome[i] = min(right - i, palindrome[i_mirror])
+                P[i] = min(right - i, P[i_mirror])
             else:
-                palindrome[i] = 0
+                P[i] = 0
 
-            while string[i + 1 + palindrome[i]] == string[i - 1 - palindrome[i]]:
-                palindrome[i] += 1
+            while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+                P[i] += 1
 
-            if i + palindrome[i] > right:
-                center, right = i, i + palindrome[i]       
+            if i + P[i] > right:
+                center, right = i, i + P[i]       
         
-        max_len, max_center = 0, 0
-        for i in xrange(1, len(string) - 1):
-            if palindrome[i] > max_len:
-                max_len = palindrome[i]
-                max_center = i
-        start = (max_center - 1 - max_len) / 2
-        return s[start : start + max_len]
-    
-    def preProcess(self, s):
-        if not s:
-            return "^$"
-        string = "^"
-        for i in s:
-            string +=  "#" + i
-        string += "#$"
-        return string
+        max_i = 0
+        for i in xrange(1, len(T) - 1):
+            if P[i] > P[max_i]:
+                max_i = i
+        start = (max_i - 1 - P[max_i]) / 2
+        return s[start : start + P[max_i]]
+
     
 if __name__ == "__main__":
     print Solution().longestPalindrome("abb")
