c3511720181273c7f06e59cd3f9b3252e832ff7b
kamyu <kamyu104@users.noreply.github.com>
2016-03-13 11:23:17 +0800

    Update palindrome-pairs.py

--- Python/palindrome-pairs.py
+++ Python/palindrome-pairs.py
@@ -38,6 +38,57 @@ class Solution(object):
                     res.append([lookup[prefix[::-1]], i])
         return res
 
+# Time:  O(n * k^2), n is the number of the words, k is the max length of the words.
+# Space: O(n * k^2)
+# Manacher solution.
+class Solution_TLE(object):
+    def palindromePairs(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[int]]
+        """
+        def manacher(s, P):
+            def preProcess(s):
+                if not s:
+                    return ['^', '$']
+                T = ['^']
+                for c in s:
+                    T +=  ["#", c]
+                T += ['#', '$']
+                return T
+    
+            T = preProcess(s)
+            center, right = 0, 0
+            for i in xrange(1, len(T) - 1):
+                i_mirror = 2 * center - i
+                if right > i:
+                    P[i] = min(right - i, P[i_mirror])
+                else:
+                    P[i] = 0
+                while T[i + 1 + P[i]] == T[i - 1 - P[i]]:
+                    P[i] += 1
+                if i + P[i] > right:
+                    center, right = i, i + P[i]
+
+        prefix, suffix = collections.defaultdict(list), collections.defaultdict(list)
+        for i, word in enumerate(words):
+            P = [0] * (2 * len(word) + 3)
+            manacher(word, P)
+            for j in xrange(len(P)):
+                if j - P[j] == 1:
+                    prefix[word[(j + P[j]) / 2:]].append(i)
+                if j + P[j] == len(P) - 2:
+                    suffix[word[:(j - P[j]) / 2]].append(i)
+        res = []
+        for i, word in enumerate(words):
+            for j in prefix[word[::-1]]:
+                if j != i:
+                    res.append([i, j])
+            for j in suffix[word[::-1]]:
+                if len(word) != len(words[j]):
+                    res.append([j, i])
+        return res
+
 
 # Time:  O(n * k^2), n is the number of the words, k is the max length of the words.
 # Space: O(n * k)
