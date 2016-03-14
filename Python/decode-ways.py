71304b64e5c936f0d261a7cc8cfed6a7971450fc
kamyu <kamyu104@users.noreply.github.com>
2016-03-14 21:40:07 +0800

    Update decode-ways.py

--- Python/decode-ways.py
+++ Python/decode-ways.py
@@ -22,7 +22,7 @@ class Solution:
         if len(s) == 0 or s[0] == '0':
             return 0
         prev, prev_prev = 1, 0
-        for i in range(len(s)):
+        for i in xrange(len(s)):
             current = 0
             if s[i] != '0':
                 current = prev
