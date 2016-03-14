09bb67ded70a8b9d999d96d4415d2bbe8b7cab2f
kamyu <kamyu104@users.noreply.github.com>
2016-03-14 21:41:17 +0800

    Update decode-ways.py

--- Python/decode-ways.py
+++ Python/decode-ways.py
@@ -15,22 +15,25 @@
 # The number of ways decoding "12" is 2.
 #
 
-class Solution:
-    # @param s, a string
-    # @return an integer
+class Solution(object):
     def numDecodings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
         if len(s) == 0 or s[0] == '0':
             return 0
         prev, prev_prev = 1, 0
         for i in xrange(len(s)):
-            current = 0
+            cur = 0
             if s[i] != '0':
-                current = prev
+                cur = prev
             if i > 0 and (s[i - 1] == '1' or (s[i - 1] == '2' and s[i] <= '6')):
-                current += prev_prev
-            prev, prev_prev = current, prev
+                cur += prev_prev
+            prev, prev_prev = cur, prev
         return prev
-            
+
+
 if __name__ == "__main__":
     for i in ["0", "10", "10", "103", "1032", "10323"]:
         print Solution().numDecodings(i)
