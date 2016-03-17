6615eb5e2fbcef0c127f50ffbdc85e5d9cb73848
kamyu <kamyu104@users.noreply.github.com>
2016-03-18 00:21:01 +0800

    Update counting-bits.cpp

--- C++/counting-bits.cpp
+++ C++/counting-bits.cpp
@@ -5,6 +5,19 @@ class Solution {
 public:
     vector<int> countBits(int num) {
         vector<int> res{0};
+        for (int i = 1; i <= num; ++i) {
+            res.emplace_back(res[i >> 1] + (i & 1));
+        }
+        return res;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    vector<int> countBits(int num) {
+        vector<int> res{0};
         for (int i = 0, cnt = res.size();
              res.size() <= num;
              i = (i + 1) % cnt) {
