49f2297bcec818e87d9d65bc1b26ea8fba2dd471
kamyu <kamyu104@users.noreply.github.com>
2016-03-20 13:42:19 +0800

    Create remove-linked-list-elements.cpp

---
+++ C++/remove-linked-list-elements.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        auto dummy = ListNode(0);
+        dummy.next = head;
+        auto *prev = &dummy, *cur = dummy.next;
+        
+        while (cur) {
+            if (cur->val == val) {
+                prev->next = cur->next;
+                delete cur;
+            } else {
+                prev = cur;
+            }
+            cur = cur->next;
+        }
+        return dummy.next;
+    }
+};
