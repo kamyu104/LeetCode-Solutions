2c95e0062c350fa8a76f9fd86e3782372927c660
kamyu <kamyu104@users.noreply.github.com>
2016-03-26 19:40:28 +0800

    Update and rename longestValidParentheses.cpp to longest-valid-parentheses.cpp

---
+++ C++/longest-valid-parentheses.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int longestValidParentheses(string s) {
+        return max(length(s.begin(), s.end(), '('), length(s.rbegin(), s.rend(), ')'));
+    }
+
+private:
+    template<typename T>
+    int length(T begin, T end, char c) {
+        int len = 0, depth = 0;
+        T start = prev(begin);
+        for (T it = begin; it != end; ++it) {
+            if (*it == c) {
+                ++depth;
+            } else {
+                --depth;
+                if (depth < 0) {
+                    start = it;
+                    depth = 0;
+                } else {
+                    if (depth == 0) {
+                        len = max(len, static_cast<int>(distance(start, it)));
+                    }
+                }
+            }
+        }
+        return len;
+    }
+};
