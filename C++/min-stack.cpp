eb26e5c67f8ade5f000733d8f6310cfe5391519e
kamyu <kamyu104@users.noreply.github.com>
2016-03-31 23:45:53 +0800

    Update min-stack.cpp

--- C++/min-stack.cpp
+++ C++/min-stack.cpp
@@ -1,7 +1,49 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(1)
 
 class MinStack {
+public:
+    void push(int number) {
+        if (elements_.empty()) {
+            elements_.emplace(0);
+            stack_min_ = number;
+        } else {
+            elements_.emplace(static_cast<int64_t>(number) - stack_min_);
+            if (number < stack_min_) {
+                stack_min_ = number; // Update min.
+            }
+        }
+    }
+
+    void pop() {
+        auto diff = elements_.top();
+        elements_.pop();
+        if (diff < 0) {
+            stack_min_ -= diff; // Restore previous min.
+        }
+    }
+
+    int top() {
+        if (elements_.top() > 0) {
+            return stack_min_ + elements_.top();
+        } else {
+            return stack_min_;
+        }
+    }
+
+    int getMin() {
+        return stack_min_;
+    }
+
+private:
+    stack<int64_t> elements_;
+    int stack_min_;
+};
+
+
+// Time:  O(n)
+// Space: O(n)
+class MinStack2 {
 public:
     void push(int number) {
         if (cached_min_with_count_.empty() || cached_min_with_count_.top().first > number) {
