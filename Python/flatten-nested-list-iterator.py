459d5661d0750e830d9bfbadaa5c6464aae4f539
kamyu <kamyu104@users.noreply.github.com>
2016-04-05 21:25:18 +0800

    Create flatten-nested-list-iterator.py

---
+++ Python/flatten-nested-list-iterator.py
@@ -0,0 +1,63 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger(object):
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class NestedIterator(object):
+
+    def __init__(self, nestedList):
+        """
+        Initialize your data structure here.
+        :type nestedList: List[NestedInteger]
+        """
+        self.__nodes = [[nestedList, 0]]
+
+
+    def next(self):
+        """
+        :rtype: int
+        """
+        nestedList, i = self.__nodes[-1]
+        self.__nodes[-1][1] += 1
+        return nestedList[i].getInteger()
+
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        while self.__nodes:
+            nestedList, i = self.__nodes[-1]
+            if i == len(nestedList):
+                self.__nodes.pop()
+            elif nestedList[i].isInteger():
+                    return True
+            else:
+                self.__nodes[-1][1] += 1
+                self.__nodes.append([nestedList[i].getList(), 0])
+        return False
+ 
+
+# Your NestedIterator object will be instantiated and called as such:
+# i, v = NestedIterator(nestedList), []
+# while i.hasNext(): v.append(i.next())
