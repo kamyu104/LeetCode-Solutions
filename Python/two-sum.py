8c55a091c3c8246632cd7830b6f0518a5769b850
kamyu <kamyu104@users.noreply.github.com>
2016-04-15 14:23:17 +0800

    Update two-sum.py

--- Python/two-sum.py
+++ Python/two-sum.py
@@ -1,25 +1,32 @@
 # Time:  O(n)
 # Space: O(n)
+
+# Given an array of integers, return indices of the two numbers
+# such that they add up to a specific target.
 #
-# Given an array of integers, find two numbers such that 
-# they add up to a specific target number.
-# The function twoSum should return indices of the two numbers such that
-# they add up to the target, 
-# where index1 must be less than index2. Please note that 
-# your returned answers (both index1 and index2) are not zero-based.
 # You may assume that each input would have exactly one solution.
 #
-# Input: numbers={2, 7, 11, 15}, target=9
-# Output: index1=1, index2=2
+# Example:
+# Given nums = [2, 7, 11, 15], target = 9,
 #
+# Because nums[0] + nums[1] = 2 + 7 = 9,
+# return [0, 1].
 
-class Solution:
+
+class Solution(object):
     def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
         lookup = {}
         for i, num in enumerate(nums):
             if target - num in lookup:
-                return (lookup[target - num] + 1, i + 1)
+                return [lookup[target - num], i]
             lookup[num] = i
+        return []
+
 
 if __name__ == '__main__':
     print "index1=%d, index2=%d" % Solution().twoSum((2, 7, 11, 15), 9)
