1d2923390ab3487eafba229fd217c99597a010cc
kamyu <kamyu104@users.noreply.github.com>
2016-04-18 10:53:58 +0800

    Create power-of-four.py

---
+++ Python/power-of-four.py
@@ -0,0 +1,19 @@
+# Time:  O(logn)
+# Space: O(logn)
+
+# Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+#
+# Example:
+# Given num = 16, return true. Given num = 5, return false.
+#
+# Follow up: Could you solve it without loops/recursion?
+
+class Solution(object):
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        while num and not (num & 3):
+            num >>= 2
+        return (num == 1)
