7a437bb9a7c0539b722ef07f70abfd18a8232ce4
kamyu <kamyu104@users.noreply.github.com>
2016-04-18 11:06:40 +0800

    Update power-of-four.cpp

--- C++/power-of-four.cpp
+++ C++/power-of-four.cpp
@@ -4,6 +4,16 @@
 class Solution {
 public:
     bool isPowerOfFour(int num) {
+        return num > 0 && (num & (num - 1)) == 0 &&
+               ((num & 0b101010101010101010101010101010101) == num);
+    }
+};
+
+// Time:  O(1)
+// Space: O(1)
+class Solution2 {
+public:
+    bool isPowerOfFour(int num) {
         while (num && !(num & 0b11)) {
             num >>= 2;
         }
