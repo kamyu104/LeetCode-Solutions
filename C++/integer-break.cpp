dfa3eee61455ab168e6255bd1cd1ecd26deab5bd
kamyu <kamyu104@users.noreply.github.com>
2016-04-19 22:56:45 +0800

    Update integer-break.cpp

--- C++/integer-break.cpp
+++ C++/integer-break.cpp
@@ -35,7 +35,7 @@ public:
         if (n % 3 == 0) {         // n = 3Q + 0, the max is 3^Q * 2^0
             res = pow(3, n / 3);
         } else if (n % 3 == 2) {  // n = 3Q + 2, the max is 3^Q * 2^1
-            res =  pow(3, n / 3) * 2;
+            res = pow(3, n / 3) * 2;
         } else {                  // n = 3Q + 4, the max is 3^Q * 2^2
             res = pow(3, n / 3 - 1) * 4;
         }
