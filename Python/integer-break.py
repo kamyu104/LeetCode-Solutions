de9ccc87888292ab1fc4bc140f033c0e02302cd9
kamyu <kamyu104@users.noreply.github.com>
2016-04-19 22:57:07 +0800

    Update integer-break.py

--- Python/integer-break.py
+++ Python/integer-break.py
@@ -52,7 +52,7 @@ class Solution(object):
         if n % 3 == 0:            #  n = 3Q + 0, the max is 3^Q * 2^0
             res = 3 ** (n // 3)
         elif n % 3 == 2:          #  n = 3Q + 2, the max is 3^Q * 2^1
-            res =  3 ** (n // 3) * 2
+            res = 3 ** (n // 3) * 2
         else:                     #  n = 3Q + 4, the max is 3^Q * 2^2
             res = 3 ** (n // 3 - 1) * 4
         return res
