769f18c10bb4fda18b478410f55012e75b31f51b
kamyu <kamyu104@users.noreply.github.com>
2016-04-20 00:37:45 +0800

    Update integer-break.py

--- Python/integer-break.py
+++ Python/integer-break.py
@@ -70,8 +70,7 @@ class Solution2(object):
             return n - 1
 
         # integerBreak(n) = max(integerBreak(n - 2) * 2, integerBreak(n - 3) * 3)
-        res = [0] * 4
-        res[1 % 4], res[2 % 4], res[3 % 4] = 1, 2, 3
+        res = [0, 1, 2, 3]
         for i in xrange(4, n + 1):
             res[i % 4] = max(res[(i - 2) % 4] * 2, res[(i - 3) % 4] * 3)
         return res[n % 4]
