b037d01dacea526e175f845742e1db09ff399655
kamyu <kamyu104@users.noreply.github.com>
2016-04-22 20:58:03 -0500

    Update rotate-image.cpp

--- C++/rotate-image.cpp
+++ C++/rotate-image.cpp
@@ -7,7 +7,7 @@ public:
         const int n = matrix.size();
         for (int i = 0; i < n / 2; ++i) {
             for (int j = i; j < n - 1 - i; ++j) {
-                int tmp = matrix[i][j];
+                const auto tmp = matrix[i][j];
                 matrix[i][j] = matrix[n - 1 - j][i];
                 matrix[n - 1- j][i] = matrix[n - 1 - i][n - 1 - j];
                 matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
