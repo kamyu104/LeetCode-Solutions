928d6a159b246a5320b396c909fcc4b4a670b0ec
kamyu <kamyu104@users.noreply.github.com>
2016-04-23 23:44:17 +0800

    Update reverse-vowels-of-a-string.cpp

--- C++/reverse-vowels-of-a-string.cpp
+++ C++/reverse-vowels-of-a-string.cpp
@@ -5,13 +5,12 @@ class Solution {
 public:
     string reverseVowels(string s) {
         for (int i = 0, j = s.length() - 1; i < j;) {
-            if (is_vowel(tolower(s[i])) && 
-                is_vowel(tolower(s[j]))) {
-                swap(s[i++], s[j--]);
-            } else if (!is_vowel(tolower(s[i]))) {
+            if (!is_vowel(tolower(s[i]))) {
                 ++i;
-            } else {
+            } else if (!is_vowel(tolower(s[j]))) {
                 --j;
+            } else {
+                swap(s[i++], s[j--]);
             }
         }
         return s;
