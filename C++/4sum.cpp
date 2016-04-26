3532497fb5770f2b1906a38cda9d98b2ce27cbda
kamyu <kamyu104@users.noreply.github.com>
2016-04-26 18:00:05 +0800

    Update 4sum.cpp

--- C++/4sum.cpp
+++ C++/4sum.cpp
@@ -1,7 +1,56 @@
-// Time:  O(n^4)
+// Time:  O(n^3)
 // Space: O(n^2)
 
 class Solution {
+public:
+    vector<vector<int> > fourSum(vector<int> &nums, int target) {
+        int len = nums.size();
+        int left, right, sum;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> tmp;
+        for (int i = 0; i < len - 3; ++i) {
+            if (i && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < len - 2; ++j) {
+                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                sum = target - nums[i] - nums[j];
+                left = j + 1, right = len - 1;
+                while (left < right) {
+                    if (nums[left] + nums[right] == sum) {
+                        tmp.clear();
+                        tmp.emplace_back(nums[i]);
+                        tmp.emplace_back(nums[j]);
+                        tmp.emplace_back(nums[left]);
+                        tmp.emplace_back(nums[right]);
+                        res.emplace_back(tmp);
+                        left++, right--;
+                        while (left < right && nums[left] == nums[left - 1]) {
+                            ++left;
+                        }
+                        while (left < right && nums[right] == nums[right + 1]) {
+                            --right;
+                        }
+                    } else {
+                        if (nums[left] + nums[right] > sum) {
+                            --right;
+                        } else {
+                            ++left;
+                        }
+                    }
+                }
+            }
+        }
+        return res;
+    }
+};
+
+// Time:  O(n^4)
+// Space: O(n^2)
+class Solution2 {
 public:
     vector<vector<int> > fourSum(vector<int> &num, int target) {
         vector<vector<int>> ans;
