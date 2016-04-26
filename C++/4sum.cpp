42788eb2b60ce10427a8bd9c33d7fc4ccbafd03f
kamyu <kamyu104@users.noreply.github.com>
2016-04-26 22:19:34 +0800

    Update 4sum.cpp

--- C++/4sum.cpp
+++ C++/4sum.cpp
@@ -3,39 +3,33 @@
 
 class Solution {
 public:
-    vector<vector<int> > fourSum(vector<int> &nums, int target) {
-        int len = nums.size();
+    vector<vector<int> > fourSum(vector<int> &num, int target) {
+        int len = num.size();
         int left, right, sum;
-        sort(nums.begin(), nums.end());
+        sort(num.begin(), num.end());
         vector<vector<int>> res;
-        vector<int> tmp;
         for (int i = 0; i < len - 3; ++i) {
-            if (i && nums[i] == nums[i - 1]) {
+            if (i && num[i] == num[i - 1]) {
                 continue;
             }
             for (int j = i + 1; j < len - 2; ++j) {
-                if (j != i + 1 && nums[j] == nums[j - 1]) {
+                if (j != i + 1 && num[j] == num[j - 1]) {
                     continue;
                 }
-                sum = target - nums[i] - nums[j];
+                sum = target - num[i] - num[j];
                 left = j + 1, right = len - 1;
                 while (left < right) {
-                    if (nums[left] + nums[right] == sum) {
-                        tmp.clear();
-                        tmp.emplace_back(nums[i]);
-                        tmp.emplace_back(nums[j]);
-                        tmp.emplace_back(nums[left]);
-                        tmp.emplace_back(nums[right]);
-                        res.emplace_back(tmp);
-                        left++, right--;
-                        while (left < right && nums[left] == nums[left - 1]) {
+                    if (num[left] + num[right] == sum) {
+                        res.emplace_back(move(vector<int>{num[i], num[j], num[left], num[right]}));
+                        ++left, --right;
+                        while (left < right && num[left] == num[left - 1]) {
                             ++left;
                         }
-                        while (left < right && nums[right] == nums[right + 1]) {
+                        while (left < right && num[right] == num[right + 1]) {
                             --right;
                         }
                     } else {
-                        if (nums[left] + nums[right] > sum) {
+                        if (num[left] + num[right] > sum) {
                             --right;
                         } else {
                             ++left;
