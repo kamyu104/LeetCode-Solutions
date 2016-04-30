98776db567f644e878a5b95a318ac531dfe5f91c
kamyu <kamyu104@users.noreply.github.com>
2016-04-30 11:19:20 +0800

    Update and rename isValidSudoku.cpp to valid-sudoku.cpp

---
+++ C++/valid-sudoku.cpp
@@ -0,0 +1,49 @@
+// Time:  O(9^2)
+// Space: O(9)
+
+class Solution {
+public:
+    bool isValidSudoku(const vector<vector<char>>& board) {
+        // Check row constraints.
+        for (int i = 0; i < board.size(); ++i) {
+            if (anyDuplicate(board, i, i + 1, 0, board.size(), board.size())) {
+                return false;
+            }
+        }
+
+        // Check column constraints.
+        for (int j = 0; j < board.size(); ++j) {
+            if (anyDuplicate(board, 0, board.size(), j, j + 1, board.size())) {
+                return false;
+            }
+        }
+
+        // Check region constraints.
+        int region_size = sqrt(board.size());
+        for (int i = 0; i < region_size; ++i) {
+            for (int j = 0; j < region_size; ++j) {
+                if (anyDuplicate(board, region_size * i, region_size * (i + 1),
+                                 region_size * j, region_size * (j + 1), board.size())) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // Return true if subarray board[start_row : end_row - 1][start_col : end_col - 1]
+    // contains any duplicates in [1 : num_elements]; otherwise return false.
+    bool anyDuplicate(const vector<vector<char>>& board, int start_row, int end_row,
+                      int start_col, int end_col, int num_elements) {
+        deque<bool> is_present(num_elements + 1, false);
+        for (int i = start_row; i < end_row; ++i) {
+            for (int j = start_col; j < end_col; ++j) {
+                if (board[i][j] != '.' && is_present[board[i][j] - '0']) {
+                    return true;
+                }
+                is_present[board[i][j] - '0'] = true;
+            }
+        }
+        return false;
+    }
+};
