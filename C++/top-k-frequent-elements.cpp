35f7f0acfe3caa054fa246d7639a04b082e73561
kamyu <kamyu104@users.noreply.github.com>
2016-05-01 21:44:56 -0500

    Update top-k-frequent-elements.cpp

--- C++/top-k-frequent-elements.cpp
+++ C++/top-k-frequent-elements.cpp
@@ -8,6 +8,7 @@ public:
         for (const auto& i : nums) {
             ++counts[i];
         }
+
         vector<pair<int, int>> p;
         for (auto it = counts.begin(); it != counts.end(); ++it) {
             p.emplace_back(-(it->second), it->first);
