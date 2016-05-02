b1f111a0f140f265af60f9186130b9a0a90093a1
kamyu <kamyu104@users.noreply.github.com>
2016-05-02 10:34:05 +0800

    Create top-k-frequent-elements.py

---
+++ Python/top-k-frequent-elements.py
@@ -0,0 +1,65 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Given a non-empty array of integers,
+# return the k most frequent elements.
+#
+# For example,
+# Given [1,1,1,2,2,3] and k = 2, return [1,2].
+#
+# Note: 
+# You may assume k is always valid,
+# 1 <= k <= number of unique elements.
+# Your algorithm's time complexity must be better
+# than O(n log n), where n is the array's size.
+
+from random import randint
+
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        counts = collections.defaultdict(int)
+        for i in nums:
+            counts[i] += 1
+
+        p = []
+        for key, val in counts.iteritems():
+            p.append((val, key))
+
+        self.kthElement(p, k);
+
+        result = []
+        for i in xrange(k):
+            result.append(p[i][1])
+
+        return result
+
+
+    def kthElement(self, nums, k):
+        def PartitionAroundPivot(left, right, pivot_idx, nums):
+            pivot_value = nums[pivot_idx][0]
+            new_pivot_idx = left
+            nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+            for i in xrange(left, right):
+                if nums[i][0] > pivot_value:
+                    nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                    new_pivot_idx += 1
+                
+            nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+            return new_pivot_idx
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            pivot_idx = randint(left, right)
+            new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, nums)
+            if new_pivot_idx == k - 1:
+                return
+            elif new_pivot_idx > k - 1:
+                right = new_pivot_idx - 1
+            else:  # new_pivot_idx < k - 1.
+                left = new_pivot_idx + 1
+  
