196d5663d27a3d0356f7b7ec921d1287e2564b45
kamyu <kamyu104@users.noreply.github.com>
2016-05-04 01:05:20 +0800

    Update subsets.cpp

--- C++/subsets.cpp
+++ C++/subsets.cpp
@@ -1,25 +1,36 @@
-// Time Complexity: O(2^n)
-// Space Complexity: O(1)
+// Time: O(n * 2^n)
+// Space: O(1)
 
 class Solution {
-    public:
-        vector<vector<int> > subsets(vector<int> &S) {
-            const int size = S.size();
-            const int setSize = 1 << size;
-            vector<vector<int> > ans;
-            vector<int> v;
-
-            sort(S.begin(), S.end());
-
-            for(int i = 0; i < setSize; ++i) {
-                for(int j = 0; j < size; j++) {
-                    if(i & (1 << j))
-                        v.push_back(S[j]);
-                }
-                ans.push_back(v);
-                v.clear();
+public:
+    vector<vector<int> > subsets(vector<int> &nums) {
+        vector<vector<int>> result(1);
+        sort(nums.begin(), nums.end());
+        for (size_t i = 0; i < nums.size(); ++i) {
+            const size_t size = result.size();
+            for (size_t j = 0; j < size; ++j) {
+                result.emplace_back(result[j]);
+                result.back().emplace_back(nums[i]);
             }
-
-            return ans;
         }
+        return result;
+    }
+};
+
+// Time: O(n * 2^n)
+// Space: O(1)
+class Solution2 {
+public:
+    vector<vector<int> > subsets(vector<int> &nums) {
+        vector<vector<int>> result(1);
+        sort(nums.begin(), nums.end());
+        for (size_t i = 0; i < nums.size(); ++i) {
+            const size_t size = result.size();
+            for (size_t j = 0; j < size; ++j) {
+                result.emplace_back(result[j]);
+                result.back().emplace_back(nums[i]);
+            }
+        }
+        return result;
+    }
 };
