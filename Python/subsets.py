5ad451c8f29ed2a0b687c6d495c8c1c941ffdff6
kamyu <kamyu104@users.noreply.github.com>
2016-05-04 00:51:08 +0800

    Update subsets.py

--- Python/subsets.py
+++ Python/subsets.py
@@ -1,6 +1,6 @@
 # Time:  O(n * 2^n)
 # Space: O(1)
-#
+
 # Given a set of distinct integers, S, return all possible subsets.
 # 
 # Note:
@@ -19,37 +19,62 @@
 #   [1,2],
 #   []
 # ]
-#
 
-class Solution:
-    # @param S, a list of integer
-    # @return a list of lists of integer
-    def subsets(self, S):
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = [[]]
+        for i in xrange(len(nums)):
+            size = len(result)
+            for j in xrange(size):
+                result.append(list(result[j]))
+                result[-1].append(nums[i])
+        return result
+
+
+# Time:  O(n * 2^n)
+# Space: O(1)
+class Solution2(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         result = []
-        i, count = 0, 1 << len(S)
-        S = sorted(S)
+        i, count = 0, 1 << len(nums)
+        nums.sort()
         
         while i < count:
             cur = []
-            for j in xrange(len(S)):
+            for j in xrange(len(nums)):
                 if i & 1 << j:
-                    cur.append(S[j])
+                    cur.append(nums[j])
             result.append(cur)
             i += 1
             
         return result
 
-class Solution2:
-    # @param S, a list of integer
-    # @return a list of lists of integer
-    def subsets(self, S):
-        return self.subsetsRecu([], sorted(S))
+
+# Time:  O(n * 2^n)
+# Space: O(1)
+class Solution3(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return self.subsetsRecu([], sorted(nums))
     
-    def subsetsRecu(self, cur, S):
-        if not S:
+    def subsetsRecu(self, cur, nums):
+        if not nums:
             return [cur]
         
-        return self.subsetsRecu(cur, S[1:]) + self.subsetsRecu(cur + [S[0]], S[1:])
-        
+        return self.subsetsRecu(cur, nums[1:]) + self.subsetsRecu(cur + [nums[0]], nums[1:])
+
+
 if __name__ == "__main__":
     print Solution().subsets([1, 2, 3])
