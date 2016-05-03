32c23719281616e7baed6209ed85ac40290a05be
kamyu <kamyu104@users.noreply.github.com>
2016-05-04 01:08:30 +0800

    Update subsetsWithDup.cpp

--- C++/subsetsWithDup.cpp
+++ C++/subsetsWithDup.cpp
@@ -1,23 +1,23 @@
-// Time Complexity: O(2^n)
-// Space Complexity: O(1)
+// Time:  O(n * 2^n)
+// Space: O(1)
 
 class Solution {
-    public:
-        vector<vector<int> > subsetsWithDup(vector<int> &S) {
-            sort(S.begin(), S.end());
-            vector<vector<int> > result(1);
-            size_t previous_size = 0;
-            for (size_t i = 0; i < S.size(); ++i) {
-                const size_t size = result.size();
-                for (size_t j = 0; j < size; ++j) {
-                    // only union non-duplicate element or new union set 
-                    if (i == 0 || S[i] != S[i-1] || j >= previous_size) { 
-                        result.push_back(result[j]);
-                        result.back().push_back(S[i]);
-                    }
+public:
+    vector<vector<int>> subsetsWithDup(vector<int> &nums) {
+        vector<vector<int>> result(1);
+        sort(nums.begin(), nums.end());
+        size_t previous_size = 0;
+        for (size_t i = 0; i < nums.size(); ++i) {
+            const size_t size = result.size();
+            for (size_t j = 0; j < size; ++j) {
+                // Only union non-duplicate element or new union set.
+                if (i == 0 || nums[i] != nums[i - 1] || j >= previous_size) {
+                    result.emplace_back(result[j]);
+                    result.back().emplace_back(nums[i]);
                 }
-                previous_size = size;
             }
-            return result;
+            previous_size = size;
         }
+        return result;
+    }
 };
