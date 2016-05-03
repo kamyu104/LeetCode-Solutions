a41bf1bedd53102adb30051d7f765335131e5581
kamyu <kamyu104@users.noreply.github.com>
2016-05-04 00:45:08 +0800

    Update subsets-ii.py

--- Python/subsets-ii.py
+++ Python/subsets-ii.py
@@ -1,6 +1,6 @@
 # Time:  O(n * 2^n)
 # Space: O(1)
-#
+
 # Given a collection of integers that might contain duplicates, S, return all possible subsets.
 # 
 # Note:
@@ -17,41 +17,67 @@
 #   [1,2],
 #   []
 # ]
-#
 
-class Solution:
-    # @param num, a list of integer
-    # @return a list of lists of integer
-    def subsetsWithDup(self, S):
+class Solution(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = [[]]
+        previous_size = 0
+        for i in xrange(len(nums)):
+            size = len(result)
+            for j in xrange(size):
+                # Only union non-duplicate element or new union set.
+                if i == 0 or nums[i] != nums[i - 1] or j >= previous_size:
+                    result.append(list(result[j]))
+                    result[-1].append(nums[i])
+            previous_size = size
+        return result
+
+
+class Solution2(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         result = []
-        i, count = 0, 1 << len(S)
-        S = sorted(S)
+        i, count = 0, 1 << len(nums)
+        nums.sort()
         
         while i < count:
             cur = []
-            for j in xrange(len(S)):
+            for j in xrange(len(nums)):
                 if i & 1 << j:
-                    cur.append(S[j])
+                    cur.append(nums[j])
             if cur not in result:
                 result.append(cur)
             i += 1
             
         return result
 
-class Solution2:
-    # @param num, a list of integer
-    # @return a list of lists of integer
-    def subsetsWithDup(self, S):
+
+class Solution3(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
         result = []
-        self.subsetsWithDupRecu(result, [], sorted(S))
+        self.subsetsWithDupRecu(result, [], sorted(nums))
         return result
     
-    def subsetsWithDupRecu(self, result, cur, S):
-        if len(S) == 0  and cur not in result:
-            result.append(cur)
-        elif S:
-            self.subsetsWithDupRecu(result, cur, S[1:])
-            self.subsetsWithDupRecu(result, cur + [S[0]], S[1:])
-        
+    def subsetsWithDupRecu(self, result, cur, nums):
+        if not nums:
+            if cur not in result:
+                result.append(cur)
+        else:
+            self.subsetsWithDupRecu(result, cur, nums[1:])
+            self.subsetsWithDupRecu(result, cur + [nums[0]], nums[1:])
+
+   
 if __name__ == "__main__":
     print Solution().subsetsWithDup([1, 2, 2])
