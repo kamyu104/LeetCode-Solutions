082517981831ff7e13bf79025f474b47f61b3c7e
kamyu <kamyu104@users.noreply.github.com>
2016-05-05 21:01:19 +0800

    Create design-tic-tac-toe.py

---
+++ Python/design-tic-tac-toe.py
@@ -0,0 +1,49 @@
+# Time:  O(1), per move.
+# Space: O(n^2)
+
+class TicTacToe(object):
+
+    def __init__(self, n):
+        """
+        Initialize your data structure here.
+        :type n: int
+        """
+        self.__rows = [[0, 0] for _ in xrange(n)]
+        self.__cols = [[0, 0] for _ in xrange(n)]
+        self.__diagonal = [0, 0]
+        self.__anti_diagonal = [0, 0]
+        
+
+    def move(self, row, col, player):
+        """
+        Player {player} makes a move at ({row}, {col}).
+        @param row The row of the board.
+        @param col The column of the board.
+        @param player The player, can be either 1 or 2.
+        @return The current winning condition, can be either:
+                0: No one wins.
+                1: Player 1 wins.
+                2: Player 2 wins.
+        :type row: int
+        :type col: int
+        :type player: int
+        :rtype: int
+        """
+        self.__rows[row][player - 1] += 1
+        self.__cols[col][player - 1] += 1
+        if row == col:
+            self.__diagonal[player - 1] += 1
+        if col == len(self.__rows) - row - 1:
+            self.__anti_diagonal[player - 1] += 1
+        if any([self.__rows[row][player - 1] == len(self.__rows), \
+                self.__cols[col][player - 1] == len(self.__cols), \
+                self.__diagonal[player - 1] == len(self.__rows), \
+                self.__anti_diagonal[player - 1] == len(self.__cols)]):
+            return player
+
+        return 0
+
+
+# Your TicTacToe object will be instantiated and called as such:
+# obj = TicTacToe(n)
+# param_1 = obj.move(row,col,player)
