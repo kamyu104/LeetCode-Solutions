87d8046defbdccea78a45a49380d053b18de5377
kamyu <kamyu104@users.noreply.github.com>
2016-05-05 21:07:38 +0800

    Update design-tic-tac-toe.py

--- Python/design-tic-tac-toe.py
+++ Python/design-tic-tac-toe.py
@@ -29,16 +29,17 @@ class TicTacToe(object):
         :type player: int
         :rtype: int
         """
-        self.__rows[row][player - 1] += 1
-        self.__cols[col][player - 1] += 1
+        i = player - 1
+        self.__rows[row][i] += 1
+        self.__cols[col][i] += 1
         if row == col:
-            self.__diagonal[player - 1] += 1
+            self.__diagonal[i] += 1
         if col == len(self.__rows) - row - 1:
-            self.__anti_diagonal[player - 1] += 1
-        if any([self.__rows[row][player - 1] == len(self.__rows), \
-                self.__cols[col][player - 1] == len(self.__cols), \
-                self.__diagonal[player - 1] == len(self.__rows), \
-                self.__anti_diagonal[player - 1] == len(self.__cols)]):
+            self.__anti_diagonal[i] += 1
+        if any([self.__rows[row][i] == len(self.__rows), \
+                self.__cols[col][i] == len(self.__cols), \
+                self.__diagonal[i] == len(self.__rows), \
+                self.__anti_diagonal[i] == len(self.__cols)]):
             return player
 
         return 0
