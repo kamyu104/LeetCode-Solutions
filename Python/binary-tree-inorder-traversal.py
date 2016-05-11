61a3881fa8f0f8fcf94492d7f38550bcb5e73692
kamyu <kamyu104@users.noreply.github.com>
2016-05-11 22:04:04 +0800

    Update binary-tree-inorder-traversal.py

--- Python/binary-tree-inorder-traversal.py
+++ Python/binary-tree-inorder-traversal.py
@@ -22,11 +22,14 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 # Morris Traversal Solution
-class Solution:
-    # @param root, a tree node
-    # @return a list of integers
+class Solution(object):
     def inorderTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
         result, prev, cur = [], None, root
         while cur:
             if cur.left is None:
@@ -49,43 +52,29 @@ class Solution:
                 
         return result
 
+
 # Time:  O(n)
-# Space: O(n)
-# Stack Solution  
-class Solution2:
-    # @param root, a tree node
-    # @return a list of integers
+# Space: O(h)
+# Stack Solution
+class Solution2(object):
     def inorderTraversal(self, root):
-        result, stack, current, last_traversed = [], [], root, None
-        while stack or current:
-            if current:
-                stack.append(current)
-                current = current.left
+        """
+        :type root: TreeNode
+        :rtype: List[int]
+        """
+        result, stack = [], [(root, False)]
+        while stack:
+            root, is_visited = stack.pop()
+            if root is None:
+                continue
+            if is_visited:
+                result.append(root.val)
             else:
-                parent = stack[-1]
-                if parent.right in (None, last_traversed):
-                    if parent.right is None:
-                        result.append(parent.val)
-                    last_traversed = stack.pop()
-                else:
-                    result.append(parent.val)
-                    current = parent.right
+                stack.append((root.right, False))
+                stack.append((root, True))
+                stack.append((root.left, False))
         return result
 
-class Solution3:
-    # @param root, a tree node
-    # @return a list of integers
-    def inorderTraversal(self, root):
-        result, stack, current = [], [], root
-        while stack or current:
-            if current:
-                stack.append(current)
-                current = current.left
-            else:
-                current = stack.pop()
-                result.append(current.val)
-                current = current.right
-        return result
 
 if __name__ == "__main__":
     root = TreeNode(1)
