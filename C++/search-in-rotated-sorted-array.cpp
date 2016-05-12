1f4c60ad8a679a8e2aaf376118a5f77e73b78ea9
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 23:01:57 +0800

    Create search-in-rotated-sorted-array.cpp

---
+++ C++/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,44 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if ((nums[mid] >= nums[left] && nums[left] <= target && target < nums[mid]) ||
+                       (nums[mid] < nums[left] && !(nums[mid] < target && target <= nums[right]))) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
+
+class Solution2 {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if ((nums[left] <= nums[mid] && nums[left] <= target && target < nums[mid]) ||
+                       (nums[left] > nums[mid] && !(nums[mid] < target && target <= nums[right - 1]))) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+};
