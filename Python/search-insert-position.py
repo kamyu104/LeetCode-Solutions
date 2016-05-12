873d98b592b2f39845fd08d5590673cfb172da2b
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 20:28:45 +0800

    Update search-insert-position.py

--- Python/search-insert-position.py
+++ Python/search-insert-position.py
@@ -24,12 +24,10 @@ class Solution(object):
         left, right = 0, len(nums) - 1
         while left <= right:
             mid = left + (right - left) / 2
-            if nums[mid] == target:
-                return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+            if nums[mid] >= target:
                 right = mid - 1
+            else:
+                left = mid + 1
 
         return left
 
