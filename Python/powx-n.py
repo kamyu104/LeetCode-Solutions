1be260c96e9928038cf26fb2122d10336b06bc7a
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 20:52:13 +0800

    Update powx-n.py

--- Python/powx-n.py
+++ Python/powx-n.py
@@ -1,27 +1,26 @@
 # Time:  O(logn)
-# Space: O(logn)
+# Space: O(1)
 # 
 # Implement pow(x, n).
 #
 
-class Solution:
-    # @param x, a float
-    # @param n, a integer
-    # @return a float
-    def pow(self, x, n):
-        if n < 0:
-            return 1 / self.powRecu(x, -n)
-        
-        return self.powRecu(x, n)
-    
-    def powRecu(self, x, n):
-        if n == 0:
-            return 1.0
-        
-        if n % 2 == 0:
-            return self.powRecu(x * x, n / 2)
-        else:
-            return x * self.powRecu(x * x, n / 2)
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        res = 1
+        abs_n = abs(n)
+        while abs_n:
+            if abs_n & 1:
+                res *= x
+            abs_n >>= 1
+            x *= x
+
+        return 1 / res if n < 0 else res
+
 
 if __name__ == "__main__":
     print Solution().pow(3, 5)
