d9489da34b1f0726bd40e4b2e6c1ac6ea600a782
kamyu <kamyu104@users.noreply.github.com>
2016-05-12 20:27:17 +0800

    Update search-insert-position.py

--- Python/search-insert-position.py
+++ Python/search-insert-position.py
@@ -14,23 +14,25 @@
 # [1,3,5,6], 0 -> 0
 #
 
-class Solution:
-    # @param A, a list of integers
-    # @param target, an integer to be inserted
-    # @return integer
-    def searchInsert(self, A, target):
-        low, high = 0, len(A) - 1
-        
-        while low <= high:
-            mid = low + (high - low) / 2
-            if A[mid] == target:
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) / 2
+            if nums[mid] == target:
                 return mid
-            elif A[mid] < target:
-                low = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                high = mid - 1
-        
-        return low
+                right = mid - 1
+
+        return left
+
 
 if __name__ == "__main__":
     print Solution().searchInsert([1, 3, 5, 6], 5)
