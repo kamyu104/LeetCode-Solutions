f8660d06fe2abe0de9dd45db1830a3847fdcd444
kamyu <kamyu104@users.noreply.github.com>
2016-05-18 13:23:30 +0800

    Create intersection-of-two-arrays.py

---
+++ Python/intersection-of-two-arrays.py
@@ -0,0 +1,35 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+# Given two arrays, write a function to compute their intersection.
+#
+# Example:
+# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2].
+#
+# Note:
+# Each element in the result must be unique.
+# The result can be in any order.
+
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort(), nums2.sort()
+        res = []
+        
+        it1, it2 = 0, 0
+        while it1 < len(nums1) and it2 < len(nums2):
+            if nums1[it1] < nums2[it2]:
+                it1 += 1
+            elif nums1[it1] > nums2[it2]:
+                it2 += 1
+            else:
+                if not res or res[-1] != nums1[it1]:
+                    res += nums1[it1],
+                it1 += 1
+                it2 += 1
+        
+        return res
