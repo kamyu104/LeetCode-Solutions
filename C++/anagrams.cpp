4209da4c52a6aae043ad2083226db1983db804e8
kamyu <kamyu104@users.noreply.github.com>
2016-05-19 21:02:06 +0800

    Update anagrams.cpp

--- C++/anagrams.cpp
+++ C++/anagrams.cpp
@@ -1,22 +1,26 @@
-// Time Complexity: O(klogk * n) ~= O(n), k is length of string, n is number of strings
-// Space Complexity: O(k * n) ~= O(n)
+// Time:  O(n * glogg), g is the max size of groups.
+// Space: O(n)
 
 class Solution {
-    public:
-        vector<string> anagrams(vector<string> &strs) {
-            vector<string> ans;
-            unordered_map<string, vector<string> > group;
-            for(auto s : strs) {
-                string k = s;
-                sort(k.begin(), k.end());
-                group[k].push_back(s);
-            }
-
-            for(auto it = group.cbegin(); it != group.cend(); ++it) {
-                if(it->second.size() > 1)
-                    ans.insert(ans.end(), it->second.begin(), it->second.end());
-            }
-
-            return ans;
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> groups;
+        for (const auto& str : strs) {
+            string tmp{str};
+            sort(tmp.begin(), tmp.end());
+            groups[tmp].emplace_back(str);
         }
-};`
+
+        vector<vector<string>> anagrams;
+        for (const auto& kvp : groups) {
+            vector<string> group;
+            for (const auto& str : kvp.second) {
+                group.emplace_back(str);
+            }
+            sort(group.begin(), group.end());
+            anagrams.emplace_back(move(group));
+        }
+
+        return anagrams;
+    }
+};
