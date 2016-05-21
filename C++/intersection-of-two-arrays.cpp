f51dc53047496864c882e5e1843e986d1371e457
kamyu <kamyu104@users.noreply.github.com>
2016-05-21 18:18:05 +0800

    Update intersection-of-two-arrays.cpp

--- C++/intersection-of-two-arrays.cpp
+++ C++/intersection-of-two-arrays.cpp
@@ -33,7 +33,7 @@ public:
 class Solution2 {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res;
+        vector<int> result;
         sort(nums1.begin(), nums1.end());
         sort(nums2.begin(), nums2.end());
         auto it1 = nums1.cbegin(), it2 = nums2.cbegin();
@@ -43,12 +43,12 @@ public:
             } else if (*it1 > *it2) {
                 ++it2;
             } else {
-                if (res.empty() || res.back() != *it1) {
-                    res.emplace_back(*it1);
+                if (result.empty() || result.back() != *it1) {
+                    result.emplace_back(*it1);
                 }
                 ++it1, ++it2;
             }
         }
-        return res;
+        return result;
     }
 };
