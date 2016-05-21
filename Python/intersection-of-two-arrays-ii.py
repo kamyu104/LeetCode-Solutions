a41254acc400ac418217e05ccb52afd854269bfc
kamyu <kamyu104@users.noreply.github.com>
2016-05-21 17:29:55 +0800

    Update intersection-of-two-arrays-ii.py

--- Python/intersection-of-two-arrays-ii.py
+++ Python/intersection-of-two-arrays-ii.py
@@ -1,3 +1,33 @@
+# If the given array is not sorted and the memory is unlimited:
+#   - Time:  O(m + n)
+#   - Space: O(min(m, n))
+# elif the given array is already sorted:
+#   if the memory is unlimited, and (m << n or m >> n)
+#     - Time:  O(min(m, n) * log(max(m, n)))
+#     - Space: O(min(m, n))
+#   else:
+#     - Time:  O(m + n)
+#     - Soace: O(1)
+# elif the memory is limited:
+#     - Time:  O(max(m, n) * log(max(m, n)))
+#     - Space: O(max(m, n))
+
+# Given two arrays, write a function to compute their intersection.
+#
+# Example:
+# Given nums1 = [1, 2, 2, 1], nums2 = [2, 2], return [2, 2].
+#
+# Note:
+# Each element in the result should appear as many times as it shows in both arrays.
+# The result can be in any order.
+#
+# Follow up:
+# - What if the given array is already sorted? How would you optimize your algorithm?
+# - What if nums1's size is small compared to num2's size? Which algorithm is better?
+# - What if elements of nums2 are stored on disk, and the memory is limited such that
+#   you cannot load all elements into the memory at once?
+
+
 # If the given array is not sorted and the memory is unlimited.
 # Time:  O(m + n)
 # Space: O(min(m, n))
@@ -25,7 +55,7 @@ class Solution(object):
         return res
 
 
-# If the given array is already sorted, and the memory is unlimited, and (m << n or m >> n)
+# If the given array is already sorted, and the memory is unlimited, and (m << n or m >> n).
 # Time:  O(min(m, n) * log(max(m, n)))
 # Space: O(min(m, n))
 # Binary search solution.
@@ -54,7 +84,7 @@ class Solution(object):
             right = binary_search(lambda x, y: x > y, nums, target)
             return right - left
 
-        nums1.sort(), nums2.sort()  # Make sure it is sorted.
+        nums1.sort(), nums2.sort()  # Make sure it is sorted, doesn't count in time.
 
         cnts = collections.defaultdict(int)
         for i in nums1:
@@ -68,7 +98,7 @@ class Solution(object):
         return res
 
 
-# If the given array is already sorted, and memory is limited or m ~ n
+# If the given array is already sorted, and the memory is limited or m ~ n.
 # Time:  O(m + n)
 # Soace: O(1)
 # Two pointers solution.
@@ -79,7 +109,35 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        nums1.sort(), nums2.sort()  # Make sure it is sorted.
+        nums1.sort(), nums2.sort()  # Make sure it is sorted, doesn't count in time.
+
+        res = []
+        
+        it1, it2 = 0, 0
+        while it1 < len(nums1) and it2 < len(nums2):
+            if nums1[it1] < nums2[it2]:
+                it1 += 1
+            elif nums1[it1] > nums2[it2]:
+                it2 += 1
+            else:
+                res += nums1[it1],
+                it1 += 1
+                it2 += 1
+        
+        return res
+
+# If the given array is not sorted, and the memory is limited.
+# Time:  O(max(m, n) * log(max(m, n)))
+# Space: O(max(m, n))
+# Two pointers solution.
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        nums1.sort(), nums2.sort()  # O(max(m, n) * log(max(m, n)))
 
         res = []
         
