c361a20a7ccd7fba454260f20d72fc3bdc3c8611
kamyu <kamyu104@users.noreply.github.com>
2016-05-21 17:54:44 +0800

    Update intersection-of-two-arrays-ii.cpp

--- C++/intersection-of-two-arrays-ii.cpp
+++ C++/intersection-of-two-arrays-ii.cpp
@@ -12,10 +12,10 @@
 //     - Time:  O(max(m, n) * log(max(m, n)))
 //     - Space: O(max(m, n))
 
-# If the given array is not sorted and the memory is unlimited.
-# Time:  O(m + n)
-# Space: O(min(m, n))
-# Hash solution.
+// If the given array is not sorted and the memory is unlimited.
+// Time:  O(m + n)
+// Space: O(min(m, n))
+// Hash solution.
 class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
@@ -40,3 +40,56 @@ public:
     }
 };
 
+
+// If the given array is already sorted, and the memory is limited or m ~ n.
+// Time:  O(m + n)
+// Soace: O(1)
+// Two pointers solution.
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        // Make sure it is sorted, doesn't count in time.
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        auto it1 = nums1.cbegin(), it2 = nums2.cbegin();
+        while (it1 != nums1.cend() && it2 != nums2.cend()) {
+            if (*it1 < *it2) {
+                ++it1;
+            } else if (*it1 > *it2) {
+                ++it2;
+            } else {
+                result.emplace_back(*it1);
+                ++it1, ++it2;
+            }
+        }
+        return result;
+    }
+};
+
+
+// If the given array is not sorted, and the memory is limited.
+// Time:  O(max(m, n) * log(max(m, n)))
+// Space: O(1)
+// Two pointer solution.
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        // O(max(m, n) * log(max(m, n)))
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+        auto it1 = nums1.cbegin(), it2 = nums2.cbegin();
+        while (it1 != nums1.cend() && it2 != nums2.cend()) {
+            if (*it1 < *it2) {
+                ++it1;
+            } else if (*it1 > *it2) {
+                ++it2;
+            } else {
+                result.emplace_back(*it1);
+                ++it1, ++it2;
+            }
+        }
+        return result;
+    }
+};
