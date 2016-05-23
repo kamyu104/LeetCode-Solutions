e2c9558838cc185c207d2ac17c30c206aa5851ee
kamyu <kamyu104@users.noreply.github.com>
2016-05-24 01:18:33 +0800

    Update android-unlock-patterns.py

--- Python/android-unlock-patterns.py
+++ Python/android-unlock-patterns.py
@@ -1,6 +1,7 @@
 # Time:  O(9 * 2^9)
 # Space: O(9 * 2^9)
 
+# DP solution.
 class Solution(object):
     def numberOfPatterns(self, m, n):
         """
@@ -56,6 +57,71 @@ class Solution(object):
                              continue
 
                     dp[merge(used, j)][j] += dp[used][i]
+
+        return res
+
+
+# Time:  O(9 * 2^9)
+# Space: O(9 * 2^9)
+# DP solution.
+class Solution2(object):
+    def numberOfPatterns(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        def merge(used, i):
+            return used | (1 << i)
+
+        def number_of_keys(i):
+            number = 0
+            while i > 0:
+                i &= i - 1
+                number += 1
+            return number
+
+        def exclude(used, i):
+            return used & ~(1 << i)
+
+        def contain(used, i):
+            return bool(used & (1 << i))
+
+        def convert(i, j):
+            return 3 * i + j
+
+        # dp[i][j]: i is the set of the numbers in binary representation,
+        #            d[i][j] is the number of ways ending with the number j.
+        dp = [[0] * 9 for _ in xrange(1 << 9)]
+        for i in xrange(9):
+            dp[merge(0, i)][i] = 1
+
+        res = 0
+        for used in xrange(len(dp)):
+            number = number_of_keys(used)
+            if number > n:
+                continue
+
+            for i in xrange(9):
+                if not contain(used, i):
+                    continue
+
+                x1, y1 = i / 3, i % 3
+                for j in xrange(9):
+                    if i == j or not contain(used, j):
+                        continue
+    
+                    x2, y2 = j / 3, j % 3
+                    if ((x1 == x2 and abs(y1 - y2) == 2) or \
+                        (y1 == y2 and abs(x1 - x2) == 2) or \
+                        (abs(x1 - x2) == 2 and abs(y1 - y2) == 2)) and \
+                       not contain(used, convert((x1 + x2) / 2, (y1 + y2) / 2)):
+                             continue
+
+                    dp[used][i] += dp[exclude(used, i)][j]
+
+                if m <= number <= n:
+                    res += dp[used][i]
 
         return res
 
