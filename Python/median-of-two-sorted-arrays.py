b77757f57f4dba1542f2a5a56f654f7dc74e0f3f
kamyu <kamyu104@users.noreply.github.com>
2016-05-28 22:08:49 +0800

    Update median-of-two-sorted-arrays.py

--- Python/median-of-two-sorted-arrays.py
+++ Python/median-of-two-sorted-arrays.py
@@ -37,7 +37,51 @@ class Solution(object):
 
         return max(Ai_minus_1, Bj)
 
- 
+
+# Time:  O(log(max(m, n)) * log(max_val - min_val))
+# Space: O(1)
+# Generic solution.
+class Solution_Generic(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        len1, len2 = len(nums1), len(nums2)
+        if (len1 + len2) % 2 == 1: 
+            return self.getKth([nums1, nums2], (len1 + len2)/2 + 1)
+        else:
+            return (self.getKth([nums1, nums2], (len1 + len2)/2) + \
+                    self.getKth([nums1, nums2], (len1 + len2)/2 + 1)) * 0.5
+
+    def getKth(self, arrays, k):
+        def binary_search(array, left, right, target, compare):
+            while left <= right:
+                mid = left + (right - left) / 2
+                if compare(array, mid, target):
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            return left
+
+        def match(arrays, target, k):
+            res = 0
+            for array in arrays:
+                if array:
+                    res += len(array) - binary_search(array, 0, len(array) - 1, target, \
+                                                      lambda array, x, y: array[x] > y)
+            return res < k
+
+        left, right = float("inf"), float("-inf")
+        for array in arrays:
+            if array:
+                left = min(left, array[0])
+                right = max(right, array[-1])
+
+        return binary_search(arrays, left, right, k, match)
+
+
 if __name__ == "__main__":
     print Solution().findMedianSortedArrays([1, 3, 5, 7], [2, 4, 6])
     print Solution().findMedianSortedArrays([1, 3, 5], [2, 4, 6])
