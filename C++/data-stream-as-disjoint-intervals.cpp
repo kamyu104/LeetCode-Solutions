97832f228de723da9b90f0c25270c8d80634f577
kamyu <kamyu104@users.noreply.github.com>
2016-05-31 22:56:05 +0800

    Update data-stream-as-disjoint-intervals.cpp

--- C++/data-stream-as-disjoint-intervals.cpp
+++ C++/data-stream-as-disjoint-intervals.cpp
@@ -1,4 +1,4 @@
-// Time:  addNum: O(logn), getIntervals: O(n)
+// Time:  addNum: O(n), getIntervals: O(n), i is the number of disjoint intervals.
 // Space: O(n)
 
 /**
@@ -11,6 +11,52 @@
  * };
  */
 class SummaryRanges {
+public:
+    /** Initialize your data structure here. */
+    SummaryRanges() {
+        
+    }
+    
+    void addNum(int val) {
+        Interval newInterval(val, val);
+        size_t i = 0;
+        vector<Interval> result;
+        // Insert intervals appeared before newInterval.
+        while (i < intervals_.size() && intervals_[i].end + 1 < newInterval.start) {
+            result.emplace_back(intervals_[i++]);
+        }
+
+        // Merge intervals that overlap with newInterval.
+        while (i < intervals_.size() && newInterval.end + 1 >= intervals_[i].start) {
+            newInterval = {min(newInterval.start, intervals_[i].start),
+                max(newInterval.end, intervals_[i].end)};
+            ++i;
+        }
+        result.emplace_back(newInterval);
+
+        // Insert intervals appearing after newInterval.
+        result.insert(result.end(), intervals_.cbegin() + i, intervals_.cend());
+        swap(result, intervals_);
+    }
+    
+    vector<Interval> getIntervals() {
+        return intervals_;
+    }
+
+private:
+    vector<Interval> intervals_;
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges obj = new SummaryRanges();
+ * obj.addNum(val);
+ * vector<Interval> param_2 = obj.getIntervals();
+ */
+
+// Time:  addNum: O(logs), getIntervals: O(s), s is the data stream's size.
+// Space: O(s)
+class SummaryRanges2 {
 public:
     /** Initialize your data structure here. */
     SummaryRanges() {
