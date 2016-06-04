1d50b15c3ae568c9d5806b78d79da7b22f7c1e51
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 13:53:21 +0800

    Update intersection-of-two-arrays.py

--- Python/intersection-of-two-arrays.py
+++ Python/intersection-of-two-arrays.py
@@ -34,10 +34,45 @@ class Solution(object):
         return res
 
 
+# Time:  O(min(m, n) * log(max(m, n)))
+# Space: O(1)
+# Binary search solution.
+class Solution2(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        if len(nums1) > len(nums2):
+            return self.intersection(nums2, nums1)
+
+        def binary_search(compare, nums, left, right, target):
+            while left < right:
+                mid = left + (right - left) / 2
+                if compare(nums[mid], target):
+                    right = mid
+                else:
+                    left = mid + 1
+            return left
+
+        nums1.sort(), nums2.sort()  # Make sure it is sorted, doesn't count in time.
+
+        res = []
+        left = 0
+        for i in nums1:
+            left = binary_search(lambda x, y: x >= y, nums2, left, len(nums2), i)
+            if left != len(nums2) and nums2[left] == i:
+                res += i,
+                left = binary_search(lambda x, y: x > y, nums2, left, len(nums2), i)
+        
+        return res
+
+
 # Time:  O(max(m, n) * log(max(m, n)))
 # Space: O(1)
 # Two pointers solution.
-class Solution2(object):
+class Solution3(object):
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
