d23cff24a2f749fef2f867d2df6ebaf99c2009f0
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 12:29:52 +0800

    Update merge-k-sorted-lists.py

--- Python/merge-k-sorted-lists.py
+++ Python/merge-k-sorted-lists.py
@@ -1,20 +1,58 @@
 # Time:  O(nlogk)
-# Space: O(k)
-#
-# Merge k sorted linked lists and return it as one sorted list. Analyze and describe its complexity.
-import heapq
+# Space: O(1)
+
+# Merge k sorted linked lists and return it as one sorted list.
+# Analyze and describe its complexity.
 
 # Definition for singly-linked list.
-class ListNode:
+class ListNode(object):
     def __init__(self, x):
         self.val = x
         self.next = None
-    
-    def __repr__(self):
-        if self:
-            return "{} -> {}".format(self.val, repr(self.next))
 
-class Solution:
+    def __repr__(self):		
+        if self:		
+            return "{} -> {}".format(self.val, 
+
+
+# Merge two by two solution.
+class Solution(object):
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        def mergeTwoLists(l1, l2):
+            curr = dummy = ListNode(0)
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            curr.next = l1 or l2
+            return dummy.next
+
+        if not lists:
+            return None
+        left, right = 0, len(lists) - 1;
+        while right > 0:
+            if left >= right:
+                left = 0
+            else:
+                lists[left] = mergeTwoLists(lists[left], lists[right])
+                left += 1
+                right -= 1
+        return lists[0]
+
+
+# Time:  O(nlogk)
+# Space: O(k)
+# Heap solution.
+import heapq
+class Solution3:
     # @param a list of ListNode
     # @return a ListNode
     def mergeKLists(self, lists):
@@ -34,6 +72,7 @@ class Solution:
                 heapq.heappush(heap, (smallest.next.val, smallest.next))
                 
         return dummy.next
+
 
 if __name__ == "__main__":
     list1 = ListNode(1)
