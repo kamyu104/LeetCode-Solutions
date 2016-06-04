bb64eb960012063a4d744edc34b2d67cac4cc7c6
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 18:01:39 +0800

    Update reverse-nodes-in-k-group.cpp

--- C++/reverse-nodes-in-k-group.cpp
+++ C++/reverse-nodes-in-k-group.cpp
@@ -14,32 +14,32 @@ public:
     ListNode* reverseKGroup(ListNode* head, int k) {
         ListNode dummy{0};
         dummy.next = head;
-        ListNode *cur = head, *cur_dummy = &dummy;
+        auto curr = head, curr_dummy = &dummy;
         int len = 0;
 
-        while (cur) {
-            ListNode *next_cur = cur->next;
+        while (curr) {
+            auto next_curr = curr->next;
             len = (len + 1) % k;
 
             if (len == 0) {
-                ListNode *next_dummy = cur_dummy->next;
-                reverse(&cur_dummy, cur->next);
-                cur_dummy = next_dummy;
+                auto next_dummy = curr_dummy->next;
+                reverse(&curr_dummy, curr->next);
+                curr_dummy = next_dummy;
             }
-            cur = next_cur;
+            curr = next_curr;
         }
         return dummy.next;
     }
 
     void reverse(ListNode **begin, const ListNode *end) {
         ListNode *first = (*begin)->next;
-        ListNode *cur = first->next;
+        ListNode *curr = first->next;
 
-        while (cur != end) {
-            first->next = cur->next;
-            cur->next = (*begin)->next;
-            (*begin)->next = cur;
-            cur = first->next;
+        while (curr != end) {
+            first->next = curr->next;
+            curr->next = (*begin)->next;
+            (*begin)->next = curr;
+            curr = first->next;
         }
     }
 };
