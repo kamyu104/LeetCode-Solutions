ff542b345cd15c6e4b13ae9206e979daac579a57
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 12:13:15 +0800

    Update merge-k-sorted-lists.cpp

--- C++/merge-k-sorted-lists.cpp
+++ C++/merge-k-sorted-lists.cpp
@@ -1,5 +1,5 @@
 // Time:  O(n * logk)
-// Space: O(k)
+// Space: O(1)
 
 /**
  * Definition for singly-linked list.
@@ -9,7 +9,92 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+ 
+// Merge two by two solution.
 class Solution {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        if (lists.empty()) {
+            return nullptr;
+        }
+
+        int left = 0, right = lists.size() - 1;
+        while (right > 0) {
+            if (left >= right) {
+                left = 0;
+            } else {
+                lists[left] = mergeTwoLists(lists[left], lists[right]);
+                ++left;
+                --right;
+            }
+        }
+        return lists[0];
+    }
+    
+private:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy = ListNode(0);
+        auto *curr = &dummy;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
+
+// Time:  O(n * logk)
+// Space: O(logk)
+// Divide and conquer solution.
+class Solution2 {
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists) {
+        return mergeKListsHelper(lists, 0, lists.size() - 1);
+    }
+
+private:
+    ListNode *mergeKListsHelper(const vector<ListNode *> &lists, int begin, int end) {
+        if (begin > end) {
+            return nullptr;
+        }
+        if (begin == end) {
+            return lists[begin];
+        }
+        return mergeTwoLists(mergeKListsHelper(lists, begin, (begin + end) / 2),
+                             mergeKListsHelper(lists, (begin + end) / 2 + 1, end));
+    }
+
+private:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy = ListNode(0);
+        auto *curr = &dummy;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
+
+
+// Time:  O(n * logk)
+// Space: O(k)
+// Heap solution.
+class Solution3 {
 public:
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         ListNode dummy(0);
@@ -43,54 +128,3 @@ public:
         return dummy.next;
     }
 };
-
-
-// Time:  O(n * logk)
-// Space: O(k)
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution2 {
-public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        return mergeKLists(lists, 0, lists.size() - 1);
-    }
-
-private:
-    ListNode *mergeKLists(const vector<ListNode *> &lists, int begin, int end) {
-        if (begin > end) {
-            return nullptr;
-        }
-        if (begin == end) {
-            return lists[begin];
-        }
-        return mergeTwoLists(mergeKLists(lists, begin, (begin + end) / 2),
-                             mergeKLists(lists, (begin + end) / 2 + 1, end));
-    }
-
-    ListNode *mergeTwoLists(ListNode *left, ListNode *right) {
-        ListNode dummy(0);
-        auto *p = &dummy;
-        for (; left && right; p = p->next) {
-            if(left->val < right->val) {
-                p->next = left;
-                left = left->next;
-            } else {
-                p->next = right;
-                right = right->next;
-            }
-        }
-        if (left) {
-            p->next = left;
-        } else {
-            p->next = right;
-        }
-        return dummy.next;
-    }
-};
-
