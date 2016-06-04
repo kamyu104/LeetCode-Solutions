27e7b89a62e3cf622e6ca6f081e2d5e61c9585c6
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 13:16:22 +0800

    Update merge-k-sorted-lists.cpp

--- C++/merge-k-sorted-lists.cpp
+++ C++/merge-k-sorted-lists.cpp
@@ -35,6 +35,7 @@ private:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode dummy = ListNode(0);
         auto *curr = &dummy;
+
         while (l1 && l2) {
             if (l1->val <= l2->val) {
                 curr->next = l1;
@@ -46,6 +47,7 @@ private:
             curr = curr->next;
         }
         curr->next = l1 ? l1 : l2;
+
         return dummy.next;
     }
 };
@@ -75,6 +77,7 @@ private:
     ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
         ListNode dummy = ListNode(0);
         auto *curr = &dummy;
+
         while (l1 && l2) {
             if (l1->val <= l2->val) {
                 curr->next = l1;
@@ -86,6 +89,7 @@ private:
             curr = curr->next;
         }
         curr->next = l1 ? l1 : l2;
+
         return dummy.next;
     }
 };
