8f7d7d7215df62b4eab423b374ae3b4b6ee2638a
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 12:34:51 +0800

    Update merge-k-sorted-lists.py

--- Python/merge-k-sorted-lists.py
+++ Python/merge-k-sorted-lists.py
@@ -49,6 +49,37 @@ class Solution(object):
 
 
 # Time:  O(nlogk)
+# Space: O(logk)
+# Divide and Conquer solution.
+class Solution2:
+    # @param a list of ListNode
+    # @return a ListNode
+    def mergeKLists(self, lists):
+        def mergeTwoLists(l1, l2):
+            curr = dummy = ListNode(0)
+            while l1 and l2:
+                if l1.val < l2.val:
+                    curr.next = l1
+                    l1 = l1.next
+                else:
+                    curr.next = l2
+                    l2 = l2.next
+                curr = curr.next
+            curr.next = l1 or l2
+            return dummy.next
+    
+        def mergeKListsHelper(lists, begin, end):
+            if begin > end:
+                return None
+            if begin == end:
+                return lists[begin]
+            return mergeTwoLists(mergeKListsHelper(lists, begin, (begin + end) / 2), \
+                                 mergeKListsHelper(lists, (begin + end) / 2 + 1, end))
+   
+        return mergeKListsHelper(lists, 0, len(lists) - 1)
+
+
+# Time:  O(nlogk)
 # Space: O(k)
 # Heap solution.
 import heapq
