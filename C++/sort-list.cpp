416f9de5c3e9c1886bc8dcd328303b3b81a149fc
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 15:37:12 +0800

    Update and rename sortList.cpp to sort-list.cpp

---
+++ C++/sort-list.cpp
@@ -0,0 +1,52 @@
+// Time:  O(nlogn)
+// Space: O(logn)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+        if (!head || !head->next) {
+            return head;
+        }
+
+        auto slow = head, fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        // Split linked list.
+        fast = slow;
+        slow = slow->next;
+        fast->next = nullptr;
+
+        return mergeTwoLists(sortList(head), sortList(slow));
+    }
+
+private:
+    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
+        ListNode dummy = ListNode{0};
+        auto *curr = &dummy;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                curr->next = l1;
+                l1 = l1->next;
+            } else {
+                curr->next = l2;
+                l2 = l2->next;
+            }
+            curr = curr->next;
+        }
+        curr->next = l1 ? l1 : l2;
+
+        return dummy.next;
+    }
+};
