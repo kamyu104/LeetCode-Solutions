b05dbc3497898b0e45f5b5cc8a106cdcf967d69f
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 15:55:21 +0800

    Update add-two-numbers.cpp

--- C++/add-two-numbers.cpp
+++ C++/add-two-numbers.cpp
@@ -12,26 +12,20 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode dummy = ListNode(0);
-        ListNode *cur = &dummy;
-        int carry = 0;
-        while (l1 || l2) {
-            int val {carry};
-            if (l1) {
-                val += l1->val;
-                l1 = l1->next;
-            }
-            if (l2) {
-                val += l2->val;
-                l2 = l2->next;
-            }
+        ListNode dummy{0};
+        auto curr = &dummy;
+
+        auto carry = 0;
+        while (l1 || l2 || carry) {
+            auto a = l1? l1->val : 0, b = l2? l2->val : 0;
+            auto val = carry + a + b;
+            curr->next = new ListNode(val % 10);
             carry = val / 10;
-            cur->next = new ListNode(val % 10);
-            cur = cur->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
+            curr = curr->next;
         }
-        if (carry) {
-            cur->next = new ListNode(carry);
-        }
-        return dummy.next;   
+
+        return dummy.next;
     }
 };
