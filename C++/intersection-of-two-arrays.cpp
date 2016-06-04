6419db80759331b78f24637a406fea15f353e058
kamyu <kamyu104@users.noreply.github.com>
2016-06-04 13:46:34 +0800

    Update intersection-of-two-arrays.cpp

--- C++/intersection-of-two-arrays.cpp
+++ C++/intersection-of-two-arrays.cpp
@@ -24,10 +24,39 @@ public:
 };
 
 
+// Time:  O(min(m, n) * log(max(m, n)))
+// Space: O(1)
+// Binary search solution.
+class Solution2 {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        if (nums1.size() > nums2.size()) {
+            return intersection(nums2, nums1);
+        }
+
+        // Make sure it is sorted, doesn't count in time.
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+        auto it = nums2.cbegin();
+        for (const auto& i : nums1) {
+            it = lower_bound(it, nums2.cend(), i);
+            if (it != nums2.end() && *it == i) {
+                result.emplace_back(*it);
+                it = upper_bound(it, nums2.cend(), i);
+            }
+        }
+        
+        return result;
+    }
+};
+
+
 // Time:  O(max(m, n) * log(max(m, n)))
 // Space: O(1)
 // Two pointers solution.
-class Solution2 {
+class Solution3 {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
         vector<int> result;
