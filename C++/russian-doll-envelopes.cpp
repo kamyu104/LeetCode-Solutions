1826a861cb43269f0aee7c8a25095f0d79ae1c14
kamyu <kamyu104@users.noreply.github.com>
2016-06-07 22:42:58 +0800

    Update russian-doll-envelopes.cpp

--- C++/russian-doll-envelopes.cpp
+++ C++/russian-doll-envelopes.cpp
@@ -6,22 +6,20 @@ public:
     int maxEnvelopes(vector<pair<int, int>>& envelopes) {
         vector<int> result;
 
-        sort(envelopes.begin(), envelopes.end());  // O(nlogn)
-        for (int i = 0; i < envelopes.size();) {
-            const auto w = envelopes[i].first;
-            auto same_count = 0;
-            while (i < envelopes.size() && envelopes[i].first == w) {
-                ++i, ++same_count;
-            }
-
-            for (int j = i - 1; j >= i - same_count; --j) {  // Insert from larger h.
-                const auto target = envelopes[j].second;
-                auto it = lower_bound(result.begin(), result.end(), target);  // O(logk)
-                if (it == result.end()) {
-                    result.emplace_back(target);
-                } else {
-                    *it = target;
+        sort(envelopes.begin(), envelopes.end(),  // O(nlogn)
+            [](const pair<int, int>& a, const pair<int, int>& b) {
+                if (a.first == b.first) {
+                    return a.second > b.second;
                 }
+                return a.first < b.first;
+            });
+        for (const auto& envelope : envelopes) {
+            const auto target = envelope.second;
+            auto it = lower_bound(result.begin(), result.end(), target);  // O(logk)
+            if (it == result.end()) {
+                result.emplace_back(target);
+            } else {
+                *it = target;
             }
         }
 
