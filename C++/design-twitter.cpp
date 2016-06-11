82e204e431ffd8936b295a4e81aca501034f441b
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 21:58:15 +0800

    Update design-twitter.cpp

--- C++/design-twitter.cpp
+++ C++/design-twitter.cpp
@@ -1,8 +1,8 @@
 // Time:  O(nlogf),     n is most recently number of tweets,
-//                      f is the number of followings.
+//                      f is the number of user's following.
 // Space: O(u + t + r), u is the number of users,
 //                      t is the total number of tweets,
-//                      r is the number of followings.
+//                      r is the total number of followings.
 
 class Twitter {
 public:
