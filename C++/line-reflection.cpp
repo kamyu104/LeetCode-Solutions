3cc3ff616dfbf67e8d4e41dd6555150da6f0fc99
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 06:58:40 -0500

    Update line-reflection.cpp

--- C++/line-reflection.cpp
+++ C++/line-reflection.cpp
@@ -1,7 +1,37 @@
+// Time:  O(n)
+// Space: O(n)
+
+// Hash solution.
+class Solution {
+public:
+    bool isReflected(vector<pair<int, int>>& points) {
+        if (points.empty()) {
+            return true;
+        }
+        unordered_map<int, unordered_set<int>> groups_by_y;
+        int left = numeric_limits<int>::max();
+        int right = numeric_limits<int>::min();
+        for (const auto& p : points) {
+            groups_by_y[p.second].emplace(p.first);
+            left = min(left, p.first);
+            right = max(right, p.first);
+        }
+        const auto mid = left + right;
+        for (const auto& kvp : groups_by_y) {
+            for (const auto& x : kvp.second) {
+                if (2 * x != mid && kvp.second.count(mid - x) == 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
+
 // Time:  O(nlogn)
 // Space: O(1)
-
-class Solution {
+// Two pointers solution.
+class Solution2 {
 public:
     bool isReflected(vector<pair<int, int>>& points) {
         if (points.empty()) {
