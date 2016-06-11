5decf17b1271a5f5f9d1b6664cdd2aeb85ad2cb8
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 20:53:14 +0800

    Update line-reflection.py

--- Python/line-reflection.py
+++ Python/line-reflection.py
@@ -21,3 +21,30 @@ class Solution(object):
                 if mid - x not in group:
                     return False
         return True
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# Hash solution.
+class Solution2(object):
+    def isReflected(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: bool
+        """
+        if not points:
+            return True
+        points.sort()
+        points[len(points)/2:] = sorted(points[len(points)/2:], \ # Space: O(n)
+                                        lambda x, y: y[1] - x[1] if x[0] == y[0] else \
+                                                     x[0] - y[0])
+        mid = points[0][0] + points[-1][0]
+        left, right = 0, len(points) - 1
+        while left <= right:
+            if (mid != points[left][0] + points[right][0]) or \
+                (points[left][0] != points[right][0] and \
+                 points[left][1] != points[right][1]):
+                return False
+            left += 1
+            right -= 1
+        return True
