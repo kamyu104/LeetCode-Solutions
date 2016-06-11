0e6de50a232dfdab509fd4e3f18e3f0b5881c84b
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 13:41:04 +0800

    Update maximal-rectangle.py

--- Python/maximal-rectangle.py
+++ Python/maximal-rectangle.py
@@ -1,14 +1,52 @@
 # Time:  O(n^2)
 # Space: O(n)
-#
+
 # Given a 2D binary matrix filled with 0's and 1's, 
 # find the largest rectangle containing all ones and return its area.
-#
 
-class Solution:
-    # @param matrix, a list of lists of 1 length string
-    # @return an integer
+# Ascending stack solution.
+class Solution(object):
     def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
+        def largestRectangleArea(heights):
+            increasing, area, i = [], 0, 0
+            while i <= len(heights):
+                if not increasing or (i < len(heights) and heights[i] > heights[increasing[-1]]):
+                    increasing.append(i)
+                    i += 1
+                else:
+                    last = increasing.pop()
+                    if not increasing:
+                        area = max(area, heights[last] * i)
+                    else:
+                        area = max(area, heights[last] * (i - increasing[-1] - 1 ))
+            return area
+
+        if not matrix:
+            return 0
+
+        result = 0
+        heights = [0] * len(matrix[0])
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                heights[j] = heights[j] + 1 if matrix[i][j] == '1' else 0
+            result = max(result, largestRectangleArea(heights))
+
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(n)
+# DP solution.
+class Solution2(object):
+    def maximalRectangle(self, matrix):
+        """
+        :type matrix: List[List[str]]
+        :rtype: int
+        """
         if not matrix:
             return 0
         
