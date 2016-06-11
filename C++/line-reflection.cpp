786ab7d4f9bcf72866a5c1da28e4442f59c0033a
kamyu <kamyu104@users.noreply.github.com>
2016-06-11 19:08:32 +0800

    Create line-reflection.cpp

---
+++ C++/line-reflection.cpp
@@ -0,0 +1,29 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isReflected(vector<pair<int, int>>& points) {
+        if (points.empty()) {
+            return true;
+        }
+        sort(points.begin(), points.end());
+        sort(points.begin(), points.begin() + distance(points.begin(), points.end()) / 2,
+            [](const pair<int, int>& a, const pair<int, int>& b) {
+                if (a.first == b.first) {
+                    return a.second > b.second;
+                }
+                return a.first < b.first;
+            });
+
+        const auto mid = points.front().first + (points.back().first - points.front().first) / 2.0;
+        for (int left = 0, right = points.size() - 1; left <= right; ++left, --right) {
+            if ((mid != points[left].first + (points[right].first - points[left].first) / 2.0) || 
+                (points[left].first != points[right].first &&
+                 points[left].second != points[right].second)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
