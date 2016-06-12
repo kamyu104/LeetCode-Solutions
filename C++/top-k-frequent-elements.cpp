7e3775d2c403a4347597e3201e3823aac67479d8
kamyu <kamyu104@users.noreply.github.com>
2016-06-12 21:09:23 +0800

    Update top-k-frequent-elements.cpp

--- C++/top-k-frequent-elements.cpp
+++ C++/top-k-frequent-elements.cpp
@@ -22,3 +22,30 @@ public:
         return result;
     }
 };
+
+// Time:  O(nlogk)
+// Space: O(n)
+// Heap solution.
+class Solution2 {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> counts;
+        for (int i = 0; i < nums.size(); ++i) {
+            ++counts[nums[i]];
+        }
+        priority_queue<pair<int, int>> heap;
+        for (auto it = counts.begin(); it != counts.end(); ++it) {
+            heap.emplace(-(it->second), it->first);
+            if (heap.size() == k + 1) {
+                heap.pop();
+            }
+        }
+        vector<int> result;
+        while (!heap.empty()) {
+            result.emplace_back(heap.top().second);
+            heap.pop();
+        }
+        reverse(result.begin(), result.end());
+        return result;
+    }
+};
