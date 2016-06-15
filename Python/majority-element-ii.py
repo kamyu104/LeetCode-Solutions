8813646aeba9e1fce00ea73b352b82740e76516e
kamyu <kamyu104@users.noreply.github.com>
2016-06-15 17:02:38 +0800

    Update majority-element-ii.py

--- Python/majority-element-ii.py
+++ Python/majority-element-ii.py
@@ -11,33 +11,33 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        k, n, hash = 3, len(nums), collections.defaultdict(int)
+        k, n, cnts = 3, len(nums), collections.defaultdict(int)
 
         for i in nums:
-            hash[i] += 1
-            # Detecting k items in hash, at least one of them must have exactly
+            cnts[i] += 1
+            # Detecting k items in cnts, at least one of them must have exactly
             # one in it. We will discard those k items by one for each.
             # This action keeps the same mojority numbers in the remaining numbers.
             # Because if x / n  > 1 / k is true, then (x - 1) / (n - k) > 1 / k is also true.
-            if len(hash) == k:
-                for j in hash.keys():
-                    hash[j] -= 1
-                    if hash[j] == 0:
-                        del hash[j]
+            if len(cnts) == k:
+                for j in cnts.keys():
+                    cnts[j] -= 1
+                    if cnts[j] == 0:
+                        del cnts[j]
 
-        # Resets hash for the following counting.
-        for i in hash.keys():
-            hash[i] = 0
+        # Resets cnts for the following counting.
+        for i in cnts.keys():
+            cnts[i] = 0
 
         # Counts the occurrence of each candidate integer.
         for i in nums:
-            if i in hash:
-                hash[i] += 1
+            if i in cnts:
+                cnts[i] += 1
 
         # Selects the integer which occurs > [n / k] times.
         result = []
-        for i in hash.keys():
-            if hash[i] > n / k:
+        for i in cnts.keys():
+            if cnts[i] > n / k:
                 result.append(i)
 
         return result
