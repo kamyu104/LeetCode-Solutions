fb18de9726b55ec36eeeb22205775e715b0e90b6
kamyu <kamyu104@users.noreply.github.com>
2016-06-16 14:04:34 +0800

    Create logger-rate-limiter.py

---
+++ Python/logger-rate-limiter.py
@@ -0,0 +1,31 @@
+# Time:  O(1), amortized
+# Space: O(k), k is the max number of messages in last 10 seconds
+
+class Logger(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.__dq = collections.deque()
+        self.__printed = set()
+
+    def shouldPrintMessage(self, timestamp, message):
+        """
+        Returns true if the message should be printed in the given timestamp, otherwise returns false. The timestamp is in seconds granularity.
+        :type timestamp: int
+        :type message: str
+        :rtype: bool
+        """
+        while self.__dq and self.__dq[0][0] <= timestamp - 10:
+            self.__printed.remove(self.__dq.popleft()[1])
+        if message in self.__printed:
+            return False
+        self.__dq.append((timestamp, message))
+        self.__printed.add(message)
+        return True
+
+
+# Your Logger object will be instantiated and called as such:
+# obj = Logger()
+# param_1 = obj.shouldPrintMessage(timestamp,message)
