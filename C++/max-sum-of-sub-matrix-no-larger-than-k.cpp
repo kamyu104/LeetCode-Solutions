8c29d2eb526932ac0974833238228ccbb7684de8
kamyu <kamyu104@users.noreply.github.com>
2016-06-22 13:40:06 +0800

    Create max-sum-of-sub-matrix-no-larger-than-k.cpp

---
+++ C++/max-sum-of-sub-matrix-no-larger-than-k.cpp
@@ -0,0 +1,40 @@
+// Time:  O(min(m, n)^2 * max(m, n) * log(max(m, n)))
+// Space: O(max(m, n))
+
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+        if (matrix.empty()) {
+            return 0;
+        }
+
+        const int m = min(matrix.size(), matrix[0].size());
+        const int n = max(matrix.size(), matrix[0].size());
+        int result = numeric_limits<int>::min();
+
+        for (int i = 0; i < m; ++i) {
+            vector<int> sums(n, 0);
+            for (int j = i; j < m; ++j) {
+                for (int k = 0; k < n; ++k) {
+                    sums[k] += (n == matrix.size()) ? matrix[k][j] : matrix[j][k];
+                }
+    
+                // Find the max subarray no more than K.
+                set<int> accu_sum_set;
+                accu_sum_set.emplace(0);
+                int accu_sum = 0, curr_max = numeric_limits<int>::min();
+                for (int sum : sums) {
+                    accu_sum += sum;
+                    auto it = accu_sum_set.lower_bound(accu_sum - k);
+                    if (it != accu_sum_set.end()) {
+                        curr_max = max(curr_max, accu_sum - *it);
+                    }
+                    accu_sum_set.emplace(accu_sum);
+                }
+                result = max(result, curr_max);
+            }
+        }
+
+        return result;
+    }
+};
