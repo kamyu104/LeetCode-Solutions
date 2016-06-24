b8924441cbb2acb4aa6580527ae4f96fb6375136
kamyu <kamyu104@users.noreply.github.com>
2016-06-24 11:06:18 +0800

    Update data-stream-as-disjoint-intervals.cpp

--- C++/data-stream-as-disjoint-intervals.cpp
+++ C++/data-stream-as-disjoint-intervals.cpp
@@ -33,7 +33,7 @@ public:
     }
 
     vector<Interval> getIntervals() {
-        return {intervals_.begin(), intervals_.end()};
+        return {intervals_.cbegin(), intervals_.cend()};
     }
 
 private:
