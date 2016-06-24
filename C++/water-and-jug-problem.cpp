91a49fb782bdff51fe5462fc8daee79024b906d7
kamyu <kamyu104@users.noreply.github.com>
2016-06-24 15:29:40 +0800

    Create water-and-jug-problem.cpp

---
+++ C++/water-and-jug-problem.cpp
@@ -0,0 +1,19 @@
+// Time:  O(logn), n is the max of (x, y)
+// Space: O(1)
+
+class Solution {
+public:
+    bool canMeasureWater(int x, int y, int z) {
+        return z <= max(x, y) && z % gcd(x, y) == 0;
+    }
+    
+private:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return b;
+    }
+};
