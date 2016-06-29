bd0e5aff503293ddf2b1b990393727d92fa41fb9
kamyu <kamyu104@users.noreply.github.com>
2016-06-28 23:48:04 -0500

    Create range-addition .py

---
+++ Python/range-addition .py
@@ -0,0 +1,20 @@
+# Time:  O(k + n)
+# Space: O(1)
+
+class Solution(object):
+    def getModifiedArray(self, length, updates):
+        """
+        :type length: int
+        :type updates: List[List[int]]
+        :rtype: List[int]
+        """
+        result = [0] * length
+        for update in updates:
+            result[update[0]] += update[2]
+            if update[1]+1 < length:
+                result[update[1]+1] -= update[2]
+
+        for i in xrange(1, length):
+            result[i] += result[i-1]
+
+        return result
