e1c1d53b5535f7633e09132071c27419efae688a
kamyu <kamyu104@users.noreply.github.com>
2016-06-29 23:23:59 +0800

    Update range-sum-query-mutable.cpp

--- C++/range-sum-query-mutable.cpp
+++ C++/range-sum-query-mutable.cpp
@@ -3,8 +3,62 @@
 //        query:  O(logn)
 // Space: O(n)
 
-// Segment Tree solution.
+// Binary Indexed Tree (BIT) solution.
 class NumArray {
+public:
+    NumArray(vector<int> &nums) : nums_(nums) {
+        bit_ = vector<int>(nums_.size() + 1);
+        for (int i = 1; i < bit_.size(); ++i) {
+            bit_[i] = nums[i - 1] + bit_[i - 1];
+        }
+        for (int i = bit_.size() - 1; i >= 1; --i) {
+            int last_i = i - lower_bit(i);
+            bit_[i] -= bit_[last_i];
+        }
+    }
+
+    void update(int i, int val) {
+        if (val - nums_[i]) {
+            add(i, val - nums_[i]);
+            nums_[i] = val;
+        }
+    }
+
+    int sumRange(int i, int j) {
+        return sum(j) - sum(i - 1);
+    }
+
+private:
+    vector<int> &nums_;
+    vector<int> bit_;
+
+    int sum(int i) {
+        ++i;
+        int sum = 0;
+        for (; i > 0; i -= lower_bit(i)) {
+            sum += bit_[i];
+        }
+        return sum;
+    }
+
+    void add(int i, int val) {
+        ++i;
+        for (; i <= nums_.size(); i += lower_bit(i)) {
+            bit_[i] += val;
+        }
+    }
+
+    inline int lower_bit(int i) {
+        return i & -i;
+    }
+};
+
+// Time:  ctor:   O(n),
+//        update: O(logn),
+//        query:  O(logn)
+// Space: O(n)
+// Segment Tree solution.
+class NumArray2 {
 public:
     NumArray(vector<int> &nums) : nums_(nums) {
         root_ = buildHelper(nums, 0, nums.size() - 1);
@@ -99,61 +153,6 @@ private:
                sumRangeHelper(root->right, start, end);
     }
 };
-
-// Time:  ctor:   O(n),
-//        update: O(logn),
-//        query:  O(logn)
-// Space: O(n)
-// Binary Indexed Tree (BIT) solution.
-class NumArray2 {
-public:
-    NumArray(vector<int> &nums) : nums_(nums) {
-        bit_ = vector<int>(nums_.size() + 1);
-        for (int i = 1; i < bit_.size(); ++i) {
-            bit_[i] = nums[i - 1] + bit_[i - 1];
-        }
-        for (int i = bit_.size() - 1; i >= 1; --i) {
-            int last_i = i - lower_bit(i);
-            bit_[i] -= bit_[last_i];
-        }
-    }
-
-    void update(int i, int val) {
-        if (val - nums_[i]) {
-            add(i, val - nums_[i]);
-            nums_[i] = val;
-        }
-    }
-
-    int sumRange(int i, int j) {
-        return sum(j) - sum(i - 1);
-    }
-
-private:
-    vector<int> &nums_;
-    vector<int> bit_;
-
-    int sum(int i) {
-        ++i;
-        int sum = 0;
-        for (; i > 0; i -= lower_bit(i)) {
-            sum += bit_[i];
-        }
-        return sum;
-    }
-
-    void add(int i, int val) {
-        ++i;
-        for (; i <= nums_.size(); i += lower_bit(i)) {
-            bit_[i] += val;
-        }
-    }
-
-    inline int lower_bit(int i) {
-        return i & -i;
-    }
-};
-
 
 // Your NumArray object will be instantiated and called as such:
 // NumArray numArray(nums);
