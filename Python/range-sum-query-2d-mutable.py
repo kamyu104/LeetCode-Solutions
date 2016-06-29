15094c3325d130a022db70fc4a0b41832bb4d67d
kamyu <kamyu104@users.noreply.github.com>
2016-06-29 21:44:43 +0800

    Update range-sum-query-2d-mutable.py

--- Python/range-sum-query-2d-mutable.py
+++ Python/range-sum-query-2d-mutable.py
@@ -1,10 +1,93 @@
-# Time:  ctor:   O(mlogm * nlogn)
+# Time:  ctor:   O(m * n)
 #        update: O(logm * logn)
 #        query:  O(logm * logn)
 # Space: O(m * n)
 
 # Binary Indexed Tree (BIT) solution.
 class NumMatrix(object):
+    def __init__(self, matrix):
+        """
+        initialize your data structure here.
+        :type matrix: List[List[int]]
+        """
+        if not matrix:
+            return
+        self.__matrix = matrix
+        self.__bit = [[0] * (len(self.__matrix[0]) + 1) \
+                      for _ in xrange(len(self.__matrix) + 1)]
+
+        bit = [[0] * (len(self.__matrix[0]) + 1) \
+               for _ in xrange(len(self.__matrix) + 1)]
+        for i in xrange(1, len(bit)):
+            for j in xrange(1, len(bit[0])):
+                bit[i][j] = matrix[i-1][j-1] + bit[i-1][j] + bit[i][j-1] - bit[i-1][j-1]
+
+        self.__bit = [[0] * (len(self.__matrix[0]) + 1) \
+                      for _ in xrange(len(self.__matrix) + 1)]
+        for i in xrange(1, len(bit)):
+            for j in xrange(1, len(bit[0])):
+                last_i = i - (i & -i)
+                last_j = j - (j & -j)
+                self.__bit[i][j] = bit[i][j] - bit[i][last_j] - bit[last_i][j]+ bit[last_i][last_j]
+
+    def update(self, row, col, val):
+        """
+        update the element at matrix[row,col] to val.
+        :type row: int
+        :type col: int
+        :type val: int
+        :rtype: void
+        """
+        if val - self.__matrix[row][col]:
+            self.__add(row, col, val - self.__matrix[row][col])
+            self.__matrix[row][col] = val
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return self.__sum(row2, col2) - self.__sum(row2, col1 - 1) - \
+               self.__sum(row1 - 1, col2) + self.__sum(row1 - 1, col1 - 1)
+
+    def __sum(self, row, col):
+        if row < 0 or col < 0:
+            return 0
+
+        row += 1
+        col += 1
+        ret = 0
+        i = row
+        while i > 0:
+            j = col
+            while j > 0:
+                ret += self.__bit[i][j]
+                j -= (j & -j)
+            i -= (i & -i)
+        return ret
+
+    def __add(self, row, col, val):
+        row += 1
+        col += 1
+        i = row
+        while i <= len(self.__matrix):
+            j = col
+            while j <= len(self.__matrix[0]):
+                self.__bit[i][j] += val
+                j += (j & -j)
+            i += (i & -i)
+
+
+# Time:  ctor:   O(mlogm * nlogn)
+#        update: O(logm * logn)
+#        query:  O(logm * logn)
+# Space: O(m * n)
+# Binary Indexed Tree (BIT) solution.
+class NumMatrix2(object):
     def __init__(self, matrix):
         """
         initialize your data structure here.
@@ -31,7 +114,6 @@ class NumMatrix(object):
             self.__add(row, col, val - self.__matrix[row][col])
             self.__matrix[row][col] = val
 
-
     def sumRegion(self, row1, col1, row2, col2):
         """
         sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
@@ -41,26 +123,23 @@ class NumMatrix(object):
         :type col2: int
         :rtype: int
         """
-        def sumRegion_bit(row, col):
-            row += 1
-            col += 1
-            ret = 0
-            i = row
-            while i > 0:
-                j = col
-                while j > 0:
-                    ret += self.__bit[i][j]
-                    j -= (j & -j)
-                i -= (i & -i)
-            return ret
-    
-        ret = sumRegion_bit(row2, col2)
-        if row1 > 0 and col1 > 0:
-            ret += sumRegion_bit(row1 - 1, col1 - 1)
-        if col1 > 0:
-            ret -= sumRegion_bit(row2, col1 - 1)
-        if row1 > 0:
-            ret -= sumRegion_bit(row1 - 1, col2)
+        return self.__sum(row2, col2) - self.__sum(row2, col1 - 1) - \
+               self.__sum(row1 - 1, col2) + self.__sum(row1 - 1, col1 - 1)
+
+    def __sum(self, row, col):
+        if row < 0 or col < 0:
+            return 0
+
+        row += 1
+        col += 1
+        ret = 0
+        i = row
+        while i > 0:
+            j = col
+            while j > 0:
+                ret += self.__bit[i][j]
+                j -= (j & -j)
+            i -= (i & -i)
         return ret
 
     def __add(self, row, col, val):
