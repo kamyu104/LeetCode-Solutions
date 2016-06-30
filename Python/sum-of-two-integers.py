7413f5b32a3fb24be4426589f8be5b80e3950fb5
kamyu <kamyu104@users.noreply.github.com>
2016-07-01 01:23:41 +0800

    Update sum-of-two-integers.py

--- Python/sum-of-two-integers.py
+++ Python/sum-of-two-integers.py
@@ -15,7 +15,7 @@ class Solution(object):
         :rtype: int
         """
         bit_length = 32
-        neg_bit, mask = 1 << (bit_length-1), ~(~0 << bit_length)
+        neg_bit, mask = (1 << bit_length) >> 1, ~(~0 << bit_length)
 
         a = (a | ~mask) if (a & neg_bit) else (a & mask)
         b = (b | ~mask) if (b & neg_bit) else (b & mask)
