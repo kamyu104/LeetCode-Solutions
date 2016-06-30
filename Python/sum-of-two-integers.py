150ab60b2f02d18af23eb1f99ff91081b6699f7d
kamyu <kamyu104@users.noreply.github.com>
2016-07-01 00:33:23 +0800

    Update sum-of-two-integers.py

--- Python/sum-of-two-integers.py
+++ Python/sum-of-two-integers.py
@@ -14,8 +14,15 @@ class Solution(object):
         :type b: int
         :rtype: int
         """
-        while b and b != 1 << 32:
-            carry = a & b
-            a ^= b
-            b = carry << 1
-        return a if b != 1 << 32 else a & 0xffffffff
+        bit_length = 32
+        neg_bit, mask = 1 << (bit_length-1), ~(~0 << bit_length)
+
+        a = a | ~mask if a & neg_bit else a & mask
+        b = b | ~mask if b & neg_bit else b & mask
+
+        while b:
+            carry = (a & b) & mask
+            a = (a ^ b) & mask
+            b = carry << 1 & mask
+
+        return a | ~mask if a & neg_bit else a
