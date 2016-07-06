56150d00b6b0b569e1721c0dd5a2d7856c4ff880
kamyu <kamyu104@users.noreply.github.com>
2016-07-06 23:35:25 +0800

    Update powx-n.cpp

--- C++/powx-n.cpp
+++ C++/powx-n.cpp
@@ -4,16 +4,16 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        double res = 1;
-        long abs_n = abs(static_cast<long>(n));
+        double result = 1;
+        long long abs_n = abs(static_cast<long long>(n));
         while (abs_n > 0) {
             if (abs_n & 1) {
-                res *= x;
+                result *= x;
             }
             abs_n >>= 1;
             x *= x;
         }
-        return n < 0 ?  1 / res : res;
+        return n < 0 ? 1 / result : result;
     }
 };
 
