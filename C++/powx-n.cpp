68b5205cb1425fe636b58dcf89c3f15173c8b834
kamyu <kamyu104@users.noreply.github.com>
2016-07-06 23:25:27 +0800

    Update powx-n.cpp

--- C++/powx-n.cpp
+++ C++/powx-n.cpp
@@ -1,5 +1,5 @@
-// Time:  O(logn)
-// Space: O(1)
+// Time:  O(logn * nlogx * logx) = O(1)
+// Space: O(nlogx) = O(1)
 
 class Solution {
 public:
@@ -14,5 +14,26 @@ public:
             x *= x;
         }
         return n < 0 ?  1 / res : res;
+    }
+};
+
+// Time:  O(logn * nlogx * logx) = O(1)
+// Space: O(nlogx) = O(1)
+// Recursive solution.
+class Solution2 {
+public:
+    double myPow(double x, int n) {
+        if (n < 0 && n != -n) {
+            return 1.0 / myPow(x, -n);
+        }
+        if (n == 0) {
+            return 1;
+        }
+        double v = myPow(x, n / 2);
+        if (n % 2 == 0) {
+            return v * v;
+        } else {
+            return v * v * x;
+        }
     }
 };
