fcd11a2d5f1ef833b51776abafb89a1f86ffea62
kamyu <kamyu104@users.noreply.github.com>
2016-07-08 21:11:51 +0800

    Update palindrome-number.cpp

--- C++/palindrome-number.cpp
+++ C++/palindrome-number.cpp
@@ -4,6 +4,24 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        int temp = x;
+        int reversed = 0;
+        while (temp != 0) {
+            reversed = reversed * 10 + temp % 10;
+            temp = temp / 10;
+        }
+        return reversed == x;
+    }
+};
+
+// Time:  O(logx) = O(1)
+// Space: O(1)
+class Solution2 {
+public:
+    bool isPalindrome(int x) {
         if(x < 0) {
             return false;
         }
@@ -22,23 +40,5 @@ public:
         }
 
         return true;
-    }
-};
-
-// Time:  O(logx) = O(1)
-// Space: O(1)
-class Solution2 {
-public:
-    bool isPalindrome(int x) {
-        if (x < 0) {
-            return false;
-        }
-        int temp = x;
-        int reversed = 0;
-        while (temp != 0) {
-            reversed = reversed * 10 + temp % 10;
-            temp = temp / 10;
-        }
-        return reversed == x;
     }
 };
