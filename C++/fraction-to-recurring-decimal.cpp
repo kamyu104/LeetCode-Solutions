7e37785ef9798a2eae42ef80491d73e80e063fcf
kamyu <kamyu104@users.noreply.github.com>
2016-07-18 21:31:58 +0800

    Create fraction-to-recurring-decimal.cpp

---
+++ C++/fraction-to-recurring-decimal.cpp
@@ -0,0 +1,37 @@
+// Time:  O(logn), where logn is the length of result strings
+// Space: O(1)
+
+class Solution {
+public:
+    string fractionToDecimal(int numerator, int denominator) {
+        string result;
+        if ((numerator ^ denominator) >> 31 && numerator != 0) {
+            result = "-";
+        }
+
+        auto dvd = llabs(numerator);
+        auto dvs = llabs(denominator);
+        result += to_string(dvd / dvs);
+        dvd %= dvs;
+        if (dvd > 0) {
+            result += ".";
+        }
+        
+        unordered_map<long long, int> lookup;
+        while (dvd) {
+            if (lookup.count(dvd)) {
+                result.insert(lookup[dvd], "(");
+                result.push_back(')');
+                break;
+            }
+
+            lookup[dvd] = result.length();
+
+            dvd *= 10;
+            result += to_string(dvd / dvs);
+            dvd %= dvs;
+        }
+        
+        return result;
+    }
+};
