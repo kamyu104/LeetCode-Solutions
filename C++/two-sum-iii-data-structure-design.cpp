5e96c298138b4eb44ea9900adc60e12d174038f2
kamyu <kamyu104@users.noreply.github.com>
2016-07-20 20:53:44 +0800

    Update two-sum-iii-data-structure-design.cpp

--- C++/two-sum-iii-data-structure-design.cpp
+++ C++/two-sum-iii-data-structure-design.cpp
@@ -5,20 +5,20 @@ class TwoSum {
 public:
 
     // Add the number to an internal data structure.
-	void add(int number) {
-	    ++lookup_[number];
-	}
+    void add(int number) {
+        ++lookup_[number];
+    }
 
     // Find if there exists any pair of numbers which sum is equal to the value.
-	bool find(int value) {
-	    for (const auto& kvp : lookup_) {
-	        const auto num = value - kvp.first;
-	        if (lookup_.count(num) && (num != kvp.first || kvp.second > 1)) {
-	            return true;
-	        }
-	    }
-	    return false;
-	}
+    bool find(int value) {
+        for (const auto& kvp : lookup_) {
+            const auto num = value - kvp.first;
+            if (lookup_.count(num) && (num != kvp.first || kvp.second > 1)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 private:
     unordered_map<int, int> lookup_;
