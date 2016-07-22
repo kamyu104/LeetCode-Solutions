57632f7a6e7ca5c4ce422f49a85e5a8ae9688f20
kamyu <kamyu104@users.noreply.github.com>
2016-07-22 21:28:40 +0800

    Update count-primes.py

--- Python/count-primes.py
+++ Python/count-primes.py
@@ -1,13 +1,11 @@
 # Time:  O(n)
 # Space: O(n)
+
 # Description:
 #
 # Count the number of prime numbers less than a non-negative number, n
 #
 # Hint: The number n could be in the order of 100,000 to 5,000,000.
-#
-
-from math import sqrt
 
 class Solution:
     # @param {integer} n
@@ -18,10 +16,14 @@ class Solution:
         
         is_prime = [True] * n
         num = n / 2
-        for i in xrange(3, int(sqrt(n))+1, 2):
+        for i in xrange(3, n, 2):
+            if i * i >= n:
+                break
+
             if is_prime[i]:
                 for j in xrange(i*i, n, 2*i):
                     if is_prime[j]:
                         num -= 1
                         is_prime[j] = False
+
         return num
