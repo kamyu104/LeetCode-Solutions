5bfac97bec0973aa24a9456c6988e47d17687f0a
kamyu <kamyu104@users.noreply.github.com>
2016-07-24 20:39:45 +0800

    Update isomorphic-strings.cpp

--- C++/isomorphic-strings.cpp
+++ C++/isomorphic-strings.cpp
@@ -1,16 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         if (s.length() != t.length()) {
             return false;
         }
-        vector<int> m1(256, 0);
-        vector<int> m2(256, 0);
-        int n = s.size();
-        for (int i = 0; i < n; ++i) {
-            if (m1[s[i]] != m2[t[i]]) return false;
-            m1[s[i]] = i + 1;
-            m2[t[i]] = i + 1;
+        vector<int> s2t(256, 0), t2s(256, 0);
+        for (int i = 0; i < s.length(); ++i) {
+            if (s2t[s[i]] == 0 && t2s[t[i]] == 0) {
+                s2t[s[i]] = t[i];
+                t2s[t[i]] = s[i];
+            } else if (s2t[s[i]] == 0 || s2t[s[i]] != t[i]) {
+                // Contradict mapping.
+                return false;
+            }
         }
         return true;
     }
