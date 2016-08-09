561a1616f4c812bb71a6c5ca2dda6e43762db8bb
kamyu <kamyu104@users.noreply.github.com>
2016-08-09 11:55:53 +0800

    Update insert-delete-getrandom-o1-duplicates-allowed.cpp

--- C++/insert-delete-getrandom-o1-duplicates-allowed.cpp
+++ C++/insert-delete-getrandom-o1-duplicates-allowed.cpp
@@ -44,6 +44,50 @@ private:
     unordered_map<int, vector<int>> used_;
 };
 
+
+// Time:  O(1)
+// Space: O(n)
+class RandomizedCollection2 {
+public:
+    /** Initialize your data structure here. */
+    RandomizedCollection() {
+        
+    }
+    
+    /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
+    bool insert(int val) {
+        list_.emplace_back(val);
+        used_.emplace(val, list_.size() - 1);
+
+        return true; 
+    }
+    
+    /** Removes a value from the collection. Returns true if the collection contained the specified element. */
+    bool remove(int val) {
+        if (!used_.count(val)) {
+            return false;
+        }
+        auto it_to_delete = used_.find(val);
+        auto it_to_back = used_.find(list_.back());
+        it_to_back->second = it_to_delete->second;
+        swap(list_[it_to_delete->second], list_.back());
+
+        used_.erase(it_to_delete);
+        list_.pop_back();
+
+        return true;  
+    }
+    
+    /** Get a random element from the collection. */
+    int getRandom() {
+        return list_[rand() % list_.size()];
+    }
+
+private:
+    vector<int> list_;
+    unordered_multimap<int, int> used_;
+};
+
 /**
  * Your RandomizedCollection object will be instantiated and called as such:
  * RandomizedCollection obj = new RandomizedCollection();
