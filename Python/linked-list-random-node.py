e759710141b6e2134343087d86409fe5577f195b
kamyu <kamyu104@users.noreply.github.com>
2016-08-10 21:20:32 +0800

    Create linked-list-random-node.py

---
+++ Python/linked-list-random-node.py
@@ -0,0 +1,53 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Given a singly linked list, return a random node's value from the linked list.
+# Each node must have the same probability of being chosen.
+#
+# Follow up:
+# What if the linked list is extremely large and its length is unknown to you?
+# Could you solve this efficiently without using extra space?
+#
+# Example:
+#
+# // Init a singly linked list [1,2,3].
+# ListNode head = new ListNode(1);
+# head.next = new ListNode(2);
+# head.next.next = new ListNode(3);
+# Solution solution = new Solution(head);
+#
+# // getRandom() should return either 1, 2, or 3 randomly.
+# Each element should have equal probability of returning.
+# solution.getRandom();
+
+
+from random import randint
+
+class Solution(object):
+
+    def __init__(self, head):
+        """
+        @param head The linked list's head. Note that the head is guanranteed to be not null, so it contains at least one node.
+        :type head: ListNode
+        """
+        self.__head = head
+
+
+    # Proof of Reservoir Sampling:
+    # https://discuss.leetcode.com/topic/53753/brief-explanation-for-reservoir-sampling
+    def getRandom(self):
+        """
+        Returns a random node's value.
+        :rtype: int
+        """
+        reservoir = self.__head.val
+        curr, n = self.__head.next, 1
+        while curr:
+            reservoir = curr.val if randint(1, n+1) == 1 else reservoir
+            curr, n = curr.next, n+1
+        return reservoir
+        
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(head)
+# param_1 = obj.getRandom()
