db58373922183b22fff45e9886120da5cd8b355b
kamyu <kamyu104@users.noreply.github.com>
2016-08-11 21:20:33 +0800

    Update ransom-note.py

--- Python/ransom-note.py
+++ Python/ransom-note.py
@@ -38,3 +38,16 @@ class Solution(object):
                     break
 
         return letters == 0
+
+# Time:  O(n)
+# Space: O(1)
+from collections import Counter
+
+class Solution2(object):
+    def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        return not Counter(ransomNote) - Counter(magazine)
