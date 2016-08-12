e0d3fb54998fbdf6e50b56451503003372d73e27
kamyu <kamyu104@users.noreply.github.com>
2016-08-12 13:26:15 +0800

    Create shuffle-an-array.py

---
+++ Python/shuffle-an-array.py
@@ -0,0 +1,56 @@
+# Time:  O(n)
+# Space: O(n)
+
+# Shuffle a set of numbers without duplicates.
+#
+# Example:
+#
+# // Init an array with set 1, 2, and 3.
+# int[] nums = {1,2,3};
+# Solution solution = new Solution(nums);
+#
+# // Shuffle the array [1,2,3] and return its result.
+# Any permutation of [1,2,3] must equally likely to be returned.
+# solution.shuffle();
+#
+# // Resets the array back to its original configuration [1,2,3].
+# solution.reset();
+#
+# // Returns the random shuffling of array [1,2,3].
+# solution.shuffle();
+
+class Solution(object):
+
+    def __init__(self, nums):
+        """
+        
+        :type nums: List[int]
+        :type size: int
+        """
+        self.__nums = nums
+
+
+    def reset(self):
+        """
+        Resets the array to its original configuration and return it.
+        :rtype: List[int]
+        """
+        return self.__nums
+        
+
+    def shuffle(self):
+        """
+        Returns a random shuffling of the array.
+        :rtype: List[int]
+        """
+        nums = list(self.__nums)
+        for i in xrange(len(nums)):
+            j = random.randint(i, len(nums)-1)
+            nums[i], nums[j] = nums[j], nums[i]
+        return nums
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
