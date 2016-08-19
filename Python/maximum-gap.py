c3dea4e405c434c76ececa6f162d19485881635c
kamyu <kamyu104@users.noreply.github.com>
2016-08-19 23:12:08 +0800

    Update maximum-gap.py

--- Python/maximum-gap.py
+++ Python/maximum-gap.py
@@ -1,6 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
-#
+
 # Given an unsorted array, find the maximum difference between 
 # 
 # the successive elements in its sorted form.
@@ -12,7 +12,6 @@
 # You may assume all elements in the array are non-negative integers
 # 
 #  and fit in the 32-bit signed integer range.
-#
 
 # bucket sort
 # Time:  O(n)
