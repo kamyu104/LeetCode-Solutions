03d4e8ac880e4a7b72d9773a520fab0826c93256
kamyu <kamyu104@users.noreply.github.com>
2016-08-22 23:21:29 +0800

    Create first-unique-character-in-a-string.cpp

---
+++ C++/first-unique-character-in-a-string.cpp
@@ -0,0 +1,24 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        using IT = list<int>::iterator;
+
+    	list<int> candidates;
+    	unordered_map<char, IT> lookup;
+    	for (int i = 0; i < s.size(); ++i) {
+    		const auto& c = s[i];
+    		if (lookup.count(c)) {
+    			if (lookup[c] != candidates.end()) {
+    				candidates.erase(lookup[c]);
+    			}
+    			lookup[c] = candidates.end();
+    		} else {
+    			lookup[c] = candidates.emplace(candidates.end(), i);
+    		}
+    	}
+    	return candidates.empty() ? -1 : candidates.front();
+    }
+};
