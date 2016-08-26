1caca70745ef6e0afd0b514937c44b75d8a2e714
kamyu <kamyu104@users.noreply.github.com>
2016-08-27 00:01:06 +0800

    Create search-insert-position.cpp

---
+++ C++/search-insert-position.cpp
@@ -0,0 +1,21 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            const auto mid = left + (right -left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+};
