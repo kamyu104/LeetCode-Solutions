59ab35b565ba00a78e5990b69e6fce248d9be248
kamyu <kamyu104@users.noreply.github.com>
2016-09-06 11:45:33 +0800

    Create longest-substring-with-at-least-k-repeating-characters.py

---
+++ Python/longest-substring-with-at-least-k-repeating-characters.py
@@ -0,0 +1,55 @@
+# Time:  O(26 * n) = O(n)
+# Space: O(26) = O(1)
+
+# Find the length of the longest substring T of a given string
+# (consists of lowercase letters only) such that every character in T
+# appears no less than k times.
+#
+# Example 1:
+#
+# Input:
+# s = "aaabb", k = 3
+#
+# Output:
+# 3
+#
+# The longest substring is "aaa", as 'a' is repeated 3 times.
+# Example 2:
+#
+# Input:
+# s = "ababbc", k = 2
+#
+# Output:
+# 5
+#
+# The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+
+# Recursive solution.
+class Solution(object):
+    def longestSubstring(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        def longestSubstringHelper(s, k, start, end):
+            count = [0] * 26
+            for i in xrange(start, end):
+                count[ord(s[i]) - ord('a')] += 1
+            max_len = 0
+            i = start
+            while i < end:
+                while i < end and count[ord(s[i]) - ord('a')] < k:
+                    i += 1
+                j = i
+                while j < end and count[ord(s[j]) - ord('a')] >= k:
+                    j += 1
+
+                if i == start and j == end:
+                    return end - start
+
+                max_len = max(max_len, longestSubstringHelper(s, k, i, j))
+                i = j
+            return max_len
+
+        return longestSubstringHelper(s, k, 0, len(s))
