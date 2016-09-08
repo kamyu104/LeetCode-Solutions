154e80dc5fb2720f0afbbc3f573d808ae969d7d9
kamyu <kamyu104@users.noreply.github.com>
2016-09-08 23:54:37 +0800

    Update validate-binary-search-tree.cpp

--- C++/validate-binary-search-tree.cpp
+++ C++/validate-binary-search-tree.cpp
@@ -1,5 +1,5 @@
 // Time:  O(n)
-// Space: O(h)
+// Space: O(1)
 
 /**
  * Definition for a binary tree node.
@@ -10,7 +10,46 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+// Morris Traversal
 class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        TreeNode *prev = nullptr;
+        TreeNode *curr = root;
+        while (curr) {
+            if (!curr->left) {
+                if (prev && prev->val >= curr->val) {
+                    return false;
+                }
+                prev = curr;
+                curr = curr->right;
+            } else {
+                TreeNode *node = curr->left;
+                while (node->right && node->right != curr) {
+                    node = node->right;
+                }
+                if (!node->right) {
+                    node->right = curr;
+                    curr = curr->left;
+                } else {
+                    if (prev && prev->val >= curr->val) {
+                        return false;
+                    }
+                    prev = curr;
+                    node->right = nullptr;
+                    curr = curr->right;
+                }
+            }
+        }
+                
+        return true;
+    }
+};
+
+// Time:  O(n)
+// Space: O(h)
+class Solution2 {
 public:
     bool isValidBST(TreeNode* root) {
         if (!root) {
