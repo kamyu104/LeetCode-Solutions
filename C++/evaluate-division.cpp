d1de407bed45046445d65cd93edd1d835fbcff0c
kamyu <kamyu104@users.noreply.github.com>
2016-09-12 01:14:14 +0800

    Update evaluate-division.cpp

--- C++/evaluate-division.cpp
+++ C++/evaluate-division.cpp
@@ -7,7 +7,6 @@ public:
         vector<double>& values, vector<pair<string, string>> query) {
 
         unordered_map<string,unordered_map<string, double>> lookup;
-        vector<double> result;
         for (int i = 0; i < values.size(); ++i) {
             lookup[equations[i].first].emplace(equations[i].second, values[i]);
             if (values[i] != 0) {
@@ -15,6 +14,7 @@ public:
             }
         }
 
+        vector<double> result;
         for (const auto& i : query) {
             unordered_set<string> visited;
             auto tmp = check(i.first, i.second, lookup, &visited);
@@ -34,12 +34,12 @@ private:
         if (lookup[up].find(down) != lookup[up].end()) {
             return {true, lookup[up][down]};
         }
-        for (const auto& i : lookup[up]) {
-            if (!visited->count(i.first)) {
-                visited->emplace(i.first);
-                const auto tmp = check(i.first, down, lookup, visited);
+        for (const auto& q : lookup[up]) {
+            if (!visited->count(q.first)) {
+                visited->emplace(q.first);
+                const auto tmp = check(q.first, down, lookup, visited);
                 if (tmp.first) {
-                    return {true, i.second * tmp.second};
+                    return {true, q.second * tmp.second};
                 }
             }
         }
