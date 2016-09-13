d79d9a1b570b551fe36fc6b35ccec9b4df3e2fb0
kamyu <kamyu104@users.noreply.github.com>
2016-09-13 13:38:38 +0800

    Update water-and-jug-problem.cpp

--- C++/water-and-jug-problem.cpp
+++ C++/water-and-jug-problem.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     bool canMeasureWater(int x, int y, int z) {
-        return z <= max(x, y) && z % gcd(x, y) == 0;
+        return z == 0 || (z <= x + y && z % gcd(x, y) == 0);
     }
     
 private:
