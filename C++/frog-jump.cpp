66305d865dbd517196c9eaac3acd38508c34ef5d
kamyu <kamyu104@users.noreply.github.com>
2016-09-19 02:30:57 +0800

    Update frog-jump.cpp

--- C++/frog-jump.cpp
+++ C++/frog-jump.cpp
@@ -13,26 +13,15 @@ public:
         dp[0] = true;
         for (int i = 0; i < stones.size(); ++i) {
             if (dp[i]) {
-                for (const auto& j : findNextStones(stones, lookup, i)) {
-                    dp[j] = true;
+                for (const auto& k : {i - 1, i, i + 1}) {
+                    const auto it = lookup.find(stones[i] + k);
+                    if (it != lookup.end()) {
+                        dp[it->second] = true;
+                    }
                 }
             }
         }
         return dp.back();
-    }
-
-private:
-    vector<int> findNextStones(const vector<int>& stones,
-                               const unordered_map<int, int>& lookup,
-                               int i) {
-        vector<int> next_stones;
-        for (const auto& k : {i - 1, i, i + 1}) {
-            const auto it = lookup.find(stones[i] + k);
-            if (it != lookup.end()) {
-                next_stones.emplace_back(it->second);
-            }
-        }
-        return next_stones;
     }
 };
 
