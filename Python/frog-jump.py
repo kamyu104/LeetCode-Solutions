e99f428486691170ba5f89e577dcac2a69d1aba2
kamyu <kamyu104@users.noreply.github.com>
2016-09-24 22:18:04 +0800

    Update frog-jump.py

--- Python/frog-jump.py
+++ Python/frog-jump.py
@@ -47,11 +47,11 @@ class Solution(object):
         if stones[1] != 1:
             return False
 
-        lookup = {s: set() for s in stones}
-        lookup[1].add(1)
+        last_jump_units = {s: set() for s in stones}
+        last_jump_units[1].add(1)
         for s in stones[:-1]:
-            for j in lookup[s]:
+            for j in last_jump_units[s]:
                 for k in (j-1, j, j+1):
-                    if k > 0 and s+k in lookup:
-                        lookup[s+k].add(k)
-        return bool(lookup[stones[-1]])
+                    if k > 0 and s+k in last_jump_units:
+                        last_jump_units[s+k].add(k)
+        return bool(last_jump_units[stones[-1]])
