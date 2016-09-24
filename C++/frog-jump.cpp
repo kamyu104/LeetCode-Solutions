106952cab836ccdd5e2cb6b4c6302ddfb3d8ba82
kamyu <kamyu104@users.noreply.github.com>
2016-09-24 22:17:38 +0800

    Update frog-jump.cpp

--- C++/frog-jump.cpp
+++ C++/frog-jump.cpp
@@ -8,22 +8,22 @@ public:
             return false;
         }
 
-        unordered_map<int, unordered_set<int>> lookup;
+        unordered_map<int, unordered_set<int>> last_jump_units;
         for (const auto& s: stones) {
-            lookup.emplace(s, {unordered_set<int>()});
+            last_jump_units.emplace(s, {unordered_set<int>()});
         }
-        lookup[1].emplace(1);
+        last_jump_units[1].emplace(1);
 
         for (int i = 0; i + 1 < stones.size(); ++i) {
-            for (const auto& j : lookup[stones[i]]) {
+            for (const auto& j : last_jump_units[stones[i]]) {
                 for (const auto& k : {j - 1, j, j + 1}) {
-                    if (k > 0 && lookup.count(stones[i] + k)) {
-                        lookup[stones[i] + k].emplace(k);
+                    if (k > 0 && last_jump_units.count(stones[i] + k)) {
+                        last_jump_units[stones[i] + k].emplace(k);
                     }
                 }
             }
         }
 
-        return !lookup[stones.back()].empty();
+        return !last_jump_units[stones.back()].empty();
     }
 };
