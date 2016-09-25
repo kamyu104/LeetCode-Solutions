058355b21374d2c5dd851554f6832a9aa78455fa
kamyu <kamyu104@users.noreply.github.com>
2016-09-25 18:09:13 +0800

    Update trapping-rain-water-ii.py

--- Python/trapping-rain-water-ii.py
+++ Python/trapping-rain-water-ii.py
@@ -33,28 +33,28 @@ class Solution(object):
         if not n:
             return 0
 
-        visited = [[False for i in xrange(n)] for j in xrange(m)]
+        is_visited = [[False for i in xrange(n)] for j in xrange(m)]
         
         heap = []
         for i in xrange(m):
             heappush(heap, [heightMap[i][0], i, 0])
-            visited[i][0] = True
+            is_visited[i][0] = True
             heappush(heap, [heightMap[i][n-1], i, n-1])
-            visited[i][n-1] = True
+            is_visited[i][n-1] = True
         for j in xrange(n):
             heappush(heap, [heightMap[0][j], 0, j])
-            visited[0][j] = True
+            is_visited[0][j] = True
             heappush(heap, [heightMap[m-1][j], m-1, j])
-            visited[m-1][j] = True
+            is_visited[m-1][j] = True
 
         trap = 0
         while heap:
             height, i, j = heappop(heap)
             for (dx, dy) in [(1,0), (-1,0), (0,1), (0,-1)]:
                 x, y = i+dx, j+dy
-                if 0 <= x < m and 0 <= y < n and not visited[x][y]:
+                if 0 <= x < m and 0 <= y < n and not is_visited[x][y]:
                     trap += max(0, height - heightMap[x][y])
                     heappush(heap, [max(height, heightMap[x][y]), x, y])
-                    visited[x][y] = True
+                    is_visited[x][y] = True
 
         return trap
