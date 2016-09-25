064c8693f641d2dba3007a1029b60199d56e1ea5
kamyu <kamyu104@users.noreply.github.com>
2016-09-25 23:34:42 +0800

    Update queue-reconstruction-by-height.cpp

--- C++/queue-reconstruction-by-height.cpp
+++ C++/queue-reconstruction-by-height.cpp
@@ -5,9 +5,9 @@ class Solution {
 public:
     vector<pair<int, int>> reconstructQueue(vector<pair<int, int>>& people) {
         sort(people.begin(), people.end(), 
-        [](const pair<int, int>& a, pair<int, int>& b) {
-            return b.first == a.first ? a.second < b.second : b.first < a.first;
-        });
+            [](const pair<int, int>& a, pair<int, int>& b) {
+                return b.first == a.first ? a.second < b.second : b.first < a.first;
+            });
         vector<pair<int, int>> result;
         for (const auto& p : people) {
             result.insert(result.begin() + p.second, p);
