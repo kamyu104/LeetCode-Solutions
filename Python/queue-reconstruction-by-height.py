603b48c84bb251fbdfdcfcce9673d06f4bad300e
kamyu <kamyu104@users.noreply.github.com>
2016-09-27 00:29:10 +0800

    Update queue-reconstruction-by-height.py

--- Python/queue-reconstruction-by-height.py
+++ Python/queue-reconstruction-by-height.py
@@ -1,4 +1,4 @@
-# Time:  O(n^2)
+# Time:  O(n * sqrt(n))
 # Space: O(n)
 
 # Suppose you have a random list of people standing in a queue. 
@@ -24,7 +24,34 @@ class Solution(object):
         :type people: List[List[int]]
         :rtype: List[List[int]]
         """
-        people.sort(key=lambda x: (-x[0], x[1]))
+        people.sort(key=lambda (h, k): (-h, k))
+
+        blocks = [[]]
+        for p in people:
+            index = p[1]
+    
+            for i, block in enumerate(blocks):
+                if index <= len(block):
+                    break
+                index -= len(block)
+            block.insert(index, p)
+
+            if len(block) * len(block) > len(people):
+                blocks.insert(i+1, block[len(block)/2:])
+                del block[len(block)/2:]
+    
+        return [p for block in blocks for p in block]
+
+
+# Time:  O(n^2)
+# Space: O(n)
+class Solution2(object):
+    def reconstructQueue(self, people):
+        """
+        :type people: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        people.sort(key=lambda (h, k): (-h, k))
         result = []
         for p in people:
             result.insert(p[1], p)
