3cb431613deda5dc897896855b5b3a36f14b6d1e
kamyu <kamyu104@users.noreply.github.com>
2016-10-02 23:34:28 +0800

    Create split-array-largest-sum.cpp

---
+++ C++/split-array-largest-sum.cpp
@@ -0,0 +1,37 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+class Solution {
+public:
+    int splitArray(vector<int>& nums, int m) {
+        int left = 0, right = 0;
+        for (const auto& num : nums) {
+            left = max(left, num);
+            right += num;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (canSplit(nums, m, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+private:
+    bool canSplit(vector<int>& nums, int m, int sum) {
+        int cnt = 1;
+        int curr_sum = 0;
+        for (const auto& num : nums) {
+            curr_sum += num;
+            if (curr_sum > sum) {
+                curr_sum = num;
+                ++cnt;
+            }
+        }
+        return cnt <= m;
+    }
+};
