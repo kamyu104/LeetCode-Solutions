a48a8dfdd927c45d8b7ffffef3f91a90c69bfe14
kamyu <kamyu104@users.noreply.github.com>
2016-10-10 02:05:42 +0800

    Create sentence-screen-fitting.cpp

---
+++ C++/sentence-screen-fitting.cpp
@@ -0,0 +1,35 @@
+// Time:  O(r + n * c)
+// Space: O(n)
+
+class Solution {
+public:
+    int wordsTyping(vector<string>& sentence, int rows, int cols) {
+        vector<int> wc(sentence.size());
+        for (int i = 0; i < sentence.size(); ++i) {
+            wc[i] = wordsFit(sentence, i, cols);
+        }
+
+        int words = 0, start = 0;
+        for (int i = 0; i < rows; ++i) {
+            words += wc[start];
+            start = (start + wc[start]) % sentence.size();
+        }
+        return words / sentence.size();
+    }
+
+private:
+    int wordsFit(const vector<string>& sentence, int start, int cols) {
+        if (sentence[start].length() > cols) {
+            return 0;
+        }
+
+        int sum = sentence[start].length(), count = 1;
+        for (int i = (start + 1) % sentence.size(); ; i = (i + 1) % sentence.size()) {
+            if (sum + 1 + sentence[i].length() > cols) {
+                return count;
+            }
+            sum += 1 + sentence[i].length();
+            ++count;
+        }
+    }
+};
