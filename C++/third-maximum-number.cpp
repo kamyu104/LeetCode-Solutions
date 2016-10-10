9ef3401e8c1299e7b71d34ecd5385afd4f5b43b8
kamyu <kamyu104@users.noreply.github.com>
2016-10-10 16:09:19 +0800

    Rename arithmetic-slices.cpp to third-maximum-number.cpp

---
+++ C++/third-maximum-number.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int count = 0;
+        vector<int> top(3, numeric_limits<int>::min());
+        for (const auto& num : nums) {
+            if (num > top[0]) {
+                top[2] = top[1];
+                top[1] = top[0];
+                top[0] = num;
+                ++count;
+            } else if (num != top[0] && num > top[1]) {
+                top[2] = top[1];
+                top[1] = num;
+                ++count;
+            } else if (num != top[0] && num != top[1] && num >= top[2]) {
+                top[2] = num;
+                ++count;
+            }
+        }
+
+        if (count < 3) { 
+            return top[0];
+        }
+        return top[2];
+    }
+};
