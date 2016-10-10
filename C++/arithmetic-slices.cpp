ad3f811f3214891e6d57b1a98bd026398742121a
kamyu <kamyu104@users.noreply.github.com>
2016-10-10 15:52:34 +0800

    Update arithmetic-slices.cpp

--- C++/arithmetic-slices.cpp
+++ C++/arithmetic-slices.cpp
@@ -7,7 +7,7 @@ public:
         int res = 0, i = 0;
         for (int i = 0; i + 2 < A.size(); ++i) {
             const auto start = i;
-            while (A[i + 2] + A[i] == 2 * A[i + 1]) {
+            while (i + 2 < A.size() && A[i + 2] + A[i] == 2 * A[i + 1]) {
                 res += (i++) - start + 1;
             }
         }
