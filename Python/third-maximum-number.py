76b1d1c3a3e1700125fa28dd8c145789620683fd
kamyu <kamyu104@users.noreply.github.com>
2016-10-10 16:13:25 +0800

    Create third-maximum-number.py

---
+++ Python/third-maximum-number.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Given an array of integers, return the 3rd Maximum Number in this array,
+# if it doesn't exist, return the Maximum Number.
+# The time complexity must be O(n) or less.
+
+class Solution(object):
+    def thirdMax(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        top = [float("-inf")] * 3
+        for num in nums:
+            if num > top[0]:
+                top[2] = top[1]
+                top[1] = top[0]
+                top[0] = num
+                count += 1
+            elif num != top[0] and num > top[1]:
+                top[2] = top[1]
+                top[1] = num
+                count += 1
+            elif num != top[0] and num != top[1] and num >= top[2]:
+                top[2] = num
+                count += 1
+
+        if count < 3:
+            return top[0]
+
+        return top[2]
