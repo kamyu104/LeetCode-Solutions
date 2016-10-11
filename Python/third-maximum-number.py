647170237096f9b979970fe7b02a708010acf342
kamyu <kamyu104@users.noreply.github.com>
2016-10-11 09:38:47 +0800

    Update third-maximum-number.py

--- Python/third-maximum-number.py
+++ Python/third-maximum-number.py
@@ -15,13 +15,10 @@ class Solution(object):
         top = [float("-inf")] * 3
         for num in nums:
             if num > top[0]:
-                top[2] = top[1]
-                top[1] = top[0]
-                top[0] = num
+                top[0], top[1], top[2] = num, top[0], top[1]
                 count += 1
             elif num != top[0] and num > top[1]:
-                top[2] = top[1]
-                top[1] = num
+                top[1], top[2] = num, top[1]
                 count += 1
             elif num != top[0] and num != top[1] and num >= top[2]:
                 top[2] = num
