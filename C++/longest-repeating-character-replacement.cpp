844b19656597396eec42b4212b6fecdc2ce80edd
kamyu <kamyu104@users.noreply.github.com>
2016-10-17 21:13:58 +0800

    Create longest-repeating-character-replacement.cpp

---
+++ C++/longest-repeating-character-replacement.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O()
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> cache(26);
+
+        int i = 0, j = 0, times = k, res = 0;
+        for (; j < s.size(); ++j) {
+            ++cache[s[j] - 'A'];
+            if (s[j] != s[i]) {
+                --times;
+                if (times < 0) {
+                    res = max(res, j - i);
+                    while (i < j && k - (j - i + 1 - cache[s[i] - 'A']) < 0) {
+                        --cache[s[i++] - 'A'];
+                    }
+                    times = k - (j - i + 1 - cache[s[i] - 'A']);
+                }
+            }
+        }
+        return max(res, j - i + min(i, times));
+    }
+};
