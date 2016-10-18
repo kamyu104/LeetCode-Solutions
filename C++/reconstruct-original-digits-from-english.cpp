b5697fb58e1cc53a0b3172cfd53e08fa23f9ee76
kamyu <kamyu104@users.noreply.github.com>
2016-10-19 00:34:35 +0800

    Create reconstruct-original-digits-from-english.cpp

---
+++ C++/reconstruct-original-digits-from-english.cpp
@@ -0,0 +1,40 @@
+
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string originalDigits(string s) {
+        const vector<string> numbers{"zero", "one", "two", "three",
+                                     "four", "five", "six", "seven",
+                                     "eight", "nine"};
+        vector<vector<int>> cnts(numbers.size(), vector<int>(26));
+        for (int i = 0; i < numbers.size(); ++i) {
+            for (const auto& c : numbers[i]) {
+                ++cnts[i][c - 'a'];
+            }
+        }
+        
+        // The order for greedy method.
+        vector<int> order{0, 2, 4, 6, 8, 1, 3, 5, 7, 9};
+        
+        // The unique char in the order.
+        vector<char> unique_chars{'z', 'o', 'w', 't', 'u', 'f', 'x', 's', 'g', 'n'};
+        vector<int> cnt(26);
+        for (const auto& c : s) {
+            ++cnt[c - 'a'];
+        }
+
+        string result;
+        for (const auto& i : order) {
+            while (cnt[unique_chars[i] - 'a'] > 0) {
+                for (int j = 0; j < cnt.size(); ++j) {
+                    cnt[j] -= cnts[i][j];
+                }
+                result += to_string(i);
+            }
+        }
+        sort(result.begin(), result.end());
+        return result;
+    }
+};
