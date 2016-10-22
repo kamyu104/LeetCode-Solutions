39bc692923fc41e11163f7820ed02fdbcc142d79
kamyu <kamyu104@users.noreply.github.com>
2016-10-22 20:49:21 +0800

    Create valid-word-square.py

---
+++ Python/valid-word-square.py
@@ -0,0 +1,15 @@
+# Time:  O(m * n)
+# Space: O(1)
+
+class Solution(object):
+    def validWordSquare(self, words):
+        """
+        :type words: List[str]
+        :rtype: bool
+        """
+        for i in xrange(len(words)):
+            for j in xrange(len(words[i])):
+                if j >= len(words) or i >= len(words[j]) or \
+                   words[j][i] != words[i][j]:
+                   return False
+        return True
