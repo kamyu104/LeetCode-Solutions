e82aa8f8381e47e3a75a3772c9f50ef3c15c5158
kamyu <kamyu104@users.noreply.github.com>
2016-10-23 21:56:03 +0800

    Update k-th-smallest-in-lexicographical-order.cpp

--- C++/k-th-smallest-in-lexicographical-order.cpp
+++ C++/k-th-smallest-in-lexicographical-order.cpp
@@ -7,7 +7,7 @@ public:
         int result = 0;
 
         vector<int> cnts(10);
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i <= 9; ++i) {
             cnts[i] = cnts[i - 1] * 10 + 1;
         }
 
@@ -20,7 +20,7 @@ public:
         for (int i = nums.size() - 1; i >= 0 && k; --i) {
             target = target * 10 + nums[i];
             const auto start = i == nums.size() - 1 ? 1 : 0;
-            for (int j = start; j < 10; ++j) {
+            for (int j = start; j <= 9; ++j) {
                 int candidate = result * 10 + j;
                 int num;
                 if (candidate < target) {
