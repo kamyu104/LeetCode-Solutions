340e5ef2e16c737f0028ac2a5a940879d89e2eaa
kamyu <kamyu104@users.noreply.github.com>
2016-10-23 21:47:46 +0800

    Update k-th-smallest-in-lexicographical-order.cpp

--- C++/k-th-smallest-in-lexicographical-order.cpp
+++ C++/k-th-smallest-in-lexicographical-order.cpp
@@ -1,7 +1,53 @@
-// Time:  O(logn * logn)
+// Time:  O(logn)
 // Space: O(logn)
 
 class Solution {
+public:
+    int findKthNumber(int n, int k) {
+        int result = 0;
+
+        vector<int> cnts(10);
+        for (int i = 1; i < 10; i++) {
+            cnts[i] = cnts[i - 1] * 10 + 1;
+        }
+
+        vector<int> nums;
+        for (int i = n; i > 0; i /= 10) {
+            nums.push_back(i % 10);
+        }
+        int total = n;
+        int target = 0;
+        for (int i = nums.size() - 1; i >= 0 && k; --i) {
+            target = target * 10 + nums[i];
+            const auto start = i == nums.size() - 1 ? 1 : 0;
+            for (int j = start; j < 10; ++j) {
+                int candidate = result * 10 + j;
+                int num;
+                if (candidate < target) {
+                    num = cnts[i + 1];
+                } else if (candidate > target) {
+                    num = cnts[i];
+                } else {
+                    num = total - cnts[i + 1] * (j - start) - cnts[i] * (9 - j);
+                }
+                if (k > num) {
+                    k -= num;
+                } else {
+                    result = candidate;
+                    --k;
+                    total = num - 1;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+
+// Time:  O(logn * logn)
+// Space: O(logn)
+class Solution2 {
 public:
     int findKthNumber(int n, int k) {
         int result = 0;
