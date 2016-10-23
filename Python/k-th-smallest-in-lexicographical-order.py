61fcad8a8360e93cb1de1752a3fb18cd4a39ccdd
kamyu <kamyu104@users.noreply.github.com>
2016-10-23 21:56:56 +0800

    Update k-th-smallest-in-lexicographical-order.py

--- Python/k-th-smallest-in-lexicographical-order.py
+++ Python/k-th-smallest-in-lexicographical-order.py
@@ -1,4 +1,4 @@
-# Time:  O(logn * logn)
+# Time:  O(logn)
 # Space: O(logn)
 
 # Given integers n and k, find the lexicographically k-th smallest integer in the range from 1 to n.
@@ -18,6 +18,49 @@
 # so the second smallest number is 10.
 
 class Solution(object):
+    def findKthNumber(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+
+        cnts = [0] * 10
+        for i in xrange(1, 10):
+            cnts[i] = cnts[i - 1] * 10 + 1
+        nums = []
+        i = n
+        while i:
+            nums.append(i % 10)
+            i /= 10
+        total, target = n, 0
+        i = len(nums) - 1
+        while i >= 0 and k > 0:
+            target = target*10 + nums[i]
+            start = int(i == len(nums)-1)
+            for j in xrange(start, 10):
+                candidate = result*10 + j
+                if candidate < target:
+                    num = cnts[i+1]
+                elif candidate > target:
+                    num = cnts[i]
+                else:
+                    num = total - cnts[i + 1]*(j-start) - cnts[i]*(9-j)
+                if k > num:
+                    k -= num
+                else:
+                    result = candidate
+                    k -= 1
+                    total = num-1
+                    break
+            i -= 1
+        return result
+
+
+# Time:  O(logn * logn)
+# Space: O(logn)
+class Solution2(object):
     def findKthNumber(self, n, k):
         """
         :type n: int
