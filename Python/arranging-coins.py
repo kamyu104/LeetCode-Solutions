01749e00350f8e3205d1d99911027b2d84bbd78d
kamyu <kamyu104@users.noreply.github.com>
2016-10-31 23:16:31 +0800

    Create arranging-coins.py

---
+++ Python/arranging-coins.py
@@ -0,0 +1,46 @@
+# Time:  O(logn)
+# Space: O(1)
+
+# You have a total of n coins that you want to form in a staircase shape,
+# where every k-th row must have exactly k coins.
+#
+# Given n, find the total number of full staircase rows that can be formed.
+#
+# n is a non-negative integer and fits within the range of a 32-bit signed integer.
+#
+# Example 1:
+#
+# n = 5
+#
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤
+#
+# Because the 3rd row is incomplete, we return 2.
+# Example 2:
+#
+# n = 8
+#
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤ ¤
+# ¤ ¤
+#
+# Because the 4th row is incomplete, we return 3.
+
+class Solution(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            mid = left + (right - left) / 2
+            if 2 * n < mid * (mid+1):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
