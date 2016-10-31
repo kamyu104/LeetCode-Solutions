a6383f71664b65b05b13a2ac95ab26d6f638946b
kamyu <kamyu104@users.noreply.github.com>
2016-10-31 23:23:44 +0800

    Update arranging-coins.py

--- Python/arranging-coins.py
+++ Python/arranging-coins.py
@@ -36,6 +36,17 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        return int((math.sqrt(8*n+1)-1) / 2)  # sqrt is O(logn) time.
+
+
+# Time:  O(logn)
+# Space: O(1)
+class Solution2(object):
+    def arrangeCoins(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         left, right = 1, n
         while left <= right:
             mid = left + (right - left) / 2
