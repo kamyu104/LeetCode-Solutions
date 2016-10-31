6d715cebe7eae274729b17687804f1025f51a0fc
kamyu <kamyu104@users.noreply.github.com>
2016-10-31 23:26:29 +0800

    Update arranging-coins.cpp

--- C++/arranging-coins.cpp
+++ C++/arranging-coins.cpp
@@ -4,6 +4,15 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
+        return static_cast<int>((sqrt(8.0 * n + 1) - 1) / 2);
+    }
+};
+
+// Time:  O(logn)
+// Space: O(1)
+class Solution2 {
+public:
+    int arrangeCoins(int n) {
         long long left = 1, right = n;
         while (left <= right) {
             const auto mid = left + (right - left) / 2;
