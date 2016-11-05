b2e128bfaa0cbe9c4bace0c5fe2e11dbbedd38f5
kamyu <kamyu104@users.noreply.github.com>
2016-11-06 00:13:23 +0800

    Update sequence-reconstruction.py

--- Python/sequence-reconstruction.py
+++ Python/sequence-reconstruction.py
@@ -1,7 +1,39 @@
-# Time:  O(|V| + |E|)
-# Space: O(|E|)
+# Time:  O(n * s), n is the size of org, s is the size of seqs
+# Space: O(n)
 
 class Solution(object):
+    def sequenceReconstruction(self, org, seqs):
+        """
+        :type org: List[int]
+        :type seqs: List[List[int]]
+        :rtype: bool
+        """
+        if not seqs:
+            return False
+        pos = [0] * (len(org) + 1)
+        for i in xrange(len(org)):
+            pos[org[i]] = i
+
+        is_matched = [False] * (len(org) + 1)
+        cnt_to_match = len(org) - 1
+        for seq in seqs:
+            for i in xrange(len(seq)):
+                if not 0 < seq[i] <= len(org):
+                    return False
+                if i == 0:
+                    continue
+                if pos[seq[i-1]] >= pos[seq[i]]:
+                    return False
+                if is_matched[seq[i-1]] == False and pos[seq[i-1]] + 1 == pos[seq[i]]:
+                    is_matched[seq[i-1]] = True
+                    cnt_to_match -= 1
+
+        return cnt_to_match == 0
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+class Solution2(object):
     def sequenceReconstruction(self, org, seqs):
         """
         :type org: List[int]
