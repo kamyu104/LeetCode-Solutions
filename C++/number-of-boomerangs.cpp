e7f75f5d6f5d4fd6c5416dd771bd0d1cab66d241
kamyu <kamyu104@users.noreply.github.com>
2016-11-06 17:47:24 +0800

    Create number-of-boomerangs.cpp

---
+++ C++/number-of-boomerangs.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+class Solution {
+public:
+    int numberOfBoomerangs(vector<pair<int, int>>& points) {
+        int result = 0;
+        for (int i = 0; i < points.size(); ++i) {
+            unordered_map<int, int> group;
+            for (int j = 0; j < points.size(); ++j) {
+                if (j == i) {
+                    continue;
+                }
+                
+                const auto dy = points[i].second - points[j].second;
+                const auto dx = points[i].first - points[j].first;
+                ++group[dx * dx + dy * dy];
+            }
+            
+            for (const auto& p : group) {
+                if (p.second > 1) {
+                    result += p.second * (p.second - 1);
+                }
+            }
+        }
+        
+        return result;
+    }
+};
