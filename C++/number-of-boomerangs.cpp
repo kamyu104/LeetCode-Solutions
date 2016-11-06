045f6e2ca9459c018a008d906ec7d5532e6b0bd4
kamyu <kamyu104@users.noreply.github.com>
2016-11-06 17:52:51 +0800

    Update number-of-boomerangs.cpp

--- C++/number-of-boomerangs.cpp
+++ C++/number-of-boomerangs.cpp
@@ -5,15 +5,15 @@ class Solution {
 public:
     int numberOfBoomerangs(vector<pair<int, int>>& points) {
         int result = 0;
+
         for (int i = 0; i < points.size(); ++i) {
             unordered_map<int, int> group;
             for (int j = 0; j < points.size(); ++j) {
                 if (j == i) {
                     continue;
                 }
-                
-                const auto dy = points[i].second - points[j].second;
                 const auto dx = points[i].first - points[j].first;
+                const auto dy = points[i].second - points[j].second;
                 ++group[dx * dx + dy * dy];
             }
             
