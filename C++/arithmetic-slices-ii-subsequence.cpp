4322084e019c68a945ebea304fc4066a7f54c35f
kamyu <kamyu104@users.noreply.github.com>
2016-11-06 19:25:34 +0800

    Update arithmetic-slices-ii-subsequence.cpp

--- C++/arithmetic-slices-ii-subsequence.cpp
+++ C++/arithmetic-slices-ii-subsequence.cpp
@@ -1,15 +1,19 @@
 // Time:  O(n^2)
 // Space: O(n * d)
 
-class Solution_TLE {
+class Solution_MLE {
 public:
     int numberOfArithmeticSlices(vector<int>& A) {
         int result = 0;
         vector<unordered_map<long long, int>> dp(A.size());
         for (int i = 1; i < A.size(); ++i) {
             for (int j = 0; j < i; ++j) {
-                dp[i][static_cast<long long>(A[i]) - A[j]] += dp[j][static_cast<long long>(A[i]) - A[j]] + 1;
-                result += dp[j][static_cast<long long>(A[i]) - A[j]];
+                const auto diff = static_cast<long long>(A[i]) - A[j];
+                ++dp[i][diff];
+                if (dp[j].count(diff)) {
+                    dp[i][diff] += dp[j][diff];
+                    result += dp[j][diff];
+                }
             }
         }
         return result;
