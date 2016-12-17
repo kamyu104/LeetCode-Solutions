de8e1e2c82d56d5fed0e14d58eeb75d1fdea6447
kamyu <kamyu104@users.noreply.github.com>
2016-12-17 21:44:58 +0800

    Create number-of-segments-in-a-string.py

---
+++ Python/number-of-segments-in-a-string.py
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Count the number of segments in a string,
+# where a segment is defined to be a contiguous
+# sequence of non-space characters.
+#
+# Please note that the string does not
+# contain any non-printable characters.
+#
+# Example:
+#
+# Input: "Hello, my name is John"
+# Output: 5
+
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = int(len(s) and s[-1] != ' ')
+        for i in xrange(1, len(s)):
+            if s[i] == ' ' and s[i-1] != ' ':
+                result += 1
+        return result
