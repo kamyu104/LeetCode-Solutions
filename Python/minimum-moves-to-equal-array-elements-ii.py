ad71439bba364c6f85141d0f779982736631ab9b
kamyu <kamyu104@users.noreply.github.com>
2016-12-17 23:35:23 +0800

    Create minimum-moves-to-equal-array-elements-ii.py

---
+++ Python/minimum-moves-to-equal-array-elements-ii.py
@@ -0,0 +1,38 @@
+# Time:  O(n) on average
+# Space: O(1)
+
+from random import randint
+
+# Quick select solution.
+class Solution(object):
+    def minMoves2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def kthElement(nums, k):
+            def PartitionAroundPivot(left, right, pivot_idx, nums):
+                pivot_value = nums[pivot_idx]
+                new_pivot_idx = left
+                nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+                for i in xrange(left, right):
+                    if nums[i] > pivot_value:
+                        nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                        new_pivot_idx += 1
+                    
+                nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+                return new_pivot_idx
+
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                pivot_idx = randint(left, right)
+                new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, nums)
+                if new_pivot_idx == k - 1:
+                    return nums[new_pivot_idx]
+                elif new_pivot_idx > k - 1:
+                    right = new_pivot_idx - 1
+                else:  # new_pivot_idx < k - 1.
+                    left = new_pivot_idx + 1
+
+        median = kthElement(nums, len(nums)/2 + 1)
+        return sum(abs(num - median) for num in nums)
