e220f3f9e8c390032e174fdff8ee624b21303c03
kamyu <kamyu104@users.noreply.github.com>
2016-12-19 22:31:57 +0800

    Update shortest-word-distance-iii.cpp

--- C++/shortest-word-distance-iii.cpp
+++ C++/shortest-word-distance-iii.cpp
@@ -5,9 +5,10 @@ class Solution {
 public:
     int shortestWordDistance(vector<string>& words, string word1, string word2) {
         int dist = INT_MAX;
+        bool is_same = (word1 == word2);
         for (int i = 0, index1 = -1, index2 = -1; i < words.size(); ++i) {
             if (words[i] == word1) {
-                if (index1 != -1) {
+                if (is_same && index1 != -1) {
                     dist = min(dist, abs(index1 - i));
                 }
                 index1 = i;
