0eca02d3df08795d6fbfca3521bfe7302582b7e1
kamyu <kamyu104@users.noreply.github.com>
2016-12-20 23:55:38 +0800

    Create total-hamming-distance.cpp

---
+++ C++/total-hamming-distance.cpp
@@ -0,0 +1,17 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int totalHammingDistance(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0; i < 8 * sizeof(int); ++i) {
+            vector<int> counts(2);
+            for (const auto& num : nums) {
+                ++counts[(num >> i) % 2];
+            }
+            result += counts[0] * counts[1];
+        }
+        return result;
+    }
+};
