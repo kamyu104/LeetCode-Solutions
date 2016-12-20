bc315c441476315a01483c3edf4345b2db4d8ca5
kamyu <kamyu104@users.noreply.github.com>
2016-12-20 20:30:46 +0800

    Update matchsticks-to-square.cpp

--- C++/matchsticks-to-square.cpp
+++ C++/matchsticks-to-square.cpp
@@ -18,21 +18,21 @@ public:
         for (int subset = 0; subset <= all; ++subset) {
             int subset_sum = 0;
             for (int i = 0; i < nums.size(); ++i) {
-        	    if (subset & (1 << i)) {
-        	        subset_sum += nums[i];
-        	    }
-            }
-        	  if (subset_sum == side_len) {
-        	      for (const auto& used_subset : used_subsets) {
-        	          if ((used_subset & subset) == 0) {
-            		        int valid_half_subset = used_subset | subset;
-            		        valid_half_subsets[valid_half_subset] = true;
-            		        if (valid_half_subsets[all ^ valid_half_subset]) {
-            		            return true;
-            		        }
-            	       }
+                if (subset & (1 << i)) {
+                    subset_sum += nums[i];
                 }
-        	      used_subsets.emplace_back(subset);
+            }
+            if (subset_sum == side_len) {
+                for (const auto& used_subset : used_subsets) {
+                    if ((used_subset & subset) == 0) {
+                        int valid_half_subset = used_subset | subset;
+                        valid_half_subsets[valid_half_subset] = true;
+                        if (valid_half_subsets[all ^ valid_half_subset]) {
+                            return true;
+                        }
+                    }
+                }
+                used_subsets.emplace_back(subset);
             }
         }
         return false;
