1683109240aaa3a1ec027038461442cd4a45f530
kamyu <kamyu104@users.noreply.github.com>
2016-12-20 12:42:49 +0800

    Create hamming-distance.cpp

---
+++ C++/hamming-distance.cpp
@@ -0,0 +1,11 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int distance = 0;
+        for (int z = x ^ y; z; z &= z - 1, ++distance);
+        return distance;
+    }
+};
