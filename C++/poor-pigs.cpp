df6e87cbc4c8d2e22d91a3d15fc79a695290b51f
kamyu <kamyu104@users.noreply.github.com>
2016-12-21 23:17:58 +0800

    Create poor-pigs.cpp

---
+++ C++/poor-pigs.cpp
@@ -0,0 +1,9 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
+        return ceil(log(buckets) / log(minutesToTest / minutesToDie + 1));
+    }
+};
