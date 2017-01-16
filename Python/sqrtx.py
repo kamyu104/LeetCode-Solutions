a889f6450608d641a5921604029cf91df257c95d
Chih-Yao Ma <cyma@gatech.edu>
2017-01-15 23:24:55 -0500

    Use floor division instead of regular division
    print Solution().sqrt(9) gave me 2.75, which should be 3.

--- Python/sqrtx.py
+++ Python/sqrtx.py
@@ -14,9 +14,9 @@ class Solution(object):
         if x < 2:
             return x
         
-        left, right = 1, x / 2
+        left, right = 1, x // 2
         while left <= right:
-            mid = left + (right - left) / 2
+            mid = left + (right - left) // 2
             if mid > x / mid:
                 right = mid - 1
             else:
