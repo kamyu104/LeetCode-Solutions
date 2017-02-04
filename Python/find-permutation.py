087fb4c2275376a41adb5fb66d48a66e8470c273
kamyu <kamyu104@users.noreply.github.com>
2017-02-04 23:20:44 +0800

    Create find-permutation.py

---
+++ Python/find-permutation.py
@@ -0,0 +1,14 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findPermutation(self, s):
+        """
+        :type s: str
+        :rtype: List[int]
+        """
+        result = []
+        for i in xrange(len(s)+1):
+            if i == len(s) or s[i] == 'I':
+                result += range(i+1, len(result), -1)
+        return result
