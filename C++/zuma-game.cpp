673b1b89d25ccc9e31841d43e91d9d8224e66565
kamyu <kamyu104@users.noreply.github.com>
2017-02-27 18:32:44 +0800

    Create zuma-game.cpp

---
+++ C++/zuma-game.cpp
@@ -0,0 +1,76 @@
+// Time:  O(b * b! * h!)
+// Space: O(b * b! * h!)
+
+class Solution {
+public:
+    int findMinStep(string board, string hand) {
+        unordered_map<string, unordered_map<string, int>> lookup;
+        sort(hand.begin(), hand.end()); 
+        int res = findMinStepHelper(board, hand, &lookup);
+        return res > hand.size() ? -1 : res;
+    }
+
+private:
+    int findMinStepHelper(const string& board, const string& hand,
+                          unordered_map<string, unordered_map<string, int>> *lookup) {
+        if (board.empty()) {
+            return 0;
+        }
+        if (hand.empty()) {
+            return MAX_STEP;
+        }
+        if ((*lookup)[board][hand]) {
+            return (*lookup)[board][hand];
+        }
+
+        int res = MAX_STEP;
+        for (int i = 0; i < hand.size(); ++i) {
+            int j = 0;
+            int n = board.size();
+            while (j < n) {
+                int k = board.find(hand[i], j);
+                if (k == string::npos) {
+                    break;
+                }
+                if (k < n - 1 && board[k] == board[k + 1]) {
+                    string next_board = shrink(board.substr(0, k) + board.substr(k + 2));
+                    string next_hand = hand.substr(0, i) + hand.substr(i + 1);
+                    res = min(res, findMinStepHelper(next_board, next_hand, lookup) + 1);
+                    ++k;
+                } else if (i > 0 && hand[i] == hand[i - 1]) {
+                    string next_board = shrink(board.substr(0, k) + board.substr(k + 1));
+                    string next_hand = hand.substr(0, i - 1) + hand.substr(i + 1);
+                    res = min(res, findMinStepHelper(next_board, next_hand, lookup) + 2);
+                }
+                j = k + 1;
+            }
+        }
+
+        (*lookup)[board][hand] = res;
+        return res;
+    }
+    
+    string shrink(const string& s) {  // Time: O(n), Space: O(n)
+        vector<pair<char, int>> stack;
+        for (int i = 0, start = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] != s[start]) {
+                if (!stack.empty() && stack.back().first == s[start]) {
+                    stack.back().second += i - start;
+                    if (stack.back().second >= 3) {
+                        stack.pop_back();
+                    }
+                } else if (i - start < 3) {
+                     stack.emplace_back(s[start], i - start);
+                }
+                start = i;
+            }
+        }
+        string result;
+        for (const auto& p : stack) {
+            result += string(p.second, p.first);
+        }
+        return result;
+    }
+
+    static const int MAX_STEP = 6;
+};
