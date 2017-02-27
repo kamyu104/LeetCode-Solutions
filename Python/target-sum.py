d594df6d0fb0b677d521013de1dbed09c8c92858
kamyu <kamyu104@users.noreply.github.com>
2017-02-28 00:23:13 +0800

    Create target-sum.py

---
+++ Python/target-sum.py
@@ -0,0 +1,46 @@
+# Time:  O(n * S)
+# Space: O(S)
+
+# You are given a list of non-negative integers, a1, a2, ..., an,
+# and a target, S. Now you have 2 symbols + and -.
+# For each integer, you should choose one from + and - as its new symbol.
+#
+# Find out how many ways to assign symbols to make sum of integers equal to target S.
+#
+# Example 1:
+# Input: nums is [1, 1, 1, 1, 1], S is 3. 
+# Output: 5
+# Explanation: 
+#
+# -1+1+1+1+1 = 3
+# +1-1+1+1+1 = 3
+# +1+1-1+1+1 = 3
+# +1+1+1-1+1 = 3
+# +1+1+1+1-1 = 3
+#
+# There are 5 ways to assign symbols to make the sum of nums be target 3.
+# Note:
+# The length of the given array is positive and will not exceed 20.
+# The sum of elements in the given array will not exceed 1000.
+# Your output answer is guaranteed to be fitted in a 32-bit integer.
+
+class Solution(object):
+    def findTargetSumWays(self, nums, S):
+        """
+        :type nums: List[int]
+        :type S: int
+        :rtype: int
+        """
+        def subsetSum(nums, S):
+            dp = collections.defaultdict(int)
+            dp[0] = 1
+            for n in nums:
+                for i in reversed(xrange(n, S+1)):
+                    if i-n in dp:
+                        dp[i] += dp[i-n]
+            return dp[S]
+
+        total = sum(nums)
+        if total < S or (S + total) % 2: return 0
+        P = (S + total) // 2
+        return subsetSum(nums, P)
