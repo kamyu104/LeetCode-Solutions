4bd7c7b81e47d29c07117b0b41911276ac0b5ba9
kamyu <kamyu104@users.noreply.github.com>
2017-02-28 18:23:52 +0800

    Update contiguous-array.cpp

--- C++/contiguous-array.cpp
+++ C++/contiguous-array.cpp
@@ -6,13 +6,13 @@ public:
     int findMaxLength(vector<int>& nums) {
         int result = 0, count = 0;
         unordered_map<int, int> lookup;
-        lookup[0] = 0;
+        lookup[0] = -1;
         for (int i = 0; i < nums.size(); ++i) {
             count += nums[i] == 1 ? 1 : -1;
             if (lookup.count(count)) {
-                result = max(result, i + 1 - lookup[count]);
+                result = max(result, i - lookup[count]);
             } else {
-                lookup[count] = i + 1;
+                lookup[count] = i;
             }
         }
         return result;
