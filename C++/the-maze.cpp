b8b4c75dc60a6ea8fb7eab218da57165a847d8da
kamyu <kamyu104@users.noreply.github.com>
2017-03-04 23:07:45 +0800

    Create the-maze.cpp

---
+++ C++/the-maze.cpp
@@ -0,0 +1,58 @@
+// Time:  O(max(r, c) * w)
+// Space: O(w)
+
+class Solution {
+public:
+    bool hasPath(vector<vector<int>>& maze, vector<int>& start, vector<int>& destination) {
+        static const vector<vector<int>> dirs = {{-1, 0}, {0, 1}, {0, -1}, {1, 0}};
+        queue<node> q;
+        unordered_set<int> visited;
+        q.emplace(0, start);
+
+        while (!q.empty()) {
+            int dist = 0;
+            vector<int> node;
+            tie(dist, node) = q.front();
+            q.pop();
+            if (visited.count(hash(maze, node))) {
+                continue;
+            }
+            if (node[0] == destination[0] &&
+                node[1] == destination[1]) {
+                return true;
+            }
+
+            visited.emplace(hash(maze, node));
+            for (const auto& dir : dirs) {
+                int neighbor_dist = 0;
+                vector<int> neighbor;
+                tie(neighbor_dist, neighbor) = findNeighbor(maze, node, dir);
+                q.emplace(dist + neighbor_dist, neighbor);
+            }
+        }
+
+        return false;
+    }
+
+private:
+    using node = pair<int, vector<int>>;
+
+    node findNeighbor(const vector<vector<int>>& maze,
+                      const vector<int>& node, const vector<int>& dir) {
+        vector<int> cur_node = node;
+        int dist = 0;
+    
+        while (0 <= cur_node[0] + dir[0] && cur_node[0] + dir[0] < maze.size() &&
+               0 <= cur_node[1] + dir[1] && cur_node[1] + dir[1] < maze[0].size() &&
+               !maze[cur_node[0] + dir[0]][cur_node[1] + dir[1]]) {
+            cur_node[0] += dir[0];
+            cur_node[1] += dir[1];
+            ++dist;
+        }
+        return {dist, cur_node};
+    }
+    
+    int hash(const vector<vector<int>>& maze, const vector<int>& node) {
+        return node[0] * maze[0].size() + node[1];
+    }
+};
