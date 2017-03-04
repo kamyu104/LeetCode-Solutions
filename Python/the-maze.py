2e6002971c2167c02fb3297bef1107a1bda00c5b
kamyu <kamyu104@users.noreply.github.com>
2017-03-04 23:04:17 +0800

    Create the-maze.py

---
+++ Python/the-maze.py
@@ -0,0 +1,36 @@
+# Time:  O(max(r, c) * w)
+# Space: O(w)
+
+class Solution(object):
+    def hasPath(self, maze, start, destination):
+        """
+        :type maze: List[List[int]]
+        :type start: List[int]
+        :type destination: List[int]
+        :rtype: bool
+        """
+        start, destination = tuple(start), tuple(destination)
+        
+        def neighbors(maze, node):
+            for dir in [(-1, 0), (0, 1), (0, -1), (1, 0)]:
+                cur_node, dist = list(node), 0
+                while 0 <= cur_node[0]+dir[0] < len(maze) and \
+                      0 <= cur_node[1]+dir[1] < len(maze[0]) and \
+                      not maze[cur_node[0]+dir[0]][cur_node[1]+dir[1]]:
+                    cur_node[0] += dir[0]
+                    cur_node[1] += dir[1]
+                    dist += 1
+                yield dist, tuple(cur_node)
+
+        queue = collections.deque([(0, start)])
+        visited = set()
+        while queue:
+            dist, node = queue.popleft()
+            if node in visited: continue
+            if node == destination:
+                return True
+            visited.add(node)
+            for neighbor_dist, neighbor in neighbors(maze, node):
+                queue.append((dist+neighbor_dist, neighbor))
+            
+        return False
