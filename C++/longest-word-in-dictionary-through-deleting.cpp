c96c4d956c474939115eeb9d7324c625320b817e
kamyu <kamyu104@users.noreply.github.com>
2017-03-05 01:24:22 +0800

    Create longest-word-in-dictionary-through-deleting.cpp

---
+++ C++/longest-word-in-dictionary-through-deleting.cpp
@@ -0,0 +1,25 @@
+// Time:  O(dlogd)
+// Space: O(1)
+
+class Solution {
+public:
+    string findLongestWord(string s, vector<string>& d) {
+        sort(d.begin(), d.end(), 
+             [](const string& a, const string&b) {
+                return a.length() != b.length() ? a.length() > b.length() : a < b;
+             });
+
+        for (const auto& word : d) {
+            int i = 0;
+            for (const auto& c : s) {
+                if (i < word.length() && word[i] == c) {
+                    ++i;
+                }
+            }
+            if (i == word.length()) {
+                return word;
+            }
+        }
+        return "";
+    }
+};
