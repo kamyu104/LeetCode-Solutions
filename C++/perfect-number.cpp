de462783f0b939b2559db7a0069a5f504cf14fdc
kamyu <kamyu104@users.noreply.github.com>
2017-04-01 18:08:25 +0800

    Create perfect-number.cpp

---
+++ C++/perfect-number.cpp
@@ -0,0 +1,21 @@
+// Time:  O(sqrt(n))
+// Space: O(1)
+
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        if (num <= 0) {
+            return false;
+        }
+        int sum = 0;
+        for (int i = 1; i * i <= num; ++i) {
+            if (num % i == 0) {
+                sum += i;
+                if (i * i != num) {
+                    sum += num / i;
+                }
+            }
+        }
+        return sum - num == num;
+    }
+};
