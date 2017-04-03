115f27849a03c720616584312115ef21ec7b22c6
kamyu <kamyu104@users.noreply.github.com>
2017-04-03 18:10:33 +0800

    Create output-contest-matches.py

---
+++ Python/output-contest-matches.py
@@ -0,0 +1,14 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def findContestMatch(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        matches = map(str, range(1, n+1))
+        while len(matches)/2:
+            matches = ["({},{})".format(matches[i], matches[-i-1]) for i in xrange(len(matches)/2)]
+        return matches[0]
+ 
