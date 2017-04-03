21dcb24cb383b2613c6f31a09220d38eb2ba88da
kamyu <kamyu104@users.noreply.github.com>
2017-04-03 17:23:34 +0800

    Create convert-bst-to-greater-tree.cpp

---
+++ C++/convert-bst-to-greater-tree.cpp
@@ -0,0 +1,35 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* convertBST(TreeNode* root) {
+        int cur_sum = 0;
+        convertBSTHelper(root, &cur_sum);
+        return root;
+    }
+
+private:
+    void convertBSTHelper(TreeNode* root, int *cur_sum){
+        if (!root) {
+            return;
+        }
+
+        if (root->right) {
+            convertBSTHelper(root->right, cur_sum);
+        }
+        root->val = (*cur_sum += root->val);
+        if (root->left) {
+            convertBSTHelper(root->left, cur_sum);
+        }
+    }
+};
