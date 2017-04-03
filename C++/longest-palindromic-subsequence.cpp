08c53628ed61b413d8ea900a02029749b2e871db
kamyu <kamyu104@users.noreply.github.com>
2017-04-03 15:00:51 +0800

    Create longest-palindromic-subsequence.cpp

---
+++ C++/longest-palindromic-subsequence.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        vector<vector<int>> dp(2, vector<int>(s.size(), 1));
+        for (int i = s.length() - 2; i >= 0; --i) {
+            for (int j = i + 1; j < s.length(); ++j) {
+                if (s[i] == s[j]) {
+                    dp[i % 2][j] = (i + 1 <= j - 1) ? 2 + dp[(i + 1) % 2][j - 1] : 2;
+                } else {
+                    dp[i % 2][j] = max(dp[(i + 1) % 2][j], dp[i % 2][j - 1]);
+                }
+            }
+        }
+        return dp[0][s.length() - 1];
+    }
+};
