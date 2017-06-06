5f08cf7bb6a6438a0243ce8d8423388ced16eae1
kamyu <kamyu104@users.noreply.github.com>
2017-06-07 00:21:34 +0800

    Create squirrel-simulation.py

---
+++ Python/squirrel-simulation.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minDistance(self, height, width, tree, squirrel, nuts):
+        """
+        :type height: int
+        :type width: int
+        :type tree: List[int]
+        :type squirrel: List[int]
+        :type nuts: List[List[int]]
+        :rtype: int
+        """
+        def distance(a, b):
+            return abs(a[0] - b[0]) + abs(a[1] - b[1])
+        
+        result = 0
+        d = float("inf")
+        for nut in nuts:
+            result += (distance(nut, tree) * 2)
+            d = min(d, distance(nut, squirrel) - distance(nut, tree))
+        return result + d
