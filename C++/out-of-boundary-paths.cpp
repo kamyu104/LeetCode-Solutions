2d564742b285ca2d23376a3b5ed26e93876f3a7c
kamyu <kamyu104@users.noreply.github.com>
2017-06-07 23:45:14 +0800

    Update out-of-boundary-paths.cpp

--- C++/out-of-boundary-paths.cpp
+++ C++/out-of-boundary-paths.cpp
@@ -10,7 +10,7 @@ public:
         for (int moves = 0; moves < N; ++moves) {
             for (int i = 0; i < m; ++i) {
                 for (int j = 0; j < n; ++j) {
-                    dp[(moves + 1) % 2][i][j] = (((i == 0 ? 1 : dp[moves % 2][i - 1][j] ) +
+                    dp[(moves + 1) % 2][i][j] = (((i == 0 ? 1 : dp[moves % 2][i - 1][j]) +
                                                  (i == m - 1 ? 1 : dp[moves % 2][i + 1][j])) % M +
                                                 ((j == 0 ? 1 : dp[moves % 2][i][j - 1]) +
                                                  (j == n - 1 ? 1 : dp[moves % 2][i][j + 1])) % M) % M;
