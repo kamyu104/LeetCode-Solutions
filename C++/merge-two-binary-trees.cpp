502e54a37a1267b9830cea06157ba0a225b0fdc8
kamyu <kamyu104@users.noreply.github.com>
2017-06-11 23:56:41 +0800

    Create merge-two-binary-trees.cpp

---
+++ C++/merge-two-binary-trees.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if (!t1) {
+            return t2;
+        }
+        if (!t2) {
+            return t1;
+        }
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
