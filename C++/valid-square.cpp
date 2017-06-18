93f3f9e48171fab3525a2b2f7e5d5831ecec1efd
kamyu <kamyu104@users.noreply.github.com>
2017-06-18 23:01:49 +0800

    Update valid-square.cpp

--- C++/valid-square.cpp
+++ C++/valid-square.cpp
@@ -4,14 +4,14 @@
 class Solution {
 public:
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        unordered_set<int> s({ d(p1, p2), d(p1, p3),
-                               d(p1, p4), d(p2, p3),
-                               d(p2, p4), d(p3, p4) });
-        return !s.count(0) && s.size() == 2;
+        unordered_set<int> counter({ dist(p1, p2), dist(p1, p3),
+                                     dist(p1, p4), dist(p2, p3),
+                                     dist(p2, p4), dist(p3, p4) });
+        return !counter.count(0) && counter.size() == 2;
     }
 
 private:
-    int d(vector<int>& p1, vector<int>& p2) {
+    int dist(vector<int>& p1, vector<int>& p2) {
         return (p1[0] - p2[0]) * (p1[0] - p2[0]) +
                (p1[1] - p2[1]) * (p1[1] - p2[1]);
     }
