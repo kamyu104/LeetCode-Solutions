941406183a6616d55daf9ca3cab0e0548bec2499
kamyu <kamyu104@users.noreply.github.com>
2017-06-18 23:04:57 +0800

    Update valid-square.py

--- Python/valid-square.py
+++ Python/valid-square.py
@@ -28,7 +28,7 @@ class Solution(object):
         def dist(p1, p2):
             return (p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2
 
-        counter = collections.Counter([dist(p1, p2), dist(p1, p3),\
-                                       dist(p1, p4), dist(p2, p3),\
-                                       dist(p2, p4), dist(p3, p4)])
-        return counter[0] == 0 and len(counter) == 2
+        lookup = set([dist(p1, p2), dist(p1, p3),\
+                      dist(p1, p4), dist(p2, p3),\
+                      dist(p2, p4), dist(p3, p4)])
+        return 0 not in lookup and len(lookup) == 2
