2ebd76cb51637020406a68af70918171b94844d6
kamyu <kamyu104@users.noreply.github.com>
2017-06-19 22:45:24 +0800

    Create longest-harmonious-subsequence.py

---
+++ Python/longest-harmonious-subsequence.py
@@ -0,0 +1,17 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def findLHS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lookup = collections.defaultdict(int)
+        result = 0
+        for num in nums:
+            lookup[num] += 1
+            for diff in [-1, 1]:
+                if (num + diff) in lookup:
+                    result = max(result, lookup[num] + lookup[num + diff])
+        return result
