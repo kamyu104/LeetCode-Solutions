d9bcd2c0ddc7aee225b33f983252c61728329831
kamyu <kamyu104@users.noreply.github.com>
2017-06-19 22:42:40 +0800

    Create longest-harmonious-subsequence.cpp

---
+++ C++/longest-harmonious-subsequence.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+        unordered_map<int, int> lookup;
+        auto result = 0;
+        for (const auto& num: nums) {
+            ++lookup[num];
+            for (const auto& diff : {-1, 1}) {
+                if (lookup.count(num + diff)) {
+                    result = max(result, lookup[num] + lookup[num + diff]);
+                }
+            }
+        }
+        return result;
+    }
+};
