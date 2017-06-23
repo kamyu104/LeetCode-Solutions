3faa2e66514bd25e1f30de8b01196aa009d92e76
kamyu <kamyu104@users.noreply.github.com>
2017-06-23 23:43:45 +0800

    Create design-compressed-string-iterator.py

---
+++ Python/design-compressed-string-iterator.py
@@ -0,0 +1,38 @@
+# Time:  O(1)
+# Space: O(1)
+
+class StringIterator(object):
+
+    def __init__(self, compressedString):
+        """
+        :type compressedString: str
+        """
+        self.__result = re.findall(r"([a-zA-Z])(\d+)", compressedString)
+        self.__index, self.__num, self.__ch = 0, 0, ' '
+
+    def next(self):
+        """
+        :rtype: str
+        """
+        if not self.hasNext():
+            return ' '
+        if self.__num == 0:
+            self.__ch = self.__result[self.__index][0]
+            self.__num = int(self.__result[self.__index][1])
+            self.__index += 1
+        self.__num -= 1
+        return self.__ch
+        
+
+    def hasNext(self):
+        """
+        :rtype: bool
+        """
+        return self.__index != len(self.__result) or self.__num != 0
+        
+
+
+# Your StringIterator object will be instantiated and called as such:
+# obj = StringIterator(compressedString)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
