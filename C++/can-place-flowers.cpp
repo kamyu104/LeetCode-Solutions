6dae5b4bf14b2132a2265db67e0bbe3424bf11ef
Allen Liu <kamyu104@gmail.com>
2017-06-24 20:45:34 +0800

    update

---
+++ C++/can-place-flowers.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        for (int i = 0; i < flowerbed.size(); ++i) {
+            if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) &&
+                (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                flowerbed[i] = 1;
+                --n;
+            }
+            if (n  <= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
