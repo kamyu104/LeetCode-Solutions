636e27f9e0ace162fcde6c9b0e61a2f60a894611
Allen Liu <kamyu104@gmail.com>
2017-06-26 22:10:15 +0800

    add

---
+++ C++/find-duplicate-file-in-system.cpp
@@ -0,0 +1,28 @@
+// Time:  O(n * l), l is the average length of file content
+// Space: O(n * l)
+
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> files;
+        for (const auto& path : paths) {
+    	    stringstream ss(path);
+    	    string root;
+    	    string s;
+    	    getline(ss, root, ' ');
+    	    while (getline(ss, s, ' ')) {
+    		    auto fileName = root + '/' + s.substr(0, s.find('('));
+    		    auto fileContent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
+    		    files[fileContent].emplace_back(fileName);
+    	    }
+        }
+    
+        vector<vector<string>> result;
+        for (const auto& file : files) {
+    	    if (file.second.size() > 1) {
+    		    result.emplace_back(file.second);
+    	    }
+        }
+        return result;
+    }
+};
