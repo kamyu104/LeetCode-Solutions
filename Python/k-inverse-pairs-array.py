cfffa29a44068721f97bbd4992cbfe58f0d09f51
kamyu <kamyu104@users.noreply.github.com>
2017-07-04 23:13:56 +0800

    Update k-inverse-pairs-array.py

--- Python/k-inverse-pairs-array.py
+++ Python/k-inverse-pairs-array.py
@@ -30,13 +30,13 @@ class Solution(object):
         :rtype: int
         """
         M = 1000000007
-        dp = [[0] * (k+1) for _ in xrange(2)]
+        dp = [[0]*(k+1) for _ in xrange(2)]
         dp[0][0] = 1
         for i in xrange(1, n+1):
-            dp[i%2] = [0] * (k+1)
+            dp[i%2] = [0]*(k+1)
             dp[i%2][0] = 1
             for j in xrange(1, k+1):
                 dp[i%2][j] = (dp[i%2][j-1] + dp[(i-1)%2][j]) % M
                 if j-i >= 0:
                     dp[i%2][j] = (dp[i%2][j] - dp[(i-1)%2][j-i]) % M
-        return dp[n % 2][k]
+        return dp[n%2][k]
