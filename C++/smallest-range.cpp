c7890b3eab11631e12b1f59304fc3bd5350b3fc6
Allen Liu <kamyu104@gmail.com>
2017-07-11 00:17:03 +0800

    add

--- C++/smallest-range.cpp
+++ C++/smallest-range.cpp
@@ -8,7 +8,7 @@ public:
         
         const auto comp = [](const pair<VIT, VIT>& p1, const pair<VIT, VIT>& p2) {
                               return *p1.first > *p2.first;
-                         };
+                          };
         
         int left = numeric_limits<int>::max(), right = numeric_limits<int>::min();
         priority_queue<pair<VIT, VIT>, vector<pair<VIT, VIT>>, decltype(comp)> min_heap(comp);
