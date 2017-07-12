4c2223b24d43aa84adb09afe1de82740807e53bd
Allen Liu <kamyu104@gmail.com>
2017-07-12 22:58:15 +0800

    add

---
+++ C++/sum-of-square-numbers.cpp
@@ -0,0 +1,16 @@
+// Time:  O(sqrt(c) * logc)
+// Space: O(1)
+
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        for (long long a = 0; a * a <= c; ++a) {
+            auto b = static_cast<int>(sqrt(c - a * a));
+            if (c - a * a == b * b) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
