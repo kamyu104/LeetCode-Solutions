34719eef8ae88af2bb7317175fa0b1773908f029
kamyu <kamyu104@users.noreply.github.com>
2017-07-12 23:10:07 +0800

    Create sum-of-square-numbers.py

---
+++ Python/sum-of-square-numbers.py
@@ -0,0 +1,25 @@
+# Time:  O(sqrt(c) * logc)
+# Space: O(1)
+
+# Given a non-negative integer c, your task is to decide
+# whether there're two integers a and b such that a2 + b2 = c.
+#
+# Example 1:
+# Input: 5
+# Output: True
+# Explanation: 1 * 1 + 2 * 2 = 5
+# Example 2:
+# Input: 3
+# Output: False
+    
+class Solution(object):
+    def judgeSquareSum(self, c):
+        """
+        :type c: int
+        :rtype: bool
+        """
+        for a in xrange(int(math.sqrt(c))+1):
+            b = int(math.sqrt(c-a**2))
+            if a**2 + b**2 == c:
+                return True
+        return False
