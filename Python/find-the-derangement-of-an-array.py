05fe1f5357bdf74e70bd161e105a15d5fff9568c
kamyu <kamyu104@users.noreply.github.com>
2017-07-13 23:50:18 +0800

    Create find-the-derangement-of-an-array.py

---
+++ Python/find-the-derangement-of-an-array.py
@@ -0,0 +1,15 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findDerangement(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        M = 1000000007
+        mul, total = 1, 0
+        for i in reversed(xrange(n+1)):
+            total = (total + M + (1 if i % 2 == 0 else -1) * mul) % M
+            mul = (mul * i) % M
+        return total
