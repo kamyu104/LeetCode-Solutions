d438be80a4cc36ddfe93c77bfbd3403a796bdbd9
Allen Liu <kamyu104@gmail.com>
2017-07-19 00:00:42 +0800

    add

---
+++ C++/decode-ways-ii.cpp
@@ -0,0 +1,35 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        static const int M = 1000000007;
+        static const int W = 3;
+        vector<long long> dp(W);
+        dp[0] = 1;
+        dp[1] = s[0] == '*' ? 9 : (s[0] == '0' ? 0 : 1);
+        for (int i = 1; i < s.length(); ++i) {
+            if (s[i] == '*') {
+                dp[(i + 1) % W] = 9 * dp[i % W];
+                if (s[i - 1] == '1') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + 9 * dp[(i - 1) % W]) % M;
+                } else if (s[i - 1] == '2') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + 6 * dp[(i - 1) % W]) % M;
+                } else if (s[i - 1] == '*') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + 15 * dp[(i - 1) % W]) % M;
+                }
+            } else {
+                dp[(i + 1) % W] = s[i] != '0' ? dp[i % W] : 0;
+                if (s[i - 1] == '1') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + dp[(i - 1) % W]) % M;
+                } else if (s[i - 1] == '2' && s[i] <= '6') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + dp[(i - 1) % W]) % M;
+                } else if (s[i - 1] == '*') {
+                    dp[(i + 1) % W] = (dp[(i + 1) % W] + (s[i] <= '6' ? 2 : 1) * dp[(i - 1) % W]) % M;
+                }
+            }
+        }
+        return static_cast<int>(dp[s.length() % W]);
+    }
+};
