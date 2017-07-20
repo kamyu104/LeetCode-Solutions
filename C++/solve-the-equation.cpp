ee2fe58afb729473a8dffcb87d6e2a6956c42839
Allen Liu <kamyu104@gmail.com>
2017-07-20 23:53:01 +0800

    update

--- C++/solve-the-equation.cpp
+++ C++/solve-the-equation.cpp
@@ -4,6 +4,33 @@
 class Solution {
 public:
     string solveEquation(string equation) {
+        auto a = 0, b = 0;
+        auto side = 1;
+        int submatches[] = { 1, 2, 3, 4 };
+        auto e = regex("(=)|([-+]?)(\\d*)(x?)");
+        for (regex_token_iterator<string::iterator> it(equation.begin(), equation.end(), e, submatches), end;
+             it != end;) {
+            auto eq = (it++)->str();
+            auto sign = (it++)->str();
+            auto num = (it++)->str();
+            auto isx = (it++)->str();
+            if (!eq.empty()) {
+                side = -1;
+            } else if (!isx.empty()) {
+                a += side * stoi(sign + "1") * stoi(num.empty() ? "1" : num);
+            } else if (!num.empty()) {
+                b -= side * stoi(sign + num);
+            }
+        }
+        return a != 0 ? "x=" + to_string(b / a) : b != 0 ? "No solution" : "Infinite solutions";
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    string solveEquation(string equation) {
         equation = regex_replace(equation, regex("(^|[+=-])x"), "$011x");
 
         auto pos = equation.find('=');
