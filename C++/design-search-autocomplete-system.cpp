52c69a9c9678071369b5083c6fc2b64ff5077146
Allen Liu <kamyu104@gmail.com>
2017-07-22 22:52:37 +0800

    add

---
+++ C++/design-search-autocomplete-system.cpp
@@ -0,0 +1,107 @@
+class AutocompleteSystem {
+public:
+    AutocompleteSystem(vector<string> sentences, vector<int> times) {
+        for (int i = 0; i < sentences.size(); ++i) {
+            trie_.insert(sentences[i], times[i]);
+        }
+    }
+    
+    vector<string> input(char c) {
+        if (c == '#') {
+            trie_.insert(cur_sent_, 1);
+            cur_sent_.clear();
+            return {};
+        } else {
+            cur_sent_.push_back(c);
+        }
+        return trie_.lookup(cur_sent_);
+    }
+
+private:
+    class TrieNode {
+    public:
+        using P = pair<int, string>; 
+        class Compare {
+        public:
+            bool operator()(const P& p1, const P& p2) {
+                return p1.first != p2.first ? p1.first > p2.first : p1.second < p2.second;
+            }
+        };
+
+        void insert(const string& s, int times) {
+            auto* p = this;
+            for (const auto& c : s) {
+                if (!p->leaves_.count(c)) {
+                    p->leaves_[c] = new TrieNode;
+                }
+                p = p->leaves_[c];
+            }
+            p->times_ += times;
+        }
+        
+        vector<string> lookup(string& s) {
+            auto* p = this;
+            for (const auto& c : s) {
+                if (!p->leaves_.count(c)) {
+                    return {};
+                }
+                p = p->leaves_[c];
+            }
+            priority_queue<P, vector<P>, Compare> min_heap;
+            traverseHelper(s, p, &min_heap);
+            vector<string> result;
+            while (!min_heap.empty()) {
+                cout << min_heap.top().second << endl;
+                result.emplace_back(min_heap.top().second);
+                min_heap.pop();
+            }
+            reverse(result.begin(), result.end());
+            return result;
+        }
+        
+        void traverseHelper(string& s, TrieNode *t, priority_queue<P, vector<P>, Compare> *min_heap) {
+            if (t->times_ > 0) {
+                min_heap->emplace(t->times_, s);
+                if (min_heap->size() == 4) {
+                    min_heap->pop();
+                }
+            }
+
+            for (auto i = 'a'; i <= 'z'; ++i) {
+                if (t->leaves_.count(i)) {
+                    s.push_back(i);
+                    traverseHelper(s, t->leaves_[i], min_heap);
+                    s.pop_back();
+                }
+            }
+            if (t->leaves_.count(' ')) {
+                s.push_back(' ');
+                traverseHelper(s, t->leaves_[' '], min_heap);
+                s.pop_back();
+            }
+        }
+        
+        TrieNode() : times_(0) {
+        }
+        
+        ~TrieNode() {
+            for (auto& kv : leaves_) {
+                if (kv.second) {
+                    delete kv.second;
+                }
+            }
+        }
+    private:
+        unordered_map<char, TrieNode *> leaves_;
+        int times_;
+    };
+
+    TrieNode trie_;
+    string cur_sent_;
+};
+
+/**
+ * Your AutocompleteSystem object will be instantiated and called as such:
+ * AutocompleteSystem obj = new AutocompleteSystem(sentences, times);
+ * vector<string> param_1 = obj.input(c);
+ */
