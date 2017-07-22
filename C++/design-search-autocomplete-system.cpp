2e09b06c2159c21b9e233a47549fca44f9e09fc3
Allen Liu <kamyu104@gmail.com>
2017-07-23 02:09:26 +0800

    update

--- C++/design-search-autocomplete-system.cpp
+++ C++/design-search-autocomplete-system.cpp
@@ -1,23 +1,34 @@
-// Time:  O(s + n), s is the length of the prefix,
-//                  n is the number of the subtree nodes of trie given with the prefix
+// Time:  O(s), s is the length of the prefix
 // Space: O(t), t is the size of trie
 
 class AutocompleteSystem {
 public:
-    AutocompleteSystem(vector<string> sentences, vector<int> times) {
+    AutocompleteSystem(vector<string> sentences, vector<int> times) : cur_node_(&trie_) {
         for (int i = 0; i < sentences.size(); ++i) {
-            trie_.insert(sentences[i], times[i]);
+            sentence_to_count_[sentences[i]] = times[i];
+            trie_.insert(sentences[i], sentence_to_count_[sentences[i]]);
         }
     }
     
     vector<string> input(char c) {
         vector<string> result;
         if (c == '#') {
-            trie_.insert(cur_sent_, 1);
-            cur_sent_.clear();
+            ++sentence_to_count_[search_];
+            trie_.insert(search_, sentence_to_count_[search_]);
+            cur_node_ = &trie_;
+            search_.clear();
         } else {
-            cur_sent_.push_back(c);
-            result = trie_.lookup(cur_sent_);
+            search_.push_back(c);
+            if (cur_node_) {
+                if (!cur_node_->leaves_.count(c)) {
+                    cur_node_ = nullptr;
+                    return {};
+                }
+                cur_node_ = cur_node_->leaves_[c];
+                for (const auto& p : cur_node_->infos_) {
+                    result.emplace_back(p.second);
+                }
+            }   
         }
         return result;
     }
@@ -25,19 +36,8 @@ public:
 private:
     class TrieNode {
     public:
-        using P = pair<int, string>;
         static const int TOP_COUNT = 3;
 
-        class Compare {
-        public:
-            bool operator()(const P& p1, const P& p2) {
-                return p1.first != p2.first ? p1.first > p2.first : p1.second < p2.second;
-            }
-        };
-
-        TrieNode() : times_(0) {
-        }
-        
         ~TrieNode() {
             for (auto& kv : leaves_) {
                 if (kv.second) {
@@ -48,66 +48,40 @@ private:
 
         // Time:  O(s)
         void insert(const string& s, int times) {
-            auto* p = this;
+            auto* cur = this;
+            cur->add_info(s, times);
             for (const auto& c : s) {
-                if (!p->leaves_.count(c)) {
-                    p->leaves_[c] = new TrieNode;
+                if (!cur->leaves_.count(c)) {
+                    cur->leaves_[c] = new TrieNode;
                 }
-                p = p->leaves_[c];
-            }
-            p->times_ += times;
-        }
-        
-        // Time:  O(s + n)
-        vector<string> lookup(string& s) {
-            auto* p = this;
-            for (const auto& c : s) {
-                if (!p->leaves_.count(c)) {
-                    return {};
-                }
-                p = p->leaves_[c];
-            }
-            priority_queue<P, vector<P>, Compare> min_heap;
-            traverseHelper(s, p, &min_heap);
-            vector<string> result;
-            while (!min_heap.empty()) {
-                result.emplace_back(min_heap.top().second);
-                min_heap.pop();
-            }
-            reverse(result.begin(), result.end());
-            return result;
-        }
-
-    private:
-        // Time:  O(n)
-        void traverseHelper(string& s, TrieNode *t, priority_queue<P, vector<P>, Compare> *min_heap) {
-            if (t->times_ > 0) {
-                min_heap->emplace(t->times_, s);
-                if (min_heap->size() == TOP_COUNT + 1) {
-                    min_heap->pop();
-                }
-            }
-
-            for (auto i = 'a'; i <= 'z'; ++i) {
-                if (t->leaves_.count(i)) {
-                    s.push_back(i);
-                    traverseHelper(s, t->leaves_[i], min_heap);
-                    s.pop_back();
-                }
-            }
-            if (t->leaves_.count(' ')) {
-                s.push_back(' ');
-                traverseHelper(s, t->leaves_[' '], min_heap);
-                s.pop_back();
+                cur = cur->leaves_[c];
+                cur->add_info(s, times);
             }
         }
         
+        // Time:  O(1)
+        void add_info(const string& s, int times) {
+            auto it = find_if(infos_.begin(), infos_.end(),
+                         [&s, &times](const pair<int, string>& p){ return p.second == s;} );
+            if (it != infos_.end()) {
+                it->first = -times;
+            } else {
+                infos_.emplace_back(-times, s);
+            }
+            sort(infos_.begin(), infos_.end());
+            if (infos_.size() > TOP_COUNT) {
+                infos_.pop_back();
+            }
+        }
+        
+        vector<pair<int, string>> infos_;
         unordered_map<char, TrieNode *> leaves_;
-        int times_;
     };
 
     TrieNode trie_;
-    string cur_sent_;
+    TrieNode *cur_node_;
+    string search_;
+    unordered_map<string, int> sentence_to_count_;
 };
 
 /**
