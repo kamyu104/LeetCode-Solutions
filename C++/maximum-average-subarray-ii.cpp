2e69a7cd95b9ec6a3f14f9c9b497a4a89fce3ba4
Allen Liu <kamyu104@gmail.com>
2017-07-26 00:37:05 +0800

    update

--- C++/maximum-average-subarray-ii.cpp
+++ C++/maximum-average-subarray-ii.cpp
@@ -6,9 +6,9 @@ public:
     double findMaxAverage(vector<int>& nums, int k) {
         double left = *min_element(nums.begin(), nums.end());
         double right = *max_element(nums.begin(), nums.end());
-        while (abs(left - right) > 0.00001) {
+        while (right - left > 0.00001) {
             double mid = left + (right - left) / 2;
-            if (!check(nums, mid, k)) {
+            if (isMidLargerOrEqualToTarget(mid, nums, k)) {
                 right = mid;
             } else {
                 left = mid;
@@ -18,23 +18,22 @@ public:
     }
 
 private:
-    bool check(const vector<int>& nums, double mid, int k) {
+    bool isMidLargerOrEqualToTarget(double mid, const vector<int>& nums, int k) {
         double sum = 0, prev = 0, min_sum = 0;
         for (int i = 0; i < k; ++i) {
             sum += nums[i] - mid;
         }
-        if (sum >= 0) {
-            return true;
+        if (sum > 0) {
+            return false;
         }
         for (int i = k; i < nums.size(); ++i) {
             sum += nums[i] - mid;
             prev += nums[i - k] - mid;
             min_sum = min(prev, min_sum);
-            if (sum >= min_sum) {
-                return true;
+            if (sum > min_sum) {
+                return false;
             }
         }
-        return true;
+        return true;  
     }
 };
-
