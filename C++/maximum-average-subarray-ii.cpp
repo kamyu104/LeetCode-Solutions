7514bdbe216bb77f0bea09c9257f84314ca63932
Allen Liu <kamyu104@gmail.com>
2017-07-26 02:01:52 +0800

    update

--- C++/maximum-average-subarray-ii.cpp
+++ C++/maximum-average-subarray-ii.cpp
@@ -1,7 +1,42 @@
-// Time:  O(nlog(max_val - min_val))
-// Space: O(1)
+// Time:  O(n)
+// Space: O(n)
 
 class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        double left = *min_element(nums.begin(), nums.end());
+        double delta = numeric_limits<double>::max();
+        while (delta > 1e-5) {
+            delta = getDelta(nums, k, left);
+            left += delta;
+        }
+        return left;
+    }
+
+private:
+    double getDelta(const vector<int>& nums, int k, double avg) {
+        vector<double> accu(nums.size() + 1);
+        int minval_pos = -1;
+        double delta = 0.0;
+        for (int i = 0; i < nums.size(); ++i) {
+            accu[i + 1] = nums[i] + accu[i] - avg;
+            if (i >= k - 1) {
+                if (minval_pos == -1 || accu[i - k + 1] < accu[minval_pos]) {
+                    minval_pos = i - k + 1;
+                }
+                if (accu[i+1] - accu[minval_pos] >= 0) {
+                    delta = max(delta, (accu[i + 1] - accu[minval_pos]) / (i + 1 - minval_pos));
+                }
+            }
+        }
+        return delta;
+    }
+};
+
+
+// Time:  O(nlogm), m is (max_val - min_val)
+// Space: O(1)
+class Solution2 {
 public:
     double findMaxAverage(vector<int>& nums, int k) {
         double left = *min_element(nums.begin(), nums.end());
