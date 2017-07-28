da55d98357e1764f9ee9b93ca3e035ba74b1a030
kamyu <kamyu104@users.noreply.github.com>
2017-07-28 23:35:03 +0800

    Create design-log-storage-system.py

---
+++ Python/design-log-storage-system.py
@@ -0,0 +1,40 @@
+# Time:  put:      O(1)
+#        retrieve: O(n + dlogd), n is the size of the total logs
+#                              , d is the size of the found logs
+# Space: O(n)
+    
+class LogSystem(object):
+
+    def __init__(self):
+        self.__logs = []
+        self.__granularity = {'Year': 5, 'Month': 8, 'Day': 11, \
+                              'Hour': 14, 'Minute': 17, 'Second': 20}
+        
+
+    def put(self, id, timestamp):
+        """
+        :type id: int
+        :type timestamp: str
+        :rtype: void
+        """
+        self.__logs.append((id, timestamp))
+
+        
+    def retrieve(self, s, e, gra):
+        """
+        :type s: str
+        :type e: str
+        :type gra: str
+        :rtype: List[int]
+        """
+        i = self.__granularity[gra]
+        begin = s[:i]
+        end = e[:i]
+        return sorted(id for id, timestamp in self.__logs \
+                      if begin <= timestamp[:i] <= end)
+
+
+# Your LogSystem object will be instantiated and called as such:
+# obj = LogSystem()
+# obj.put(id,timestamp)
+# param_2 = obj.retrieve(s,e,gra)
