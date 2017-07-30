5986d0e598c56f3cdabe6d25b2d1ee9f28d87739
Allen Liu <kamyu104@gmail.com>
2017-07-30 22:28:37 +0800

    add

--- C++/set-mismatch.cpp
+++ C++/set-mismatch.cpp
@@ -4,6 +4,29 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
+        int x_xor_y = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            x_xor_y ^= nums[i] ^ (i + 1);
+        }
+        int bit = x_xor_y & ~(x_xor_y - 1);
+        vector<int> result(2);
+        for (int i = 0; i < nums.size(); ++i) {
+            result[static_cast<bool>(nums[i] & bit)] ^= nums[i];
+            result[static_cast<bool>((i + 1) & bit)] ^= i + 1;
+        }
+        if (find(nums.begin(), nums.end(), result[0]) == nums.end()) {
+            swap(result[0], result[1]);
+        }
+        return result;
+    }
+};
+
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
         vector<int> result(2);
         for (const auto& i : nums) {
             if (nums[abs(i) - 1] < 0) {  // twice
