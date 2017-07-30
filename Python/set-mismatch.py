7e949ef05b5443afa55b734338eb4f543af42fbc
kamyu <kamyu104@users.noreply.github.com>
2017-07-30 22:31:16 +0800

    Update set-mismatch.py

--- Python/set-mismatch.py
+++ Python/set-mismatch.py
@@ -23,13 +23,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        x_xor_y = reduce(operator.xor, nums) ^ reduce(operator.xor, xrange(1, len(nums)+1))
+        x_xor_y = 0
+        for i in xrange(len(nums)):
+            x_xor_y ^= nums[i] ^ (i+1)
         bit =  x_xor_y & ~(x_xor_y - 1)
         result = [0, 0]
         for i, num in enumerate(nums):
             result[bool(num & bit)] ^= num
             result[bool((i+1) & bit)] ^= i+1
-        if result[1] in nums: result[0], result[1] = result[1], result[0]
+        if result[0] not in nums:
+            result[0], result[1] = result[1], result[0]
         return result
 
 
