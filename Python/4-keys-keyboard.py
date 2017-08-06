6ffb94346a06f204c9469f18a05ac92d2e6f0915
kamyu <kamyu104@users.noreply.github.com>
2017-08-07 00:03:52 +0800

    Update 4-keys-keyboard.py

--- Python/4-keys-keyboard.py
+++ Python/4-keys-keyboard.py
@@ -28,7 +28,7 @@ class Solution2(object):
         :rtype: int
         """
         if N < 7: return N
-        dp = [i for i in xrange(N+1)]
+        dp = range(N+1)
         for i in xrange(7, N+1):
             dp[i % 6] = max(dp[(i-4) % 6]*3, dp[(i-5) % 6]*4)
         return dp[N % 6]
