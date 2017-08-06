8994787ea552415d703ee26e81097e19c950632d
kamyu <kamyu104@users.noreply.github.com>
2017-08-06 23:53:43 +0800

    Update 4-keys-keyboard.py

--- Python/4-keys-keyboard.py
+++ Python/4-keys-keyboard.py
@@ -1,7 +1,27 @@
-# Time:  O(n)
+# Time:  O(1)
 # Space: O(1)
 
 class Solution(object):
+    def maxA(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N <= 6: return N
+        if N == 10: return 20
+
+        n  = N // 5 + 1  # n3 + n4 increases one every 5 keys 
+        # (1) n     =     n3 +     n4
+        # (2) N + 1 = 4 * n3 + 5 * n4
+        #     5 x (1) - (2) => 5*n - N - 1 = n3
+        n3 = 5*n - N - 1  
+        n4 = n - n3
+        return 3**n3 * 4**n4
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
     def maxA(self, N):
         """
         :type N: int
