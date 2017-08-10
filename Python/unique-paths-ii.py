65fc35581603f242c515345c4531f3f9e50cdf4c
Swaroop Gadiyaram <swaroop.g7@gmail.com>
2017-08-09 19:19:10 -0700

    Update unique-paths-ii.py

--- Python/unique-paths-ii.py
+++ Python/unique-paths-ii.py
@@ -32,10 +32,10 @@ class Solution:
         
         ways = [0]*n
         ways[0] = 1
-        for i in range(m):
+        for i in xrange(m):
             if obstacleGrid[i][0] == 1:
                 ways[0] = 0
-            for j in range(n):
+            for j in xrange(n):
                 if obstacleGrid[i][j] == 1:
                     ways[j] = 0
                 elif j>0:
