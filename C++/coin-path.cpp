e2ee1571b0f10955db3c3efbac3e01d418aa2db9
kamyu <kamyu104@users.noreply.github.com>
2017-08-13 11:14:13 +0800

    Create coin-path.cpp

---
+++ C++/coin-path.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n * B)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> cheapestJump(vector<int>& A, int B) {
+        vector<int> result;
+        if (A.empty() || A.back() == -1) {
+            return result;
+        }
+        const int n = A.size();
+        vector<int> dp(n, numeric_limits<int>::max()), next(n, -1);
+        dp[n - 1] = A[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            if (A[i] == -1) {
+                continue;
+            }
+            for (int j = i + 1; j <= min(i + B, n - 1); ++j) {
+                if (dp[j] == numeric_limits<int>::max()) {
+                    continue;
+                }
+                if (A[i] + dp[j] < dp[i]) {
+                    dp[i] = A[i] + dp[j];
+                    next[i] = j;
+                }
+            }
+        }
+        if (dp[0] == numeric_limits<int>::max()) {
+            return result;
+        }
+        int k = 0;
+        while (k != -1) {
+            result.emplace_back(k + 1);
+            k = next[k];
+        }
+        return result;
+    }
+};
