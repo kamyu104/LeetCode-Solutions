371fb73d49361fa2676b277e55f461c3f9ca6b2b
kamyu <kamyu104@users.noreply.github.com>
2017-08-15 21:44:28 +0800

    Create find-k-closest-elements.py

---
+++ Python/find-k-closest-elements.py
@@ -0,0 +1,36 @@
+# Time:  O(logn + k)
+# Space: O(1)
+
+# Given a sorted array, two integers k and x, find the k closest elements to x in the array.
+# The result should also be sorted in ascending order.
+# If there is a tie, the smaller elements are always preferred.
+#
+# Example 1:
+# Input: [1,2,3,4,5], k=4, x=3
+# Output: [1,2,3,4]
+# Example 2:
+# Input: [1,2,3,4,5], k=4, x=-1
+# Output: [1,2,3,4]
+# Note:
+# The value k is positive and will always be smaller than the length of the sorted array.
+# Length of the given array is positive and will not exceed 10^4
+# Absolute value of elements in the array and x will not exceed 10^4
+
+class Solution(object):
+    def findClosestElements(self, arr, k, x):
+        """
+        :type arr: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        i = bisect.bisect_left(arr, x)
+        left, right = i-1, i
+        while k:
+            if right >= len(arr) or \
+               (left >= 0 and abs(arr[left]-x) <= abs(arr[right]-x)):
+                left -= 1
+            else:
+                right += 1
+            k -= 1
+        return arr[left+1:right]
