decc9408bbc43105c06241a55b7c00e06554eff2
kamyu <kamyu104@users.noreply.github.com>
2017-08-21 21:52:46 +0800

    Create image-smoother.cpp

---
+++ C++/image-smoother.cpp
@@ -0,0 +1,34 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
+        const auto m = M.size(), n = M[0].size();
+        vector<vector<int>> result(M);
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                result[i][j] = getGray(M, i, j);
+            }
+        }
+        return result;
+    }
+private:
+    int getGray(const vector<vector<int>>& M, int i, int j) {
+        const auto& m = M.size(), n = M[0].size();
+        static const vector<pair<int, int>> directions = { {-1, -1}, {0, -1}, {1, -1},
+                                                           {-1,  0},          {1,  0},
+                                                           {-1,  1} ,{0,  1}, {1,  1} };
+        double total = M[i][j];
+        int count = 1;
+        for (const auto& direction : directions) {
+            const auto& ii = i + direction.first;
+            const auto& jj = j + direction.second;
+            if (0 <= ii && ii < m && 0 <= jj & jj < n) {
+                total += M[ii][jj];
+                ++count;
+            }
+        }
+        return static_cast<int>(total / count);
+    }
+};
