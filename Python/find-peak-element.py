86e43b125ce006f6dc26a623f922614d221ab802
Swaroop Gadiyaram <swaroop.g7@gmail.com>
2017-08-23 11:31:37 -0700

    Update find-peak-element.py
    Adding another solution with same complexity but less case checks

--- Python/find-peak-element.py
+++ Python/find-peak-element.py
@@ -37,7 +37,22 @@ class Solution(object):
                 left = mid + 1
        
         return left
-
+    
+    def findPeakElement2(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        
+        while left < right:
+            mid = left + (right - left) / 2
+            if nums[mid] > nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 1
+       
+        return left
 
 if __name__ == "__main__":
    # print Solution().findPeakElement([1,2,1])
