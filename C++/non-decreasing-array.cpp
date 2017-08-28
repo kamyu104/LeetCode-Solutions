3ad8dcbf47a30984664ac3775814fd24dcb2e05c
kamyu <kamyu104@users.noreply.github.com>
2017-08-28 22:48:40 +0800

    Update non-decreasing-array.cpp

--- C++/non-decreasing-array.cpp
+++ C++/non-decreasing-array.cpp
@@ -12,8 +12,8 @@ public:
                 }
                 if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
                     prev = nums[i];      // nums[i - 1] = nums[i], prev = nums[i]
-                } else {
-                    prev = nums[i - 1];  // nums[i] = nums[i - 1], prev = nums[i]
+//              } else {
+//                  prev = nums[i - 1];  // nums[i] = nums[i - 1], prev = nums[i]
                 }
             } else {
                 prev = nums[i];
