9d5cdc1a86f2f0582e8094885b9885fdf7a008f2
kamyu <kamyu104@users.noreply.github.com>
2017-08-28 22:32:29 +0800

    Create non-decreasing-array.cpp

---
+++ C++/non-decreasing-array.cpp
@@ -0,0 +1,24 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int cnt = 0;
+        for (int i = 1, prev = nums[0]; i < nums.size(); ++i) {
+            if (prev > nums[i]) {
+                if (cnt++) {
+                    return false;
+                }
+                if (i - 2 < 0 || nums[i - 2] <= nums[i]) {
+                    prev = nums[i];      // nums[i - 1] = nums[i], prev = nums[i]
+                } else {
+                    prev = nums[i - 1];  // nums[i] = nums[i - 1], prev = nums[i]
+                }
+            } else {
+                prev = nums[i];
+            }
+        }
+        return true;
+    }
+};
