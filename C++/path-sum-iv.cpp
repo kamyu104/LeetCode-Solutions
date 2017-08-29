95c6b63a980c650f5b56c2c9aeb946a4983a14db
kamyu <kamyu104@users.noreply.github.com>
2017-08-29 22:22:32 +0800

    Create path-sum-iv.cpp

---
+++ C++/path-sum-iv.cpp
@@ -0,0 +1,41 @@
+// Time:  O(n)
+// Space: O(w), w is the max node count of the level in the tree
+
+class Solution {
+public:
+    int pathSum(vector<int>& nums) {
+        if (nums.empty()) {
+            return 0;
+        }
+        int result = 0;
+        queue<node> q;
+        node dummy(10);
+        node* parent_ptr = &dummy;
+        for (const auto& num : nums) {
+            node child(num);
+            while (!parent_ptr->isParent(child)) {
+                result += parent_ptr->leaf ? parent_ptr->val : 0;
+                parent_ptr = &q.front();
+                q.pop();
+            }
+            parent_ptr->leaf = false;
+            child.val += parent_ptr->val;
+            q.emplace(move(child));
+        }
+        while (!q.empty()) {
+            result += q.front().val;
+            q.pop();
+        }
+        return result;
+    }
+
+private:
+    struct node {
+        int level, i, val;
+        bool leaf;
+        node(int n) : level(n / 100 - 1), i((n % 100) / 10 - 1), val(n % 10), leaf(true) {};
+        inline bool isParent(const node& other) {
+            return level == other.level - 1 && i == other.i / 2;
+        };
+    };
+};
