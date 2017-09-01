9df9342334098ce3b417fb25bd49a34cc19dfba3
kamyu <kamyu104@users.noreply.github.com>
2017-09-01 13:23:37 +0800

    Create kth-smallest-number-in-multiplication-table.cpp

---
+++ C++/kth-smallest-number-in-multiplication-table.cpp
@@ -0,0 +1,26 @@
+// Time:  O(m * log(m * n))
+
+class Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (count(mid, m, n) >= k) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+private:
+    int count(int target, int m, int n) {
+        auto count = 0;
+        for (int i = 1; i <= m; ++i) {
+            count += min(target / i , n);
+        }
+        return count;
+    }
+};
