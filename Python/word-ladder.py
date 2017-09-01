d6edcad407fac6a07a1f93c5357ff0f385edca4a
kamyu <kamyu104@users.noreply.github.com>
2017-09-01 23:51:32 +0800

    Update word-ladder.py

--- Python/word-ladder.py
+++ Python/word-ladder.py
@@ -32,7 +32,7 @@ class Solution(object):
         distance, cur, visited, lookup = 0, [beginWord], set([beginWord]), set(wordList)
 
         while cur:
-            _next = []
+            next_queue = []
 
             for word in cur:
                 if word == endWord:
@@ -41,10 +41,10 @@ class Solution(object):
                     for j in 'abcdefghijklmnopqrstuvwxyz':
                         candidate = word[:i] + j + word[i + 1:]
                         if candidate not in visited and candidate in lookup:
-                            _next.append(candidate)
+                            next_queue.append(candidate)
                             visited.add(candidate)
             distance += 1
-            cur = _next
+            cur = next_queue
 
         return 0
 
