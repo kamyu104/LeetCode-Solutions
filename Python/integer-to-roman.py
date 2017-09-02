dabfcc2ba9552cfbb174b8cf62bc1fbfd4cb2ed0
kamyu <kamyu104@users.noreply.github.com>
2017-09-02 15:17:51 +0800

    Update integer-to-roman.py

--- Python/integer-to-roman.py
+++ Python/integer-to-roman.py
@@ -6,11 +6,16 @@
 # Input is guaranteed to be within the range from 1 to 3999.
 #
 
-
-class Solution:
-    # @return a string
+class Solution(object):
     def intToRoman(self, num):
-        numeral_map = {1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L", 90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M"}
+        """
+        :type num: int
+        :rtype: str
+        """
+        numeral_map = {1: "I", 4: "IV", 5: "V", 9: "IX", \
+                       10: "X", 40: "XL", 50: "L", 90: "XC", \
+                       100: "C", 400: "CD", 500: "D", 900: "CM", \
+                       1000: "M"}
         keyset, result = sorted(numeral_map.keys()), ""
         
         while num > 0:
@@ -21,6 +26,7 @@ class Solution:
                     
         return result
 
+ 
 if __name__ == "__main__":
     print Solution().intToRoman(999)
     print Solution().intToRoman(3999)
