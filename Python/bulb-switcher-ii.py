f16165310c88e4d6602ad788b2cbcd44bc1ae660
kamyu <kamyu104@users.noreply.github.com>
2017-09-03 16:35:12 +0800

    Update bulb-switcher-ii.py

--- Python/bulb-switcher-ii.py
+++ Python/bulb-switcher-ii.py
@@ -11,6 +11,6 @@ class Solution(object):
         if m == 0:            return 1
         if n == 1:            return 2
         if m == 1 and n == 2: return 3
-        if m == 1 or n == 2   return 4
+        if m == 1 or  n == 2: return 4
         if m == 2:            return 7
         return 8
