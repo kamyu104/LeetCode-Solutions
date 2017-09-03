16e539bc120d24c37de140bfb1ee0771b6cb5905
kamyu <kamyu104@users.noreply.github.com>
2017-09-03 13:36:58 +0800

    Create trim-a-binary-search-tree.cpp

---
+++ C++/trim-a-binary-search-tree.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int L, int R) {
+        if (!root) {
+            return nullptr;
+        }
+        if (root->val < L) {
+            return trimBST(root->right, L, R);
+        }
+        if (root->val > R) {
+            return trimBST(root->left, L, R);
+        }
+        root->left = trimBST(root->left, L, R);
+        root->right = trimBST(root->right, L, R);
+        return root;
+    }
+};
