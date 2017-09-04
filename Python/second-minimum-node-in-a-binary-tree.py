36db02ab4439b418bd0225e7bcf915791a079051
kamyu <kamyu104@users.noreply.github.com>
2017-09-04 13:00:17 +0800

    Update second-minimum-node-in-a-binary-tree.py

--- Python/second-minimum-node-in-a-binary-tree.py
+++ Python/second-minimum-node-in-a-binary-tree.py
@@ -1,6 +1,34 @@
 # Time:  O(n)
 # Space: O(h)
 
+# Given a non-empty special binary tree consisting of nodes with the non-negative value,
+# where each node in this tree has exactly two or zero sub-node. If the node has two sub-nodes,
+# then this node's value is the smaller value among its two sub-nodes.
+#
+# Given such a binary tree, you need to output the second minimum value in the set made of
+# all the nodes' value in the whole tree.
+#
+# If no such second minimum value exists, output -1 instead.
+#
+# Example 1:
+# Input: 
+#     2
+#    / \
+#   2   5
+#      / \
+#     5   7
+#
+# Output: 5
+# Explanation: The smallest value is 2, the second smallest value is 5.
+# Example 2:
+# Input: 
+#     2
+#    / \
+#   2   2
+#
+# Output: -1
+# Explanation: The smallest value is 2, but there isn't any second smallest value.
+    
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
