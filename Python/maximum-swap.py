07746d1c0cb7d375b43448247b34146c632baaae
kamyu <kamyu104@users.noreply.github.com>
2017-09-04 12:59:02 +0800

    Update maximum-swap.py

--- Python/maximum-swap.py
+++ Python/maximum-swap.py
@@ -1,6 +1,20 @@
 # Time:  O(l), l is the length of the number string
 # Space: O(l)
 
+# Given a non-negative integer, you could swap two digits at most once
+# to get the maximum valued number. Return the maximum valued number you could get.
+#
+# Example 1:
+# Input: 2736
+# Output: 7236
+# Explanation: Swap the number 2 and the number 7.
+# Example 2:
+# Input: 9973
+# Output: 9973
+# Explanation: No swap.
+# Note:
+# The given number is in the range [0, 10^8]
+
 class Solution(object):
     def maximumSwap(self, num):
         """
