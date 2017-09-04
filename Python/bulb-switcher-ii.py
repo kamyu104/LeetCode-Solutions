1327b787da63dbb35b5b6c563ec9586d33524247
kamyu <kamyu104@users.noreply.github.com>
2017-09-04 13:01:59 +0800

    Update bulb-switcher-ii.py

--- Python/bulb-switcher-ii.py
+++ Python/bulb-switcher-ii.py
@@ -1,6 +1,30 @@
 # Time:  O(1)
 # Space: O(1)
 
+# There is a room with n lights which are turned on initially and 4 buttons on the wall.
+# After performing exactly m unknown operations towards buttons,
+# you need to return how many different kinds of status of the n lights could be.
+#
+# Suppose n lights are labeled as number [1, 2, 3 ..., n], function of these 4 buttons are given below:
+#   1. Flip all the lights.
+#   3. Flip lights with even numbers.
+#   3. Flip lights with odd numbers.
+#   4. Flip lights with (3k + 1) numbers, k = 0, 1, 2, ...
+#
+# Example 1:
+# Input: n = 1, m = 1.
+# Output: 2
+# Explanation: Status can be: [on], [off]
+# Example 2:
+# Input: n = 2, m = 1.
+# Output: 3
+# Explanation: Status can be: [on, off], [off, on], [off, off]
+# Example 3:
+# Input: n = 3, m = 1.
+# Output: 4
+# Explanation: Status can be: [off, on, off], [on, off, on], [off, off, off], [off, on, on].
+# Note: n and m both fit in range [0, 1000].
+    
 class Solution(object):
     def flipLights(self, n, m):
         """
