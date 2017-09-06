c5ab9fd01bed1bff4c2f3256eae0442aab300772
kamyu <kamyu104@users.noreply.github.com>
2017-09-06 22:10:28 +0800

    Update equal-tree-partition.py

--- Python/equal-tree-partition.py
+++ Python/equal-tree-partition.py
@@ -1,45 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
 
-# Given a binary tree with n nodes, your task is to check 
-# if it's possible to partition the tree to two trees which have the equal sum 
-# of values after removing exactly one edge on the original tree.
-#
-# Example 1:
-# Input:     
-#     5
-#    / \
-#   10 10
-#     /  \
-#    2   3
-#
-# Output: True
-# Explanation: 
-#     5
-#    / 
-#   10
-#     
-# Sum: 15
-#
-#    10
-#   /  \
-#  2    3
-#
-# Sum: 15
-# Example 2:
-# Input:     
-#     1
-#    / \
-#   2  10
-#     /  \
-#    2   20
-#
-# Output: False
-# Explanation: You can't split the tree into two trees with equal sum after removing exactly one edge on the tree.
-# Note:
-# The range of tree node value is in the range of [-100000, 100000].
-# 1 <= n <= 10000
-
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
