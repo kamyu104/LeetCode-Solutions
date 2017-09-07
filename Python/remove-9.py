1a7d6c2b9d485da7444c1b160f825825466c36e9
kamyu <kamyu104@users.noreply.github.com>
2017-09-07 23:24:27 +0800

    Update remove-9.py

--- Python/remove-9.py
+++ Python/remove-9.py
@@ -1,17 +1,5 @@
 # Time:  O(logn)
 # Space: O(1)
-
-# Start from integer 1, remove any integer that contains 9 such as 9, 19, 29...
-#
-# So now, you will have a new integer sequence: 1, 2, 3, 4, 5, 6, 7, 8, 10, 11, ...
-#
-# Given a positive integer n, you need to return the n-th integer after removing.
-# Note that 1 will be the first integer.
-#
-# Example 1:
-# Input: 9
-# Output: 10
-# Hint: n will not exceed 9 x 10^8.
     
 class Solution(object):
     def newInteger(self, n):
