97cffb9ebcec3fbbeb2deff600d2b12b942b4bd1
kamyu <kamyu104@users.noreply.github.com>
2017-09-10 13:50:50 +0800

    Create implement-magic-dictionary.py

---
+++ Python/implement-magic-dictionary.py
@@ -0,0 +1,55 @@
+# Time:  O(n), n is the length of the word
+# Space: O(d)
+
+class MagicDictionary(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        _trie = lambda: collections.defaultdict(_trie)
+        self.trie = _trie()
+
+
+    def buildDict(self, dict):
+        """
+        Build a dictionary through a list of words
+        :type dict: List[str]
+        :rtype: void
+        """
+        for s in dict:
+            curr = self.trie
+            for c in s: 
+                curr = curr[c] 
+            curr.setdefault("_end")
+
+
+    def search(self, word):
+        """
+        Returns if there is any word in the trie that equals to the given word after modifying exactly one character
+        :type word: str
+        :rtype: bool
+        """
+        def find(curr, i, mistakeAllowed): 
+            if i == len(word):
+                if "_end" in curr and not mistakeAllowed: 
+                    return True
+                return False 
+            if word[i] not in curr: 
+                return any(find(curr[c], i+1, False) for c in curr if c != "_end") \
+                           if mistakeAllowed else False 
+            
+            if mistakeAllowed: 
+                return find(curr[word[i]], i+1, True) or \
+                       any(find(curr[c], i+1, False) \
+                           for c in curr if c not in ("_end", word[i]))
+            return find(curr[word[i]], i+1, False)
+            
+        return find(self.trie, 0, True)    
+        
+
+
+# Your MagicDictionary object will be instantiated and called as such:
+# obj = MagicDictionary()
+# obj.buildDict(dict)
+# param_2 = obj.search(word)
