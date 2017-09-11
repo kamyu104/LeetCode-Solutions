1c0dc9c825f65d603def358e5c6b4fee227ac4d1
kamyu <kamyu104@users.noreply.github.com>
2017-09-11 21:14:06 +0800

    Update longest-continuous-increasing-subsequence.py

--- Python/longest-continuous-increasing-subsequence.py
+++ Python/longest-continuous-increasing-subsequence.py
@@ -1,6 +1,21 @@
 # Time:  O(n)
 # Space: O(1)
 
+# Given an unsorted array of integers,
+# find the length of longest continuous increasing subsequence.
+#
+# Example 1:
+# Input: [1,3,5,4,7]
+# Output: 3
+# Explanation: The longest continuous increasing subsequence is [1,3,5], its length is 3. 
+# Even though [1,3,5,7] is also an increasing subsequence,
+# it's not a continuous one where 5 and 7 are separated by 4. 
+# Example 2:
+# Input: [2,2,2,2,2]
+# Output: 1
+# Explanation: The longest continuous increasing subsequence is [2], its length is 1. 
+# Note: Length of the array will not exceed 10,000.
+
 class Solution(object):
     def findLengthOfLCIS(self, nums):
         """
