aae5c6c3433f9bd7aa4e108012abfb2eaa468832
kamyu <kamyu104@users.noreply.github.com>
2017-09-11 21:18:52 +0800

    Update implement-magic-dictionary.py

--- Python/implement-magic-dictionary.py
+++ Python/implement-magic-dictionary.py
@@ -1,6 +1,27 @@
 # Time:  O(n), n is the length of the word
 # Space: O(d)
 
+# Implement a magic directory with buildDict, and search methods.
+#
+# For the method buildDict, you'll be given a list of non-repetitive words to build a dictionary.
+#
+# For the method search, you'll be given a word, and judge whether
+# if you modify exactly one character into another character in this word,
+# the modified word is in the dictionary you just built.
+#
+# Example 1:
+# Input: buildDict(["hello", "leetcode"]), Output: Null
+# Input: search("hello"), Output: False
+# Input: search("hhllo"), Output: True
+# Input: search("hell"), Output: False
+# Input: search("leetcoded"), Output: False
+# Note:
+# You may assume that all the inputs are consist of lowercase letters a-z.
+# For contest purpose, the test data is rather small by now.
+# You could think about highly efficient algorithm after the contest.
+# Please remember to RESET your class variables declared in class MagicDictionary,
+# as static/class variables are persisted across multiple test cases. Please see here for more details.
+
 class MagicDictionary(object):
 
     def __init__(self):
