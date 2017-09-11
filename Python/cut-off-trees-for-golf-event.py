80c7aec5a9d36566ad5929c7457d28c51e671dee
kamyu <kamyu104@users.noreply.github.com>
2017-09-11 21:17:25 +0800

    Update cut-off-trees-for-golf-event.py

--- Python/cut-off-trees-for-golf-event.py
+++ Python/cut-off-trees-for-golf-event.py
@@ -1,6 +1,52 @@
 # Time:  O(t * (logt + m * n)), t is the number of trees
 # Space: O(t + m * n)
 
+# You are asked to cut off trees in a forest for a golf event.
+# The forest is represented as a non-negative 2D map, in this map:
+#
+# 0 represents the obstacle can't be reached.
+# 1 represents the ground can be walked through.
+# The place with number bigger than 1 represents a tree can be walked through,
+# and this positive number represents the tree's height.
+#
+# You are asked to cut off all the trees in this forest in the order of tree's height -
+# always cut off the tree with lowest height first. And after cutting, the original place
+# has the tree will become a grass (value 1).
+#
+# You will start from the point (0, 0)
+# and you should output the minimum steps you need to walk to cut off all the trees.
+# If you can't cut off all the trees, output -1 in that situation.
+#
+# You are guaranteed that no two trees have the same height and there is at least one tree needs to be cut off.
+#
+# Example 1:
+# Input: 
+# [
+#  [1,2,3],
+#  [0,0,4],
+#  [7,6,5]
+# ]
+# Output: 6
+# Example 2:
+# Input: 
+# [
+#  [1,2,3],
+#  [0,0,0],
+#  [7,6,5]
+# ]
+# Output: -1
+# Example 3:
+# Input: 
+# [
+#  [2,3,4],
+#  [0,0,5],
+#  [8,7,6]
+# ]
+# Output: 6
+# Explanation: You started from the point (0,0) and you can cut off the tree
+# in (0,0) directly without walking.
+# Hint: size of the given matrix will not exceed 50x50.
+
 class Solution(object):
     def cutOffTree_TLE(self, forest):
         """
