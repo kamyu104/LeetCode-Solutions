8bc83df74073d8a677f27d74b03d9ec8120a04ae
kamyu <kamyu104@users.noreply.github.com>
2017-09-11 21:12:01 +0800

    Update number-of-longest-increasing-subsequence.py

--- Python/number-of-longest-increasing-subsequence.py
+++ Python/number-of-longest-increasing-subsequence.py
@@ -1,6 +1,20 @@
 # Time:  O(n^2)
 # Space: O(n)
 
+# Given an unsorted array of integers, find the number of longest increasing subsequence.
+#
+# Example 1:
+# Input: [1,3,5,4,7]
+# Output: 2
+# Explanation: The two longest increasing subsequence are [1, 3, 4, 7] and [1, 3, 5, 7].
+# Example 2:
+# Input: [2,2,2,2,2]
+# Output: 5
+# Explanation: The length of longest continuous increasing subsequence is 1, and there are
+# 5 subsequences' length is 1, so output 5.
+# Note: Length of the given array will be not exceed 2000 and the answer is guaranteed
+# to be fit in 32-bit signed int.
+    
 class Solution(object):
     def findNumberOfLIS(self, nums):
         """
