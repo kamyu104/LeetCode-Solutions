54cfd3e01915c166cb3ab5b5d31f11906d8b6cc1
kamyu <kamyu104@users.noreply.github.com>
2017-09-14 01:16:04 +0800

    Update cut-off-trees-for-golf-event.py

--- Python/cut-off-trees-for-golf-event.py
+++ Python/cut-off-trees-for-golf-event.py
@@ -54,7 +54,6 @@ class Solution(object):
         :type forest: List[List[int]]
         :rtype: int
         """
-        m, n = len(forest), len(forest[0])
         def minStep(p1, p2):
             min_steps = abs(p1[0]-p2[0])+abs(p1[1]-p2[1])
             stack1, stack2 = [p1], []
@@ -106,52 +105,7 @@ class Solution(object):
 
             return min_steps
 
-        seq = sorted([(forest[i][j], (i, j))
-                      for i in xrange(m) for j in xrange(n)
-                      if forest[i][j]])
-        if seq[0][1] != (0, 0):
-            seq.insert(0, (0, (0, 0)))
-
-        result = 0
-        for i in xrange(len(seq)-1):
-            step = minStep(seq[i][1], seq[i+1][1])
-            if step < 0:
-                return -1
-            result += step
-        return result
-
-
-# Time:  O(t * (logt + m * n)), t is the number of trees
-# Space: O(t + m * n)
-class Solution_TLE(object):
-    def cutOffTree(self, forest):
-        """
-        :type forest: List[List[int]]
-        :rtype: int
-        """
         directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-        
-        def minStep(forest, start, tree, m, n):
-            step = 0
-            lookup = set()
-            q = collections.deque()
-            q.append(start);
-            lookup.add(start[0]*n+start[1])
-            while q:
-                size = len(q)
-                for _ in xrange(size):
-                    curr = q.popleft()
-                    if curr == tree[1]:
-                        return step
-                    for direction in directions:
-                        i, j = curr[0]+direction[0], curr[1]+direction[1]
-                        if not (0 <= i < m and 0 <= j < n and forest[i][j] and (i*n+j) not in lookup):
-                            continue
-                        q.append((i, j))
-                        lookup.add(i*n+j)
-                step += 1
-            return -1
-        
         m, n = len(forest), len(forest[0])
         min_heap = []
         for i in xrange(m):
@@ -163,7 +117,55 @@ class Solution_TLE(object):
         result = 0
         while min_heap:
             tree = heapq.heappop(min_heap)
-            step = minStep(forest, start, tree, m, n)
+            step = minStep(start, tree[1])
+            if step < 0:
+                return -1
+            result += step
+            start = tree[1]
+        return result
+
+# Time:  O(t * (logt + m * n)), t is the number of trees
+# Space: O(t + m * n)
+class Solution_TLE(object):
+    def cutOffTree(self, forest):
+        """
+        :type forest: List[List[int]]
+        :rtype: int
+        """
+        def minStep(p1, p2):
+            step = 0
+            used = set()
+            q = collections.deque()
+            q.append(p1);
+            used.add(start[0]*n+start[1])
+            while q:
+                size = len(q)
+                for _ in xrange(size):
+                    (i, j) = q.popleft()
+                    if (i, j) == p2:
+                        return step
+                    for direction in directions:
+                        ii, jj = i+direction[0], j+direction[1]
+                        if not (0 <= ii < m and 0 <= jj < n and forest[ii][jj] and (ii*n+jj) not in used):
+                            continue
+                        q.append((ii, jj))
+                        used.add(ii*n+jj)
+                step += 1
+            return -1
+        
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
+        m, n = len(forest), len(forest[0])
+        min_heap = []
+        for i in xrange(m):
+            for j in xrange(n):
+                if forest[i][j] > 1:
+                    heapq.heappush(min_heap, (forest[i][j], (i, j)))
+
+        start = (0, 0)
+        result = 0
+        while min_heap:
+            tree = heapq.heappop(min_heap)
+            step = minStep(start, tree[1])
             if step < 0:
                 return -1
             result += step
