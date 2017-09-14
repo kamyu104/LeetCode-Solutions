f03ad59fbbc58967b7be47f020a4a0fd3caa068e
kamyu <kamyu104@users.noreply.github.com>
2017-09-15 02:36:09 +0800

    Update cut-off-trees-for-golf-event.py

--- Python/cut-off-trees-for-golf-event.py
+++ Python/cut-off-trees-for-golf-event.py
@@ -66,10 +66,10 @@ class Solution(object):
             closer, detour = [p1], []
             lookup = set()
             while True:
-                if not closer:  # cannot find a path in the closer expansion
+                if not closer:  # cannot find a path in the closer expansions
                     if not detour:  # no other possible path
                         return -1
-                    # try other possible paths in another detour expansion with extra 2-step cost
+                    # try other possible paths in detour expansions with extra 2-step cost
                     min_steps += 2
                     closer, detour = detour, closer
                 i, j = closer.pop()
