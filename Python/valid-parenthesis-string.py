4126f6ffc76cc62576d84ec31ed5bda41f6d1c5b
kamyu <kamyu104@users.noreply.github.com>
2017-09-17 16:02:27 +0800

    Update valid-parenthesis-string.py

--- Python/valid-parenthesis-string.py
+++ Python/valid-parenthesis-string.py
@@ -9,16 +9,8 @@ class Solution(object):
         """
         lower, upper = 0, 0  # keep lower bound and upper bound of '(' counts
         for c in s:
-            if c == '(':
-                lower += 1
-                upper += 1
-            elif c == ')':
-                if upper == 0:  # '(' count is not enough
-                    return False
-                lower = max(lower-1, 0);
-                upper -= 1
-            else:
-                lower = max(lower-1, 0);
-                upper += 1
+            lower += 1 if c == '(' else -1
+            upper -= 1 if c == ')' else -1
+            if upper < 0: break
+            lower = max(lower, 0)
         return lower == 0  # range of '(' count is valid
-  
