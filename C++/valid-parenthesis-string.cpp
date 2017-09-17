f646a1d92e2828a7c66018deb479b90e12ad7fb9
kamyu <kamyu104@users.noreply.github.com>
2017-09-17 16:04:29 +0800

    Update valid-parenthesis-string.cpp

--- C++/valid-parenthesis-string.cpp
+++ C++/valid-parenthesis-string.cpp
@@ -6,19 +6,10 @@ public:
     bool checkValidString(string s) {
         int lower = 0, upper = 0;  //  keep lower bound and upper bound of '(' counts
         for (const auto& c : s) {
-            if (c == '(') {
-                ++lower;
-                ++upper;
-            } else if (c == ')') {
-                if (upper == 0) {  // '(' count is not enough
-                    return false;
-                }
-                lower = max(lower - 1, 0);
-                --upper;
-            } else {
-                lower = max(lower - 1, 0);
-                ++upper;
-            }
+            lower += (c == '(') ? 1 : -1;
+            upper -= (c == ')') ? 1 : -1;
+            if (upper < 0) break;
+            lower = max(lower, 0);
         }
         return lower == 0;  // range of '(' count is valid
     }
