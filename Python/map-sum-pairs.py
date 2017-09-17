05339f74baf840eb37e6b6b5ca8084496c9691db
kamyu <kamyu104@users.noreply.github.com>
2017-09-17 17:14:42 +0800

    Create map-sum-pairs.py

---
+++ Python/map-sum-pairs.py
@@ -0,0 +1,55 @@
+# Time:  O(n), n is the length of key
+# Space: O(t), t is the total size of trie 
+
+class MapSum(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        _trie = lambda: collections.defaultdict(_trie)
+        self.__root = _trie()
+
+    
+    def insert(self, key, val):
+        """
+        :type key: str
+        :type val: int
+        :rtype: void
+        """
+        # Time: O(n)
+        curr = self.__root
+        for c in key:
+            curr = curr[c]
+        delta = val
+        if "_end" in curr:
+            delta -= curr["_end"]
+        
+        curr = self.__root
+        for c in key:
+            curr = curr[c]
+            if "_count" in curr:
+                curr["_count"] += delta
+            else:
+                curr["_count"] = delta
+        curr["_end"] = val
+
+
+    def sum(self, prefix):
+        """
+        :type prefix: str
+        :rtype: int
+        """
+        # Time: O(n)
+        curr = self.__root
+        for c in prefix:
+            if c not in curr:
+                return 0
+            curr = curr[c]
+        return curr["_count"]
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
