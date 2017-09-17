068b53c0b0af0253c207ad867993a5e415512cda
kamyu <kamyu104@users.noreply.github.com>
2017-09-17 13:52:19 +0800

    Create 24-game.py

---
+++ Python/24-game.py
@@ -0,0 +1,30 @@
+# Time:  O(n^3 * 4^n)
+# Space: O(n^2)
+
+from fractions import Fraction
+from operator import *
+
+class Solution(object):
+    def judgePoint24(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        def dfs(nums):
+            if len(nums) == 1:
+                return nums[0] == 24
+            ops = [add, sub, mul, div]
+            for i in xrange(len(nums)):
+                for j in xrange(len(nums)):
+                    if i == j:
+                        continue
+                    for op in ops:
+                        if op == div and nums[j] == 0:
+                            continue
+                        next_nums = [nums[k] for k in xrange(len(nums)) if k not in [i, j]]
+                        next_nums.append(op(nums[i], nums[j]))
+                        if dfs(next_nums):
+                            return True
+            return False
+
+        return dfs(map(lambda x: Fraction(x, 1), nums))
