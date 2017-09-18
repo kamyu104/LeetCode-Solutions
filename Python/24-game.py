eb0b5d57f84221a2f1b487b8645209cd013f0689
kamyu <kamyu104@users.noreply.github.com>
2017-09-18 21:14:32 +0800

    Update 24-game.py

--- Python/24-game.py
+++ Python/24-game.py
@@ -18,10 +18,39 @@
 # For example, with [1, 1, 1, 1] as input, the expression -1 - 1 - 1 - 1 is not allowed.
 # You cannot concatenate numbers together. For example, if the input is [1, 2, 1, 2], we cannot write this as 12 + 12.
 
-from fractions import Fraction
-from operator import *
+from operator import add, sub, mul, truediv
 
 class Solution(object):
+    def judgePoint24(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) == 1:
+            return abs(nums[0]-24) < 1e-6
+        ops = [add, sub, mul, truediv]
+        for i in xrange(len(nums)):
+            for j in xrange(len(nums)):
+                if i == j:
+                    continue
+                next_nums = [nums[k] for k in xrange(len(nums)) if k not in [i, j]]
+                for op in ops:
+                    if ((op is add or op is mul) and j > i) or \
+                       (op == truediv and nums[j] == 0):
+                            continue
+                    next_nums.append(op(nums[i], nums[j]))
+                    if self.judgePoint24(next_nums):
+                        return True
+                    next_nums.pop()
+        return False
+
+
+# Time:  O(n^3 * 4^n) = O(1), n = 4
+# Space: O(n^2) = O(1)
+from fractions import Fraction
+from operator import add, sub, mul, div
+
+class Solution2(object):
     def judgePoint24(self, nums):
         """
         :type nums: List[int]
