e8ec7dca62f66927f4048ceca9b29c966a5e4c97
kamyu <kamyu104@users.noreply.github.com>
2017-09-18 20:44:52 +0800

    Update map-sum-pairs.py

--- Python/map-sum-pairs.py
+++ Python/map-sum-pairs.py
@@ -1,6 +1,21 @@
 # Time:  O(n), n is the length of key
 # Space: O(t), t is the number of nodes in trie
 
+# Implement a MapSum class with insert, and sum methods.
+#
+# For the method insert, you'll be given a pair of (string, integer).
+# The string represents the key and the integer represents the value.
+# If the key already existed, then the original key-value pair will be overridden to the new one.
+#
+# For the method sum, you'll be given a string representing the prefix,
+# and you need to return the sum of all the pairs' value whose key starts with the prefix.
+#
+# Example 1:
+# Input: insert("apple", 3), Output: Null
+# Input: sum("ap"), Output: 3
+# Input: insert("app", 2), Output: Null
+# Input: sum("ap"), Output: 5
+
 class MapSum(object):
 
     def __init__(self):
