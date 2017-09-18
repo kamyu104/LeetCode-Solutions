1d21caad8cfef171924a3cb98b86179cddea2d7a
kamyu <kamyu104@users.noreply.github.com>
2017-09-18 20:49:35 +0800

    Update valid-parenthesis-string.py

--- Python/valid-parenthesis-string.py
+++ Python/valid-parenthesis-string.py
@@ -1,6 +1,26 @@
 # Time:  O(n)
 # Space: O(1)
 
+# Given a string containing only three types of characters: '(', ')' and '*',
+# write a function to check whether this string is valid. We define the validity of a string by these rules:
+#
+# Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+# Any right parenthesis ')' must have a corresponding left parenthesis '('.
+# Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+# '*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string.
+# An empty string is also valid.
+# Example 1:
+# Input: "()"
+# Output: True
+# Example 2:
+# Input: "(*)"
+# Output: True
+# Example 3:
+# Input: "(*))"
+# Output: True
+# Note:
+# The string size will be in the range [1, 100].
+
 class Solution(object):
     def checkValidString(self, s):
         """
