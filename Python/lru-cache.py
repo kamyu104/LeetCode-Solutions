93f97d432c0e4880d2b22a6a4be6d4a045ec306d
kamyu <kamyu104@users.noreply.github.com>
2017-09-21 20:34:13 +0800

    Update lru-cache.py

--- Python/lru-cache.py
+++ Python/lru-cache.py
@@ -1,13 +1,30 @@
 # Time:  O(1), per operation.
 # Space: O(k), k is the capacity of cache.
 
-# Design and implement a data structure for Least Recently Used (LRU) cache. 
-# It should support the following operations: get and set.
-# 
-# get(key) - Get the value (will always be positive) of the key if the key exists in the cache, otherwise return -1.
-# 
-# set(key, value) - Set or insert the value if the key is not already present. 
-# When the cache reached its capacity, it should invalidate the least recently used item before inserting a new item.
+# Design and implement a data structure for Least Recently Used (LRU) cache.
+# It should support the following operations: get and put.
+#
+# get(key) - Get the value (will always be positive) of the key if the key exists in the cache,
+# otherwise return -1.
+# put(key, value) - Set or insert the value if the key is not already present.
+# When the cache reached its capacity,
+# it should invalidate the least recently used item before inserting a new item.
+#
+# Follow up:
+# Could you do both operations in O(1) time complexity?
+#
+# Example:
+#
+# LRUCache cache = new LRUCache( 2 /* capacity */ );
+#
+# cache.put(1, 1);
+# cache.put(2, 2);
+# cache.get(1);       // returns 1
+# cache.put(3, 3);    // evicts key 2
+# cache.get(2);       // returns -1 (not found)
+# cache.put(4, 4);    // evicts key 1
+# cache.get(1);       // returns -1 (not found)
+# cache.get(3);       // returns 3
 
 class ListNode(object):
     def __init__(self, key, val):
