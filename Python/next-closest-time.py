7bd7d81fab86cea674c986ad7bf17b08a8610a72
kamyu <kamyu104@users.noreply.github.com>
2017-09-24 17:33:11 +0800

    Create next-closest-time.py

---
+++ Python/next-closest-time.py
@@ -0,0 +1,19 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def nextClosestTime(self, time):
+        """
+        :type time: str
+        :rtype: str
+        """
+        h, m = time.split(":")
+        curr = int(h) * 60 + int(m)
+        result = None
+        for i in xrange(curr+1, curr+1441):
+            t = i % 1440
+            h, m = t // 60, t % 60
+            result = "%02d:%02d" % (h, m)
+            if set(result) <= set(time):
+                break
+        return result
