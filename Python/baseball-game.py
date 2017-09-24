2898ce7a26da700acd2a65ece0f1c37873cb27d4
kamyu <kamyu104@users.noreply.github.com>
2017-09-24 17:43:41 +0800

    Create baseball-game.py

---
+++ Python/baseball-game.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def calPoints(self, ops):
+        """
+        :type ops: List[str]
+        :rtype: int
+        """
+        history = []
+        for op in ops:
+            if op == '+':
+                history.append(history[-1] + history[-2])
+            elif op == 'D':
+                history.append(history[-1] * 2)
+            elif op == 'C':
+                history.pop()
+            else:
+                history.append(int(op))
+        return sum(history)
