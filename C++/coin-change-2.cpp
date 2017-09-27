befce34a30d386d98656915250c35e0ca681ef6b
kamyu <kamyu104@users.noreply.github.com>
2017-09-28 01:04:03 +0800

    Create coin-change-2.cpp

---
+++ C++/coin-change-2.cpp
@@ -0,0 +1,16 @@
+// Time:  O(n * m)
+// Space: O(m)
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (const auto& coin : coins) {
+            for(int i = coin; i <= amount; ++i) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
