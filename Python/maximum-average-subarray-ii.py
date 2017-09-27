d66ab15184f2624844e93e9d9851b1c779faaf15
kamyu <kamyu104@users.noreply.github.com>
2017-09-27 23:23:35 +0800

    Update maximum-average-subarray-ii.py

--- Python/maximum-average-subarray-ii.py
+++ Python/maximum-average-subarray-ii.py
@@ -1,22 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
 
-# Given an array consisting of n integers, find the contiguous subarray
-# whose length is greater than or equal to k that has the maximum average value.
-# And you need to output the maximum average value.
-#
-# Example 1:
-# Input: [1,12,-5,-6,50,3], k = 4
-# Output: 12.75
-# Explanation:
-# when length is 5, maximum average value is 10.8,
-# when length is 6, maximum average value is 9.16667.
-# Thus return 12.75.
-# Note:
-# 1 <= k <= n <= 10,000.
-# Elements of the given array will be in range [-10,000, 10,000].
-# The answer with the calculation error less than 1e-5 will be accepted.
-
 class Solution(object):
     def findMaxAverage(self, nums, k):
         """
