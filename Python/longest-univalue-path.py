b5331c7fc413241a4cdaed4a7458fb26eea516c5
kamyu <kamyu104@users.noreply.github.com>
2017-10-01 19:56:39 +0800

    Update longest-univalue-path.py

--- Python/longest-univalue-path.py
+++ Python/longest-univalue-path.py
@@ -1,6 +1,35 @@
 # Time:  O(n)
 # Space: O(h)
 
+# Given a binary tree, find the length of the longest path
+# where each node in the path has the same value. This path may or may not pass through the root.
+#
+# Note: The length of path between two nodes is represented by the number of edges between them.
+#
+# Example 1:
+# Input:
+#
+#               5
+#              / \
+#             4   5
+#            / \   \
+#           1   1   5
+# Output:
+# 2
+#
+# Example 2:
+# Input:
+#
+#               1
+#              / \
+#             4   5
+#            / \   \
+#           4   4   5
+# Output:
+# 2
+#
+# Note: The given binary tree has not more than 10000 nodes. The height of the tree is not more than 1000.
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, x):
