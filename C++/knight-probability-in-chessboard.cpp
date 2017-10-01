dea3c3232fd03b5e7f275de5c76309cf4e04a19d
kamyu <kamyu104@users.noreply.github.com>
2017-10-01 18:17:48 +0800

    Update knight-probability-in-chessboard.cpp

--- C++/knight-probability-in-chessboard.cpp
+++ C++/knight-probability-in-chessboard.cpp
@@ -9,8 +9,8 @@ public:
              {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1}};
         vector<vector<vector<double>>> dp(2, vector<vector<double>>(N, vector<double>(N, 1.0l)));
         for (int step = 1; step <= K; ++step) {
-            for(int i = 0; i < N; ++i) {
-                for(int j = 0; j < N; ++j) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < N; ++j) {
                     dp[step % 2][i][j] = 0;
                     for (const auto& direction : directions) {
                         auto r = i + direction.first;
