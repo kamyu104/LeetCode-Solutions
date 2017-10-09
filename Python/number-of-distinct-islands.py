b741ca282fce318e32cc6435de0a080df0bcec31
kamyu <kamyu104@users.noreply.github.com>
2017-10-09 19:30:51 +0800

    Create number-of-distinct-islands.py

---
+++ Python/number-of-distinct-islands.py
@@ -0,0 +1,30 @@
+# Time:  O(m * n)
+# Space: O(m * n)
+
+class Solution(object):
+    def numDistinctIslands(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        directions = {'l':[-1,  0], 'r':[ 1,  0], \
+                      'u':[ 0,  1], 'd':[ 0, -1]}
+
+        def dfs(i, j, grid, island):
+            if not (0 <= i < len(grid) and \
+                    0 <= j < len(grid[0]) and \
+                    grid[i][j] > 0):
+                return False
+            grid[i][j] *= -1
+            for k, v in directions.iteritems():
+                island.append(k);
+                dfs(i+v[0], j+v[1], grid, island)
+            return True
+
+        islands = set()
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                island = []
+                if dfs(i, j, grid, island):
+                    islands.add("".join(island))
+        return len(islands)
