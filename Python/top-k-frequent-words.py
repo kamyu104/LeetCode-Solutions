d96a0de413464f1e9ec7f21dd63c8d6dc2be2c8f
kamyu <kamyu104@users.noreply.github.com>
2017-10-13 13:42:37 +0800

    Update top-k-frequent-words.py

--- Python/top-k-frequent-words.py
+++ Python/top-k-frequent-words.py
@@ -72,3 +72,33 @@ class Solution(object):
                 right = new_pivot_idx - 1
             else:  # new_pivot_idx < k - 1.
                 left = new_pivot_idx + 1
+
+                
+# Time:  O(nlogk)
+# Space: O(n)
+# Heap Solution
+class Solution2(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        class MinHeapObj(object):
+            def __init__(self,val): self.val = val
+            def __lt__(self,other): return self.val[1] > other.val[1] if self.val[0] == other.val[0] else self.val < other.val
+            def __eq__(self,other): return self.val == other.val
+            def __str__(self): return str(self.val)
+
+        counts = collections.defaultdict(int)
+        for i in words:
+            counts[i] += 1
+        min_heap = []
+        for word, count in counts.iteritems():
+            heapq.heappush(min_heap, MinHeapObj((count, word)))
+            if len(min_heap) == k+1:
+                heapq.heappop(min_heap)
+        result = []
+        while min_heap:
+            result.append(heapq.heappop(min_heap).val[1])
+        return result[::-1]
