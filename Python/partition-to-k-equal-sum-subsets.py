0ffa5a3cec70288f8b6e68075f42576c664ca11c
kamyu <kamyu104@users.noreply.github.com>
2017-10-15 09:10:52 -0500

    Update partition-to-k-equal-sum-subsets.py

--- Python/partition-to-k-equal-sum-subsets.py
+++ Python/partition-to-k-equal-sum-subsets.py
@@ -1,7 +1,46 @@
+# Time:  O(n*2^n)
+# Space: O(2^n)
+
+# Given an array of integers nums and a positive integer k,
+# find whether it's possible to divide this array into k non-empty subsets whose sums are all equal.
+#
+# Example 1:
+# Input: nums = [4, 3, 2, 3, 5, 2, 1], k = 4
+# Output: True
+# Explanation: It's possible to divide it into 4 subsets (5), (1, 4), (2,3), (2,3) with equal sums.
+# Note:
+#
+# 1 <= k <= len(nums) <= 16.
+# 0 < nums[i] < 10000.
+
+# Memoization solution.
+class Solution(object):
+    def canPartitionKSubsets(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        def dfs(nums, target, used, todo, lookup):
+            if lookup[used] is None:
+                targ = (todo-1)%target + 1
+                lookup[used] = any(dfs(nums, target, used | (1<<i), todo-num, lookup) \
+                                   for i, num in enumerate(nums) \
+                                   if ((used>>i) & 1) == 0 and num <= targ)
+            return lookup[used]
+        
+        total = sum(nums)
+        if total%k or max(nums) > total//k:
+            return False
+        lookup = [None] * (1 << len(nums))
+        lookup[-1] = True
+        return dfs(nums, total//k, 0, total, lookup)
+    
+    
 # Time:  O(k^(n-k) * k!)
 # Space: O(n)
-
-class Solution(object):
+# DFS solution with pruning.
+class Solution2(object):
     def canPartitionKSubsets(self, nums, k):
         """
         :type nums: List[int]
@@ -22,7 +61,7 @@ class Solution(object):
             return False
 
         total = sum(nums)
-        if total%k != 0:
+        if total%k != 0 or max(nums) > total//k:
             return False
         nums.sort(reverse=True)
         subset_sums = [0] * k
