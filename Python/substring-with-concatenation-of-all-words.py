dbb06317571dc750879c084adad3a29a3b155724
kamyu <kamyu104@users.noreply.github.com>
2017-10-26 10:17:31 -0500

    Update substring-with-concatenation-of-all-words.py

--- Python/substring-with-concatenation-of-all-words.py
+++ Python/substring-with-concatenation-of-all-words.py
@@ -1,18 +1,19 @@
 # Time:  O((m + n) * k), where m is string length, n is dictionary size, k is word length
 # Space: O(n * k)
 
-# You are given a string, S, and a list of words, L, that are all of the same length. 
-# Find all starting indices of substring(s) in S that is a concatenation of each word 
-# in L exactly once and without any intervening characters.
-# 
+# You are given a string, s, and a list of words, words,
+# that are all of the same length. Find all starting indices of substring(s)
+# in s that is a concatenation of each word in words exactly once and
+# without any intervening characters.
+#
 # For example, given:
-# S: "barfoothefoobarman"
-# L: ["foo", "bar"]
-# 
+# s: "barfoothefoobarman"
+# words: ["foo", "bar"]
+#
 # You should return the indices: [0,9].
 # (order does not matter).
-#
 
+# Sliding window solution
 class Solution(object):
     def findSubstring(self, s, words):
         """
