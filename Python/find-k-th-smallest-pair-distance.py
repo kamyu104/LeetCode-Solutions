3e87ffb66967056bfe1aab7c918fffdc0a94c650
kamyu <kamyu104@users.noreply.github.com>
2017-10-29 11:00:47 -0500

    Update find-k-th-smallest-pair-distance.py

--- Python/find-k-th-smallest-pair-distance.py
+++ Python/find-k-th-smallest-pair-distance.py
@@ -30,7 +30,7 @@ class Solution(object):
         :rtype: int
         """
         # Sliding window solution
-        def possible(guess, nums):
+        def possible(guess, nums, k):
             count, left = 0, 0
             for right, num in enumerate(nums):
                 while num-nums[left] > guess:
@@ -42,7 +42,7 @@ class Solution(object):
         left, right = 0, nums[-1]-nums[0]+1
         while left < right:
             mid = left + (right-left)/2
-            if possible(mid, nums):
+            if possible(mid, nums, k):
                 right = mid
             else:
                 left = mid+1
