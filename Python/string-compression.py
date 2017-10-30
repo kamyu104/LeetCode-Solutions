636d73d4d7d6661392ad7564f1fc7a6eee3c6abe
kamyu <kamyu104@users.noreply.github.com>
2017-10-30 09:29:11 -0500

    Update string-compression.py

--- Python/string-compression.py
+++ Python/string-compression.py
@@ -1,32 +1,67 @@
 # Time:  O(n)
 # Space: O(1)
 
+# Given an array of characters, compress it in-place.
+# The length after compression must always be smaller than or equal to the original array.
+# Every element of the array should be a character (not int) of length 1.
+# After you are done modifying the input array in-place, return the new length of the array.
+#
+# Follow up:
+# Could you solve it using only O(1) extra space?
+#
+# Example 1:
+# Input:
+# ["a","a","b","b","c","c","c"]
+#
+# Output:
+# Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+#
+# Explanation:
+# "aa" is replaced by "a2". "bb" is replaced by "b2". "ccc" is replaced by "c3".
+# Example 2:
+# Input:
+# ["a"]
+#
+# Output:
+# Return 1, and the first 1 characters of the input array should be: ["a"]
+#
+# Explanation:
+# Nothing is replaced.
+# Example 3:
+# Input:
+# ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+#
+# Output:
+# Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+#
+# Explanation:
+# Since the character "a" does not repeat, it is not compressed. "bbbbbbbbbbbb" is replaced by "b12".
+# Notice each digit has it's own entry in the array.
+# Note:
+# All characters have an ASCII value in [35, 126].
+# 1 <= len(chars) <= 1000.
+
 class Solution(object):
     def compress(self, chars):
         """
         :type chars: List[str]
         :rtype: int
         """
-        i, j = 0, 0
-        while j < len(chars):
-            if j+1 == len(chars) or chars[j] != chars[j+1]:
-                chars[i] = chars[j]
-                i += 1
-                j += 1
-            else:
-                k = j
-                while j < len(chars) and chars[j] == chars[k]:
-                    j += 1
-                chars[i] = chars[k]
-                i += 1
-                n, left = j-k, i
-                while n > 0:
-                    chars[i] = chr(n%10+ord('0'))
-                    n /= 10
-                    i += 1
-                right = i-1
-                while left < right:
-                    chars[left], chars[right] = chars[right], chars[left]
-                    left += 1
-                    right -= 1
-        return i
+        anchor, write = 0, 0
+        for read, c in enumerate(chars):
+            if read+1 == len(chars) or chars[read+1] != c:
+                chars[write] = chars[anchor]
+                write += 1
+                if read > anchor:
+                    n, left = read-anchor+1, write
+                    while n > 0:
+                        chars[write] = chr(n%10+ord('0'))
+                        write += 1
+                        n /= 10
+                    right = write-1
+                    while left < right:
+                        chars[left], chars[right] = chars[right], chars[left]
+                        left += 1
+                        right -= 1
+                anchor = read+1
+        return write
