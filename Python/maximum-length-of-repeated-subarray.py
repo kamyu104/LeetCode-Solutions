494624c7f1c3af6c93d6873f575e0b4f1b64df01
kamyu <kamyu104@users.noreply.github.com>
2017-10-30 10:21:10 -0500

    Update maximum-length-of-repeated-subarray.py

--- Python/maximum-length-of-repeated-subarray.py
+++ Python/maximum-length-of-repeated-subarray.py
@@ -1,4 +1,4 @@
-# Time:  O(m * n)
+# Time:  O(m * n * logn(min(m, n))
 # Space: O(min(m, n))
 
 # Given two integer arrays A and B,
@@ -15,7 +15,54 @@
 # 1 <= len(A), len(B) <= 1000
 # 0 <= A[i], B[i] < 100
 
+# Binary search + rolling hash solution (226 ms)
 class Solution(object):
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        if len(A) > len(B): return findLength(B, A)
+        M, p = 10**9+7, 113
+        p_inv = pow(p, M-2, M)
+        def check(guess):
+            def rolling_hashes(source, length):
+                if length == 0:
+                    yield 0, 0
+                    return
+
+                val, power = 0, 1
+                for i, x in enumerate(source):
+                    val = (val + x*power) % M
+                    if i < length - 1:
+                        power = (power*p) % M
+                    else:
+                        yield val, i-(length-1)
+                        val = (val-source[i-(length-1)])*p_inv % M
+
+            hashes = collections.defaultdict(list)
+            for hash_val, i in rolling_hashes(A, guess):
+                hashes[hash_val].append(i)
+            for hash_val, j in rolling_hashes(B, guess):
+                if any(A[i:i+guess] == B[j:j+guess] for i in hashes[hash_val]):
+                    return True
+            return False
+
+        left, right = 0, min(len(A), len(B)) + 1
+        while left < right:
+            mid = left + (right-left)/2
+            if not check(mid):  # find the min idx such that check(idx) == false
+                right = mid
+            else:
+                left = mid+1
+        return left-1
+
+    
+# Time:  O(m * n)
+# Space: O(min(m, n))
+# dp solution (3752 ms)
+class Solution2(object):
     def findLength(self, A, B):
         """
         :type A: List[int]
