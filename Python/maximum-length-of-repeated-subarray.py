5a29c3560fa2978ac6c26b38eef4298befeab08c
kamyu <kamyu104@users.noreply.github.com>
2017-10-30 10:29:27 -0500

    Update maximum-length-of-repeated-subarray.py

--- Python/maximum-length-of-repeated-subarray.py
+++ Python/maximum-length-of-repeated-subarray.py
@@ -1,4 +1,4 @@
-# Time:  O(m * n * logn(min(m, n))
+# Time:  O(m * n)
 # Space: O(min(m, n))
 
 # Given two integer arrays A and B,
@@ -15,8 +15,31 @@
 # 1 <= len(A), len(B) <= 1000
 # 0 <= A[i], B[i] < 100
 
-# Binary search + rolling hash solution (226 ms)
+# dp solution (3752 ms)
 class Solution(object):
+    def findLength(self, A, B):
+        """
+        :type A: List[int]
+        :type B: List[int]
+        :rtype: int
+        """
+        if len(A) > len(B): return findLength(B, A)
+        result = 0
+        dp = [[0] * (len(B)+1) for _ in xrange(2)]
+        for i in xrange(len(A)):
+            for j in xrange(len(B)):
+                if A[i] == B[j]:
+                    dp[(i+1)%2][j+1] = dp[i%2][j]+1
+                else:
+                    dp[(i+1)%2][j+1] = 0
+            result = max(result, max(dp[(i+1)%2]))
+        return result
+
+    
+# Time:  O(m * n * log(min(m, n)))
+# Space: O(min(m, n))  
+# Binary search + rolling hash solution (226 ms)
+class Solution2(object):
     def findLength(self, A, B):
         """
         :type A: List[int]
@@ -58,11 +81,11 @@ class Solution(object):
                 left = mid+1
         return left-1
 
-    
-# Time:  O(m * n)
-# Space: O(min(m, n))
-# dp solution (3752 ms)
-class Solution2(object):
+
+# Time:  O(m * n * min(m, n) * log(min(m, n)))
+# Space: O(min(m^2, n^2))
+# Binary search (122 ms)
+class Solution3(object):
     def findLength(self, A, B):
         """
         :type A: List[int]
@@ -70,13 +93,20 @@ class Solution2(object):
         :rtype: int
         """
         if len(A) > len(B): return findLength(B, A)
-        result = 0
-        dp = [[0] * (len(B)+1) for _ in xrange(2)]
-        for i in xrange(len(A)):
-            for j in xrange(len(B)):
-                if A[i] == B[j]:
-                    dp[(i+1)%2][j+1] = dp[i%2][j]+1
-                else:
-                    dp[(i+1)%2][j+1] = 0
-            result = max(result, max(dp[(i+1)%2]))
-        return result
+        
+        def check(length):
+            lookup = set(A[i:i+length] \
+                       for i in xrange(len(A)-length+1))
+            return any(B[j:j+length] in lookup \
+                       for j in xrange(len(B)-length+1))
+
+        A = ''.join(map(chr, A))
+        B = ''.join(map(chr, B))
+        left, right = 0, min(len(A), len(B)) + 1
+        while left < right:
+            mid = left + (right-left)/2
+            if not check(mid):  # find the min idx such that check(idx) == false
+                right = mid
+            else:
+                left = mid+1
+        return left-1
