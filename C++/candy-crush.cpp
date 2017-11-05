b18a2f4070e637fe106fe04d29ec5dd2a0138a3a
kamyu <kamyu104@users.noreply.github.com>
2017-11-05 07:31:30 -0600

    Create candy-crush.cpp

---
+++ C++/candy-crush.cpp
@@ -0,0 +1,43 @@
+// Time:  O((R * C)^2)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> candyCrush(vector<vector<int>>& board) {
+        const auto R = board.size(), C = board[0].size();
+        bool changed = true;
+        while (changed) {
+            changed = false;
+            for (int r = 0; r < R; ++r) {
+                for (int c = 0; c + 2 < C; ++c) {
+                    auto v = abs(board[r][c]);
+                    if (v != 0 && v == abs(board[r][c + 1]) && v == abs(board[r][c + 2])) {
+                        board[r][c] = board[r][c + 1] = board[r][c + 2] = -v;
+                        changed = true;
+                    }
+                }
+            }
+            for (int r = 0; r + 2 < R; ++r) {
+                for (int c = 0; c < C; ++c) {
+                    auto v = abs(board[r][c]);
+                    if (v != 0 && v == abs(board[r + 1][c]) && v == abs(board[r + 2][c])) {
+                        board[r][c] = board[r + 1][c] = board[r + 2][c] = -v;
+                        changed = true;
+                    }
+                }
+            }
+            for (int c = 0; c < C; ++c) {
+                int i = R - 1;
+                for (int r = R - 1; r >= 0; --r) {
+                    if (board[r][c] > 0) {
+                        board[i--][c] = board[r][c];
+                    }
+                }
+                while (i >= 0) {
+                    board[i--][c] = 0;
+                }
+            }
+        }
+        return board;
+    }
+};
