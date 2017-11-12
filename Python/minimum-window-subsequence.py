8ab88972ad8071aede3d143d417d97b9580a967c
kamyu <kamyu104@users.noreply.github.com>
2017-11-12 09:59:15 -0600

    Update minimum-window-subsequence.py

--- Python/minimum-window-subsequence.py
+++ Python/minimum-window-subsequence.py
@@ -9,18 +9,18 @@ class Solution(object):
         :rtype: str
         """
         dp = [[None for _ in xrange(len(S))] for _ in xrange(2)]
-        for i, c in enumerate(S):
+        for j, c in enumerate(S):
             if c == T[0]:
-                dp[0][i] = i
+                dp[0][j] = j
                 
-        for j in xrange(1, len(T)):
+        for i in xrange(1, len(T)):
             prev = None
-            dp[j%2] = [None] * len(S)
-            for i, c in enumerate(S):
-                if prev is not None and c == T[j]:
-                    dp[j%2][i] = prev
-                if dp[(j-1)%2][i] is not None:
-                    prev = dp[(j-1)%2][i]
+            dp[i%2] = [None] * len(S)
+            for j, c in enumerate(S):
+                if prev is not None and c == T[i]:
+                    dp[i%2][j] = prev
+                if dp[(i-1)%2][j] is not None:
+                    prev = dp[(i-1)%2][j]
 
         start, end = 0, len(S)
         for j, i in enumerate(dp[(len(T)-1)%2]):
