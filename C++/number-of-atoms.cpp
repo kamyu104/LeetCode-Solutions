ff5835b85719753889865a1ce8bd7a0b2989408d
kamyu <kamyu104@users.noreply.github.com>
2017-11-12 10:46:42 -0600

    Create number-of-atoms.cpp

---
+++ C++/number-of-atoms.cpp
@@ -0,0 +1,40 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    string countOfAtoms(string formula) {
+        stack<map<string, int>> stk;
+        stk.emplace();
+        int submatches[] = { 1, 2, 3, 4, 5 };
+        auto e = regex("([A-Z][a-z]*)(\\d*)|(\\()|(\\))(\\d*)");
+        for (regex_token_iterator<string::iterator> it(formula.begin(), formula.end(), e, submatches), end;
+             it != end;) {
+            auto name = (it++)->str();
+            auto m1 = (it++)->str();
+            auto left_open = (it++)->str();
+            auto right_open = (it++)->str();
+            auto m2 = (it++)->str();
+            if (!name.empty()) {
+                stk.top()[name] += stoi(!m1.empty() ? m1 : "1");
+            }
+            if (!left_open.empty()) {
+                stk.emplace();
+            }
+            if (!right_open.empty()) {
+                const auto top = move(stk.top()); stk.pop();
+                for (const auto& kvp: top) {
+                    stk.top()[kvp.first] += kvp.second * stoi(!m2.empty() ? m2 : "1");
+                }
+            }
+        }
+        string result;
+        for (const auto& kvp : stk.top()) {
+            result += kvp.first;
+            if (kvp.second > 1) {
+                result += to_string(kvp.second);
+            }
+        }
+        return result;
+    }
+};
