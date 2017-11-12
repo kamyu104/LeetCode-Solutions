aa72fa68968cbc6a4d05856537c49fa54cae1368
kamyu <kamyu104@users.noreply.github.com>
2017-11-12 11:20:33 -0600

    Create split-linked-list-in-parts.cpp

---
+++ C++/split-linked-list-in-parts.cpp
@@ -0,0 +1,41 @@
+// Time:  O(n)
+// Space: O(1)
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<ListNode*> splitListToParts(ListNode* root, int k) {
+        auto n = 0;
+        auto curr = root;
+        while (curr) {
+            curr = curr->next;
+            n += 1;
+        }
+        auto width = n / k;
+        auto remainder = n % k;
+
+        vector<ListNode *> result;
+        curr = root;
+        for (int i = 0; i < k; ++i) {
+            auto head = curr;
+            for (int j = 0; curr &&
+                 j < width - 1 + static_cast<int>(i < remainder); ++j) {
+                curr = curr->next;
+            }
+            if (curr) {
+                auto tail = curr;
+                curr = curr->next;
+                tail->next = nullptr;
+            }
+            result.emplace_back(head);
+        }
+        return result;
+    }
+};
