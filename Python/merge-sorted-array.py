bd0ec7d707d2891b03aa0efe3c9d423fc519d5c1
KEQI HUANG <keqih@smu.edu>
2017-11-14 17:44:34 -0600

    Update merge-sorted-array.py
    add another solution of merge-sorted-array.py

--- Python/merge-sorted-array.py
+++ Python/merge-sorted-array.py
@@ -34,3 +34,29 @@ if __name__ == "__main__":
     B = [2, 4, 6, 7]
     Solution().merge(A, 3, B, 4)
     print A
+    
+    
+# you may get a input like this,
+# nums1 : [0]
+# m : 0
+# nums2 : [1]
+# n : 1
+# so you need to judge if n is still large than 0
+class Solution2:
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        while m > 0 and n > 0:
+            if nums1[m-1] > nums2[n-1]:
+                nums1[m+n-1] = nums1[m-1]
+                m -= 1
+            else:
+                nums1[m+n-1] = nums2[n-1]
+                n -= 1
+        if n > 0:
+            nums1[:n] = nums2[:n]
