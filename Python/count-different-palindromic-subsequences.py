da9c7d9144157eef7740989c31f5fc59ce4d5b7e
kamyu <kamyu104@users.noreply.github.com>
2017-11-20 09:47:52 -0600

    Update count-different-palindromic-subsequences.py

--- Python/count-different-palindromic-subsequences.py
+++ Python/count-different-palindromic-subsequences.py
@@ -1,6 +1,34 @@
 # Time:  O(n^2)
 # Space: O(n^2)
 
+# Given a string S, find the number of different non-empty palindromic subsequences in S,
+# and return that number modulo 10^9 + 7.
+#
+# A subsequence of a string S is obtained by deleting 0 or more characters from S.
+#
+# A sequence is palindromic if it is equal to the sequence reversed.
+#
+# Two sequences A_1, A_2, ... and B_1, B_2, ... are different if there is some i for which A_i != B_i.
+#
+# Example 1:
+# Input: 
+# S = 'bccb'
+# Output: 6
+# Explanation: 
+# The 6 different non-empty palindromic subsequences are 'b', 'c', 'bb', 'cc', 'bcb', 'bccb'.
+# Note that 'bcb' is counted only once, even though it occurs twice.
+#
+# Example 2:
+# Input: 
+# S = 'abcdabcdabcdabcdabcdabcdabcdabcddcbadcbadcbadcbadcbadcbadcbadcba'
+# Output: 104860361
+#   
+# Explanation: 
+# There are 3104860382 different non-empty palindromic subsequences, which is 104860361 modulo 10^9 + 7.
+# Note:
+# - The length of S will be in the range [1, 1000].
+# - Each character S[i] will be in the set {'a', 'b', 'c', 'd'}.
+
 class Solution(object):
     def countPalindromicSubsequences(self, S):
         """
