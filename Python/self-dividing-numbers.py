155da7de046f3363023c7547c7abd443b4a7e0a0
kamyu <kamyu104@users.noreply.github.com>
2017-11-20 09:44:49 -0600

    Update self-dividing-numbers.py

--- Python/self-dividing-numbers.py
+++ Python/self-dividing-numbers.py
@@ -1,6 +1,24 @@
 # Time:  O(nlogn)
 # Space: O(1)
 
+# A self-dividing number is a number that is divisible by every digit it contains.
+#
+# For example, 128 is a self-dividing number because 128 % 1 == 0,
+# 128 % 2 == 0, and 128 % 8 == 0.
+#
+# Also, a self-dividing number is not allowed to contain the digit zero.
+#
+# Given a lower and upper number bound, output a list of every possible self dividing number,
+# including the bounds if possible.
+#
+# Example 1:
+# Input: 
+# left = 1, right = 22
+# Output: [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+#
+# Note:
+# - The boundaries of each input argument are 1 <= left <= right <= 10000.
+
 class Solution(object):
     def selfDividingNumbers(self, left, right):
         """
