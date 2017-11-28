618ad8fe9ccbe54912fcac3ed0001a3d61788a7f
kamyu <kamyu104@users.noreply.github.com>
2017-11-28 08:28:29 -0600

    Update minimum-factorization.py

--- Python/minimum-factorization.py
+++ Python/minimum-factorization.py
@@ -1,24 +1,6 @@
 # Time:  O(loga)
 # Space: O(1)
 
-# Given a positive integer a,
-# find the smallest positive integer b whose multiplication of each digit equals to a.
-#
-# If there is no answer or the answer is not fit in 32-bit signed integer, then return 0.
-#
-# Example 1
-# Input:
-#
-# 48 
-# Output:
-# 68
-# Example 2
-# Input:
-#
-# 15
-# Output:
-# 35
-
 class Solution(object):
     def smallestFactorization(self, a):
         """
