5ce997feb659d7f4985557ba72c8b16badf4e492
kamyu <kamyu104@users.noreply.github.com>
2017-12-03 09:52:34 -0600

    Update delete-and-earn.cpp

--- C++/delete-and-earn.cpp
+++ C++/delete-and-earn.cpp
@@ -4,15 +4,15 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        vector<int> values(10001);
+        vector<int> vals(10001);
         for (const auto& num : nums) {
-            values[num] += num;
+            vals[num] += num;
         }
-        int val_i = values[0], val_i_1 = 0, val_i_2 = 0;
-        for (int i = 1; i < values.size(); ++i) {
+        int val_i = vals[0], val_i_1 = 0, val_i_2 = 0;
+        for (int i = 1; i < vals.size(); ++i) {
             val_i_2 = val_i_1;
             val_i_1 = val_i;
-            val_i = max(values[i] + val_i_2, val_i_1);
+            val_i = max(vals[i] + val_i_2, val_i_1);
         }
         return val_i;
     }
