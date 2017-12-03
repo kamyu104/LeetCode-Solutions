d502c3cbc6e1aa8d99354f5332470c1a1cccd4ae
kamyu <kamyu104@users.noreply.github.com>
2017-12-03 09:15:01 -0600

    Create daily-temperatures.py

---
+++ Python/daily-temperatures.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        result = [0] * len(temperatures)
+        stk = []
+        for i in xrange(len(temperatures)):
+            while stk and \
+                  temperatures[stk[-1]] < temperatures[i]:
+                idx = stk.pop()
+                result[idx] = i-idx
+            stk.append(i)
+        return result 
