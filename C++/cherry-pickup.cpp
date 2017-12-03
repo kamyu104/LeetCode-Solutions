7f52dd8bfdeb546f19af7a2d2a039e6e836d46e6
kamyu <kamyu104@users.noreply.github.com>
2017-12-03 11:08:10 -0600

    Create cherry-pickup.cpp

---
+++ C++/cherry-pickup.cpp
@@ -0,0 +1,43 @@
+// Time:  O(n^3)
+// Space: O(n^2)
+
+class Solution {
+public:
+    int cherryPickup(vector<vector<int>>& grid) {
+        // dp holds the max # of cherries two k-length paths can pickup.
+        // The two k-length paths arrive at (i, k - i) and (j, k - j), 
+        // respectively.
+        const int n = grid.size();
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        dp[0][0] = grid[0][0];
+
+        const int max_len = 2 * (n - 1);
+        for (int k = 1; k <= max_len; ++k) {
+            for (int i = min(k + 1, n) - 1; i >= 0; --i) {
+                for (int j = min(k + 1, n) - 1; j >= i; --j) {
+                    if (k - i < 0 || k - i >= n ||
+                        k - j < 0 || k - j >= n) {
+                        continue;
+                    }
+                    if (grid[i][k - i] == -1 ||
+                        grid[j][k - j] == -1) {
+                        dp[i][j] = -1;
+                        continue;
+                    }
+                    int cnt = grid[i][k - i] + ((i == j) ? 0 : grid[j][k - j]);
+                    int max_cnt = -1;
+                    static const vector<pair<int, int>> directions{{0, 0}, {-1, 0}, {0, -1}, {-1, -1}};
+                    for (const auto& direction : directions) {
+                        const auto ii = i + direction.first;
+                        const auto jj = j + direction.second;
+                        if (ii >= 0 && jj >= 0 && dp[ii][jj] >= 0) {
+                            max_cnt = max(max_cnt, dp[ii][jj] + cnt);
+                        }
+                    }
+                    dp[i][j] = max_cnt;
+                }
+            }
+        }
+        return max(dp[n - 1][n - 1], 0);
+    }
+};
