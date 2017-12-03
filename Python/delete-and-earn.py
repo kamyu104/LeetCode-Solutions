e31e2d4482bdad26fcb8f1d2114fb04945d639de
kamyu <kamyu104@users.noreply.github.com>
2017-12-03 09:53:02 -0600

    Create delete-and-earn.py

---
+++ Python/delete-and-earn.py
@@ -0,0 +1,17 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def deleteAndEarn(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        vals = [0] * 10001
+        for num in nums:
+            vals[num] += num
+        val_i, val_i_1 = vals[0], 0
+        for i in xrange(1, len(vals)):
+            val_i_1, val_i_2 = val_i, val_i_1
+            val_i = max(vals[i] + val_i_2, val_i_1)
+        return val_i
