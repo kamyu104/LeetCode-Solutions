0c8f4edd396f101121c1e3a8126ea1627a6f7b73
kamyu <kamyu104@users.noreply.github.com>
2017-12-04 09:23:24 -0600

    Update monotone-increasing-digits.py

--- Python/monotone-increasing-digits.py
+++ Python/monotone-increasing-digits.py
@@ -1,6 +1,26 @@
 # Time:  O(logn) = O(1)
 # Space: O(logn) = O(1)
 
+# Given a non-negative integer N,
+# find the largest number that is less than or equal to N with monotone increasing digits.
+#
+# (Recall that an integer has monotone increasing digits if and only
+# if each pair of adjacent digits x and y satisfy x <= y.)
+#
+# Example 1:
+# Input: N = 10
+# Output: 9
+#
+# Example 2:
+# Input: N = 1234
+# Output: 1234
+#  
+# Example 3:
+# Input: N = 332
+# Output: 299
+#   
+# Note: N is an integer in the range [0, 10^9].
+
 class Solution(object):
     def monotoneIncreasingDigits(self, N):
         """
