c250f40f03ea8405af5e66a87b8ee051039805bc
kamyu <kamyu104@users.noreply.github.com>
2017-12-04 09:24:42 -0600

    Update daily-temperatures.py

--- Python/daily-temperatures.py
+++ Python/daily-temperatures.py
@@ -1,6 +1,16 @@
 # Time:  O(n)
 # Space: O(n)
 
+# Given a list of daily temperatures, produce a list that,
+# for each day in the input, tells you how many days you would have to wait until
+# a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+#
+# For example, given the list temperatures = [73, 74, 75, 71, 69, 72, 76, 73],
+# your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+#
+# Note: The length of temperatures will be in the range [1, 30000].
+#       Each temperature will be an integer in the range [30, 100].
+
 class Solution(object):
     def dailyTemperatures(self, temperatures):
         """
