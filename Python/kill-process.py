2b749ce056a62c1ea8e87072a49b27be99e971b7
kamyu <kamyu104@users.noreply.github.com>
2017-12-06 10:00:15 -0600

    Update kill-process.py

--- Python/kill-process.py
+++ Python/kill-process.py
@@ -1,40 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
 
-# Given n processes, each process has a unique PID (process id)
-# and its PPID (parent process id).
-#
-# Each process only has one parent process, but may have one or more children processes.
-# This is just like a tree structure. Only one process has PPID that is 0,
-# which means this process has no parent process. All the PIDs will be distinct positive integers.
-#
-# We use two list of integers to represent a list of processes,
-# where the first list contains PID for each process
-# and the second list contains the corresponding PPID.
-#
-# Now given the two lists, and a PID representing a process you want to kill,
-# return a list of PIDs of processes that will be killed in the end.
-# You should assume that when a process is killed,
-# all its children processes will be killed.
-# No order is required for the final answer.
-#
-# Example 1:
-# Input: 
-# pid =  [1, 3, 10, 5]
-# ppid = [3, 0, 5, 3]
-# kill = 5
-# Output: [5,10]
-# Explanation: 
-#           3
-#          /   \
-#         1     5
-#              /
-#             10
-# Kill 5 will also kill 10.
-# Note:
-# The given kill id is guaranteed to be one of the given PIDs.
-# n >= 1.
-
 # DFS solution.
 class Solution(object):
     def killProcess(self, pid, ppid, kill):
