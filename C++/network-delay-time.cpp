3af65cd2453bd89643cebda5c62a0f7d28614056
kamyu <kamyu104@users.noreply.github.com>
2017-12-10 09:53:26 -0600

    Create network-delay-time.cpp

---
+++ C++/network-delay-time.cpp
@@ -0,0 +1,32 @@
+// Time:  O((|E| + |V|) * log|V|)
+// Space: O(|E| + |V|)
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
+        using P = pair<int, int>;
+        priority_queue<P, vector<P>, greater<P>> min_heap;
+        vector<vector<P>> adj(N);
+        for (const auto& time : times) {
+            int u, v, w;
+            tie(u, v, w) = make_tuple(time[0] - 1, time[1] - 1, time[2]);
+            adj[u].emplace_back(v, w);
+        }
+
+        int result = 0;
+        unordered_set<int> lookup;
+        min_heap.emplace(0, K - 1);
+        while (!min_heap.empty() && lookup.size() != N) {
+            int u;
+            tie(result, u) = min_heap.top(); min_heap.pop();
+            lookup.emplace(u);
+            for (const auto& kvp : adj[u]) {
+                int v, w;
+                tie(v, w) = kvp;
+                if (lookup.count(v)) continue;
+                min_heap.emplace(result + w, v);
+            }
+        }
+        return lookup.size() == N ? result : -1;
+    }
+};
