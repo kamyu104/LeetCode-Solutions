4c1383ef405211a779c97e8508a3c5b01af03a6f
kamyu <kamyu104@users.noreply.github.com>
2017-12-10 12:33:36 -0600

    Update prefix-and-suffix-search.py

--- Python/prefix-and-suffix-search.py
+++ Python/prefix-and-suffix-search.py
@@ -1,47 +1,54 @@
 # Time:  O(m + n), m is the number of prefix match, n is the number of suffix match
 # Space: O(t * w), t is the number of nodes in trie, w is the number of words
 
+class Trie(object):
+    
+    def __init__(self):
+        _trie = lambda: collections.defaultdict(_trie)
+        self.__trie = _trie()
+
+    def insert(self, word, i):
+        def add_word(cur, i):
+            if "_words" not in cur:
+                cur["_words"] = []
+            cur["_words"].append(i)
+
+        cur = self.__trie
+        add_word(cur, i)
+        for c in word:
+            cur = cur[c]
+            add_word(cur, i)
+
+    def find(self, word):
+        cur = self.__trie
+        for c in word:
+            if c not in cur:
+                return []
+            cur = cur[c]
+        return cur["_words"]
+
+
 class WordFilter(object):
 
     def __init__(self, words):
         """
         :type words: List[str]
-        """
-        def insert(trie, word, i):
-            def add_word(cur, i):
-                if "_words" not in cur:
-                    cur["_words"] = []
-                cur["_words"].append(i)
-                
-            cur = trie
-            add_word(cur, i)
-            for c in word:
-                cur = cur[c]
-                add_word(cur, i)
-                
+        """   
         _trie = lambda: collections.defaultdict(_trie)
-        self.__prefix_trie = _trie()
-        self.__suffix_trie = _trie()
+        self.__prefix_trie = Trie()
+        self.__suffix_trie = Trie()
         for i in reversed(xrange(len(words))):
-            insert(self.__prefix_trie, words[i], i)
-            insert(self.__suffix_trie, words[i][::-1], i)
+            self.__prefix_trie.insert(words[i], i)
+            self.__suffix_trie.insert(words[i][::-1], i)
 
     def f(self, prefix, suffix):
         """
         :type prefix: str
         :type suffix: str
         :rtype: int
-        """
-        def find(trie, word):
-            cur = trie
-            for c in word:
-                if c not in cur:
-                    return []
-                cur = cur[c]
-            return cur["_words"]
-        
-        prefix_match = find(self.__prefix_trie, prefix)
-        suffix_match = find(self.__suffix_trie, suffix[::-1])
+        """        
+        prefix_match = self.__prefix_trie.find(prefix)
+        suffix_match = self.__suffix_trie.find(suffix[::-1])
         i, j = 0, 0
         while i != len(prefix_match) and j != len(suffix_match):
             if prefix_match[i] == suffix_match[j]:
