e0691d08b1d24b3c0795887a6455db119c24f8cd
kamyu <kamyu104@users.noreply.github.com>
2017-12-12 11:23:55 -0600

    Update network-delay-time.py

--- Python/network-delay-time.py
+++ Python/network-delay-time.py
@@ -1,6 +1,21 @@
 # Time:  O((|E| + |V|) * log|V|)
 # Space: O(|E| + |V|)
 
+# There are N network nodes, labelled 1 to N.
+#
+# Given times, a list of travel times as directed edges times[i] = (u, v, w),
+# where u is the source node, v is the target node,
+# and w is the time it takes for a signal to travel from source to target.
+#
+# Now, we send a signal from a certain node K.
+# How long will it take for all nodes to receive the signal? If it is impossible, return -1.
+#
+# Note:
+# - N will be in the range [1, 100].
+# - K will be in the range [1, N].
+# - The length of times will be in the range [1, 6000].
+# - All edges times[i] = (u, v, w) will have 1 <= u, v <= N and 1 <= w <= 100.
+
 # Dijkstra's algorithm
 class Solution(object):
     def networkDelayTime(self, times, N, K):
