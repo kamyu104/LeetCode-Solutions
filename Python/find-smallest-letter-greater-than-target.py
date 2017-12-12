dd385f1d0522e62a71b6b41f38626bc3ad3c9385
kamyu <kamyu104@users.noreply.github.com>
2017-12-12 11:22:01 -0600

    Update find-smallest-letter-greater-than-target.py

--- Python/find-smallest-letter-greater-than-target.py
+++ Python/find-smallest-letter-greater-than-target.py
@@ -1,6 +1,46 @@
 # Time:  O(logn)
 # Space: O(1)
 
+# Given a list of sorted characters letters containing only lowercase letters,
+# and given a target letter target, find the smallest element in the list that is larger than the given target.
+#
+# Letters also wrap around. For example, if the target is target = 'z' and letters = ['a', 'b'], the answer is 'a'.
+#
+# Examples:
+# Input:
+# letters = ["c", "f", "j"]
+# target = "a"
+# Output: "c"
+#
+# Input:
+# letters = ["c", "f", "j"]
+# target = "c"
+# Output: "f"
+#
+# Input:
+# letters = ["c", "f", "j"]
+# target = "d"
+# Output: "f"
+#
+# Input:
+# letters = ["c", "f", "j"]
+# target = "g"
+# Output: "j"
+#
+# Input:
+# letters = ["c", "f", "j"]
+# target = "j"
+# Output: "c"
+#
+# Input:
+# letters = ["c", "f", "j"]
+# target = "k"
+# Output: "c"
+# Note:
+# - letters has a length in range [2, 10000].
+# - letters consists of lowercase letters, and contains at least 2 unique letters.
+# - target is a lowercase letter.
+
 class Solution(object):
     def nextGreatestLetter(self, letters, target):
         """
