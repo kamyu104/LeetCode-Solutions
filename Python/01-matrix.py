7795654c78582c7669c14203446ba95a03f7b65f
kamyu <kamyu104@users.noreply.github.com>
2017-12-30 19:36:02 +0800

    Update 01-matrix.py

--- Python/01-matrix.py
+++ Python/01-matrix.py
@@ -59,3 +59,31 @@ class Solution(object):
                 matrix[i][j] = matrix[cell[0]][cell[1]]+1
         
         return matrix
+
+
+# Time:  O(m * n)
+# Space: O(m * n)
+# dp solution
+class Solution2(object):
+    def updateMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        dp = [[float("inf")]*len(matrix[0]) for _ in xrange(len(matrix))]
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[i])):
+                if matrix[i][j] == 0:
+                    dp[i][j] = 0
+                else:
+                    if i > 0:
+                        dp[i][j] = min(dp[i][j], dp[i-1][j]+1)
+                    if j > 0:
+                        dp[i][j] = min(dp[i][j], dp[i][j-1]+1)
+        for i in reversed(xrange(len(matrix))):
+            for j in reversed(xrange(len(matrix[i]))):
+                if i < len(matrix)-1:
+                    dp[i][j] = min(dp[i][j], dp[i+1][j]+1)
+                if j < len(matrix[i])-1:
+                    dp[i][j] = min(dp[i][j], dp[i][j+1]+1)
+        return dp
