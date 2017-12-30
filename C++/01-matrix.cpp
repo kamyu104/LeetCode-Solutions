e5b434b8b42c255d6f7fb7d448d6c55f164b2112
kamyu <kamyu104@users.noreply.github.com>
2017-12-30 19:25:15 +0800

    Update 01-matrix.cpp

--- C++/01-matrix.cpp
+++ C++/01-matrix.cpp
@@ -35,3 +35,43 @@ public:
         return matrix;
     }
 };
+
+// Time:  O(m * n)
+// Space: O(m * n)
+// dp solution
+class Solution2 {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        vector<vector<int> > dp(matrix.size(),
+                                vector<int>(matrix[0].size(),
+                                            numeric_limits<int>::max() - 10000));
+
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[i].size(); ++j) {
+                if (matrix[i][j] == 0)
+                    dp[i][j] = 0;
+                else {
+                    if (i > 0) {
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    if (j > 0) {
+                        dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        for (int i = matrix.size() - 1; i >= 0; --i) {
+            for (int j = matrix[i].size() - 1; j >= 0; --j) {
+                if (i < matrix.size() - 1) {
+                    dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+                }
+                if (j < matrix[i].size() - 1) {
+                    dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+                }
+            }
+        }
+
+        return dp;
+    }
+};
