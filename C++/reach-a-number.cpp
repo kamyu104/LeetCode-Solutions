fce89ec667645381a99277f463de8e04e6f5cfdd
kamyu <kamyu104@users.noreply.github.com>
2017-12-31 16:46:13 +0800

    Update reach-a-number.cpp

--- C++/reach-a-number.cpp
+++ C++/reach-a-number.cpp
@@ -1,7 +1,20 @@
-// Time:  O(sqrt(n))
+// Time:  O(logn)
 // Space: O(1)
 
 class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        auto k = static_cast<int>(ceil((-1 + sqrt(1 + 8.0 * target)) / 2));
+        target -= k * (k + 1) / 2;
+        return (target % 2 == 0) ? k : k + 1 + k % 2;
+    }
+};
+
+
+// Time:  O(sqrt(n))
+// Space: O(1)
+class Solution2 {
 public:
     int reachNumber(int target) {
         target = abs(target);
