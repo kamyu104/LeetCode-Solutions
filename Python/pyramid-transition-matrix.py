3e95a8a22b1a40aed7278f9568546a67a58dcfc8
kamyu <kamyu104@users.noreply.github.com>
2018-01-01 21:42:24 +0800

    Update pyramid-transition-matrix.py

--- Python/pyramid-transition-matrix.py
+++ Python/pyramid-transition-matrix.py
@@ -1,6 +1,6 @@
-# Time:  O(a^(b*(b+1)/2-1)),  a is the size of allowed, 
-#                             b is the length of bottom
-# Space: O(a + b^2) 
+# Time:  O((a^(b+1)-a)/(a-1)) = O(a^b) , a is the size of allowed, 
+#                                        b is the length of bottom
+# Space: O((a^(b+1)-a)/(a-1)) = O(a^b) 
 
 # We are stacking blocks to form a pyramid. Each block has a color which is a one letter string, like `'Z'`.
 #
@@ -46,25 +46,29 @@ class Solution(object):
         :type allowed: List[str]
         :rtype: bool
         """
-        def pyramidTransitionHelper(bottom, edges):
-            def dfs(bottom, edges, new_bottom, idx):
+        def pyramidTransitionHelper(bottom, edges, lookup):
+            def dfs(bottom, edges, new_bottom, idx, lookup):
                 if idx == len(bottom)-1:
-                    return pyramidTransitionHelper(new_bottom, edges)
+                    return pyramidTransitionHelper(new_bottom, edges, lookup)
                 for i in edges[ord(bottom[idx])-ord('A')][ord(bottom[idx+1])-ord('A')]:
                     new_bottom[idx] = chr(i+ord('A'));
-                    if dfs(bottom, edges, new_bottom, idx+1):
+                    if dfs(bottom, edges, new_bottom, idx+1, lookup):
                         return True
                 return False
             
             if len(bottom) == 1:
                 return True
+            if "".join(bottom) in lookup:
+                return False
+            lookup.add("".join(bottom))
             for i in xrange(len(bottom)-1):
                 if not edges[ord(bottom[i])-ord('A')][ord(bottom[i+1])-ord('A')]:
                     return False
             new_bottom = ['A']*(len(bottom)-1)
-            return dfs(bottom, edges, new_bottom, 0)
+            return dfs(bottom, edges, new_bottom, 0, lookup)
     
         edges = [[[] for _ in xrange(7)] for _ in xrange(7)]
         for s in allowed:
             edges[ord(s[0])-ord('A')][ord(s[1])-ord('A')].append(ord(s[2])-ord('A'))
-        return pyramidTransitionHelper(bottom, edges)
+        return pyramidTransitionHelper(bottom, edges, set())
+    
