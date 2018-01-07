75b7d9dc321921f20f687cc290630aaacb050cb5
kamyu <kamyu104@users.noreply.github.com>
2018-01-07 19:27:15 +0800

    Update bold-words-in-string.py

--- Python/bold-words-in-string.py
+++ Python/bold-words-in-string.py
@@ -34,3 +34,29 @@ class Solution(object):
             if lookup[i] and (i == len(S)-1 or not lookup[i+1]):
                 result.append("</b>");
         return "".join(result)
+
+
+# Time:  O(n * d * l), l is the average length of words
+# Space: O(n)
+class Solution2(object):
+    def boldWords(self, words, S):
+        """
+        :type words: List[str]
+        :type S: str
+        :rtype: str
+        """
+        lookup = [0] * len(S)
+        for d in words:
+            pos = S.find(d)
+            while pos != -1:
+                lookup[pos:pos+len(d)] = [1] * len(d)
+                pos = S.find(d, pos+1)
+
+        result = []
+        for i in xrange(len(S)):
+            if lookup[i] and (i == 0 or not lookup[i-1]):
+                result.append("<b>")
+            result.append(S[i])
+            if lookup[i] and (i == len(S)-1 or not lookup[i+1]):
+                result.append("</b>");
+        return "".join(result)
