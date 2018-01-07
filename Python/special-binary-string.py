a99ea115409834b30bb7e439ebdc093ce7e53bad
kamyu <kamyu104@users.noreply.github.com>
2018-01-07 19:04:02 +0800

    Update special-binary-string.py

--- Python/special-binary-string.py
+++ Python/special-binary-string.py
@@ -9,11 +9,11 @@ class Solution(object):
         :rtype: str
         """
         result = []
-        j = count = 0
+        anchor = count = 0
         for i, v in enumerate(S):
             count += 1 if v == '1' else -1
             if count == 0:
-                result.append("1{}0".format(self.makeLargestSpecial(S[j+1:i])))
-                j = i+1
+                result.append("1{}0".format(self.makeLargestSpecial(S[anchor+1:i])))
+                anchor = i+1
         result.sort(reverse = True)
         return "".join(result)
