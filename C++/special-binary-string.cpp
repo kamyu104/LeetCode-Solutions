30c381678088db7ad0d18b162d2332a5f868aeeb
kamyu <kamyu104@users.noreply.github.com>
2018-01-07 19:03:05 +0800

    Update special-binary-string.cpp

--- C++/special-binary-string.cpp
+++ C++/special-binary-string.cpp
@@ -10,11 +10,9 @@ public:
         for (int i = 0; i < S.length(); ++i) {
             (S[i] == '1') ? ++count : --count;
             if (count == 0) {
-                string tmp;
-                tmp += "1";
-                tmp += makeLargestSpecial(S.substr(anchor + 1, i - anchor - 1));
-                tmp += "0";
-                result.emplace_back(move(tmp));
+                result.emplace_back("1");
+                result.back() += makeLargestSpecial(S.substr(anchor + 1, i - anchor - 1));
+                result.back() += "0";
                 anchor = i + 1;
             }
         }
