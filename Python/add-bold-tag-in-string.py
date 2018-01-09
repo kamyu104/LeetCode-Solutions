3aeded88eeaabc833700fb774c451048a803e717
kamyu <kamyu104@users.noreply.github.com>
2018-01-09 14:10:22 +0800

    Update add-bold-tag-in-string.py

--- Python/add-bold-tag-in-string.py
+++ Python/add-bold-tag-in-string.py
@@ -1,6 +1,7 @@
-# Time:  O(s * d * l), l is the average string length
-# Space: O(s)
+# Time:  O(n * d * l), l is the average string length
+# Space: O(n)
 
+# 59ms
 class Solution(object):
     def addBoldTag(self, s, dict):
         """
@@ -14,6 +15,44 @@ class Solution(object):
             while pos != -1:
                 lookup[pos:pos+len(d)] = [1] * len(d)
                 pos = s.find(d, pos + 1)
+
+        result = []
+        for i in xrange(len(s)):
+            if lookup[i] and (i == 0 or not lookup[i-1]):
+                result.append("<b>")
+            result.append(s[i])
+            if lookup[i] and (i == len(s)-1 or not lookup[i+1]):
+                result.append("</b>");
+        return "".join(result)
+
+    
+# Time:  O(n * l), l is the average string length
+# Space: O(t)    , t is the size of trie
+# trie solution, 439ms
+class Solution2(object):
+    def addBoldTag(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: str
+        """
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie).setdefault("_end")
+
+        lookup = [False] * len(s)
+        for i in xrange(len(s)):
+            curr = trie
+            k = -1
+            for j in xrange(i, len(s)):
+                if s[j] not in curr:
+                    break
+                curr = curr[s[j]]
+                if "_end" in curr:
+                    k = j
+            for j in xrange(i, k+1):
+                lookup[j] = True
 
         result = []
         for i in xrange(len(s)):
