f48883289cc42818d60f5b75fa9db131c4b57eb8
kamyu <kamyu104@users.noreply.github.com>
2018-01-10 01:54:33 +0800

    Update plus-one.cpp

--- C++/plus-one.cpp
+++ C++/plus-one.cpp
@@ -1,7 +1,27 @@
 // Time:  O(n)
 // Space: O(1)
 
+// in-place solution
 class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        for (int i = digits.size() - 1; i >= 0; --i) {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            } else {
+                ++digits[i];
+                return digits;
+            }
+        }
+        digits[0] = 1;
+        digits.emplace_back(0);
+        return digits;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
 public:
     vector<int> plusOne(vector<int>& digits) {
         vector<int> result(digits.rbegin(), digits.rend());
