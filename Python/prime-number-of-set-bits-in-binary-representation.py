f3caa79045c6d88eb695aa8f4556afcb90d255a9
kamyu <kamyu104@users.noreply.github.com>
2018-01-14 18:30:38 +0800

    Create prime-number-of-set-bits-in-binary-representation.py

---
+++ Python/prime-number-of-set-bits-in-binary-representation.py
@@ -0,0 +1,20 @@
+# Time:  O(log(R - L)) = O(1)
+# Space: O(1)
+
+class Solution(object):
+    def countPrimeSetBits(self, L, R):
+        """
+        :type L: int
+        :type R: int
+        :rtype: int
+        """
+        def bitCount(n):
+            result = 0
+            while n:
+                n &= n-1
+                result += 1
+            return result
+        
+        primes = {2, 3, 5, 7, 11, 13, 17, 19}
+        return sum(bitCount(i) in primes
+                   for i in xrange(L, R+1))
