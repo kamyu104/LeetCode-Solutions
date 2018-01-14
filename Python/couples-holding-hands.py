41fe757ca320a5ecde528e463f1d2b7880354ef4
kamyu <kamyu104@users.noreply.github.com>
2018-01-14 20:40:22 +0800

    Create couples-holding-hands.py

---
+++ Python/couples-holding-hands.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        N = len(row)//2
+        couples = [[] for _ in xrange(N)]
+        for seat, num in enumerate(row):
+            couples[num//2].append(seat//2)
+        adj = [[] for _ in xrange(N)]
+        for couch1, couch2 in couples:
+            adj[couch1].append(couch2)
+            adj[couch2].append(couch1)
+            
+        result = N
+        for couch in xrange(N):
+            if not adj[couch]: continue
+            result -= 1
+            couch1, couch2 = couch, adj[couch].pop()
+            while couch2 != couch:
+                adj[couch2].remove(couch1)
+                couch1, couch2 = couch2, adj[couch2].pop()
+        return result
