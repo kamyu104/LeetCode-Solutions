e727c954fd18491aa1b22a082aa0705484093f4b
kamyu <kamyu104@users.noreply.github.com>
2018-01-14 18:49:55 +0800

    Create partition-labels.py

---
+++ Python/partition-labels.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        lookup = {c: i for i, c in enumerate(S)}
+        first, last = 0, 0
+        result = []
+        for i, c in enumerate(S):
+            last = max(last, lookup[c])
+            if i == last:
+                result.append(i-first+1)
+                first = i+1
+        return result
