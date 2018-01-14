74281da9cf5673a9727b0defad027a2a96443ae7
kamyu <kamyu104@users.noreply.github.com>
2018-01-14 20:39:16 +0800

    Create couples-holding-hands.cpp

---
+++ C++/couples-holding-hands.cpp
@@ -0,0 +1,33 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int minSwapsCouples(vector<int>& row) {
+        int N = row.size() / 2;
+        vector<vector<int>> couples(N);
+        for (int seat = 0; seat < row.size(); ++seat) {
+            couples[row[seat] / 2].emplace_back(seat / 2);
+        }
+        vector<vector<int>> adj(N);
+        for (const auto& couple : couples) {
+            adj[couple[0]].emplace_back(couple[1]);
+            adj[couple[1]].emplace_back(couple[0]);
+        }
+        int result = N;
+        for (int couch = 0; couch < N; ++couch) {
+            if (adj[couch].empty()) {
+                continue;
+            }
+            --result;
+            int couch1 = couch;
+            int couch2 = adj[couch1].back(); adj[couch1].pop_back();
+            while (couch2 != couch) {
+                adj[couch2].erase(find(adj[couch2].begin(), adj[couch2].end(), couch1));
+                couch1 = couch2;
+                couch2 = adj[couch1].back(); adj[couch1].pop_back();
+            }
+        }
+        return result;
+    }
+};
