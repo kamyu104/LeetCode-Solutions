3b8f868764111514528aa8da21bf10d235dccde9
kamyu <kamyu104@users.noreply.github.com>
2018-01-22 00:55:31 +0800

    Create max-chunks-to-make-sorted-ver-2.py

---
+++ Python/max-chunks-to-make-sorted-ver-2.py
@@ -0,0 +1,19 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+class Solution(object):
+    def maxChunksToSorted(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        def compare(i1, i2):
+            return arr[i1]-arr[i2] if arr[i1] != arr[i2] else i1-i2
+        
+        idxs = [i for i in xrange(len(arr))]
+        result, max_i = 0, 0
+        for i, v in enumerate(sorted(idxs, cmp=compare)):
+            max_i = max(max_i, v)
+            if max_i == i:
+                result += 1
+        return result
