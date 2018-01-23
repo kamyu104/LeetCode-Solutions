ca8a4b80ebbd03350f7d4d30576128d916d9a15e
kamyu <kamyu104@users.noreply.github.com>
2018-01-23 23:02:04 +0800

    Create basic-calculator-iii.py

---
+++ Python/basic-calculator-iii.py
@@ -0,0 +1,46 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def calculate(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        operands, operators = [], []
+        operand = ""
+        for i in reversed(xrange(len(s))):
+            if s[i].isdigit():
+                operand += s[i]
+                if i == 0  or not s[i-1].isdigit():
+                    operands.append(int(operand[::-1]))
+                    operand = ""
+            elif s[i] == ')' or s[i] == '*' or s[i] == '/':
+                operators.append(s[i])
+            elif s[i] == '+' or s[i] == '-':
+                while operators and \
+                      (operators[-1] == '*' or operators[-1] == '/'):
+                    self.compute(operands, operators)
+                operators.append(s[i])
+            elif s[i] == '(':
+                while operators[-1] != ')':
+                    self.compute(operands, operators)
+                operators.pop()
+                
+        while operators:
+            self.compute(operands, operators)
+            
+        return operands[-1]
+
+    def compute(self, operands, operators):
+        left, right = operands.pop(), operands.pop()
+        op = operators.pop()
+        if op == '+':
+            operands.append(left + right)
+        elif op == '-':
+            operands.append(left - right)
+        elif op == '*':
+            operands.append(left * right)
+        elif op == '/':
+            operands.append(left / right)
+        
