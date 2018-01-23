af61e9445e3e7ed072c6904068b9b52c0840d134
kamyu <kamyu104@users.noreply.github.com>
2018-01-24 00:30:55 +0800

    Update reorganized-string.py

--- Python/reorganized-string.py
+++ Python/reorganized-string.py
@@ -1,6 +1,23 @@
 # Time:  O(nloga) = O(n), a is the size of alphabet
 # Space: O(a) = O(1)
 
+# Given a string S, check if the letters can be rearranged
+# so that two characters that are adjacent to each other are not the same.
+#
+# If possible, output any possible result.  If not possible, return the empty string.
+#
+# Example 1:
+#
+# Input: S = "aab"
+# Output: "aba"
+# Example 2:
+#
+# Input: S = "aaab"
+# Output: ""
+#
+# Note:
+# - S will consist of lowercase letters and have length in range [1, 500].
+
 class Solution(object):
     def reorganizeString(self, S):
         """
