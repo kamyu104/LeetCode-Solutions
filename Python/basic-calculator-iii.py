ff2130b5801a6f5f0010e2726e0413b24fc56795
kamyu <kamyu104@users.noreply.github.com>
2018-01-24 00:38:38 +0800

    Update basic-calculator-iii.py

--- Python/basic-calculator-iii.py
+++ Python/basic-calculator-iii.py
@@ -1,6 +1,26 @@
 # Time:  O(n)
 # Space: O(n)
 
+# Implement a basic calculator to evaluate a simple expression string.
+#
+# The expression string may contain open ( and closing parentheses ),
+# the plus + or minus sign -, non-negative integers and empty spaces .
+#
+# The expression string contains only non-negative integers, +, -, *, / operators ,
+# open ( and closing parentheses ) and empty spaces .
+# The integer division should truncate toward zero.
+#
+# You may assume that the given expression is always valid.
+#
+# Some examples:
+#
+# "1 + 1" = 2
+# " 6-4 / 2 " = 4
+# "2*(5+5*2)/3+(6/2+8)" = 21
+# "(2+6* 3+5- (3*14/7+2)*5)+3"=-12
+#
+# Note: Do not use the eval built-in library function.
+
 class Solution(object):
     def calculate(self, s):
         """
