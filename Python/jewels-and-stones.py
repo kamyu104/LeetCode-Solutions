a8614d4131aa34524aaca5d6c6bf8a89149f64bd
kamyu <kamyu104@users.noreply.github.com>
2018-01-30 00:01:35 +0800

    Update jewels-and-stones.py

--- Python/jewels-and-stones.py
+++ Python/jewels-and-stones.py
@@ -1,6 +1,26 @@
 # Time:  O(m + n)
 # Space: O(n)
 
+# You're given strings J representing the types of stones that are jewels,
+# and S representing the stones you have.
+# Each character in S is a type of stone you have.
+# You want to know how many of the stones you have are also jewels.
+#
+# The letters in J are guaranteed distinct, and all characters in J and S are letters.
+# Letters are case sensitive, so "a" is considered a different type of stone from "A".
+#
+# Example 1:
+# Input: J = "aA", S = "aAAbbbb"
+# Output: 3
+# Example 2:
+#
+# Input: J = "z", S = "ZZ"
+# Output: 0
+#
+# Note:
+# - S and J will consist of letters and have length at most 50.
+# - The characters in J are distinct.
+
 class Solution(object):
     def numJewelsInStones(self, J, S):
         """
