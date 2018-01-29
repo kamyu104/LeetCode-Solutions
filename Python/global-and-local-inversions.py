c8b8fcc8f30a34354986ef9f53b275dbc72df59b
kamyu <kamyu104@users.noreply.github.com>
2018-01-30 00:06:20 +0800

    Update global-and-local-inversions.py

--- Python/global-and-local-inversions.py
+++ Python/global-and-local-inversions.py
@@ -1,6 +1,27 @@
 # Time:  O(n)
 # Space: O(1)
 
+# We have some permutation A of [0, 1, ..., N - 1], where N is the length of A.
+# The number of (global) inversions is the number of i < j with 0 <= i < j < N and A[i] > A[j].
+# The number of local inversions is the number of i with 0 <= i < N and A[i] > A[i+1].
+# Return true if and only if the number of global inversions is equal to the number of local inversions.
+#
+# Example 1:
+#
+# Input: A = [1,0,2]
+# Output: true
+# Explanation: There is 1 global inversion, and 1 local inversion.
+# Example 2:
+#
+# Input: A = [1,2,0]
+# Output: false
+# Explanation: There are 2 global inversions, and 1 local inversion.
+#
+# Note:
+# - A will be a permutation of [0, 1, ..., A.length - 1].
+# - A will have length in range [1, 5000].
+# - The time limit for this problem has been reduced.
+
 class Solution(object):
     def isIdealPermutation(self, A):
         """
