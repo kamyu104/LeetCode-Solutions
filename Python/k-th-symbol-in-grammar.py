d509f98ead066e905999a22d5264019b1dcb7068
kamyu <kamyu104@users.noreply.github.com>
2018-02-06 10:52:11 +0800

    Create k-th-symbol-in-grammar.py

---
+++ Python/k-th-symbol-in-grammar.py
@@ -0,0 +1,18 @@
+# Time:  O(logn)
+# Space: O(1)
+    
+class Solution(object):
+    def kthGrammar(self, N, K):
+        """
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        def bitCount(n):
+            result = 0
+            while n:
+                n &= n - 1
+                result += 1
+            return result
+        
+        return bitCount(K-1) % 2
