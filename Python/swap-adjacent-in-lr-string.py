3233294db6ff8d40c31ef79cb0236e1a87772db9
kamyu <kamyu104@users.noreply.github.com>
2018-02-09 22:08:51 +0800

    Update swap-adjacent-in-lr-string.py

--- Python/swap-adjacent-in-lr-string.py
+++ Python/swap-adjacent-in-lr-string.py
@@ -1,6 +1,28 @@
 # Time:  O(n)
 # Space: O(1)
 
+# In a string composed of 'L', 'R', and 'X' characters, like "RXXLRXRXL",
+# a move consists of either replacing one occurrence of "XL" with "LX",
+# or replacing one occurrence of "RX" with "XR".
+# Given the starting string start and the ending string end,
+# return True if and only if there exists a sequence of moves to transform one string to the other.
+#
+# Example:
+# Input: start = "RXXLRXRXL", end = "XRLXXRRLX"
+# Output: True
+#
+# Explanation:
+# We can transform start to end following these steps:
+# RXXLRXRXL ->
+# XRXLRXRXL ->
+# XRLXRXRXL ->
+# XRLXXRRXL ->
+# XRLXXRRLX
+#
+# Note:
+# - 1 <= len(start) = len(end) <= 10000.
+# - Both start and end will only consist of characters in {'L', 'R', 'X'}.
+
 class Solution(object):
     def canTransform(self, start, end):
         """
