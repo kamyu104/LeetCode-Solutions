0b83a509c3883850c79f77cb5b49a868478bc650
Artem Muterko <artieua@gmail.com>
2018-02-16 12:36:29 +0200

    Add a bit more readable solution for 'Toeplitz matrix' problem

--- Python/toeplitz-matrix.py
+++ Python/toeplitz-matrix.py
@@ -38,3 +38,18 @@ class Solution(object):
         return all(i == 0 or j == 0 or matrix[i-1][j-1] == val
                    for i, row in enumerate(matrix)
                    for j, val in enumerate(row))
+
+
+class Solution2(object):
+    def isToeplitzMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        for row_index, row in enumerate(matrix):
+            for digit_index, digit in enumerate(row):
+                if not row_index or not digit_index:
+                    continue
+                if matrix[row_index - 1][digit_index - 1] != digit:
+                    return False
+        return True
