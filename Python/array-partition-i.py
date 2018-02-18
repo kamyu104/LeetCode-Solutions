4a121289c034958ecf5dbe3c2ecf9471a72c66ca
Artem Muterko <artieua@gmail.com>
2018-02-18 15:55:59 +0200

    Add alternative solution for 'Array Partition' problem

--- Python/array-partition-i.py
+++ Python/array-partition-i.py
@@ -42,3 +42,13 @@ class Solution2(object):
         for i in xrange(0, len(nums), 2):
             result += nums[i]
         return result
+
+
+class Solution3(object):
+    def arrayPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums = sorted(nums)
+        return sum([nums[i] for i in range(0, len(nums), 2)])
