0a99eaa8821656fbdf525dfadc75e2a61be05d57
Artem Muterko <artieua@gmail.com>
2018-02-18 17:54:55 +0200

    Add alternative solution for 'Encode and decode tiny url' problem

--- Python/encode-and-decode-tinyurl.py
+++ Python/encode-and-decode-tinyurl.py
@@ -10,13 +10,13 @@
 # You just need to ensure that a URL can be encoded to a tiny URL
 # and the tiny URL can be decoded to the original URL.
 
+
 class Codec:
     def __init__(self):
         self.__random_length = 6
         self.__tiny_url = "http://tinyurl.com/"
         self.__alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
         self.__lookup = {}
-
 
     def encode(self, longUrl):
         """Encodes a URL to a shortened URL.
@@ -35,7 +35,6 @@ class Codec:
             key = getRand()
         self.__lookup[key] = longUrl
         return self.__tiny_url + key
-        
 
     def decode(self, shortUrl):
         """Decodes a shortened URL to its original URL.
@@ -44,7 +43,35 @@ class Codec:
         :rtype: str
         """
         return self.__lookup[shortUrl[len(self.__tiny_url):]]
-        
+
+
+from hashlib import sha256
+
+
+class Codec2:
+
+    def __init__(self):
+        self._cache = {}
+        self.url = 'http://tinyurl.com/'
+
+    def encode(self, long_url):
+        """Encodes a URL to a shortened URL.
+
+        :type long_url: str
+        :rtype: str
+        """
+        key = sha256(long_url.encode()).hexdigest()[:6]
+        self._cache[key] = long_url
+        return self.url + key
+
+    def decode(self, short_url):
+        """Decodes a shortened URL to its original URL.
+
+        :type short_url: str
+        :rtype: str
+        """
+        key = short_url.replace(self.url, '')
+        return self._cache[key]
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
