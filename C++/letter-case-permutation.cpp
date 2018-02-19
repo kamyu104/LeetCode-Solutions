937bedca4cb0fb7d580aa5a2b588cd8963969e2f
kamyu <kamyu104@users.noreply.github.com>
2018-02-19 13:58:56 +0800

    Create letter-case-permutation.cpp

---
+++ C++/letter-case-permutation.cpp
@@ -0,0 +1,24 @@
+// Time:  O(n * 2^n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<string> letterCasePermutation(string S) {
+        vector<string> result{""};
+        for (const auto& c : S) {
+            if (isalpha(c)) {
+                const auto& size = result.size();
+                for (int i = 0; i < size; ++i) {
+                    result.emplace_back(result[i]);
+                    result.back() += toupper(c);
+                    result[i] += tolower(c);
+                }
+            } else {
+                for (auto &s : result) {
+                    s += c;
+                }
+            }
+        }
+        return result;
+    }
+};
