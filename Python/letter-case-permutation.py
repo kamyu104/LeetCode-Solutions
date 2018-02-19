c2ac3585d2bf4776acbb7ceb3c621101a8c0e5a6
kamyu <kamyu104@users.noreply.github.com>
2018-02-19 14:09:21 +0800

    Update letter-case-permutation.py

--- Python/letter-case-permutation.py
+++ Python/letter-case-permutation.py
@@ -1,5 +1,5 @@
 # Time:  O(n * 2^n)
-# Space: O(1)
+# Space: O(n * 2^n)
 
 # Given a string S, we can transform every letter individually to be lowercase
 # or uppercase to create another string.  Return a list of all possible strings we could create.
