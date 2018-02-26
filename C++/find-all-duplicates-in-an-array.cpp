c470545dc0b38f80e9e249e14adb34a6fe2110b7
kamyu <kamyu104@users.noreply.github.com>
2018-02-26 21:47:14 +0800

    Update find-all-duplicates-in-an-array.cpp

--- C++/find-all-duplicates-in-an-array.cpp
+++ C++/find-all-duplicates-in-an-array.cpp
@@ -5,6 +5,21 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> result;
+        for (const auto& i : nums) {
+            if (nums[abs(i) - 1] < 0) {
+                result.emplace_back(abs(i));
+            } else {
+                nums[abs(i) - 1] *= -1;
+            }
+        }
+        return result;
+    }
+};
+
+class Solution2 {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> result;
         int i = 0;
         while (i < nums.size()) {
             if (nums[i] != nums[nums[i] - 1]) {
