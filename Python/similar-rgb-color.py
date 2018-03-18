6e39875f98154c55b3a85c2a5b9b80809e3c3fa8
kamyu <kamyu104@users.noreply.github.com>
2018-03-18 21:41:36 +0800

    Create similar-rgb-color.py

---
+++ Python/similar-rgb-color.py
@@ -0,0 +1,18 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def similarRGB(self, color):
+        """
+        :type color: str
+        :rtype: str
+        """
+        def rounding(color):
+            q, r = divmod(int(color, 16), 17)
+            if r > 8: q += 1
+            return '{:02x}'.format(17*q)
+
+        return '#' + \
+                rounding(color[1:3]) + \
+                rounding(color[3:5]) + \
+                rounding(color[5:7])
