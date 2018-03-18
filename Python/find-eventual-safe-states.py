5051be5f7f63ebd400ba8aac5c9201f17fa42708
kamyu <kamyu104@users.noreply.github.com>
2018-03-18 23:22:43 +0800

    Create find-eventual-safe-states.py

---
+++ Python/find-eventual-safe-states.py
@@ -0,0 +1,26 @@
+# Time:  O(|V| + |E|)
+# Space: O(|V|)
+
+class Solution(object):
+    def eventualSafeNodes(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: List[int]
+        """
+        WHITE, GRAY, BLACK = 0, 1, 2
+        
+        def dfs(graph, node, lookup):
+            if lookup[node] != WHITE:
+                return lookup[node] == BLACK
+            lookup[node] = GRAY
+            for child in graph[node]:
+                if lookup[child] == BLACK:
+                    continue
+                if lookup[child] == GRAY or \
+                   not dfs(graph, child, lookup):
+                    return False
+            lookup[node] = BLACK
+            return True
+
+        lookup = collections.defaultdict(int)
+        return filter(lambda node: dfs(graph, node, lookup), xrange(len(graph)))
