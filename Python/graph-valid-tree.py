05527c0d4655f5d8c1aaf7875acc82232c04b5a0
kamyu <kamyu104@users.noreply.github.com>
2018-03-19 20:47:24 +0800

    Update graph-valid-tree.py

--- Python/graph-valid-tree.py
+++ Python/graph-valid-tree.py
@@ -2,47 +2,38 @@
 # Space: O(|V| + |E|)
 
 # BFS solution. Same complexity but faster version.
-class Solution:
+class Solution(object):
     # @param {integer} n
     # @param {integer[][]} edges
     # @return {boolean}
     def validTree(self, n, edges):
         if len(edges) != n - 1:  # Check number of edges.
             return False
-        elif n == 1:
-            return True
 
-        # A structure to track each node's [visited_from, neighbors]
-        visited_from = [-1] * n
+        # init node's neighbors in dict
         neighbors = collections.defaultdict(list)
         for u, v in edges:
             neighbors[u].append(v)
             neighbors[v].append(u)
-        
-        if len(neighbors) != n:  # Check number of nodes.
-            return False
 
         # BFS to check whether the graph is valid tree.
         visited = {}
         q = collections.deque([0])
         while q:
-            i = q.popleft()
-            visited[i] = True
-            for node in neighbors[i]:
-                if node != visited_from[i]:
-                    if node in visited:
-                        return False
-                    else:
-                        visited[node] = True
-                        visited_from[node] = i
-                        q.append(node)
+            curr = q.popleft()
+            visited[curr] = True
+            for node in neighbors[curr]:
+                if node not in visited:
+                    visited[node] = True
+                    q.append(node)
+
         return len(visited) == n
 
 
 # Time:  O(|V| + |E|)
 # Space: O(|V| + |E|)
 # BFS solution.
-class Solution2:
+class Solution2(object):
     # @param {integer} n
     # @param {integer[][]} edges
     # @return {boolean}
