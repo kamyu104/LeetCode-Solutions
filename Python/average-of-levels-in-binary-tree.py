aa1dd542d8aa60896360a5cd26a28b421489cc7c
oshapeman <cgj_19@163.com>
2018-03-26 18:23:11 +0800

    Update average-of-levels-in-binary-tree.py

--- Python/average-of-levels-in-binary-tree.py
+++ Python/average-of-levels-in-binary-tree.py
@@ -45,6 +45,6 @@ class Solution(object):
                     next_q.append(n.left)
                 if n.right:
                     next_q.append(n.right)
-            q, next_q = next_q, q
+            q = next_q
             result.append(float(total) / count)
         return result
