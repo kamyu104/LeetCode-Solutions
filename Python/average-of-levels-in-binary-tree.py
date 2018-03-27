a328aa4325cb1be87a2829c73cbce2bd3a4991cf
kamyu <kamyu104@users.noreply.github.com>
2018-03-27 21:10:09 +0800

    Update average-of-levels-in-binary-tree.py

--- Python/average-of-levels-in-binary-tree.py
+++ Python/average-of-levels-in-binary-tree.py
@@ -33,10 +33,9 @@ class Solution(object):
         :rtype: List[float]
         """
         result = []
-        q = collections.deque([root])
+        q, next_q = collections.deque([root]), collections.deque([])
         while q:
             total, count = 0, 0
-            next_q = collections.deque([])
             while q:
                 n = q.popleft()
                 total += n.val;
@@ -45,6 +44,6 @@ class Solution(object):
                     next_q.append(n.left)
                 if n.right:
                     next_q.append(n.right)
-            q = next_q
+            q, next_q = next_q, q
             result.append(float(total) / count)
         return result
