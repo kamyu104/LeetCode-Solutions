8fb86212d77165f23e2973112b93d3937d803880
Keqi Huang <keqih@smu.edu>
2018-03-28 22:00:59 -0500

    Update n-queens.py

--- Python/n-queens.py
+++ Python/n-queens.py
@@ -62,5 +62,35 @@ class Solution2:
                 if i not in solution and reduce(lambda acc, j: abs(row - j) != abs(i - solution[j]) and acc, xrange(len(solution)), True):
                     self.solveNQueensRecu(solution + [i], row + 1, n)
 
+                    
+                    
+# For any point (x,y), if we want the new point (p,q) don't share the same row, column, or diagonal.
+# then there must have ```p+q != x+y``` and ```p-q!= x-y``` 
+# the former focus on eliminate 'left bottom right top' diagonal; 
+# the latter focus on eliminate 'left top right bottom' diagonal
+
+# - col_per_row: the list of column index per row
+# - cur_row：current row we are seraching for valid column
+# - xy_diff：the list of x-y
+# - xy_sum：the list of x+y   
+class Solution3(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        def dfs(col_per_row, xy_diff, xy_sum):
+            cur_row = len(col_per_row)
+            if cur_row == n:
+                ress.append(col_per_row)
+            for col in range(n):
+                if col not in col_per_row and cur_row-col not in xy_diff and cur_row+col not in xy_sum:
+                    dfs(col_per_row+[col], xy_diff+[cur_row-col], xy_sum+[cur_row+col])
+        ress = []
+        dfs([], [], [])
+        return [['.'*i + 'Q' + '.'*(n-i-1) for i in res] for res in ress]
+                    
+                    
+                    
 if __name__ == "__main__":
     print Solution().solveNQueens(8)
