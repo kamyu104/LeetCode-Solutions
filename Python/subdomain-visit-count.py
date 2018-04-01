ab86af3adc5ad29b8031a572155eb696c9d2c2bd
kamyu <kamyu104@users.noreply.github.com>
2018-04-02 00:15:09 +0800

    Update subdomain-visit-count.py

--- Python/subdomain-visit-count.py
+++ Python/subdomain-visit-count.py
@@ -57,16 +57,10 @@ try:
     defaultdict.iteritems
 except AttributeError:
     # Python 3
-    def itervalues(d):
-        return iter(d.values())
-
     def iteritems(d):
         return iter(d.items())
 else:
     # Python 2
-    def itervalues(d):
-        return d.itervalues()
-
     def iteritems(d):
         return d.iteritems()
 
