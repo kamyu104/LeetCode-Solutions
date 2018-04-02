7238d260fe832784799fd835b494bb2aea4fffd6
Allen Liu <kamyu104@gmail.com>
2018-04-03 00:30:16 +0800

    Fix flake8 related issues

--- Python/3sum.py
+++ Python/3sum.py
@@ -1,6 +1,6 @@
 # Time:  O(n^2)
 # Space: O(1)
-#
+
 # Given an array S of n integers,
 # are there elements a, b, c in S such that a + b + c = 0?
 # Find all unique triplets in the array which gives the sum of zero.
