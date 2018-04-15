ce59f464c5136943801409c6cf274a81bce1a41e
Allen Liu <kamyu104@gmail.com>
2018-04-15 20:53:53 +0800

    Update race-car.py

--- Python/race-car.py
+++ Python/race-car.py
@@ -62,10 +62,12 @@ class Solution(object):
             dp[i] = min(dp[i], k + 1 + dp[2**k-1 - i])
 
             # case 3. drive less then 2^k-1, and turn back some distance,
-            #         and turn back again to make speed is positive
-            #         seq(i) = shortest(seq(i), A^(k-1) -> R -> A^j -> R -> seq(i - (2^(k-1)-1) + (2^j-1)),
+            #         and turn back again to make the direction is the same
+            #         seq(i) = shortest(seq(i), A^(k-1) -> R -> A^j -> R ->
+            #                                   seq(i - (2^(k-1)-1) + (2^j-1)),
             #                  where 0 <= j < k-1)
-            #         => dp[i] = min(dp[i], (k-1) + 1 + j + 1 + dp[i - (2**(k-1)-1) + (2**j-1)])
+            #         => dp[i] = min(dp[i], (k-1) + 1 + j + 1 +
+            #                               dp[i - (2**(k-1)-1) + (2**j-1)])
             for j in xrange(k-1):
                 dp[i] = min(dp[i], k+j+1 + dp[i - 2**(k-1) + 2**j])
 
