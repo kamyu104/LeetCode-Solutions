cae58c9e5b6d4bf607b65e12f1d3050415a07b04
kamyu <kamyu104@users.noreply.github.com>
2018-04-15 20:31:34 +0800

    Create race-car.cpp

---
+++ C++/race-car.cpp
@@ -0,0 +1,36 @@
+// Time : O(nlogn), n is the value of the target
+// Space: O(n)
+
+class Solution {
+public:
+    int racecar(int target) {
+        vector<int> dp(target + 1, numeric_limits<int>::max());
+        dp[0] = 0;
+        for (int i = 1; i <= target; ++i) {
+            int k = bitLength(i);
+            if (i == (1 << k) - 1) {
+                dp[i] = k;
+                continue;
+            }
+            dp[i] = min(dp[i], dp[(1 << k) - 1 - i] + k + 1);
+            for (int j = 0; j < k; ++j) {
+                dp[i] = min(dp[i], dp[i - (1 << (k - 1)) + (1 << j)] + k + j + 1);
+            }
+        }
+        return dp[target];
+    }
+
+private:
+    uint32_t bitLength(uint32_t n) {
+        uint32_t left = 1, right = 32;
+        while (left <= right) {
+            auto mid = left + (right - left) / 2;
+            if ((1 << mid) > n) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
