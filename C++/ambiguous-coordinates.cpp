7590dc5cb19a5493a08f8d2b5b57e0ce7d050a16
kamyu <kamyu104@users.noreply.github.com>
2018-04-16 00:01:16 +0800

    Create ambiguous-coordinates.cpp

---
+++ C++/ambiguous-coordinates.cpp
@@ -0,0 +1,45 @@
+// Time:  O(n^4)
+// Space: O(n^2)
+
+class Solution {
+public:
+    vector<string> ambiguousCoordinates(string S) {
+        vector<string> result;
+        for (int i = 1; i < S.length() - 2; ++i) {
+            const auto& lefts = make(S, 1, i);
+            const auto& rights = make(S, i + 1, S.length() - 2 - i);
+            for (const auto& left : lefts) {
+                for (const auto& right : rights) {
+                    string s;
+                    s += "(";
+                    s += left;
+                    s += ", ";
+                    s += right;
+                    s += ")";
+                    result.emplace_back(move(s));
+                }
+            }
+        }
+        return result;
+    }
+
+private:
+    // Time:  O(n^2)
+    // Space: O(n^2)
+    vector<string> make(const string& S, int i, int n) {
+        vector<string> result;
+        for (int d = 1; d <= n; ++d) {
+            const auto& left = S.substr(i, d);
+            const auto& right = S.substr(i + d, n - d);
+            if ((left.front() != '0' || left == "0") &&
+                 right.back() != '0') {
+                string s;
+                s += left;
+                s += (d < n ? "." : "");
+                s += right;
+                result.emplace_back(move(s));
+            }
+        }
+        return result;
+    }
+};
