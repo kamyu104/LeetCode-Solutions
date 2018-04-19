9c04454df95234cbf6af1caf9cc502ac9069a18c
Aven Xiangwen Liu <CMWENLIU@GMAIL.COM>
2018-04-19 15:27:37 -0500

    Update image-smoother.py
    Another solution with getGray function simplified.

--- Python/image-smoother.py
+++ Python/image-smoother.py
@@ -47,3 +47,25 @@ class Solution(object):
             for j in xrange(len(M[0])):
                 result[i][j] = getGray(M, i, j);
         return result
+# Another solution with getGray function simplified.    
+class Solution(object):
+    def imageSmoother(self, M):
+        """
+        :type M: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        def getGray(M, i, j):
+            total, count = 0, 0.0
+            for r in xrange(-1, 2):
+                for c in xrange(-1, 2):
+                    ii, jj = i + r, j + c
+                    if 0 <= ii < len(M) and 0 <= jj < len(M[0]):
+                        total += M[ii][jj]
+                        count += 1.0
+            return int(total / count)
+
+        result = [[0 for _ in xrange(len(M[0]))] for _ in xrange(len(M))]
+        for i in xrange(len(M)):
+            for j in xrange(len(M[0])):
+                result[i][j] = getGray(M, i, j);
+        return result
