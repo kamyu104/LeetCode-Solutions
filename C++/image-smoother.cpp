7627e76e4405250a0b26e45255840cef0029c132
kamyu <kamyu104@users.noreply.github.com>
2018-04-20 23:10:12 +0800

    Update image-smoother.cpp

--- C++/image-smoother.cpp
+++ C++/image-smoother.cpp
@@ -17,17 +17,16 @@ public:
 private:
     int getGray(const vector<vector<int>>& M, int i, int j) {
         const auto& m = M.size(), n = M[0].size();
-        static const vector<pair<int, int>> directions = { {-1, -1}, {0, -1}, {1, -1},
-                                                           {-1,  0}, {0,  0}, {1,  0},
-                                                           {-1,  1}, {0,  1}, {1,  1} };
         double total = 0.0;
         int count = 0;
-        for (const auto& direction : directions) {
-            const auto& ii = i + direction.first;
-            const auto& jj = j + direction.second;
-            if (0 <= ii && ii < m && 0 <= jj && jj < n) {
-                total += M[ii][jj];
-                ++count;
+        for (int r = -1; r < 2; ++r) {
+            for (int c = -1; c < 2; ++c) {
+                const auto& ii = i + r;
+                const auto& jj = j + c;
+                if (0 <= ii && ii < m && 0 <= jj && jj < n) {
+                    total += M[ii][jj];
+                    ++count;
+                }
             }
         }
         return static_cast<int>(total / count);
