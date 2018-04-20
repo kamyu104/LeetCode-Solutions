6b1380136945a3e6b82b29c92a51b9a254fb985d
kamyu <kamyu104@users.noreply.github.com>
2018-04-20 23:50:08 +0800

    Update image-smoother.cpp

--- C++/image-smoother.cpp
+++ C++/image-smoother.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     vector<vector<int>> imageSmoother(vector<vector<int>>& M) {
-        const auto m = M.size(), n = M[0].size();
+        const auto& m = M.size(), &n = M[0].size();
         vector<vector<int>> result(M);
         for (int i = 0; i < m; ++i) {
             for (int j = 0; j < n; ++j) {
@@ -16,7 +16,7 @@ public:
 
 private:
     int getGray(const vector<vector<int>>& M, int i, int j) {
-        const auto& m = M.size(), n = M[0].size();
+        const auto& m = M.size(), &n = M[0].size();
         double total = 0.0;
         int count = 0;
         for (int r = -1; r < 2; ++r) {
