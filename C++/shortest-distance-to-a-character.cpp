ab69283c24346d9f5a047a1a43cc60a035e4c280
kamyu <kamyu104@users.noreply.github.com>
2018-04-22 17:52:43 +0800

    Create shortest-distance-to-a-character.cpp

---
+++ C++/shortest-distance-to-a-character.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> shortestToChar(string S, char C) {
+        vector<int> result(S.length(), S.length());
+        int prev = -S.length();
+        for (int i = 0; i < S.length(); ++i) {
+            if (S[i] == C) {
+                prev = i;
+            }
+            result[i] = min(result[i], abs(i - prev));
+        }
+        for (int i = S.length() - 1; i >= 0; --i) {
+            if (S[i] == C) {
+                prev = i;
+            }
+            result[i] = min(result[i], abs(i - prev));
+        }
+        return result;
+    }
+};
