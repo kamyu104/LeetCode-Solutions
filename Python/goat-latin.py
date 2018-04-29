837645ac5faa2ed78475f60bb3eb430a085e3774
Allen Liu <kamyu104@gmail.com>
2018-04-29 17:39:11 +0800

    Update goat-latin.py

--- Python/goat-latin.py
+++ Python/goat-latin.py
@@ -49,10 +49,10 @@ class Solution(object):
         :type S: str
         :rtype: str
         """
-        def convert():
+        def convert(S):
             vowel = set('aeiouAEIOU')
             for i, word in enumerate(S.split(), 1):
                 if word[0] not in vowel:
                     word = word[1:] + word[:1]
                 yield word + 'ma' + 'a'*i
-        return " ".join(convert())
+        return " ".join(convert(S))
