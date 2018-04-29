3225f0279b150835a0822316e6405db4439a3538
kamyu <kamyu104@users.noreply.github.com>
2018-04-29 17:24:12 +0800

    Update string-to-integer-atoi.py

--- Python/string-to-integer-atoi.py
+++ Python/string-to-integer-atoi.py
@@ -45,21 +45,22 @@ class Solution(object):
 
         if len(str) == i:
             return result
-        else:
-            sign = 1
-            if str[i] == "+":
-                i += 1
-            elif str[i] == "-":
-                sign = -1
-                i += 1
 
-            while i < len(str) and '0' <= str[i] <= '9':
-                if result > (INT_MAX - int(str[i])) / 10:
-                    return INT_MAX if sign > 0 else INT_MIN
-                result = result * 10 + int(str[i])
-                i += 1
+        sign = 1
+        if str[i] == "+":
+            i += 1
+        elif str[i] == "-":
+            sign = -1
+            i += 1
 
-            return sign * result
+        while i < len(str) and '0' <= str[i] <= '9':
+            if result > (INT_MAX - int(str[i])) / 10:
+                return INT_MAX if sign > 0 else INT_MIN
+            result = result * 10 + int(str[i])
+            i += 1
+
+        return sign * result
+
 
 if __name__ == "__main__":
     print Solution().atoi("")
