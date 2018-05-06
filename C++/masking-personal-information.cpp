e0c3fe3839ae8ea86781aa5c640583397c8009c3
kamyu <kamyu104@users.noreply.github.com>
2018-05-07 00:52:04 +0800

    Create masking-personal-information.cpp

---
+++ C++/masking-personal-information.cpp
@@ -0,0 +1,29 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    string maskPII(string S) {
+        auto at = S.find("@");
+        if (at != string::npos) {
+            transform(S.begin(), S.end(), S.begin(), ::tolower);
+            return S.substr(0, 1) + "*****" + S.substr(at - 1);
+        }
+        string digits;
+        for (const auto& c : S) {
+            if (::isdigit(c)) {
+                digits.push_back(c);
+            }
+        }
+        string local{"***-***-"};
+        local += digits.substr(digits.length() - 4);
+        if (digits.length() == 10) {
+            return local;
+        }
+        return digits.length() == 10 ?
+                   local : "+" +
+                           string(digits.length() - 10, '*') +
+                           "-" +
+                           local;
+    }
+};
