31aafd389d0bccdeb0e6e145af3c64fb4993633e
kamyu <kamyu104@users.noreply.github.com>
2018-05-13 22:59:38 +0800

    Update find-and-replace-in-string.cpp

--- C++/find-and-replace-in-string.cpp
+++ C++/find-and-replace-in-string.cpp
@@ -1,18 +1,25 @@
-// Time:  O(n * m), m is the number of targets
+// Time:  O(n + m), m is the number of targets
 // Space: O(n)
 
 class Solution {
 public:
     string findReplaceString(string S, vector<int>& indexes, vector<string>& sources, vector<string>& targets) {
         vector<pair<int, string>> bucket(S.size());
-        for (auto i = 0; i < indexes.size(); ++i)  {
+        for (int i = 0; i < indexes.size(); ++i)  {
             if (S.find(sources[i], indexes[i]) == indexes[i]) {
                 bucket[indexes[i]] = {sources[i].size(), targets[i]};
             }
         }
-        for (int i = S.size() - 1; i >= 0; --i) {
-            S.replace(i, bucket[i].first, bucket[i].second);
+        string result;
+        int i = 0, last = 0;
+        for (i = 0; i < S.length(); ++i) {
+            if (bucket[i].first) {
+                result += S.substr(last, i - last);
+                result += bucket[i].second;
+                last = i + bucket[i].first;
+            }
         }
-        return S;
+        result += S.substr(last, i - last);
+        return result;
     }
 };
