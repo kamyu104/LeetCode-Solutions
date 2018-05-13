0b5524bc1e8fab6a9e059b0a3e753b1f93a50d90
kamyu <kamyu104@users.noreply.github.com>
2018-05-13 23:37:36 +0800

    Create flipping-an-image.cpp

---
+++ C++/flipping-an-image.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        for (auto& row : A) {
+            for (int i = 0; i < (row.size() + 1) / 2; ++i) {
+                row[i] ^= 1;
+                if (i != row.size() + ~i) {
+                    row[row.size() + ~i] ^= 1;
+                }
+                swap(row[i], row[row.size() + ~i]);
+            }
+        }
+        return A;
+    }
+};
