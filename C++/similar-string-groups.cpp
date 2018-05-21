966b7146730bb3ecce7e3964afe4ff94e1c0c30e
kamyu <kamyu104@users.noreply.github.com>
2018-05-22 00:51:46 +0800

    Update similar-string-groups.cpp

--- C++/similar-string-groups.cpp
+++ C++/similar-string-groups.cpp
@@ -58,3 +58,86 @@ private:
             int size_;
     };
 };
+
+// Time:  O(n*l*min(n, l^2))
+// Space: O(n*l^3)
+class Solution_MLE {
+public:
+    int numSimilarGroups(vector<string>& A) {
+        const int N = A.size(), L = A[0].length();
+        UnionFind union_find(A.size());
+        if (N < L*L) {
+            for (int i = 0; i < N; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    if (isSimilar(A[i], A[j])) {
+                        union_find.union_set(i, j);
+                    }
+                }
+            }
+        } else {
+            unordered_map<string, vector<int>> buckets;
+            for (int i = 0; i < A.size(); ++i) {
+                auto word = A[i];
+                buckets[word].emplace_back(i);
+                for (int j1 = 0; j1 < L; ++j1) {
+                    for (int j2 = 0; j2 < j1; ++j2) {
+                        swap(word[j1], word[j2]);
+                        buckets[word].emplace_back(i);
+                        swap(word[j1], word[j2]);
+                    }
+                }
+            }
+            for (const auto& word : A) {
+                for (int i = 0; i < buckets[word].size(); ++i) {
+                    for (int j = 0; j < i; ++j) {
+                        union_find.union_set(buckets[word][i], buckets[word][j]);
+                    }
+                }
+            }
+        }
+        return union_find.size();
+    }
+    
+private:
+    bool isSimilar(const string &a, const string &b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); ++i) {
+            if (a[i] != b[i]) {
+                ++diff;
+            }
+        }
+        return diff == 2;
+    }
+    
+    class UnionFind {
+        public:
+            UnionFind(const int n) : set_(n), size_(n) {
+                iota(set_.begin(), set_.end(), 0);
+            }
+
+            int find_set(const int x) {
+               if (set_[x] != x) {
+                   set_[x] = find_set(set_[x]);  // Path compression.
+               }
+               return set_[x];
+            }
+
+            bool union_set(const int x, const int y) {
+                int x_root = find_set(x), y_root = find_set(y);
+                if (x_root == y_root) {
+                    return false;
+                }
+                set_[min(x_root, y_root)] = max(x_root, y_root);
+                --size_;
+                return true;
+            }
+        
+            int size() const {
+                return size_;
+            }
+
+        private:
+            vector<int> set_;
+            int size_;
+    };
+};
