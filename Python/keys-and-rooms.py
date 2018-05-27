d42e2911d054e63e0e01f9803c8df839f92bdcda
kamyu <kamyu104@users.noreply.github.com>
2018-05-28 03:00:32 +0800

    Update keys-and-rooms.py

--- Python/keys-and-rooms.py
+++ Python/keys-and-rooms.py
@@ -1,3 +1,6 @@
+# Time:  O(n!)
+# Space: O(n)
+
 # There are N rooms and you start in room 0.
 # Each room has a distinct number in 0, 1, 2, ..., N-1,
 # and each room may have some keys to access the next room.
