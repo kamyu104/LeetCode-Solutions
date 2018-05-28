051721dc1affcce2660530773efc877f0588491b
kamyu <kamyu104@users.noreply.github.com>
2018-05-28 19:50:51 +0800

    Update guess-the-word.cpp

--- C++/guess-the-word.cpp
+++ C++/guess-the-word.cpp
@@ -22,7 +22,7 @@ public:
         vector<int> possible(wordlist.size());
         iota(possible.begin(), possible.end(), 0);
         int n = 0;
-        while (!possible.empty() && n < 6) {
+        while (n < 6) {
             auto guess = solve(H, possible);
             n = master.guess(wordlist[guess]);
             vector<int> new_possible;
@@ -78,34 +78,51 @@ private:
 class Solution2 {
 public:
     void findSecretWord(vector<string>& wordlist, Master& master) {
-        for (int i = 0, n = 0; i < 10 && n < 6; ++i) {
-            unordered_map<string, int> count;
-            for (const auto& w1 : wordlist) {
-                for (const auto& w2 : wordlist) {
-                    if (match(w1, w2) == 0) {
-                        ++count[w1];
-                    }
+        vector<vector<int>> H(wordlist.size(), vector<int>(wordlist.size()));
+        for (int i = 0; i < wordlist.size(); ++i) {
+            for (int j = 0; j < wordlist.size(); ++j) {
+                H[i][j] = match(wordlist[i], wordlist[j]);
+            }
+        }
+
+        vector<int> possible(wordlist.size());
+        iota(possible.begin(), possible.end(), 0);
+        int n = 0;
+        while (n < 6) {
+            auto guess = solve(H, possible);
+            n = master.guess(wordlist[guess]);
+            vector<int> new_possible;
+            for (const auto& j : possible) {
+                if (H[guess][j] == n) {
+                    new_possible.emplace_back(j);
                 }
             }
-            string guess;
-            for (const auto& w : wordlist) {
-                if (guess.empty() ||
-                    count[w] < count[guess]) {
-                    guess = w;
-                }
-            }
-            n = master.guess(guess);
-            vector<string> new_wordlist;
-            for (const auto& w : wordlist) {
-                if (match(w, guess) == n) {
-                    new_wordlist.emplace_back(w);
-                }
-            }
-            wordlist = new_wordlist;
+            possible = new_possible;
         }
     }
 
 private:
+    int solve(const vector<vector<int>>& H,
+              const vector<int>& possible) {
+
+        vector<int> min_max_group = possible;
+        int best_guess = -1; 
+        for (const auto& guess : possible) {
+            vector<vector<int>> groups(7);
+            for (const auto& j : possible) {
+                if (j != guess) {
+                    groups[H[guess][j]].emplace_back(j);
+                }
+            }
+            int max_group_i = 0;
+            if (groups[max_group_i].size() < min_max_group.size()) {
+                min_max_group = groups[max_group_i];
+                best_guess = guess;
+            }
+        }
+        return best_guess;
+    }
+
     int match(const string& a, const string& b) {
         int matches = 0;
         for (int i = 0; i < a.length(); ++i) {
