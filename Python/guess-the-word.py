da059fe99e154710464be90e7c29b5a6ae5b2c21
kamyu <kamyu104@users.noreply.github.com>
2018-05-28 19:39:54 +0800

    Update guess-the-word.py

--- Python/guess-the-word.py
+++ Python/guess-the-word.py
@@ -70,29 +70,27 @@ class Solution(object):
         :type master: Master
         :rtype: None
         """
-        def solve(H, possible, lookup):
+        def solve(H, possible):
             min_max_group, best_guess = possible, None
-            for guess in xrange(len(H)):
-                if guess not in lookup:
-                    groups = [[] for _ in xrange(7)]
-                    for j in possible:
-                        if j != guess:
-                            groups[H[guess][j]].append(j)
-                    max_group = max(groups, key=len)
-                    if len(max_group) < len(min_max_group):
-                        min_max_group, best_guess = max_group, guess
+            for guess in possible:
+                groups = [[] for _ in xrange(7)]
+                for j in possible:
+                    if j != guess:
+                        groups[H[guess][j]].append(j)
+                max_group = max(groups, key=len)
+                if len(max_group) < len(min_max_group):
+                    min_max_group, best_guess = max_group, guess
             return best_guess
 
         H = [[sum(a == b for a, b in itertools.izip(wordlist[i], wordlist[j]))
                   for j in xrange(len(wordlist))]
                   for i in xrange(len(wordlist))]
-        possible, lookup = range(len(wordlist)), set()
+        possible = range(len(wordlist))
         n = 0
         while possible and n < 6:
-            guess = solve(H, possible, lookup)
+            guess = solve(H, possible)
             n = master.guess(wordlist[guess])
             possible = [j for j in possible if H[guess][j] == n]
-            lookup.add(guess)
 
 
 # Time:  O(n^2)
