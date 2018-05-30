6b20fdd6556aa841a71a4d97493e2e2ecf7c70b6
kamyu <kamyu104@users.noreply.github.com>
2018-05-30 09:42:45 +0800

    Update remove-duplicates-from-sorted-array.py

--- Python/remove-duplicates-from-sorted-array.py
+++ Python/remove-duplicates-from-sorted-array.py
@@ -11,21 +11,17 @@
 # Your function should return length = 2, and A is now [1,2].
 #
 
-class Solution:
+
+class Solution(object):
     # @param a list of integers
     # @return an integer
     def removeDuplicates(self, A):
         if not A:
             return 0
 
-        last, i = 0, 1
-        while i < len(A):
+        last = 0
+        for i in xrange(len(A)):
             if A[last] != A[i]:
                 last += 1
                 A[last] = A[i]
-            i += 1
-
         return last + 1
-
-if __name__ == "__main__":
-    print Solution().removeDuplicates([1, 1, 2])
