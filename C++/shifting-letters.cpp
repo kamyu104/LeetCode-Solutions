160162395cc69e87790f783d0c7bc1c2fb91104f
kamyu <kamyu104@users.noreply.github.com>
2018-06-10 16:16:49 +0800

    Update shifting-letters.cpp

--- C++/shifting-letters.cpp
+++ C++/shifting-letters.cpp
@@ -8,10 +8,7 @@ public:
         auto times = accumulate(shifts.cbegin(), shifts.cend(), 0L) % 26;
         for (int i = 0; i < S.length(); ++i) {
             result.push_back('a' + (S[i] - 'a' + times) % 26);
-            times = (times - shifts[i]) % 26;
-            if (times < 0) {
-                times += 26;
-            }
+            times = ((times - shifts[i]) % 26 + 26) % 26;
         }
         return result;
     }
