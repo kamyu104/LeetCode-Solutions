1cc7c556db6c3496d4fba42408eefe8348a97183
kamyu <kamyu104@users.noreply.github.com>
2018-06-18 19:20:50 +0800

    Update exam-room.cpp

--- C++/exam-room.cpp
+++ C++/exam-room.cpp
@@ -6,7 +6,7 @@ class ExamRoom {
 public:
     ExamRoom(int N) : num_(N) {
         segment_iters_[make_pair(-1, num_)] = 
-            max_bst_.emplace(make_shared<Segment>(-1, num_, num_, 0));
+            max_bst_.emplace(make_shared<Segment>(num_, 0, -1, num_));
         seats_[-1] = make_pair(-1, num_);
         seats_[num_] = make_pair(-1, num_);
     }
@@ -16,28 +16,28 @@ public:
         segment_iters_.erase(make_pair(curr->l, curr->r));
         if (curr->l == -1 && curr->r == num_) {
             segment_iters_[make_pair(curr->l + 1, curr->r)] = max_bst_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r - 1,
                                      curr->r - 1,
-                                     curr->r - 1));
+                                     curr->l + 1, curr->r));
         } else if (curr->l == -1) {
             segment_iters_[make_pair(curr->l + 1, curr->r)] = max_bst_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r / 2,
                                      curr->r / 2,
-                                     curr->r / 2));
+                                     curr->l + 1, curr->r));
         } else if (curr->r == num_) {
             segment_iters_[make_pair(curr->l, curr->r - 1)] = max_bst_.emplace(
-                make_shared<Segment>(curr->l, curr->r - 1,
-                                     (curr->r - 1 - curr->l) / 2,
-                                     (curr->r - 1 - curr->l) / 2 + curr->l)); 
+                make_shared<Segment>((curr->r - 1 - curr->l) / 2,
+                                     (curr->r - 1 - curr->l) / 2 + curr->l,
+                                     curr->l, curr->r - 1)); 
         } else {
             segment_iters_[make_pair(curr->l, curr->pos)] = max_bst_.emplace(
-                make_shared<Segment>(curr->l, curr->pos,
-                                     (curr->pos - curr->l) / 2,
-                                     (curr->pos - curr->l) / 2 + curr->l));
+                make_shared<Segment>((curr->pos - curr->l) / 2,
+                                     (curr->pos - curr->l) / 2 + curr->l,
+                                     curr->l, curr->pos));
             segment_iters_[make_pair(curr->pos, curr->r)] = max_bst_.emplace(
-                make_shared<Segment>(curr->pos, curr->r,
-                                     (curr->r - curr->pos) / 2,
-                                     (curr->r - curr->pos) / 2 + curr->pos));
+                make_shared<Segment>((curr->r - curr->pos) / 2,
+                                     (curr->r - curr->pos) / 2 + curr->pos,
+                                     curr->pos, curr->r));
         }
         seats_[curr->pos] = make_pair(curr->l, curr->r);
         seats_[curr->l].second = curr->pos;
@@ -59,24 +59,24 @@ public:
         
         if (neighbors.first == -1 && neighbors.second == num_) {
             segment_iters_[neighbors] = max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1, 
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.first == -1) {
             segment_iters_[neighbors] = max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1,
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.second == num_) {
             segment_iters_[neighbors] = max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second - 1 - neighbors.first,
-                                     neighbors.second - 1));
+                make_shared<Segment>(neighbors.second - 1 - neighbors.first,
+                                     neighbors.second - 1, 
+                                     neighbors.first, neighbors.second));
         } else {
             segment_iters_[neighbors] = max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     (neighbors.second - neighbors.first) / 2,
-                                     (neighbors.second - neighbors.first) / 2 + neighbors.first));
+                make_shared<Segment>((neighbors.second - neighbors.first) / 2,
+                                     (neighbors.second - neighbors.first) / 2 + neighbors.first,
+                                     neighbors.first, neighbors.second));
         }
         seats_[neighbors.first].second = neighbors.second;
         seats_[neighbors.second].first = neighbors.first;
@@ -84,12 +84,12 @@ public:
     
 private:
     struct Segment {
-        int l;
-        int r;
         int dis;
         int pos;
-        Segment(int l, int r, int dis, int pos) : 
-            l(l), r(r), dis(dis), pos(pos) {
+        int l;
+        int r;
+        Segment(int dis, int pos, int l, int r) : 
+            dis(dis), pos(pos), l(l), r(r) {
         }
     };
     
@@ -125,7 +125,7 @@ private:
 class ExamRoom2 {
 public:
     ExamRoom2(int N) : num_(N) {
-        max_bst_.emplace(make_shared<Segment>(-1, num_, num_, 0));
+        max_bst_.emplace(make_shared<Segment>(num_, 0, -1, num_));
         seats_[-1] = make_pair(-1, num_);
         seats_[num_] = make_pair(-1, num_);
     }
@@ -141,28 +141,28 @@ public:
         const auto curr = *max_bst_.begin(); max_bst_.erase(max_bst_.begin());
         if (curr->l == -1 && curr->r == num_) {
             max_bst_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r - 1,
                                      curr->r - 1,
-                                     curr->r - 1));
+                                     curr->l + 1, curr->r));
         } else if (curr->l == -1) {
             max_bst_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r / 2,
                                      curr->r / 2,
-                                     curr->r / 2));
+                                     curr->l + 1, curr->r));
         } else if (curr->r == num_) {
             max_bst_.emplace(
-                make_shared<Segment>(curr->l, curr->r - 1,
-                                     (curr->r - 1 - curr->l) / 2,
-                                     (curr->r - 1 - curr->l) / 2 + curr->l)); 
+                make_shared<Segment>((curr->r - 1 - curr->l) / 2,
+                                     (curr->r - 1 - curr->l) / 2 + curr->l,
+                                     curr->l, curr->r - 1)); 
         } else {
             max_bst_.emplace(
-                make_shared<Segment>(curr->l, curr->pos,
-                                     (curr->pos - curr->l) / 2,
-                                     (curr->pos - curr->l) / 2 + curr->l));
+                make_shared<Segment>((curr->pos - curr->l) / 2,
+                                     (curr->pos - curr->l) / 2 + curr->l,
+                                     curr->l, curr->pos));
             max_bst_.emplace(
-                make_shared<Segment>(curr->pos, curr->r,
-                                     (curr->r - curr->pos) / 2,
-                                     (curr->r - curr->pos) / 2 + curr->pos));
+                make_shared<Segment>((curr->r - curr->pos) / 2,
+                                     (curr->r - curr->pos) / 2 + curr->pos,
+                                     curr->pos, curr->r));
         }
         seats_[curr->pos] = make_pair(curr->l, curr->r);
         seats_[curr->l].second = curr->pos;
@@ -175,24 +175,24 @@ public:
         seats_.erase(p);
         if (neighbors.first == -1 && neighbors.second == num_) {
             max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1,
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.first == -1) {
             max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1,
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.second == num_) {
             max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second - 1 - neighbors.first,
-                                     neighbors.second - 1));
+                make_shared<Segment>(neighbors.second - 1 - neighbors.first,
+                                     neighbors.second - 1,
+                                     neighbors.first, neighbors.second));
         } else {
             max_bst_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     (neighbors.second - neighbors.first) / 2,
-                                     (neighbors.second - neighbors.first) / 2 + neighbors.first));
+                make_shared<Segment>((neighbors.second - neighbors.first) / 2,
+                                     (neighbors.second - neighbors.first) / 2 + neighbors.first,
+                                     neighbors.first, neighbors.second));
         }
         seats_[neighbors.first].second = neighbors.second;
         seats_[neighbors.second].first = neighbors.first;
@@ -200,12 +200,12 @@ public:
     
 private:
     struct Segment {
-        int l;
-        int r;
         int dis;
         int pos;
-        Segment(int l, int r, int dis, int pos) : 
-            l(l), r(r), dis(dis), pos(pos) {
+        int l;
+        int r;
+        Segment(int dis, int pos, int l, int r) : 
+            dis(dis), pos(pos), l(l), r(r) {
         }
     };
     
@@ -230,7 +230,7 @@ private:
 class ExamRoom3 {
 public:
     ExamRoom3(int N) : num_(N) {
-        max_heap_.emplace(make_shared<Segment>(-1, num_, num_, 0));
+        max_heap_.emplace(make_shared<Segment>(num_, 0, -1, num_));
         seats_[-1] = make_pair(-1, num_);
         seats_[num_] = make_pair(-1, num_);
     }
@@ -246,28 +246,28 @@ public:
         const auto curr = max_heap_.top(); max_heap_.pop();
         if (curr->l == -1 && curr->r == num_) {
             max_heap_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r - 1,
                                      curr->r - 1,
-                                     curr->r - 1));
+                                     curr->l + 1, curr->r));
         } else if (curr->l == -1) {
             max_heap_.emplace(
-                make_shared<Segment>(curr->l + 1, curr->r,
+                make_shared<Segment>(curr->r / 2,
                                      curr->r / 2,
-                                     curr->r / 2));
+                                     curr->l + 1, curr->r));
         } else if (curr->r == num_) {
             max_heap_.emplace(
-                make_shared<Segment>(curr->l, curr->r - 1,
-                                     (curr->r - 1 - curr->l) / 2,
-                                     (curr->r - 1 - curr->l) / 2 + curr->l)); 
+                make_shared<Segment>((curr->r - 1 - curr->l) / 2,
+                                     (curr->r - 1 - curr->l) / 2 + curr->l,
+                                     curr->l, curr->r - 1)); 
         } else {
             max_heap_.emplace(
-                make_shared<Segment>(curr->l, curr->pos,
-                                     (curr->pos - curr->l) / 2,
-                                     (curr->pos - curr->l) / 2 + curr->l));
+                make_shared<Segment>((curr->pos - curr->l) / 2,
+                                     (curr->pos - curr->l) / 2 + curr->l,
+                                     curr->l, curr->pos));
             max_heap_.emplace(
-                make_shared<Segment>(curr->pos, curr->r,
-                                     (curr->r - curr->pos) / 2,
-                                     (curr->r - curr->pos) / 2 + curr->pos));
+                make_shared<Segment>((curr->r - curr->pos) / 2,
+                                     (curr->r - curr->pos) / 2 + curr->pos,
+                                     curr->pos, curr->r));
         }
         seats_[curr->pos] = make_pair(curr->l, curr->r);
         seats_[curr->l].second = curr->pos;
@@ -280,24 +280,24 @@ public:
         seats_.erase(p);
         if (neighbors.first == -1 && neighbors.second == num_) {
             max_heap_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1,
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.first == -1) {
             max_heap_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second,
-                                     neighbors.first + 1));
+                make_shared<Segment>(neighbors.second,
+                                     neighbors.first + 1,
+                                     neighbors.first, neighbors.second));
         } else if (neighbors.second == num_) {
             max_heap_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     neighbors.second - 1 - neighbors.first,
-                                     neighbors.second - 1));
+                make_shared<Segment>(neighbors.second - 1 - neighbors.first,
+                                     neighbors.second - 1,
+                                     neighbors.first, neighbors.second));
         } else {
             max_heap_.emplace(
-                make_shared<Segment>(neighbors.first, neighbors.second,
-                                     (neighbors.second - neighbors.first) / 2,
-                                     (neighbors.second - neighbors.first) / 2 + neighbors.first));
+                make_shared<Segment>((neighbors.second - neighbors.first) / 2,
+                                     (neighbors.second - neighbors.first) / 2 + neighbors.first,
+                                     neighbors.first, neighbors.second));
         }
         seats_[neighbors.first].second = neighbors.second;
         seats_[neighbors.second].first = neighbors.first;
@@ -305,12 +305,12 @@ public:
     
 private:
     struct Segment {
-        int l;
-        int r;
         int dis;
         int pos;
-        Segment(int l, int r, int dis, int pos) : 
-            l(l), r(r), dis(dis), pos(pos) {
+        int l;
+        int r;
+        Segment(int dis, int pos, int l, int r) : 
+            dis(dis), pos(pos), l(l), r(r) {
         }
     };
     
