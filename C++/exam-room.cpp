3e6ebce83173f9797f6b5db0426cc8fc0651521c
kamyu <kamyu104@users.noreply.github.com>
2018-06-18 14:50:30 +0800

    Update exam-room.cpp

--- C++/exam-room.cpp
+++ C++/exam-room.cpp
@@ -5,7 +5,7 @@
 class ExamRoom {
 public:
     ExamRoom(int N) : num_(N) {
-        max_heap.emplace(new Segment(-1, num_, num_, 0));
+        max_heap.emplace(make_shared<Segment>(-1, num_, num_, 0));
         neighbors[-1] = make_pair(-1, num_);
         neighbors[num_] = make_pair(-1, num_);
     }
@@ -19,33 +19,33 @@ public:
         const auto curr = max_heap.top(); max_heap.pop();
         if (curr->l == -1 && curr->r == num_) {
             max_heap.emplace(
-                new Segment(curr->l + 1, curr->r,
-                            curr->r - 1,
-                            curr->r - 1));
+                make_shared<Segment>(curr->l + 1, curr->r,
+                                     curr->r - 1,
+                                     curr->r - 1));
             neighbors[curr->l + 1] = make_pair(curr->l, curr->r);
         } else if (curr->l == -1) {
             max_heap.emplace(
-                new Segment(curr->l + 1, curr->r,
-                            curr->r / 2,
-                            curr->r / 2));
+                make_shared<Segment>(curr->l + 1, curr->r,
+                                    curr->r / 2,
+                                    curr->r / 2));
             neighbors[curr->l + 1] = make_pair(curr->l, curr->r);
             neighbors[curr->r].first = curr->l + 1;
         } else if (curr->r == num_) {
             max_heap.emplace(
-                new Segment(curr->l, curr->r - 1,
-                            (curr->r - 1 - curr->l) / 2,
-                            (curr->r - 1 - curr->l) / 2 + curr->l)); 
+                make_shared<Segment>(curr->l, curr->r - 1,
+                                    (curr->r - 1 - curr->l) / 2,
+                                    (curr->r - 1 - curr->l) / 2 + curr->l)); 
             neighbors[curr->r - 1] = make_pair(curr->l, curr->r);
             neighbors[curr->l].second = curr->r - 1;
         } else {
             max_heap.emplace(
-                new Segment(curr->l, curr->pos,
-                            (curr->pos - curr->l) / 2,
-                            (curr->pos - curr->l) / 2 + curr->l));
+                make_shared<Segment>(curr->l, curr->pos,
+                                    (curr->pos - curr->l) / 2,
+                                    (curr->pos - curr->l) / 2 + curr->l));
             max_heap.emplace(
-                new Segment(curr->pos, curr->r,
-                            (curr->r - curr->pos) / 2,
-                            (curr->r - curr->pos) / 2 + curr->pos));
+                make_shared<Segment>(curr->pos, curr->r,
+                                    (curr->r - curr->pos) / 2,
+                                    (curr->r - curr->pos) / 2 + curr->pos));
             neighbors[curr->pos] = make_pair(curr->l, curr->r);
             neighbors[curr->l].second = curr->pos;
             neighbors[curr->r].first = curr->pos;
@@ -58,26 +58,26 @@ public:
         neighbors.erase(p);
         if (nei.first == -1 && nei.second == num_) {
             max_heap.emplace(
-                new Segment(nei.first, nei.second,
-                            nei.second,
-                            nei.first + 1));  
+                make_shared<Segment>(nei.first, nei.second,
+                                    nei.second,
+                                    nei.first + 1));  
         } else if (nei.first == -1) {
             max_heap.emplace(
-                new Segment(nei.first, nei.second,
-                            nei.second,
-                            nei.first + 1));
+                make_shared<Segment>(nei.first, nei.second,
+                                     nei.second,
+                                     nei.first + 1));
             neighbors[nei.second].first = -1;
         } else if (nei.second == num_) {
             max_heap.emplace(
-                new Segment(nei.first, nei.second,
-                            nei.second - 1 - nei.first,
-                            nei.second - 1));
+                make_shared<Segment>(nei.first, nei.second,
+                                     nei.second - 1 - nei.first,
+                                     nei.second - 1));
             neighbors[nei.first].second = nei.second;
         } else {
             max_heap.emplace(
-                new Segment(nei.first, nei.second,
-                            (nei.second - nei.first) / 2,
-                            (nei.second - nei.first) / 2 + nei.first));   
+                make_shared<Segment>(nei.first, nei.second,
+                                     (nei.second - nei.first) / 2,
+                                     (nei.second - nei.first) / 2 + nei.first));   
             neighbors[nei.first].second = nei.second;
             neighbors[nei.second].first = nei.first;
         }
