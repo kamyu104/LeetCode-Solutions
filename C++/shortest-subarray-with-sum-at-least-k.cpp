1188831fa5e46c06207c55425ec9a4615ece5643
kamyu <kamyu104@users.noreply.github.com>
2018-07-01 19:24:31 +0800

    Create shortest-subarray-with-sum-at-least-k.cpp

---
+++ C++/shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int shortestSubarray(vector<int>& A, int K) {
+        vector<int> accumulated_sum(A.size() + 1, 0);
+        partial_sum(A.cbegin(), A.cend(), next(accumulated_sum.begin()), plus<int>());
+        
+        int result = numeric_limits<int>::max();
+        deque<int> mono_increasing_q;
+        for (int i = 0; i < accumulated_sum.size(); ++i) {
+            while (!mono_increasing_q.empty() &&
+                   accumulated_sum[i] <= accumulated_sum[mono_increasing_q.back()]) {
+                mono_increasing_q.pop_back();
+            }
+            while (!mono_increasing_q.empty() &&
+                   accumulated_sum[i] - accumulated_sum[mono_increasing_q.front()] >= K) {
+                result = min(result, i - mono_increasing_q.front());
+                mono_increasing_q.pop_front();
+            }
+            mono_increasing_q.emplace_back(i);
+        }
+        return result != numeric_limits<int>::max() ? result : -1;
+    }
+};
