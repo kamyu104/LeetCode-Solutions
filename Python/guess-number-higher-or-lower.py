948aa30fc08033357ccbeff42d9293ae8e1b21f4
Sergey Sudakovich <sergey@cumulusnetworks.com>
2018-07-07 13:56:16 -0700

    Make flake8 happy for Python2.7

--- Python/guess-number-higher-or-lower.py
+++ Python/guess-number-higher-or-lower.py
@@ -25,7 +25,7 @@ class Solution(object):
         left, right = 1, n
         while left <= right:
             mid = left + (right - left) / 2
-            if guess(mid) <= 0:
+            if guess(mid) <= 0: # noqa
                 right = mid - 1
             else:
                 left = mid + 1
