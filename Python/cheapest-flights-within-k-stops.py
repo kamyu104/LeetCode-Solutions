d5e8b1bba7c4689ce6c6e89493a529b3ce5ffb60
kamyu <kamyu104@users.noreply.github.com>
2018-07-08 16:06:56 +0800

    Update cheapest-flights-within-k-stops.py

--- Python/cheapest-flights-within-k-stops.py
+++ Python/cheapest-flights-within-k-stops.py
@@ -50,9 +50,9 @@ class Solution(object):
         for u, v, w in flights:
             adj[u].append((v, w))
         best = collections.defaultdict(lambda: collections.defaultdict(lambda: float("inf")))
-        min_heap = [(0, src, K+1)]
+        min_heap = [(0, K+1, src)]
         while min_heap:
-            result, u, k = heapq.heappop(min_heap)
+            result, k, u = heapq.heappop(min_heap)
             if k < 0 or best[k][u] < result:
                 continue
             if u == dst:
@@ -60,5 +60,5 @@ class Solution(object):
             for v, w in adj[u]:
                 if result+w < best[k-1][v]:
                     best[k-1][v] = result+w                    
-                    heapq.heappush(min_heap, (result+w, v, k-1))
+                    heapq.heappush(min_heap, (result+w, k-1, v))
         return -1
