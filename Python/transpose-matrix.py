07e774a9dc6a6f3d8707abfb1b7ee1eebf26cde6
kamyu <kamyu104@users.noreply.github.com>
2018-07-08 19:10:45 +0800

    Update transpose-matrix.py

--- Python/transpose-matrix.py
+++ Python/transpose-matrix.py
@@ -36,3 +36,14 @@ class Solution(object):
             for c, val in enumerate(row):
                 result[c][r] = val
         return result
+
+
+# Time:  O(r * c)
+# Space: O(1)
+class Solution2(object):
+    def transpose(self, A):
+        """
+        :type A: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return zip(*A)
