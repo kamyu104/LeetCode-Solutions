9c7e5b21807f72c6cadf20083cab3c2ff12a76ed
kamyu <kamyu104@users.noreply.github.com>
2018-07-14 16:53:10 +0800

    Update serialize-and-deserialize-n-ary-tree.py

--- Python/serialize-and-deserialize-n-ary-tree.py
+++ Python/serialize-and-deserialize-n-ary-tree.py
@@ -1,13 +1,13 @@
 # Time:  O(n)
 # Space: O(h)
 
-"""
 # Definition for a Node.
 class Node(object):
     def __init__(self, val, children):
         self.val = val
         self.children = children
-"""
+
+
 class Codec:
 
     def serialize(self, root):
