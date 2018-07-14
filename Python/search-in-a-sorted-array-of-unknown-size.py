98be5f03b7f2479b3ef1b0991280feaba376c950
kamyu <kamyu104@users.noreply.github.com>
2018-07-14 19:29:57 +0800

    Create search-in-a-sorted-array-of-unknown-size.py

---
+++ Python/search-in-a-sorted-array-of-unknown-size.py
@@ -0,0 +1,21 @@
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, 19999
+        while left <= right:
+            mid = left + (right-left)//2
+            response = reader.get(mid)
+            if response > target:
+                right = mid-1
+            elif response < target:
+                left = mid+1
+            else:
+                return mid
+        return -1
