8f709d5237082ed689fdf3a97358506fa602c179
kamyu <kamyu104@users.noreply.github.com>
2018-07-14 19:18:49 +0800

    Create flatten-a-multilevel-doubly-linked-list.py

---
+++ Python/flatten-a-multilevel-doubly-linked-list.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Definition for a Node.
+class Node(object):
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+class Solution(object):
+    def flatten(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        curr = head
+        while curr:
+            if curr.child:
+                curr_next = curr.next
+                curr.child.prev = curr
+                curr.next = curr.child
+                last_child = curr
+                while last_child.next:
+                    last_child = last_child.next
+                if curr_next:
+                    last_child.next = curr_next
+                    curr_next.prev = last_child
+                curr.child = None
+            curr = curr.next
+        return head
