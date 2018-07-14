f3784e8cdf939f5484fdf494558cc1cf759acfee
kamyu <kamyu104@users.noreply.github.com>
2018-07-14 19:40:29 +0800

    Create binary-search.cpp

---
+++ C++/binary-search.cpp
@@ -0,0 +1,20 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            auto mid = left + (right-left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
