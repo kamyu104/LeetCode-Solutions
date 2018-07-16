869318de56e3682c4d09a6f17a403712d7e52f62
kamyu <kamyu104@users.noreply.github.com>
2018-07-16 22:00:55 +0800

    Create implement-rand10-using-rand7.py

---
+++ Python/implement-rand10-using-rand7.py
@@ -0,0 +1,43 @@
+# Time:  O(1)
+# Space: O(1)
+
+# Given a function rand7 which generates a uniform random integer in the range 1 to 7,
+# write a function rand10 which generates a uniform random integer in the range 1 to 10.
+#
+# Do NOT use system's Math.random().
+#
+# Example 1:
+#
+# Input: 1
+# Output: [7]
+# Example 2:
+#
+# Input: 2
+# Output: [8,4]
+# Example 3:
+#
+# Input: 3
+# Output: [8,1,10]
+#
+# Note:
+#
+# rand7 is predefined.
+# Each testcase has one argument: n, the number of times that rand10 is called.
+#
+# Follow up:
+# - What is the expected value for the number of calls to rand7() function?
+# - Could you minimize the number of calls to rand7()?
+#
+# The rand7() API is already defined for you.
+# def rand7():
+# @return a random integer in the range 1 to 7
+
+class Solution(object):
+    def rand10(self):
+        """
+        :rtype: int
+        """
+        while True:
+            x = (rand7()-1)*7 + (rand7()-1)
+            if x < 40:
+                return x%10 + 1
