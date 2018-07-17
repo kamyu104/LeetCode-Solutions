2f53faf7841b5b82b51c34b72d581ab564aba4f4
kamyu <kamyu104@users.noreply.github.com>
2018-07-17 23:52:55 +0800

    Rename construct-quad-tree.cpp to quad-tree-intersection.cpp

---
+++ C++/quad-tree-intersection.cpp
@@ -0,0 +1,44 @@
+// Time:  O(n)
+// Space: O(h)
+
+/*
+// Definition for a QuadTree node.
+class Node {
+public:
+    bool val;
+    bool isLeaf;
+    Node* topLeft;
+    Node* topRight;
+    Node* bottomLeft;
+    Node* bottomRight;
+
+    Node() {}
+
+    Node(bool _val, bool _isLeaf, Node* _topLeft, Node* _topRight, Node* _bottomLeft, Node* _bottomRight) {
+        val = _val;
+        isLeaf = _isLeaf;
+        topLeft = _topLeft;
+        topRight = _topRight;
+        bottomLeft = _bottomLeft;
+        bottomRight = _bottomRight;
+    }
+};
+*/
+class Solution {
+public:
+    Node* intersect(Node* quadTree1, Node* quadTree2) {
+        if (quadTree1->isLeaf && quadTree2->isLeaf) {
+            return new Node(quadTree1->val || quadTree2->val,
+                            true, nullptr, nullptr, nullptr, nullptr);
+        } else if (quadTree1->isLeaf) {
+            return quadTree1->val ? quadTree1 : quadTree2;
+        } else if (quadTree2->isLeaf) {
+            return quadTree2->val ? quadTree2 : quadTree1;
+        }
+        return new Node(true, false,
+                        intersect(quadTree1->topLeft, quadTree2->topLeft),
+                        intersect(quadTree1->topRight, quadTree2->topRight),
+                        intersect(quadTree1->bottomLeft, quadTree2->bottomLeft),
+                        intersect(quadTree1->bottomRight, quadTree2->bottomRight));
+    }
+};
