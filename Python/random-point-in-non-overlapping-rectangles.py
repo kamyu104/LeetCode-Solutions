49725b1cd9e29eaba59d6f40695119d9e1455404
kamyu <kamyu104@users.noreply.github.com>
2018-07-29 02:45:53 +0800

    Update random-point-in-non-overlapping-rectangles.py

--- Python/random-point-in-non-overlapping-rectangles.py
+++ Python/random-point-in-non-overlapping-rectangles.py
@@ -60,8 +60,7 @@ class Solution(object):
         target = random.randint(0, self.__prefix_sum[-1]-1)
         left = bisect.bisect_right(self.__prefix_sum, target)
         rect = self.__rects[left]
-        width = rect[2]-rect[0]+1
-        height = rect[3]-rect[1]+1
+        width, height = rect[2]-rect[0]+1, rect[3]-rect[1]+1
         base = self.__prefix_sum[left]-width*height
         return [rect[0]+(target-base)%width, rect[1]+(target-base)//width]
 
