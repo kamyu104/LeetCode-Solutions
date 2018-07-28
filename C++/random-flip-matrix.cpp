c0dd9720a61b8c1702a1c2326d3230ec289fb9b7
kamyu <kamyu104@users.noreply.github.com>
2018-07-29 02:13:54 +0800

    Create random-flip-matrix.cpp

---
+++ C++/random-flip-matrix.cpp
@@ -0,0 +1,45 @@
+// Time:  O(1)
+// Space: O(min(f, r * c))
+
+class Solution {
+public:
+    Solution(int n_rows, int n_cols) :
+        n_rows_(n_rows),
+        n_cols_(n_cols),
+        n_(n_rows * n_cols),
+        gen_{(random_device())()} {
+        
+    }
+    
+    vector<int> flip() {
+        uniform_int_distribution<int> uni(0, --n_);
+        const auto target = uni(gen_);
+        int x = get(target, target);
+        lookup_[target] = get(n_, n_);
+        return {x / n_cols_, x % n_cols_};
+    }
+    
+    void reset() {
+        lookup_.clear();
+        n_ = n_rows_ * n_cols_;
+    }
+
+private:
+    int get(int key, int default_value) {
+        return lookup_.count(key) ? lookup_[key] : default_value;
+    }
+
+    int n_rows_;
+    int n_cols_;
+    int n_;
+    unordered_map<int, int> lookup_;
+    default_random_engine gen_;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(n_rows, n_cols);
+ * vector<int> param_1 = obj.flip();
+ * obj.reset();
+ */
+ 
