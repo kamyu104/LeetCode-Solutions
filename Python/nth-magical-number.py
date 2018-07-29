0820ce3ef005755efb41c08082ecf0cce970685f
kamyu <kamyu104@users.noreply.github.com>
2018-07-29 14:28:49 +0800

    Create nth-magical-number.py

---
+++ Python/nth-magical-number.py
@@ -0,0 +1,29 @@
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def nthMagicalNumber(self, N, A, B):
+        """
+        :type N: int
+        :type A: int
+        :type B: int
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+
+        def check(A, B, N, lcm, target):
+            return target//A + target//B - target//lcm >= N
+
+        lcm = A*B // gcd(A, B)
+        left, right = min(A, B), max(A, B)*N
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(A, B, N, lcm, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left % (10**9 + 7)
+ 
