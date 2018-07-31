3d008ac9f2649a61cb17a9b4a8acc0bfb1f337dd
kamyu <kamyu104@users.noreply.github.com>
2018-08-01 02:10:57 +0800

    Update partition-equal-subset-sum.py

--- Python/partition-equal-subset-sum.py
+++ Python/partition-equal-subset-sum.py
@@ -36,7 +36,7 @@ class Solution(object):
         dp = [False] * (s/2 + 1)
         dp[0] = True
         for num in nums:
-            for i in xrange(1, len(dp)):
+            for i in reversed(xrange(1, len(dp))):
                 if num <= i:
                     dp[i] = dp[i] or dp[i - num]
         return dp[-1]
