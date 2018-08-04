03d451305b78f103e473ab0e1118c0f26d514f86
kamyu <kamyu104@users.noreply.github.com>
2018-08-04 14:21:10 +0800

    Update read-n-characters-given-read4.cpp

--- C++/read-n-characters-given-read4.cpp
+++ C++/read-n-characters-given-read4.cpp
@@ -13,10 +13,14 @@ public:
     int read(char *buf, int n) {
         int read_bytes = 0;
         char buffer[4];
-        while (int size = read4(buffer)) {
-            size = min(size, n - read_bytes);
-            for (int i = 0; i < size; ++i) {
-                buf[read_bytes++] = buffer[i];
+        for (int i = 0; i <= n / 4; ++i) {
+            if (int size = read4(buffer)) {
+                size = min(size, n - read_bytes);
+                for (int j = 0; j < size; ++j) {
+                    buf[read_bytes++] = buffer[j];
+                }
+            } else {
+                break;
             }
         }
         return read_bytes;
