eeb55b0ba0b6bae039a20652389fc44694333cec
kamyu <kamyu104@users.noreply.github.com>
2018-08-19 18:46:41 +0800

    Create find-and-replace-pattern.py

---
+++ Python/find-and-replace-pattern.py
@@ -0,0 +1,21 @@
+# Time:  O(n * l)
+# Space: O(l)
+
+import itertools
+
+
+class Solution(object):
+    def findAndReplacePattern(self, words, pattern):
+        """
+        :type words: List[str]
+        :type pattern: str
+        :rtype: List[str]
+        """
+        def match(word):
+            lookup = {}
+            for x, y in itertools.izip(pattern, word):
+                if lookup.setdefault(x, y) != y:
+                    return False
+            return len(set(lookup.values())) == len(lookup.values())
+
+        return filter(match, words)
