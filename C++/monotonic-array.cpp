435f7515cf653e80b74026529bab012256658b49
kamyu <kamyu104@users.noreply.github.com>
2018-09-02 20:25:06 +0800

    Create monotonic-array.cpp

---
+++ C++/monotonic-array.cpp
@@ -0,0 +1,17 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        bool inc = false, dec = false;
+        for(int i = 0; i + 1 < A.size(); ++i) {
+            if (A[i] < A[i + 1]) {
+                inc = true;
+            } else if (A[i] > A[i + 1]) {
+                dec = false;
+            }
+        }
+        return inc && dec == false;
+    }
+};
