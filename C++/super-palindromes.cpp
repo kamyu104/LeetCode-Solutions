3bdd18c316ece846a6ce7562309f1d387b882ff7
kamyu <kamyu104@users.noreply.github.com>
2018-09-16 20:30:03 +0800

    Create super-palindromes.cpp

---
+++ C++/super-palindromes.cpp
@@ -0,0 +1,45 @@
+// Time:  O(n^0.25 * logn)
+// Space: O(logn)
+
+class Solution {
+public:
+    int superpalindromesInRange(string L, string R) {
+        const auto K = static_cast<int>(pow(10, (R.length() + 1) * 0.25));
+        const int64_t l = stol(L), r = stol(R);
+        int result = 0;
+
+        // count odd length
+        for (int k = 0; k < K; ++k) {
+            const string s = to_string(k), rev_s(s.rbegin(), s.rend());
+            int64_t v = stol(s + rev_s.substr(1));
+            v *= v;
+            if (v > r) {
+                break;
+            }
+            if (v >= l && is_palindrome(v)) {
+                ++result;
+            }
+        }
+
+        // count even length
+        for (int k = 0; k < K; ++k) {
+            const string s = to_string(k), rev_s(s.rbegin(), s.rend());
+            int64_t v = stol(s + rev_s);
+            v *= v;
+            if (v > r) {
+                break;
+            }
+            if (v >= l && is_palindrome(v)) {
+                ++result;
+            }
+        }
+
+        return result;
+    }
+
+private:
+    bool is_palindrome(int64_t k) {
+        const string s = to_string(k), rev_s(s.rbegin(), s.rend());
+        return s == rev_s;
+    }
+};
