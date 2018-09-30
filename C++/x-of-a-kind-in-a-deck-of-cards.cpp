9b704f0a918bf0f81a7b81a6702da2f846806f38
kamyu <kamyu104@users.noreply.github.com>
2018-09-30 19:41:51 +0800

    Create x-of-a-kind-in-a-deck-of-cards.cpp

---
+++ C++/x-of-a-kind-in-a-deck-of-cards.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    bool hasGroupsSizeX(vector<int>& deck) {
+        unordered_map<int, int> count;
+        for (const auto& i : deck) {
+            ++count[i];
+        }
+        int result = 0;
+        for (const auto& kvp : count) {
+            result = gcd(result, kvp.second);
+        }
+        return result > 1;
+    }
+
+private:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+};
