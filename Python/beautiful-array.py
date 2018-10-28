abc77ae582eaed18332a3faef9e0791dfbf51bdc
kamyu <kamyu104@users.noreply.github.com>
2018-10-28 14:11:08 +0800

    Create beautiful-array.py

---
+++ Python/beautiful-array.py
@@ -0,0 +1,13 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+class Solution(object):
+    def beautifulArray(self, N):
+        """
+        :type N: int
+        :rtype: List[int]
+        """
+        result = [1]
+        while len(result) < N:
+            result = [i*2 - 1 for i in result] + [i*2 for i in result]
+        return [i for i in result if i <= N]
