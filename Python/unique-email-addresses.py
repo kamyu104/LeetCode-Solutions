58ddad3ddb42319e0890652353dc0e4baa1639de
kamyu <kamyu104@users.noreply.github.com>
2018-10-28 14:36:04 +0800

    Create unique-email-addresses.py

---
+++ Python/unique-email-addresses.py
@@ -0,0 +1,19 @@
+# Time:  O(n * l)
+# Space: O(n * l)
+
+class Solution(object):
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        def convert(email):
+            name, domain = email.split('@')
+            name = name.split("+")[0]
+            return "".join(["".join(name.split(".")), '@', domain])
+
+        lookup = set()
+        for email in emails:
+            lookup.add(convert(email))
+        return len(lookup)
+  
