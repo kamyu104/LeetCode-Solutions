1ba8dd32a8021a0739f7c7276ece32d891580ad1
kamyu <kamyu104@users.noreply.github.com>
2018-10-28 14:10:24 +0800

    Create beautiful-array.cpp

---
+++ C++/beautiful-array.cpp
@@ -0,0 +1,26 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> beautifulArray(int N) {
+        vector<int> result{1};
+        while (result.size() < N) {
+            vector<int> tmp;
+            for (const auto& i : result) {
+                tmp.emplace_back(i * 2 - 1);
+            }
+            for (const auto& i : result) {
+                tmp.emplace_back(i * 2);
+            }
+            result = move(tmp);
+        }
+        vector<int> tmp = move(result);
+        for (const auto& i : tmp) {
+            if (i <= N) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
