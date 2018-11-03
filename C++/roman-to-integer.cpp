b8d1e6cff14fb27e75bfd34a08251cbdb3daa815
kamyu <kamyu104@users.noreply.github.com>
2018-11-03 19:08:26 +0800

    Update roman-to-integer.cpp

--- C++/roman-to-integer.cpp
+++ C++/roman-to-integer.cpp
@@ -27,7 +27,7 @@ public:
         unordered_map<char, int> numeral_map = {{'I',    1}, {'V',   5}, {'X',  10},
                                                 {'L',   50}, {'C', 100}, {'D', 500},
                                                 {'M', 1000}};
-        return accumulate(s.crbegin() , s.crend() , 0 ,
+        return accumulate(s.crbegin(), s.crend(), 0,
                           [&numeral_map](int sum , char c) {
                               return sum += ((numeral_map[c] * 5 <= sum) ? 
                                             -numeral_map[c] : numeral_map[c]);
