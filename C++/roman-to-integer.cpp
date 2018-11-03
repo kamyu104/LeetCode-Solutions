a57df3f09b1a86bcf61fe149121e9222a1e5c943
BodomMoon <BodomMoon@gmail.com>
2018-11-03 17:59:59 +0800

    Update roman-to-integer.cpp     Make solution more clear

--- C++/roman-to-integer.cpp
+++ C++/roman-to-integer.cpp
@@ -7,14 +7,8 @@ public:
         unordered_map<char, int> numeral_map = {{'I',    1}, {'V',   5}, {'X',  10},
                                                 {'L',   50}, {'C', 100}, {'D', 500},
                                                 {'M', 1000}};
-        int decimal = 0;
-        for (int i = 0; i < s.length(); ++i) {
-            if (i > 0 && numeral_map[s[i]] > numeral_map[s[i - 1]]) {
-                decimal += numeral_map[s[i]] - 2 * numeral_map[s[i - 1]];
-            } else {
-                decimal += numeral_map[s[i]];
-            }
-        }
-        return decimal;
+        return accumulate(s.crbegin() , s.crend() , 0 , [&numeral_map](int sum , char c){
+            return sum += ((numeral_map[c] * 5 <= sum) ?  -numeral_map[c]: numeral_map[c]);
+        });
     }
 };
