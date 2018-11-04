8bd1479bbe9ac40bfe6f384b502955e0c291a595
kamyu <kamyu104@users.noreply.github.com>
2018-11-04 21:18:19 +0800

    Create stamping-the-sequence.cpp

---
+++ C++/stamping-the-sequence.cpp
@@ -0,0 +1,64 @@
+// Time:  O((n - m) * m)
+// Space: O((n - m) * m)
+
+class Solution {
+public:
+    vector<int> movesToStamp(string stamp, string target) {
+        const int M = stamp.size(), N = target.size();
+        
+        queue<int> q;
+        vector<bool> lookup(N);
+        vector<int> result;
+        vector<pair<unordered_set<int>, unordered_set<int>>> A;
+        for (int i = 0; i < N - M + 1; ++i) {
+            unordered_set<int> made, todo;
+            for (int j = 0; j < M; ++j) {
+                if (stamp[j] == target[i + j]) {
+                    made.emplace(i + j);
+                } else {
+                    todo.emplace(i + j);
+                }
+            }
+            A.emplace_back(made, todo);
+            if (!todo.empty()) {
+                continue;
+            }
+            result.emplace_back(i);
+            for (const auto& m : made) {
+                if (lookup[m]) {
+                    continue;
+                }
+                q.emplace(m);
+                lookup[m] = true;
+            }
+        }
+        
+        while (!q.empty()) {
+            auto i = q.front(); q.pop();
+            for (int j = max(0, i - M + 1); j < min(N - M, i) + 1; ++j) {
+                unordered_set<int>& made = A[j].first;
+                unordered_set<int>& todo = A[j].second;
+                if (!todo.count(i)) {
+                    continue;
+                }
+                todo.erase(i);
+                if (!todo.empty()) {
+                    continue;
+                }
+                result.emplace_back(j);
+                for (const auto& m : made) {
+                    if (lookup[m]) {
+                        continue;
+                    }
+                    q.emplace(m);
+                    lookup[m] = true;
+                }
+            }
+        }
+        if (std::all_of(lookup.cbegin(), lookup.cend(), [](bool i){ return i; })) {
+            reverse(result.begin(), result.end());
+            return result;
+        }
+        return {};
+    }
+};
