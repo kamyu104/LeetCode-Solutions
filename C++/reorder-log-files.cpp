3614960e6f66b9f1fcf6016e762bc3cd50022981
kamyu <kamyu104@users.noreply.github.com>
2018-11-11 16:28:40 +0800

    Update reorder-log-files.cpp

--- C++/reorder-log-files.cpp
+++ C++/reorder-log-files.cpp
@@ -5,13 +5,15 @@ class Solution {
 public:
     vector<string> reorderLogFiles(vector<string>& logs) {
         auto pivot = stable_partition(logs.begin(), logs.end(),
-                                      [](const auto& log) {
+                                      []( const auto& log ) {
                                           return isalpha(log.back());
                                       });
         sort(logs.begin(), pivot,
              [](const auto& a, const auto& b) {
                  int i = a.find_first_of(' '), j = b.find_first_of(' ');
-                 return a.substr(i, a.length() - i) < b.substr(j, b.length() - j);
+                 return a.substr(i) != b.substr(j) ?
+                            a.substr(i) < b.substr(j) :
+                            a.substr(0, i) < b.substr(0, j);
              });
         return logs;
     }
