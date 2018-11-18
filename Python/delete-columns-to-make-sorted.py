4710c0e566c55895443dd40a1f12c9ab12cf569e
kamyu <kamyu104@users.noreply.github.com>
2018-11-18 15:32:14 +0800

    Update delete-columns-to-make-sorted.py

--- Python/delete-columns-to-make-sorted.py
+++ Python/delete-columns-to-make-sorted.py
@@ -1,10 +1,27 @@
 # Time:  O(n * l)
 # Space: O(1)
 
+class Solution(object):
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        result = 0
+        for c in xrange(len(A[0])):
+            for r in xrange(1, len(A)):
+                if A[r-1][c] > A[r][c]:
+                    result += 1
+                    break
+        return result
+
+
+# Time:  O(n * l)
+# Space: O(n)
 import itertools
 
 
-class Solution(object):
+class Solution2(object):
     def minDeletionSize(self, A):
         """
         :type A: List[str]
