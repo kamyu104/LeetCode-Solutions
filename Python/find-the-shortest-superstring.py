d7c945ab6a09298aaf1e2fff76a6b3d8f2e3144c
kamyu <kamyu104@users.noreply.github.com>
2018-11-18 13:42:04 +0800

    Update find-the-shortest-superstring.py

--- Python/find-the-shortest-superstring.py
+++ Python/find-the-shortest-superstring.py
@@ -7,8 +7,8 @@ class Solution(object):
         :type A: List[str]
         :rtype: str
         """
-        N = len(A)
-        overlaps = [[0]*N for _ in xrange(N)]
+        n = len(A)
+        overlaps = [[0]*n for _ in xrange(n)]
         for i, x in enumerate(A):
             for j, y in enumerate(A):
                 if i == j:
@@ -18,14 +18,14 @@ class Solution(object):
                         overlaps[i][j] = l
                         break
 
-        dp = [[0]*N for _ in xrange(1<<N)]
-        prev = [[None]*N for _ in xrange(1<<N)]
-        for mask in xrange(1, 1<<N):
-            for bit in xrange(N):
+        dp = [[0]*n for _ in xrange(1<<n)]
+        prev = [[None]*n for _ in xrange(1<<n)]
+        for mask in xrange(1, 1<<n):
+            for bit in xrange(n):
                 if (mask>>bit) & 1 == 0:
                     continue
                 prev_mask = mask^(1<<bit)
-                for i in xrange(N):
+                for i in xrange(n):
                     if (prev_mask>>i) & 1 == 0:
                         continue
                     value = dp[prev_mask][i] + overlaps[i][bit]
@@ -33,15 +33,15 @@ class Solution(object):
                         dp[mask][bit] = value
                         prev[mask][bit] = i
         
-        i = max(xrange(N), key = dp[-1].__getitem__)
+        i = max(xrange(n), key = dp[-1].__getitem__)
         words = []
-        mask = (1<<N)-1
+        mask = (1<<n)-1
         while i is not None:
             words.append(i)
             mask, i = mask^(1<<i), prev[mask][i]
         words.reverse()
         lookup = set(words)
-        words.extend([i for i in xrange(N) if i not in lookup])
+        words.extend([i for i in xrange(n) if i not in lookup])
 
         result = [A[words[0]]]
         for i in xrange(1, len(words)):
