b9eb3df59dce7fac1e764a4b1c0c0e0b7f70b678
kamyu <kamyu104@users.noreply.github.com>
2018-12-02 17:43:41 +0800

    Create reveal-cards-in-increasing-order.py

---
+++ Python/reveal-cards-in-increasing-order.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def deckRevealedIncreasing(self, deck):
+        """
+        :type deck: List[int]
+        :rtype: List[int]
+        """
+        d = collections.deque()
+        deck.sort(reverse=True)
+        for i in deck:
+            if d:
+                d.appendleft(d.pop())
+            d.appendleft(i)
+        return list(d)
