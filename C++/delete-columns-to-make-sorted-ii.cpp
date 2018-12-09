e67786c272bfb2601cc40fefe50e686a6d254225
kamyu <kamyu104@users.noreply.github.com>
2018-12-09 20:41:32 +0800

    Create delete-columns-to-make-sorted-ii.cpp

---
+++ C++/delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,57 @@
+// Time:  O(n * m)
+// Space: O(n)
+
+class Solution {
+public:
+    int minDeletionSize(vector<string>& A) {
+        int result = 0;
+        unordered_set<int> unsorted;
+        for (int i = 0; i < A.size() - 1; ++i) {
+            unsorted.emplace(i);
+        }
+        for (int j = 0; j < A[0].size(); ++j) {
+            if (any_of(unsorted.begin(), unsorted.end(),
+                       [&](int i) {
+                           return A[i][j] > A[i + 1][j];
+                       })
+               ) {
+                ++result;
+            } else {
+                unordered_set<int> tmp(unsorted);
+                for (const auto& i : tmp) {
+                    if (A[i][j] < A[i + 1][j]) {
+                        unsorted.erase(i);
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * m)
+// Space: O(n)
+class Solution2 {
+public:
+    int minDeletionSize(vector<string>& A) {
+        int result = 0;
+        vector<bool> is_sorted(A.size() - 1);
+        for (int j = 0; j < A[0].size(); ++j) {
+            vector<bool> tmp(is_sorted);
+            int i = 0;
+            for (; i < A.size() - 1; ++i) {
+                if (A[i][j] > A[i + 1][j] && tmp[i] == false) {
+                    ++result;
+                    break;
+                }
+                if (A[i][j] < A[i + 1][j]) {
+                    tmp[i] = true;
+                }
+            }
+            if (i == A.size() - 1) {
+                is_sorted = move(tmp);
+            }
+        }
+        return result;
+    }
+};
