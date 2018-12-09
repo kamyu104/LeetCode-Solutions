1c6e754c5e9d28ec85ae2770a19f5058e1f024a7
kamyu <kamyu104@users.noreply.github.com>
2018-12-09 20:44:36 +0800

    Create delete-columns-to-make-sorted-ii.py

---
+++ Python/delete-columns-to-make-sorted-ii.py
@@ -0,0 +1,40 @@
+# Time:  O(n * m)
+# Space: O(n)
+
+class Solution(object):
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        result = 0
+        unsorted = set(range(len(A)-1))
+        for j in xrange(len(A[0])):
+            if any(A[i][j] > A[i+1][j] for i in unsorted):
+                result += 1
+            else:
+                unsorted -= set(i for i in unsorted if A[i][j] < A[i+1][j])
+        return result
+
+
+# Time:  O(n * m)
+# Space: O(n)
+class Solution2(object):
+    def minDeletionSize(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        result = 0
+        is_sorted = [False]*(len(A)-1)
+        for j in xrange(len(A[0])):
+            tmp = is_sorted[:]
+            for i in xrange(len(A)-1):
+                if A[i][j] > A[i+1][j] and tmp[i] == False:
+                    result += 1
+                    break
+                if A[i][j] < A[i+1][j]:
+                    tmp[i] = True
+            else:
+                is_sorted = tmp
+        return result
