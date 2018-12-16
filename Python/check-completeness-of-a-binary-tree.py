7ad85eac10269fd552dde88fa541e361bdf850c3
kamyu <kamyu104@users.noreply.github.com>
2018-12-16 16:42:28 +0800

    Create check-completeness-of-a-binary-tree.py

---
+++ Python/check-completeness-of-a-binary-tree.py
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(w)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isCompleteTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        prev_level, current = [], [(root, 1)]
+        count = 0
+        while current:
+            count += len(current)
+            next_level = []
+            for node, v in current:
+                if not node:
+                    continue
+                next_level.append((node.left, 2*v))
+                next_level.append((node.right, 2*v+1))
+            prev_level, current = current, next_level
+        return prev_level[-1][1] == count
