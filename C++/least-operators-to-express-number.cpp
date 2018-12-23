4dc311ac75850fac5ca934a49e764e9c43a7c507
kamyu <kamyu104@users.noreply.github.com>
2018-12-23 15:03:03 +0800

    Create least-operators-to-express-number.cpp

---
+++ C++/least-operators-to-express-number.cpp
@@ -0,0 +1,21 @@
+// Time:  O(logn) = O(32) = O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    int leastOpsExpressTarget(int x, int target) {
+        int pos = 0, neg = 0, k;
+        for (k = 0; target > 0; ++k, target /= x) {
+            int r = target % x;
+            if (k > 0) {
+                tie(pos, neg) = make_pair(
+                                    min(r * k + pos, (r + 1) * k + neg),
+                                    min((x - r) * k + pos, (x - r - 1) * k + neg)
+                                );
+            } else {
+                tie(pos, neg) = make_pair(r * 2, (x - r) * 2);
+            }
+        }
+        return min(pos, k + neg) - 1;
+    }
+};
