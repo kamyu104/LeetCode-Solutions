7323c1fb68fe40353cbbca42598204c5ee65f044
kamyu <kamyu104@users.noreply.github.com>
2018-12-23 18:00:20 +0800

    Create minimum-area-rectangle-ii.py

---
+++ Python/minimum-area-rectangle-ii.py
@@ -0,0 +1,25 @@
+# Time:  O(n^2 ~ n^3)
+# Space: O(n^2)
+
+import collections
+import itertools
+
+
+class Solution(object):
+    def minAreaFreeRect(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: float
+        """
+        points.sort()
+        points = [complex(*z) for z in points]
+        lookup = collections.defaultdict(list)
+        for P, Q in itertools.combinations(points, 2):
+            lookup[P-Q].append((P+Q) / 2)
+
+        result = float("inf")
+        for A, candidates in lookup.iteritems():
+            for P, Q in itertools.combinations(candidates, 2):
+                if A.real * (P-Q).real + A.imag * (P-Q).imag == 0.0:
+                    result = min(result, abs(A) * abs(P-Q))
+        return result if result < float("inf") else 0.0
