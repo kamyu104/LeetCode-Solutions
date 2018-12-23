f6ca0558f050893a163df691c2c2ceb34f6ecaba
kamyu <kamyu104@users.noreply.github.com>
2018-12-23 15:08:45 +0800

    Create least-operators-to-express-number.py

---
+++ Python/least-operators-to-express-number.py
@@ -0,0 +1,20 @@
+# Time:  O(logn) = O(32) = O(1)
+# Space: O(1)
+
+class Solution(object):
+    def leastOpsExpressTarget(self, x, target):
+        """
+        :type x: int
+        :type target: int
+        :rtype: int
+        """
+        pos, neg, k = 0, 0, 0
+        while target:
+            target, r = divmod(target, x)
+            if k:
+                pos, neg = min(r*k + pos, (r+1)*k + neg), \
+                           min((x-r)*k + pos, (x-r-1)*k + neg)
+            else:
+                pos, neg = r*2, (x-r)*2
+            k += 1
+        return min(pos, k+neg) - 1
