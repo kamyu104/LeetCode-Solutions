6dc714b06737d06b87284219311809de871dd8a7
kamyu <kamyu104@users.noreply.github.com>
2018-12-30 15:13:14 +0800

    Create vowel-spellchecker.py

---
+++ Python/vowel-spellchecker.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(w)
+
+class Solution(object):
+    def spellchecker(self, wordlist, queries):
+        """
+        :type wordlist: List[str]
+        :type queries: List[str]
+        :rtype: List[str]
+        """
+        def todev(word):
+            return "".join('*' if c.lower() in set(['a', 'e', 'i', 'o', 'u']) else c.lower()
+                           for c in word)
+
+        words = set(wordlist)
+        caps = {}
+        vows = {}
+
+        for word in wordlist:
+            caps.setdefault(word.lower(), word)
+            vows.setdefault(todev(word), word)
+
+        def check(query):
+            if query in words:
+                return query
+            lower = query.lower()
+            if lower in caps:
+                return caps[lower]
+            devow = todev(lower)
+            if devow in vows:
+                return vows[devow]
+            return ""
+        return map(check, queries)
