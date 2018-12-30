b02cfc7a941830b99edcd0993a9348ab38a20d3d
kamyu <kamyu104@users.noreply.github.com>
2018-12-30 14:16:48 +0800

    Create binary-tree-cameras.cpp

---
+++ C++/binary-tree-cameras.cpp
@@ -0,0 +1,37 @@
+// Time:  O(n)
+// Space: O(h)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int minCameraCover(TreeNode* root) {
+        int result = 0;
+        if (dfs(root, &result) == UNCOVERED) {
+            ++result;
+        }
+        return result;
+    }
+
+private:
+    int dfs(TreeNode* root, int *result) {
+        int left = root->left ? dfs(root->left, result) : COVERED;
+        int right = root->right ? dfs(root->right, result) : COVERED;
+        if (left == UNCOVERED || right == UNCOVERED) {
+            ++(*result);
+            return CAMERA;
+        }
+        return (left == CAMERA || right == CAMERA) ? COVERED : UNCOVERED;
+    }
+    
+    enum NODE {
+        UNCOVERED, COVERED, CAMERA
+    };
+};
