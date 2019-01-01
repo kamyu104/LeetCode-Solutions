617525775f3277c520a2078bb3c7968a1fc0dd81
kamyu <kamyu104@users.noreply.github.com>
2019-01-01 16:49:51 +0800

    Update climbing-stairs.py

--- Python/climbing-stairs.py
+++ Python/climbing-stairs.py
@@ -1,7 +1,36 @@
-# Time:  O(n)
+# Time:  O(logn)
 # Space: O(1)
 
 class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def matrix_expo(A, K):
+            result = [[int(i==j) for j in xrange(len(A))] \
+                      for i in xrange(len(A))]
+            while K:
+                if K % 2:
+                    result = matrix_mult(result, A)
+                A = matrix_mult(A, A)
+                K /= 2
+            return result
+
+        def matrix_mult(A, B):
+            ZB = zip(*B)
+            return [[sum(a*b for a, b in itertools.izip(row, col)) \
+                     for col in ZB] for row in A]
+
+        T = [[1, 1],
+             [1, 0]]
+
+        return matrix_expo(T, n)[0][0]
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
     """
     :type n: int
     :rtype: int
