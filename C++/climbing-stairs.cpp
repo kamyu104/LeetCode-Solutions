bb2baecf59e33147677282900243eef6b8d8e4ed
kamyu <kamyu104@users.noreply.github.com>
2019-01-01 16:56:09 +0800

    Create climbing-stairs.cpp

---
+++ C++/climbing-stairs.cpp
@@ -0,0 +1,58 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int climbStairs(int n) {
+	        vector<vector<int>> T = {{1, 1},
+                                     {1, 0}};
+
+        return matrixExpo(T, n)[0][0];
+    }
+
+private:
+    vector<vector<int>> matrixExpo(const vector<vector<int>>& A, int pow) {
+        vector<vector<int>> result(A.size(), vector<int>(A.size()));
+        vector<vector<int>> A_exp(A);
+        for (int i = 0; i < A.size(); ++i) {
+            result[i][i] = 1;
+        }
+        while (pow) {
+            if (pow % 2 == 1) {
+                result = matrixMult(result, A_exp);
+            }
+            A_exp = matrixMult(A_exp, A_exp);
+            pow /= 2;
+        }
+        return result;
+    }
+
+    vector<vector<int>> matrixMult(const vector<vector<int>>& A, const vector<vector<int>>& B) {
+        vector<vector<int>> result(A.size(), vector<int>(A.size()));
+        for (int i = 0; i < A.size(); ++i) {
+            for (int j = 0; j < B[0].size(); ++j) {
+                int64_t entry = 0;
+                for (int k = 0; k < B.size(); ++k) {
+                    entry = (static_cast<int64_t>(A[i][k]) * B[k][j] + entry);
+                }
+                result[i][j] = static_cast<int>(entry);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(logn)
+// Space: O(1)
+class Solution2 {
+public:
+    int climbStairs(int n) {
+        vector<int> steps(3, 0);
+        steps[0] = 1;
+        steps[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            steps[i % 3] = steps[(i - 1) % 3] + steps[(i - 2) % 3];
+        }
+        return steps[n % 3];
+    }
+};
