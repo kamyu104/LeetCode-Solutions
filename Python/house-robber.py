39d23364864f6e71ff85b62bf5590094005c08eb
kamyu <kamyu104@users.noreply.github.com>
2019-01-04 11:05:58 +0800

    Update house-robber.py

--- Python/house-robber.py
+++ Python/house-robber.py
@@ -4,21 +4,7 @@
 class Solution(object):
     # @param num, a list of integer
     # @return an integer
-    def rob(self, num):
-        if len(num) == 0:
-            return 0
-
-        if len(num) == 1:
-            return num[0]
-
-        num_i, num_i_1 = max(num[1], num[0]), num[0]
-        for i in xrange(2, len(num)):
-            num_i_1, num_i_2 = num_i, num_i_1
-            num_i = max(num[i] + num_i_2, num_i_1)
-
-        return num_i
-
-    def rob2(self, nums):
+    def rob(self, nums):
         """
         :type nums: List[int]
         :rtype: int
