ee2b7e29256b1673caab05d702bd55c66d099b3a
BodomMoon <BodomMoon@gmail.com>
2019-01-04 10:56:46 +0800

    Update house-robber.cpp

--- C++/house-robber.cpp
+++ C++/house-robber.cpp
@@ -5,9 +5,9 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int last = 0, result = 0;
-        for (const auto i : nums) {
+        for (const auto& i : nums) {
             auto tmp = result;
-            result = max(last + i,result);
+            result = max(last + i, result);
             last = tmp;
         }
         return result;
