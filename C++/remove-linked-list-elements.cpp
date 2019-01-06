36a2516836d86a35cef287ec8fc5260efbf5136a
BodomMoon <BodomMoon@gmail.com>
2019-01-06 20:54:50 +0800

    Update remove-linked-list-elements.cpp

--- C++/remove-linked-list-elements.cpp
+++ C++/remove-linked-list-elements.cpp
@@ -12,7 +12,29 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        for (auto** indirect = &head; *indirect != nullptr; ) {
+        ListNode dummy{0};
+        dummy.next = head;
+        auto *prev = &dummy, *cur = dummy.next;
+        
+        while (cur) {
+            if (cur->val == val) {
+                prev->next = cur->next;
+                delete cur;
+            } else {
+                prev = cur;
+            }
+            cur = cur->next;
+        }
+        return dummy.next;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        for (auto* indirect = &head; *indirect != nullptr; ) {
             if ((*indirect)->val == val) {
                 *indirect = (*indirect)->next;
                 continue;
