4443ccc91cc3f5d2af0c66b15a8f55a5622b666c
kamyu <kamyu104@users.noreply.github.com>
2019-01-06 15:39:25 +0800

    Create equal-rational-numbers.py

---
+++ Python/equal-rational-numbers.py
@@ -0,0 +1,33 @@
+# Time:  O(1)
+# Space: O(1)
+
+from fractions import Fraction
+
+
+class Solution(object):
+    def isRationalEqual(self, S, T):
+        """
+        :type S: str
+        :type T: str
+        :rtype: bool
+        """
+        def frac(S):
+            if '.' not in S:
+                return Fraction(int(S), 1)
+
+            i = S.index('.')
+            result = Fraction(int(S[:i]), 1)
+            non_repeat_part = S[i+1:]
+            if '(' not in non_repeat_part:
+                if non_repeat_part:
+                    result += Fraction(int(non_repeat_part), 10**len(non_repeat_part))
+                return result
+
+            i = non_repeat_part.index('(')
+            if i:
+                result += Fraction(int(non_repeat_part[:i]), 10**i)
+            repeat_part = non_repeat_part[i+1:-1]
+            result += Fraction(int(repeat_part), 10**i * (10**len(repeat_part)-1))
+            return result
+
+        return frac(S) == frac(T)
