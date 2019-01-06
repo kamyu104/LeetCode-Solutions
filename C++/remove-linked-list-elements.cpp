7f89e53f529d2080b5440ec131311b956e7d6ecf
BodomMoon <BodomMoon@gmail.com>
2019-01-06 20:59:03 +0800

    Update remove-linked-list-elements.cpp

--- C++/remove-linked-list-elements.cpp
+++ C++/remove-linked-list-elements.cpp
@@ -14,7 +14,7 @@ public:
     ListNode* removeElements(ListNode* head, int val) {
         ListNode dummy{0};
         dummy.next = head;
-        auto *prev = &dummy, *cur = dummy.next;
+        auto prev = &dummy, cur = dummy.next;
         
         while (cur) {
             if (cur->val == val) {
