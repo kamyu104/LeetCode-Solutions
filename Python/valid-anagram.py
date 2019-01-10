18a7807c127eae36792452df13f255b0ee88f72b
kamyu <kamyu104@users.noreply.github.com>
2019-01-10 12:46:54 +0800

    Update valid-anagram.py

--- Python/valid-anagram.py
+++ Python/valid-anagram.py
@@ -12,31 +12,6 @@ class Solution(object):
     def isAnagram(self, s, t):
         if len(s) != len(t):
             return False
-
-        count = {}
-
-        for c in s:
-            if c.lower() in count:
-                count[c.lower()] += 1
-            else:
-                count[c.lower()] = 1
-
-        for c in t:
-            if c.lower() in count:
-                count[c.lower()] -= 1
-            else:
-                count[c.lower()] = -1
-            if count[c.lower()] < 0:
-                return False
-
-        return True
-
-    def isAnagram2(self, s, t):
-        return all([s.count(c)==t.count(c) for c in string.ascii_lowercase])
-
-    def isAnagram3(self, s, t):
-        if len(s) != len(t):
-            return False
         count = collections.defaultdict(int)
         for c in s:
             count[c] += 1
@@ -45,6 +20,7 @@ class Solution(object):
             if count[c] < 0:
                 return False
         return True
+
 
 # Time:  O(nlogn)
 # Space: O(n)
