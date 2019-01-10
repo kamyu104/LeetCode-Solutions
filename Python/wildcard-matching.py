8864f310675222adbf3da95b2d0fb04e5b02f4e5
kamyu <kamyu104@users.noreply.github.com>
2019-01-11 01:13:01 +0800

    Update wildcard-matching.py

--- Python/wildcard-matching.py
+++ Python/wildcard-matching.py
@@ -3,10 +3,13 @@
 
 # iterative solution with greedy
 class Solution(object):
-    # @param s, an input string
-    # @param p, a pattern string
-    # @return a boolean
     def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        count = 0  # used for check complexity
         p_ptr, s_ptr, last_s_ptr, last_p_ptr = 0, 0, -1, -1
         while s_ptr < len(s):
             if p_ptr < len(p) and (s[s_ptr] == p[p_ptr] or p[p_ptr] == '?'):
@@ -22,10 +25,13 @@ class Solution(object):
                 p_ptr = last_p_ptr
             else:
                 return False
-
+            count += 1
+ 
         while p_ptr < len(p) and p[p_ptr] == '*':
             p_ptr += 1
+            count += 1
 
+        assert(count <= len(p) * len(s) + 1)
         return p_ptr == len(p)
 
 
