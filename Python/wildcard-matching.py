c185739be90f2545aa81b7c9e13648703cc6c27c
kamyu <kamyu104@users.noreply.github.com>
2019-01-11 01:15:19 +0800

    Update wildcard-matching.py

--- Python/wildcard-matching.py
+++ Python/wildcard-matching.py
@@ -9,7 +9,7 @@ class Solution(object):
         :type p: str
         :rtype: bool
         """
-        count = 0  # used for check complexity
+        count = 0  # used for complexity check
         p_ptr, s_ptr, last_s_ptr, last_p_ptr = 0, 0, -1, -1
         while s_ptr < len(s):
             if p_ptr < len(p) and (s[s_ptr] == p[p_ptr] or p[p_ptr] == '?'):
@@ -25,11 +25,11 @@ class Solution(object):
                 p_ptr = last_p_ptr
             else:
                 return False
-            count += 1  # used for check complexity
+            count += 1  # used for complexity check
  
         while p_ptr < len(p) and p[p_ptr] == '*':
             p_ptr += 1
-            count += 1  # used for check complexity
+            count += 1  # used for complexity check
 
         assert(count <= len(p) * len(s) + 1)
         return p_ptr == len(p)
