4655b73f4159b4be83f25d82a843795160abe777
SangHee Kim <sangheestyle@gmail.com>
2019-01-13 19:46:05 +0200

    update closest-binary-search-tree-value.py (#29)
    * update closest-binary-search-tree-value.py
    
    make it a bit clear
    
    * Update closest-binary-search-tree-value.py

--- Python/closest-binary-search-tree-value.py
+++ Python/closest-binary-search-tree-value.py
@@ -13,12 +13,11 @@ class Solution(object):
         while root:
             if abs(root.val - target) < gap:
                 gap = abs(root.val - target)
-                closest = root
+                closest = root.val
             if target == root.val:
                 break
             elif target < root.val:
                 root = root.left
             else:
                 root = root.right
-        return closest.val
-
+        return closest
