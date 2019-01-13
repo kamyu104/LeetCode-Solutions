7c0fb482c4b7ebd929ed6c8d703809b8d63d251b
kamyu <kamyu104@users.noreply.github.com>
2019-01-13 17:27:40 +0800

    Create fibonacci-number.py

---
+++ Python/fibonacci-number.py
@@ -0,0 +1,50 @@
+# Time:  O(logn)
+# Space: O(1)
+
+import itertools
+
+
+class Solution(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        def matrix_expo(A, K):
+            result = [[int(i==j) for j in xrange(len(A))] \
+                      for i in xrange(len(A))]
+            while K:
+                if K % 2:
+                    result = matrix_mult(result, A)
+                A = matrix_mult(A, A)
+                K /= 2
+            return result
+
+        def matrix_mult(A, B):
+            ZB = zip(*B)
+            return [[sum(a*b for a, b in itertools.izip(row, col)) \
+                     for col in ZB] for row in A]
+
+        if N == 0:
+            return 0
+
+        T = [[1, 1],
+             [1, 0]]
+        return matrix_expo(T, N-1)[0][0]
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0:
+            return 0
+
+        prev, current = 0, 1
+        for i in xrange(N-1):
+            prev, current = current, prev + current,
+        return current
