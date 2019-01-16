505dddc37f4cacf806575060b9f9f42e85b566b2
kamyu <kamyu104@users.noreply.github.com>
2019-01-16 21:38:07 +0800

    Update palindrome-number.cpp

--- C++/palindrome-number.cpp
+++ C++/palindrome-number.cpp
@@ -10,10 +10,20 @@ public:
         int temp = x;
         int reversed = 0;
         while (temp != 0) {
+            if (isOverflow(reversed, temp % 10)) {
+                return false;
+            }
             reversed = reversed * 10 + temp % 10;
             temp = temp / 10;
         }
         return reversed == x;
+    }
+
+private:
+    bool isOverflow(int q, int r) {
+        static const int max_q = numeric_limits<int>::max() / 10;
+        static const int max_r = numeric_limits<int>::max() % 10;
+        return (q > max_q) || (q == max_q && r > max_r);
     }
 };
 
