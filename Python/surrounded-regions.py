0b0bb68e0f73e2d61ed707127b6c1f4f31961bfa
kamyu <kamyu104@users.noreply.github.com>
2019-01-19 22:20:06 +0800

    Update surrounded-regions.py

--- Python/surrounded-regions.py
+++ Python/surrounded-regions.py
@@ -5,31 +5,38 @@ import collections
 
 
 class Solution(object):
-    # @param board, a 2D array
-    # Capture all regions by modifying the input board in-place.
-    # Do not return any value.
     def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: void Do not return anything, modify board in-place instead.
+        """
         if not board:
             return
         q = collections.deque()
 
         for i in xrange(len(board)):
-            q.append((i, 0))
-            q.append((i, len(board[0]) - 1))
+            if board[i][0] == 'O':
+                board[i][0] = 'V'
+                q.append((i, 0))
+            if board[i][len(board[0]) - 1] == 'O':
+                board[i][len(board[0]) - 1] = 'V'
+                q.append((i, len(board[0]) - 1))
 
-        for j in xrange(len(board[0])):
-            q.append((0, j))
-            q.append((len(board) - 1, j))
+        for i in xrange(1, len(board[0])-1):
+            if board[0][i] == 'O':
+                board[0][i] = 'V'
+                q.append((0, i))
+            if board[len(board) - 1][i] == 'O':
+                board[len(board) - 1][i] = 'V'
+                q.append((len(board) - 1, i))
 
         while q:
             i, j = q.popleft()
-            if board[i][j] in ['O', 'V']:
-                board[i][j] = 'V'
-                for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
-                    if 0 <= x < len(board) and 0 <= y < len(board[0]) and \
-                       board[x][y] == 'O':
-                        board[x][y] = 'V'
-                        q.append((x, y))
+            for x, y in [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]:
+                if 0 <= x < len(board) and 0 <= y < len(board[0]) and \
+                   board[x][y] == 'O':
+                    board[x][y] = 'V'
+                    q.append((x, y))
 
         for i in xrange(len(board)):
             for j in xrange(len(board[0])):
@@ -37,4 +44,3 @@ class Solution(object):
                     board[i][j] = 'X'
                 else:
                     board[i][j] = 'O'
-
