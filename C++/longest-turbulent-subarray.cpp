7f6976dbbb99dfd950cfbd61a24ed7532c27c3e6
kamyu <kamyu104@users.noreply.github.com>
2019-01-20 17:14:29 +0800

    Create longest-turbulent-subarray.cpp

---
+++ C++/longest-turbulent-subarray.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxTurbulenceSize(vector<int>& A) {
+        const auto& cmp = [](int a, int b) { return a != b ? (a < b ? 1 : -1) : 0; };
+        int result = 1;
+        int start = 0;
+        for (int i = 1; i < A.size(); ++i) {
+            if (i == A.size() - 1 ||
+               cmp(A[i - 1], A[i]) * cmp(A[i], A[i + 1]) != -1) {
+                result = max(result, i - start + 1);
+                start = i;
+            }
+        }
+        return result;
+    }
+};
