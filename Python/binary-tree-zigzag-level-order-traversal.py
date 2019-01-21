d9daa1b7109a8cb7c0b07b806b405ff1ca9ea487
kamyu <kamyu104@users.noreply.github.com>
2019-01-21 20:51:08 +0800

    Update binary-tree-zigzag-level-order-traversal.py

--- Python/binary-tree-zigzag-level-order-traversal.py
+++ Python/binary-tree-zigzag-level-order-traversal.py
@@ -14,30 +14,6 @@ class Solution(object):
     def zigzagLevelOrder(self, root):
         if root is None:
             return []
-        result, current, level = [], [root], 1
-        while current:
-            next_level, vals = [], []
-            for node in current:
-                vals.append(node.val)
-                if node.left:
-                    next_level.append(node.left)
-                if node.right:
-                    next_level.append(node.right)
-            if level % 2:
-                result.append(vals)
-            else:
-                result.append(vals[::-1])
-            level += 1
-            current = next_level
-        return result
-
-
-class Solution2(object):
-    # @param root, a tree node
-    # @return a list of lists of integers
-    def zigzagLevelOrder(self, root):
-        if root is None:
-            return []
         result, current = [], [root]
         while current:
             next_level, vals = [], []
