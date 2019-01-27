ba9513b7ef05130bcc10cc305b617427f40e0111
Sanghee Kim <sangheestyle@gmail.com>
2019-01-27 14:00:05 +0200

    add DFS solution

--- Python/permutations.py
+++ Python/permutations.py
@@ -22,3 +22,33 @@ class Solution(object):
                 cur.pop()
                 used[i] = False
 
+
+# time: O(n!)
+# space: O(n!)
+
+class Solution2(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res = []
+        self.dfs(nums, [], res)
+        return res
+
+    def dfs(self, nums, path, res):
+        if not nums:
+            res.append(path)
+
+        for i in range(len(nums)):
+            # e.g., [1, 2, 3]: 3! = 6 cases
+            # idx -> nums, path
+            # 0 -> [2, 3], [1] -> 0: [3], [1, 2] -> [], [1, 2, 3]
+            #                  -> 1: [2], [1, 3] -> [], [1, 3, 2]
+            #
+            # 1 -> [1, 3], [2] -> 0: [3], [2, 1] -> [], [2, 1, 3]
+            #                  -> 1: [1], [2, 3] -> [], [2, 3, 1]
+            #
+            # 2 -> [1, 2], [3] -> 0: [2], [3, 1] -> [], [3, 1, 2]
+            #                  -> 1: [1], [3, 2] -> [], [3, 2, 1]
+            self.dfs(nums[:i] + nums[i+1:], path + [nums[i]], res)
