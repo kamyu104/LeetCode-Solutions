2dccaf9acac6e1bfe734949d849a22481741449b
kamyu <kamyu104@users.noreply.github.com>
2019-01-27 16:54:33 +0800

    Create string-without-aaa-or-bbb.cpp

---
+++ C++/string-without-aaa-or-bbb.cpp
@@ -0,0 +1,26 @@
+// Time:  O(A + B)
+// Space: O(1)
+
+class Solution {
+public:
+    string strWithout3a3b(int A, int B) {
+        string result;
+        bool put_A = false;
+        while (A || B) {
+            if (result.length() >= 2 &&
+                result.back() == result[result.length() - 2]) {
+                put_A = (result.back() == 'b');
+            } else {
+                put_A = (A >= B);
+            }
+            if (put_A) {
+                --A;
+                result.push_back('a');
+            } else {
+                --B;
+                result.push_back('b');
+            }
+        }
+        return result;
+    }
+};
