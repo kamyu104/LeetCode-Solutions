9bb89fcd788100a4c57cfa7fef15ac1bea1471b8
kamyu <kamyu104@users.noreply.github.com>
2019-01-27 19:26:02 +0800

    Update triples-with-bitwise-and-equal-to-zero.py

--- Python/triples-with-bitwise-and-equal-to-zero.py
+++ Python/triples-with-bitwise-and-equal-to-zero.py
@@ -1,10 +1,52 @@
-# Time:  O(n^3)
-# Space: O(n^2)
+# Time:  O(nlogn), n is max of A
+# Space: O(n)
 
 import collections
 
 
+# https://blog.csdn.net/john123741/article/details/76576925
+# FWT solution
 class Solution(object):
+    def countTriplets(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        def FWT(A):
+            B = A[:]
+            d = 1
+            while d < len(B):
+                for i in xrange(0, len(B), d << 1):
+                    for j in xrange(d):
+                        B[i+j] = B[i+j] + B[i+j+d]
+                d <<= 1
+            return B
+
+        def UFWT(A):
+            B = A[:]
+            d = 1
+            while d < len(B):
+                for i in xrange(0, len(B), d << 1):
+                    for j in xrange(d):
+                        B[i+j] = B[i+j] - B[i+j+d]
+                d <<= 1
+            return B
+
+        n, max_A = 1, max(A)
+        while n <= max_A:
+            n *= 2
+        count = collections.Counter(A)
+        B = [count[i] for i in xrange(n)]
+        C = UFWT([x**3 for x in FWT(B)])
+        return C[0]
+
+
+# Time:  O(n^3), n is the length of A
+# Space: O(n^2)
+import collections
+
+
+class Solution2(object):
     def countTriplets(self, A):
         """
         :type A: List[int]
