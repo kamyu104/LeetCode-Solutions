7d3757fd049509fcf7593b7b02b2a4aabc9e7f0a
kamyu <kamyu104@users.noreply.github.com>
2019-01-27 18:58:59 +0800

    Update triples-with-bitwise-and-equal-to-zero.cpp

--- C++/triples-with-bitwise-and-equal-to-zero.cpp
+++ C++/triples-with-bitwise-and-equal-to-zero.cpp
@@ -1,7 +1,54 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// https://blog.csdn.net/john123741/article/details/76576925
+// FWT solution
+class Solution {
+public:
+    int countTriplets(vector<int>& A) {
+        static const int k = 3;
+
+        vector<int> B(1 << 16); 
+        for (const auto& x : A) {
+            ++B[x];
+        }
+
+        FWT(&B);
+        for(auto& x : B) {
+            x = pow(x, k);
+        }
+        UFWT(&B);
+
+        return B[0];
+    }
+    
+private:
+    void FWT(vector<int> *A) {
+        for (int d = 1; d < A->size(); d <<= 1) {
+            for (int m = d << 1, i = 0; i < A->size(); i += m) {
+                for (int j = 0; j < d ; ++j) {  
+                    int x = (*A)[i + j], y = (*A)[i + j + d];  
+                    (*A)[i + j] = x + y;
+                }
+            }  
+        }
+    }
+    
+    void UFWT(vector<int> *A) {
+        for (int d = 1; d < A->size(); d <<= 1) {
+            for (int m = d << 1, i = 0; i < A->size(); i += m) {
+                for (int j = 0; j < d ; ++j) {  
+                    int x = (*A)[i + j], y = (*A)[i + j + d];  
+                    (*A)[i + j] = x - y;
+                }
+            }  
+        }
+    }
+};
+
 // Time:  O(n^3)
 // Space: O(n^2)
-
-class Solution {
+class Solution2 {
 public:
     int countTriplets(vector<int>& A) {
         unordered_map<int, int> count;
