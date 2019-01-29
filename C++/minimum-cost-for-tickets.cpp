7839db660aa81795449ab71cbdfa9bbaaf987308
kamyu <kamyu104@users.noreply.github.com>
2019-01-29 10:02:07 +0800

    Update minimum-cost-for-tickets.cpp

--- C++/minimum-cost-for-tickets.cpp
+++ C++/minimum-cost-for-tickets.cpp
@@ -5,19 +5,21 @@ class Solution {
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         static vector<int> durations{1, 7, 30};
-
-        vector<int> dp(days.size() + 1, numeric_limits<int>::max());
+        
+        const int W = durations.back();
+        vector<int> dp(W, numeric_limits<int>::max());
         dp[0] = 0;
         vector<int> last_buy_days{0, 0, 0};
         for (int i = 1; i < days.size() + 1; ++i) {
+            dp[i % W] = numeric_limits<int>::max();
             for (int j = 0; j < durations.size(); ++j) {
                 while (i - 1 < days.size() &&
                        days[i-1] > days[last_buy_days[j]]+durations[j] - 1) {
                     ++last_buy_days[j];  // Time: O(n)
                 }
-                dp[i] = min(dp[i], dp[last_buy_days[j]]+costs[j]);
+                dp[i % W] = min(dp[i % W], dp[last_buy_days[j] % W]+costs[j]);
             }
         }
-        return dp.back();
+        return dp[days.size() % W];
     }
 };
