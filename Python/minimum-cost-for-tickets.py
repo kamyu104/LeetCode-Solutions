3f7d273506ffd7368605b0a3037f518deb78ca99
kamyu <kamyu104@users.noreply.github.com>
2019-01-29 10:11:40 +0800

    Update minimum-cost-for-tickets.py

--- Python/minimum-cost-for-tickets.py
+++ Python/minimum-cost-for-tickets.py
@@ -1,5 +1,5 @@
 # Time:  O(n)
-# space: O(n)
+# space: O(1)
 
 class Solution(object):
     def mincostTickets(self, days, costs):
@@ -8,14 +8,16 @@ class Solution(object):
         :type costs: List[int]
         :rtype: int
         """
-        dp = [float('inf') for i in xrange(len(days)+1)]
+        dp = [float("inf") for i in xrange(len(days)+1)]
         dp[0] = 0
         last_buy_days = [0, 0, 0]
         durations = [1, 7, 30]
+        W = durations[-1]
         for i in xrange(1,len(days)+1):
             for j in xrange(len(durations)):
+                dp[i%W] = float("inf")
                 while i-1 < len(days) and \
                       days[i-1] > days[last_buy_days[j]]+durations[j]-1:
                     last_buy_days[j] += 1  # Time: O(n)
-                dp[i] = min(dp[i], dp[last_buy_days[j]]+costs[j])
-        return dp[-1]
+                dp[i%W] = min(dp[i%W], dp[last_buy_days[j]%W]+costs[j])
+        return dp[len(days)%W]
