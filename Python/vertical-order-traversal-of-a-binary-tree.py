e13bf2aa12d51db598d3bbd165f875bb50d661f5
kamyu <kamyu104@users.noreply.github.com>
2019-02-03 14:18:55 +0800

    Create vertical-order-traversal-of-a-binary-tree.py

---
+++ Python/vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,34 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def verticalTraversal(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        def dfs(node, lookup, x, y):
+            if not node:
+                return
+            lookup[x][y].append(node)
+            dfs(node.left, lookup, x-1, y+1)
+            dfs(node.right, lookup, x+1, y+1)
+                
+        lookup = collections.defaultdict(lambda: collections.defaultdict(list))
+        dfs(root, lookup, 0, 0)
+
+        result = []
+        for x in sorted(lookup):
+            report = []
+            for y in sorted(lookup[x]):
+                report.extend(sorted(node.val for node in lookup[x][y]))
+            result.append(report)
+        return result
