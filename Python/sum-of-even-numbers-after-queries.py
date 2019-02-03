a7d60f8a7b24ba27f5960189ed583d47186d722b
kamyu <kamyu104@users.noreply.github.com>
2019-02-03 13:50:04 +0800

    Create sum-of-even-numbers-after-queries.py

---
+++ Python/sum-of-even-numbers-after-queries.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def sumEvenAfterQueries(self, A, queries):
+        """
+        :type A: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        total = sum(v for v in A if v % 2 == 0)
+        
+        result = []
+        for v, i in queries:
+            if A[i] % 2 == 0:
+                total -= A[i]
+            A[i] += v
+            if A[i] % 2 == 0:
+                total += A[i]
+            result.append(total)
+        return result
