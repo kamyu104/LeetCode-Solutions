c016fde41596b57b40771d82bd22702bb196b2ed
kamyu <kamyu104@users.noreply.github.com>
2019-02-03 13:49:10 +0800

    Create sum-of-even-numbers-after-queries.cpp

---
+++ C++/sum-of-even-numbers-after-queries.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<int> sumEvenAfterQueries(vector<int>& A, vector<vector<int>>& queries) {
+        auto total = accumulate(A.cbegin(), A.cend(), 0,
+                                [](int sum, int val) {
+                                   return sum + (val % 2 == 0 ? val : 0); 
+                                });
+
+        vector<int> result;
+        for (const auto& q : queries) {
+            int v = q[0], i = q[1];
+            if (A[i] % 2 == 0) {
+                total -= A[i];
+            }
+            A[i] += v;
+            if (A[i] % 2 == 0) {
+                total += A[i];
+            }
+            result.emplace_back(total);
+        }
+        return result;
+    }
+};
