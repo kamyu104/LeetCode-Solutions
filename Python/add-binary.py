74e787f8e81e8362c44e5c2776613878ce25aac4
Sanghee Kim <sangheestyle@gmail.com>
2019-02-10 23:30:19 +0200

    add a solution

--- Python/add-binary.py
+++ Python/add-binary.py
@@ -20,3 +20,26 @@ class Solution(object):
             result += str(carry)
         return result[::-1]
 
+
+# time: O(n)
+# space: O(1)
+from itertools import izip_longest
+
+
+class Solution2(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        result = ""
+        carry = 0
+        for x, y in izip_longest(reversed(a), reversed(b), fillvalue="0"):
+            carry, remainder = divmod(int(x)+int(y)+carry, 2)
+            result += str(remainder)
+        
+        if carry:
+            result += str(carry)
+        
+        return result[::-1]
