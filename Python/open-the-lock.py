0a135d5e83aa2d322639376be443bf72071076db
Sanghee Kim <sangheestyle@gmail.com>
2019-02-10 16:46:39 +0200

    update open-the-lock.py

--- Python/open-the-lock.py
+++ Python/open-the-lock.py
@@ -27,7 +27,7 @@ class Solution(object):
                         if neighbor not in lookup:
                             lookup.add(neighbor)
                             next_q.append(neighbor)
-            q, next_q = next_q, []
+            q = next_q
             depth += 1
         return -1
 
