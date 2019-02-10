385943b6b547aa415eec9d25fb919c6e2c563909
kamyu <kamyu104@users.noreply.github.com>
2019-02-10 19:01:47 +0800

    Update subarrays-with-k-different-integers.cpp

--- C++/subarrays-with-k-different-integers.cpp
+++ C++/subarrays-with-k-different-integers.cpp
@@ -4,6 +4,32 @@
 class Solution {
 public:
     int subarraysWithKDistinct(vector<int>& A, int K) {
+        return atMostK(A, K) - atMostK(A, K - 1);
+    }
+
+private:
+    int atMostK(const vector<int>& A, int K) {
+        int result = 0, left = 0;
+        unordered_map<int, int> count;
+        for (int right = 0; right < A.size(); ++right) {
+            ++count[A[right]];
+            while (count.size() > K) {
+                if (!--count[A[left]]) {
+                    count.erase(A[left]);
+                }
+                ++left;
+            }
+            result += right - left + 1;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(k)
+class Solution2 {
+public:
+    int subarraysWithKDistinct(vector<int>& A, int K) {
         Window window1, window2;
         int result = 0, left1 = 0, left2 = 0;
         for (const auto& i : A) {
