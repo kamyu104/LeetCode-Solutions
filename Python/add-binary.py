196aeb421b712de5fca837b023905d379de2eae2
SangHee Kim <sangheestyle@gmail.com>
2019-02-11 15:51:21 +0200

    add a solution (#68)
    * add a solution
    
    * Update add-binary.py

--- Python/add-binary.py
+++ Python/add-binary.py
@@ -1,7 +1,6 @@
 # Time:  O(n)
 # Space: O(1)
 
-
 class Solution(object):
     # @param a, a string
     # @param b, a string
@@ -14,9 +13,32 @@ class Solution(object):
                 val += int(a[-(i + 1)])
             if i < len(b):
                 val += int(b[-(i + 1)])
-            carry, val = val / 2, val % 2
+            carry, val = divmod(val, 2)
             result += str(val)
         if carry:
             result += str(carry)
         return result[::-1]
 
+
+# Time:  O(n)
+# Space: O(1)
+from itertools import izip_longest
+
+
+class Solution2(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        result = ""
+        carry = 0
+        for x, y in izip_longest(reversed(a), reversed(b), fillvalue="0"):
+            carry, remainder = divmod(int(x)+int(y)+carry, 2)
+            result += str(remainder)
+        
+        if carry:
+            result += str(carry)
+        
+        return result[::-1]
