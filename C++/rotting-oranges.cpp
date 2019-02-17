517831b67126eeea7adbef89e31e80811c342cbe
kamyu <kamyu104@users.noreply.github.com>
2019-02-17 17:17:57 +0800

    Create rotting-oranges.cpp

---
+++ C++/rotting-oranges.cpp
@@ -0,0 +1,41 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        static const vector<pair<int, int>> directions{{0, 1}, {1, 0},
+                                                       {0, -1}, {-1, 0}};
+
+        int count = 0;
+        queue<tuple<int, int, int>> q;
+        for (int r = 0; r < grid.size(); ++r) {
+            for (int c = 0; c < grid[r].size(); ++c) {
+                if (grid[r][c] == 2) {
+                    q.emplace(r, c, 0);
+                } else if (grid[r][c]  == 1) {
+                    ++count;
+                }
+            }
+        }
+
+        int result = 0;
+        while (!q.empty()) {
+            int r, c;
+            tie(r, c, result) = q.front(); q.pop();
+            for (const auto& d : directions) {
+                int nr = r + d.first, nc = c + d.second;
+                if  (!(0 <= nr && nr < grid.size() &&
+                       0 <= nc && nc < grid[r].size())) {
+                    continue;
+                }
+                if (grid[nr][nc] == 1) {
+                    --count;
+                    grid[nr][nc] = 2;
+                    q.emplace(nr, nc, result + 1);
+                }
+            }
+        }
+        return (count == 0) ? result : -1;
+    }
+};
