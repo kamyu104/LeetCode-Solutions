b780921285b02009c29d1d671eb8e465f51ce6b2
kamyu <kamyu104@users.noreply.github.com>
2019-02-19 22:50:13 +0800

    Update optimal-account-balancing.py

--- Python/optimal-account-balancing.py
+++ Python/optimal-account-balancing.py
@@ -1,5 +1,5 @@
 # Time:  O(n * 2^n), n is the size of the debt.
-# Space: O(n * 2^n)
+# Space: O(2^n)
 
 import collections
 
@@ -10,32 +10,25 @@ class Solution(object):
         :type transactions: List[List[int]]
         :rtype: int
         """
-        account = collections.defaultdict(int)
+        accounts = collections.defaultdict(int)
         for transaction in transactions:
-            account[transaction[0]] += transaction[2]
-            account[transaction[1]] -= transaction[2]
+            accounts[transaction[0]] += transaction[2]
+            accounts[transaction[1]] -= transaction[2]
 
-        debt = []
-        for v in account.values():
-            if v:
-                debt.append(v)
-
-        if not debt:
+        debts = [account for account in accounts.values() if account]
+            
+        if not debts:
             return 0
 
-        n = 1 << len(debt)
-        dp, subset = [float("inf")] * n, []
-        for i in xrange(1, n):
-            net_debt, number = 0, 0
-            for j in xrange(len(debt)):
-                if i & 1 << j:
-                    net_debt += debt[j]
-                    number += 1
-            if net_debt == 0:
-                dp[i] = number - 1
-                for s in subset:
-                    if (i & s) == s:
-                        dp[i] = min(dp[i], dp[s] + dp[i - s])
-                subset.append(i)
-        return dp[-1]
-
+        dp = [0]*(2**len(debts))
+        sums = [0]*(2**len(debts))
+        for i in xrange(len(dp)):
+            for j in xrange(len(debts)):
+                if (i & (1<<j)) == 0:
+                    nxt = i | (1<<j)
+                    sums[nxt] = sums[i]+debts[j]
+                    if sums[nxt] == 0:
+                        dp[nxt] = max(dp[nxt], dp[i]+1)
+                    else:
+                        dp[nxt] = max(dp[nxt], dp[i])
+        return len(debts)-dp[-1]
