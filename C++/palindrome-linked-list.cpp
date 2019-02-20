9d6a362618c08d6cbbf09cdfacd517dd9553b0d4
Lysander <fraiic@163.com>
2019-02-21 00:02:17 +0800

    fix 234. Palindrome Linked List

--- C++/palindrome-linked-list.cpp
+++ C++/palindrome-linked-list.cpp
@@ -22,9 +22,9 @@ public:
             head = head_next;
         }
 
-        // If the number of the nodes is odd,
+        // If the number of the nodes is even,
         // set the head of the tail list to the next of the median node.
-        ListNode *tail = fast ? head->next : head;
+        ListNode *tail = fast ? head : head->next;
 
         // Compare the reversed first half list with the second half list.
         // And restore the reversed first half list.
