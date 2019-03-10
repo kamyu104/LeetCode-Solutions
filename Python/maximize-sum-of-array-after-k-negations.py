72569e831b7ebfe50fe3a580aa8d68cb42b602a0
kamyu <kamyu104@users.noreply.github.com>
2019-03-10 17:15:25 +0800

    Update maximize-sum-of-array-after-k-negations.py

--- Python/maximize-sum-of-array-after-k-negations.py
+++ Python/maximize-sum-of-array-after-k-negations.py
@@ -1,7 +1,52 @@
-# Time:  O(nlogn)
+# Time:  O(n) ~ O(n^2), O(n) on average.
 # Space: O(1)
 
+import random
+
+
+# quick select solution
 class Solution(object):
+    def largestSumAfterKNegations(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        def kthElement(nums, k, compare):
+            def PartitionAroundPivot(left, right, pivot_idx, nums, compare):
+                new_pivot_idx = left
+                nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+                for i in xrange(left, right):
+                    if compare(nums[i], nums[right]):
+                        nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                        new_pivot_idx += 1
+
+                nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+                return new_pivot_idx
+
+            left, right = 0, len(nums) - 1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, nums, compare)
+                if new_pivot_idx == k - 1:
+                    return
+                elif new_pivot_idx > k - 1:
+                    right = new_pivot_idx - 1
+                else:  # new_pivot_idx < k - 1.
+                    left = new_pivot_idx + 1
+                    
+        kthElement(A, K, lambda a, b: a < b)
+        remain = K
+        for i in xrange(K):
+            if A[i] < 0:
+                A[i] = -A[i]
+                remain -= 1
+        return sum(A) - ((remain)%2)*min(A)*2
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+class Solution2(object):
     def largestSumAfterKNegations(self, A, K):
         """
         :type A: List[int]
