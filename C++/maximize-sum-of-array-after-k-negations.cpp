28b96f076781523585b1d3b1f911568de4983e51
kamyu <kamyu104@users.noreply.github.com>
2019-03-10 17:01:15 +0800

    Update maximize-sum-of-array-after-k-negations.cpp

--- C++/maximize-sum-of-array-after-k-negations.cpp
+++ C++/maximize-sum-of-array-after-k-negations.cpp
@@ -1,7 +1,24 @@
-// Time:  O(nlogn)
+// Time:  O(n) ~ O(n^2), O(n) on average.
 // Space: O(1)
 
 class Solution {
+public:
+    int largestSumAfterKNegations(vector<int>& A, int K) {
+        nth_element(A.begin(), A.begin() + K, A.end());
+        for (int i = 0; K > 0 && i < A.size(); ++i) {
+            if (A[i] < 0) {
+                A[i] = -A[i];
+                --K;
+            }
+        }
+        return accumulate(A.cbegin(), A.cend(), 0) -
+               (K % 2) * *min_element(A.cbegin(), A.cend()) * 2;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+class Solution2 {
 public:
     int largestSumAfterKNegations(vector<int>& A, int K) {
         sort(A.begin(), A.end());
