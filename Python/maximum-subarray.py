7db8418a33434c1287e0a03c72a787bcfe07bd1c
kamyu <kamyu104@users.noreply.github.com>
2019-03-18 21:43:29 +0800

    Update maximum-subarray.py

--- Python/maximum-subarray.py
+++ Python/maximum-subarray.py
@@ -7,12 +7,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if max(nums) < 0:
-            return max(nums)
+        max_nums = max(nums)
+        if max_nums < 0:
+            return max_nums
         global_max, local_max = 0, 0
         for x in nums:
             local_max = max(0, local_max + x)
             global_max = max(global_max, local_max)
         return global_max
-
-
