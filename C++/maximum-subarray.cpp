71a8e25b94506a82e88563066fb31d3548a9d23d
kamyu <kamyu104@users.noreply.github.com>
2019-03-18 21:44:04 +0800

    Update maximum-subarray.cpp

--- C++/maximum-subarray.cpp
+++ C++/maximum-subarray.cpp
@@ -1,22 +1,18 @@
-// Time:  O(logn)
+// Time:  O(n)
 // Space: O(1)
 
 class Solution {
 public:
-
     int maxSubArray(vector<int>& nums) {
-      int max_num = nums[0];
-      for(auto &i : nums){
-        max_num = std::max(i, max_num);
-      }
-      if(max_num < 0)
-        return max_num;
-      int global_max = 0, local_max = 0;
-      for(auto &x : nums){
-        local_max = std::max(0, local_max + x);
-        global_max = std::max(global_max, local_max);
-      }
-      return global_max;
+        auto max_num = *max_element(nums.cbegin(), nums.cend());
+        if (max_num < 0) {
+            return max_num;
+        }
+        int global_max = 0, local_max = 0;
+        for (const auto &x : nums) {
+           local_max = std::max(0, local_max + x);
+           global_max = std::max(global_max, local_max);
+        }
+        return global_max;
     }
-    
 };
