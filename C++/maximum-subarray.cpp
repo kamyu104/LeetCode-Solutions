257c8699b203c826c0b0a1f5a5c673ae1bd4f1b1
ycsheu <38524722+ycsheu@users.noreply.github.com>
2019-03-18 21:44:21 +0800

    add Maximum Subarray C++ version (#73)
    * Update README.md
    
    * Update README.md
    
    * Create maximum-subarray.cpp
    
    * Update maximum-subarray.cpp

---
+++ C++/maximum-subarray.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        auto max_num = *max_element(nums.cbegin(), nums.cend());
+        if (max_num < 0) {
+            return max_num;
+        }
+        int global_max = 0, local_max = 0;
+        for (const auto &x : nums) {
+           local_max = std::max(0, local_max + x);
+           global_max = std::max(global_max, local_max);
+        }
+        return global_max;
+    }
+};
