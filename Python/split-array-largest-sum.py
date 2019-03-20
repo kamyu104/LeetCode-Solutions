4f84df2bc7ac2cc981f8aaef23e5b71888d427aa
kamyu <kamyu104@users.noreply.github.com>
2019-03-20 13:29:19 +0800

    Update split-array-largest-sum.py

--- Python/split-array-largest-sum.py
+++ Python/split-array-largest-sum.py
@@ -1,6 +1,7 @@
 # Time:  O(nlogs), s is the sum of nums
 # Space: O(1)
 
+# Minimize the largest subarray sum.
 class Solution(object):
     def splitArray(self, nums, m):
         """
@@ -17,11 +18,7 @@ class Solution(object):
                     cnt += 1
             return cnt <= m
 
-        left, right = 0, 0
-        for num in nums:
-            left = max(left, num)
-            right += num
-
+        left, right = max(nums), sum(nums)
         while left <= right:
             mid = left + (right - left) / 2
             if canSplit(nums, m, mid):
@@ -30,3 +27,33 @@ class Solution(object):
                 left = mid + 1
         return left
 
+
+# Time:  O(nlogs), s is the sum of nums
+# Space: O(1)
+
+# Maximize the smallest subarray sum.
+class SolutionFindMaxMin(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        def canSplit(nums, m, s):
+            cnt, curr_sum = 1, 0
+            for num in nums:
+                curr_sum += num
+                if curr_sum > s:
+                    curr_sum = 0
+                    cnt += 1
+            return cnt <= m
+
+        left, right = min(nums), sum(nums)
+        while left <= right:
+            mid = left + (right - left) / 2
+            x = canSplit(nums, m, mid)
+            if x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
