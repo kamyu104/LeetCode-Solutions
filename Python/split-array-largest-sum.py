421ed7f4f37f3fbdbcf0e729f60cec1246326528
kamyu <kamyu104@users.noreply.github.com>
2019-03-20 14:21:58 +0800

    Update split-array-largest-sum.py

--- Python/split-array-largest-sum.py
+++ Python/split-array-largest-sum.py
@@ -57,3 +57,36 @@ class SolutionFindMaxMin(object):
             else:
                 left = mid + 1
         return left
+
+    
+# Time:  O(nlogs), s is the sum of nums
+# Space: O(1)
+
+# Maximize the smallest subarray sum.
+class SolutionFindMaxMin2(object):
+    def splitArray(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: int
+        """
+        def canSplit(nums, m, s):
+            cnt, curr_sum = 0, 0
+            for num in nums:
+                curr_sum += num
+                if curr_sum >= s:
+                    curr_sum = 0
+                    cnt += 1
+            return cnt < m
+
+        left, right = min(nums), sum(nums)
+        for i in xrange(left, right+1):
+            canSplit(nums, m, i)
+        while left <= right:
+            mid = left + (right - left) / 2
+            x = canSplit(nums, m, mid)
+            if x:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left - 1
