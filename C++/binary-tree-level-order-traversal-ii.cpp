85582f35cb80f3c6a51fcd303c285e34495e88c9
ycsheu <38524722+ycsheu@users.noreply.github.com>
2019-03-22 15:23:16 +0800

    Create binary-tree-level-order-traversal-ii.cpp

---
+++ C++/binary-tree-level-order-traversal-ii.cpp
@@ -0,0 +1,46 @@
+// Time:  O(n)
+// Space: O(n)
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
+        queue<TreeNode *> que;
+
+        if (root != nullptr) {
+            que.emplace(root);
+        }
+
+        while (!que.empty()) {
+            vector<int> level;
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                auto *front = que.front();
+                que.pop();
+                level.emplace_back(front->val);
+                if (front->left != nullptr) {
+                    que.emplace(front->left);
+                }
+                if (front->right != nullptr) {
+                    que.emplace(front->right);
+                }
+            }
+            result.emplace_back(move(level));
+        }
+        
+        // The only difference between binary-tree-level-order-traversal
+        // and binary-tree-level-order-traversal-ii
+        reverse(result.begin(), result.end());
+        
+        return result;
+    }
+};
