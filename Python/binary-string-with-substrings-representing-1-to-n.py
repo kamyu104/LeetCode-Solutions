631172f1db1c3bb7ed2de901df1933896e9777fd
kamyu <kamyu104@users.noreply.github.com>
2019-03-24 18:52:45 +0800

    Create binary-string-with-substrings-representing-1-to-n.py

---
+++ Python/binary-string-with-substrings-representing-1-to-n.py
@@ -0,0 +1,14 @@
+# Time:  O(n^2), n is the length of S
+# Space: O(1)
+
+class Solution(object):
+    def queryString(self, S, N):
+        """
+        :type S: str
+        :type N: int
+        :rtype: bool
+        """
+        # since S with length n has at most different n-k+1 k-digit numbers
+        # => given S with length n, N is at most 2(n-k+1)
+        # => N <= 2(n-k+1) < 2n = 2 * S.length
+        return all(bin(i)[2:] in S for i in reversed(xrange(N//2, N+1)))
