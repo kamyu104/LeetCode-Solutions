38bd1b4a7eae4601437b63252af29d530adf9dfe
kamyu <kamyu104@users.noreply.github.com>
2019-03-24 18:58:08 +0800

    Create partition-array-into-three-parts-with-equal-sum.cpp

---
+++ C++/partition-array-into-three-parts-with-equal-sum.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& A) {
+        const auto& total = accumulate(A.cbegin(), A.cend(), 0);
+        if (total % 3 != 0) {
+            return false;
+        }
+        int curr = 0, parts = 0;
+        for (const auto& x : A) {
+            curr += x;
+            if (sum == total / 3) {
+                ++parts, curr = 0;
+            }
+        }
+        return parts >= 3;
+    }
+};
