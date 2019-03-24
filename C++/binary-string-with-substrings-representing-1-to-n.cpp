7d3a2578a35eb8448bd59908645265276b7105e9
kamyu <kamyu104@users.noreply.github.com>
2019-03-24 18:51:09 +0800

    Create binary-string-with-substrings-representing-1-to-n.cpp

---
+++ C++/binary-string-with-substrings-representing-1-to-n.cpp
@@ -0,0 +1,19 @@
+// Time:  O(S^2)
+// Space: O(1)
+    
+class Solution {
+public:
+    bool queryString(string S, int N) {
+        // since S with length n has at most different n-k+1 k-digit numbers
+        // => given S with length n, valid N is at most 2(n-k+1)
+        // => valid N <= 2(n-k+1) < 2n = 2 * S.length
+        for (int i = N; i > N / 2; --i) {
+            // if bin_i is a substring of S, bin_i/2 is too
+            const auto& b = bitset<32>(i).to_string();
+            if (S.find(b.substr(b.find("1"))) == string::npos) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
