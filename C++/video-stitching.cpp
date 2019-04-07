e6b79c7de2f459407786faf0523e9719dea2fbaf
kamyu <kamyu104@users.noreply.github.com>
2019-04-08 06:19:30 +0800

    Update video-stitching.cpp

--- C++/video-stitching.cpp
+++ C++/video-stitching.cpp
@@ -4,8 +4,8 @@
 class Solution {
 public:
     int videoStitching(vector<vector<int>>& clips, int T) {
-        int result = 0;
-        int curr_reachable = -1, reachable = 0;
+        int result = 1;
+        int curr_reachable = 0, reachable = 0;
         sort(clips.begin(), clips.end());
         for (const auto& clip : clips) {
             int left = clip[0], right = clip[1];
