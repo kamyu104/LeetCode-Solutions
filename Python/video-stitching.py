0786d9601a6450ca0e58f6d1e0042734f176a902
kamyu <kamyu104@users.noreply.github.com>
2019-04-08 04:49:24 +0800

    Update video-stitching.py

--- Python/video-stitching.py
+++ Python/video-stitching.py
@@ -12,10 +12,12 @@ class Solution(object):
         curr_reachable, reachable = -1, 0
         clips.sort()
         for left, right in clips:
-            if reachable >= T or left > reachable:
-                break
+            if left > reachable:
+                return -1
             elif left > curr_reachable:
                 curr_reachable = reachable
                 result += 1
             reachable = max(reachable, right)
-        return result if reachable >= T else -1
+            if reachable >= T:
+                return result
+        return -1
