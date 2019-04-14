28702d7c81abe782926c209a53d8f3ee0f12b355
kamyu <kamyu104@users.noreply.github.com>
2019-04-14 15:34:43 +0800

    Update divisor-game.py

--- Python/divisor-game.py
+++ Python/divisor-game.py
@@ -7,11 +7,11 @@ class Solution(object):
         :type N: int
         :rtype: bool
         """
-        # 1. if Alice gets an even, she can choose x = 1
-        #    to make Bob always get an odd
-        # 2. if Bob gets an odd, he can only choose x = 1 or other odds
-        #    and Alice can still get an even
-        # 3. at the end, Bob can only choose x = 1 and Alice wins
-        # 4. in summary, Alice wins if only if she gets an even and 
-        #    keeps even until Bob loses
+        # 1. if we get an even, we can choose x = 1
+        #    to make the opponent always get an odd
+        # 2. if the opponent gets an odd, he can only choose x = 1 or other odds
+        #    and we can still get an even
+        # 3. at the end, the opponent can only choose x = 1 and we win
+        # 4. in summary, we win if only if we get an even and 
+        #    keeps even until the opponent loses
         return N % 2 == 0
