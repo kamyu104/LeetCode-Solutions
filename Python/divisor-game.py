cc10067a6504315a9166aa4f5a74575e63b76610
kamyu <kamyu104@users.noreply.github.com>
2019-04-14 15:17:20 +0800

    Create divisor-game.py

---
+++ Python/divisor-game.py
@@ -0,0 +1,10 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def divisorGame(self, N):
+        """
+        :type N: int
+        :rtype: bool
+        """
+        return N % 2 == 0
