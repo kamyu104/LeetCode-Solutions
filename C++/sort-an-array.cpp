2d6fff9bc5a6f0d90bd905651934dc7886052db3
kamyu <kamyu104@users.noreply.github.com>
2019-04-22 02:55:49 +0800

    Update sort-an-array.cpp

--- C++/sort-an-array.cpp
+++ C++/sort-an-array.cpp
@@ -32,3 +32,28 @@ private:
         copy(tmp.begin(), tmp.end(), nums->begin() + start);
     }
 };
+
+// Time:  O(nlogn), on average
+// Space: O(logn), on average
+// quick sort solution
+class Solution2 {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        quickSort(0, nums.size() / 2, nums.size(),
+                  &nums);
+        return nums;
+    }
+
+private:
+    void quickSort(int start, int mid, int end,
+                   vector<int> *nums) {
+        if (end - start <= 1) {
+            return;
+        }
+        nth_element(nums->begin() + start,
+                    nums->begin() + mid,
+                    nums->begin() + end);
+        quickSort(start, start + (mid - start) / 2, mid, nums);
+        quickSort(mid, mid + (end - mid) / 2, end, nums);
+    }
+};
