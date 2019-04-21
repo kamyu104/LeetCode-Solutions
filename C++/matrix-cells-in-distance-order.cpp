b486391a692d99fd8fcbc353bf55517e5fd2d730
kamyu <kamyu104@users.noreply.github.com>
2019-04-21 23:50:02 +0800

    Update matrix-cells-in-distance-order.cpp

--- C++/matrix-cells-in-distance-order.cpp
+++ C++/matrix-cells-in-distance-order.cpp
@@ -6,12 +6,12 @@ public:
     vector<vector<int>> allCellsDistOrder(int R, int C, int r0, int c0) {
         vector<vector<int>> result{{r0, c0}};
         for (auto d = 1; d < R + C; ++d) {
-            append(R, C, r0 + d, c0, &result);
-            for (int x = d - 1; x >= -d + 1; --x) {
+            append(R, C, r0 - d, c0, &result);
+            for (int x = -d + 1; x < d; ++x) {
                 append(R, C, r0 + x, c0 + d - abs(x), &result);
                 append(R, C, r0 + x, c0 + abs(x) - d, &result);
             }
-            append(R, C, r0 - d, c0, &result);
+            append(R, C, r0 + d, c0, &result);
          }
          return result;
     }
