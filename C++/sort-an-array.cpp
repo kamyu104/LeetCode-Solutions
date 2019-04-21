acfbea839638d0af89e917fd79691270ae003443
kamyu <kamyu104@users.noreply.github.com>
2019-04-22 04:26:54 +0800

    Update sort-an-array.cpp

--- C++/sort-an-array.cpp
+++ C++/sort-an-array.cpp
@@ -5,22 +5,18 @@
 class Solution {
 public:
     vector<int> sortArray(vector<int>& nums) {
-        mergeSort(0, nums.size(),
-                  numeric_limits<int>::min(),
-                  numeric_limits<int>::max(),
-                  &nums);
+        mergeSort(0, nums.size(), &nums);
         return nums;
     }
 
 private:
-    void mergeSort(int start, int end, int lower, int upper,
-                         vector<int> *nums) {
+    void mergeSort(int start, int end, vector<int> *nums) {
         if (end - start <= 1) {
             return;
         }
-        int mid = start + (end - start) / 2;
-        mergeSort(start, mid, lower, upper, nums);
-        mergeSort(mid, end, lower, upper, nums);
+        const auto& mid = start + (end - start) / 2;
+        mergeSort(start, mid, nums);
+        mergeSort(mid, end, nums);
         int r = mid;
         vector<int> tmp;
         for (int i = start; i < mid; ++i) {
