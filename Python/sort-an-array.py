671aa53c4108b1687e447d5ddd358527da4a809b
kamyu <kamyu104@users.noreply.github.com>
2019-04-22 03:08:11 +0800

    Update sort-an-array.py

--- Python/sort-an-array.py
+++ Python/sort-an-array.py
@@ -25,3 +25,48 @@ class Solution(object):
 
         mergeSort(0, len(nums), float("-inf"), float("inf"), nums)
         return nums
+
+
+# Time:  O(nlogn), on average
+# Space: O(logn), on average
+import random
+# quick sort solution
+class Solution2(object):
+    def sortArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def kthElement(nums, left, mid, right, compare):
+            def PartitionAroundPivot(left, right, pivot_idx, nums, compare):
+                new_pivot_idx = left
+                nums[pivot_idx], nums[right] = nums[right], nums[pivot_idx]
+                for i in xrange(left, right):
+                    if compare(nums[i], nums[right]):
+                        nums[i], nums[new_pivot_idx] = nums[new_pivot_idx], nums[i]
+                        new_pivot_idx += 1
+
+                nums[right], nums[new_pivot_idx] = nums[new_pivot_idx], nums[right]
+                return new_pivot_idx
+
+            right -= 1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                new_pivot_idx = PartitionAroundPivot(left, right, pivot_idx, nums, compare)
+                if new_pivot_idx == mid - 1:
+                    return
+                elif new_pivot_idx > mid - 1:
+                    right = new_pivot_idx - 1
+                else:  # new_pivot_idx < mid - 1.
+                    left = new_pivot_idx + 1
+                    
+        def quickSort(start, mid, end, nums):
+            if end - start <= 1:
+                return
+            kthElement(nums, start, mid, end, lambda a, b: a < b)
+            mid = start + (end - start) / 2
+            quickSort(start, start + (mid - start) / 2, mid, nums)
+            quickSort(mid, mid + (end - mid) / 2, end, nums)
+
+        quickSort(0, len(nums) / 2, len(nums), nums)
+        return nums
