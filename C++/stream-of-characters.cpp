9d96cb58c5f35322cee51c93d01200639e896be2
kamyu <kamyu104@users.noreply.github.com>
2019-04-21 22:20:18 +0800

    Create stream-of-characters.cpp

---
+++ C++/stream-of-characters.cpp
@@ -0,0 +1,121 @@
+// Time:  ctor:  O(n), n is the total size of patterns
+//       query: O(m + z), m is the total size of query string
+//                      , z is the number of all matched strings 
+// Space: O(t), t is the total size of ac automata trie
+
+// Aho–Corasick automata
+// reference:
+// 1. http://web.stanford.edu/class/archive/cs/cs166/cs166.1166/lectures/02/Small02.pdf
+// 2. http://algo.pw/algo/64/python
+
+struct AhoNode {
+    vector<AhoNode *> children;
+    vector<int> indices;
+    AhoNode *suffix;
+    AhoNode *output;
+    AhoNode() :
+        children(26, nullptr),
+        suffix(nullptr),
+        output(nullptr) {}
+};
+
+class AhoTrie {
+public:
+    AhoTrie(const vector<string>& patterns) {
+        root_ = CreateACTrie(patterns);
+        node_ = CreateACSuffixAndOutputLinks(root_);
+    }
+
+    bool query(char letter) {
+        while (node_ && !node_->children[letter - 'a']) {
+            node_ = node_->suffix;
+        }
+        if (!node_) {
+            node_ = root_;
+            return false;
+        }
+    
+        vector<int> result;
+        node_ = node_->children[letter - 'a'];
+        for (const auto& i : node_->indices) {
+            result.emplace_back(i);
+        }
+        auto output = node_->output;
+        while (output) {
+            for (const auto& i : output->indices) {
+                result.emplace_back(i);
+            }
+            output = output->output;
+        }
+        return !result.empty();
+    }
+
+private:
+    AhoNode *CreateACTrie(const vector<string>& patterns) {
+        auto root = new AhoNode();
+        for (int i = 0; i < patterns.size(); ++i) {
+            auto node = root;
+            for (const auto& c : patterns[i]) {
+                if (!node->children[c - 'a']) {
+                    node->children[c - 'a'] = new AhoNode();
+                }
+                node = node->children[c - 'a'];
+            }
+            node->indices.emplace_back(i);
+        }
+        return root;
+    }
+    
+    AhoNode *CreateACSuffixAndOutputLinks(AhoNode *root) {
+        queue<AhoNode *> q;
+        for (auto node : root->children) {
+            if (!node) {
+                continue;
+            }
+            q.emplace(node);
+            node->suffix = root;
+        }
+                
+        while (!q.empty()) {
+            auto node = q.front(); q.pop();
+            for (int c = 0; c < node->children.size(); ++c) {
+                if (!node->children[c]) {
+                    continue;
+                }
+                auto child = node->children[c];
+                q.emplace(child);
+                auto fail = node->suffix;
+                while (fail && !fail->children[c]) {
+                    fail = fail->suffix;
+                }
+                child->suffix = fail ? fail->children[c] : root;
+                child->output = !child->suffix->indices.empty() ?
+                    child->suffix : child->suffix->output;
+            }
+        }
+        
+        return root;
+    }
+    
+    AhoNode *root_;
+    AhoNode *node_;
+};
+
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) : trie_(words) {
+    }
+    
+    bool query(char letter) {
+        return trie_.query(letter);
+    }
+
+private:
+    AhoTrie trie_;
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
