a7c6c40fd71c67291817c6e20b41c5699a6ea181
kamyu <kamyu104@users.noreply.github.com>
2019-04-22 01:32:23 +0800

    Update stream-of-characters.py

--- Python/stream-of-characters.py
+++ Python/stream-of-characters.py
@@ -63,8 +63,8 @@ class AhoTrie(object):
                 
         return root
 
-    def __get_ac_node_outputs(self, node):  # Time:  O(z), 
-                                            # in this question could be O(1) if it early returns when result is not empty
+    def __get_ac_node_outputs(self, node):  # Time:  O(z), in this question, it could be O(1) if it early returns
+                                            # when result is not empty
         result = []
         for i in node.indices:
             result.append(i)
