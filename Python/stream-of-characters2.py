9a36c55a60a7eb5409c9500458857b838d709a19
kamyu <kamyu104@users.noreply.github.com>
2019-04-21 21:17:43 +0800

    Update stream-of-characters2.py

--- Python/stream-of-characters2.py
+++ Python/stream-of-characters2.py
@@ -14,7 +14,7 @@ import collections
 
 class AhoNode(object):
     def __init__(self):
-        self.states = collections.defaultdict(AhoNode)
+        self.children = collections.defaultdict(AhoNode)
         self.suffix = None
         self.outputs = []
 
@@ -22,13 +22,13 @@ class AhoNode(object):
 class AhoTrie(object):
 
     def query(self, letter):
-        while self.__node and letter not in self.__node.states:
+        while self.__node and letter not in self.__node.children:
             self.__node = self.__node.suffix
         if not self.__node:
             self.__node = self.__root
             return False
  
-        self.__node = self.__node.states[letter]
+        self.__node = self.__node.children[letter]
         result = self.__node.outputs
         return len(result) > 0
     
@@ -43,24 +43,24 @@ class AhoTrie(object):
         for i, pattern in enumerate(patterns):
             node = root
             for c in pattern:
-                node = node.states[c]
+                node = node.children[c]
             node.outputs.append(i)
         return root
 
     def __create_ac_suffix_and_output_links(self, root):  # Time:  O(n + p^2), Space: O(t + p^2)
         queue = collections.deque()
-        for node in root.states.itervalues():
+        for node in root.children.itervalues():
             queue.append(node)
             node.suffix = root
 
         while queue:
             node = queue.popleft()
-            for key, child in node.states.iteritems():
+            for key, child in node.children.iteritems():
                 queue.append(child)
                 fail = node.suffix
-                while fail and key not in fail.states:
+                while fail and key not in fail.children:
                     fail = fail.suffix
-                child.suffix = fail.states[key] if fail else root
+                child.suffix = fail.children[key] if fail else root
                 child.outputs += child.suffix.outputs  # Time: O(p^2)
 
 
