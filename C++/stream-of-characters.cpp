36e800e7ae0a095994c0492e798ae927a51719aa
kamyu <kamyu104@users.noreply.github.com>
2019-04-21 23:15:19 +0800

    Update stream-of-characters.cpp

--- C++/stream-of-characters.cpp
+++ C++/stream-of-characters.cpp
@@ -2,6 +2,7 @@
 //       query: O(m + z), m is the total size of query string
 //                      , z is the number of all matched strings 
 // Space: O(t), t is the total size of ac automata trie
+//            , memory could be further improved by DAT (double-array trie)
 
 // Aho–Corasick automata
 // reference:
