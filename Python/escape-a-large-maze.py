43f7e179032f62c2e74609f952a81b7a0e099c6b
kamyu <kamyu104@users.noreply.github.com>
2019-04-28 15:06:15 +0800

    Update escape-a-large-maze.py

--- Python/escape-a-large-maze.py
+++ Python/escape-a-large-maze.py
@@ -1,61 +1,40 @@
-// Time:  O(B^2)
-// Space: O(B)
+# Time:  O(B^2)
+# Space: O(B)
+# Time:  O(B^2)
+# Space: O(B)
 
-class Solution {
-public:
-    template <typename T>
-    struct PairHash {
-        size_t operator()(const pair<T, T>& p) const {
-            size_t seed = 0;
-            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            return seed;
-        }
-    };
-    
-    bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
-        unordered_set<pair<int, int>, PairHash<int>> blocks;
-        for (const auto& block : blocked) {
-            blocks.emplace(block[0], block[1]);
-        }
-        return bfs(blocks, make_pair(source[0], source[1]), make_pair(target[0], target[1]));
-    }
-    
-private:
-    bool bfs(const unordered_set<pair<int, int>, PairHash<int>>& blocks,
-             const pair<int, int>& source, const pair<int, int>& target) {
-        static const int R = 1e6;
-        static const int C = 1e6;
-        static const vector<pair<int, int>> directions{{0, -1}, {0, 1},
-                                                       {-1, 0}, {1, 0}};
+class Solution(object):
+    def isEscapePossible(self, blocked, source, target):
+        """
+        :type blocked: List[List[int]]
+        :type source: List[int]
+        :type target: List[int]
+        :rtype: bool
+        """
+        R, C = 10**6, 10**6
+        directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
 
-        const auto& max_area_surrounded_by_blocks = blocks.size() * (blocks.size() - 1) / 2;
-        unordered_set<pair<int, int>, PairHash<int>> lookup{source};
-        if (lookup.size() > max_area_surrounded_by_blocks) {
-            return true;
-        }
-        queue<pair<int, int>> q({source});
-        while (!q.empty()) {
-            auto source = q.front();  q.pop();
-            if (source == target) {
-                return true;
-            }
-            for (const auto& dir : directions) {
-                const auto cr = source.first + dir.first, cc = source.second + dir.second;
-                
-                if (!((0 <= cr && cr < R) &&
-                      (0 <= cc && cc < C) &&
-                      !lookup.count(make_pair(cr, cc)) &&
-                      !blocks.count(make_pair(cr, cc)))) {
-                    continue;
-                }
-                lookup.emplace(cr, cc);
-                if (lookup.size() > max_area_surrounded_by_blocks) {
-                    return true;
-                }
-                q.emplace(cr, cc);
-            }
-        }
-        return false;
-    }
-};
+        def bfs(blocked, source, target):
+            max_area_surrounded_by_blocks = len(blocked)*(len(blocked)-1)//2
+            lookup = set([source])
+            if len(lookup) > max_area_surrounded_by_blocks:
+                return True
+            q = collections.deque([(source)])
+            while q:
+                source = q.popleft()
+                if source == target:
+                    return True
+                for direction in directions:
+                    cr, cc = source[0]+direction[0], source[1]+direction[1]
+                    if not ((0 <= cr < R) and
+                            (0 <= cc < C) and 
+                            (cr, cc) not in lookup and
+                            (cr, cc) not in blocked):
+                        continue
+                    lookup.add((cr, cc))
+                    if len(lookup) > max_area_surrounded_by_blocks:
+                        return True
+                    q.append((cr, cc))
+            return False
+        
+        return bfs(set(map(tuple, blocked)), tuple(source), tuple(target))
