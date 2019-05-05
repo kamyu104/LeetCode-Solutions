a37ff33a64896e344f14838daf4a3f917d6560ee
kamyu <kamyu104@users.noreply.github.com>
2019-05-05 15:33:56 +0800

    Update moving-stones-until-consecutive-ii.cpp

--- C++/moving-stones-until-consecutive-ii.cpp
+++ C++/moving-stones-until-consecutive-ii.cpp
@@ -5,7 +5,7 @@ class Solution {
 public:
     vector<int> numMovesStonesII(vector<int>& stones) {
         sort(stones.begin(), stones.end());
-        int left = 0, min_moves = stones.size();
+        int left = 0, min_moves = numeric_limits<int>::max();
         int max_moves = max(*stones.crbegin() - stones[1], *next(stones.crbegin(), 1) - stones[0]) -
                         (stones.size() - 2);
         for (int right = 0; right < stones.size(); ++right) {
