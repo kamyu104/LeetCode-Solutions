953280704e3a7f6536a4ea614e2db4a59e141bc5
kamyu <kamyu104@users.noreply.github.com>
2019-05-05 15:34:31 +0800

    Update moving-stones-until-consecutive-ii.py

--- Python/moving-stones-until-consecutive-ii.py
+++ Python/moving-stones-until-consecutive-ii.py
@@ -8,7 +8,7 @@ class Solution(object):
         :rtype: List[int]
         """
         stones.sort()
-        left, min_moves = 0, len(stones)
+        left, min_moves = 0, float("inf")
         max_moves = max(stones[-1]-stones[1], stones[-2]-stones[0]) - (len(stones)-2)
         for right in xrange(len(stones)):
             while stones[right]-stones[left]+1 > len(stones): # find window size <= len(stones)
