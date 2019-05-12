5e8da3616f6343e37d65aed248157b0738690da6
kamyu <kamyu104@users.noreply.github.com>
2019-05-12 14:17:15 +0800

    Update longest-duplicate-substring.py

--- Python/longest-duplicate-substring.py
+++ Python/longest-duplicate-substring.py
@@ -13,29 +13,30 @@ class Solution(object):
         M = 10**9+7
         D = 26
 
-        def check(A, L):
+        def check(S, L):
             if L < 0:
                 print L
             p = pow(D, L, M)
-            curr = reduce(lambda x, y: (D*x + y) % M, A[:L], 0)
+            curr = reduce(lambda x, y: (D*x+ord(y)-ord('a')) % M, S[:L], 0)
             lookup = collections.defaultdict(list)
             lookup[curr].append(L-1)
             for i in xrange(L, len(S)):
-                curr = (D*curr + A[i] - A[i-L]*p) % M
+                curr = ((D*curr) % M + ord(S[i])-ord('a') -
+                        ((ord(S[i-L])-ord('a'))*p) % M) % M
                 if curr in lookup:
                     for j in lookup[curr]:  # check if same string when hash is the smae
-                        if A[j-L+1:j+1] == A[i-L+1:i+1]:
+                        if S[j-L+1:j+1] == S[i-L+1:i+1]:
                             return i-L+1
                 lookup[curr].append(i)
             return 0
 
-        A = [ord(c)-ord('a') for c in S]
-        left, right = 0, len(S)
+        result, left, right = 0, 0, len(S)
         while left <= right:
             mid = left + (right-left)//2
-            if not check(A, mid):
+            if not check(S, mid):
                 right = mid-1
             else:
                 left = mid+1
-        result = check(A, right)
-        return S[result:result+right]
+        result = check(S, right)
+        return S[result:result + right]
+    
