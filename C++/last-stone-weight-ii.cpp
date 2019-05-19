5291eac3a5b9927bef32fe72e4582f76114ee752
kamyu <kamyu104@users.noreply.github.com>
2019-05-20 02:44:05 +0800

    Update last-stone-weight-ii.cpp

--- C++/last-stone-weight-ii.cpp
+++ C++/last-stone-weight-ii.cpp
@@ -5,7 +5,6 @@ class Solution {
 public:
     int lastStoneWeightII(vector<int>& stones) {
         unordered_set<int> dp = {0};
-        const auto& S = accumulate(stones.cbegin(), stones.cend(), 0);
         for (const auto& stone : stones) {
             auto tmp(dp);
             for (const auto& i : dp) {
@@ -13,6 +12,7 @@ public:
             }
             dp = move(tmp);
         };
+        const auto& S = accumulate(stones.cbegin(), stones.cend(), 0);
         auto result = numeric_limits<int>::max();
         for (const auto& i : dp) {
             result = min(result, abs(i - (S - i)));
