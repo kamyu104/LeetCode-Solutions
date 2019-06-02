e59571a731706dd6a4adc036ef4c1235768e3461
kamyu <kamyu104@users.noreply.github.com>
2019-06-03 02:25:13 +0800

    Update campus-bikes-ii.py

--- Python/campus-bikes-ii.py
+++ Python/campus-bikes-ii.py
@@ -1,5 +1,5 @@
 # Time:  O(w * b * 2^b)
-# Space: O(w * b * 2^b)
+# Space: O(b * 2^b)
 
 class Solution(object):  # this is slower than Solution2 in python
     def assignBikes(self, workers, bikes):
@@ -11,17 +11,19 @@ class Solution(object):  # this is slower than Solution2 in python
         def manhattan(p1, p2):
             return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
         
-        dp = [[float("inf")] * ((1<<len(bikes))) for _ in xrange(len(workers)+1)]
+        dp = [[float("inf")]*((1<<len(bikes))) for _ in xrange(2)]
         dp[0][0] = 0
         for i in xrange(len(workers)):
+            dp[(i+1)%2] = [float("inf")] * ((1<<len(bikes)))
             for j in xrange(len(bikes)):
                 for taken in xrange((1<<len(bikes))):
                     if taken & (1<<j):
                         continue
-                    dp[i+1][taken|(1<<j)] = min(dp[i+1][taken|(1<<j)],
-                                                dp[i][taken] +
-                                                manhattan(workers[i], bikes[j]))
-        return min(dp[-1])
+                    dp[(i+1)%2][taken|(1<<j)] = \
+                        min(dp[(i+1)%2][taken|(1<<j)],
+                            dp[i%2][taken] +
+                            manhattan(workers[i], bikes[j]))
+        return min(dp[len(workers)%2])
 
 
 # Time:  O((w * b * 2^b) * log(w * b * 2^b))
