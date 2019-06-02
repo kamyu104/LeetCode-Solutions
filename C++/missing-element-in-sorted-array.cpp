f6f01ca208091b308b266789ff0ed0f1d9fcea17
kamyu <kamyu104@users.noreply.github.com>
2019-06-02 16:44:26 +0800

    Create missing-element-in-sorted-array.cpp

---
+++ C++/missing-element-in-sorted-array.cpp
@@ -0,0 +1,27 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int missingElement(vector<int>& nums, int k) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            const auto& mid = left + (right - left) / 2;
+            if (check(nums, k, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[right] + (k - missingCount(nums, right));
+    }
+    
+private:
+    int missingCount(const vector<int>& nums, int x) {
+        return (nums[x] - nums[0] + 1) - (x - 0 + 1);
+    }
+    
+    bool check(const vector<int>& nums, int k, int x) {
+        return k <= missingCount(nums, x);
+    }
+};
