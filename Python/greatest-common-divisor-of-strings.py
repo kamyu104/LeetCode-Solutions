e56279b538cf576efef14eb57b31e7174a4528fd
kamyu <kamyu104@users.noreply.github.com>
2019-06-02 20:43:01 +0800

    Create greatest-common-divisor-of-strings.py

---
+++ Python/greatest-common-divisor-of-strings.py
@@ -0,0 +1,28 @@
+# Time:  O(m + n)
+# Space: O(1)
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        def check(s, common):
+            i = 0
+            for c in s:
+                if c != common[i]:
+                    return False
+                i = (i+1)%len(common)
+            return True
+    
+        def gcd(a, b):  # Time: O((logn)^2)
+            while b:
+                a, b = b, a % b
+            return a
+        
+        if not str1 or not str2:
+            return ""
+        c = gcd(len(str1), len(str2))
+        result = str1[:c]
+        return result if check(str1, result) and check(str2, result) else ""
