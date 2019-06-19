daa9da9697814a6ba4ba1b74885602204b54dad5
kamyu <kamyu104@users.noreply.github.com>
2019-06-19 12:14:20 +0800

    Update super-egg-drop.py

--- Python/super-egg-drop.py
+++ Python/super-egg-drop.py
@@ -9,7 +9,7 @@ class Solution(object):
         :rtype: int
         """
         def check(n, K, N):
-            # Each combinatin of n moves with k broken eggs could represent a unique F.
+            # Each combination of n moves with k broken eggs could represent a unique F.
             # Thus, the range size of F that all cominations can cover 
             # is the sum of C(n, k), k = 1..K
             total, c = 0, 1
