5ee0b36aaa2ebbdf50abbd2607f2bfbd2cf37ca8
kamyu <kamyu104@users.noreply.github.com>
2019-06-23 15:11:01 +0800

    Update brace-expansion.py

--- Python/brace-expansion.py
+++ Python/brace-expansion.py
@@ -2,7 +2,47 @@
 #                         , l is the length of a word
 # Space: O(p*l)
 
+import itertools
+
+
 class Solution(object):
+    def expand(self, S):  # nested is fine
+        """
+        :type S: str
+        :rtype: List[str]
+        """
+        def form_words(options):
+            words = list(map("".join, itertools.product(*options)))
+            words.sort()
+            return words
+
+        def generate_option(expr, i):
+            option_set = set()
+            while i[0] != len(expr) and expr[i[0]] != "}":
+                i[0] += 1  # { or ,
+                for option in generate_words(expr, i):
+                    option_set.add(option)
+            i[0] += 1  # }
+            option = list(option_set)
+            option.sort()
+            return option
+
+        def generate_words(expr, i):
+            options = []
+            while i[0] != len(expr) and expr[i[0]] not in ",}":
+                tmp = []
+                if expr[i[0]] not in "{,}":
+                    tmp.append(expr[i[0]])
+                    i[0] += 1  # a-z
+                elif expr[i[0]] == "{":
+                    tmp = generate_option(expr, i)
+                options.append(tmp)
+            return form_words(options)
+
+        return generate_words(S, [0])
+
+
+class Solution2(object):
     def expand(self, S):  # nested is fine
         """
         :type S: str
