efa8b606e84def108f116a8aecb596aa396ea588
kamyu <kamyu104@users.noreply.github.com>
2019-07-11 18:58:12 +0800

    Update falling-squares.cpp

--- C++/falling-squares.cpp
+++ C++/falling-squares.cpp
@@ -3,22 +3,22 @@
 
 class Solution {
 public:
-    vector<int> fallingSquares(vector<pair<int, int>>& positions) {
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
         vector<int> result;
         map<int, int> heights;
         int maxH = heights[-1] = 0;
         for (const auto& p : positions) {
-            auto it0 = heights.upper_bound(p.first);
-            auto it1 = heights.lower_bound(p.first + p.second);
+            auto it0 = heights.upper_bound(p[0]);
+            auto it1 = heights.lower_bound(p[0] + p[1]);
             int h0 = prev(it0)->second;
             int h1 = prev(it1)->second;
             for (auto it = it0; it != it1; ++it) {
                 h0 = max(h0, it->second);
             }
             heights.erase(it0, it1);
-            heights[p.first] = h0 + p.second;
-            heights[p.first + p.second] = h1;
-            maxH = max(maxH, h0 + p.second);
+            heights[p[0]] = h0 + p[1];
+            heights[p[0] + p[1]] = h1;
+            maxH = max(maxH, h0 + p[1]);
             result.emplace_back(maxH);
         }
         return result;
@@ -30,19 +30,19 @@ public:
 // Segment Tree solution.
 class Solution2 {
 public:
-    vector<int> fallingSquares(vector<pair<int, int>>& positions) {
+    vector<int> fallingSquares(vector<vector<int>>& positions) {
         set<int> index;
         for (const auto& position : positions) {
-            index.emplace(position.first);
-            index.emplace(position.first + position.second - 1);
+            index.emplace(position[0]);
+            index.emplace(position[0] + position[1] - 1);
         }
         SegmentTree tree(index.size());
         auto max_height = 0;
         vector<int> result;
         for (const auto& position : positions) {
-            const auto L = distance(index.begin(), index.find(position.first));
-            const auto R = distance(index.begin(), index.find(position.first + position.second - 1));
-            const auto h = tree.query(L, R) + position.second;
+            const auto L = distance(index.begin(), index.find(position[0]));
+            const auto R = distance(index.begin(), index.find(position[0] + position[1] - 1));
+            const auto h = tree.query(L, R) + position[1];
             tree.update(L, R, h);
             max_height = max(max_height, h);
             result.emplace_back(max_height);
@@ -100,9 +100,9 @@ private:
         vector<int> tree_, lazy_;
 
         void apply(int x, int val) {
-            tree_[x] = max(tree_[x], val);
+            tree_[x] = val;
             if (x < N_) {
-                lazy_[x] = max(tree_[x], val);
+                lazy_[x] = val;
             }
         }
 
