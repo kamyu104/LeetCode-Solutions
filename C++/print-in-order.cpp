a33a0cf8ce21c31e9477f68f50a86a9289346adf
kamyu <kamyu104@users.noreply.github.com>
2019-07-13 02:42:30 +0800

    Update print-in-order.cpp

--- C++/print-in-order.cpp
+++ C++/print-in-order.cpp
@@ -4,31 +4,31 @@
 class Foo {
 public:
     Foo() {
+        m1_.lock();
         m2_.lock();
-        m3_.lock();
     }
 
     void first(function<void()> printFirst) {
-        m1_.lock();
         // printFirst() outputs "first". Do not change or remove this line.
         printFirst();
-        m2_.unlock();
-    }
-
-    void second(function<void()> printSecond) {
-        m2_.lock();
-        // printSecond() outputs "second". Do not change or remove this line.
-        printSecond();
-        m3_.unlock();
-    }
-
-    void third(function<void()> printThird) {
-        m3_.lock();
-        // printThird() outputs "third". Do not change or remove this line.
-        printThird();
         m1_.unlock();
     }
 
+    void second(function<void()> printSecond) {
+        m1_.lock();
+        // printSecond() outputs "second". Do not change or remove this line.
+        printSecond();
+        m1_.unlock();
+        m2_.unlock();
+    }
+
+    void third(function<void()> printThird) {
+        m2_.lock();
+        // printThird() outputs "third". Do not change or remove this line.
+        printThird();
+        m2_.unlock();
+    }
+
 private:
-    mutex m1_, m2_, m3_;
+    mutex m1_, m2_;
 };
