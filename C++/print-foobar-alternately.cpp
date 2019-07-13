f3df3d25ba9b96e098439becc459e169298f170e
kamyu <kamyu104@users.noreply.github.com>
2019-07-13 14:55:44 +0800

    Update print-foobar-alternately.cpp

--- C++/print-foobar-alternately.cpp
+++ C++/print-foobar-alternately.cpp
@@ -1,9 +1,49 @@
 // Time:  O(n)
 // Space: O(1)
 
+
 class FooBar {
 public:
     FooBar(int n) : n_(n) {
+    }
+
+    void foo(function<void()> printFoo) {
+        for (int i = 0; i < n_; ++i) {
+            {
+                unique_lock<mutex> l(m_);
+                wait_.wait(l, [this]() { return !curr_; });
+                curr_ = !curr_;
+            }
+            // printFoo() outputs "foo". Do not change or remove this line.
+        	printFoo();
+            wait_.notify_one();
+        }
+    }
+
+    void bar(function<void()> printBar) {
+        for (int i = 0; i < n_; ++i) {
+            {
+                unique_lock<mutex> l(m_);
+                wait_.wait(l, [this]() { return curr_; });
+                curr_ = !curr_;
+            }
+            // printBar() outputs "bar". Do not change or remove this line.
+        	printBar();
+            wait_.notify_one();
+        }
+    }
+private:
+    int n_;
+    bool curr_ = false;
+    mutex m_;
+    condition_variable wait_;
+};
+
+// Time:  O(n)
+// Space: O(1)
+class FooBar2 {
+public:
+    FooBar2(int n) : n_(n) {
         m2_.lock();
     }
 
@@ -11,7 +51,7 @@ public:
         for (int i = 0; i < n_; ++i) {
             m1_.lock();
             // printFoo() outputs "foo". Do not change or remove this line.
-        	  printFoo();
+        	 printFoo();
             m2_.unlock();
         }
     }
