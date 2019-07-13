3b5d9dc80e29bdd8da62a31473bd06ed67d505e3
kamyu <kamyu104@users.noreply.github.com>
2019-07-13 14:39:05 +0800

    Update print-zero-even-odd.cpp

--- C++/print-zero-even-odd.cpp
+++ C++/print-zero-even-odd.cpp
@@ -12,32 +12,35 @@ public:
     // printNumber(x) outputs "x", where x is an integer.
     void zero(function<void(int)> printNumber) {
         for (int i = 0; i < n_; ++i) {
-            unique_lock<mutex> l(m_);
-            wait_.wait(l, [this]() { return curr_ % 4 == 0 ||
-                                            curr_ % 4 == 2; });
-            printNumber(0);
-            ++curr_;
-            l.unlock();
+            {
+                unique_lock<mutex> l(m_);
+                wait_.wait(l, [this]() { return curr_ % 4 == 0 ||
+                                                curr_ % 4 == 2; });
+                printNumber(0);
+                ++curr_;
+            }
             wait_.notify_all();
         }
     }
 
     void even(function<void(int)> printNumber) {
         for (int i = 0; i < n_ / 2; ++i) {
-            unique_lock<mutex> l(m_);
-            wait_.wait(l, [this]() { return curr_ % 4 == 3; });
-            printNumber((curr_++ / 2) + 1);
-            l.unlock();
+            {
+                unique_lock<mutex> l(m_);
+                wait_.wait(l, [this]() { return curr_ % 4 == 3; });
+                printNumber((curr_++ / 2) + 1);
+            }
             wait_.notify_all();
         } 
     }
 
     void odd(function<void(int)> printNumber) {
          for (int i = 0; i < (n_ + 1) / 2; ++i) {
-            unique_lock<mutex> l(m_);
-            wait_.wait(l, [this]() { return curr_ % 4 == 1; });
-            printNumber((curr_++ / 2) + 1);
-            l.unlock();
+            {
+                unique_lock<mutex> l(m_);
+                wait_.wait(l, [this]() { return curr_ % 4 == 1; });
+                printNumber((curr_++ / 2) + 1);
+            }
             wait_.notify_all();
         } 
     }
