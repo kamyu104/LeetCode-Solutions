58222b432e896d2e402c1c3e7d808c6068b95905
kamyu <kamyu104@users.noreply.github.com>
2019-07-13 17:16:33 +0800

    Update building-h2o.py

--- Python/building-h2o.py
+++ Python/building-h2o.py
@@ -4,8 +4,33 @@
 import threading
 
 
-# TLE
 class H2O(object):
+    def __init__(self):
+        self.__l = threading.Lock()
+        self.__Hs = []
+        self.__Os = []
+
+    def hydrogen(self, releaseHydrogen):
+        with self.__l:
+            self.__Hs.append(releaseHydrogen)
+            self.__output()
+
+    def oxygen(self, releaseOxygen):
+        with self.__l:
+            self.__Os.append(releaseOxygen)
+            self.__output()
+
+    def __output(self):
+        while len(self.__Hs) >= 2 and \
+              len(self.__Os) >= 1:
+            self.__Hs.pop()()
+            self.__Hs.pop()()
+            self.__Os.pop()()
+
+
+# Time:  O(n)
+# Space: O(1)
+class H2O2(object):
     def __init__(self):
         self.__nH = 0
         self.__nO = 0
