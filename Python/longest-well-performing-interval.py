97aec260f8a32c5b28edb947ff2f3da15f288b45
kamyu <kamyu104@users.noreply.github.com>
2019-07-16 23:23:27 +0800

    Update longest-well-performing-interval.py

--- Python/longest-well-performing-interval.py
+++ Python/longest-well-performing-interval.py
@@ -15,7 +15,7 @@ class Solution(object):
                 result = i+1
             elif accu-1 in lookup:
                 # lookup[accu-1] is the leftmost idx with smaller accu,
-                # because from 1 to some positive k,
+                # because for i from 1 to some positive k,
                 # lookup[accu-i] is a strickly increasing sequence
                 result = max(result, i-lookup[accu-1])
             lookup.setdefault(accu, i)
