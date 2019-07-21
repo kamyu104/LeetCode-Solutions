01d8d19ebd36c7b200c6c6916f03154f082f83c7
kamyu <kamyu104@users.noreply.github.com>
2019-07-21 17:44:12 +0800

    Create maximum-of-absolute-value-expression.cpp

---
+++ C++/maximum-of-absolute-value-expression.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxAbsValExpr(vector<int>& arr1, vector<int>& arr2) {
+        int result = 0;
+        for (const auto& c1 : {1, -1}) {
+            for (const auto& c2 : {1, -1}) {
+                int min_prev = c1 * arr1[0] + c2 * arr2[0] + 0;
+                for (int i = 1; i < arr1.size(); ++i) {
+                    const auto& curr = c1 * arr1[i] + c2 * arr2[i] + i;
+                    result = max(result, curr - min_prev);
+                    min_prev = min(min_prev, curr);
+                }
+            }
+        }
+        return result;
+    }
+};
