acfc6e2b08a5271f32a61b67707f5771112c1397
kamyu <kamyu104@users.noreply.github.com>
2019-08-04 18:11:24 +0800

    Update longest-chunked-palindrome-decomposition.py

--- Python/longest-chunked-palindrome-decomposition.py
+++ Python/longest-chunked-palindrome-decomposition.py
@@ -3,14 +3,6 @@
 
 # Rabin-Karp Algorithm
 class Solution(object):
-    def __init__(self):
-        self.__N = 1000
-        self.__MOD = 10**9+7
-        self.__D = 26
-        self.__pow = [1]*(self.__N+1)
-        for i in xrange(1, len(self.__pow)):
-            self.__pow[i] = self.__pow[i-1]*self.__D % self.__MOD
-        
     def longestDecomposition(self, text):
         """
         :type text: str
@@ -22,13 +14,16 @@ class Solution(object):
                     return False
             return True
 
+        MOD = 10**9+7
+        D = 26
         result = 0
-        l, left, right = 0, 0, 0
+        l, left, right, pow_D = 0, 0, 0, 1
         for i in xrange(len(text)):
+            left = (D*left + (ord(text[i])-ord('a'))) % MOD
+            right = (pow_D*(ord(text[-1-i])-ord('a')) + right) % MOD
             l += 1
-            left = (self.__pow[1]*left + (ord(text[i])-ord('a'))) % self.__MOD
-            right = (self.__pow[l-1]*(ord(text[-1-i])-ord('a')) + right) % self.__MOD
+            pow_D = (pow_D*D) % MOD 
             if left == right and compare(text, l, i-l+1, len(text)-1-i):
                 result += 1
-                l, left, right = 0, 0, 0
+                l, left, right, pow_D = 0, 0, 0, 1
         return result
