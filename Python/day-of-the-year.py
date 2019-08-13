748da5750770099e010da1b41f8c809125dc70e2
kamyu <kamyu104@users.noreply.github.com>
2019-08-13 23:37:06 +0800

    Update day-of-the-year.py

--- Python/day-of-the-year.py
+++ Python/day-of-the-year.py
@@ -2,6 +2,27 @@
 # Space: O(1)
 
 class Solution(object):
+    def __init__(self):
+        def dayOfMonth(M):
+            return (28 if (M == 2) else 31-(M-1)%7%2)
+
+        self.__lookup = [0]*12
+        for M in xrange(1, len(self.__lookup)):
+            self.__lookup[M] += self.__lookup[M-1]+dayOfMonth(M)
+            
+    def dayOfYear(self, date):
+        """
+        :type date: str
+        :rtype: int
+        """
+        Y, M, D = map(int, date.split("-"))
+        leap = 1 if M > 2 and (((Y % 4 == 0) and (Y % 100 != 0)) or (Y % 400 == 0)) else 0
+        return self.__lookup[M-1]+D+leap
+
+
+# Time:  O(1)
+# Space: O(1)
+class Solution2(object):
     def dayOfYear(self, date):
         """
         :type date: str
