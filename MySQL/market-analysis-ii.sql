1771446a907e90a4eff34cd7ffa748094e8d1088
kamyu <kamyu104@users.noreply.github.com>
2019-08-14 02:12:05 +0800

    Update market-analysis-ii.sql

--- MySQL/market-analysis-ii.sql
+++ MySQL/market-analysis-ii.sql
@@ -9,10 +9,10 @@ SELECT user_id AS seller_id,
        END AS 2nd_item_fav_brand 
 FROM   (SELECT user_id, 
                favorite_brand, 
-               (SELECT    item_brand 
+               (SELECT    item_id
                 FROM      orders o 
-                LEFT JOIN items i 
-                ON        o.item_id = i.item_id 
                 WHERE     user_id = o.seller_id 
-                ORDER BY order_date limit 1, 1) item_brand 
+                ORDER BY order_date limit 1, 1) item_id
         FROM   users) u
+        LEFT JOIN items i 
+        ON        u.item_id = i.item_id 
