63dcb1abfe9d4afa1ce255e0aeb9d9cb7a22d48e
kamyu <kamyu104@users.noreply.github.com>
2019-08-13 22:17:10 +0800

    Create check-if-a-number-is-majority-element-in-a-sorted-array.cpp

---
+++ C++/check-if-a-number-is-majority-element-in-a-sorted-array.cpp
@@ -0,0 +1,21 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isMajorityElement(vector<int>& nums, int target) {
+        if (nums.size() % 2) {
+            if (nums[nums.size() / 2] != target) {
+                return false;
+            }
+        } else {
+            if (nums[nums.size() / 2 - 1] != nums[nums.size() / 2] ||
+                nums[nums.size() / 2] != target) {
+                return false;
+            }
+        }
+        const auto& l = lower_bound(nums.cbegin(), nums.cend(), target);
+        const auto& r = upper_bound(nums.cbegin(), nums.cend(), target);
+        return r - l;
+    }
+};
