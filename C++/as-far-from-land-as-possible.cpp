c585bacbdeef3d9fc8477e992c92995407b4aeb7
kamyu <kamyu104@users.noreply.github.com>
2019-08-21 00:06:15 +0800

    Create as-far-from-land-as-possible.cpp

---
+++ C++/as-far-from-land-as-possible.cpp
@@ -0,0 +1,41 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        static const vector<pair<int, int>> directions{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        queue<pair<int, int>> q;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[i].size(); ++j) {
+                if (grid[i][j]) {
+                    q.emplace(i, j);
+                }
+            }
+        }
+        if (q.size() == grid.size() * grid[0].size()) {
+            return -1;
+        }
+        int level = -1;
+        while (!q.empty()) {
+            queue<pair<int, int>> next_q;
+            while (!q.empty()) {
+                const auto [x, y] = q.front(); q.pop();
+                for (const auto& [dx, dy] : directions) {
+                    const auto& nx = x + dx;
+                    const auto& ny = y + dy;
+                    if (!(0 <= nx && nx < grid.size() && 
+                          0 <= ny && ny < grid[0].size() && 
+                          grid[nx][ny] == 0)) {
+                        continue;
+                    }
+                    next_q.emplace(nx, ny);
+                    grid[nx][ny] = 1;
+                }
+            }
+            q = move(next_q);
+            ++level;
+        }
+        return level;
+    }
+};
