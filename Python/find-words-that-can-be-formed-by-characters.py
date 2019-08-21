116f5da6c7669a08392fafa686d3a2f1dc9db138
kamyu <kamyu104@users.noreply.github.com>
2019-08-21 09:30:37 +0800

    Update find-words-that-can-be-formed-by-characters.py

--- Python/find-words-that-can-be-formed-by-characters.py
+++ Python/find-words-that-can-be-formed-by-characters.py
@@ -11,5 +11,26 @@ class Solution(object):
         :type chars: str
         :rtype: int
         """
+        def check(s, count2):
+            count1 = collections.Counter()
+            for c in s:
+                count1[c] += 1
+                if count2[c] < count1[c]:
+                    return False
+            return True
+        
+        count = collections.Counter(chars)
+        return sum(len(word) for word in words if check(word, count))
+    
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def countCharacters(self, words, chars):
+        """
+        :type words: List[str]
+        :type chars: str
+        :rtype: int
+        """
         count = collections.Counter(chars)
         return sum(len(word) for word in words if not collections.Counter(word)-count)
