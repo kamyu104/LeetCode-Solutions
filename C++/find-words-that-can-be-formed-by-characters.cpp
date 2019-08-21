9cf2d6e6f2e615f86bc62c99817e55e46391fa96
kamyu <kamyu104@users.noreply.github.com>
2019-08-21 10:06:12 +0800

    Update find-words-that-can-be-formed-by-characters.cpp

--- C++/find-words-that-can-be-formed-by-characters.cpp
+++ C++/find-words-that-can-be-formed-by-characters.cpp
@@ -7,8 +7,7 @@ public:
         const auto& count = counter(chars);
         int result = 0;
         for (const auto& word : words) {
-            if (word.length() <= chars.length() &&
-                check(word, count)) {
+            if (check(word, count)) {
                 result += word.length();
             }
         }
@@ -26,46 +25,13 @@ private:
     
     bool check(const string& s,
                const unordered_map<char, int>& count2) {
+        if (word.length() > chars.length()) {
+            return false;
+        }
         unordered_map<char, int> count1;
         for (const auto& c : s) {
             ++count1[c];
             if (!count2.count(c) || count2.at(c) < count1[c]) {
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
-// Time:  O(m * n), m is the length of chars, n is the number of words
-// Space: O(1)
-class Solution2 {
-public:
-    int countCharacters(vector<string>& words, string chars) {
-        const auto& count = counter(chars);
-        int result = 0;
-        for (const auto& word : words) {
-            if (word.length() <= chars.length() &&
-                check(counter(word), count)) {
-                result += word.length();
-            }
-        }
-        return result;
-    }
-
-private:
-    unordered_map<char, int> counter(const string& s) {
-        unordered_map<char, int> result;
-        for (const auto& c : s) {
-            ++result[c];
-        }
-        return result;
-    }
-    
-    bool check(const unordered_map<char, int>& count1,
-               const unordered_map<char, int>& count2) {
-        for (const auto& [k, v] : count1) {
-            if (!count2.count(k) || count2.at(k) < v) {
                 return false;
             }
         }
