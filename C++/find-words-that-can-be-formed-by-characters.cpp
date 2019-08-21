603d7719454185f500b504af435e98c9d322c59d
kamyu <kamyu104@users.noreply.github.com>
2019-08-21 09:25:59 +0800

    Update find-words-that-can-be-formed-by-characters.cpp

--- C++/find-words-that-can-be-formed-by-characters.cpp
+++ C++/find-words-that-can-be-formed-by-characters.cpp
@@ -7,6 +7,41 @@ public:
         const auto& count = counter(chars);
         int result = 0;
         for (const auto& word : words) {
+            if (check(word, count)) {
+                result += word.length();
+            }
+        }
+        return result;
+    }
+
+private:
+    unordered_map<char, int> counter(const string& s) {
+        unordered_map<char, int> result;
+        for (const auto& c : s) {
+            ++result[c];
+        }
+        return result;
+    }
+    
+    bool check(const string& s,
+               const unordered_map<char, int>& count2) {
+        unordered_map<char, int> count1;
+        for (const auto& c : s) {
+            ++count1[c];
+            if (!count2.count(c) || count2.at(c) < count1[c]) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+class Solution2 {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+        const auto& count = counter(chars);
+        int result = 0;
+        for (const auto& word : words) {
             if (check(counter(word), count)) {
                 result += word.length();
             }
