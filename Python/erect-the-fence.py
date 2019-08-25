cce1c35abd63238dde0022ef1d0fa62bc63d4a23
kamyu <kamyu104@users.noreply.github.com>
2019-08-25 15:24:42 +0800

    Update erect-the-fence.py

--- Python/erect-the-fence.py
+++ Python/erect-the-fence.py
@@ -12,13 +12,11 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         def ccw(A, B, C):
-            return (B[0]-A[0])*(C[1]-A[1]) - \
-                   (B[1]-A[1])*(C[0]-A[0])
+            return (B[0]-A[0])*(C[1]-A[1]) - (B[1]-A[1])*(C[0]-A[0])
 
         hull = []
-        points.sort(key=lambda p: (p[0], p[1]))
-        for i in itertools.chain(xrange(len(points)), \
-                                 reversed(xrange(len(points)))):
+        points.sort()
+        for i in itertools.chain(xrange(len(points)), reversed(xrange(len(points)))):
             while len(hull) >= 2 and \
                   ccw(hull[-2], hull[-1],  points[i]) < 0:
                 hull.pop()
