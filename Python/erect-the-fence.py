e325e86d800d8ef90fcdc73e895168ee8050a559
kamyu <kamyu104@users.noreply.github.com>
2019-08-25 18:46:43 +0800

    Update erect-the-fence.py

--- Python/erect-the-fence.py
+++ Python/erect-the-fence.py
@@ -14,14 +14,17 @@ class Solution(object):
         def ccw(A, B, C):
             return (B[0]-A[0])*(C[1]-A[1]) - (B[1]-A[1])*(C[0]-A[0])
 
+        if len(points) <= 1:
+           return points
+
         hull = []
         points.sort()
         for i in itertools.chain(xrange(len(points)), reversed(xrange(len(points)-1))):
             while len(hull) >= 2 and ccw(hull[-2], hull[-1], points[i]) < 0:
                 hull.pop()
             hull.append(points[i])
-        if len(hull) >= 2:
-           hull.pop()
+        hull.pop()
+
         for i in xrange(1, (len(hull)+1)//2):
             if hull[i] != hull[-1]:
                 break
