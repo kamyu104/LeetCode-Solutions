1e0a1d2faa332d91f769b75c43fcf3d7f68bf20e
kamyu <kamyu104@users.noreply.github.com>
2019-08-25 15:27:26 +0800

    Update erect-the-fence.py

--- Python/erect-the-fence.py
+++ Python/erect-the-fence.py
@@ -18,7 +18,7 @@ class Solution(object):
         points.sort()
         for i in itertools.chain(xrange(len(points)), reversed(xrange(len(points)))):
             while len(hull) >= 2 and \
-                  ccw(hull[-2], hull[-1],  points[i]) < 0:
+                  ccw(hull[-2], hull[-1], points[i]) < 0:
                 hull.pop()
             hull.append(points[i])
         return list(set(map(tuple, hull)))
