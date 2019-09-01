9418ba68937ada6722d9589ac436e60b9f08ba10
kamyu <kamyu104@users.noreply.github.com>
2019-09-01 19:04:24 +0800

    Update count-primes.py

--- Python/count-primes.py
+++ Python/count-primes.py
@@ -8,23 +8,20 @@ class Solution(object):
         if n <= 2:
             return 0
 
-        is_prime = [True] * n
-        num = n / 2
+        is_prime = [True]*(n//2)
+        cnt = len(is_prime)
         for i in xrange(3, n, 2):
             if i * i >= n:
                 break
-
-            if not is_prime[i]:
+            if not is_prime[i//2]:
                 continue
-
             for j in xrange(i*i, n, 2*i):
-                if not is_prime[j]:
+                if not is_prime[j//2]:
                     continue
+                cnt -= 1
+                is_prime[j//2] = False
 
-                num -= 1
-                is_prime[j] = False
-
-        return num
+        return cnt
 
     def countPrimes2(self, n):
         """
