9e391e468213fd6243f238347da28d25ad3f5931
kamyu <kamyu104@users.noreply.github.com>
2019-09-09 05:31:24 +0800

    Update maximum-subarray.cpp

--- C++/maximum-subarray.cpp
+++ C++/maximum-subarray.cpp
@@ -4,15 +4,11 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        const auto& max_num = *max_element(nums.cbegin(), nums.cend());
-        if (max_num < 0) {
-            return max_num;
-        }
-        int global_max = 0, local_max = 0;
+        int result = numeric_limits<int>::min(), curr = numeric_limits<int>::min();
         for (const auto &x : nums) {
-           local_max = std::max(0, local_max + x);
-           global_max = std::max(global_max, local_max);
+           curr = (curr == numeric_limits<int>::min()) ? x : max(curr + x, x);
+           result = max(result, curr);
         }
-        return global_max;
+        return result;
     }
 };
