3ea80164ed472cfc6870c8a9b01cdd5de31dfcac
kamyu <kamyu104@users.noreply.github.com>
2019-09-09 07:43:11 +0800

    Update make-array-strictly-increasing.cpp

--- C++/make-array-strictly-increasing.cpp
+++ C++/make-array-strictly-increasing.cpp
@@ -4,35 +4,33 @@
 class Solution {
 public:
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
-        sort(arr2.begin(), arr2.end());
-        unordered_map<int, int> dp = {{-1, 0}};
-        for (const auto& i : arr1) {
+        std::sort(arr2.begin(), arr2.end());
+        arr2.erase(std::unique(arr2.begin(), arr2.end()), arr2.end());
+        unordered_map<int, int> dp = {{0, arr1[0]}, {1, arr2[0]}};
+        for (int i = 1; i < arr1.size(); ++i) {
             unordered_map<int, int> next_dp;
             for (const auto& [j, count] : dp) {
-                if (i > j) {
-                    if (!next_dp.count(i)) {
-                        next_dp[i] = dp[j];
+                if (dp[j] < arr1[i]) {
+                    if (!next_dp.count(j)) {
+                        next_dp[j] = arr1[i];
                     } else {
-                        next_dp[i] = min(next_dp[i], dp[j]);
+                        next_dp[j] = min(next_dp[j], arr1[i]);
                     }
                 }
-                const auto& it = upper_bound(arr2.cbegin(), arr2.cend(), j);
+                const auto& it = upper_bound(arr2.cbegin(), arr2.cend(), dp[j]);
                 if (it != arr2.cend()) {
-                    if (!next_dp.count(*it)) {
-                        next_dp[*it] = dp[j] + 1;
+                    if (!next_dp.count(j + 1)) {
+                        next_dp[j + 1] = *it;
                     } else {
-                        next_dp[*it] = min(next_dp[*it], dp[j] + 1);
+                        next_dp[j + 1] = min(next_dp[j + 1], *it);
                     }
                 }
             }
             dp = move(next_dp);
         }
-        if (!dp.empty()) {
-            return min_element(dp.cbegin(), dp.cend(),
-                               [](const auto& a, const auto& b) {
-                                   return a.second < b.second;
-                               })->second;
+        if (dp.empty()) {
+            return -1;
         }
-        return -1;
+        return min_element(dp.cbegin(), dp.cend())->first;
     }
 };
