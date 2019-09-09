37f6b1e00e50364536d01e71c878691547eb61dd
kamyu <kamyu104@users.noreply.github.com>
2019-09-09 08:10:26 +0800

    Update make-array-strictly-increasing.cpp

--- C++/make-array-strictly-increasing.cpp
+++ C++/make-array-strictly-increasing.cpp
@@ -5,16 +5,15 @@ class Solution {
 public:
     int makeArrayIncreasing(vector<int>& arr1, vector<int>& arr2) {
         std::sort(arr2.begin(), arr2.end());
-        arr2.erase(std::unique(arr2.begin(), arr2.end()), arr2.end());
-        unordered_map<int, int> dp = {{0, arr1[0]}, {1, arr2[0]}};  // dp[min_cost] = end_with_val
-        for (int i = 1; i < arr1.size(); ++i) {
+        unordered_map<int, int> dp = {{0, -1}};  // dp[min_cost] = end_with_val
+        for (const auto& val1 : arr1) {
             unordered_map<int, int> next_dp;
             for (const auto& [cost, val] : dp) {
-                if (val < arr1[i]) {
+                if (val < val1) {
                     if (!next_dp.count(cost)) {
-                        next_dp[cost] = arr1[i];
+                        next_dp[cost] = val1;
                     } else {
-                        next_dp[cost] = min(next_dp[cost], arr1[i]);
+                        next_dp[cost] = min(next_dp[cost], val1);
                     }
                 }
                 const auto& it = upper_bound(arr2.cbegin(), arr2.cend(), val);
