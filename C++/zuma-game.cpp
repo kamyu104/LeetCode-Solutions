9d93e9827c5e630286cd4f4a6fdc404033b1fb8b
kamyu <kamyu104@users.noreply.github.com>
2019-09-11 22:19:51 +0800

    Update zuma-game.cpp

--- C++/zuma-game.cpp
+++ C++/zuma-game.cpp
@@ -1,7 +1,70 @@
+// the problem should either relax restriction on time complexity or
+// add a description that a ball can be only inserted beside a ball with same color
+
+// Time:  O((b+h) * h!*(b+h-1)!/(b-1)!)
+// Space: O((b+h) * h!*(b+h-1)!/(b-1)!)
+// brute force solution
+class Solution_TLE_BUT_CORRECT {
+public:
+    int findMinStep(string board, string hand) {
+        unordered_map<string, unordered_map<string, int>> lookup;
+        sort(hand.begin(), hand.end()); 
+        int result = findMinStepHelper(board, hand, &lookup);
+        return result > hand.size() ? -1 : result;
+    }
+
+private:
+    int findMinStepHelper(const string& board, const string& hand,
+                          unordered_map<string, unordered_map<string, int>> *lookup) {
+        if (board.empty()) {
+            return 0;
+        }
+        if (hand.empty()) {
+            return MAX_STEP;
+        }
+        if ((*lookup)[board][hand]) {
+            return (*lookup)[board][hand];
+        }
+
+        int result = MAX_STEP;
+        for (int i = 0; i < hand.size(); ++i) {
+            for (int j = 0; j <= board.size(); ++j) {
+                const auto& next_board = shrink(board.substr(0, j) + hand.substr(i, 1) + board.substr(j));
+                const auto& next_hand = hand.substr(0, i) + hand.substr(i + 1);
+                result = min(result, findMinStepHelper(next_board, next_hand, lookup) + 1);
+            }
+        }
+        return (*lookup)[board][hand] = result;
+    }
+    
+    string shrink(const string& s) {  // Time: O(n), Space: O(n)
+        vector<pair<char, int>> stack;
+        for (int i = 0, start = 0; i <= s.size(); ++i) {
+            if (i == s.size() || s[i] != s[start]) {
+                if (!stack.empty() && stack.back().first == s[start]) {
+                    stack.back().second += i - start;
+                    if (stack.back().second >= 3) {
+                        stack.pop_back();
+                    }
+                } else if (!s.empty() && i - start < 3) {
+                    stack.emplace_back(s[start], i - start);
+                }
+                start = i;
+            }
+        }
+        string result;
+        for (const auto& p : stack) {
+            result += string(p.second, p.first);
+        }
+        return result;
+    }
+
+    static const int MAX_STEP = 6;
+};
+
 // Time:  O(b * b! * h!)
 // Space: O(b * b! * h!)
-
-class Solution {
+class Solution_WRONG_GREEDY_BUT_ACCEPT {
 public:
     int findMinStep(string board, string hand) {
         unordered_map<string, unordered_map<string, int>> lookup;
