30a3f5f8c59cb5a621873610acb992be2efd8693
kamyu <kamyu104@users.noreply.github.com>
2019-09-16 03:22:00 +0800

    Create k-concatenation-maximum-sum.cpp

---
+++ C++/k-concatenation-maximum-sum.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int kConcatenationMaxSum(vector<int>& arr, int k) {
+        static const int MOD = 1e9 + 7;
+        if (k == 1) {
+            return maxSubKArray(arr, 1) % MOD;
+        }
+        return (maxSubKArray(arr, 2) +
+                (k - 2) * max(accumulate(arr.cbegin(), arr.cend(), 0ll), 0ll)) % MOD;
+    }
+
+private:
+    int maxSubKArray(const vector<int>& nums, int k) {
+        int result = 0;
+        int curr = 0;
+        for (int i = 0; i < k; ++i) {
+            for (const auto& x : nums) {
+                curr = max(curr + x, x);
+                result = max(result, curr);
+            }
+        }
+        return result;
+    }
+};
