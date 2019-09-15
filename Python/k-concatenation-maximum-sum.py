b2e5c2704b545ffa4105d45fd55ac3b4ffb23c1e
kamyu <kamyu104@users.noreply.github.com>
2019-09-16 05:28:13 +0800

    Update k-concatenation-maximum-sum.py

--- Python/k-concatenation-maximum-sum.py
+++ Python/k-concatenation-maximum-sum.py
@@ -9,7 +9,7 @@ class Solution(object):
         :rtype: int
         """
         def max_sub_k_array(arr, k):
-            result, curr = 0, 0
+            result, curr = float("-inf"), float("-inf")
             for _ in xrange(k):
                 for x in arr:
                     curr = max(curr+x, x)
@@ -18,5 +18,5 @@ class Solution(object):
         
         MOD = 10**9+7
         if k == 1:
-            return max_sub_k_array(arr, 1) % MOD
-        return (max_sub_k_array(arr, 2) + (k-2)*max(sum(arr), 0)) % MOD
+            return max(max_sub_k_array(arr, 1), 0) % MOD
+        return (max(max_sub_k_array(arr, 2), 0) + (k-2)*max(sum(arr), 0)) % MOD
