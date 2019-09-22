07b728754080e9c409eb0dba8593c45dc199720e
kamyu <kamyu104@users.noreply.github.com>
2019-09-22 18:24:55 +0800

    Update minimum-knight-moves.py

--- Python/minimum-knight-moves.py
+++ Python/minimum-knight-moves.py
@@ -8,15 +8,12 @@ class Solution(object):
         :type y: int
         :rtype: int
         """
-        COST = {(0, 0):0, (1, 1):2, (1, 0):3}  # special cases
         def dp(x, y, lookup):
             x, y = abs(x), abs(y)
             if x < y:
                 x, y = y, x
-            if (x, y) in COST:
-                return COST[(x, y)]
             if (x, y) not in lookup:
                 lookup[(x, y)] = min(dp(x-1, y-2, lookup),
                                      dp(x-2, y-1, lookup)) + 1
             return lookup[(x, y)]
-        return dp(x, y, {})
+        return dp(x, y, {(0, 0):0, (1, 1):2, (1, 0):3})  # special cases
