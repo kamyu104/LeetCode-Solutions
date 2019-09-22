b44a85fe3161813afd2f70b22368c2547eb9a6c1
kamyu <kamyu104@users.noreply.github.com>
2019-09-22 13:56:06 +0800

    Create minimum-knight-moves.py

---
+++ Python/minimum-knight-moves.py
@@ -0,0 +1,23 @@
+# Time:  O(n^2)
+# Space: O(n^2)
+
+class Solution(object):
+    def minKnightMoves(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        def dp(x, y, lookup):
+            x, y = abs(x), abs(y)
+            if x < y:
+                x, y = y, x
+            if x == 0 and y == 0:
+                return 0
+            if x == 1 and y == 0:
+                return 3  # (0, 0) => (-2, 1) => (0, 2) => (1, 0)
+            if (x, y) not in lookup:
+                lookup[(x, y)] = min(dp(x-1, y-2, lookup),
+                                     dp(x-2, y-1, lookup)) + 1
+            return lookup[(x, y)]
+        return dp(x, y, {})
