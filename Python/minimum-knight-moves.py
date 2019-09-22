8f49fe021357165947d5977d5734161c31e08b9e
kamyu <kamyu104@users.noreply.github.com>
2019-09-22 19:16:57 +0800

    Update minimum-knight-moves.py

--- Python/minimum-knight-moves.py
+++ Python/minimum-knight-moves.py
@@ -1,7 +1,46 @@
-# Time:  O(n^2)
-# Space: O(n^2)
+# Time:  O(1)
+# Space: O(1)
 
 class Solution(object):
+    def minKnightMoves(self, x, y):
+        """
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        # we can observere from:
+        # [0]
+        # [3, 2]
+        # [2, 1, 4]
+        # [3, 2, 3, 2]
+        # [2, 3, 2, 3, 4]
+        # [3, 4, 3, 4, 3, 4]
+        # [4, 3, 4, 3, 4, 5, 4]
+        # [5, 4, 5, 4, 5, 4, 5, 6]
+        # [4, 5, 4, 5, 4, 5, 6, 5, 6]
+        # [5, 6, 5, 6, 5, 6, 5, 6, 7, 6]
+        # [6, 5, 6, 5, 6, 5, 6, 7, 6, 7, 8]
+        # [7, 6, 7, 6, 7, 6, 7, 6, 7, 8, 7, 8]
+        # [6, 7, 6, 7, 6, 7, 6, 7, 8, 7, 8, 9, 8]
+        # [7, 8, 7, 8, 7, 8, 7, 8, 7, 8, 9, 8, 9, 10]
+        # [8, 7, 8, 7, 8, 7, 8, 7, 8, 9, 8, 9, 10, 9, 10]
+        # [9, 8, 9, 8, 9, 8, 9, 8, 9, 8, 9, 10, 9, 10, 11, 10]
+
+        x, y = abs(x), abs(y)
+        if x < y:
+            x, y = y, x
+        lookup = {(0, 0):0, (1, 0):3, (2, 2):4}  # special cses
+        if (x, y) in lookup:
+            return lookup[(x, y)]
+        delta = x-y
+        if y > delta:  # if 2y > x, every period 3 of y with same delta is increased by 2
+            return delta - 2*((delta-y)//3)
+        # if 2y <= x, every period 4 of y with same delta is increased by 2
+        return delta - 2*((delta-y)//4)
+
+# Time:  O(n^2)
+# Space: O(n^2)
+class Solution2(object):
     def minKnightMoves(self, x, y):
         """
         :type x: int
