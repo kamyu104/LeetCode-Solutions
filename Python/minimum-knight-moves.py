02917927895db194dd0511243fbc221364157b49
kamyu <kamyu104@users.noreply.github.com>
2019-09-24 03:41:54 +0800

    Update minimum-knight-moves.py

--- Python/minimum-knight-moves.py
+++ Python/minimum-knight-moves.py
@@ -32,14 +32,18 @@ class Solution(object):
         lookup = {(0, 0):0, (1, 0):3, (2, 2):4}  # special cases
         if (x, y) in lookup:
             return lookup[(x, y)]
-        delta = x-y
-        if y > delta:
-            # if 2y > x, every period 3 of y with same delta is increased by 2 (diagonal line)
-            # ex. (1, 1) ~ (11, 11) ~ ... : 2,4(special case),2 => 4,4,4 => 6,6,6 => 8,8,8 => ...
-            return delta - 2*((delta-y)//3)
-        # if 2y <= x, every period 4 of y with same delta is increased by 2 (vertical line)
+        k = x-y
+        if y > k:
+            # if 2y > x, every period 3 of y with same delta is increased by 2
+            # and start from (2k, k) with (k) (diagonal line)
+            # ex. (0, 0) ~ (12, 12) ~ ... : 0 => 2,4(special case),2 => 4,4,4 => 6,6,6 => 8,8,8 => ...
+            # ex. (2, 1) ~ (14, 13) ~ ... : 1 => 3,3,3 => 5,5,5 => 7,7,7 => 9,9,9 => ...
+            return k - 2*((k-y)//3)
+        # if 2y <= x, every period 4 of y with same delta is increased by 2
+        # and start from (2k, k) with (k) (vertical line)
         # ex. (0, 0) ~ (11, 0) ~ ... : 0,3(special case),2,3 => 2,3,4,5 => 4,5,6,7 => ...
-        return delta - 2*((delta-y)//4)
+        # ex. (2, 1) ~ (13, 1) ~ ... : 1,2,3,4 => 3,4,5,6 => 5,6,7,8 => ...
+        return k - 2*((k-y)//4)
 
 
 # Time:  O(n^2)
