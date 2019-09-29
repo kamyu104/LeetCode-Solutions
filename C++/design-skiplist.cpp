b6e92a7ebe6fa2190b9695ba177c9ee164504888
kamyu <kamyu104@users.noreply.github.com>
2019-09-30 02:05:09 +0800

    Update design-skiplist.cpp

--- C++/design-skiplist.cpp
+++ C++/design-skiplist.cpp
@@ -105,10 +105,10 @@ private:
     }
     
     int random_level() {
-        static const double P = 0.25;
+        static const int P_inv = 2;
         static const int MAX_LEVEL = 32; // enough for 2^32 elements
         int level = 1;
-        while (uniform_int_distribution<int>{1, 0xFFFF}(gen_) < P * 0xFFFF &&
+        while (uniform_int_distribution<int>{1, P_inv}(gen_) == 1 &&
                level < MAX_LEVEL) {
             ++level;
         }
@@ -223,10 +223,10 @@ private:
     }
     
     int random_level() {
-        static const double P = 0.25;
+        static const int P_inv = 2;
         static const int MAX_LEVEL = 32;  // enough for 2^32 elements
         int level = 1;
-        while (uniform_int_distribution<int>{1, 0xFFFF}(gen_) < P * 0xFFFF &&
+        while (uniform_int_distribution<int>{1, P_inv}(gen_) == 1 &&
                level < MAX_LEVEL) {
             ++level;
         }
