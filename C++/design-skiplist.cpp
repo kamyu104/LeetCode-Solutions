c76d894917775cd3daa8c2c58bba617afe97ff10
kamyu <kamyu104@users.noreply.github.com>
2019-09-29 19:44:40 +0800

    Create design-skiplist.cpp

---
+++ C++/design-skiplist.cpp
@@ -0,0 +1,118 @@
+// Time:  O(logn) on average for each operation
+// Space: O(n)
+
+// see proof in reference : https://kunigami.blog/2012/09/25/skip-lists-in-python/
+class Skiplist {
+private:
+    class SkipNode {
+    public:
+        SkipNode() : SkipNode(0, -1) {
+            
+        }
+    
+        SkipNode(int level, int num)
+         : num(num)
+         , nexts(level) {
+            
+        }
+
+        int num;
+        vector<shared_ptr<SkipNode>> nexts;
+    };
+
+public:
+    Skiplist()
+    : gen_((random_device())())
+    , len_(0)
+    , head_(make_shared<SkipNode>()) {
+        
+    }
+    
+    bool search(int target) {
+        return find(target, find_prev_nodes(target)) != nullptr;
+    }
+    
+    void add(int num) {
+        auto node = make_shared<SkipNode>(random_level(), num);
+        if (head_->nexts.size() < node->nexts.size()) {
+            head_->nexts.resize(node->nexts.size());
+        }
+        auto prevs = find_prev_nodes(num);
+        for (int i = 0; i < node->nexts.size(); ++i) {
+            node->nexts[i] = prevs[i]->nexts[i];
+            prevs[i]->nexts[i] = node;
+        }
+        ++len_;
+    }
+    
+    bool erase(int num) {
+        auto prevs = find_prev_nodes(num);
+        auto curr = find(num, prevs);
+        if (!curr) {
+            return false;
+        }
+        --len_;
+        for (int i = curr->nexts.size() - 1; i >= 0; --i) {
+            prevs[i]->nexts[i] = curr->nexts[i];
+            if (!head_->nexts[i]) {
+                head_->nexts.pop_back();
+            }
+        }
+        return true;
+    }
+
+    int size() const {
+        return len_;
+    }
+
+private:
+    shared_ptr<SkipNode> find(int num, vector<shared_ptr<SkipNode>> prevs) {
+        if (!prevs.empty()) {
+            auto candidate = prevs[0]->nexts[0];
+            if (candidate && candidate->num == num) {
+                return candidate;
+            }
+        }
+        return nullptr;
+    }
+    
+    vector<shared_ptr<SkipNode>> find_prev_nodes(int num) {
+        vector<shared_ptr<SkipNode>> prevs(head_->nexts.size());
+        auto curr = head_;
+        for (int i = head_->nexts.size() - 1; i >= 0; --i) {
+            while (curr->nexts[i] && curr->nexts[i]->num < num) {
+                curr = curr->nexts[i];
+            }
+            prevs[i] = curr;
+        }
+        return prevs;
+    }
+    
+    int random_level() {
+        static const int P = 1;
+        static const int MAX_LEVEL = 16;
+        int level = 1;
+        while (uniform_int_distribution<int>{1, P}(gen_) != 1 &&
+               level < MAX_LEVEL) {
+            ++level;
+        }
+        return level;
+    }
+    
+    void print_list() {
+        for (int i = head_->nexts.size() - 1; i >= 0; --i) {
+            auto curr = head_;
+            cout << curr->nexts[i]->num;
+            curr = curr->nexts[i];
+            while (curr->nexts[i]) {
+                cout << "->" << curr->nexts[i]->num;
+                curr = curr->nexts[i];
+            }
+            cout << endl;
+        }
+    }
+
+    default_random_engine gen_;
+    int len_;
+    shared_ptr<SkipNode> head_;
+};
