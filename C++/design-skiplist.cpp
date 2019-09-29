e02ef5ae27aef5bba9d2fe42b536bb63229a9b3e
kamyu <kamyu104@users.noreply.github.com>
2019-09-30 02:28:30 +0800

    Update design-skiplist.cpp

--- C++/design-skiplist.cpp
+++ C++/design-skiplist.cpp
@@ -106,7 +106,7 @@ private:
     
     int random_level() {
         static const int P_INV = 2;  // P_INV = 4 in redis implementation
-        static const int MAX_LEVEL = 32; // enough for 2^32 elements
+        static const int MAX_LEVEL = 32;  // enough for 2^32 elements
         int level = 1;
         while (uniform_int_distribution<int>{1, P_INV}(gen_) <= 1 &&
                level < MAX_LEVEL) {
