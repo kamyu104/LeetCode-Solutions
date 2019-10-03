3ebfa70692f370b25c313e8e9e665dba77cc48bc
kamyu <kamyu104@users.noreply.github.com>
2019-10-03 12:50:28 +0800

    Update team-scores-in-football-tournament.sql

--- MySQL/team-scores-in-football-tournament.sql
+++ MySQL/team-scores-in-football-tournament.sql
@@ -13,12 +13,13 @@ LEFT JOIN
              ELSE 0
          END AS points
   FROM Matches
-  UNION ALL SELECT guest_team AS team_id,
-                   CASE
-                       WHEN host_goals < guest_goals THEN 3
-                       WHEN host_goals = guest_goals THEN 1
-                       ELSE 0
-                   END AS points
+  UNION ALL
+  SELECT guest_team AS team_id,
+         CASE
+             WHEN host_goals < guest_goals THEN 3
+             WHEN host_goals = guest_goals THEN 1
+             ELSE 0
+           END AS points
   FROM Matches) m
 ON t.team_id = m.team_id
 GROUP BY t.team_id
