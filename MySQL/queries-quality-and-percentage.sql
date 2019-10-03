e975221013a86905095e028025fca3a01680c791
kamyu <kamyu104@users.noreply.github.com>
2019-10-03 12:12:16 +0800

    Update queries-quality-and-percentage.sql

--- MySQL/queries-quality-and-percentage.sql
+++ MySQL/queries-quality-and-percentage.sql
@@ -5,4 +5,5 @@ SELECT query_name,
        ROUND(AVG(rating * 100 / POSITION)) / 100 AS quality,
        ROUND(SUM(rating < 3) * 100 / COUNT(*), 2) AS poor_query_percentage
 FROM Queries
-GROUP BY query_name;
+GROUP BY query_name
+ORDER BY NULL;
