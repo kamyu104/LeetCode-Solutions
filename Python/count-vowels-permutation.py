0bff622cf38bd8ac463202479b79766d133d1a78
kamyu <kamyu104@users.noreply.github.com>
2019-10-06 13:00:29 +0800

    Update count-vowels-permutation.py

--- Python/count-vowels-permutation.py
+++ Python/count-vowels-permutation.py
@@ -45,5 +45,5 @@ class Solution2(object):
         MOD = 10**9 + 7
         a, e, i, o, u = 1, 1, 1, 1, 1
         for _ in xrange(1, n):
-            a, e, i, o, u = (e+i+u) % MOD, (a+i) % MOD, (e+o) % MOD, i % MOD, (i+o) % MOD
+            a, e, i, o, u = (e+i+u) % MOD, (a+i) % MOD, (e+o) % MOD, i, (i+o) % MOD
         return (a+e+i+o+u) % MOD
