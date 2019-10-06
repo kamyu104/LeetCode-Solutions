a00e45ee73e28f2d53a5fb1d9515e09146007f01
kamyu <kamyu104@users.noreply.github.com>
2019-10-06 16:23:16 +0800

    Update two-sum-bsts.py

--- Python/two-sum-bsts.py
+++ Python/two-sum-bsts.py
@@ -34,7 +34,6 @@ class Solution(object):
                         stack.append((root.left, False))
                         stack.append((root, True))
                         stack.append((root.right, False))
-                    
         
         left_gen, right_gen = inorder_gen(root1, True), inorder_gen(root2, False)
         left, right = next(left_gen), next(right_gen)
