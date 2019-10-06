3a59f0040d94d7ab121aa63cdb5f37f394597eff
kamyu <kamyu104@users.noreply.github.com>
2019-10-06 14:12:01 +0800

    Create play-with-chips.py

---
+++ Python/play-with-chips.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def minCostToMoveChips(self, chips):
+        """
+        :type chips: List[int]
+        :rtype: int
+        """
+        count = [0]*2
+        for p in chips:
+            count[p%2] += 1
+        return min(count)
