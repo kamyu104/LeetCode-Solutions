02942d705cf144eecee49d3fd213fd8ce86f8001
kamyu <kamyu104@users.noreply.github.com>
2019-10-21 01:53:44 +0800

    Create missing-number-in-arithmetic-progression.py

---
+++ Python/missing-number-in-arithmetic-progression.py
@@ -0,0 +1,21 @@
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def missingNumber(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        def check(arr, d, x):
+            return arr[x] != arr[0] + d*x
+
+        d = (arr[-1]-arr[0])//len(arr)
+        left, right = 0, len(arr)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(arr, d, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return arr[0] + d*left
