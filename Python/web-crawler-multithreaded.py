3ae6636d6027b7017bdd51b96756937dd260f16a
kamyu <kamyu104@users.noreply.github.com>
2019-11-02 19:14:49 +0800

    Create web-crawler-multithreaded.py

---
+++ Python/web-crawler-multithreaded.py
@@ -0,0 +1,80 @@
+# Time:  O(|V| + |E|)
+# Space: O(|V|)
+
+import threading
+import collections
+
+
+# """
+# This is HtmlParser's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+class HtmlParser(object):
+   def getUrls(self, url):
+       """
+       :type url: str
+       :rtype List[str]
+       """
+       pass
+
+
+class Solution(object):
+    NUMBER_OF_WORKERS = 8
+    
+    def __init__(self):
+        self.__cv = threading.Condition()
+        self.__q = collections.deque()
+        self.__working_count = 0
+
+    def crawl(self, startUrl, htmlParser):
+        """
+        :type startUrl: str
+        :type htmlParser: HtmlParser
+        :rtype: List[str]
+        """
+        SCHEME = "http://"
+        def hostname(url):
+            pos = url.find('/', len(SCHEME))
+            if pos == -1:
+                return url
+            return url[:pos]
+
+        def worker(lookup):
+            while True:
+                with self.__cv:
+                    while not self.__q:
+                        self.__cv.wait()
+                    from_url = self.__q.popleft()
+                    if from_url is None:
+                        break
+                    self.__working_count += 1
+                name = hostname(from_url)
+                for to_url in htmlParser.getUrls(from_url):
+                    if name != hostname(to_url):
+                        continue
+                    with self.__cv:
+                        if to_url not in lookup:
+                           lookup.add(to_url)
+                           self.__q.append(to_url)
+                           self.__cv.notifyAll()
+                with self.__cv:
+                    self.__working_count -= 1
+                    if not self.__q and self.__working_count == 0:
+                        self.__cv.notifyAll()
+
+        workers = []
+        self.__q = collections.deque([startUrl])
+        lookup = set([startUrl])
+        for i in xrange(self.NUMBER_OF_WORKERS):
+            t = threading.Thread(target=worker, args=(lookup,))
+            t.start()
+            workers.append(t)        
+        with self.__cv:
+            while self.__q or self.__working_count:
+                self.__cv.wait()
+            for i in xrange(self.NUMBER_OF_WORKERS):
+                self.__q.append(None)
+            self.__cv.notifyAll()
+        for t in workers:
+            t.join()
+        return list(lookup)
