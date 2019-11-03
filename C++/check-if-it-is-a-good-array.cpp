35418c723cbcfce18b264b78768f557f0bb1520b
kamyu <kamyu104@users.noreply.github.com>
2019-11-03 14:23:41 +0800

    Create check-if-it-is-a-good-array.cpp

---
+++ C++/check-if-it-is-a-good-array.cpp
@@ -0,0 +1,17 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+class Solution {
+public:
+    bool isGoodArray(vector<int>& nums) {
+        // Bézout's identity
+        int result = nums[0];
+        for (const auto& num : nums) {
+            result = std::gcd(result, num);  // built-in gcd since C++17, O(logn)
+            if (result == 1) {
+                break;
+            }
+        }
+        return result == 1;
+    }
+};
