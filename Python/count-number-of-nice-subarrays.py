f40b478bb03adcfec960a22b189e6505eb4a2222
kamyu <kamyu104@users.noreply.github.com>
2019-11-03 15:48:21 +0800

    Create count-number-of-nice-subarrays.py

---
+++ Python/count-number-of-nice-subarrays.py
@@ -0,0 +1,45 @@
+# Time:  O(n)
+# Space: O(k)
+
+class Solution(object):
+    def numberOfSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def atMost(nums, k):
+            result, left, count = 0, 0, 0
+            for right in xrange(len(nums)):
+                count += nums[right]%2
+                while count > k:
+                    count -= nums[left]%2
+                    left += 1
+                result += right-left+1
+            return result
+
+        return atMost(nums, k) - atMost(nums, k-1)
+
+
+# Time:  O(n)
+# Space: O(k)
+import collections
+
+
+class Solution2(object):
+    def numberOfSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        dq = collections.deque([-1])
+        for i in xrange(len(nums)):
+            if nums[i]%2:
+                dq.append(i)
+            if len(dq) > k+1:
+                dq.popleft()
+            if len(dq) == k+1:
+                result += dq[1]-dq[0]
+        return result
