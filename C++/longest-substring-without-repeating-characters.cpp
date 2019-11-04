0dd1176f3b0a30dec52e3c09ba5a6b7edc7d2261
RanceJen <BodomMoon@gmail.com>
2019-11-04 12:36:04 +0800

    Update shorter and clearer solution

--- C++/longest-substring-without-repeating-characters.cpp
+++ C++/longest-substring-without-repeating-characters.cpp
@@ -4,22 +4,19 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // Record the last occurrence of each char.
-        unordered_map<char, size_t> last_occurrence;
-        size_t starting_idx = 0, ans = 0;
-        for (size_t i = 0; i < s.size(); ++i) {
-            auto it(last_occurrence.find(s[i]));
-            if (it == last_occurrence.cend()) {
-                last_occurrence.emplace_hint(it, s[i], i);
-            } else {  // s[i] appeared before. Check its validity.
-                if (it->second >= starting_idx) {
-                    ans = max(ans, i - starting_idx);
-                    starting_idx = it->second + 1;
-                }
-                it->second = i;
+        return solution(s);
+    }
+private:
+    int solution(const string& s) {
+        unordered_map<char, int> lastOccurrence;
+        int startIdx = 0, result = 0;
+        for (auto i = 0; i < s.size(); ++i) {
+            if (lastOccurrence.find(s[i]) != lastOccurrence.cend() && lastOccurrence[s[i]] >= startIdx) {
+                startIdx = lastOccurrence[s[i]];
             }
+            lastOccurrence[s[i]] = i;
+            result = max(result, i - startIdx);
         }
-        ans = max(ans, s.size() - starting_idx);
-        return ans;
+        return result;
     }
 };
