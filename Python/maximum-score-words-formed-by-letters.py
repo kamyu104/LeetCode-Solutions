413258947150932cb26b871aed9cf625da576b65
kamyu <kamyu104@users.noreply.github.com>
2019-11-10 22:56:36 +0800

    Update maximum-score-words-formed-by-letters.py

--- Python/maximum-score-words-formed-by-letters.py
+++ Python/maximum-score-words-formed-by-letters.py
@@ -21,10 +21,10 @@ class Solution(object):
                              curr_score+word_scores[i], letter_count-word_counts[i],
                              result)
 
-        result = [0]
         letter_count = collections.Counter(letters)    
         word_counts = map(collections.Counter, words)
         word_scores = [sum(score[ord(c)-ord('a')] for c in words[i])
                        for i in xrange(len(words))]
+        result = [0]
         backtracking(words, word_scores, word_counts, 0, 0, letter_count, result)
         return result[0]
