7dcaeb173aa5a22a19d688b663fb50e71a59267f
kamyu <kamyu104@users.noreply.github.com>
2019-11-10 19:49:33 +0800

    Update maximum-score-words-formed-by-letters.py

--- Python/maximum-score-words-formed-by-letters.py
+++ Python/maximum-score-words-formed-by-letters.py
@@ -12,19 +12,19 @@ class Solution(object):
         :type score: List[int]
         :rtype: int
         """
-        def backtracking(words, scores, word_counts, curr, curr_score, count, result):
+        def backtracking(words, word_scores, word_counts, curr, curr_score, count, result):
             result[0] = max(result[0], curr_score) 
             for i in xrange(curr, len(words)):
                 if any(count[c] < word_counts[i][c] for c in word_counts[i]):
                     continue
-                backtracking(words, scores, word_counts, i+1,
-                             curr_score+scores[i], count-word_counts[i],
+                backtracking(words, word_scores, word_counts, i+1,
+                             curr_score+word_scores[i], count-word_counts[i],
                              result)
 
         result = [0]
         count = collections.Counter(letters)    
         word_counts = map(collections.Counter, words)
-        scores = {i:sum(score[ord(c)-ord('a')] for c in words[i])
-                  for i in xrange(len(words))}
-        backtracking(words, scores, word_counts, 0, 0, count, result)
+        word_scores = [sum(score[ord(c)-ord('a')] for c in words[i])
+                       for i in xrange(len(words))]
+        backtracking(words, word_scores, word_counts, 0, 0, count, result)
         return result[0]
