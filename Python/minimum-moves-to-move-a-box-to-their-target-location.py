307d6c0b492f0a711ac5ae9055b4d319bf7acac5
kamyu <kamyu104@users.noreply.github.com>
2019-11-17 15:35:54 +0800

    Update minimum-moves-to-move-a-box-to-their-target-location.py

--- Python/minimum-moves-to-move-a-box-to-their-target-location.py
+++ Python/minimum-moves-to-move-a-box-to-their-target-location.py
@@ -23,14 +23,15 @@ class Solution(object):
                 p = closer.pop()
                 if p == t:
                     break
-                if p not in lookup:
-                    lookup.add(p)
-                    for dx, dy in directions:
-                        np = (p[0]+dx, p[1]+dy)
-                        if 0 <= np[0] < len(grid) and 0 <= np[1] < len(grid[0]) and \
-                           grid[np[0]][np[1]] != '#' and np not in lookup:
-                            is_closer = dot((dx, dy), (t[0]-p[0], t[1]-p[1])) > 0
-                            (closer if is_closer else detour).append(np)
+                if p in lookup:
+                    continue
+                lookup.add(p)
+                for dx, dy in directions:
+                    np = (p[0]+dx, p[1]+dy)
+                    if not (0 <= np[0] < len(grid) and 0 <= np[1] < len(grid[0]) and
+                       grid[np[0]][np[1]] != '#' and np not in lookup):
+                        continue
+                    (closer if dot((dx, dy), (t[0]-p[0], t[1]-p[1])) > 0 else detour).append(np)
             return True
 
         def g(p1, p2):
@@ -49,16 +50,17 @@ class Solution(object):
                 b, p = closer.pop()
                 if b == t:
                     break
-                if b not in lookup:
-                    lookup.add((b, p))
-                    for dx, dy in directions:
-                        nb, np = (b[0]+dx, b[1]+dy), (b[0]-dx, b[1]-dy)
-                        if 0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and \
-                           0 <= np[0] < len(grid) and  0 <= np[1] < len(grid[0]) and \
-                           grid[nb[0]][nb[1]] != '#' and grid[np[0]][np[1]] != '#' and \
-                           (nb, b) not in lookup and can_reach(b, p, np):
-                            is_closer = dot((dx, dy), (t[0]-b[0], t[1]-b[1])) > 0
-                            (closer if is_closer else detour).append((nb, b))
+                if b in lookup:
+                    continue
+                lookup.add((b, p))
+                for dx, dy in directions:
+                    nb, np = (b[0]+dx, b[1]+dy), (b[0]-dx, b[1]-dy)
+                    if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and
+                            0 <= np[0] < len(grid) and  0 <= np[1] < len(grid[0]) and
+                            grid[nb[0]][nb[1]] != '#' and grid[np[0]][np[1]] != '#' and
+                            (nb, b) not in lookup and can_reach(b, p, np)):
+                        continue
+                    (closer if dot((dx, dy), (t[0]-b[0], t[1]-b[1])) > 0 else detour).append((nb, b))
             return f
         
         b, p, t = None, None, None
