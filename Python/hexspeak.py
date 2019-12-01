2157fc48afd9c9e5a3bc28ae31257c88c8c04f77
kamyu <kamyu104@users.noreply.github.com>
2019-12-02 00:15:50 +0800

    Update hexspeak.py

--- Python/hexspeak.py
+++ Python/hexspeak.py
@@ -18,3 +18,15 @@ class Solution(object):
                 return "ERROR"
             result.append(lookup[r])
         return "".join(reversed(result))
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def toHexspeak(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        result = hex(int(num)).upper()[2:].replace('0', 'O').replace('1', 'I')
+        return result if all(c in "ABCDEFOI" for c in result) else "ERROR"
