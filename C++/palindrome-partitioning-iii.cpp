f07cf5b65b906a02c7dff3fe3911582011d73924
kamyu <kamyu104@users.noreply.github.com>
2019-12-01 23:40:26 +0800

    Create palindrome-partitioning-iii.cpp

---
+++ C++/palindrome-partitioning-iii.cpp
@@ -0,0 +1,32 @@
+// Time:  O(k * n^2)
+// Space: O(n^2)
+
+class Solution {
+public:
+    int palindromePartition(string s, int k) {
+        // dp1[i][j]: minimum number of changes to make s[i, j] palindrome
+        vector<vector<int>> dp1(s.length(), vector<int>(s.length()));
+        for (int l = 1; l <= s.length(); ++l) {
+            for (int i = 0; i + l <= s.length(); ++i) {
+                int j = i + l - 1;
+                if (i == j - 1) {
+                    dp1[i][j] = s[i] == s[j] ? 0 : 1;
+                }  else if (i != j) {
+                    dp1[i][j] = s[i] == s[j] ? dp1[i + 1][j - 1] : dp1[i + 1][j - 1] + 1;
+                }
+            }
+        }
+        // dp2[d][i]: minimum number of changes to divide s[0, i] into d palindromes
+        vector<vector<int>> dp2(2, vector<int>(s.length(), numeric_limits<int>::max()));
+        dp2[1] = dp1[0];
+        for (int d = 2; d <= k; ++d) {
+            dp2[d % 2] = vector<int>(s.length(), numeric_limits<int>::max());
+            for (int i = d - 1; i < s.length(); ++i) {
+                for (int j = d - 2; j < i; ++j) {
+                    dp2[d % 2][i] = min(dp2[d % 2][i], dp2[(d - 1) % 2][j] + dp1[j + 1][i]);
+                }
+            }
+        }
+        return dp2[k % 2][s.length() - 1];
+    }
+};
