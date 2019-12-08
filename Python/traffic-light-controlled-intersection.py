fdf4d2e9380f2c34dd1da589ae29904de715e31e
kamyu <kamyu104@users.noreply.github.com>
2019-12-09 02:39:26 +0800

    Create traffic-light-controlled-intersection.py

---
+++ Python/traffic-light-controlled-intersection.py
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(1)
+
+import threading
+
+
+class TrafficLight(object):
+    
+    def __init__(self):
+        self.__lock = threading.Lock()
+        self.__light = 1
+
+    def carArrived(self, carId, roadId, direction, turnGreen, crossCar):
+        """
+        :type roadId: int --> // ID of the car
+        :type carId: int --> // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+        :type direction: int --> // Direction of the car
+        :type turnGreen: method --> // Use turnGreen() to turn light to green on current road
+        :type crossCar: method --> // Use crossCar() to make car cross the intersection
+        :rtype: void
+        """
+        with self.__lock:
+            if self.__light != roadId:
+                self.__light = roadId
+                turnGreen()
+            crossCar()
