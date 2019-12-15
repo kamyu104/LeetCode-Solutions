7adbcabf2d60ffa744869384fb091a23be801e75
kamyu <kamyu104@users.noreply.github.com>
2019-12-15 21:21:12 +0800

    Update shortest-path-in-a-grid-with-obstacles-elimination.py

--- Python/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ Python/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,6 +1,7 @@
 # Time:  O(m * n * k)
 # Space: O(m * n)
 
+# A* Search Algorithm without heap
 class Solution(object):
     def shortestPath(self, grid, k):
         """
@@ -24,17 +25,18 @@ class Solution(object):
                     f += dh
                     closer, detour = detour, closer
                 b, k = closer.pop()
-                if b == t:
-                    return f
                 if b in lookup and lookup[b] >= k:
                     continue
                 lookup[b] = k
+                if b == t:
+                    return f
                 for dx, dy in directions:
                     nb = (b[0]+dx, b[1]+dy)
                     if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and
-                            (grid[nb[0]][nb[1]] == 0 or k > 0)):
+                            (grid[nb[0]][nb[1]] == 0 or k > 0) and
+                            (nb not in lookup or lookup[nb] < k)):
                         continue
                     (closer if dot((dx, dy), (t[0]-b[0], t[1]-b[1])) > 0 else detour).append((nb, k-int(grid[nb[0]][nb[1]] == 1)))
             return -1
-        
+
         return a_star(grid, (0, 0), (len(grid)-1, len(grid[0])-1), k)
