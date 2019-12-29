03f5a7062a34e4a43b89f4b21f5015e9f9a648ac
kamyu <kamyu104@users.noreply.github.com>
2019-12-29 23:54:21 +0800

    Create sum-of-mutated-array-closest-to-target.py

---
+++ Python/sum-of-mutated-array-closest-to-target.py
@@ -0,0 +1,69 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Solution(object):
+    def findBestValue(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        arr.sort(reverse=True)
+        max_arr = arr[0]
+        while arr and arr[-1]*len(arr) <= target:
+            target -= arr.pop()
+        # let x = ceil(t/n)-1
+        # (1) (t/n-1/2) <= x:
+        #    return x, which is equal to ceil(t/n)-1 = ceil(t/n-1/2) = (2t+n-1)//2n
+        # (2) (t/n-1/2) > x:
+        #    return x+1, which is equal to ceil(t/n) = ceil(t/n-1/2) = (2t+n-1)//2n
+        # (1) + (2) => both return (2t+n-1)//2n
+        return max_arr if not arr else (2*target+len(arr)-1)//(2*len(arr))
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+class Solution2(object):
+    def findBestValue(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        arr.sort(reverse=True)
+        max_arr = arr[0]
+        while arr and arr[-1]*len(arr) <= target:
+            target -= arr.pop()
+        if not arr:
+            return max_arr
+        x = (target-1)//len(arr)
+        return x if target-x*len(arr) <= (x+1)*len(arr)-target else x+1
+
+
+# Time:  O(nlogm), m is the max of arr, which may be larger than n
+# Space: O(1)
+class Solution3(object):
+    def findBestValue(self, arr, target):
+        """
+        :type arr: List[int]
+        :type target: int
+        :rtype: int
+        """
+        def total(arr, v):
+            result = 0
+            for x in arr:
+                result += min(v, x)
+            return result
+
+        def check(arr, v, target):
+            return total(arr, v) >= target
+        
+        max_arr = max(arr)
+        left, right = 1, max_arr
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(arr, mid, target):
+                right = mid-1
+            else:
+                left = mid+1
+        return left-1 if target-total(arr, left-1) <= total(arr, left)-target else left
