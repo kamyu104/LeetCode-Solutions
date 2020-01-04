08d00c431f106b4eb500dce25a2466d3d9743a7e
kamyu <kamyu104@users.noreply.github.com>
2020-01-05 02:15:55 +0800

    Create running-total-for-different-genders.sql

---
+++ MySQL/running-total-for-different-genders.sql
@@ -0,0 +1,14 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT gender, 
+       day, 
+       CASE 
+         WHEN gender = 'F' THEN @f_accu := @f_accu + score_points 
+         ELSE @m_accu := @m_accu + score_points 
+       end total 
+FROM   scores, 
+       (SELECT @f_accu := 0, 
+               @m_accu := 0) init 
+ORDER  BY gender, 
+          day 
