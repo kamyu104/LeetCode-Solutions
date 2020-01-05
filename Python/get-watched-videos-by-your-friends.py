c2998fedd41903beb5d08375c6c8ad0140fc9aea
kamyu <kamyu104@users.noreply.github.com>
2020-01-05 14:32:15 +0800

    Create get-watched-videos-by-your-friends.py

---
+++ Python/get-watched-videos-by-your-friends.py
@@ -0,0 +1,18 @@
+# Time:  O(n + vlogv), v is the number of the level videos
+# Space: O(w)
+
+class Solution(object):
+    def watchedVideosByFriends(self, watchedVideos, friends, id, level):
+        """
+        :type watchedVideos: List[List[str]]
+        :type friends: List[List[int]]
+        :type id: int
+        :type level: int
+        :rtype: List[str]
+        """
+        curr_level, lookup = set([id]), set([id])
+        for _ in xrange(level):
+            curr_level = set(j for i in curr_level for j in friends[i] if j not in lookup)
+            lookup |= curr_level
+        count = collections.Counter([v for i in curr_level for v in watchedVideos[i]])
+        return sorted(count.keys(), key=lambda x: (count[x], x))
