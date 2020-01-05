773f5a3982303e32372787639598b6161bf4fedf
kamyu <kamyu104@users.noreply.github.com>
2020-01-05 14:35:27 +0800

    Update longest-common-subsequence.cpp

--- C++/longest-common-subsequence.cpp
+++ C++/longest-common-subsequence.cpp
@@ -4,18 +4,18 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        if (text1.size() < text2.size()) {
+                if (text1.length() < text2.length()) {
             return longestCommonSubsequence(text2, text1);
         }
-        vector<vector<int>> dp(2, vector<int>(text2.size() + 1));
-        for (int i = 1; i <= text1.size(); ++i) {
-            for (int j = 1; j <= text2.size(); ++j) {
+        vector<vector<int>> dp(2, vector<int>(text2.length() + 1));
+        for (int i = 1; i <= text1.length(); ++i) {
+            for (int j = 1; j <= text2.length(); ++j) {
                 dp[i % 2][j] = (text1[i - 1] == text2[j - 1])
                                ? dp[(i - 1) % 2][j - 1] + 1
                                : max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
                                
             }
         }
-        return dp[text1.size() % 2][text2.size()];
+        return dp[text1.length() % 2][text2.length()];
     }
 };
