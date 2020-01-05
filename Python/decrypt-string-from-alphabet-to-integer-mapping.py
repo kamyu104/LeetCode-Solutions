56e7db4078412bdecb27a88d2df147c7f5a936ec
kamyu <kamyu104@users.noreply.github.com>
2020-01-05 14:59:52 +0800

    Update decrypt-string-from-alphabet-to-integer-mapping.py

--- Python/decrypt-string-from-alphabet-to-integer-mapping.py
+++ Python/decrypt-string-from-alphabet-to-integer-mapping.py
@@ -45,3 +45,20 @@ class Solution2(object):
                 result.append(alpha(s[i]))
                 i -= 1
         return "".join(reversed(result))
+
+# Time:  O(n)
+# Space: O(n)
+import re
+
+
+# regex solution
+class Solution3(object):
+    def freqAlphabets(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def alpha(num):
+            return chr(ord('a') + int(num)-1)
+
+        return "".join(alpha(i[:2]) for i in re.findall(r"\d\d#|\d", s))
