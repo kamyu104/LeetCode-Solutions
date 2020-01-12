883a9701d559de565d5270c9aff27cd663c71dd5
kamyu <kamyu104@users.noreply.github.com>
2020-01-13 02:11:55 +0800

    Create distinct-echo-substrings.py

---
+++ Python/distinct-echo-substrings.py
@@ -0,0 +1,69 @@
+# Time:  O(n^2 + d), d is the duplicated of result substrings size
+# Space: O(r), r is the size of result substrings set
+
+class Solution(object):
+    def distinctEchoSubstrings(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        result = set()
+        for l in xrange(1, len(text)//2+1):
+            count = sum(text[i] == text[i+l] for i in xrange(l))
+            for i in xrange(len(text)-2*l):
+                if count == l:
+                    result.add(text[i:i+l])
+                count += (text[i+l] == text[i+l+l]) - (text[i] == text[i+l])
+            if count == l:
+                result.add(text[len(text)-2*l:len(text)-2*l+l])
+        return len(result)
+
+
+# Time:  O(n^2 + d), d is the duplicated of result substrings size
+# Space: O(r), r is the size of result substrings set
+class Solution2(object):
+    def distinctEchoSubstrings(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        MOD = 10**9+7
+        D = 27  # a-z and ''
+        result = set()
+        for i in xrange(len(text)-1):
+            left, right, pow_D = 0, 0, 1
+            for l in xrange(1, min(i+2, len(text)-i)):
+                left = (D*left + (ord(text[i-l+1])-ord('a')+1)) % MOD
+                right = (pow_D*(ord(text[i+l])-ord('a')+1) + right) % MOD
+                if left == right:  # assumed no collision
+                    result.add(left)
+                pow_D = (pow_D*D) % MOD 
+        return len(result)
+
+
+# Time:  O(n^3 + d), d is the duplicated of result substrings size
+# Space: O(r), r is the size of result substrings set
+class Solution_TLE(object):
+    def distinctEchoSubstrings(self, text):
+        """
+        :type text: str
+        :rtype: int
+        """
+        def compare(text, l, s1, s2):
+            for i in xrange(l):
+                if text[s1+i] != text[s2+i]:
+                    return False
+            return True
+
+        MOD = 10**9+7
+        D = 27  # a-z and ''
+        result = set()
+        for i in xrange(len(text)):
+            left, right, pow_D = 0, 0, 1
+            for l in xrange(1, min(i+2, len(text)-i)):
+                left = (D*left + (ord(text[i-l+1])-ord('a')+1)) % MOD
+                right = (pow_D*(ord(text[i+l])-ord('a')+1) + right) % MOD
+                if left == right and compare(text, l, i-l+1, i+1):
+                    result.add(text[i+1:i+1+l])
+                pow_D = (pow_D*D) % MOD 
+        return len(result)
