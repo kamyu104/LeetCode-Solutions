ce1b071ec4b9b8689013bf8fd05ab7955b33dc1c
kamyu <kamyu104@users.noreply.github.com>
2020-01-12 19:56:48 +0800

    Update range-sum-query-2d-immutable.py

--- Python/range-sum-query-2d-immutable.py
+++ Python/range-sum-query-2d-immutable.py
@@ -15,10 +15,8 @@ class NumMatrix(object):
         self.__sums = [[0 for _ in xrange(n+1)] for _ in xrange(m+1)]
         for i in xrange(1, m+1):
             for j in xrange(1, n+1):
-                self.__sums[i][j] = self.__sums[i][j-1] + matrix[i-1][j-1]
-        for j in xrange(1, n+1):
-            for i in xrange(1, m+1):
-                self.__sums[i][j] += self.__sums[i-1][j]
+                self.__sums[i][j] = self.__sums[i][j-1] + self.__sums[i-1][j] - \
+                                    self.__sums[i-1][j-1] + matrix[i-1][j-1]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
