01f8d5008cdae97fca9e3cc648b9d947c0f0d691
kamyu <kamyu104@users.noreply.github.com>
2020-01-13 23:51:30 +0800

    Update minimum-flips-to-make-a-or-b-equal-to-c.cpp

--- C++/minimum-flips-to-make-a-or-b-equal-to-c.cpp
+++ C++/minimum-flips-to-make-a-or-b-equal-to-c.cpp
@@ -4,6 +4,23 @@
 class Solution {
 public:
     int minFlips(int a, int b, int c) {
+        int diff = (a | b) ^ c;
+        return number_of_1_bits(diff) + number_of_1_bits(diff & (a & b));
+    }
+
+private:
+    int number_of_1_bits(int n) {
+        int result = 0;
+        for (; n; n &= n - 1, ++result);
+        return result;
+    }
+};
+
+// Time:  O(31)
+// Space: O(1)
+class Solution2 {
+public:
+    int minFlips(int a, int b, int c) {
         int result = 0;
         for (int i = 0; i < 31; ++i) {
             int a_i = a & 1, b_i = b & 1, c_i = c & 1;
