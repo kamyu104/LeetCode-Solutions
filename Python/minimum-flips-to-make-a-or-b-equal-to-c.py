f5b1e872d44defbe68559fca7e3a8cf53875103f
kamyu <kamyu104@users.noreply.github.com>
2020-01-13 23:48:48 +0800

    Update minimum-flips-to-make-a-or-b-equal-to-c.py

--- Python/minimum-flips-to-make-a-or-b-equal-to-c.py
+++ Python/minimum-flips-to-make-a-or-b-equal-to-c.py
@@ -9,6 +9,27 @@ class Solution(object):
         :type c: int
         :rtype: int
         """
+        def number_of_1_bits(n):
+            result = 0
+            while n:
+                n &= n-1
+                result += 1
+            return result
+
+        diff = (a|b)^c
+        return number_of_1_bits(diff) + number_of_1_bits(diff & (a&b))
+
+
+# Time:  O(31)
+# Space: O(1)
+class Solution2(object):
+    def minFlips(self, a, b, c):
+        """
+        :type a: int
+        :type b: int
+        :type c: int
+        :rtype: int
+        """
         result = 0
         for i in xrange(31):
             a_i, b_i, c_i = map(lambda x: x&1, [a, b, c])
