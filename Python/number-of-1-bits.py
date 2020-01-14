61d8dcef0d89b600c70f071c87243add2fb024e7
kamyu <kamyu104@users.noreply.github.com>
2020-01-14 22:19:25 +0800

    Update number-of-1-bits.py

--- Python/number-of-1-bits.py
+++ Python/number-of-1-bits.py
@@ -1,4 +1,5 @@
 # Time:  O(32), bit shift in python is not O(1), it's O(k), k is the number of bits shifted
+#             , see https://github.com/python/cpython/blob/2.7/Objects/longobject.c#L3652
 # Space: O(1)
 
 class Solution(object):
