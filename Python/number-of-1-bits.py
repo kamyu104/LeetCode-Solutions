1b27c0b298dc1d9964e98c4825b022bc069b7901
kamyu <kamyu104@users.noreply.github.com>
2020-01-14 09:29:31 +0800

    Update number-of-1-bits.py

--- Python/number-of-1-bits.py
+++ Python/number-of-1-bits.py
@@ -1,7 +1,21 @@
-# Time:  O(logn) = O(32)
+# Time:  O(log(logn)) = O(5)
 # Space: O(1)
 
 class Solution(object):
+    # @param n, an integer
+    # @return an integer
+    def hammingWeight(self, n):
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555)
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333)
+        n = (n & 0x0F0F0F0F) + ((n >> 4) & 0x0F0F0F0F)
+        n = (n & 0x00FF00FF) + ((n >> 8) & 0x00FF00FF)
+        n = (n & 0x0000FFFF) + ((n >> 16) & 0x0000FFFF)
+        return n
+
+    
+# Time:  O(logn) = O(32)
+# Space: O(1)
+class Solution2(object):
     # @param n, an integer
     # @return an integer
     def hammingWeight(self, n):
