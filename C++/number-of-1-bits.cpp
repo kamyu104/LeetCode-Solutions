1515c2d61fc3a93716f913cda6752cc10a0c409d
kamyu <kamyu104@users.noreply.github.com>
2020-01-14 09:28:32 +0800

    Update number-of-1-bits.cpp

--- C++/number-of-1-bits.cpp
+++ C++/number-of-1-bits.cpp
@@ -1,7 +1,22 @@
-// Time:  O(logn) = O(32)
+// Time:  O(logn(logn)) = O(5)
 // Space: O(1)
 
 class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        n = (n & 0x55555555) + ((n >> 1) & 0x55555555);
+        n = (n & 0x33333333) + ((n >> 2) & 0x33333333);
+        n = (n & 0x0F0F0F0F) + ((n >> 4) & 0x0F0F0F0F);
+        n = (n & 0x00FF00FF) + ((n >> 8) & 0x00FF00FF);
+        n = (n & 0x0000FFFF) + ((n >> 16) & 0x0000FFFF);
+        return n;
+    }
+};
+
+
+// Time:  O(logn) = O(32)
+// Space: O(1)
+class Solution2 {
 public:
     int hammingWeight(uint32_t n) {
         int count = 0;
