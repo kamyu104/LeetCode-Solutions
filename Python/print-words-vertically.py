cd3fc677d58fc1d09b4482a9a20266e22313578c
kamyu <kamyu104@users.noreply.github.com>
2020-01-19 17:27:48 +0800

    Create print-words-vertically.py

---
+++ Python/print-words-vertically.py
@@ -0,0 +1,13 @@
+# Time:  O(n)
+# Space: O(h)
+
+import itertools
+
+
+class Solution(object):
+    def printVertically(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        return ["".join(c).rstrip() for c in itertools.izip_longest(*s.split(), fillvalue=' ')]
