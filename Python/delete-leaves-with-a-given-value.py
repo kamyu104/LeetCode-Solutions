d0fcbc647d6d7e9e46539c573f7d9d762d7f0d92
kamyu <kamyu104@users.noreply.github.com>
2020-01-19 17:28:45 +0800

    Create delete-leaves-with-a-given-value.py

---
+++ Python/delete-leaves-with-a-given-value.py
@@ -0,0 +1,23 @@
+# Time:  O(n)
+# Space: O(h)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def removeLeafNodes(self, root, target):
+        """
+        :type root: TreeNode
+        :type target: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        return None if root.left == root.right and root.val == target else root
