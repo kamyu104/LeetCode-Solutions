954d10c2e328e1ae648d0c713ccb20d4956e4242
kamyu <kamyu104@users.noreply.github.com>
2020-01-24 15:38:38 +0800

    Update list-the-products-ordered-in-a-period.sql

--- MySQL/list-the-products-ordered-in-a-period.sql
+++ MySQL/list-the-products-ordered-in-a-period.sql
@@ -7,7 +7,7 @@ FROM   (SELECT product_id,
                Sum(unit) AS unit 
         FROM   orders 
         WHERE  order_date BETWEEN '2020-02-01' AND '2020-02-29' 
-        GROUP  BY product_id) o 
+        GROUP  BY product_id 
+        HAVING unit >= 100) o 
        INNER JOIN products p 
-              ON o.product_id = p.product_id 
-WHERE  o.unit >= 100 
+               ON o.product_id = p.product_id 
