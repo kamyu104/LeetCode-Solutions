11870a7b5ac24cff8303793b04be711d4daf58c2
kamyu <kamyu104@users.noreply.github.com>
2020-01-24 15:32:09 +0800

    Create list-the-products-ordered-in-a-period.sql

---
+++ MySQL/list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,13 @@
+# Time:  O(n)  
+# Space: O(n)  
+
+SELECT p.product_name, 
+       o.unit 
+FROM   (SELECT product_id, 
+               Sum(unit) AS unit 
+        FROM   orders 
+        WHERE  order_date BETWEEN '2020-02-01' AND '2020-02-29' 
+        GROUP  BY product_id) o 
+       INNER JOIN products p 
+              ON o.product_id = p.product_id 
+HAVING o.unit >= 100 
