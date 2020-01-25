9bfbd31552191fb487feefb8a298bd685cd1b5f2
kamyu <kamyu104@users.noreply.github.com>
2020-01-26 02:49:26 +0800

    Create sort-the-matrix-diagonally.cpp

---
+++ C++/sort-the-matrix-diagonally.cpp
@@ -0,0 +1,24 @@
+// Time:  O(m * n * log(min(m, n))
+// Space: O(m * n)
+
+class Solution {
+public:
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        unordered_map<int, vector<int>> lookup;
+        for (int i = 0; i < mat.size(); ++i) {
+            for (int j = 0; j < mat[0].size(); ++j) {
+                lookup[i - j].emplace_back(mat[i][j]);
+            }
+        }
+        for (auto& [k, v] : lookup) {
+            sort(v.begin(), v.end());
+        }
+        for (int i = mat.size() - 1; i >= 0; --i) {
+            for (int j = mat[0].size() - 1; j >= 0; --j) {
+                mat[i][j] = lookup[i - j].back();
+                lookup[i - j].pop_back();
+            }
+        }
+        return mat;
+    }
+};
