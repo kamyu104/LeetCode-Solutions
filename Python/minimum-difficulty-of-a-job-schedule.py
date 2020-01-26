2b05ef8998d0eb11ba7bed5c09c833336921f88f
kamyu <kamyu104@users.noreply.github.com>
2020-01-26 13:58:15 +0800

    Create minimum-difficulty-of-a-job-schedule.py

---
+++ Python/minimum-difficulty-of-a-job-schedule.py
@@ -0,0 +1,26 @@
+# Time:  O(n^2 * d)
+# Space: O(n * d)
+
+class Solution(object):
+    def minDifficulty(self, jobDifficulty, d):
+        """
+        :type jobDifficulty: List[int]
+        :type d: int
+        :rtype: int
+        """
+        if len(jobDifficulty) < d:
+            return -1;
+
+        max_in_range = [[0]*len(jobDifficulty) for _ in xrange(len(jobDifficulty))]
+        for i in xrange(len(jobDifficulty)):
+            max_in_range[i][i] = jobDifficulty[i]
+            for j in xrange(i+1, len(jobDifficulty)):
+                max_in_range[i][j] = max(max_in_range[i][j-1], jobDifficulty[j])
+        
+        dp = [[float("inf")]*d for _ in xrange(len(jobDifficulty))]
+        for i in xrange(len(jobDifficulty)):
+            dp[i][0] = max_in_range[0][i]
+            for j in xrange(1, d):
+                for k in reversed(xrange(i)):
+                    dp[i][j] = min(dp[i][j], dp[k][j-1] + max_in_range[k+1][i])
+        return dp[len(jobDifficulty)-1][d-1]
