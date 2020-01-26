ab62b7a128bd17c5a1c1b336897f0efcb4de91e9
kamyu <kamyu104@users.noreply.github.com>
2020-01-26 16:10:33 +0800

    Update filter-restaurants-by-vegan-friendly-price-and-distance.cpp

--- C++/filter-restaurants-by-vegan-friendly-price-and-distance.cpp
+++ C++/filter-restaurants-by-vegan-friendly-price-and-distance.cpp
@@ -1,19 +1,22 @@
-// Time:  O(nlogn)
-// Space: O(1)
+// Time:  O(rlogr), r is the number of result
+// Space: O(r)
 
 class Solution {
 public:
     vector<int> filterRestaurants(vector<vector<int>>& restaurants, int veganFriendly, int maxPrice, int maxDistance) {
-        sort(restaurants.begin(), restaurants.end(),
-             [](const auto& a, const auto& b) {
-                 return vector<int>{-a[1], -a[0], a[2], a[3], a[4]} < vector<int>{-b[1], -b[0], b[2], b[3], b[4]};
-             });
         vector<int> result;
-        for (const auto& r : restaurants) {
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < restaurants.size(); ++i) {
+            const auto& r = restaurants[i];
             if (r[2] >= veganFriendly && r[3] <= maxPrice && r[4] <= maxDistance) {
+                lookup[r[0]] = i;  
                 result.emplace_back(r[0]);
             }
         }
+        sort(result.begin(), result.end(),
+             [&restaurants, &lookup](const auto& i, const auto& j) {
+                 return vector<int>{-restaurants[lookup[i]][1], -restaurants[lookup[i]][0]} < vector<int>{-restaurants[lookup[j]][1], -restaurants[lookup[j]][0]};
+             });
         return result;
     }
 };
