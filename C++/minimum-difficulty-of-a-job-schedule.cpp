25c157a071b4676af70526bf21e12ba3ea6d4b76
kamyu <kamyu104@users.noreply.github.com>
2020-01-28 10:15:50 +0800

    Update minimum-difficulty-of-a-job-schedule.cpp

--- C++/minimum-difficulty-of-a-job-schedule.cpp
+++ C++/minimum-difficulty-of-a-job-schedule.cpp
@@ -1,5 +1,5 @@
-// Time:  O(n^2 * d)
-// Space: O(n^2)
+// Time:  O(d * n^2)
+// Space: O(d * n)
 
 class Solution {
 public:
@@ -8,27 +8,23 @@ public:
             return -1;
         }
 
-        vector<vector<int>> max_in_range(jobDifficulty.size(),
-                                         vector<int>(jobDifficulty.size()));
-        for (int i = 0; i < jobDifficulty.size(); ++i) {
-            max_in_range[i][i] = jobDifficulty[i];
-            for (int j = i + 1; j < jobDifficulty.size(); ++j) {
-                max_in_range[i][j] = max(max_in_range[i][j - 1], jobDifficulty[j]);
-            }
+        vector<vector<int>> dp(d,
+                               vector<int>(jobDifficulty.size(), numeric_limits<int>::max()));
+        dp[0][0] = jobDifficulty[0];
+        for (int i = 1; i < jobDifficulty.size(); ++i) {
+            dp[0][i] = max(dp[0][i - 1], jobDifficulty[i]);
         }
-        
-        vector<vector<int>> dp(jobDifficulty.size(),
-                               vector<int>(d, numeric_limits<int>::max()));
-        for (int i = 0; i < jobDifficulty.size(); ++i) {
-            dp[i][0] = max_in_range[0][i];
-            for (int j = 1; j < d; ++j) {
-                for (int k = i - 1; k >= 0; --k) {
-                    if (dp[k][j - 1] != numeric_limits<int>::max()) {
-                        dp[i][j] = min(dp[i][j], dp[k][j - 1] + max_in_range[k + 1][i]);
+        for (int i = 1; i < d; ++i) {
+            for (int j = i; j < jobDifficulty.size(); ++j) {
+                int curr_max = jobDifficulty[j];
+                for (int k = j; k >= i; --k) {
+                    curr_max = max(curr_max, jobDifficulty[k]);
+                    if (dp[i - 1][k - 1] != numeric_limits<int>::max()) {
+                        dp[i][j] = min(dp[i][j], dp[i - 1][k - 1] + curr_max);
                     }
                 }
             }
         }
-        return dp[jobDifficulty.size() - 1][d - 1];
+        return dp[d - 1][jobDifficulty.size() - 1];
     }
 };
