b5883c593322d7c16b6f7afb67a7e9ed5ef1df54
kamyu <kamyu104@users.noreply.github.com>
2020-01-29 15:06:54 +0800

    Update reverse-substrings-between-each-pair-of-parentheses.py

--- Python/reverse-substrings-between-each-pair-of-parentheses.py
+++ Python/reverse-substrings-between-each-pair-of-parentheses.py
@@ -1,7 +1,34 @@
-# Time:  O(n^2)
+# Time:  O(n)
 # Space: O(n)
 
 class Solution(object):
+    def reverseParentheses(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        stk, lookup = [], {}
+        for i, c in enumerate(s):
+            if c == '(':
+                stk.append(i)
+            elif c == ')':
+                j = stk.pop()
+                lookup[i], lookup[j] = j, i
+        result = []
+        i, d = 0, 1
+        while i < len(s):
+            if s[i] == '(' or s[i] == ')':
+                i = lookup[i]
+                d *= -1
+            else:
+                result.append(s[i])
+            i += d
+        return "".join(result)
+
+
+# Time:  O(n^2)
+# Space: O(n)
+class Solution2(object):
     def reverseParentheses(self, s):
         """
         :type s: str
