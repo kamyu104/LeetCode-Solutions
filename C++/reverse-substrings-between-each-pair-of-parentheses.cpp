1ce0d3f6e1b813822601881ece87d90b28bbb088
kamyu <kamyu104@users.noreply.github.com>
2020-01-29 15:06:23 +0800

    Update reverse-substrings-between-each-pair-of-parentheses.cpp

--- C++/reverse-substrings-between-each-pair-of-parentheses.cpp
+++ C++/reverse-substrings-between-each-pair-of-parentheses.cpp
@@ -1,7 +1,35 @@
-// Time:  O(n^2)
+// Time:  O(n)
 // Space: O(n)
 
 class Solution {
+public:
+    string reverseParentheses(string s) {
+        vector<int> stk;
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] == '(') {
+                stk.emplace_back(i);
+            } else if (s[i] == ')') {
+                int j = stk.back(); stk.pop_back();
+                lookup[i] = j, lookup[j] = i;
+            }
+        }
+        string result;
+        for (int i = 0, d = 1; i < s.length(); i += d) {
+            if (s[i] == '(' || s[i] == ')') {
+                i = lookup[i];
+                d *= -1;
+            } else {
+                result.push_back(s[i]);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+class Solution2 {
 public:
     string reverseParentheses(string s) {
         vector<string> stk = {""};
