f922f91f2e2fa5c3f4c71f2e193372b383784f2b
kamyu <kamyu104@users.noreply.github.com>
2020-02-03 05:38:08 +0800

    Update jump-game-v.cpp

--- C++/jump-game-v.cpp
+++ C++/jump-game-v.cpp
@@ -1,8 +1,125 @@
-// Time:  O(nlogn)
+// Time:  O(n)
 // Space: O(n)
 
-// mono stack + bottom-up dp + segment tree
+// sliding window + top-down dp
 class Solution {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        vector<vector<int>> left(arr.size());
+        deque<int> decreasing_dq;
+        for (int i = 0; i < arr.size(); ++i) {
+            if (!decreasing_dq.empty() && i - decreasing_dq.front() == d + 1) {
+                decreasing_dq.pop_front();
+            }
+            while (!decreasing_dq.empty() && arr[decreasing_dq.back()] < arr[i]) {
+                if (!left[i].empty() && arr[left[i].back()] != arr[decreasing_dq.back()]) {
+                    left[i].clear();
+                }
+                left[i].emplace_back(decreasing_dq.back());
+                decreasing_dq.pop_back();
+            }
+            decreasing_dq.emplace_back(i);
+        }
+        vector<vector<int>> right(arr.size());
+        decreasing_dq.clear();
+        for (int i = arr.size() - 1; i >= 0; --i) {
+            if (!decreasing_dq.empty() && decreasing_dq.front() - i == d + 1) {
+                decreasing_dq.pop_front();
+            }
+            while (!decreasing_dq.empty() && arr[decreasing_dq.back()] < arr[i]) {
+                if (!right[i].empty() && arr[right[i].back()] != arr[decreasing_dq.back()]) {
+                    right[i].clear();
+                }
+                right[i].emplace_back(decreasing_dq.back());
+                decreasing_dq.pop_back();
+            }
+            decreasing_dq.emplace_back(i);
+        }
+
+        int result = 0;
+        vector<int> lookup(arr.size());
+        for (int i = 0; i < arr.size(); ++i) {
+            result = max(result, dp(arr, d, i, left, right, &lookup));
+        }
+        return result;
+    }
+
+private:
+    int dp(const vector<int>& arr, int d, int i,
+           const vector<vector<int>>& left, const vector<vector<int>>& right,
+           vector<int> *lookup) {
+        if ((*lookup)[i]) {
+            return (*lookup)[i];
+        }
+        (*lookup)[i] = 1;
+        for (const auto& j : left[i]) {
+            (*lookup)[i] = max((*lookup)[i], dp(arr, d, j, left, right, lookup) + 1);
+        }
+        for (const auto& j : right[i]) {
+            (*lookup)[i] = max((*lookup)[i], dp(arr, d, j, left, right, lookup) + 1);
+        }
+        return (*lookup)[i];
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// mono stack + bottom-up dp
+class Solution2 {
+public:
+    int maxJumps(vector<int>& arr, int d) {
+        vector<vector<int>> left(arr.size());
+        vector<int>decreasing_stk;
+        for (int i = 0; i < arr.size(); ++i) {
+            while (!decreasing_stk.empty() && arr[decreasing_stk.back()] < arr[i]) {
+                if (i - decreasing_stk.back() <= d) {
+                    if (!left[i].empty() && arr[left[i].back()] != arr[decreasing_stk.back()]) {
+                        left[i].clear();
+                    }
+                    left[i].emplace_back(decreasing_stk.back());
+                }
+                decreasing_stk.pop_back();
+            }
+            decreasing_stk.emplace_back(i);
+        }
+        vector<vector<int>> right(arr.size());
+        decreasing_stk.clear();
+        for (int i = arr.size() - 1; i >= 0; --i) {
+            while (!decreasing_stk.empty() && arr[decreasing_stk.back()] < arr[i]) {
+                if (decreasing_stk.back() - i <= d) {
+                    if (!right[i].empty() && arr[right[i].back()] != arr[decreasing_stk.back()]) {
+                        right[i].clear();
+                    }
+                    right[i].emplace_back(decreasing_stk.back());
+                }
+                decreasing_stk.pop_back();
+            }
+            decreasing_stk.emplace_back(i);
+        }
+        
+        vector<pair<int, int>> sorted_arr;
+        for (int i = 0; i < arr.size(); ++i) {
+            sorted_arr.emplace_back(arr[i], i);
+        }
+        sort(sorted_arr.begin(), sorted_arr.end());
+        vector<int> dp(arr.size(), 1);
+        for (const auto& [_, i] : sorted_arr) {
+            dp[i] = 1;
+            for (const auto& j : left[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+            for (const auto& j : right[i]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+        return *max_element(dp.cbegin(), dp.cend());
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// mono stack + bottom-up dp + segment tree
+class Solution3 {
 public:
     int maxJumps(vector<int>& arr, int d) {
         vector<int> left(arr.size()), decreasing_stk;
@@ -121,121 +238,4 @@ private:
             }
         }
     };
-};
-
-// Time:  O(max(nlogn, n * d))
-// Space: O(n)
-// mono stack + bottom-up dp
-class Solution2 {
-public:
-    int maxJumps(vector<int>& arr, int d) {
-        vector<vector<int>> left(arr.size());
-        vector<int>decreasing_stk;
-        for (int i = 0; i < arr.size(); ++i) {
-            while (!decreasing_stk.empty() && arr[decreasing_stk.back()] < arr[i]) {
-                if (i - decreasing_stk.back() <= d) {
-                    if (!left[i].empty() && arr[left[i].back()] != arr[decreasing_stk.back()]) {
-                        left[i].clear();
-                    }
-                    left[i].emplace_back(decreasing_stk.back());
-                }
-                decreasing_stk.pop_back();
-            }
-            decreasing_stk.emplace_back(i);
-        }
-        vector<vector<int>> right(arr.size());
-        decreasing_stk.clear();
-        for (int i = arr.size() - 1; i >= 0; --i) {
-            while (!decreasing_stk.empty() && arr[decreasing_stk.back()] < arr[i]) {
-                if (decreasing_stk.back() - i <= d) {
-                    if (!right[i].empty() && arr[right[i].back()] != arr[decreasing_stk.back()]) {
-                        right[i].clear();
-                    }
-                    right[i].emplace_back(decreasing_stk.back());
-                }
-                decreasing_stk.pop_back();
-            }
-            decreasing_stk.emplace_back(i);
-        }
-        
-        vector<pair<int, int>> sorted_arr;
-        for (int i = 0; i < arr.size(); ++i) {
-            sorted_arr.emplace_back(arr[i], i);
-        }
-        sort(sorted_arr.begin(), sorted_arr.end());
-        vector<int> dp(arr.size(), 1);
-        for (const auto& [_, i] : sorted_arr) {
-            dp[i] = 1;
-            for (const auto& j : left[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-            for (const auto& j : right[i]) {
-                dp[i] = max(dp[i], dp[j] + 1);
-            }
-        }
-        return *max_element(dp.cbegin(), dp.cend());
-    }
-};
-
-// Time:  O(n) ~ O(n * d), if all elements of arr are unique, it could be O(n)
-// Space: O(n)
-// sliding window + top-down dp
-class Solution3 {
-public:
-    int maxJumps(vector<int>& arr, int d) {
-        vector<vector<int>> left(arr.size());
-        deque<int> decreasing_dq;
-        for (int i = 0; i < arr.size(); ++i) {
-            if (!decreasing_dq.empty() && i - decreasing_dq.front() == d + 1) {
-                decreasing_dq.pop_front();
-            }
-            while (!decreasing_dq.empty() && arr[decreasing_dq.back()] < arr[i]) {
-                if (!left[i].empty() && arr[left[i].back()] != arr[decreasing_dq.back()]) {
-                    left[i].clear();
-                }
-                left[i].emplace_back(decreasing_dq.back());
-                decreasing_dq.pop_back();
-            }
-            decreasing_dq.emplace_back(i);
-        }
-        vector<vector<int>> right(arr.size());
-        decreasing_dq.clear();
-        for (int i = arr.size() - 1; i >= 0; --i) {
-            if (!decreasing_dq.empty() && decreasing_dq.front() - i == d + 1) {
-                decreasing_dq.pop_front();
-            }
-            while (!decreasing_dq.empty() && arr[decreasing_dq.back()] < arr[i]) {
-                if (!right[i].empty() && arr[right[i].back()] != arr[decreasing_dq.back()]) {
-                    right[i].clear();
-                }
-                right[i].emplace_back(decreasing_dq.back());
-                decreasing_dq.pop_back();
-            }
-            decreasing_dq.emplace_back(i);
-        }
-
-        int result = 0;
-        vector<int> lookup(arr.size());
-        for (int i = 0; i < arr.size(); ++i) {
-            result = max(result, dp(arr, d, i, left, right, &lookup));
-        }
-        return result;
-    }
-
-private:
-    int dp(const vector<int>& arr, int d, int i,
-           const vector<vector<int>>& left, const vector<vector<int>>& right,
-           vector<int> *lookup) {
-        if ((*lookup)[i]) {
-            return (*lookup)[i];
-        }
-        (*lookup)[i] = 1;
-        for (const auto& j : left[i]) {
-            (*lookup)[i] = max((*lookup)[i], dp(arr, d, j, left, right, lookup) + 1);
-        }
-        for (const auto& j : right[i]) {
-            (*lookup)[i] = max((*lookup)[i], dp(arr, d, j, left, right, lookup) + 1);
-        }
-        return (*lookup)[i];
-    }
 };
