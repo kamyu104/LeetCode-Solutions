fa41de9c3a2631b9fa8c3f01c9f084c9396305d8
kamyu <kamyu104@users.noreply.github.com>
2020-02-03 05:45:48 +0800

    Update jump-game-v.py

--- Python/jump-game-v.py
+++ Python/jump-game-v.py
@@ -1,5 +1,87 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+import itertools
+
+
+# sliding window + top-down dp
+class Solution(object):
+    def maxJumps(self, arr, d):
+        """
+        :type arr: List[int]
+        :type d: int
+        :rtype: int
+        """
+        def dp(arr, d, i, left, right, lookup):
+            if lookup[i]:
+                return lookup[i]
+            lookup[i] = 1
+            for j in itertools.chain(left[i], right[i]):
+                # each dp[j] will be visited at most twice 
+                lookup[i] = max(lookup[i], dp(arr, d, j, left, right, lookup)+1)
+            return lookup[i]
+
+        left, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
+        for i in xrange(len(arr)):
+            if decreasing_dq and i - decreasing_dq[0] == d+1:
+                decreasing_dq.popleft()
+            while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
+                if left[i] and arr[left[i][-1]] != arr[decreasing_dq[-1]]:
+                    left[i] = []
+                left[i].append(decreasing_dq.pop())
+            decreasing_dq.append(i)
+        right, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
+        for i in reversed(xrange(len(arr))):
+            if decreasing_dq and decreasing_dq[0] - i == d+1:
+                decreasing_dq.popleft()
+            while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
+                if right[i] and arr[right[i][-1]] != arr[decreasing_dq[-1]]:
+                    right[i] = []
+                right[i].append(decreasing_dq.pop())
+            decreasing_dq.append(i)
+
+        lookup = [0]*len(arr)
+        return max(itertools.imap(lambda x: dp(arr, d, x, left, right, lookup), xrange(len(arr))))
+
+
 # Time:  O(nlogn)
 # Space: O(n)
+# mono stack + bottom-up dp
+class Solution2(object):
+    def maxJumps(self, arr, d):
+        """
+        :type arr: List[int]
+        :type d: int
+        :rtype: int
+        """
+        left, decreasing_stk = [[] for _ in xrange(len(arr))], []
+        for i in xrange(len(arr)):
+            while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
+                if i - decreasing_stk[-1] <= d:
+                    if left[i] and arr[left[i][-1]] != arr[decreasing_stk[-1]]:
+                        left[i] = []
+                    left[i].append(decreasing_stk[-1])
+                decreasing_stk.pop()
+            decreasing_stk.append(i)
+        right, decreasing_stk = [[] for _ in xrange(len(arr))], []
+        for i in reversed(xrange(len(arr))):
+            while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
+                if decreasing_stk[-1] - i <= d:
+                    if right[i] and arr[right[i][-1]] != arr[decreasing_stk[-1]]:
+                        right[i] = []
+                    right[i].append(decreasing_stk[-1])
+                decreasing_stk.pop()
+            decreasing_stk.append(i)
+
+        dp = [0]*len(arr)
+        for a, i in sorted([a, i] for i, a in enumerate(arr)):
+            dp[i] = 1
+            for j in itertools.chain(left[i], right[i]):
+                # each dp[j] will be visited at most twice 
+                dp[i] = max(dp[i], dp[j]+1)
+        return max(dp)
+
 
 # Template:
 # https://github.com/kamyu104/FacebookHackerCup-2018/blob/master/Final%20Round/the_claw.py
@@ -81,8 +163,10 @@ class SegmentTree(object):
         return ",".join(map(str, showList))
 
 
+# Time:  O(nlogn)
+# Space: O(n)
 # mono stack + bottom-up dp + segment tree
-class Solution(object):
+class Solution3(object):
     def maxJumps(self, arr, d):
         """
         :type arr: List[int]
@@ -108,85 +192,3 @@ class Solution(object):
         for _, i in sorted([x, i] for i, x in enumerate(arr)):
             segment_tree.update(i, i, segment_tree.query(left[i], right[i]) + 1)
         return segment_tree.query(0, len(arr)-1)
-
-
-# Time:  O(max(nlogn, n * d))
-# Space: O(n)
-# mono stack + bottom-up dp
-class Solution2(object):
-    def maxJumps(self, arr, d):
-        """
-        :type arr: List[int]
-        :type d: int
-        :rtype: int
-        """
-        left, decreasing_stk = [[] for _ in xrange(len(arr))], []
-        for i in xrange(len(arr)):
-            while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
-                if i - decreasing_stk[-1] <= d:
-                    if left[i] and arr[left[i][-1]] != arr[decreasing_stk[-1]]:
-                        left[i] = []
-                    left[i].append(decreasing_stk[-1])
-                decreasing_stk.pop()
-            decreasing_stk.append(i)
-        right, decreasing_stk = [[] for _ in xrange(len(arr))], []
-        for i in reversed(xrange(len(arr))):
-            while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
-                if decreasing_stk[-1] - i <= d:
-                    if right[i] and arr[right[i][-1]] != arr[decreasing_stk[-1]]:
-                        right[i] = []
-                    right[i].append(decreasing_stk[-1])
-                decreasing_stk.pop()
-            decreasing_stk.append(i)
-
-        dp = [0]*len(arr)
-        for a, i in sorted([a, i] for i, a in enumerate(arr)):
-            dp[i] = 1
-            for j in itertools.chain(left[i], right[i]):
-                dp[i] = max(dp[i], dp[j]+1)
-        return max(dp)
-
-
-# Time:  O(n) ~ O(n * d), if all elements of arr are unique, it could be O(n)
-# Space: O(n) 
-import collections
-import itertools
-
-
-# sliding window + top-down dp
-class Solution3(object):
-    def maxJumps(self, arr, d):
-        """
-        :type arr: List[int]
-        :type d: int
-        :rtype: int
-        """
-        def dp(arr, d, i, left, right, lookup):
-            if lookup[i]:
-                return lookup[i]
-            lookup[i] = 1
-            for j in itertools.chain(left[i], right[i]):
-                lookup[i] = max(lookup[i], dp(arr, d, j, left, right, lookup)+1)
-            return lookup[i]
-
-        left, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
-        for i in xrange(len(arr)):
-            if decreasing_dq and i - decreasing_dq[0] == d+1:
-                decreasing_dq.popleft()
-            while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
-                if left[i] and arr[left[i][-1]] != arr[decreasing_dq[-1]]:
-                    left[i] = []
-                left[i].append(decreasing_dq.pop())
-            decreasing_dq.append(i)
-        right, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
-        for i in reversed(xrange(len(arr))):
-            if decreasing_dq and decreasing_dq[0] - i == d+1:
-                decreasing_dq.popleft()
-            while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
-                if right[i] and arr[right[i][-1]] != arr[decreasing_dq[-1]]:
-                    right[i] = []
-                right[i].append(decreasing_dq.pop())
-            decreasing_dq.append(i)
-
-        lookup = [0]*len(arr)
-        return max(itertools.imap(lambda x: dp(arr, d, x, left, right, lookup), xrange(len(arr))))
