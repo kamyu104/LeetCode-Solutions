08748a303c664415b20de6e764a8917715ac4e25
kamyu <kamyu104@users.noreply.github.com>
2020-02-03 07:13:49 +0800

    Update the-k-weakest-rows-in-a-matrix.cpp

--- C++/the-k-weakest-rows-in-a-matrix.cpp
+++ C++/the-k-weakest-rows-in-a-matrix.cpp
@@ -4,6 +4,39 @@
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        vector<int> result;
+        unordered_set<int> lookup;
+        for (int j = 0; j < mat[0].size(); ++j) {
+            for (int i = 0; i < mat.size(); ++i) {
+                if (mat[i][j] || lookup.count(i)) {
+                    continue;
+                }
+                lookup.emplace(i);
+                result.emplace_back(i);
+                if (!--k) {
+                    return result;
+                }
+            }
+        }
+        for (int i = 0; i < mat.size(); ++i) {
+            if (lookup.count(i)) {
+                continue;
+            }
+            lookup.emplace(i);
+            result.emplace_back(i);
+            if (!--k) {
+                break;
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(k)
+class Solution2 {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         OrderedDict<int, bool> lookup;
         for (int j = 0; j < mat[0].size(); ++j) {
             for (int i = 0; i < mat.size(); ++i) {
@@ -67,7 +100,7 @@ private:
 
 // Time:  O(m * n + klogk)
 // Space: O(m)
-class Solution2 {
+class Solution3 {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         vector<pair<int, int>> nums;
