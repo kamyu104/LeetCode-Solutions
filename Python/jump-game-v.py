a1a90d755e6b2f37fdeb4f1b27af2c1b823f5020
kamyu <kamyu104@users.noreply.github.com>
2020-02-03 04:55:36 +0800

    Update jump-game-v.py

--- Python/jump-game-v.py
+++ Python/jump-game-v.py
@@ -120,27 +120,29 @@ class Solution2(object):
         :type d: int
         :rtype: int
         """
-        left, decreasing_stk = range(len(arr)), []
+        left, decreasing_stk = [[] for _ in xrange(len(arr))], []
         for i in xrange(len(arr)):
             while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
                 if i - decreasing_stk[-1] <= d:
-                    left[i] = decreasing_stk[-1]
+                    if left[i] and arr[left[i][-1]] != arr[decreasing_stk[-1]]:
+                        left[i] = []
+                    left[i].append(decreasing_stk[-1])
                 decreasing_stk.pop()
             decreasing_stk.append(i)
-        right, decreasing_stk = range(len(arr)), []
+        right, decreasing_stk = [[] for _ in xrange(len(arr))], []
         for i in reversed(xrange(len(arr))):
             while decreasing_stk and arr[decreasing_stk[-1]] < arr[i]:
                 if decreasing_stk[-1] - i <= d:
-                    right[i] = decreasing_stk[-1]
+                    if right[i] and arr[right[i][-1]] != arr[decreasing_stk[-1]]:
+                        right[i] = []
+                    right[i].append(decreasing_stk[-1])
                 decreasing_stk.pop()
             decreasing_stk.append(i)
 
         dp = [0]*len(arr)
         for a, i in sorted([a, i] for i, a in enumerate(arr)):
             dp[i] = 1
-            for j in xrange(left[i], right[i]+1):
-                if j == i:
-                    continue
+            for j in itertools.chain(left[i], right[i]):
                 dp[i] = max(dp[i], dp[j]+1)
         return max(dp)
 
@@ -163,25 +165,27 @@ class Solution3(object):
             if lookup[i]:
                 return lookup[i]
             lookup[i] = 1
-            for j in xrange(left[i], right[i]+1):
-                if j == i:
-                    continue
+            for j in itertools.chain(left[i], right[i]):
                 lookup[i] = max(lookup[i], dp(arr, d, j, left, right, lookup)+1)
             return lookup[i]
 
-        left, decreasing_dq = range(len(arr)), collections.deque()
+        left, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
         for i in xrange(len(arr)):
             if decreasing_dq and i - decreasing_dq[0] == d+1:
                 decreasing_dq.popleft()
             while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
-                left[i] = decreasing_dq.pop()
+                if left[i] and arr[left[i][-1]] != arr[decreasing_dq[-1]]:
+                    left[i] = []
+                left[i].append(decreasing_dq.pop())
             decreasing_dq.append(i)
-        right, decreasing_dq = range(len(arr)), collections.deque()
+        right, decreasing_dq = [[] for _ in xrange(len(arr))], collections.deque()
         for i in reversed(xrange(len(arr))):
             if decreasing_dq and decreasing_dq[0] - i == d+1:
                 decreasing_dq.popleft()
             while decreasing_dq and arr[decreasing_dq[-1]] < arr[i]:
-                right[i] = decreasing_dq.pop()
+                if right[i] and arr[right[i][-1]] != arr[decreasing_dq[-1]]:
+                    right[i] = []
+                right[i].append(decreasing_dq.pop())
             decreasing_dq.append(i)
 
         lookup = [0]*len(arr)
