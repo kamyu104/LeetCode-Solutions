e736a7dd64b762e0999d5114ef94a1cc3ae734d6
kamyu <kamyu104@users.noreply.github.com>
2020-02-03 06:44:32 +0800

    Update the-k-weakest-rows-in-a-matrix.cpp

--- C++/the-k-weakest-rows-in-a-matrix.cpp
+++ C++/the-k-weakest-rows-in-a-matrix.cpp
@@ -1,7 +1,73 @@
-// Time:  O(m * n + klogk)
-// Space: O(m)
+// Time:  O(m * n)
+// Space: O(k)
 
 class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        OrderedDict<int, bool> lookup;
+        for (int j = 0; j < mat[0].size(); ++j) {
+            for (int i = 0; i < mat.size(); ++i) {
+                if (mat[i][j] || lookup.count(i)) {
+                    continue;
+                }
+                lookup[i] = true;
+                if (!--k) {
+                    return lookup.keys();
+                }
+            }
+        }
+        for (int i = 0; i < mat.size(); ++i) {
+            if (lookup.count(i)) {
+                continue;
+            }
+            lookup[i] = true;
+            if (!--k) {
+                return lookup.keys();
+            }
+        }
+        return {};
+    }
+
+private:
+    template<typename K, typename V>
+    class OrderedDict {
+    public:
+        bool count(const K& key) const {
+            return map_.count(key);
+        }
+        
+        V& operator[](const K& key) {
+            if (!map_.count(key)) {
+                list_.emplace_front();
+                list_.begin()->first = key;
+                map_[key] = list_.begin();
+            }
+            return map_[key]->second;
+        }
+
+        void popitem() {
+            auto del = list_.front(); list_.pop_front();
+            map_.erase(del.first);
+        }
+        
+        vector<K> keys() const {
+            vector<K> result;
+            for (const auto& [k, _] : list_) {
+                result.emplace_back(k);
+            }
+            reverse(result.begin(), result.end());
+            return result;
+        }
+    
+    private:
+        list<pair<K, V>> list_;
+        unordered_map<K, typename list<pair<K, V>>::iterator> map_;
+    };
+};
+
+// Time:  O(m * n + klogk)
+// Space: O(m)
+class Solution2 {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
         vector<pair<int, int>> nums;
