e39ef6cffef31a44dbfc7a2333c3b69598661aa1
kamyu <kamyu104@users.noreply.github.com>
2020-02-04 09:13:18 +0800

    Update number-of-transactions-per-visit.sql

--- MySQL/number-of-transactions-per-visit.sql
+++ MySQL/number-of-transactions-per-visit.sql
@@ -1,10 +1,10 @@
-# Time:  O(m + n + rlogr)  
+# Time:  O(m + n)  
 # Space: O(m + n)  
 
 SELECT seq.transactions_count AS transactions_count,
        Ifnull(v.visits_count, 0) AS visits_count 
-FROM   (SELECT @max_count := CAST(@max_count - 1 AS SIGNED) AS transactions_count 
-        FROM   (SELECT @max_count := CAST(Ifnull(Max(count), 0) + 1 AS SIGNED)
+FROM   (SELECT @count := CAST(@count + 1 AS SIGNED) AS transactions_count 
+        FROM   (SELECT @count := -1, @max_count := Ifnull(Max(count), 0)
                 FROM   (SELECT Count(1) AS count 
                         FROM   transactions 
                         GROUP  BY user_id, 
@@ -15,8 +15,8 @@ FROM   (SELECT @max_count := CAST(@max_count - 1 AS SIGNED) AS transactions_coun
                            UNION ALL 
                            SELECT user_id 
                            FROM   transactions) AS m_n
-        WHERE  @max_count > 0 
-        ORDER  BY transactions_count) AS seq
+        WHERE  @count < @max_count
+        ) AS seq
        LEFT JOIN (SELECT transactions_count, 
                          Count(1) AS visits_Count 
                   FROM   (SELECT Count(transaction_date) AS transactions_count 
