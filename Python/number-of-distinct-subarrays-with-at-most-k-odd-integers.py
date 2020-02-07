5df7ee0e6c5e441494882a14082aad058b29ceb8
Allen Liu <kamyu104@gmail.com>
2020-02-08 03:40:50 +0800

    update

--- Python/number-of-distinct-subarrays-with-at-most-k-odd-integers.py
+++ Python/number-of-distinct-subarrays-with-at-most-k-odd-integers.py
@@ -13,7 +13,7 @@ class Solution(object):
                     result += 1
                 trie = trie[A[i]]
             return result
-        
+
         _trie = lambda: collections.defaultdict(_trie)
         trie = _trie()
         result, left, count = 0, 0, 0
@@ -22,5 +22,31 @@ class Solution(object):
             while count > K:
                 count -= A[left]%2
                 left += 1
+            result += countDistinct(A, left, right, trie)
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(t), t is the size of trie
+class Solution2(object):
+    def distinctSubarraysWithAtMostKOddIntegers(self, A, K):
+        def countDistinct(A, left, right, trie):  # Time: O(n), Space: O(t)
+            result = 0
+            for i in xrange(left, right+1):
+                if A[i] not in trie:
+                    result += 1
+                trie = trie[A[i]]
+            return result
+
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        result = 0
+        for left in xrange(len(A)):
+            count = 0
+            for right in xrange(left, len(A)):
+                count += A[right]%2
+                if count > K:
+                    right -= 1
+                    break
             result += countDistinct(A, left, right, trie)
         return result
