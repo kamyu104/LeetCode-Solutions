2e734fd05cb30a19a37793a03d22be5662c42203
kamyu <kamyu104@users.noreply.github.com>
2020-02-09 20:52:51 +0800

    Create minimum-number-of-steps-to-make-two-strings-anagram.py

---
+++ Python/minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,12 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minSteps(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: int
+        """
+        diff = collections.Counter(s) - collections.Counter(t)
+        return sum(diff.itervalues())
