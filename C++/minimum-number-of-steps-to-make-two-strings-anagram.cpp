cf188b09d0f017ffa94dd83728e4e024611d6d58
kamyu <kamyu104@users.noreply.github.com>
2020-02-09 20:52:28 +0800

    Create minimum-number-of-steps-to-make-two-strings-anagram.cpp

---
+++ C++/minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        const auto& count1 = counter(s);
+        const auto& count2 = counter(t);
+        int result = 0;
+        for (int i = 0; i < count1.size(); ++i) {
+            result += max(count1[i] - count2[i], 0);
+        }
+        return result;
+    }
+    
+private:
+    vector<int> counter(const string& s) const {
+        vector<int> count(26);
+        for (const auto& c : s) {
+            if (isalpha(c)) {
+                ++count[tolower(c) - 'a'];
+            }
+        }
+        return count;
+    }
+};
