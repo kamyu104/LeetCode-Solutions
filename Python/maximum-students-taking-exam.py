5f1cf6d7090802fd9871a364a51ccb902141c18c
kamyu <kamyu104@users.noreply.github.com>
2020-02-09 18:21:12 +0800

    Update maximum-students-taking-exam.py

--- Python/maximum-students-taking-exam.py
+++ Python/maximum-students-taking-exam.py
@@ -151,3 +151,34 @@ class Solution2(object):
                 if seats[i][j] == '.':
                     count += 1
         return count - Hungarian(seats)
+
+
+# Time:  O(m * 2^n * 2^n) = O(m * 4^n)
+# Space: O(2^n)
+# dp solution
+class Solution3(object):
+    def maxStudents(self, seats):
+        """
+        :type seats: List[List[str]]
+        :rtype: int
+        """
+        def popcount(n):
+            result = 0
+            while n:
+                n &= n - 1
+                result += 1
+            return result
+        
+        dp = {0: 0}
+        for row in seats:
+            invalid_mask = sum(1 << c for c, v in enumerate(row) if v == '#')
+            new_dp = {}
+            for mask1, v1 in dp.iteritems():
+                for mask2 in xrange(1 << len(seats[0])):
+                    if (mask2 & invalid_mask) or \
+                       (mask2 & (mask1 << 1)) or (mask2 & (mask1 >> 1)) or \
+                       (mask2 & (mask2 << 1)) or (mask2 & (mask2 >> 1)):
+                        continue
+                    new_dp[mask2] = max(new_dp.get(mask2, 0), v1+popcount(mask2))
+            dp = new_dp
+        return max(dp.itervalues()) if dp else 0
