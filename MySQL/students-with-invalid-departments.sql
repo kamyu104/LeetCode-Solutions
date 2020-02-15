5bef146db398db97c0697fa2c9eb0a90043bd0c9
kamyu <kamyu104@users.noreply.github.com>
2020-02-15 18:55:31 +0800

    Create students-with-invalid-departments.sql

---
+++ MySQL/students-with-invalid-departments.sql
@@ -0,0 +1,17 @@
+# Time:  O(n) 
+# Space: O(n) 
+SELECT s.id, 
+       s.name 
+FROM   students s 
+       LEFT JOIN departments d 
+              ON s.department_id = d.id 
+WHERE  d.id IS NULL; 
+
+# Time:  O(n) 
+# Space: O(n) 
+SELECT s.id, 
+       s.name 
+FROM   students s
+WHERE  NOT EXISTS (SELECT id 
+                   FROM   departments d
+                   WHERE  d.id = s.department_id); 
