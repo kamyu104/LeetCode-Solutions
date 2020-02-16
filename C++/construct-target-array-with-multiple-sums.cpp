28df685b44aa5327940adfae6f9a8f82b3216562
kamyu <kamyu104@users.noreply.github.com>
2020-02-16 19:23:19 +0800

    Create construct-target-array-with-multiple-sums.cpp

---
+++ C++/construct-target-array-with-multiple-sums.cpp
@@ -0,0 +1,28 @@
+// Time:  O(log(max(t)) * logn)
+// Space: O(n)
+
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        // (1) x + remain = y
+        // (2) y + remain = total
+        // (1) - (2) => x - y = y - total
+        //           => x = 2*y - total
+        auto total = accumulate(begin(target), end(target), 0ll);
+        priority_queue<int> max_heap(target.cbegin(), target.cend());
+        while (total != target.size()) {
+            const auto y = max_heap.top(); max_heap.pop();
+            const auto& remain = total - y;
+            auto x = y - remain;
+            if (x <= 0) {
+                return false;
+            }
+            if (x > remain) {  // for case [1, 1000000000]
+                x = x % remain + remain;
+            }
+            max_heap.emplace(x);
+            total = x + remain;
+        }
+        return true;
+    }
+};
