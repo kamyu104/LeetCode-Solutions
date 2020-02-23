ee3e9f40b62ca0cadbff4700122fe8210bca08cc
kamyu <kamyu104@users.noreply.github.com>
2020-02-23 22:26:48 +0800

    Create validate-binary-tree-nodes.py

---
+++ Python/validate-binary-tree-nodes.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(h)
+
+class Solution(object):
+    def validateBinaryTreeNodes(self, n, leftChild, rightChild):
+        """
+        :type n: int
+        :type leftChild: List[int]
+        :type rightChild: List[int]
+        :rtype: bool
+        """
+        roots = set(range(n)) - set(leftChild) - set(rightChild)
+        if len(roots) != 1:
+            return False
+        root, = roots
+        stk = [root]
+        lookup = set([root])
+        while stk:
+            node = stk.pop()
+            for c in (leftChild[node], rightChild[node]):
+                if c < 0:
+                    continue
+                if c in lookup:
+                    return False
+                lookup.add(c)
+                stk.append(c)
+        return len(lookup) == n
