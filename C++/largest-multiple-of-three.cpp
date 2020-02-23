e3ff408d7790d001d3e05073327331388f6f08ad
kamyu <kamyu104@users.noreply.github.com>
2020-02-24 05:24:27 +0800

    Update largest-multiple-of-three.cpp

--- C++/largest-multiple-of-three.cpp
+++ C++/largest-multiple-of-three.cpp
@@ -10,8 +10,8 @@ public:
             {2, {{2}, {5}, {8}, {1, 1}, {1, 4}, {4, 4}, {1, 7}, {4, 7}, {7, 7}}}
         };
         unordered_map<int, int> count = counter(digits);
-        const auto& total = accumulate(cbegin(digits), cend(digits), 0);
-        for (const auto& deletes : lookup.at(total % 3)) {
+        const auto& r = accumulate(cbegin(digits), cend(digits), 0) % 3;
+        for (const auto& deletes : lookup.at(r)) {
             if (remove(deletes, &count)) {
                 break;
             }
@@ -53,12 +53,12 @@ class Solution2 {
 public:
     string largestMultipleOfThree(vector<int>& digits) {
         unordered_map<int, int> count = counter(digits);
-        const auto& total = accumulate(cbegin(digits), cend(digits), 0);
-        if (total % 3) {
+        const auto& r = accumulate(cbegin(digits), cend(digits), 0) % 3;
+        if (r) {
             bool is_found = false;
             for (int i = 0; i < 10; ++i) {
                 vector<int> deletes = {i};
-                if (remove(deletes, total, &count)) {
+                if (remove(deletes, r, &count)) {
                     is_found = true;
                     break;
                 }
@@ -67,7 +67,7 @@ public:
                 for (int i = 0; i < 10; ++i) {
                     for (int j = 0; j <= i; ++j) {
                         vector<int> deletes = {i, j};
-                        if (remove(deletes, total, &count)) {
+                        if (remove(deletes, r, &count)) {
                             break;
                         }
                     }
@@ -82,9 +82,9 @@ public:
     }
 
 private:
-    bool remove(const vector<int>& deletes, int total, unordered_map<int, int> *count) {
+    bool remove(const vector<int>& deletes, int r, unordered_map<int, int> *count) {
         const auto& delete_count = counter(deletes);
-        if (accumulate(cbegin(deletes), cend(deletes), 0) % 3 == total % 3 &&
+        if (accumulate(cbegin(deletes), cend(deletes), 0) % 3 == r &&
             all_of(cbegin(delete_count), cend(delete_count),
                    [&count](const auto& kvp) {
                        return (*count)[kvp.first] >= kvp.second;
