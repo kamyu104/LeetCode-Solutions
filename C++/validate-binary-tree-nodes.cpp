3230751ba1665a9dbe65e0ef0f6e0c76bcbc1591
kamyu <kamyu104@users.noreply.github.com>
2020-02-23 22:26:19 +0800

    Create validate-binary-tree-nodes.cpp

---
+++ C++/validate-binary-tree-nodes.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n)
+// Space: O(h)
+
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        unordered_set<int> roots;
+        for (int i = 0; i < n; ++i) {
+            roots.emplace(i);
+        }
+        for (const auto& i : leftChild) {
+            roots.erase(i);
+        }
+        for (const auto& i : rightChild) {
+            roots.erase(i);
+        }
+        if (roots.size() != 1) {
+            return false;
+        }
+        const auto& root = *cbegin(roots);
+        vector<int> stk({root});
+        unordered_set<int> lookup({root});
+        while (!stk.empty()) {
+            const auto node = stk.back(); stk.pop_back();
+            for (const auto& i : {leftChild[node], rightChild[node]}) {
+                if (i < 0) {
+                    continue;
+                }
+                if (lookup.count(i)) {
+                    return false;
+                }
+                lookup.emplace(i);
+                stk.emplace_back(i);
+            }
+        }
+        return true;
+    }
+};
