f40c86b9ebea4ff524ab93f3c22d3cb9b733f561
kamyu <kamyu104@users.noreply.github.com>
2020-02-23 22:29:30 +0800

    Create count-all-valid-pickup-and-delivery-options.py

---
+++ Python/count-all-valid-pickup-and-delivery-options.py
@@ -0,0 +1,15 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def countOrders(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        result = 1
+        for i in reversed(xrange(2, 2*n+1, 2)):
+            print i
+            result = result * i*(i-1)//2 % MOD
+        return result
