c481f90f1d9e97af35fda0b52de285bc200f325d
kamyu <kamyu104@users.noreply.github.com>
2020-03-01 20:49:19 +0800

    Update minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py

--- Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -4,6 +4,7 @@
 import collections
 
 
+# A* Search Algorithm without heap
 class Solution(object):
     def minCost(self, grid):
         """
@@ -11,22 +12,58 @@ class Solution(object):
         :rtype: int
         """
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+        def a_star(grid, b, t):
+            R, C = len(grid), len(grid[0])
+            f, dh = 0, 1
+            closer, detour = [(b, 0)], []
+            lookup = {(0, 0): 0}
+            while closer or detour:
+                if not closer:
+                    f += dh
+                    closer, detour = detour, closer
+                b, d = closer.pop()
+                if b == t:
+                    return f
+                if b in lookup and lookup[b] < d:
+                    continue
+                for nd, (dr, dc) in enumerate(directions, 1):
+                    nb = (b[0]+dr, b[1]+dc)
+                    cost = 1-(nd == grid[b[0]][b[1]])
+                    if not (0 <= nb[0] < R and 0 <= nb[1] < C and
+                            (nb not in lookup or lookup[nb] > d+cost)):
+                        continue
+                    lookup[nb] = d+cost
+                    (closer if not cost else detour).append(((nb, d+cost)))
+            return -1
+
+        return a_star(grid, (0, 0), (len(grid)-1, len(grid[0])-1))
+
+    
+# Time:  O(m * n)
+# Space: O(m * n)
+class Solution2(object):
+    def minCost(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         R, C = len(grid), len(grid[0])
-        dq = collections.deque([(0, 0, 0)])
+        dq = collections.deque([((0, 0), 0)])
         lookup = {(0, 0): 0}
         while dq:
-            d, r, c = dq.popleft()
-            if r == R-1 and c == C-1:
+            b, d = dq.popleft()
+            if b == (R-1, C-1):
                 return d
             for nd, (dr, dc) in enumerate(directions, 1):
-                nr, nc = r+dr, c+dc
-                cost = 1-(nd == grid[r][c])
-                if not (0 <= nr < R and 0 <= nc < C and
-                        ((nr, nc) not in lookup or d+cost < lookup[nr, nc])):
+                nb = (b[0]+dr, b[1]+dc)
+                cost = 1-(nd == grid[b[0]][b[1]])
+                if not (0 <= nb[0] < R and 0 <= nb[1] < C and
+                        (nb not in lookup or lookup[nb] > d+cost)):
                     continue
-                lookup[nr, nc] = d+cost
+                lookup[nb] = d+cost
                 if cost:
-                    dq.append((d+cost, nr, nc))
+                    dq.append((nb, d+cost))
                 else:
-                    dq.appendleft((d, nr, nc))
+                    dq.appendleft((nb, d))
         return -1  # never reach here
