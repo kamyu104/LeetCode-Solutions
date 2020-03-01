62292f87413bb1d0b34687ec6f31bd7b5c0f6dd3
kamyu <kamyu104@users.noreply.github.com>
2020-03-01 20:59:28 +0800

    Update minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp

--- C++/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ C++/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,7 +1,58 @@
 // Time:  O(m * n)
 // Space: O(m * n)
 
+// A* Search Algorithm without heap
 class Solution {
+public:
+    int minCost(vector<vector<int>>& grid) {
+        pair<int, int> b = {0, 0};
+        pair<int, int> t = {grid.size() - 1, grid[0].size() - 1}; 
+        return a_star(grid, b, t);
+    }
+
+private:
+    int a_star(const vector<vector<int>>& grid,
+               const pair<int, int>& b,
+               const pair<int, int>& t) {
+
+        static const vector<tuple<int, int, int>> directions = {{1, 0, 1}, {2, 0, -1},
+                                                                {3, 1, 0}, {4, -1, 0}};
+        int f = 0, dh = 1;
+        vector<pair<pair<int, int>, int>> closer{{b, 0}}, detour;
+        unordered_map<int, int> lookup;
+        while (!closer.empty() || !detour.empty()) {
+            if (closer.empty()) {
+                f += dh;
+                swap(closer, detour);
+            }
+            const auto [b, d] = closer.back(); closer.pop_back();
+            if (b == t) {
+                return f;
+            }
+            for (const auto& [nd, dr, dc] : directions) {
+                const pair<int, int>& nb = {b.first + dr, b.second + dc};
+                const auto& cost = 1 - (nd == grid[b.first][b.second]);
+                if (!(0 <= nb.first && nb.first < grid.size() &&
+                      0 <= nb.second && nb.second < grid[0].size() &&
+                      (!lookup.count(nb.first * grid[0].size() + nb.second) ||
+                       lookup[nb.first * grid[0].size() + nb.second] > d + cost))) {
+                    continue;
+                }
+                lookup[nb.first * grid[0].size() + nb.second] = d + cost;
+                if (!cost) {
+                    closer.emplace_back(nb, d);
+                } else {
+                    detour.emplace_back(nb, d + cost);
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(m * n)
+class Solution2 {
 public:
     int minCost(vector<vector<int>>& grid) {
         static const vector<tuple<int, int, int>> directions = {{1, 0, 1}, {2, 0, -1},
@@ -18,7 +69,7 @@ public:
                 const auto& cost = 1 - (nd == grid[r][c]);
                 if (!(0 <= nr && nr < grid.size() && 0 <= nc && nc < grid[0].size() &&
                       (!lookup.count(nr * grid[0].size() + nc) ||
-                       d + cost < lookup[nr * grid[0].size() + nc]))) {
+                       lookup[nr * grid[0].size() + nc] > d + cost))) {
                     continue;
                 }
                 lookup[nr * grid[0].size() + nc] = d + cost;
