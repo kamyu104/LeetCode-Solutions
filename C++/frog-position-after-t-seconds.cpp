1a3b52b20dfd5f81c4e37123209776a94c70090c
kamyu <kamyu104@users.noreply.github.com>
2020-03-08 21:20:38 +0800

    Update frog-position-after-t-seconds.cpp

--- C++/frog-position-after-t-seconds.cpp
+++ C++/frog-position-after-t-seconds.cpp
@@ -5,32 +5,27 @@ class Solution {
 public:
     double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
         unordered_map<int, vector<int>> G;
-        G[ROOT] = {};
+        G[1] = {};
         for (const auto& edge : edges) {
             G[edge[0]].emplace_back(edge[1]);
             G[edge[1]].emplace_back(edge[0]);
         }
-        unordered_set<int> lookup;
-        return dfs(G, ROOT, t, target, &lookup);
+        return dfs(G, 1, t, target, 0);
     }
 
 private:
     double dfs(const unordered_map<int, vector<int>>& G,
-               int node, int t, int target,
-               unordered_set<int> *lookup) {
-        if (!(G.at(node).size() - int(node != ROOT)) || !t) {
+               int node, int t, int target, int parent) {
+        if (!(G.at(node).size() - int(parent != 0)) || !t) {
             return (node == target);
         }
-        lookup->emplace(node);
         double result = 0.0;
         for (const auto& child : G.at(node)) {
-            if (lookup->count(child)) {
+            if (child == parent) {
                 continue;
             }
-            result += dfs(G, child, t - 1, target, lookup);
+            result += dfs(G, child, t - 1, target, node);
         }
-        return result / (G.at(node).size() - int(node != ROOT));
+        return result / (G.at(node).size() - int(parent != 0));
     }
-    
-    const int ROOT = 1;
 };
