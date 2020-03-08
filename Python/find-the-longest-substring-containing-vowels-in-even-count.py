5507f8d4ce9eed758589dffe8caec47088e7d740
kamyu <kamyu104@users.noreply.github.com>
2020-03-08 19:07:33 +0800

    Create find-the-longest-substring-containing-vowels-in-even-count.py

---
+++ Python/find-the-longest-substring-containing-vowels-in-even-count.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def findTheLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        VOWELS = "aeiou"
+        result, mask, lookup = 0, 0, [None]*(2**len(VOWELS))
+        lookup[0] = -1
+        for i, c in enumerate(s):
+            index = VOWELS.find(c)
+            mask ^= (1 << index) if index >= 0 else 0
+            if lookup[mask] is None:
+                lookup[mask] = i
+            result = max(result, i-lookup[mask])
+        return result
