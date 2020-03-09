a4e9f374b2119555e2a6975ea1cb06937f0b5020
kamyu <kamyu104@users.noreply.github.com>
2020-03-09 12:32:58 +0800

    Update frog-position-after-t-seconds.py

--- Python/frog-position-after-t-seconds.py
+++ Python/frog-position-after-t-seconds.py
@@ -4,7 +4,75 @@
 import collections
 
 
+# bfs solution with better precision
 class Solution(object):
+    def frogPosition(self, n, edges, t, target):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type t: int
+        :type target: int
+        :rtype: float
+        """                
+        G = collections.defaultdict(list)
+        for u, v in edges:
+            G[u].append(v)
+            G[v].append(u)
+
+        stk = [(t, 1, 0, 1)]
+        while stk:
+            new_stk = []
+            while stk:
+                t, node, parent, choices = stk.pop()
+                if not t or not (len(G[node])-(parent != 0)):
+                    if node == target:
+                        return 1.0/choices
+                    continue
+                for child in G[node]:
+                    if child == parent:
+                        continue
+                    new_stk.append((t-1, child, node,
+                                    choices*(len(G[node])-(parent != 0))))
+            stk = new_stk
+        return 0.0
+
+
+# Time:  O(n)
+# Space: O(n)
+# dfs solution with stack with better precision
+class Solution2(object):
+    def frogPosition(self, n, edges, t, target):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type t: int
+        :type target: int
+        :rtype: float
+        """                
+        G = collections.defaultdict(list)
+        for u, v in edges:
+            G[u].append(v)
+            G[v].append(u)
+
+        stk = [(t, 1, 0, 1)]
+        while stk:
+            t, node, parent, choices = stk.pop()
+            if not t or not (len(G[node])-(parent != 0)):
+                if node == target:
+                    return 1.0/choices
+                continue
+            for child in G[node]:
+                if child == parent:
+                    continue
+                stk.append((t-1, child, node,
+                            choices*(len(G[node])-(parent != 0))))
+        return 0.0
+
+
+# Time:  O(n)
+# Space: O(n)
+# dfs solution with recursion with better precision
+class Solution3(object):
     def frogPosition(self, n, edges, t, target):
         """
         :type n: int
@@ -35,7 +103,8 @@ class Solution(object):
 
 # Time:  O(n)
 # Space: O(n)
-class Solution2(object):
+# dfs solution with recursion
+class Solution4(object):
     def frogPosition(self, n, edges, t, target):
         """
         :type n: int
