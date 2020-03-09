0dd659b36a545443ec203db66937a5e367a3b718
kamyu <kamyu104@users.noreply.github.com>
2020-03-09 13:28:13 +0800

    Update time-needed-to-inform-all-employees.cpp

--- C++/time-needed-to-inform-all-employees.cpp
+++ C++/time-needed-to-inform-all-employees.cpp
@@ -1,7 +1,38 @@
 // Time:  O(n)
 // Space: O(n)
 
+// dfs solution with stack
 class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        unordered_map<int, vector<int>> children;
+        for (int i = 0; i < manager.size(); ++i) {
+            if (manager[i] != -1) {
+                children[manager[i]].emplace_back(i);
+            }
+        }
+ 
+        int result = 0;
+        vector<pair<int, int>> stk = {{headID, 0}};
+        while (!stk.empty()) {
+            auto [node, curr] = stk.back(); stk.pop_back();
+            curr += informTime[node];
+            result = max(result, curr);
+            if (!children.count(node)) {
+                continue;
+            }
+            for (const auto& child : children.at(node)) {
+                stk.emplace_back(child, curr);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// dfs solution with recursion
+class Solution2 {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
         unordered_map<int, vector<int>> children;
