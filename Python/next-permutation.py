b0100967c1a6a6a36a2635d924871ceb692a26e4
kamyu <kamyu104@users.noreply.github.com>
2020-03-13 23:49:04 +0800

    Update next-permutation.py

--- Python/next-permutation.py
+++ Python/next-permutation.py
@@ -2,22 +2,47 @@
 # Space: O(1)
 
 class Solution(object):
-    # @param {integer[]} nums
-    # @return {void} Do not return anything, modify nums in-place instead.
-    def nextPermutation(self, num):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
         k, l = -1, 0
-        for i in xrange(len(num) - 1):
-            if num[i] < num[i + 1]:
+        for i in reversed(xrange(len(nums)-1)):
+            if nums[i] < nums[i+1]:
+                k = i
+                break
+        else:
+            nums.reverse()
+            return
+
+        for i in reversed(xrange(k+1, len(nums))):
+            if nums[i] > nums[k]:
+                l = i
+                break
+        nums[k], nums[l] = nums[l], nums[k]
+        nums[k+1:] = nums[:k:-1]
+        
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        k, l = -1, 0
+        for i in xrange(len(nums)-1):
+            if nums[i] < nums[i+1]:
                 k = i
 
         if k == -1:
-            num.reverse()
+            nums.reverse()
             return
 
-        for i in xrange(k + 1, len(num)):
-            if num[i] > num[k]:
+        for i in xrange(k+1, len(nums)):
+            if nums[i] > nums[k]:
                 l = i
-
-        num[k], num[l] = num[l], num[k]
-        num[k + 1:] = num[:k:-1]
-
+        nums[k], nums[l] = nums[l], nums[k]
+        nums[k+1:] = nums[:k:-1]
