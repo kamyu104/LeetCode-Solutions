db12635c60627bf13b3dcde377dfaa1842296f68
kamyu <kamyu104@users.noreply.github.com>
2020-03-14 13:42:46 +0800

    Create find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py

---
+++ Python/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(h)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def getTargetCopy(self, original, cloned, target):
+        """
+        :type original: TreeNode
+        :type cloned: TreeNode
+        :type target: TreeNode
+        :rtype: TreeNode
+        """
+        def preorder_gen(node):
+            stk = [node]
+            while stk:
+                node = stk.pop()
+                if not node:
+                    continue
+                yield node
+                stk.append(node.right)
+                stk.append(node.left)
+            
+        for node1, node2 in itertools.izip(preorder_gen(original),
+                                           preorder_gen(cloned)):
+            if node1 == target:
+                return node2
