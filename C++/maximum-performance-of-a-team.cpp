1b811737d454e2495770b6fdfaf792d14b9c7a2d
kamyu <kamyu104@users.noreply.github.com>
2020-03-15 16:33:58 +0800

    Create maximum-performance-of-a-team.cpp

---
+++ C++/maximum-performance-of-a-team.cpp
@@ -0,0 +1,25 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        static const int MOD = 1e9 + 7;
+        uint64_t result = 0, s_sum = 0;
+        vector<pair<int, int>> engineers;
+        for (int i = 0; i < speed.size(); ++i) {
+            engineers.emplace_back(efficiency[i], speed[i]);
+        }
+        sort(engineers.begin(), engineers.end(), greater<pair<int, int>>());
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        for (const auto& [e, s] : engineers) {
+            s_sum += s;
+            min_heap.emplace(s);
+            if (min_heap.size() > k) {
+                s_sum -= min_heap.top(); min_heap.pop();
+            }
+            result = max(result, s_sum * e);
+        }
+        return result % MOD;
+    }
+};
