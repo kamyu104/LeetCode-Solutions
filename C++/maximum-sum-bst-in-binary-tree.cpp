d092e1f37451829665db876c49ad3bb30be71468
kamyu <kamyu104@users.noreply.github.com>
2020-03-15 18:39:02 +0800

    Update maximum-sum-bst-in-binary-tree.cpp

--- C++/maximum-sum-bst-in-binary-tree.cpp
+++ C++/maximum-sum-bst-in-binary-tree.cpp
@@ -10,7 +10,48 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// dfs solution with stack
 class Solution {
+public:
+    int maxSumBST(TreeNode* root) {
+        using RET = tuple<bool, int, int, int>;
+        int result = 0;
+        RET ret;
+        vector<tuple<TreeNode*, shared_ptr<vector<RET>>, RET *>> stk = {{root, nullptr, &ret}};
+        while (!stk.empty()) {
+            const auto [node, tmp, ret] = stk.back(); stk.pop_back();
+            if (tmp) {
+                const auto& [lvalid, lsum, lmin, lmax] = (*tmp)[0];
+                const auto& [rvalid, rsum, rmin, rmax] = (*tmp)[1];
+                if (lvalid && rvalid && lmax < node->val && node->val < rmin) {
+                    const auto& total = lsum + node->val + rsum;
+                    result = max(result, total);
+                    *ret = {true, total, min(lmin, node->val), max(node->val, rmax)};
+                } else {
+                    *ret = {false, 0, 0, 0};
+                }
+            } else {
+                if (!node) {
+                    *ret = {true, 0,
+                            numeric_limits<int>::max(),
+                            numeric_limits<int>::min()};
+                    continue;
+                }
+                const auto& tmp = make_shared<vector<RET>>(2);
+                stk.emplace_back(node, tmp, ret);
+                stk.emplace_back(node->right, nullptr, &((*tmp)[1]));
+                stk.emplace_back(node->left, nullptr, &((*tmp)[0]));
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(h)
+// dfs solution with recursion
+class Solution2 {
 public:
     int maxSumBST(TreeNode* root) {
         int result = 0;
