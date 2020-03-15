1050736e0a5b2ebafb4dd6e7b6c81b3a600e58a0
kamyu <kamyu104@users.noreply.github.com>
2020-03-15 16:35:45 +0800

    Create maximum-performance-of-a-team.py

---
+++ Python/maximum-performance-of-a-team.py
@@ -0,0 +1,26 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+import itertools
+import heapq
+
+
+class Solution(object):
+    def maxPerformance(self, n, speed, efficiency, k):
+        """
+        :type n: int
+        :type speed: List[int]
+        :type efficiency: List[int]
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        result, s_sum = 0, 0
+        min_heap = []
+        for e, s in sorted(itertools.izip(efficiency, speed), reverse=True):
+            s_sum += s
+            heapq.heappush(min_heap, s)
+            if len(min_heap) > k:
+                s_sum -= heapq.heappop(min_heap)
+            result = max(result,s_sum*e)
+        return result % MOD
