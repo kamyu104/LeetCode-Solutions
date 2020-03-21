01d61b534e8d783f2797b16d4425c85d447dcb3c
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 04:01:03 +0800

    Update find-the-distance-value-between-two-arrays.py

--- Python/find-the-distance-value-between-two-arrays.py
+++ Python/find-the-distance-value-between-two-arrays.py
@@ -1,27 +1,6 @@
-# Time:  O(nlogn)
+# Time:  O((n + m) * logm)
 # Space: O(1)
 
-class Solution(object):
-    def findTheDistanceValue(self, arr1, arr2, d):
-        """
-        :type arr1: List[int]
-        :type arr2: List[int]
-        :type d: int
-        :rtype: int
-        """
-        arr1.sort(), arr2.sort()
-        result, i, j = 0, 0, 0
-        while i < len(arr1) and j < len(arr2):
-            if arr1[i]-arr2[j] > d:
-                j += 1
-                continue
-            result += arr2[j]-arr1[i] > d
-            i += 1
-        return result+len(arr1)-i
-
-
-# Time:  O(nlogn)
-# Space: O(1)
 import bisect
 
 
@@ -41,3 +20,24 @@ class Solution2(object):
             right = arr2[j] if j < len(arr2) else float("inf")
             result += left+d < x < right-d
         return result
+
+
+# Time:  O(nlogn + mlogm)
+# Space: O(1)
+class Solution2(object):
+    def findTheDistanceValue(self, arr1, arr2, d):
+        """
+        :type arr1: List[int]
+        :type arr2: List[int]
+        :type d: int
+        :rtype: int
+        """
+        arr1.sort(), arr2.sort()
+        result, i, j = 0, 0, 0
+        while i < len(arr1) and j < len(arr2):
+            if arr1[i]-arr2[j] > d:
+                j += 1
+                continue
+            result += arr2[j]-arr1[i] > d
+            i += 1
+        return result+len(arr1)-i
