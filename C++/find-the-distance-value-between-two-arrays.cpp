00c4cef9040485dd21fe5ce000c5b67735e5e354
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 04:02:23 +0800

    Update find-the-distance-value-between-two-arrays.cpp

--- C++/find-the-distance-value-between-two-arrays.cpp
+++ C++/find-the-distance-value-between-two-arrays.cpp
@@ -1,6 +1,23 @@
-// Time:  O(nlogn)
+// Time:  O((n + m) * logm)
 // Space: O(1)
 
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(begin(arr2), end(arr2));
+        int result = 0;
+        for (const auto& x : arr1) {
+            auto j = distance(cbegin(arr2), lower_bound(cbegin(arr2), cend(arr2), x));
+            const auto& left = (j - 1 >= 0) ? arr2[j - 1] : numeric_limits<int>::min();
+            const auto& right = (j < arr2.size()) ? arr2[j] : numeric_limits<int>::max();
+            result += left + d < x && x < right - d; 
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn + mlogm)
+// Space: O(1)
 class Solution2 {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
@@ -15,22 +32,5 @@ public:
             ++i;
         }
         return result + arr1.size() - i;
-    }
-};
-
-// Time:  O(nlogn)
-// Space: O(1)
-class Solution2 {
-public:
-    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
-        sort(begin(arr2), end(arr2));
-        int result = 0;
-        for (const auto& x : arr1) {
-            auto j = distance(cbegin(arr2), lower_bound(cbegin(arr2), cend(arr2), x));
-            const auto& left = (j - 1 >= 0) ? arr2[j - 1] : numeric_limits<int>::min();
-            const auto& right = (j < arr2.size()) ? arr2[j] : numeric_limits<int>::max();
-            result += left + d < x && x < right - d; 
-        }
-        return result;
     }
 };
