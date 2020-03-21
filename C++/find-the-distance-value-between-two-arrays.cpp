35945a690d7be3df5ccde7712a5dcd6af5ec24e4
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 03:57:37 +0800

    Update find-the-distance-value-between-two-arrays.cpp

--- C++/find-the-distance-value-between-two-arrays.cpp
+++ C++/find-the-distance-value-between-two-arrays.cpp
@@ -1,7 +1,7 @@
 // Time:  O(nlogn)
 // Space: O(1)
 
-class Solution {
+class Solution2 {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
         sort(begin(arr1), end(arr1)), sort(begin(arr2), end(arr2));
@@ -15,5 +15,22 @@ public:
             ++i;
         }
         return result + arr1.size() - i;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+class Solution2 {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(begin(arr2), end(arr2));
+        int result = 0;
+        for (const auto& x : arr1) {
+            auto j = distance(cbegin(arr2), lower_bound(cbegin(arr2), cend(arr2), x));
+            const auto& left = (j - 1 >= 0) ? arr2[j - 1] : numeric_limits<int>::min();
+            const auto& right = (j < arr2.size()) ? arr2[j] : numeric_limits<int>::max();
+            result += left + d < x && x < right - d; 
+        }
+        return result;
     }
 };
