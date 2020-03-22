1bf5b5b9c33c367b672e5119529e468315e059b7
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 20:43:52 +0800

    Update four-divisors.py

--- Python/four-divisors.py
+++ Python/four-divisors.py
@@ -1,10 +1,36 @@
 # Time:  O(n * sqrt(n))
-# Space: O(sqrt(n))
+# Space: O(1)
 
+class Solution(object):
+    def sumFourDivisors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        for num in nums:
+            facs, i = [], 1
+            while i*i <= num:
+                if num % i:
+                    i+= 1
+                    continue
+                facs.append(i)
+                if i != num//i:
+                    facs.append(num//i)
+                    if len(facs) > 4:
+                        break
+                i += 1
+            if len(facs) == 4:            
+                result += sum(facs)
+        return result 
+
+
+# Time:  O(n * sqrt(n))
+# Space: O(sqrt(n))
 import itertools
 
 
-class Solution(object):
+class Solution2(object):
     def sumFourDivisors(self, nums):
         """
         :type nums: List[int]
@@ -34,29 +60,3 @@ class Solution(object):
                 p, q = facs[0][0], facs[1][0]
                 result += (1 + p) * (1 + q)
         return result
-
-    
-# Time:  O(n * sqrt(n))
-# Space: O(1)
-class Solution2(object):
-    def sumFourDivisors(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        result = 0
-        for num in nums:
-            facs, i = [], 1
-            while i*i <= num:
-                if num % i:
-                    i+= 1
-                    continue
-                facs.append(i)
-                if i != num//i:
-                    facs.append(num//i)
-                    if len(facs) > 4:
-                        break
-                i += 1
-            if len(facs) == 4:            
-                result += sum(facs)
-        return result 
