1507119d5e0c86f8c6161ea8635da547d4e0d7fb
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 20:25:25 +0800

    Update create-target-array-in-the-given-order.cpp

--- C++/create-target-array-in-the-given-order.cpp
+++ C++/create-target-array-in-the-given-order.cpp
@@ -1,7 +1,26 @@
 // Time:  O(n^2)
-// Space: O(1)
-
+// Space: O(n)
 class Solution {
+public:
+    vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
+        for (int i = 0; i < nums.size(); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (index[j] >= index[i]) {
+                    ++index[j];
+                }
+            }
+        }
+        vector<int> result(nums.size(),0);
+        for (int i = 0; i < nums.size(); ++i){
+            result[index[i]] = nums[i];
+        }
+        return result;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(1)
+class Solution2 {
 public:
     vector<int> createTargetArray(vector<int>& nums, vector<int>& index) {
         vector<int> result;
