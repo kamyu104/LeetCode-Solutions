5989398fe41076876736fc998e5fbec8578ad660
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 20:43:07 +0800

    Update four-divisors.cpp

--- C++/four-divisors.cpp
+++ C++/four-divisors.cpp
@@ -1,7 +1,35 @@
 // Time:  O(n * sqrt(n))
-// Space: O(sqrt(n))
+// Space: O(1)
 
 class Solution {
+public:
+    int sumFourDivisors(vector<int>& nums) {
+        int result = 0;
+        for (const auto& num : nums) {
+            vector<int> facs;
+            for (int i = 1; i * i <= num; ++i) {
+                if (num % i) {
+                    continue;
+                }
+                facs.emplace_back(i);
+                if (i != num / i) {
+                    facs.emplace_back(num / i);
+                    if (facs.size() > 4) {
+                        break;
+                    }
+                }
+            }
+            if (facs.size() == 4) {
+                result += accumulate(cbegin(facs), cend(facs), 0);
+            } 
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * sqrt(n))
+// Space: O(sqrt(n))
+class Solution2 {
 public:
     int sumFourDivisors(vector<int>& nums) {
         int result = 0;
@@ -34,34 +62,6 @@ private:
         }
         if (x > 1) {
             result.emplace_back(x, 1);
-        }
-        return result;
-    }
-};
-
-// Time:  O(n * sqrt(n))
-// Space: O(sqrt(n))
-class Solution2 {
-public:
-    int sumFourDivisors(vector<int>& nums) {
-        int result = 0;
-        for (const auto& num : nums) {
-            vector<int> facs;
-            for (int i = 1; i * i <= num; ++i) {
-                if (num % i) {
-                    continue;
-                }
-                facs.emplace_back(i);
-                if (i != num / i) {
-                    facs.emplace_back(num / i);
-                    if (facs.size() > 4) {
-                        break;
-                    }
-                }
-            }
-            if (facs.size() == 4) {
-                result += accumulate(cbegin(facs), cend(facs), 0);
-            } 
         }
         return result;
     }
