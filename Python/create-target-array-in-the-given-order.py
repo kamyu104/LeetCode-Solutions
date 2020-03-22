596e0fd4fd0f41462b962a0d6941873e9658337f
kamyu <kamyu104@users.noreply.github.com>
2020-03-22 20:23:30 +0800

    Update create-target-array-in-the-given-order.py

--- Python/create-target-array-in-the-given-order.py
+++ Python/create-target-array-in-the-given-order.py
@@ -1,10 +1,29 @@
 # Time:  O(n^2)
 # Space: O(1)
 
+class Solution(object):
+    def createTargetArray(self, nums, index):
+        """
+        :type nums: List[int]
+        :type index: List[int]
+        :rtype: List[int]
+        """
+        for i in xrange(len(nums)):
+            for j in xrange(i):
+                if index[i] <= index[j]:
+                    index[j] += 1
+        result = [0]*(len(nums))
+        for i in xrange(len(nums)):
+            result[index[i]] = nums[i]
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(1)
 import itertools
 
 
-class Solution(object):
+class Solution2(object):
     def createTargetArray(self, nums, index):
         """
         :type nums: List[int]
@@ -15,3 +34,4 @@ class Solution(object):
         for i, x in itertools.izip(index, nums):
             result.insert(i, x)
         return result
+
