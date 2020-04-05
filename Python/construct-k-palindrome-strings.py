919308a07bbb275f830b3081e1c8af0aeaeb0d6c
kamyu <kamyu104@users.noreply.github.com>
2020-04-05 21:10:53 +0800

    Create construct-k-palindrome-strings.py

---
+++ Python/construct-k-palindrome-strings.py
@@ -0,0 +1,13 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def canConstruct(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: bool
+        """
+        count = collections.Counter(s)
+        odd = sum(v%2 for v in count.itervalues())
+        return odd <= k <= len(s)
