491676fdd3ebe85f28ffbba019bc7415165bc767
kamyu <kamyu104@users.noreply.github.com>
2020-04-05 20:23:22 +0800

    Create customers-who-bought-products-a-and-b-but-not-c.sql

---
+++ MySQL/customers-who-bought-products-a-and-b-but-not-c.sql
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(m)
+
+SELECT c.customer_id, 
+       c.customer_name 
+FROM   customers AS c 
+       INNER JOIN (SELECT customer_id, 
+                          SUM(CASE 
+                                WHEN product_name = 'A' THEN 1 
+                                WHEN product_name = 'B' THEN 1 
+                                WHEN product_name = 'C' THEN -1 
+                                ELSE 0 
+                              END) AS total 
+                   FROM   (SELECT DISTINCT customer_id, 
+                                           product_name 
+                           FROM   orders) t 
+                   GROUP  BY customer_id 
+                   HAVING total = 2) AS o 
+               ON c.customer_id = o.customer_id;
