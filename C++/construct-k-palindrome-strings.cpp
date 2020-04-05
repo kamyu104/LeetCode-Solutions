b73f5786299cb87c0d56e8ca1cc8ed4932e7bbc6
kamyu <kamyu104@users.noreply.github.com>
2020-04-05 21:10:21 +0800

    Create construct-k-palindrome-strings.cpp

---
+++ C++/construct-k-palindrome-strings.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        const auto& count = counter(s);
+        const auto& odd = accumulate(cbegin(count), cend(count), 0,
+                              [](const auto& a, const auto& b) {
+                                  return a + b.second % 2;
+                          });
+        return odd <= k && k <= s.length();
+    }
+
+private:
+	unordered_map<int, int> counter(const string& word) {
+        unordered_map<int, int> count;
+        for (const auto& c : word) {
+            ++count[c];
+        }
+        return count;
+    }
+};
