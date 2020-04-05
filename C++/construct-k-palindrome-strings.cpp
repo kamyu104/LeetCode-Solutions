ad875ab33744cd513dc9ebdb869d0c90d3d6a673
kamyu <kamyu104@users.noreply.github.com>
2020-04-05 21:46:28 +0800

    Update construct-k-palindrome-strings.cpp

--- C++/construct-k-palindrome-strings.cpp
+++ C++/construct-k-palindrome-strings.cpp
@@ -4,6 +4,19 @@
 class Solution {
 public:
     bool canConstruct(string s, int k) {
+        bitset<26> odd;
+        for (const auto& c : s) {
+            odd.flip(c - 'a');
+        }
+        return odd.count() <= k && k <= s.length();
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    bool canConstruct(string s, int k) {
         const auto& count = counter(s);
         const auto& odd = accumulate(cbegin(count), cend(count), 0,
                               [](const auto& a, const auto& b) {
