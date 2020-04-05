9d6985591443989f23546de9c8fc438e7c8af9d6
kamyu <kamyu104@users.noreply.github.com>
2020-04-05 21:01:15 +0800

    Update count-largest-group.py

--- Python/count-largest-group.py
+++ Python/count-largest-group.py
@@ -13,5 +13,5 @@ class Solution(object):
         count = collections.Counter()
         for x in xrange(1, n+1):
             count[sum(map(int, str(x)))] += 1
-        m = max(count.itervalues())
-        return sum(v == m for v in count.itervalues())
+        max_count = max(count.itervalues())
+        return sum(v == max_count for v in count.itervalues())
