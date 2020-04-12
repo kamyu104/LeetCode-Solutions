00af92b76a838303bb102f4fe545820d81cae386
kamyu <kamyu104@users.noreply.github.com>
2020-04-12 19:31:33 +0800

    Update climbing-stairs.py

--- Python/climbing-stairs.py
+++ Python/climbing-stairs.py
@@ -27,7 +27,7 @@ class Solution(object):
 
         T = [[1, 1],
              [1, 0]]
-        return matrix_expo(T, n)[0][0]
+        return matrix_mult([[1,  1]], matrix_expo(T, n-1))[0][0]  # [a1, a0] * T^(n-1)
 
 
 # Time:  O(n)
