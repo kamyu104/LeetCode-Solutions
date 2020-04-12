2cd4657522de55cf8bd9f06c28316f513379ffc1
kamyu <kamyu104@users.noreply.github.com>
2020-04-12 20:47:02 +0800

    Create string-matching-in-an-array.sql

---
+++ MySQL/string-matching-in-an-array.sql
@@ -0,0 +1,12 @@
+# Time:  O(nlogn) 
+# Space: O(n) 
+
+SELECT name, 
+       Ifnull(Sum(distance), 0) AS travelled_distance 
+FROM   users 
+       LEFT JOIN rides 
+              ON users.id = rides.user_id 
+GROUP  BY name 
+ORDER  BY travelled_distance DESC, 
+          name; 
+          
