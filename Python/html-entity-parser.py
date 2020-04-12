94ac1e68ca0cf3196c53153623541a83f3771f91
kamyu <kamyu104@users.noreply.github.com>
2020-04-12 18:07:21 +0800

    Update html-entity-parser.py

--- Python/html-entity-parser.py
+++ Python/html-entity-parser.py
@@ -91,3 +91,31 @@ class Solution(object):
                 i += len(patterns[positions[j][1]])
                 j += 1
         return "".join(result)
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution(object):
+    def entityParser(self, text):
+        """
+        :type text: str
+        :rtype: str
+        """
+        patterns = ["&quot;", "&apos;", "&amp;", "&gt;", "&lt;", "&frasl;"]
+        chars = ["\"", "'", "&", ">", "<", "/"]
+        result = []
+        i, j = 0, 0
+        while i != len(text):
+            if text[i] != '&':                    
+                result.append(text[i])
+                i += 1
+            else:
+                for j, pattern in enumerate(patterns):
+                    if pattern == text[i:i+len(pattern)]:
+                        result.append(chars[j])
+                        i += len(pattern)
+                        break
+                else:
+                    result.append(text[i])
+                    i += 1
+        return "".join(result)
