4fd3eb57878a69dd50142ba95cedd5e133977fde
kamyu <kamyu104@users.noreply.github.com>
2020-04-12 20:34:15 +0800

    Update string-matching-in-an-array.py

--- Python/string-matching-in-an-array.py
+++ Python/string-matching-in-an-array.py
@@ -84,3 +84,45 @@ class Solution(object):
                     if j != i:
                         lookup.add(j)
         return [words[i] for i in lookup]
+
+
+# Time:  O(n^2 * l), n is the number of strings
+# Space: O(l)      , l is the max length of strings
+class Solution2(object):
+    def stringMatching(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        def getPrefix(pattern):
+            prefix = [-1]*len(pattern)
+            j = -1
+            for i in xrange(1, len(pattern)):
+                while j != -1 and pattern[j+1] != pattern[i]:
+                    j = prefix[j]
+                if pattern[j+1] == pattern[i]:
+                    j += 1
+                prefix[i] = j
+            return prefix
+            
+        def kmp(text, pattern):
+            if not pattern:
+                return 0
+            prefix = getPrefix(pattern)
+            j = -1
+            for i in xrange(len(text)):
+                while j != -1 and pattern[j+1] != text[i]:
+                    j = prefix[j]
+                if pattern[j+1] == text[i]:
+                    j += 1
+                if j+1 == len(pattern):
+                    return i-j
+            return -1
+            
+        result = []
+        for i, pattern in enumerate(words):
+            for j, text in enumerate(words):
+                if i != j and kmp(text, pattern) != -1:
+                    result.append(pattern)
+                    break
+        return result
