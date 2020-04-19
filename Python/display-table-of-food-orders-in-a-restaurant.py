fac3b74532a17c87c0b173d8581294554c8d0799
kamyu <kamyu104@users.noreply.github.com>
2020-04-19 21:37:28 +0800

    Create display-table-of-food-orders-in-a-restaurant.py

---
+++ Python/display-table-of-food-orders-in-a-restaurant.py
@@ -0,0 +1,22 @@
+# Time:  O(n + tlogt + flogf)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def displayTable(self, orders):
+        """
+        :type orders: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        table_count = collections.defaultdict(collections.Counter)
+        for name, table, food in orders:
+            table_count[int(table)][food] += 1
+        foods = sorted({food for _, _, food in orders})
+        result = [["Table"]]
+        result[0].extend(foods)
+        for table in sorted(table_count):
+            result.append([str(table)])
+            result[-1].extend(str(table_count[table][food]) for food in foods)
+        return result
