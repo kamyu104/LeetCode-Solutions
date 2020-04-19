f6a1554b3256a998fe26b7c4b4e72f0c9365751a
kamyu <kamyu104@users.noreply.github.com>
2020-04-19 18:53:12 +0800

    Create reformat-the-string.cpp

---
+++ C++/reformat-the-string.cpp
@@ -0,0 +1,48 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string reformat(string s) {
+        unordered_map<char, int> count;
+        int alpha_cnt = 0;
+        for (const auto& c : s) {
+            ++count[c];
+            if (isalpha(c)) {
+                ++alpha_cnt;
+            }
+        }
+        if (abs(int(s.length()) - 2 * alpha_cnt) > 1) {
+            return "";
+        }
+        
+        char a = 'a', a_end = 'z';
+        char b = '0', b_end = '9';
+        if (alpha_cnt < s.length() - alpha_cnt) {
+            swap(a, b);
+            swap(a_end, b_end);
+        }
+        string result;
+        while (result.length() < s.length()) {
+            a = next_char(a, a_end, &count);
+            b = next_char(b, b_end, &count);
+            result.push_back(a);
+            result.push_back(b);
+        }
+        if (result.back() == '\0') {
+            result.pop_back();
+        }
+        return result;
+    }
+
+private:
+    char next_char(char start, char end, unordered_map<char, int> *count) {
+        for (char c = start; c <= end; ++c) {
+            if ((*count)[c]) {
+                --(*count)[c];
+                return c;
+            }
+        }
+        return '\0';
+    }
+};
