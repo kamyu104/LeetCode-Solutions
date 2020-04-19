63e4340baf4b6a910f5c781e51c84b14bb94ae73
kamyu <kamyu104@users.noreply.github.com>
2020-04-19 17:59:39 +0800

    Create restore-the-array.py

---
+++ Python/restore-the-array.py
@@ -0,0 +1,25 @@
+# Time:  O(nlogk)
+# Space: O(logk)
+
+class Solution(object):
+    def numberOfArrays(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        klen = len(str(k))
+        dp = [0]*(klen+1)
+        dp[len(s)%len(dp)] = 1
+        for i in reversed(xrange(len(s))):
+            dp[i%len(dp)] = 0
+            if s[i] == '0':
+                continue
+            curr = 0
+            for j in xrange(i, min(i+klen, len(s))):
+                curr = 10*curr + int(s[j])
+                if curr > k:
+                    break
+                dp[i%len(dp)] = (dp[i%len(dp)] + dp[(j+1)%len(dp)]) % MOD
+        return dp[0]
