b498f490b0ff07eb00f56f5803d1059c6975c75c
kamyu <kamyu104@users.noreply.github.com>
2020-05-03 20:20:08 +0800

    Create longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py

---
+++ Python/longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.py
@@ -0,0 +1,54 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def longestSubarray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        max_dq, min_dq = collections.deque(), collections.deque()
+        left = 0
+        for right, num in enumerate(nums):
+            while max_dq and nums[max_dq[-1]] <= num:
+                max_dq.pop()
+            max_dq.append(right)
+            while min_dq and nums[min_dq[-1]] >= num:
+                min_dq.pop()
+            min_dq.append(right)
+            if nums[max_dq[0]]-nums[min_dq[0]] > limit:
+                if max_dq[0] == left:
+                    max_dq.popleft()
+                if min_dq[0] == left:
+                    min_dq.popleft()
+                left += 1  # advance left by one to not count in result
+        return len(nums)-left
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def longestSubarray(self, nums, limit):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :rtype: int
+        """
+        max_dq, min_dq = collections.deque(), collections.deque()
+        result, left = 0, 0
+        for right, num in enumerate(nums):
+            while max_dq and nums[max_dq[-1]] <= num:
+                max_dq.pop()
+            max_dq.append(right)
+            while min_dq and nums[min_dq[-1]] >= num:
+                min_dq.pop()
+            min_dq.append(right)
+            while nums[max_dq[0]]-nums[min_dq[0]] > limit:  # both always exist "right" element
+                if max_dq[0] == left:
+                    max_dq.popleft()
+                if min_dq[0] == left:
+                    min_dq.popleft()
+                left += 1
+            result = max(result, right-left+1)
+        return result
