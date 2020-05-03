67b44b9a05805e97127ca1a83dadf27b9ab63717
kamyu <kamyu104@users.noreply.github.com>
2020-05-04 03:02:13 +0800

    Update find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py

--- Python/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py
+++ Python/find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py
@@ -28,3 +28,40 @@ class Solution(object):
         for r in xrange(1, len(mat)):
             result = kSmallestPairs(result, mat[r], k)
         return result[k-1]
+
+
+# Time:  O(k * log(m * MAX_NUM)) ~ O(k * m * log(m * MAX_NUM))
+# Space: O(m)
+class Solution2(object):
+    def kthSmallest(self, mat, k):
+        """
+        :type mat: List[List[int]]
+        :type k: int
+        :rtype: int
+        """        
+        def countArraysHaveSumLessOrEqual(mat, k, r, target):  # Time: O(k * m) ~ O(k * m)
+            if target < 0:
+                return 0
+            if r == len(mat):
+                return 1
+            result = 0
+            for c in xrange(len(mat[0])):
+                cnt = countArraysHaveSumLessOrEqual(mat, k-result, r+1, target-mat[r][c])
+                if not cnt:
+                    break
+                result += cnt
+                if result > k:
+                    break
+            return result
+        
+        MAX_NUM = 5000
+        left, right = len(mat), len(mat)*MAX_NUM
+        while left <= right:
+            mid = left + (right-left)//2
+            cnt = countArraysHaveSumLessOrEqual(mat, k, 0, mid)
+            if cnt >= k:
+                right = mid-1
+            else:
+                left = mid+1
+        return left
+
