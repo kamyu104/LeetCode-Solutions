5e3940e1ef1746560f248bebd4ed5c6b44af2821
kamyu <kamyu104@users.noreply.github.com>
2020-05-03 23:42:48 +0800

    Update check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py

--- Python/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py
+++ Python/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py
@@ -63,11 +63,11 @@ class Solution3(object):
         :type arr: List[int]
         :rtype: bool
         """
-        def dfs(node, depth):
+        def dfs(node, arr, depth):
             if not node or depth == len(arr) or node.val != arr[depth]:
                 return False
             if depth+1 == len(arr) and node.left == node.right:
                 return True
-            return dfs(node.left, depth+1) or dfs(node.right, depth+1)
+            return dfs(node.left, arr, depth+1) or dfs(node.right, arr, depth+1)
 
-        return dfs(root, 0)
+        return dfs(root, arr, 0)
