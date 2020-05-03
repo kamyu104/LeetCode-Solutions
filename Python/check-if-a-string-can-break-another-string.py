1a078d9a64e8de5ad6493bc4424461bdeb562273
kamyu <kamyu104@users.noreply.github.com>
2020-05-03 18:03:00 +0800

    Update check-if-a-string-can-break-another-string.py

--- Python/check-if-a-string-can-break-another-string.py
+++ Python/check-if-a-string-can-break-another-string.py
@@ -12,17 +12,17 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        def is_break(counter1, counter2):
+        def is_break(count1, count2):
             curr1, curr2 = 0, 0
             for c in string.ascii_lowercase:
-                curr1 += counter1[c]
-                curr2 += counter2[c]
+                curr1 += count1[c]
+                curr2 += count2[c]
                 if curr1 < curr2:
                     return False
             return True
 
-        counter1, counter2 = collections.Counter(s1), collections.Counter(s2)
-        return is_break(counter1, counter2) or is_break(counter2, counter1)
+        count1, count2 = collections.Counter(s1), collections.Counter(s2)
+        return is_break(count1, count2) or is_break(count2, count1)
     
 
 # Time:  O(nlogn)
