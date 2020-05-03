2101972410515a946bea9e18c4d41199ba0f1e2a
kamyu <kamyu104@users.noreply.github.com>
2020-05-03 23:24:01 +0800

    Create check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py

---
+++ Python/check-if-a-string-is-a-valid-sequence-from-root-to-leaves-path-in-a-binary-tree.py
@@ -0,0 +1,73 @@
+# Time:  O(n)
+# Space: O(w)
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+# bfs solution
+class Solution(object):
+    def isValidSequence(self, root, arr):
+        """
+        :type root: TreeNode
+        :type arr: List[int]
+        :rtype: bool
+        """
+        q = [root]
+        for depth in xrange(len(arr)):
+            new_q = []
+            for _ in xrange(len(q)):
+                node = q.pop()
+                if not node or node.val != arr[depth]:
+                    continue
+                if depth+1 == len(arr) and node.left == node.right:
+                    return True
+                new_q.extend(child for child in (node.left, node.right) if child)
+            q = new_q
+        return False
+
+
+# Time:  O(n)
+# Space: O(h)
+# dfs solution with stack
+class Solution2(object):
+    def isValidSequence(self, root, arr):
+        """
+        :type root: TreeNode
+        :type arr: List[int]
+        :rtype: bool
+        """
+        s = [(root, 0)]
+        while s:
+            node, depth = s.pop()
+            if not node or depth == len(arr) or node.val != arr[depth]:
+                continue
+            if depth+1 == len(arr) and node.left == node.right:
+                return True
+            s.append((node.right, depth+1))
+            s.append((node.left, depth+1))
+        return False
+
+
+# Time:  O(n)
+# Space: O(h)
+# dfs solution with recursion
+class Solution3(object):
+    def isValidSequence(self, root, arr):
+        """
+        :type root: TreeNode
+        :type arr: List[int]
+        :rtype: bool
+        """
+        def dfs(node, depth):
+            if not node or depth == len(arr) or node.val != arr[depth]:
+                return False
+            if depth+1 == len(arr) and node.left == node.right:
+                return True
+            return dfs(node.left, depth+1) or dfs(node.right, depth+1)
+
+        return dfs(root, 0)
