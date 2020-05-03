ae8e93e9d46c960cbf4945b6db39d3b23d407a05
kamyu <kamyu104@users.noreply.github.com>
2020-05-03 19:11:04 +0800

    Create destination-city.cpp

---
+++ C++/destination-city.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string destCity(vector<vector<string>>& paths) {
+        unordered_set<string> A, B;
+        for (const auto& path : paths) {
+            A.emplace(path[0]);
+            B.emplace(path[1]);
+        }
+        for (const auto& dst : B) {
+            if (!A.count(dst)) {
+                return dst;
+            }
+        }
+        return "";
+    }
+};
