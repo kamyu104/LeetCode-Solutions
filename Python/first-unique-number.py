d9628635b17e716fcc464fa86284eb63265447cd
kamyu <kamyu104@users.noreply.github.com>
2020-05-04 01:43:07 +0800

    Create first-unique-number.py

---
+++ Python/first-unique-number.py
@@ -0,0 +1,40 @@
+# Time:  ctor: O(k)
+#        add: O(1)
+#        showFirstUnique: O(1)
+# Space: O(n)
+
+import collections
+
+
+class FirstUnique(object):
+
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.__q = collections.OrderedDict()
+        self.__dup = set()
+        for num in nums:
+            self.add(num)
+
+    def showFirstUnique(self):
+        """
+        :rtype: int
+        """
+        if self.__q:
+            return next(iter(self.__q))
+        return -1
+    
+
+    def add(self, value):
+        """
+        :type value: int
+        :rtype: None
+        """
+        if value not in self.__dup and value not in self.__q:
+            self.__q[value] = None
+            return
+        if value in self.__q:
+            self.__q.pop(value)
+            self.__dup.add(value)
+
