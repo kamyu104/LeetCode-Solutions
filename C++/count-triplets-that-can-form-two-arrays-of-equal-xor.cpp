9b71f85b3cbf782108f089b01ee3ad69b47beb71
kamyu <kamyu104@users.noreply.github.com>
2020-05-12 03:45:11 +0800

    Create count-triplets-that-can-form-two-arrays-of-equal-xor.cpp

---
+++ C++/count-triplets-that-can-form-two-arrays-of-equal-xor.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr) {
+        unordered_map<int, pair<int, int>> count_sum;
+        count_sum[0] = {1, 0};
+        int result = 0;
+        for (int i = 0, prefix = 0; i < arr.size(); ++i) {
+            prefix ^= arr[i];
+            auto& [c, t] = count_sum[prefix];
+            result += c * i - t;
+            ++c, t += i + 1;
+        }
+        return result;
+    }
+};
