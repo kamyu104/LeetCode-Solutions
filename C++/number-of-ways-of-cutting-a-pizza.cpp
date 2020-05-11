e22fb7c024446e5da6f67dea3bbfec0aa752506e
kamyu <kamyu104@users.noreply.github.com>
2020-05-12 05:20:09 +0800

    Create number-of-ways-of-cutting-a-pizza.cpp

---
+++ C++/number-of-ways-of-cutting-a-pizza.cpp
@@ -0,0 +1,39 @@
+// Time:  O(m * n * k * (m + n))
+// Space: O(m * n * k)
+
+class Solution {
+public:
+    int ways(vector<string>& pizza, int k) {
+        static int MOD = 1e9 + 7;
+        vector<vector<int>> prefix(pizza.size(), vector<int>(pizza[0].size()));
+        for (int j = pizza[0].size() - 1; j >= 0; --j) {
+            for (int accu = 0, i = pizza.size() - 1; i >= 0; --i) {
+                accu += pizza[i][j] == 'A';
+                prefix[i][j] = ((j + 1 != pizza[0].size()) ? prefix[i][j + 1] : 0) + accu;
+            }
+        }
+        vector<vector<vector<uint64_t>>> dp(pizza.size(),
+                                            vector<vector<uint64_t>>(pizza[0].size(),
+                                            vector<uint64_t>(k)));
+        for (int i = pizza.size() - 1; i >= 0; --i) {
+            for (int j = pizza[0].size() - 1; j >= 0; --j) {
+                dp[i][j][0] = 1;
+                for (int m = 1; m < k; ++m) {
+                    for (int n = i + 1; n < pizza.size() && prefix[n][j]; ++n) {
+                        if (prefix[i][j] == prefix[n][j]) {
+                            continue;
+                        }
+                        dp[i][j][m] = (dp[i][j][m] + dp[n][j][m - 1]) % MOD;
+                    }
+                    for (int n = j + 1; n < pizza[0].size() && prefix[i][n]; ++n) {
+                        if (prefix[i][j] == prefix[i][n]) {
+                            continue;
+                        }
+                        dp[i][j][m] = (dp[i][j][m] + dp[i][n][m - 1]) % MOD;
+                    }
+                }
+            }
+        }
+        return dp[0][0][k - 1];
+    }
+};
