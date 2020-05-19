9ff1927eb955184ec7a7a5253ebdf4f91b521eaf
kamyu <kamyu104@users.noreply.github.com>
2020-05-20 01:05:57 +0800

    Update intersection-of-two-arrays-ii.cpp

--- C++/intersection-of-two-arrays-ii.cpp
+++ C++/intersection-of-two-arrays-ii.cpp
@@ -122,3 +122,16 @@ public:
         return result;
     }
 };
+
+// Time:  O(mlogm + nlogn)
+// Space: O(m + n)
+// simple solution
+class Solution2 {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> result;
+        sort(begin(nums1), end(nums1)); sort(begin(nums2), end(nums2));
+        set_intersection(cbegin(nums1), cend(nums1), cbegin(nums2), cend(nums2), back_inserter(result));
+        return result;
+    }
+};
