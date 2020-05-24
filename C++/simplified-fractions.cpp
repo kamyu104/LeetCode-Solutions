b67102cbb87ea7d5cf2ef51105af25f0fe1701aa
kamyu <kamyu104@users.noreply.github.com>
2020-05-25 00:44:07 +0800

    Create simplified-fractions.cpp

---
+++ C++/simplified-fractions.cpp
@@ -0,0 +1,41 @@
+// Time:  O(n^2 * logn)
+// Space: O(n^2)
+
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        unordered_set<pair<int, int>, PairHash<int>> lookup;
+        for (int b = 1; b <= n; ++b) {
+            for (int a = 1; a < b; ++a) {
+                const auto& g = gcd(a, b);
+                lookup.emplace(a / g, b / g);
+            }
+        }
+        vector<string> result;
+        transform(cbegin(lookup), cend(lookup), back_inserter(result),
+                  [](const auto& kvp) {
+                      return to_string(kvp.first) + "/" + to_string(kvp.second);
+                  });
+        return result;
+    }
+
+private:
+    int gcd(int a, int b) {
+        while (b != 0) {
+            int tmp = b;
+            b = a % b;
+            a = tmp;
+        }
+        return a;
+    }
+    
+    template <typename T>
+    struct PairHash {
+        size_t operator()(const pair<T, T>& p) const {
+            size_t seed = 0;
+            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            return seed;
+        }
+    };
+};
