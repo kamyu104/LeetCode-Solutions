f1bd6527fc7ab2ba1a190ec6fe02e0b822109590
kamyu <kamyu104@users.noreply.github.com>
2020-05-25 00:17:11 +0800

    Create active-users.sql

---
+++ MySQL/active-users.sql
@@ -0,0 +1,34 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT DISTINCT 
+       r.id,
+       r.name
+FROM
+  (SELECT a_l.id,
+          a_l.name,
+          @accu := CASE
+                       WHEN a_l.name = @prev AND 
+                            DATEDIFF(a_l.login_date, @login_date) = 1
+                       THEN @accu + 1
+                       ELSE 1
+                   END AS accu,
+          @prev := a_l.name AS prev,
+          @login_date := a_l.login_date AS login_date
+   FROM (
+           (SELECT DISTINCT
+                   a.id,
+                   a.name,
+                   l.login_date
+            FROM accounts a
+            LEFT JOIN logins l
+            ON a.id = l.id
+            ORDER BY a.id,
+                     a.name,
+                     l.login_date) a_l,
+           (SELECT @accu := 0,
+                   @prev := "",
+                   @login_date := "1970-01-01") init
+        )
+  ) r
+WHERE r.accu = 5;
