710fdb5aac03ee66bc0a17bca1c0c80bf981d4ea
kamyu <kamyu104@users.noreply.github.com>
2020-05-25 00:44:57 +0800

    Create simplified-fractions.py

---
+++ Python/simplified-fractions.py
@@ -0,0 +1,17 @@
+# Time:  O(n^2 * logn)
+# Space: O(n^2)
+
+import fractions
+
+class Solution(object):
+    def simplifiedFractions(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        lookup = set()
+        for b in xrange(1, n+1):
+            for a in xrange(1, b):
+                g = fractions.gcd(a, b)
+                lookup.add((a//g, b//g))
+        return ["{}/{}".format(*fr) for fr in lookup]
