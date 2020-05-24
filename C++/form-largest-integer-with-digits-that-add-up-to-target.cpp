2f9739aa4e6116cc43acf8efb91fa20b4647f277
kamyu <kamyu104@users.noreply.github.com>
2020-05-25 02:18:41 +0800

    Update form-largest-integer-with-digits-that-add-up-to-target.cpp

--- C++/form-largest-integer-with-digits-that-add-up-to-target.cpp
+++ C++/form-largest-integer-with-digits-that-add-up-to-target.cpp
@@ -28,3 +28,36 @@ public:
         return result;
     }
 };
+
+// Time:  O(t)
+// Space: O(t)
+class Solution2 {
+public:
+    string largestNumber(vector<int>& cost, int target) {
+        const auto& key = [](const auto& a) {
+            return pair{accumulate(cbegin(a), cend(a), 0), a};
+        };
+        vector<vector<int>> dp = {vector<int>(9)};
+        for (int t = 1; t <= target; ++t) {
+            dp.emplace_back();
+            for (int i = 0; i < 9; ++i) {
+                if (t < cost[i] || dp[t - cost[i]].empty()) {
+                    continue;
+                }
+                auto curr = dp[t - cost[i]];
+                ++curr[8 - i];
+                if (key(curr) > key(dp[t])) {
+                    dp[t] = move(curr);
+                }
+            }
+        }
+        if (dp.back().empty()) {
+            return "0";
+        }
+        string result;
+        for (int i = 0; i < dp.back().size(); ++i) {
+            result += string(dp.back()[i], '1' + 8 - i);
+        }
+        return result;
+    }
+};
