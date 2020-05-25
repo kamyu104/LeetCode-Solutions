6ad93a357816e5fa1df2f5dcc773c03781b4e71e
kamyu <kamyu104@users.noreply.github.com>
2020-05-25 23:49:49 +0800

    Create people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cpp

---
+++ C++/people-whose-list-of-favorite-companies-is-not-a-subset-of-another-list.cpp
@@ -0,0 +1,42 @@
+// Time:  O(n * c * l + n^2 * c), n is favoriteCompanies.length
+//                              , c is the max of favoriteCompanies[i].length
+//                              , l is the max of favoriteCompanies[i][j].length
+// Space: O(n * c * l)
+
+class Solution {
+public:
+    vector<int> peopleIndexes(vector<vector<string>>& favoriteCompanies) {
+        unordered_map<string, int> lookup;
+        vector<unordered_set<int>> comps;
+        for (const auto& cs : favoriteCompanies) {
+            comps.emplace_back();
+            for (const auto& c : cs) {
+                if (!lookup.count(c)) {
+                    const auto id = lookup.size();
+                    lookup[c] = id;
+                }
+                comps.back().emplace(lookup[c]);
+            }
+        }
+        vector<int> result;
+        for (int i = 0; i < comps.size(); ++i) {
+            bool is_not_subset = true;
+            for (int j = 0; j < comps.size(); ++j) {
+                if (i == j) {
+                    continue;
+                }
+                if (all_of(cbegin(comps[i]), cend(comps[i]),
+                           [&](const auto& a) {
+                               return comps[j].count(a);
+                           })) {
+                    is_not_subset = false;
+                    break;
+                }
+            }
+            if (is_not_subset) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
