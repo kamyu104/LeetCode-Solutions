51af99473d61621755b679dc7f8e59e674148267
kamyu <kamyu104@users.noreply.github.com>
2020-05-31 21:44:16 +0800

    Create probability-of-a-two-boxes-having-the-same-number-of-distinct-balls.py

---
+++ Python/probability-of-a-two-boxes-having-the-same-number-of-distinct-balls.py
@@ -0,0 +1,38 @@
+# Time:  O(k^3 * n^2)
+# Space: O(k^2 * n)
+
+import collections
+
+
+class Solution(object):
+    def getProbability(self, balls):
+        """
+        :type balls: List[int]
+        :rtype: float
+        """
+        def nCr(n, r):
+            if n-r < r:
+                return nCr(n, n-r)
+            c = 1
+            for k in xrange(1, r+1):
+                c *= n-k+1
+                c //= k
+            return c
+        
+        def candidates(lookup, n):  # Time: O(n^2), Space: O(n)
+            if n not in lookup:
+                lookup[n] = {i:nCr(n, i) for i in xrange(n+1)}
+            return lookup[n]
+        
+        dp, lookup = collections.defaultdict(int), {}
+        dp[0, 0] = 1  # dp[i, j] is the ways of number difference i and color difference j
+        for n in balls:  # O(k) times
+            new_dp = collections.defaultdict(int)
+            for ndiff, cdiff in dp:  # O(k^2 * n) times
+                for k, count in candidates(lookup, n).iteritems():  # O(n) times
+                    new_ndiff = ndiff+(k-(n-k))
+                    new_cdiff = cdiff-1 if k == 0 else (cdiff+1 if k == n else cdiff)
+                    new_dp[new_ndiff, new_cdiff] += dp[ndiff, cdiff]*count
+            dp = new_dp
+        total = sum(balls)
+        return float(dp[0, 0])/nCr(total, total//2)
