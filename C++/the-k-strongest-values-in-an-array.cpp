d897395367d91b99e6743dd361eb2ad351a08ef7
kamyu <kamyu104@users.noreply.github.com>
2020-06-08 01:16:37 +0800

    Update the-k-strongest-values-in-an-array.cpp

--- C++/the-k-strongest-values-in-an-array.cpp
+++ C++/the-k-strongest-values-in-an-array.cpp
@@ -21,6 +21,26 @@ public:
     vector<int> getStrongest(vector<int>& arr, int k) {
         sort(begin(arr), end(arr));
         const auto m = arr[(arr.size() -1) / 2];
+        int left = 0, right = arr.size() - 1;
+        vector<int> result;
+        while (result.size() < k) {
+            if (m - arr[left] > arr[right] - m) {
+                result.push_back(arr[left++]);  
+            } else {
+                result.push_back(arr[right--]);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+class Solution3 {
+public:
+    vector<int> getStrongest(vector<int>& arr, int k) {
+        sort(begin(arr), end(arr));
+        const auto m = arr[(arr.size() -1) / 2];
         sort(begin(arr), end(arr), [&](int a, int b) { 
             return abs(a - m) != abs(b - m) ?  abs(a - m) > abs(b - m) : a > b; 
         });
