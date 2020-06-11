7e0154b1174cf854408d366bef5f16385a25be3f
Jenishk56 <jenish.khunt@gmail.com>
2020-06-11 23:01:07 +0200

    Correcting based on some edge cases
    ["apap","app"]
    "abcdefghijklmnopqrstuvwxyz"

--- Python/verifying-an-alien-dictionary.py
+++ Python/verifying-an-alien-dictionary.py
@@ -1,23 +1,30 @@
 # Time:  O(n * l), l is the average length of words
 # Space: O(1)
 
-class Solution(object):
-    def isAlienSorted(self, words, order):
-        """
-        :type words: List[str]
-        :type order: str
-        :rtype: bool
-        """
-        lookup = {c: i for i, c in enumerate(order)}
-        for i in xrange(len(words)-1):
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        lookup = {}
+        count = 0
+        for char in order:
+            lookup[char] = count + 1
+            count+=1
+            
+        n = len(words)
+        for i in range(n-1):
             word1 = words[i]
             word2 = words[i+1]
-            for k in xrange(min(len(word1), len(word2))):
+            n1 = len(word1)
+            n2 = len(word2)
+            n = min(n1, n2)
+            count = 0
+            for k in range(n):
                 if word1[k] != word2[k]:
                     if lookup[word1[k]] > lookup[word2[k]]:
                         return False
                     break
-            else:
-                if len(word1) > len(word2):
-                    return False
+                else:
+                    count+=1
+            if count == n and n1 > n2:
+                return False
+                    
         return True
