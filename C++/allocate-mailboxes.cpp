513b3f36aae19f6fe58d6312d248f9b8e90da67e
kamyu <kamyu104@users.noreply.github.com>
2020-06-17 22:19:02 +0800

    Create allocate-mailboxes.cpp

---
+++ C++/allocate-mailboxes.cpp
@@ -0,0 +1,31 @@
+// Time:  O(m *n^2)
+// Space: O(n)
+
+class Solution {
+public:
+    int minDistance(vector<int>& houses, int k) {
+        sort(begin(houses), end(houses));
+        vector<int> prefix(houses.size() + 1);
+        for (int i = 0; i < houses.size(); ++i) {
+            prefix[i + 1] = prefix[i] + houses[i];
+        }
+        vector<int> dp(houses.size());
+        for (int j = 0; j < houses.size(); ++j) {
+            dp[j] = cost(prefix, 0, j);
+        }
+        for (int m = 1; m < k; ++m) {
+            for (int j = houses.size() - 1; j >= 0; --j) {
+                for (int i = m; i <= j; ++i) {
+                    dp[j] = min(dp[j], dp[i - 1] + cost(prefix, i, j));
+                }
+            }
+        }
+        return dp.back();
+    }
+
+private:
+    int cost(const vector<int>& prefix, int i, int j) {
+        return (prefix[j + 1] - prefix[(i + j + 1) / 2]) -
+               (prefix[(i + j) / 2 + 1] - prefix[i]);
+    }
+};
