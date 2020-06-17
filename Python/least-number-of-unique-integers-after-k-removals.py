8f2e2e64716b43f2a16a0b5c06f5f131cd241c8e
kamyu <kamyu104@users.noreply.github.com>
2020-06-17 22:59:54 +0800

    Create least-number-of-unique-integers-after-k-removals.py

---
+++ Python/least-number-of-unique-integers-after-k-removals.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def findLeastNumOfUniqueInts(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count = collections.Counter(arr)
+        result, count_count = len(count), Counter(count.itervalues())
+        for c in xrange(1, len(arr)+1): 
+            if k < c*count_count[c]:
+                result -= k//c
+                break
+            k -= c*count_count[c]
+            result -= count_count[c]                
+        return result
