b5ce8e176eea72c747d528bcca6556c52d10a9ff
kamyu <kamyu104@users.noreply.github.com>
2020-06-17 22:58:55 +0800

    Create least-number-of-unique-integers-after-k-removals.cpp

---
+++ C++/least-number-of-unique-integers-after-k-removals.cpp
@@ -0,0 +1,40 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int findLeastNumOfUniqueInts(vector<int>& arr, int k) {
+        const auto& count = counter(arr);
+        const auto& count_count = counter(count);
+        int result = count.size();
+        for (int c = 1; c <= arr.size(); ++c) {
+            if (!count_count.count(c)) {
+                continue;
+            }
+            if (k < c * count_count.at(c)) {
+                result -= k / c;
+                break;
+            }
+            k -= c * count_count.at(c);
+            result -= count_count.at(c);
+        }
+        return result;
+    }
+
+private:
+    unordered_map<int, int> counter(const vector<int>& arr) {
+        unordered_map<int, int> result;
+        for (const auto& i : arr) {
+            ++result[i];
+        }
+        return result;
+    }
+    
+    unordered_map<int, int> counter(const unordered_map<int, int>& count) {
+        unordered_map<int, int> result;
+        for (const auto& [_, i] : count) {
+            ++result[i];
+        }
+        return result;
+    }
+};
