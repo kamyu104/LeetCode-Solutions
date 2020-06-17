2f83207f5f06ab9c02da9964724979e21d9b3ac3
kamyu <kamyu104@users.noreply.github.com>
2020-06-17 20:51:28 +0800

    Update find-two-non-overlapping-sub-arrays-each-with-target-sum.py

--- Python/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ Python/find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -8,7 +8,7 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-        prefix, dp = {0: -1}, [float("inf")]*len(arr)  # dp[i]: min len of target subarray until i
+        prefix, dp = {0: -1}, [float("inf")]*len(arr)  # dp[i], min len of target subarray until i
         result = min_len = float("inf")
         accu = 0
         for right in xrange(len(arr)):
@@ -17,6 +17,7 @@ class Solution(object):
             if accu-target in prefix:
                 left = prefix[accu-target]
                 min_len = min(min_len, right-left)
-                result = min(result, dp[left] + (right-left))
+                if left != -1:
+                    result = min(result, dp[left] + (right-left))
             dp[right] = min_len
         return result if result != float("inf") else -1
