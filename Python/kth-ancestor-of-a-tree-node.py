5228d5980f685160a8a06bbfd5bb7e1edce81dae
kamyu <kamyu104@users.noreply.github.com>
2020-06-18 02:50:37 +0800

    Update kth-ancestor-of-a-tree-node.py

--- Python/kth-ancestor-of-a-tree-node.py
+++ Python/kth-ancestor-of-a-tree-node.py
@@ -12,13 +12,13 @@ class TreeAncestor(object):
         :type n: int
         :type parent: List[int]
         """
-        par = [[p] for p in parent]
+        par = [[p] if p != -1 else [] for p in parent]
         i, max_depth = 0, 1
         while i < max_depth:
             for p in par:
-                if not (i < len(p) and p[i] != -1):
+                if not (i < len(p) and i < len(par[p[i]])):
                     continue
-                p.append(par[p[i]][i] if i < len(par[p[i]]) else -1)
+                p.append(par[p[i]][i])
                 if len(p) > max_depth:
                     max_depth = len(p)
             i += 1
@@ -33,7 +33,7 @@ class TreeAncestor(object):
         par, i, pow_i_of_2 = self.__parent, 0, 1
         while pow_i_of_2 <= k:
             if k & pow_i_of_2:
-                if not (i < len(par[node]) and par[node][i] != -1):
+                if not (i < len(par[node])):
                     return -1
                 node = par[node][i]
             i += 1
