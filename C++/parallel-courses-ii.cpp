107a8c5246acfe5b5b7894f4d877385cc6c60eaf
kamyu <kamyu104@users.noreply.github.com>
2020-06-29 03:00:19 +0800

    Update parallel-courses-ii.cpp

--- C++/parallel-courses-ii.cpp
+++ C++/parallel-courses-ii.cpp
@@ -1,8 +1,61 @@
+// Time:  O(nlogn + e), e is the number of edges in graph
+// Space: O(n)
+
+class Solution {
+public:
+    int minNumberOfSemesters(int n, vector<vector<int>>& dependencies, int k) {
+        vector<vector<int>> graph(n);
+        vector<int> degrees(n);
+        for (const auto &d: dependencies) {
+            ++degrees[d[1] - 1];
+            graph[d[0] - 1].emplace_back(d[1] - 1);
+        }
+        vector<int> depths(n, -1);
+        for (int i = 0; i < n; ++i) {
+            dfs(graph, i, &depths);
+        }
+        priority_queue<pair<int, int>> max_heap;
+        for (int i = 0; i < n; ++i) {
+            if (!degrees[i]) {
+                max_heap.emplace(depths[i], i);
+            }
+        }
+        int result = 0;
+        while (!max_heap.empty()) {
+            vector<pair<int, int>> new_q;
+            for (int i = 0; !max_heap.empty() && i < k; ++i) {
+                const auto [depth, node] = max_heap.top(); max_heap.pop();
+                for (const auto& child : graph[node]) {
+                    if (!--degrees[child]) {
+                        new_q.emplace_back(depths[child], child);
+                    }
+                }
+            }
+            ++result;
+            for (const auto& p : new_q) {
+                max_heap.emplace(p);
+            }
+        }
+        return result;
+    }
+
+private:
+    int dfs(const vector<vector<int>> &graph, int idx, vector<int> *depths) {
+        if ((*depths)[idx] == -1) {
+            int depth = 0;
+            for (const auto& child : graph[idx]) {
+                depth = max(depth, dfs(graph, child, depths));
+            }
+            (*depths)[idx] = depth + 1;
+        }
+        return (*depths)[idx];
+    }
+};
+
 // Time:  O((n * C(c, min(c, k))) * 2^n)
 // Space: O(2^n)
-
-// concise solution
-class Solution {
+// concise dp solution
+class Solution2 {
 public:
     int minNumberOfSemesters(int n, vector<vector<int>>& dependencies, int k) {
         vector<int> reqs(n);
@@ -60,8 +113,8 @@ private:
 
 // Time:  O((n * C(c, min(c, k))) * 2^n)
 // Space: O(2^n)
-// embedded combination solution
-class Solution2 {
+// embedded combination dp solution
+class Solution3 {
 public:
     int minNumberOfSemesters(int n, vector<vector<int>>& dependencies, int k) {
         static const auto& choice_mask =
