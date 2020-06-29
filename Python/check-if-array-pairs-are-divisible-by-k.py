bb711c73b832fafbf6188cae196df025b598e810
kamyu <kamyu104@users.noreply.github.com>
2020-06-30 02:03:19 +0800

    Update check-if-array-pairs-are-divisible-by-k.py

--- Python/check-if-array-pairs-are-divisible-by-k.py
+++ Python/check-if-array-pairs-are-divisible-by-k.py
@@ -11,8 +11,6 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        if len(arr)%2:
-            return False
         count = collections.Counter(i%k for i in arr)
         return (0 not in count or not count[0]%2) and \
                 all(k-i in count and count[i] == count[k-i] for i in xrange(1, k) if i in count)
