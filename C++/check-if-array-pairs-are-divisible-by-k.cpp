6baffeeda5716e02e45cf2efa30de3b84a8c8746
kamyu <kamyu104@users.noreply.github.com>
2020-06-30 02:02:58 +0800

    Update check-if-array-pairs-are-divisible-by-k.cpp

--- C++/check-if-array-pairs-are-divisible-by-k.cpp
+++ C++/check-if-array-pairs-are-divisible-by-k.cpp
@@ -4,9 +4,6 @@
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
-        if (arr.size() % 2) {
-            return false;
-        }
         unordered_map<int, int> count;
         for (const auto& i : arr) {
             ++count[(i % k + k) % k];
