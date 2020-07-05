c4ce18dc28312f09599f0c51f31a6b920d42e452
kamyu <kamyu104@users.noreply.github.com>
2020-07-06 01:25:59 +0800

    Create minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.py

---
+++ Python/minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.py
@@ -0,0 +1,44 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+import collections
+
+
+class BIT(object):  # Fenwick Tree, 1-indexed
+    def __init__(self, n):
+        self.__bit = [0] * n
+
+    def add(self, i, val):
+        while i < len(self.__bit):
+            self.__bit[i] += val
+            i += (i & -i)
+
+    def sum(self, i):
+        result = 0
+        while i > 0:
+            result += self.__bit[i]
+            i -= (i & -i)
+        return result
+
+
+class Solution(object):
+    def minInteger(self, num, k):
+        """
+        :type num: str
+        :type k: int
+        :rtype: str
+        """
+        lookup = collections.defaultdict(deque)
+        bit = BIT(len(num)+1)
+        for i, d in enumerate(num):
+            bit.add(i+1, 1)
+            lookup[int(d)].append(i)
+        result = []
+        for _ in xrange(len(num)):
+            for d in xrange(10):
+                if lookup[d] and bit.sum(lookup[d][0]) <= k:
+                    k -= bit.sum(lookup[d][0])
+                    bit.add(lookup[d].popleft()+1,-1)
+                    result.append(d)
+                    break
+        return "".join(map(str, result))
