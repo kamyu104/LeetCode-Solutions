afd4c0f56dfffbff3ccc4f8fa03422615c391757
kamyu <kamyu104@users.noreply.github.com>
2020-07-06 01:24:56 +0800

    Create minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp

---
+++ C++/minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp
@@ -0,0 +1,55 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class BIT {  // Fenwick Tree, 1-indexed
+public:
+    BIT(int n) : bit_(n) {
+        
+    }
+
+    void add(int i, int val) {
+        for (; i < bit_.size(); i += lower_bit(i)) {
+            bit_[i] += val;
+        }
+    }
+    
+    int sum(int i) {
+        int sum = 0;
+        for (; i > 0; i -= lower_bit(i)) {
+            sum += bit_[i];
+        }
+        return sum;
+    }
+
+    inline int lower_bit(int i) {
+        return i & -i;
+    }
+
+private:
+    vector<int> bit_;
+};
+
+class Solution {
+public:
+    string minInteger(string num, int k) {
+        unordered_map<int, deque<int>> lookup;
+        BIT bit(num.length() + 1);
+        for (int i = 0; i < num.length(); ++i) {
+            lookup[num[i] - '0'].emplace_back(i);
+            bit.add(i + 1, 1);
+        }
+        string result;
+        for (int i = 0; i < num.length(); ++i) {
+            for (int d = 0; d <= 9; ++d) {
+                if (!lookup[d].empty() && bit.sum(lookup[d].front()) <= k) {
+                    k -= bit.sum(lookup[d].front());
+                    bit.add(lookup[d].front() + 1, -1);
+                    lookup[d].pop_front();
+                    result.push_back('0' + d);
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
