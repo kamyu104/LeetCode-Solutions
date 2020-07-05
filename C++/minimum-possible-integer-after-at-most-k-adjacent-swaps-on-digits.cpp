72dcc224829b9c63bef348fdbc81d853b6a56549
kamyu <kamyu104@users.noreply.github.com>
2020-07-06 01:53:23 +0800

    Update minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp

--- C++/minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp
+++ C++/minimum-possible-integer-after-at-most-k-adjacent-swaps-on-digits.cpp
@@ -32,19 +32,19 @@ private:
 class Solution {
 public:
     string minInteger(string num, int k) {
-        unordered_map<int, deque<int>> lookup;
+        unordered_map<int, vector<int>> lookup;
         BIT bit(num.length() + 1);
-        for (int i = 0; i < num.length(); ++i) {
+        for (int i = num.length() - 1; i >= 0; --i) {
             lookup[num[i] - '0'].emplace_back(i + 1);
             bit.add(i + 1, 1);
         }
         string result;
         for (int i = 0; i < num.length(); ++i) {
             for (int d = 0; d <= 9; ++d) {
-                if (!lookup[d].empty() && bit.sum(lookup[d].front() - 1) <= k) {
-                    k -= bit.sum(lookup[d].front() - 1);
-                    bit.add(lookup[d].front(), -1);
-                    lookup[d].pop_front();
+                if (!lookup[d].empty() && bit.sum(lookup[d].back() - 1) <= k) {
+                    k -= bit.sum(lookup[d].back() - 1);
+                    bit.add(lookup[d].back(), -1);
+                    lookup[d].pop_back();
                     result.push_back('0' + d);
                     break;
                 }
