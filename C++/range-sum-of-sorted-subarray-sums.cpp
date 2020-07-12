cc9bec5b3629fe7e2057c4c168506c5367682d12
kamyu <kamyu104@users.noreply.github.com>
2020-07-12 23:49:48 +0800

    Update range-sum-of-sorted-subarray-sums.cpp

--- C++/range-sum-of-sorted-subarray-sums.cpp
+++ C++/range-sum-of-sorted-subarray-sums.cpp
@@ -6,24 +6,24 @@ class Solution {
 public:
     int rangeSum(vector<int>& nums, int n, int left, int right) {
         static const int MOD = 1e9 + 7;
-        vector<int> prefix(cbegin(nums), cend(nums));
-        for (int i = 0; i + 1 < nums.size(); ++i) {
+        vector<uint64_t> prefix(cbegin(nums), cend(nums));
+        for (uint64_t i = 0; i + 1 < nums.size(); ++i) {
             prefix[i + 1] += prefix[i];
         }
-        const auto min = *min_element(cbegin(nums), cend(nums));
-        const auto& max = accumulate(cbegin(nums), cend(nums), 0);
+        const uint64_t min = *min_element(cbegin(nums), cend(nums));
+        const uint64_t& max = accumulate(cbegin(nums), cend(nums), 0ull);
         return (sumLessOrEqualTo(prefix, nums, min, max, right) -
                 sumLessOrEqualTo(prefix, nums, min, max, left - 1)) % MOD;
     }
     
 private:
-    int sumLessOrEqualTo(const vector<int>& prefix,
-                         const vector<int>& nums,
-                         int left, int right,
-                         int target) {
+    uint64_t sumLessOrEqualTo(const vector<uint64_t>& prefix,
+                              const vector<int>& nums,
+                              uint64_t left, uint64_t right,
+                              uint64_t target) {
         while (left <= right) {
             const auto mid = left + (right - left) / 2;
-            if (countUntil(nums, mid) - target >= 0) {
+            if (countUntil(nums, mid) >= target) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
@@ -32,9 +32,9 @@ private:
         return sumUntil(nums, prefix, left) - left * (countUntil(nums, left) - target);
     }
     
-    int countUntil(const vector<int>& nums,
-                   int target) {
-        int result = 0, curr = 0;
+    uint64_t countUntil(const vector<int>& nums,
+                        uint64_t target) {
+        uint64_t result = 0, curr = 0;
         for (int left = 0, right = 0; right < nums.size(); ++right) {
             curr += nums[right];
             for (; curr > target; ++left) {
@@ -45,10 +45,10 @@ private:
         return result;
     }
     
-    int sumUntil(const vector<int>& nums,
-                 const vector<int>& prefix,
-                 int target) {
-        int result = 0, curr = 0, total = 0;
+    uint64_t sumUntil(const vector<int>& nums,
+                      const vector<uint64_t>& prefix,
+                      uint64_t target) {
+        uint64_t result = 0, curr = 0, total = 0;
         for (int left = 0, right = 0; right < nums.size(); ++right) {
             curr += nums[right];
             total += nums[right] * (right - left + 1);
