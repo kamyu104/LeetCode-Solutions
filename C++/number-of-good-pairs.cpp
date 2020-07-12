c19a4e1164e9afbfcc22b33cb7aea13cf74b88bc
kamyu <kamyu104@users.noreply.github.com>
2020-07-12 19:07:03 +0800

    Create number-of-good-pairs.cpp

---
+++ C++/number-of-good-pairs.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) {
+        int result = 0;
+        for (const auto& [n, c] : counter(nums)) {
+            result += c * (c - 1) / 2;
+        }
+        return result;
+    }
+
+private:
+    unordered_map<int, int> counter(const vector<int>& arr) {
+        unordered_map<int, int> result;
+        for (const auto& i : arr) {
+            ++result[i];
+        }
+        return result;
+    }
+};
