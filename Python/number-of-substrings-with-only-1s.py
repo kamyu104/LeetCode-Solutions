03e721418333ee207f887614d6b83ce1f57e477d
kamyu <kamyu104@users.noreply.github.com>
2020-07-12 19:13:43 +0800

    Create number-of-substrings-with-only-1s.py

---
+++ Python/number-of-substrings-with-only-1s.py
@@ -0,0 +1,15 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def numSub(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        MOD = 10**9+7
+        result, count = 0, 0
+        for c in s:
+            count = count+1 if c == '1' else 0
+            result = (result + count) % MOD
+        return result
