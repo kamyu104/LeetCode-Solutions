a4466b66f76fbe33c95eeaf6fa7edbdcf5a41a3c
kamyu <kamyu104@users.noreply.github.com>
2020-07-21 00:22:16 +0800

    Update find-a-value-of-a-mysterious-function-closest-to-target.cpp

--- C++/find-a-value-of-a-mysterious-function-closest-to-target.cpp
+++ C++/find-a-value-of-a-mysterious-function-closest-to-target.cpp
@@ -2,6 +2,68 @@
 // Space: O(logm)
 
 class Solution {
+   public:
+    int closestToTarget(std::vector<int>& arr, int target) {
+        static const int LOGM = 20;
+        BitCount count(LOGM);
+        int result = numeric_limits<int>::max();
+        int left = 0;
+        for (int right = 0; right < arr.size(); ++right) {
+            count += arr[right];
+            while (left <= right) {
+                int f = count.number(right - left + 1);
+                result = min(result, abs(f - target));
+                if (f >= target) {
+                    break;
+                }
+                count -= arr[left++];
+            }
+        }
+        return result;
+    }
+
+   private:
+    class BitCount {
+    public:
+        BitCount(int n) : n_(n), count_(n) {
+            
+        }
+
+        int number(int min_count) const {
+            int num = 0;
+            for (int i = 0; i < n_; ++i) {
+                if (count_[i] >= min_count) {
+                    num |= 1 << i;
+                }
+            }
+            return num;
+        }
+
+        void operator+=(int num) {
+            for (int i = 0; i < n_; ++i) {
+                if (num & (1 << i)) {
+                    ++count_[i];
+                }
+            }
+        }
+        
+        void operator-=(int num) {
+            for (int i = 0; i < n_; ++i) {
+                if (num & (1 << i)) {
+                    --count_[i];
+                }
+            }
+        }
+
+    private:        
+        int n_;
+        vector<int> count_;
+    };
+};
+
+// Time:  O(nlogm), m is the max value of arr
+// Space: O(logm)
+class Solution2 {
 public:
     int closestToTarget(vector<int>& arr, int target) {
         int result = numeric_limits<int>::max();
