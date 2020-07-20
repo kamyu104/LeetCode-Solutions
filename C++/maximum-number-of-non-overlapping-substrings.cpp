81d363c3a4671c536085fe4a4ec3aae7fbb86be7
kamyu <kamyu104@users.noreply.github.com>
2020-07-21 01:07:41 +0800

    Update maximum-number-of-non-overlapping-substrings.cpp

--- C++/maximum-number-of-non-overlapping-substrings.cpp
+++ C++/maximum-number-of-non-overlapping-substrings.cpp
@@ -44,3 +44,42 @@ private:
         return right;
     }
 };
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    vector<string> maxNumOfSubstrings(string s) {
+        vector<int> first(26, numeric_limits<int>::max());
+        vector<int> last(26, numeric_limits<int>::min());
+        for (int i = 0; i < s.length(); ++i) {
+            first[s[i] - 'a'] = min(first[s[i] - 'a'], i);
+            last[s[i] - 'a'] = max(last[s[i] - 'a'], i);
+        }
+        vector<pair<int, int>> intervals;
+        for (int c = 0; c < first.size(); ++c) {
+            if (first[c] == numeric_limits<int>::max()) {
+                continue;
+            }
+            int left = first[c], right = last[c];
+            for (int i = left; i <= right; ++i) {
+                left = min(left, first[s[i] - 'a']);
+                right = max(right, last[s[i] - 'a']);
+            }
+            if (left == first[c]) {
+                intervals.emplace_back(right, left);
+            }
+        }
+        sort(begin(intervals), end(intervals));  // Time: O(26log26)
+        vector<string> result;
+        int prev = -1;
+        for (const auto& [right, left] : intervals) {
+            if (left <= prev) {
+                continue;
+            }
+            result.emplace_back(s.substr(left, right - left + 1));
+            prev = right;
+        }
+        return result;
+    }
+};
