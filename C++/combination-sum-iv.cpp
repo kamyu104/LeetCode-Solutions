33fa7523db78e1268c736c4d732c0152429969dd
kamyu <kamyu104@users.noreply.github.com>
2020-07-24 16:44:47 +0800

    Update combination-sum-iv.cpp

--- C++/combination-sum-iv.cpp
+++ C++/combination-sum-iv.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int> dp(target + 1, 0);
+        vector<uint32_t> dp(target + 1, 0);
         dp[0] = 1;
         sort(nums.begin(), nums.end());
 
