51be59dd98d5f63492426fa12f72d61c2f913aa6
kamyu <kamyu104@users.noreply.github.com>
2020-08-01 01:19:16 +0800

    Create find-the-index-of-the-large-integer.cpp

---
+++ C++/find-the-index-of-the-large-integer.cpp
@@ -0,0 +1,18 @@
+// Time:  O(logn)
+// Space: O(1)
+
+class Solution {
+public:
+    int getIndex(ArrayReader &reader) {
+        int left = 0, right = reader.length() - 1;
+        while (left < right) {
+            const auto& mid = left + (right - left) / 2;
+            if (reader.compareSub(left, mid, (right - left + 1) % 2 ? mid : mid + 1, right) >= 0) {
+                right = mid;
+            }  else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
