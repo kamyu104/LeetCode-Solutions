cf6d171a323059fa4e8e4a849c9f9ef6428e2812
kamyu <kamyu104@users.noreply.github.com>
2020-08-01 15:45:23 +0800

    Update shuffle-string.cpp

--- C++/shuffle-string.cpp
+++ C++/shuffle-string.cpp
@@ -1,7 +1,31 @@
 // Time:  O(n)
 // Space: O(1)
 
+// in-place solution
 class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        for (int i = 0; i < s.length(); ++i) {
+            if (s[i] < 0) {
+                continue;
+            }
+            char move = -s[i];
+            for (int j = indices[i]; j != i ; j = indices[j]) {
+                swap(s[j], move);
+                move = -move;
+            }
+            s[i] = move;
+        }
+        for (auto& c : s) {
+            c = - c;
+        }
+        return s;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
 public:
     string restoreString(string s, vector<int>& indices) {
         string result(s.length(), 0);
