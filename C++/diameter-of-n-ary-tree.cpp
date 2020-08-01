96dc5154054368b8667ddef16b7d804843e06972
kamyu <kamyu104@users.noreply.github.com>
2020-08-01 15:09:29 +0800

    Update diameter-of-n-ary-tree.cpp

--- C++/diameter-of-n-ary-tree.cpp
+++ C++/diameter-of-n-ary-tree.cpp
@@ -1,29 +1,7 @@
 // Time:  O(n)
 // Space: O(h)
 
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    vector<Node*> children;
-
-    Node() {}
-
-    Node(int _val) {
-        val = _val;
-    }
-
-    Node(int _val, vector<Node*> _children) {
-        val = _val;
-        children = _children;
-    }
-};
-*/
-
-// Time:  O(n)
-// Space: O(h)
-class Solution2 {
+class Solution {
 public:
     int diameter(Node* root) {
         return iter_dfs(root).first;
