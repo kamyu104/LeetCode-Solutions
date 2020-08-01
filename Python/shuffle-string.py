a1704ed99eac0ebc1024486d45ce9e4e26bf2a45
kamyu <kamyu104@users.noreply.github.com>
2020-08-01 15:46:58 +0800

    Update shuffle-string.py

--- Python/shuffle-string.py
+++ Python/shuffle-string.py
@@ -4,7 +4,32 @@
 import itertools
 
 
+# in-place solution
 class Solution(object):
+    def restoreString(self, s, indices):
+        """
+        :type s: str
+        :type indices: List[int]
+        :rtype: str
+        """
+        result = map(ord, s)
+        for i, c in enumerate(result):
+            if c < 0:
+                continue
+            move, j = -c, indices[i]
+            while j != i:
+                result[j], move = move, result[j]
+                move, j = -move, indices[j]
+            result[i] = move
+        return "".join(map(lambda x: chr(-x), result))
+
+
+# Time:  O(n)
+# Space: O(1)
+import itertools
+
+
+class Solution2(object):
     def restoreString(self, s, indices):
         """
         :type s: str
