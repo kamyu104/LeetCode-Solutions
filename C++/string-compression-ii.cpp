17f3c398f637dadd8703b4fe24399728ce3175e9
kamyu <kamyu104@users.noreply.github.com>
2020-08-01 14:21:11 +0800

    Create string-compression-ii.cpp

---
+++ C++/string-compression-ii.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n^2 * k)
+// Space: O(n * k)
+
+class Solution {
+public:
+    int getLengthOfOptimalCompression(string s, int k) {
+        vector<vector<int>> dp(s.length() + 1, vector<int>(k + 1, s.length()));
+        dp[0][0] = 0;
+        for (int i = 1; i <= s.length(); ++i) {
+            for (int j = 0; j <= k; ++j) {
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    dp[i][j] = min(dp[i][j], dp[i - 1][j - 1]);
+                }
+                int keep = 0, del = 0;
+                for (int m = i; m <= s.length(); ++m) {
+                    if (s[i - 1] == s[m - 1]) {
+                        ++keep;
+                    } else {
+                        ++del;
+                    }
+                    if (j + del > k) {
+                        break;
+                    }
+                    int length = dp[i - 1][j] + ((keep >= 2) ? 2 : 1);
+                    for (int cnt = keep; cnt >= 10; cnt /= 10) {
+                        ++length;
+                    }
+                    dp[m][j + del] = min(dp[m][j + del], length);
+                }
+            }
+        }
+        return dp[s.length()][k];
+    }
+};
