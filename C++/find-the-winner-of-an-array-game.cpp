30d4dda65e19b7b7317a2ce8e64ccc2fa69b654d
kamyu <kamyu104@users.noreply.github.com>
2020-08-02 14:54:40 +0800

    Create find-the-winner-of-an-array-game.cpp

---
+++ C++/find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int result = arr[0];
+        for (int i = 1, count = 0; i < arr.size(); ++i) {
+            if (arr[i] > result) {
+                result = arr[i];
+                count = 0;
+            }
+            ++count;
+            if (count == k) {
+                break;
+            }
+        }
+        return result;
+    }
+};
