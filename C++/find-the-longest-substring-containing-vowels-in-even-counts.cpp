5698e83521bbb0f81c397bcc113aec8dc108d380
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 23:49:44 +0800

    Update find-the-longest-substring-containing-vowels-in-even-counts.cpp

--- C++/find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ C++/find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -6,12 +6,12 @@ public:
     int findTheLongestSubstring(string s) {
         static const string VOWELS = "aeiou";
         int result = 0, mask = 0;
-        vector<int> lookup(1 << VOWELS.length(), -2);
+        vector<int> lookup(1 << VOWELS.length(), s.length());
         lookup[0] = -1;
         for (int i = 0; i < s.length(); ++i) {
             const auto& pos = VOWELS.find(s[i]);
             mask ^= pos != string::npos ? 1 << pos : 0;
-            if (lookup[mask] == -2) {
+            if (lookup[mask] == s.length()) {
                 lookup[mask] = i;
             }
             result = max(result, i - lookup[mask]);
