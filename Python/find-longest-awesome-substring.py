2ba05c2288b7d471b048140e21bc90d02f31ac37
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 23:58:44 +0800

    Update find-longest-awesome-substring.py

--- Python/find-longest-awesome-substring.py
+++ Python/find-longest-awesome-substring.py
@@ -7,14 +7,14 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        ALPHA_BET_SIZE = 10
-        result, mask, lookup = 0, 0, [len(s)]*(2**ALPHA_BET_SIZE)
+        ALPHABET_SIZE = 10
+        result, mask, lookup = 0, 0, [len(s)]*(2**ALPHABET_SIZE)
         lookup[0] = -1
         for i, ch in enumerate(s):
             mask ^= 2**(ord(ch)-ord('0'))
             if lookup[mask] == len(s):
                 lookup[mask] = i
             result = max(result, i - lookup[mask])
-            for d in xrange(ALPHA_BET_SIZE):
+            for d in xrange(ALPHABET_SIZE):
                 result = max(result, i - lookup[mask^(2**d)])
         return result
