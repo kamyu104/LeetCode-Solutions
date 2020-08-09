3e6f524da1aa8da7a3755ab2571b10e1174fbb49
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 23:50:28 +0800

    Update find-the-longest-substring-containing-vowels-in-even-counts.py

--- Python/find-the-longest-substring-containing-vowels-in-even-counts.py
+++ Python/find-the-longest-substring-containing-vowels-in-even-counts.py
@@ -8,12 +8,12 @@ class Solution(object):
         :rtype: int
         """
         VOWELS = "aeiou"
-        result, mask, lookup = 0, 0, [None]*(2**len(VOWELS))
+        result, mask, lookup = 0, 0, [len(s)]*(2**len(VOWELS))
         lookup[0] = -1
         for i, c in enumerate(s):
             index = VOWELS.find(c)
             mask ^= (1 << index) if index >= 0 else 0
-            if lookup[mask] is None:
+            if lookup[mask] == len(s):
                 lookup[mask] = i
             result = max(result, i-lookup[mask])
         return result
