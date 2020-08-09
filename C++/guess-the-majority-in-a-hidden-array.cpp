23b67f6c19e6e8c008e06762f229549696b20751
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 18:48:38 +0800

    Create guess-the-majority-in-a-hidden-array.cpp

---
+++ C++/guess-the-majority-in-a-hidden-array.cpp
@@ -0,0 +1,41 @@
+// Time:  O(n), n queries
+// Space: O(1)
+
+class Solution {
+public:
+    int guessMajority(ArrayReader &reader) {
+        int count_a = 1, count_b = 0, idx_b = -1;
+        const auto& value_0_1_2_3 = reader.query(0, 1, 2, 3);
+        int value_0_1_2_i = -1;
+        for (int i = reader.length() - 1; i >= 4; --i) {
+            value_0_1_2_i = reader.query(0, 1, 2, i);
+            if (value_0_1_2_i == value_0_1_2_3) {  // nums[i] == nums[3]
+                ++count_a;
+            } else {
+                ++count_b;
+                idx_b = i;
+            }
+        }
+        const auto& value_0_1_2_4 = value_0_1_2_i;
+        for (int i = 0; i < 3; ++i) {
+            vector<int> a_b;
+            for (int j = 0; j < 3; ++j) {
+                if (j == i) {
+                    continue;
+                }
+                a_b.emplace_back(j);
+            }
+            int value_a_b_3_4 = reader.query(a_b[0], a_b[1], 3, 4);
+            if (value_a_b_3_4 == value_0_1_2_4) {  // nums[i] == nums[3]
+                ++count_a;
+            } else {
+                ++count_b;
+                idx_b = i;
+            }
+        }
+        if (count_a == count_b) {
+            return -1;
+        }
+        return count_a > count_b ? 3 : idx_b;
+    }
+};
