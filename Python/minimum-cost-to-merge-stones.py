bf820ed2855019c9da730f1301ff183cd76cfe97
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 20:36:20 +0800

    Update minimum-cost-to-merge-stones.py

--- Python/minimum-cost-to-merge-stones.py
+++ Python/minimum-cost-to-merge-stones.py
@@ -1,7 +1,6 @@
-# Time:  O(n^3)
-# Space: O(n^3)
+# Time:  O(n^3/k)
+# Space: O(n^2)
 
-# memoization (top-down dp)
 class Solution(object):
     def mergeStones(self, stones, K):
         """
@@ -9,28 +8,17 @@ class Solution(object):
         :type K: int
         :rtype: int
         """
-        def dp(prefix, K, i, j, k, lookup):
-            if (i, j, k) in lookup:
-                return lookup[i, j, k]
-            if i == j:
-                result = 0 if k == 1 else float("inf")
-            else:
-                if k == 1:
-                    result = dp(prefix, K, i, j, K, lookup) + \
-                             prefix[j+1] - prefix[i]
-                else:
-                    result = float("inf")
-                    for mid in xrange(i, j, K-1):
-                        result = min(result, dp(prefix, K, i, mid, 1, lookup) + 
-                                             dp(prefix, K, mid+1, j, k-1, lookup))
-            lookup[i, j, k] = result
-            return result
-        
         if (len(stones)-1) % (K-1):
             return -1
-        lookup = {}
         prefix = [0]
         for x in stones:
             prefix.append(prefix[-1]+x)
-        result = dp(prefix, K, 0, len(stones)-1, 1, lookup)
-        return result if result != float("inf") else 0
+        dp = [[0]*len(stones) for _ in xrange(len(stones))]
+        for l in xrange(K-1, len(stones)):
+            for i in xrange(len(stones)-l):
+                dp[i][i+l] = float("inf")
+                for j in xrange(i, i+l, K-1):
+                    dp[i][i+l] = min(dp[i][i+l], dp[i][j]+dp[j+1][i+l])
+                if l % (K-1) == 0:
+                    dp[i][i+l] += prefix[i+l+1] - prefix[i]
+        return dp[0][len(stones)-1]
