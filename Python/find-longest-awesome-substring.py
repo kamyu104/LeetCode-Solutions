68725a231cfd13d67e8c8ad9843dbc66eb487aea
kamyu <kamyu104@users.noreply.github.com>
2020-08-09 23:58:11 +0800

    Create find-longest-awesome-substring.py

---
+++ Python/find-longest-awesome-substring.py
@@ -0,0 +1,20 @@
+# Time:  O(10 * n)
+# Space: O(1024)
+
+class Solution(object):
+    def longestAwesome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ALPHA_BET_SIZE = 10
+        result, mask, lookup = 0, 0, [len(s)]*(2**ALPHA_BET_SIZE)
+        lookup[0] = -1
+        for i, ch in enumerate(s):
+            mask ^= 2**(ord(ch)-ord('0'))
+            if lookup[mask] == len(s):
+                lookup[mask] = i
+            result = max(result, i - lookup[mask])
+            for d in xrange(ALPHA_BET_SIZE):
+                result = max(result, i - lookup[mask^(2**d)])
+        return result
