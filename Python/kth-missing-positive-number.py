69179fc79fe7a260e5b492ff0058a33479dd80cd
kamyu <kamyu104@users.noreply.github.com>
2020-08-10 01:45:29 +0800

    Create kth-missing-positive-number.py

---
+++ Python/kth-missing-positive-number.py
@@ -0,0 +1,21 @@
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def findKthPositive(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def check(arr, k, x):
+            return arr[x]-(x+1) >= k
+
+        left, right = 0, len(arr)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(arr, k, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left+k
