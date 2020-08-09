df59ae4a0d94125664ba868766839aa66eb5ef19
kamyu <kamyu104@users.noreply.github.com>
2020-08-10 03:41:46 +0800

    Update kth-missing-positive-number.cpp

--- C++/kth-missing-positive-number.cpp
+++ C++/kth-missing-positive-number.cpp
@@ -1,24 +1,23 @@
 // Time:  O(logn)
 // Space: O(1)
 
-
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
         int left = 0, right = arr.size() - 1;
         while (left <= right) {
             const auto& mid = left + (right - left) / 2;
-            if (check(arr, k, mid)) {
+            if (!check(arr, k, mid)) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left + k;  // left ? arr[left - 1] + (k - (arr[left - 1] - ((left - 1) + 1))) : k;
+        return right + 1 + k;  // right >= 0 ? arr[right] + (k - (arr[right] - (right + 1))) : k;
     }
 
 private:
     bool check(const vector<int>& arr, int k, int x) {
-        return arr[x] - (x + 1) >= k;
+        return arr[x] - (x + 1) < k;
     }
 };
