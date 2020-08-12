4b25ad940307340d4e83e8f1aa432591f257d3e8
Parth Purvesh <43883971+czynot@users.noreply.github.com>
2020-08-12 14:02:12 +0530

    Update sqrtx.py to a single line
    Can someone please refer my profile to Google? Thanks xD

--- Python/sqrtx.py
+++ Python/sqrtx.py
@@ -7,17 +7,6 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if x < 2:
-            return x
-
-        left, right = 1, x // 2
-        while left <= right:
-            mid = left + (right - left) // 2
-            if mid > x / mid:
-                right = mid - 1
-            else:
-                left = mid + 1
-
-        return left - 1
+        return int(x ** 2)
 
 
