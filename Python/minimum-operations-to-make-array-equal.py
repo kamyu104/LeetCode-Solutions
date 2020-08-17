26206bca2dde358cdfcf7dd0989c639212a1cfee
kamyu <kamyu104@users.noreply.github.com>
2020-08-17 22:38:26 +0800

    Create minimum-operations-to-make-array-equal.py

---
+++ Python/minimum-operations-to-make-array-equal.py
@@ -0,0 +1,13 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def minOperations(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # total = (2i+1 for i in xrange(n)) = n^2
+        # left_half_total = (2i+1 for i in xrange(n//2)) = (n//2)^2
+        # result = (n//2) * (total//n) - left_half_total = (n//2)*(n-n//2) = (n//2)*((n+1)//2)
+        return (n//2)*((n+1)//2)
