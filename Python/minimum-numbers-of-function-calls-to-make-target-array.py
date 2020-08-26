a1d673393bc4ce7b710629b3c5809cc071713f0c
kamyu <kamyu104@users.noreply.github.com>
2020-08-26 23:40:33 +0800

    Create minimum-numbers-of-function-calls-to-make-target-array.py

---
+++ Python/minimum-numbers-of-function-calls-to-make-target-array.py
@@ -0,0 +1,22 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Solution(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def popcount(n):
+            result = 0
+            while n:
+                n &= n-1
+                result += 1
+            return result
+
+        result, max_len = 0, 0
+        for num in nums:
+            result += popcount(num)
+            max_len = max(max_len, num.bit_length())
+        return result + max_len-1
+            
