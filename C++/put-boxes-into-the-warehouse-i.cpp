001f2f4c1ccccbd4309b5ea59b5fc17b424705f1
kamyu <kamyu104@users.noreply.github.com>
2020-08-27 21:04:48 +0800

    Update put-boxes-into-the-warehouse-i.cpp

--- C++/put-boxes-into-the-warehouse-i.cpp
+++ C++/put-boxes-into-the-warehouse-i.cpp
@@ -4,6 +4,25 @@
 class Solution {
 public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
+        sort(rbegin(boxes), rend(boxes));
+        int result = 0;
+        for (const auto& h : boxes) {
+            if (h > warehouse[result]) {
+                continue;
+            }
+            if (++result == warehouse.size()) {
+                break;
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn + m)
+// Space: O(1)
+class Solution2 {
+public:
+    int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
         sort(begin(boxes), end(boxes));
         for (int i = 1; i < warehouse.size(); ++i) {
             warehouse[i] = min(warehouse[i], warehouse[i - 1]);
