8af84c0be63fdb80fa784a48b0b251da83603910
kamyu <kamyu104@users.noreply.github.com>
2020-08-27 21:02:34 +0800

    Update put-boxes-into-the-warehouse-i.py

--- Python/put-boxes-into-the-warehouse-i.py
+++ Python/put-boxes-into-the-warehouse-i.py
@@ -8,6 +8,26 @@ class Solution(object):
         :type warehouse: List[int]
         :rtype: int
         """
+        boxes.sort(reverse=True)
+        result = 0
+        for h in boxes:
+            if h > warehouse[result]:
+                continue
+            result += 1
+            if result == len(warehouse):
+                break
+        return result
+
+
+# Time:  O(nlogn + m)
+# Space: O(1)
+class Solution2(object):
+    def maxBoxesInWarehouse(self, boxes, warehouse):
+        """
+        :type boxes: List[int]
+        :type warehouse: List[int]
+        :rtype: int
+        """
         boxes.sort()
         for i in xrange(1, len(warehouse)):
             warehouse[i] = min(warehouse[i], warehouse[i-1])
