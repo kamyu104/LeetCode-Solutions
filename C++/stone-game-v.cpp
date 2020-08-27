93613df9ff8d5e0276117cce6e83bd4205c03d46
kamyu <kamyu104@users.noreply.github.com>
2020-08-28 00:38:38 +0800

    Update stone-game-v.cpp

--- C++/stone-game-v.cpp
+++ C++/stone-game-v.cpp
@@ -11,11 +11,12 @@ public:
         vector<vector<int>> mid(n, vector<int>(n));
         for (int l = 1; l <= n; ++l) {
             for (int i = 0; i <= n - l; ++i) {
-                int p = (l == 1) ? i + 1 : mid[i][i + l - 2];
-                while (prefix[p] - prefix[i] < prefix[i + l] - prefix[p]) {
+                int j = i + l - 1;
+                int p = (l == 1) ? i + 1 : mid[i][j - 1];
+                while (prefix[p] - prefix[i] < prefix[j + 1] - prefix[p]) {
                     ++p;  // Time: O(n^2) in total
                 }
-                mid[i][i + l - 1] = p;
+                mid[i][j] = p;
             }
         }
         
@@ -27,21 +28,22 @@ public:
         vector<vector<int>> dp(n, vector<int>(n));
         for (int l = 2; l <= n; ++l) {
             for (int i = 0; i <= n - l; ++i) {
-                const auto& p = mid[i][i + l - 1];
+                int j = i + l - 1;
+                const auto& p = mid[i][j];
                 int max_score = 0;
-                if (prefix[p] - prefix[i] == prefix[i + l] - prefix[p]) {
-                    max_score = max(rmq[i][p - 1], rmq[i + l - 1][p]);
+                if (prefix[p] - prefix[i] == prefix[j + 1] - prefix[p]) {
+                    max_score = max(rmq[i][p - 1], rmq[j][p]);
                 } else {
                     if (i <= p - 2) {
                         max_score = max(max_score, rmq[i][p - 2]);
                     }
-                    if (p <= i + l - 1) {
-                        max_score = max(max_score, rmq[i + l - 1][p]);
+                    if (p <= j) {
+                        max_score = max(max_score, rmq[j][p]);
                     }
                 }
-                dp[i][i + l - 1] = max_score;
-                rmq[i][i + l - 1] = max(rmq[i][i + l - 2], (prefix[i + l] - prefix[i]) + max_score);
-                rmq[i + l - 1][i] = max(rmq[i + l - 1][i + 1], (prefix[i + l] - prefix[i]) + max_score);
+                dp[i][j] = max_score;
+                rmq[i][j] = max(rmq[i][j - 1], (prefix[j + 1] - prefix[i]) + max_score);
+                rmq[j][i] = max(rmq[j][i + 1], (prefix[j + 1] - prefix[i]) + max_score);
             }
         }
         return dp[0][n - 1];
