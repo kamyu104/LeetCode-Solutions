06d499a3061f0aee4af0044a3ec8ba2b627d96bd
kamyu <kamyu104@users.noreply.github.com>
2020-08-31 01:42:28 +0800

    Update unique-orders-and-customers-per-month.sql

--- MySQL/unique-orders-and-customers-per-month.sql
+++ MySQL/unique-orders-and-customers-per-month.sql
@@ -1,8 +1,9 @@
 # Time:  O(n)
 # Space: O(n)
 
-SELECT LEFT(order_date, 7) month, COUNT(DISTINCT order_id) order_count,
-        COUNT(DISTINCT customer_id) customer_count
+SELECT LEFT(order_date, 7) month,
+       COUNT(DISTINCT order_id) order_count,
+       COUNT(DISTINCT customer_id) customer_count
 FROM orders
 WHERE invoice > 20
 GROUP BY 1
