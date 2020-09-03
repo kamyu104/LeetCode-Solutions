10a5987a42e1cabc39f92b247bc0557e0c0fe6ff
kamyu <kamyu104@users.noreply.github.com>
2020-09-04 00:34:13 +0800

    Update minimum-window-substring.py

--- Python/minimum-window-substring.py
+++ Python/minimum-window-substring.py
@@ -12,14 +12,13 @@ class Solution(object):
         :rtype: str
         """
         count, remain = collections.Counter(t), len(t)
-        i = 0
-        left = right = -1
+        i, left, right = 0, -1, -1
         for j, c in enumerate(s):
             remain -= count[c] > 0
             count[c] -= 1
             if remain:
                 continue
-            while i < j and count[s[i]] < 0:
+            while count[s[i]] < 0:
                 count[s[i]] += 1
                 i += 1
             if right == -1 or j-i+1 < right-left+1:
