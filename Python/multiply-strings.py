d698fec3b19e18219da8d9e900968e30cc306e47
kamyu <kamyu104@users.noreply.github.com>
2020-09-03 23:14:07 +0800

    Update multiply-strings.py

--- Python/multiply-strings.py
+++ Python/multiply-strings.py
@@ -8,24 +8,19 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
-        num1, num2 = num1[::-1], num2[::-1]
-        res = [0] * (len(num1) + len(num2))
-        for i in xrange(len(num1)):
-            for j in xrange(len(num2)):
-                res[i + j] += int(num1[i]) * int(num2[j])
-                res[i + j + 1] += res[i + j] / 10
-                res[i + j] %= 10
-
-        # Skip leading 0s.
-        i = len(res) - 1
-        while i > 0 and res[i] == 0:
-            i -= 1
-
-        return ''.join(map(str, res[i::-1]))
+        result = [0]*(len(num1)+len(num2))
+        for i in reversed(xrange(len(num1))):
+            for j in reversed(xrange(len(num2))):
+                result[i+j+1] += int(num1[i])*int(num2[j])
+                result[i+j] += result[i+j+1]//10
+                result[i+j+1] %= 10
+        for i in xrange(len(result)):
+            if result[i]:
+                return "".join(map(lambda x: str(x), result[i:]))
+        return "0"
 
 # Time:  O(m * n)
 # Space: O(m + n)
-# Using built-in bignum solution.
 class Solution2(object):
     def multiply(self, num1, num2):
         """
@@ -33,6 +28,31 @@ class Solution2(object):
         :type num2: str
         :rtype: str
         """
+        num1, num2 = num1[::-1], num2[::-1]
+        result = [0]*(len(num1)+len(num2))
+        for i in xrange(len(num1)):
+            for j in xrange(len(num2)):
+                result[i+j] += int(num1[i])*int(num2[j])
+                result[i+j+1] += result[i+j]//10
+                result[i+j] %= 10
+
+        for i in reversed(xrange(len(result))):
+            if result[i]:
+                break
+        print result
+        for i in reversed(xrange(len(result))):
+            if result[i]:
+                break
+        return "".join(map(str, result[i::-1]))
+
+# Time:  O(m * n)
+# Space: O(m + n)
+# Using built-in bignum solution.
+class Solution3(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
         return str(int(num1) * int(num2))
-
-
