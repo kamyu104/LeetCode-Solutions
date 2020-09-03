aa31ee45d1578cdb722715bfe9e14ba6aa08f304
kamyu <kamyu104@users.noreply.github.com>
2020-09-03 21:04:45 +0800

    Create dot-product-of-two-sparse-vectors.cpp

---
+++ C++/dot-product-of-two-sparse-vectors.cpp
@@ -0,0 +1,31 @@
+// Time:  ctor: O(n)
+//        dot_product: O(min(n, m))
+// Space: O(n)
+
+class SparseVector {
+public:
+    SparseVector(vector<int> &nums) {
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i]) {
+                lookup_[i] = nums[i];
+            }
+        }
+    }
+    
+    int dotProduct(SparseVector& vec) {
+        auto a = this, b = &vec;
+        if (a->lookup_.size() > b->lookup_.size()) {
+            swap(a, b);
+        }
+        int result = 0;
+        for (const auto& [i, v] : a->lookup_) {
+            if (b->lookup_.count(i)) {
+                result += a->lookup_[i] * b->lookup_[i];
+            }
+        }
+        return result;
+    }
+
+private:
+    unordered_map<int, int> lookup_;
+};
