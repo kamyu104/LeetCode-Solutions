e096b0682d4e57d1caa7a512e2570fe8c106f037
kamyu <kamyu104@users.noreply.github.com>
2020-09-03 21:03:43 +0800

    Create dot-product-of-two-sparse-vectors.py

---
+++ Python/dot-product-of-two-sparse-vectors.py
@@ -0,0 +1,20 @@
+# Time:  ctor: O(n)
+#        dot_product: O(min(n, m))
+# Space: O(n)
+
+class SparseVector:
+    def __init__(self, nums):
+        """
+        :type nums: List[int]
+        """
+        self.lookup = {i:v for i, v in enumerate(nums) if v}
+
+    # Return the dotProduct of two sparse vectors
+    def dotProduct(self, vec):
+        """
+        :type vec: 'SparseVector'
+        :rtype: int
+        """
+        if len(self.lookup) > len(vec.lookup):
+            self, vec = vec, self
+        return sum(v*vec.lookup[i] for i, v in self.lookup.iteritems() if i in vec.lookup)
