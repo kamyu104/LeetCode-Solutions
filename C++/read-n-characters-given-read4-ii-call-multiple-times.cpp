a34a75bf9f7afbdc3f4b582eff4b11182ffda46b
kamyu <kamyu104@users.noreply.github.com>
2020-09-04 23:43:07 +0800

    Update read-n-characters-given-read4-ii-call-multiple-times.cpp

--- C++/read-n-characters-given-read4-ii-call-multiple-times.cpp
+++ C++/read-n-characters-given-read4-ii-call-multiple-times.cpp
@@ -4,7 +4,30 @@
 // Forward declaration of the read4 API.
 int read4(char *buf);
 
+// shorter version of Solution2
 class Solution {
+public:
+    /**
+     * @param buf Destination buffer
+     * @param n   Maximum number of characters to read
+     * @return    The number of characters read
+     */
+    int read(char *buf, int n) {
+        int i = 0;
+        while (i < n && (i4_ < n4_ || (i4_ = 0) < (n4_ = read4(buf4_)))) {
+            buf[i++] = buf4_[i4_++];
+        }
+        return i;
+    }
+
+private:
+    char buf4_[4];
+    int i4_ = 0, n4_ = 0;
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
 public:
     /**
      * @param buf Destination buffer
