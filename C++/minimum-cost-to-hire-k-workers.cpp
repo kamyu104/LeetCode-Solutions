9d25b8f7fa71818a792cce20a76ef1975cfa67b9
kamyu <kamyu104@users.noreply.github.com>
2020-09-05 19:41:54 +0800

    Update minimum-cost-to-hire-k-workers.cpp

--- C++/minimum-cost-to-hire-k-workers.cpp
+++ C++/minimum-cost-to-hire-k-workers.cpp
@@ -6,21 +6,20 @@ public:
     double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int K) {
         vector<pair<double, int>> workers;
         for (int i = 0; i < quality.size(); ++i) {
-            workers.emplace_back(static_cast<double>(wage[i]) / quality[i],
-                                 quality[i]);
+            workers.emplace_back(static_cast<double>(wage[i]) / quality[i], quality[i]);
         }
-        sort(workers.begin(), workers.end());
+        sort(begin(workers), end(workers));
         auto result = numeric_limits<double>::max();
         auto sum = 0.0;
         priority_queue<int> max_heap;
-        for (const auto& worker: workers) {
-            sum += worker.second;
-            max_heap.emplace(worker.second);
+        for (const auto& [ratio, q]: workers) {
+            sum += q;
+            max_heap.emplace(q);
             if (max_heap.size() > K) {
                 sum -= max_heap.top(), max_heap.pop();
             }
             if (max_heap.size() == K) {
-                result = min(result, sum * worker.first);
+                result = min(result, sum * ratio);
             }
         }
         return result;
