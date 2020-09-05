aa81d5b452c808fd0e4adfe607e0b818baa52952
kamyu <kamyu104@users.noreply.github.com>
2020-09-05 16:26:38 +0800

    Update trapping-rain-water.cpp

--- C++/trapping-rain-water.cpp
+++ C++/trapping-rain-water.cpp
@@ -4,6 +4,21 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
+        int result = 0, left = 0, right = height.size()-1, level = 0;
+        while (left < right) {
+            int lower = height[height[left] < height[right] ? left++ : right--];
+            level = max(level, lower);
+            result += level - lower;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    int trap(vector<int>& height) {
         if (height.empty()) {
             return 0;
         }
