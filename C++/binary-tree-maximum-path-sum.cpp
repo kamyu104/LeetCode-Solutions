6a045922cf9656655a3a19fedf7e5a454bead18a
kamyu <kamyu104@users.noreply.github.com>
2020-09-05 22:46:12 +0800

    Update binary-tree-maximum-path-sum.cpp

--- C++/binary-tree-maximum-path-sum.cpp
+++ C++/binary-tree-maximum-path-sum.cpp
@@ -60,17 +60,19 @@ public:
 private:
     int iter_dfs(TreeNode *node) {
         int result = numeric_limits<int>::min(), max_sum = 0;
-        vector<tuple<int, TreeNode *, shared_ptr<int>, shared_ptr<int>, int*>> stk = {{1, node, nullptr,  nullptr, &max_sum}};
+        vector<tuple<int, TreeNode *, unique_ptr<int>, unique_ptr<int>, int*>> stk;
+        stk.emplace_back(1, node, nullptr,  nullptr, &max_sum);
         while (!stk.empty()) {
-            const auto [step, node, ret1, ret2, ret] = stk.back(); stk.pop_back();
+            const auto [step, node, ret1, ret2, ret] = move(stk.back()); stk.pop_back();
             if (step == 1) {
                 if (!node) {
                     continue;
                 }
-                auto ret1 = make_shared<int>(), ret2 = make_shared<int>();
-                stk.emplace_back(2, node, ret1, ret2, ret);
-                stk.emplace_back(1, node->right, nullptr, nullptr, ret2.get());
-                stk.emplace_back(1, node->left, nullptr, nullptr, ret1.get());
+                auto ret1 = make_unique<int>(), ret2 = make_unique<int>();
+                auto p1 = ret1.get(), p2 = ret2.get();
+                stk.emplace_back(2, node, move(ret1), move(ret2), ret);
+                stk.emplace_back(1, node->right, nullptr, nullptr, p2);
+                stk.emplace_back(1, node->left, nullptr, nullptr, p1);
             } else if (step == 2) {
                 result = max({result, node->val + max(*ret1, 0) + max(*ret2, 0)});
                 *ret = node->val + max({*ret1, *ret2, 0});
