13831e6bfdb671950d603e4da693cba74bb23d9f
kamyu <kamyu104@users.noreply.github.com>
2020-09-05 16:31:23 +0800

    Update trapping-rain-water.cpp

--- C++/trapping-rain-water.cpp
+++ C++/trapping-rain-water.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        int result = 0, left = 0, right = height.size()-1, level = 0;
+        int result = 0, left = 0, right = height.size() - 1, level = 0;
         while (left < right) {
             int lower = height[height[left] < height[right] ? left++ : right--];
             level = max(level, lower);
