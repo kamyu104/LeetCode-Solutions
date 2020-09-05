e2b29a475dac845ef13ebde3138b9f3818f6bd8d
kamyu <kamyu104@users.noreply.github.com>
2020-09-05 23:59:26 +0800

    Update and rename ladderLength.cpp to word-ladder.cpp

---
+++ C++/word-ladder.cpp
@@ -0,0 +1,78 @@
+// Time:  O(26 * d * l) = O(d * l), d is the size of wordlist, l is the max length of words
+// Space: O(d * l)
+
+// two-end bfs
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> lookup(cbegin(wordList), cend(wordList));
+        if (!lookup.count(endWord)) {
+            return 0;
+        }
+        int ladder = 2;
+        for (unordered_set<string> left = {beginWord}, right = {endWord};
+             !left.empty() && !right.empty();
+             ++ladder) {
+            if (left.size() > right.size()) {
+                swap(left, right);
+            }
+            unordered_set<string> new_left;
+            for (const auto& word : left) {
+                auto new_word = word;
+                for (int i = 0; i < new_word.length(); ++i) {
+                    char prev = new_word[i];
+                    for (int j = 0; j < 26; ++j) {
+                        new_word[i] = 'a' + j;
+                        if (right.count(new_word)) {
+                            return ladder;
+                        }
+                        if (lookup.count(new_word)) {
+                            lookup.erase(new_word);
+                            new_left.emplace(new_word);
+                        }
+                    }
+                    new_word[i] = prev;
+                }
+            }
+            left = move(new_left);
+        }
+        return 0;
+    }
+};
+
+// Time:  O(26 * d * l)
+// Time:  O(d * l)
+class Solution2 {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> lookup(cbegin(wordList), cend(wordList));
+        if (!lookup.count(endWord)) {
+            return 0;
+        }
+        int ladder = 2;
+        for (unordered_set<string> q = {beginWord};
+             !q.empty();
+             ++ladder) {
+            unordered_set<string> new_q;
+            for (const auto& word : q) {
+                auto new_word = word;
+                for (int i = 0; i < new_word.length(); ++i) {
+                    char prev = new_word[i];
+                    for (int j = 0; j < 26; ++j) {
+                        new_word[i] = 'a' + j;
+                        if (new_word == endWord) {
+                            return ladder;
+                        }
+                        if (lookup.count(new_word)) {
+                            lookup.erase(new_word);
+                            new_q.emplace(new_word);
+                        }
+                    }
+                    new_word[i] = prev;
+                }
+            }
+            q = move(new_q);
+        }
+        return 0;
+    }
+};
