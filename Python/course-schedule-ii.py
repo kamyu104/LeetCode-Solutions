cf503e99751a3bfad53f7048105d6fb529ce49c4
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 23:03:34 +0800

    Update course-schedule-ii.py

--- Python/course-schedule-ii.py
+++ Python/course-schedule-ii.py
@@ -12,23 +12,23 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
-        indegree = collections.defaultdict(set)
-        outdegree = collections.defaultdict(set)
+        in_degree = collections.defaultdict(set)
+        out_degree = collections.defaultdict(set)
         for i, j in prerequisites:
-            indegree[i].add(j)
-            outdegree[j].add(i)
-        q = collections.deque([i for i in xrange(numCourses) if i not in indegree])
+            in_degree[i].add(j)
+            out_degree[j].add(i)
+        q = collections.deque([i for i in xrange(numCourses) if i not in in_degree])
         result = []
         while q:
             node = q.popleft()
             result.append(node)
-            for i in outdegree[node]:
-                indegree[i].remove(node)
-                if not indegree[i]:
+            for i in out_degree[node]:
+                in_degree[i].remove(node)
+                if not in_degree[i]:
                     q.append(i)
-                    del indegree[i]
-            del outdegree[node]
-        return result if not indegree and not outdegree else []
+                    del in_degree[i]
+            del out_degree[node]
+        return result if not in_degree and not out_degree else []
 
 
 # Time:  O(|V| + |E|)
@@ -41,20 +41,20 @@ class Solution2(object):
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
-        indegree = collections.defaultdict(set)
-        outdegree = collections.defaultdict(set)
+        in_degree = collections.defaultdict(set)
+        out_degree = collections.defaultdict(set)
         for i, j in prerequisites:
-            indegree[i].add(j)
-            outdegree[j].add(i)
-        stk = [i for i in xrange(numCourses) if i not in indegree]
+            in_degree[i].add(j)
+            out_degree[j].add(i)
+        stk = [i for i in xrange(numCourses) if i not in in_degree]
         result = []
         while stk:
             node = stk.pop()
             result.append(node)
-            for i in outdegree[node]:
-                indegree[i].remove(node)
-                if not indegree[i]:
+            for i in out_degree[node]:
+                in_degree[i].remove(node)
+                if not in_degree[i]:
                     stk.append(i)
-                    del indegree[i]
-            del outdegree[node]
-        return result if not indegree and not outdegree else []
+                    del in_degree[i]
+            del out_degree[node]
+        return result if not in_degree and not out_degree else []
