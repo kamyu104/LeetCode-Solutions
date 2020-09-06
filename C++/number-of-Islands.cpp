af071804ff4a39611a40ce469222bb012f239c0f
kamyu <kamyu104@users.noreply.github.com>
2020-09-07 00:55:10 +0800

    Update number-of-Islands.cpp

--- C++/number-of-Islands.cpp
+++ C++/number-of-Islands.cpp
@@ -30,33 +30,33 @@ public:
 
 private:
     class UnionFind {
-        public:
-            UnionFind(const int n) : set_(n), count_(n) {
-                iota(set_.begin(), set_.end(), 0);
-            }
+    public:
+        UnionFind(const int n) : set_(n), count_(n) {
+            iota(set_.begin(), set_.end(), 0);
+        }
 
-            int find_set(const int x) {
-               if (set_[x] != x) {
-                   set_[x] = find_set(set_[x]);  // Path compression.
-               }
-               return set_[x];
-            }
+        int find_set(const int x) {
+           if (set_[x] != x) {
+               set_[x] = find_set(set_[x]);  // Path compression.
+           }
+           return set_[x];
+        }
 
-            void union_set(const int x, const int y) {
-                int x_root = find_set(x), y_root = find_set(y);
-                if (x_root != y_root) {
-                    set_[min(x_root, y_root)] = max(x_root, y_root);
-                    --count_;
-                }
+        void union_set(const int x, const int y) {
+            int x_root = find_set(x), y_root = find_set(y);
+            if (x_root != y_root) {
+                set_[min(x_root, y_root)] = max(x_root, y_root);
+                --count_;
             }
+        }
 
-            int size() const {
-                return count_;
-            }
+        int size() const {
+            return count_;
+        }
 
-        private:
-            vector<int> set_;
-            int count_;
+    private:
+        vector<int> set_;
+        int count_;
     };
     
     int index(int n, int i, int j) {
