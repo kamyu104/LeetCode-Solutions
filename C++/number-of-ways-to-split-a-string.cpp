cc9bc5ed1bde6447b583fd5b7cf89eda54d9775d
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 12:18:49 +0800

    Create number-of-ways-to-split-a-string.cpp

---
+++ C++/number-of-ways-to-split-a-string.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int numWays(string s) {
+        static const int MOD = 1e9 + 7;
+        int ones = count_if(cbegin(s), cend(s),
+                            [](const auto& x) {
+                                return x == '1';
+                            });
+        if (ones % 3) {
+            return 0;
+        }
+        ones /= 3;
+        if (ones == 0) {
+            return static_cast<int64_t>(s.length() - 1) * (s.length() - 2) / 2 % MOD;
+        }
+        int count = 0, left = 0, right = 0;
+        for (const auto& c : s) {
+            if (c == '1') {
+                ++count;
+            }
+            if (count == ones) {
+                ++left;
+            } else if (count == 2 * ones) {
+                ++right;
+            }
+        }
+        return static_cast<int64_t>(left) * right % MOD;
+    }
+};
