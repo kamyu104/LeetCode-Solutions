6de00bb0ec2b27a882c0cf7856ad14d503748549
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 23:02:56 +0800

    Update course-schedule-ii.py

--- Python/course-schedule-ii.py
+++ Python/course-schedule-ii.py
@@ -1,6 +1,10 @@
-from collections import defaultdict, deque
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+
+import collections
 
 
+# bfs solution
 class Solution(object):
     def findOrder(self, numCourses, prerequisites):
         """
@@ -8,30 +12,49 @@ class Solution(object):
         :type prerequisites: List[List[int]]
         :rtype: List[int]
         """
-        res, zero_in_degree_queue = [], deque()
-        in_degree, out_degree = defaultdict(set), defaultdict(set)
-
+        indegree = collections.defaultdict(set)
+        outdegree = collections.defaultdict(set)
         for i, j in prerequisites:
-            in_degree[i].add(j)
-            out_degree[j].add(i)
+            indegree[i].add(j)
+            outdegree[j].add(i)
+        q = collections.deque([i for i in xrange(numCourses) if i not in indegree])
+        result = []
+        while q:
+            node = q.popleft()
+            result.append(node)
+            for i in outdegree[node]:
+                indegree[i].remove(node)
+                if not indegree[i]:
+                    q.append(i)
+                    del indegree[i]
+            del outdegree[node]
+        return result if not indegree and not outdegree else []
 
-        for i in xrange(numCourses):
-            if i not in in_degree:
-                zero_in_degree_queue.append(i)
 
-        while zero_in_degree_queue:
-            prerequisite = zero_in_degree_queue.popleft()
-            res.append(prerequisite)
-
-            if prerequisite in out_degree:
-                for course in out_degree[prerequisite]:
-                    in_degree[course].discard(prerequisite)
-                    if not in_degree[course]:
-                        zero_in_degree_queue.append(course)
-
-                del out_degree[prerequisite]
-
-        if out_degree:
-            return []
-
-        return res
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+# dfs solution
+class Solution2(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        indegree = collections.defaultdict(set)
+        outdegree = collections.defaultdict(set)
+        for i, j in prerequisites:
+            indegree[i].add(j)
+            outdegree[j].add(i)
+        stk = [i for i in xrange(numCourses) if i not in indegree]
+        result = []
+        while stk:
+            node = stk.pop()
+            result.append(node)
+            for i in outdegree[node]:
+                indegree[i].remove(node)
+                if not indegree[i]:
+                    stk.append(i)
+                    del indegree[i]
+            del outdegree[node]
+        return result if not indegree and not outdegree else []
