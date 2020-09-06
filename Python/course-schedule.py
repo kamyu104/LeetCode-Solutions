daff178b121d0987159974f83c3c9008bbc1d734
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 23:02:14 +0800

    Update course-schedule.py

--- Python/course-schedule.py
+++ Python/course-schedule.py
@@ -6,7 +6,7 @@ import collections
 
 # bfs solution
 class Solution(object):
-    def findOrder(self, numCourses, prerequisites):
+    def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
@@ -18,24 +18,22 @@ class Solution(object):
             in_degree[i].add(j)
             out_degree[j].add(i)
         q = collections.deque([i for i in xrange(numCourses) if i not in in_degree])
-        result = []
         while q:
             node = q.popleft()
-            result.append(node)
             for i in out_degree[node]:
                 in_degree[i].remove(node)
                 if not in_degree[i]:
                     q.append(i)
                     del in_degree[i]
             del out_degree[node]
-        return result if not in_degree and not out_degree else []
+        return not in_degree and not out_degree
 
 
 # Time:  O(|V| + |E|)
 # Space: O(|E|)
 # dfs solution
 class Solution2(object):
-    def findOrder(self, numCourses, prerequisites):
+    def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
@@ -47,14 +45,12 @@ class Solution2(object):
             in_degree[i].add(j)
             out_degree[j].add(i)
         stk = [i for i in xrange(numCourses) if i not in in_degree]
-        result = []
         while stk:
             node = stk.pop()
-            result.append(node)
             for i in out_degree[node]:
                 in_degree[i].remove(node)
                 if not in_degree[i]:
                     stk.append(i)
                     del in_degree[i]
             del out_degree[node]
-        return result if not in_degree and not out_degree else []
+        return not in_degree and not out_degree
