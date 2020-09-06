ae3c90db64ddd79dcd4390af660d6cd20e17a282
kamyu <kamyu104@users.noreply.github.com>
2020-09-06 12:15:39 +0800

    Update count-all-possible-routes.cpp

--- C++/count-all-possible-routes.cpp
+++ C++/count-all-possible-routes.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n^2 * f)
+// Time:  O(nlogn + n * f)
 // Space: O(n * f)
 
 class Solution {
@@ -6,19 +6,60 @@ public:
     int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
         static const int MOD = 1e9 + 7;
 
+        int s = locations[start];
+        int f = locations[finish];
+        sort(begin(locations), end(locations));
+        start = distance(cbegin(locations), lower_bound(cbegin(locations), cend(locations), s));
+        finish = distance(cbegin(locations), lower_bound(cbegin(locations), cend(locations), f));
+
+        vector<vector<int>> left(locations.size(), vector<int>(fuel + 1));  // left[i][f], last move is toward left to location i by f fuel
+        vector<vector<int>> right(locations.size(), vector<int>(fuel + 1));  // right[i][f], last move is toward right to location i by f fuel
+        for (int f = 1; f <= fuel; ++f) {
+            for (int j = 0; j < locations.size() - 1; ++j) {
+                int d = locations[j + 1] - locations[j];
+                if (f > d) {
+                    left[j][f] = (right[j + 1][f - d] + 2 * left[j + 1][f - d] % MOD) % MOD;
+                } else if (f == d) {
+                    left[j][f] = int(j + 1 == start);
+                }
+            }
+            for (int j = 1; j < locations.size(); ++j) {
+                int d = locations[j] - locations[j - 1];
+                if (f > d) {
+                    right[j][f] = (left[j - 1][f - d] + 2 * right[j - 1][f - d] % MOD) % MOD;
+                } else if (f == d) {
+                    right[j][f] = int(j - 1 == start);
+                }
+            }
+        }
+        int result = int(start == finish);
+        for (int f = 1; f <= fuel; ++f) {
+            result = ((result + left[finish][f]) % MOD + right[finish][f]) % MOD;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n^2 * f)
+// Space: O(n * f)
+class Solution2 {
+public:
+    int countRoutes(vector<int>& locations, int start, int finish, int fuel) {
+        static const int MOD = 1e9 + 7;
+
         vector<vector<int>> dp(locations.size(), vector<int>(fuel + 1));
-        dp[start][fuel] = 1;
-        for (int k = fuel; k >= 0; --k) {
+        dp[start][0] = 1;
+        for (int f = 1; f <= fuel; ++f) {
             for (int i = 0; i < locations.size(); ++i) {
                 for (int j = 0; j < locations.size(); ++j) {
                     if (i == j) {
                         continue;
                     }
-                    int amount = abs(locations[i] - locations[j]);
-                    if (k + amount > fuel) {
+                    int d = abs(locations[i] - locations[j]);
+                    if (f - d < 0) {
                         continue;
                     }
-                    dp[i][k] = (static_cast<int64_t>(dp[i][k]) + dp[j][k + amount]) % MOD;
+                    dp[i][f] = (static_cast<int64_t>(dp[i][f]) + dp[j][f - d]) % MOD;
                 }
             }
         }
