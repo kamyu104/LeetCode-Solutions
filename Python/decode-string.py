404966230d9f7bd599623d8926753a6c35e5d5db
kamyu <kamyu104@users.noreply.github.com>
2020-09-07 22:40:27 +0800

    Update decode-string.py

--- Python/decode-string.py
+++ Python/decode-string.py
@@ -7,22 +7,18 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        curr, nums, strs = [], [], []
-        n = 0
-
+        n, curr, nums, strs = 0, [], [], []
         for c in s:
             if c.isdigit():
-                n = n * 10 + ord(c) - ord('0')
+                n = n*10 + ord(c)-ord('0')
             elif c == '[':
                 nums.append(n)
-                n = 0
                 strs.append(curr)
-                curr = []
+                n, curr = 0, []
             elif c == ']':
-                strs[-1].extend(curr * nums.pop())
+                strs[-1].extend(curr*nums.pop())
                 curr = strs.pop()
             else:
                 curr.append(c)
-
-        return "".join(strs[-1]) if strs else "".join(curr)
+        return "".join(curr)
 
