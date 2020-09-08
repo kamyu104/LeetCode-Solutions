6f6f5f755da0bd8d1fa75cd58e536d3985a81dd9
kamyu <kamyu104@users.noreply.github.com>
2020-09-09 00:13:32 +0800

    Update robot-room-cleaner.py

--- Python/robot-room-cleaner.py
+++ Python/robot-room-cleaner.py
@@ -17,19 +17,16 @@ class Solution(object):
             robot.turnRight()
 
         def dfs(pos, robot, d, lookup):
-            if pos in lookup:
-                return
-            lookup.add(pos)
-
             robot.clean()
             for _ in directions:
-                if robot.move():
-                    dfs((pos[0]+directions[d][0],
-                         pos[1]+directions[d][1]),
-                        robot, d, lookup)
-                    goBack(robot)
+                new_pos = (pos[0]+directions[d][0],
+                           pos[1]+directions[d][1])
+                if new_pos not in lookup:
+                    lookup.add(new_pos)
+                    if robot.move():
+                        dfs(new_pos, robot, d, lookup)
+                        goBack(robot)
                 robot.turnRight()
                 d = (d+1) % len(directions)
         
         dfs((0, 0), robot, 0, set())
-
