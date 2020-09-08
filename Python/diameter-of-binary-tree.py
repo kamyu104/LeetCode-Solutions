4e73b5819bbec1a2beaad2ac3280669bb8a5c214
kamyu <kamyu104@users.noreply.github.com>
2020-09-08 22:20:41 +0800

    Update diameter-of-binary-tree.py

--- Python/diameter-of-binary-tree.py
+++ Python/diameter-of-binary-tree.py
@@ -1,18 +1,55 @@
 # Time:  O(n)
 # Space: O(h)
 
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
 class Solution(object):
     def diameterOfBinaryTree(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        return self.depth(root, 0)[1]
-    
-    def depth(self, root, diameter):
-        if not root: 
-            return 0, diameter
-        left, diameter = self.depth(root.left, diameter)
-        right, diameter = self.depth(root.right, diameter)
-        return 1 + max(left, right), max(diameter, left + right)
+        def iter_dfs(node):
+            result = 0
+            max_depth = [0]
+            stk = [(1, [node, max_depth])]
+            while stk:
+                step, params = stk.pop()
+                if step == 1:
+                    node, ret = params
+                    if not node:
+                        continue
+                    ret1, ret2 = [0], [0]
+                    stk.append((2, [node, ret1, ret2, ret]))
+                    stk.append((1, [node.right, ret2]))
+                    stk.append((1, [node.left, ret1]))
+                elif step == 2:
+                    node, ret1, ret2, ret = params
+                    result = max(result, ret1[0]+ret2[0])
+                    ret[0] = 1+max(ret1[0], ret2[0])
+            return result
+        
+        return iter_dfs(root)
 
+
+# Time:  O(n)
+# Space: O(h)
+class Solution2(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        def dfs(root):
+            if not root: 
+                return 0, 0
+            left_d, left = dfs(root.left)
+            right_d, right = dfs(root.right)
+            return max(left_d, right_d, left+right), 1+max(left, right)
+ 
+        return dfs(root)[0]
