7862147ebc7eca4bd7486148c0a6f3ba116ea3c6
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 00:54:45 +0800

    Update letter-combinations-of-a-phone-number.py

--- Python/letter-combinations-of-a-phone-number.py
+++ Python/letter-combinations-of-a-phone-number.py
@@ -1,43 +1,47 @@
 # Time:  O(n * 4^n)
 # Space: O(n)
 
+# iterative solution
 class Solution(object):
-    # @return a list of strings, [s1, s2]
     def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
         if not digits:
             return []
 
-        lookup, result = ["", "", "abc", "def", "ghi", "jkl", "mno", \
-                          "pqrs", "tuv", "wxyz"], [""]
-
+        result = [""]
+        lookup = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
         for digit in reversed(digits):
             choices = lookup[int(digit)]
             m, n = len(choices), len(result)
-            result += [result[i % n] for i in xrange(n, m * n)]
-
-            for i in xrange(m * n):
-                result[i] = choices[i / n] + result[i]
-
+            result += [result[i % n] for i in xrange(n, m*n)]
+            for i in xrange(m*n):
+                result[i] = choices[i//n] + result[i]
         return result
 
 
 # Time:  O(n * 4^n)
 # Space: O(n)
-# Recursive Solution
+# recursive solution
 class Solution2(object):
-    # @return a list of strings, [s1, s2]
     def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        def letterCombinationsRecu(result, digits, lookup, curr, n):
+            if n == len(digits):
+                result.append(curr)
+                return
+            for choice in lookup[int(digits[n])]:
+                letterCombinationsRecu(result, digits, lookup, curr+choice, n+1)
+
         if not digits:
             return []
-        lookup, result = ["", "", "abc", "def", "ghi", "jkl", "mno", \
-                          "pqrs", "tuv", "wxyz"], []
-        self.letterCombinationsRecu(result, digits, lookup, "", 0)
+        result = []
+        lookup = ["", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"]
+        letterCombinationsRecu(result, digits, lookup, "", 0)
         return result
-
-    def letterCombinationsRecu(self, result, digits, lookup, cur, n):
-        if n == len(digits):
-            result.append(cur)
-        else:
-            for choice in lookup[int(digits[n])]:
-                self.letterCombinationsRecu(result, digits, lookup, cur + choice, n + 1)
 
