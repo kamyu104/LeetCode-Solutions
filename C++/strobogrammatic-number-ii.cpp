0c54d1ff24d7a6cff5226ce84bf415439a0ec4fd
kamyu <kamyu104@users.noreply.github.com>
2020-09-09 23:30:01 +0800

    Update strobogrammatic-number-ii.cpp

--- C++/strobogrammatic-number-ii.cpp
+++ C++/strobogrammatic-number-ii.cpp
@@ -4,10 +4,42 @@
 class Solution {
 public:
     vector<string> findStrobogrammatic(int n) {
+        static const unordered_map<string, string> lookup{{"0", "0"}, {"1", "1"},
+                                                          {"6", "9"}, {"8", "8"}, 
+                                                          {"9", "6"}};
+        vector<string> result = {""};
+        if (n % 2) {
+            result = {"0", "1", "8"};
+        }
+        for (int i = n % 2; i < n; i += 2) {
+            vector<string> new_result;
+            for (const auto& num : result) {
+                for (const auto& [a, b] : lookup) {
+                    
+                    if (i != n - 2 || a != "0") {
+                        new_result.emplace_back(a + num + b);
+                    }
+                }
+            }
+            result = move(new_result);
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * 5^(n/2))
+// Space: O(n)
+class Solution2 {
+public:
+    vector<string> findStrobogrammatic(int n) {
         return findStrobogrammaticRecu(n, n);
     }
-    
+
+private:
     vector<string> findStrobogrammaticRecu(const int n, int k) {
+        static const unordered_map<string, string> lookup{{"0", "0"}, {"1", "1"},
+                                                          {"6", "9"}, {"8", "8"}, 
+                                                          {"9", "6"}};
         if (k == 0) {
             return {""};
         } else if (k == 1) {
@@ -24,9 +56,4 @@ public:
         }
         return result;
     }
-
-private:
-    const unordered_map<string, string> lookup{{"0", "0"}, {"1", "1"},
-                                               {"6", "9"}, {"8", "8"}, 
-                                               {"9", "6"}};
 };
