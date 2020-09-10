55b725153da2a921d3b88a0685e118b54a88fecb
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 22:47:55 +0800

    Update word-search-ii.cpp

--- C++/word-search-ii.cpp
+++ C++/word-search-ii.cpp
@@ -1,5 +1,5 @@
-// Time:  O(m * n * h), h is height of trie
-// Space: O(26^h)
+// Time:  O(m * n * 4 * 3^(h - 1)) ~= O(m * n * 3^h), h is the height of trie
+// Space: O(t), t is the number of nodes in trie
 
 class Solution {
 private:
