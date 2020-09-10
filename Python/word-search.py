c2eebe1b9f00cae39f52c3985dae4845551389c1
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 22:49:20 +0800

    Update word-search.py

--- Python/word-search.py
+++ Python/word-search.py
@@ -1,5 +1,5 @@
-# Time:  O(m * n * l)
-# Space: O(l)
+# Time:  O(m * n * 4 * 3^(h - 1)) ~= O(m * n * 3^h), h is the height of trie
+# Space: O(t), t is the number of nodes in trie
 
 class Solution(object):
     # @param board, a list of lists of 1 length string
