c8c487bf62e23ce49186073fdf67f431444dbe01
kamyu <kamyu104@users.noreply.github.com>
2020-09-11 00:15:35 +0800

    Update search-for-a-range.cpp

--- C++/search-for-a-range.cpp
+++ C++/search-for-a-range.cpp
@@ -4,10 +4,11 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        const auto start = lower_bound(nums.cbegin(), nums.cend(), target);
-        const auto end = upper_bound(nums.cbegin(), nums.cend(), target);
-        if (start != nums.cend() && *start == target) {
-            return {start - nums.cbegin(), end - nums.cbegin() - 1};
+        const auto start = lower_bound(cbegin(nums), cend(nums), target);
+        const auto end = upper_bound(cbegin(nums), cend(nums), target);
+        if (start != cend(nums) && *start == target) {
+            return {static_cast<int>(start - cbegin(nums)),
+                    static_cast<int>(end - cbegin(nums) - 1)};
         }
         return {-1, -1};
     }
@@ -18,18 +19,58 @@ public:
     vector<int> searchRange(vector<int> &nums, int target) {
         const int begin = lower_bound(nums, target);
         const int end = upper_bound(nums, target);
-
-        if (begin < nums.size() && nums[begin] == target) {
+        if (begin < size(nums) && nums[begin] == target) {
             return {begin, end - 1};
         }
-
         return {-1, -1};
     }
 
 private:
     int lower_bound(vector<int> &nums, int target) {
         int left = 0;
-        int right = nums.size();
+        int right = size(nums) - 1;
+        // Find min left s.t. A[left] >= target.
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+    int upper_bound(vector<int> &nums, int target) {
+        int left = 0;
+        int right = size(nums) - 1;
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
+
+class Solution3 {
+public:
+    vector<int> searchRange(vector<int> &nums, int target) {
+        const int begin = lower_bound(nums, target);
+        const int end = upper_bound(nums, target);
+        if (begin < size(nums) && nums[begin] == target) {
+            return {begin, end - 1};
+        }
+        return {-1, -1};
+    }
+
+private:
+    int lower_bound(vector<int> &nums, int target) {
+        int left = 0;
+        int right = size(nums);
         // Find min left s.t. A[left] >= target.
         while (left < right) {
             const auto mid = left + (right - left) / 2;
@@ -44,7 +85,7 @@ private:
 
     int upper_bound(vector<int> &nums, int target) {
         int left = 0;
-        int right = nums.size();
+        int right = size(nums);
         // Find min left s.t. A[left] > target.
         while (left < right) {
             const auto mid = left + (right - left) / 2;
