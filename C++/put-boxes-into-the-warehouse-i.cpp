f71f692fe91d328ac4d1fb41a51bd51b83675c9b
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 22:21:31 +0800

    Update put-boxes-into-the-warehouse-i.cpp

--- C++/put-boxes-into-the-warehouse-i.cpp
+++ C++/put-boxes-into-the-warehouse-i.cpp
@@ -1,4 +1,4 @@
-// Time:  O(nlogn + m)
+// Time:  O(nlogn)
 // Space: O(1)
 
 class Solution {
@@ -10,7 +10,7 @@ public:
             if (h > warehouse[result]) {
                 continue;
             }
-            if (++result == warehouse.size()) {
+            if (++result == size(warehouse)) {
                 break;
             }
         }
@@ -24,16 +24,16 @@ class Solution2 {
 public:
     int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
         sort(begin(boxes), end(boxes));
-        for (int i = 1; i < warehouse.size(); ++i) {
+        for (int i = 1; i < size(warehouse); ++i) {
             warehouse[i] = min(warehouse[i], warehouse[i - 1]);
         }
         int result = 0, curr = 0;
-        for (int i = warehouse.size() - 1; i >= 0; --i) {
+        for (int i = size(warehouse)- 1; i >= 0; --i) {
             if (boxes[curr] > warehouse[i]) {
                 continue;
             }
             ++result;
-            if (++curr == boxes.size()) {
+            if (++curr == size(boxes)) {
                 break;
             }
         }
