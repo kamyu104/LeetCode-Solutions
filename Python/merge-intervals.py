487b295a656549c26515c6790f9f648b2be34794
kamyu <kamyu104@users.noreply.github.com>
2020-09-11 00:36:51 +0800

    Update merge-intervals.py

--- Python/merge-intervals.py
+++ Python/merge-intervals.py
@@ -1,32 +1,17 @@
 # Time:  O(nlogn)
 # Space: O(1)
 
-class Interval(object):
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-
-    def __repr__(self):
-        return "[{}, {}]".format(self.start, self.end)
-
-
 class Solution(object):
     def merge(self, intervals):
         """
-        :type intervals: List[Interval]
-        :rtype: List[Interval]
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
         """
-        if not intervals:
-            return intervals
-
-        intervals.sort(key=lambda x: x.start)
-        iterator = iter(intervals)
-        result = [next(iterator)]
-        for current in iterator:
-            prev = result[-1]
-            if current.start <= prev.end:
-                prev.end = max(current.end, prev.end)
+        intervals.sort()
+        result = []
+        for interval in intervals:
+            if not result or interval[0] > result[-1][1]:
+                result.append(interval)
             else:
-                result.append(current)
-
+                result[-1][1] = max(result[-1][1], interval[1])
         return result
