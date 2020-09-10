d32ec857062d7b5676e2d6dc232e09f2c760bda0
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 22:16:06 +0800

    Create put-boxes-into-the-warehouse-ii.cpp

---
+++ C++/put-boxes-into-the-warehouse-ii.cpp
@@ -0,0 +1,21 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+class Solution {
+public:
+    int maxBoxesInWarehouse(vector<int>& boxes, vector<int>& warehouse) {
+        sort(begin(boxes), end(boxes), greater<int>());
+        int left = 0, right = size(warehouse) - 1;
+        for (const auto& h : boxes) {
+            if (h <= warehouse[left]) {
+                ++left;
+            } else if (h <= warehouse[right]) {
+                --right;
+            }
+            if (left > right) {
+                break;
+            }
+        }
+        return left + (size(warehouse) - 1 - right);
+    }
+};
