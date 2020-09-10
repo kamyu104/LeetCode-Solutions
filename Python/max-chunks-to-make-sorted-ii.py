80be95e5b10afd96bd47f97ecd0ca4185b83cd0a
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 19:48:46 +0800

    Update max-chunks-to-make-sorted-ii.py

--- Python/max-chunks-to-make-sorted-ii.py
+++ Python/max-chunks-to-make-sorted-ii.py
@@ -10,10 +10,7 @@ class Solution(object):
         """
         result, increasing_stk = 0, []
         for num in arr:
-            if not increasing_stk or increasing_stk[-1] <= num:
-                increasing_stk.append(num)
-                continue
-            max_num = increasing_stk.pop()
+            max_num = num if not increasing_stk else max(increasing_stk[-1], num)
             while increasing_stk and increasing_stk[-1] > num:
                 increasing_stk.pop()
             increasing_stk.append(max_num)
