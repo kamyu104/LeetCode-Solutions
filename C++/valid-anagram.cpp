e6b0b20cc40eb0bdcc52acc9399a7886c5da686b
kamyu <kamyu104@users.noreply.github.com>
2020-09-11 00:58:36 +0800

    Update valid-anagram.cpp

--- C++/valid-anagram.cpp
+++ C++/valid-anagram.cpp
@@ -4,39 +4,35 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        if (size(s) != size(t)) {
             return false;
         }
 
         unordered_map<char, int> count;
-
         for (const auto& c: s) {
-            ++count[tolower(c)];
+            ++count[c];
         }
-
         for (const auto& c: t) {
-            --count[tolower(c)];
-            if (count[tolower(c)] < 0) {
+            --count[c];
+            if (count[c] < 0) {
                 return false;
             }
         }
-
         return true;
     }
 };
 
 // Time:  O(nlogn)
-// Space: O(n)
+// Space: O(1)
 class Solution2 {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) {
+        if (size(s) != size(t)) {
             return false;
         }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
+        sort(begin(s), end(s));
+        sort(begin(t), end(t));
         return s == t;
     }
 };
