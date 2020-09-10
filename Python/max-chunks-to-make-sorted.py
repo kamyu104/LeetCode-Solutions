516754341a1dfb9b3463d12ae6ce9c54da6dcc33
kamyu <kamyu104@users.noreply.github.com>
2020-09-10 14:07:08 +0800

    Update max-chunks-to-make-sorted.py

--- Python/max-chunks-to-make-sorted.py
+++ Python/max-chunks-to-make-sorted.py
@@ -14,3 +14,23 @@ class Solution(object):
                 result += 1
         return result
 
+    
+# Time:  O(n)
+# Space: O(n)
+# mono stack solution
+class Solution2(object):
+    def maxChunksToSorted(self, arr):
+        """
+        :type arr: List[int]
+        :rtype: int
+        """
+        result, increasing_stk = 0, []
+        for num in arr:
+            if not increasing_stk or increasing_stk[-1] <= num:
+                increasing_stk.append(num)
+                continue
+            max_num = increasing_stk.pop()
+            while increasing_stk and increasing_stk[-1] > num:
+                increasing_stk.pop()
+            increasing_stk.append(max_num)
+        return len(increasing_stk)
