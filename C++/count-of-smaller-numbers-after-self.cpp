59361357bbe64eb55362de6b6e5c85e88e461846
kamyu <kamyu104@users.noreply.github.com>
2020-09-12 00:54:54 +0800

    Update count-of-smaller-numbers-after-self.cpp

--- C++/count-of-smaller-numbers-after-self.cpp
+++ C++/count-of-smaller-numbers-after-self.cpp
@@ -1,8 +1,104 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
-// BST solution. (40ms)
+// BIT solution. (24ms)
 class Solution {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        // Get the place (position in the ascending order) of each number.
+        vector<int> idx(size(nums));
+        iota(begin(idx), end(idx), 0);
+        vector<int> lookup(size(nums));
+        sort(begin(idx), end(idx),
+             [&nums](const auto& a, const auto& b) {
+                 return nums[a] == nums[b] ? a < b : nums[a] < nums[b];
+             });
+        for (int i = 0; i < size(idx); ++i) {
+            lookup[idx[i]] = i;
+        }
+        // Count the smaller elements after the number.
+        BIT bit(size(nums));
+        vector<int> result(size(nums));
+        for (int i = size(nums) - 1; i >= 0; --i) {
+            result[i] = bit.query(lookup[i] - 1);
+            bit.add(lookup[i], 1);
+        }
+        return result;
+    }
+
+private:
+    class BIT {
+    public:
+        BIT(int n) : bit_(n + 1) {  // 0-indexed
+        }
+        
+        void add(int i, int val) {
+            ++i;
+            for (; i < bit_.size(); i += lower_bit(i)) {
+                bit_[i] += val;
+            }
+        }
+
+        int query(int i) const {
+            ++i;
+            int total = 0;
+            for (; i > 0; i -= lower_bit(i)) {
+                total += bit_[i];
+            }
+            return total;
+        }
+    
+    private:
+        int lower_bit(int i) const {
+            return i & -i;
+        }
+        
+        vector<int> bit_;
+    };
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// Divide and Conquer solution. (80ms)
+class Solution2 {
+public:
+    vector<int> countSmaller(vector<int>& nums) {
+        vector<int> counts(nums.size());
+        vector<pair<int, int>> num_idxs;
+        for (int i = 0; i < nums.size(); ++i) {
+            num_idxs.emplace_back(nums[i], i);
+        }
+        countAndMergeSort(&num_idxs, 0, num_idxs.size() - 1, &counts);
+        return counts;
+    }
+
+    void countAndMergeSort(vector<pair<int, int>> *num_idxs, int start, int end, vector<int> *counts) {
+        if (end - start <= 0) {  // The number of range [start, end] of which size is less than 2 doesn't need sort.
+            return;
+        }
+        int mid = start + (end - start) / 2;
+        countAndMergeSort(num_idxs, start, mid, counts);
+        countAndMergeSort(num_idxs, mid + 1, end, counts);
+
+        int r = mid + 1;
+        vector<pair<int, int>> tmp;
+        for (int i = start; i <= mid; ++i) {
+            // Merge the two sorted arrays into tmp.
+            while (r <= end && (*num_idxs)[r].first < (*num_idxs)[i].first) {
+                tmp.emplace_back((*num_idxs)[r++]);
+            }
+            tmp.emplace_back((*num_idxs)[i]);
+            (*counts)[(*num_idxs)[i].second] += r - (mid + 1);
+        }
+        // Copy tmp back to num_idxs.
+        copy(tmp.begin(), tmp.end(), num_idxs->begin() + start);
+    }
+};
+
+// Time:  O(nlogn) ~ O(n^2)
+// Space: O(n)
+// BST solution. (40ms)
+class Solution3 {
 public:
     class BSTreeNode {
     public:
@@ -77,86 +173,5 @@ public:
             }
         }
         return 0;
-    }
-};
-
-// Time:  O(nlogn)
-// Space: O(n)
-// BIT solution. (56ms)
-class Solution2 {
-public:
-    vector<int> countSmaller(vector<int>& nums) {
-        // Get the place (position in the ascending order) of each number.
-        vector<int> sorted_nums(nums), places(nums.size());
-        sort(sorted_nums.begin(), sorted_nums.end());
-        for (int i = 0; i < nums.size(); ++i) {
-            places[i] = 
-                lower_bound(sorted_nums.begin(), sorted_nums.end(), nums[i]) -
-                sorted_nums.begin();
-        }
-        // Count the smaller elements after the number.
-        vector<int> bit(nums.size() + 1), ans(nums.size());
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            ans[i] = query(bit, places[i]);
-            add(bit, places[i] + 1, 1);
-        }
-        return ans;
-    }
-
-private:
-    void add(vector<int>& bit, int i, int val) {
-        for (; i < bit.size(); i += lower_bit(i)) {
-            bit[i] += val;
-        }
-    }
-
-    int query(const vector<int>& bit, int i) {
-        int sum = 0;
-        for (; i > 0; i -= lower_bit(i)) {
-            sum += bit[i];
-        }
-        return sum;
-    }
-
-    int lower_bit(int i) {
-        return i & -i;
-    }
-};
-
-// Time:  O(nlogn)
-// Space: O(n)
-// Divide and Conquer solution. (80ms)
-class Solution3 {
-public:
-    vector<int> countSmaller(vector<int>& nums) {
-        vector<int> counts(nums.size());
-        vector<pair<int, int>> num_idxs;
-        for (int i = 0; i < nums.size(); ++i) {
-            num_idxs.emplace_back(nums[i], i);
-        }
-        countAndMergeSort(&num_idxs, 0, num_idxs.size() - 1, &counts);
-        return counts;
-    }
-
-    void countAndMergeSort(vector<pair<int, int>> *num_idxs, int start, int end, vector<int> *counts) {
-        if (end - start <= 0) {  // The number of range [start, end] of which size is less than 2 doesn't need sort.
-            return;
-        }
-        int mid = start + (end - start) / 2;
-        countAndMergeSort(num_idxs, start, mid, counts);
-        countAndMergeSort(num_idxs, mid + 1, end, counts);
-
-        int r = mid + 1;
-        vector<pair<int, int>> tmp;
-        for (int i = start; i <= mid; ++i) {
-            // Merge the two sorted arrays into tmp.
-            while (r <= end && (*num_idxs)[r].first < (*num_idxs)[i].first) {
-                tmp.emplace_back((*num_idxs)[r++]);
-            }
-            tmp.emplace_back((*num_idxs)[i]);
-            (*counts)[(*num_idxs)[i].second] += r - (mid + 1);
-        }
-        // Copy tmp back to num_idxs.
-        copy(tmp.begin(), tmp.end(), num_idxs->begin() + start);
     }
 };
