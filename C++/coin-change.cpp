43580ccf4cbb5be67385da3da45aa52768315441
kamyu <kamyu104@users.noreply.github.com>
2020-09-12 23:02:21 +0800

    Update coin-change.cpp

--- C++/coin-change.cpp
+++ C++/coin-change.cpp
@@ -5,17 +5,17 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-        vector<int> amounts(amount + 1, numeric_limits<int>::max());
-        amounts[0] = 0;
+        vector<int> dp(amount + 1, numeric_limits<int>::max());
+        dp[0] = 0;
         for (int i = 0; i <= amount; ++i) {
-            if (amounts[i] != numeric_limits<int>::max()) {
+            if (dp[i] != numeric_limits<int>::max()) {
                 for (const auto& coin : coins) {
                     if (coin <= numeric_limits<int>::max() - i && i + coin <= amount) {
-                        amounts[i + coin] = min(amounts[i + coin], amounts[i] + 1);
+                        dp[i + coin] = min(dp[i + coin], dp[i] + 1);
                     }
                 }
             }
         }
-        return amounts[amount] == numeric_limits<int>::max() ? -1 : amounts[amount];
+        return dp[amount] == numeric_limits<int>::max() ? -1 : dp[amount];
     }
 };
