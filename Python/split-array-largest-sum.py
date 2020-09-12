b85fa207e51b4793f81e98db2e10d72330dfa9d6
kamyu <kamyu104@users.noreply.github.com>
2020-09-12 23:19:46 +0800

    Update split-array-largest-sum.py

--- Python/split-array-largest-sum.py
+++ Python/split-array-largest-sum.py
@@ -1,7 +1,6 @@
 # Time:  O(nlogs), s is the sum of nums
 # Space: O(1)
 
-# Minimize the largest subarray sum.
 class Solution(object):
     def splitArray(self, nums, m):
         """
@@ -9,7 +8,7 @@ class Solution(object):
         :type m: int
         :rtype: int
         """
-        def canSplit(nums, m, s):
+        def check(nums, m, s):
             cnt, curr_sum = 1, 0
             for num in nums:
                 curr_sum += num
@@ -20,73 +19,9 @@ class Solution(object):
 
         left, right = max(nums), sum(nums)
         while left <= right:
-            mid = left + (right - left) / 2
+            mid = left + (right - left) // 2
             if canSplit(nums, m, mid):
                 right = mid - 1
             else:
                 left = mid + 1
         return left
-
-
-# Time:  O(nlogs), s is the sum of nums
-# Space: O(1)
-
-# Maximize the smallest subarray sum.
-class SolutionFindMaxMin(object):
-    def splitArray(self, nums, m):
-        """
-        :type nums: List[int]
-        :type m: int
-        :rtype: int
-        """
-        def canSplit(nums, m, s):
-            cnt, curr_sum = 1, 0
-            for num in nums:
-                curr_sum += num
-                if curr_sum > s:
-                    curr_sum = 0
-                    cnt += 1
-            return cnt <= m
-
-        left, right = min(nums), sum(nums)
-        while left <= right:
-            mid = left + (right - left) / 2
-            x = canSplit(nums, m, mid)
-            if x:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left
-
-    
-# Time:  O(nlogs), s is the sum of nums
-# Space: O(1)
-
-# Maximize the smallest subarray sum.
-class SolutionFindMaxMin2(object):
-    def splitArray(self, nums, m):
-        """
-        :type nums: List[int]
-        :type m: int
-        :rtype: int
-        """
-        def canSplit(nums, m, s):
-            cnt, curr_sum = 0, 0
-            for num in nums:
-                curr_sum += num
-                if curr_sum >= s:
-                    curr_sum = 0
-                    cnt += 1
-            return cnt < m
-
-        left, right = min(nums), sum(nums)
-        for i in xrange(left, right+1):
-            canSplit(nums, m, i)
-        while left <= right:
-            mid = left + (right - left) / 2
-            x = canSplit(nums, m, mid)
-            if x:
-                right = mid - 1
-            else:
-                left = mid + 1
-        return left - 1
