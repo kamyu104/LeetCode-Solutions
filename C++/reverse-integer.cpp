ce703d78428d8399df63465a19ce90f91ea4453d
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 02:30:42 +0800

    Update reverse-integer.cpp

--- C++/reverse-integer.cpp
+++ C++/reverse-integer.cpp
@@ -10,7 +10,7 @@ public:
                 (result == numeric_limits<int>::max() / 10 && x % 10 > numeric_limits<int>::max() % 10)) {
                 return 0;
             }
-            if (result < numeric_limits<int>::min() /10 ||
+            if (result < numeric_limits<int>::min() / 10 ||
                 (result == numeric_limits<int>::min() / 10 && x % 10 < numeric_limits<int>::min() % 10)) {
                 return 0;
             }
