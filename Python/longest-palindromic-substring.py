c4091420e38315525e370613e49aff20a0bcf916
kamyu <kamyu104@users.noreply.github.com>
2020-09-12 20:11:48 +0800

    Update longest-palindromic-substring.py

--- Python/longest-palindromic-substring.py
+++ Python/longest-palindromic-substring.py
@@ -40,3 +40,24 @@ class Solution(object):
         return s[start : start + P[max_i]]
 
 
+# Time:  O(n^2)
+# Space: O(1)
+class Solution2(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def expand(s, left, right):
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return (right-left+1)-2
+        
+        left, right = -1, -2
+        for i in xrange(len(s)):
+            l = max(expand(s, i, i), expand(s, i, i+1))
+            if l > right-left+1:
+                right = i+l//2
+                left = right-l+1
+        return s[left:right+1] if left >= 0 else ""
