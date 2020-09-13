47befbc0d658a346d07b333d6849a8be7ce98539
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 19:50:22 +0800

    Update guess-the-word.cpp

--- C++/guess-the-word.cpp
+++ C++/guess-the-word.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n^2)
+// Time:  O(n)
 // Space: O(n)
 
 /**
@@ -10,6 +10,62 @@
  * };
  */
 class Solution {
+public:
+    void findSecretWord(vector<string>& wordlist, Master& master) {
+        vector<int> possible(size(wordlist));
+        iota(begin(possible), end(possible), 0);
+        int n = 0;
+        while (n < 6) {
+            auto guess = find_guess_with_most_frequent_chars(wordlist, possible);
+            n = master.guess(wordlist[guess]);
+            vector<int> new_possible;
+            for (const auto& j : possible) {
+                if (match(wordlist[guess], wordlist[j]) == n) {
+                    new_possible.emplace_back(j);
+                }
+            }
+            possible = move(new_possible);
+        }
+    }
+
+private:
+    int find_guess_with_most_frequent_chars(
+        const vector<string>& wordlist,
+        const vector<int>& possible) {
+        vector<int> count(26);
+        for (int i = 0; i < 6; ++i) {
+            for (const auto& p : possible) {
+                ++count[wordlist[p][i] - 'a'];
+            }
+        }
+        int guess = 0, max_score = 0;
+        for (const auto& p : possible) {
+            int score = 0;
+            for (int i = 0; i < 6; ++i) {
+                score += count[wordlist[p][i] - 'a'];
+            }
+            if (score > max_score) {
+                max_score = score;
+                guess = p;
+            }
+        }
+        return guess;
+    }
+
+    int match(const string& a, const string& b) {
+        int matches = 0;
+        for (int i = 0; i < size(a); ++i) {
+            if (a[i] == b[i]) {
+                ++matches;
+            }
+        }
+        return matches;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+class Solution2 {
 public:
     void findSecretWord(vector<string>& wordlist, Master& master) {
         vector<vector<int>> H(size(wordlist), vector<int>(size(wordlist)));
@@ -75,7 +131,7 @@ private:
 
 // Time:  O(n^2)
 // Space: O(n)
-class Solution2 {
+class Solution3 {
 public:
     void findSecretWord(vector<string>& wordlist, Master& master) {
         vector<vector<int>> H(size(wordlist), vector<int>(size(wordlist)));
@@ -114,7 +170,7 @@ private:
                     groups[H[guess][j]].emplace_back(j);
                 }
             }
-            int max_group_i = 0;
+            int max_group_i = 0;  // assumed the size of the other groups equals to the size of 0-group
             if (size(groups[max_group_i]) < min_max_size) {
                 min_max_size = size(groups[max_group_i]);
                 best_guess = guess;
