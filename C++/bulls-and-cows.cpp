567a376e82b8080eacfb7009a20ea883b552927a
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 16:50:20 +0800

    Update bulls-and-cows.cpp

--- C++/bulls-and-cows.cpp
+++ C++/bulls-and-cows.cpp
@@ -5,17 +5,15 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        unordered_map<char, int> s_lookup, g_lookup;
+        unordered_map<char, int> lookup;
         int A = 0, B = 0;
-        const int n = min(secret.length(), guess.length());
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < size(secret); ++i) {
             const char s = secret[i];
             const char g = guess[i];
             if (s == g) {
                 ++A;
             } else {
-                (s_lookup[g] > 0) ? --s_lookup[g], ++B : ++g_lookup[g];
-                (g_lookup[s] > 0) ? --g_lookup[s], ++B : ++s_lookup[s]; 
+                B += int(lookup[s]++ < 0) + int(lookup[g]-- > 0);
              }
         }
         return to_string(A).append("A").append(to_string(B)).append("B");
@@ -37,8 +35,7 @@ public:
                 ++B;
             }
         }
-        const int n = min(secret.length(), guess.length());
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < size(secret); ++i) {
             if (secret[i] == guess[i]) {
                 ++A, --B;
             }
