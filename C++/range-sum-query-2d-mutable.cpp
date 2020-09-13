fc1c532c2469d0a096f54e6df2ec67c1ba2a3d5a
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 17:28:35 +0800

    Update range-sum-query-2d-mutable.cpp

--- C++/range-sum-query-2d-mutable.cpp
+++ C++/range-sum-query-2d-mutable.cpp
@@ -1,12 +1,81 @@
-// Time:  ctor:   O(m * n),
-//        update: O(logm + logn),
-//        query:  O(logm + logn)
+// Time:  ctor:   O(m * n)
+//        update: O(logm * logn)
+//        query:  O(logm * logn)
 // Space: O(m * n)
 
-// Segment Tree solution.
+// 2d - Binary Indexed Tree (BIT) solution.
 class NumMatrix {
 public:
     NumMatrix(vector<vector<int>> &matrix) : matrix_(matrix) {
+        if (matrix_.empty()) {
+            return;
+        }
+        bit_ = vector<vector<int>>(matrix_.size() + 1,
+                                   vector<int>(matrix_[0].size() + 1));
+        for (int i = 1; i < bit_.size(); ++i) {
+            for (int j = 1; j < bit_[0].size(); ++j) {
+                bit_[i][j] = matrix[i - 1][j - 1] + bit_[i - 1][j] +
+                             bit_[i][j - 1] - bit_[i - 1][j - 1];
+            }
+        }
+        for (int i = bit_.size() - 1; i >= 1; --i) {
+            for (int j = bit_[0].size() - 1; j >= 1; --j) {
+                int last_i = i - lower_bit(i), last_j = j - lower_bit(j);
+                bit_[i][j] = bit_[i][j] - bit_[i][last_j] -
+                             bit_[last_i][j] + bit_[last_i][last_j];
+            }
+        }
+    }
+
+    void update(int row, int col, int val) {
+        if (val - matrix_[row][col]) {
+            add(row, col, val - matrix_[row][col]);
+            matrix_[row][col] = val;
+        }
+    }
+
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sum(row2, col2) - sum(row2, col1 - 1) -
+               sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
+    }
+
+private:
+    vector<vector<int>> &matrix_;
+    vector<vector<int>> bit_;
+
+    int sum(int row, int col) {
+        ++row, ++col;
+        int sum = 0;
+        for (int i = row; i > 0; i -= lower_bit(i)) {
+            for (int j = col; j > 0; j -= lower_bit(j)) {
+                sum += bit_[i][j];
+            }
+        }
+        return sum;
+    }
+
+    void add(int row, int col, int val) {
+        ++row, ++col;
+        for (int i = row; i <= matrix_.size(); i += lower_bit(i)) {
+            for (int j = col; j <= matrix_[0].size(); j += lower_bit(j)) {
+                bit_[i][j] += val;
+            }
+        }
+    }
+
+    inline int lower_bit(int i) {
+        return i & -i;
+    }
+};
+
+// Time:  ctor:   O(m * n * (logm + logn)),
+//        update: O(logm + logn),
+//        query:  O(logm + logn)
+// Space: O(m * n)
+// Quad Segment Tree solution.
+class NumMatrix2 {
+public:
+    NumMatrix2(vector<vector<int>> &matrix) : matrix_(matrix) {
         if (!matrix.empty() && !matrix[0].empty()) {
             const int m = matrix.size();
             const int n =  matrix[0].size();
@@ -121,76 +190,6 @@ private:
         return sum;
     }
 };
-
-// Time:  ctor:   O(m * n)
-//        update: O(logm * logn)
-//        query:  O(logm * logn)
-// Space: O(m * n)
-// Binary Indexed Tree (BIT) solution.
-class NumMatrix2 {
-public:
-    NumMatrix(vector<vector<int>> &matrix) : matrix_(matrix) {
-        if (matrix_.empty()) {
-            return;
-        }
-        bit_ = vector<vector<int>>(matrix_.size() + 1,
-                                   vector<int>(matrix_[0].size() + 1));
-        for (int i = 1; i < bit_.size(); ++i) {
-            for (int j = 1; j < bit_[0].size(); ++j) {
-                bit_[i][j] = matrix[i - 1][j - 1] + bit_[i - 1][j] +
-                             bit_[i][j - 1] - bit_[i - 1][j - 1];
-            }
-        }
-        for (int i = bit_.size() - 1; i >= 1; --i) {
-            for (int j = bit_[0].size() - 1; j >= 1; --j) {
-                int last_i = i - lower_bit(i), last_j = j - lower_bit(j);
-                bit_[i][j] = bit_[i][j] - bit_[i][last_j] -
-                             bit_[last_i][j] + bit_[last_i][last_j];
-            }
-        }
-    }
-
-    void update(int row, int col, int val) {
-        if (val - matrix_[row][col]) {
-            add(row, col, val - matrix_[row][col]);
-            matrix_[row][col] = val;
-        }
-    }
-
-    int sumRegion(int row1, int col1, int row2, int col2) {
-        return sum(row2, col2) - sum(row2, col1 - 1) -
-               sum(row1 - 1, col2) + sum(row1 - 1, col1 - 1);
-    }
-
-private:
-    vector<vector<int>> &matrix_;
-    vector<vector<int>> bit_;
-
-    int sum(int row, int col) {
-        ++row, ++col;
-        int sum = 0;
-        for (int i = row; i > 0; i -= lower_bit(i)) {
-            for (int j = col; j > 0; j -= lower_bit(j)) {
-                sum += bit_[i][j];
-            }
-        }
-        return sum;
-    }
-
-    void add(int row, int col, int val) {
-        ++row, ++col;
-        for (int i = row; i <= matrix_.size(); i += lower_bit(i)) {
-            for (int j = col; j <= matrix_[0].size(); j += lower_bit(j)) {
-                bit_[i][j] += val;
-            }
-        }
-    }
-
-    inline int lower_bit(int i) {
-        return i & -i;
-    }
-};
-
 
 // Your NumMatrix object will be instantiated and called as such:
 // NumMatrix numMatrix(matrix);
