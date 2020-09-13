551ed6b137adfda2256d2b6974f786151d1e0ce8
kamyu <kamyu104@users.noreply.github.com>
2020-09-13 15:09:15 +0800

    Update check-if-string-is-transformable-with-substring-sort-operations.cpp

--- C++/check-if-string-is-transformable-with-substring-sort-operations.cpp
+++ C++/check-if-string-is-transformable-with-substring-sort-operations.cpp
@@ -4,21 +4,21 @@
 class Solution {
 public:
     bool isTransformable(string s, string t) {
-        vector<vector<int>> idx(10);
+        vector<vector<int>> idxs(10);
         for (int i = size(s) - 1; i >= 0; --i) {
-            idx[s[i] - '0'].emplace_back(i);
+            idxs[s[i] - '0'].emplace_back(i);
         }
         for (const auto& c : t) {
             int d = c - '0';
-            if (empty(idx[d])) {
+            if (empty(idxs[d])) {
                 return false;
             }
             for (int i = 0; i < d; ++i) {  // a char can be moved left to the current position if it meets no smaller one
-                if (!empty(idx[i]) && idx[i].back() < idx[d].back()) {
+                if (!empty(idxs[i]) && idxs[i].back() < idxs[d].back()) {
                     return false;
                 }
             }
-            idx[d].pop_back();
+            idxs[d].pop_back();
         }
         return true;
     }
