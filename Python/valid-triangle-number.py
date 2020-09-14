592c95c033b16cddaa79235380958af71b02913f
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 07:03:57 +0800

    Update valid-triangle-number.py

--- Python/valid-triangle-number.py
+++ Python/valid-triangle-number.py
@@ -9,6 +9,27 @@ class Solution(object):
         """
         result = 0
         nums.sort()
+        for i in reversed(xrange(2, len(nums))):
+            left, right = 0, i-1
+            while left < right:
+                if nums[left]+nums[right] > nums[i]:
+                    result += right-left
+                    right -= 1
+                else:
+                    left += 1
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(1)
+class Solution2(object):
+    def triangleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        nums.sort()
         for i in xrange(len(nums)-2):
             if nums[i] == 0:
                 continue
