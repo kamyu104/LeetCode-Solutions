f9097ab1fcd7cf4aafc73839f344682ff03242e0
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 07:00:30 +0800

    Update valid-triangle-number.cpp

--- C++/valid-triangle-number.cpp
+++ C++/valid-triangle-number.cpp
@@ -5,6 +5,28 @@ class Solution {
 public:
     int triangleNumber(vector<int>& nums) {
         int result = 0;
+        sort(begin(nums), end(nums));
+        for (int i = size(nums) - 1; i >= 2; --i) {
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] > nums[i]) {
+                    result += right - left;
+                    --right;
+                } else {
+                    ++left;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(1)
+class Solution2 {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int result = 0;
         sort(nums.begin(), nums.end());
         for (int i = 0; i + 2 < nums.size(); ++i) {
             if (nums[i] == 0) {
@@ -21,4 +43,3 @@ public:
         return result;
     }
 };
-
