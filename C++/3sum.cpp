e2ea1085b23b1777994388c7bde09b6787c70be6
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 07:21:33 +0800

    Update 3sum.cpp

--- C++/3sum.cpp
+++ C++/3sum.cpp
@@ -8,34 +8,31 @@ public:
      * @return : Find all unique triplets in the array which gives the sum of zero.
      */
     vector<vector<int>> threeSum(vector<int> &nums) {
-        vector<vector<int>> ans;
-        const int target = 0;
-
-        // Make nums in increasing order. Time: O(nlogn)
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < static_cast<int>(nums.size()) - 2; ++i) {
-            if (i == 0 || nums[i] != nums[i - 1]) {  // Skip duplicated.
-                for (int j = i + 1, k = nums.size() - 1; j < k; ) {  // Time: O(n) for each i.
-                    if (j - 1 > i  && nums[j] == nums[j - 1]) {  // Skip duplicated.
-                        ++j;
-                    } else if (k + 1 < nums.size() && nums[k] == nums[k + 1]) {  // Skip duplicated.
-                        --k;
-                    } else {
-                        const auto sum = nums[i] + nums[j] + nums[k];
-                        if (sum > target) {  // Should decrease sum.
-                            --k;
-                        } else if (sum < target) {  // Should increase sum.
-                            ++j;
-                        } else {
-                            ans.push_back({nums[i], nums[j], nums[k]});
-                            ++j, --k;
-                        }
+        sort(begin(nums), end(nums));
+        vector<vector<int>> result;
+        for (int i = size(nums) - 1; i >= 2; --i) {
+            if (i + 1 < size(nums) && nums[i] == nums[i + 1]) {
+                continue;
+            }
+            const auto& target = -nums[i];
+            int left = 0, right = i - 1;
+            while (left < right) {
+                if (nums[left] + nums[right] < target) {
+                    ++left;
+                } else if (nums[left] + nums[right] > target) {
+                    --right;
+                } else {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        ++left;
                     }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        --right;
+                    }
+                    ++left; --right;
                 }
             }
         }
-
-        return ans;
+        return result;
     }
 };
