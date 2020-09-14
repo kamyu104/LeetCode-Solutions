35969e9d6457a9b717b5c322dee2e597471aa881
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 07:33:26 +0800

    Update 3sum.py

--- Python/3sum.py
+++ Python/3sum.py
@@ -21,12 +21,12 @@ class Solution(object):
                     right -= 1
                 else:
                     result.append([nums[left], nums[right], nums[i]])
-                    while left < right and nums[left] == nums[left+1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right-1]:
-                        right -= 1
                     left += 1
                     right -= 1
+                    while left < right and nums[left] == nums[left-1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right+1]:
+                        right -= 1
         return result
 
 
