65aff7c5aabdefd54ce65704b9912b046cf5d935
kamyu <kamyu104@users.noreply.github.com>
2020-09-16 00:21:59 +0800

    Update my-calendar-iii.cpp

--- C++/my-calendar-iii.cpp
+++ C++/my-calendar-iii.cpp
@@ -1,7 +1,29 @@
-// Time:  O(n^2)
+// Time:  O(nlogn) ~ O(n^2)
 // Space: O(n)
 
-class MyCalendarThree {
+class MyCalendarThree{
+public:
+    MyCalendarThree() {
+        events_.emplace(-1, 0);
+    }
+    
+    int book(int start, int end) {
+        auto sit = events_.emplace(start, prev(events_.upper_bound(start))->second).first;
+        auto eit = events_.emplace(end, prev(events_.upper_bound(end))->second).first;
+        for (; sit != eit; ++sit) {
+            count_ = max(count_, ++(sit->second));
+        }
+        return count_;
+    }
+    
+private:
+    map<int, int> events_;
+    int count_ = 0;
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+class MyCalendarThree2 {
 public:
     MyCalendarThree() {
         
