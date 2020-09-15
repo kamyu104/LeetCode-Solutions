b86835e272d1704d00de73e00c949f42820c39b2
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 08:37:38 +0800

    Update reverse-bits.cpp

--- C++/reverse-bits.cpp
+++ C++/reverse-bits.cpp
@@ -1,7 +1,21 @@
-// Time:  O(logn) = O(32)
+// Time:  O(32)
 // Space: O(1)
 
 class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+};
+
+// Time:  O(logn) = O(32)
+// Space: O(1)
+class Solution2 {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t result = 0;
