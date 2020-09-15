a7bc802dac2ec69435f8dfd9b69d68574616657f
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 23:41:37 +0800

    Update super-egg-drop.cpp

--- C++/super-egg-drop.cpp
+++ C++/super-egg-drop.cpp
@@ -18,9 +18,19 @@ public:
 
 private:
     bool check(int n, int K, int N) {
-        // Each combination of n moves with k broken eggs could represent a unique F.
-        // Thus, the range size of F that all cominations can cover 
-        // is the sum of C(n, k), k = 1..K
+        // let f(n, K) be the max number of floors could be solved by n moves and K eggs,
+        // we want to do binary search to find min of n, s.t. f(n, K) >= N,
+        // if we use one move to drop egg with X floors
+        // 1. if it breaks, we can search new X in the range [X+1, X+f(n-1, K-1)]
+        // 2. if it doesn't break, we can search new X in the range [X-f(n-1, K), X-1]
+        // => f(n, K) = (X+f(n-1, K-1))-(X-f(n-1, K))+1 = f(n-1, K-1)+f(n-1, K)+1
+        // => (1) f(n, K)   = f(n-1, K)  +1+f(n-1, K-1)
+        //    (2) f(n, K-1) = f(n-1, K-1)+1+f(n-1, K-2)
+        // let g(n, K) = f(n, K)-f(n, K-1), and we substract (1) by (2)
+        // => g(n, K) = g(n-1, K)+g(n-1, K-1), obviously, it is binomial coefficient
+        // => C(n, K) = g(n, K) = f(n, K)-f(n, K-1)
+        // => f(n, K) = C(n, K)+f(n, K-1) = C(n, K) + C(n, K-1) + ... + C(n, 1) + f(n, 0) = sum(C(n, k) for k in [1, K])
+        // => all we have to do is to check sum(C(n, k) for k in [1, K]) >= N
         int total = 0, c = 1;
         for (int k = 1; k <= K; ++k) {
             c *= n - k + 1;
