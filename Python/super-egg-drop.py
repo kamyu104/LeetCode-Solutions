eb339fd250df9606b68209524edb87e06f96646d
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 21:58:40 +0800

    Update super-egg-drop.py

--- Python/super-egg-drop.py
+++ Python/super-egg-drop.py
@@ -9,17 +9,17 @@ class Solution(object):
         :rtype: int
         """
         def check(n, K, N):
-            # f(n, K): the max number of floors could be solved by n moves and K eggs
+            # let f(n, K) be the max number of floors could be solved by n moves and K eggs,
             # we want to do binary search to find min of n, s.t. f(n, K) >= N
             # => f(n, K) = f(n-1, K)+1 + // [1            , f(n-1, K)+1            ] is solvable by one move with one break
             #              f(n-1, K-1)   // [f(n-1, K)+1+1, f(n-1, K)+1+f(n-1, K-1)] is then solvable by remaining moves and eggs
             # => (1) f(n, K)   = f(n-1, K)  +1+f(n-1, K-1)
             #    (2) f(n, K-1) = f(n-1, K-1)+1+f(n-1, K-2)
-            # let g(n, K) = f(n, K)-f(n, K-1), (1)-(2)
-            # => g(n, K) = g(n-1, K)+g(n-1, K-1), it is binomial coefficient
+            # let g(n, K) = f(n, K)-f(n, K-1), and we substract (1) by (2)
+            # => g(n, K) = g(n-1, K)+g(n-1, K-1), we can find that it is binomial coefficient
             # => C(n, K) = g(n, K) = f(n, K)-f(n, K-1)
             # => f(n, K) = C(n, K)+f(n, K-1) = C(n, K) + C(n, K-1) + ... + C(n, 1) + f(n, 0) = sum(C(n, k) for k in [1, K])
-            # => check sum(C(n, k) for k in [1, K]) >= N
+            # => all we have to do is to check sum(C(n, k) for k in [1, K]) >= N
             total, c = 0, 1
             for k in xrange(1, K+1):
                 c *= n-k+1
