6cf25e529685c7ca14b713e43a7ce43459b82105
kamyu <kamyu104@users.noreply.github.com>
2020-09-15 08:00:47 +0800

    Update 4sum.py

--- Python/4sum.py
+++ Python/4sum.py
@@ -13,29 +13,29 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         nums.sort()
-        res = []
+        result = []
         for i in xrange(len(nums) - 3):
             if i and nums[i] == nums[i - 1]:
                 continue
             for j in xrange(i + 1, len(nums) - 2):
                 if j != i + 1 and nums[j] == nums[j - 1]:
                     continue
-                sum = target - nums[i] - nums[j]
+                total = target - nums[i] - nums[j]
                 left, right = j + 1, len(nums) - 1
                 while left < right:
-                    if nums[left] + nums[right] == sum:
-                        res.append([nums[i], nums[j], nums[left], nums[right]])
+                    if nums[left] + nums[right] == total:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
                         right -= 1
                         left += 1
                         while left < right and nums[left] == nums[left - 1]:
                             left += 1
                         while left < right and nums[right] == nums[right + 1]:
                             right -= 1
-                    elif nums[left] + nums[right] > sum:
+                    elif nums[left] + nums[right] > total:
                         right -= 1
                     else:
                         left += 1
-        return res
+        return result
 
 
 # Time:  O(n^2 * p)
