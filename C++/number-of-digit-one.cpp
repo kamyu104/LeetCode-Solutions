b82a3384db2e32d0f6c98ed10615c04d66059017
kamyu <kamyu104@users.noreply.github.com>
2020-09-17 23:46:03 +0800

    Update number-of-digit-one.cpp

--- C++/number-of-digit-one.cpp
+++ C++/number-of-digit-one.cpp
@@ -4,11 +4,12 @@
 class Solution {
 public:
     int countDigitOne(int n) {
-        static const int digit = 1;
-        int result = 0;
+        static const int DIGIT = 1;
+        int is_zero = (DIGIT == 0) ? 1 : 0;
+        int result = is_zero;
         for (int64_t base = 1; n >= base; base *= 10) {
-            result += n / (10 * base) * base +
-                      min(base, max(n % (10 * base) - digit * base + 1, 0l));
+            result += (n / (10 * base) - is_zero) * base +
+                      min(base, max(n % (10 * base) - DIGIT * base + 1, 0l));
         }
         return result;
     }
