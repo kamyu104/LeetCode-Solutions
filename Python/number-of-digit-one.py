b5c8b4f0820c594d6bdb0f7d47842de0c049aa5c
kamyu <kamyu104@users.noreply.github.com>
2020-09-17 23:29:09 +0800

    Update number-of-digit-one.py

--- Python/number-of-digit-one.py
+++ Python/number-of-digit-one.py
@@ -1,3 +1,4 @@
+
 # Time:  O(logn)
 # Space: O(1)
 
@@ -7,41 +8,11 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        pivot, result = 1, 0
-        while n >= pivot:
-            result += (n//(10*pivot))*pivot + \
-                      min(pivot, max(n%(10*pivot) - pivot + 1, 0))
-            pivot *= 10
+        digit, result = 1, 0
+        base = 1
+        while n >= base:
+            result += (n//(10*base))*base + \
+                      min(base, max(n%(10*base) - digit*base + 1, 0))
+            base *= 10
         return result
-
-
-# Time:  O(logn)
-# Space: O(1)
-class Solution2(object):
-    # @param {integer} n
-    # @return {integer}
-    def countDigitOne(self, n):
-        k = 1
-        cnt, multiplier, left_part = 0, 1, n
-
-        while left_part > 0:
-            # split number into: left_part, curr, right_part
-            curr = left_part % 10
-            right_part = n % multiplier
-
-            # count of (c000 ~ oooc000) = (ooo + (k < curr)? 1 : 0) * 1000
-            cnt += (left_part / 10 + (k < curr)) * multiplier
-
-            # if k == 0, oooc000 = (ooo - 1) * 1000
-            if k == 0 and multiplier > 1:
-                cnt -= multiplier
-
-            # count of (oook000 ~ oookxxx): count += xxx + 1
-            if curr == k:
-                cnt += right_part + 1
-
-            left_part /= 10
-            multiplier *= 10
-
-        return cnt
 
