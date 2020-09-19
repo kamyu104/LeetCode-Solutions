6bdb3f250c526ae85fa0f5fb8ab39304ac2047d2
kamyu <kamyu104@users.noreply.github.com>
2020-09-20 02:32:27 +0800

    Update sum-of-all-odd-length-subarrays.cpp

--- C++/sum-of-all-odd-length-subarrays.cpp
+++ C++/sum-of-all-odd-length-subarrays.cpp
@@ -14,7 +14,7 @@ public:
         // for each number x with its position i, as the graph depicted above,
         // (begin, pos) pair represents a subarray arr[begin:pos+1] containing x, marked 1 if odd length else 0,
         // so the total number of 0 and 1 are exactly the total number of subarrays with x, which is (i-0+1)*((len(arr)-1)-i+1),
-        // because the number of 1 is always equal to or one more than the number of 0, (always begins with 1010... and alternatively flip)
+        // because the number of 1 is always equal to or one more than the number of 0, (always begins with 1010... and alternatively flips)
         // so there are ceil(count/2) odd length subarrays with x
         int result = 0;
         for (int i = 0; i < size(arr); ++i) {
