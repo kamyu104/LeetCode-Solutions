eaa2d32c567178d05392cc6a24aa5b632518eefb
kamyu <kamyu104@users.noreply.github.com>
2020-09-20 17:01:06 +0800

    Create make-sum-divisible-by-p.py

---
+++ Python/make-sum-divisible-by-p.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def minSubarray(self, nums, p):
+        """
+        :type nums: List[int]
+        :type p: int
+        :rtype: int
+        """
+        residue = sum(nums) % p
+        if not residue:
+            return 0
+        result = len(nums)
+        curr, lookup = 0, {0: -1}
+        for i, num in enumerate(nums):
+            curr = (curr+num) % p
+            lookup[curr] = i
+            if (curr-residue) % p in lookup:
+                result = min(result, i-lookup[(curr-residue)%p])
+        return result if result < len(nums) else -1
