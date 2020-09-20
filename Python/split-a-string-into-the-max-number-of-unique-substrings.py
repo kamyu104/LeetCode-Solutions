4681158419238dd308d6ad18e09f19940c65928b
kamyu <kamyu104@users.noreply.github.com>
2020-09-21 00:37:39 +0800

    Update split-a-string-into-the-max-number-of-unique-substrings.py

--- Python/split-a-string-into-the-max-number-of-unique-substrings.py
+++ Python/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -16,18 +16,22 @@ class Solution(object):
     
         result = 1
         total = 2**(len(s)-1)
-        for mask in xrange(total):
+        mask = 0
+        while mask < total:
             if popcount(mask) < result:
+                mask += 1
                 continue
-            lookup, curr, base = set(), [], 1
+            lookup, curr, base = set(), [], total//2
             for i in xrange(len(s)):
                 curr.append(s[i])
-                if (mask&base) or base == total:
+                if (mask&base) or base == 0:
                     if "".join(curr) in lookup:
+                        mask += base if base else 1
                         break
                     lookup.add("".join(curr))
                     curr = []
-                base <<= 1
+                base >>= 1
             else:
                 result = max(result, len(lookup))
+                mask += 1
         return result
