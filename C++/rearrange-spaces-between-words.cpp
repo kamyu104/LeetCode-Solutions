48c10844c9494b5fa92f49e5ab32ce67e3436695
kamyu <kamyu104@users.noreply.github.com>
2020-09-20 20:31:49 +0800

    Create rearrange-spaces-between-words.cpp

---
+++ C++/rearrange-spaces-between-words.cpp
@@ -0,0 +1,45 @@
+// Time:  O(n)
+// Space: O(1)
+
+// inplace solution
+class Solution {
+public:
+    string reorderSpaces(string text) {
+        // count spaces and words 
+        int space_count = 0, word_count = 0;
+        for (int i = 0; i < size(text); ++i) {
+            if (text[i] == ' ') {
+                ++space_count;
+            } else if (i == 0 || text[i - 1] == ' ') {
+                ++word_count;
+            }
+        }
+        // rearrange the spaces to the right
+        int left = 0, curr = 0;
+        for (int i = 0; i < size(text); ++i) {
+            bool is_swapped = false;
+            while (i < size(text) && text[i] != ' ') {
+                swap(text[left++], text[i++]);
+                is_swapped = true;
+            }
+            if (is_swapped) {
+                ++left;
+            }
+        }
+        // rearrange the spaces to the left
+        int equal_count = word_count - 1 > 0 ? space_count / (word_count - 1) : 0;
+        int extra_count = word_count - 1 > 0 ? space_count % (word_count - 1) : space_count;
+        int right = size(text) - 1 - extra_count;
+        for (int i = size(text) - 1; i >= 0; --i) {
+            bool is_swapped = false;
+            while (i >= 0 && text[i] != ' ') {
+                swap(text[right--], text[i--]);
+                is_swapped = true;
+            }
+            if (is_swapped) {
+                right -= equal_count;
+            }
+        }
+        return text;
+    }
+};
