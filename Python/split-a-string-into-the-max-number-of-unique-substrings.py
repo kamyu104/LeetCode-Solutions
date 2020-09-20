582b68aa40f6d7022c2f6754a1bd1b70a3b6c85e
kamyu <kamyu104@users.noreply.github.com>
2020-09-20 21:52:39 +0800

    Create split-a-string-into-the-max-number-of-unique-substrings.py

---
+++ Python/split-a-string-into-the-max-number-of-unique-substrings.py
@@ -0,0 +1,33 @@
+# Time:  O(n * 2^(n - 1))
+# Space: O(n)
+
+class Solution(object):
+    def maxUniqueSplit(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def popcount(n):
+            count = 0
+            while n:
+                n &= n-1
+                count += 1
+            return count
+    
+        result = 1
+        total = 2**(len(s)-1)
+        for bitmask in xrange(total):
+            if popcount(bitmask) < result:
+                continue
+            lookup, curr, base = set(), [], 1
+            for i in xrange(len(s)):
+                curr.append(s[i])
+                if (bitmask&base) or base == total:
+                    if "".join(curr) in lookup:
+                        break
+                    lookup.add("".join(curr))
+                    curr = []
+                base <<= 1
+            else:
+                result = max(result, len(lookup))
+        return result
