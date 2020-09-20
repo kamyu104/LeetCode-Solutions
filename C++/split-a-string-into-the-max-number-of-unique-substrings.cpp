dfa387c87fb6d1772436ca4b335e60df4fb40e87
kamyu <kamyu104@users.noreply.github.com>
2020-09-21 00:41:15 +0800

    Update split-a-string-into-the-max-number-of-unique-substrings.cpp

--- C++/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ C++/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -19,7 +19,7 @@ public:
                 if ((mask & base) || base == 0) {
                     if (!lookup.emplace(curr).second) {
                         unique = false;
-                        mask += base ? base : 1;
+                        mask += base ? base : 1; // pruning
                         break;
                     }
                     curr.clear();
