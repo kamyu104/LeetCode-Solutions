70d82ab8ef8a9761e60e60148c2cc306a9def76d
kamyu <kamyu104@users.noreply.github.com>
2020-09-21 02:04:12 +0800

    Update strange-printer-ii.py

--- Python/strange-printer-ii.py
+++ Python/strange-printer-ii.py
@@ -28,7 +28,43 @@ class Solution(object):
                 elif step == 2:
                     lookup[color] = VISITED
             return False
-                    
+
+        boxes = collections.defaultdict(lambda:[len(targetGrid), len(targetGrid[0]), -1, -1])
+        adj = collections.defaultdict(set)
+        for r, row in enumerate(targetGrid):
+            for c, v in enumerate(row):
+                boxes[v][0] = min(boxes[v][0], r)
+                boxes[v][1] = min(boxes[v][1], c)
+                boxes[v][2] = max(boxes[v][2], r)
+                boxes[v][3] = max(boxes[v][3], c)
+        for color, (min_r, min_c, max_r, max_c) in boxes.iteritems():
+            for r in xrange(min_r, max_r+1):
+                for c in xrange(min_c, max_c+1):
+                    if targetGrid[r][c] != color:
+                        adj[color].add(targetGrid[r][c])
+
+        lookup = {}
+        return all(color in lookup or not has_cycle(adj, color, lookup) for color in boxes.iterkeys())
+            
+
+# Time:  O(c * m * n + e), c is the number of colors
+#                        , e is the number of edges in adj, at most O(c^2)
+# Space: O(e)
+class Solution2(object):
+    def isPrintable(self, targetGrid):
+        """
+        :type targetGrid: List[List[int]]
+        :rtype: bool
+        """
+        VISITING, VISITED = range(2)
+        def has_cycle(adj, color, lookup):
+            lookup[color] = VISITING
+            for new_color in adj[color]:
+                if (new_color not in lookup and has_cycle(adj, new_color, lookup)) or \
+                   lookup[new_color] == VISITING:
+                    return True
+            lookup[color] = VISITED
+            return False          
 
         MAX_COLOR = 60
         adj = collections.defaultdict(set)
@@ -50,10 +86,4 @@ class Solution(object):
                         adj[color].add(targetGrid[r][c])
 
         lookup = {}
-        for color in xrange(1, MAX_COLOR+1): 
-            if color in lookup:
-                continue
-            if has_cycle(adj, color, lookup):
-                return False
-        return True
-            
+        return all(color in lookup or not has_cycle(adj, color, lookup) for color in xrange(1, MAX_COLOR+1))
