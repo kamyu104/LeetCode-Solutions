c259b1320fe59092ec951f696dbff96f4f01bd21
kamyu <kamyu104@users.noreply.github.com>
2020-09-21 19:04:22 +0800

    Update missing-element-in-sorted-array.py

--- Python/missing-element-in-sorted-array.py
+++ Python/missing-element-in-sorted-array.py
@@ -12,14 +12,14 @@ class Solution(object):
             return (nums[x]-nums[0]+1)-(x-0+1)
 
         def check(nums, k, x):
-            return k <= missing_count(nums, x)
+            return k > missing_count(nums, x)
 
         left, right = 0, len(nums)-1
-        while left <= right:
+        while left <= right:  # find the largest right s.t. k > missingCount(nums, x)
             mid = left + (right-left)//2
-            if check(nums, k, mid):
+            if not check(nums, k, mid):
                 right = mid-1
             else:
                 left = mid+1
-        assert(not check(nums, k, right))
+        assert(check(nums, k, right))
         return nums[right] + (k-missing_count(nums, right))
