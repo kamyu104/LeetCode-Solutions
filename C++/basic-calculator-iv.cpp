d9b3ce20494104ed1c0e11a3ac83b9cc5924e814
kamyu <kamyu104@users.noreply.github.com>
2020-09-25 00:15:31 +0800

    Update basic-calculator-iv.cpp

--- C++/basic-calculator-iv.cpp
+++ C++/basic-calculator-iv.cpp
@@ -122,7 +122,7 @@ private:
     class Compare {
        public:
           bool operator()
-              (const ContType& x,const ContType& y) {
+              (const ContType& x,const ContType& y) const {
               return x.size() != y.size() ? x.size() > y.size() : x < y;
           }
     };
@@ -142,6 +142,66 @@ private:
 };
 
 class Solution {
+public:
+    vector<string> basicCalculatorIV(string expression, vector<string>& evalvars, vector<int>& evalints) {
+        unordered_map<string, int> lookup;
+        for (int i = 0; i < evalvars.size(); ++i) {
+            lookup[evalvars[i]] = evalints[i];
+        }
+        return parse(expression).eval(lookup);
+    }
+
+private:
+    Poly parse(const string& s) {
+        static const unordered_map<char, int> precedence = {{'+', 0}, {'-', 0}, {'*', 1}};
+
+        stack<Poly> operands;
+        stack<char> operators;
+        string operand;
+        for (int i = 0; i < size(s); ++i) {
+            if (isalnum(s[i])) {
+                operand.push_back(s[i]);
+                if (i + 1 == size(s) || !isalnum(s[i + 1])) {
+                    operands.emplace(Poly(operand));
+                    operand = "";
+                }
+            } else if (s[i] == '(') {
+                operators.emplace(s[i]);
+            } else if (s[i] == ')') {
+                while (operators.top() != '(') {
+                    compute(&operands, &operators);
+                }
+                operators.pop();
+            } else if (precedence.count(s[i])) {
+                while (!empty(operators) && precedence.count(operators.top()) && 
+                       precedence.at(operators.top()) >= precedence.at(s[i])) {
+                     compute(&operands, &operators);
+                }
+                operators.emplace(s[i]);
+            }
+        }
+        while (!empty(operators)) {
+            compute(&operands, &operators);
+        }
+        return operands.top();
+    }
+
+    template<typename T>
+    void compute(stack<T> *operands, stack<char> *operators) {
+        const auto right = move(operands->top()); operands->pop();
+        const auto left = move(operands->top()); operands->pop();
+        const char op = operators->top(); operators->pop();
+        if (op == '+') {
+            operands->emplace(left + right);
+        } else if (op == '-') {
+            operands->emplace(left - right);
+        } else if (op == '*') {
+            operands->emplace(left * right);
+        }
+    }
+};
+
+class Solution2 {
 public:
     vector<string> basicCalculatorIV(string expression, vector<string>& evalvars, vector<int>& evalints) {
         unordered_map<string, int> lookup;
