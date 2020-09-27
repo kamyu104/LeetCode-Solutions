681f6e41c93c23edbaf49a70cf66dd2cecf21b02
kamyu <kamyu104@users.noreply.github.com>
2020-09-27 17:38:14 +0800

    Create crawler-log-folder.py

---
+++ Python/crawler-log-folder.py
@@ -0,0 +1,17 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minOperations(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: int
+        """
+        result = 0
+        for log in logs:
+            if log == '../':
+                if result > 0:
+                    result -= 1
+            elif log != './':
+                result += 1
+        return result
