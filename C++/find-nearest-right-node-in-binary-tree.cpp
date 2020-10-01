fa58943e6c3b778a2d2c823811925687148ed477
kamyu <kamyu104@users.noreply.github.com>
2020-10-01 23:01:53 +0800

    Create find-nearest-right-node-in-binary-tree.cpp

---
+++ C++/find-nearest-right-node-in-binary-tree.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(w)
+
+class Solution {
+public:
+    TreeNode* findNeartestRightNode(TreeNode* root, TreeNode* u) {
+        vector<TreeNode *> q = {root};
+        while (!empty(q)) {
+            vector<TreeNode *> new_q;
+            for (int i = 0; i < size(q); ++i) {
+                if (q[i] == u) {
+                    return i + 1 < size(q) ? q[i + 1] : nullptr;
+                }
+                if (q[i]->left) {
+                    new_q.emplace_back(q[i]->left);
+                }
+                if (q[i]->right) {
+                    new_q.emplace_back(q[i]->right);
+                }
+            }
+            q = move(new_q);
+        }
+        return nullptr;
+    }
+};
