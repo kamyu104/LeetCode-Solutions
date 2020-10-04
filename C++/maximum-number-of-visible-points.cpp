93bf412bae5fd0b66c2b0f224672688102cf4cc9
kamyu <kamyu104@users.noreply.github.com>
2020-10-04 22:32:26 +0800

    Create maximum-number-of-visible-points.cpp

---
+++ C++/maximum-number-of-visible-points.cpp
@@ -0,0 +1,33 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int visiblePoints(vector<vector<int>>& points, int angle, vector<int>& location) {
+        static const double PI = atan2(0, -1);
+
+        vector<double> arr;
+        int extra = 0;
+        for (const auto& p : points) {
+            if (p == location) {
+                ++extra;
+                continue;
+            }
+            arr.emplace_back(atan2(p[1] - location[1], p[0] - location[0]));
+        }
+        sort(begin(arr), end(arr));
+        const int n = size(arr);
+        for (int i = 0; i < n; ++i) {  // make it circular
+            arr.emplace_back(arr[i] + 2.0 * PI);
+        }
+        const double d = 2.0 * PI * (angle / 360.0);
+        int result = 0;
+        for (int left = 0, right = 0; right < size(arr); ++right) {
+            while (arr[right] - arr[left] > d) {
+                ++left;
+            }
+            result = max(result, right - left + 1);
+        }
+        return result + extra;
+    }
+};
