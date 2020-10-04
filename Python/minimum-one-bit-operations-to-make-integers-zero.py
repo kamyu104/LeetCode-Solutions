cdf98f99d71415b08ccd860b09355d1fee4889bd
kamyu <kamyu104@users.noreply.github.com>
2020-10-04 22:31:35 +0800

    Create minimum-one-bit-operations-to-make-integers-zero.py

---
+++ Python/minimum-one-bit-operations-to-make-integers-zero.py
@@ -0,0 +1,47 @@
+# Time:  O(logn)
+# Space: O(1)
+
+# reference: https://en.wikipedia.org/wiki/Gray_code
+class Solution(object):
+    def minimumOneBitOperations(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def gray_to_binary(n):
+            result = 0
+            while n:
+                result ^= n
+                n >>= 1
+            return result
+        
+        # f(n) is actually the inverse of gray code
+        return gray_to_binary(n)
+
+
+# Time:  O(logn)
+# Space: O(1)
+class Solution2(object):
+    def minimumOneBitOperations(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # [observation1]:
+        # f(1) = 1
+        # f(10) = 2 * f(1) + 1 = 3
+        # f(100) = 2 * f(10) + 1 = 7
+        # by mathematical induction
+        # => f(2^k) = 2^(k+1)-1
+        #
+        # [observation2]:
+        # let pos be an array of positions where the bit is 1 in ascending order:
+        # f(1XX...X) = f(100...0) - f(XX...X)
+        #            = (2^(pos[k-1]+1)-1) - f(XX...X)
+        # by mathematical induction
+        # => f(n) = (2^(pos[k-1]+1)-1) - (2^(pos[k-2])+1) + ... + (-1)^(k-1) * (2^(pos[0]+1)-1)
+        result = 0
+        while n:
+            result = -result - (n^(n-1))  # 2^(pos[i]+1)-1
+            n &= n-1
+        return abs(result)
