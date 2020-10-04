be5f1a48def8a45b217e7e142aa71676a26e1fbf
kamyu <kamyu104@users.noreply.github.com>
2020-10-05 01:30:49 +0800

    Update minimum-one-bit-operations-to-make-integers-zero.cpp

--- C++/minimum-one-bit-operations-to-make-integers-zero.cpp
+++ C++/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -5,6 +5,8 @@
 class Solution {
 public:
     int minimumOneBitOperations(int n) {
+        // f(0XX...X) + f(1XX...X) = f(100...0) implies it is a gray code
+        // => f(n) is actually the inverse of gray code
         return gray_to_binary(n);
     }
 
