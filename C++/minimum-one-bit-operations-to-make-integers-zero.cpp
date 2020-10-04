2ae65e32e6f6e2ec99fcbfb9c8b78f346a6e5f1e
kamyu <kamyu104@users.noreply.github.com>
2020-10-04 22:31:05 +0800

    Create minimum-one-bit-operations-to-make-integers-zero.cpp

---
+++ C++/minimum-one-bit-operations-to-make-integers-zero.cpp
@@ -0,0 +1,45 @@
+// Time:  O(logn)
+// Space: O(1)
+
+// reference: https://en.wikipedia.org/wiki/Gray_code
+class Solution {
+public:
+    int minimumOneBitOperations(int n) {
+        return gray_to_binary(n);
+    }
+
+private:
+    int gray_to_binary(int n) {
+        int result = 0;
+        for (; n > 0; n >>= 1) {
+            result ^= n;
+        }
+        return result;
+    }
+};
+
+// Time:  O(logn)
+// Space: O(1)
+class Solution2 {
+public:
+    int minimumOneBitOperations(int n) {
+        // [observation1]:
+        // f(1) = 1
+        // f(10) = 2 * f(1) + 1 = 3
+        // f(100) = 2 * f(10) + 1 = 7
+        // by mathematical induction
+        // => f(2^k) = 2^(k+1)-1
+        //
+        // [observation2]:
+        // let pos be an array of positions where the bit is 1 in ascending order:
+        // f(1XX...X) = f(100...0) - f(XX...X)
+        //            = (2^(pos[k-1]+1)-1) - f(XX...X)
+        // by mathematical induction
+        // => f(n) = (2^(pos[k-1]+1)-1) - (2^(pos[k-2])+1) + ... + (-1)^(k-1) * (2^(pos[0]+1)-1)
+        int result = 0;
+        for (; n > 0; n &= n - 1) {
+            result = -(result + (n ^ (n - 1)));
+        }
+        return abs(result);
+    }
+};
