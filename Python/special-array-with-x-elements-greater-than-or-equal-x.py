54264bec72123808d9333a67bcd0be8ed38eae09
kamyu <kamyu104@users.noreply.github.com>
2020-10-05 05:12:12 +0800

    Update special-array-with-x-elements-greater-than-or-equal-x.py

--- Python/special-array-with-x-elements-greater-than-or-equal-x.py
+++ Python/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -30,6 +30,46 @@ class Solution2(object):
         :rtype: int
         """
         MAX_NUM = 1000
+        def inplace_counting_sort(nums, reverse=False):  # Time: O(n)
+            count = [0]*(MAX_NUM+1)
+            for num in nums:
+                count[num] += 1
+            for i in xrange(1, len(count)):
+                count[i] += count[i-1]
+            for i in reversed(xrange(len(nums))):  # stable sort and inplace
+                if nums[i] < 0:  # processed
+                    continue
+                while i != count[nums[i]]-1:
+                    count[nums[i]] -= 1
+                    nums[count[nums[i]]], nums[i] = ~nums[i], nums[count[nums[i]]]
+                count[nums[i]] -= 1
+                nums[i] = ~nums[i]
+            for i in xrange(len(nums)):
+                nums[i] = ~nums[i]  # restore values
+            if reverse:
+                nums.reverse()
+    
+        inplace_counting_sort(nums, reverse=True)
+        left, right = 0, len(nums)-1
+        while left <= right:  # Time: O(logn)
+            mid = left + (right-left)//2
+            if nums[mid] <= mid:
+                right = mid-1
+            else:
+                left = mid+1
+        return -1 if left < len(nums) and nums[left] == left else left
+
+
+# Time:  O(n)
+# Space: O(1)
+# counting sort + binary search solution
+class Solution3(object):
+    def specialArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        MAX_NUM = 1000
         def counting_sort(nums, reverse=False):  # Time: O(n)
             count = [0]*(MAX_NUM+1)
             for num in nums:
@@ -58,7 +98,7 @@ class Solution2(object):
 # Time:  O(nlogn)
 # Space: O(1)
 # sort solution
-class Solution3(object):
+class Solution4(object):
     def specialArray(self, nums):
         """
         :type nums: List[int]
