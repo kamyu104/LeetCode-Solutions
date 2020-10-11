f924f3e648cb9fc455d632b3300855dc84a23f24
kamyu <kamyu104@users.noreply.github.com>
2020-10-12 00:10:30 +0800

    Create maximal-network-rank.py

---
+++ Python/maximal-network-rank.py
@@ -0,0 +1,54 @@
+# Time:  O(nlogn) ~ O(n^2)
+# Space: O(n + m)
+
+# optimized from Solution2
+class Solution(object):
+    def maximalNetworkRank(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        degree = [0]*n
+        adj = collections.defaultdict(set)
+        for a, b in roads:
+            degree[a] += 1
+            degree[b] += 1
+            adj[a].add(b)
+            adj[b].add(a)
+        sorted_idx = range(n)
+        sorted_idx.sort(key=lambda x:-degree[x])
+        m = 2
+        while m < n:
+            if degree[sorted_idx[m]] != degree[sorted_idx[1]]:
+                break
+            m += 1
+        result = degree[sorted_idx[0]] + degree[sorted_idx[1]] - 1  # at least sum(top2 values) - 1
+        for i in xrange(m-1):  # only need to check pairs of top2 values
+            for j in xrange(i+1, m):
+                if degree[sorted_idx[i]]+degree[sorted_idx[j]]-int(sorted_idx[i] in adj and sorted_idx[j] in adj[sorted_idx[i]]) > result:  # if equal to ideal sum of top2 values, break
+                    return degree[sorted_idx[i]]+degree[sorted_idx[j]]-int(sorted_idx[i] in adj and sorted_idx[j] in adj[sorted_idx[i]])                                                 
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(n + m)
+class Solution2(object):
+    def maximalNetworkRank(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        degree = [0]*n
+        adj = collections.defaultdict(set)
+        for a, b in roads:
+            degree[a] += 1
+            degree[b] += 1
+            adj[a].add(b)
+            adj[b].add(a)
+        result = 0
+        for i in xrange(n-1):
+            for j in xrange(i+1, n):
+                result = max(result, degree[i]+degree[j]-int(i in adj and j in adj[i]))
+        return result
