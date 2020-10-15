4d3fcac05d86504224c9a0d14ab36cea0c42bbd5
kamyu <kamyu104@users.noreply.github.com>
2020-10-16 01:19:51 +0800

    Update best-time-to-buy-and-sell-stock-iv.py

--- Python/best-time-to-buy-and-sell-stock-iv.py
+++ Python/best-time-to-buy-and-sell-stock-iv.py
@@ -1,29 +1,28 @@
 # Time:  O(k * n)
 # Space: O(k)
 
+def maxProfit(self, k, prices):
+        """
+        :type k: int
+        :type prices: List[int]
+        :rtype: int
+        """
+        def maxAtMostNPairsProfit(sprices):
+            profit = 0
+            for i in xrange(len(prices) - 1):
+                profit += max(0, prices[i + 1] - prices[i])
+            return profit
 
-class Solution(object):
-    # @return an integer as the maximum profit
-    def maxProfit(self, k, prices):
-        if k >= len(prices) / 2:
-            return self.maxAtMostNPairsProfit(prices)
+        def maxAtMostKPairsProfit(prices, k):
+            max_buy = [float("-inf") for _ in xrange(k + 1)]
+            max_sell = [0 for _ in xrange(k + 1)]
+            for i in xrange(len(prices)):
+                for j in xrange(1, k + 1):
+                    max_buy[j] = max(max_buy[j], max_sell[j-1] - prices[i])
+                    max_sell[j] = max(max_sell[j], max_buy[j] + prices[i])
+            return max_sell[k]
 
-        return self.maxAtMostKPairsProfit(prices, k)
+        if k >= len(prices) // 2:
+            return maxAtMostNPairsProfit(prices)
 
-    def maxAtMostNPairsProfit(self, prices):
-        profit = 0
-        for i in xrange(len(prices) - 1):
-            profit += max(0, prices[i + 1] - prices[i])
-        return profit
-
-    def maxAtMostKPairsProfit(self, prices, k):
-        max_buy = [float("-inf") for _ in xrange(k + 1)]
-        max_sell = [0 for _ in xrange(k + 1)]
-
-        for i in xrange(len(prices)):
-            for j in xrange(1, min(k, i/2+1) + 1):
-                max_buy[j] = max(max_buy[j], max_sell[j-1] - prices[i])
-                max_sell[j] = max(max_sell[j], max_buy[j] + prices[i])
-
-        return max_sell[k]
-
+        return maxAtMostKPairsProfit(prices, k)
