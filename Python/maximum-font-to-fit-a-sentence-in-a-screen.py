3ddcf6d68bec1c240e7462faa45c68623200743c
kamyu <kamyu104@users.noreply.github.com>
2020-10-16 12:39:53 +0800

    Create maximum-font-to-fit-a-sentence-in-a-screen.py

---
+++ Python/maximum-font-to-fit-a-sentence-in-a-screen.py
@@ -0,0 +1,46 @@
+# Time:  O(n + logm), n is the length of text, m is the number of fonts
+# Space: O(1)
+
+import collections
+
+
+class FontInfo(object):
+    def getWidth(self, fontSize, ch):
+        """
+        :type fontSize: int
+        :type ch: char
+        :rtype int
+        """
+        pass
+    
+    def getHeight(self, fontSize):
+        """
+        :type fontSize: int
+        :rtype int
+        """
+        pass
+
+
+class Solution(object):
+    def maxFont(self, text, w, h, fonts, fontInfo):
+        """
+        :type text: str
+        :type w: int
+        :type h: int
+        :type fonts: List[int]
+        :type fontInfo: FontInfo
+        :rtype: int
+        """
+        def check(count, w, h, fonts, fontInfo, x):  # Time: O(1)
+            return (fontInfo.getHeight(fonts[mid]) <= h and
+                    sum(cnt * fontInfo.getWidth(fonts[mid], c) for c, cnt in count.iteritems()) <= w)
+
+        count = collections.Counter(text)
+        left, right = 0, len(fonts)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(count, w, h, fonts, fontInfo, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return fonts[right] if right >= 0 else -1
