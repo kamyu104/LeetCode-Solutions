d4a8bf717fb8a7e4faaed63ac68804c464261ba9
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 18:27:36 +0800

    Update lru-cache.cpp

--- C++/lru-cache.cpp
+++ C++/lru-cache.cpp
@@ -22,6 +22,49 @@ public:
     void put(int key, int value) {
         // If cache is full while inserting, remove the last one.
         if (map_.find(key) == map_.end() && list_.size() == capa_) {
+            auto del = list_.front(); list_.pop_front();
+            map_.erase(del.first);
+        }
+        update(key, value);
+    }
+    
+private:
+    list<pair<int, int>> list_; // key, value
+    unordered_map<int, list<pair<int, int>>::iterator> map_; // key, list iterator
+    int capa_;
+    
+    // Update (key, iterator of (key, value)) pair
+    void update(int key, int value) {
+        auto it = map_.find(key);
+        if (it != map_.end()) {
+            list_.erase(it->second);
+        }
+        list_.emplace_back(key, value);
+        map_[key] = prev(end(list_));
+    }
+};
+
+// Time:  O(1), per operation.
+// Space: O(k), k is the capacity of cache.
+class LRUCache2 {
+public:
+    LRUCache2(int capacity) : capa_(capacity) {
+    }
+    
+    int get(int key) {
+        if (map_.find(key) != map_.end()) {
+            // It key exists, update it.
+            const auto value = map_[key]->second;
+            update(key, value);
+            return value;
+        } else {
+            return -1;
+        }
+    }
+    
+    void put(int key, int value) {
+        // If cache is full while inserting, remove the last one.
+        if (map_.find(key) == map_.end() && list_.size() == capa_) {
             auto del = list_.back(); list_.pop_back();
             map_.erase(del.first);
         }
