6864863a1ba04207caba4d07f8487ad2a4cc7aab
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 12:08:26 +0800

    Update path-with-minimum-effort.py

--- Python/path-with-minimum-effort.py
+++ Python/path-with-minimum-effort.py
@@ -30,10 +30,69 @@ class Solution(object):
         return -1
 
 
+# Time:  O(m * n * log(m * n) + m * n * α(m * n)) = O(m * n * log(m * n))
+# Space: O(m * n)
+import collections
+
+
+class UnionFind(object):  # Time: (n * α(n)), Space: O(n)
+    def __init__(self, n):
+        self.set = range(n)
+        self.rank = range(n)
+
+    def find_set(self, x):
+        stk = []
+        while self.set[x] != x:  # path compression
+            stk.append(x)
+            x = self.set[x]
+        while stk:
+            self.set[stk.pop()] = x
+        return x
+
+    def union_set(self, x, y):
+        x_root, y_root = map(self.find_set, (x, y))
+        if x_root == y_root:
+            return False
+        if self.rank[x_root] < self.rank[y_root]:  # union by rank
+            self.set[x_root] = y_root
+        elif self.rank[x_root] > self.rank[y_root]:
+            self.set[y_root] = x_root
+        else:
+            self.set[y_root] = x_root
+            self.rank[x_root] += 1
+        return True
+
+
+class Solution2(object):
+    def minimumEffortPath(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: int
+        """
+        def index(n, i, j):
+            return i*n + j
+    
+        diffs = []
+        for i in xrange(len(heights)):
+            for j in xrange(len(heights[0])):
+                if i > 0:
+                    diffs.append((abs(heights[i][j]-heights[i-1][j]), index(len(heights[0]), i-1, j), index(len(heights[0]), i, j)))
+                if j > 0:
+                    diffs.append((abs(heights[i][j]-heights[i][j-1]), index(len(heights[0]), i, j-1), index(len(heights[0]), i, j)))
+        diffs.sort()
+        union_find = UnionFind(len(heights)*len(heights[0]))
+        for d, i, j in diffs:
+            if union_find.union_set(i, j):
+                if union_find.find_set(index(len(heights[0]), 0, 0)) == \
+                   union_find.find_set(index(len(heights[0]), len(heights)-1, len(heights[0])-1)):
+                    return d
+        return 0
+
+
 # Time:  O(m * n * logh)
 # Space: O(m * n)
 # bi-bfs solution
-class Solution2(object):
+class Solution3(object):
     def minimumEffortPath(self, heights):
         """
         :type heights: List[List[int]]
@@ -80,7 +139,7 @@ import collections
 
 
 # bfs solution
-class Solution3(object):
+class Solution4(object):
     def minimumEffortPath(self, heights):
         """
         :type heights: List[List[int]]
