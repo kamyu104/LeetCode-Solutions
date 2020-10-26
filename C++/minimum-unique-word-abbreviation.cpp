397117251bbe25796d5cffd4c79ea2391146d355
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 17:25:29 +0800

    Update minimum-unique-word-abbreviation.cpp

--- C++/minimum-unique-word-abbreviation.cpp
+++ C++/minimum-unique-word-abbreviation.cpp
@@ -1,4 +1,4 @@
-// Time:  O(2^n)
+// Time:  O(n * 2^n)
 // Space: O(n)
 
 class Solution {
@@ -6,21 +6,20 @@ public:
     string minAbbreviation(string target, vector<string>& dictionary) {
         vector<int> diffs;
         dictionary_to_diffs(target, dictionary, &diffs);
-
-        if (diffs.empty()) {
-            return to_string(target.length());
+        if (empty(diffs)) {
+            return to_string(size(target));
         }
-
-        int bits = (1 << target.length()) - 1;
-        for (int i = 0; i < (1 << target.length()); ++i) {
-            if (all_of(diffs.begin(), diffs.end(), [&i](int d) { return d & i; } )) {
-                if (bits_len(target, i) < bits_len(target, bits)) {
-                    bits = i;
+        string result = target;
+        int total = 1 << size(target);
+        for (int mask = 0; mask < total; ++mask) {
+            if (all_of(cbegin(diffs), cend(diffs), [&mask](int d) { return d & mask; })) {
+                auto abbr = bits_to_abbr(target, mask);
+                if (size(abbr) < size(result)) {
+                    result = move(abbr);
                 }
             }
         }
-
-        return bits_to_abbr(target, bits);
+        return result;
     }
 
 private:
@@ -31,7 +30,6 @@ private:
             if (word.length() != target.length()) {
                 continue;
             }
-
             int bits = 0;
             for (int i = 0; i < word.length(); ++i) {
                 if (target[i] != word[i]) {
@@ -42,32 +40,20 @@ private:
         }
     }
 
-    int bits_len(const string& target, int bits) {
-        int total = target.length();
-        for (int i = 0; i < target.length() - 1; ++i) {
-            if (((bits >> i) & 0b11) == 0) {
-                --total;
-            }
-        }
-        return total;
-    }
-
     string bits_to_abbr(const string& target, int bits) {
         string abbr;
-
         int pre = 0;
-        for (int i = 0, prev = 0; i < target.length(); ++i, bits >>= 1) {
+        for (int i = 0, prev = 0; i < size(target); ++i, bits >>= 1) {
             if (bits & 1) {
                 if (i - pre > 0) {
                     abbr += to_string(i - pre);
                 }
                 pre = i + 1;
                 abbr.push_back(target[i]);
-            } else if (i == target.length() - 1) {
+            } else if (i == size(target) - 1) {
                 abbr += to_string(i - pre + 1);
             }
         }
-
         return abbr;
     }
 };
