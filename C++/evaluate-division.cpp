cd642eb51ec83a29e34e463d5da618afa4d31bed
kamyu <kamyu104@users.noreply.github.com>
2020-10-27 04:04:50 +0800

    Update evaluate-division.cpp

--- C++/evaluate-division.cpp
+++ C++/evaluate-division.cpp
@@ -41,6 +41,54 @@ private:
             if (xp == yp) {
                 return false;
             }
+            if (rank_[xp] < rank_[yp]) {
+                set_[xp] = pair(yp, r * yr / xr);
+            } else if (rank_[xp] > rank_[yp]) {
+                set_[yp] = pair(xp, 1.0 / r * xr / yr);
+            } else {
+                set_[yp] = pair(xp, 1.0 / r * xr / yr);
+                ++rank_[xp];
+            }
+            return true;
+        }
+
+        double query_set(const string& x, const string& y) {
+            if (!set_.count(x) || !set_.count(y)) {
+                return -1.0;
+            }
+            const auto& [xp, xr] = find_set(x);
+            const auto& [yp, yr] = find_set(y);
+            return (xp == yp) ? xr / yr : -1.0;
+        }
+
+    private:
+        unordered_map<string, pair<string, double>> set_;
+        unordered_map<string, int> rank_;
+    };
+    
+    class UnionFindPathCompressionOnly {
+    public:
+        UnionFindPathCompressionOnly() {
+        }
+                
+        pair<string, double> find_set(const string& x) {
+            if (!set_.count(x)) {
+                set_[x] = pair(x, 1.0);
+            }
+            const auto& [xp, xr] = set_[x];
+            if (x != xp) {
+                const auto& [pp, pr] = find_set(xp);  // Path compression.
+                set_[x] = pair(pp, xr * pr);
+            }
+            return set_[x];
+        }
+
+        bool union_set(const string& x, const string& y, double r) {
+            const auto& [xp, xr] = find_set(x);
+            const auto& [yp, yr] = find_set(y);
+            if (xp == yp) {
+                return false;
+            }
             set_[xp] = pair(yp, r * yr / xr); 
             return true;
         }
