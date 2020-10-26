002902f69cd27f66c563c1f6e704f180f4535452
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 15:04:38 +0800

    Update 01-matrix.py

--- Python/01-matrix.py
+++ Python/01-matrix.py
@@ -1,35 +1,31 @@
 # Time:  O(m * n)
-# Space: O(m * n)
+# Space: O(1)
 
-import collections
-
-
+# dp solution
 class Solution(object):
     def updateMatrix(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: List[List[int]]
         """
-        queue = collections.deque()
+        dp = [[float("inf")]*len(matrix[0]) for _ in xrange(len(matrix))]
         for i in xrange(len(matrix)):
-            for j in xrange(len(matrix[0])):
-                if matrix[i][j] == 0:
-                    queue.append((i, j))
-                else:
-                    matrix[i][j] = float("inf")
-
-        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        while queue:
-            cell = queue.popleft()
-            for dir in dirs:
-                i, j = cell[0]+dir[0], cell[1]+dir[1]
-                if not (0 <= i < len(matrix) and
-                        0 <= j < len(matrix[0]) and
-                        matrix[i][j] > matrix[cell[0]][cell[1]]+1):
+            for j in xrange(len(matrix[i])):
+                if not matrix[i][j]:
                     continue
-                queue.append((i, j))
-                matrix[i][j] = matrix[cell[0]][cell[1]]+1
-
+                matrix[i][j] = float("inf")
+                if i > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i-1][j]+1)
+                if j > 0:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j-1]+1)
+        for i in reversed(xrange(len(matrix))):
+            for j in reversed(xrange(len(matrix[i]))):
+                if not matrix[i][j]:
+                    continue
+                if i < len(matrix)-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i+1][j]+1)
+                if j < len(matrix[i])-1:
+                    matrix[i][j] = min(matrix[i][j], matrix[i][j+1]+1)
         return matrix
 
 
@@ -63,3 +59,36 @@ class Solution2(object):
                         dp[i][j] = min(dp[i][j], dp[i][j+1]+1)
         return dp
 
+
+# Time:  O(m * n)
+# Space: O(m * n)
+import collections
+
+
+class Solution3(object):
+    def updateMatrix(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        queue = collections.deque()
+        for i in xrange(len(matrix)):
+            for j in xrange(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    queue.append((i, j))
+                else:
+                    matrix[i][j] = float("inf")
+
+        dirs = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+        while queue:
+            cell = queue.popleft()
+            for dir in dirs:
+                i, j = cell[0]+dir[0], cell[1]+dir[1]
+                if not (0 <= i < len(matrix) and
+                        0 <= j < len(matrix[0]) and
+                        matrix[i][j] > matrix[cell[0]][cell[1]]+1):
+                    continue
+                queue.append((i, j))
+                matrix[i][j] = matrix[cell[0]][cell[1]]+1
+
+        return matrix
