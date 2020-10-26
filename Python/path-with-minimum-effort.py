0796e3c7370a0ddebc00f2241d15473295060adb
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 12:27:50 +0800

    Update path-with-minimum-effort.py

--- Python/path-with-minimum-effort.py
+++ Python/path-with-minimum-effort.py
@@ -173,3 +173,41 @@ class Solution4(object):
             else:
                 left = mid+1
         return left
+
+
+# Time:  O(m * n * logh)
+# Space: O(m * n)
+# dfs solution
+class Solution5(object):
+    def minimumEffortPath(self, heights):
+        """
+        :type heights: List[List[int]]
+        :rtype: int
+        """
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        def check(heights, x):
+            lookup = [[False]*len(heights[0]) for _ in xrange(len(heights))]
+            stk = [(0, 0)]
+            while stk:
+                r, c = stk.pop()
+                if (r, c) == (len(heights)-1, len(heights[0])-1):
+                    return True
+                for dr, dc in directions:
+                    nr, nc = r+dr, c+dc
+                    if not (0 <= nr < len(heights) and
+                                0 <= nc < len(heights[0]) and
+                                abs(heights[nr][nc]-heights[r][c]) <= x and
+                                not lookup[nr][nc]):
+                            continue
+                    lookup[nr][nc] = True
+                    stk.append((nr, nc))
+            return False            
+        
+        left, right = 0, 10**6
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(heights, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
