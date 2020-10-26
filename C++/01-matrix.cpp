d1919db359f2f07c7152595316ab38b05f2c5e9d
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 14:29:40 +0800

    Update 01-matrix.cpp

--- C++/01-matrix.cpp
+++ C++/01-matrix.cpp
@@ -1,7 +1,51 @@
 // Time:  O(m * n)
 // Space: O(m * n)
 
+// dp solution
 class Solution {
+public:
+    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
+        vector<vector<int> > dp(matrix.size(),
+                                vector<int>(matrix[0].size(),
+                                            numeric_limits<int>::max()));
+
+        for (int i = 0; i < matrix.size(); ++i) {
+            for (int j = 0; j < matrix[i].size(); ++j) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i > 0 && dp[i - 1][j] != numeric_limits<int>::max()) {
+                        dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
+                    }
+                    if (j > 0 && dp[i][j - 1] != numeric_limits<int>::max()) {
+                        dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
+                    }
+                }
+            }
+        }
+
+        for (int i = matrix.size() - 1; i >= 0; --i) {
+            for (int j = matrix[i].size() - 1; j >= 0; --j) {
+                if (matrix[i][j] == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (i < matrix.size() - 1 && dp[i + 1][j] != numeric_limits<int>::max()) {
+                        dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
+                    }
+                    if (j < matrix[i].size() - 1 && dp[i][j + 1] != numeric_limits<int>::max()) {
+                        dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
+                    }
+                }
+            }
+        }
+
+        return dp;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(m * n)
+class Solution2 {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
         queue<pair<int, int>> queue;
@@ -23,8 +67,8 @@ public:
             for (const auto& dir : dirs) {
                 auto i = cell.first + dir.first;
                 auto j = cell.second + dir.second;
-                if (i < 0 || i >= matrix.size() || j < 0 || j >= matrix[0].size() || 
-                    matrix[i][j] <= matrix[cell.first][cell.second] + 1) {
+                if (!(0 <= i && i < matrix.size() && 0 <= j && j < matrix[0].size() && 
+                      matrix[i][j] > matrix[cell.first][cell.second] + 1)) {
                         continue;
                 }
                 queue.emplace(i, j);
@@ -33,49 +77,5 @@ public:
         }
         
         return matrix;
-    }
-};
-
-// Time:  O(m * n)
-// Space: O(m * n)
-// dp solution
-class Solution2 {
-public:
-    vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-        vector<vector<int> > dp(matrix.size(),
-                                vector<int>(matrix[0].size(),
-                                            numeric_limits<int>::max() - 10000));
-
-        for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[i].size(); ++j) {
-                if (matrix[i][j] == 0) {
-                    dp[i][j] = 0;
-                } else {
-                    if (i > 0) {
-                        dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1);
-                    }
-                    if (j > 0) {
-                        dp[i][j] = min(dp[i][j], dp[i][j - 1] + 1);
-                    }
-                }
-            }
-        }
-
-        for (int i = matrix.size() - 1; i >= 0; --i) {
-            for (int j = matrix[i].size() - 1; j >= 0; --j) {
-                if (matrix[i][j] == 0) {
-                    dp[i][j] = 0;
-                } else {
-                    if (i < matrix.size() - 1) {
-                        dp[i][j] = min(dp[i][j], dp[i + 1][j] + 1);
-                    }
-                    if (j < matrix[i].size() - 1) {
-                        dp[i][j] = min(dp[i][j], dp[i][j + 1] + 1);
-                    }
-                }
-            }
-        }
-
-        return dp;
     }
 };
