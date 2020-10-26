4b6deba75c658ff6e27a93b3845eab886c4fa786
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 19:26:53 +0800

    Update design-snake-game.cpp

--- C++/design-snake-game.cpp
+++ C++/design-snake-game.cpp
@@ -8,7 +8,7 @@ public:
         @param height - screen height 
         @param food - A list of food positions
         E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
-    SnakeGame(int width, int height, vector<pair<int, int>> food) :
+    SnakeGame(int width, int height, vector<vector<int>>& food) :
                width_{width}, height_{height}, score_{0},
                food_{food.begin(), food.end()}, snake_{{0, 0}}  {
                    
@@ -20,20 +20,20 @@ public:
         @return The game's score after the move. Return -1 if game over. 
         Game over when snake crosses the screen boundary or bites its body. */
     int move(string direction) {
-        const auto x = snake_.back().first + direction_[direction].first;
-        const auto y = snake_.back().second + direction_[direction].second;
+        const auto x = snake_.back()[0] + direction_[direction].first;
+        const auto y = snake_.back()[1] + direction_[direction].second;
         const auto tail = snake_.back();
 
-        auto it = lookup_.find(hash(snake_.front().first, snake_.front().second));
+        auto it = lookup_.find(hash(snake_.front()[0], snake_.front()[1]));
         lookup_.erase(it);
         snake_.pop_front();
         if (!valid(x, y)) {
             return -1;
-        } else if (!food_.empty() && food_.front().first == x && food_.front().second == y) {
+        } else if (!food_.empty() && food_.front()[0] == x && food_.front()[1] == y) {
             ++score_;
             food_.pop_front();
             snake_.push_front(tail);
-            lookup_.emplace(hash(tail.first, tail.second));
+            lookup_.emplace(hash(tail[0], tail[1]));
         }
         snake_.push_back({x, y});
         lookup_.emplace(hash(x, y));
@@ -53,15 +53,8 @@ private:
     }
 
     int width_, height_, score_;
-    deque<pair<int, int>> food_, snake_;
+    deque<vector<int>> food_, snake_;
     unordered_multiset<int> lookup_;
     unordered_map<string, pair<int, int>> direction_ = {{"U", {-1, 0}}, {"L", {0, -1}},
                                                         {"R", {0, 1}}, {"D", {1, 0}}};
 };
-
-/**
- * Your SnakeGame object will be instantiated and called as such:
- * SnakeGame obj = new SnakeGame(width, height, food);
- * int param_1 = obj.move(direction);
- */
- 
