b4992c5a716d015e6b3c02f394e13438f35dfa88
kamyu <kamyu104@users.noreply.github.com>
2020-10-27 01:23:40 +0800

    Update k-closest-points-to-origin.cpp

--- C++/k-closest-points-to-origin.cpp
+++ C++/k-closest-points-to-origin.cpp
@@ -7,7 +7,7 @@ public:
         static const auto& dist = [](const vector<int>& v) {
             return v[0] * v[0] + v[1] * v[1];
         };
-        nth_element(points.begin(), points.begin() + K, points.end(),
+        nth_element(points.begin(), points.begin() + K - 1, points.end(),
                     [&](const vector<int>& a, const vector<int>& b) {
                         return dist(a) < dist(b);
                     });
