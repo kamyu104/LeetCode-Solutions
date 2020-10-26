17b9c8dac617ee33412090a3b71461780b57d861
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 19:44:39 +0800

    Update design-snake-game.py

--- Python/design-snake-game.py
+++ Python/design-snake-game.py
@@ -1,7 +1,7 @@
 # Time:  O(1) per move
 # Space: O(s), s is the current length of the snake.
 
-from collections import defaultdict, deque
+from collections import deque
 
 class SnakeGame(object):
 
@@ -19,11 +19,11 @@ class SnakeGame(object):
         self.__width = width
         self.__height = height
         self.__score = 0
-        self.__food = deque(food)
+        self.__f = 0
+        self.__food = food
         self.__snake = deque([(0, 0)])
         self.__direction = {"U": (-1, 0), "L": (0, -1), "R": (0, 1), "D": (1, 0)}
-        self.__lookup = defaultdict(int)
-        self.__lookup[(0, 0)] += 1
+        self.__lookup = {(0, 0)}
 
     def move(self, direction):
         """
@@ -41,21 +41,17 @@ class SnakeGame(object):
         d = self.__direction[direction]
         x, y = self.__snake[-1][0] + d[0], self.__snake[-1][1] + d[1]
 
-        tail = self.__snake[-1]
-        self.__lookup[self.__snake[0]] -= 1
-        if self.__lookup[self.__snake[0]] == 0:
-            self.__lookup.pop(self.__snake[0])
+        tail = self.__snake[0]
+        self.__lookup.remove(tail)
         self.__snake.popleft()
         if not valid(x, y):
             return -1
-        elif self.__food and (self.__food[0][0], self.__food[0][1]) == (x, y):
+        elif self.__f != len(self.__food) and (self.__food[self.__f][0], self.__food[self.__f][1]) == (x, y):
             self.__score += 1
-            self.__food.popleft()
+            self.__f += 1
             self.__snake.appendleft(tail)
-            self.__lookup[tail] += 1
-        self.__snake += (x, y),
-        self.__lookup[(x, y)] += 1
+            self.__lookup.add(tail)
+        self.__snake.append((x, y))
+        self.__lookup.add((x, y))
         return self.__score
-
-
 
