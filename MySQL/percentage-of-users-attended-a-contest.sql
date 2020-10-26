5a4347965ba8a46ba340fc31fc39f7cd771f36a2
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 12:36:33 +0800

    Update percentage-of-users-attended-a-contest.sql

--- MySQL/percentage-of-users-attended-a-contest.sql
+++ MySQL/percentage-of-users-attended-a-contest.sql
@@ -2,8 +2,7 @@
 # Space: O(n)
 
 SELECT contest_id,
-       (Round(Count(contest_id) * 10000 /
-              (SELECT Count(*) FROM Users)) / 100) AS percentage
+       ROUND(COUNT(user_id) * 100 / (SELECT COUNT(*) FROM Users), 2) AS percentage
 FROM Register
 GROUP BY contest_id
 ORDER BY 2 DESC,
