1753e7350c006a20097279463e5df1d29e288729
kamyu <kamyu104@users.noreply.github.com>
2020-10-26 12:30:25 +0800

    Update path-with-minimum-effort.cpp

--- C++/path-with-minimum-effort.cpp
+++ C++/path-with-minimum-effort.cpp
@@ -240,3 +240,59 @@ private:
     };
  };
  
+// Time:  O(m * n * logh)
+// Space: O(m * n)
+// dfs solution
+class Solution5 {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+        static const int MAX_H = 1e6;
+        int left = 0, right = MAX_H;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (check(heights, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+
+private:
+    bool check(const vector<vector<int>>& heights, int x) {
+        static const vector<pair<int, int>> directions{{0, 1}, {1, 0},
+                                                       {0, -1}, {-1, 0}};
+
+        vector<pair<int, int>> stk({{0, 0}});
+        vector<vector<int>> lookup(size(heights), vector<int>(size(heights[0])));
+        while (!empty(stk)) {
+            const auto [r, c] = stk.back(); stk.pop_back();
+            if (r == size(heights) - 1 && c == size(heights[0]) - 1) {
+                return true;
+            }
+            for (const auto& [dr, dc] : directions) {
+                int nr = r + dr, nc = c + dc;
+                if (!(0 <= nr && nr < size(heights) &&
+                      0 <= nc && nc < size(heights[0]) &&
+                      abs(heights[nr][nc] - heights[r][c]) <= x &&
+                      !lookup[nr][nc])) {
+                    continue;
+                }
+                lookup[nr][nc] = true;
+                stk.emplace_back(nr, nc);
+            }
+        }
+        return false;
+    }
+    
+    template <typename T>
+    struct PairHash {
+        size_t operator()(const pair<T, T>& p) const {
+            size_t seed = 0;
+            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            return seed;
+        }
+    };
+ };
