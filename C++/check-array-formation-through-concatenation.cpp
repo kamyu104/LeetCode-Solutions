1aea0b5959452c25427c84f0760ec6c9e5a63d29
kamyu <kamyu104@users.noreply.github.com>
2020-11-01 22:18:36 +0800

    Create check-array-formation-through-concatenation.cpp

---
+++ C++/check-array-formation-through-concatenation.cpp
@@ -0,0 +1,24 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    bool canFormArray(vector<int>& arr, vector<vector<int>>& pieces) {
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < size(pieces); ++i) {
+            lookup[pieces[i][0]] = i;
+        }
+        for (int i = 0; i < size(arr);) {
+            if (!lookup.count(arr[i])) {
+                return false;
+            }
+            for (const auto& c : pieces[lookup[arr[i]]]) {
+                if (i == size(arr) || arr[i] != c) {
+                    return false;
+                }
+                ++i;
+            }
+        }
+        return true;
+    }
+};
