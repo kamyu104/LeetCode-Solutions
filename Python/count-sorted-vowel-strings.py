1b72293a115dd8bef29f09176ae0e141d7adeffe
kamyu <kamyu104@users.noreply.github.com>
2020-11-01 22:17:59 +0800

    Create count-sorted-vowel-strings.py

---
+++ Python/count-sorted-vowel-strings.py
@@ -0,0 +1,19 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def countVowelStrings(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def nCr(n, r):  # Time: O(n), Space: O(1)
+            if n-r < r:
+                return nCr(n, n-r)
+            c = 1
+            for k in xrange(1, r+1):
+                c *= n-k+1
+                c //= k
+            return c
+    
+        return nCr(n+4, 4)  # H(5, n) = nCr(n+5-1, n) = nCr(n+4, 4)
