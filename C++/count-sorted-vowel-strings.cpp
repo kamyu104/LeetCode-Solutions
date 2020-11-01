e84bcebb11ba13f64ba2d23649f0dc7dce0ae153
kamyu <kamyu104@users.noreply.github.com>
2020-11-01 22:17:17 +0800

    Create count-sorted-vowel-strings.cpp

---
+++ C++/count-sorted-vowel-strings.cpp
@@ -0,0 +1,22 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    int countVowelStrings(int n) {
+        return nCr(n + 4, 4);  // H(5, n) = nCr(n+5-1, n) = nCr(n+4, 4)
+    }
+
+private:
+    uint64_t nCr(int n, int r) {  // Time: O(n), Space: O(1)
+        if (n - r < r) {
+            return nCr(n, n - r);
+        }
+        uint64_t c = 1;
+        for (int k = 1; k <= r; ++k) {
+            c *= n - k + 1;
+            c /= k;
+        }
+        return c;
+    }
+};
