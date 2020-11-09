f6534ffcdba00d029181e2a06fdabdbf2caeee96
kamyu <kamyu104@users.noreply.github.com>
2020-11-09 12:27:11 +0800

    Update pancake-sorting.cpp

--- C++/pancake-sorting.cpp
+++ C++/pancake-sorting.cpp
@@ -1,7 +1,68 @@
-// Time:  O(n^2)
-// Space: O(1)
+// Time:  O(nlogn)
+// Space: O(n)
 
 class Solution {
+public:
+    vector<int> pancakeSort(vector<int>& arr) {
+        BIT bit(size(arr));
+        vector<int> result;
+        bit.add(arr[0] - 1, 1);
+        for (int i = 1; i < size(arr); bit.add(arr[i++] - 1, 1)) {
+            int count = bit.query((arr[i] - 1) - 1);
+            if (count  >= i) {
+                continue;
+            }
+            if (count == 0) {
+                if (i > 1) {
+                    result.emplace_back(i);
+                }
+                result.emplace_back(i + 1);
+            } else {
+                if (count > 1){
+                    result.emplace_back(count);
+                }
+                result.emplace_back(i);
+                result.emplace_back(i + 1);
+                result.emplace_back(count + 1);
+            }
+        }
+        return result;
+    }
+
+private:
+    class BIT {
+    public:
+        BIT(int n) : bit_(n + 1) {  // 0-indexed
+        }
+        
+        void add(int i, int val) {
+            ++i;
+            for (; i < size(bit_); i += lower_bit(i)) {
+                bit_[i] += val;
+            }
+        }
+
+        int query(int i) const {
+            ++i;
+            int total = 0;
+            for (; i > 0; i -= lower_bit(i)) {
+                total += bit_[i];
+            }
+            return total;
+        }
+    
+    private:
+        int lower_bit(int i) const {
+            return i & -i;
+        }
+        
+        vector<int> bit_;
+    };
+};
+
+// Time:  O(n^2)
+// Space: O(1)
+class Solution2 {
 public:
     vector<int> pancakeSort(vector<int>& A) {
         vector<int> result;
