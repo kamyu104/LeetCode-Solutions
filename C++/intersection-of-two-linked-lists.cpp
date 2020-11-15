ee1e20107bdff104efc4da3e1ee61e41648fd3f5
kamyu <kamyu104@users.noreply.github.com>
2020-11-15 23:08:43 +0800

    Update intersection-of-two-linked-lists.cpp

--- C++/intersection-of-two-linked-lists.cpp
+++ C++/intersection-of-two-linked-lists.cpp
@@ -13,32 +13,10 @@ class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode *curA = headA, *curB = headB;
-        ListNode *begin = nullptr, *tailA = nullptr, *tailB = nullptr;
-        while (curA && curB) {
-            if (curA == curB) {
-                begin = curA;
-                break;
-            }
-   
-            if (curA->next) {
-                curA = curA->next;
-            } else if (!tailA) {
-                tailA = curA;
-                curA = headB;
-            } else {
-                break;
-            }
-
-            if (curB->next) {
-                curB = curB->next;
-            } else if (!tailB) {
-                tailB = curB;
-                curB = headA;
-            } else {
-                break;
-            }
+        while (curA != curB) {
+            curA = curA ? curA->next : headB;
+            curB = curB ? curB->next : headA;
         }
-        
-        return begin;
+        return curA;
     }
 };
