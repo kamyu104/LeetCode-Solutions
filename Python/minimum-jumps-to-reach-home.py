b2894f39469adcdecb467b7bc87909bfd5b966bb
kamyu <kamyu104@users.noreply.github.com>
2020-11-16 02:25:09 +0800

    Create minimum-jumps-to-reach-home.py

---
+++ Python/minimum-jumps-to-reach-home.py
@@ -0,0 +1,37 @@
+# Time:  O(max(x, max(forbidden)) + a + b)
+# Space: O(max(x, max(forbidden)) + a + b)
+
+class Solution(object):
+    def minimumJumps(self, forbidden, a, b, x):
+        """
+        :type forbidden: List[int]
+        :type a: int
+        :type b: int
+        :type x: int
+        :rtype: int
+        """
+        max_f = max(forbidden)
+        max_val = x+b if a >= b or x > max_f else max_f+a+b
+        lookup = set()      
+        for pos in forbidden:
+            lookup.add((pos, True))
+            lookup.add((pos, False))
+        result = 0
+        q = [(0, True)]
+        lookup.add((0, True))
+        while q:
+            new_q = []
+            for pos, can_back in q:
+                if pos == x:
+                    return result
+                if pos+a <= max_val and (pos+a, True) not in lookup:
+                    lookup.add((pos+a, True))
+                    new_q.append((pos+a, True))
+                if not can_back:
+                    continue
+                if pos-b >= 0 and (pos-b, False) not in lookup:
+                    lookup.add((pos-b, False))
+                    new_q.append((pos-b, False))
+            q = new_q
+            result += 1
+        return -1
