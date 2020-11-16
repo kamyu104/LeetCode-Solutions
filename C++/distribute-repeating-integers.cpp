58672df8d813b77c5c1772ed466aea78cd9fc1fc
kamyu <kamyu104@users.noreply.github.com>
2020-11-16 23:57:22 +0800

    Update distribute-repeating-integers.cpp

--- C++/distribute-repeating-integers.cpp
+++ C++/distribute-repeating-integers.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n * 4^m) 
+// Time:  O(m * 3^m) 
 // Space: O(2^m)
 
 class Solution {
@@ -20,18 +20,24 @@ public:
         vector<vector<int>> dp(2, vector<int>(total + 1));
         dp[0][0] = 1;
         int i = 0;
-        for (const auto& [num, cnt] : count) {  // Time: O(n)
+        vector<int> cnts;
+        for (const auto& [_, cnt] : count) {
+            cnts.emplace_back(cnt);
+        }
+        nth_element(begin(cnts), begin(cnts) + size(quantity) - 1, end(cnts), greater<int>());
+        cnts.resize(min(size(cnts), size(quantity)));
+        for (int i = 0; i < size(cnts); ++i) {  // Time: O(m)
             dp[(i + 1) % 2] = vector<int>(total + 1);
-            for (int mask = total; mask >= 0; --mask) {  // Time: O(2^m)
+            // Time: O(3^m), see https://cp-algorithms.com/algebra/all-submasks.html
+            for (int mask = total; mask >= 0; --mask) {
                 dp[(i + 1) % 2][mask] |= dp[i % 2][mask];
-                for (int subMask = mask; subMask > 0; subMask = (subMask - 1) & mask) {  // Time: O(2^m)
-                    if (requirement[subMask] <= cnt && dp[i % 2][mask ^ subMask]) {
+                for (int subMask = mask; subMask > 0; subMask = (subMask - 1) & mask) {
+                    if (requirement[subMask] <= cnts[i] && dp[i % 2][mask ^ subMask]) {
                         dp[(i + 1) % 2][mask] = 1;
                     }
                 }
             }
-            ++i;
         }
-        return dp[size(count) % 2][total];
+        return dp[size(cnts) % 2][total];
     }
 };
