5bbb7bbf15935b2a7a1502d6ca94472a941050d5
kamyu <kamyu104@users.noreply.github.com>
2020-11-17 00:09:31 +0800

    Update distribute-repeating-integers.py

--- Python/distribute-repeating-integers.py
+++ Python/distribute-repeating-integers.py
@@ -1,7 +1,8 @@
-# Time:  O(m * 3^m) 
+# Time:  O(n + m * 3^m) 
 # Space: O(2^m)
 
 import collections
+import random
 
 
 class Solution(object):
@@ -11,6 +12,32 @@ class Solution(object):
         :type quantity: List[int]
         :rtype: bool
         """
+        def nth_element(nums, n, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+
+            left, right = 0, len(nums)-1
+            while left <= right:
+                pivot_idx = randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+
         count = collections.Counter(nums)
         total = (1<<len(quantity))-1
         requirement = [0]*(total+1)
@@ -25,7 +52,7 @@ class Solution(object):
         i = 0
         cnts = count.values()
         if len(quantity) < len(cnts):  # at most use top m cnts
-            cnts.sort(reverse=True)
+            nth_element(cnts, len(quantity)-1, lambda a, b: a > b)
             cnts = cnts[:len(quantity)]
         for cnt in cnts:  # Time: O(m)
             dp[(i+1)%2] = [0]*(total+1)
