dba2e1190e7b5de9fa5c4338556cb2948f445eaf
kamyu <kamyu104@users.noreply.github.com>
2020-11-18 09:33:37 +0800

    Update distribute-repeating-integers.py

--- Python/distribute-repeating-integers.py
+++ Python/distribute-repeating-integers.py
@@ -56,7 +56,9 @@ class Solution(object):
             cnts = cnts[:len(quantity)]
         for cnt in cnts:  # Time: O(m)
             dp[(i+1)%2] = [0]*(total+1)
-            # Time: O(3^m), see https://cp-algorithms.com/algebra/all-submasks.html
+            # submask enumeration:
+            # => sum(nCr(m, k) * 2^k for k in xrange(m)) = (1 + 2)^m = 3^m
+            # => Time: O(3^m), see https://cp-algorithms.com/algebra/all-submasks.html
             for mask in reversed(xrange(total+1)):
                 dp[(i+1)%2][mask] |= dp[i%2][mask]
                 sub_mask = mask
