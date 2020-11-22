2f77273f478e47a92487105160400b47fd5ba008
kamyu <kamyu104@users.noreply.github.com>
2020-11-22 17:46:42 +0800

    Update minimum-initial-energy-to-finish-tasks.cpp

--- C++/minimum-initial-energy-to-finish-tasks.cpp
+++ C++/minimum-initial-energy-to-finish-tasks.cpp
@@ -9,6 +9,7 @@ public:
                  return a[1] - a[0] < b[1] - b[0];  // sort by waste in asc
              });
         int result = 0;
+        // you can see proof here, https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/discuss/944633/Explanation-on-why-sort-by-difference
         for (const auto& task : tasks) {  // we need to pick all the wastes, so greedily to pick the least waste first is always better
             result = max(result + task[0], task[1]);
         }
