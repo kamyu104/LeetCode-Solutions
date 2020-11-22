6aa0325aaa512ee1b05be81ff1bf9c26d7082570
kamyu <kamyu104@users.noreply.github.com>
2020-11-22 17:46:07 +0800

    Update minimum-initial-energy-to-finish-tasks.py

--- Python/minimum-initial-energy-to-finish-tasks.py
+++ Python/minimum-initial-energy-to-finish-tasks.py
@@ -9,6 +9,7 @@ class Solution(object):
         """
         tasks.sort(key=lambda x: x[1]-x[0])  # sort by waste in asc
         result = 0
+        # you can see proof here, https://leetcode.com/problems/minimum-initial-energy-to-finish-tasks/discuss/944633/Explanation-on-why-sort-by-difference
         for a, m in tasks:  # we need to pick all the wastes, so greedily to pick the least waste first is always better
             result = max(result+a, m)
         return result
