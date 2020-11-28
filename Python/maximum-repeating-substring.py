fabeeaea6dc53a0d53da68104b578609f90bbb9e
kamyu <kamyu104@users.noreply.github.com>
2020-11-29 01:13:50 +0800

    Update maximum-repeating-substring.py

--- Python/maximum-repeating-substring.py
+++ Python/maximum-repeating-substring.py
@@ -1,7 +1,7 @@
 # Time:  O(n)
 # Space: O(m)
 
-# kmp solution
+# optimized kmp solution
 class Solution(object):
     def maxRepeating(self, sequence, word):
         """
@@ -30,8 +30,46 @@ class Solution(object):
                 j = prefix[j]
             if word[j+1] == sequence[i]:
                 j += 1
-            if j == len(word)-1:     
+            if j+1 == len(word):     
                 count = count+1 if i-prev == len(word) else 1
                 result = max(result, count)
                 j, prev = -1, i
         return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# kmp solution
+class Solution2(object):
+    def maxRepeating(self, sequence, word):
+        """
+        :type sequence: str
+        :type word: str
+        :rtype: int
+        """
+        def getPrefix(pattern):
+            prefix = [-1] * len(pattern)
+            j = -1
+            for i in xrange(1, len(pattern)):
+                while j > -1 and pattern[j + 1] != pattern[i]:
+                    j = prefix[j]
+                if pattern[j+1] == pattern[i]:
+                    j += 1
+                prefix[i] = j
+            return prefix
+
+        if len(sequence) < len(word):
+            return 0
+
+        new_word = word*(len(sequence)//len(word))
+        prefix = getPrefix(new_word)
+        result, count, j, prev = 0, 0, -1, -1
+        for i in xrange(len(sequence)):
+            while j > -1 and new_word[j+1] != sequence[i]:
+                j = prefix[j]
+            if new_word[j+1] == sequence[i]:
+                j += 1
+            result = max(result, j+1)
+            if j+1 == len(new_word):     
+                break
+        return result//len(word)
