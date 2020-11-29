cc657fe85871d708c0b01011c5c2433f6865c61b
kamyu <kamyu104@users.noreply.github.com>
2020-11-29 17:33:24 +0800

    Create minimize-deviation-in-array.py

---
+++ Python/minimize-deviation-in-array.py
@@ -0,0 +1,20 @@
+# Time:  O((n * log(max_num)) * logn)
+# Space: O(n)
+
+class Solution(object):
+    def minimumDeviation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_heap = [-num*2 if num%2 else -num for num in nums]
+        heapq.heapify(max_heap)
+        min_elem = -max(max_heap)
+        result = float("inf")
+        while len(max_heap) == len(nums):
+            num = -heapq.heappop(max_heap)
+            result = min(result, num-min_elem)
+            if not num%2:
+                min_elem = min(min_elem, num//2)
+                heapq.heappush(max_heap, -num//2)
+        return result
