b34ae5db33d080905117e916d8703391defef204
kamyu <kamyu104@users.noreply.github.com>
2020-12-07 03:15:28 +0800

    Create minimum-incompatibility.cpp

---
+++ C++/minimum-incompatibility.cpp
@@ -0,0 +1,140 @@
+// Time:  O(max(n * 2^n, 3^n))
+// Space: O(2^n)
+
+class Solution {
+public:
+    int minimumIncompatibility(vector<int>& nums, int k) {
+        const vector<int> candidates = findCandidates(nums, k);  // Time: O(n * 2^n)
+        const int total = (1 << size(nums)) - 1;
+        vector<int> dp(total + 1, numeric_limits<int>::max());
+        dp[0] = 0;
+        for (int mask = 0; mask <= total; ++mask) {  // Time: O(3^n)
+            if (__builtin_popcount(mask) % (size(nums) / k) != 0) {
+                continue;
+            }
+			// submask enumeration:
+            // => sum(nCr(m, k) * 2^k for k in xrange(m+1)) = (1 + 2)^m = 3^m
+            // => Time: O(3^m), see https://cp-algorithms.com/algebra/all-submasks.html
+            for (int submask = mask; submask; submask = (submask - 1) & mask) {
+                if (candidates[submask] == numeric_limits<int>::max() ||
+                    dp[mask - submask] == numeric_limits<int>::max()) {
+                    continue;
+                }
+                dp[mask] = min(dp[mask], dp[mask - submask] + candidates[submask]);
+            }
+        }
+        return dp.back() != numeric_limits<int>::max() ? dp.back() : -1;
+    }
+
+private:                        
+    vector<int> findCandidates(const vector<int>& nums, int k) {
+        const int total = (1 << size(nums)) - 1;
+        vector<int> result(total + 1, numeric_limits<int>::max());
+        for (int mask = 0; mask <= total; ++mask) {
+            if (__builtin_popcount(mask) != size(nums) / k) {
+                continue;
+            }
+            int lookup = 0;
+            int mx = 0, mn = numeric_limits<int>::max();
+            bool duplicated = false;
+            for (int i = 0; i < size(nums); ++i) {
+                if ((mask & (1 << i)) == 0) {
+                    continue;
+                }
+                if (lookup & (1 << nums[i])) {
+                    duplicated = true;
+                    break;
+                }
+                lookup |= (1 << nums[i]);
+                mx = max(mx, nums[i]);
+                mn = min(mn, nums[i]);
+            }
+            if (duplicated) {
+                continue;
+            }
+            result[mask] = mx - mn;
+        }
+        return result;
+    }
+};
+
+// Time:  O(sum(i*d * nCr(i*d, d) * nCr(n, i*d) for i in xrange(1, k+1))) < O(sum(n * 2^m * nCr(n, m) for m in xrange(n+1))) = O(n * 3^n)
+// Space: O(n * k)
+class Solution_TLE {
+public:
+    int minimumIncompatibility(vector<int>& nums, int k) {
+        unordered_map<string, int> lookup;
+        int result = backtracking(nums, size(nums) / k, &lookup);
+        return result != numeric_limits<int>::max() ? result : -1;
+    }
+
+private:
+    int backtracking(const vector<int>& nums, int d, unordered_map<string, int> *lookup) {
+        if (empty(nums)) {
+            return 0;
+        }
+        string k = key(nums);
+        if (!lookup->count(k)) {
+            int ret = numeric_limits<int>::max();
+            combinations(size(nums), d,
+                         [this, &nums, &d, &lookup, &ret](const vector<int>& idxs) {
+                             unordered_set<int> new_nums_set;
+                             for (const auto& idx : idxs) {
+                                 new_nums_set.emplace(nums[idx]);
+                             }
+                             if (size(new_nums_set) < d) {
+                                 return;
+                             }
+                             int mx = *max_element(cbegin(new_nums_set), cend(new_nums_set));
+                             int mn = *min_element(cbegin(new_nums_set), cend(new_nums_set));
+                             vector<int> left;
+                             for (const auto& num : nums) {
+                                 if (new_nums_set.count(num)) {
+                                     new_nums_set.erase(num);
+                                     continue;
+                                 }
+                                 left.emplace_back(num);
+                             }
+                             int result = backtracking(left, d, lookup);
+                             if (result != numeric_limits<int>::max()) {
+                                 ret = min(ret, mx - mn + result);
+                             }
+                         });
+            (*lookup)[k] = ret;
+        }
+        return (*lookup)[k];
+    }
+
+    string key(const vector<int>& nums) {
+        string result;
+        for (const auto& num : nums) {
+            result += to_string(num);
+            result.push_back(',');
+        }
+        return result;
+    }
+
+    void combinations(int n, int k, const function<void (const vector<int>&)>& callback) {
+        static const auto& next_pos =
+            [](const auto& n, const auto& k, const auto& idxs) {
+                int i = k - 1;
+                for (; i >= 0; --i) {
+                    if (idxs[i] != i + n - k) {
+                        break;
+                    }
+                }
+                return i;
+            };
+    
+        vector<int> idxs(k);
+        iota(begin(idxs), end(idxs), 0);
+        callback(idxs);
+        for (int i; (i = next_pos(n, k, idxs)) >= 0;) {
+            ++idxs[i];
+            for (int j = i + 1; j < k; ++j) {
+                idxs[j] = idxs[j - 1] + 1;
+            }
+            callback(idxs);
+        }
+    }
+};
