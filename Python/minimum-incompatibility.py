cc809124c0b9df10cc6b1dae4e2d8e335696346a
kamyu <kamyu104@users.noreply.github.com>
2020-12-07 02:21:25 +0800

    Create minimum-incompatibility.py

---
+++ Python/minimum-incompatibility.py
@@ -0,0 +1,89 @@
+# Time:  O(sum(i*d * nCr(i*d, d) * nCr(len(nums), i*d) for i in xrange(1, k+1)))
+# Space: O(n * k)
+
+class Solution(object):
+    def minimumIncompatibility(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        inf = (len(nums)-1)*(len(nums)//k)+1
+        def backtracking(nums, d, lookup):
+            if not nums:
+                return 0
+            if nums not in lookup:
+                ret = inf
+                for new_nums in itertools.combinations(nums, d):
+                    new_nums_set = set(new_nums)
+                    if len(new_nums_set) < d:
+                        continue
+                    left = []
+                    for num in nums:
+                        if num in new_nums_set:
+                            new_nums_set.remove(num)
+                            continue
+                        left.append(num)
+                    ret = min(ret, max(new_nums)-min(new_nums) + backtracking(tuple(left), d, lookup))
+                lookup[nums] = ret
+            return lookup[nums]
+        
+        result = backtracking(tuple(nums), len(nums)//k, {})
+        return result if result != inf else -1
+
+
+# Time:  O(max(n * 2^n, 3^n))
+# Space: O(2^n)
+class Solution_TLE(object):
+    def minimumIncompatibility(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        inf = (len(nums)-1)*(len(nums)//k)+1
+        POW = [1]
+        for i in xrange(len(nums)):
+            POW.append(POW[-1]<<1)
+        
+        def popcount(n):
+            result = 0
+            while n:
+                n &= n - 1
+                result += 1
+            return result
+    
+        def find_candidates(nums, k):
+            total = POW[len(nums)]-1
+            m = len(nums)//k
+            result = [inf]*(total+1)
+            for mask in xrange(total+1):
+                if popcount(mask) != m:
+                    continue
+                lookup = 0
+                mx, mn = 0, inf
+                for i in xrange(len(nums)):
+                    if mask&POW[i] == 0:
+                        continue
+                    if lookup&POW[nums[i]]:
+                        break
+                    lookup |= POW[nums[i]]
+                    mx = max(mx, nums[i])
+                    mn = min(mn, nums[i])
+                else:
+                    result[mask] = mx-mn
+            return result
+        
+        candidates = find_candidates(nums, k)
+        m = len(nums)//k
+        total = POW[len(nums)]-1
+        dp = [inf]*(total+1)
+        dp[0] = 0
+        for mask in xrange(total+1):
+            if popcount(mask) % m != 0:
+                continue
+            submask = mask
+            while submask:
+                dp[mask] = min(dp[mask], dp[mask-submask] + candidates[submask])
+                submask = (submask-1)&mask
+        return dp[-1] if dp[-1] != inf else -1
