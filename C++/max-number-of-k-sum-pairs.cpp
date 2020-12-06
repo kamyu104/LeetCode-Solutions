6415c48eb0e8b60ea593e399d02b2f0046a8fb3b
kamyu <kamyu104@users.noreply.github.com>
2020-12-07 03:31:35 +0800

    Update max-number-of-k-sum-pairs.cpp

--- C++/max-number-of-k-sum-pairs.cpp
+++ C++/max-number-of-k-sum-pairs.cpp
@@ -5,18 +5,13 @@ class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
         unordered_map<int, int> lookup;
-        for (const auto& num : nums) {
-            ++lookup[num];
-        }
         int result = 0;
-        for (const auto& [num, cnt] : lookup) {
-            if (k - num == num) {
-                result += cnt / 2;
-            } else if (lookup.count(k - num))  {
-                int c = min(cnt, lookup[k - num]);
-                result += c;
-                lookup[num] -= c;
-                lookup[k - num] -= c;
+        for (const auto& num : nums) {
+            if (lookup.count(k - num) && lookup[k - num]) {
+                --lookup[k - num];
+                ++result;
+            } else {
+                ++lookup[num];
             }
         }
         return result;
