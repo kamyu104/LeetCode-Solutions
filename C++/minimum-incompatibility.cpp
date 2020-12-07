bc26b5987864409bb8ee9d5bde11cf3171eb18af
kamyu <kamyu104@users.noreply.github.com>
2020-12-07 21:50:44 +0800

    Update minimum-incompatibility.cpp

--- C++/minimum-incompatibility.cpp
+++ C++/minimum-incompatibility.cpp
@@ -144,6 +144,8 @@ private:
 // wrong with greedy solution
 // nums = [15, 9, 7, 10, 15, 14, 12, 2, 10, 8, 10, 13, 4, 11, 2]
 // k = 5
+// greedy  => [[2, 4, 7], [2, 8, 9], [10, 11, 12], [10, 13, 15], [10, 14, 15]] => 24
+// correct => [[2, 4, 7], [2, 8, 10], [9, 10, 11], [10, 12, 15], [13, 14, 15]] => 22
 // optimized from Solution_Wrong_Greedy
 class Solution_Wrong_Greedy_Map {
 public:
@@ -218,6 +220,8 @@ private:
 // wrong with greedy solution
 // nums = [15, 9, 7, 10, 15, 14, 12, 2, 10, 8, 10, 13, 4, 11, 2]
 // k = 5
+// greedy  => [[2, 4, 7], [2, 8, 9], [10, 11, 12], [10, 13, 15], [10, 14, 15]] => 24
+// correct => [[2, 4, 7], [2, 8, 10], [9, 10, 11], [10, 12, 15], [13, 14, 15]] => 22
 class Solution_Wrong_Greedy {
 public:
     int minimumIncompatibility(vector<int>& nums, int k) {
