1e89cb4229d63c56ece1ccaee10c22d756700a1f
kamyu <kamyu104@users.noreply.github.com>
2020-12-07 16:48:56 +0800

    Update minimum-incompatibility.py

--- Python/minimum-incompatibility.py
+++ Python/minimum-incompatibility.py
@@ -1,10 +1,179 @@
-# Time:  O(nlogn + k * n), could be improved to O(nlogn) by ordered map or skip list
+# Time:  O(nlogn)
 # Space: O(n)
 
 import collections
+from random import randint, seed
 
 
+# Template modified from:
+# https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/design-skiplist.py
+class SkipNode(object):
+    def __init__(self, level=0, val=None):
+        self.val = val
+        self.nexts = [None]*level
+        self.prevs = [None]*level
+
+class SkipList(object):
+    P_NUMERATOR, P_DENOMINATOR = 1, 2  # P = 1/4 in redis implementation
+    MAX_LEVEL = 32  # enough for 2^32 elements
+
+    def __init__(self, end=float("inf"), can_duplicated=False, cmp=lambda x, y: x < y):
+        seed(0)
+        self.__head = SkipNode()
+        self.__len = 0
+        self.__can_duplicated = can_duplicated
+        self.__cmp = cmp
+        self.add(end)
+        self.__end = self.find(end)
+
+    def begin(self):
+        return self.__head.nexts[0]
+    
+    def end(self):
+        return self.__end
+
+    def lower_bound(self, target):
+        return self.__lower_bound(target, self.__find_prev_nodes(target))
+
+    def find(self, target):
+        return self.__find(target, self.__find_prev_nodes(target))
+        
+    def add(self, val):
+        if not self.__can_duplicated and self.find(val):
+            return self.find(val), False
+        node = SkipNode(self.__random_level(), val)
+        if len(self.__head.nexts) < len(node.nexts): 
+            self.__head.nexts.extend([None]*(len(node.nexts)-len(self.__head.nexts)))
+        prevs = self.__find_prev_nodes(val)
+        for i in xrange(len(node.nexts)):
+            node.nexts[i] = prevs[i].nexts[i]
+            if prevs[i].nexts[i]:
+                prevs[i].nexts[i].prevs[i] = node
+            prevs[i].nexts[i] = node
+            node.prevs[i] = prevs[i]
+        self.__len += 1
+        return node if self.__can_duplicated else (node, True)
+
+    def remove(self, it):
+        prevs = it.prevs
+        curr = self.__find(it.val, prevs)
+        if not curr:
+            return self.__end
+        self.__len -= 1   
+        for i in reversed(xrange(len(curr.nexts))):
+            prevs[i].nexts[i] = curr.nexts[i]
+            if curr.nexts[i]:
+                curr.nexts[i].prevs[i] = prevs[i]
+            if not self.__head.nexts[i]:
+                self.__head.nexts.pop()
+        return curr.nexts[0]
+    
+    def __lower_bound(self, val, prevs):
+        if prevs:
+            candidate = prevs[0].nexts[0]
+            if candidate:
+                return candidate
+        return None
+
+    def __find(self, val, prevs):
+        candidate = self.__lower_bound(val, prevs)
+        if candidate and candidate.val == val:
+            return candidate
+        return None
+
+    def __find_prev_nodes(self, val):
+        prevs = [None]*len(self.__head.nexts)
+        curr = self.__head
+        for i in reversed(xrange(len(self.__head.nexts))):
+            while curr.nexts[i] and self.__cmp(curr.nexts[i].val, val):
+                curr = curr.nexts[i]
+            prevs[i] = curr
+        return prevs
+
+    def __random_level(self):
+        level = 1
+        while randint(1, SkipList.P_DENOMINATOR) <= SkipList.P_NUMERATOR and \
+              level < SkipList.MAX_LEVEL:
+            level += 1
+        return level
+    
+    def __iter__(self):
+        it = self.begin()
+        while it != self.end():
+            yield it.val
+            it = it.nexts[0]
+
+    def __len__(self):
+        return self.__len-1  # excluding end node
+
+    def __str__(self):
+        result = []
+        for i in reversed(xrange(len(self.__head.nexts))):
+            result.append([])
+            curr = self.__head.nexts[i]
+            while curr:
+                result[-1].append(str(curr.val))
+                curr = curr.nexts[i]
+        return "\n".join(map(lambda x: "->".join(x), result))
+
+
+# optimized from Solution2
 class Solution(object):
+    def minimumIncompatibility(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def greedy(nums, k, is_reversed):
+            count = collections.Counter(nums)
+            if max(count.itervalues()) > k:
+                return -1
+            ordered_set = SkipList() if not is_reversed else SkipList(end=float("-inf"), cmp=lambda x, y: x > y)
+            freq_to_nodes = collections.defaultdict(collections.OrderedDict)
+            for x in sorted(count.keys(), reverse=is_reversed):
+                ordered_set.add(x)
+                freq_to_nodes[count[x]][x] = count[x]
+            stks = [[] for _ in xrange(k)] 
+            curr = 0
+            while ordered_set:  # the while loop runs O(k) times, and the inner loops runs O(n) times
+                if len(stks)-curr in freq_to_nodes:  # fill the deterministic elements into the remaining subsets
+                    for x in freq_to_nodes[len(stks)-curr].iterkeys():
+                        for i in xrange(curr, len(stks)):
+                            stks[i].append(x)
+                        count.pop(x)
+                        ordered_set.remove(ordered_set.find(x))
+                    freq_to_nodes.pop(len(stks)-curr)
+                # greedily fill the contiguous ordered elements into the first vacant subset until it is full,
+                # otherwise, the result sum would get larger
+                it = ordered_set.begin()
+                while it != ordered_set.end():
+                    x = it.val
+                    stks[curr].append(x)
+                    freq_to_nodes[count[x]].pop(x)
+                    if not freq_to_nodes[count[x]]:
+                        freq_to_nodes.pop(count[x])
+                    count[x] -= 1
+                    if not count[x]:
+                        count.pop(x)
+                        it = ordered_set.remove(it)
+                    else:
+                        freq_to_nodes[count[x]][x] = count[x]
+                        it = it.nexts[0]
+                    if len(stks[curr]) == len(nums)//k:
+                        curr += 1
+                        break
+            return sum([max(stk)-min(stk) for stk in stks])
+
+        return min(greedy(nums, k, False), greedy(nums, k, True))  # two possible minimas
+
+
+# Time:  O(nlogn + k * n), could be improved to O(nlogn) by ordered map or skip list
+# Space: O(n)
+import collections
+
+
+class Solution2(object):
     def minimumIncompatibility(self, nums, k):
         """
         :type nums: List[int]
@@ -47,7 +216,7 @@ class Solution(object):
 import itertools
 
 
-class Solution2(object):
+class Solution3(object):
     def minimumIncompatibility(self, nums, k):
         """
         :type nums: List[int]
