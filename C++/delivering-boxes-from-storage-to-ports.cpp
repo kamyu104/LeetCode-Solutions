a1d8a78145832109a96d072722991ab4ac048d54
kamyu <kamyu104@users.noreply.github.com>
2020-12-15 00:12:27 +0800

    Create delivering-boxes-from-storage-to-ports.cpp

---
+++ C++/delivering-boxes-from-storage-to-ports.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int boxDelivering(vector<vector<int>>& boxes, int portsCount, int maxBoxes, int maxWeight) {
+        vector<int> dp(size(boxes) + 1);
+        int left = 0, cost = 1, curr = 0;
+        for (int right = 0; right < size(boxes); ++right) {
+            if (right == 0 || boxes[right][0] != boxes[right - 1][0]) {
+                ++cost;
+            }
+            curr += boxes[right][1];
+            while (right - left + 1 > maxBoxes ||
+                   curr > maxWeight ||
+                   (left + 1 < right + 1 && dp[left + 1] == dp[left])) {
+                curr -= boxes[left][1];
+                if (boxes[left + 1][0] != boxes[left][0]) {
+                    --cost;
+                }
+                ++left;
+            }
+            dp[right + 1] = dp[(left - 1) + 1] + cost;
+        }
+        return dp[size(boxes)];
+    }
+};
