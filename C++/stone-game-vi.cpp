8cc6bd30fa2467e1f00655be256b414c776bc4f0
kamyu <kamyu104@users.noreply.github.com>
2020-12-14 23:37:34 +0800

    Create stone-game-vi.cpp

---
+++ C++/stone-game-vi.cpp
@@ -0,0 +1,19 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int stoneGameVI(vector<int>& aliceValues, vector<int>& bobValues) {
+        vector<vector<int>> sorted_vals;
+        vector<int> result(2);
+        for (int i = 0; i < size(aliceValues); ++i) {
+            sorted_vals.push_back({aliceValues[i] + bobValues[i],
+                                   aliceValues[i], bobValues[i]});
+        }
+        sort(begin(sorted_vals), end(sorted_vals), greater<vector<int>>());
+        for (int i = 0; i < size(sorted_vals); ++i) {
+            result[i % 2] += sorted_vals[i][1 + i % 2];
+        }
+        return (result[0] == result[1]) ? 0 : (result[0] > result[1]) ? 1 : -1;
+    }
+};
