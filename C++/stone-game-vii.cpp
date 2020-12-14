41b4462f7b2b35a304d131f0cd44b2b40210c7b6
kamyu <kamyu104@users.noreply.github.com>
2020-12-15 01:48:54 +0800

    Update stone-game-vii.cpp

--- C++/stone-game-vii.cpp
+++ C++/stone-game-vii.cpp
@@ -1,5 +1,5 @@
 // Time:  O(n^2)
-// Space: O(n^2)
+// Space: O(n)
 
 class Solution {
 public:
@@ -11,11 +11,11 @@ public:
         const auto& score = [&prefix](int i, int j) {
             return prefix[j + 1] - prefix[i];
         };
-        vector<vector<int>> dp(size(stones) , vector<int>(size(stones)));
+        vector<vector<int>> dp(2, vector<int>(size(stones)));
         for (int i = size(stones) - 1; i >= 0; --i) {
             for (int j = i + 1; j < size(stones); ++j) {
-                dp[i][j] = max(score(i + 1, j) - dp[i + 1][j],
-                               score(i, j - 1) - dp[i][j - 1]);
+                dp[i % 2][j] = max(score(i + 1, j) - dp[(i + 1) % 2][j],
+                                   score(i, j - 1) - dp[i % 2][j - 1]);
             }
         }
         return dp[0][size(stones) - 1];
