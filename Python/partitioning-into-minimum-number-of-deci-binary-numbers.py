4434a73e01869fe52f1f9ae1c6d1240bb854ef3b
kamyu <kamyu104@users.noreply.github.com>
2020-12-15 00:48:49 +0800

    Create partitioning-into-minimum-number-of-deci-binary-numbers.py

---
+++ Python/partitioning-into-minimum-number-of-deci-binary-numbers.py
@@ -0,0 +1,10 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minPartitions(self, n):
+        """
+        :type n: str
+        :rtype: int
+        """
+        return int(max(n))
