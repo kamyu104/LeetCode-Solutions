a91211eee56bc4ace0aec70986e6d24cf42dc118
kamyu <kamyu104@users.noreply.github.com>
2020-12-15 00:13:12 +0800

    Create delivering-boxes-from-storage-to-ports.py

---
+++ Python/delivering-boxes-from-storage-to-ports.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def boxDelivering(self, boxes, portsCount, maxBoxes, maxWeight):
+        """
+        :type boxes: List[List[int]]
+        :type portsCount: int
+        :type maxBoxes: int
+        :type maxWeight: int
+        :rtype: int
+        """
+        dp = [0]*(len(boxes)+1)
+        left, cost, curr = 0, 1, 0
+        for right in xrange(len(boxes)):
+            if right == 0 or boxes[right][0] != boxes[right-1][0]:
+                cost += 1
+            curr += boxes[right][1]
+            while right-left+1 > maxBoxes or \
+                  curr > maxWeight or \
+                  (left+1 < right+1 and dp[left+1] == dp[left]):
+                curr -= boxes[left][1]
+                if boxes[left+1][0] != boxes[left][0]:
+                    cost -= 1
+                left += 1
+            dp[right+1] = dp[(left-1)+1] + cost
+        return dp[len(boxes)]
