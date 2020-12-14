e64bf429861727472830451fa3e542d968ce0b10
kamyu <kamyu104@users.noreply.github.com>
2020-12-15 01:47:46 +0800

    Update stone-game-vii.py

--- Python/stone-game-vii.py
+++ Python/stone-game-vii.py
@@ -1,5 +1,5 @@
 # Time:  O(n^2)
-# Space: O(n^2)
+# Space: O(n)
 
 class Solution(object):
     def stoneGameVII(self, stones):
@@ -13,8 +13,8 @@ class Solution(object):
         prefix = [0]
         for stone in stones:
             prefix.append(prefix[-1]+stone)
-        dp = [[0 for _ in xrange(len(stones))] for _ in xrange(len(stones))]
+        dp = [[0 for _ in xrange(len(stones))] for _ in xrange(2)]
         for i in reversed(xrange(len(stones))):
             for j in xrange(i+1, len(stones)):
-                dp[i][j] = max(score(i+1, j)-dp[i+1][j], score(i, j-1)-dp[i][j-1])
+                dp[i%2][j] = max(score(i+1, j)-dp[(i+1)%2][j], score(i, j-1)-dp[i%2][j-1])
         return dp[0][-1]
