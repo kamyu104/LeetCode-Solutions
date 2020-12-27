29a784084d119498f6a9c2b30f0bd02125e45c9b
kamyu <kamyu104@users.noreply.github.com>
2020-12-28 02:01:52 +0800

    Create minimum-adjacent-swaps-for-k-consecutive-ones.py

---
+++ Python/minimum-adjacent-swaps-for-k-consecutive-ones.py
@@ -0,0 +1,22 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def minMoves(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def moves(i, j):
+            return prefix[j+1]-prefix[i]
+
+        idxs = [i for i, x in enumerate(nums) if x]
+        prefix = [0]*(len(idxs)+1)
+        for i in xrange(len(idxs)):
+            prefix[i+1] = prefix[i]+idxs[i]
+        result = float("inf")
+        for i in xrange(len(idxs)-k+1):
+            result = min(result, -moves(i, i+(k+1)//2-1) + moves(i+k//2, i+k-1))  # take each i+k//2 as median, find min dist to median
+        result -= (k//2)*((k+1)//2)  # rollback extra moves to the expected positions
+        return result
