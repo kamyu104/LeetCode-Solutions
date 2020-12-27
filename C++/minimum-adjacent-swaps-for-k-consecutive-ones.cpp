0ddc03ea3b57eb9bc85bdca27f4064557e0486b3
kamyu <kamyu104@users.noreply.github.com>
2020-12-28 02:01:02 +0800

    Create minimum-adjacent-swaps-for-k-consecutive-ones.cpp

---
+++ C++/minimum-adjacent-swaps-for-k-consecutive-ones.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int minMoves(vector<int>& nums, int k) {
+        vector<int> idxs;
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i]) {
+                idxs.emplace_back(i);
+            }
+        }
+        vector<uint64_t> prefix(size(idxs) + 1);
+        for (int i = 0; i < size(idxs); ++i) {
+            prefix[i + 1] = prefix[i] + idxs[i];
+        }
+        const auto& score = [&prefix](int i, int j) {
+                                return prefix[j + 1] - prefix[i];
+                            };
+        uint64_t result = numeric_limits<uint64_t>::max();
+        for (int i = 0; i < size(idxs) - k + 1; ++i) {
+            result = min(result, -score(i, i + (k + 1) / 2 - 1) + score(i + k / 2, i + k - 1));
+        }
+        result -= (k / 2) * ((k + 1) / 2);
+        return result;
+    }
+};
