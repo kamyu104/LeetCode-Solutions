23fad19450287b4a65ef7d4945af88bc9c43ea95
kamyu <kamyu104@users.noreply.github.com>
2020-12-27 17:59:04 +0800

    Update number-of-distinct-substrings-in-a-string.cpp

--- C++/number-of-distinct-substrings-in-a-string.cpp
+++ C++/number-of-distinct-substrings-in-a-string.cpp
@@ -5,10 +5,10 @@ class Solution {
 public:
     int countDistinct(string s) {
         vector<array<int, 26>> trie(1);
-        for(int i = 0; i < size(s); ++i) {
+        for (int i = 0; i < size(s); ++i) {
             int curr = 0;
-            for(int j = i; j < size(s); ++j) {
-                if(!trie[curr][s[j] - 'a']) {
+            for (int j = i; j < size(s); ++j) {
+                if (!trie[curr][s[j] - 'a']) {
                     trie.emplace_back();
                     trie[curr][s[j] - 'a'] = size(trie) - 1;
                 }
