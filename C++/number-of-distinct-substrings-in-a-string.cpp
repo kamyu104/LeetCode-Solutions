cace1a5ea8061fe67b3099fa40e775ea5b892c08
kamyu <kamyu104@users.noreply.github.com>
2020-12-27 17:56:53 +0800

    Create number-of-distinct-substrings-in-a-string.cpp

---
+++ C++/number-of-distinct-substrings-in-a-string.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n^2)
+// Space: O(t), t is the number of trie nodes
+
+class Solution {
+public:
+    int countDistinct(string s) {
+        vector<array<int, 26>> trie(1);
+        for(int i = 0; i < size(s); ++i) {
+            int curr = 0;
+            for(int j = i; j < size(s); ++j) {
+                if(!trie[curr][s[j] - 'a']) {
+                    trie.emplace_back();
+                    trie[curr][s[j] - 'a'] = size(trie) - 1;
+                }
+                curr = trie[curr][s[j] - 'a'];
+            }
+        }
+        return size(trie) - 1;
+    }
+};
