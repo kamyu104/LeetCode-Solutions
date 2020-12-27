858d4754cd78f2f60bb2288f6731497b846cfe03
kamyu <kamyu104@users.noreply.github.com>
2020-12-28 02:56:17 +0800

    Update minimum-adjacent-swaps-for-k-consecutive-ones.py

--- Python/minimum-adjacent-swaps-for-k-consecutive-ones.py
+++ Python/minimum-adjacent-swaps-for-k-consecutive-ones.py
@@ -17,6 +17,6 @@ class Solution(object):
             prefix[i+1] = prefix[i]+idxs[i]
         result = float("inf")
         for i in xrange(len(idxs)-k+1):
-            result = min(result, -moves(i, i+(k+1)//2-1) + moves(i+k//2, i+k-1))  # take each i+k//2 as median, find min dist to median
+            result = min(result, -moves(i, i+k//2-1) + moves(i+(k+1)//2, i+k-1))  # take each i+k//2 as median, find min dist to median
         result -= (k//2)*((k+1)//2)  # rollback extra moves to the expected positions
         return result
