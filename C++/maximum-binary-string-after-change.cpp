779aa2a41a76817e4e6a7b00ab2f0f796d91b079
kamyu <kamyu104@users.noreply.github.com>
2020-12-28 01:13:24 +0800

    Create maximum-binary-string-after-change.cpp

---
+++ C++/maximum-binary-string-after-change.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string maximumBinaryString(string binary) {
+        int zeros = 0, ones = 0;
+        for (auto& c : binary) {
+            if (c == '0') {
+                ++zeros;
+            } else if (zeros == 0) {
+                ++ones;
+            }
+            c = '1';
+        }
+        if (ones < size(binary)) {
+            binary[ones + zeros - 1] = '0';
+        }
+        return binary;
+    }
+};
