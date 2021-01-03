4cd8e0cd361b674aef8fd6da236e477f6dc0347b
kamyu <kamyu104@users.noreply.github.com>
2021-01-03 18:26:44 +0800

    Update last-substring-in-lexicographical-order.py

--- Python/last-substring-in-lexicographical-order.py
+++ Python/last-substring-in-lexicographical-order.py
@@ -1,10 +1,32 @@
 # Time:  O(n)
-# Space: O(n)
+# Space: O(1)
 
+class Solution(object):
+    def lastSubstring(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        left, right, l = 0, 1, 0
+        while right+l < len(s):
+            if s[left+l] == s[right+l]:
+                l += 1
+                continue
+            if s[left+l] > s[right+l]:
+                right += l+1
+            else:
+                left = max(right, left+l+1)
+                right = left+1
+            l = 0
+        return s[left:]
+
+
+# Time:  O(n)
+# Space: O(n)
 import collections
 
 
-class Solution(object):
+class Solution2(object):
     def lastSubstring(self, s):
         """
         :type s: str
