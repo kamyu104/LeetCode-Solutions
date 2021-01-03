e6c8ea8f5448d8461db9afc207a63327729b783d
kamyu <kamyu104@users.noreply.github.com>
2021-01-03 18:28:58 +0800

    Update last-substring-in-lexicographical-order.cpp

--- C++/last-substring-in-lexicographical-order.cpp
+++ C++/last-substring-in-lexicographical-order.cpp
@@ -1,7 +1,30 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(1)
 
 class Solution {
+public:
+    string lastSubstring(string s) {
+        int left = 0, right = 1, l = 0;
+        while (right + l < size(s)) {
+            if (s[left + l] == s[right + l]) {
+                ++l;
+                continue;
+            }
+            if (s[left + l] > s[right + l]) {
+                right += l + 1;
+            } else {
+                left = max(right, left + l + 1);
+                right = left + 1;
+            }
+            l = 0;
+        }
+        return s.substr(left);
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
 public:
     string lastSubstring(string s) {
         unordered_map<char, vector<size_t>> count;
