368faf22254b3f5ddb4267e912fb160b3ec44669
kamyu <kamyu104@users.noreply.github.com>
2021-01-10 23:37:51 +0800

    Create minimize-hamming-distance-after-swap-operations.cpp

---
+++ C++/minimize-hamming-distance-after-swap-operations.cpp
@@ -0,0 +1,72 @@
+// Time:  O(n * α(n)) ~= O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    int minimumHammingDistance(vector<int>& source, vector<int>& target, vector<vector<int>>& allowedSwaps) {
+        UnionFind uf(size(source));
+        for (const auto& swap : allowedSwaps) {
+            uf.union_set(swap[0], swap[1]);
+        }
+        unordered_map<int, unordered_set<int>> groups;
+        for (int i = 0; i < size(source); ++i) {
+            groups[uf.find_set(i)].emplace(i);
+        }
+        int result = 0;
+        for (const auto& [_, group] : groups) {
+            unordered_map<int, int> source_cnt, target_cnt;
+            for (const auto& i : group) {
+                ++source_cnt[source[i]];
+                ++target_cnt[target[i]];
+            }
+            for (const auto& [v , cnt] : source_cnt) {
+                result += max(cnt - target_cnt[v], 0);
+            }
+        }
+        return result;
+    }
+
+private:
+    class UnionFind {
+    public:
+        UnionFind(const int n)
+         : set_(n)
+         , rank_(n)
+         , count_(n) {
+            iota(set_.begin(), set_.end(), 0);
+        }
+
+        int find_set(const int x) {
+           if (set_[x] != x) {
+               set_[x] = find_set(set_[x]);  // Path compression.
+           }
+           return set_[x];
+        }
+
+        bool union_set(const int x, const int y) {
+            int x_root = find_set(x), y_root = find_set(y);
+            if (x_root == y_root) {
+                return false;
+            }
+            if (rank_[x_root] < rank_[y_root]) {  // Union by rank.
+                set_[x_root] = y_root;
+            } else if (rank_[x_root] > rank_[y_root]) {
+                set_[y_root] = x_root;
+            } else {
+                set_[y_root] = x_root;
+                ++rank_[x_root];
+            }
+            --count_;
+            return true;
+        }
+
+        int size() const {
+            return count_;
+        }
+
+    private:
+        vector<int> set_;
+        vector<int> rank_;
+        int count_;
+    };
+};
