e5044575acadbc1d0e4accd2c1aae22e5f93e6cd
kamyu <kamyu104@users.noreply.github.com>
2021-01-10 22:42:26 +0800

    Create number-of-ways-to-reconstruct-a-tree.cpp

---
+++ C++/number-of-ways-to-reconstruct-a-tree.cpp
@@ -0,0 +1,47 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int checkWays(vector<vector<int>>& pairs) {
+        unordered_map<int, unordered_set<int>> adj;
+        for (const auto& pair : pairs) {
+            adj[pair[0]].emplace(pair[1]);
+            adj[pair[1]].emplace(pair[0]);
+        }
+        int n = size(adj);
+        bool mul = false;
+        unordered_set<int> lookup;
+        vector<int> nodes;
+        transform(begin(adj), end(adj), back_inserter(nodes),
+                  [](const auto& kvp) { return kvp.first; });
+        sort(begin(nodes), end(nodes),
+             [&adj](const auto& a, const auto& b) {
+                 return size(adj[a]) > size(adj[b]);
+             });
+        int result = 1;
+        for (const auto& node : nodes) {
+            lookup.emplace(node);
+            int parent = 0;
+            for (const auto& x : adj[node]) {
+                if (!lookup.count(x)) {
+                    continue;
+                }
+                if (parent == 0 || size(adj[x]) < size(adj[parent])) {
+                    parent = x;
+                }
+            }
+            if (parent) {
+                for (const auto& x : adj[node]) {
+                    if (x != parent && !adj[parent].count(x)) {
+                        return 0;
+                    }
+                }
+                mul |= size(adj[node]) == size(adj[parent]);
+            } else if (size(adj[node]) != n - 1) {
+                return 0;
+            }
+        }
+        return result + mul;
+    }
+};
