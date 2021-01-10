7977a11823ebc0f334869a64f040246e86361204
kamyu <kamyu104@users.noreply.github.com>
2021-01-11 00:04:23 +0800

    Update minimize-hamming-distance-after-swap-operations.py

--- Python/minimize-hamming-distance-after-swap-operations.py
+++ Python/minimize-hamming-distance-after-swap-operations.py
@@ -1,6 +1,45 @@
-# Time:  O(n * α(n)) ~= O(n)
+# Time:  O(n)
 # Space: O(n)
 
+class Solution(object):
+    def minimumHammingDistance(self, source, target, allowedSwaps):
+        """
+        :type source: List[int]
+        :type target: List[int]
+        :type allowedSwaps: List[List[int]]
+        :rtype: int
+        """
+        def iter_flood_fill(adj, node, lookup, idxs):
+            stk = [node]
+            while stk:
+                node = stk.pop()
+                if node in lookup:
+                    continue
+                lookup.add(node)
+                idxs.append(node)
+                for child in adj[node]:
+                    stk.append(child)
+
+        adj = [set() for i in xrange(len(source))]
+        for i, j in allowedSwaps:
+            adj[i].add(j)
+            adj[j].add(i)
+        result = 0
+        lookup = set()
+        for i in xrange(len(source)):
+            if i in lookup:
+                continue
+            idxs = []
+            iter_flood_fill(adj, i, lookup, idxs)
+            source_cnt = collections.Counter([source[i] for i in idxs])
+            target_cnt = collections.Counter([target[i] for i in idxs])
+            diff = source_cnt-target_cnt
+            result += sum(diff.itervalues())
+        return result
+
+
+# Time:  O(n * α(n)) ~= O(n)
+# Space: O(n)
 import collections
 
 
@@ -32,7 +71,7 @@ class UnionFind(object):  # Time: (n * α(n)), Space: O(n)
         return True
 
 
-class Solution(object):
+class Solution2(object):
     def minimumHammingDistance(self, source, target, allowedSwaps):
         """
         :type source: List[int]
@@ -43,7 +82,7 @@ class Solution(object):
         uf = UnionFind(len(source))
         for x, y in allowedSwaps: 
             uf.union_set(x, y)
-        groups = defaultdict(set)
+        groups = collections.defaultdict(set)
         for i in xrange(len(source)):
             groups[uf.find_set(i)].add(i)
         result = 0
