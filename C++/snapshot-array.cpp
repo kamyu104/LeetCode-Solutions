8d4fcf5dc352a1a7dfb7e3a1be02cee95897ad19
kamyu <kamyu104@users.noreply.github.com>
2021-01-17 02:19:36 +0800

    Update snapshot-array.cpp

--- C++/snapshot-array.cpp
+++ C++/snapshot-array.cpp
@@ -9,6 +9,39 @@ public:
     
     void set(int index, int val) {
         if (!A_.count(index)) {
+            A_[index].emplace_back(0, 0);
+        }
+        A_[index].emplace_back(snap_id_, val);
+    }
+    
+    int snap() {
+        return snap_id_++;
+    }
+    
+    int get(int index, int snap_id) {
+        if (!A_.count(index)) {
+            A_[index].emplace_back(0, 0);
+        }
+        const auto& it = prev(lower_bound(A_[index].cbegin(), A_[index].cend(),
+                                          make_pair(snap_id + 1, 0)));
+        return it->second;
+    }
+
+private:
+    unordered_map<int, vector<pair<int, int>>> A_;
+    int snap_id_ = 0;
+};
+
+// Time:  set: O(logn)
+//        get: O(logn), n is the total number of set
+// Space: O(n)
+class SnapshotArray2 {
+public:
+    SnapshotArray(int length) {
+    }
+    
+    void set(int index, int val) {
+        if (!A_.count(index)) {
             A_[index][0] = 0;
         }
         A_[index][snap_id_] = val;
