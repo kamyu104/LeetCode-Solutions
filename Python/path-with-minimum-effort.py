6be6b09286b6cb31811f909c13bdd5d3eb5822ae
kamyu <kamyu104@users.noreply.github.com>
2021-01-17 00:26:41 +0800

    Update path-with-minimum-effort.py

--- Python/path-with-minimum-effort.py
+++ Python/path-with-minimum-effort.py
@@ -42,7 +42,7 @@ import collections
 class UnionFind(object):  # Time: (n * α(n)), Space: O(n)
     def __init__(self, n):
         self.set = range(n)
-        self.rank = range(n)
+        self.rank = [0]*n
 
     def find_set(self, x):
         stk = []
