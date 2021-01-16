1f37ebae968c5af1f302398ea3989ce048348fee
kamyu <kamyu104@users.noreply.github.com>
2021-01-17 02:27:22 +0800

    Update snapshot-array.cpp

--- C++/snapshot-array.cpp
+++ C++/snapshot-array.cpp
@@ -11,7 +11,11 @@ public:
         if (!A_.count(index)) {
             A_[index].emplace_back(0, 0);
         }
-        A_[index].emplace_back(snap_id_, val);
+        if (A_[index].back().first == snap_id_) {
+            A_[index].back().second = val;
+        } else {
+            A_[index].emplace_back(snap_id_, val);
+        }
     }
     
     int snap() {
@@ -23,7 +27,7 @@ public:
             A_[index].emplace_back(0, 0);
         }
         const auto& it = prev(lower_bound(A_[index].cbegin(), A_[index].cend(),
-                                          make_pair(snap_id + 1, 0)));
+                                          make_pair(snap_id + 1, numeric_limits<int>::min())));
         return it->second;
     }
 
@@ -37,7 +41,7 @@ private:
 // Space: O(n)
 class SnapshotArray2 {
 public:
-    SnapshotArray(int length) {
+    SnapshotArray2(int length) {
     }
     
     void set(int index, int val) {
