57a110aa6fcde389eead2fb1b762595ceb077aa0
kamyu <kamyu104@users.noreply.github.com>
2021-01-18 03:15:00 +0800

    Update cat-and-mouse.py

--- Python/cat-and-mouse.py
+++ Python/cat-and-mouse.py
@@ -52,3 +52,72 @@ class Solution(object):
                     color[ni, nj, nt] = c
                     q.append((ni, nj, nt, c))
         return color[MOUSE_START, CAT_START, MOUSE]
+
+    
+# Time:  O(n^3)
+# Space: O(n^2)
+import collections
+
+
+class Solution2(object):
+    def catMouseGame(self, graph):
+        """
+        :type graph: List[List[int]]
+        :rtype: int
+        """
+        HOLE, MOUSE_START, CAT_START = range(3)
+        DRAW, MOUSE, CAT = 0, 1, 2
+        def parents(m, c, t):
+            if t == CAT:
+                for nm in graph[m]:
+                    yield nm
+            else:
+                for nc in graph[c]:
+                    yield nc
+
+        color = collections.defaultdict(int)
+        degree = {}
+        ignore = set(graph[HOLE])
+        for m in xrange(len(graph)):
+            for c in xrange(len(graph)):
+                degree[m, c, MOUSE] = len(graph[m])
+                degree[m, c, CAT] = len(graph[c])-(c in ignore)
+        q1 = collections.deque()
+        q2 = collections.deque()
+        for i in xrange(len(graph)):
+            if i == HOLE:
+                continue
+            color[HOLE, i, CAT] = MOUSE
+            q1.append((HOLE, i, CAT))
+            for t in [MOUSE, CAT]:
+                color[i, i, t] = CAT
+                q2.append((i, i, t))
+        while q1:
+            i, j, t = q1.popleft()
+            if t == CAT:
+                for ni in parents(i, j, t):
+                    if ni != HOLE and j != HOLE and ni != j and color[ni, j, MOUSE] == DRAW:
+                        color[ni, j, MOUSE] = MOUSE
+                        q1.append((ni, j, MOUSE))
+            else:
+                for nj in parents(i, j, t):
+                    if i != HOLE and nj != HOLE and i != nj and color[i, nj, CAT] == DRAW:
+                        degree[i, nj, CAT] -= 1
+                        if not degree[i, nj, CAT]:
+                            color[i, nj, CAT] = MOUSE
+                            q1.append((i, nj, CAT))
+        while q2:
+            i, j, t = q2.popleft()
+            if t == MOUSE:
+                for nj in parents(i, j, t):
+                    if i != HOLE and nj != HOLE and i != nj and color[i, nj, CAT] == DRAW:
+                        color[i, nj, CAT] = CAT
+                        q2.append((i, nj, CAT))
+            else:
+                for ni in parents(i, j, t):
+                    if ni != HOLE and j != HOLE and ni != j and color[ni, j, MOUSE] == DRAW:
+                        degree[ni, j, MOUSE] -= 1
+                        if not degree[ni, j, MOUSE]:
+                            color[ni, j, MOUSE] = CAT
+                            q2.append((ni, j, MOUSE))
+        return color[MOUSE_START, CAT_START, MOUSE]
