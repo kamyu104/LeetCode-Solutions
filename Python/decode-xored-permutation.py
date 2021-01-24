615293d03130d55e6acfae828baf4f20862e8cc2
kamyu <kamyu104@users.noreply.github.com>
2021-01-25 00:58:04 +0800

    Create decode-xored-permutation.py

---
+++ Python/decode-xored-permutation.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def decode(self, encoded):
+        """
+        :type encoded: List[int]
+        :rtype: List[int]
+        """
+        curr = 0
+        for i in xrange((len(encoded)+1) + 1):
+            curr ^= i
+            if i <= len(encoded) and i%2 == 1:
+                curr ^= encoded[i]
+        result = [curr]
+        for x in encoded:
+            result.append(result[-1]^x)
+        return result
