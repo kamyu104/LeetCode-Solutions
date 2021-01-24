654956e9fb817d8652108064eca9b7bae906374e
kamyu <kamyu104@users.noreply.github.com>
2021-01-25 00:57:28 +0800

    Create decode-xored-permutation.cpp

---
+++ C++/decode-xored-permutation.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded) {
+        int curr = 0;
+        for (int i = 0; i <= size(encoded) + 1; ++i) {
+            curr ^= i;
+            if (i <= size(encoded) && i % 2 == 1) {
+                curr ^= encoded[i];
+            }
+        }
+        vector<int> result = {curr};
+        for (const auto& x: encoded) {
+            result.emplace_back(result.back() ^ x);
+        }
+        return result;
+    }
+};
