310d4affe6fa3cc8de8a612ec0d598837bacf879
kamyu <kamyu104@users.noreply.github.com>
2021-01-31 18:04:31 +0800

    Create restore-the-array-from-adjacent-pairs.py

---
+++ Python/restore-the-array-from-adjacent-pairs.py
@@ -0,0 +1,17 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def restoreArray(self, adjacentPairs):
+        """
+        :type adjacentPairs: List[List[int]]
+        :rtype: List[int]
+        """
+        adj = collections.defaultdict(list)
+        for u, v in adjacentPairs: 
+            adj[u].append(v)
+            adj[v].append(u)
+        result = next([x, adj[x][0]] for x in adj if len(adj[x]) == 1)
+        while len(result) != len(adjacentPairs)+1:
+            result.append(adj[result[-1]][adj[result[-1]][0] == result[-2]])
+        return result
