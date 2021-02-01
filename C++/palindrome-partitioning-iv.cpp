85faf2c3695a8304333e9941d1612bd55835776a
kamyu <kamyu104@users.noreply.github.com>
2021-02-01 12:56:40 +0800

    Update palindrome-partitioning-iv.cpp

--- C++/palindrome-partitioning-iv.cpp
+++ C++/palindrome-partitioning-iv.cpp
@@ -4,31 +4,14 @@
 class Solution {
 public:
     bool checkPartitioning(string s) {
-        return modifiedManacher(s);
-    }
-
-private:
-    bool modifiedManacher(const string& s) {
-        string T = preProcess(s);
-        const int n = size(T);
-        vector<int> P(n);
+        const auto& P = manacher(s);
         vector<int> prefix, suffix;
-        int C = 0, R = 0;
-        for (int i = 1; i < n - 1; ++i) {
-            int i_mirror = 2 * C - i;
-            P[i] = (R > i) ? min(R - i, P[i_mirror]) : 0;
-            while (T[i + 1 + P[i]] == T[i - 1 - P[i]]) {
-                ++P[i];
-            }
-            if (i + 1 + P[i] == n - 1) {
-                suffix.emplace_back(i);
-            }
+        for (int i = 1; i < size(P) - 1; ++i) {
             if (i - 1 - P[i] == 0) {
                 prefix.emplace_back(i);
             }
-            if (i + P[i] > R) {
-                C = i;
-                R = i + P[i];
+            if (i + 1 + P[i] == size(P) - 1) {
+                suffix.emplace_back(i);
             }
         }
         for (const auto& i : prefix) {
@@ -41,6 +24,26 @@ private:
             }
         }
         return false;
+    }
+
+private:
+    vector<int> manacher(const string& s) {
+        string T = preProcess(s);
+        const int n = size(T);
+        vector<int> P(n);
+        int C = 0, R = 0;
+        for (int i = 1; i < n - 1; ++i) {
+            int i_mirror = 2 * C - i;
+            P[i] = (R > i) ? min(R - i, P[i_mirror]) : 0;
+            while (T[i + 1 + P[i]] == T[i - 1 - P[i]]) {
+                ++P[i];
+            }
+            if (i + P[i] > R) {
+                C = i;
+                R = i + P[i];
+            }
+        }
+        return P;
     }
         
     string preProcess(const string& s) {
