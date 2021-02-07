c0c266c4b858ea2f66633846ba69a87573c04f67
kamyu <kamyu104@users.noreply.github.com>
2021-02-08 02:35:59 +0800

    Create closest-subsequence-sum.py

---
+++ Python/closest-subsequence-sum.py
@@ -0,0 +1,36 @@
+# Time:  O(n * 2^(n/2))
+# Space: O(n * 2^(n/2))
+
+import bisect
+
+
+class Solution(object):
+    def minAbsDifference(self, nums, goal):
+        """
+        :type nums: List[int]
+        :type goal: int
+        :rtype: int
+        """
+        result = abs(goal)
+        sums1 = set([0])
+        for i in xrange(len(nums)//2):
+            for x in list(sums1):
+                if x+nums[i] in sums1:
+                    continue
+                sums1.add(x+nums[i])
+                result = min(result, abs(goal-x-nums[i]))  # case of right half part is 0
+        sorted_sums1 = sorted(sums1)
+        sums2 = set([0])
+        for i in xrange(len(nums)//2, len(nums)):
+            for x in list(sums2):
+                if x+nums[i] in sums2:
+                    continue
+                sums2.add(x+nums[i])
+                ni = bisect.bisect_left(sorted_sums1, goal-x-nums[i])
+                if ni < len(sorted_sums1):
+                    result = min(result, abs(goal-x-nums[i]-sorted_sums1[ni]))
+                if ni > 0:
+                    result = min(result, abs(goal-x-nums[i]-sorted_sums1[ni-1]))
+                if result == 0:
+                    return result
+        return result
