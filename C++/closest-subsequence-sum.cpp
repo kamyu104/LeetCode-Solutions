1c3f8b0ebfb5a807f8e0b4cbcedd7e94674e8ff5
kamyu <kamyu104@users.noreply.github.com>
2021-02-08 02:35:10 +0800

    Create closest-subsequence-sum.cpp

---
+++ C++/closest-subsequence-sum.cpp
@@ -0,0 +1,39 @@
+// Time:  O(n * 2^(n/2))
+// Space: O(n * 2^(n/2))
+
+class Solution {
+public:
+    int minAbsDifference(vector<int>& nums, int goal) {
+        int result = abs(goal);
+        unordered_set<int> sums1{0};
+        for (auto i = 0; i < size(nums) / 2; ++i) {
+            for (const auto& x : vector<int>(cbegin(sums1), cend(sums1))) {
+                if (!sums1.emplace(x + nums[i]).second) {
+                    continue;
+                }
+                result = min(result, abs(goal - x - nums[i]));  // case of right half part is 0
+            }
+        }
+        vector<int> sorted_sums1(cbegin(sums1), cend(sums1));
+        sort(begin(sorted_sums1), end(sorted_sums1));
+        unordered_set<int> sums2{0};
+        for (int i = size(nums) / 2; i < size(nums); ++i) {
+            for (const auto& x : vector<int>(cbegin(sums2), cend(sums2))) {
+                if (!sums2.emplace(x + nums[i]).second) {
+                    continue;
+                }
+                auto cit = lower_bound(cbegin(sorted_sums1), cend(sorted_sums1), goal - x - nums[i]);
+                if (cit != cend(sorted_sums1)) {
+                    result = min(result, abs(goal - x - nums[i] - *cit));
+                }
+                if (cit != cbegin(sorted_sums1)) {
+                    result = min(result, abs(goal - x - nums[i] - *prev(cit)));
+                }
+                if (result == 0) {
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+};
