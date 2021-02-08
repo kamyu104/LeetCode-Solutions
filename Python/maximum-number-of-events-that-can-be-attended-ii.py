598b76d3a5de782ee8d728cd4c20c6f697c9b357
kamyu <kamyu104@users.noreply.github.com>
2021-02-08 09:42:07 +0800

    Update maximum-number-of-events-that-can-be-attended-ii.py

--- Python/maximum-number-of-events-that-can-be-attended-ii.py
+++ Python/maximum-number-of-events-that-can-be-attended-ii.py
@@ -17,5 +17,27 @@ class Solution(object):
         dp = [[0]*(k+1) for _ in xrange(len(events)+1)]
         for i in xrange(1, len(events)+1):
             for j in xrange(1, k+1):
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1], dp[prev[i-1]+1][j-1]+events[i-1][2])
+                dp[i][j] = max(dp[i-1][j], dp[prev[i-1]+1][j-1]+events[i-1][2])
         return dp[-1][-1]
+
+
+# Time:  O(nlogn + n * k)
+# Space: O(n * k)
+import bisect
+
+
+class Solution2(object):
+    def maxValue(self, events, k):
+        """
+        :type events: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        events.sort()
+        sorted_starts = [x[0] for x in events]
+        nxt = [bisect.bisect_right(sorted_starts, event[1])-1 for event in events]
+        dp = [[0]*(k+1) for _ in xrange(len(events)+1)]
+        for i in reversed(xrange(len(events))):
+            for j in xrange(1, k+1):
+                dp[i][j] = max(dp[i+1][j], dp[nxt[i]+1][j-1]+events[i][2])
+        return dp[0][-1]
