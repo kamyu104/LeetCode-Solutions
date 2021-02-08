892567ad3f47c49ad6e4fb72d06c2a22b7536241
kamyu <kamyu104@users.noreply.github.com>
2021-02-08 23:02:23 +0800

    Update maximum-number-of-events-that-can-be-attended-ii.py

--- Python/maximum-number-of-events-that-can-be-attended-ii.py
+++ Python/maximum-number-of-events-that-can-be-attended-ii.py
@@ -13,11 +13,11 @@ class Solution(object):
         """
         events.sort(key=lambda x: x[1])
         sorted_ends = [x[1] for x in events]
-        prv = [bisect.bisect_left(sorted_ends, event[0])-1 for event in events]
         dp = [[0]*(k+1) for _ in xrange(len(events)+1)]
         for i in xrange(1, len(events)+1):
+            prev_i_1 = bisect.bisect_left(sorted_ends, events[i-1][0])-1
             for j in xrange(1, k+1):
-                dp[i][j] = max(dp[i-1][j], dp[prv[i-1]+1][j-1]+events[i-1][2])
+                dp[i][j] = max(dp[i-1][j], dp[prev_i_1+1][j-1]+events[i-1][2])
         return dp[-1][-1]
 
 
@@ -35,9 +35,9 @@ class Solution2(object):
         """
         events.sort()
         sorted_starts = [x[0] for x in events]
-        nxt = [bisect.bisect_right(sorted_starts, event[1])-1 for event in events]
         dp = [[0]*(k+1) for _ in xrange(len(events)+1)]
         for i in reversed(xrange(len(events))):
+            next_i = bisect.bisect_right(sorted_starts, events[i][1])-1
             for j in xrange(1, k+1):
-                dp[i][j] = max(dp[i+1][j], dp[nxt[i]+1][j-1]+events[i][2])
+                dp[i][j] = max(dp[i+1][j], dp[next_i+1][j-1]+events[i][2])
         return dp[0][-1]
