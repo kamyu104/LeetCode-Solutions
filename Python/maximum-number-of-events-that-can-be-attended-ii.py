e5f61702a9cfd73287901265592bdbd80cd50d3f
kamyu <kamyu104@users.noreply.github.com>
2021-02-08 23:03:33 +0800

    Update maximum-number-of-events-that-can-be-attended-ii.py

--- Python/maximum-number-of-events-that-can-be-attended-ii.py
+++ Python/maximum-number-of-events-that-can-be-attended-ii.py
@@ -15,9 +15,9 @@ class Solution(object):
         sorted_ends = [x[1] for x in events]
         dp = [[0]*(k+1) for _ in xrange(len(events)+1)]
         for i in xrange(1, len(events)+1):
-            prev_i_1 = bisect.bisect_left(sorted_ends, events[i-1][0])-1
+            prev_i_m_1 = bisect.bisect_left(sorted_ends, events[i-1][0])-1
             for j in xrange(1, k+1):
-                dp[i][j] = max(dp[i-1][j], dp[prev_i_1+1][j-1]+events[i-1][2])
+                dp[i][j] = max(dp[i-1][j], dp[prev_i_m_1+1][j-1]+events[i-1][2])
         return dp[-1][-1]
 
 
