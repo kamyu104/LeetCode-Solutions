72d19d7536158d4a7c9d2135a360461576510815
kamyu <kamyu104@users.noreply.github.com>
2021-02-15 19:13:08 +0800

    Create minimum-limit-of-balls-in-a-bag.py

---
+++ Python/minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,21 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+class Solution(object):
+    def minimumSize(self, nums, maxOperations):
+        """
+        :type nums: List[int]
+        :type maxOperations: int
+        :rtype: int
+        """
+        def check(nums, maxOperations, x):
+            return sum((num+x-1)//x-1 for num in nums) <= maxOperations
+    
+        left, right = 1, max(nums)
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(nums, maxOperations, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
