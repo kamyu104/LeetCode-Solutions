3d0b9a385cb82daad48088945c21591e693b3a85
kamyu <kamyu104@users.noreply.github.com>
2021-02-15 19:12:30 +0800

    Create minimum-limit-of-balls-in-a-bag.cpp

---
+++ C++/minimum-limit-of-balls-in-a-bag.cpp
@@ -0,0 +1,24 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+class Solution {
+public:
+    int minimumSize(vector<int>& nums, int maxOperations) {
+        const auto& check = [&nums, &maxOperations](int x) {
+            return accumulate(cbegin(nums), cend(nums), 0,
+                              [&x](int total, int a) {
+                                  return total + ((a + x - 1) / x - 1);
+                              }) <= maxOperations;
+        };
+        int left = 1, right = *max_element(cbegin(nums), cend(nums));
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
