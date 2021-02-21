e0032f4868a3334583d848f65034c177633be735
kamyu <kamyu104@users.noreply.github.com>
2021-02-21 22:21:06 +0800

    Create buildings-with-an-ocean-view.cpp

---
+++ C++/buildings-with-an-ocean-view.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        vector<int> result;
+        for (int i = size(heights) - 1; i >= 0; --i) {
+            if (empty(result) || heights[i] > heights[result.back()]) {
+                result.emplace_back(i);
+            }
+        }
+        return {rbegin(result), rend(result)};
+    }
+};
+
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    vector<int> findBuildings(vector<int>& heights) {
+        vector<int> result;
+        for (int i = 0; i < size(heights); ++i) {
+            while (!empty(result) && heights[result.back()] <= heights[i]) {
+                result.pop_back();
+            }
+            result.emplace_back(i);
+        }
+        return result;
+    }
+};
