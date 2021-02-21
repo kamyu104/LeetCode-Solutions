36542d88928e6e87fb3a3506e9d3107181fbfc63
kamyu <kamyu104@users.noreply.github.com>
2021-02-21 23:51:07 +0800

    Create longest-nice-substring.cpp

---
+++ C++/longest-nice-substring.cpp
@@ -0,0 +1,27 @@
+// Time:  O(26 * n) = O(n)
+// Space: O(26 * n) = O(n)
+
+class Solution {
+public:
+    string longestNiceSubstring(string s) {
+        array<array<int, 2>, 26> lookup = {0};
+        for (const auto& c : s) {
+            lookup[tolower(c) - 'a'][isupper(c) != 0] = 1;
+        }
+        string result;
+        for (int i = 0, prev = -1; i <= size(s); ++i) {
+            if (!(i == size(s) || !lookup[tolower(s[i]) - 'a'][0] || !lookup[tolower(s[i]) - 'a'][1])) {
+                continue;
+            }
+            if (prev == -1 && i == size(s)) {
+                return s;
+            }
+            const auto tmp = longestNiceSubstring(s.substr(prev + 1, i - prev - 1));
+            if (size(tmp) > size(result)) {
+                result = move(tmp);
+            }
+            prev = i;
+        }
+        return result;
+    }
+};
