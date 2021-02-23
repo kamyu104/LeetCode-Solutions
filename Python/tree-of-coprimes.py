22dc112b3f3e14b8b62b905a36e5be3be0e4b6f9
kamyu <kamyu104@users.noreply.github.com>
2021-02-24 00:24:58 +0800

    Create tree-of-coprimes.py

---
+++ Python/tree-of-coprimes.py
@@ -0,0 +1,93 @@
+# Time:  O(50 * n) = O(n)
+# Space: O(n)
+
+import collections
+import fractions
+
+
+class Solution(object):
+    def getCoprimes(self, nums, edges):
+        """
+        :type nums: List[int]
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """        
+        def iter_backtracking(nums, adj):
+            result = [-1]*len(nums)
+            path = collections.defaultdict(list)
+            stk = [(1, (-1, 0, 0))]
+            while stk:
+                step, params = stk.pop()
+                if step == 1:
+                    prev, node, depth = params
+                    stk.append((4, (node,)))
+                    stk.append((3, (prev, node, depth)))
+                    stk.append((2, (node,)))
+                elif step == 2:
+                    node = params[0]
+                    max_d = -1
+                    for x in path.iterkeys():
+                        if fractions.gcd(nums[node], x) != 1:
+                            continue
+                        if path[x][-1][1] > max_d:
+                            max_d = path[x][-1][1]
+                            result[node] = path[x][-1][0]
+                elif step == 3:
+                    prev, node, depth = params
+                    path[nums[node]].append((node, depth))
+                    for nei in adj[node]:
+                        if nei == prev:
+                            continue
+                        stk.append((1, (node, nei, depth+1)))
+                elif step == 4:
+                    node = params[0]
+                    path[nums[node]].pop()
+                    if not path[nums[node]]:
+                        path.pop(nums[node])
+            return result
+
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        return iter_backtracking(nums, adj)
+
+
+# Time:  O(50 * n) = O(n)
+# Space: O(n)
+import collections
+import fractions
+
+
+class Solution2(object):
+    def getCoprimes(self, nums, edges):
+        """
+        :type nums: List[int]
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """        
+        def backtracking(nums, adj, prev, node, depth, path):
+            max_d = -1
+            for x in path.iterkeys():
+                if fractions.gcd(nums[node], x) != 1:
+                    continue
+                if path[x][-1][1] > max_d:
+                    max_d = path[x][-1][1]
+                    result[node] = path[x][-1][0]
+            path[nums[node]].append((node, depth))
+            for nei in adj[node]:
+                if nei == prev:
+                    continue
+                backtracking(nums, adj, node, nei, depth+1, path)
+            path[nums[node]].pop()
+            if not path[nums[node]]:
+                path.pop(nums[node])
+
+        result = [-1]*len(nums)
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        path = collections.defaultdict(list)
+        backtracking(nums, adj, -1, 0, 0, path)
+        return result
