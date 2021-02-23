b73b1b6bc94e091a82c0f9d986d24e4f6592e019
kamyu <kamyu104@users.noreply.github.com>
2021-02-24 01:42:29 +0800

    Create merge-strings-alternately.cpp

---
+++ C++/merge-strings-alternately.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string result;
+        for (int i = 0; i < size(word1) || i < size(word2); ++i) {
+            if (i < size(word1)) {
+                result.push_back(word1[i]);
+            }
+            if (i < size(word2)) {
+                result.push_back(word2[i]);
+            }
+        }
+        return result;
+    }
+};
