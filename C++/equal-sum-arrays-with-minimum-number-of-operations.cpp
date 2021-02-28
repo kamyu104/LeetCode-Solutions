857200c0f6b51d55c6e1a5df7ffc685cfd8ce5c2
kamyu <kamyu104@users.noreply.github.com>
2021-02-28 20:40:38 +0800

    Create equal-sum-arrays-with-minimum-number-of-operations.cpp

---
+++ C++/equal-sum-arrays-with-minimum-number-of-operations.cpp
@@ -0,0 +1,36 @@
+// Time:  O(m + n)
+// Space: O(1)
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums1, vector<int>& nums2) {
+        if (6 * size(nums1) < 1 * nums2.size() || 6 * size(nums2) < 1 * size(nums1)) {
+            return -1;
+        }
+        vector<int> count(6);
+        int diff = accumulate(cbegin(nums2), cend(nums2), 0) - accumulate(cbegin(nums1), cend(nums1), 0);
+        if (diff < 0) {
+            swap(nums1, nums2);
+            diff = -diff;
+        }
+        for (const auto& num : nums1) {
+            ++count[6 - num];
+        }
+        for (const auto& num : nums2) {
+            ++count[num - 1];
+        }
+        int result = 0;
+        for (int i = 5; i >= 1; --i) {
+            if (!count[i]) {
+                continue;
+            }
+            int cnt = min(count[i], (diff + i - 1) / i);
+            result += cnt;
+            diff -= i * cnt;
+            if (diff <= 0) {
+                break;
+            }
+        }
+        return result;
+    }
+};
