ebdd01b3b98fe93b0974227e30e3d51efe32ffb7
kamyu <kamyu104@users.noreply.github.com>
2021-03-09 14:16:59 +0800

    Update check-if-number-is-a-sum-of-powers-of-three.cpp

--- C++/check-if-number-is-a-sum-of-powers-of-three.cpp
+++ C++/check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -4,12 +4,7 @@
 class Solution {
 public:
     bool checkPowersOfThree(int n) {
-        while (n) {
-            if (n % 3 == 2) {
-                return false;
-            }
-            n /= 3;
-        }
-        return true;
+        for (; n > 0 && n % 3 != 2; n /= 3);
+        return n == 0;
     }
 };
