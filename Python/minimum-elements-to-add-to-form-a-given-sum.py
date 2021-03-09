f56ff669fe0ef1b9bb5bdba4660a319662d7ed9c
kamyu <kamyu104@users.noreply.github.com>
2021-03-09 17:35:32 +0800

    Create minimum-elements-to-add-to-form-a-given-sum.py

---
+++ Python/minimum-elements-to-add-to-form-a-given-sum.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def minElements(self, nums, limit, goal):
+        """
+        :type nums: List[int]
+        :type limit: int
+        :type goal: int
+        :rtype: int
+        """
+        return (abs(sum(nums)-goal) + (limit-1))//limit
