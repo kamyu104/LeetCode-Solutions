9b9386e757bc2fb326e959c87cea85daea7e5122
kamyu <kamyu104@users.noreply.github.com>
2021-03-09 15:18:38 +0800

    Create sum-of-beauty-of-all-substrings.py

---
+++ Python/sum-of-beauty-of-all-substrings.py
@@ -0,0 +1,17 @@
+# Time:  O(n^2)
+# Space: O(1)
+
+class Solution(object):
+    def beautySum(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0 
+        for i in xrange(len(s)):
+            lookup = [0]*26
+            for j in xrange(i, len(s)):
+                lookup[ord(s[j])-ord('a')] += 1
+                print min(x for x in lookup if x)
+                result += max(lookup) - min(x for x in lookup if x)
+        return result
