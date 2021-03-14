17f1b2270e4a0eaa1e207bbca1062e69ad9ea2ba
kamyu <kamyu104@users.noreply.github.com>
2021-03-15 01:18:58 +0800

    Update maximum-score-of-a-good-subarray.py

--- Python/maximum-score-of-a-good-subarray.py
+++ Python/maximum-score-of-a-good-subarray.py
@@ -7,9 +7,40 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        stk, result, i = [-1], 0, 0
-        for i in xrange(len(heights)+1):
-            while stk[-1] != -1 and (i == len(heights) or heights[stk[-1]] >= heights[i]):
-                result = max(result, heights[stk.pop()]*((i-1)-stk[-1]))
-            stk.append(i) 
+        result = curr = nums[k]
+        left = right = k
+        while left-1 >= 0 or right+1 < len(nums):
+            # choosing larger one to expand is always better than or equal to choosing smaller one
+            if (nums[left-1] if left-1 >= 0 else 0) <= (nums[right+1] if right+1 < len(nums) else 0):
+                right += 1
+            else:
+                left -= 1
+            curr = min(curr, nums[left], nums[right])
+            result = max(result, curr*(right-left+1))
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+class Solution2(object):
+    def maximumScore(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def score(nums, k):
+            left_prefix = [nums[k]]*(k+1)
+            for i in reversed(xrange(k)):
+                left_prefix[i] = min(left_prefix[i+1], nums[i])
+            right_prefix = [nums[k]]*(len(nums)-k)
+            for i in xrange(k+1, len(nums)):
+                right_prefix[i-k] = min(right_prefix[i-k-1], nums[i])
+            result = nums[k]
+            for j in xrange(len(right_prefix)):
+                i = bisect.bisect_left(left_prefix, right_prefix[j])
+                if i >= 0:
+                    result = max(result, right_prefix[j]*(k+j-i+1))
+            return result
+
+        return max(score(nums, k), score(nums[::-1], len(nums)-k-1))
