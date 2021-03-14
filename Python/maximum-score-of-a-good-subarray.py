386dd0e05f2a28df3d060a47b1aedf332e192edc
kamyu <kamyu104@users.noreply.github.com>
2021-03-15 00:45:44 +0800

    Create maximum-score-of-a-good-subarray.py

---
+++ Python/maximum-score-of-a-good-subarray.py
@@ -0,0 +1,15 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def largestRectangleArea(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: int
+        """
+        stk, result, i = [-1], 0, 0
+        for i in xrange(len(heights)+1):
+            while stk[-1] != -1 and (i == len(heights) or heights[stk[-1]] >= heights[i]):
+                result = max(result, heights[stk.pop()]*((i-1)-stk[-1]))
+            stk.append(i) 
+        return result
