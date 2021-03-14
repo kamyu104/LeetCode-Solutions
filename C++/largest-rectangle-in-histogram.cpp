f5b62bd26fa77fd282eff8c5dafbb393e1b067c1
kamyu <kamyu104@users.noreply.github.com>
2021-03-15 02:00:28 +0800

    Update largest-rectangle-in-histogram.cpp

--- C++/largest-rectangle-in-histogram.cpp
+++ C++/largest-rectangle-in-histogram.cpp
@@ -7,8 +7,7 @@ public:
         vector<int> stk = {-1};
         int result = 0;
         for (int i = 0; i <= size(heights); ++i) {
-            while (stk.back() != -1 &&
-                   (i == size(heights) || heights[stk.back()] >= heights[i])) {
+            while (stk.back() != -1 && (i == size(heights) || heights[stk.back()] >= heights[i])) {
                 int last = stk.back(); stk.pop_back();
                 result = max(result, heights[last] * ((i - 1) - stk.back()));
             }
