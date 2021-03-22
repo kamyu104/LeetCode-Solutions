3e3bcfd707beb82414317b3b96920a0208e3ea27
kamyu <kamyu104@users.noreply.github.com>
2021-03-22 12:43:26 +0800

    Create maximum-value-at-a-given-index-in-a-bounded-array.py

---
+++ Python/maximum-value-at-a-given-index-in-a-bounded-array.py
@@ -0,0 +1,27 @@
+# Time:  O(logn)
+# Space: O(1)
+
+class Solution(object):
+    def maxValue(self, n, index, maxSum):
+        """
+        :type n: int
+        :type index: int
+        :type maxSum: int
+        :rtype: int
+        """
+        def check(n, index, maxSum, x):
+            y = max(x-index, 0)
+            total = (x+y)*(x-y+1)//2
+            y = max(x-((n-1)-index), 0)
+            total += (x+y)*(x-y+1)//2
+            return total-x <= maxSum
+
+        maxSum -= n
+        left, right = 0, maxSum
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(n, index, maxSum, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return 1+right
