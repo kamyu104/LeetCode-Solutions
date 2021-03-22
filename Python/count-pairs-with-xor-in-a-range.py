93f535741abeab2102ebbac39c85a4d718f2bf62
kamyu <kamyu104@users.noreply.github.com>
2021-03-22 14:49:07 +0800

    Create count-pairs-with-xor-in-a-range.py

---
+++ Python/count-pairs-with-xor-in-a-range.py
@@ -0,0 +1,72 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# dp solution
+class Solution(object):
+    def countPairs(self, nums, low, high):
+        """
+        :type nums: List[int]
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        def count(nums, x):
+            result = 0
+            dp = collections.Counter(nums)
+            while x:
+                if x&1:
+                    result += sum(dp[(x^1)^k]*dp[k] for k in dp.iterkeys())  # count of xor pair with 00001***** pattern
+                dp = collections.Counter({k>>1: dp[k]+dp[k^1] for k in dp.iterkeys()})
+                x >>= 1
+            return result//2
+    
+        return count(nums, high+1)-count(nums, low)
+
+
+# Time:  O(n)
+# Space: O(n)
+# trie solution
+class Trie(object):
+    def __init__(self):
+        self.__root = {}
+        
+    def insert(self, num):
+        node = self.__root
+        for i in reversed(xrange(32)):
+            curr = (num>>i) & 1
+            if curr not in node:
+                node[curr] = {"_count":0}
+            node = node[curr]
+            node["_count"] += 1
+                
+    def query(self, num, limit):
+        node, result = self.__root, 0
+        for i in reversed(xrange(32)):
+            curr = (num>>i) & 1
+            bit = (limit>>i) & 1
+            if bit:
+                if curr in node:
+                    result += node[curr]["_count"]  # count of xor pair with 00001***** pattern
+            if bit^curr not in node:
+                break
+            node = node[bit^curr]
+        return result
+
+
+class Solution2(object):
+    def countPairs(self, nums, low, high):
+        """
+        :type nums: List[int]
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        result = 0
+        trie = Trie()
+        for x in nums:
+            result += trie.query(x, high+1)-trie.query(x, low)
+            trie.insert(x)
+        return result
