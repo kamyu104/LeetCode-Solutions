20f5f7d46bfcc9a60346955d7fda7fab03310165
kamyu <kamyu104@users.noreply.github.com>
2021-03-22 12:07:12 +0800

    Create number-of-orders-in-the-backlog.py

---
+++ Python/number-of-orders-in-the-backlog.py
@@ -0,0 +1,27 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+class Solution(object):
+    def getNumberOfBacklogOrders(self, orders):
+        """
+        :type orders: List[List[int]]
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+        buy, sell  = [], []  # max_heap, min_heap
+        for p, a, t in orders:
+            if t == 0:
+                heapq.heappush(buy, [-p, a])
+            else:
+                heapq.heappush(sell, [p, a])
+            while sell and buy and sell[0][0] <= -buy[0][0]:
+                k = min(buy[0][1], sell[0][1])
+                tmp = heapq.heappop(buy)
+                tmp[1] -= k
+                if tmp[1]:
+                    heapq.heappush(buy, tmp)
+                tmp = heapq.heappop(sell)
+                tmp[1] -= k
+                if tmp[1]:
+                    heapq.heappush(sell, tmp)
+        return reduce(lambda x, y: (x+y) % MOD, (a for _, a in buy + sell))
