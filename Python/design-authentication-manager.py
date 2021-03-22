293190310aceb1482e1e37263b3a8b6eda248c8c
kamyu <kamyu104@users.noreply.github.com>
2021-03-22 10:18:42 +0800

    Create design-authentication-manager.py

---
+++ Python/design-authentication-manager.py
@@ -0,0 +1,50 @@
+# Time:  ctor:     O(1)
+#        generate: O(1), amortized
+#        renew:    O(1), amortized
+#        count:    O(1), amortized
+
+import collections
+
+
+class AuthenticationManager(object):
+
+    def __init__(self, timeToLive):
+        """
+        :type timeToLive: int
+        """
+        self.__time = timeToLive
+        self.__lookup = collections.OrderedDict()
+
+    def __evict(self, currentTime):
+        while self.__lookup and next(self.__lookup.itervalues()) <= currentTime:
+            self.__lookup.popitem(last=False)
+
+    def generate(self, tokenId, currentTime):
+        """
+        :type tokenId: str
+        :type currentTime: int
+        :rtype: None
+        """
+        self.__evict(currentTime)
+        self.__lookup[tokenId] = currentTime + self.__time
+
+
+    def renew(self, tokenId, currentTime):
+        """
+        :type tokenId: str
+        :type currentTime: int
+        :rtype: None
+        """
+        self.__evict(currentTime)            
+        if tokenId not in self.__lookup:
+            return
+        del self.__lookup[tokenId]
+        self.__lookup[tokenId] = currentTime + self.__time
+
+    def countUnexpiredTokens(self, currentTime):
+        """
+        :type currentTime: int
+        :rtype: int
+        """
+        self.__evict(currentTime)
+        return len(self.__lookup)
