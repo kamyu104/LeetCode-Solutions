c1f1afd3a5d6bfb52c6ab098e028052d36d120c0
kamyu <kamyu104@users.noreply.github.com>
2021-03-29 23:59:05 +0800

    Create evaluate-the-bracket-pairs-of-a-string.cpp

---
+++ C++/evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n + m)
+// Space: O(n + m)
+
+class Solution {
+public:
+    string evaluate(string s, vector<vector<string>>& knowledge) {
+        unordered_map<string, string> lookup;
+        for (const auto& pair : knowledge) {
+            lookup[pair[0]] = pair[1];
+        }
+        string result, curr;
+        bool has_pair = false;;
+        for (int i = 0; i < size(s); ++i) {
+            if (s[i] == '(') {
+                has_pair = true;
+            } else if (s[i] == ')') {
+                has_pair = false;
+                if (lookup.count(curr)) {
+                    for (const auto& c : lookup[curr]) {
+                        result.push_back(c);
+                    }
+                } else {
+                    result.push_back('?');
+                }
+                curr.clear();
+            } else if (has_pair) {
+                curr.push_back(s[i]);
+            } else {
+                result.push_back(s[i]);
+            }
+        }
+        return result;
+    }
+};
