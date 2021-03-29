0cea665d41be1f518ee79bd6257b0e4c7dcb5a20
kamyu <kamyu104@users.noreply.github.com>
2021-03-29 23:59:50 +0800

    Create evaluate-the-bracket-pairs-of-a-string.py

---
+++ Python/evaluate-the-bracket-pairs-of-a-string.py
@@ -0,0 +1,25 @@
+# Time:  O(n + m)
+# Space: O(n + m)
+
+class Solution(object):
+    def evaluate(self, s, knowledge):
+        """
+        :type s: str
+        :type knowledge: List[List[str]]
+        :rtype: str
+        """
+        lookup = {k: v for k, v in knowledge}
+        result, curr = [], []
+        has_pair = False
+        for c in s:
+            if c == '(':
+                has_pair = True
+            elif c == ')':
+                has_pair = False
+                result.append(lookup.get("".join(curr), '?'))
+                curr = []
+            elif is_pair:
+                curr.append(c)
+            else:
+                result.append(c)
+        return "".join(result)
