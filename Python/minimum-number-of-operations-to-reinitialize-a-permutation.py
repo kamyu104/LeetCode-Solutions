808725511d53a047f00377e0466dfeabc966c9e0
kamyu <kamyu104@users.noreply.github.com>
2021-03-29 22:42:52 +0800

    Create minimum-number-of-operations-to-reinitialize-a-permutation.py

---
+++ Python/minimum-number-of-operations-to-reinitialize-a-permutation.py
@@ -0,0 +1,42 @@
+# Time:  O(sqrt(n))
+# Space: O(sqrt(n))
+
+class Solution(object):
+    def reinitializePermutation(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # reference: https://cp-algorithms.com/algebra/discrete-log.html
+        def discrete_log(a, b, m):
+            a %= m
+            b %= m
+            n = int(sqrt(m))+1
+            an = pow(a, n, m)
+            vals = {}
+            curr = b
+            for q in xrange(n+1):
+                vals[curr] = q
+                curr = curr*a % m
+            curr = 1
+            for p in xrange(1, n+1):
+                curr = curr*an % m
+                if curr in vals:
+                    return n*p-vals[curr]
+            return -1
+
+        return 1+discrete_log(2, n//2, n-1)  # find min x s.t. 2^x mod (n-1) = n/2, result is x + 1
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def reinitializePermutation(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        result, i = 0, 1
+        while not result or i > 1:
+            i = (i*2)%(n-1)
+            result += 1
+        return result
