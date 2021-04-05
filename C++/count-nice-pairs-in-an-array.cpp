dd01df5b8c217a946ac0334a3e5327369263b0ac
kamyu <kamyu104@users.noreply.github.com>
2021-04-05 20:43:11 +0800

    Create count-nice-pairs-in-an-array.cpp

---
+++ C++/count-nice-pairs-in-an-array.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int countNicePairs(vector<int>& nums) {
+        static const int MOD = 1e9 + 7;
+
+        unordered_map<int, int> lookup;
+        int result = 0;
+        for (const auto& i : nums) {
+            result = (result + lookup[i - rev(i)]++) % MOD;
+        }
+        return result;
+    }
+
+private:
+    int rev(int i) {
+        int result = 0;
+        for (; i; i /= 10) {
+            result = result * 10 + i % 10;
+        }
+        return result;
+    }
+};
