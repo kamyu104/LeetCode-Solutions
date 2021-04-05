1d454bb26092086e5bb06715327218cdd70d61b3
kamyu <kamyu104@users.noreply.github.com>
2021-04-05 20:43:43 +0800

    Create count-nice-pairs-in-an-array.py

---
+++ Python/count-nice-pairs-in-an-array.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(1)
+
+import collections
+
+
+class Solution(object):
+    def countNicePairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        MOD = 10**9 + 7
+
+        def rev(x):
+            result = 0
+            while x:
+                x, r = divmod(x, 10)
+                result = result*10+r
+            return result
+        
+        result = 0
+        lookup = collections.defaultdict(int)
+        for num in nums:
+            result = (result + lookup[num-rev(num)])%MOD
+            lookup[num-rev(num)] += 1
+        return result
