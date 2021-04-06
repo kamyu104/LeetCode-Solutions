272264cf634d661eb4b1852f53fff49051833790
kamyu <kamyu104@users.noreply.github.com>
2021-04-06 16:06:30 +0800

    Update sentence-similarity-iii.cpp

--- C++/sentence-similarity-iii.cpp
+++ C++/sentence-similarity-iii.cpp
@@ -8,24 +8,16 @@ public:
             swap(sentence1, sentence2);
         }
         int count = 0;
-        for (int i = 0; i <= size(sentence1); ++i) {
-            char c1 = i != size(sentence1) ? sentence1[i] : ' ';
-            char c2 = i != size(sentence2) ? sentence2[i] : ' ';
-            if (c1 != c2) {
-                break;
-            }
-            if (c1 == ' ') {
-                ++count;
-            }
-        }
-        for (int i = 0; i <= size(sentence1); ++i) {
-            char c1 = i != size(sentence1) ? sentence1[size(sentence1) - 1 - i] : ' ';
-            char c2 = i != size(sentence2) ? sentence2[size(sentence2) - 1 - i] : ' ';
-            if (c1 != c2) {
-                break;
-            }
-            if (c1 == ' ') {
-                ++count;
+        for (int step = 0; step < 2; ++step) {
+            for (int i = 0; i <= size(sentence1); ++i) {
+                char c1 = i != size(sentence1) ? sentence1[step == 0 ? i : size(sentence1) - 1 - i] : ' ';
+                char c2 = i != size(sentence2) ? sentence2[step == 0 ? i : size(sentence2) - 1 - i] : ' ';
+                if (c1 != c2) {
+                    break;
+                }
+                if (c1 == ' ') {
+                    ++count;
+                }
             }
         }
         return count >= count_if(cbegin(sentence1), cend(sentence1),
