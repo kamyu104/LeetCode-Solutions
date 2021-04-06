d4ccc6b8e29a2c4d82e1842218bbeb303cb162b7
kamyu <kamyu104@users.noreply.github.com>
2021-04-06 15:27:38 +0800

    Create number-of-different-subsequences-gcds.py

---
+++ Python/number-of-different-subsequences-gcds.py
@@ -0,0 +1,24 @@
+# Time:  O(m * (1 + 1/2 + 1/3 + ... + 1/m)) = O(mlogm), m is max of nums
+# Space: O(n)
+
+import fractions
+
+
+class Solution(object):
+    def countDifferentSubsequenceGCDs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_num, nums_set = max(nums), set(nums)
+        result = 0
+        for i in xrange(1, max_num+1):
+            d = 0
+            for x in xrange(i, max_num+1, i):
+                if x not in nums_set:
+                    continue
+                d = fractions.gcd(d, x)
+                if d == i:
+                    result += 1
+                    break
+        return result
