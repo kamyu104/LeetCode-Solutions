62d21ccf17501d45f4e1219379b9d0d589e467bb
kamyu <kamyu104@users.noreply.github.com>
2021-04-18 19:58:03 +0800

    Create maximum-transaction-each-day.sql

---
+++ MySQL/maximum-transaction-each-day.sql
@@ -0,0 +1,20 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT transaction_id
+FROM (SELECT transaction_id, 
+             DENSE_RANK() OVER(PARTITION BY DATE(day) ORDER BY amount DESC) AS ranks
+      FROM Transactions) tmp
+WHERE ranks = 1
+ORDER BY transaction_id;
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+SELECT transaction_id
+FROM (SELECT DATE(day) as date_yyyymmdd, MAX(amount) as max_amount
+      FROM Transactions
+      GROUP BY DATE(day)) tmp
+      INNER JOIN Transactions t
+      ON date_yyyymmdd = DATE(t.day) AND max_amount = amount
+ORDER BY transaction_id;
