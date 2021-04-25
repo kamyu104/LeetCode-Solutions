5bf40ca32c05fda975b1815ce03ed356542c9d62
kamyu <kamyu104@users.noreply.github.com>
2021-04-25 18:00:19 +0800

    Create remove-duplicates-from-an-unsorted-linked-list.py

---
+++ Python/remove-duplicates-from-an-unsorted-linked-list.py
@@ -0,0 +1,32 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def deleteDuplicatesUnsorted(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        count = collections.defaultdict(int)
+        curr = head
+        while curr:
+            count[curr.val] += 1
+            curr = curr.next
+        dummy = ListNode(0, head)
+        curr = dummy
+        while curr.next:
+            if count[curr.next.val] == 1:
+                curr = curr.next
+            else:
+                curr.next = curr.next.next
+        return dummy.next
