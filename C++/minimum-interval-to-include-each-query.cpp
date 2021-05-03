613243a8065a32d03e4e2a4315515608f31d2356
kamyu <kamyu104@users.noreply.github.com>
2021-05-03 12:37:50 +0800

    Create minimum-interval-to-include-each-query.cpp

---
+++ C++/minimum-interval-to-include-each-query.cpp
@@ -0,0 +1,31 @@
+// Time:  O(nlogn + klogk + klogn)
+// space: O(n + k)
+
+class Solution {
+public:
+    vector<int> minInterval(vector<vector<int>>& intervals, vector<int>& queries) {
+        sort(begin(intervals), end(intervals));
+        vector<pair<int, int>> sorted_queries;
+        for (int i = 0; i < size(queries); ++i) {
+            sorted_queries.emplace_back(queries[i], i);
+        }
+        sort(begin(sorted_queries), end(sorted_queries));
+        using P = pair<int, int>;
+        priority_queue<P, vector<P>, greater<P>> min_heap;
+        int i = 0;
+        vector<int> result(size(queries), -1);
+        for (const auto& [q, idx] : sorted_queries) {
+            while (i != size(intervals) && intervals[i][0] <= q) {
+                if (intervals[i][1] >= q) {
+                    min_heap.emplace(intervals[i][1] - intervals[i][0] + 1, intervals[i][1]);
+                }
+                ++i;
+            }
+            while (!empty(min_heap) && min_heap.top().second < q) {
+                min_heap.pop();
+            }
+            result[idx] = (!empty(min_heap)) ? min_heap.top().first : -1;
+        }
+        return result;
+    }
+};
