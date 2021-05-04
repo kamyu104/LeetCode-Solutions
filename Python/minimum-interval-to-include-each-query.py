c94335bbe909ff9c0c469464e2b9f3b7d3198383
kamyu <kamyu104@users.noreply.github.com>
2021-05-04 23:11:11 +0800

    Update minimum-interval-to-include-each-query.py

--- Python/minimum-interval-to-include-each-query.py
+++ Python/minimum-interval-to-include-each-query.py
@@ -12,17 +12,16 @@ class Solution(object):
         :rtype: List[int]
         """
         intervals.sort()
-        sorted_queries = [(q, i) for i, q in enumerate(queries)]
-        sorted_queries.sort()
+        queries = [(q, i) for i, q in enumerate(queries)]
+        queries.sort()
         min_heap = []
         i = 0
         result =[-1]*len(queries)
-        for q, idx in sorted_queries:
+        for q, idx in queries:
             while i != len(intervals) and intervals[i][0] <= q:
-                if intervals[i][1] >= q:
-                    heapq.heappush(min_heap, [intervals[i][1]-intervals[i][0]+1, intervals[i][1]])
+                heapq.heappush(min_heap, [intervals[i][1]-intervals[i][0]+1, i])
                 i += 1
-            while min_heap and min_heap[0][1] < q:
+            while min_heap and intervals[min_heap[0][1]][1] < q:
                 heapq.heappop(min_heap)
             result[idx] = min_heap[0][0] if min_heap else -1
         return result
