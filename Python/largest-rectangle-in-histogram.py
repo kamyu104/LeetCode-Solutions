a8f0fe9737255c8612fd1b34e14dfae5217e8efb
kamyu <kamyu104@users.noreply.github.com>
2021-05-15 10:19:11 +0800

    Update largest-rectangle-in-histogram.py

--- Python/largest-rectangle-in-histogram.py
+++ Python/largest-rectangle-in-histogram.py
@@ -7,7 +7,7 @@ class Solution(object):
         :type heights: List[int]
         :rtype: int
         """
-        stk, result, i = [-1], 0, 0
+        stk, result = [-1], 0
         for i in xrange(len(heights)+1):
             while stk[-1] != -1 and (i == len(heights) or heights[stk[-1]] >= heights[i]):
                 result = max(result, heights[stk.pop()]*((i-1)-stk[-1]))
