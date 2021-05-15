57923e8c6350d628bb44244aefc09a3049684641
kamyu <kamyu104@users.noreply.github.com>
2021-05-15 15:47:15 +0800

    Update longest-word-with-all-prefixes.cpp

--- C++/longest-word-with-all-prefixes.cpp
+++ C++/longest-word-with-all-prefixes.cpp
@@ -1,47 +1,98 @@
 // Time:  O(n)
 // Space: O(t), t is the number of nodes in trie
 
-class TrieNode { 
-public:
-    bool isString = false;
-    unordered_map<char, unique_ptr<TrieNode>> leaves;
-        
-    void Insert(const string& s) {
-        auto p = this;
-        for (const auto& c : s) {
-            if (!p->leaves.count(c)) {
-                p->leaves[c] = make_unique<TrieNode>();
-            }
-            p = p->leaves[c].get();
-        }
-        p->isString = true;
-    }
-};
-
 class Solution {
+private:
+    class TrieNode { 
+    public:
+        int idx = -1;
+        unordered_map<char, unique_ptr<TrieNode>> leaves;
+
+        void Insert(int i, const string& s) {
+            auto p = this;
+            for (const auto& c : s) {
+                if (!p->leaves.count(c)) {
+                    p->leaves[c] = make_unique<TrieNode>();
+                }
+                p = p->leaves[c].get();
+            }
+            p->idx = i;
+
+        }
+    };
 public:
     string longestWord(vector<string>& words) {
         auto trie = make_unique<TrieNode>();
-        for (const auto& word : words) {
-            trie->Insert(word);
+        for (int i = 0; i < size(words); ++i) {
+            trie->Insert(i, words[i]);
         }
-        string result, curr;
-        dfs(trie.get(), &curr, &result);
-        return result;
+        return iter_dfs(words, trie.get());
     }
 
 private:
-    void dfs(TrieNode *node, string *curr, string *result) {
-        if (size(*curr) > size(*result)) {
-            *result = *curr;
+    string iter_dfs(const vector<string>& words, TrieNode *node) {
+        int result = -1;
+        vector<TrieNode *> stk = {node};
+        while (!empty(stk)) {
+            const auto node = stk.back(); stk.pop_back();
+            if (node->idx != -1 && (result == -1 || size(words[node->idx]) > size(words[result]))) {
+                result = node->idx;
+            }
+            for (char c = 'z'; c >= 'a'; --c) {
+                if (!node->leaves.count(c) || node->leaves[c]->idx == -1) {
+                    continue;
+                }
+                stk.emplace_back(node->leaves[c].get());
+            }
+        }
+        return result != -1 ? words[result] : "";
+    }
+};
+
+
+// Time:  O(n)
+// Space: O(t), t is the number of nodes in trie
+class Solution2 {
+private:
+    class TrieNode { 
+    public:
+        int idx = -1;
+        unordered_map<char, unique_ptr<TrieNode>> leaves;
+
+        void Insert(int i, const string& s) {
+            auto p = this;
+            for (const auto& c : s) {
+                if (!p->leaves.count(c)) {
+                    p->leaves[c] = make_unique<TrieNode>();
+                }
+                p = p->leaves[c].get();
+            }
+            p->idx = i;
+
+        }
+    };
+
+public:
+    string longestWord(vector<string>& words) {
+        auto trie = make_unique<TrieNode>();
+        for (int i = 0; i < size(words); ++i) {
+            trie->Insert(i, words[i]);
+        }
+        int result = -1;
+        dfs(words, trie.get(), &result);
+        return result != -1 ? words[result] : "";
+    }
+
+private:
+    void dfs(const vector<string>& words, TrieNode *node, int *result) {
+        if (node->idx != -1 && (*result == -1 || size(words[node->idx]) > size(words[*result]))) {
+            *result = node->idx;
         }
         for (char c = 'a'; c <= 'z'; ++c) {
-            if (!node->leaves.count(c) || !node->leaves[c]->isString) {
+            if (!node->leaves.count(c) || node->leaves[c]->idx == -1) {
                 continue;
             }
-            curr->push_back(c);
-            dfs(node->leaves[c].get(), curr, result);
-            curr->pop_back();
+            dfs(words, node->leaves[c].get(), result);
         }
     }
 };
