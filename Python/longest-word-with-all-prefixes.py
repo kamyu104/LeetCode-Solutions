11546916acdd44da0b76d748ee9a4b97ce081f3a
kamyu <kamyu104@users.noreply.github.com>
2021-05-15 16:03:16 +0800

    Update longest-word-with-all-prefixes.py

--- Python/longest-word-with-all-prefixes.py
+++ Python/longest-word-with-all-prefixes.py
@@ -11,20 +11,53 @@ class Solution(object):
         :type words: List[str]
         :rtype: str
         """
-        def dfs(trie, curr, result):
-            if len(curr) > len(result):
-                result[:] = curr
-            for c in string.ascii_lowercase:
-                if c not in trie or "_end" not in trie[c]:
-                    continue
-                curr.append(c)
-                dfs(trie[c], curr, result)
-                curr.pop()
+        def iter_dfs(node):
+            result = -1
+            stk = [node]
+            while stk:
+                node = stk.pop()
+                if result == -1 or len(words[node["_end"]]) > len(words[result]):
+                    result = node["_end"]
+                for c in reversed(string.ascii_lowercase):
+                    if c not in node or "_end" not in node[c]:
+                        continue
+                    stk.append(node[c])
+            return result       
     
         _trie = lambda: collections.defaultdict(_trie)
         trie = _trie()
-        for word in words:
-            reduce(dict.__getitem__, word, trie).setdefault("_end")
-        result = []
-        dfs(trie, [], result)
-        return "".join(result)
+        trie["_end"] = -1
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie)["_end"] = i
+        result = iter_dfs(trie)
+        return words[result] if result != -1 else "" 
+
+
+# Time:  O(n)
+# Space: O(t), t is the number of nodes in trie
+import collections
+import string
+
+
+class Solution2(object):
+    def longestWord(self, words):
+        """
+        :type words: List[str]
+        :rtype: str
+        """
+        def dfs(node, result):
+            if result[0] == -1 or len(words[node["_end"]]) > len(words[result[0]]):
+                result[0] = node["_end"]
+            for c in string.ascii_lowercase:
+                if c not in node or "_end" not in node[c]:
+                    continue
+                dfs(node[c], result)
+    
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        trie["_end"] = -1
+        for i, word in enumerate(words):
+            reduce(dict.__getitem__, word, trie)["_end"] = i
+        result = [-1]
+        dfs(trie, result)
+        return words[result[0]] if result[0] != -1 else ""
