a5fc91fb71ef9356a0d7f87ef9aec79a93265d85
kamyu <kamyu104@users.noreply.github.com>
2021-05-17 17:32:03 +0800

    Create sum-of-floored-pairs.cpp

---
+++ C++/sum-of-floored-pairs.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n/1+n/2+...+n/n) = O(nlogn), n is the max of nums
+// Space: O(n)
+
+class Solution {
+public:
+    int sumOfFlooredPairs(vector<int>& nums) {
+        static const int MOD = 1e9 + 7;
+        vector<int> prefix(*max_element(cbegin(nums), cend(nums)) + 1);
+        unordered_map<int, int> count;
+        for (const auto& num : nums) {
+            ++count[num];
+        }
+        for (const auto& [num, cnt] : count) {
+            for (int j = num; j < size(prefix); j += num) {
+                prefix[j] += cnt;
+            }
+        }
+        for (int i = 0; i + 1 < size(prefix); ++i) {
+            prefix[i + 1] += prefix[i];
+        }
+        return accumulate(cbegin(nums), cend(nums), 0,
+                          [&prefix](int total, int x) {
+                              return (total + prefix[x]) % MOD;
+                          });
+    }
+};
