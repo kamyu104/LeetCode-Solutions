46042008e4dad4a190001fa121853bb18939a196
kamyu <kamyu104@users.noreply.github.com>
2021-05-17 16:38:43 +0800

    Create rotating-the-box.cpp

---
+++ C++/rotating-the-box.cpp
@@ -0,0 +1,19 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        vector<vector<char>> result(size(box[0]), vector<char>(size(box), '.'));
+        for (int i = 0; i < size(box); ++i) {
+            for (int j = size(box[0]) - 1, k = size(box[0]) - 1; j >= 0; --j) {
+                if (box[i][j] == '.') {
+                    continue;
+                }
+                k = (box[i][j] == '*') ? j : k;
+                result[k--][size(box) -1 - i] = box[i][j];
+            }
+        }
+        return result;
+    }
+};
