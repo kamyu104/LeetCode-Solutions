e5c8cd7f4e532179849157c36f4f11e4c3938c9d
kamyu <kamyu104@users.noreply.github.com>
2021-05-17 18:30:25 +0800

    Create finding-pairs-with-a-certain-sum.py

---
+++ Python/finding-pairs-with-a-certain-sum.py
@@ -0,0 +1,36 @@
+# Time:  ctor: O(n1 + n2)
+#        add:  O(1)
+#        count: O(n1)
+# Space: O(n1 + n2)
+
+import collections
+
+
+class FindSumPairs(object):
+
+    def __init__(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        """
+        self.__nums2 = nums2
+        self.__count1 = collections.Counter(nums1)
+        self.__count2 = collections.Counter(nums2)
+
+    def add(self, index, val):
+        """
+        :type index: int
+        :type val: int
+        :rtype: None
+        """
+        self.__count2[self.__nums2[index]] -= 1
+        self.__nums2[index] += val
+        self.__count2[self.__nums2[index]] += 1
+
+
+    def count(self, tot):
+        """
+        :type tot: int
+        :rtype: int
+        """
+        return sum(cnt * self.__count2[tot-x] for x, cnt in self.__count1.iteritems())
