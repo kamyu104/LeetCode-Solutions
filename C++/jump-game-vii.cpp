4c4b539b55b6ef68ffd294ca3627064d5ee97aac
kamyu <kamyu104@users.noreply.github.com>
2021-05-23 18:42:36 +0800

    Update jump-game-vii.cpp

--- C++/jump-game-vii.cpp
+++ C++/jump-game-vii.cpp
@@ -19,3 +19,26 @@ public:
         return dp.back();
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+
+// bfs solution
+class Solution2 {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        queue<int> q({0});
+        int reachable = 0, i = -1;
+        while (!empty(q)) {
+            i = q.front(); q.pop();
+            for (int j = max(i + minJump, reachable + 1); j <= min(i + maxJump, int(size(s)) - 1); ++j) {
+                if (s[j] != '0') {
+                    continue;
+                }
+                q.emplace(j);
+            }
+            reachable = i + maxJump;
+        }
+        return i == size(s) - 1;
+    }
+};
