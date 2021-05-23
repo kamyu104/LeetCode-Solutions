42dbd7f523cf2aa813adf8d0c54bdef317d17056
kamyu <kamyu104@users.noreply.github.com>
2021-05-23 15:51:44 +0800

    Create product-of-two-run-length-encoded-arrays.cpp

---
+++ C++/product-of-two-run-length-encoded-arrays.cpp
@@ -0,0 +1,27 @@
+// Time:  O(m + n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> findRLEArray(vector<vector<int>>& encoded1, vector<vector<int>>& encoded2) {
+        vector<vector<int>> result;
+        for (int i = 0, j = 0, remain1 = 0, remain2 = 0;
+             (remain1 || i < size(encoded1)) && (remain2 || j < size(encoded2));) {
+            if (!remain1) {
+                remain1 = encoded1[i++][1];
+            }
+            if (!remain2) {
+                remain2 = encoded2[j++][1];
+            }
+            int cnt = min(remain1, remain2);
+            remain1 -= cnt;
+            remain2 -= cnt;
+            if (!empty(result) && result.back()[0] == encoded1[i - 1][0] * encoded2[j - 1][0]) {
+                result.back()[1] += cnt;
+            } else {
+                result.push_back({encoded1[i - 1][0] * encoded2[j - 1][0], cnt});
+            }
+        }
+        return result;
+    }
+};
