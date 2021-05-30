7ce4824877523220f640c1cae5d17d8b54f848d8
kamyu <kamyu104@users.noreply.github.com>
2021-05-30 16:18:52 +0800

    Create minimize-maximum-pair-sum-in-array.cpp

---
+++ C++/minimize-maximum-pair-sum-in-array.cpp
@@ -0,0 +1,14 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int result = 0;
+        for (int i = 0; i < size(nums) / 2; ++i) {
+            result = max(result, nums[i] + nums[size(nums) - 1 - i]);
+        }
+        return result;
+    }
+};
