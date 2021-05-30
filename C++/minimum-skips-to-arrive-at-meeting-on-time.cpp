7ffd834850bb92972096ff892a373448b1518922
kamyu <kamyu104@users.noreply.github.com>
2021-05-30 19:35:41 +0800

    Update minimum-skips-to-arrive-at-meeting-on-time.cpp

--- C++/minimum-skips-to-arrive-at-meeting-on-time.cpp
+++ C++/minimum-skips-to-arrive-at-meeting-on-time.cpp
@@ -7,7 +7,7 @@ public:
         vector<int> dp((size(dist) - 1) + 1);  // dp[i]: (min time by i skips) * speed
         for (int i = 0; i < size(dist); ++i) {
             for (int j = size(dp) - 1; j >= 0; --j) {
-                dp[j] = i < size(dist) - 1 ? ceil(dp[j] + dist[i], speed) * speed : dp[j] + dist[i];
+                dp[j] = (i < size(dist) - 1) ? ceil(dp[j] + dist[i], speed) * speed : dp[j] + dist[i];
                 if (j - 1 >= 0) {
                     dp[j] = min(dp[j], dp[j - 1] + dist[i]);
                 }
