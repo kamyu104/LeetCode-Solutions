1fe3cec26faf31a1676d9d4f8aed2ab1c904c7f3
kamyu <kamyu104@users.noreply.github.com>
2021-05-30 16:05:12 +0800

    Create get-biggest-three-rhombus-sums-in-a-grid.cpp

---
+++ C++/get-biggest-three-rhombus-sums-in-a-grid.cpp
@@ -0,0 +1,45 @@
+// Time:  O(m * n * min(m, n))
+// Space: O(m * n)
+
+class Solution {
+public:
+    vector<int> getBiggestThree(vector<vector<int>>& grid) {
+        static const int K = 3;
+        vector<vector<int>> left{grid}, right{grid};
+        for (int i = 1; i < size(grid); ++i) {
+            for (int j = 0; j < size(grid[0]) - 1; ++j) {
+                left[i][j] += left[i - 1][j + 1];
+            }
+        }
+        for (int i = 1; i < size(grid); ++i) {
+            for (int j = 1; j < size(grid[0]); ++j) {
+                right[i][j] += right[i - 1][j - 1];
+            }
+        }
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        unordered_set<int> lookup;
+        for (int k = 0; k <= (min(size(grid), size(grid[0])) + 1) / 2; ++k) {
+            for (int i = k; i < size(grid) - k; ++i) {
+                for (int j = k; j < size(grid[0]) - k; ++j) {
+                    int total = k ? ((left[i][j - k] - left[i - k][j]) + (right[i][j + k] - right[i - k][j]) + grid[i - k][j]) +  
+                                    ((left[i + k][j] - left[i][j + k]) + (right[i + k][j] - right[i][j - k]) - grid[i + k][j])
+                                  : grid[i][j];
+                    if (lookup.count(total)) {
+                        continue;
+                    }
+                    lookup.emplace(total);
+                    min_heap.emplace(total);
+                    if (size(min_heap) == K + 1) {
+                        lookup.erase(min_heap.top()); min_heap.pop();
+                    }
+                }
+            }
+        }
+        vector<int> result;
+        while (!min_heap.empty()) {
+            result.emplace_back(min_heap.top()); min_heap.pop();
+        }
+        reverse(begin(result), end(result));
+        return result;
+    }
+};
