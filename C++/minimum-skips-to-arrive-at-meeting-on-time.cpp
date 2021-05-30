cf91e8ad92da2d511d2b23572591feb70be47df1
kamyu <kamyu104@users.noreply.github.com>
2021-05-30 18:35:51 +0800

    Create minimum-skips-to-arrive-at-meeting-on-time.cpp

---
+++ C++/minimum-skips-to-arrive-at-meeting-on-time.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+class Solution {
+public:
+    int minSkips(vector<int>& dist, int speed, int hoursBefore) {
+        vector<int> dp(size(dist) + 1);  // dp[i]: (min time by i skips) * speed
+        for (int i = 0; i < size(dist); ++i) {
+            for (int j = size(dist); j >= 0; --j) {
+                dp[j] = i < size(dist) - 1 ? ceil(dp[j] + dist[i], speed) * speed : dp[j] + dist[i];
+                if (j - 1 >= 0) {
+                    dp[j] = min(dp[j], dp[j - 1] + dist[i]);
+                }
+            }
+        }
+        const int64_t target = int64_t(hoursBefore) * speed;
+        for (int i = 0; i < size(dist); ++i) {
+            if (dp[i] <= target) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+private:
+    int ceil(int a, int b) {
+        return (a + b - 1) / b;
+    }
+};
