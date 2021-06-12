802179fcd5624edd330dfcb22a7911d13093340c
kamyu <kamyu104@users.noreply.github.com>
2021-06-12 12:46:24 +0800

    Update minimum-number-of-flips-to-make-the-binary-string-alternating.py

--- Python/minimum-number-of-flips-to-make-the-binary-string-alternating.py
+++ Python/minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -9,7 +9,7 @@ class Solution(object):
         """
         result = float("inf")
         cnt1 = cnt2 = 0
-        for i in xrange(2*len(s)-1):
+        for i in xrange(2*len(s)-1 if len(s)%2 else len(s)):
             if i >= len(s):
                 cnt1 -= int(s[i%len(s)])^((i-len(s))%2)^0
                 cnt2 -= int(s[i%len(s)])^((i-len(s))%2)^1
