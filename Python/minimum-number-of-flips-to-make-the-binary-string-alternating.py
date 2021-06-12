a2b8fac8a00ac5c1661264c082a3d9cce1c4dca7
kamyu <kamyu104@users.noreply.github.com>
2021-06-12 10:33:30 +0800

    Create minimum-number-of-flips-to-make-the-binary-string-alternating.py

---
+++ Python/minimum-number-of-flips-to-make-the-binary-string-alternating.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minFlips(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = float("inf")
+        cnt1 = cnt2 = 0
+        for i in xrange(2*len(s)-1):
+            if i >= len(s):
+                cnt1 -= int(s[i%len(s)])^((i-len(s))%2)^0
+                cnt2 -= int(s[i%len(s)])^((i-len(s))%2)^1
+            cnt1 += int(s[i%len(s)])^(i%2)^0
+            cnt2 += int(s[i%len(s)])^(i%2)^1
+            if i >= len(s)-1:
+                result = min(result, cnt1, cnt2)
+        return result
