4feb745e0d7071fa4da00916b085100913b4402d
kamyu <kamyu104@users.noreply.github.com>
2021-06-12 12:47:59 +0800

    Update minimum-number-of-flips-to-make-the-binary-string-alternating.cpp

--- C++/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ C++/minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -6,7 +6,8 @@ public:
     int minFlips(string s) {
         int result = numeric_limits<int>::max();
         int cnt1 = 0, cnt2 = 0;
-        for (int i = 0; i + 1 < 2 * size(s); ++i) {
+        int l = (size(s) % 2) ? 2 * size(s) - 1 : size(s);
+        for (int i = 0; i < l; ++i) {
             if (i >= size(s)) {
                 cnt1 -= (s[i % size(s)] - '0') ^ ((i - size(s)) % 2) ^ 0;
                 cnt2 -= (s[i % size(s)] - '0') ^ ((i - size(s)) % 2) ^ 1;
