18e5480158f025c2109bff01281b5e7e1b24dd44
kamyu <kamyu104@users.noreply.github.com>
2021-06-14 20:23:50 +0800

    Create the-earliest-and-latest-rounds-where-players-compete.py

---
+++ Python/the-earliest-and-latest-rounds-where-players-compete.py
@@ -0,0 +1,34 @@
+# Time:  O(n^2) states * O(n^2) per states = O(n^4)
+# Space: O(n^2 + (n/2)^2 + (n/4)^2 + ... ) = O(n^2)
+
+class Solution(object):
+    def earliestAndLatest(self, n, firstPlayer, secondPlayer):
+        """
+        :type n: int
+        :type firstPlayer: int
+        :type secondPlayer: int
+        :rtype: List[int]
+        """
+        def memoization(t, l, r, lookup):
+            # t: total number of players,
+            # l: number of players left to first player,
+            # r: number of players right to second player
+            if (t, l, r) not in lookup:
+                if l == r:
+                    return (1, 1)
+                if l > r:
+                    l, r, = r, l
+                result = [float("inf"), 0]
+                for i in xrange(l+1):
+                    l_lose_cnt, nt = l-i, (t+1)//2
+                    min_j = max(l_lose_cnt, r-((t-nt)-l_lose_cnt))  # j >= l_lose_cnt and j >= r-((t-nt)-l_lose_cnt)
+                    max_j = min((r-i)-1, ((nt-1)-i)-1)  # j+1 <= r-i and j+1 <= (nt-1)-i
+                    for j in xrange(min_j, max_j+1):
+                        assert(l_lose_cnt <= j <= (r-i)-1 and
+                               r-((t-nt)-l_lose_cnt) <= j <= ((nt-1)-i)-1)
+                        tmp = memoization(nt, i, j, lookup)
+                        result = min(result[0], tmp[0]), max(result[1], tmp[1])
+                lookup[t, l, r] = (result[0]+1, result[1]+1)
+            return lookup[t, l, r]
+        
+        return memoization(n, firstPlayer-1, n-secondPlayer, {})
