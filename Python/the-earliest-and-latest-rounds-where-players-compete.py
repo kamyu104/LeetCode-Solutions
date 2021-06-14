36188026b3f617def4f9bd6ed08dfa122081753e
kamyu <kamyu104@users.noreply.github.com>
2021-06-14 20:37:18 +0800

    Update the-earliest-and-latest-rounds-where-players-compete.py

--- Python/the-earliest-and-latest-rounds-where-players-compete.py
+++ Python/the-earliest-and-latest-rounds-where-players-compete.py
@@ -22,10 +22,10 @@ class Solution(object):
                 for i in xrange(l+1):
                     l_lose_cnt, nt = l-i, (t+1)//2
                     min_j = max(l_lose_cnt, r-((t-nt)-l_lose_cnt))  # j >= l_lose_cnt and j >= r-((t-nt)-l_lose_cnt)
-                    max_j = min((r-i)-1, ((nt-1)-i)-1)  # j+1 <= r-i and j+1 <= (nt-1)-i
+                    max_j = min(r-(i+1), (nt-(i+1))-1)  # j <= r-(i+1) and j <= (nt-(i+1))-1
                     for j in xrange(min_j, max_j+1):
-                        assert(l_lose_cnt <= j <= (r-i)-1 and
-                               r-((t-nt)-l_lose_cnt) <= j <= ((nt-1)-i)-1)
+                        assert(l_lose_cnt <= j <= r-(i+1) and
+                               r-((t-nt)-l_lose_cnt) <= j <= (nt-(i+1))-1)
                         tmp = memoization(nt, i, j, lookup)
                         result = min(result[0], tmp[0]), max(result[1], tmp[1])
                 lookup[t, l, r] = (result[0]+1, result[1]+1)
