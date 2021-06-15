d313f94a1acf4457b6be251b53f8b3dbbd9699d0
kamyu <kamyu104@users.noreply.github.com>
2021-06-15 19:32:15 +0800

    Update confusing-number-ii.py

--- Python/confusing-number-ii.py
+++ Python/confusing-number-ii.py
@@ -1,79 +1,63 @@
-# Time:  O(logn)
-# Space: O(logn)
-
 class Solution(object):
-    def confusingNumberII(self, N):
+    def confusingNumberII(self, n):
         """
-        :type N: int
+        :type n: int
         :rtype: int
-        """        
+        """
         lookup = {"0":"0", "1":"1", "6":"9", "8":"8", "9":"6"}
         centers = {"0":"0", "1":"1", "8":"8"}
 
-        def totalCount(N):   
-            s = str(N)
+        def totalCount(n):  # count all numbers in the pattern of [01689]{1,len(n)} in the range of [1, n]
+            s = str(n)
             total = 0 
             p = len(lookup)**(len(s)-1)
-            for i in xrange(len(s)):
-                if i+1 == len(s):
-                    for c in lookup.iterkeys():
-                        total += int(c <= s[i])
-                    continue
-                smaller = 0
-                for c in lookup.iterkeys():
-                    smaller += int(c < s[i])
+            for i in xrange(len(s)+1):
+                if i == len(s):
+                    total += 1
+                    break
+                smaller = sum(int(c < s[i]) for c in lookup.iterkeys() if len(s) != 1 or c != '0')
                 total += smaller * p
                 if s[i] not in lookup:
                     break
                 p //= len(lookup)
             return total
 
-        def validCountInLessLength(N):
-            s = str(N)
+        def validCountInLessLength(n):  # count unconfused numbers in the pattern of [01689]{1,len(n)-1} in the range of [1, n]
+            s = str(n)
             valid = 0
             total = len(centers)
-            for i in xrange(1, len(s), 2):
+            for i in xrange(1, len(s), 2):  # count unconfused numbers for each odd length less than s
                 if i == 1:
                     valid += total
                 else:
                     valid += total * (len(lookup)-1)
                     total *= len(lookup)
             total = 1
-            for i in xrange(2, len(s), 2):
+            for i in xrange(2, len(s), 2):  # count unconfused numbers for each even length less than s
                 valid += total * (len(lookup)-1)
                 total *= len(lookup)
             return valid
 
-        def validCountInFullLength(N):
-            s = str(N)
+        def validCountInFullLength(n):  # count unconfused numbers in the pattern of [01689]{len(n)} in the range of [1, n]
+            s = str(n)
             half_s = s[:(len(s)+1)//2]
             total = 0
-            p =  len(lookup)**(len(half_s)-2) * len(centers) if (len(s) % 2) else len(lookup)**(len(half_s)-1)
+            p = len(lookup)**(len(half_s)-2) * len(centers) if (len(s) % 2) else len(lookup)**(len(half_s)-1)
             choices = centers if (len(s) % 2) else lookup
             for i in xrange(len(half_s)):
                 if i+1 == len(half_s):
-                    for c in choices.iterkeys():
-                        if c == '0' and i == 0:
-                            continue
-                        total += int(c < half_s[i])
+                    total += sum(int(c < half_s[i]) for c in choices.iterkeys() if i != 0 or c != '0')
                     if half_s[i] not in choices:
                         break
-                    tmp = list(half_s)
-                    for i in reversed(xrange(len(half_s)-(len(s) % 2))):
-                        tmp.append(lookup[half_s[i]])
-                    if int("".join(tmp)) <= N:
-                        total += 1
-                    continue
+                    tmp = list(half_s)+[lookup[half_s[i]] for i in reversed(xrange(len(half_s)-(len(s) % 2)))]
+                    total += int("".join(tmp)) <= n
+                    break
+                smaller = sum(int(c < half_s[i]) for c in lookup.iterkeys() if i != 0 or c != '0')
 
-                smaller = 0
-                for c in lookup.iterkeys():
-                    if c == '0' and i == 0:
-                        continue
-                    smaller += int(c < half_s[i])
                 total += smaller * p
                 if half_s[i] not in lookup:
                     break
                 p //= len(lookup)
             return total
-            
-        return totalCount(N) - validCountInLessLength(N) - validCountInFullLength(N)
+        
+        return totalCount(n) - validCountInLessLength(n) - validCountInFullLength(n)
