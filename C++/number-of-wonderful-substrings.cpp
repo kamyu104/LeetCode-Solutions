fbca1e9698c9edf67d8636b568356d81dba11291
kamyu <kamyu104@users.noreply.github.com>
2021-06-28 02:21:27 +0800

    Create number-of-wonderful-substrings.cpp

---
+++ C++/number-of-wonderful-substrings.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(2^10)
+
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        static const int ALPHABET_SIZE = 10;
+        vector<int> count(pow(2, ALPHABET_SIZE) + 1);
+        count[0] = 1;
+        long long result = 0;
+        int curr = 0;
+        for (const auto& c : word) {
+            curr ^= 1 << (c - 'a');
+            result += count[curr];
+            for (int i = 0; i < ALPHABET_SIZE; ++i) {
+                result += count[curr ^ (1 << i)];
+            }
+            ++count[curr];
+        }
+        return result;
+    }
+};
