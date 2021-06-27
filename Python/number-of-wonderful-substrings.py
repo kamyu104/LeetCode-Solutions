b76cd93fe66d249a12b5484191b00cae489a5654
kamyu <kamyu104@users.noreply.github.com>
2021-06-28 02:21:49 +0800

    Create number-of-wonderful-substrings.py

---
+++ Python/number-of-wonderful-substrings.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(2^10)
+
+class Solution(object):
+    def wonderfulSubstrings(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        ALPHABET_SIZE = 10
+        count = [0]*(2**ALPHABET_SIZE+1)
+        count[0] = 1
+        result = curr = 0
+        for c in word:
+            curr ^= 1<<(ord(c)-ord('a'))
+            result += count[curr]
+            result += sum(count[curr^(1<<i)] for i in xrange(ALPHABET_SIZE))
+            count[curr] += 1
+        return result
