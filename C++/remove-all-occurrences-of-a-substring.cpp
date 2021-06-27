4e252ae1d580150cd88b0a31f95da68c5813d12d
kamyu <kamyu104@users.noreply.github.com>
2021-06-27 20:36:06 +0800

    Create remove-all-occurrences-of-a-substring.cpp

---
+++ C++/remove-all-occurrences-of-a-substring.cpp
@@ -0,0 +1,45 @@
+// Time:  O(n + m)
+// Space: O(n + m)
+
+// kmp solution
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        const auto& prefix = getPrefix(part);
+        string result;
+        vector<int> lookup;
+        int i = -1;
+        for (const auto& c : s) {
+            while (i != -1 && part[i + 1] != c) {
+                i = prefix[i];
+            }
+            if (part[i + 1] == c) {
+                ++i;
+            }
+            result.push_back(c);
+            lookup.emplace_back(i);
+            if (i == size(part) - 1) {
+                result.erase(begin(result) + size(result) - size(part), end(result));
+                lookup.erase(begin(lookup) + size(lookup) - size(part), end(lookup));
+                i = !empty(lookup) ? lookup.back() : -1;
+            }
+        }
+        return result;
+    }
+
+private:
+    vector<int> getPrefix(const string& pattern) {
+        vector<int> prefix(pattern.length(), -1);
+        int j = -1;
+        for (int i = 1; i < pattern.length(); ++i) {
+            while (j != -1 && pattern[j + 1] != pattern[i]) {
+                j = prefix[j];
+            }
+            if (pattern[j + 1] == pattern[i]) {
+                ++j;
+            }
+            prefix[i] = j;
+        }
+        return prefix;
+    }
+};
