7bc179a4436bc9b8cb7ab9081b7d2c4374ef77aa
kamyu <kamyu104@users.noreply.github.com>
2021-07-02 22:14:25 +0800

    Update leetcodify-friends-recommendations.sql

--- MySQL/leetcodify-friends-recommendations.sql
+++ MySQL/leetcodify-friends-recommendations.sql
@@ -4,11 +4,8 @@
 WITH recommend_cte
      AS (SELECT DISTINCT a.user_id user_id,
                          b.user_id recommended_id
-         FROM   listens a,
-                listens b
-         WHERE  a.user_id != b.user_id
-                AND a.day = b.day
-                AND a.song_id = b.song_id
+         FROM   listens a
+                INNER JOIN listens b ON a.user_id != b.user_id AND a.day = b.day AND a.song_id = b.song_id
          GROUP  BY a.user_id,
                    b.user_id,
                    a.day
@@ -21,8 +18,6 @@ FROM   recommend_cte a
 WHERE  NOT EXISTS
      (SELECT 1
       FROM   friendship b
-      WHERE  (a.user_id = b.user1_id
-              AND a.recommended_id = b.user2_id )
+      WHERE  (a.user_id = b.user1_id AND a.recommended_id = b.user2_id )
              OR
-             (a.user_id = b.user2_id
-              AND a.recommended_id = b.user1_id)); 
+             (a.user_id = b.user2_id AND a.recommended_id = b.user1_id)); 
