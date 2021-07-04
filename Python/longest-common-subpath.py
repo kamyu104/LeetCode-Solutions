298172b8ef8988459af1c254955fa64dd57dd98b
kamyu <kamyu104@users.noreply.github.com>
2021-07-04 23:05:09 +0800

    Update longest-common-subpath.py

--- Python/longest-common-subpath.py
+++ Python/longest-common-subpath.py
@@ -34,3 +34,40 @@ class Solution(object):
             else:
                 left = mid+1
         return right
+
+
+# Time:  O(m * nlogn)
+# Space: O(n)
+class Solution2(object):
+    def longestCommonSubpath(self, n, paths):
+        """
+        :type n: int
+        :type paths: List[List[int]]
+        :rtype: int
+        """
+        def RabinKarp(arr, x):
+            h = reduce(lambda h,x: (h*P+x)%MOD, (arr[i] for i in xrange(x)), 0)
+            power = pow(P, x, MOD)
+            lookup = {h}
+            for i in xrange(x, len(arr)):
+                h = (h*P - arr[i-x]*power + arr[i])%MOD
+                lookup.add(h)
+            return lookup
+        
+        def check(paths, x):
+            intersect = RabinKarp(paths[0], x)
+            for i in xrange(1, len(paths)):
+                intersect = set.intersection(intersect, RabinKarp(paths[i], x))
+                if not intersect:
+                    return False
+            return True
+
+        MOD, P = 10**11+19, max(x for p in paths for x in p)+1
+        left, right = 1, min(len(p) for p in paths)
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(paths, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return right
