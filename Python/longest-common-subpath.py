a8416bc0c5e769fb6e222ce2ad51b5afd27d0b92
kamyu <kamyu104@users.noreply.github.com>
2021-07-04 18:21:31 +0800

    Create longest-common-subpath.py

---
+++ Python/longest-common-subpath.py
@@ -0,0 +1,36 @@
+# Time:  O(m * nlogn)
+# Space: O(n)
+
+class Solution(object):
+    def longestCommonSubpath(self, n, paths):
+        """
+        :type n: int
+        :type paths: List[List[int]]
+        :rtype: int
+        """
+        def RabinKarp(arr, x):  # double hashing
+            hashes = tuple([reduce(lambda h,x: (h*p+x)%MOD, (arr[i] for i in xrange(x)), 0) for p in P])
+            powers = [pow(p, x, MOD) for p in P]
+            lookup = {hashes}
+            for i in xrange(x, len(arr)):
+                hashes = tuple([(hashes[j]*P[j] - arr[i-x]*powers[j] + arr[i])%MOD for j in xrange(len(P))])  # in smaller datasets, tuple from list is much faster than tuple from generator, see https://stackoverflow.com/questions/16940293/why-is-there-no-tuple-comprehension-in-python
+                lookup.add(hashes)
+            return lookup
+        
+        def check(paths, x):
+            intersect = RabinKarp(paths[0], x)
+            for i in xrange(1, len(paths)):
+                intersect = set.intersection(intersect, RabinKarp(paths[i], x))
+                if not intersect:
+                    return False
+            return True
+
+        MOD, P = 10**9+7, (113, 109)
+        left, right = 1, min(len(p) for p in paths)
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(paths, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return right
