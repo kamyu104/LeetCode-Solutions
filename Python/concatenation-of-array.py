399804ee84c8478e2f5795755ccafa3b9459683d
kamyu <kamyu104@users.noreply.github.com>
2021-07-12 21:11:29 +0800

    Update concatenation-of-array.py

--- Python/concatenation-of-array.py
+++ Python/concatenation-of-array.py
@@ -7,4 +7,27 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
+        nums.extend(nums)
+        return nums
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         return nums+nums
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution3(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums*2
