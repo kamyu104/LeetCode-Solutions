6f4fcc56e596c4d091103beff51f0ccf4642eca2
kamyu <kamyu104@users.noreply.github.com>
2021-07-12 21:10:06 +0800

    Create concatenation-of-array.py

---
+++ Python/concatenation-of-array.py
@@ -0,0 +1,10 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums+nums
