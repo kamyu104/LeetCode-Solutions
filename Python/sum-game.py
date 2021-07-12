29c7852b2a183a3f3aaff890585a9ca70cfedc00
kamyu <kamyu104@users.noreply.github.com>
2021-07-12 18:53:39 +0800

    Update sum-game.py

--- Python/sum-game.py
+++ Python/sum-game.py
@@ -11,10 +11,10 @@ class Solution(object):
         #     alice will try to choose a number to maximize or minimize the diff of both half sums,
         #     and bob will try to choose the same number to minimize or maximize the diff of both half sums.
         #     in the end, it turns that only one half has '?' and the diff of both half sums is still the same as original
-        # (2) if smaller half has no '?', alice wins
+        # (2) if smaller half has no '?', then alice wins
         # (3) if smaller half has '?'
-        #     (3.1) cnt of '?' is odd, alice can choose the last number to make the diff of both half sums != 0, alice wins
-        #     (3.2) cnt of '?' is even
+        #     (3.1) if cnt of '?' is odd, alice can choose the last number to make the diff of both half sums != 0, then alice wins
+        #     (3.2) if cnt of '?' is even
         #           (3.2.1) if larger-smaller = cnt/2 * 9, bob can always make a pair of sum 9 no matter what alice chooses, then bob wins
         #           (3.2.2) if larger-smaller > cnt/2 * 9, alice can always choose 0 no matter what bob chooses, then alice wins
         #           (3.2.3) if larger-smaller < cnt/2 * 9, alice can always choose 9 no matter what bob chooses, then alice wins
