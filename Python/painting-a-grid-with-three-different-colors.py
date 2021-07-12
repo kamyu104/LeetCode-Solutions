2cc426649e90b9f5ddc4a7f7dd19a51ec507fbb4
kamyu <kamyu104@users.noreply.github.com>
2021-07-13 06:35:01 +0800

    Update painting-a-grid-with-three-different-colors.py

--- Python/painting-a-grid-with-three-different-colors.py
+++ Python/painting-a-grid-with-three-different-colors.py
@@ -61,9 +61,14 @@ class Solution2(object):
         dp = collections.Counter({3**m-1: 1})
         for idx in xrange(m*n):
             r, c = divmod(idx, m)
+            # sliding window with size m doesn't cross rows:
+            #   [3, 2, ..., 2] => 3*2^(m-1) combinations
             assert(r != 0 or c != 0 or len(dp) == 1)
             assert(r != 0 or c == 0 or len(dp) == 3*2**(c-1))
             assert(r == 0 or c != 0 or len(dp) == 3*2**(m-1))
+            # sliding window with size m crosses rows:
+            #   [*, ..., *, *, 3, 2, ..., 2] => 3*3 *2^(m-2) combinations
+            #   [2, ..., 2, 3, *, *, ..., *]
             assert(r == 0 or c == 0 or len(dp) == 3**2 * 2**(m-2))
             new_dp = collections.Counter()
             for mask, v in dp.iteritems():
