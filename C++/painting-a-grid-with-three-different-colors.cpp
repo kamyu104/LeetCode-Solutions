5419ca777df3d424236e9816b2128847bb2fcb02
kamyu <kamyu104@users.noreply.github.com>
2021-07-13 20:12:03 +0800

    Update painting-a-grid-with-three-different-colors.cpp

--- C++/painting-a-grid-with-three-different-colors.cpp
+++ C++/painting-a-grid-with-three-different-colors.cpp
@@ -17,11 +17,19 @@ public:
         for (const auto& mask : masks) {  // O(3^m) leaves in depth O(m) => Time: O(m * 3^m), Space: O(3^m)
             backtracking(mask, -1, basis, &adj[mask]);
         }
-        // 'o' uses the same color with its bottom-left one, 
-        // 'x' uses the remaining color different from its left one and bottom-left one,
-        // k is the cnt of 'o', 
-        //   [2, 1(o), 1(x), 1(o), ..., 1(o), 1(x)] => nCr(m-1, k) * 3 * 2 * 2^k for k in xrange(m) = 3 * 2 * (2+1)^(m-1) = 2*3^m combinations
-        //   [3,    2,    1,    2, ...,  2,      1]
+        // proof:
+        //   'o' uses the same color with its bottom-left one, 
+        //   'x' uses the remaining color different from its left one and bottom-left one,
+        //   k is the cnt of 'o', 
+        //    [2, 1(o), 1(x), 1(o), ..., 1(o), 1(x)] => nCr(m-1, k) * 3 * 2 * 2^k for k in xrange(m) = 3 * 2 * (2+1)^(m-1) = 2*3^m combinations
+        //    [3,    2,    1,    2, ...,  2,      1]
+        // another proof:
+        //   given previous pair of colors, each pair of '?' has 3 choices of colors
+        //     [2, ?, ?, ..., ?] => 3 * 2 * 3^(m-1) = 2*3^m combinations
+        //         |  |       |
+        //         3  3       3
+        //         |  |       |
+        //     [3, ?, ?, ..., ?]
         assert(accumulate(cbegin(adj), cend(adj), 0,
                           [](const auto& total, const auto& kvp) {
                               return total + size(kvp.second);
