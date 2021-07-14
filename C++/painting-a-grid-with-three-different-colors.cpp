b36c12d0e88a65d007c7b0f15ed8cfa0f844ce9e
kamyu <kamyu104@users.noreply.github.com>
2021-07-15 04:19:59 +0800

    Update painting-a-grid-with-three-different-colors.cpp

--- C++/painting-a-grid-with-three-different-colors.cpp
+++ C++/painting-a-grid-with-three-different-colors.cpp
@@ -1,122 +1,21 @@
-// Time:  O((m + n) * 3^m) = O(n * 3^m) since m <= n
-// Space: O(3^m)
+// Time:  O(m * 3^m + 2^(3 * m) * logn)
+// Space: O(2^(2 * m))
 
-// better complexity for small m, large n
+// better complexity for small m, super large n
 class Solution {
 public:
     int colorTheGrid(int m, int n) {
-        static const int MOD = 1e9 + 7;
-
         if (m > n) {
             swap(m, n);
         }
         const int basis = pow(3, m - 1);
-        vector<int> masks;
+        unordered_set<int> masks;
         backtracking(-1, -1, basis, &masks);  // Time: O(2^m), Space: O(2^m)
         assert(size(masks) == 3 * pow(2, m - 1));
-        unordered_map<int, vector<int>> adj;
+        unordered_map<int, unordered_set<int>> adj;
         for (const auto& mask : masks) {  // O(3^m) leaves in depth O(m) => Time: O(m * 3^m), Space: O(3^m)
             backtracking(mask, -1, basis, &adj[mask]);
         }
-        // proof:
-        //   'o' uses the same color with its bottom-left one, 
-        //   'x' uses the remaining color different from its left one and bottom-left one,
-        //   k is the cnt of 'o', 
-        //    [3, 1(o), 1(x), 1(o), ..., 1(o), 1(x)] => nCr(m-1, k) * 3 * 2 * 2^k for k in xrange(m) = 3 * 2 * (2+1)^(m-1) = 2*3^m combinations
-        //    [2,    2,    1,    2, ...,  2,      1]
-        // another proof:
-        //   given previous pair of colors, each pair of '?' has 3 choices of colors
-        //     [3, ?, ?, ..., ?] => 3 * 2 * 3^(m-1) = 2*3^m combinations
-        //         |  |       |
-        //         3  3       3
-        //         |  |       |
-        //     [2, ?, ?, ..., ?]
-        assert(accumulate(cbegin(adj), cend(adj), 0,
-                          [](const auto& total, const auto& kvp) {
-                              return total + size(kvp.second);
-                          }) == 2 * pow(3, m));
-        unordered_map<int, int> lookup;
-        for (const auto& mask : masks) {  // Time: O(m * 2^m)
-            lookup[mask] = normalize(m, mask);
-        }
-        unordered_map<int, int> dp;
-        for (const auto& mask : masks) {  // normalize colors to speed up performance
-            ++dp[lookup[mask]];
-        }
-        unordered_map<int, unordered_map<int, int>> normalized_adj;
-        for (const auto& mask1 : masks) {
-            for (const auto& mask2 : adj[mask1]) {
-                if (mask1 == lookup[mask1]) {
-                    normalized_adj[lookup[mask1]][lookup[mask2]] = (normalized_adj[lookup[mask1]][lookup[mask2]] + 1) % MOD;
-                }
-            }
-        }
-        // divided by 3 * 2 is since the first two colors in upper row are normalized to speed up performance
-        assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
-                          [](const auto& total, const auto& kvp) {
-                              return total + size(kvp.second);
-                          }) <= 2 * pow(3, m) / 3 / 2);
-        // since one color in lower row could be also normalized, lower bound is upper bound divided by at most 3
-        assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
-                          [](const auto& total, const auto& kvp) {
-                              return total + size(kvp.second);
-                          }) >= 2 * pow(3, m) / 3 / 2 / 3);
-        for (int i = 0; i < n - 1; ++i) {  // Time: O(n * 3^m), Space: O(2^m)
-            assert(size(dp) == 3 * pow(2, m - 1) / 3 / (m >= 2 ? 2 : 1));  // divided by 3 * 2 is since the first two colors are normalized to speed up performance
-            unordered_map<int, int> new_dp;
-            for (const auto [mask, v] : dp) {
-                for (const auto& [new_mask, cnt] : normalized_adj[mask]) {
-                    new_dp[lookup[new_mask]] = (new_dp[lookup[new_mask]] + (v * int64_t(cnt)) % MOD) % MOD;
-                }
-            }
-            dp = move(new_dp);
-        }
-        return accumulate(cbegin(dp), cend(dp), 0,
-                          [](const auto& total, const auto& kvp) {
-                              return (total + kvp.second) % MOD;
-                          });  // Time: O(2^m)
-    }
-
-private:
-    void backtracking(int mask1, int mask2, int basis, vector<int> *result) {  // Time: O(2^m), Space: O(2^m)
-        if (!basis) {
-            result->emplace_back(mask2);
-            return;
-        }
-        for (int i = 0; i < 3; ++i) {
-            if ((mask1 == -1 || mask1 / basis % 3 != i) && (mask2 == -1 || mask2 / (basis * 3) % 3 != i)) {
-                backtracking(mask1, mask2 != -1 ? mask2 + i * basis : i * basis, basis / 3, result);
-            }
-        }
-    }
- 
-    int normalize(int m, int mask) {
-        unordered_map<int, int> norm;
-        int result = 0, basis = 1;
-        for (; m; --m, basis *= 3) {
-            int x = mask / basis % 3;
-            if (!norm.count(x)) {
-                norm[x] = size(norm);
-            }
-            result += norm[x] * basis;
-        }
-        return result;
-    }
-};
-
-// Time:  O(m * 3^m + 2^(3 * m) * logn)
-// Space: O(2^(2 * m))
-// better complexity for small m, super large n
-class Solution2 {
-public:
-    int colorTheGrid(int m, int n) {
-        if (m > n) {
-            swap(m, n);
-        }
-        const int basis = pow(3, m - 1);
-        const auto& masks = find_masks(m, basis);  // alternative of backtracking, Time: O(2^m), Space: O(2^m)
-        assert(size(masks) == 3 * pow(2, m - 1));
-        const auto& adj = find_adj(m, basis, masks);  // alternative of backtracking, Time: O(m * 3^m), Space: O(3^m)
         assert(accumulate(cbegin(adj), cend(adj), 0,
                           [](const auto& total, const auto& kvp) {
                               return total + size(kvp.second);
@@ -138,12 +37,12 @@ public:
                 }
             }
         }
-        // divided by 3 * 2 is since the first two colors in upper row are normalized to speed up performance
+        // upper bound divided by 3 * 2 is since the first two colors in upper row are normalized to speed up performance
         assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
                           [](const auto& total, const auto& kvp) {
                               return total + size(kvp.second);
                           }) <= 2 * pow(3, m) / 3 / 2);
-        // since one color in lower row could be also normalized, lower bound is upper bound divided by at most 3
+        // lower bound divided by one more 3 is since one color in lower row could be also normalized
         assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
                           [](const auto& total, const auto& kvp) {
                               return total + size(kvp.second);
@@ -165,46 +64,16 @@ public:
     }
 
 private:
-    unordered_set<int> find_masks(int m, int basis) {  // Time: 3 + 3*2 + 3*2*2 + ... + 3*2^(m-1) = 3 * (2^m - 1) = O(2^m), Space: O(2^m)
-        unordered_set<int> masks = {0};
-        for (int c = 0; c < m; ++c) {
-            unordered_set<int> new_masks;
-            for (const auto& mask : masks) {
-                unordered_set<int> choices = {0, 1, 2};
-                if (c > 0) {
-                    choices.erase(mask / basis);  // get left grid
-                }
-                for (const auto&x : choices) {
-                    new_masks.emplace((x * basis) + (mask / 3));  // encoding mask
-                }
+    void backtracking(int mask1, int mask2, int basis, unordered_set<int> *result) {  // Time: O(2^m), Space: O(2^m)
+        if (!basis) {
+            result->emplace(mask2);
+            return;
+        }
+        for (int i = 0; i < 3; ++i) {
+            if ((mask1 == -1 || mask1 / basis % 3 != i) && (mask2 == -1 || mask2 / (basis * 3) % 3 != i)) {
+                backtracking(mask1, mask2 != -1 ? mask2 + i * basis : i * basis, basis / 3, result);
             }
-            masks = move(new_masks);
         }
-        return masks;
-    }
-
-    unordered_map<int, unordered_set<int>> find_adj(int m, int basis, const unordered_set<int>& masks) {  // Time: O(m * 3^m), Space: O(3^m)
-        unordered_map<int, unordered_set<int>> adj;
-        for (const auto& mask : masks) {  // O(2^m)
-            adj[mask].emplace(mask);
-        }
-        for (int c = 0; c < m; ++c) {
-            unordered_map<int, unordered_set<int>> new_adj;
-            for (const auto& [mask1, mask2s] : adj) {
-                for (const auto& mask : mask2s) {
-                    unordered_set<int> choices = {0, 1, 2};
-                    choices.erase(mask % 3);  // get up grid;
-                    if (c > 0) {
-                        choices.erase(mask / basis);  // get left grid
-                    }
-                    for (const auto&x : choices) {
-                        new_adj[mask1].emplace((x * basis) + (mask / 3));  // encoding mask
-                    }
-                }
-            }
-            adj = move(new_adj);
-        }
-        return adj;
     }
 
     vector<vector<int>> matrixExpo(const vector<vector<int>>& A, int pow) {
@@ -251,6 +120,137 @@ private:
     }
 
     static const int MOD = 1e9 + 7;
+};
+
+// Time:  O((m + n) * 3^m) = O(n * 3^m) since m <= n
+// Space: O(3^m)
+// better complexity for small m, large n
+class Solution2 {
+public:
+    int colorTheGrid(int m, int n) {
+        static const int MOD = 1e9 + 7;
+
+        if (m > n) {
+            swap(m, n);
+        }
+        const int basis = pow(3, m - 1);
+        const auto& masks = find_masks(m, basis);  // alternative of backtracking, Time: O(2^m), Space: O(2^m)
+        assert(size(masks) == 3 * pow(2, m - 1));
+        const auto& adj = find_adj(m, basis, masks);  // alternative of backtracking, Time: O(m * 3^m), Space: O(3^m)
+        // proof:
+        //   'o' uses the same color with its bottom-left one, 
+        //   'x' uses the remaining color different from its left one and bottom-left one,
+        //   k is the cnt of 'o', 
+        //    [3, 1(o), 1(x), 1(o), ..., 1(o), 1(x)] => nCr(m-1, k) * 3 * 2 * 2^k for k in xrange(m) = 3 * 2 * (2+1)^(m-1) = 2*3^m combinations
+        //    [2,    2,    1,    2, ...,  2,      1]
+        // another proof:
+        //   given previous pair of colors, each pair of '?' has 3 choices of colors
+        //     [3, ?, ?, ..., ?] => 3 * 2 * 3^(m-1) = 2*3^m combinations
+        //         |  |       |
+        //         3  3       3
+        //         |  |       |
+        //     [2, ?, ?, ..., ?]
+        assert(accumulate(cbegin(adj), cend(adj), 0,
+                          [](const auto& total, const auto& kvp) {
+                              return total + size(kvp.second);
+                          }) == 2 * pow(3, m));
+        unordered_map<int, int> lookup;
+        for (const auto& mask : masks) {  // Time: O(m * 2^m)
+            lookup[mask] = normalize(m, mask);
+        }
+        unordered_map<int, int> dp;
+        for (const auto& mask : masks) {  // normalize colors to speed up performance
+            ++dp[lookup[mask]];
+        }
+        unordered_map<int, unordered_map<int, int>> normalized_adj;
+        for (const auto& mask1 : masks) {
+            for (const auto& mask2 : adj.at(mask1)) {
+                if (mask1 == lookup[mask1]) {
+                    normalized_adj[lookup[mask1]][lookup[mask2]] = (normalized_adj[lookup[mask1]][lookup[mask2]] + 1) % MOD;
+                }
+            }
+        }
+        // divided by 3 * 2 is since the first two colors in upper row are normalized to speed up performance
+        assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
+                          [](const auto& total, const auto& kvp) {
+                              return total + size(kvp.second);
+                          }) <= 2 * pow(3, m) / 3 / 2);
+        // since one color in lower row could be also normalized, lower bound is upper bound divided by at most 3
+        assert(accumulate(cbegin(normalized_adj), cend(normalized_adj), 0,
+                          [](const auto& total, const auto& kvp) {
+                              return total + size(kvp.second);
+                          }) >= 2 * pow(3, m) / 3 / 2 / 3);
+        for (int i = 0; i < n - 1; ++i) {  // Time: O(n * 3^m), Space: O(2^m)
+            assert(size(dp) == 3 * pow(2, m - 1) / 3 / (m >= 2 ? 2 : 1));  // divided by 3 * 2 is since the first two colors are normalized to speed up performance
+            unordered_map<int, int> new_dp;
+            for (const auto [mask, v] : dp) {
+                for (const auto& [new_mask, cnt] : normalized_adj[mask]) {
+                    new_dp[lookup[new_mask]] = (new_dp[lookup[new_mask]] + (v * int64_t(cnt)) % MOD) % MOD;
+                }
+            }
+            dp = move(new_dp);
+        }
+        return accumulate(cbegin(dp), cend(dp), 0,
+                          [](const auto& total, const auto& kvp) {
+                              return (total + kvp.second) % MOD;
+                          });  // Time: O(2^m)
+    }
+
+private:
+    unordered_set<int> find_masks(int m, int basis) {  // Time: 3 + 3*2 + 3*2*2 + ... + 3*2^(m-1) = 3 * (2^m - 1) = O(2^m), Space: O(2^m)
+        unordered_set<int> masks = {0};
+        for (int c = 0; c < m; ++c) {
+            unordered_set<int> new_masks;
+            for (const auto& mask : masks) {
+                unordered_set<int> choices = {0, 1, 2};
+                if (c > 0) {
+                    choices.erase(mask / basis);  // get left grid
+                }
+                for (const auto&x : choices) {
+                    new_masks.emplace((x * basis) + (mask / 3));  // encoding mask
+                }
+            }
+            masks = move(new_masks);
+        }
+        return masks;
+    }
+
+    unordered_map<int, unordered_set<int>> find_adj(int m, int basis, const unordered_set<int>& masks) {  // Time: O(m * 3^m), Space: O(3^m)
+        unordered_map<int, unordered_set<int>> adj;
+        for (const auto& mask : masks) {  // O(2^m)
+            adj[mask].emplace(mask);
+        }
+        for (int c = 0; c < m; ++c) {
+            unordered_map<int, unordered_set<int>> new_adj;
+            for (const auto& [mask1, mask2s] : adj) {
+                for (const auto& mask : mask2s) {
+                    unordered_set<int> choices = {0, 1, 2};
+                    choices.erase(mask % 3);  // get up grid;
+                    if (c > 0) {
+                        choices.erase(mask / basis);  // get left grid
+                    }
+                    for (const auto&x : choices) {
+                        new_adj[mask1].emplace((x * basis) + (mask / 3));  // encoding mask
+                    }
+                }
+            }
+            adj = move(new_adj);
+        }
+        return adj;
+    }
+ 
+    int normalize(int m, int mask) {
+        unordered_map<int, int> norm;
+        int result = 0, basis = 1;
+        for (; m; --m, basis *= 3) {
+            int x = mask / basis % 3;
+            if (!norm.count(x)) {
+                norm[x] = size(norm);
+            }
+            result += norm[x] * basis;
+        }
+        return result;
+    }
 };
 
 // Time:  (m * n grids) * (O(3*3*2^(m-2)) possible states per grid) = O(n * m * 2^m)
