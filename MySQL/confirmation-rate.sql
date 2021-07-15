2a25c484d1952d6de93f75ab50a21ee52ff205ce
kamyu <kamyu104@users.noreply.github.com>
2021-07-16 05:01:21 +0800

    Create confirmation-rate.sql

---
+++ MySQL/confirmation-rate.sql
@@ -0,0 +1,17 @@
+# Time:  O(m + n)
+# Space: O(m + n)
+
+WITH rate_cte AS
+  (SELECT user_id,
+          ROUND(SUM(CASE
+                        WHEN action='confirmed' THEN 1
+                        ELSE 0
+                    END) / COUNT(*), 2) AS confirmation_rate
+   FROM Confirmations
+   GROUP BY user_id
+   ORDER BY NULL)
+
+SELECT s.user_id,
+       IFNULL(r.confirmation_rate, 0) AS confirmation_rate
+FROM Signups s
+LEFT JOIN rate_cte r ON s.user_id = r.user_id;
