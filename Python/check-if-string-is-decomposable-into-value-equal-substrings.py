c750202ac94c1735dd0d8e2ee38da71ec9fabbc5
kamyu <kamyu104@users.noreply.github.com>
2021-07-15 21:08:37 +0800

    Update check-if-string-is-decomposable-into-value-equal-substrings.py

--- Python/check-if-string-is-decomposable-into-value-equal-substrings.py
+++ Python/check-if-string-is-decomposable-into-value-equal-substrings.py
@@ -7,6 +7,31 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        found, i = False, 0
+        while i < len(s):
+            l = 1
+            for j in xrange(i+1, min(i+3, len(s))):
+                if s[j] != s[i]:
+                    break
+                l += 1
+            if l < 2:
+                return False
+            if l == 2:
+                if found:
+                    return False
+                found = True
+            i += l  
+        return found
+        
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def isDecomposable(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
         found, l = False, 0
         for i, c in enumerate(s):
             if not l or c != s[i-1]:
