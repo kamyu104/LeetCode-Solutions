c666d3f4d7391bc04481e820246a8d061e59e1bb
kamyu <kamyu104@users.noreply.github.com>
2021-07-18 21:46:25 +0800

    Update minimum-numbers-of-function-calls-to-make-target-array.cpp

--- C++/minimum-numbers-of-function-calls-to-make-target-array.cpp
+++ C++/minimum-numbers-of-function-calls-to-make-target-array.cpp
@@ -6,12 +6,14 @@ public:
     int minOperations(vector<int>& nums) {
         int result = 0, max_len = 1;
         for (const auto& num : nums) {
-            if (!num) {
-                continue;
-            }
             result += __builtin_popcount(num);
-            max_len = max(max_len, 32 - __builtin_clz(num));
+            max_len = max(max_len, bit_length(num));
         }
         return result + max_len - 1;
+    }
+
+private:
+    int bit_length(int x) {
+        return x != 0 ? 32 - __builtin_clz(x) : 1;
     }
 };
