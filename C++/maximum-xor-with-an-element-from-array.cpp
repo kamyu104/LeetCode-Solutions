36b59077bda11b259fbc71c0538b8ac8f3a09960
kamyu <kamyu104@users.noreply.github.com>
2021-07-18 21:23:51 +0800

    Update maximum-xor-with-an-element-from-array.cpp

--- C++/maximum-xor-with-an-element-from-array.cpp
+++ C++/maximum-xor-with-an-element-from-array.cpp
@@ -1,47 +1,55 @@
-// Time:  O(nlogn + mlogm)
-// Space: O(n)
+// Time:  O(nlogn + mlogm + nlogk + mlogk), k is max(max(nums), max(xi))
+// Space: O(nlogk)
 
 class Trie {
 public:
-    Trie() : nodes(1) {}
+    Trie(int bit_length)
+     : bit_length_(bit_length)
+     , nodes_(1) {}
     
     void insert(int num) {
         int idx = 0;
-        for (int i = 31; i >= 0; --i) {
+        for (int i = bit_length_; i >= 0; --i) {
             int curr = (num >> i) & 1;
-            if (!nodes[idx][curr]) {
-                nodes.emplace_back();
-                nodes[idx][curr] = size(nodes) - 1;
+            if (!nodes_[idx][curr]) {
+                nodes_.emplace_back();
+                nodes_[idx][curr] = size(nodes_) - 1;
             }
-            idx = nodes[idx][curr];
+            idx = nodes_[idx][curr];
         }
     }
 
     int query(int num) {
-        if (size(nodes) == 1) {
+        if (size(nodes_) == 1) {
             return -1;
         }
         int result = 0, idx = 0;
-        for (int i = 31; i >= 0; --i) {
+        for (int i = bit_length_; i >= 0; --i) {
             int curr = (num >> i) & 1;
-            if (nodes[idx][1 ^ curr]) {
-                idx = nodes[idx][1 ^ curr];
+            if (nodes_[idx][1 ^ curr]) {
+                idx = nodes_[idx][1 ^ curr];
                 result |= 1 << i;
             } else {
-                idx = nodes[idx][curr];
+                idx = nodes_[idx][curr];
             }
         }
         return result;
     }
 
 private:
-    vector<array<int, 2>> nodes;
+    const int bit_length_;
+    vector<array<int, 2>> nodes_;
 };
 
 class Solution {
 public:
     vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
         sort(begin(nums), end(nums));
+        int max_val = max(nums.back(),
+                          (*max_element(cbegin(queries), cend(queries),
+                                        [](const auto& a, const auto& b) {
+                                            return a[0] < b[0];
+                                        }))[0]);
         for (int i = 0; i < size(queries); ++i) {
             queries[i].insert(begin(queries[i]), i);
         }
@@ -50,7 +58,7 @@ public:
                  return a[2] < b[2];
              });
         vector<int> result(size(queries));
-        Trie trie;
+        Trie trie(32 - __builtin_clz(max_val));
         int j = 0;
         for (const auto& q : queries) {
             int i = q[0], x = q[1], m = q[2];
