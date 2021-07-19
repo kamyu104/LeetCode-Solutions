0c73a0afcd2b1edefbc0a214bbaf08a12671dd99
kamyu <kamyu104@users.noreply.github.com>
2021-07-19 22:16:56 +0800

    Update maximum-xor-of-two-numbers-in-an-array.py

--- Python/maximum-xor-of-two-numbers-in-an-array.py
+++ Python/maximum-xor-of-two-numbers-in-an-array.py
@@ -20,3 +20,47 @@ class Solution(object):
                     break
 
         return result
+
+
+ # Time:  O(nlogk), k is max(nums)
+# Space: O(nlogk)
+
+class Trie(object):
+    def __init__(self, bit_length):
+        self.__root = {}
+        self.__bit_length = bit_length
+        
+    def insert(self, num):
+        node = self.__root
+        for i in reversed(xrange(self.__bit_length)):
+            curr = (num>>i) & 1
+            if curr not in node:
+                node[curr] = {}
+            node = node[curr]
+                
+    def query(self, num):
+        if not self.__root: 
+            return -1
+        node, result = self.__root, 0
+        for i in reversed(xrange(self.__bit_length)):
+            curr = (num>>i) & 1
+            if 1^curr in node:
+                node = node[1^curr]
+                result |= 1<<i
+            else:
+                node = node[curr]
+        return result
+
+
+class Solution2(object):
+    def findMaximumXOR(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        trie = Trie(max(nums).bit_length())
+        result = 0
+        for num in nums:
+            trie.insert(num)
+            result = max(result, trie.query(num))
+        return result
