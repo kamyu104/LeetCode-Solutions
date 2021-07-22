cf39b0d800a93f03083ee2e9e1d8525c4bbdce81
kamyu <kamyu104@users.noreply.github.com>
2021-07-22 16:30:38 +0800

    Update longest-common-subsequence-between-sorted-arrays.cpp

--- C++/longest-common-subsequence-between-sorted-arrays.cpp
+++ C++/longest-common-subsequence-between-sorted-arrays.cpp
@@ -4,16 +4,20 @@
 class Solution {
 public:
     vector<int> longestCommomSubsequence(vector<vector<int>>& arrays) {
-        vector<int> result = *min_element(cbegin(arrays), cend(arrays),
-                                          [](const auto& a, const auto& b) {
-                                              return size(a) < size(b);
-                                          });
-        for (const auto& arr : arrays) {
+        const auto min_cit = min_element(cbegin(arrays), cend(arrays),
+                                         [](const auto& a, const auto& b) {
+                                             return size(a) < size(b);
+                                         });
+        vector<int> result = *min_cit;
+        for (auto cit = cbegin(arrays); cit != cend(arrays); ++cit) {
+            if (cit == min_cit) {
+                continue;
+            }
             vector<int> new_result;
-            for (int i = 0, j = 0; i != size(result) && j != size(arr);) {
-                if (result[i] < arr[j]) {
+            for (int i = 0, j = 0; i != size(result) && j != size(*cit);) {
+                if (result[i] < (*cit)[j]) {
                     ++i;
-                } else if (result[i] > arr[j]) {
+                } else if (result[i] > (*cit)[j]) {
                     ++j;
                 } else {
                     new_result.emplace_back(result[i]);
