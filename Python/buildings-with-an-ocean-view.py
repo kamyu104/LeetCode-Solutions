681ce8c01c2efa8e5fe1f9567314739b484ca53d
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 04:54:58 +0800

    Update buildings-with-an-ocean-view.py

--- Python/buildings-with-an-ocean-view.py
+++ Python/buildings-with-an-ocean-view.py
@@ -1,23 +1,7 @@
 # Time:  O(n)
-# Space: O(1)
+# Space: O(n)
 
 class Solution(object):
-    def findBuildings(self, heights):
-        """
-        :type heights: List[int]
-        :rtype: List[int]
-        """
-        result = []
-        for i in reversed(xrange(len(heights))):
-            if not result or heights[result[-1]] < heights[i]:
-                result.append(i)
-        result.reverse()
-        return result
-
-
-# Time:  O(n)
-# Space: O(n)
-class Solution2(object):
     def findBuildings(self, heights):
         """
         :type heights: List[int]
@@ -28,4 +12,20 @@ class Solution2(object):
             while result and heights[result[-1]] <= h:
                 result.pop()
             result.append(i)
+        return result
+
+
+# Time:  O(n)
+# Space: O(1)
+class Solution2(object):
+    def findBuildings(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for i in reversed(xrange(len(heights))):
+            if not result or heights[result[-1]] < heights[i]:
+                result.append(i)
+        result.reverse()
         return result
