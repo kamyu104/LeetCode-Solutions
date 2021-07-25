33d3ee69348ee21a6e3b6be62c5ddb5eea272837
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 04:24:37 +0800

    Update delete-duplicate-folders-in-system.py

--- Python/delete-duplicate-folders-in-system.py
+++ Python/delete-duplicate-folders-in-system.py
@@ -1,13 +1,72 @@
-# Time:  O(m * nlogn + l * t^2), m is the max number of folders in a path,
-#                              , n is the number of paths
-#                              , l is the max length of folder name
-#                              , t is the size of trie
-# Space: O(l * t^2)
+# Time:  O(m * nlogn + l * t), m is the max number of folders in a path,
+#                            , n is the number of paths
+#                            , l is the max length of folder name
+#                            , t is the size of trie
+# Space: O(l * t)
 
 import collections
 
 
 class Solution(object):
+    def deleteDuplicateFolder(self, paths):
+        """
+        :type paths: List[List[str]]
+        :rtype: List[List[str]]
+        """
+        def mark(node, lookup, ids):
+            node_id = ids[tuple("%s %s" % (subfolder, mark(child, lookup, ids)) for subfolder, child in sorted(node.iteritems()) if child != "_del")]
+            if node_id != 0:
+                if node_id in lookup:
+                    lookup[node_id]["_del"]
+                    node["_del"]
+                else:
+                    lookup[node_id] = node
+            return node_id
+        
+        def getid(root, lookup, trees):
+            if not root:
+                return
+            node_id = lookup[root.val,
+                             getid(root.left, lookup, trees),
+                             getid(root.right, lookup, trees)]
+            trees[node_id].append(root)
+            return node_id
+
+        trees = collections.defaultdict(list)
+        lookup = collections.defaultdict()
+        lookup.default_factory = lookup.__len__
+        
+        def sweep(node, path, result):
+            if path:
+                result.append(path[:])
+            for subfolder, child in node.iteritems():
+                if "_del" in child:
+                    continue
+                path.append(subfolder)
+                sweep(child, path, result)
+                path.pop()
+
+        _trie = lambda: collections.defaultdict(_trie)
+        trie = _trie()
+        for path in paths:
+            reduce(dict.__getitem__, path, trie)
+        lookup, result = {}, []
+        ids = collections.defaultdict()
+        ids.default_factory = ids.__len__
+        mark(trie, lookup, ids)
+        sweep(trie, [], result)
+        return result
+
+
+# Time:  O(m * nlogn + l * t^2), m is the max number of folders in a path,
+#                              , n is the number of paths
+#                              , l is the max length of folder name
+#                              , t is the size of trie
+# Space: O(l * t^2)
+import collections
+
+
+class Solution2(object):
     def deleteDuplicateFolder(self, paths):
         """
         :type paths: List[List[str]]
