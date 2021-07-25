5a27b52189c18b040d7755dc15b63e082f1e9a51
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 06:35:24 +0800

    Update delete-duplicate-folders-in-system.py

--- Python/delete-duplicate-folders-in-system.py
+++ Python/delete-duplicate-folders-in-system.py
@@ -29,32 +29,32 @@ class Solution(object):
                     lookup[node_id] = node
             return node_id
 
-        def sweep(node, names, path, result):
+        def sweep(node, id_folders, path, result):
             if path:
-                result.append([names[i] for i in path])
+                result.append([id_folders[i] for i in path])
             for subfolder_id, child in node.iteritems():
                 if "_del" in child:
                     continue
                 path.append(subfolder_id)
-                sweep(child, names, path, result)
+                sweep(child, id_folders, path, result)
                 path.pop()
 
         _trie = lambda: collections.defaultdict(_trie)
         trie = _trie()
         folder_ids = collections.defaultdict()
         folder_ids.default_factory = folder_ids.__len__
-        names = {}
+        id_folders = {}
         for path in paths:
             node = trie
             for folder in path:
-                if folder_ids[folder] not in names:
-                    names[folder_ids[folder]] = folder
+                if folder_ids[folder] not in id_folders:
+                    id_folders[folder_ids[folder]] = folder
                 node = node[folder_ids[folder]]
         lookup, result = {}, []
         node_ids = collections.defaultdict()
         node_ids.default_factory = node_ids.__len__
         mark(trie, lookup, node_ids)
-        sweep(trie, names, [], result)
+        sweep(trie, id_folders, [], result)
         return result
 
 
