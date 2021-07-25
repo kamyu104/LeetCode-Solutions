364241d3d5c1aaa407da323626142a23d5f7e0e5
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 00:18:34 +0800

    Update maximum-compatibility-score-sum.cpp

--- C++/maximum-compatibility-score-sum.cpp
+++ C++/maximum-compatibility-score-sum.cpp
@@ -6,20 +6,20 @@
 // Time:  O(N^2 * M)
 // Space: O(N + M)
 pair<int, vector<int>> hungarian(const vector<vector<int>> &a) {
-	if (empty(a)) {
+    if (empty(a)) {
         return {0, {}};
     }
-	int n = size(a) + 1, m = size(a[0]) + 1;
-	vector<int> u(n), v(m), p(m), ans(n - 1);
-	for (int i = 1; i < n; ++i) {
-		p[0] = i;
-		int j0 = 0;  // add "dummy" worker 0
-		vector<int> dist(m, numeric_limits<int>::max()), pre(m, -1);
-		vector<bool> done(m + 1);
-		do {  // dijkstra
-			done[j0] = true;
-			int i0 = p[j0], j1, delta = numeric_limits<int>::max();
-			for (int j = 1; j < m; ++j) {
+    int n = size(a) + 1, m = size(a[0]) + 1;
+    vector<int> u(n), v(m), p(m), ans(n - 1);
+    for (int i = 1; i < n; ++i) {
+        p[0] = i;
+        int j0 = 0;  // add "dummy" worker 0
+        vector<int> dist(m, numeric_limits<int>::max()), pre(m, -1);
+        vector<bool> done(m + 1);
+        do {  // dijkstra
+            done[j0] = true;
+            int i0 = p[j0], j1, delta = numeric_limits<int>::max();
+            for (int j = 1; j < m; ++j) {
                 if (!done[j]) {
                     auto cur = a[i0 - 1][j - 1] - u[i0] - v[j];
                     if (cur < dist[j]) {
@@ -30,26 +30,26 @@ pair<int, vector<int>> hungarian(const vector<vector<int>> &a) {
                     }
                 }
             }
-			for (int j = 0; j < m; ++j) {
-				if (done[j]) {
+            for (int j = 0; j < m; ++j) {
+                if (done[j]) {
                     u[p[j]] += delta, v[j] -= delta;
                 } else {
                     dist[j] -= delta;
                 }
-			}
-			j0 = j1;
-		} while (p[j0]);
-		while (j0) {  // update alternating path
-			int j1 = pre[j0];
-			p[j0] = p[j1], j0 = j1;
-		}
-	}
-	for (int j = 1; j < m; ++j) {
+            }
+            j0 = j1;
+        } while (p[j0]);
+        while (j0) {  // update alternating path
+            int j1 = pre[j0];
+            p[j0] = p[j1], j0 = j1;
+        }
+    }
+    for (int j = 1; j < m; ++j) {
         if (p[j]) {
             ans[p[j] - 1] = j - 1;
         }
     }
-	return {-v[0], ans};  // min cost
+    return {-v[0], ans};  // min cost
 }
 
 class Solution {
@@ -84,6 +84,7 @@ public:
         vector<int> m_bitmasks = bitmasks(mentors);
         int mask = pow(2, size(mentors)) - 1;
         vector<int> dp(mask + 1, -1);
+        dp[0] = 0;
         return solve(s_bitmasks, m_bitmasks, 0, mask, size(students), size(students[0]), &dp);
     }
 
@@ -92,9 +93,6 @@ private:
               const vector<int>& m_bitmasks,
               int i, int mask, int m, int n,
               vector<int> *dp){
-        if (i == m) {
-            return 0;
-        }
         if ((*dp)[mask] == -1) {
             int result = 0;
             for (int j = 0, basis = 1; j < m; ++j, basis <<= 1) {
