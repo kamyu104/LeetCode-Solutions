51ec313822c0ce119371b3a667b6786785d93a51
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 04:06:39 +0800

    Update find-duplicate-subtrees.py

--- Python/find-duplicate-subtrees.py
+++ Python/find-duplicate-subtrees.py
@@ -11,17 +11,19 @@ class Solution(object):
         :rtype: List[TreeNode]
         """
         def getid(root, lookup, trees):
-            if root:
-                node_id = lookup[root.val, \
-                                 getid(root.left, lookup, trees), \
-                                 getid(root.right, lookup, trees)]
-                trees[node_id].append(root)
-                return node_id
+            if not root:
+                return
+            node_id = lookup[root.val,
+                             getid(root.left, lookup, trees),
+                             getid(root.right, lookup, trees)]
+            trees[node_id].append(root)
+            return node_id
+
         trees = collections.defaultdict(list)
         lookup = collections.defaultdict()
         lookup.default_factory = lookup.__len__
         getid(root, lookup, trees)
-        return [roots[0] for roots in trees.values() if len(roots) > 1]
+        return [roots[0] for roots in trees.itervalues() if len(roots) > 1]
 
 
 # Time:  O(n * h)
