8910ded1dd4fab84548e948e58b8edcfa04e9fb6
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 06:02:27 +0800

    Update delete-duplicate-folders-in-system.py

--- Python/delete-duplicate-folders-in-system.py
+++ Python/delete-duplicate-folders-in-system.py
@@ -54,7 +54,7 @@ class Solution(object):
         return result
 
 
-# Time:  O(n * m * l + m * tlogt + l * t^2), m is the max number of folders in a path,
+# Time:  O(n * m * l + l * tlogt + l * t^2), m is the max number of folders in a path,
 #                                          , n is the number of paths
 #                                          , l is the max length of folder name
 #                                          , t is the size of trie
