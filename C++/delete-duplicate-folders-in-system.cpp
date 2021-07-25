3fc2496d69ea2737e77b6f9b8e6dc3e498f1cecc
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 06:30:37 +0800

    Update delete-duplicate-folders-in-system.cpp

--- C++/delete-duplicate-folders-in-system.cpp
+++ C++/delete-duplicate-folders-in-system.cpp
@@ -30,15 +30,22 @@ private:
     class TrieNode { 
     public:
         bool deleted = false;
-        unordered_map<string, unique_ptr<TrieNode>> leaves;
+        unordered_map<int, unique_ptr<TrieNode>> leaves;
 
-        void Insert(const vector<string>& s) {
+        void Insert(const vector<string>& s,
+                    unordered_map<string, int> *folder_ids,
+                    unordered_map<int, string> *names) {
             auto p = this;
             for (const auto& c : s) {
-                if (!p->leaves.count(c)) {
-                    p->leaves[c] = make_unique<TrieNode>();
+                if (!folder_ids->count(c)) {
+                    (*folder_ids)[c] = size(*folder_ids);
+                    (*names)[(*folder_ids)[c]] = c;
                 }
-                p = p->leaves[c].get();
+                const auto folder_id = (*folder_ids)[c];
+                if (!p->leaves.count(folder_id)) {
+                    p->leaves[folder_id] = make_unique<TrieNode>();
+                }
+                p = p->leaves[folder_id].get();
             }
         }
     };
@@ -46,30 +53,27 @@ private:
 public:
     vector<vector<string>> deleteDuplicateFolder(vector<vector<string>>& paths) {
         auto trie = make_unique<TrieNode>();
+        unordered_map<string, int> folder_ids;
+        unordered_map<int, string> names;
         for (const auto& path : paths) {
-            trie->Insert(path);
+            trie->Insert(path, &folder_ids, &names);
         }
         unordered_map<int, TrieNode*> lookup;
         unordered_map<vector<pair<int, int>>, int, VectorPairHash<int>> node_ids;
-        unordered_map<string, int> folder_ids;
-        mark(trie.get(), &lookup, &node_ids, &folder_ids);
+        mark(trie.get(), &lookup, &node_ids);
         vector<vector<string>> result;
-        vector<string> path;
-        sweep(trie.get(), &path, &result);
+        vector<int> path;
+        sweep(trie.get(), names, &path, &result);
         return result;
     }
 
 private:
     int mark(TrieNode *node,
              unordered_map<int, TrieNode*> *lookup,
-             unordered_map<vector<pair<int, int>>, int, VectorPairHash<int>> *node_ids,
-             unordered_map<string, int> *folder_ids) {
+             unordered_map<vector<pair<int, int>>, int, VectorPairHash<int>> *node_ids) {
         vector<pair<int, int>> id_pairs;
-        for (const auto& [subfolder, child] : node->leaves) {
-            if (!(*folder_ids).count(subfolder)) {
-                (*folder_ids)[subfolder] = size(*folder_ids);
-            }
-            id_pairs.emplace_back((*folder_ids)[subfolder], mark(child.get(), lookup, node_ids, folder_ids));
+        for (const auto& [subfolder_id, child] : node->leaves) {
+            id_pairs.emplace_back(subfolder_id, mark(child.get(), lookup, node_ids));
         }
         sort(begin(id_pairs), end(id_pairs));
         if (!node_ids->count(id_pairs)) {
@@ -87,16 +91,23 @@ private:
         return node_id;
     }
 
-    void sweep(TrieNode *node, vector<string> *path, vector<vector<string>> *result) {
+    void sweep(TrieNode *node,
+               const unordered_map<int, string>& names,
+               vector<int> *path,
+               vector<vector<string>> *result) {
         if (!empty(*path)) {
-            result->emplace_back(*path);
+            result->emplace_back();
+            transform(cbegin(*path), cend(*path), back_inserter(result->back()),
+                      [&names](const auto& i) {
+                          return names.at(i);
+                      });
         }
-        for (const auto& [subfolder, child] : node->leaves) {
+        for (const auto& [subfolder_id, child] : node->leaves) {
             if (child->deleted) {
                 continue;
             }
-            path->emplace_back(subfolder);
-            sweep(child.get(), path, result);
+            path->emplace_back(subfolder_id);
+            sweep(child.get(), names, path, result);
             path->pop_back();
         }
     }
