210a03ef0daf763530c89c0bc58549d3b842e9d3
kamyu <kamyu104@users.noreply.github.com>
2021-07-25 19:28:04 +0800

    Create number-of-visible-people-in-a-queue.py

---
+++ Python/number-of-visible-people-in-a-queue.py
@@ -0,0 +1,38 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def canSeePersonsCount(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: List[int]
+        """
+        result = [0]*len(heights)
+        stk = []
+        for i, h in enumerate(heights):
+            while stk and heights[stk[-1]] <= h:
+                result[stk.pop()] += 1
+            if stk:
+                result[stk[-1]] += 1
+            stk.append(i)
+        return result
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def canSeePersonsCount(self, heights):
+        """
+        :type heights: List[int]
+        :rtype: List[int]
+        """
+        result = [0]*len(heights)
+        stk = []
+        for i in reversed(xrange(len(heights))):
+            cnt = 0
+            while stk and heights[stk[-1]] < heights[i]:
+                stk.pop()
+                cnt += 1
+            result[i] = cnt+1 if stk else cnt
+            stk.append(i)
+        return result
