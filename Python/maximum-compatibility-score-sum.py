f5bb3af32bea4dde3a3a56a5a03c7212d196ae8a
kamyu <kamyu104@users.noreply.github.com>
2021-07-26 08:35:47 +0800

    Update maximum-compatibility-score-sum.py

--- Python/maximum-compatibility-score-sum.py
+++ Python/maximum-compatibility-score-sum.py
@@ -4,49 +4,7 @@
 import itertools
 
 
-# Template translated from:
-# https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/WeightedMatching.h
-# Time:  O(N^2 * M)
-# Space: O(N + M)
-def hungarian(a):
-    if not a:
-        return 0, []
-    n, m = len(a)+1, len(a[0])+1
-    u, v, p, ans = [0]*n, [0]*m, [0]*m, [0]*(n-1)
-    for i in xrange(1, n):
-        p[0] = i
-        j0 = 0  # add "dummy" worker 0
-        dist, pre = [float("inf")]*m, [-1]*m
-        done = [False]*(m+1)
-        while True:  # dijkstra
-            done[j0] = True
-            i0, j1, delta = p[j0], None, float("inf")
-            for j in xrange(1, m):
-                if done[j]:
-                    continue
-                cur = a[i0-1][j-1]-u[i0]-v[j]
-                if cur < dist[j]:
-                    dist[j], pre[j] = cur, j0
-                if dist[j] < delta:
-                    delta, j1 = dist[j], j
-            for j in xrange(m):
-                if done[j]:
-                    u[p[j]] += delta
-                    v[j] -= delta
-                else:
-                    dist[j] -= delta
-            j0 = j1
-            if not p[j0]:
-                break
-        while j0:  # update alternating path
-            j1 = pre[j0]
-            p[j0], j0 = p[j1], j1
-    for j in xrange(1, m):
-        if p[j]:
-            ans[p[j]-1] = j-1
-    return -v[0], ans  # min cost
-
-
+# weighted bipartite matching solution
 class Solution(object):
     def maxCompatibilitySum(self, students, mentors):
         """
@@ -54,6 +12,46 @@ class Solution(object):
         :type mentors: List[List[int]]
         :rtype: int
         """
+        # Template translated from:
+        # https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/WeightedMatching.h
+        def hungarian(a):  # Time: O(n^2 * m), Space: O(n + m)
+            if not a:
+                return 0, []
+            n, m = len(a)+1, len(a[0])+1
+            u, v, p, ans = [0]*n, [0]*m, [0]*m, [0]*(n-1)
+            for i in xrange(1, n):
+                p[0] = i
+                j0 = 0  # add "dummy" worker 0
+                dist, pre = [float("inf")]*m, [-1]*m
+                done = [False]*(m+1)
+                while True:  # dijkstra
+                    done[j0] = True
+                    i0, j1, delta = p[j0], None, float("inf")
+                    for j in xrange(1, m):
+                        if done[j]:
+                            continue
+                        cur = a[i0-1][j-1]-u[i0]-v[j]
+                        if cur < dist[j]:
+                            dist[j], pre[j] = cur, j0
+                        if dist[j] < delta:
+                            delta, j1 = dist[j], j
+                    for j in xrange(m):
+                        if done[j]:
+                            u[p[j]] += delta
+                            v[j] -= delta
+                        else:
+                            dist[j] -= delta
+                    j0 = j1
+                    if not p[j0]:
+                        break
+                while j0:  # update alternating path
+                    j1 = pre[j0]
+                    p[j0], j0 = p[j1], j1
+            for j in xrange(1, m):
+                if p[j]:
+                    ans[p[j]-1] = j-1
+            return -v[0], ans  # min cost
+
         def score(s, m):
             return sum(int(a == b) for a, b in itertools.izip(s, m))
 
