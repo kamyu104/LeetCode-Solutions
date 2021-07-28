6d8579ba0c52d05f2070cd46fd19ba880402499d
kamyu <kamyu104@users.noreply.github.com>
2021-07-28 19:11:09 +0800

    Update strong-friendship.sql

--- MySQL/strong-friendship.sql
+++ MySQL/strong-friendship.sql
@@ -8,7 +8,7 @@ WITH friendship_cte AS
     SELECT user2_id AS user1_id, user1_id AS user2_id FROM Friendship
 )
 
-SELECT a.user1_id, a.user2_id, COUNT(b.user2_id) AS common_friend
+SELECT a.user1_id, a.user2_id, COUNT(*) AS common_friend
 FROM Friendship AS a
 INNER JOIN friendship_cte AS b
 ON b.user1_id = a.user2_id
@@ -16,5 +16,5 @@ WHERE EXISTS (SELECT 1 FROM friendship_cte AS c
               WHERE c.user1_id = a.user1_id
               AND c.user2_id = b.user2_id)
 GROUP BY a.user1_id, a.user2_id
-HAVING COUNT(b.user2_id) >= 3
+HAVING common_friend >= 3
 ORDER BY NULL;
