bfda940aa08b55af00b68f791b8a2ebce6d21628
kamyu <kamyu104@users.noreply.github.com>
2021-07-31 07:19:18 +0800

    Update restaurant-growth.sql

--- MySQL/restaurant-growth.sql
+++ MySQL/restaurant-growth.sql
@@ -1,18 +1,24 @@
 # Time:  O(n^2)
 # Space: O(n)
 
-SELECT a.visited_on             AS visited_on, 
-       Sum(b.day_sum)           AS amount, 
-       Round(Avg(b.day_sum), 2) AS average_amount 
-FROM   (SELECT visited_on, 
-               Sum(amount) AS day_sum 
-        FROM   customer 
-        GROUP  BY visited_on) a 
-       LEFT JOIN (SELECT visited_on, 
-                         Sum(amount) AS day_sum 
-                  FROM   customer 
-                  GROUP  BY visited_on) b 
-              ON Datediff(a.visited_on, b.visited_on) BETWEEN 0 AND 6 
+SELECT a.visited_on                AS visited_on, 
+       SUM(b.day_sum)              AS amount, 
+       ROUND(AVG(b.day_sum), 2)    AS average_amount 
+FROM
+(
+    SELECT visited_on, SUM(amount) AS day_sum 
+    FROM   customer 
+    GROUP  BY visited_on
+    ORDER  BY NULL
+) a 
+LEFT JOIN
+(
+    SELECT visited_on, SUM(amount) AS day_sum 
+    FROM   customer 
+    GROUP  BY visited_on
+    ORDER  BY NULL
+) b 
+ON DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6 
 GROUP  BY a.visited_on 
 HAVING Count(b.visited_on) = 7 
-
+ORDER  BY NULL;
