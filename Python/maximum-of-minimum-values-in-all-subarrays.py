ebac655b45d5f1777378ab9f2e6f70c618e43680
kamyu <kamyu104@users.noreply.github.com>
2021-07-30 21:56:28 +0800

    Create maximum-of-minimum-values-in-all-subarrays.py

---
+++ Python/maximum-of-minimum-values-in-all-subarrays.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def findMaximums(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def find_bound(nums, direction, init):
+            result = [0]*len(nums)
+            stk = [init]
+            for i in direction(xrange(len(nums))):
+                while stk[-1] not in (-1, len(nums)) and nums[stk[-1]] >= nums[i]:
+                    stk.pop()
+                result[i] = stk[-1]
+                stk.append(i)
+            return result
+
+        left = find_bound(nums, lambda x: x, -1)
+        right = find_bound(nums, reversed, len(nums))
+        result = [-1]*len(nums)
+        for i, v in enumerate(nums):
+            result[((right[i]-1)-left[i])-1] = max(result[((right[i]-1)-left[i])-1], v)
+        for i in reversed(xrange(len(nums)-1)):
+            result[i] = max(result[i], result[i+1])
+        return result
