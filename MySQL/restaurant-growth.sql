767bfd39d0bf8ca377068269d969ca7bb7ab8214
kamyu <kamyu104@users.noreply.github.com>
2021-07-31 07:51:05 +0800

    Update restaurant-growth.sql

--- MySQL/restaurant-growth.sql
+++ MySQL/restaurant-growth.sql
@@ -1,13 +1,17 @@
 # Time:  O(nlogn)
 # Space: O(n)
 
+WITH total_cte AS
+(
+  SELECT visited_on, 
+         SUM(amount)  OVER (ORDER BY visited_on
+                            RANGE BETWEEN INTERVAL 6 DAY PRECEDING
+                            AND CURRENT ROW) as amount,
+         DENSE_RANK() OVER (ORDER BY visited_on) as rnk
+  FROM Customer
+)
 SELECT DISTINCT visited_on, amount, ROUND(amount/7, 2) AS average_amount
-FROM (SELECT visited_on, 
-             SUM(amount)  OVER (ORDER BY visited_on
-                                RANGE BETWEEN INTERVAL 6 DAY PRECEDING
-                                AND CURRENT ROW) as amount,
-             DENSE_RANK() OVER (ORDER BY visited_on) as rnk
-      FROM Customer) AS tmp
+FROM total_cte
 WHERE rnk >= 7;
 
 # Time:  O(n^2)
