d0e950e260aabc7b83174433e529c66e0cc7f8d9
kamyu <kamyu104@users.noreply.github.com>
2021-08-02 01:52:12 +0800

    Create two-sum.rb

---
+++ Ruby/two-sum.rb
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(n)
+
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Integer[]}
+def two_sum(nums, target)
+    lookup = {}
+    nums.each_with_index do |x, i|
+        if lookup.has_key?(target - x)
+            return [lookup[target - x], i] 
+        end
+        lookup[nums[i]] = i
+    end
+    return []
+end
