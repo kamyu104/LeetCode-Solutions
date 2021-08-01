090bc0acac1d7f012e3a93c593e62701fc6cf08f
kamyu <kamyu104@users.noreply.github.com>
2021-08-02 01:32:57 +0800

    Update two-sum.ts

--- TypeScript/two-sum.ts
+++ TypeScript/two-sum.ts
@@ -3,7 +3,7 @@
 
 function twoSum(nums: number[], target: number): number[] {
     let lookup = new Map();
-    for (let i = 0; i < nums.length; i += 1) {
+    for (let i = 0; i < nums.length; i++) {
         if (lookup.has(target - nums[i])) {
             return [lookup.get(target - nums[i]), i];
         }
