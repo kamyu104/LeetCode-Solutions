953d39d3d0b725a0359a4d80d79d136dfd0fbb0a
kamyu <kamyu104@users.noreply.github.com>
2021-08-02 01:30:52 +0800

    Create two-sum.ts

---
+++ TypeScript/two-sum.ts
@@ -0,0 +1,12 @@
+// Time:  O(n)
+// Space: O(1)
+
+function twoSum(nums: number[], target: number): number[] {
+    let lookup = new Map();
+    for (let i = 0; i < nums.length; i += 1) {
+        if (lookup.has(target - nums[i])) {
+            return [lookup.get(target - nums[i]), i];
+        };
+        lookup.set(nums[i], i);
+    }
+};
