7541115932bc22aeafc9772133926283eff6e714
kamyu <kamyu104@users.noreply.github.com>
2021-08-08 23:44:21 +0800

    Create remove-stones-to-minimize-the-total.py

---
+++ Python/remove-stones-to-minimize-the-total.py
@@ -0,0 +1,19 @@
+# Time:  O(klogn)
+# Space: O(1)
+
+import heapq
+
+
+class Solution(object):
+    def minStoneSum(self, piles, k):
+        """
+        :type piles: List[int]
+        :type k: int
+        :rtype: int
+        """
+        for i, x in enumerate(piles):
+            piles[i] = -x
+        heapq.heapify(piles)
+        for i in xrange(k):
+            heapq.heappush(piles, heapq.heappop(piles)//2)
+        return -sum(piles)
