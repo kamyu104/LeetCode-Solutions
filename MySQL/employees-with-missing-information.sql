bc9761243471a26748b0b0270e199e20324e3fae
kamyu <kamyu104@users.noreply.github.com>
2021-08-09 01:36:00 +0800

    Update employees-with-missing-information.sql

--- MySQL/employees-with-missing-information.sql
+++ MySQL/employees-with-missing-information.sql
@@ -1,6 +1,21 @@
 # Time:  O(nlogn)
 # Space: O(n)
 
+WITH all_employee_info_cte AS
+(
+    SELECT employee_id FROM Employees
+    UNION ALL
+    SELECT employee_id FROM Salaries
+)
+
+SELECT employee_id
+FROM all_employee_info_cte
+GROUP BY employee_id
+HAVING COUNT(*) != 2
+ORDER BY 1;
+
+# Time:  O(nlogn)
+# Space: O(n)
 WITH all_employee_id_cte AS
 (
     SELECT employee_id FROM Employees
@@ -19,7 +34,6 @@ SELECT employee_id
 FROM all_employee_id_cte a
 WHERE NOT EXISTS (SELECT 1 FROM complete_employee_id_cte b WHERE a.employee_id = b.employee_id)
 ORDER BY 1 ASC;
-
 
 # Time:  O(nlogn)
 # Space: O(n)
