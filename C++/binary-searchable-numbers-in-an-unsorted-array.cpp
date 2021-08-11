cc4698646155a2755d87515a1dc38c224f0fa1de
kamyu <kamyu104@users.noreply.github.com>
2021-08-12 01:13:36 +0800

    Update binary-searchable-numbers-in-an-unsorted-array.cpp

--- C++/binary-searchable-numbers-in-an-unsorted-array.cpp
+++ C++/binary-searchable-numbers-in-an-unsorted-array.cpp
@@ -4,7 +4,6 @@
 class Solution {
 public:
     int binarySearchableNumbers(vector<int>& nums) {
-        static const bool count_distinct = true;
         vector<int> right(size(nums) + 1, numeric_limits<int>::max());
         for (int i = size(nums); i >= 1; --i) {
             right[i - 1] = min(right[i], nums[i - 1]);
@@ -18,9 +17,6 @@ public:
             }
             left = max(left, nums[i]);
             
-        }
-        if (count_distinct) {
-            return size(result);
         }
         int cnt = 0;
         for (const auto& x : nums) {
