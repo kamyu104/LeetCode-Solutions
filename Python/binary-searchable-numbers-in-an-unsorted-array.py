646743f68543519369b73630d445b3157d76c721
kamyu <kamyu104@users.noreply.github.com>
2021-08-12 00:11:52 +0800

    Update binary-searchable-numbers-in-an-unsorted-array.py

--- Python/binary-searchable-numbers-in-an-unsorted-array.py
+++ Python/binary-searchable-numbers-in-an-unsorted-array.py
@@ -7,14 +7,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left = [float("-inf")]*(len(nums)+1)
-        for i in xrange(len(nums)):
-            left[i+1] = max(left[i], nums[i])
         right = [float("inf")]*(len(nums)+1)
         for i in reversed(xrange(1, len(nums)+1)):
             right[i-1] = min(right[i], nums[i-1])
-        result = 0
+        result, left = 0, float("-inf")
         for i in xrange(len(nums)):
-            if left[i] <= nums[i] <= right[i+1]:
+            if left <= nums[i] <= right[i+1]:
                 result += 1
+            left = max(left, nums[i])
         return result
