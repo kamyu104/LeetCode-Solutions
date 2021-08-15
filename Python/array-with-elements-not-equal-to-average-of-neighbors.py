039ebf0b3d999bdce6fb389648a329806ec306d4
kamyu <kamyu104@users.noreply.github.com>
2021-08-15 16:49:18 +0800

    Update array-with-elements-not-equal-to-average-of-neighbors.py

--- Python/array-with-elements-not-equal-to-average-of-neighbors.py
+++ Python/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -67,6 +67,6 @@ class Solution2(object):
         :rtype: List[int]
         """
         nums.sort()
-        med = (len(nums)-1)//2
-        nums[::2], nums[1::2] = nums[med::-1], nums[:med:-1]
+        mid = (len(nums)-1)//2
+        nums[::2], nums[1::2] = nums[mid::-1], nums[:mid:-1]
         return nums
