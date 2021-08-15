25d3414fb10bb82e33516fde05363659728a5ead
kamyu <kamyu104@users.noreply.github.com>
2021-08-15 16:47:47 +0800

    Create array-with-elements-not-equal-to-average-of-neighbors.py

---
+++ Python/array-with-elements-not-equal-to-average-of-neighbors.py
@@ -0,0 +1,72 @@
+# Time:  O(n) ~ O(n^2), O(n) on average
+# Space: O(1)
+
+# Tri Partition (aka Dutch National Flag Problem) with virtual index solution
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def nth_element(nums, n, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+
+            left, right = 0, len(nums)-1
+            while left <= right:
+                pivot_idx = randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+
+        def reversedTriPartitionWithVI(nums, val):
+            def idx(i, N):
+                return (1 + 2 * (i)) % N
+
+            N = len(nums)//2 * 2 + 1
+            i, j, n = 0, 0, len(nums) - 1
+            while j <= n:
+                if nums[idx(j, N)] > val:
+                    nums[idx(i, N)], nums[idx(j, N)] = nums[idx(j, N)], nums[idx(i, N)]
+                    i += 1
+                    j += 1
+                elif nums[idx(j, N)] < val:
+                    nums[idx(j, N)], nums[idx(n, N)] = nums[idx(n, N)], nums[idx(j, N)]
+                    n -= 1
+                else:
+                    j += 1
+
+        mid = (len(nums)-1)//2
+        nth_element(nums, mid)
+        reversedTriPartitionWithVI(nums, nums[mid])
+        return nums
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# Sorting and reorder solution
+class Solution2(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        med = (len(nums)-1)//2
+        nums[::2], nums[1::2] = nums[med::-1], nums[:med:-1]
+        return nums
