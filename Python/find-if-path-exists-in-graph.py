8fe7b6f97930b653a3c40e3f89c8fbeb2017254c
kamyu <kamyu104@users.noreply.github.com>
2021-08-17 01:57:19 +0800

    Update find-if-path-exists-in-graph.py

--- Python/find-if-path-exists-in-graph.py
+++ Python/find-if-path-exists-in-graph.py
@@ -77,3 +77,36 @@ class Solution2(object):
             adj[u].append(v)
             adj[v].append(u)
         return bfs(adj, start, end) >= 0
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|V| + |E|)
+# dfs solution
+class Solution3(object):
+    def validPath(self, n, edges, start, end):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        def dfs(adj, start, target):
+            stk = [start]
+            lookup = set(stk)
+            while stk:
+                pos = stk.pop()
+                if pos == target:
+                    return True
+                for nei in reversed(adj[pos]):
+                    if nei in lookup:
+                        continue
+                    lookup.add(nei)
+                    stk.append(nei)
+            return False 
+
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        return dfs(adj, start, end)
