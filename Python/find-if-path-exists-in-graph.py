627642c5d8b6410da321f8a523672d89d8a49391
kamyu <kamyu104@users.noreply.github.com>
2021-08-16 21:30:44 +0800

    Create find-if-path-exists-in-graph.py

---
+++ Python/find-if-path-exists-in-graph.py
@@ -0,0 +1,79 @@
+# Time:  O(|V| + |E|)
+# Space: O(|V| + |E|)
+
+import collections
+
+
+# bi-bfs solution
+class Solution(object):
+    def validPath(self, n, edges, start, end):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        def bi_bfs(adj, start, target):
+            lookup = set()
+            left, right = set([start]), set([target])
+            steps = 0
+            while left:
+                for pos in left:
+                    lookup.add(pos)
+                new_left = set()
+                for pos in left:
+                    if pos in right: 
+                        return steps
+                    for nei in adj[pos]:
+                        if nei in lookup:
+                            continue
+                        new_left.add(nei)
+                left = new_left
+                steps += 1
+                if len(left) > len(right): 
+                    left, right = right, left
+            return -1
+
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        return bi_bfs(adj, start, end) >= 0
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|V| + |E|)
+# bfs solution
+class Solution2(object):
+    def validPath(self, n, edges, start, end):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        def bfs(adj, start, target):
+            q = [start]
+            lookup = set(q)
+            steps = 0
+            while q:
+                new_q = []
+                for pos in q:
+                    if pos == target:
+                        return steps
+                    for nei in adj[pos]:
+                        if nei in lookup:
+                            continue
+                        lookup.add(nei)
+                        new_q.append(nei)
+                q = new_q
+                steps += 1
+            return -1  
+
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        return bfs(adj, start, end) >= 0
