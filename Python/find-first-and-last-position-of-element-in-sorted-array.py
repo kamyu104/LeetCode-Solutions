051f7d72fd87343a973c5fdfd42f031892294494
kamyu <kamyu104@users.noreply.github.com>
2021-08-18 02:53:46 +0800

    Update find-first-and-last-position-of-element-in-sorted-array.py

--- Python/find-first-and-last-position-of-element-in-sorted-array.py
+++ Python/find-first-and-last-position-of-element-in-sorted-array.py
@@ -9,39 +9,39 @@ class Solution(object):
         :rtype: List[int]
         """
         # Find the first idx where nums[idx] >= target
-        left = self.binarySearch(lambda x, y: x >= y, nums, target)
-        if left >= len(nums) or nums[left] != target:
+        left = self.binarySearch(len(nums), lambda i: nums[i] >= target)
+        if left == len(nums) or nums[left] != target:
             return [-1, -1]
         # Find the first idx where nums[idx] > target
-        right = self.binarySearch(lambda x, y: x > y, nums, target)
+        right = self.binarySearch(len(nums), lambda i: nums[i] > target)
         return [left, right - 1]
 
-    def binarySearch(self, compare, nums, target):
-        left, right = 0, len(nums)
+    def binarySearch(self, n, check):
+        left, right = 0, n
         while left < right:
-            mid = left + (right - left) / 2
-            if compare(nums[mid], target):
+            mid = left + (right-left)//2
+            if check(mid):
                 right = mid
             else:
-                left = mid + 1
+                left = mid+1
         return left
 
-    def binarySearch2(self, compare, nums, target):
-        left, right = 0, len(nums) - 1
+    def binarySearch2(self, n, check):
+        left, right = 0, n-1
         while left <= right:
-            mid = left + (right - left) / 2
-            if compare(nums[mid], target):
-                right = mid - 1
+            mid = left + (right-left)//2
+            if check(mid):
+                right = mid-1
             else:
-                left = mid + 1
+                left = mid+1
         return left
 
-    def binarySearch3(self, compare, nums, target):
-        left, right = -1, len(nums)
+    def binarySearch3(self, n, check):
+        left, right = -1, n
         while right - left >= 2:
-            mid = left + (right - left) / 2
-            if compare(nums[mid], target):
+            mid = left + (right-left)//2
+            if check(mid):
                 right = mid
             else:
                 left = mid
-        return right
+        return right  # left+1
