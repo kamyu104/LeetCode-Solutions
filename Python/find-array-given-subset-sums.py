cb747b7664fc7e4524ac76046e213ea9b72c647a
kamyu <kamyu104@users.noreply.github.com>
2021-08-24 03:01:00 +0800

    Create find-array-given-subset-sums.py

---
+++ Python/find-array-given-subset-sums.py
@@ -0,0 +1,84 @@
+# Time:  O(n * 2^n)
+# Space: O(2^n)
+
+import collections
+import operator
+
+
+# optimized from solution2
+class Solution(object):
+    def recoverArray(self, n, sums):
+        """
+        :type n: int
+        :type sums: List[int]
+        :rtype: List[int]
+        """
+        dp = {k: v for k, v in collections.Counter(sums).iteritems()}
+        total = reduce(operator.ior, dp.itervalues(), 0)
+        basis = total&-total  # find rightmost bit 1
+        if basis > 1:
+            for k in dp:
+                dp[k] //= basis
+        result = [0]*(basis.bit_length()-1)
+        sorted_nums = sorted(dp.iterkeys())  # Time: O(2^n * log(2^n)) = O(n * 2^n)
+        shift = 0
+        while len(result) < n-1:  # log(2^n) times, Total Time: O(2^n)
+            new_dp = {}
+            new_sorted_nums = []
+            new_shift = sorted_nums[0]-sorted_nums[1]
+            assert(new_shift < 0)
+            for x in sorted_nums:
+                if not dp[x]:
+                    continue
+                dp[x-new_shift] -= dp[x]
+                new_dp[x-new_shift] = dp[x]
+                new_sorted_nums.append(x-new_shift)
+            dp = new_dp
+            sorted_nums = new_sorted_nums
+            if shift in new_dp:
+                result.append(new_shift)
+            else:
+                result.append(-new_shift)
+                shift -= new_shift
+        if len(result) == n-1:
+            result.append(sum(dp) - 2*shift)
+        return result
+
+
+# Time:  O(n * 2^n)
+# Space: O(2^n)
+import collections
+
+
+class Solution2(object):
+    def recoverArray(self, n, sums):
+        """
+        :type n: int
+        :type sums: List[int]
+        :rtype: List[int]
+        """
+        dp = {k: v for k, v in collections.Counter(sums).iteritems()}
+        result = []
+        sorted_nums = sorted(dp.iterkeys())  # Time: O(2^n * log(2^n)) = O(n * 2^n)
+        shift = 0
+        while len(result) < n-1:  # log(2^n) times, Total Time: O(2^n)
+            new_dp = {}
+            new_sorted_nums = []
+            new_shift = sorted_nums[0]-sorted_nums[1] if dp[sorted_nums[0]] == 1 else 0
+            assert(new_shift <= 0)
+            for x in sorted_nums:
+                if not dp[x]:
+                    continue
+                dp[x-new_shift] -= dp[x] if new_shift else dp[x]//2
+                new_dp[x-new_shift] = dp[x]
+                new_sorted_nums.append(x-new_shift)
+            dp = new_dp
+            sorted_nums = new_sorted_nums
+            if shift in new_dp:
+                result.append(new_shift)
+            else:
+                result.append(-new_shift)
+                shift -= new_shift
+        if len(result) == n-1:
+            result.append(sum(dp) - 2*shift)
+        return result
