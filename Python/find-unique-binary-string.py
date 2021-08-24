102dc4435265f55d4214d2c9e66ebb197dd1e0b4
kamyu <kamyu104@users.noreply.github.com>
2021-08-24 15:08:40 +0800

    Update find-unique-binary-string.py

--- Python/find-unique-binary-string.py
+++ Python/find-unique-binary-string.py
@@ -8,3 +8,27 @@ class Solution(object):
         :rtype: str
         """
         return "".join("01"[nums[i][i] == '0'] for i in xrange(len(nums)))
+
+
+# Time:  O(k * n) = O(n^2)
+# Space: O(k) = O(n)
+class Solution2(object):
+    def findDifferentBinaryString(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: str
+        """
+        lookup = set(map(lambda x: int(x, 2), nums))  # Time: O(k * n) = O(n^2)
+        return next(bin(i)[2:].zfill(len(nums[0])) for i in xrange(2**len(nums[0])) if i not in lookup)  # Time: O(k) = O(n)
+
+
+# Time:  O(k * n + n * 2^n) = O(n * 2^n)
+# Space: O(k) = O(n) ~ O(2^n)
+class Solution_Extra(object):
+    def findAllDifferentBinaryStrings(self, nums):
+        """
+        :type nums: List[str]
+        :rtype: List[str]
+        """
+        lookup = set(map(lambda x: int(x, 2), nums))  # Time: O(k * n + n * 2^n) = O(n * 2^n)
+        return [bin(i)[2:].zfill(len(nums[0])) for i in xrange(2**len(nums[0])) if i not in lookup]  # Time: O(2^n + n * (2^n - k))
