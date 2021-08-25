e3fd994d4512906ac97117df45e1193da02ef0d9
kamyu <kamyu104@users.noreply.github.com>
2021-08-25 10:26:14 +0800

    Update find-array-given-subset-sums.cpp

--- C++/find-array-given-subset-sums.cpp
+++ C++/find-array-given-subset-sums.cpp
@@ -64,29 +64,29 @@ public:
         for (const auto& sum : sums) {
             ++dp[sum - min_sum];
         }
-        vector<int> sorted_nums;
+        vector<int> sorted_sums;
         for (int x = min_sum; x <= max_sum; ++x) {
             if (dp[x - min_sum]) {
-                sorted_nums.emplace_back(x);
+                sorted_sums.emplace_back(x);
             }
         }
         int shift = 0;
         vector<int> result;
         while (n--) { // log(2^n) times, each time costs O(2^(n-len(result)))+O(r), Total Time: O(2^n + n * r)
             vector<int> new_dp(max_sum - min_sum + 1);
-            vector<int> new_sorted_nums;
-            const int new_shift = (dp[sorted_nums[0] - min_sum] == 1) ? sorted_nums[0] - sorted_nums[1] : 0;
+            vector<int> new_sorted_sums;
+            const int new_shift = (dp[sorted_sums[0] - min_sum] == 1) ? sorted_sums[0] - sorted_sums[1] : 0;
             assert(new_shift <= 0);
-            for (const auto& x : sorted_nums) {
+            for (const auto& x : sorted_sums) {
                 if (!dp[x - min_sum]) {
                     continue;
                 }
                 dp[(x - new_shift) - min_sum] -= new_shift ? dp[x - min_sum] : dp[x - min_sum] / 2;
                 new_dp[(x - new_shift) - min_sum] = dp[x - min_sum];
-                new_sorted_nums.emplace_back(x - new_shift);
+                new_sorted_sums.emplace_back(x - new_shift);
             }
             dp = move(new_dp);
-            sorted_nums = move(new_sorted_nums);
+            sorted_sums = move(new_sorted_sums);
             if (dp[shift - min_sum]) {  // contain 0, choose this side
                 result.emplace_back(new_shift);
             } else {  // contain no 0, choose another side and shift 0 offset
@@ -108,11 +108,11 @@ public:
         for (const auto& sum : sums) {
             ++dp[sum];
         }
-        vector<int> sorted_nums;
+        vector<int> sorted_sums;
         for (const auto& [k, _] : dp) {
-            sorted_nums.emplace_back(k);
+            sorted_sums.emplace_back(k);
         }
-        sort(begin(sorted_nums), end(sorted_nums));  // Time: O(2^n * log(2^n)) = O(n * 2^n)
+        sort(begin(sorted_sums), end(sorted_sums));  // Time: O(2^n * log(2^n)) = O(n * 2^n)
         int shift = 0;
         uint32_t total = 0;
         for (const auto& [k, v] : dp) {
@@ -128,19 +128,19 @@ public:
         n -= bit_length(basis) - 1;
         while (n--) {  // log(2^n) times, each time costs O(2^(n-len(result))), Total Time: O(2^n)
             unordered_map<int, int> new_dp;
-            vector<int> new_sorted_nums;
-            const int new_shift = (dp[sorted_nums[0]] == 1) ? sorted_nums[0] - sorted_nums[1] : 0;
+            vector<int> new_sorted_sums;
+            const int new_shift = (dp[sorted_sums[0]] == 1) ? sorted_sums[0] - sorted_sums[1] : 0;
             assert(new_shift <= 0);
-            for (const auto& x : sorted_nums) {
+            for (const auto& x : sorted_sums) {
                 if (!dp[x]) {
                     continue;
                 }
                 dp[x - new_shift] -= new_shift ? dp[x] : dp[x] / 2;
                 new_dp[x - new_shift] = dp[x];
-                new_sorted_nums.emplace_back(x - new_shift);
+                new_sorted_sums.emplace_back(x - new_shift);
             }
             dp = move(new_dp);
-            sorted_nums = move(new_sorted_nums);
+            sorted_sums = move(new_sorted_sums);
             if (dp.count(shift)) {  // contain 0, choose this side
                 result.emplace_back(new_shift);
             } else {  // contain no 0, choose another side and shift 0 offset
@@ -167,28 +167,28 @@ public:
         for (const auto& sum : sums) {
             ++dp[sum];
         }
-        vector<int> sorted_nums;
+        vector<int> sorted_sums;
         for (const auto& [k, _] : dp) {
-            sorted_nums.emplace_back(k);
+            sorted_sums.emplace_back(k);
         }
-        sort(begin(sorted_nums), end(sorted_nums));  // Time: O(2^n * log(2^n)) = O(n * 2^n)
+        sort(begin(sorted_sums), end(sorted_sums));  // Time: O(2^n * log(2^n)) = O(n * 2^n)
         int shift = 0;
         vector<int> result;
         while (n--) {  // log(2^n) times, each time costs O(2^(n-len(result))), Total Time: O(2^n)
             unordered_map<int, int> new_dp;
-            vector<int> new_sorted_nums;
-            const int new_shift = (dp[sorted_nums[0]] == 1) ? sorted_nums[0] - sorted_nums[1] : 0;
+            vector<int> new_sorted_sums;
+            const int new_shift = (dp[sorted_sums[0]] == 1) ? sorted_sums[0] - sorted_sums[1] : 0;
             assert(new_shift <= 0);
-            for (const auto& x : sorted_nums) {
+            for (const auto& x : sorted_sums) {
                 if (!dp[x]) {
                     continue;
                 }
                 dp[x - new_shift] -= new_shift ? dp[x] : dp[x] / 2;
                 new_dp[x - new_shift] = dp[x];
-                new_sorted_nums.emplace_back(x - new_shift);
+                new_sorted_sums.emplace_back(x - new_shift);
             }
             dp = move(new_dp);
-            sorted_nums = move(new_sorted_nums);
+            sorted_sums = move(new_sorted_sums);
             if (dp.count(shift)) {  // contain 0, choose this side
                 result.emplace_back(new_shift);
             } else {  // contain no 0, choose another side and shift 0 offset
