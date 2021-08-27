9ec8b1f2efc47214a94fda0e67eea1aefc9b8363
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-08-28 01:14:42 +0530

    Update reverse-integer.cpp

--- C++/reverse-integer.cpp
+++ C++/reverse-integer.cpp
@@ -4,6 +4,29 @@
 class Solution {
 public:
     int reverse(int x) {
+        int result = 0;
+        while (x) {
+            if (result > numeric_limits<int>::max() / 10 ||
+                (result == numeric_limits<int>::max() / 10 && x % 10 > numeric_limits<int>::max() % 10)) {
+                return 0;
+            }
+            if (result < numeric_limits<int>::min() / 10 ||
+                (result == numeric_limits<int>::min() / 10 && x % 10 < numeric_limits<int>::min() % 10)) {
+                return 0;
+            }
+            result *= 10;
+            result += x % 10;
+            x /= 10;
+        }
+        return result;
+    }
+};
+
+
+//Time : O(1)
+class Solution2 {
+public:
+    int reverse(int x) {
         long long ans = 0;
         
         if(x/10 == 0)
