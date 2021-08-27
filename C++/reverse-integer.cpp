86f62dc8b20031c87317f5a820f2dd42bf05c3bf
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2021-08-27 20:26:11 +0530

    Fastest solution
    Executes in 0ms time, uses 5.9MB of storage

--- C++/reverse-integer.cpp
+++ C++/reverse-integer.cpp
@@ -4,20 +4,16 @@
 class Solution {
 public:
     int reverse(int x) {
-        int result = 0;
-        while (x) {
-            if (result > numeric_limits<int>::max() / 10 ||
-                (result == numeric_limits<int>::max() / 10 && x % 10 > numeric_limits<int>::max() % 10)) {
-                return 0;
-            }
-            if (result < numeric_limits<int>::min() / 10 ||
-                (result == numeric_limits<int>::min() / 10 && x % 10 < numeric_limits<int>::min() % 10)) {
-                return 0;
-            }
-            result *= 10;
-            result += x % 10;
-            x /= 10;
+        long long ans = 0;
+        
+        if(x/10 == 0)
+            return x;
+        
+        while(x){
+            ans = ans*10 + x%10;
+            x = x/10;
         }
-        return result;
+        
+        return (ans > INT_MAX || ans < INT_MIN) ? 0 : ans;
     }
 };
