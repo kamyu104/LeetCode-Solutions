90beefebc2ab29d6a1e04aa8f309ac915023c236
kamyu <kamyu104@users.noreply.github.com>
2021-08-30 18:10:08 +0800

    Update minimum-number-of-work-sessions-to-finish-the-tasks.cpp

--- C++/minimum-number-of-work-sessions-to-finish-the-tasks.cpp
+++ C++/minimum-number-of-work-sessions-to-finish-the-tasks.cpp
@@ -26,3 +26,30 @@ public:
         return dp.back().first;
     }
 };
+
+// Time:  O(n * 2^n)
+// Space: O(2^n)
+
+class Solution2 {
+public:
+    int minSessions(vector<int>& tasks, int sessionTime) {
+        // dp[mask]: min used time by choosing tasks in mask bitset
+        vector<int> dp(1 << size(tasks), numeric_limits<int>::max());
+        dp[0] = 0;
+        for (int mask = 0; mask < size(dp) - 1; ++mask) {
+            int basis = 1;
+            for (auto task : tasks) {
+                const int new_mask = mask | basis;
+                basis <<= 1;
+                if (new_mask == mask) {
+                    continue;
+                }
+                if (dp[mask] % sessionTime + task > sessionTime) {
+                    task += sessionTime - dp[mask] % sessionTime;  // take a break
+                }
+                dp[new_mask] = min(dp[new_mask], dp[mask] + task);
+            }
+        }
+        return (dp.back() + sessionTime - 1) / sessionTime;
+    }
+};
