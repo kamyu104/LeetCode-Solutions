19fed22e6422081caef7fc1264c4a4a16cb5825f
kamyu <kamyu104@users.noreply.github.com>
2021-08-30 18:06:45 +0800

    Update minimum-number-of-work-sessions-to-finish-the-tasks.py

--- Python/minimum-number-of-work-sessions-to-finish-the-tasks.py
+++ Python/minimum-number-of-work-sessions-to-finish-the-tasks.py
@@ -24,3 +24,28 @@ class Solution(object):
                 else:
                     dp[new_mask] = min(dp[new_mask], [dp[mask][0]+1, task])
         return dp[-1][0]
+
+
+# Time:  O(n * 2^n)
+# Space: O(2^n)
+class Solution2(object):
+    def minSessions(self, tasks, sessionTime):
+        """
+        :type tasks: List[int]
+        :type sessionTime: int
+        :rtype: int
+        """
+        # dp[mask]: min used time by choosing tasks in mask bitset
+        dp = [float("inf") for _ in xrange(1<<len(tasks))]
+        dp[0] = 0
+        for mask in xrange(len(dp)-1):
+            basis = 1
+            for task in tasks:
+                new_mask = mask|basis
+                basis <<= 1
+                if new_mask == mask:
+                    continue
+                if dp[mask]%sessionTime + task > sessionTime:
+                    task += sessionTime-dp[mask]%sessionTime  # take a break
+                dp[new_mask] = min(dp[new_mask], dp[mask]+task)
+        return (dp[-1]+sessionTime-1)//sessionTime
