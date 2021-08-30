7e28754c29c88bc222da8f1988d2aad8f7ef192a
kamyu <kamyu104@users.noreply.github.com>
2021-08-30 21:29:41 +0800

    Update number-of-unique-good-subsequences.py

--- Python/number-of-unique-good-subsequences.py
+++ Python/number-of-unique-good-subsequences.py
@@ -12,8 +12,8 @@ class Solution(object):
         has_zero = False
         for b in binary:
             if b == '1':
-                ends1 = (ends1+ends0+1)%MOD  # add extra ends0 new distinct subsequences and add one "1"*count_of_curr_1s
+                ends1 = (ends1+ends0+1)%MOD  # add extra ends0 new distinct subsequences by a mapping rule and add one "1"*count_of_curr_1s
             else:
-                ends0 = (ends0+ends1)%MOD  # add extra ends1 new distinct subsequences and don't add one "0"*count_of_curr_0s
+                ends0 = (ends0+ends1)%MOD  # add extra ends1 new distinct subsequences by a mapping rule and don't add one "0"*count_of_curr_0s
                 has_zero = True
         return (ends0+ends1+int(has_zero))%MOD  # add "0" if has_zero
