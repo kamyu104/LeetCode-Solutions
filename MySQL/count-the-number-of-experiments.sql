16e9c529ce13fee88d06eb9ea59c33b6b42ddce8
kamyu <kamyu104@users.noreply.github.com>
2021-09-05 18:17:18 +0800

    Update count-the-number-of-experiments.sql

--- MySQL/count-the-number-of-experiments.sql
+++ MySQL/count-the-number-of-experiments.sql
@@ -2,28 +2,30 @@
 # Space: O(n)
 
 WITH platforms_cte AS (
-  SELECT 'IOS' AS platform
-  UNION ALL
-  SELECT 'Android'
-  UNION ALL
-  SELECT 'Web'
-), experiment_names_cte AS (
-  SELECT 'Programming' AS experiment_name
-  UNION ALL
-  SELECT 'Sports'
-  UNION ALL
-  SELECT 'Reading'
-), platforms_and_experiments_cte AS (
-  SELECT * FROM platforms_cte CROSS JOIN experiment_names_cte
+    SELECT 'IOS' AS platform
+    UNION ALL
+    SELECT 'Android'
+    UNION ALL
+    SELECT 'Web'
+),
+experiment_names_cte AS (
+    SELECT 'Programming' AS experiment_name
+    UNION ALL
+    SELECT 'Sports'
+    UNION ALL
+    SELECT 'Reading'
+),
+platforms_and_experiments_cte AS (
+    SELECT * FROM platforms_cte CROSS JOIN experiment_names_cte
 )
 
 SELECT
-  a.platform,
-  a.experiment_name,
-  COUNT(b.platform) AS num_experiments
+    a.platform,
+    a.experiment_name,
+    COUNT(b.platform) AS num_experiments
 FROM
-  platforms_and_experiments_cte a
-  LEFT JOIN Experiments b
-  ON a.platform = b.platform AND a.experiment_name = b.experiment_name
+    platforms_and_experiments_cte a
+    LEFT JOIN Experiments b
+    ON a.platform = b.platform AND a.experiment_name = b.experiment_name
 GROUP BY a.platform, a.experiment_name
 ORDER BY NULL;
