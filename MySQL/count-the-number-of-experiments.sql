cbffd9e462d362dcc48abb9c3bf1d2c922561bac
kamyu <kamyu104@users.noreply.github.com>
2021-09-05 18:13:18 +0800

    Create count-the-number-of-experiments.sql

---
+++ MySQL/count-the-number-of-experiments.sql
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(n)
+
+WITH platforms_cte AS (
+  SELECT 'IOS' AS platform
+  UNION ALL
+  SELECT 'Android'
+  UNION ALL
+  SELECT 'Web'
+), experiment_names_cte AS (
+  SELECT 'Programming' AS experiment_name
+  UNION ALL
+  SELECT 'Sports'
+  UNION ALL
+  SELECT 'Reading'
+), platforms_and_experiments_cte AS (
+  SELECT * FROM platforms_cte CROSS JOIN experiment_names_cte
+)
+
+SELECT
+  a.platform,
+  a.experiment_name,
+  COUNT(b.platform) AS num_experiments
+FROM
+  platforms_and_experiments_cte a
+  LEFT JOIN Experiments b
+  ON a.platform = b.platform AND a.experiment_name = b.experiment_name
+GROUP BY a.platform, a.experiment_name
+ORDER BY NULL;
