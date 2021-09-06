e7119fab0eba1e676df48acf881c5099496b834a
kamyu <kamyu104@users.noreply.github.com>
2021-09-06 12:07:24 +0800

    Update count-special-quadruplets.py

--- Python/count-special-quadruplets.py
+++ Python/count-special-quadruplets.py
@@ -20,3 +20,21 @@ class Solution(object):
                         result += lookup[nums[a]+nums[b]+nums[c]]
             lookup[nums[c]] += 1
         return result
+
+    
+# Time:  O(n^2) ~ O(n^3)
+# Space: O(n^2)
+import collections
+
+
+class Solution2(object):
+    def countQuadruplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lookup = collections.defaultdict(list)
+        for d in xrange(3, len(nums)):
+            for c in xrange(2, d):
+                lookup[nums[d]-nums[c]].append(c)
+        return sum(sum(b < c for c in lookup[nums[a]+nums[b]]) for b in xrange(1, len(nums)-2) for a in xrange(b))
