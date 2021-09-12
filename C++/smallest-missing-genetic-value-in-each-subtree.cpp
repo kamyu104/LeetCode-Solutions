0de138fccc71db225e455343906f6ab52e0132eb
kamyu <kamyu104@users.noreply.github.com>
2021-09-12 20:19:05 +0800

    Create smallest-missing-genetic-value-in-each-subtree.cpp

---
+++ C++/smallest-missing-genetic-value-in-each-subtree.cpp
@@ -0,0 +1,43 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> smallestMissingValueSubtree(vector<int>& parents, vector<int>& nums) {
+        vector<int> result(size(parents), 1);
+        int i = distance(cbegin(nums), find(cbegin(nums), cend(nums), 1));
+        if (i == size(nums)) {
+            return result;
+        }
+        vector<vector<int>> adj(size(parents));
+        for (int i = 1; i < size(parents); ++i) {
+            adj[parents[i]].emplace_back(i);
+        }
+        unordered_set<int> lookup;
+        for (int miss = 1; i >= 0; i = parents[i]) {
+            iter_dfs(adj, nums, i, &lookup);
+            for (; lookup.count(miss); ++miss);
+            result[i] = miss;
+        }
+        return result;
+    }
+
+private:
+    void iter_dfs(const vector<vector<int>>& adj,
+                  const vector<int>& nums,
+                  int i,
+                  unordered_set<int> *lookup) {
+
+        vector<int> stk = {i};
+        while (!empty(stk)) {
+            const int i = stk.back(); stk.pop_back();
+            if (lookup->count(nums[i])) {
+                continue;
+            }
+            lookup->emplace(nums[i]);
+            for (const auto& j : adj[i]) {
+                stk.emplace_back(j);
+            }
+        }
+    }
+};
