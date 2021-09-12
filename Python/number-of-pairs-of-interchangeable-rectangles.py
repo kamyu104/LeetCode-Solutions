b10a8fc38e75eda5509f223576f2d3dcce4e7e31
kamyu <kamyu104@users.noreply.github.com>
2021-09-12 20:12:33 +0800

    Create number-of-pairs-of-interchangeable-rectangles.py

---
+++ Python/number-of-pairs-of-interchangeable-rectangles.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+import fractions
+
+
+class Solution(object):
+    def interchangeableRectangles(self, rectangles):
+        """
+        :type rectangles: List[List[int]]
+        :rtype: int
+        """
+        count = collections.defaultdict(int)
+        for w, h in rectangles:
+            g = fractions.gcd(w, h)  # Time: O((logx)^2) ~= O(1)
+            count[(w//g, h//g)] += 1
+        return sum(c*(c-1)//2 for c in count.itervalues())
