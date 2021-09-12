d8cd78fdf3a281bea52ba562d406b786a44e3437
kamyu <kamyu104@users.noreply.github.com>
2021-09-12 20:12:40 +0800

    Create number-of-pairs-of-interchangeable-rectangles.cpp

---
+++ C++/number-of-pairs-of-interchangeable-rectangles.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& rectangles) {
+        unordered_map<int, unordered_map<int, int>> lookup;
+        for (const auto& rect : rectangles) {
+            const int g = gcd(rect[0], rect[1]);  // Time: O((logx)^2) ~= O(1)
+            ++lookup[rect[0] / g][rect[1] / g];
+        }
+        int64_t result = 0;
+        for (const auto& [_, kvp] : lookup) {
+            for (const auto& [_, c] : kvp) {
+                result += static_cast<int64_t>(c) * (c - 1) / 2;
+            }
+        }
+        return result;
+    }
+};
