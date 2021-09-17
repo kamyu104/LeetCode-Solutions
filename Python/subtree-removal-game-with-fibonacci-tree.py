ffa3de1e4e90017be23d87bcd95a26211cd851f5
kamyu <kamyu104@users.noreply.github.com>
2021-09-17 19:06:21 +0800

    Update subtree-removal-game-with-fibonacci-tree.py

--- Python/subtree-removal-game-with-fibonacci-tree.py
+++ Python/subtree-removal-game-with-fibonacci-tree.py
@@ -31,7 +31,7 @@ class Solution2(object):
         :type n: int
         :rtype: bool
         """ 
-        grundy = [0, 1]
+        grundy = [0, 1]  # 0-indexed
         for i in xrange(2, n):
             grundy[i%2] = (grundy[(i-1)%2]+1)^(grundy[(i-2)%2]+1)  # colon principle, replace the branches by a non-branching stalk of length equal to their nim sum
         return grundy[(n-1)%2]
