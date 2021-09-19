bef7e46c44a3e7e89a9bef4720d93b405c7c5688
kamyu <kamyu104@users.noreply.github.com>
2021-09-19 22:53:03 +0800

    Create count-number-of-pairs-with-absolute-difference-k.py

---
+++ Python/count-number-of-pairs-with-absolute-difference-k.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        lookup = collections.defaultdict(int)
+        result = 0
+        for x in nums:
+            if x-k in lookup:
+                result += lookup[x-k]
+            if x+k in lookup:
+                result += lookup[x+k]
+            lookup[x] += 1            
+        return result
