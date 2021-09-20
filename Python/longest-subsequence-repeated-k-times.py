aab2499704204a9e70597cd077ed90e9f362bdcb
kamyu <kamyu104@users.noreply.github.com>
2021-09-20 15:07:27 +0800

    Create longest-subsequence-repeated-k-times.py

---
+++ Python/longest-subsequence-repeated-k-times.py
@@ -0,0 +1,49 @@
+# Time:  O(n * (n/k)!)
+# Space: O(n)
+
+class Solution(object):
+    def longestSubsequenceRepeatedK(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        def check(s, k, curr):
+            if not curr:
+                return True
+            i = 0
+            for c in s:
+                if c != curr[i]:
+                    continue
+                i += 1
+                if i != len(curr):
+                    continue
+                i = 0
+                k -= 1
+                if not k:
+                    return True
+            return False
+
+        def backtracking(s, k, curr, cnts, result):
+            if not check(s, k, curr):
+                return
+            if len(curr) > len(result):
+                result[:] = curr
+            for c in reversed(string.ascii_lowercase):
+                if cnts[c] < k:
+                    continue
+                cnts[c] -= k
+                curr.append(c)
+                backtracking(s, k, curr, cnts, result)
+                curr.pop()
+                cnts[c] += k
+                    
+        cnts = collections.Counter(s)
+        new_s = []
+        for c in s:
+            if cnts[c] < k:
+                continue
+            new_s.append(c)
+        result =[]
+        backtracking(new_s, k, [], cnts, result)
+        return "".join(result)
