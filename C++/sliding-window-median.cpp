c783023bc4d48bb8f98d1142e4d044b17de16c68
kamyu <kamyu104@users.noreply.github.com>
2021-09-29 20:50:07 +0800

    Update sliding-window-median.cpp

--- C++/sliding-window-median.cpp
+++ C++/sliding-window-median.cpp
@@ -4,9 +4,9 @@
 class Solution {
 public:
     vector<double> medianSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> min_bst(cbegin(nums), cbegin(nums) + k);
+        multiset<double> min_bst(cbegin(nums), cbegin(nums) + k);
         auto mid = next(cbegin(min_bst), k / 2);
-        vector<double> result = {(double(*mid) + *prev(mid, 1 - k % 2)) / 2};
+        vector<double> result = {((*mid) + *prev(mid, 1 - k % 2)) / 2};
         for (int i = k; i < size(nums); ++i) {
             min_bst.emplace(nums[i]);
             if (nums[i] < *mid) {
@@ -16,12 +16,11 @@ public:
                 ++mid;
             }
             min_bst.erase(min_bst.lower_bound(nums[i - k]));
-            result.emplace_back((double(*mid) + *prev(mid, 1 - k % 2)) / 2);
+            result.emplace_back(((*mid) + *prev(mid, 1 - k % 2)) / 2);
         }
         return result;
     }
 };
-
 
 // Time:  O(nlogk)
 // Space: O(k)
