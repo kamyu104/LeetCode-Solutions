27e7a810f0a0571c84c4879713b412533d964759
kamyu <kamyu104@users.noreply.github.com>
2021-09-29 21:10:25 +0800

    Update sliding-window-median.py

--- Python/sliding-window-median.py
+++ Python/sliding-window-median.py
@@ -1,11 +1,34 @@
+# Time:  O(nlogk)
+# Space: O(k)
+
+from sortedcontainers import SortedList
+
+
+class Solution(object):
+    def medianSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[float]
+        """
+        sl = SortedList()
+        for i in xrange(k):
+            sl.add(float(nums[i]))
+        result = [(sl[k//2]+sl[k//2-(1-k%2)])/2]
+        for i in xrange(k, len(nums)):
+            sl.add(float(nums[i]))
+            del sl[sl.bisect_left(nums[i-k])]
+            result.append((sl[k//2]+sl[k//2-(1-k%2)])/2)
+        return result
+
+
 # Time:  O(nlogn) due to lazy delete
 # Space: O(n)
-
 import collections
 import heapq
 
 
-class Solution(object):
+class Solution2(object):
     def medianSlidingWindow(self, nums, k):
         """
         :type nums: List[int]
@@ -35,8 +58,5 @@ class Solution(object):
                 if not to_remove[min_heap[0]]:
                     del to_remove[min_heap[0]]
                 heapq.heappop(min_heap)
-            if k%2:
-                result.append(float(min_heap[0]))
-            else:
-                result.append((min_heap[0]+(-max_heap[0]))/2.0)
+            result.append(float(min_heap[0]) if k%2 else (min_heap[0]-max_heap[0])/2.0)
         return result
