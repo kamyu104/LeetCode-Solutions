5b3c6f2b5ead7f42c28f04e819a32916b433ef68
kamyu <kamyu104@users.noreply.github.com>
2021-09-30 19:29:05 +0800

    Update guess-number-higher-or-lower-ii.py

--- Python/guess-number-higher-or-lower-ii.py
+++ Python/guess-number-higher-or-lower-ii.py
@@ -7,10 +7,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        pay = [[0] * n for _ in xrange(n+1)]
+        dp = [[0]*(n+1) for _ in xrange(n+1)]  # dp[i][j]: min pay in [i+1, j+1)
         for i in reversed(xrange(n)):
-            for j in xrange(i+1, n):
-                pay[i][j] = min(k+1 + max(pay[i][k-1], pay[k+1][j]) \
-                                for k in xrange(i, j+1))
-        return pay[0][n-1]
-
+            for j in xrange(i+2, n+1):
+                dp[i][j] = min(k+1 + max(dp[i][k], dp[k+1][j]) for k in xrange(i, j))
+        return dp[0][n]
