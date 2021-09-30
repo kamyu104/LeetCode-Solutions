95694b663b67136ca431a61140ae81e69c893fad
kamyu <kamyu104@users.noreply.github.com>
2021-09-30 19:57:31 +0800

    Update guess-number-higher-or-lower-ii.py

--- Python/guess-number-higher-or-lower-ii.py
+++ Python/guess-number-higher-or-lower-ii.py
@@ -1,7 +1,21 @@
 # Time:  O(n^3)
 # Space: O(n^2)
-
 class Solution(object):
+    def getMoneyAmount(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        dp = [[0]*(n+1) for _ in xrange(n+1)]  # dp[i][j]: min pay in [i+1, j+1)
+        for j in xrange(n+1):
+            for i in reversed(xrange(j-1)):
+                dp[i][j] = min((k+1) + max(dp[i][k], dp[k+1][j]) for k in xrange(i, j))
+        return dp[0][n]
+
+
+# Time:  O(n^3)
+# Space: O(n^2)
+class Solution2(object):
     def getMoneyAmount(self, n):
         """
         :type n: int
