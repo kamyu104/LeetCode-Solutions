82dec4096066a479b9c652babc270d1f68ebcf70
kamyu <kamyu104@users.noreply.github.com>
2021-10-04 03:13:18 +0800

    Update number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp

--- C++/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ C++/number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -4,6 +4,34 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
+        unordered_map<int, int> lookup;
+        int result = 0;
+        for (const auto& num : nums) {
+            if (size(num) > size(target)) {
+                continue;
+            }
+            if (!target.compare(0, size(num), num)) {  // in c++ 20, we can directly use starts_with, see https://en.cppreference.com/w/cpp/string/basic_string/starts_with
+                result += lookup[-(size(target) - size(num))];
+            }
+            if (!target.compare(size(target) - size(num), size(num), num)) {
+                result += lookup[size(target) - size(num)];
+            }
+            if (!target.compare(0, size(num), num)) {  // in c++ 20, we can directly use ends_with, see https://en.cppreference.com/w/cpp/string/basic_string/ends_with
+                ++lookup[size(num)];
+            }
+            if (!target.compare(size(target) - size(num), size(num), num)) {
+                ++lookup[-size(num)];
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * l), n is the size of nums, l is the average length of the digit string in nums
+// Space: O(n)
+class Solution2 {
+public:
+    int numOfPairs(vector<string>& nums, string target) {
         unordered_map<int, int> prefix, suffix;
         int result = 0;
         for (const auto& num : nums) {
