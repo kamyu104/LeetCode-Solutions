8ec5160ae9455e5cae54fc978824aa847760deca
kamyu <kamyu104@users.noreply.github.com>
2021-10-04 02:46:47 +0800

    Create number-of-pairs-of-strings-with-concatenation-equal-to-target.py

---
+++ Python/number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -0,0 +1,25 @@
+# Time:  O(n * l), n is the size of nums, l is the average length of the digit string in nums
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def numOfPairs(self, nums, target):
+        """
+        :type nums: List[str]
+        :type target: str
+        :rtype: int
+        """
+        prefix, suffix = collections.Counter(), collections.Counter()
+        result = 0
+        for num in nums:
+            if target.startswith(num):
+                result += suffix[len(target)-len(num)]
+            if target.endswith(num):
+                result += prefix[len(target)-len(num)]
+            if target.startswith(num):
+                prefix[len(num)] += 1
+            if target.endswith(num):
+                suffix[len(num)] += 1
+        return result
