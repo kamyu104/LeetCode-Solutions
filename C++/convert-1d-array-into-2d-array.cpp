83872b0586dd4babfeed18c7c8e4cc7f1901803d
kamyu <kamyu104@users.noreply.github.com>
2021-10-04 00:19:02 +0800

    Create convert-1d-array-into-2d-array.cpp

---
+++ C++/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,16 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        if (size(original) != m * n) {
+            return {};
+        }
+        vector<vector<int>> result;
+        for (int i = 0; i < size(original); i += n) {
+            result.emplace_back(cbegin(original) + i, cbegin(original) + i + n);
+        }
+        return result;
+    }
+};
