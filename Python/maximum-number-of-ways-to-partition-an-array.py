a13d83fb65d6519f1f4f18acf5317cbe9e822ef5
kamyu <kamyu104@users.noreply.github.com>
2021-10-04 20:45:38 +0800

    Create maximum-number-of-ways-to-partition-an-array.py

---
+++ Python/maximum-number-of-ways-to-partition-an-array.py
@@ -0,0 +1,28 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def waysToPartition(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        right = collections.Counter()
+        total = sum(nums)
+        prefix = 0
+        for i in xrange(len(nums)-1):
+            prefix += nums[i]
+            right[prefix-(total-prefix)] += 1
+        result = right[0]
+        left = collections.Counter()
+        prefix = 0
+        for x in nums:
+            result = max(result, left[k-x]+right[-(k-x)])
+            prefix += x
+            left[prefix-(total-prefix)] += 1
+            right[prefix-(total-prefix)] -= 1
+        return result
