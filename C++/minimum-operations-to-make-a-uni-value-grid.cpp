1d6981ebf2a21c40fcea5fab6ccb01c8ae8be31d
kamyu <kamyu104@users.noreply.github.com>
2021-10-11 02:59:45 +0800

    Create minimum-operations-to-make-a-uni-value-grid.cpp

---
+++ C++/minimum-operations-to-make-a-uni-value-grid.cpp
@@ -0,0 +1,26 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        vector<int> nums;
+        for (const auto& row : grid) {
+            nums.insert(end(nums), cbegin(row), cend(row));
+        }
+        int mod_group = -1;
+        for (const auto& num : nums) {
+            if (mod_group == -1) {
+                mod_group = num % x;
+            } else if (mod_group != num % x) {
+                return -1;
+            }
+        }
+        nth_element(begin(nums), begin(nums) + size(nums) / 2, end(nums));
+        const auto& median = nums[size(nums) / 2];
+        return accumulate(cbegin(nums), cend(nums), 0,
+                          [&median, &x](const auto& total, const auto& a) {
+                              return total + abs(a - median) / x;
+                          });
+    }
+};
