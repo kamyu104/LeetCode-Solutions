f32d74f1ed9fde084a985b14c6de51aa7777ca9d
kamyu <kamyu104@users.noreply.github.com>
2021-10-11 03:00:14 +0800

    Create minimum-operations-to-make-a-uni-value-grid.py

---
+++ Python/minimum-operations-to-make-a-uni-value-grid.py
@@ -0,0 +1,42 @@
+# Time:  O(m * n)
+# Space: O(m * n)
+
+class Solution(object):
+    def minOperations(self, grid, x):
+        """
+        :type grid: List[List[int]]
+        :type x: int
+        :rtype: int
+        """
+        def nth_element(nums, n, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+
+            left, right = 0, len(nums)-1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+
+        nums = [v for row in grid for v in row]
+        if len(set(v%x for v in nums)) > 1:
+            return -1
+        nth_element(nums, len(nums)//2)
+        median = nums[len(nums)//2]
+        return sum(abs(v-median)//x for v in nums)
