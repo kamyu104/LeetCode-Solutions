777b204db66886ecf1e06b5f768350e29bcc5974
kamyu <kamyu104@users.noreply.github.com>
2021-10-17 21:46:44 +0800

    Update second-minimum-time-to-reach-destination.py

--- Python/second-minimum-time-to-reach-destination.py
+++ Python/second-minimum-time-to-reach-destination.py
@@ -62,9 +62,9 @@ class Solution2(object):
         :type change: int
         :rtype: int
         """
-        def bfs(adj, src, dst):
-            q = [src]
-            dist = {src:0}
+        def bfs(adj, start):
+            q = [start]
+            dist = {start:0}
             while q:
                 new_q = []
                 for u in q:
@@ -88,13 +88,13 @@ class Solution2(object):
         for u, v in edges:
             adj[u-1].append(v-1)
             adj[v-1].append(u-1)
-        dist_to_dst, dist_to_src = bfs(adj, 0, n-1), bfs(adj, n-1, 0)
+        dist_to_end, dist_to_start = bfs(adj, 0), bfs(adj, n-1)
 
-        dist = dist_to_dst[n-1]+2  # always exists
+        dist = dist_to_end[n-1]+2  # always exists
         for i in xrange(n):  # case of detour
-            if dist_to_dst[i]+dist_to_src[i] == dist_to_dst[n-1]:
+            if dist_to_end[i]+dist_to_start[i] == dist_to_end[n-1]:
                 continue
-            dist = min(dist, dist_to_dst[i]+dist_to_src[i])  # find second min
-            if dist == dist_to_dst[n-1]+1:
+            dist = min(dist, dist_to_end[i]+dist_to_start[i])  # find second min
+            if dist == dist_to_end[n-1]+1:
                 break
         return calc_time(time, change, dist)
