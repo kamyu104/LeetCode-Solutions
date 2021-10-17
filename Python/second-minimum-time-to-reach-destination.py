2b1d9e3cb7033118a31ec57908fd445cd9223297
kamyu <kamyu104@users.noreply.github.com>
2021-10-17 21:40:24 +0800

    Update second-minimum-time-to-reach-destination.py

--- Python/second-minimum-time-to-reach-destination.py
+++ Python/second-minimum-time-to-reach-destination.py
@@ -10,6 +10,56 @@ class Solution(object):
         :type change: int
         :rtype: int
         """
+        def bi_bfs(adj, start, target):
+            left, right = {start}, {target}
+            lookup = set()
+            result = steps = 0
+            while left and (not result or result+2 > steps):
+                for pos in left:
+                    lookup.add(pos)
+                new_left = set()
+                for pos in left: 
+                    if pos in right:
+                        if result and result < steps:
+                            return result+1
+                        elif not result:
+                            result = steps
+                    for nei in adj[pos]:
+                        if nei in lookup:
+                            continue
+                        new_left.add(nei)
+                left = new_left
+                steps += 1
+                if len(left) > len(right): 
+                    left, right = right, left
+            return result+2
+
+        def calc_time(time, change, dist):
+            result = 0
+            for _ in xrange(dist):
+                if result//change%2:
+                    result = (result//change+1)*change
+                result += time
+            return result
+
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u-1].append(v-1)
+            adj[v-1].append(u-1)
+        return calc_time(time, change, bi_bfs(adj, 0, n-1))
+
+
+# Time:  O(|V| + |E|) = O(|E|) since graph is connected, O(|E|) >= O(|V|) 
+# Space: O(|V| + |E|) = O(|E|)
+class Solution2(object):
+    def secondMinimum(self, n, edges, time, change):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type time: int
+        :type change: int
+        :rtype: int
+        """
         def bfs(adj, src, dst):
             q = [src]
             dist = {src:0}
