0b552fb0a8f29e90c0415302e7529529b81ba2e3
kamyu <kamyu104@users.noreply.github.com>
2021-10-17 21:13:28 +0800

    Update second-minimum-time-to-reach-destination.py

--- Python/second-minimum-time-to-reach-destination.py
+++ Python/second-minimum-time-to-reach-destination.py
@@ -40,8 +40,9 @@ class Solution(object):
 
         dist = dist_to_dst[n-1]+2  # always exists
         for i in xrange(n):  # case of detour
-            if dist_to_dst[i]+dist_to_src[i] > dist_to_dst[n-1]:
-                dist = min(dist, dist_to_dst[i]+dist_to_src[i])  # find second min
-                if dist == dist_to_dst[n-1]+1:
-                    break
+            if dist_to_dst[i]+dist_to_src[i] == dist_to_dst[n-1]:
+                continue
+            dist = min(dist, dist_to_dst[i]+dist_to_src[i])  # find second min
+            if dist == dist_to_dst[n-1]+1:
+                break
         return calc_time(time, change, dist)
