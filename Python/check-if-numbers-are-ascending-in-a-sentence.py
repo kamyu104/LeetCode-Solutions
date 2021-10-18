c485a981d9e5c12bf39986651a680669368477dd
kamyu <kamyu104@users.noreply.github.com>
2021-10-18 09:53:43 +0800

    Create check-if-numbers-are-ascending-in-a-sentence.py

---
+++ Python/check-if-numbers-are-ascending-in-a-sentence.py
@@ -0,0 +1,32 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def areNumbersAscending(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        prev = curr = -1
+        for i, c in enumerate(s):
+            if c.isdigit():
+                curr = max(curr, 0)*10+int(c)
+                continue
+            if prev != -1 and curr != -1 and prev >= curr:
+                return False
+            if curr != -1:
+                prev = curr
+            curr = -1            
+        return curr == -1 or prev < curr
+
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def areNumbersAscending(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        nums = [int(x) for x in s.split() if x.isdigit()]
+        return all(nums[i] < nums[i+1] for i in xrange(len(nums)-1))
