efb9410b9c58105fcb8895ceea9ecd1178988429
kamyu <kamyu104@users.noreply.github.com>
2021-10-18 08:43:27 +0800

    Create the-time-when-the-network-becomes-idle.cpp

---
+++ C++/the-time-when-the-network-becomes-idle.cpp
@@ -0,0 +1,33 @@
+// Time:  O(|V| + |E|) = O(|E|) since graph is connected, O(|E|) >= O(|V|) 
+// Space: O(|V| + |E|) = O(|E|)
+
+class Solution {
+public:
+    int networkBecomesIdle(vector<vector<int>>& edges, vector<int>& patience) {
+        vector<vector<int>> adj(size(patience));
+        for (auto &edge : edges) {
+            adj[edge[0]].emplace_back(edge[1]);
+            adj[edge[1]].emplace_back(edge[0]);
+        }
+        vector<bool> lookup(size(patience));
+        lookup[0] = true;
+        vector<int> q{{0}};
+        int result = 0, step = 1;
+        while (!empty(q)) {
+            vector<int> new_q;
+            for (const auto& u : q) {
+                for (int v : adj[u]) {
+                    if (lookup[v]) {
+                        continue;
+                    }
+                    lookup[v] = true;
+                    new_q.emplace_back(v);
+                    result = max(result, ((step * 2) - 1) / patience[v] * patience[v] + (step * 2));
+                }
+            }
+            q = move(new_q);
+            ++step;
+        }
+        return 1 + result;
+    }
+};
