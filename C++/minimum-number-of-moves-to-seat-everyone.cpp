83254dfa06ad48200529f4428ac7ddba6dec50a3
kamyu <kamyu104@users.noreply.github.com>
2021-10-18 09:04:09 +0800

    Create minimum-number-of-moves-to-seat-everyone.cpp

---
+++ C++/minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,15 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        sort(begin(seats), end(seats));
+        sort(begin(students), end(students));
+        int result = 0;
+        for (int i = 0; i < size(seats); ++i) {
+            result += abs(seats[i] - students[i]);
+        }
+        return result;
+    }
+};
