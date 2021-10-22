1b65bd11c455bc11e2a0e53095e9a5a7c6115021
kamyu <kamyu104@users.noreply.github.com>
2021-10-22 09:40:36 +0800

    Update sort-linked-list-already-sorted-using-absolute-values.cpp

--- C++/sort-linked-list-already-sorted-using-absolute-values.cpp
+++ C++/sort-linked-list-already-sorted-using-absolute-values.cpp
@@ -4,52 +4,17 @@
 class Solution {
 public:
     ListNode* sortLinkedList(ListNode* head) {
-        ListNode dummy;
-        auto tail = &dummy, curr = head;
+        auto tail = head;
+        auto curr = head->next;
+        head->next = nullptr;
         while (curr) {
             auto nxt = curr->next;
             if (curr->val >= 0) {
                 curr->next = nullptr;
                 tail->next = curr;
                 tail = curr;
-            } else {
-                if (tail == &dummy) {
-                    tail = curr;
-                }
-                curr->next = dummy.next;
-                dummy.next = curr;
-            }
-            curr = nxt;
-        }
-        return dummy.next;
-    }
-};
-
-
-// Time:  O(n)
-// Space: O(1)
-class Solution2 {
-public:
-    ListNode* sortLinkedList(ListNode* head) {
-        auto curr = head;
-        head = nullptr;
-        ListNode *tail = nullptr;
-        while (curr) {
-            auto nxt = curr->next;
-            if (curr->val >= 0) {
-                if (!head) {
-                    head = curr;
-                }
-                curr->next = nullptr;
-                if (tail) {
-                    tail->next = curr;
-                }
-                tail = curr;
                 
             } else {
-                if (!tail) {
-                    tail = curr;
-                }
                 curr->next = head;
                 head = curr;
             }
