c85ec165b596c1cf527fbb6a46d53c89145aec90
kamyu <kamyu104@users.noreply.github.com>
2021-10-24 12:40:14 +0800

    Create number-of-valid-words-in-a-sentence.py

---
+++ Python/number-of-valid-words-in-a-sentence.py
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def countValidWords(self, sentence):
+        """
+        :type sentence: str
+        :rtype: int
+        """
+        result = token = hyphen = 0
+        for i in xrange(len(sentence)+1):
+            if i == len(sentence) or sentence[i] == ' ':
+                if token == 1:
+                    result += 1
+                token = hyphen = 0
+                continue
+            if sentence[i].isdigit() or \
+               (sentence[i] in '!.,' and not (i == len(sentence)-1 or sentence[i+1] == ' ')) or \
+               (sentence[i] == '-' and not (hyphen == 0 and 0 < i < len(sentence)-1 and sentence[i-1].isalpha() and sentence[i+1].isalpha())):
+                token = -1
+                continue
+            if token == 0:
+                token = 1
+            if sentence[i] == '-':
+                hyphen = 1
+        return result
