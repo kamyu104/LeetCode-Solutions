46ab6422f574e4fea185e60797c6bd4a501327a4
kamyu <kamyu104@users.noreply.github.com>
2021-10-30 16:59:46 +0800

    Update minimum-cost-to-separate-sentence-into-rows.py

--- Python/minimum-cost-to-separate-sentence-into-rows.py
+++ Python/minimum-cost-to-separate-sentence-into-rows.py
@@ -1,9 +1,6 @@
 # Time:  O(s + n * k), n is the number of the word_lens
 # Space: O(k)
 
-import collections
-
-
 class Solution(object):
     def minimumCost(self, sentence, k):
         """
@@ -21,24 +18,24 @@ class Solution(object):
         if len(sentence) <= k:
             return 0
 
-        dp, word_lens = collections.deque(), collections.deque()  # dp[i]: min cost of word_lens[i:]
-        t1 = t2 = -1
+        dp, word_lens = [], []  # dp[i]: min cost of word_lens[-1-i:]
+        t = -1
         for l in lens(sentence):
-            dp.appendleft(float("inf"))
-            word_lens.appendleft(l)
-            t1 += l+1
-            t2 += l+1
-            if t1 <= k:
-                dp[0] = 0
+            dp.append(float("inf"))
+            word_lens.append(l)
+            t += l+1
+            if t <= k:
+                dp[-1] = 0
                 continue
-            while t2-(word_lens[-1]+1) > k:  # minimize len(word_lens) s.t. sum(word_lens) > k
-                t2 -= (word_lens.pop()+1)
-                dp.pop()
             total = l
-            for j in xrange(1, len(dp)):
-                dp[0] = min(dp[0], dp[j] + (k-total)**2)
+            for j in reversed(xrange(len(dp)-1)):
+                dp[-1] = min(dp[-1], dp[j] + (k-total)**2)
                 total += (word_lens[j]+1)
-        return dp[0]
+                if total > k:
+                    word_lens[:] = word_lens[j:]  # minimize len(word_lens) s.t. sum(word_lens) > k
+                    dp[:] = dp[j:]
+                    break
+        return dp[-1]
 
 
 # Time:  O(s + n * k), n is the number of the word_lens
