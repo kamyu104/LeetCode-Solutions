c9a364b053e9c18cc42f25027f5b21fe39dfe8ef
kamyu <kamyu104@users.noreply.github.com>
2021-10-30 16:50:35 +0800

    Update minimum-cost-to-separate-sentence-into-rows.cpp

--- C++/minimum-cost-to-separate-sentence-into-rows.cpp
+++ C++/minimum-cost-to-separate-sentence-into-rows.cpp
@@ -8,6 +8,45 @@ public:
             return 0;
         }
 
+        deque<int> dp, word_lens;  // dp[i]: min cost of word_lens[i:]
+        for (int i = size(sentence) - 1, j = size(sentence) - 1, t1 = -1, t2 = -1; i >= -1; --i) {
+            if (i != -1 && sentence[i] != ' ') {
+                continue;
+            }
+            const int l = j - i;
+            j = i - 1;
+            dp.emplace_front(numeric_limits<int>::max());
+            word_lens.emplace_front(l);
+            t1 += l + 1;
+            t2 += l + 1;
+            if (t1 <= k) {
+                dp[0] = 0;
+                continue;
+            }
+            while (t2 - (word_lens.back() + 1) > k) {  // minimize len(word_lens) s.t. sum(word_lens) > k
+                t2 -= (word_lens.back() + 1);
+                word_lens.pop_back();
+                dp.pop_back();
+            }
+            int total = l;
+            for (int j = 1; j < size(dp); ++j) {
+                dp[0] = min(dp[0], dp[j] + (k - total) * (k - total));
+                total += (word_lens[j] + 1);
+            }
+        }
+        return dp[0];
+    }
+};
+
+// Time:  O(s + n * k), n is the number of the word_lens
+// Space: O(n)
+class Solution2 {
+public:
+    int minimumCost(string sentence, int k) {
+        if (size(sentence) <= k) {
+            return 0;
+        }
+
         vector<int> word_lens;
         for (int i = 0, j = 0; i <= size(sentence); ++i) {
             if (i != size(sentence) && sentence[i] != ' ') {
@@ -16,7 +55,7 @@ public:
             word_lens.emplace_back(i - j);
             j = i + 1;
         }
-        vector<int> dp(size(word_lens), numeric_limits<int>::max());  // dp[i]: min cost of sentence[i:]
+        vector<int> dp(size(word_lens), numeric_limits<int>::max());  // dp[i]: min cost of word_lens[i:]
         int i = size(word_lens) - 1;
         for (int total = -1; total + (word_lens[i] + 1) <= k; --i) {  // find max i s.t. the length of the last line > k
             total += (word_lens[i] + 1);
@@ -31,10 +70,9 @@ public:
     }
 };
 
-
 // Time:  O(s + n * k), n is the number of the word_lens
 // Space: O(n)
-class Solution2 {
+class Solution3 {
 public:
     int minimumCost(string sentence, int k) {
         if (size(sentence) <= k) {
