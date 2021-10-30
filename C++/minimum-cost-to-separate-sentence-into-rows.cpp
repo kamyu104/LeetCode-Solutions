9554d7bc7ba7e33fd6dddad7861fff21c6b48b74
kamyu <kamyu104@users.noreply.github.com>
2021-10-30 17:02:23 +0800

    Update minimum-cost-to-separate-sentence-into-rows.cpp

--- C++/minimum-cost-to-separate-sentence-into-rows.cpp
+++ C++/minimum-cost-to-separate-sentence-into-rows.cpp
@@ -8,33 +8,33 @@ public:
             return 0;
         }
 
-        deque<int> dp, word_lens;  // dp[i]: min cost of word_lens[i:]
+        vector<int> dp, word_lens;  // dp[i]: min cost of word_lens[i:]
         for (int i = size(sentence) - 1, j = size(sentence) - 1, t1 = -1, t2 = -1; i >= -1; --i) {
             if (i != -1 && sentence[i] != ' ') {
                 continue;
             }
             const int l = j - i;
             j = i - 1;
-            dp.emplace_front(numeric_limits<int>::max());
-            word_lens.emplace_front(l);
+            dp.emplace_back(numeric_limits<int>::max());
+            word_lens.emplace_back(l);
             t1 += l + 1;
             t2 += l + 1;
             if (t1 <= k) {
-                dp[0] = 0;
+                dp.back() = 0;
                 continue;
             }
-            while (t2 - (word_lens.back() + 1) > k) {  // minimize len(word_lens) s.t. sum(word_lens) > k
-                t2 -= (word_lens.back() + 1);
-                word_lens.pop_back();
-                dp.pop_back();
-            }
             int total = l;
-            for (int j = 1; j < size(dp); ++j) {
-                dp[0] = min(dp[0], dp[j] + (k - total) * (k - total));
+            for (int j = size(dp) - 2; j >= 0; --j) {
+                dp.back() = min(dp.back(), dp[j] + (k - total) * (k - total));
                 total += (word_lens[j] + 1);
+                if (total > k) {
+                    word_lens = vector<int>(cbegin(word_lens) + j, cend(word_lens));  // minimize len(word_lens) s.t. sum(word_lens) > k
+                    dp = vector<int>(cbegin(dp) + j, cend(dp));
+                    break;
+                }
             }
         }
-        return dp[0];
+        return dp.back();
     }
 };
 
