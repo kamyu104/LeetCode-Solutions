b6b31480c7ec87e5b2b465264f0cb4e0cc12edaf
kamyu <kamyu104@users.noreply.github.com>
2021-11-01 00:38:08 +0800

    Create minimum-operations-to-convert-number.cpp

---
+++ C++/minimum-operations-to-convert-number.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n * m), m is max x
+// Space: O(m)
+
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums, int start, int goal) {
+        static const int MAX_X = 1000;
+
+        vector<int> new_nums;
+        for (const auto& y : nums) {
+            if (y && ((0 <= y && y <= MAX_X) || (0 <= (goal - y) && (goal - y) <= MAX_X) || (0 <= (goal + y) && (goal + y) <= MAX_X) || (0 <= (goal ^ y) && (goal ^ y) <= MAX_X))) {
+                new_nums.emplace_back(y);
+            }
+        }
+        nums = move(new_nums);
+        vector<pair<int, int>> q = {{start, 0}};
+        unordered_set<int> lookup = {start};
+        while (!empty(q)) {
+            vector<pair<int, int>> new_q;
+            for (const auto& [x, steps] : q) {
+                for (const auto& y : nums) {
+                    for (const auto& nx : {x + y, x - y, x ^ y}) {
+                        if (nx == goal) {
+                            return steps + 1;
+                        }
+                        if (!(0 <= nx && nx <= MAX_X) || lookup.count(nx)) {
+                            continue;
+                        }
+                        lookup.emplace(nx);
+                        new_q.emplace_back(nx, steps + 1);
+                    }
+                }
+            }
+            q = move(new_q);
+        }
+        return -1;
+    }
+};
