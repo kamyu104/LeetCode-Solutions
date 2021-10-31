6732b256be9e7e41d6fbc6eb0b22de1c80785be1
kamyu <kamyu104@users.noreply.github.com>
2021-10-31 21:30:34 +0800

    Update number-of-valid-move-combinations-on-chessboard.py

--- Python/number-of-valid-move-combinations-on-chessboard.py
+++ Python/number-of-valid-move-combinations-on-chessboard.py
@@ -19,17 +19,18 @@ class Solution(object):
             result = 0
             r, c = positions[i]
             r, c = r-1, c-1
-            if not lookup[r][c]:
-                lookup[r][c] = all_mask  # stopped at (r, c)
+            mask = all_mask
+            if not (lookup[r][c]&mask):
+                lookup[r][c] += mask  # stopped at (r, c)
                 result += backtracking(pieces, positions, i+1, lookup)
-                lookup[r][c] = 0          
+                lookup[r][c] -= mask          
             for dr, dc in directions[pieces[i]]:
                 bit, nr, nc = 1, r+dr, c+dc
                 mask = all_mask  # (mask&bit == 1): (log2(bit)+1)th second is occupied
                 while 0 <= nr < 8 and 0 <= nc < 8 and not (lookup[nr][nc]&bit):
                     lookup[nr][nc] += bit
                     mask -= bit
-                    if not lookup[nr][nc]&mask:  # stopped at (nr, nc)
+                    if not (lookup[nr][nc]&mask):  # stopped at (nr, nc)
                         lookup[nr][nc] += mask
                         result += backtracking(pieces, positions, i+1, lookup)
                         lookup[nr][nc] -= mask
@@ -38,5 +39,5 @@ class Solution(object):
                     bit, nr, nc = bit>>1, nr-dr, nc-dc
                     lookup[nr][nc] -= bit
             return result
-    
+
         return backtracking(pieces, positions, 0, [[0]*8 for _ in range(8)])
