ab3c84898da5ea46b5a4cc78febfa3278cfd7ea4
kamyu <kamyu104@users.noreply.github.com>
2021-11-01 11:30:07 +0800

    Create plates-between-candles.cpp

---
+++ C++/plates-between-candles.cpp
@@ -0,0 +1,31 @@
+// Time:  O(n + q)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
+        vector<int> left(size(s)), prefix(size(s));
+        for (int i = 0, curr = -1, cnt = 0; i < size(s); ++i) {
+            if (s[i] == '|') {
+                curr = i;
+                ++cnt;
+            }
+            left[i] = curr;
+            prefix[i] = cnt;
+        }
+        vector<int> right(size(s));
+        for (int i = size(s) - 1, curr = size(s), cnt = 0; i >= 0; --i) {
+            if (s[i] == '|') {
+                curr = i;
+                ++cnt;
+            }
+            right[i] = curr;
+        }
+        vector<int> result;
+        for (const auto& q : queries) {
+            const int l = right[q[0]], r = left[q[1]];
+            result.emplace_back(l < r ? (r - l + 1) - (prefix[r] - prefix[l] + 1) : 0);
+        }
+        return result;
+    }
+};
