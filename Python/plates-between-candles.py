c2f95f205cf6f229339348d3955933333218a19f
kamyu <kamyu104@users.noreply.github.com>
2021-11-01 11:30:37 +0800

    Create plates-between-candles.py

---
+++ Python/plates-between-candles.py
@@ -0,0 +1,25 @@
+# Time:  O(n + q)
+# Space: O(n)
+
+class Solution(object):
+    def platesBetweenCandles(self, s, queries):
+        """
+        :type s: str
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        left, prefix = [0]*len(s), [0]*len(s)
+        curr, cnt = -1, 0
+        for i in xrange(len(s)):
+            if s[i] == '|':
+                curr = i
+                cnt += 1
+            left[i] = curr
+            prefix[i] = cnt
+        right = [0]*len(s)
+        curr = len(s)
+        for i in reversed(xrange(len(s))):
+            if s[i] == '|':
+                curr = i
+            right[i] = curr
+        return [max((left[r]-right[l]+1) - (prefix[left[r]]-prefix[right[l]]+1), 0) for l, r in queries]
