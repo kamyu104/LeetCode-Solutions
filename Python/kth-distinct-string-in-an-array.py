f9a8a725cd0ec13ab72bfbd673f787ea6100efdc
kamyu <kamyu104@users.noreply.github.com>
2021-11-01 12:21:32 +0800

    Create kth-distinct-string-in-an-array.py

---
+++ Python/kth-distinct-string-in-an-array.py
@@ -0,0 +1,13 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def kthDistinct(self, arr, k):
+        """
+        :type arr: List[str]
+        :type k: int
+        :rtype: str
+        """
+        count = collections.Counter(arr)
+        arr = [x for x in arr if count[x] == 1]
+        return arr[k-1] if k-1 < len(arr) else ""
