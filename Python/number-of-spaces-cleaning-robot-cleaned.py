e4887e3cd55c4c59f3cf1079023aed024b2dbcbb
kamyu <kamyu104@users.noreply.github.com>
2021-11-06 17:51:34 +0800

    Update number-of-spaces-cleaning-robot-cleaned.py

--- Python/number-of-spaces-cleaning-robot-cleaned.py
+++ Python/number-of-spaces-cleaning-robot-cleaned.py
@@ -7,11 +7,11 @@ class Solution(object):
         :type room: List[List[int]]
         :rtype: int
         """
-        DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         d = r = c = 0
         while not room[r][c]&(1<<(d+1)): 
             room[r][c] |= (1<<(d+1))
-            dr, dc = DIRECTIONS[d]
+            dr, dc = directions[d]
             nr, nc = r+dr, c+dc
             if 0 <= nr < len(room) and 0 <= nc < len(room[0]) and not (room[nr][nc]&1):
                 r, c = nr, nc
