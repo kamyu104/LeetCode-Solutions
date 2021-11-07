b81f4261c44741d1f0289af6d132834aa724306f
kamyu <kamyu104@users.noreply.github.com>
2021-11-08 00:18:53 +0800

    Update count-vowel-substrings-of-a-string.py

--- Python/count-vowel-substrings-of-a-string.py
+++ Python/count-vowel-substrings-of-a-string.py
@@ -26,3 +26,36 @@ class Solution(object):
                 k += 1
             result += k-j
         return result
+
+
+# Time:  O(n)
+# Space: O(1)
+import collections
+
+
+class Solution2(object):
+    def countVowelSubstrings(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        VOWELS = set("aeiou")
+        k = 5
+        def atMost(word, k):
+            cnt = collections.Counter()
+            result, left, count = 0, 0, 0
+            for right in xrange(len(word)):
+                if word[right] not in VOWELS:
+                    cnt = collections.Counter()
+                    left = right+1
+                    continue
+                cnt[word[right]] += 1
+                while len(cnt) > k:
+                    cnt[word[left]] -=1
+                    if not cnt[word[left]]:
+                        del cnt[word[left]]
+                    left += 1
+                result += right-left+1
+            return result
+
+        return atMost(word, k) - atMost(word, k-1)
