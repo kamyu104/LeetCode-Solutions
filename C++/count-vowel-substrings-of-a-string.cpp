0e65433a86783d74f7c97cb30b0504faafa67896
kamyu <kamyu104@users.noreply.github.com>
2021-11-08 00:43:26 +0800

    Update count-vowel-substrings-of-a-string.cpp

--- C++/count-vowel-substrings-of-a-string.cpp
+++ C++/count-vowel-substrings-of-a-string.cpp
@@ -10,7 +10,7 @@ public:
         int result = 0;
         for (int i = 0, j = 0, k = 0; i < size(word); ++i) {
             if (!VOWELS.count(word[i])) {
-                cnt = unordered_map<int, int>();
+                cnt.clear();
                 j = k = i + 1;
                 continue;
             }
@@ -22,6 +22,41 @@ public:
                 }
             }
             result += k - j;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    int countVowelSubstrings(string word) {
+        static const int k = 5;
+
+        return atMostK(word, k) - atMostK(word, k - 1);
+    }
+
+private:
+    int atMostK(const string& word, int k) {
+        static const unordered_set<int> VOWELS = {'a', 'e', 'i', 'o', 'u'};
+
+        unordered_map<int, int> cnt;
+        int result = 0, left = 0;
+        for (int right = 0; right < size(word); ++right) {
+            if (!VOWELS.count(word[right])) {
+                cnt.clear();
+                left = right + 1;
+                continue;
+            }
+            ++cnt[word[right]];
+            while (size(cnt) > k) {
+                if (!--cnt[word[left]]) {
+                    cnt.erase(word[left]);
+                }
+                ++left;
+            }
+            result += right - left + 1;
         }
         return result;
     }
