0dc7dbee6dd74f4be67c2a2e8b4bb60c66933809
kamyu <kamyu104@users.noreply.github.com>
2021-11-07 22:39:49 +0800

    Update maximum-path-quality-of-a-graph.py

--- Python/maximum-path-quality-of-a-graph.py
+++ Python/maximum-path-quality-of-a-graph.py
@@ -78,3 +78,36 @@ class Solution2(object):
         result = [0]
         dfs(values, adj, 0, maxTime, 0, [0]*len(adj), set(), result)
         return result[0]
+
+
+# Time: O(|V| + |E| + 4^(maxTime/min(times))) = O(|V| + |E| + 4^10)
+# Time: O(|V| + |E|)
+class Solution3(object):
+    def maximalPathQuality(self, values, edges, maxTime):
+        """
+        :type values: List[int]
+        :type edges: List[List[int]]
+        :type maxTime: int
+        :rtype: int
+        """
+        def dfs(values, adj, u, time, total, lookup, lookup2):
+            lookup[u] += 1
+            if lookup[u] == 1:
+                total += values[u]
+            result = 0
+            if not u:
+                result = max(result, total)
+            for v, t in adj[u]:
+                if (u, v) in lookup2 or time < t:  # same directed edge won't be visited twice
+                    continue
+                lookup2.add((u, v))
+                result = max(result, dfs(values, adj, v, time-t, total, lookup, lookup2))
+                lookup2.remove((u, v))
+            lookup[u] -= 1
+            return result
+
+        adj = [[] for _ in xrange(len(values))]
+        for u, v, t in edges:
+            adj[u].append((v, t))
+            adj[v].append((u, t))
+        return dfs(values, adj, 0, maxTime, 0, [0]*len(adj), set())
