fc32d5d40a52edd958f79397f4f6c99bfd1f9a35
kamyu <kamyu104@users.noreply.github.com>
2021-11-07 15:05:55 +0800

    Create minimized-maximum-of-products-distributed-to-any-store.py

---
+++ Python/minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,24 @@
+# Time:  O(nlogr), r is the max quantity
+# Space: O(1)
+
+class Solution(object):
+    def minimizedMaximum(self, n, quantities):
+        """
+        :type n: int
+        :type quantities: List[int]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+(b-1))//b
+
+        def check(n, quantities, x):
+            return sum(ceil_divide(q, x) for q in quantities) <= n
+         
+        left, right = 1, max(quantities)
+        while left <= right:
+            mid = left+(right-left)//2
+            if check(n, quantities, mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
