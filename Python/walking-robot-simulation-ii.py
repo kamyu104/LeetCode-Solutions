a52692912081cf85fd063b2edbce60c62ff24e48
kamyu <kamyu104@users.noreply.github.com>
2021-11-16 19:31:24 +0800

    Update walking-robot-simulation-ii.py

--- Python/walking-robot-simulation-ii.py
+++ Python/walking-robot-simulation-ii.py
@@ -11,16 +11,13 @@ class Robot(object):
         self.__w = width
         self.__h = height
         self.__curr = 0
-        self.__moved = False
 
     def move(self, num):
         """
         :type num: int
         :rtype: None
         """
-        if not self.__moved:
-            self.__moved = True
-        self.__curr = (self.__curr+num) % (2*((self.__w-1)+(self.__h-1)))
+        self.__curr += num
 
     def getPos(self):
         """
@@ -35,9 +32,9 @@ class Robot(object):
         return self.__getPosDir()[1]
 
     def __getPosDir(self):
-        n = self.__curr
+        n = self.__curr % (2*((self.__w-1)+(self.__h-1)))
         if n < self.__w:
-            return [[n, 0], "South" if n == 0 and self.__moved else "East"]
+            return [[n, 0], "South" if n == 0 and self.__curr else "East"]
         n -= self.__w-1
         if n < self.__h:
             return [[self.__w-1, n], "North"]
