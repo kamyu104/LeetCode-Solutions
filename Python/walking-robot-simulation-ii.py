eb1fa40508217a4b8eecb575cbc78b6359fd1a5b
kamyu <kamyu104@users.noreply.github.com>
2021-11-16 19:39:05 +0800

    Update walking-robot-simulation-ii.py

--- Python/walking-robot-simulation-ii.py
+++ Python/walking-robot-simulation-ii.py
@@ -23,6 +23,59 @@ class Robot(object):
         """
         :rtype: List[int]
         """
+        n = self.__curr % (2*((self.__w-1)+(self.__h-1)))
+        if n < self.__w:
+            return [n, 0]
+        n -= self.__w-1
+        if n < self.__h:
+            return [self.__w-1, n]
+        n -= self.__h-1
+        if n < self.__w:
+            return [(self.__w-1)-n, self.__h-1]
+        n -= self.__w-1
+        return [0, (self.__h-1)-n]
+
+    def getDir(self):
+        """
+        :rtype: str
+        """
+        n = self.__curr % (2*((self.__w-1)+(self.__h-1)))
+        if n < self.__w:
+            return "South" if n == 0 and self.__curr else "East"
+        n -= self.__w-1
+        if n < self.__h:
+            return "North"
+        n -= self.__h-1
+        if n < self.__w:
+            return "West"
+        n -= self.__w-1
+        return "South"
+
+
+# Time:  O(1)
+# Space: O(1)
+class Robot2(object):
+
+    def __init__(self, width, height):
+        """
+        :type width: int
+        :type height: int
+        """
+        self.__w = width
+        self.__h = height
+        self.__curr = 0
+
+    def move(self, num):
+        """
+        :type num: int
+        :rtype: None
+        """
+        self.__curr += num
+
+    def getPos(self):
+        """
+        :rtype: List[int]
+        """
         return self.__getPosDir()[0] 
 
     def getDir(self):
