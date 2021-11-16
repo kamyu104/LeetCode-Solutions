bc805b879a050a478eb93dcd58e6b825c8d55baa
kamyu <kamyu104@users.noreply.github.com>
2021-11-16 19:32:33 +0800

    Create walking-robot-simulation-ii.cpp

---
+++ C++/walking-robot-simulation-ii.cpp
@@ -0,0 +1,43 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Robot {
+public:
+    Robot(int width, int height) : w_(width), h_(height) {
+        
+    }
+    
+    void move(int num) {
+        curr_ += num;
+    }
+    
+    vector<int> getPos() {
+        return getPosDir().first;
+    }
+    
+    string getDir() {
+        return getPosDir().second;
+    }
+
+private:
+    pair<vector<int>, string> getPosDir() {
+        int n = curr_ % (2 * ((w_ - 1) + h_ - 1));
+        if (n < w_) {
+            return {{n, 0}, (n == 0 && curr_) ? "South" : "East"};
+        }
+        n -= (w_ - 1);
+        if (n < h_) {
+            return {{w_ - 1, n}, "North"};
+        }
+        n -= (h_ - 1);
+        if (n < w_) {
+            return {{(w_ - 1) - n, h_ - 1}, "West"};
+        }
+        n -= (w_ - 1);
+        return {{0, (h_ - 1) - n}, "South"};
+    }
+
+    const int w_;
+    const int h_;
+    int curr_ = 0;
+};
