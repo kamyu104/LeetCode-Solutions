9ec473ce02794687e2097b653f682f721eadc7ce
kamyu <kamyu104@users.noreply.github.com>
2021-11-17 20:37:02 +0800

    Create reverse-nodes-in-even-length-groups.py

---
+++ Python/reverse-nodes-in-even-length-groups.py
@@ -0,0 +1,32 @@
+# Time:  O(n)
+# Space: O(1)
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution(object):
+    def reverseEvenLengthGroups(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        prev, l = head, 2
+        while prev.next:
+            curr, cnt = prev, 0
+            for _ in xrange(l):
+                if not curr.next:
+                    break
+                cnt += 1
+                curr = curr.next
+            l += 1
+            if cnt%2:
+                prev = curr
+                continue
+            curr, last = prev.next, None
+            for _ in xrange(cnt):
+                curr.next, curr, last = last, curr.next, curr
+            prev.next, prev.next.next, prev = last, curr, prev.next
+        return head
