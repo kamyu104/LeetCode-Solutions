5a267417d6d7b93b74a62f6430de0405efacac81
kamyu <kamyu104@users.noreply.github.com>
2021-11-17 20:34:16 +0800

    Create reverse-nodes-in-even-length-groups.cpp

---
+++ C++/reverse-nodes-in-even-length-groups.cpp
@@ -0,0 +1,33 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    ListNode* reverseEvenLengthGroups(ListNode* head) {
+        auto prev = head;
+        int l = 2;
+        while (prev->next) {
+            auto curr = prev;
+            int cnt = 0;
+            for (int _ = 0; _ < l && curr->next; ++_, ++cnt, curr = curr->next);
+            ++l;
+            if (cnt % 2) {
+                prev = curr;
+                continue;
+            }
+            curr = prev->next;
+            ListNode *last = nullptr;
+            for (int _ = 0; _ < cnt; ++_) {
+                auto nxt = curr->next;
+                curr->next = last;
+                last = curr;
+                curr = nxt;
+            }
+            auto tail = prev->next;
+            tail->next = curr;
+            prev->next = last;
+            prev = tail;
+        }
+        return head;
+    }
+};
