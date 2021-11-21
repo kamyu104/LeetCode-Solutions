7ed67399a09e5b1467f56afddc22c21e82a7fb83
kamyu <kamyu104@users.noreply.github.com>
2021-11-22 01:47:52 +0800

    Create paths-in-maze-that-lead-to-same-room.cpp

---
+++ C++/paths-in-maze-that-lead-to-same-room.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n^3)
+// Space: O(n)
+
+class Solution {
+public:
+    int numberOfPaths(int n, vector<vector<int>>& corridors) {
+        vector<unordered_set<int>> adj(n);
+        for (const auto& x : corridors) {
+            adj[min(x[0], x[1]) - 1].emplace(max(x[0], x[1]) - 1);
+        }
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            for (const auto& j : adj[i]) {
+                for (const auto& k : adj[j]) {
+                    result += adj[i].count(k);
+                }
+            }
+        }
+        return result;
+    }
+};
