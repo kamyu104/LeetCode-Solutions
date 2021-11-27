163d87e3e108f54d3b5ed4f5360da37dfd559367
kamyu <kamyu104@users.noreply.github.com>
2021-11-27 20:09:24 +0800

    Update reverse-string.cpp

--- C++/reverse-string.cpp
+++ C++/reverse-string.cpp
@@ -3,11 +3,10 @@
 
 class Solution {
 public:
-    string reverseString(string s) {
-        for (int i = 0, j = s.length() - 1; i < j; ++i, --j) {
+    void reverseString(vector<char>&  s) {
+        for (int i = 0, j = s.size() - 1; i < j; ++i, --j) {
             swap(s[i], s[j]);
         }
-        return s;
     }
 };
 
@@ -15,8 +14,7 @@ public:
 // Space: O(1)
 class Solution2 {
 public:
-    string reverseString(string s) {
+    void reverseString(vector<char>& s) {
         reverse(s.begin(), s.end());
-        return s;
     }
 };
