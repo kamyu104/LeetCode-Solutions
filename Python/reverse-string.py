bbbed422bc1539e962039aa7d9daec69ca541ef7
kamyu <kamyu104@users.noreply.github.com>
2021-11-27 20:07:34 +0800

    Update reverse-string.py

--- Python/reverse-string.py
+++ Python/reverse-string.py
@@ -4,13 +4,11 @@
 class Solution(object):
     def reverseString(self, s):
         """
-        :type s: str
-        :rtype: str
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
         """
         i, j = 0, len(s) - 1
         while i < j:
             s[i], s[j] = s[j], s[i]
             i += 1
             j -= 1
-        return s
-
