1030b92f701409b4ed067f8314fedb4f1534fb99
kamyu <kamyu104@users.noreply.github.com>
2021-11-30 09:23:35 +0800

    Create removing-minimum-and-maximum-from-array.py

---
+++ Python/removing-minimum-and-maximum-from-array.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def targetIndices(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        less = sum(x < target for x in nums)
+        return range(less, less+sum(x == target for x in nums))
