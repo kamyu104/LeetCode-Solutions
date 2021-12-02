f635997e891465644146ca91afcaa1c34c8550fb
kamyu <kamyu104@users.noreply.github.com>
2021-12-02 09:31:28 +0800

    Update count-primes.cpp

--- C++/count-primes.cpp
+++ C++/count-primes.cpp
@@ -1,7 +1,38 @@
-// Time:  O(n/2 + n/3 + ... + n/p) = O(nlog(logn)), see https://mathoverflow.net/questions/4596/on-the-series-1-2-1-3-1-5-1-7-1-11
+// Time: O(n)
 // Space: O(n)
 
 class Solution {
+public:
+    int countPrimes(int n) {
+        return size(linear_sieve_of_eratosthenes(n - 1));
+    }
+
+private:
+    vector<int64_t> linear_sieve_of_eratosthenes(int64_t n) {  // Time: O(n), Space: O(n)
+        vector<int64_t> spf(n + 1);
+        vector<int64_t> primes;
+        for (int64_t i = 2; i <= n; ++i) {
+            if (spf[i] == 0) {
+                spf[i] = i;
+                primes.emplace_back(i);
+            }
+            for (const auto& p : primes) {
+                if (i * p > n) {
+                    break;
+                }
+                spf[i * p] = p;
+                if (p > spf[i]) {
+                    break;
+                }
+            }
+        }
+        return primes;
+    }
+};
+
+// Time:  O(n/2 + n/3 + ... + n/p) = O(nlog(logn)), see https://mathoverflow.net/questions/4596/on-the-series-1-2-1-3-1-5-1-7-1-11
+// Space: O(n)
+class Solution2 {
 public:
     int countPrimes(int n) {
         if (n <= 2) {
