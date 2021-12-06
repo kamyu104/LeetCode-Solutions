6874af33ab851baffb0bb13822c9951f74341a1c
kamyu <kamyu104@users.noreply.github.com>
2021-12-06 13:16:17 +0800

    Update finding-3-digit-even-numbers.py

--- Python/finding-3-digit-even-numbers.py
+++ Python/finding-3-digit-even-numbers.py
@@ -1,6 +1,5 @@
 # Time:  O(n), n is 10^3
 # Space: O(1)
-
 import collections
 
 
@@ -19,12 +18,66 @@ class Solution(object):
         return result
 
 
+# Time:  O(1) ~ O(n), n is 10^3
+# Space: O(1)
+import collections
+
+
+class Node(object):
+    def __init__(self, val=None, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution2(object):
+    def findEvenNumbers(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        k = 3
+        def backtracking(curr, dummy, result):
+            if len(curr) == k:
+                result.append(reduce(lambda x, y: x*10+y, curr))
+                return
+            node = dummy.right
+            while node:
+                if (not curr and node.val[0] == 0) or (len(curr) == k-1 and node.val[0]%2 != 0):
+                    node = node.right
+                    continue
+                node.val[1] -= 1
+                if node.val[1] == 0:
+                    if node.left:
+                        node.left.right = node.right
+                    if node.right:
+                        node.right.left = node.left
+                curr.append(node.val[0])
+                backtracking(curr, dummy, result)
+                curr.pop()
+                if node.val[1] == 0:
+                    if node.left:
+                        node.left.right = node
+                    if node.right:
+                        node.right.left = node
+                node.val[1] += 1
+                node = node.right
+
+        prev = dummy = Node()
+        for digit, cnt in sorted(map(list, collections.Counter(digits).iteritems())):
+            prev.right = Node(val=[digit, cnt], left=prev)
+            prev = prev.right
+        result = []
+        backtracking([], dummy, result)
+        return result
+
+
 # Time:  O(1) ~ O(nlogn), n is 10^3
 # Space: O(1)
 import collections
 
 
-class Solution2(object):
+class Solution3(object):
     def findEvenNumbers(self, digits):
         """
         :type digits: List[int]
