79fed285fe1448beec13e38be78e0ad54a845e4f
kamyu <kamyu104@users.noreply.github.com>
2021-12-18 15:59:20 +0800

    Create rings-and-rods.cpp

---
+++ C++/rings-and-rods.cpp
@@ -0,0 +1,17 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int countPoints(string rings) {
+        static const int k = 10;
+        static const unordered_map<char, int> bits = {{'R', 0b001}, {'G', 0b010}, {'B', 0b100}};
+
+        vector<int> rods(k);
+        for (int i = 0; i < size(rings); i += 2) {
+            rods[rings[i + 1] - '0'] |= bits.at(rings[i]);
+        }
+        return count(begin(rods), end(rods), 0b111);
+    }
+};
+
