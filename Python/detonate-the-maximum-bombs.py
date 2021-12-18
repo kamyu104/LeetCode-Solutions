b8bc3a1dad3f07fd015e3ace919224653fcae596
kamyu <kamyu104@users.noreply.github.com>
2021-12-18 12:47:11 +0800

    Create detonate-the-maximum-bombs.py

---
+++ Python/detonate-the-maximum-bombs.py
@@ -0,0 +1,67 @@
+# Time:  O(|V| * |E|)
+# Space: O(|V| + |E|)
+
+# bfs solution
+class Solution(object):
+    def maximumDetonation(self, bombs):
+        """
+        :type bombs: List[List[int]]
+        :rtype: int
+        """        
+        adj = [[] for _ in xrange(len(bombs))]
+        for i, (xi, yi, ri) in enumerate(bombs):
+            for j, (xj, yj, _) in enumerate(bombs):
+                if i == j:
+                    continue
+                if (xi-xj)**2+(yi-yj)**2 <= ri**2:
+                    adj[i].append(j)
+        result = 0
+        for i in xrange(len(bombs)):
+            q = [i]
+            lookup = {i}
+            while q:
+                new_q = []
+                for u in q:
+                    for v in adj[u]:
+                        if v in lookup:
+                            continue
+                        lookup.add(v)
+                        new_q.append(v)
+                q = new_q
+            result = max(result, len(lookup))
+            if result == len(bombs):
+                break
+        return result
+
+
+# Time:  O(|V| * |E|)
+# Space: O(|V| + |E|)
+# dfs solution
+class Solution2(object):
+    def maximumDetonation(self, bombs):
+        """
+        :type bombs: List[List[int]]
+        :rtype: int
+        """        
+        adj = [[] for _ in xrange(len(bombs))]
+        for i, (xi, yi, ri) in enumerate(bombs):
+            for j, (xj, yj, _) in enumerate(bombs):
+                if i == j:
+                    continue
+                if (xi-xj)**2+(yi-yj)**2 <= ri**2:
+                    adj[i].append(j)
+        result = 0
+        for i in xrange(len(bombs)):
+            stk = [i]
+            lookup = {i}
+            while stk:
+                u = stk.pop()
+                for v in adj[u]:
+                    if v in lookup:
+                        continue
+                    lookup.add(v)
+                    stk.append(v)
+            result = max(result, len(lookup))
+            if result == len(bombs):
+                break
+        return result
