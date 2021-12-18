5f59191dba00e22594fd098f001a3e59eb1c4828
kamyu <kamyu104@users.noreply.github.com>
2021-12-18 14:50:44 +0800

    Create find-subsequence-of-length-k-with-the-largest-sum.cpp

---
+++ C++/find-subsequence-of-length-k-with-the-largest-sum.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> maxSubsequence(vector<int>& nums, int k) {
+        vector<int> partition(begin(nums), end(nums));
+        nth_element(begin(partition), begin(partition) + k - 1, end(partition), greater<int>());
+        int cnt = count(begin(partition), begin(partition) + k, partition[k - 1]);
+        vector<int> result;
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] > partition[k - 1] || (nums[i] == partition[k - 1] && --cnt >= 0)) {
+                result.emplace_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
