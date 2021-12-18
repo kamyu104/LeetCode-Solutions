c5fe611eb0832c2866b05ed4d16714ef564866a5
kamyu <kamyu104@users.noreply.github.com>
2021-12-18 09:30:01 +0800

    Create number-of-unique-flavors-after-sharing-k-candies.py

---
+++ Python/number-of-unique-flavors-after-sharing-k-candies.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def shareCandies(self, candies, k):
+        """
+        :type candies: List[int]
+        :type k: int
+        :rtype: int
+        """
+        cnt = collections.Counter(candies[i] for i in xrange(k, len(candies)))
+        result = curr = len(cnt)
+        for i in xrange(k, len(candies)):
+            cnt[candies[i]] -= 1
+            curr += (cnt[candies[i-k]] == 0) - (cnt[candies[i]] == 0)
+            cnt[candies[i-k]] += 1
+            result = max(result, curr)
+        return result
