e91937971665cd7e976a83dfd4b56376cf18e158
kamyu <kamyu104@users.noreply.github.com>
2021-12-19 16:32:10 +0800

    Create minimum-operations-to-make-the-array-k-increasing.py

---
+++ Python/minimum-operations-to-make-the-array-k-increasing.py
@@ -0,0 +1,24 @@
+# Time:  O(k * (n/k)log(n/k)) = O(nlog(n/k))
+# Space: O(n)
+
+import bisect
+
+
+class Solution(object):
+    def kIncreasing(self, arr, k):
+        """
+        :type arr: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def longest_non_decreasing_subsequence(arr):
+            result = []
+            for x in arr:
+                right = bisect.bisect_right(result, x)
+                if right == len(result):
+                    result.append(x)
+                else:
+                    result[right] = x
+            return len(result)
+
+        return len(arr) - sum(longest_non_decreasing_subsequence((arr[j] for j in xrange(i, len(arr), k))) for i in xrange(k))
