a2c2f420a6bd0d66cbc881b791557d117e5c3ddc
kamyu <kamyu104@users.noreply.github.com>
2021-12-19 16:41:05 +0800

    Create minimum-operations-to-make-the-array-k-increasing.cpp

---
+++ C++/minimum-operations-to-make-the-array-k-increasing.cpp
@@ -0,0 +1,22 @@
+// Time:  O(k * (n/k)log(n/k)) = O(nlog(n/k))
+// Space: O(n)
+
+class Solution {
+public:
+    int kIncreasing(vector<int>& arr, int k) {
+        int result = size(arr);
+        for (int i = 0; i < k; ++i) {
+            vector<int> longest_non_decreasing_subsequence;
+            for (int j = i; j < size(arr); j += k) {
+                auto it = upper_bound(begin(longest_non_decreasing_subsequence), end(longest_non_decreasing_subsequence), arr[j]);
+                if (it == end(longest_non_decreasing_subsequence)) {
+                    longest_non_decreasing_subsequence.emplace_back(arr[j]);
+                } else {
+                    *it = arr[j];
+                }
+            }
+            result -= size(longest_non_decreasing_subsequence);
+        }
+        return result;
+    }
+};
