977971b33760dd1f95508d53c09059999123422d
kamyu <kamyu104@users.noreply.github.com>
2021-12-26 20:57:32 +0800

    Create a-number-after-a-double-reversal.py

---
+++ Python/a-number-after-a-double-reversal.py
@@ -0,0 +1,10 @@
+# Time:  O(1)
+# Space: O(1)
+
+class Solution(object):
+    def isSameAfterReversals(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num == 0 or num%10
