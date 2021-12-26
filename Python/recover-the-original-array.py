f6607c121c425e45b2935e4bf25cbc2ccc96e248
kamyu <kamyu104@users.noreply.github.com>
2021-12-27 00:56:49 +0800

    Create recover-the-original-array.py

---
+++ Python/recover-the-original-array.py
@@ -0,0 +1,34 @@
+# Time:  O(n^2)
+# Space: O(n)
+
+import collections
+
+
+class Solution(object):
+    def recoverArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def check(k, cnt, result):
+            for x in nums:
+                if cnt[x] == 0:
+                    continue
+                if cnt[x+2*k] == 0:
+                    return False
+                cnt[x] -= 1
+                cnt[x+2*k] -= 1
+                result.append(x+k)
+            return True
+            
+        nums.sort()
+        cnt = collections.Counter(nums)
+        for i in xrange(1, len(nums)//2+1):
+            k = nums[i]-nums[0]
+            if k == 0 or k%2:
+                continue
+            k //= 2
+            result = []
+            if  check(k, cnt.copy(), result):
+                return result
+        return []
