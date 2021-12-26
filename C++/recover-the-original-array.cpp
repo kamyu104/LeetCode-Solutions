e93783d8b6cfe3c16cc018ee49dd2c25b753b254
kamyu <kamyu104@users.noreply.github.com>
2021-12-27 00:56:05 +0800

    Create recover-the-original-array.cpp

---
+++ C++/recover-the-original-array.cpp
@@ -0,0 +1,39 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+class Solution {
+public:
+    vector<int> recoverArray(vector<int>& nums) {    
+        auto check = [&nums](int k, unordered_map<int, int> cnt, vector<int> *result) {
+            for (const auto& x : nums) {
+                if (cnt[x] == 0) {
+                    continue;
+                }
+                if (cnt[x + 2 * k] == 0) {
+                    return false;
+                }
+                --cnt[x];
+                --cnt[x + 2 * k];
+                result->emplace_back(x + k);
+            }
+            return true;
+        };
+        sort(begin(nums), end(nums));
+        unordered_map<int, int> cnt;
+        for (const auto& x : nums) {
+            ++cnt[x];
+        }
+        for (int i = 1; i <= size(nums) / 2; ++i) {
+            int k = nums[i] - nums[0];
+            if (k == 0 || k % 2) {
+                continue;
+            }
+            k /= 2;
+            vector<int> result;
+            if (check(k, cnt, &result)) {
+                return result;
+            }
+        }
+        return {};
+    }
+};
