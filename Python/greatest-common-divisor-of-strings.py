6459b9376f4aa9dd65a8cf716cfb8538f803a3d6
kamyu <kamyu104@users.noreply.github.com>
2021-12-30 03:51:30 +0800

    Update greatest-common-divisor-of-strings.py

--- Python/greatest-common-divisor-of-strings.py
+++ Python/greatest-common-divisor-of-strings.py
@@ -16,7 +16,7 @@ class Solution(object):
                 i = (i+1)%len(common)
             return True
     
-        def gcd(a, b):  # Time: O((logn)^2)
+        def gcd(a, b):  # Time: O(log(min(a, b)))
             while b:
                 a, b = b, a % b
             return a
