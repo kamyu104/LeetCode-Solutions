af0e0cce43f3e0be7f65f3e676f4713546be1687
kamyu <kamyu104@users.noreply.github.com>
2022-01-02 06:32:28 +0800

    Update minimum-operations-to-remove-adjacent-ones-in-matrix.cpp

--- C++/minimum-operations-to-remove-adjacent-ones-in-matrix.cpp
+++ C++/minimum-operations-to-remove-adjacent-ones-in-matrix.cpp
@@ -3,49 +3,49 @@
 
 // template from https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/hopcroftKarp.h
 bool dfs(int a, int L, vector<vector<int>>& g, vector<int>& btoa, vector<int>& A, vector<int>& B) {
-	if (A[a] != L) return 0;
-	A[a] = -1;
-	for (int b : g[a]) if (B[b] == L + 1) {
-		B[b] = 0;
-		if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B))
-			return btoa[b] = a, 1;
-	}
-	return 0;
+    if (A[a] != L) return 0;
+    A[a] = -1;
+    for (int b : g[a]) if (B[b] == L + 1) {
+        B[b] = 0;
+        if (btoa[b] == -1 || dfs(btoa[b], L + 1, g, btoa, A, B))
+            return btoa[b] = a, 1;
+    }
+    return 0;
 }
 
 int hopcroftKarp(vector<vector<int>>& g, vector<int>& btoa) {
-	int res = 0;
-	vector<int> A(g.size()), B(btoa.size()), cur, next;
-	for (;;) {
-		fill(begin(A), end(A), 0);
-		fill(begin(B), end(B), 0);
-		/// Find the starting nodes for BFS (i.e. layer 0).
-		cur.clear();
-		for (int a : btoa) if(a != -1) A[a] = -1;
-		for (int a = 0; a < size(g); ++a) if(A[a] == 0) cur.push_back(a);
-		/// Find all layers using bfs.
-		for (int lay = 1;; lay++) {
-			bool islast = 0;
-			next.clear();
-			for (int a : cur) for (int b : g[a]) {
-				if (btoa[b] == -1) {
-					B[b] = lay;
-					islast = 1;
-				}
-				else if (btoa[b] != a && !B[b]) {
-					B[b] = lay;
-					next.push_back(btoa[b]);
-				}
-			}
-			if (islast) break;
-			if (next.empty()) return res;
-			for (int a : next) A[a] = lay;
-			cur.swap(next);
-		}
-		/// Use DFS to scan for augmenting paths.
-		for (int a = 0; a < size(g); ++a)
-			res += dfs(a, 0, g, btoa, A, B);
-	}
+    int res = 0;
+    vector<int> A(g.size()), B(btoa.size()), cur, next;
+    for (;;) {
+        fill(begin(A), end(A), 0);
+        fill(begin(B), end(B), 0);
+        /// Find the starting nodes for BFS (i.e. layer 0).
+        cur.clear();
+        for (int a : btoa) if(a != -1) A[a] = -1;
+        for (int a = 0; a < size(g); ++a) if(A[a] == 0) cur.push_back(a);
+        /// Find all layers using bfs.
+        for (int lay = 1;; lay++) {
+            bool islast = 0;
+            next.clear();
+            for (int a : cur) for (int b : g[a]) {
+                if (btoa[b] == -1) {
+                    B[b] = lay;
+                    islast = 1;
+                }
+                else if (btoa[b] != a && !B[b]) {
+                    B[b] = lay;
+                    next.push_back(btoa[b]);
+                }
+            }
+            if (islast) break;
+            if (next.empty()) return res;
+            for (int a : next) A[a] = lay;
+            cur.swap(next);
+        }
+        /// Use DFS to scan for augmenting paths.
+        for (int a = 0; a < size(g); ++a)
+            res += dfs(a, 0, g, btoa, A, B);
+    }
 }
 
 class Solution {
