03184deda20e7705fd688906f37a0f9d6651c22d
kamyu <kamyu104@users.noreply.github.com>
2022-01-02 20:20:16 +0800

    Create maximum-employees-to-be-invited-to-a-meeting.py

---
+++ Python/maximum-employees-to-be-invited-to-a-meeting.py
@@ -0,0 +1,46 @@
+# Time:  O(n)
+# Space: O(n)
+
+class Solution(object):
+    def maximumInvitations(self, favorite):
+        """
+        :type favorite: List[int]
+        :rtype: int
+        """
+        def find_cycles(adj):
+            result = []
+            lookup = [False]*len(adj)
+            for u in xrange(len(adj)):
+                cnt = {}
+                while not lookup[u]:
+                    lookup[u] = True
+                    cnt[u] = len(cnt)
+                    u = adj[u]
+                if u in cnt:
+                    result.append((u, len(cnt)-cnt[u]))
+            return result
+
+        def bfs(adj, u, exclude):
+            result = 0
+            q = [u]
+            while q:
+                result += 1
+                new_q = []
+                for u in q:
+                    for v in adj[u]:
+                        if v == exclude:
+                            continue
+                        new_q.append(v)
+                q = new_q
+            return result
+            
+        inv_adj = [[] for _ in xrange(len(favorite))]  
+        for u, v in enumerate(favorite):
+            inv_adj[v].append(u)
+        result = cycle_2s = 0
+        for u, l in find_cycles(favorite):
+            if l > 2:
+                result = max(result, l)
+                continue
+            cycle_2s += bfs(inv_adj, u, favorite[u])+bfs(inv_adj, favorite[u], u)
+        return max(result, cycle_2s)
