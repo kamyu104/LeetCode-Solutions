d696285dd4a5a235697fd35e81bb144a1c1fa1cc
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 23:03:36 +0800

    Update minimum-swaps-to-group-all-1s-together-ii.cpp

--- C++/minimum-swaps-to-group-all-1s-together-ii.cpp
+++ C++/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -6,7 +6,7 @@ public:
     int minSwaps(vector<int>& nums) {
         const int w = count(cbegin(nums), cend(nums), 1);
         int result = w, cnt = w;
-        for (int i = 0; i < size(nums) * 2; ++i) {
+        for (int i = 0; i < size(nums) + (w - 1); ++i) {
             if (i >= w) {
                 cnt += nums[(i - w) % size(nums)];
             }
