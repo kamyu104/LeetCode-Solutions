d10ef2ace52e3b252d6abfe5e73c19f6ca6f3c83
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 20:31:21 +0800

    Create minimum-swaps-to-group-all-1s-together-ii.cpp

---
+++ C++/minimum-swaps-to-group-all-1s-together-ii.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        const int w = count(cbegin(nums), cend(nums), 1);
+        int result = w, cnt = w;
+        for (int i = 0; i < size(nums) * 2; ++i) {
+            if (i >= w) {
+                cnt += nums[(i - w) % size(nums)];
+            }
+            cnt -= nums[i % size(nums)];
+            result = min(result, cnt);
+        }
+        return result;
+    }
+};
