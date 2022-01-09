b8cf5b54a94b7476501b1e7d45dad64620c1a4fa
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 18:16:18 +0800

    Update maximum-twin-sum-of-a-linked-list.cpp

--- C++/maximum-twin-sum-of-a-linked-list.cpp
+++ C++/maximum-twin-sum-of-a-linked-list.cpp
@@ -1,16 +1,6 @@
 // Time:  O(n)
 // Space: O(1)
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     int pairSum(ListNode* head) {
