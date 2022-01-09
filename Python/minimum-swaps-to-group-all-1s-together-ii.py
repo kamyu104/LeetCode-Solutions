5a3ee3d380cf373d6a98c9318e06a6ff41f6deed
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 20:32:28 +0800

    Create minimum-swaps-to-group-all-1s-together-ii.py

---
+++ Python/minimum-swaps-to-group-all-1s-together-ii.py
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(1)
+
+class Solution(object):
+    def minSwaps(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = cnt = w = nums.count(1)
+        for i in xrange(len(nums)*2):
+            if i >= w:
+                cnt += nums[(i-w)%len(nums)]
+            cnt -= nums[i%len(nums)]
+            result = min(result, cnt)
+        return result
