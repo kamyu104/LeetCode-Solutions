e0a9ba2702b1420850b2eacdceccf5cdd2a9db66
kamyu <kamyu104@users.noreply.github.com>
2022-01-10 00:07:34 +0800

    Update check-if-every-row-and-column-contains-all-numbers.cpp

--- C++/check-if-every-row-and-column-contains-all-numbers.cpp
+++ C++/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,26 +1,7 @@
 // Time:  O(n^2)
-// Space: O(1)
-
-class Solution{
-public:
-    bool checkValid(vector<vector<int>>& matrix) {
-        for (int i = 0; i < size(matrix); ++i) {
-            int r = 0, c = 0;
-            for (int j = 0; j < size(matrix[0]); ++j) {
-                r ^= matrix[i][j] ^ (j + 1);
-                c ^= matrix[j][i] ^ (j + 1);
-            }
-            if (r || c) {
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
-// Time:  O(n^2)
 // Space: O(n)
-class Solution2 {
+
+class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         for (int i = 0; i < size(matrix); ++i) {
@@ -30,6 +11,26 @@ public:
                 c.emplace(matrix[j][i]);
             }
             if (size(r) != size(matrix[0]) || size(c) != size(matrix)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(1)
+// [[1,3,3,4,4],[4,1,3,3,4],[4,4,1,3,3],[3,4,4,1,3],[3,3,4,4,1]]
+class Solution_Wrong{
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        for (int i = 0; i < size(matrix); ++i) {
+            int r = 0, c = 0;
+            for (int j = 0; j < size(matrix[0]); ++j) {
+                r ^= matrix[i][j] ^ (j + 1);
+                c ^= matrix[j][i] ^ (j + 1);
+            }
+            if (r || c) {
                 return false;
             }
         }
