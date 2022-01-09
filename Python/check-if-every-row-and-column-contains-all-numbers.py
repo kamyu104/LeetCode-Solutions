ccb35fb1ad76747deb31cb29c152e33fd69ca4f2
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 20:01:58 +0800

    Update check-if-every-row-and-column-contains-all-numbers.py

--- Python/check-if-every-row-and-column-contains-all-numbers.py
+++ Python/check-if-every-row-and-column-contains-all-numbers.py
@@ -1,5 +1,5 @@
 # Time:  O(m * n)
-# Space: O(n)
+# Space: O(1)
 
 class Solution(object):
     def checkValid(self, matrix):
@@ -7,15 +7,16 @@ class Solution(object):
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        return all(len(set(row)) == len(matrix) for row in matrix) and all(len(set(matrix[i][j] for i in xrange(len(matrix)))) == len(matrix) for j in xrange(len(matrix[0])))
+        return all(reduce(lambda x, y: x^y, (matrix[i][j]^(j+1) for j in xrange(len(matrix[0])))) == 0 for i in xrange(len(matrix))) and \
+               all(reduce(lambda x, y: x^y, (matrix[i][j]^(i+1) for i in xrange(len(matrix)))) == 0 for j in xrange(len(matrix[0])))
 
 
-# [[1,1,1,4],[1,1,4,1],[1,4,1,1],[4,1,1,1]]
-class Solution_Wrong(object):
+# Time:  O(m * n)
+# Space: O(n)
+class Solution2(object):
     def checkValid(self, matrix):
         """
         :type matrix: List[List[int]]
         :rtype: bool
         """
-        return all(reduce(lambda x, y: x^y, (matrix[i][j]^(j+1) for j in xrange(len(matrix[0])))) == 0 for i in xrange(len(matrix))) and \
-               all(reduce(lambda x, y: x^y, (matrix[i][j]^(i+1) for i in xrange(len(matrix)))) == 0 for j in xrange(len(matrix[0])))
+        return all(len(set(row)) == len(matrix) for row in matrix) and all(len(set(matrix[i][j] for i in xrange(len(matrix)))) == len(matrix) for j in xrange(len(matrix[0])))
