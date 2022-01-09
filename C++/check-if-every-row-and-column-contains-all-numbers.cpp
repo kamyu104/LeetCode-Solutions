985d27014f3168b196fe4bff3009fba147695476
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 20:01:11 +0800

    Update check-if-every-row-and-column-contains-all-numbers.cpp

--- C++/check-if-every-row-and-column-contains-all-numbers.cpp
+++ C++/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,27 +1,7 @@
 // Time:  O(m * n)
-// Space: O(n)
-
-class Solution {
-public:
-    bool checkValid(vector<vector<int>>& matrix) {
-        for (int i = 0; i < size(matrix); ++i) {
-            unordered_set<int> r, c;
-            for (int j = 0; j < size(matrix[0]); ++j) {
-                r.emplace(matrix[i][j]);
-                c.emplace(matrix[j][i]);
-            }
-            if (size(r) != size(matrix[0]) || size(c) != size(matrix)) {
-                return false;
-            }
-        }
-        return true;
-    }
-};
-
-// Time:  O(m * n)
 // Space: O(1)
-// [[1,1,1,4],[1,1,4,1],[1,4,1,1],[4,1,1,1]]
-class Solution_Wrong {
+
+class Solution{
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         for (int i = 0; i < size(matrix); ++i) {
@@ -31,6 +11,25 @@ public:
                 c ^= matrix[j][i] ^ (j + 1);
             }
             if (r || c) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(n)
+class Solution2 {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        for (int i = 0; i < size(matrix); ++i) {
+            unordered_set<int> r, c;
+            for (int j = 0; j < size(matrix[0]); ++j) {
+                r.emplace(matrix[i][j]);
+                c.emplace(matrix[j][i]);
+            }
+            if (size(r) != size(matrix[0]) || size(c) != size(matrix)) {
                 return false;
             }
         }
