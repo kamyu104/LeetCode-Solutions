6994613441ca7acfe9ba129ed3d3442672b3ed9f
kamyu <kamyu104@users.noreply.github.com>
2022-01-09 19:57:11 +0800

    Update check-if-every-row-and-column-contains-all-numbers.cpp

--- C++/check-if-every-row-and-column-contains-all-numbers.cpp
+++ C++/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,7 +1,27 @@
 // Time:  O(m * n)
-// Space: O(1)
+// Space: O(n)
 
 class Solution {
+public:
+    bool checkValid(vector<vector<int>>& matrix) {
+        for (int i = 0; i < size(matrix); ++i) {
+            unordered_set<int> r, c;
+            for (int j = 0; j < size(matrix[0]); ++j) {
+                r.emplace(matrix[i][j]);
+                c.emplace(matrix[j][i]);
+            }
+            if (size(r) != size(matrix[0]) || size(c) != size(matrix)) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(1)
+// [[1,1,1,4],[1,1,4,1],[1,4,1,1],[4,1,1,1]]
+class Solution_Wrong {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
         for (int i = 0; i < size(matrix); ++i) {
