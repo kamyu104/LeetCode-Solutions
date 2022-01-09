ab8cf9b2f9dcf559c24d83c09dbe1339ceeab500
kamyu <kamyu104@users.noreply.github.com>
2022-01-10 00:08:16 +0800

    Update check-if-every-row-and-column-contains-all-numbers.py

--- Python/check-if-every-row-and-column-contains-all-numbers.py
+++ Python/check-if-every-row-and-column-contains-all-numbers.py
@@ -1,18 +1,6 @@
 # Time:  O(n^2)
-# Space: O(1)
-
-class Solution(object):
-    def checkValid(self, matrix):
-        """
-        :type matrix: List[List[int]]
-        :rtype: bool
-        """
-        return all(reduce(lambda x, y: x^y, (matrix[i][j]^(j+1) for j in xrange(len(matrix[0])))) == 0 for i in xrange(len(matrix))) and \
-               all(reduce(lambda x, y: x^y, (matrix[i][j]^(i+1) for i in xrange(len(matrix)))) == 0 for j in xrange(len(matrix[0])))
-
-
-# Time:  O(n^2)
 # Space: O(n)
+
 class Solution2(object):
     def checkValid(self, matrix):
         """
@@ -20,3 +8,15 @@ class Solution2(object):
         :rtype: bool
         """
         return all(len(set(row)) == len(matrix) for row in matrix) and all(len(set(matrix[i][j] for i in xrange(len(matrix)))) == len(matrix) for j in xrange(len(matrix[0])))
+
+# Time:  O(n^2)
+# Space: O(1)
+# [[1,3,3,4,4],[4,1,3,3,4],[4,4,1,3,3],[3,4,4,1,3],[3,3,4,4,1]]
+class Solution_Wrong(object):
+    def checkValid(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: bool
+        """
+        return all(reduce(lambda x, y: x^y, (matrix[i][j]^(j+1) for j in xrange(len(matrix[0])))) == 0 for i in xrange(len(matrix))) and \
+               all(reduce(lambda x, y: x^y, (matrix[i][j]^(i+1) for i in xrange(len(matrix)))) == 0 for j in xrange(len(matrix[0])))
