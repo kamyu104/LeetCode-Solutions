040a89ad128a17089524568b4c653cd2e49a51dc
kamyu <kamyu104@users.noreply.github.com>
2022-01-24 03:13:52 +0800

    Create rearrange-array-elements-by-sign.py

---
+++ Python/rearrange-array-elements-by-sign.py
@@ -0,0 +1,47 @@
+# Time:  O(n)
+# Space: O(1)
+
+# two pointers
+class Solution(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def pos():
+            for x in nums:
+                if x > 0:
+                    yield x
+        
+        def neg():
+            for x in nums:
+                if x < 0:
+                    yield x
+        
+        gen_pos = pos()
+        gen_neg = neg()
+        return [next(gen_pos) if i%2 == 0 else next(gen_neg)  for i in xrange(len(nums))]
+
+
+# Time:  O(n)
+# Space: O(n)
+# array, implementation
+class Solution2(object):
+    def rearrangeArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        pos, neg = [], []
+        for i in reversed(xrange(len(nums))):
+            if nums[i] > 0:
+                pos.append(nums[i])
+            else:
+                neg.append(nums[i])
+        result = []
+        for i in xrange(len(nums)):
+            if i%2 == 0:
+                result.append(pos.pop())
+            else:
+                result.append(neg.pop())
+        return result
