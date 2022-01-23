a4a6564bbe2de1e244f98ac64fa183be89808784
kamyu <kamyu104@users.noreply.github.com>
2022-01-24 03:27:08 +0800

    Update rearrange-array-elements-by-sign.cpp

--- C++/rearrange-array-elements-by-sign.cpp
+++ C++/rearrange-array-elements-by-sign.cpp
@@ -22,8 +22,43 @@ public:
 
 // Time:  O(n)
 // Space: O(1)
-// array, implementation
+// generator
 class Solution2 {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int pos = 0, neg = 0;
+        auto next_pos = [&nums, &pos]() {
+            for (; pos < size(nums); ++pos) {
+                if (nums[pos] > 0) {
+                    return nums[pos++];
+                }
+            }
+            return -1;
+        };
+        auto next_neg = [&nums, &neg]() {
+            for (; neg < size(nums); ++neg) {
+                if (nums[neg] < 0) {
+                    return nums[neg++];
+                }
+            }
+            return -1;
+        };
+        vector<int> result;
+        for (int i = 0; i < size(nums); ++i) {
+            if (i % 2 == 0) {
+                result.emplace_back(next_pos());
+            } else {
+                result.emplace_back(next_neg());
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+// array, implementation
+class Solution3 {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> pos, neg;
