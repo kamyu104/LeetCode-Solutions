153ee5278ebc308f62cb4ecc871b4dae0d76f36c
kamyu <kamyu104@users.noreply.github.com>
2022-01-24 00:11:50 +0800

    Update count-the-hidden-sequences.cpp

--- C++/count-the-hidden-sequences.cpp
+++ C++/count-the-hidden-sequences.cpp
@@ -6,8 +6,8 @@ class Solution {
 public:
     int numberOfArrays(vector<int>& differences, int lower, int upper) {
         int64_t total = 0, mn = 0, mx = 0;
-        for (const auto& d : differences) {
-            total += d;
+        for (const auto& x : differences) {
+            total += x;
             mn = min(mn, total);
             mx = max(mx, total);
         }
