90415cfc5f3f727ea538c2e580ac78133bbf0544
kamyu <kamyu104@users.noreply.github.com>
2022-01-31 14:07:56 +0800

    Create minimum-number-of-lines-to-cover-points.cpp

---
+++ C++/minimum-number-of-lines-to-cover-points.cpp
@@ -0,0 +1,75 @@
+// Time:  O(n^2 + n*2^n) = O(n*2^n)
+// Space: O(n^2)
+
+// math, bitmasks
+class Solution {
+private:
+    template <typename A, typename B, typename C>
+    struct TupleHash {
+        size_t operator()(const tuple<A, B, C>& p) const {
+            size_t seed = 0;
+            A a; B b; C c;
+            tie(a, b, c) = p;
+            seed ^= std::hash<A>{}(a) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            seed ^= std::hash<B>{}(b) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            seed ^= std::hash<C>{}(c) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            return seed;
+        }
+    };
+
+    template <typename T>
+    struct PairHash {
+        size_t operator()(const pair<T, T>& p) const {
+            size_t seed = 0;
+            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            return seed;
+        }
+    };
+
+public:
+    int minimumLines(vector<vector<int>>& points) {
+        auto ceil_divide = [](int a, int b) {
+            return (a + b - 1) / b;
+        };
+        unordered_map<tuple<int, int, int>, unordered_set<pair<int, int>, PairHash<int>>, TupleHash<int, int, int>> lookup;
+        for (int i = 0; i < size(points); ++i) {
+            const int x1 = points[i][0], y1 = points[i][1];
+            for (int j = i + 1; j < size(points); ++j) {
+                const int x2 = points[j][0], y2 = points[j][1];
+                // (x-x1)/(x2-x1) = (y-y1)/(y2-y1)
+                // => (y2-y1)x -(x2-x1)y = x1(y2-y1) - y1(x2-x1)
+                int a = y2 - y1;
+                int b = -(x2 - x1);
+                int c = x1 * (y2 - y1) - y1 * (x2 - x1);
+                const int g = gcd(gcd(a, b), c);
+                a /= g, b /= g, c /= g;
+                lookup[{a, b, c}].emplace(x1, y1);
+                lookup[{a, b, c}].emplace(x2, y2);
+            }
+        }
+        vector<tuple<int, int, int>> lines;
+        for (const auto& [l, p] : lookup) {
+            if (size(p) > 2) {
+                lines.emplace_back(l);
+            }
+        }
+        assert(size(lines) <= size(points) / 2);  // 1 extra colinear point per 2 points
+        int result = numeric_limits<int>::max();
+        const int mask_upper_bound = 1 << size(lines);
+        for (int mask = 0; mask < mask_upper_bound; ++mask) {
+            const int cnt = __builtin_popcount(mask);
+            unordered_set<pair<int, int>, PairHash<int>> covered;
+            
+            for (int i = 0, bit = 1; bit <= mask; bit <<= 1, ++i) {
+                if (mask & bit) {
+                    for (const auto& x : lookup[lines[i]]) {
+                        covered.emplace(x);
+                    }
+                }
+            }
+            result = min(result, cnt + ceil_divide(size(points) - size(covered), 2));
+        }
+        return result;
+    }
+};
