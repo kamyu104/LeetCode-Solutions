99f2a8710da589295423d01b564bd95df6ce4b21
kamyu <kamyu104@users.noreply.github.com>
2022-01-31 14:44:51 +0800

    Create the-number-of-passengers-in-each-bus-ii.sql

---
+++ MySQL/the-number-of-passengers-in-each-bus-ii.sql
@@ -0,0 +1,23 @@
+# Time:  O(p * b + blogb)
+# Space: O(P * b)
+
+WITH arrival_time_cte AS
+(
+    SELECT bus_id, b.arrival_time, capacity, count(passenger_id) AS prefix_sum
+    FROM Buses b
+    LEFT JOIN Passengers p
+    ON p.arrival_time <= b.arrival_time
+    GROUP BY bus_id
+    ORDER BY arrival_time
+)
+
+SELECT bus_id, passengers_cnt FROM
+(
+    SELECT bus_id, capacity, prefix_sum,
+           @passengers_cnt := LEAST(capacity, prefix_sum-@accum) as passengers_cnt, 
+           @accum := @accum + @passengers_cnt
+    FROM arrival_time_cte,
+         (SELECT @accum := 0,
+                 @passengers_cnt := 0) init
+) t
+ORDER BY bus_id;
