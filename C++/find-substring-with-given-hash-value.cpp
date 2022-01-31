e00e506a9571edb55aaf91351ca68d28f0974160
kamyu <kamyu104@users.noreply.github.com>
2022-01-31 15:52:42 +0800

    Create find-substring-with-given-hash-value.cpp

---
+++ C++/find-substring-with-given-hash-value.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(1)
+
+// rolling hash
+class Solution {
+public:
+    string subStrHash(string s, int power, int modulo, int k, int hashValue) {
+        int idx = 0;
+        for (int64_t i = size(s) - 1, pw = 1, h = 0; i >= 0; --i) {
+            if (i > size(s) - k) {
+                pw = (pw * power) % modulo;
+            } 
+            if (i + k < size(s)) {
+                h = (h - (s[i + k] - 'a' + 1) * pw % modulo + modulo) % modulo;
+            }
+            h = (h * power % modulo + (s[i] - 'a' + 1)) % modulo;
+            if (h == hashValue) {
+                idx = i;
+            }
+        }        
+        return s.substr(idx, k);
+    }
+};
