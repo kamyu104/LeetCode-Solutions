eebb316abd3528a19044894beae841cafd726b89
kamyu <kamyu104@users.noreply.github.com>
2022-02-08 05:15:31 +0800

    Update minimum-time-to-remove-all-cars-containing-illegal-goods.cpp

--- C++/minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ C++/minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -13,3 +13,23 @@ public:
         return result;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+// dp
+class Solution2 {
+public:
+    int minimumTime(string s) {
+        vector<int> right(size(s) + 1);
+        for (int i = size(s) - 1; i >= 0; --i) {
+            right[i] = min(right[i + 1] + 2 * (s[i] == '1'), static_cast<int>(size(s)) - i);
+        }
+        int left = 0;
+        int result = left + right[0];
+        for (int i = 1; i <= size(s); ++i) {
+            left = min(left + 2 * (s[i - 1] == '1'), i);
+            result = min(result, left + right[i]);
+        }
+        return result;
+    }
+};
