aa2a237da1323d3322ea2e80b490a06ac64573bb
kamyu <kamyu104@users.noreply.github.com>
2022-02-07 23:10:27 +0800

    Update partition-array-according-to-given-pivot.cpp

--- C++/partition-array-according-to-given-pivot.cpp
+++ C++/partition-array-according-to-given-pivot.cpp
@@ -5,16 +5,16 @@
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
-        int less = 0, great = 0;
+        int less = 0, greater = 0;
         for (const auto& x : nums) {
             if (x < pivot) {
                 ++less;
             } else if (x > pivot) {
-                ++great;
+                ++greater;
             }
         }
         vector<int> result(size(nums), pivot);
-        int left = 0, right = size(nums) - great;
+        int left = 0, right = size(nums) - greater;
         for (const auto& x: nums) {
             if (x < pivot) {
                 result[left++] = x;
