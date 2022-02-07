f58c9699055b328474e9ea1e2ef394f7f9702735
kamyu <kamyu104@users.noreply.github.com>
2022-02-07 18:30:35 +0800

    Create minimum-sum-of-four-digit-number-after-splitting-digits.py

---
+++ Python/minimum-sum-of-four-digit-number-after-splitting-digits.py
@@ -0,0 +1,53 @@
+# Time:  O(d) = O(1), d is the number of digits
+# Space: O(d) = O(1)
+
+# greedy
+class Solution(object):
+    def minimumSum(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        def inplace_counting_sort(nums, reverse=False):  # Time: O(n)
+            count = [0]*(max(nums)+1)
+            for num in nums:
+                count[num] += 1
+            for i in xrange(1, len(count)):
+                count[i] += count[i-1]
+            for i in reversed(xrange(len(nums))):  # inplace but unstable sort
+                if nums[i] < 0:  # processed
+                    continue
+                while i != count[nums[i]]-1:
+                    count[nums[i]] -= 1
+                    nums[count[nums[i]]], nums[i] = ~nums[i], nums[count[nums[i]]]
+                count[nums[i]] -= 1
+                nums[i] = ~nums[i]
+            for i in xrange(len(nums)):
+                nums[i] = ~nums[i]  # restore values
+            if reverse:  # unstable sort
+                nums.reverse()
+    
+        nums = map(int, list(str(num)))
+        inplace_counting_sort(nums)
+        a = b = 0
+        for x in nums:
+            a = a*10+x
+            a, b = b, a
+        return a+b
+
+
+# Time:  O(dlogd) = O(1), d is the number of digits
+# Space: O(d) = O(1)
+# greedy
+class Solution2(object):
+    def minimumSum(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        nums = sorted(map(int, list(str(num))))
+        a = b = 0
+        for x in nums:
+            a = a*10+x
+            a, b = b, a
+        return a+b
