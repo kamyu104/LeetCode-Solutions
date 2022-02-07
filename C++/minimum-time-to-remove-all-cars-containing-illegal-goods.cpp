28219ce78b765fbc6c65d76fb68e15fed4147281
kamyu <kamyu104@users.noreply.github.com>
2022-02-08 05:34:59 +0800

    Update minimum-time-to-remove-all-cars-containing-illegal-goods.cpp

--- C++/minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
+++ C++/minimum-time-to-remove-all-cars-containing-illegal-goods.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int minimumTime(string s) {
         int left = 0;
-        int result = left + size(s);
+        int result = left + (size(s) - 0);
         for (int i = 1; i <= size(s); ++i) {
             left = min(left + 2 * (s[i - 1] == '1'), i);
             result = min(result, left + static_cast<int>(size(s)) - i);
