77d4e56af890af0ff33ad0cb880ebf091c86545c
kamyu <kamyu104@users.noreply.github.com>
2022-02-08 05:12:35 +0800

    Update minimum-time-to-remove-all-cars-containing-illegal-goods.py

--- Python/minimum-time-to-remove-all-cars-containing-illegal-goods.py
+++ Python/minimum-time-to-remove-all-cars-containing-illegal-goods.py
@@ -13,3 +13,23 @@ class Solution(object):
             dp = min(dp+2*(c == '1'), i+1)
             result = min(result, dp+((len(s)-1)-i))
         return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# dp
+class Solution2(object):
+    def minimumTime(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result, right = len(s), [0]*(len(s)+1)
+        for i in reversed(xrange(len(s))):
+            right[i] = min(right[i+1]+2*(s[i] == '1'), len(s)-i)
+        left = 0
+        result = left+right[0]
+        for i in xrange(1, len(s)+1):
+            left = min(left+2*(s[i-1] == '1'), i)     
+            result = min(result, left+right[i])
+        return result
