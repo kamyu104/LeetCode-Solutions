e20774ddc72dacf98ec3514e5ffc4126f1347428
kamyu <kamyu104@users.noreply.github.com>
2022-02-14 13:58:53 +0800

    Update minimum-operations-to-make-the-array-alternating.py

--- Python/minimum-operations-to-make-the-array-alternating.py
+++ Python/minimum-operations-to-make-the-array-alternating.py
@@ -28,3 +28,24 @@ class Solution(object):
             return len(nums)-even[even_top[0]]-(odd[odd_top[0]] if odd_top[0] != -1 else 0)
         return min(len(nums)-even[even_top[0]]-(odd[odd_top[1]] if odd_top[1] != -1 else 0),
                    len(nums)-odd[odd_top[0]]-(even[even_top[1]] if even_top[1] != -1 else 0))
+
+# Time:  O(n)
+# Space: O(n)
+import collections
+
+
+# freq table
+class Solution2(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        even = collections.Counter(nums[i] for i in xrange(0, len(nums), 2))
+        odd = collections.Counter(nums[i] for i in xrange(1, len(nums), 2))
+        even_top = even.most_common(2)  # Time: O(nlogk)
+        odd_top = odd.most_common(2)  # Time: O(nlogk)
+        if not odd_top or even_top[0][0] != odd_top[0][0]:
+            return len(nums)-even_top[0][1]-(odd_top[0][1] if odd_top else 0)
+        return min(len(nums)-even_top[0][1]-(odd_top[1][1] if len(odd_top) == 2 else 0),
+                   len(nums)-odd_top[0][1]-(even_top[1][1] if len(even_top) == 2 else 0))
