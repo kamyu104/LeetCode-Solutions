048b03dccc4127aa298842b545ae897b128600ff
kamyu <kamyu104@users.noreply.github.com>
2022-02-14 14:00:58 +0800

    Update minimum-operations-to-make-the-array-alternating.py

--- Python/minimum-operations-to-make-the-array-alternating.py
+++ Python/minimum-operations-to-make-the-array-alternating.py
@@ -10,10 +10,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        even = collections.Counter(nums[i] for i in xrange(0, len(nums), 2))
-        odd = collections.Counter(nums[i] for i in xrange(1, len(nums), 2))
-        even_top = even.most_common(2)  # Time: O(nlogk)
-        odd_top = odd.most_common(2)  # Time: O(nlogk)
+        even_top = collections.Counter(nums[i] for i in xrange(0, len(nums), 2)).most_common(2)  # Time: O(nlogk)
+        odd_top = collections.Counter(nums[i] for i in xrange(1, len(nums), 2)).most_common(2)  # Time: O(nlogk)
         if not odd_top or even_top[0][0] != odd_top[0][0]:
             return len(nums)-even_top[0][1]-(odd_top[0][1] if odd_top else 0)
         return min(len(nums)-even_top[0][1]-(odd_top[1][1] if len(odd_top) == 2 else 0),
