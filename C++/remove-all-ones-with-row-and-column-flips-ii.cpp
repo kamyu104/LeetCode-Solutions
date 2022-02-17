96ec2dd43f9cb5aaa056a99ed873c55b6c14f585
kamyu <kamyu104@users.noreply.github.com>
2022-02-17 21:42:42 +0800

    Create remove-all-ones-with-row-and-column-flips-ii.cpp

---
+++ C++/remove-all-ones-with-row-and-column-flips-ii.cpp
@@ -0,0 +1,58 @@
+// Time:  O(m * n * 2^(m * n))
+// Space: O(2^(m * n))
+
+// dp, bitmasks
+class Solution {
+public:
+    int removeOnes(vector<vector<int>>& grid) {
+        static const int INF = numeric_limits<int>::max();
+
+        vector<int> rows(size(grid));
+        int bit = 1;
+        for (int i = 0; i < size(grid); ++i) {
+            int mask = 0;
+            for (int j = 0; j < size(grid[0]); ++j) {
+                mask += bit;
+                bit <<= 1;
+            }
+            rows[i] = mask;
+        }
+        vector<int> cols(size(grid[0]));
+        bit = 1;
+        for (int j = 0; j < size(grid[0]); ++j) {
+            int mask = 0;
+            int bit2 = bit;
+            for (int i = 0; i < size(grid); ++i) {
+                mask += bit2;
+                bit2 <<= size(grid[0]);
+                }
+            cols[j] = mask;
+            bit <<= 1;
+        }
+        int full_mask = (1 << (size(grid) * size(grid[0]))) - 1;
+        vector<vector<int>> masks(size(grid), vector<int>(size(grid[0]), full_mask));
+        int target = 0;
+        bit = 1;
+        for (int i = 0; i < size(grid); ++i) {
+            for (int j = 0; j < size(grid[0]); ++j) {
+                target += bit * grid[i][j];
+                masks[i][j] -= (rows[i] + cols[j] - bit);
+                bit <<= 1;
+            }
+        }
+        vector<int> dp(target + 1, INF);
+        dp[0] = 0;
+        for (int mask = 1; mask <= target; ++mask) {
+            for (int i = 0; i < size(grid); ++i) {
+                for (int j = 0; j < size(grid[0]); ++j) {
+                    if (grid[i][j]) {
+                        if (dp[mask & masks[i][j]] != INF) {
+                            dp[mask] = min(dp[mask], dp[mask & masks[i][j]] + 1);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[target];
+    }
+};
