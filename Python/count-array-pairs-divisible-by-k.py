8287c52b94522ed2a89f05df639737e520a628bf
kamyu <kamyu104@users.noreply.github.com>
2022-02-22 20:50:39 +0800

    Update count-array-pairs-divisible-by-k.py

--- Python/count-array-pairs-divisible-by-k.py
+++ Python/count-array-pairs-divisible-by-k.py
@@ -28,3 +28,30 @@ class Solution(object):
                     continue
                 result += cnt[x]*cnt[y] if x != y else cnt[x]*(cnt[x]-1)//2
         return result
+
+
+# Time:  O(nlogk + n * sqrt(k))
+# Space: O(sqrt(k)), number of factors of k is at most sqrt(k)
+import collections
+
+
+# math, combinatorics
+class Solution2(object):
+    def countPairs(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def gcd(x, y):
+            while y:
+                x, y = y, x%y
+            return x
+
+        result = 0
+        gcds = collections.Counter()
+        for x in nums:
+            gcd_i = gcd(x, k)
+            result += sum(cnt for gcd_j, cnt in gcds.iteritems() if gcd_i*gcd_j%k == 0)
+            gcds[gcd_i] += 1
+        return result
