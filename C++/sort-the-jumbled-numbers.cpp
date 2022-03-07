4468bf30a47d7a9149013bcd5ea1db85f356407d
kamyu <kamyu104@users.noreply.github.com>
2022-03-08 03:05:23 +0800

    Create sort-the-jumbled-numbers.cpp

---
+++ C++/sort-the-jumbled-numbers.cpp
@@ -0,0 +1,30 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// sort
+class Solution {
+public:
+    vector<int> sortJumbled(vector<int>& mapping, vector<int>& nums) {
+        const auto& transform = [&mapping](int x) {
+            if (!x) {
+                return mapping[x];
+            }
+            int result = 0;
+            for (int base = 1; x; x /= 10, base *= 10) {
+                result += mapping[x % 10] * base;
+            }
+            return result;
+        };
+
+        vector<pair<int, int>> order;
+        for (int i = 0; i < size(nums); ++i) {
+            order.emplace_back(transform(nums[i]), i);
+        }
+        sort(begin(order), end(order));
+        vector<int> result;
+        for (const auto& [_, i] : order) {
+            result.emplace_back(nums[i]);
+        }
+        return result;
+    }
+};
