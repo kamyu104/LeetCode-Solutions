9f00f5c57f2acfa8146f774899228ee6abe2a726
kamyu <kamyu104@users.noreply.github.com>
2022-03-08 03:05:48 +0800

    Create sort-the-jumbled-numbers.py

---
+++ Python/sort-the-jumbled-numbers.py
@@ -0,0 +1,22 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# sort
+class Solution(object):
+    def sortJumbled(self, mapping, nums):
+        """
+        :type mapping: List[int]
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def transform(mapping, x):
+            if not x:
+                return mapping[x]
+            result, base = 0, 1
+            while x:
+                result += mapping[x%10]*base
+                x //= 10
+                base *= 10
+            return result
+
+        return [nums[i] for _, i in sorted((transform(mapping, nums[i]), i) for i in xrange(len(nums)))]
