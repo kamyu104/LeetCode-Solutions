b290a73c3f946f1172615a8c5bed73a028f7fdb5
kamyu <kamyu104@users.noreply.github.com>
2022-03-08 09:56:20 +0800

    Create all-ancestors-of-a-node-in-a-directed-acyclic-graph.py

---
+++ Python/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -0,0 +1,60 @@
+# Time:  O(|V| * |E|)
+# Space: O(|V| * |E|)
+
+# dfs
+class Solution(object):
+    def getAncestors(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        def iter_dfs(adj, i, result):
+            lookup = [False]*len(adj)
+            stk = [i]
+            while stk:
+                u = stk.pop()
+                for v in reversed(adj[u]):
+                    if lookup[v]:
+                        continue
+                    lookup[v] = True
+                    stk.append(v)
+                    result[v].append(i)
+                    
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+        result = [[] for _ in xrange(n)]
+        for u in xrange(n):
+            iter_dfs(adj, u, result)
+        return result
+
+
+# Time:  O(|V| * |E| * log(|V| * |E|))
+# Space: O(|V| * |E|)
+# topological sort
+class Solution2(object):
+    def getAncestors(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        result = [set() for _ in xrange(n)]
+        in_degree = [0]*n
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            in_degree[v] += 1
+            result[v].add(u)
+        q = [u for u, d in enumerate(in_degree) if not d]
+        while q:
+            new_q = []
+            for u in q:
+                for v in adj[u]:
+                    result[v].update(result[u])
+                    in_degree[v] -= 1
+                    if not in_degree[v]:
+                        new_q.append(v)
+            q = new_q
+        return [sorted(s) for s in result]
