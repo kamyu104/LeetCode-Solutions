8243f26b87ff2970aa56af8b88eb3b3c8ce09a36
kamyu <kamyu104@users.noreply.github.com>
2022-03-08 10:03:04 +0800

    Update all-ancestors-of-a-node-in-a-directed-acyclic-graph.py

--- Python/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
+++ Python/all-ancestors-of-a-node-in-a-directed-acyclic-graph.py
@@ -32,8 +32,43 @@ class Solution(object):
 
 # Time:  O(|V| * |E| * log(|V| * |E|))
 # Space: O(|V| * |E|)
-# topological sort
+# bfs
 class Solution2(object):
+    def getAncestors(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        def bfs(adj, i, result):
+            lookup = [False]*len(adj)
+            q = [i]
+            lookup[i] = True
+            while q:
+                new_q = []
+                for u in q:
+                    for v in adj[u]:
+                        if lookup[v]:
+                            continue
+                        lookup[v] = True
+                        new_q.append(v)
+                        result[i].append(v)
+                q = new_q
+            result[i].sort()
+
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[v].append(u)
+        result = [[] for _ in xrange(n)]
+        for u in xrange(n):
+            bfs(adj, u, result) 
+        return result
+
+
+# Time:  O(|V| * |E| * log(|V| * |E|))
+# Space: O(|V| * |E|)
+# topological sort
+class Solution3(object):
     def getAncestors(self, n, edges):
         """
         :type n: int
