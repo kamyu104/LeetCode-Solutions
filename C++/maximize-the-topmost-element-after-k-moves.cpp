eead157c3cabdccbe5c355b532947514aab8c5bd
kamyu <kamyu104@users.noreply.github.com>
2022-03-14 11:33:27 +0800

    Create maximize-the-topmost-element-after-k-moves.cpp

---
+++ C++/maximize-the-topmost-element-after-k-moves.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(1)
+
+// constructive algorithms
+class Solution {
+public:
+    int maximumTop(vector<int>& nums, int k) {
+        if (size(nums) == 1 && k % 2 == 1) {
+            return -1;
+        }
+        if (k <= 1) {
+            return nums[k];
+        }
+        int result = 0;
+        for (int i = 0; i < min(k + 1, static_cast<int>(size(nums))); ++i) {
+            if (i != k - 1) {
+                result = max(result, nums[i]);
+            }
+        }
+        return result;
+    }
+};
