eae8e063b4d2757a7bc2889a89ac0f426f404abd
kamyu <kamyu104@users.noreply.github.com>
2022-03-14 11:28:09 +0800

    Create maximize-the-topmost-element-after-k-moves.py

---
+++ Python/maximize-the-topmost-element-after-k-moves.py
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(1)
+
+# constructive algorithms
+class Solution(object):
+    def maximumTop(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        if len(nums) == 1 == k%2:
+            return -1
+        if k <= 1:
+            return nums[k]
+        return max(nums[i] for i in xrange(min(k+1, len(nums))) if i != k-1)
