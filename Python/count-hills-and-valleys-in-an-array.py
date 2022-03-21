46329a83bb408e5fdf6abbbd3473e9bc6d875a13
kamyu <kamyu104@users.noreply.github.com>
2022-03-21 16:47:17 +0800

    Create count-hills-and-valleys-in-an-array.py

---
+++ Python/count-hills-and-valleys-in-an-array.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(1)
+
+# counting, simulation
+class Solution(object):
+    def countCollisions(self, directions):
+        """
+        :type directions: str
+        :rtype: int
+        """
+        result = cnt = 0
+        smooth = 1
+        for x in directions:
+            if x == 'R':
+                cnt += 1
+            elif x == 'S' or cnt or not smooth:
+                result += cnt+int(x == 'L')
+                cnt = smooth = 0
+        return result
