9dafea2c39cc1e7d60f5bdfdec5c99117530d648
kamyu <kamyu104@users.noreply.github.com>
2022-03-21 17:09:02 +0800

    Update count-hills-and-valleys-in-an-array.cpp

--- C++/count-hills-and-valleys-in-an-array.cpp
+++ C++/count-hills-and-valleys-in-an-array.cpp
@@ -8,10 +8,10 @@ public:
         int result = 0, inc = -1;
         for (int i = 0; i + 1 < size(nums); ++i) {
             if (nums[i] < nums[i + 1]) {
-                result += (inc == 0) ? 1 : 0;
+                result += static_cast<int>(inc == 0);
                 inc = 1;
             } else if (nums[i] > nums[i + 1]) {
-                result += (inc == 1) ? 1 : 0;
+                result += static_cast<int>(inc == 1);
                 inc = 0;
             }
         }
