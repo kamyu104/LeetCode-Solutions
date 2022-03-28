00eb3b07a4a790e416c4f9c5870dde815910d8ac
kamyu <kamyu104@users.noreply.github.com>
2022-03-28 22:36:52 +0800

    Create maximum-value-of-k-coins-from-piles.cpp

---
+++ C++/maximum-value-of-k-coins-from-piles.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n * k^2)
+// Space: O(k)
+
+// dp
+class Solution {
+public:
+    int maxValueOfCoins(vector<vector<int>>& piles, int k) {
+        vector<int> dp(1);
+        for (const auto& pile : piles) {
+            vector<int> new_dp(min(static_cast<int>(size(dp) + size(pile)), k + 1));
+            for (int i = 0; i < size(dp); ++i) {
+                for (int j = 0, curr = 0; j <= min(k - i, static_cast<int>(size(pile))); ++j) {
+                    new_dp[i + j] = max(new_dp[i + j], dp[i] + curr);
+                    curr += j < size(pile) ? pile[j] : 0;
+                }
+            }
+            dp = move(new_dp);
+        }
+        return dp.back();
+    }
+};
