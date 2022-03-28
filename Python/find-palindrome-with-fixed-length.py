61fbc7a3393218bc0011419b487ffa0f63be9cd9
kamyu <kamyu104@users.noreply.github.com>
2022-03-28 21:19:58 +0800

    Create find-palindrome-with-fixed-length.py

---
+++ Python/find-palindrome-with-fixed-length.py
@@ -0,0 +1,18 @@
+# Time:  O(n * l)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def kthPalindrome(self, queries, intLength):
+        """
+        :type queries: List[int]
+        :type intLength: int
+        :rtype: List[int]
+        """
+        def f(l, x):
+            if 10**((l-1)//2)+(x-1) > 10**((l+1)//2)-1:
+                return -1
+            s = str(10**((l-1)//2)+(x-1))
+            return s+s[::-1][l%2:]
+
+        return [f(intLength, x) for x in queries]
