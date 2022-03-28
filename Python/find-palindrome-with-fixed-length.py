4b15cee850fd02f201d107bb047242300beb4e90
kamyu <kamyu104@users.noreply.github.com>
2022-03-28 22:59:11 +0800

    Update find-palindrome-with-fixed-length.py

--- Python/find-palindrome-with-fixed-length.py
+++ Python/find-palindrome-with-fixed-length.py
@@ -9,6 +9,33 @@ class Solution(object):
         :type intLength: int
         :rtype: List[int]
         """
+        def reverse(x):
+            result = 0
+            while x:
+                result = result*10+x%10
+                x //= 10
+            return result
+
+        def f(l, x):
+            x = 10**((l-1)//2)+(x-1)
+            if x > 10**((l+1)//2)-1:
+                return -1
+            return x*10**(l//2)+reverse(x//10 if l%2 else x)
+
+        return [f(intLength, x) for x in queries]
+
+
+
+# Time:  O(n * l)
+# Space: O(l)
+# math
+class Solution2(object):
+    def kthPalindrome(self, queries, intLength):
+        """
+        :type queries: List[int]
+        :type intLength: int
+        :rtype: List[int]
+        """
         def f(l, x):
             if 10**((l-1)//2)+(x-1) > 10**((l+1)//2)-1:
                 return -1
