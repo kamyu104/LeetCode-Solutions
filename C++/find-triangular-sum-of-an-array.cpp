073cdf882086d6bd034246ebe863257b5824860e
kamyu <kamyu104@users.noreply.github.com>
2022-04-03 23:58:43 +0800

    Update find-triangular-sum-of-an-array.cpp

--- C++/find-triangular-sum-of-an-array.cpp
+++ C++/find-triangular-sum-of-an-array.cpp
@@ -31,8 +31,8 @@ public:
             return make_pair(x, cnt);
         };
     
-        unordered_map<int, vector<int>> EXP = {{2, exp_mod(2, 10)}, {5, exp_mod(5, 10)}};  // {2:[6, 2, 4, 8], 5:[5]}
-        unordered_map<int, int> INV = {{1, inv_mod(1, 10)}, {3, inv_mod(3, 10)}, {7, inv_mod(7, 10)}, {9, inv_mod(9, 10)}};  // {1:1, 3:7, 7:3, 9:9}
+        const unordered_map<int, vector<int>> EXP = {{2, exp_mod(2, 10)}, {5, exp_mod(5, 10)}};  // {2:[6, 2, 4, 8], 5:[5]}
+        const unordered_map<int, int> INV = {{1, inv_mod(1, 10)}, {3, inv_mod(3, 10)}, {7, inv_mod(7, 10)}, {9, inv_mod(9, 10)}};  // {1:1, 3:7, 7:3, 9:9}
         int result = 0;
         int nCr = 1;
         unordered_map<int, int> cnt = {{2, 0}, {5, 0}};
@@ -40,9 +40,9 @@ public:
             if (!cnt[2] && !cnt[5]) {
                 result = (result + nCr * nums[i]) % 10;
             } else if (cnt[2] && !cnt[5]) {
-                result = (result + nCr * EXP[2][cnt[2] % size(EXP[2])] * nums[i]) % 10;
+                result = (result + nCr * EXP.at(2)[cnt[2] % size(EXP.at(2))] * nums[i]) % 10;
             } else if (!cnt[2] && cnt[5]) {
-                result = (result + nCr*EXP[5][cnt[5] % size(EXP[5])] * nums[i]) % 10;
+                result = (result + nCr*EXP.at(5)[cnt[5] % size(EXP.at(5))] * nums[i]) % 10;
             }
             int mul = (size(nums) - 1) - i;
             tie(mul, cnt[2]) = factor_p(mul, 2, cnt[2], 1);
@@ -51,7 +51,7 @@ public:
             tie(div, cnt[2]) = factor_p(div, 2, cnt[2], -1);
             tie(div, cnt[5]) = factor_p(div, 5, cnt[5], -1);
             nCr = nCr * mul % 10;
-            nCr = nCr * INV[div % 10] % 10;
+            nCr = nCr * INV.at(div % 10) % 10;
         }
         return result;
     }
