8f515f093385a3d756ec9136cd9376037cdb7e54
kamyu <kamyu104@users.noreply.github.com>
2022-04-04 20:59:05 +0800

    Create encrypt-and-decrypt-strings.py

---
+++ Python/encrypt-and-decrypt-strings.py
@@ -0,0 +1,34 @@
+# Time:  ctor:    O(m + d), m is len(keys), d is sum(len(x) for x in dictionary)
+#        encrypt: O(n)
+#        decrypt: O(n)
+# Space: O(m + d)
+
+import collections
+import itertools
+
+
+# hash
+class Encrypter(object):
+
+    def __init__(self, keys, values, dictionary):
+        """
+        :type keys: List[str]
+        :type values: List[str]
+        :type dictionary: List[str]
+        """
+        self.__lookup = {k: v for k, v in itertools.izip(keys, values)}
+        self.__cnt = collections.Counter(self.encrypt(x) for x in dictionary)
+        
+    def encrypt(self, word1):
+        """
+        :type word1: str
+        :rtype: str
+        """
+        return "".join(self.__lookup[c] for c in word1)
+
+    def decrypt(self, word2):
+        """
+        :type word2: str
+        :rtype: int
+        """
+        return self.__cnt[word2]
