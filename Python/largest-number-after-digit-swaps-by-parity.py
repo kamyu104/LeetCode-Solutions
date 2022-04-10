2d6350e52ad15449bd687535d8e2ffe6151fcbd5
kamyu <kamyu104@users.noreply.github.com>
2022-04-11 02:31:56 +0800

    Update largest-number-after-digit-swaps-by-parity.py

--- Python/largest-number-after-digit-swaps-by-parity.py
+++ Python/largest-number-after-digit-swaps-by-parity.py
@@ -17,13 +17,13 @@ class Solution(object):
 
         cnt = count(num)
         result = 0
-        idx = [0, 1]
+        digit = [0, 1]
         base = 1
         while num:
             num, d = divmod(num, 10)
-            while not cnt[idx[d%2]]:
-                idx[d%2] += 2
-            cnt[idx[d%2]] -= 1
-            result += idx[d%2]*base
+            while not cnt[digit[d%2]]:
+                digit[d%2] += 2
+            cnt[digit[d%2]] -= 1
+            result += digit[d%2]*base
             base *= 10
         return result
