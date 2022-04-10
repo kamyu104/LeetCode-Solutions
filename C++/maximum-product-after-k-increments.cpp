8af924762a1393e05c887e891aa1a0b2463b7070
kamyu <kamyu104@users.noreply.github.com>
2022-04-10 22:56:10 +0800

    Create maximum-product-after-k-increments.cpp

---
+++ C++/maximum-product-after-k-increments.cpp
@@ -0,0 +1,62 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// math, sort
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        static const int MOD = 1e9 + 7;
+        sort(begin(nums), end(nums));
+        auto total = accumulate(cbegin(nums), cend(nums), 0ll);
+        int i = size(nums) - 1;
+        for (; i >= 0; --i) {
+            if (nums[i] * static_cast<int64_t>(i + 1) - total <= k) {
+                break;
+            }
+            total -= nums[i];
+        }
+        const int q = (k + total) / (i + 1);
+        const int r = (k + total) % (i + 1);
+        return ((static_cast<uint64_t>(powmod(q, (i + 1) - r, MOD)) * powmod(q + 1, r, MOD) % MOD) *
+                accumulate(cbegin(nums) + i + 1, cend(nums), 1ull,
+                          [=](const auto& total, const auto& x) {
+                              return total * x % MOD;
+                          })) % MOD;
+    }
+
+private:
+    uint32_t powmod(uint32_t a, uint32_t b, uint32_t mod) {
+        a %= mod;
+        uint64_t result = 1;
+        while (b) {
+            if (b & 1) {
+                result = result * a % mod;
+            }
+            a = uint64_t(a) * a % mod;
+            b >>= 1;
+        }
+        return result;
+    }
+};
+
+
+// Time:  O(n + klogn)
+// Space: O(1)
+// heap
+class Solution2 {
+public:
+    int maximumProduct(vector<int>& nums, int k) {
+        make_heap(begin(nums), end(nums), greater<int>());
+        while (--k >= 0) {
+            int x = nums.front();
+            pop_heap(begin(nums), end(nums), greater<int>()); nums.pop_back();
+            nums.emplace_back(x + 1);
+            push_heap(begin(nums), end(nums), greater<int>());
+        }
+        return accumulate(cbegin(nums), cend(nums), 1ull,
+                          [](const auto& total, const auto& x) {
+                              static const int MOD = 1e9 + 7;
+                              return total * x % MOD;
+                          });
+    }
+};
