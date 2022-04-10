26a9ece64ce2e523df79b4c6d52b1bbb05d38f4e
kamyu <kamyu104@users.noreply.github.com>
2022-04-11 07:28:30 +0800

    Update maximum-total-beauty-of-the-gardens.cpp

--- C++/maximum-total-beauty-of-the-gardens.cpp
+++ C++/maximum-total-beauty-of-the-gardens.cpp
@@ -9,8 +9,8 @@ public:
         sort(begin(flowers), it);
         const long long n = distance(begin(flowers), it);
         long long prefix = 0, suffix = accumulate(begin(flowers), it, 0ll);
-        long long result = 0, left = 0;
-        for (long long right = 0; right <= n; ++right) {
+        long long result = 0;
+        for (long long right = 0, left = 0; right <= n; ++right) {
             const auto total = newFlowers - (static_cast<long long>(n - right) * target - suffix);
             if (right < n) {
                 suffix -= flowers[right];
@@ -18,7 +18,7 @@ public:
             if (total < 0) {
                 continue;
             }
-            while (left + 1 <= right && (left == 0 || (total + prefix) / left > flowers[left])) {
+            while (!(left == right || (left && (total + prefix) / left <= flowers[left]))) {
                 prefix += flowers[left++];
             }
             const auto mn = min(left ? (total + prefix) / left : 0ll, static_cast<long long>(target) - 1);
@@ -32,6 +32,39 @@ public:
 // Space: O(n)
 // sort, prefix sum, greedy, binary search
 class Solution2 {
+public:
+    long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
+        const auto it = partition(begin(flowers), end(flowers), [&target](int x) { return x < target; });
+        sort(begin(flowers), it);
+        const long long n = distance(begin(flowers), it);
+        vector<long long> prefix(n + 1);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + flowers[i];
+        }
+        long long result = 0, suffix = 0;
+        for (long long right = n, left = n; right >= 0; --right) {
+            const auto total = newFlowers - (static_cast<long long>(n - right) * target - suffix);
+            if (right - 1 >= 0) {
+                suffix += flowers[right - 1];
+            }
+            if (total < 0) {
+                break;
+            }
+            left = min(left, right);
+            while (!(left == 0 || (prefix[left] - prefix[left - 1]) * left - prefix[left] <= total)) {
+                --left;
+            }
+            const auto mn = min(left ? (total + prefix[left]) / left : 0ll, static_cast<long long>(target) - 1);
+            result = max(result, mn * partial + (static_cast<long long>(size(flowers)) - right) * full);
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// sort, prefix sum, greedy, binary search
+class Solution3 {
 public:
     long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
         const auto& check = [](const auto& prefix, const auto& total, auto x) {
@@ -76,7 +109,7 @@ public:
 // Time:  O(nlogn)
 // Space: O(n)
 // sort, prefix sum, greedy, binary search
-class Solution3 {
+class Solution4 {
 public:
     long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
         const auto& check = [](const auto& prefix, const auto& total, auto x) {
@@ -100,15 +133,14 @@ public:
         for (int i = 0; i < n; ++i) {
             prefix[i + 1] = prefix[i] + flowers[i];
         }
-        long long suffix = accumulate(begin(flowers), it, 0ll);
-        long long result = 0;
-        for (long long right = 0; right <= n; ++right) {
+        long long result = 0, suffix = 0;
+        for (long long right = n; right >= 0; --right) {
             const auto total = newFlowers - (static_cast<long long>(n - right) * target - suffix);
-            if (right < n) {
-                suffix -= flowers[right];
+            if (right - 1 >= 0) {
+                suffix += flowers[right - 1];
             }
             if (total < 0) {
-                continue;
+                break;
             }
             const auto left = binary_search_right(prefix, total, 1, right);
             const auto mn = min(left ? (total + prefix[left]) / left : 0ll, static_cast<long long>(target) - 1);
