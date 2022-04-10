2e1e378e16696d33fba9d9605c78c1b4cb0c5be3
kamyu <kamyu104@users.noreply.github.com>
2022-04-10 19:48:05 +0800

    Create the-users-that-are-eligible-for-discount.sql

---
+++ MySQL/the-users-that-are-eligible-for-discount.sql
@@ -0,0 +1,10 @@
+# Time:  O(n)
+# Space: O(n)
+
+CREATE PROCEDURE getUserIDs(startDate DATE, endDate DATE, minAmount INT)
+BEGIN
+    SELECT DISTINCT user_id
+    FROM Purchases
+    WHERE time_stamp >= startDate AND time_stamp <= endDate AND amount >= minAmount
+    ORDER BY user_id;
+END
