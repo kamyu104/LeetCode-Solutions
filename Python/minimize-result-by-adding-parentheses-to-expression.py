5ff751cd2d7929c8c3784a1ab2f332e5feaddc37
kamyu <kamyu104@users.noreply.github.com>
2022-04-10 21:31:27 +0800

    Update minimize-result-by-adding-parentheses-to-expression.py

--- Python/minimize-result-by-adding-parentheses-to-expression.py
+++ Python/minimize-result-by-adding-parentheses-to-expression.py
@@ -8,26 +8,26 @@ class Solution(object):
         :type expression: str
         :rtype: str
         """
-        def stoi(expression, i, j):
+        def stoi(s, i, j):
             if i == j:
                 return 1
             result = 0
             for k in xrange(i, j):
-                result = result*10+(ord(expression[k])-ord('0'))
+                result = result*10+(ord(s[k])-ord('0'))
             return result
 
         result = None
         min_val = float("inf")
         pos = expression.index('+')
         for i in xrange(pos):
-            for j in xrange(pos+2, len(expression)+1):
+            for j in xrange(pos+1, len(expression)):
                 val = (stoi(expression, 0, i)*
-                       (stoi(expression, i, pos)+stoi(expression, pos+1, j))*
-                       stoi(expression, j, len(expression)))
+                       (stoi(expression, i, pos)+stoi(expression, pos+1, j+1))*
+                       stoi(expression, j+1, len(expression)))
                 if val < min_val:
                     min_val = val
                     result = (i, j)
-        return expression[:result[0]] + '(' + expression[result[0]:result[1]] + ')' + expression[result[1]:]
+        return expression[:result[0]] + '(' + expression[result[0]:result[1]+1] + ')' + expression[result[1]+1:]
 
     
 # Time:  O(n^2)
@@ -43,11 +43,11 @@ class Solution2(object):
         min_val = float("inf")
         pos = expression.index('+')
         for i in xrange(pos):
-            for j in xrange(pos+2, len(expression)+1):
+            for j in xrange(pos+1, len(expression)):
                 val = (int(expression[:i] or "1")*
-                       (int(expression[i:pos])+int(expression[pos+1:j]))*
-                       int(expression[j:] or "1"))
+                       (int(expression[i:pos])+int(expression[pos+1:j+1]))*
+                       int(expression[j+1:] or "1"))
                 if val < min_val:
                     min_val = val
                     result = (i, j)
-        return expression[:result[0]] + '(' + expression[result[0]:result[1]] + ')' + expression[result[1]:]
+        return expression[:result[0]] + '(' + expression[result[0]:result[1]+1] + ')' + expression[result[1]+1:]
