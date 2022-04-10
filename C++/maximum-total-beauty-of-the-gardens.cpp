c35869a7872fe667ce45a55bca6ea336ec555b61
kamyu <kamyu104@users.noreply.github.com>
2022-04-11 07:00:47 +0800

    Update maximum-total-beauty-of-the-gardens.cpp

--- C++/maximum-total-beauty-of-the-gardens.cpp
+++ C++/maximum-total-beauty-of-the-gardens.cpp
@@ -34,10 +34,10 @@ public:
 class Solution2 {
 public:
     long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
-        const auto& check = [](const auto& prefix, int total, int x) {
+        const auto& check = [](const auto& prefix, const auto& total, auto x) {
             return x && (total + prefix[x]) / x <= prefix[x + 1] - prefix[x];
         };
-        const auto& binary_search = [&check](const auto& prefix, int total, int left, int right) {
+        const auto& binary_search = [&check](const auto& prefix, const auto& total, auto left, auto right) {
             while (left <= right) {
                 const auto mid = left + (right - left) / 2;
                 if (check(prefix, total, mid)) {
@@ -66,6 +66,51 @@ public:
                 continue;
             }
             const auto left = binary_search(prefix, total, 0, right - 1);
+            const auto mn = min(left ? (total + prefix[left]) / left : 0ll, static_cast<long long>(target) - 1);
+            result = max(result, mn * partial + (static_cast<long long>(size(flowers)) - right) * full);
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// sort, prefix sum, greedy, binary search
+class Solution3 {
+public:
+    long long maximumBeauty(vector<int>& flowers, long long newFlowers, int target, int full, int partial) {
+        const auto& check = [](const auto& prefix, const auto& total, auto x) {
+            return x == 0 || (prefix[x] - prefix[x - 1]) * x - prefix[x] <= total;
+        };
+        const auto& binary_search_right = [&check](const auto& prefix, const auto& total, auto left, auto right) {
+            while (left <= right) {
+                const auto mid = left + (right - left) / 2;
+                if (!check(prefix, total, mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return right;
+        };
+        const auto it = partition(begin(flowers), end(flowers), [&target](int x) { return x < target; });
+        sort(begin(flowers), it);
+        const long long n = distance(begin(flowers), it);
+        vector<long long> prefix(n + 1);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + flowers[i];
+        }
+        long long suffix = accumulate(begin(flowers), it, 0ll);
+        long long result = 0;
+        for (long long right = 0; right <= n; ++right) {
+            const auto total = newFlowers - (static_cast<long long>(n - right) * target - suffix);
+            if (right < n) {
+                suffix -= flowers[right];
+            }
+            if (total < 0) {
+                continue;
+            }
+            const auto left = binary_search_right(prefix, total, 1, right);
             const auto mn = min(left ? (total + prefix[left]) / left : 0ll, static_cast<long long>(target) - 1);
             result = max(result, mn * partial + (static_cast<long long>(size(flowers)) - right) * full);
         }
