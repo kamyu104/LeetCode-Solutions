dec05e70b62ea27d136a1d0e28e28fa0ac0d553f
kamyu <kamyu104@users.noreply.github.com>
2022-04-10 19:46:39 +0800

    Update the-number-of-users-that-are-eligible-for-discount.sql

--- MySQL/the-number-of-users-that-are-eligible-for-discount.sql
+++ MySQL/the-number-of-users-that-are-eligible-for-discount.sql
@@ -6,6 +6,6 @@ BEGIN
   RETURN (
     SELECT COUNT(DISTINCT user_id) AS user_cnt
     FROM Purchases
-    WHERE time_stamp >= startDate AND time_stamp <= endDate AND amount >= minamount
+    WHERE time_stamp >= startDate AND time_stamp <= endDate AND amount >= minAmount
   );
 END
