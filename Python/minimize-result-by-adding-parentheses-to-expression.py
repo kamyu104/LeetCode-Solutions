0f376274b941daa64a55f5bdee7e3feb6d2dc3d6
kamyu <kamyu104@users.noreply.github.com>
2022-04-11 11:37:43 +0800

    Update minimize-result-by-adding-parentheses-to-expression.py

--- Python/minimize-result-by-adding-parentheses-to-expression.py
+++ Python/minimize-result-by-adding-parentheses-to-expression.py
@@ -22,6 +22,47 @@ class Solution(object):
         result = None
         min_val = float("inf")
         pos = expression.index('+')
+        left, right = stoi(expression, 0, pos), stoi(expression, pos+1, len(expression))
+        base1 = 10**pos
+        for i in xrange(pos):
+            base2 = 10**(len(expression)-(pos+1)-1)
+            for j in xrange(pos+1, len(expression)):
+                a, b = divmod(left, base1) if left >= base1 else (1, left)
+                c, d = divmod(right, base2) if base2 > 1 else (right, 1)
+                val = a*(b+c)*d
+                if val < min_val:
+                    min_val = val
+                    result = (i, j)
+                base2 //= 10
+            base1 //= 10
+        return "".join(itertools.chain((expression[i] for i in xrange(result[0])),
+                                       '(', (expression[i] for i in xrange(result[0], result[1]+1)), ')',
+                                       (expression[i] for i in xrange(result[1]+1, len(expression)))))
+
+
+# Time:  O(n^3)
+# Space: O(1)
+import itertools
+
+
+# brute force
+class Solution2(object):
+    def minimizeResult(self, expression):
+        """
+        :type expression: str
+        :rtype: str
+        """
+        def stoi(s, i, j):
+            if i == j:
+                return 1
+            result = 0
+            for k in xrange(i, j):
+                result = result*10+(ord(s[k])-ord('0'))
+            return result
+
+        result = None
+        min_val = float("inf")
+        pos = expression.index('+')
         for i in xrange(pos):
             for j in xrange(pos+1, len(expression)):
                 val = (stoi(expression, 0, i)*
@@ -35,10 +76,10 @@ class Solution(object):
                                        (expression[i] for i in xrange(result[1]+1, len(expression)))))
 
     
-# Time:  O(n^2)
+# Time:  O(n^3)
 # Space: O(n)
 # brute force
-class Solution2(object):
+class Solution3(object):
     def minimizeResult(self, expression):
         """
         :type expression: str
