81e570b0d80a3bacc3c96e3f4f848e036c9fb305
kamyu <kamyu104@users.noreply.github.com>
2022-04-11 12:55:24 +0800

    Update minimize-result-by-adding-parentheses-to-expression.cpp

--- C++/minimize-result-by-adding-parentheses-to-expression.cpp
+++ C++/minimize-result-by-adding-parentheses-to-expression.cpp
@@ -45,46 +45,33 @@ public:
     }
 };
 
-// Time:  O(n^3)
-// Space: O(1)
+// Time:  O(n^2)
+// Space: O(2)
 // brute force
 class Solution2 {
 public:
     string minimizeResult(string expression) {
-        const auto& stoi = [](const auto& s, int i, int j) {
-            if (i == j) {
-                return 1;
-            }
-            int result = 0;
-            for (int k = i; k < j; ++k) {
-                result = result * 10 + (s[k] - '0');
-            }
-            return result;
-        };
         const auto& pos = expression.find('+');
+        int left = stoi(expression.substr(0, pos)), right = stoi(expression.substr(pos + 1));  // Space: O(n)
         int min_val = numeric_limits<int>::max();
         pair<int, int> best;
-        for (int i = 0; i < pos; ++i) {
-            for (int j = pos + 1; j < size(expression); ++j) {
-                const auto& val = stoi(expression, 0, i) *
-                                  (stoi(expression, i, pos) + stoi(expression, pos + 1, j + 1)) *
-                                  stoi(expression, j + 1, size(expression));
+        const int base2_init = pow(10, size(expression) - (pos + 1) - 1);
+        for (int i = 0, base1 = pow(10, pos); i < pos; ++i, base1 /= 10) {
+            for (int j = pos + 1, base2 = base2_init; j < size(expression); ++j, base2 /= 10) {
+                const int a = left / base1, b = left % base1;
+                const int c = right / base2, d = right % base2;
+                const auto& val = max(a, 1) * (b + c) * max(d, 1);
                 if (val < min_val) {
                     min_val = val;
                     best = {i, j};
                 }
             }
         }
-        string result;
-        for (int i = 0; i < size(expression); ++i) {
-            if (i == best.first) {
-                result.push_back('(');
-            }
-            result.push_back(expression[i]);
-            if (i == best.second) {
-                result.push_back(')');
-            }
-        }
+        string result = expression.substr(0, best.first);  // Space: O(n)
+        result += "(";
+        result += expression.substr(best.first, best.second + 1 - best.first);  // Space: O(n)
+        result += ")";
+        result += expression.substr(best.second + 1);  // Space: O(n)
         return result;
     }
 };
@@ -102,18 +89,18 @@ public:
             for (int j = pos + 1; j < size(expression); ++j) {
                 const auto& val = (i != 0 ? stoi(expression.substr(0, i)) : 1) *
                                   (stoi(expression.substr(i, pos - i)) + stoi(expression.substr(pos + 1, j + 1 - (pos + 1)))) *
-                                  (j + 1 != size(expression) ? stoi(expression.substr(j + 1)) : 1);
+                                  (j + 1 != size(expression) ? stoi(expression.substr(j + 1)) : 1);  // Space: O(n)
                 if (val < min_val) {
                     min_val = val;
                     best = {i, j};
                 }
             }
         }
-        string result = expression.substr(0, best.first);
+        string result = expression.substr(0, best.first);  // Space: O(n)
         result += "(";
-        result += expression.substr(best.first, best.second + 1 - best.first);
+        result += expression.substr(best.first, best.second + 1 - best.first);  // Space: O(n)
         result += ")";
-        result += expression.substr(best.second + 1);
+        result += expression.substr(best.second + 1);  // Space: O(n)
         return result;
     }
 };
