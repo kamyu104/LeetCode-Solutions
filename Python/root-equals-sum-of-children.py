423a1b9f33959b6af8cdf14413f7763f02a5c143
kamyu <kamyu104@users.noreply.github.com>
2022-04-18 09:11:04 +0800

    Create root-equals-sum-of-children.py

---
+++ Python/root-equals-sum-of-children.py
@@ -0,0 +1,12 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1+num2
