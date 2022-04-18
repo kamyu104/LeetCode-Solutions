f840cb1dcb4aec63a5b62cf04bdc92d9decd608a
kamyu <kamyu104@users.noreply.github.com>
2022-04-18 23:07:50 +0800

    Update longest-path-with-different-adjacent-characters.py

--- Python/longest-path-with-different-adjacent-characters.py
+++ Python/longest-path-with-different-adjacent-characters.py
@@ -1,8 +1,50 @@
 # Time:  O(n)
-# Space: O(h)
+# Space: O(w)
 
-# tree, dfs
+import collections
+
+
+# tree, bfs, topological sort
 class Solution(object):
+    def longestPath(self, parent, s):
+        """
+        :type parent: List[int]
+        :type s: str
+        :rtype: int
+        """
+        def topological_sort(adj, q):
+            top2 = collections.defaultdict(lambda:[0]*2)
+            result = 1
+            while q:
+                new_q = []
+                for (u, l) in q:
+                    for v in adj[u]:
+                        if s[v] != s[u]:
+                            if l > top2[v][0]:
+                                top2[v][0], top2[v][1] = l, top2[v][0]
+                            elif l > top2[v][1]:
+                                top2[v][1] = l
+                        in_degree[v] -= 1
+                        if in_degree[v]:
+                            continue
+                        new_q.append((v, top2[v][0]+1))
+                        result = max(result, top2[v][0]+top2[v][1]+1)
+                        del top2[v]
+                q = new_q
+            return result
+
+        adj = [[] for _ in xrange(len(s))]
+        in_degree = [0]*len(s)
+        for i in xrange(1, len(parent)):
+            adj[i].append(parent[i])
+            in_degree[parent[i]] += 1
+        return topological_sort(adj, [(i, 1) for i, d in enumerate(in_degree) if not d])
+
+
+# Time:  O(n)
+# Space: O(h)
+# tree, dfs
+class Solution2(object):
     def longestPath(self, parent, s):
         """
         :type parent: List[int]
@@ -50,7 +92,7 @@ class Solution(object):
 # Time:  O(n)
 # Space: O(h)
 # tree, dfs
-class Solution2(object):
+class Solution3(object):
     def longestPath(self, parent, s):
         """
         :type parent: List[int]
