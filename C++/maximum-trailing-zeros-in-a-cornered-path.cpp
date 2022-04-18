a84499c3bc45213f9ea77a9d780529d7ba1a15f2
kamyu <kamyu104@users.noreply.github.com>
2022-04-18 22:09:13 +0800

    Create maximum-trailing-zeros-in-a-cornered-path.cpp

---
+++ C++/maximum-trailing-zeros-in-a-cornered-path.cpp
@@ -0,0 +1,53 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+// prefix sum
+class Solution {
+public:
+    int maxTrailingZeros(vector<vector<int>>& grid) {
+        const auto& factor = [](int x) {
+            static const array<int, 2> primes = {2, 5};
+            array<int, 2> cnt = {0, 0};
+            for (int i = 0, y = x; i < size(primes); ++i) {
+                while (y && y % primes[i] == 0) {
+                    y /= primes[i];
+                    ++cnt[i];
+                }
+            }
+            return cnt;
+        };
+        const auto& add = [](const auto& a, const auto& b) {
+            return array<int, 2>{a[0] + b[0], a[1] + b[1]};
+        };
+        const auto& sub = [](const auto& a, const auto& b) {
+            return array<int, 2>{a[0] - b[0], a[1] - b[1]};
+        };
+        const auto& count = [](const auto& a) {
+            return min(a[0], a[1]);
+        };
+
+        vector<vector<array<int, 2>>> left(size(grid), vector<array<int, 2>>(size(grid[0])));
+        for (int i = 0; i < size(grid); ++i) {
+            left[i][0] = factor(grid[i][0]);
+            for (int j = 1; j < size(grid[0]); ++j) {
+                left[i][j] = add(left[i][j - 1], factor(grid[i][j]));
+            }
+        }
+        int result = 0;
+        for (int j = 0; j < size(grid[0]); ++j) {
+            array<int, 2> total = {0, 0};
+            for (int i = 0; i < size(grid); ++i) {
+                total = add(total, factor(grid[i][j]));
+            }
+            array<int, 2> up = {0, 0};
+            for (int i = 0; i < size(grid); ++i) {
+                auto right = j ? sub(left[i].back(), left[i][j - 1]) : left[i].back();
+                result = max({result, count(add(left[i][j], up)), count(add(right, up))});
+                up = add(up, factor(grid[i][j]));
+                const auto& down = sub(total, up);
+                result = max({result, count(add(left[i][j], down)), count(add(right, down))});
+            }
+        }
+        return result;
+    }
+};
