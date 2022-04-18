689b074b06e198d9aa0d356552f0227103d2a0e0
kamyu <kamyu104@users.noreply.github.com>
2022-04-19 02:09:30 +0800

    Update count-positions-on-street-with-required-brightness.cpp

--- C++/count-positions-on-street-with-required-brightness.cpp
+++ C++/count-positions-on-street-with-required-brightness.cpp
@@ -1,18 +1,20 @@
 // Time:  O(n + l)
-// Space: O(n)
+// Space: O(min(n, l))
 
 // line sweep
 class Solution {
 public:
     int meetRequirement(int n, vector<vector<int>>& lights, vector<int>& requirement) {
-        vector<int> cnt(n + 1);
+        unordered_map<int, int> cnt;
         for (int i = 0; i < size(lights); ++i) {
             ++cnt[max(lights[i][0] - lights[i][1], 0)];
             --cnt[min(lights[i][0] + lights[i][1], n - 1) + 1];
         }
         int result = 0, curr = 0;
         for (int i = 0; i < size(requirement); ++i) {
-            curr += cnt[i];
+            if (cnt.count(i)) {
+                curr += cnt[i];
+            }
             if (curr >= requirement[i]) {
                 ++result;
             }
