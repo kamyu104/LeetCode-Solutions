f46235f6f4ab4f9b216b75e8ac1340d2e214fb6b
kamyu <kamyu104@users.noreply.github.com>
2022-04-18 15:24:59 +0800

    Create design-an-atm-machine.py

---
+++ Python/design-an-atm-machine.py
@@ -0,0 +1,35 @@
+# Time:  ctor:     O(1)
+#        deposit:  O(1)
+#        withdraw: O(1)
+# Space: O(1)
+
+# greedy
+class ATM(object):
+
+    def __init__(self):
+        self.__vals = [20, 50, 100, 200, 500]
+        self.__cnt = [0]*len(self.__vals)
+
+    def deposit(self, banknotesCount):
+        """
+        :type banknotesCount: List[int]
+        :rtype: None
+        """
+        for i, x in enumerate(banknotesCount):
+            self.__cnt[i] += x
+
+    def withdraw(self, amount):
+        """
+        :type amount: int
+        :rtype: List[int]
+        """
+        result = [0]*5
+        for i in reversed(xrange(len(self.__vals))):
+            result[i] = min(amount//self.__vals[i], self.__cnt[i])
+            amount -= result[i]*self.__vals[i]
+        if amount:
+            return [-1]
+        for i, c in enumerate(result):
+            self.__cnt[i] -= c
+        return result
+        
