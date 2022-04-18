9e21437db3c8c48a36cc34e7a86dbfee8348cc6c
kamyu <kamyu104@users.noreply.github.com>
2022-04-18 09:33:14 +0800

    Create count-positions-on-street-with-required-brightness.cpp

---
+++ C++/count-positions-on-street-with-required-brightness.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(n)
+
+// line sweep
+class Solution {
+public:
+    int meetRequirement(int n, vector<vector<int>>& lights, vector<int>& requirement) {
+        vector<int> cnt(n + 1);
+        for (int i = 0; i < size(lights); ++i) {
+            ++cnt[max(lights[i][0] - lights[i][1], 0)];
+            --cnt[min(lights[i][0] + lights[i][1], n - 1) + 1];
+        }
+        int result = 0, curr = 0;
+        for (int i = 0; i < size(requirement); ++i) {
+            curr += cnt[i];
+            if (curr >= requirement[i]) {
+                ++result;
+            }
+        }
+        return result;
+    }
+};
