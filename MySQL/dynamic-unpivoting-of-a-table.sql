0bcfaedb7e1c19fe960694cdd095838fe6d3273d
kamyu <kamyu104@users.noreply.github.com>
2022-04-26 21:22:06 +0800

    Create dynamic-unpivoting-of-a-table.sql

---
+++ MySQL/dynamic-unpivoting-of-a-table.sql
@@ -0,0 +1,21 @@
+# Time:  O(n * m)
+# Space: O(n * m)
+
+CREATE PROCEDURE UnpivotProducts()
+BEGIN
+	  SET SESSION group_concat_max_len = 1000000;
+    
+    SET @sql_query = NULL;
+	  SELECT GROUP_CONCAT(
+        CONCAT(
+            'SELECT product_id, "', column_name, '" AS store, ', column_name, ' AS price FROM products WHERE ', column_name, ' IS NOT NULL'
+        ) SEPARATOR ' UNION ALL '
+    )
+    INTO @sql_query
+    FROM information_schema.columns
+    WHERE table_schema = 'test' AND table_name = 'products' AND  column_name != 'product_id';
+    
+    PREPARE sql_query FROM @sql_query;
+    EXECUTE sql_query;
+    DEALLOCATE PREPARE sql_query;
+END
