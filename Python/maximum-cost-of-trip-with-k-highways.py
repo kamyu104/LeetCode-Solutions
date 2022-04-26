9113ca45efe8a52a85df14e40ba0b5eb0fb179b9
kamyu <kamyu104@users.noreply.github.com>
2022-04-26 16:09:55 +0800

    Update maximum-cost-of-trip-with-k-highways.py

--- Python/maximum-cost-of-trip-with-k-highways.py
+++ Python/maximum-cost-of-trip-with-k-highways.py
@@ -1,37 +1,6 @@
 # Time:  O(n^2 * 2^n)
 # Space: O(n * 2^n)
 
-# bfs based dp
-class Solution(object):
-    def maximumCost(self, n, highways, k):
-        """
-        :type n: int
-        :type highways: List[List[int]]
-        :type k: int
-        :rtype: int
-        """
-        if k+1 > n:  # required to optimize, otherwise, TLE or MLE
-            return -1
-        adj = [[] for _ in xrange(n)]
-        for c1, c2, t in highways:
-            adj[c1].append((c2, t))
-            adj[c2].append((c1, t))
-        result = -1
-        dp = [(u, 1<<u, 0) for u in xrange(n)]
-        while dp:
-            new_dp = []
-            for u, mask, total in dp:
-                if bin(mask).count('1') == k+1:
-                    result = max(result, total)
-                for v, t in adj[u]:
-                    if mask&(1<<v) == 0:
-                        new_dp.append((v, mask|(1<<v), total+t))
-            dp = new_dp
-        return result
-
-
-# Time:  O(n^2 * 2^n)
-# Space: O(n * 2^n)
 import itertools
 
 
@@ -71,4 +40,35 @@ class Solution2(object):
                         dp[new_mask][1] = [v]
                         if bin(mask).count('1') == k:
                             result = max(result, dp[new_mask][0])
+        return result
+
+
+# Time:  O(n^2 * 2^n)
+# Space: O(n * 2^n)
+# bfs based dp
+class Solution2(object):
+    def maximumCost(self, n, highways, k):
+        """
+        :type n: int
+        :type highways: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        if k+1 > n:  # required to optimize, otherwise, TLE or MLE
+            return -1
+        adj = [[] for _ in xrange(n)]
+        for c1, c2, t in highways:
+            adj[c1].append((c2, t))
+            adj[c2].append((c1, t))
+        result = -1
+        dp = [(u, 1<<u, 0) for u in xrange(n)]
+        while dp:
+            new_dp = []
+            for u, mask, total in dp:
+                if bin(mask).count('1') == k+1:
+                    result = max(result, total)
+                for v, t in adj[u]:
+                    if mask&(1<<v) == 0:
+                        new_dp.append((v, mask|(1<<v), total+t))
+            dp = new_dp
         return result
