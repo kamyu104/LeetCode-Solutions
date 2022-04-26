3af188b8e4d249a4dae40cffc6de3cdeeeca92bc
kamyu <kamyu104@users.noreply.github.com>
2022-04-26 17:09:46 +0800

    Update intersection-of-multiple-arrays.cpp

--- C++/intersection-of-multiple-arrays.cpp
+++ C++/intersection-of-multiple-arrays.cpp
@@ -1,8 +1,31 @@
+// Time:  O(n * l + r), n = len(nums), l = len(nums[0])
+// Space: O(r), r = max(nums)-min(nums)
+
+// counting sort
+class Solution {
+public:
+    vector<int> intersection(vector<vector<int>>& nums) {
+        static const int MAX_NUM = 1000;
+        vector<int> cnt(MAX_NUM + 1);
+        for (const auto& num : nums) {
+            for (const auto& x : num) {
+                ++cnt[x];
+            }
+        }
+        vector<int> result;
+        for (int i = 1; i <= MAX_NUM; ++i) {
+            if (cnt[i] == size(nums)) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
+
 // Time:  O(n * l + r), n = len(nums), l = len(nums[0]), r = max(nums)-min(nums)
 // Space: O(l)
-
 // set, counting sort
-class Solution {
+class Solution2 {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
         unordered_set<int> lookup(cbegin(nums[0]), cend(nums[0]));
@@ -33,7 +56,7 @@ public:
 // Time:  O(n * l + llogl), n = len(nums), l = len(nums[0])
 // Space: O(l)
 // set, sort
-class Solution2 {
+class Solution3 {
 public:
     vector<int> intersection(vector<vector<int>>& nums) {
         unordered_set<int> lookup(cbegin(nums[0]), cend(nums[0]));
