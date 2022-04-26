ec4ab054b3499ab72a7d900d50fc0d779c15b475
kamyu <kamyu104@users.noreply.github.com>
2022-04-26 19:49:11 +0800

    Create count-number-of-rectangles-containing-each-point.py

---
+++ Python/count-number-of-rectangles-containing-each-point.py
@@ -0,0 +1,27 @@
+# Time:  O(nlogn + m * max_y * logn), n = len(rectangles), m = len(points)
+# Space: O(n)
+
+import bisect
+
+
+# bucket sort, binary search
+class Solution(object):
+    def countRectangles(self, rectangles, points):
+        """
+        :type rectangles: List[List[int]]
+        :type points: List[List[int]]
+        :rtype: List[int]
+        """
+        max_y = max(y for _, y in rectangles)
+        buckets = [[] for _ in xrange(max_y+1)]
+        for x, y in rectangles:
+            buckets[y].append(x)
+        for bucket in buckets:
+            bucket.sort()
+        result = []
+        for x, y in points:
+            cnt = 0
+            for y in xrange(y, max_y+1):
+                cnt += len(buckets[y])-bisect.bisect_left(buckets[y], x)
+            result.append(cnt)
+        return result
