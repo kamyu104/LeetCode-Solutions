7f3bcf0d86b58ad17b9822bfc0f2649a1b7d4a61
kamyu <kamyu104@users.noreply.github.com>
2022-05-03 00:26:30 +0800

    Create escape-the-spreading-fire.py

---
+++ Python/escape-the-spreading-fire.py
@@ -0,0 +1,44 @@
+# Time:  O(m * n)
+# Space: O(m * n)
+
+# bfs
+class Solution(object):
+    def maximumMinutes(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        DIRECTIONS = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        FIRE, WALL, PERSON = 1, 2, 3
+        INF = 10**9
+        def bfs(grid):
+            time = {FIRE:[[INF]*len(grid[0]) for _ in xrange(len(grid))],
+                    PERSON:[[INF]*len(grid[0]) for _ in xrange(len(grid))]}
+            d = 0
+            q = [(r, c, FIRE) for r in xrange(len(grid)) for c in xrange(len(grid[0])) if grid[r][c] == FIRE]
+            q.append((0, 0, PERSON))
+            for r, c, t in q:
+                time[t][r][c] = d
+            while q:
+                new_q = []
+                for r, c, t in q:
+                    for dr, dc in DIRECTIONS:
+                        nr, nc = dr+r, dc+c
+                        if not (0 <= nr < len(grid) and 0 <= nc < len(grid[0]) and
+                                grid[nr][nc] != WALL and time[t][nr][nc] == INF and
+                                (t == FIRE or
+                                 d+1 < time[FIRE][nr][nc] or (d+1 == time[FIRE][nr][nc] and (nr, nc) == (len(grid)-1, len(grid[0])-1)))):
+                            continue
+                        time[t][nr][nc] = d+1
+                        new_q.append((nr, nc, t))
+                q = new_q
+                d += 1
+            return time
+
+        time = bfs(grid)
+        if time[PERSON][-1][-1] == INF:
+            return -1
+        if time[FIRE][-1][-1] == INF:
+            return INF
+        diff = time[FIRE][-1][-1]-time[PERSON][-1][-1]
+        return diff if diff+2 in (time[FIRE][-1][-2]-time[PERSON][-1][-2], time[FIRE][-2][-1]-time[PERSON][-2][-1]) else diff-1
