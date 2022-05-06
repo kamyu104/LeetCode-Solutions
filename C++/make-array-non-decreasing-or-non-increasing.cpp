8b8d2590e3c220b53f5a37b3f7affbbc6b9e874f
kamyu <kamyu104@users.noreply.github.com>
2022-05-07 04:41:20 +0800

    Create make-array-non-decreasing-or-non-increasing.cpp

---
+++ C++/make-array-non-decreasing-or-non-increasing.cpp
@@ -0,0 +1,49 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// greedy, heap
+class Solution {
+public:
+    int convertArray(vector<int>& nums) {
+        const auto& f = [](const auto& begin, const auto& end) {
+            int result = 0;
+            priority_queue<int> max_heap;
+            for (auto it = begin; it != end; ++it) {
+                if (!empty(max_heap) && *it < max_heap.top()) {
+                    result += max_heap.top() - *it; max_heap.pop();
+                    max_heap.emplace(*it);
+                }
+                max_heap.emplace(*it);
+            }
+            return result;
+        };
+      
+        return min(f(cbegin(nums), cend(nums)), f(crbegin(nums), crend(nums)));
+    }    
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+// dp
+class Solution2 {
+public:
+    int convertArray(vector<int>& nums) {
+        unordered_set<int> nums_set(cbegin(nums), cend(nums));
+        vector<int> vals(cbegin(nums_set), cend(nums_set));
+        sort(begin(vals), end(vals));
+        const auto& f = [&](const auto& begin, const auto& end) {
+            int result = 0;
+            unordered_map<int, int> dp;  // dp[i]: min(cnt(j) for j in vals if j <= i)
+            for (auto it = begin; it != end; ++it) {
+                int prefix = numeric_limits<int>::max();
+                for (const auto& i : vals) {
+                    prefix= min(prefix, dp[i] + abs(i - *it));
+                    dp[i] = prefix;
+                }
+            }
+            return dp[vals.back()];
+        };
+      
+        return min(f(cbegin(nums), cend(nums)), f(crbegin(nums), crend(nums)));
+    }    
+};
