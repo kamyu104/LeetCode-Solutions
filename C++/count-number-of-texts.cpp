4fc9d47d7c30a429f05d387bed0be8d4dff180ea
kamyu <kamyu104@users.noreply.github.com>
2022-05-08 22:06:22 +0800

    Update count-number-of-texts.cpp

--- C++/count-number-of-texts.cpp
+++ C++/count-number-of-texts.cpp
@@ -9,7 +9,7 @@ public:
         vector<int> dp(5, 1);
         for (int i = 1; i <= size(pressedKeys); ++i) {
             dp[i % 5] = 0;
-            int start = (pressedKeys[i - 1] == '7' || pressedKeys[i - 1] == '9') ? i - 4 : i - 3;
+            const int start = (pressedKeys[i - 1] == '7' || pressedKeys[i - 1] == '9') ? i - 4 : i - 3;
             for (int j = i - 1; j >= max(start, 0) && pressedKeys[j] == pressedKeys[i - 1]; --j) {
                 dp[i % 5] = (dp[i % 5] + dp[j % 5]) % MOD;
             }            
