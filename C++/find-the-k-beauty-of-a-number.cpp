24f22548935b4de0213eedbec75af6fc130ea88a
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 12:12:01 +0800

    Create find-the-k-beauty-of-a-number.cpp

---
+++ C++/find-the-k-beauty-of-a-number.cpp
@@ -0,0 +1,22 @@
+// Time:  O(logn)
+// Space: O(logn)
+
+// sliding window
+class Solution {
+public:
+    int divisorSubstrings(int num, int k) {
+        int result = 0;
+        string s = to_string(num);
+        int base = pow(10, k - 1);
+        for (int i = 0, curr = 0; i < size(s); ++i) {
+            if (i - k >= 0) {
+                curr -= (s[i - k] - '0') * base;
+            }
+            curr = curr * 10 + (s[i] - '0');
+            if (i + 1 >= k) {
+                result += static_cast<int>(curr && num % curr == 0);
+            }
+        }
+        return result;
+    }
+};
