fef083830ac4aca07cec1ab4bfe0d4c97a4cb630
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 21:46:26 +0800

    Update maximum-white-tiles-covered-by-a-carpet.py

--- Python/maximum-white-tiles-covered-by-a-carpet.py
+++ Python/maximum-white-tiles-covered-by-a-carpet.py
@@ -38,7 +38,6 @@ class Solution2(object):
             if left-1 >= 0:
                 gap -= tiles[left][0]-tiles[left-1][1]-1
             r = l+carpetLen-1
-            result = max(result, min(tiles[right][1]-tiles[left][0]+1, carpetLen)-gap)
             while right+1 < len(tiles) and r+1 >= tiles[right+1][0]:
                 right += 1
                 gap += tiles[right][0]-tiles[right-1][1]-1
