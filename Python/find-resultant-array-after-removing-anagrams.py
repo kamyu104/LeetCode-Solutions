d613da1881ec2416ccbe15f20b8000e36ddf1291
kamyu <kamyu104@users.noreply.github.com>
2022-05-17 04:14:01 +0800

    Update find-resultant-array-after-removing-anagrams.py

--- Python/find-resultant-array-after-removing-anagrams.py
+++ Python/find-resultant-array-after-removing-anagrams.py
@@ -43,3 +43,18 @@ class Solution2(object):
             prev = s
             result.append(x)
         return result
+
+
+# Time:  O(n * llogl)
+# Space: O(l)
+import collections
+
+
+# sort
+class Solution3(object):
+    def removeAnagrams(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        return [words[i] for i in xrange(len(words)) if i == 0 or sorted(words[i-1]) != sorted(words[i])]
