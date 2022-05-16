31909bd05d5b01282308103b8680588aa5c2938c
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 22:31:45 +0800

    Update maximum-white-tiles-covered-by-a-carpet.py

--- Python/maximum-white-tiles-covered-by-a-carpet.py
+++ Python/maximum-white-tiles-covered-by-a-carpet.py
@@ -1,31 +1,8 @@
 # Time:  O(nlogn)
 # Space: O(1)
 
-# sliding window
+# sliding window, optimized from solution3
 class Solution(object):
-    def maximumWhiteTiles(self, tiles, carpetLen):
-        """
-        :type tiles: List[List[int]]
-        :type carpetLen: int
-        :rtype: int
-        """
-        tiles.sort()
-        result = left = gap = 0
-        for right in xrange(len(tiles)):
-            if right-1 >= 0:
-                gap += tiles[right][0]-tiles[right-1][1]-1
-            l = tiles[right][1]-carpetLen+1
-            while tiles[left][1] < l-1:
-                left += 1
-                gap -= tiles[left][0]-tiles[left-1][1]-1
-            result = max(result, min(tiles[right][1]-tiles[left][0]+1, carpetLen)-gap)
-        return result
-
-
-# Time:  O(nlogn)
-# Space: O(1)
-# sliding window
-class Solution2(object):
     def maximumWhiteTiles(self, tiles, carpetLen):
         """
         :type tiles: List[List[int]]
@@ -41,6 +18,29 @@ class Solution2(object):
             while right+1 < len(tiles) and r+1 >= tiles[right+1][0]:
                 right += 1
                 gap += tiles[right][0]-tiles[right-1][1]-1
+            result = max(result, min(tiles[right][1]-tiles[left][0]+1, carpetLen)-gap)
+        return result
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+# sliding window, optimized from solution4
+class Solution2(object):
+    def maximumWhiteTiles(self, tiles, carpetLen):
+        """
+        :type tiles: List[List[int]]
+        :type carpetLen: int
+        :rtype: int
+        """
+        tiles.sort()
+        result = left = gap = 0
+        for right in xrange(len(tiles)):
+            if right-1 >= 0:
+                gap += tiles[right][0]-tiles[right-1][1]-1
+            l = tiles[right][1]-carpetLen+1
+            while tiles[left][1] < l-1:
+                left += 1
+                gap -= tiles[left][0]-tiles[left-1][1]-1
             result = max(result, min(tiles[right][1]-tiles[left][0]+1, carpetLen)-gap)
         return result
 
@@ -67,5 +67,32 @@ class Solution3(object):
             r = l+carpetLen-1
             right = bisect.bisect_right(tiles, [r+1])-1
             extra = max(tiles[right][1]-r, 0)
+            result = max(result, (prefix[right+1]-prefix[left])-extra)
+        return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import bisect
+
+
+# prefix sum, binary search
+class Solution4(object):
+    def maximumWhiteTiles(self, tiles, carpetLen):
+        """
+        :type tiles: List[List[int]]
+        :type carpetLen: int
+        :rtype: int
+        """
+        tiles.sort()
+        rights = [r for _, r in tiles]
+        prefix = [0]*(len(tiles)+1)
+        for i, (l, r) in enumerate(tiles):
+            prefix[i+1] = prefix[i]+(r-l+1)
+        result = 0
+        for right, (_, r) in enumerate(tiles):
+            l = r-carpetLen+1
+            left = bisect.bisect_left(rights, l-1)
+            extra = max(l-tiles[left][0], 0)
             result = max(result, (prefix[right+1]-prefix[left])-extra)
         return result
