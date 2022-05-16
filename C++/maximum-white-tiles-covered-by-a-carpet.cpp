f628319542a52c1f2b32132f9b69f42814788f23
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 21:54:32 +0800

    Update maximum-white-tiles-covered-by-a-carpet.cpp

--- C++/maximum-white-tiles-covered-by-a-carpet.cpp
+++ C++/maximum-white-tiles-covered-by-a-carpet.cpp
@@ -23,9 +23,33 @@ public:
 };
 
 // Time:  O(nlogn)
+// Space: O(1)
+// sliding window
+class Solution2 {
+public:
+    int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
+        sort(begin(tiles), end(tiles));
+        int result = 0;
+        for (int left = 0, right = 0, gap = 0; left < size(tiles); ++left) {
+            if (left - 1 >= 0) {
+                gap -= tiles[left][0] - tiles[left - 1][1] - 1;
+            }
+            const int r = tiles[left][0] + carpetLen - 1;
+            while (right + 1 < size(tiles) && r + 1 >= tiles[right + 1][0]) {
+                ++right;
+                gap += tiles[right][0] - tiles[right - 1][1] - 1;
+            }
+            const int extra = max(tiles[right][1] - r, 0);
+            result = max(result, ((tiles[right][1] - tiles[left][0] + 1) - gap) - extra);
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
 // Space: O(n)
 // prefix sum, binary search
-class Solution2 {
+class Solution3 {
 public:
     int maximumWhiteTiles(vector<vector<int>>& tiles, int carpetLen) {
         sort(begin(tiles), end(tiles));
@@ -34,11 +58,11 @@ public:
             prefix[i + 1] = prefix[i] + (tiles[i][1] - tiles[i][0] + 1);
         }
         int result = 0;
-        for (int i = 0; i < size(tiles); ++i) {
-            const int r = tiles[i][0] + carpetLen - 1;
-            const int j = distance(cbegin(tiles), upper_bound(cbegin(tiles), cend(tiles), vector<int>(1, r + 1)));
-            const int extra = max(tiles[j - 1][1] - r, 0);
-            result = max(result, (prefix[j] - prefix[i]) - extra);
+        for (int left = 0; left < size(tiles); ++left) {
+            const int r = tiles[left][0] + carpetLen - 1;
+            const int right = distance(cbegin(tiles), prev(upper_bound(cbegin(tiles), cend(tiles), vector<int>(1, r + 1))));
+            const int extra = max(tiles[right][1] - r, 0);
+            result = max(result, (prefix[right + 1] - prefix[left]) - extra);
         }
         return result;
     }
