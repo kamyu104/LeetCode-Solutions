04cacb569b7200065b6fab785d2ed8b39a92c279
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 12:12:47 +0800

    Update find-the-k-beauty-of-a-number.cpp

--- C++/find-the-k-beauty-of-a-number.cpp
+++ C++/find-the-k-beauty-of-a-number.cpp
@@ -6,8 +6,8 @@ class Solution {
 public:
     int divisorSubstrings(int num, int k) {
         int result = 0;
-        string s = to_string(num);
-        int base = pow(10, k - 1);
+        const auto& s = to_string(num);
+        const int base = pow(10, k - 1);
         for (int i = 0, curr = 0; i < size(s); ++i) {
             if (i - k >= 0) {
                 curr -= (s[i - k] - '0') * base;
