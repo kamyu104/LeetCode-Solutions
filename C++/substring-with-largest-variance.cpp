68e8e51c0a4cc75681dcb3e3c5bac22c6f68c3b0
kamyu <kamyu104@users.noreply.github.com>
2022-05-16 16:38:53 +0800

    Create substring-with-largest-variance.cpp

---
+++ C++/substring-with-largest-variance.cpp
@@ -0,0 +1,44 @@
+// Time:  O(a^2 * n), a is the size of alphabets
+// Space: O(a)
+
+// kadane's algorithm
+class Solution {
+public:
+    int largestVariance(string s) {
+        const auto& modified_kadane = [&s](const auto& x, const auto& y) {
+            int result = 0;
+            vector<int> lookup(2);
+            count(cbegin(s), cend(s), x);
+            vector<int> remain = {static_cast<int>(count(cbegin(s), cend(s), x)),
+                                  static_cast<int>(count(cbegin(s), cend(s), y))};
+            int curr = 0;
+            for (const auto& c : s) {
+                if (!(c == x || c == y)) {
+                    continue;
+                }
+                lookup[c != x] = 1;
+                --remain[c != x];
+                curr += (c == x) ? 1 : -1;
+                if (curr < 0 && remain[0] && remain[1]) {
+                    curr = lookup[0] = lookup[1] = 0;  // reset states if the remain has both x, y
+                }
+                if (lookup[0] && lookup[1]) {
+                    result = max(result, curr);  // update result if x, y both exist
+                }
+            }
+            return result;
+        };
+        
+        unordered_set<char> alphabets(cbegin(s), cend(s));
+        int result = 0;
+        for (const auto& x : alphabets) {
+            for (const auto& y: alphabets) {
+                if (x == y) {
+                    continue;
+                }
+                result = max(result, modified_kadane(x, y));
+            }
+        }
+        return result;
+    }
+};
