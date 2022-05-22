68a39fdfb365c0431b7ffe81314e3f3218aa3b30
kamyu <kamyu104@users.noreply.github.com>
2022-05-22 15:42:28 +0800

    Create sum-of-total-strength-of-wizards.py

---
+++ Python/sum-of-total-strength-of-wizards.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(n)
+
+# mono stack, prefix sum
+class Solution(object):
+    def totalStrength(self, strength):
+        """
+        :type strength: List[int]
+        :rtype: int
+        """
+        MOD = 10**9+7
+        prefix = [0]*(len(strength)+1)
+        for i in xrange(len(strength)):
+            prefix[i+1] = (prefix[i]+strength[i])%MOD
+        prefix2 = [0]*(len(strength)+1)
+        for i in xrange(len(strength)):
+            prefix2[i+1] = (prefix2[i]+strength[i]*(i+1))%MOD
+        suffix = [0]*(len(strength)+1)
+        for i in reversed(xrange(len(strength))):
+            suffix[i] = (suffix[i+1]+strength[i])%MOD
+        suffix2 = [0]*(len(strength)+1)
+        for i in reversed(xrange(len(strength))):
+            suffix2[i] = (suffix2[i+1]+strength[i]*(len(strength)-i))%MOD
+        stk, result = [-1], 0
+        for i in xrange(len(strength)+1):
+            while stk[-1] != -1 and (i == len(strength) or strength[stk[-1]] >= strength[i]):
+                # strength[i] >= strength[y] for i in [x, y]
+                # strength[i] > strength[y] for i in [y+1, z]
+                x, y, z = stk[-2]+1, stk.pop(), i-1
+                result = (result+(strength[y]*((z-y+1)*((prefix2[y+1]-prefix2[x])-x*(prefix[y+1]-prefix[x]))+
+                                               (y-x+1)*((suffix2[y+1]-suffix2[z+1])-(len(strength)-(z+1))*(suffix[y+1]-suffix[z+1])))))%MOD
+            stk.append(i)
+        return result
