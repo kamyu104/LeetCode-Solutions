49c60bc2052fa38e38e5c711e6be24bcb5d839b7
kamyu <kamyu104@users.noreply.github.com>
2022-05-23 11:59:55 +0800

    Create closest-node-to-path-in-tree.py

---
+++ Python/closest-node-to-path-in-tree.py
@@ -0,0 +1,101 @@
+# Time:  O(nlogn + q)
+# Space: O(n)
+
+import collections
+from functools import partial
+
+
+# Template:
+# https://github.com/kamyu104/FacebookHackerCup-2021/blob/main/Round%202/chainblock2.py
+# Tarjan's Offline LCA Algorithm
+class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
+    def __init__(self, n):
+        self.set = range(n)
+        self.rank = [0]*n
+        self.ancestor = range(n)  # added
+
+    def find_set(self, x):
+        stk = []
+        while self.set[x] != x:  # path compression
+            stk.append(x)
+            x = self.set[x]
+        while stk:
+            self.set[stk.pop()] = x
+        return x
+
+    def union_set(self, x, y):
+        x, y = self.find_set(x), self.find_set(y)
+        if x == y:
+            return False
+        if self.rank[x] > self.rank[y]:  # union by rank
+            x, y = y, x
+        self.set[x] = self.set[y]
+        if self.rank[x] == self.rank[y]:
+            self.rank[y] += 1
+        return True
+
+    def find_ancestor_of_set(self, x):  # added
+        return self.ancestor[self.find_set(x)]
+
+    def update_ancestor_of_set(self, x):  # added
+        self.ancestor[self.find_set(x)] = x
+
+
+class TreeInfos(object):  # Time: O(N), Space: O(N + Q), N is the number of nodes
+    def __init__(self, children, pairs):
+        def preprocess(curr, parent):
+            # depth of the node i
+            D[curr] = 1 if parent == -1 else D[parent]+1
+
+        def divide(curr, parent):
+            stk.append(partial(postprocess, curr))
+            for i in reversed(xrange(len(children[curr]))):
+                child = children[curr][i]
+                if child == parent:
+                    continue
+                stk.append(partial(conquer, child, curr))
+                stk.append(partial(divide, child, curr))
+            stk.append(partial(preprocess, curr, parent))
+
+        def conquer(curr, parent):
+            uf.union_set(curr, parent)
+            uf.update_ancestor_of_set(parent)
+
+        def postprocess(u):
+            lookup[u] = True
+            for v in pairs[u]:
+                if not lookup[v]:
+                    continue
+                lca[min(u, v), max(u, v)] = uf.find_ancestor_of_set(v)
+
+        N = len(children)
+        D, uf, lca = [0]*N, UnionFind(N), {}
+        stk, lookup = [], [False]*N
+        stk.append(partial(divide, 0, -1))
+        while stk:
+            stk.pop()()
+        self.D, self.lca = D, lca
+
+
+# Tarjan's Offline LCA Algorithm
+class Solution(object):
+    def closestNode(self, n, edges, query):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type query: List[List[int]]
+        :rtype: List[int]
+        """
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        pairs = collections.defaultdict(set)
+        for start, end, node in query:
+            if start > end:
+                start, end = end, start
+            pairs[start].add(end), pairs[end].add(start)
+            pairs[start].add(node), pairs[node].add(start)
+            pairs[end].add(node), pairs[node].add(end)
+        tree_infos = TreeInfos(adj, pairs)
+        return [max((tree_infos.lca[min(x, y), max(x, y)] for x, y in ((start, end), (start, node), (end, node))), key=lambda x:tree_infos.D[x]) for start, end, node in query]
