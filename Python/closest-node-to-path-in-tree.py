9282543473632e0be34880a56487e2b8ce42198f
kamyu <kamyu104@users.noreply.github.com>
2022-05-23 14:12:35 +0800

    Update closest-node-to-path-in-tree.py

--- Python/closest-node-to-path-in-tree.py
+++ Python/closest-node-to-path-in-tree.py
@@ -102,6 +102,7 @@ class Solution(object):
 # Space: O(nlogn)
 from functools import partial
 
+
 # Template:
 # https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20H/dependent_events2.py
 class TreeInfos2(object):  # Time: O(NlogN), Space: O(NlogN), N is the number of nodes
@@ -170,4 +171,61 @@ class Solution2(object):
         for u, v in edges:
             adj[u].append(v), adj[v].append(u)
         tree_infos = TreeInfos2(adj)
+        return [max((tree_infos.lca(x, y) for x, y in ((start, end), (start, node), (end, node))), key=lambda x: tree_infos.D[x]) for start, end, node in query]
+
+
+# Time:  O(n + q * h)
+# Space: O(n)
+from functools import partial
+
+
+# Template:
+# https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20H/dependent_events2.py
+class TreeInfos3(object):  # Time: O(NlogN), Space: O(NlogN), N is the number of nodes
+    def __init__(self, children):  # modified
+        def preprocess(curr, parent):
+            # depth of the node i
+            D[curr] = 1 if parent == -1 else D[parent]+1
+            # ancestors of the node i
+            P[curr] = parent
+
+        def divide(curr, parent):
+            for i in reversed(xrange(len(children[curr]))):
+                child = children[curr][i]
+                if child == parent:
+                    continue
+                stk.append(partial(divide, child, curr))
+            stk.append(partial(preprocess, curr, parent))
+
+        N = len(children)
+        D, P = [0]*N, [0]*N
+        stk = []
+        stk.append(partial(divide, 0, -1))
+        while stk:
+            stk.pop()()
+        self.D, self.P = D, P
+
+    def lca(self, a, b):
+        while self.D[a] > self.D[b]:
+            a = self.P[a]
+        while self.D[b] > self.D[a]:
+            b = self.P[b]
+        while a != b:
+            a, b = self.P[a], self.P[b]
+        return a
+
+
+# lca
+class Solution3(object):
+    def closestNode(self, n, edges, query):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type query: List[List[int]]
+        :rtype: List[int]
+        """
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v), adj[v].append(u)
+        tree_infos = TreeInfos3(adj)
         return [max((tree_infos.lca(x, y) for x, y in ((start, end), (start, node), (end, node))), key=lambda x: tree_infos.D[x]) for start, end, node in query]
