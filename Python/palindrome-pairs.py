938b550eba43b4b85d42d14b4ca4c54667795a54
kamyu <kamyu104@users.noreply.github.com>
2022-05-25 18:56:28 +0800

    Update palindrome-pairs.py

--- Python/palindrome-pairs.py
+++ Python/palindrome-pairs.py
@@ -19,22 +19,22 @@ class Solution(object):
             return True
  
         res = []
-        lookup = {}
-        lookup2 = set()
+        lookup = collections.defaultdict(dict)
         for i, word in enumerate(words):
-            lookup[word] = i
-            lookup2.add(len(word))
+            lookup[len(word)][word] = i
 
         for i in xrange(len(words)):
             for j in xrange(len(words[i]) + 1):
-                if j in lookup2 and is_palindrome(words[i], j, len(words[i])-1):
+                if j in lookup and is_palindrome(words[i], j, len(words[i])-1):
                     suffix = words[i][:j][::-1]
-                    if suffix in lookup and lookup[suffix] != i:
-                        res.append([i, lookup[suffix]])
-                if j > 0 and len(words[i])-j in lookup2 and is_palindrome(words[i], 0, j-1):
+                    bucket = lookup[len(suffix)]
+                    if suffix in bucket and bucket[suffix] != i:
+                        res.append([i, bucket[suffix]])
+                if j > 0 and len(words[i])-j in lookup and is_palindrome(words[i], 0, j-1):
                     prefix = words[i][j:][::-1]
-                    if prefix in lookup and lookup[prefix] != i:
-                        res.append([lookup[prefix], i])
+                    bucket = lookup[len(prefix)]
+                    if prefix in bucket and bucket[prefix] != i:
+                        res.append([bucket[prefix], i])
         return res
 
 
