c73b69d45b14cc8ce108160571d5632d404c51db
kamyu <kamyu104@users.noreply.github.com>
2022-05-25 18:56:25 +0800

    Update palindrome-pairs.cpp

--- C++/palindrome-pairs.cpp
+++ C++/palindrome-pairs.cpp
@@ -5,27 +5,27 @@ class Solution {
 public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
         vector<vector<int>> res;
-        unordered_map<string, int> lookup;
-        unordered_set<int> lookup2;
+        unordered_map<int, unordered_map<string, int>> lookup;
         for (int i = 0; i < words.size(); ++i) {
-            lookup[words[i]] = i;
-            lookup2.emplace(size(words[i]));
+            lookup[size(words[i])][words[i]] = i;
         }
 
         for (int i = 0; i < words.size(); ++i) {
             for (int j = 0; j <= words[i].length(); ++j) {
-                if (lookup2.count(j) && is_palindrome(words[i], j, words[i].length() - 1)) {
+                if (lookup.count(j) && is_palindrome(words[i], j, words[i].length() - 1)) {
                     string suffix = words[i].substr(0, j); 
                     reverse(suffix.begin(), suffix.end());
-                    if (lookup.find(suffix) != lookup.end() && i != lookup[suffix]) {
-                        res.push_back({i, lookup[suffix]});
+                    auto& bucket = lookup[size(suffix)];
+                    if (bucket.count(suffix) && i != bucket[suffix]) {
+                        res.push_back({i, bucket[suffix]});
                     }
                 }
-                if (j > 0 && lookup2.count(size(words[i]) - j) && is_palindrome(words[i], 0, j - 1)) {
+                if (j > 0 && lookup.count(size(words[i]) - j) && is_palindrome(words[i], 0, j - 1)) {
                     string prefix = words[i].substr(j);
                     reverse(prefix.begin(), prefix.end());
-                    if (lookup.find(prefix) != lookup.end() && lookup[prefix] != i) {
-                        res.push_back({lookup[prefix], i});
+                    auto& bucket = lookup[size(prefix)];
+                    if (bucket.count(prefix) && bucket[prefix] != i) {
+                        res.push_back({bucket[prefix], i});
                     }
                 }
             }
