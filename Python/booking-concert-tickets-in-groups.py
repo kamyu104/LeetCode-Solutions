57cfff6f95240dbad14926d5a6decdd0307780cb
kamyu <kamyu104@users.noreply.github.com>
2022-05-30 23:03:37 +0800

    Update booking-concert-tickets-in-groups.py

--- Python/booking-concert-tickets-in-groups.py
+++ Python/booking-concert-tickets-in-groups.py
@@ -7,9 +7,9 @@
 # https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/longest-substring-of-one-repeating-character.py
 class SegmentTree(object):
     def __init__(self, N,
-                 build_fn=lambda _: None,
-                 query_fn=lambda x, y: y if x is None else x if y is None else [max(x[0], y[0]), x[1]+y[1]],
-                 update_fn=lambda x, y: y if x is None else [x[0]+y[0], x[1]+y[1]]):
+                 build_fn=lambda _: float("inf"),
+                 query_fn=lambda x, y: y if x is None else x if y is None else min(x, y),
+                 update_fn=lambda x: x):
         self.tree = [None]*(2*2**((N-1).bit_length()))
         self.base = len(self.tree)//2
         self.query_fn = query_fn
@@ -21,7 +21,7 @@ class SegmentTree(object):
 
     def update(self, i, h):
         x = self.base+i
-        self.tree[x] = self.update_fn(self.tree[x], h)
+        self.tree[x] = self.update_fn(h)
         while x > 1:
             x //= 2
             self.tree[x] = self.query_fn(self.tree[x*2], self.tree[x*2+1])
@@ -52,7 +52,9 @@ class BookMyShow(object):
         :type n: int
         :type m: int
         """
-        self.__st = SegmentTree(n, build_fn=lambda _: [m]*2)
+        self.__st = SegmentTree(n,
+                                build_fn=lambda _: [m]*2,
+                                query_fn=lambda x, y: y if x is None else x if y is None else [max(x[0], y[0]), x[1]+y[1]])
         self.__m = m
         self.__i = 0
 
@@ -69,10 +71,11 @@ class BookMyShow(object):
             i = 2*i+int(self.__st.tree[2*i][0] < k)
         if i-self.__st.base > maxRow:
             return []
-        cnt = self.__m-self.__st.tree[i][0]
+        cnt = self.__st.tree[i][0]
+        c = self.__m-cnt
         i -= self.__st.base
-        self.__st.update(i, [-k]*2)
-        return [i, cnt]
+        self.__st.update(i, [cnt-k]*2)
+        return [i, c]
 
     def scatter(self, k, maxRow):
         """
@@ -85,10 +88,10 @@ class BookMyShow(object):
         for i in xrange(self.__i, maxRow+1):
             cnt = self.__st.tree[self.__st.base+i][1]
             c = min(cnt, k)
-            self.__st.update(i, [-c]*2)
             cnt -= c
             if not cnt:
                 self.__i += 1
+            self.__st.update(i, [cnt]*2)
             k -= c
             if not k:
                 break
