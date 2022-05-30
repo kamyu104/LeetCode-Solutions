1afb8a1b38bac4cf96f2c9f9c777d8a15d5b8c99
kamyu <kamyu104@users.noreply.github.com>
2022-05-30 16:11:22 +0800

    Update number-of-visible-people-in-a-queue.cpp

--- C++/number-of-visible-people-in-a-queue.cpp
+++ C++/number-of-visible-people-in-a-queue.cpp
@@ -7,11 +7,14 @@ public:
         vector<int> result(size(heights));
         vector<int> stk;
         for (int i = 0; i < size(heights); ++i) {
-            while (!empty(stk) && heights[stk.back()] <= heights[i]) {
+            while (!empty(stk) && heights[stk.back()] < heights[i]) {
                 ++result[stk.back()]; stk.pop_back();
             }
             if (!empty(stk)) {
                 ++result[stk.back()];
+            }
+            if (!empty(stk) && heights[stk.back()] == heights[i]) {
+                stk.pop_back();
             }
             stk.emplace_back(i);
         }
@@ -32,6 +35,9 @@ public:
                 stk.pop_back();
             }
             result[i] = !empty(stk) ? cnt + 1 : cnt;
+            if (!empty(stk) && heights[stk.back()] == heights[i]) {
+                stk.pop_back();
+            }
             stk.emplace_back(i);
         }
         return result;
