4b7a8ad0a6f75fdd1a7965524d4880a798dbc7bf
kamyu <kamyu104@users.noreply.github.com>
2022-05-31 15:38:53 +0800

    Update minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py

--- Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -13,7 +13,6 @@ class Solution(object):
         """
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         def a_star(grid, b, t):
-            R, C = len(grid), len(grid[0])
             f, dh = 0, 1
             closer, detour = [b], []
             lookup = set()
@@ -29,7 +28,7 @@ class Solution(object):
                 lookup.add(b)
                 for nd, (dr, dc) in enumerate(directions, 1):
                     nb = (b[0]+dr, b[1]+dc)
-                    if not (0 <= nb[0] < R and 0 <= nb[1] < C and nb not in lookup):
+                    if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and nb not in lookup):
                         continue
                     (closer if nd == grid[b[0]][b[1]] else detour).append(nb)
             return -1
@@ -47,25 +46,22 @@ class Solution2(object):
         :rtype: int
         """
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-        R, C = len(grid), len(grid[0])
-        b, t = (0, 0), (R-1, C-1)
+        b, t = (0, 0), (len(grid)-1, len(grid[0])-1)
         dq = collections.deque([(b, 0)])
-        lookup = {b: 0}
+        lookup = set()
         while dq:
             b, d = dq.popleft()
             if b == t:
                 return d
-            if lookup[b] < d:
+            if b in lookup:
                 continue
+            lookup.add(b)
             for nd, (dr, dc) in enumerate(directions, 1):
                 nb = (b[0]+dr, b[1]+dc)
-                cost = 1 if nd != grid[b[0]][b[1]] else 0
-                if not (0 <= nb[0] < R and 0 <= nb[1] < C and
-                        (nb not in lookup or lookup[nb] > d+cost)):
+                if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and nb not in lookup):
                     continue
-                lookup[nb] = d+cost
-                if not cost:
+                if nd == grid[b[0]][b[1]]:
                     dq.appendleft((nb, d))
                 else:
-                    dq.append((nb, d+cost))
+                    dq.append((nb, d+1))
         return -1  # never reach here
