de40a796585237275e1e639db0ce9bddc9da3781
kamyu <kamyu104@users.noreply.github.com>
2022-06-08 02:50:30 +0800

    Create products-with-three-or-more-orders-in-two-consecutive-years.sql

---
+++ MySQL/products-with-three-or-more-orders-in-two-consecutive-years.sql
@@ -0,0 +1,17 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+WITH consecutive_cte AS
+ (SELECT product_id, 
+         YEAR(purchase_date) AS curr_year,
+         LEAD(YEAR(purchase_date), 1) OVER(PARTITION BY product_id
+                                           ORDER BY YEAR(purchase_date)) - YEAR(purchase_date) AS year_diff
+  FROM orders
+  GROUP BY curr_year, product_id
+  HAVING COUNT(order_id) >= 3
+  ORDER BY NULL
+ )
+ 
+SELECT DISTINCT product_id
+FROM consecutive_cte
+WHERE year_diff = 1;
