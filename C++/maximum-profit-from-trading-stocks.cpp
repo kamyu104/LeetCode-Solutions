6b21fb57f32a279020f734272f66011938faebf6
kamyu <kamyu104@users.noreply.github.com>
2022-06-08 02:23:28 +0800

    Create maximum-profit-from-trading-stocks.cpp

---
+++ C++/maximum-profit-from-trading-stocks.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n)
+// Space: O(1)
+
+// dp, optimized from solution2
+class Solution {
+public:
+    int maximumProfit(vector<int>& present, vector<int>& future, int budget) {
+        vector<int> dp(budget + 1);
+        for (int i = 0; i < size(present); ++i) {
+            if (future[i] - present[i] < 0) {
+                continue;
+            }
+            for (int b = budget; b >= present[i]; --b) {
+                dp[b] = max(dp[b], dp[b - present[i]] + (future[i] - present[i]));
+            }
+        }
+        return dp.back();
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    int maximumProfit(vector<int>& present, vector<int>& future, int budget) {
+        vector<vector<int>> dp(2, vector<int>(budget + 1));
+        for (int i = 0; i < size(present); ++i) {
+            for (int b = 0; b <= budget; ++b) {
+                dp[(i + 1) % 2][b] = max(dp[i % 2][b], b - present[i] >= 0 ? dp[i % 2][b - present[i]] + (future[i] - present[i]) : 0);
+            }
+        }
+        return dp[size(present) % 2].back();
+    }
+};
