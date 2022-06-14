15cccd0a21dce3046f4025640cafdf5f9d76b113
kamyu <kamyu104@users.noreply.github.com>
2022-06-15 01:24:26 +0800

    Create fair-distribution-of-cookies.cpp

---
+++ C++/fair-distribution-of-cookies.cpp
@@ -0,0 +1,27 @@
+// Time:  O(k * 3^n)
+// Space: O(2^n)
+
+// dp, submask enumeration
+class Solution {
+public:
+    int distributeCookies(vector<int>& cookies, int k) {
+        vector<int> total(1 << size(cookies));
+        for (int mask = 0; mask < (1 << size(cookies)); ++mask) {
+            for (int i = 0; i < size(cookies); ++i) {
+                if (mask & (1 << i)) {
+                    total[mask] += cookies[i];
+                }
+            }
+        }
+        vector<vector<int>> dp(2, vector<int>(1 << size(cookies), numeric_limits<int>::max()));
+        dp[0][0] = 0;
+        for (int i = 0; i < k; ++i) {
+            for (int mask = 0; mask < (1 << size(cookies)); ++mask) {
+                for (int submask = mask; submask; submask = (submask - 1) & mask) {
+                    dp[(i + 1) % 2][mask] = min(dp[(i + 1) % 2][mask], max(total[submask], dp[i % 2][mask ^ submask]));
+                }
+            }
+        }
+        return dp[k % 2].back();
+    }   
+};
