f9f20ead6937cbedda38b2f8e03145b1cdb39893
kamyu <kamyu104@users.noreply.github.com>
2022-06-14 19:04:40 +0800

    Create strong-password-checker-ii.cpp

---
+++ C++/strong-password-checker-ii.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+// string
+class Solution {
+public:
+    bool strongPasswordCheckerII(string password) {
+        static const unordered_set<char> SPECIAL = {'!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '+'};
+        
+        return size(password) >= 8 &&
+               any_of(cbegin(password), cend(password), ::islower) &&
+               any_of(cbegin(password), cend(password), ::isupper) &&
+               any_of(cbegin(password), cend(password), ::isdigit) &&
+               any_of(cbegin(password), cend(password), [&](const auto& c) { return SPECIAL.count(c); }) &&
+               adjacent_find(cbegin(password), cend(password)) == cend(password);
+              
+    }
+};
