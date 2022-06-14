49c8159c9a990513391a005fefa0e790ad3bbdf6
kamyu <kamyu104@users.noreply.github.com>
2022-06-14 22:51:38 +0800

    Create count-subarrays-with-score-less-than-k.cpp

---
+++ C++/count-subarrays-with-score-less-than-k.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+// sliding window
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, long long k) {
+        int64_t result = 0, total = 0;
+        int left = 0;
+        for (int right = 0; right < size(nums); ++right) {
+            total += nums[right];
+            while (total * (right - left + 1) >= k) {
+                total -= nums[left++];
+            }
+            result += right - left + 1;
+        }
+        return result;
+    }
+};
