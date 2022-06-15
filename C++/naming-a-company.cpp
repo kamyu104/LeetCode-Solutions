9285cbeb482276a1703f73868f45524de61abfb5
kamyu <kamyu104@users.noreply.github.com>
2022-06-16 03:19:22 +0800

    Update naming-a-company.cpp

--- C++/naming-a-company.cpp
+++ C++/naming-a-company.cpp
@@ -1,8 +1,39 @@
-// Time:  O(26^2 * n * l)
+// Time:  O(26 * n * l)
 // Space: O(n * l)
 
 // hash table, math
 class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        vector<unordered_set<string>> lookup(26);
+        for (const auto& idea : ideas) {
+            lookup[idea[0] - 'a'].emplace(idea.substr(1));
+        }
+        vector<vector<int64_t>> cnt(26, vector<int64_t>(26));
+        for (int i = 0; i < 26; ++i) {
+            for (const auto& x : lookup[i]) {
+                for (int j = 0; j < 26; ++j) {
+                    if (j == i) {
+                        continue;
+                    }
+                    cnt[i][j] += !lookup[j].count(x);
+                }
+            }
+        }
+        int64_t result = 0;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = i + 1; j < 26; ++j) {
+                result += cnt[i][j] * cnt[j][i];
+            }
+        }
+        return result * 2;
+    }
+};
+
+// Time:  O(26^2 * n * l)
+// Space: O(n * l)
+// hash table, math
+class Solution2 {
 public:
     long long distinctNames(vector<string>& ideas) {
         vector<unordered_set<string>> lookup(26);
