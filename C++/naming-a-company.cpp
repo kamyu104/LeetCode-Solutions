255a175f9fa4e52e8e6e2c2babf5525762ce6310
kamyu <kamyu104@users.noreply.github.com>
2022-06-16 03:30:19 +0800

    Update naming-a-company.cpp

--- C++/naming-a-company.cpp
+++ C++/naming-a-company.cpp
@@ -1,8 +1,32 @@
-// Time:  O(26 * n * l)
+// Time:  O(26^2 * n * l)
 // Space: O(n * l)
 
 // hash table, math
 class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        vector<unordered_set<string>> lookup(26);
+        for (const auto& idea : ideas) {
+            lookup[idea[0] - 'a'].emplace(idea.substr(1));
+        }
+        int64_t result = 0;
+        for (int i = 0; i < 26; ++i) {
+            for (int j = i + 1; j < 26; ++j) {
+                int common = 0;
+                for (const auto& x : lookup[i]) {
+                    common += lookup[j].count(x);
+                }
+                result += (size(lookup[i]) - common) * (size(lookup[j]) - common);
+            }
+        }
+        return result * 2;
+    }
+};
+
+// Time:  O(26 * n * l)
+// Space: O(n * l)
+// hash table, math
+class Solution2 {
 public:
     long long distinctNames(vector<string>& ideas) {
         vector<unordered_set<string>> lookup(26);
@@ -24,30 +48,6 @@ public:
         for (int i = 0; i < 26; ++i) {
             for (int j = i + 1; j < 26; ++j) {
                 result += cnt[i][j] * cnt[j][i];
-            }
-        }
-        return result * 2;
-    }
-};
-
-// Time:  O(26^2 * n * l)
-// Space: O(n * l)
-// hash table, math
-class Solution2 {
-public:
-    long long distinctNames(vector<string>& ideas) {
-        vector<unordered_set<string>> lookup(26);
-        for (const auto& idea : ideas) {
-            lookup[idea[0] - 'a'].emplace(idea.substr(1));
-        }
-        int64_t result = 0;
-        for (int i = 0; i < 26; ++i) {
-            for (int j = i + 1; j < 26; ++j) {
-                int common = 0;
-                for (const auto& x : lookup[i]) {
-                    common += lookup[j].count(x);
-                }
-                result += (size(lookup[i]) - common) * (size(lookup[j]) - common);
             }
         }
         return result * 2;
