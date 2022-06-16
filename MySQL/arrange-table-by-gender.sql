2b51704ea9165e51437a8d4c4aa31d6e92745786
kamyu <kamyu104@users.noreply.github.com>
2022-06-16 22:02:51 +0800

    Create arrange-table-by-gender.sql

---
+++ MySQL/arrange-table-by-gender.sql
@@ -0,0 +1,18 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+WITH genders_cte AS (
+    SELECT *,
+           RANK() OVER(PARTITION BY gender ORDER BY user_id) AS rnk,
+           CASE gender
+                WHEN 'female' THEN 1
+                WHEN 'other'  THEN 2
+                ELSE 3
+           END AS rnk2
+    FROM Genders
+    ORDER BY NULL
+)
+
+SELECT user_id, gender
+FROM genders_cte
+ORDER BY rnk, rnk2;
