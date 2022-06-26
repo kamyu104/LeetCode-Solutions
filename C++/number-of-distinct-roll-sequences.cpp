298b2dc390dc0fa996d4bd2ed8442f3170debabd
kamyu <kamyu104@users.noreply.github.com>
2022-06-26 19:57:15 +0800

    Create number-of-distinct-roll-sequences.cpp

---
+++ C++/number-of-distinct-roll-sequences.cpp
@@ -0,0 +1,47 @@
+// Time:  O(6^3 * n)
+// Space: O(6^2)
+
+// dp
+class Solution {
+public:
+    int distinctSequences(int n) {
+        static const int MOD = 1e9 + 7;
+        if (n == 1) {
+            return 6;
+        }
+        vector<vector<int>> dp(6, vector<int>(6));
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                if (i != j && gcd(i + 1, j + 1) == 1) {
+                    dp[i][j] = 1;
+                }
+            }
+        }
+        for (int _ = 0; _ < n - 2; ++_) {
+            vector<vector<int>> new_dp(6, vector<int>(6));
+            for (int i = 0; i < 6; ++i) {
+                for (int j = 0; j < 6; ++j) {
+                    if (!dp[i][j]) {
+                        continue;
+                    }
+                    for (int k = 0; k < 6; ++k) {
+                        if (!dp[j][k]) {
+                            continue;
+                        }
+                        if (k != i) {
+                            new_dp[i][j] = (new_dp[i][j] + dp[j][k]) % MOD;
+                        }
+                    }
+                }
+            }
+            dp = move(new_dp);
+        }
+        int result = 0;
+        for (int i = 0; i < 6; ++i) {
+            for (int j = 0; j < 6; ++j) {
+                result = (result + dp[i][j]) % MOD;
+            }
+        }
+        return result;
+    }
+};
