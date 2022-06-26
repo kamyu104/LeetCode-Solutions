dd8fc3d18a0be71cbbe340b434564c792578f318
kamyu <kamyu104@users.noreply.github.com>
2022-06-26 21:03:52 +0800

    Create maximum-score-of-spliced-array.py

---
+++ Python/maximum-score-of-spliced-array.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(1)
+
+# greedy, kadane algorithm
+class Solution(object):
+    def maximumsSplicedArray(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        def kadane(a):
+            result = curr = 0
+            for x in a:
+                curr = max(curr+x, 0)
+                result = max(result, curr)
+            return result
+    
+        return max(sum(nums1)+kadane((nums2[i]-nums1[i] for i in xrange(len(nums1)))),
+                   sum(nums2)+kadane((nums1[i]-nums2[i] for i in xrange(len(nums2)))))
