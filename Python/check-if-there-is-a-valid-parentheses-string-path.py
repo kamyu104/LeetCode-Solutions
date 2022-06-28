0cba3cb026bc75a00d3a373ac53c6bb428538c24
kamyu <kamyu104@users.noreply.github.com>
2022-06-29 03:10:48 +0800

    Update check-if-there-is-a-valid-parentheses-string-path.py

--- Python/check-if-there-is-a-valid-parentheses-string-path.py
+++ Python/check-if-there-is-a-valid-parentheses-string-path.py
@@ -1,11 +1,30 @@
-# Time:  O(m * n)
-# Space: O(n)
+# Time:  O((m * n) * (m + n) / 32)
+# Space: O(n * (m + n) / 32)
 
 import collections
 
 
-# dp, optimized from solution2
+# dp with bitsets
 class Solution(object):
+    def hasValidPath(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: bool
+        """
+        if (len(grid)+len(grid[0])-1)%2:
+            return False
+        dp = [0]*(len(grid[0])+1)
+        for i in xrange(len(grid)):
+            dp[0] = int(not i)
+            for j in xrange(len(grid[0])):
+                dp[j+1] = (dp[j]|dp[j+1])<<1 if grid[i][j] == '(' else (dp[j]|dp[j+1])>>1
+        return dp[-1]&1
+
+
+# Time:  O(m * n)
+# Space: O(n)
+# dp, optimized from solution1 (wrong answer)
+class Solution_WA(object):
     def hasValidPath(self, grid):
         """
         :type grid: List[List[str]]
@@ -23,23 +42,4 @@ class Solution(object):
                     dp[j+1] = [float("inf"), float("-inf")]
                 else:
                     dp[j+1][0] = max(dp[j+1][0], dp[j+1][1]%2)
-        return dp[len(grid[0])][0] == 0
-
-
-# Time:  O((m * n) * (m + n) / 32)
-# Space: O(n * (m + n) / 32)
-# dp
-class Solution2(object):
-    def hasValidPath(self, grid):
-        """
-        :type grid: List[List[str]]
-        :rtype: bool
-        """
-        if (len(grid)+len(grid[0])-1)%2:
-            return False
-        dp = [0]*(len(grid[0])+1)
-        for i in xrange(len(grid)):
-            dp[0] = int(not i)
-            for j in xrange(len(grid[0])):
-                dp[j+1] = (dp[j]|dp[j+1])<<1 if grid[i][j] == '(' else (dp[j]|dp[j+1])>>1
-        return dp[-1]&1
+        return dp[-1][0] == 0
