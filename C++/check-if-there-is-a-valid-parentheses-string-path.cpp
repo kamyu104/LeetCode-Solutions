c1619cd71e4dbaf0439329b3d8916153a2711f87
kamyu <kamyu104@users.noreply.github.com>
2022-06-29 03:13:28 +0800

    Update check-if-there-is-a-valid-parentheses-string-path.cpp

--- C++/check-if-there-is-a-valid-parentheses-string-path.cpp
+++ C++/check-if-there-is-a-valid-parentheses-string-path.cpp
@@ -1,8 +1,28 @@
+// Time:  O((m * n) * (m + n) / 32)
+// Space: O(n * (m + n) / 32)
+
+// dp with bitsets
+class Solution {
+public:
+    bool hasValidPath(vector<vector<char>>& grid) {
+        if ((size(grid) + size(grid[0]) - 1) % 2 == 1) {
+            return false;
+        }
+        vector<bitset<100>> dp(size(grid[0]) + 1);
+        for (int i = 0; i < size(grid); ++i) {
+            dp[0][0] = !i;
+            for (int j = 0; j < size(grid[0]); ++j) {
+                dp[j + 1] = (grid[i][j] == '(') ? (dp[j] | dp[j + 1]) << 1: (dp[j] | dp[j + 1]) >> 1;
+            }
+        }
+        return dp.back()[0];
+    }
+};
+
 // Time:  O(m * n)
 // Space: O(n)
-
-// dp, optimized from solution2
-class Solution {
+// dp, optimized from solution1 (wrong answer)
+class Solution_WA {
 public:
     bool hasValidPath(vector<vector<char>>& grid) {
         const int MAX_M = 100;
@@ -24,26 +44,5 @@ public:
             }
         }
         return dp.back().first == 0;
-    }
-};
-
-
-// Time:  O((m * n) * (m + n) / 32)
-// Space: O(n * (m + n) / 32)
-// dp
-class Solution2 {
-public:
-    bool hasValidPath(vector<vector<char>>& grid) {
-        if ((size(grid) + size(grid[0]) - 1) % 2 == 1) {
-            return false;
-        }
-        vector<bitset<100>> dp(size(grid[0]) + 1);
-        for (int i = 0; i < size(grid); ++i) {
-            dp[0][0] = !i;
-            for (int j = 0; j < size(grid[0]); ++j) {
-                dp[j + 1] = (grid[i][j] == '(') ? (dp[j] | dp[j + 1]) << 1: (dp[j] | dp[j + 1]) >> 1;
-            }
-        }
-        return dp.back()[0];
     }
 };
