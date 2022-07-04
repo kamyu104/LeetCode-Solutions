6430cbb23fe88848afffb0b9f7ae0fe5252ce10a
kamyu <kamyu104@users.noreply.github.com>
2022-07-04 18:23:31 +0800

    Update number-of-increasing-paths-in-a-grid.py

--- Python/number-of-increasing-paths-in-a-grid.py
+++ Python/number-of-increasing-paths-in-a-grid.py
@@ -1,8 +1,47 @@
 # Time:  O(m * n)
 # Space: O(m * n)
 
-# top-down dp, memoization
+# topological sort, bottom-up dp
 class Solution(object):
+    def countPaths(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        MOD = 10**9+7
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+        degree = [[0]*len(grid[0]) for _ in xrange(len(grid))]
+        q = []
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                for di, dj in directions:
+                        ni, nj = i+di, j+dj
+                        if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[i][j] > grid[ni][nj]:
+                            degree[i][j] += 1
+                if not degree[i][j]:
+                    q.append((i, j))
+        dp = [[1]*len(grid[0]) for _ in xrange(len(grid))]
+        result = 0
+        while q:
+            new_q = []
+            for i, j in q:
+                result = (result+dp[i][j])%MOD
+                for di, dj in directions:
+                    ni, nj = i+di, j+dj
+                    if 0 <= ni < len(grid) and 0 <= nj < len(grid[0]) and grid[i][j] < grid[ni][nj]:
+                        dp[ni][nj] = (dp[ni][nj]+dp[i][j])%MOD
+                        degree[ni][nj] -= 1
+                        if not degree[ni][nj]:
+                            new_q.append((ni, nj))
+                            
+            q = new_q
+        return result
+
+
+# Time:  O(m * n)
+# Space: O(m * n)
+# top-down dp, memoization
+class Solution2(object):
     def countPaths(self, grid):
         """
         :type grid: List[List[int]]
