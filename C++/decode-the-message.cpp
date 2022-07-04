9fa2e1e0af041e683159b7cafed7d7b6ce3f8d38
kamyu <kamyu104@users.noreply.github.com>
2022-07-04 16:02:30 +0800

    Create decode-the-message.cpp

---
+++ C++/decode-the-message.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n + m)
+// Space: O(1)
+
+class Solution {
+public:
+    string decodeMessage(string key, string message) {
+        vector<int> lookup(26, -1);
+        int i = 0;
+        for (auto x : key) {
+            x -= 'a';
+            if (x < 0 || lookup[x] != -1) {
+                continue;
+            }
+            lookup[x] = i++;
+        }
+        transform(begin(message), end(message), begin(message),
+                  [&](const auto& x) {
+                      return x != ' ' ? 'a' + lookup[x - 'a'] : ' ';
+                  });
+        return message;
+    }
+};
