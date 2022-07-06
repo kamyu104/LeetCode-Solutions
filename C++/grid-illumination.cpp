b3d76ff8d21b58fb161edc68dabb53a4a8304a9e
Lasantha Karunarathne <90686718+Emmakaru@users.noreply.github.com>
2022-07-06 10:38:49 +0530

    updated grid-illumination cpp
    added fixed answer..there was a wrong answer earlyer

--- C++/grid-illumination.cpp
+++ C++/grid-illumination.cpp
@@ -2,62 +2,51 @@
 // Space: O(l)
 
 class Solution {
-public:
-    template <typename T>
-    struct PairHash {
-        size_t operator()(const pair<T, T>& p) const {
-            size_t seed = 0;
-            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            return seed;
-        }
-    };
+ public:
+  vector<int> gridIllumination(int N, vector<vector<int>>& lamps,
+                               vector<vector<int>>& queries) {
+    vector<int> ans;
+    unordered_map<int, int> rows;
+    unordered_map<int, int> cols;
+    unordered_map<int, int> diag1;
+    unordered_map<int, int> diag2;
+    unordered_set<pair<int, int>, pairHash> lampsSet;
 
-public:
-    vector<int> gridIllumination(int N, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
-        static const vector<pair<int, int>> directions = 
-            {{0, -1}, {0, 1}, {-1, 0}, {1, 0},
-             {-1, -1}, {1, -1}, {-1, 1}, {1, 1}};
-
-        unordered_set<pair<int, int>, PairHash<int>> lookup;
-        unordered_map<int, int> row;
-        unordered_map<int, int> col;
-        unordered_map<int, int> diag;
-        unordered_map<int, int> anti;
-        for (const auto& lamp : lamps) {
-            int r, c;
-            tie(r, c) = make_pair(lamp[0], lamp[1]);
-            lookup.emplace(r, c);
-            ++row[r];
-            ++col[c];
-            ++diag[r - c];
-            ++anti[r + c];
-        }
-        
-        vector<int> result;
-        for (const auto& query : queries) {
-            int r, c;
-            tie(r, c) = make_pair(query[0], query[1]);
-            if (row[r] || col[c] ||
-                diag[r - c] || anti[r + c]) {
-                result.emplace_back(1);
-            } else {
-                result.emplace_back(0);
-            }
-            for (const auto& d : directions) {
-                int nr, nc;
-                tie(nr, nc) = make_pair(r + d.first, c + d.second);
-                if (!(0 <= nr && nr < N && 0 <= nc && nc < N &&
-                      lookup.count(make_pair(nr, nc)))) {
-                    continue;
-                }
-                lookup.erase(make_pair(nr, nc));
-                --row[nr];
-                --col[nc];
-                --diag[nr - nc];
-                --anti[nr + nc];
-            }
-        }
-        return result;
+    for (vector<int>& lamp : lamps) {
+      int i = lamp[0];
+      int j = lamp[1];
+      if (lampsSet.insert({i, j}).second) {
+        ++rows[i];
+        ++cols[j];
+        ++diag1[i + j];
+        ++diag2[i - j];
+      }
     }
+
+    for (const auto& q : queries) {
+      int i = q[0];
+      int j = q[1];
+      if (rows[i] || cols[j] || diag1[i + j] || diag2[i - j]) {
+        ans.push_back(1);
+        for (int y = max(0, i - 1); y < min(N, i + 2); ++y)
+          for (int x = max(0, j - 1); x < min(N, j + 2); ++x)
+            if (lampsSet.erase({y, x})) {
+              --rows[y];
+              --cols[x];
+              --diag1[y + x];
+              --diag2[y - x];
+            }
+      } else
+        ans.push_back(0);
+    }
+
+    return ans;
+  }
+
+ private:
+  struct pairHash {
+    size_t operator()(const pair<int, int>& p) const {
+      return p.first ^ p.second;
+    }
+  };
 };
