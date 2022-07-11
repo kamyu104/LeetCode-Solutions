b2728dc885422459c27ef7bc5357a2d802aed33e
kamyu <kamyu104@users.noreply.github.com>
2022-07-11 19:48:29 +0800

    Update minimum-amount-of-time-to-fill-cups.cpp

--- C++/minimum-amount-of-time-to-fill-cups.cpp
+++ C++/minimum-amount-of-time-to-fill-cups.cpp
@@ -1,11 +1,23 @@
 // Time:  O(1)
 // Space: O(1)
 
-// constructive algorithms
+// math
 class Solution {
 public:
     int fillCups(vector<int>& amount) {
         return max(*max_element(cbegin(amount), cend(amount)),
                    (accumulate(cbegin(amount), cend(amount), 0) + 1) / 2);
+    }
+};
+
+// Time:  O(1)
+// Space: O(1)
+// constructive algorithms
+class Solution2 {
+public:
+    int fillCups(vector<int>& amount) {
+        const int mx = *max_element(cbegin(amount), cend(amount));
+        const int total = accumulate(cbegin(amount), cend(amount), 0);
+        return total - mx <= mx ? mx : (total + 1) / 2;
     }
 };
