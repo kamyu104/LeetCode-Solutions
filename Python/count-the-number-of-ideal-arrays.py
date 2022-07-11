0112ac220928362b7c695433fb08151378581567
kamyu <kamyu104@users.noreply.github.com>
2022-07-12 04:24:21 +0800

    Update count-the-number-of-ideal-arrays.py

--- Python/count-the-number-of-ideal-arrays.py
+++ Python/count-the-number-of-ideal-arrays.py
@@ -1,11 +1,65 @@
-# Time:  O(n * mlogm)
-# Space: O(n + m)
+# Time:  O(sqrt(m) + nlogm + n * sqrt(m) / log(sqrt(m)))
+# Space: O(sqrt(m))
 
 import collections
 
 
-# dp, combinatorics
+# dp, factorization, combinatorics
 class Solution(object):
+    def idealArrays(self, n, maxValue):
+        """
+        :type n: int
+        :type maxValue: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        fact, inv, inv_fact = [[1]*2 for _ in xrange(3)]
+        def nCr(n, k):
+            while len(inv) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(inv) % MOD)
+                inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+                inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
+            return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
+
+        def linear_sieve_of_eratosthenes(n):  # Time: O(n), Space: O(n)
+            primes = []
+            spf = [-1]*(n+1)  # the smallest prime factor
+            for i in xrange(2, n+1):
+                if spf[i] == -1:
+                    spf[i] = i
+                    primes.append(i)
+                for p in primes:
+                    if i*p > n or p > spf[i]:
+                        break
+                    spf[i*p] = p
+            return primes
+
+        primes = linear_sieve_of_eratosthenes(int(maxValue**0.5))
+        result = 0
+        for x in xrange(1, maxValue+1):
+            dp = collections.Counter()
+            for p in primes:
+                if x < p:
+                    break
+                while x%p == 0:
+                    dp[p] += 1
+                    x //= p
+            if x != 1:
+                dp[x] += 1
+            total = 1
+            for c in dp.itervalues():
+                total = (total*nCr(n+c-1, c))%MOD  # H(n, c) = nCr(n + c - 1, n)
+            result = (result+total)%MOD
+        return result
+
+
+# Time:  O(n * mlogm)
+# Space: O(n + m)
+import collections
+
+
+# dp, combinatorics
+class Solutio2n(object):
     def idealArrays(self, n, maxValue):
         """
         :type n: int
