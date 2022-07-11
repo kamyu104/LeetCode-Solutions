f37ae502cb53657c902ea368e762cfdd62dffd8a
kamyu <kamyu104@users.noreply.github.com>
2022-07-11 19:46:20 +0800

    Update and rename minimum-amount-of-time-to-fill-cups.ph to minimum-amount-of-time-to-fill-cups.py

---
+++ Python/minimum-amount-of-time-to-fill-cups.py
@@ -0,0 +1,23 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def fillCups(self, amount):
+        """
+        :type amount: List[int]
+        :rtype: int
+        """
+        return max(max(amount), (sum(amount)+1)//2)
+
+# Time:  O(1)
+# Space: O(1)
+# constructive algorithms
+class Solution2(object):
+    def fillCups(self, amount):
+        """
+        :type amount: List[int]
+        :rtype: int
+        """
+        mx, total = max(amount), sum(amount)
+        return mx if sum(amount)-mx <= mx else (total+1)//2
