83ac43c0081f7872a8ccd327be7bdb6b6cfa0f77
kamyu <kamyu104@users.noreply.github.com>
2022-07-11 20:06:57 +0800

    Create smallest-number-in-infinite-set.cpp

---
+++ C++/smallest-number-in-infinite-set.cpp
@@ -0,0 +1,32 @@
+// Time:  ctor:        O(1)
+//        popSmallest: O(logn)
+//        addBack:     O(logn)
+// Space: O(n)
+
+// bst
+class SmallestInfiniteSet {
+public:
+    SmallestInfiniteSet() {
+        
+    }
+    
+    int popSmallest() {
+        if (!empty(bst_)) {
+            const int result = *cbegin(bst_);
+            bst_.erase(result);
+            return result;
+        }
+        return n_++;
+    }
+    
+    void addBack(int num) {
+        if (num >= n_ || bst_.count(num)) {
+            return;
+        }
+        bst_.emplace(num);
+    }
+
+private:
+    int n_ = 1;
+    set<int> bst_;
+};
