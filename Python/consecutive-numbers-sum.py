15e5f5dca26ab985862e14eceb4a2d4d4da0a755
kamyu <kamyu104@users.noreply.github.com>
2022-07-12 18:02:58 +0800

    Update consecutive-numbers-sum.py

--- Python/consecutive-numbers-sum.py
+++ Python/consecutive-numbers-sum.py
@@ -10,7 +10,7 @@ class Solution(object):
         # x + x+1 + x+2 + ... + x+l-1 = N = 2^k * M, where k >= 0 and M is odd
         # => l*x + (l-1)*l/2 = 2^k * M
         # => x = (2^k * M - (l-1)*l/2) / l = 2^k * M/l - (l-1)/2 is a positive integer
-        # => l could be 2^(k+1)*c or factor of M (only one of them must be possible) 
+        # => l is either 2^(k+1)*M/f or f (f is factors of M)
         # => the answer is the number of all odd factors of M
         # if prime factorization of N is 2^k * p1^a * p2^b * ..
         # => answer is the number of all odd factors = (a+1) * (b+1) * ...
