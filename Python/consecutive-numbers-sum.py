5b30f84da027be90c5c705c8177db244e4199e4e
kamyu <kamyu104@users.noreply.github.com>
2022-07-12 17:40:27 +0800

    Update consecutive-numbers-sum.py

--- Python/consecutive-numbers-sum.py
+++ Python/consecutive-numbers-sum.py
@@ -10,16 +10,7 @@ class Solution(object):
         # x + x+1 + x+2 + ... + x+l-1 = N = 2^k * M, where k >= 0 and M is odd
         # => l*x + (l-1)*l/2 = 2^k * M
         # => x = (2^k * M - (l-1)*l/2) / l = 2^k * M/l - (l-1)/2 is a positive integer
-        # => l could be 2^(k+1) or any odd factor of M (only one of 2^(k+1) and M must be possible, proof as follows)
-        #    s.t. x = 2^k * M/l - (l-1)/2 is a positive integer, and also unique
-        #
-        #     proof:
-        #     if l = 2^(k+1) is impossible
-        #     <=> 2^k * M/(2^(k+1)) - (2^(k+1)-1)/2 <= 0
-        #     <=> M - 2^(k+1) <= -1
-        #     <=> 2^(k+1) >= M+1
-        #     <=> 2^k * M/M - (M-1)/2 >= 1, l = M is possible
-        #    
+        # => l could be 2^(k+1)*c or factor of M (only one of them must be possible) 
         # => the answer is the number of all odd factors of M
         # if prime factorization of N is 2^k * p1^a * p2^b * ..
         # => answer is the number of all odd factors = (a+1) * (b+1) * ...
