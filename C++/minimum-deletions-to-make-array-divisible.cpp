ce6c056fab9490b5198a00ca78e1d9c6cad66fc2
kamyu <kamyu104@users.noreply.github.com>
2022-07-19 22:58:33 +0800

    Create minimum-deletions-to-make-array-divisible.cpp

---
+++ C++/minimum-deletions-to-make-array-divisible.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n + m + logr), r is max(nums)
+// Space: O(n)
+
+// math, gcd
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& numsDivide) {
+        const int g = accumulate(cbegin(numsDivide), cend(numsDivide), numsDivide[0],
+                                 [](const auto& total, const auto& x) {
+                                     return gcd(total, x);
+                                 });
+        int mn = numeric_limits<int>::max();
+        for (const auto& x : nums) {
+            if (g % x == 0) {
+                mn = min(mn, x);
+            }
+        }
+        return mn != numeric_limits<int>::max() ? accumulate(cbegin(nums), cend(nums), 0,
+                                                             [&](const auto& total, const auto& x) {
+                                                                 return total + static_cast<int>(x < mn);
+                                                             }): -1;
+    }
+};
