bbffd45d9e5b5b0916860bf0e98b65eb46e521c7
kamyu <kamyu104@users.noreply.github.com>
2022-07-19 22:59:36 +0800

    Create minimum-deletions-to-make-array-divisible.py

---
+++ Python/minimum-deletions-to-make-array-divisible.py
@@ -0,0 +1,22 @@
+# Time:  O(n + m + logr), r is max(nums)
+# Space: O(n)
+
+# gcd
+class Solution(object):
+    def minOperations(self, nums, numsDivide):
+        """
+        :type nums: List[int]
+        :type numsDivide: List[int]
+        :rtype: int
+        """
+        def gcd(a, b):  # Time: O(log(min(a, b)))
+            while b:
+                a, b = b, a%b
+            return a
+
+        g = reduce(gcd, numsDivide)
+        mn = float("inf")
+        for x in nums:
+            if g%x == 0:
+                mn = min(mn, x)
+        return sum(x < mn for x in nums) if mn != float("inf") else -1
