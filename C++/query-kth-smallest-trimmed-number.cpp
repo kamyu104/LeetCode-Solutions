43c89ab5abb6533b2f0f691b66285f7952f8eb93
kamyu <kamyu104@users.noreply.github.com>
2022-07-19 22:36:05 +0800

    Create query-kth-smallest-trimmed-number.cpp

---
+++ C++/query-kth-smallest-trimmed-number.cpp
@@ -0,0 +1,92 @@
+// Time:  O(q + n * l)
+// Space: O(l + n + q)
+
+// radix sort
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<vector<pair<int, int>>> lookup(size(nums[0]) + 1);
+        for (int i = 0; i < size(queries); ++i) {
+            lookup[queries[i][1]].emplace_back(queries[i][0], i);
+        }
+        vector<int> idxs(size(nums));
+        iota(begin(idxs), end(idxs), 0);
+        vector<int> result(size(queries));
+        for (int l = 1; l <= size(nums[0]); ++l) {
+            vector<int> cnt(10);
+            for (int i = 0; i < size(idxs); ++i) {
+                const int d = nums[idxs[i]][size(nums[idxs[i]]) - l]- '0';
+                ++cnt[d];
+            }
+            for (int d = 0; d < 9; ++d) {
+                cnt[d + 1] += cnt[d];
+            }
+            vector<int> new_idxs(size(nums));
+            for (int i = size(idxs) - 1; i >= 0; --i) {
+                const int d = nums[idxs[i]][size(nums[idxs[i]]) - l] - '0';
+                --cnt[d];
+                new_idxs[cnt[d]] = idxs[i];
+            }
+            idxs = move(new_idxs);
+            for (const auto& [k, i] : lookup[l]) {
+                result[i] = idxs[k - 1];
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(q * n * t) on average
+// Space: O(n)
+// quick select
+class Solution2 {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<int> idxs(size(nums));
+        iota(begin(idxs), end(idxs), 0);
+        vector<int> result;
+        for (const auto& q : queries) {
+            nth_element(begin(idxs), begin(idxs) + (q[0] - 1), end(idxs),
+                        [&](const auto& a, const auto& b) {
+                            const int cmp = nums[a].compare(size(nums[a]) - q[1], string::npos,
+                                                            nums[b], size(nums[b]) - q[1], string::npos);
+                            return cmp ? cmp < 0 : a < b;
+                        }
+            );
+            result.emplace_back(idxs[q[0] - 1]);
+        }
+        return result;
+    }
+};
+
+// Time:  O(q + nlogn * l)
+// Space: O(l + n + q)
+// sort
+class Solution3 {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        vector<vector<pair<int, int>>> lookup(size(nums[0]) + 1);
+        for (int i = 0; i < size(queries); ++i) {
+            lookup[queries[i][1]].emplace_back(queries[i][0], i);
+        }
+        vector<int> idxs(size(nums));
+        iota(begin(idxs), end(idxs), 0);
+        vector<int> result(size(queries));
+        for (int t = 1; t <= size(nums[0]); ++t) {
+            if (empty(lookup[t])) {
+                continue;
+            }
+            sort(begin(idxs), end(idxs),
+                       [&](const auto& a, const auto& b) {
+                           const int cmp = nums[a].compare(size(nums[a]) - t, string::npos,
+                                                           nums[b], size(nums[b]) - t, string::npos);
+                           return cmp ? cmp < 0 : a < b;
+                       }
+            );
+            for (const auto& [k, i] : lookup[t]) {
+                result[i] = idxs[k - 1];
+            }
+        }
+        return result;
+    }
+};
