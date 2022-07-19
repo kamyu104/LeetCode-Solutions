34a08ea851527029247fbdfffbf207ba40d5c76e
kamyu <kamyu104@users.noreply.github.com>
2022-07-19 20:22:59 +0800

    Create maximum-number-of-pairs-in-array.cpp

---
+++ C++/maximum-number-of-pairs-in-array.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n + r)
+// Space: O(r), r = max(nums)
+
+// freq table
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        static const int MAX_NUM = 100;
+        vector<int> cnt(MAX_NUM + 1);
+        int pair_cnt = 0;
+        for (const auto& x : nums) {
+            cnt[x] ^= 1;
+            if (!cnt[x]) {
+                ++pair_cnt;
+            }
+        }
+        return {pair_cnt, static_cast<int>(size(nums)) - 2 * pair_cnt};
+    }
+};
+
+
+// Time:  O(n)
+// Space: O(r), r = max(nums)
+// freq table
+class Solution2 {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        int pair_cnt = 0;
+        for (const auto& x : nums) {
+            cnt[x] ^= 1;
+            if (!cnt[x]) {
+                ++pair_cnt;
+            }
+        }
+        return {pair_cnt, static_cast<int>(size(nums)) - 2 * pair_cnt};
+    }
+};
