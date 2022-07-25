6f64951eed23e77074f363462dc99ead422e71ab
kamyu <kamyu104@users.noreply.github.com>
2022-07-25 22:40:44 +0800

    Update compute-the-rank-as-a-percentage.sql

--- MySQL/compute-the-rank-as-a-percentage.sql
+++ MySQL/compute-the-rank-as-a-percentage.sql
@@ -4,5 +4,17 @@
 SELECT student_id,
        department_id, 
        ROUND(100 * PERCENT_RANK() OVER (PARTITION BY department_id
-                                        ORDER BY mark DESC) , 2) AS percentage 
+                                        ORDER BY mark DESC)
+             , 2) AS percentage 
+FROM Students;
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+SELECT student_id,
+       department_id, 
+       ROUND(IFNULL(100 * (RANK()   OVER (PARTITION BY department_id ORDER BY mark DESC) - 1) / 
+                          (COUNT(1) OVER (PARTITION BY department_id) - 1)
+                    , 0)
+             , 2) AS percentage 
 FROM Students;
