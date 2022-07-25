3f60473d41637bab4ecab9764fd90f7166880b20
kamyu <kamyu104@users.noreply.github.com>
2022-07-26 03:27:13 +0800

    Create equal-row-and-column-pairs.py

---
+++ Python/equal-row-and-column-pairs.py
@@ -0,0 +1,16 @@
+# Time:  O(n^2)
+# Space: O(n^2)
+
+import collections
+
+
+# hash table
+class Solution(object):
+    def equalPairs(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        cnt1 = collections.Counter(tuple(row) for row in grid)
+        cnt2 = collections.Counter(tuple(col) for col in itertools.izip(*grid))
+        return sum(cnt1[k]*cnt2[k] for k in cnt1.iterkeys() if k in cnt2)
