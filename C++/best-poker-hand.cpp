5a11d2a24e6f254ede65c1d12aa84f7542972604
kamyu <kamyu104@users.noreply.github.com>
2022-07-26 01:49:36 +0800

    Create best-poker-hand.cpp

---
+++ C++/best-poker-hand.cpp
@@ -0,0 +1,18 @@
+// Time:  O(1)
+// Space: O(1)
+
+// freq table
+class Solution {
+public:
+    string bestHand(vector<int>& ranks, vector<char>& suits) {
+        static const vector<string> LOOKUP = {"", "High Card", "Pair", "Three of a Kind", "Three of a Kind", "Three of a Kind"};
+        if (all_of(cbegin(suits), cend(suits), [&](const auto& x) { return x == suits[0]; })) {
+            return "Flush";
+        }
+        vector<int> cnt(13);
+        for (const auto& x : ranks) {
+            ++cnt[x - 1];
+        }
+        return LOOKUP[*max_element(cbegin(cnt), cend(cnt))];
+    }
+};
