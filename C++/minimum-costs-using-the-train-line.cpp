904c2dd273d93e6e4d947e9ddc230398a11adcbb
kamyu <kamyu104@users.noreply.github.com>
2022-08-04 11:05:04 +0800

    Update minimum-costs-using-the-train-line.cpp

--- C++/minimum-costs-using-the-train-line.cpp
+++ C++/minimum-costs-using-the-train-line.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     vector<long long> minimumCosts(vector<int>& regular, vector<int>& express, int expressCost) {
         vector<long long> result;
-        vector<long long> dp = {0, expressCost};
+        vector<long long> dp = {0, expressCost};  // dp[0]: min cost of regular route to curr stop, dp[1]: min cost of express route to curr stop
         for (int i = 0; i < size(regular); ++i) {
             dp = {min(dp[0] + regular[i], dp[1] + express[i]),
                   min(dp[0] + regular[i] + expressCost, dp[1] + express[i])};
