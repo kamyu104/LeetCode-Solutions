0200133889cd9eb0092206d3ec1dd03dd4971767
kamyu <kamyu104@users.noreply.github.com>
2022-08-14 23:30:58 +0800

    Create merge-similar-items.cpp

---
+++ C++/merge-similar-items.cpp
@@ -0,0 +1,22 @@
+// Time:  O((m + n) * log(m + n))
+// Space: O(m + n)
+
+// freq table
+class Solution {
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>>& items1, vector<vector<int>>& items2) {
+        unordered_map<int, int> cnt;
+        for (const auto& x : items1) {
+            cnt[x[0]] += x[1];
+        }
+        for (const auto& x : items2) {
+            cnt[x[0]] += x[1];
+        }
+        vector<vector<int>> result;
+        for (const auto& [k, v] : cnt) {
+            result.push_back({k, v});
+        }
+        sort(begin(result), end(result));
+        return result;
+    }
+};
