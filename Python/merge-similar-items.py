eda8424e5036ec57a264a96b734812eb66378754
kamyu <kamyu104@users.noreply.github.com>
2022-08-14 23:27:28 +0800

    Create merge-similar-items.py

---
+++ Python/merge-similar-items.py
@@ -0,0 +1,12 @@
+# Time:  O((m + n) * log(m + n))
+# Space: O(m + n)
+
+# freq table
+class Solution(object):
+    def mergeSimilarItems(self, items1, items2):
+        """
+        :type items1: List[List[int]]
+        :type items2: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        return sorted((Counter(dict(items1))+Counter(dict(items2))).iteritems())
