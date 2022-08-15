39c2493d49211e17c300fc46f8c81ea64f11d77a
kamyu <kamyu104@users.noreply.github.com>
2022-08-15 15:02:45 +0800

    Create minimize-maximum-value-in-a-grid.py

---
+++ Python/minimize-maximum-value-in-a-grid.py
@@ -0,0 +1,16 @@
+# Time:  O((m * n) * log(m * n))
+# Space: O(m * n)
+
+# sort
+class Solution(object):
+    def minScore(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        idxs = [(i, j) for i in xrange(len(grid)) for j in xrange(len(grid[0]))]
+        idxs.sort(key=lambda x: grid[x[0]][x[1]])
+        row_max, col_max = [0]*len(grid), [0]*len(grid[0])
+        for i, j in idxs:
+            grid[i][j] = row_max[i] = col_max[j] = max(row_max[i], col_max[j])+1
+        return grid
