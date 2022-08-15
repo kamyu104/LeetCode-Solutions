bd1baaec7b503090f9ff2056c8fe5b54ab0575bb
kamyu <kamyu104@users.noreply.github.com>
2022-08-15 10:43:02 +0800

    Update task-scheduler.py

--- Python/task-scheduler.py
+++ Python/task-scheduler.py
@@ -13,9 +13,4 @@ class Solution(object):
         """
         counter = Counter(tasks)
         _, max_count = counter.most_common(1)[0]
-
-        result = (max_count-1) * (n+1)
-        for count in counter.values():
-            if count == max_count:
-                result += 1
-        return max(result, len(tasks))
+        return max((max_count-1) * (n+1) + sum(count == max_count for count in counter.itervalues()), len(tasks))
