4b669c53fbd0c4244deff4001c3ec7b2657bec6f
kamyu <kamyu104@users.noreply.github.com>
2022-08-15 10:55:54 +0800

    Create task-scheduler-ii.cpp

---
+++ C++/task-scheduler-ii.cpp
@@ -0,0 +1,16 @@
+// Time:  O(n)
+// Space: O(n)
+
+// freq table
+class Solution {
+public:
+    long long taskSchedulerII(vector<int>& tasks, int space) {
+        unordered_map<int, int64_t> lookup;
+        int64_t result = 0;
+        for (const auto& t : tasks) {
+            result = max(lookup[t], result + 1);
+            lookup[t] = result + space + 1;
+        }
+        return result;
+    }
+};
