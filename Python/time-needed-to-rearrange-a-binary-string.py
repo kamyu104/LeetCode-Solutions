077e93c1dd26a240c66611de23fc2aede4860014
kamyu <kamyu104@users.noreply.github.com>
2022-08-22 18:34:03 +0800

    Create time-needed-to-rearrange-a-binary-string.py

---
+++ Python/time-needed-to-rearrange-a-binary-string.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(1)
+
+# dp
+class Solution(object):
+    def secondsToRemoveOccurrences(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = cnt = 0
+        for i, c in enumerate(s): 
+            if c == '0':
+                cnt += 1
+                continue
+            if cnt:
+                result = max(result+1, cnt)
+        return result 
