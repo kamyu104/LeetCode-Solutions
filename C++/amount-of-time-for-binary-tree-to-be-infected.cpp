3f320b47331a0cffa2f642336fe070d7d1917acb
kamyu <kamyu104@users.noreply.github.com>
2022-08-23 01:59:01 +0800

    Update amount-of-time-for-binary-tree-to-be-infected.cpp

--- C++/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ C++/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,8 +1,86 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(h)
 
-// bfs
+// iterative dfs
 class Solution {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        const auto& iter_dfs = [&]() {
+            int result = -1;
+            using RET = pair<int, int>;
+            RET ret = {-1, -1};
+            vector<tuple<int, TreeNode *, shared_ptr<RET>, shared_ptr<RET>, RET *>> stk;
+            stk.emplace_back(1, root, nullptr, nullptr, &ret);
+            while (!empty(stk)) {
+                auto [step, curr, left, right, ret] = stk.back(); stk.pop_back();
+                if (step == 1) {
+                    if (!curr) {
+                        continue;
+                    }
+                    auto left = make_shared<RET>(-1, -1), right = make_shared<RET>(-1, -1);
+                    stk.emplace_back(2, curr, left, right, ret);
+                    stk.emplace_back(1, curr->right, nullptr, nullptr, right.get());
+                    stk.emplace_back(1, curr->left, nullptr, nullptr, left.get());
+                } else if (step == 2) {
+                    int d = -1;
+                    if (curr->val == start) {
+                        d = 0;
+                        result = max(left->first, right->first) + 1;
+                    } else if (left->second >= 0) {
+                        d = left->second + 1;
+                        result = max(result, right->first + 1 + d);
+                    } else if (right->second >= 0) {
+                        d = right->second + 1;
+                        result = max(result, left->first + 1 + d);
+                    }
+                    *ret = make_pair(max(left->first, right->first) + 1, d);  // [height, dist_to_start]
+                }
+            }
+            return result;
+        };
+
+        return iter_dfs();
+    }
+};
+
+
+// Time:  O(n)
+// Space: O(h)
+// dfs
+class Solution2 {
+public:
+    int amountOfTime(TreeNode* root, int start) {
+        int result = -1;
+
+        const function<pair<int, int>(TreeNode *)> dfs = [&](TreeNode *curr) {
+            if (!curr) {
+                return make_pair(-1, -1);
+            }
+            const auto& left = dfs(curr->left);
+            const auto& right = dfs(curr->right);
+            int d = -1;
+            if (curr->val == start) {
+                d = 0;
+                result = max(left.first, right.first) + 1;
+            } else if (left.second >= 0) {
+                d = left.second + 1;
+                result = max(result, right.first + 1 + d);
+            } else if (right.second >= 0) {
+                d = right.second + 1;
+                result = max(result, left.first + 1 + d);
+            }
+            return make_pair(max(left.first, right.first) + 1, d);  // [height, dist_to_start]
+        };
+
+        dfs(root);
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// bfs
+class Solution3 {
 public:
     int amountOfTime(TreeNode* root, int start) {
         const auto& bfs = [](const auto& root) {
