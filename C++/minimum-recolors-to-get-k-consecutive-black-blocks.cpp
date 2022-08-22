d687cef0ef215a1e49e8c5567dc0b9e4506d076c
kamyu <kamyu104@users.noreply.github.com>
2022-08-22 17:51:50 +0800

    Create minimum-recolors-to-get-k-consecutive-black-blocks.cpp

---
+++ C++/minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+// sliding window
+class Solution {
+public:
+    int minimumRecolors(string blocks, int k) {
+        int result = k;
+        for (int i = 0, curr = 0; i < size(blocks); ++i) {
+            curr += static_cast<int>(blocks[i] == 'W');
+            if (i + 1 - k < 0) {
+                continue;
+            }
+            result = min(result, curr);
+            curr -= static_cast<int>(blocks[i] == 'W');
+        }
+        return result;
+    }
+};
