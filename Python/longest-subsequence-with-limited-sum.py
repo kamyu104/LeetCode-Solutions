47dad645e3253c4f5db61c7147209c064b10ee9a
kamyu <kamyu104@users.noreply.github.com>
2022-08-30 21:52:24 +0800

    Create longest-subsequence-with-limited-sum.py

---
+++ Python/longest-subsequence-with-limited-sum.py
@@ -0,0 +1,18 @@
+# Time:  O(nlogn + qlogn)
+# Space: O(1)
+
+import bisect
+
+
+# binary search
+class Solution(object):
+    def answerQueries(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        for i in xrange(len(nums)-1):
+            nums[i+1] += nums[i]
+        return [bisect.bisect_right(nums, q) for q in queries]
