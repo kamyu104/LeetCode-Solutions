e31beee709fcb3f80b752efa69819620c37ca132
kamyu <kamyu104@users.noreply.github.com>
2022-08-31 02:05:16 +0800

    Create build-a-matrix-with-conditions.cpp

---
+++ C++/build-a-matrix-with-conditions.cpp
@@ -0,0 +1,59 @@
+// Time:  O(k + r + c), r = len(rowConditions), c = len(colConditions)
+// Space: O(k + r + c)
+
+// topological sort
+class Solution {
+public:
+    vector<vector<int>> buildMatrix(int k, vector<vector<int>>& rowConditions, vector<vector<int>>& colConditions) {
+        const auto& topological_sort = [&](const auto& conditions) {
+            vector<vector<int>> adj(k);
+            vector<int> in_degree(k);
+            for (const auto& c : conditions) {
+                const int u = c[0] - 1, v = c[1] - 1;
+                adj[u].emplace_back(v);
+                ++in_degree[v];
+            }
+            vector<int> result;
+            vector<int> q;
+            for (int u = 0; u < k; ++u) {
+                if (!in_degree[u]) {
+                    q.emplace_back(u);
+                }
+            }
+            while (!empty(q)) {
+                vector<int> new_q;
+                for (const auto& u : q) {
+                    result.emplace_back(u);
+                    for (const auto& v : adj[u]) {
+                        --in_degree[v];
+                        if (in_degree[v]) {
+                            continue;
+                        }
+                        new_q.emplace_back(v);
+                    }
+                }
+                q = move(new_q);
+            }
+            return result;
+        };
+
+        const auto& row_order = topological_sort(rowConditions);
+        const auto& col_order = topological_sort(colConditions);
+        if (!(size(row_order) == k && size(col_order) == k)) {
+            return {};
+        }
+        vector<int> row_idx(k);
+        for (int i = 0; i < k; ++i) {
+            row_idx[row_order[i]] = i;
+        }
+        vector<int> col_idx(k);
+        for (int i = 0; i < k; ++i) {
+            col_idx[col_order[i]] = i;
+        }
+        vector<vector<int>> result(k, vector<int>(k));
+        for (int i = 0; i < k; ++i) {
+            result[row_idx[i]][col_idx[i]] = i + 1;
+        }
+        return result;
+    }
+};
