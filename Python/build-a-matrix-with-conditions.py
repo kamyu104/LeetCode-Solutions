88f2dfba2642b8925482298f1219dc1e3216f62d
kamyu <kamyu104@users.noreply.github.com>
2022-08-31 02:05:51 +0800

    Create build-a-matrix-with-conditions.py

---
+++ Python/build-a-matrix-with-conditions.py
@@ -0,0 +1,44 @@
+# Time:  O(k + r + c), r = len(rowConditions), c = len(colConditions)
+# Space: O(k + r + c)
+
+# topological sort
+class Solution(object):
+    def buildMatrix(self, k, rowConditions, colConditions):
+        """
+        :type k: int
+        :type rowConditions: List[List[int]]
+        :type colConditions: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        def topological_sort(conditions):
+            adj = [[] for _ in xrange(k)]
+            in_degree = [0]*k
+            for u, v in conditions:
+                u -= 1
+                v -= 1
+                adj[u].append(v)
+                in_degree[v] += 1
+            result = []
+            q = [u for u in xrange(k) if not in_degree[u]]
+            while q:
+                new_q = []
+                for u in q:
+                    result.append(u)
+                    for v in adj[u]:
+                        in_degree[v] -= 1
+                        if in_degree[v]:
+                            continue
+                        new_q.append(v)
+                q = new_q
+            return result
+
+        row_order = topological_sort(rowConditions)
+        col_order = topological_sort(colConditions)
+        if not (len(row_order) == k and len(col_order) == k):
+            return []
+        row_idx = {x:i for i, x in enumerate(row_order)}
+        col_idx = {x:i for i, x in enumerate(col_order)}
+        result = [[0]*k for _ in xrange(k)]
+        for i in xrange(k):
+            result[row_idx[i]][col_idx[i]] = i+1
+        return result
