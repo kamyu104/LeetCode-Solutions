48c5dd84105429179daaad4b2945a4a38ece8747
kamyu <kamyu104@users.noreply.github.com>
2022-08-31 00:41:38 +0800

    Update minimum-amount-of-time-to-collect-garbage.cpp

--- C++/minimum-amount-of-time-to-collect-garbage.cpp
+++ C++/minimum-amount-of-time-to-collect-garbage.cpp
@@ -5,21 +5,26 @@
 class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
-        int result = 0;
-        unordered_map<char, int> lookup;
-        for (int i = size(garbage) - 1; i >= 0; --i) {
-            for (const auto& c : garbage[i]) {
-                if (!lookup.count(c)) {
-                    lookup[c] = i;
+        const auto& find_lasts = [&]() {
+            unordered_map<char, int> lookup;
+            for (int i = size(garbage) - 1; i >= 0; --i) {
+                for (const auto& c : garbage[i]) {
+                    if (!lookup.count(c)) {
+                        lookup[c] = i;
+                        if (size(lookup) == 3) {
+                            return lookup;
+                        }
+                    }
                 }
             }
-            result += size(garbage[i]);
-        }
+            return lookup;
+        };
         vector<int> lasts;
-        for (const auto& [_, v] : lookup) {
+        for (const auto& [_, v] : find_lasts()) {
             lasts.emplace_back(v);
         }
         sort(rbegin(lasts), rend(lasts));
+        int result = 0;
         for (int i = 0, curr = 0; i < size(garbage); ++i) {
             for (; !empty(lasts) && lasts.back() == i; lasts.pop_back()) {
                 result += curr;
@@ -27,6 +32,7 @@ public:
             if (i < size(travel)) {
                 curr += travel[i];
             }
+            result += size(garbage[i]);
         }
         return result;
     }
