1ffad3cc7ab82ad46bc25624cc8c9ce84280660b
kamyu <kamyu104@users.noreply.github.com>
2022-08-30 21:51:32 +0800

    Create longest-subsequence-with-limited-sum.cpp

---
+++ C++/longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,18 @@
+// Time:  O(nlogn + qlogn)
+// Space: O(1)
+
+// binary search
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        sort(begin(nums), end(nums));
+        for (int i = 0; i + 1 < size(nums); ++i) {
+            nums[i + 1] += nums[i];
+        }
+        vector<int> result;
+        for (const auto& q : queries) {
+            result.emplace_back(distance(cbegin(nums), upper_bound(cbegin(nums), cend(nums), q)));
+        }
+        return result;
+    }
+};
