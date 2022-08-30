8e4181b1a2157c3e8fe237ac654c2b3499cdfd9c
kamyu <kamyu104@users.noreply.github.com>
2022-08-31 00:31:12 +0800

    Update minimum-amount-of-time-to-collect-garbage.cpp

--- C++/minimum-amount-of-time-to-collect-garbage.cpp
+++ C++/minimum-amount-of-time-to-collect-garbage.cpp
@@ -6,6 +6,39 @@ class Solution {
 public:
     int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         int result = 0;
+        unordered_map<char, int> lookup;
+        for (int i = size(garbage) - 1; i >= 0; --i) {
+            for (const auto& c : garbage[i]) {
+                if (!lookup.count(c)) {
+                    lookup[c] = i;
+                }
+            }
+            result += size(garbage[i]);
+        }
+        vector<int> lasts;
+        for (const auto& [_, v] : lookup) {
+            lasts.emplace_back(v);
+        }
+        sort(rbegin(lasts), rend(lasts));
+        for (int i = 0, curr = 0; i < size(garbage); ++i) {
+            for (; !empty(lasts) && lasts.back() == i; lasts.pop_back()) {
+                result += curr;
+            }
+            if (i < size(travel)) {
+                curr += travel[i];
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * l), l = max(g for g in garbage) = O(10)
+// Space: O(1)
+// simulation
+class Solution2 {
+public:
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
+        int result = 0;
         for (const auto& t : "MPG") {
             int curr = 0;
             for (int i = 0; i < size(garbage); ++i) {
