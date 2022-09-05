380b72b4bb5c2d1f10515a7336287be5eb07c195
kamyu <kamyu104@users.noreply.github.com>
2022-09-06 05:35:22 +0800

    Update count-strictly-increasing-subarrays.py

--- Python/count-strictly-increasing-subarrays.py
+++ Python/count-strictly-increasing-subarrays.py
@@ -8,6 +8,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        result = l = 1
+        for i in xrange(1, len(nums)):
+            if nums[i-1] >= nums[i]:
+                l = 0
+            l += 1
+            result += l
+        return result
+
+
+# Time:  O(n)
+# Space: O(1)
+# two pointers
+class Solution2(object):
+    def countSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         result = left = 0
         for right in xrange(len(nums)):
             if not (right-1 >= 0 and nums[right-1] < nums[right]):
