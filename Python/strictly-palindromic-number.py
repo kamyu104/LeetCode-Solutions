99db4a674dbb48b659417de2e65467b42fe148d7
kamyu <kamyu104@users.noreply.github.com>
2022-09-06 14:15:39 +0800

    Create strictly-palindromic-number.py

---
+++ Python/strictly-palindromic-number.py
@@ -0,0 +1,11 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def isStrictlyPalindromic(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return False
