7324f51dd222da3eeaf270868706b441623b0800
kamyu <kamyu104@users.noreply.github.com>
2022-09-06 18:44:17 +0800

    Create number-of-ways-to-reach-a-position-after-exactly-k-steps.py

---
+++ Python/number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -0,0 +1,23 @@
+# Time:  O(k)
+# Space: O(k)
+
+# combinatorics
+class Solution(object):
+    def numberOfWays(self, startPos, endPos, k):
+        """
+        :type startPos: int
+        :type endPos: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        fact, inv, inv_fact = [[1]*2 for _ in xrange(3)]
+        def nCr(n, k):
+            while len(inv) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(inv) % MOD)
+                inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+                inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
+            return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
+
+        r = k-abs(endPos-startPos)
+        return nCr(k, r//2) if r >= 0 and r%2 == 0 else 0  
