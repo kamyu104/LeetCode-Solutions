43b2172ce71b63767685cf9051a41e444df0ec53
kamyu <kamyu104@users.noreply.github.com>
2022-09-12 00:21:27 +0800

    Create longest-increasing-subsequence-ii.py

---
+++ Python/longest-increasing-subsequence-ii.py
@@ -0,0 +1,59 @@
+# Time:  O(nlogr), r = max(nums)
+# Space: O(r)
+
+# Range Maximum Query
+class SegmentTree(object):
+    def __init__(self, N,
+                 build_fn=lambda _: 0,
+                 query_fn=lambda x, y: y if x is None else x if y is None else max(x, y),
+                 update_fn=lambda x: x):
+        self.tree = [None]*(2*2**((N-1).bit_length()))
+        self.base = len(self.tree)//2
+        self.query_fn = query_fn
+        self.update_fn = update_fn
+        for i in xrange(self.base, self.base+N):
+            self.tree[i] = build_fn(i-self.base)
+        for i in reversed(xrange(1, self.base)):
+            self.tree[i] = query_fn(self.tree[2*i], self.tree[2*i+1])
+
+    def update(self, i, h):
+        x = self.base+i
+        self.tree[x] = self.update_fn(h)
+        while x > 1:
+            x //= 2
+            self.tree[x] = self.query_fn(self.tree[x*2], self.tree[x*2+1])
+
+    def query(self, L, R):
+        if L > R:
+            return 0
+        L += self.base
+        R += self.base
+        left = right = None
+        while L <= R:
+            if L & 1:
+                left = self.query_fn(left, self.tree[L])
+                L += 1
+            if R & 1 == 0:
+                right = self.query_fn(self.tree[R], right)
+                R -= 1
+            L //= 2
+            R //= 2
+        return self.query_fn(left, right)
+
+
+# segment tree
+class Solution(object):
+    def lengthOfLIS(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        st = SegmentTree(max(nums))
+        for x in nums:
+            x -= 1
+            mx = st.query(max(x-k, 0), x-1)+1
+            st.update(x, mx)
+            result = max(result, mx)
+        return result
