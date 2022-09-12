2ed8b1118dfed1f93876241eacaf68cb3510a0b9
kamyu <kamyu104@users.noreply.github.com>
2022-09-13 01:18:37 +0800

    Update erect-the-fence.py

--- Python/erect-the-fence.py
+++ Python/erect-the-fence.py
@@ -1,30 +1,42 @@
 # Time:  O(nlogn)
 # Space: O(n)
 
-import itertools
-
-
 # Monotone Chain Algorithm
+# Template: https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#Python
 class Solution(object):
     def outerTrees(self, points):
         """
         :type points: List[List[int]]
         :rtype: List[List[int]]
         """
-        def ccw(A, B, C):
-            return (B[0]-A[0])*(C[1]-A[1]) - (B[1]-A[1])*(C[0]-A[0])
+        # Sort the points lexicographically (tuples are compared lexicographically).
+        # Remove duplicates to detect the case we have just one unique point.
+        points = sorted(set(tuple(x) for x in points))
 
+        # Boring case: no points or a single point, possibly repeated multiple times.
         if len(points) <= 1:
             return points
 
-        hull = []
-        points.sort()
-        for i in itertools.chain(xrange(len(points)), reversed(xrange(len(points)-1))):
-            while len(hull) >= 2 and ccw(hull[-2], hull[-1], points[i]) < 0:
-                hull.pop()
-            hull.append(points[i])
-        for i in xrange(len(hull)//2):
-            if hull[i] != hull[-1]:
-                break
-            hull.pop()
-        return hull
+        # 2D cross product of OA and OB vectors, i.e. z-component of their 3D cross product.
+        # Returns a positive value, if OAB makes a counter-clockwise turn,
+        # negative for clockwise turn, and zero if the points are collinear.
+        def cross(o, a, b):
+            return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+
+        # Build lower hull 
+        lower = []
+        for p in points:
+            while len(lower) >= 2 and cross(lower[-2], lower[-1], p) < 0:  # modified
+                lower.pop()
+            lower.append(p)
+
+        # Build upper hull
+        upper = []
+        for p in reversed(points):
+            while len(upper) >= 2 and cross(upper[-2], upper[-1], p) < 0:  # modified
+                upper.pop()
+            upper.append(p)
+
+        # Concatenation of the lower and upper hulls gives the convex hull.
+        # Last point of each list is omitted because it is repeated at the beginning of the other list. 
+        return list(set(lower[:-1] + upper[:-1]))
