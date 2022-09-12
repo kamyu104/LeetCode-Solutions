ff9dc97392ffd7209b4d0d87a0cd42b8ea74329a
kamyu <kamyu104@users.noreply.github.com>
2022-09-13 01:29:39 +0800

    Update erect-the-fence.cpp

--- C++/erect-the-fence.cpp
+++ C++/erect-the-fence.cpp
@@ -1,46 +1,45 @@
 // Time:  O(nlogn)
 // Space: O(n)
 
+
+// Monotone Chain Algorithm
+// Template: https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain#C++
 class Solution {
 public:
-    vector<vector<int>> outerTrees(vector<vector<int>>& points) {
-	const auto& orientation = [](const vector<int>& p,
-                                     const vector<int>& q,
-                                     const vector<int>& r) {
-                                         return (q[0] - p[0]) * (r[1] - p[1]) - 
-                                                (q[1] - p[1]) * (r[0] - p[0]);
-                                  };
-        if (points.size() <= 1) {
-            return points;
+    vector<vector<int>> outerTrees(vector<vector<int>>& P) {
+        // 3D cross product of OA and OB vectors, (i.e z-component of their "2D" cross product, but remember that it is not defined in "2D").
+        // Returns a positive value, if OAB makes a counter-clockwise turn,
+        // negative for clockwise turn, and zero if the points are collinear.
+        const auto& cross = [](const vector<int>& O, const vector<int>& A, const vector<int>& B) {
+            return (A[0] - O[0]) * (B[1] - O[1]) - (A[1] - O[1]) * (B[0] - O[0]);
+        };
+
+        // Returns a list of points on the convex hull in counter-clockwise order.
+        // Note: the last point in the returned list is the same as the first one.
+        size_t n = P.size(), k = 0;
+        if (n <= 3) {
+            return P;
+        }
+        vector<vector<int>> H(2*n);
+
+        // Sort points lexicographically
+        sort(P.begin(), P.end());
+
+        // Build lower hull
+        for (size_t i = 0; i < n; ++i) {
+            while (k >= 2 && cross(H[k-2], H[k-1], P[i]) < 0) k--; // modified
+            H[k++] = P[i];
         }
 
-        vector<vector<int>> hull;
-        sort(points.begin(), points.end());
-        for (int i = 0; i < points.size(); ++i) {
-            while (hull.size() >= 2 && 
-                   orientation(hull[hull.size() - 2],
-                               hull[hull.size() - 1],
-                               points[i]) < 0) {
-                hull.pop_back();
-            }
-            hull.emplace_back(points[i]);
+        // Build upper hull
+        for (size_t i = n-1, t = k+1; i > 0; --i) {
+            while (k >= t && cross(H[k-2], H[k-1], P[i-1]) < 0) k--; // modified
+            H[k++] = P[i-1];
         }
-        for (int i = points.size() - 2; i >= 0; --i) {
-            while (hull.size() >= 2 && 
-                   orientation(hull[hull.size() - 2],
-                               hull[hull.size() - 1],
-                               points[i]) < 0) {
-                hull.pop_back();
-            }
-            hull.emplace_back(points[i]);
-        }
-        const int cnt = hull.size() / 2;
-        for (int i = 0; i < cnt; ++i) {
-            if (hull[i] != hull.back()) {
-                break;
-            }
-            hull.pop_back();
-        }
-        return hull;
+
+        H.resize(k-1);
+        sort(begin(H), end(H));
+        H.erase(unique(begin(H), end(H)), end(H));
+        return H;
     }
 };
