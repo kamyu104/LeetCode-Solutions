09ce6e67771c45b5a4a5ae94b8794aeebf40cbe7
kamyu <kamyu104@users.noreply.github.com>
2022-09-21 23:57:41 +0800

    Create design-sql.py

---
+++ Python/design-sql.py
@@ -0,0 +1,44 @@
+# Time:  ctor:       O(t), t is the number of tables
+#        insertRow:  O(l), l is the number of columns
+#        deleteRow:  O(1)
+#        selectCell: O(l)
+# Space: O(n * l), n is the number of rows
+
+import itertools
+
+
+# hash table
+class SQL(object):
+
+    def __init__(self, names, columns):
+        """
+        :type names: List[str]
+        :type columns: List[int]
+        """
+        self.__table = {name:[column] for name, column in itertools.izip(names, columns)}
+
+    def insertRow(self, name, row):
+        """
+        :type name: str
+        :type row: List[str]
+        :rtype: None
+        """
+        row.append(False)  # soft delete
+        self.__table[name].append(row)
+
+    def deleteRow(self, name, rowId):
+        """
+        :type name: str
+        :type rowId: int
+        :rtype: None
+        """
+        self.__table[name][rowId][-1] = True  # soft delete
+
+    def selectCell(self, name, rowId, columnId):
+        """
+        :type name: str
+        :type rowId: int
+        :type columnId: int
+        :rtype: str
+        """
+        return self.__table[name][rowId][columnId-1] if self.__table[name][rowId][-1] == False else None
