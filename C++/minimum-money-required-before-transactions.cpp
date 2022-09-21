ce66f1d0ec42b4117badb4735a4b459b58966f25
kamyu <kamyu104@users.noreply.github.com>
2022-09-22 03:33:33 +0800

    Create minimum-money-required-before-transactions.cpp

---
+++ C++/minimum-money-required-before-transactions.cpp
@@ -0,0 +1,15 @@
+// Time:  O(n)
+// Space: O(1)
+
+// greedy, constructive algorithms
+class Solution {
+public:
+    long long minimumMoney(vector<vector<int>>& transactions) {
+        int64_t result = 0, last = 0;
+        for (const auto& t : transactions) {
+            last = max(last, static_cast<int64_t>(t[0] - max(t[0] - t[1], 0)));  // t[0] - max(t[0] - t[1], 0) == min(t[0], t[1])
+            result += max(t[0] - t[1], 0);
+        }
+        return result + last;
+    }
+};
