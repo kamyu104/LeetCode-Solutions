91ec1d96f4e2c46b655b304010d200d8420b2f05
kamyu <kamyu104@users.noreply.github.com>
2022-09-26 12:24:39 +0800

    Update closest-fair-integer.cpp

--- C++/closest-fair-integer.cpp
+++ C++/closest-fair-integer.cpp
@@ -8,33 +8,29 @@ public:
         string digits = to_string(n);
         string result;
         if (size(digits) % 2 == 0) {
-            int left_even = count_if(cbegin(digits), cend(digits), [](const auto& x) { return (x - '0') % 2 == 0; });
-            if (left_even == size(digits) / 2) {
+            vector<int> left(2);
+            for (const auto& d : digits) {
+                ++left[(d - '0') % 2];
+            }
+            if (left[0] == size(digits) / 2) {
                 return n;
             }
             for (int i = size(digits) - 1; i >= size(digits) / 2; --i) {
-                if ((digits[i] - '0') % 2 == 0) {
-                    --left_even;
-                }
-                const int left_odd = i - left_even;
-                int right_even = size(digits) / 2 - left_even;
-                int right_odd = size(digits) / 2 - left_odd;
-                if (right_even < 0 || right_odd < 0) {
+                --left[(digits[i] - '0') % 2];
+                vector<int> right = {static_cast<int>(size(digits)) / 2 - left[0],
+                                     static_cast<int>(size(digits)) / 2 - left[1]};
+                if (any_of(cbegin(right), cend(right), [](const auto& x) { return x < 0; })) {
                     continue;
                 }
-                const char d = (right_odd == 0 && digits[i] % 2 == 0) || (right_even == 0 and digits[i] % 2 == 1) ? digits[i] + 2 : digits[i] + 1;
+                const char d = (right[((digits[i] + 1) - '0') % 2] - 1 >= 0) ? digits[i] + 1 : digits[i] + 2;
                 if (d > '9') {
                     continue;
                 }
-                if ((d - '0') % 2 == 0) {
-                    --right_even;
-                } else {
-                    --right_odd;
-                }
+                --right[(d - '0') % 2];
                 result.append(digits, 0, i);
                 result.append(1, d);
-                result.append(right_even, '0');
-                result.append(right_odd, '1');
+                result.append(right[0], '0');
+                result.append(right[1], '1');
                 break;
             }
         }
