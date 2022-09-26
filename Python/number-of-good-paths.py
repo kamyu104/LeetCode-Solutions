cec4b4677562da931caefb91123b720b5fc83729
kamyu <kamyu104@users.noreply.github.com>
2022-09-26 10:39:42 +0800

    Update number-of-good-paths.py

--- Python/number-of-good-paths.py
+++ Python/number-of-good-paths.py
@@ -19,7 +19,7 @@ class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
             self.set[stk.pop()] = x
         return x
 
-    def union_set(self, x, y, v):
+    def union_set(self, x, y, v):  # modified
         x, y = self.find_set(x), self.find_set(y)
         if x == y:
             return 0  # modified
@@ -28,9 +28,9 @@ class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
         self.set[x] = self.set[y]
         if self.rank[x] == self.rank[y]:
             self.rank[y] += 1
-        cx, cy = self.cnt[x][v], self.cnt[y][v]   # added
-        self.cnt[y] = collections.Counter({v:cx+cy})   # added
-        return cx*cy # modified
+        cx, cy = self.cnt[x][v], self.cnt[y][v]  # added
+        self.cnt[y] = collections.Counter({v:cx+cy})  # added
+        return cx*cy  # modified
 
 
 # union find
