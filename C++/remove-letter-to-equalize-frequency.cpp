4588f1a544d490c2cf0f10e83ec9f99ce5c4b25e
kamyu <kamyu104@users.noreply.github.com>
2022-10-02 19:31:35 +0800

    Create remove-letter-to-equalize-frequency.cpp

---
+++ C++/remove-letter-to-equalize-frequency.cpp
@@ -0,0 +1,63 @@
+// Time:  O(n)
+// Space: O(1)
+
+// freq table, edge cases
+class Solution {
+public:
+    bool equalFrequency(string word) {
+        vector<int> cnt(26);
+        for (const auto& c : word) {
+            ++cnt[c - 'a'];
+        }
+        unordered_map<int, int> cnt2;
+        for (const auto& c : cnt) {
+            if (c) {
+                ++cnt2[c];
+            }
+        }
+        if (size(cnt2) > 2) {
+            return false;
+        }
+        if (size(cnt2) == 1) {
+            return cbegin(cnt2)->first == 1 || cbegin(cnt2)->second == 1;
+        }
+        int a = cbegin(cnt2)->first;
+        int b = next(cbegin(cnt2))->first;
+        if (a > b) {
+            swap(a, b);
+        }
+        return (a == 1 && cnt2[a] == 1) || (a + 1 == b && cnt2[b] == 1);
+    }
+};
+
+// Time:  O(26 * n)
+// Space: O(1)
+// brute force, freq table
+class Solution2 {
+public:
+    bool equalFrequency(string word) {
+        vector<int> cnt(26);
+        for (const auto& c : word) {
+            ++cnt[c - 'a'];
+        }
+        for (const auto& c : word) {
+            --cnt[c - 'a'];
+            int i = 0;
+            for (int expect = 0; i < size(cnt); ++i) {
+                if (!cnt[i]) {
+                    continue;
+                }
+                if (!expect) {
+                    expect = cnt[i];
+                } else if (expect != cnt[i]) {
+                    break;
+                }
+            }
+            if (i == size(cnt)) {
+                return true;
+            }
+            ++cnt[c - 'a'];
+        }
+        return false;
+    }
+};
