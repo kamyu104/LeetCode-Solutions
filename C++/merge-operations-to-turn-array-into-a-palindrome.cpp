e155d07eaffce7fe12eccb1894444d434b27496b
kamyu <kamyu104@users.noreply.github.com>
2022-10-04 13:01:46 +0800

    Create merge-operations-to-turn-array-into-a-palindrome.cpp

---
+++ C++/merge-operations-to-turn-array-into-a-palindrome.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(1)
+
+// constructive algorithms, two pointers
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        int result = 0;
+        int left = -1, right = size(nums);
+        int64_t l = nums[++left], r = nums[--right];
+        while (left < right) {
+            if (l == r) {
+                l = nums[++left];
+                r = nums[--right];
+                continue;
+            }
+            if (l < r) {
+                l += nums[++left];
+            } else {
+                r += nums[--right];
+            }
+            ++result;
+        }
+        return result;
+    }
+};
