2a5244cc9b6722eb850d095789972e3cacef41b7
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 20:02:59 +0800

    Create number-of-common-factors.cpp

---
+++ C++/number-of-common-factors.cpp
@@ -0,0 +1,17 @@
+// Time:  O(sqrt(log(min(a, b))))
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int commonFactors(int a, int b) {
+        const int g = gcd(a, b);  // log(min(a, b))
+        int result = 0;
+        for (int x = 1; x * x <= g; ++x) {
+            if (g % x == 0) {
+                result += g / x == x ? 1 : 2;
+            }
+        }
+        return result;
+    }
+};
