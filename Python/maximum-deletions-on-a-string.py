478e1a78512bed4b1da13cc2d84a1df8aaa32246
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 21:25:34 +0800

    Create maximum-deletions-on-a-string.py

---
+++ Python/maximum-deletions-on-a-string.py
@@ -0,0 +1,21 @@
+# Time:  O(n^2)
+# Space: O(n^2)
+
+# dp
+class Solution(object):
+    def deleteString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if all(x == s[0] for x in s):
+            return len(s)
+        dp = [[0]*(len(s)+1) for i in xrange(len(s)+1)]  # dp[i][j]: max prefix length of s[i:] and s[j:]
+        dp2 = [1]*len(s)  # dp2[i]: max operations of s[i:]
+        for i in reversed(xrange(len(s)-1)):
+            for j in xrange(i+1, len(s)):
+                if s[j] == s[i]:
+                    dp[i][j] = dp[i+1][j+1]+1
+                if dp[i][j] >= j-i:
+                    dp2[i] = max(dp2[i], dp2[j]+1)
+        return dp2[0]
