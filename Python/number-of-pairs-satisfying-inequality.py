cc8cd5e2af5bdb649a613c3bee3c40a9aed778b9
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 22:35:55 +0800

    Update number-of-pairs-satisfying-inequality.py

--- Python/number-of-pairs-satisfying-inequality.py
+++ Python/number-of-pairs-satisfying-inequality.py
@@ -20,3 +20,47 @@ class Solution(object):
             result += sl.bisect_right((x-y)+diff)
             sl.add(x-y)
         return result
+
+    
+# Time:  O(nlogn)
+# Space: O(n)
+import itertools
+import bisect
+
+
+class BIT(object):  # 0-indexed.
+    def __init__(self, n):
+        self.__bit = [0]*(n+1)  # Extra one for dummy node.
+
+    def add(self, i, val):
+        i += 1  # Extra one for dummy node.
+        while i < len(self.__bit):
+            self.__bit[i] += val
+            i += (i & -i)
+
+    def query(self, i):
+        i += 1  # Extra one for dummy node.
+        ret = 0
+        while i > 0:
+            ret += self.__bit[i]
+            i -= (i & -i)
+        return ret
+
+
+# bit, fenwick tree
+class Solution2(object):
+    def numberOfPairs(self, nums1, nums2, diff):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type diff: int
+        :rtype: int
+        """
+        sorted_nums = sorted(set(x-y for x, y in itertools.izip(nums1, nums2)))
+        num_to_idx = {x:i for i, x in enumerate(sorted_nums)}
+        result = 0
+        bit = BIT(len(num_to_idx))
+        for x, y in itertools.izip(nums1, nums2):
+            result += bit.query(bisect.bisect_right(sorted_nums, (x-y)+diff)-1)
+            bit.add(num_to_idx[x-y], 1)
+        return result
