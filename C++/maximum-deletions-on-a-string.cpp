b29f42906a0cb41d6a771442a2d9f0d93e833754
kamyu <kamyu104@users.noreply.github.com>
2022-10-06 04:32:00 +0800

    Update maximum-deletions-on-a-string.cpp

--- C++/maximum-deletions-on-a-string.cpp
+++ C++/maximum-deletions-on-a-string.cpp
@@ -8,17 +8,17 @@ public:
         if (all_of(cbegin(s), cend(s), [&](const auto& x) { return x == s[0]; })) {
             return size(s);
         }
-        vector<vector<int>> dp(2, vector<int>(size(s) + 1));  // dp[i%2][j]: max prefix length of s[i:] and s[j:]
-        vector<int> dp2(size(s), 1);  // dp2[i]: max operation count of s[i:]
+        vector<vector<int>> dp2(2, vector<int>(size(s) + 1));  // dp2[i%2][j]: max prefix length of s[i:] and s[j:]
+        vector<int> dp(size(s), 1);  // dp[i]: max operation count of s[i:]
         for (int i = size(s) - 2; i >= 0; --i) {
             for (int j = i + 1; j < size(s); ++j) {
-                dp[i % 2][j] = (s[j] == s[i]) ? dp[(i + 1) % 2][j + 1] + 1 : 0;
-                if (dp[i % 2][j] >= j - i) {
-                    dp2[i] = max(dp2[i], dp2[j] + 1);
+                dp2[i % 2][j] = (s[j] == s[i]) ? dp2[(i + 1) % 2][j + 1] + 1 : 0;
+                if (dp2[i % 2][j] >= j - i) {
+                    dp[i] = max(dp[i], dp[j] + 1);
                 }
             }
         }
-        return dp2[0];
+        return dp[0];
     }
 };
 
