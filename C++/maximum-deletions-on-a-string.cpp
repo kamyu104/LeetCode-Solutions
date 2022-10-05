4e756020579453aaa88f1f075ac98fd1c3b5d77a
kamyu <kamyu104@users.noreply.github.com>
2022-10-06 02:30:58 +0800

    Update maximum-deletions-on-a-string.cpp

--- C++/maximum-deletions-on-a-string.cpp
+++ C++/maximum-deletions-on-a-string.cpp
@@ -1,8 +1,31 @@
 // Time:  O(n^2)
 // Space: O(n)
 
-// dp, kmp algorithm
+// dp
 class Solution {
+public:
+    int deleteString(string s) {
+        if (all_of(cbegin(s), cend(s), [&](const auto& x) { return x == s[0]; })) {
+            return size(s);
+        }
+        vector<vector<int>> dp(2, vector<int>(size(s) + 1));  // dp[i % 2][j]: max prefix length of s[i:] and s[j:]
+        vector<int> dp2(size(s), 1);  // dp2[i]: max operation count of s[i:]
+        for (int i = size(s) - 2; i >= 0; --i) {
+            for (int j = i + 1; j < size(s); ++j) {
+                dp[i % 2][j] = (s[j] == s[i]) ? dp[(i + 1) % 2][j + 1] + 1 : 0;
+                if (dp[i % 2][j] >= j - i) {
+                    dp2[i] = max(dp2[i], dp2[j] + 1);
+                }
+            }
+        }
+        return dp2[0];
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+// dp, kmp algorithm
+class Solution2 {
 public:
     int deleteString(string s) {
         if (all_of(cbegin(s), cend(s), [&](const auto& x) { return x == s[0]; })) {
@@ -34,31 +57,5 @@ public:
             }
         }
         return dp[0];
-    }
-};
-
-
-// Time:  O(n^2)
-// Space: O(n^2)
-// dp
-class Solution2 {
-public:
-    int deleteString(string s) {
-        if (all_of(cbegin(s), cend(s), [&](const auto& x) { return x == s[0]; })) {
-            return size(s);
-        }
-        vector<vector<int>> dp(size(s) + 1, vector<int>(size(s) + 1));  // dp[i][j]: max prefix length of s[i:] and s[j:]
-        vector<int> dp2(size(s), 1);  // dp2[i]: max operation count of s[i:]
-        for (int i = size(s) - 2; i >= 0; --i) {
-            for (int j = i + 1; j < size(s); ++j) {
-                if (s[j] == s[i]) {
-                    dp[i][j] = dp[i + 1][j + 1] + 1;
-                }
-                if (dp[i][j] >= j - i) {
-                    dp2[i] = max(dp2[i], dp2[j] + 1);
-                }
-            }
-        }
-        return dp2[0];
     }
 };
