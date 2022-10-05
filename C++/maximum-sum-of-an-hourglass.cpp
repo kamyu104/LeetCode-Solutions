c6e2a1c7717de43790a9ff9afa484e98f75735c8
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 20:27:08 +0800

    Create maximum-sum-of-an-hourglass.cpp

---
+++ C++/maximum-sum-of-an-hourglass.cpp
@@ -0,0 +1,22 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+// brute force
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+        const auto& total = [&](int i, int j) {
+            return grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + 
+                                grid[i + 1][j + 1] + 
+                   grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2];
+        };
+
+        int result = 0;
+        for (int i = 0; i + 2 < size(grid); ++i) {
+            for (int j = 0; j + 2 < size(grid[0]); ++j) {
+                result = max(result, total(i, j));
+            }
+        }
+        return result;
+    }
+};
