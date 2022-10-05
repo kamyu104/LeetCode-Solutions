331bdc5b57dae71f5cba1f225ae4669938febbaa
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 20:59:44 +0800

    Create minimize-xor.cpp

---
+++ C++/minimize-xor.cpp
@@ -0,0 +1,20 @@
+// Time:  O(logn)
+// Space: O(1)
+
+// bit manipulation
+class Solution {
+public:
+    int minimizeXor(int num1, int num2) {
+        const int cnt1 = __builtin_popcount(num1), cnt2 = __builtin_popcount(num2);
+        int result = num1;
+        int cnt = abs(cnt1 - cnt2);
+        const int expect = cnt1 >= cnt2 ? 1 : 0;
+        for (int i = 0; cnt; ++i) {
+            if (((num1 >> i) & 1) == expect) {
+                --cnt;
+                result ^= 1 << i;
+            } 
+        }
+        return result;
+    }
+};
