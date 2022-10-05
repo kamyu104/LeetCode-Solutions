f2b2fbac0bb66a07856830f2def1756631ae15de
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 20:27:35 +0800

    Create maximum-sum-of-an-hourglass.py

---
+++ Python/maximum-sum-of-an-hourglass.py
@@ -0,0 +1,16 @@
+# Time:  O(m * n)
+# Space: O(m * n)
+
+# brute force
+class Solution(object):
+    def maxSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def total(i, j):
+            return (grid[i][j]+grid[i][j+1]+grid[i][j+2]+
+                               grid[i+1][j+1]+
+                    grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2])
+
+        return max(total(i, j) for i in xrange(len(grid)-2) for j in xrange(len(grid[0])-2))
