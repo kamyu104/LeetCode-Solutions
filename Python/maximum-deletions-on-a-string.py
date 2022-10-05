f69484b04036e6dee4c216185974128391763eee
kamyu <kamyu104@users.noreply.github.com>
2022-10-06 02:29:03 +0800

    Update maximum-deletions-on-a-string.py

--- Python/maximum-deletions-on-a-string.py
+++ Python/maximum-deletions-on-a-string.py
@@ -1,8 +1,29 @@
 # Time:  O(n^2)
 # Space: O(n)
 
-# dp, kmp algorithm
+# dp
 class Solution(object):
+    def deleteString(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if all(x == s[0] for x in s):
+            return len(s)
+        dp = [[0]*(len(s)+1) for i in xrange(2)]  # dp[j]: max prefix length of s[i:] and s[j:]
+        dp2 = [1]*len(s)  # dp2[i]: max operation count of s[i:]
+        for i in reversed(xrange(len(s)-1)):
+            for j in xrange(i+1, len(s)):
+                dp[i%2][j] = dp[(i+1)%2][j+1]+1 if s[j] == s[i] else 0
+                if dp[i%2][j] >= j-i:
+                    dp2[i] = max(dp2[i], dp2[j]+1)
+        return dp2[0]
+
+
+# Time:  O(n^2)
+# Space: O(n)
+# dp, kmp algorithm
+class Solution2(object):
     def deleteString(self, s):
         """
         :type s: str
@@ -29,24 +50,3 @@ class Solution(object):
                     dp[i] = max(dp[i], dp[i+(prefix[j]+1)]+1)
         return dp[0]
 
-
-# Time:  O(n^2)
-# Space: O(n^2)
-# dp
-class Solution2(object):
-    def deleteString(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        if all(x == s[0] for x in s):
-            return len(s)
-        dp = [[0]*(len(s)+1) for i in xrange(len(s)+1)]  # dp[i][j]: max prefix length of s[i:] and s[j:]
-        dp2 = [1]*len(s)  # dp2[i]: max operation count of s[i:]
-        for i in reversed(xrange(len(s)-1)):
-            for j in xrange(i+1, len(s)):
-                if s[j] == s[i]:
-                    dp[i][j] = dp[i+1][j+1]+1
-                if dp[i][j] >= j-i:
-                    dp2[i] = max(dp2[i], dp2[j]+1)
-        return dp2[0]
