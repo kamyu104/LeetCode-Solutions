f83acf250fc2470a96c34aa26fcc5dcb09f8c20b
kamyu <kamyu104@users.noreply.github.com>
2022-10-06 03:43:11 +0800

    Update maximum-deletions-on-a-string.cpp

--- C++/maximum-deletions-on-a-string.cpp
+++ C++/maximum-deletions-on-a-string.cpp
@@ -59,3 +59,41 @@ public:
         return dp[0];
     }
 };
+
+// Time:  O(n^2)
+// Space: O(n)
+// rolling hash
+class Solution3 {
+public:
+    int deleteString(string s) {
+        static const int MOD = 1e9 + 7;
+        static vector<int> P = {113, 109};
+        if (all_of(cbegin(s), cend(s), [&](const auto& x) { return x == s[0]; })) {
+            return size(s);
+        }
+        vector<vector<int64_t>> power(size(P), vector<int64_t>(1, 1));
+        vector<vector<int64_t>> prefix(size(P), vector<int64_t>(1));
+        const auto& hash = [&](int i, int j) {
+            vector<int> h(size(P));
+            for (int idx = 0; idx < size(P); ++idx) {
+                h[idx] = ((prefix[idx][j + 1] - prefix[idx][i] * power[idx][j - i + 1]) % MOD + MOD) % MOD;
+            }
+            return h;
+        };
+        for (const auto& x : s) {
+            for (int idx = 0; idx < size(P); ++idx) {
+                power[idx].emplace_back(power[idx].back() * P[idx] % MOD);
+                prefix[idx].emplace_back((prefix[idx].back() * P[idx] + (x - 'a')) % MOD);
+            }
+        }
+        vector<int> dp(size(s), 1);  // dp[i]: max operation count of s[i:]
+        for (int i = size(s) - 2; i >= 0; --i) {              
+            for (int j = 1; j <= (size(s) - i) / 2 ; ++j) {
+                if (hash(i, i + j - 1) == hash(i + j, i + 2 * j - 1)) {
+                    dp[i] = max(dp[i], dp[i + j] + 1);
+                }
+            }
+        }
+        return dp[0];
+    }
+};
