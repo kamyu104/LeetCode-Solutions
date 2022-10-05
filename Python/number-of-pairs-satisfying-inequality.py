c1a132f77798fa07e83f41b79f366ea56414c1ff
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 22:51:52 +0800

    Update number-of-pairs-satisfying-inequality.py

--- Python/number-of-pairs-satisfying-inequality.py
+++ Python/number-of-pairs-satisfying-inequality.py
@@ -64,3 +64,45 @@ class Solution2(object):
             result += bit.query(bisect.bisect_right(sorted_nums, (x-y)+diff)-1)
             bit.add(num_to_idx[x-y], 1)
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import itertools
+
+
+# merge sort
+class Solution3(object):
+    def numberOfPairs(self, nums1, nums2, diff):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type diff: int
+        :rtype: int
+        """
+        def merge_sort(nums, left, right, result):
+            if left == right:
+                return
+            mid = left+(right-left)//2
+            merge_sort(nums, left, mid, result)
+            merge_sort(nums, mid+1, right, result)
+            r = mid+1
+            for l in xrange(left, mid+1):
+                while r < right+1 and nums[l]-nums[r] > diff:
+                    r += 1
+                result[0] += right-r+1
+            tmp = []
+            l, r = left, mid+1
+            while l < mid+1 or r < right+1:
+                if r >= right+1 or (l < mid+1 and nums[l] <= nums[r]):
+                    tmp.append(nums[l])
+                    l += 1
+                else:
+                    tmp.append(nums[r])
+                    r += 1
+            nums[left:right+1] = tmp
+
+        nums = [x-y for x, y in itertools.izip(nums1, nums2)]
+        result = [0]
+        merge_sort(nums, 0, len(nums)-1, result)
+        return result[0]
