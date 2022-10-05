8213d2b1f5c98b95bb1e72d15caade91f76dc675
kamyu <kamyu104@users.noreply.github.com>
2022-10-05 20:05:55 +0800

    Create number-of-common-factors.py

---
+++ Python/number-of-common-factors.py
@@ -0,0 +1,24 @@
+# Time:  O(sqrt(log(min(a, b))))
+# Space: O(1)
+
+# math
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        def gcd(a, b):  # Time: O(log(min(a, b)))
+            while b:
+                a, b = b, a%b
+            return a
+        
+        g = gcd(a, b)
+        result = 0
+        x = 1
+        while x*x <= g:
+            if g%x == 0:
+                result += 1 if g//x == x else 2
+            x += 1
+        return result
