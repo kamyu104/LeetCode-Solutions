a5927ff536a72d564c55b93cd2fc423b7774f251
kamyu <kamyu104@users.noreply.github.com>
2022-10-07 20:12:37 +0800

    Update maximize-total-tastiness-of-purchased-fruits.cpp

--- C++/maximize-total-tastiness-of-purchased-fruits.cpp
+++ C++/maximize-total-tastiness-of-purchased-fruits.cpp
@@ -7,7 +7,7 @@ public:
     int maxTastiness(vector<int>& price, vector<int>& tastiness, int maxAmount, int maxCoupons) {
         vector<vector<int>> dp(maxAmount + 1, vector<int>(maxCoupons + 1));
         for (int k = 0; k < size(price); ++k) {
-            for (int i = maxAmount; i >= price[k] / 2; --i) {
+            for (int i = maxAmount; i - price[k] / 2 >= 0; --i) {
                 for (int j = maxCoupons; j >= 0; --j) {
                     if (i - price[k] >= 0) {
                         dp[i][j] = max(dp[i][j], tastiness[k] + dp[i - price[k]][j]);
