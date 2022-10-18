2709ea5aa93261747def419c9a408cc011c73604
kamyu <kamyu104@users.noreply.github.com>
2022-10-18 18:03:38 +0800

    Create the-employee-that-worked-on-the-longest-task.cpp

---
+++ C++/the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int hardestWorker(int n, vector<vector<int>>& logs) {
+        int result = -1;
+        for (int i = 0, prev = 0, t = 0; i < size(logs); prev = logs[i++][1]) {
+            if (logs[i][1] - prev > t) {
+                t = logs[i][1] - prev;
+                result = logs[i][0];
+            } else if (logs[i][1] - prev == t && logs[i][0] < result) {
+                result = logs[i][0];
+            }
+        }
+        return result;
+    }
+};
