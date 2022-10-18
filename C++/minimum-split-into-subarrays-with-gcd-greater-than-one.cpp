88c2614c5bd6bead42d857f4038aa9b90fcc33e7
kamyu <kamyu104@users.noreply.github.com>
2022-10-18 22:33:59 +0800

    Create minimum-split-into-subarrays-with-gcd-greater-than-one.cpp

---
+++ C++/minimum-split-into-subarrays-with-gcd-greater-than-one.cpp
@@ -0,0 +1,18 @@
+// Time:  O(nlogr), r is the max of nums
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    int minimumSplits(vector<int>& nums) {
+        int result = 1, g = 0;
+        for (const auto& x : nums) {
+            g = gcd(g, x);
+            if (g == 1) {
+                g = x;
+                ++result;
+            }
+        }
+        return result;
+    }
+};
