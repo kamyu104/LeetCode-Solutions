4d731e4b54c660a3929edb517ccdbb9a8be97457
kamyu <kamyu104@users.noreply.github.com>
2022-10-18 22:32:30 +0800

    Create minimum-split-into-subarrays-with-gcd-greater-than-one.py

---
+++ Python/minimum-split-into-subarrays-with-gcd-greater-than-one.py
@@ -0,0 +1,23 @@
+# Time:  O(nlogr), r = max(nums)
+# Space: O(1)
+
+# greedy
+class Solution(object):
+    def minimumSplits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a%b
+            return a
+
+        result = 1
+        g = 0
+        for x in nums:
+            g = gcd(g, x)
+            if g == 1:
+                g = x
+                result += 1
+        return result
