7d59e7efd8dccdc5b4c6c0f6dd4ba30d6d97e33c
kamyu <kamyu104@users.noreply.github.com>
2022-10-20 02:47:20 +0800

    Update sum-of-number-and-its-reverse.py

--- Python/sum-of-number-and-its-reverse.py
+++ Python/sum-of-number-and-its-reverse.py
@@ -1,8 +1,27 @@
 # Time:  O(nlogn)
-# Space: O(logn)
+# Space: O(1)
 
 # brute force
 class Solution(object):
+    def sumOfNumberAndReverse(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        def reverse(n):
+            result = 0
+            while n:
+                result = result*10 + n%10
+                n //= 10            
+            return result
+
+        return any(x+reverse(x) == num for x in xrange(num//2, num+1))
+
+
+# Time:  O(nlogn)
+# Space: O(logn)
+# brute force
+class Solution2(object):
     def sumOfNumberAndReverse(self, num):
         """
         :type num: int
