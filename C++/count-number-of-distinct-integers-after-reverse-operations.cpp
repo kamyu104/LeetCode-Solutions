85a46eab81cb09abc56c77465034d3991367416c
kamyu <kamyu104@users.noreply.github.com>
2022-10-19 23:53:50 +0800

    Create count-number-of-distinct-integers-after-reverse-operations.cpp

---
+++ C++/count-number-of-distinct-integers-after-reverse-operations.cpp
@@ -0,0 +1,22 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// hash table
+class Solution {
+public:
+    int countDistinctIntegers(vector<int>& nums) {
+        const auto& reverse = [](int n) {
+            int result = 0;
+            for (; n; n /= 10) {
+                result = result * 10 + n % 10;
+            }
+            return result;
+        };
+
+        unordered_set<int> lookup(cbegin(nums), cend(nums));
+        for (const auto& x : nums) {
+            lookup.emplace(reverse(x));
+        } 
+        return size(lookup);
+    }
+};
