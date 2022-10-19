6c4b11269664ff3825bba8de73421729021e465c
kamyu <kamyu104@users.noreply.github.com>
2022-10-20 02:49:30 +0800

    Update count-number-of-distinct-integers-after-reverse-operations.py

--- Python/count-number-of-distinct-integers-after-reverse-operations.py
+++ Python/count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,4 +1,4 @@
-# Time:  O(nlogn)
+# Time:  O(nlogr), r = max(nums)
 # Space: O(n)
 
 # hash table   
@@ -8,4 +8,30 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        def reverse(n):
+            result = 0
+            while n:
+                result = result*10 + n%10
+                n //= 10            
+            return result
+
+        return len({y for x in nums for y in (x, reverse(x))})
+
+
+# Time:  O(nlogr), r = max(nums)
+# Space: O(n)
+# hash table   
+class Solution2(object):
+    def countDistinctIntegers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def reverse(n):
+            result = 0
+            while n:
+                result = result*10 + n%10
+                n //= 10            
+            return result
+
         return len({y for x in nums for y in (x, int(str(x)[::-1]))})
