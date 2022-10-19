8f65ac6befcbf0c72eb334bc7727fe58a71e08a5
kamyu <kamyu104@users.noreply.github.com>
2022-10-20 01:06:43 +0800

    Create sum-of-number-and-its-reverse.cpp

---
+++ C++/sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,23 @@
+// Time:  O(nlogn)
+// Space: O(logn)
+
+// brute force
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        const auto& reverse = [](int n) {
+            int result = 0;
+            for (; n; n /= 10) {
+                result = result * 10 + n % 10;
+            }
+            return result;
+        };
+
+        for (int x = num / 2; x <= num; ++x) {
+            if (x + reverse(x) == num) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
