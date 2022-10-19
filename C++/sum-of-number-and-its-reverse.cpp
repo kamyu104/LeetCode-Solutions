4027904571a903d15f1dc2eac055410ca641a99d
kamyu <kamyu104@users.noreply.github.com>
2022-10-20 07:23:41 +0800

    Update sum-of-number-and-its-reverse.cpp

--- C++/sum-of-number-and-its-reverse.cpp
+++ C++/sum-of-number-and-its-reverse.cpp
@@ -1,8 +1,44 @@
+// Time:  O(log10(n) * 2^(log10(n)/2)) = O(n^(1/(2*log2(10))) * log10(n))
+// Space: O(log10(n))
+
+// backtracking
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        const function<int(int, int)> backtracking = [&](int num, int total) {
+            if (num == 0) {
+                return true;
+            }
+            if (total == 1) {
+                return false;
+            }
+            if (num <= 18) {
+                return (num % 2 == 0) || (num == 11 && (total == 0 || total == 11));
+            }
+            for (const auto& x : {num % 10, 10 + num % 10}) {
+                int base = 11;
+                if (!(1 <= x && x <= 18 && x * base<= num)) {
+                    continue;
+                }
+                for (; x * ((base - 1) * 10 + 1) <= num; base = (base - 1) * 10 + 1);
+                if (!(total == 0 || total == base)) {
+                    continue;
+                }
+                if (backtracking((num - x *base) / 10, base / 100 + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        };
+
+        return backtracking(num, 0);
+    }
+};
+
 // Time:  O(nlogn)
 // Space: O(1)
-
 // brute force
-class Solution {
+class Solution2 {
 public:
     bool sumOfNumberAndReverse(int num) {
         const auto& reverse = [](int n) {
