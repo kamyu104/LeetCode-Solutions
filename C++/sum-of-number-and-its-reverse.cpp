5ca9fca3ed7bcd2d535cd063809744de24df6663
kamyu <kamyu104@users.noreply.github.com>
2022-10-20 08:38:08 +0800

    Update sum-of-number-and-its-reverse.cpp

--- C++/sum-of-number-and-its-reverse.cpp
+++ C++/sum-of-number-and-its-reverse.cpp
@@ -1,4 +1,4 @@
-// Time:  O(log10(n) * 2^(log10(n)/2)) = O(log10(n) * n^(1/(2*log2(10))))
+// Time:  O(2^(log10(n)/2)) = O( n^(1/(2*log2(10))))
 // Space: O(log10(n))
 
 // backtracking
@@ -13,18 +13,19 @@ public:
                 return false;
             }
             if (num <= 18) {
-                return (num % 2 == 0) || (num == 11 && (total == 0 || total == 11));
+                return (num % 2 == 0) || (num == 11 && total == 0);
             }
             for (const auto& x : {num % 10, 10 + num % 10}) {
+                if (!(1 <= x && x <= 18)) {
+                    continue;
+                }
                 int base = 11;
-                if (!(1 <= x && x <= 18 && x * base<= num)) {
-                    continue;
+                if (total) {
+                    base = total;
+                } else {
+                    for (; x * ((base - 1) * 10 + 1) <= num; base = (base - 1) * 10 + 1);
                 }
-                for (; x * ((base - 1) * 10 + 1) <= num; base = (base - 1) * 10 + 1);
-                if (!(total == 0 || total == base)) {
-                    continue;
-                }
-                if (backtracking((num - x *base) / 10, base / 100 + 1)) {
+                if (11 <= base && base <= num / x && backtracking((num - x * base) / 10, base / 100 + 1)) {
                     return true;
                 }
             }
