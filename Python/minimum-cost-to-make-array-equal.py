851ba13864c9c632cfd951172cae75bdf025b43d
kamyu <kamyu104@users.noreply.github.com>
2022-10-26 14:34:09 +0800

    Update minimum-cost-to-make-array-equal.py

--- Python/minimum-cost-to-make-array-equal.py
+++ Python/minimum-cost-to-make-array-equal.py
@@ -1,5 +1,5 @@
 # Time:  O(nlogn)
-# Space: O(1)
+# Space: O(n)
 
 import itertools
 
@@ -12,25 +12,61 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
+        def f(x):
+            return sum(abs(y-x)*c for y, c in itertools.izip(nums, cost))
+
         def check(x, t):
             return sum(c for y, c in itertools.izip(nums, cost) if y <= x) >= t
     
+        idxs = range(len(nums))
+        idxs.sort(key=lambda x: nums[x])
+        left, right = 0, len(idxs)-1
         total = sum(cost)
-        left, right = min(nums), max(nums)
         median = (total+1)//2
         while left <= right:
             mid = left+(right-left)//2
-            if check(mid, median):
+            if check(nums[idxs[mid]], median):
                 right = mid-1
             else:
                 left = mid+1
-        return sum(abs(x-left)*c for x, c in itertools.izip(nums, cost))
+        return f(nums[idxs[left]])
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import itertools
+
+
+# binary search
+class Solution2(object):
+    def minCost(self, nums, cost):
+        """
+        :type nums: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        def f(x):
+            return sum(abs(y-x)*c for y, c in itertools.izip(nums, cost))
+    
+        def check(x):
+            return x+1 == len(idxs) or f(nums[idxs[x]]) < f(nums[idxs[x+1]])
+
+        idxs = range(len(nums))
+        idxs.sort(key=lambda x: nums[x])
+        left, right = 0, len(idxs)-1
+        while left <= right:
+            mid = left+(right-left)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return f(nums[idxs[left]])
 
 
 # Time:  O(nlogn)
 # Space: O(n)
 # prefix sum
-class Solution2(object):
+class Solution3(object):
     def minCost(self, nums, cost):
         """
         :type nums: List[int]
