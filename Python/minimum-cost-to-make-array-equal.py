2f2999a9e7df3b0854db2beeb45fa4527577cffc
kamyu <kamyu104@users.noreply.github.com>
2022-10-26 12:03:26 +0800

    Update minimum-cost-to-make-array-equal.py

--- Python/minimum-cost-to-make-array-equal.py
+++ Python/minimum-cost-to-make-array-equal.py
@@ -25,3 +25,33 @@ class Solution(object):
             else:
                 left = mid+1
         return sum(abs(x-left)*c for x, c in itertools.izip(nums, cost))
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# prefix sum
+class Solution2(object):
+    def minCost(self, nums, cost):
+        """
+        :type nums: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        idxs = range(len(nums))
+        idxs.sort(key=lambda x: nums[x])
+        prefix = [0]*(len(cost)+1)
+        left = 0
+        for i in xrange(len(cost)):
+            if i-1 >= 0:
+                left += prefix[i]*(nums[idxs[i]]-nums[idxs[i-1]])
+            prefix[i+1] = prefix[i]+cost[idxs[i]]
+        result = float("inf")
+        suffix = right = 0
+        for i in reversed(xrange(len(cost))):
+            if i+1 < len(idxs):
+                right += suffix*(nums[idxs[i+1]]-nums[idxs[i]])
+            result = min(result, left+right)
+            if i-1 >= 0:
+                left -= prefix[i]*(nums[idxs[i]]-nums[idxs[i-1]])
+            suffix += cost[idxs[i]]
+        return result
