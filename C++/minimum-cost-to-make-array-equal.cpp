02579f11030a821eab0c6eff731a146349c65a3e
kamyu <kamyu104@users.noreply.github.com>
2022-10-26 14:36:14 +0800

    Update minimum-cost-to-make-array-equal.cpp

--- C++/minimum-cost-to-make-array-equal.cpp
+++ C++/minimum-cost-to-make-array-equal.cpp
@@ -5,6 +5,14 @@
 class Solution {
 public:
     long long minCost(vector<int>& nums, vector<int>& cost) {
+        const auto& f = [&](int x) {
+            int64_t result = 0;
+            for (int i = 0; i < size(nums); ++i) {
+                result += static_cast<int64_t>(abs(nums[i] - x)) * cost[i];
+            }
+            return result;
+        };
+
         const auto& check = [&](int x, int64_t t) {
             int64_t cnt = 0;
             for (int i = 0; i < size(nums); ++i) {
@@ -15,23 +23,24 @@ public:
             return cnt >= t;
         };
 
+        
+        vector<int> idxs(size(nums));
+        iota(begin(idxs), end(idxs), 0);
+        sort(begin(idxs), end(idxs), [&](const auto& a, const auto& b) {
+            return nums[a] < nums[b];
+        });
+        int left = 0, right = size(idxs) - 1;
         const int64_t total = accumulate(cbegin(cost), cend(cost), 0ll);
-        int left = *min_element(cbegin(nums), cend(nums));
-        int right = *max_element(cbegin(nums), cend(nums));
         const int64_t median = (total + 1) / 2;
         while (left <= right) {
             const int mid = left + (right - left) / 2;
-            if (check(mid, median)) {
+            if (check(nums[idxs[mid]], median)) {
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        int64_t result = 0;
-        for (int i = 0; i < size(nums); ++i) {
-            result += static_cast<int64_t>(abs(nums[i] - left)) * cost[i];
-        }
-        return result;
+        return f(nums[idxs[left]]);
     }
 };
 
