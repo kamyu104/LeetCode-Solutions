2356d378de436951c71cd81dd2b90547dab4cee3
kamyu <kamyu104@users.noreply.github.com>
2022-10-26 12:10:52 +0800

    Update minimum-cost-to-make-array-equal.cpp

--- C++/minimum-cost-to-make-array-equal.cpp
+++ C++/minimum-cost-to-make-array-equal.cpp
@@ -34,3 +34,39 @@ public:
         return result;
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(n)
+// prefix sum
+class Solution2 {
+public:
+    long long minCost(vector<int>& nums, vector<int>& cost) {
+        vector<int> idxs(size(nums));
+        iota(begin(idxs), end(idxs), 0);
+        sort(begin(idxs), end(idxs),
+             [&](const auto& a, const auto& b) {
+                 return nums[a] < nums[b];
+             });
+        vector<int64_t> prefix(size(cost) + 1);
+        int64_t left = 0;
+        for (int i = 0; i < size(cost); ++i) {
+            if (i - 1 >= 0) {
+                left += prefix[i] * (nums[idxs[i]] - nums[idxs[i - 1]]);
+            }
+            prefix[i + 1] = prefix[i] + cost[idxs[i]];
+        }
+        int64_t result = numeric_limits<int64_t>::max();
+        int64_t suffix = 0, right = 0;
+        for (int i = size(cost) - 1; i >= 0; --i) {
+            if (i + 1 < size(idxs)) {
+                right += suffix * (nums[idxs[i + 1]] - nums[idxs[i]]);
+            }
+            result = min(result, left + right);
+            if (i - 1 >= 0) {
+                left -= prefix[i] * (nums[idxs[i]] - nums[idxs[i - 1]]);
+            }
+            suffix += cost[idxs[i]];
+        }
+        return result;
+    }
+};
