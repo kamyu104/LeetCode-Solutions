33f28ba8f841b83bcebc4758445101215511b971
kamyu <kamyu104@users.noreply.github.com>
2022-10-26 12:32:28 +0800

    Create minimum-number-of-operations-to-make-arrays-similar.cpp

---
+++ C++/minimum-number-of-operations-to-make-arrays-similar.cpp
@@ -0,0 +1,20 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// greedy, sort
+class Solution {
+public:
+    long long makeSimilar(vector<int>& nums, vector<int>& target) {
+        const auto& cmp = [](const auto& a, const auto& b) {
+            return pair(a % 2, a) < pair(b % 2, b);
+        };
+
+        sort(begin(nums), end(nums), cmp);
+        sort(begin(target), end(target), cmp);
+        int64_t result = 0;
+        for (int i = 0; i < size(nums); ++i) {
+            result += abs(nums[i] - target[i]);
+        }
+        return result / 4;
+    }
+};
