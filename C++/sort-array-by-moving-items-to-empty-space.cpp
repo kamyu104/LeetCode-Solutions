c8c5cda4c8cb2b3752cbe930cfb03dfc68b0313d
kamyu <kamyu104@users.noreply.github.com>
2022-11-04 15:41:24 +0800

    Update sort-array-by-moving-items-to-empty-space.cpp

--- C++/sort-array-by-moving-items-to-empty-space.cpp
+++ C++/sort-array-by-moving-items-to-empty-space.cpp
@@ -6,6 +6,35 @@ class Solution {
 public:
     int sortArray(vector<int>& nums) {
         const auto& min_moves = [&](int d) {
+            const auto& index = [&](int x, int d) {
+                return (x == 0) ? d * (size(nums) - 1) : x - d;
+            };
+            vector<int> a(nums);
+            int result = 0;
+            for (int i = 0; i < size(a); ++i) {
+                int l = 1;
+                bool has_zero = (a[i] == 0);
+                for (; index(a[i], d) != i; has_zero |= (a[i] == 0), ++l) {
+                    swap(a[i], a[index(a[i], d)]);
+                }
+                if (l >= 2) {
+                    result += has_zero ? l - 1 : l + 1;
+                }
+            }
+            return result;
+        };
+
+        return min(min_moves(0), min_moves(1));
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// greedy
+class Solution2 {
+public:
+    int sortArray(vector<int>& nums) {
+        const auto& min_moves = [&](int d) {
             vector<int> lookup(size(nums));
             for (int i = 0; i < size(nums); ++i) {
                 lookup[nums[i]] = i;
