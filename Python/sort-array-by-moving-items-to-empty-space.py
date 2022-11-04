7466e0173669dadd9a16bc77241aeb1c787f504d
kamyu <kamyu104@users.noreply.github.com>
2022-11-04 16:00:20 +0800

    Update sort-array-by-moving-items-to-empty-space.py

--- Python/sort-array-by-moving-items-to-empty-space.py
+++ Python/sort-array-by-moving-items-to-empty-space.py
@@ -26,35 +26,3 @@ class Solution(object):
             return result
 
         return min(min_moves(0), min_moves(1))
-
-
-# Time:  O(n)
-# Space: O(n)
-# greedy
-class Solution2(object):
-    def sortArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        def min_moves(d):
-            lookup = [0]*len(nums)
-            for i, x in enumerate(nums):
-                lookup[x] = i
-            result, i = 0, 1
-            while i < len(nums):
-                if lookup[0] != d*(len(nums)-1): 
-                    ni = lookup[0]+d
-                else:
-                    for i in xrange(i, len(nums)):
-                        if lookup[i]+d != i:
-                            break
-                    else:
-                        i = len(nums)
-                        continue
-                    ni = i
-                lookup[0], lookup[ni] = lookup[ni], lookup[0]
-                result += 1
-            return result
-
-        return min(min_moves(0), min_moves(1))
