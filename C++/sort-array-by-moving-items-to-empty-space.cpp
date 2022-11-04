d6a72dce68a10756ff0d2524337fbd57075bfd9a
kamyu <kamyu104@users.noreply.github.com>
2022-11-04 15:45:30 +0800

    Update sort-array-by-moving-items-to-empty-space.cpp

--- C++/sort-array-by-moving-items-to-empty-space.cpp
+++ C++/sort-array-by-moving-items-to-empty-space.cpp
@@ -6,7 +6,7 @@ class Solution {
 public:
     int sortArray(vector<int>& nums) {
         const auto& min_moves = [&](int d) {
-            const auto& index = [&](int x, int d) {
+            const auto& index = [&](int x) {
                 return (x == 0) ? d * (size(nums) - 1) : x - d;
             };
             vector<int> a(nums);
@@ -14,8 +14,8 @@ public:
             for (int i = 0; i < size(a); ++i) {
                 int l = 1;
                 bool has_zero = (a[i] == 0);
-                for (; index(a[i], d) != i; has_zero |= (a[i] == 0), ++l) {
-                    swap(a[i], a[index(a[i], d)]);
+                for (; index(a[i]) != i; has_zero |= (a[i] == 0), ++l) {
+                    swap(a[i], a[index(a[i])]);
                 }
                 if (l >= 2) {
                     result += has_zero ? l - 1 : l + 1;
