561b91785a06139844f5f0a3ab1eb1dac16a2226
kamyu <kamyu104@users.noreply.github.com>
2022-11-06 20:10:20 +0800

    Create total-cost-to-hire-k-workers.cpp

---
+++ C++/total-cost-to-hire-k-workers.cpp
@@ -0,0 +1,28 @@
+// Time:  O(c + klogc)
+// Space: O(c)
+
+// heap
+class Solution {
+public:
+    long long totalCost(vector<int>& costs, int k, int candidates) {
+        int left = candidates;
+        int right = max(static_cast<int>(size(costs)) - candidates, candidates) - 1;
+        priority_queue<int, vector<int>, greater<int>> min_heap1(cbegin(costs), cbegin(costs) + left);
+        priority_queue<int, vector<int>, greater<int>> min_heap2(cbegin(costs) + right + 1, cend(costs));
+        int64_t result = 0;
+        while (k--) {
+            if (empty(min_heap2) || (!empty(min_heap1) && min_heap1.top() <= min_heap2.top())) {
+                result += min_heap1.top(); min_heap1.pop();
+                if (left <= right) {
+                    min_heap1.emplace(costs[left++]);
+                }
+            } else {
+                result += min_heap2.top(); min_heap2.pop();
+                if (left <= right) {
+                    min_heap2.emplace(costs[right--]);
+                }
+            }
+        }
+        return result;
+    }
+};
