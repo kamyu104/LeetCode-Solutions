e15fe6edd0bb88df26124b2387d66f9028692bae
kamyu <kamyu104@users.noreply.github.com>
2022-11-07 01:27:48 +0800

    Update minimum-total-distance-traveled.cpp

--- C++/minimum-total-distance-traveled.cpp
+++ C++/minimum-total-distance-traveled.cpp
@@ -1,5 +1,5 @@
 // Time:  O(mlogm + nlogn + m * n)
-// Space: O(m * n)
+// Space: O(n)
 
 // sort, dp, prefix sum, mono deque
 class Solution {
@@ -8,22 +8,24 @@ public:
         static const int64_t INF = numeric_limits<int64_t>::max();
         sort(begin(robot), end(robot));
         sort(begin(factory), end(factory));
-        vector<vector<int64_t>> dp(size(factory) + 1, vector<int64_t>(size(robot), INF));  // dp[i][j]: min of factory[:i+1] and robot[:j]
+        vector<int64_t> dp(size(robot), INF);  // dp[j]: min of factory[:i+1] and robot[:j]
         for (int i = 0; i < size(factory); ++i) {
             int64_t prefix = 0;
             deque<pair<int64_t, int>> dq = {{0, -1}};
+            vector<int64_t> new_dp(size(robot), INF);
             for (int j = 0; j < size(robot); ++j) {
                 prefix += abs(robot[j] - factory[i][0]);
                 if (j - dq.back().second == factory[i][1] + 1) {
                     dq.pop_back();
                 }
-                while (!empty(dq) && dq.front().first >= (dp[i][j] != INF ? dp[i][j] - prefix : INF)) {
+                while (!empty(dq) && dq.front().first >= (dp[j] != INF ? dp[j] - prefix : INF)) {
                     dq.pop_front();
                 }
-                dq.emplace_front((dp[i][j] != INF ? dp[i][j] - prefix : INF), j);
-                dp[i + 1][j] = dq.back().first != INF ? dq.back().first + prefix : INF;
+                dq.emplace_front((dp[j] != INF ? dp[j] - prefix : INF), j);
+                new_dp[j] = dq.back().first != INF ? dq.back().first + prefix : INF;
             }
+            dp = move(new_dp);
         }
-        return dp.back().back();
+        return dp.back();
     }
 };
