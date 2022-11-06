fa3f8049d62c521745d84e405df55e86dd9f95fe
kamyu <kamyu104@users.noreply.github.com>
2022-11-07 06:56:47 +0800

    Update minimum-total-distance-traveled.cpp

--- C++/minimum-total-distance-traveled.cpp
+++ C++/minimum-total-distance-traveled.cpp
@@ -27,3 +27,32 @@ public:
         return dp.back();
     }
 };
+
+// Time:  O(mlogm + nlogn + m * n * k)
+// Space: O(n)
+// sort, dp
+class Solution2 {
+public:
+    long long minimumTotalDistance(vector<int>& robot, vector<vector<int>>& factory) {
+        static const int64_t INF = numeric_limits<int64_t>::max();
+        sort(begin(robot), end(robot));
+        sort(begin(factory), end(factory));
+        vector<int64_t> dp(size(robot) + 1, INF);  // dp[j]: min of factory[:i] and robot[:j]
+        dp[0] = 0;
+        for (int i = 0; i < size(factory); ++i) {
+            vector<int64_t> new_dp(size(robot) + 1, INF);
+            new_dp[0] = 0;
+            for (int j = 1; j <= size(robot); ++j) {
+                int64_t prefix = 0;
+                for (int k = 0; k <= min(factory[i][1], j); ++k) {
+                    new_dp[j] = min(new_dp[j], dp[j - k] != INF ? dp[j - k] + prefix : INF);
+                    if ((j - 1) - k >= 0) {
+                        prefix += abs(robot[(j - 1) - k] - factory[i][0]);
+                    }
+                }
+            }
+            dp = move(new_dp);
+        }
+        return dp.back();
+    }
+};
