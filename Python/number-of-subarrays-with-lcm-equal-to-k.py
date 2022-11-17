e3912e7d4b9e7d1924f89965c2018089726bc5e3
kamyu <kamyu104@users.noreply.github.com>
2022-11-18 03:20:56 +0800

    Create number-of-subarrays-with-lcm-equal-to-k.py

---
+++ Python/number-of-subarrays-with-lcm-equal-to-k.py
@@ -0,0 +1,11 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def convertTemperature(self, celsius):
+        """
+        :type celsius: float
+        :rtype: List[float]
+        """
+        return [celsius + 273.15, celsius * 1.80 + 32.00]
