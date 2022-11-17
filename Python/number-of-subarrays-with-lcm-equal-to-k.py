2494cf453a8f38f75c2e2a37811c22f99a94e97a
kamyu <kamyu104@users.noreply.github.com>
2022-11-18 03:21:42 +0800

    Update number-of-subarrays-with-lcm-equal-to-k.py

--- Python/number-of-subarrays-with-lcm-equal-to-k.py
+++ Python/number-of-subarrays-with-lcm-equal-to-k.py
@@ -8,4 +8,4 @@ class Solution(object):
         :type celsius: float
         :rtype: List[float]
         """
-        return [celsius + 273.15, celsius * 1.80 + 32.00]
+        return [celsius+273.15, celsius*1.80+32.00]
