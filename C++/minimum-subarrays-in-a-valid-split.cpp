33b9eabb7893badd09aaf5712191cb8fa472c28a
kamyu <kamyu104@users.noreply.github.com>
2022-11-17 18:19:50 +0800

    Create minimum-subarrays-in-a-valid-split.cpp

---
+++ C++/minimum-subarrays-in-a-valid-split.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n^2 * logr), r = max(nums)
+// Space: O(n)
+
+// dp
+class Solution {
+public:
+    int validSubarraySplit(vector<int>& nums) {
+        static const int INF = numeric_limits<int>::max();
+
+        vector<int> dp(size(nums) + 1, INF);  // dp[i]: min number of subarrays in nums[:i]
+        dp[0] = 0;
+        for (int i = 1; i <= size(nums); ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (gcd(nums[j], nums[i - 1]) != 1) {
+                    if (dp[j] != INF) {
+                        dp[i] = min(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+        }
+        return dp.back() != INF ? dp.back() : -1;
+    }
+};
