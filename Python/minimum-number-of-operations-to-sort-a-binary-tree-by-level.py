ebe172399957bcc02b790e9664f8c86768cb4489
kamyu <kamyu104@users.noreply.github.com>
2022-11-18 13:27:51 +0800

    Create minimum-number-of-operations-to-sort-a-binary-tree-by-level.py

---
+++ Python/minimum-number-of-operations-to-sort-a-binary-tree-by-level.py
@@ -0,0 +1,32 @@
+# Time:  O(n + wlogw)
+# Space: O(w)
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        pass
+
+
+# bfs, sort
+class Solution(object):
+    def minimumOperations(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        result = 0
+        q = [root]
+        while q:
+            new_q = []
+            for node in q:
+                if node.left:
+                    new_q.append(node.left)
+                if node.right:
+                    new_q.append(node.right)
+            idx = range(len(q))
+            idx.sort(key=lambda x: q[x].val)
+            for i in xrange(len(q)):
+                while idx[i] != i:
+                    idx[idx[i]], idx[i] = idx[i], idx[idx[i]]
+                    result += 1
+            q = new_q
+        return result
