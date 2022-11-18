773d6669d2071da4ef3664d75857d729cf0f8392
kamyu <kamyu104@users.noreply.github.com>
2022-11-18 15:03:03 +0800

    Update maximum-number-of-non-overlapping-palindrome-substrings.cpp

--- C++/maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ C++/maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -7,14 +7,14 @@ public:
     int maxPalindromes(string s, int k) {
         int result = 0;
         for (int mid = 0, prev = 0; mid < 2 * size(s) - 1; ++mid) {
-            int left = mid / 2, right = mid / 2 + mid % 2;
-            while (left >= prev && right < size(s) && s[left] == s[right]) {
+            for (int left = mid / 2, right = mid / 2 + mid % 2;
+                 left >= prev && right < size(s) && s[left] == s[right];
+                 --left, ++right) {
                 if (right - left + 1 >= k) {
                     prev = right + 1;
                     ++result;
                     break;
                 }
-                --left, ++right;
             }
         }
         return result;
