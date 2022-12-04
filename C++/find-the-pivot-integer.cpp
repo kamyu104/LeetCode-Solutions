dd1fd52620f395a61d8a15ef220e9a8d36ef8ff6
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 11:40:39 +0800

    Update find-the-pivot-integer.cpp

--- C++/find-the-pivot-integer.cpp
+++ C++/find-the-pivot-integer.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int pivotInteger(int n) {
-        int x = sqrt((n + 1) * n / 2);
+        int x = sqrt((n + 1) * n / 2) + 0.5;
         return x * x == (n + 1) * n / 2 ? x : -1;
     }
 };
