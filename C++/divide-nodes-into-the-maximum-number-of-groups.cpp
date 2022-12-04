ceddaba87abcf80c80d97b5b779dc9c1a2ce3998
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 23:37:34 +0800

    Update divide-nodes-into-the-maximum-number-of-groups.cpp

--- C++/divide-nodes-into-the-maximum-number-of-groups.cpp
+++ C++/divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -70,3 +70,78 @@ public:
         return result;
     }
 };
+
+// Time:  O(n^2)
+// Space: O(n)
+// bfs
+class Solution2 {
+public:
+    int magnificentSets(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for (const auto& e : edges) {
+            adj[e[0] - 1].emplace_back(e[1] - 1);
+            adj[e[1] - 1].emplace_back(e[0] - 1);
+        }
+        vector<bool> lookup(n);
+        const auto& bfs = [&](int u) {
+            vector<int> group = {u};
+            vector<int> q = {u};
+            lookup[u] = true;
+            while (!empty(q)) {
+                vector<int> new_q;
+                for (const auto& u : q) {
+                    group.emplace_back(u);
+                    for (const auto& v : adj[u]) {
+                        if (lookup[v]) {
+                            continue;
+                        }
+                        lookup[v] = true;
+                        new_q.emplace_back(v);
+                    }
+                }
+                q = move(new_q);
+            }
+            return group;
+        };
+        const auto& bfs2 = [&](int u) {
+            int result = 0;
+            vector<bool> lookup(n);
+            unordered_set<int> q = {u};
+            lookup[u] = true;
+            for (; !empty(q); ++result) {
+                unordered_set<int> new_q;
+                for (const auto& u : q) {
+                    for (const auto& v : adj[u]) {
+                        if (q.count(v)) {
+                            return 0;
+                        }
+                        if (lookup[v]) {
+                            continue;
+                        }
+                        lookup[v] = true;
+                        new_q.emplace(v);
+                    }
+                }
+                q = move(new_q);
+            }
+            return result;
+        };
+
+        int result = 0;
+        for (int u = 0; u < n; ++u) {
+            if (lookup[u]) {
+                continue;
+            }
+            const auto& group = bfs(u);
+            int mx = -1;
+            for (const auto& x : group) {
+                mx = max(mx, bfs2(x));
+            }
+            if (mx == 0) {
+                return -1;
+            }
+            result += mx;
+        }
+        return result;
+    }
+};
