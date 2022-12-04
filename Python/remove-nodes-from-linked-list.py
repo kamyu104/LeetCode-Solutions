901cd49756b69d9256a591ee6b43576f16c2a0c3
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 13:54:03 +0800

    Create remove-nodes-from-linked-list.py

---
+++ Python/remove-nodes-from-linked-list.py
@@ -0,0 +1,24 @@
+# Time:  O(n)
+# Space: O(n)
+
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        pass
+
+
+# mono stack
+class Solution(object):
+    def removeNodes(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        stk = []
+        while head:
+            while stk and stk[-1].val < head.val:
+                stk.pop()
+            if stk:
+                stk[-1].next = head
+            stk.append(head)
+            head.next, head = None, head.next
+        return stk[0]
