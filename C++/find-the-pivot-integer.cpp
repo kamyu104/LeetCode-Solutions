f85bac19615c864dd87d27aa86afc274ec9dd12f
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 11:39:22 +0800

    Create find-the-pivot-integer.cpp

---
+++ C++/find-the-pivot-integer.cpp
@@ -0,0 +1,11 @@
+// Time:  O(1)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int pivotInteger(int n) {
+        int x = sqrt((n + 1) * n / 2);
+        return x * x == (n + 1) * n / 2 ? x : -1;
+    }
+};
