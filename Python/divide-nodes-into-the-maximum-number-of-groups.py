ff407a21081b7e7c9ee3fdb28e4c25e8e01a687d
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 23:32:35 +0800

    Update divide-nodes-into-the-maximum-number-of-groups.py

--- Python/divide-nodes-into-the-maximum-number-of-groups.py
+++ Python/divide-nodes-into-the-maximum-number-of-groups.py
@@ -56,3 +56,64 @@ class Solution(object):
                 return -1
             result += max(bfs(x) for x in group)
         return result
+
+# Time:  O(n^2)
+# Space: O(n)
+# bfs
+class Solution2(object):
+    def magnificentSets(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        def bfs(u):
+            group = []
+            q = {u}
+            lookup[u] = True
+            while q:
+                new_q = set()
+                for u in q:
+                    group.append(u)
+                    for v in adj[u]:
+                        if lookup[v]:
+                            continue
+                        lookup[v] = True
+                        new_q.add(v)
+                q = new_q
+            return group
+    
+        def bfs2(u):
+            result = 0
+            lookup = [False]*n
+            q = {u}
+            lookup[u] = True
+            while q:
+                new_q = set()
+                for u in q:
+                    for v in adj[u]:
+                        if v in q:
+                            return -1
+                        if lookup[v]:
+                            continue
+                        lookup[v] = True
+                        new_q.add(v)
+                q = new_q
+                result += 1
+            return result
+    
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u-1].append(v-1)
+            adj[v-1].append(u-1)
+        result = 0
+        lookup = [0]*n
+        for u in xrange(n):
+            if lookup[u]:
+                continue
+            group = bfs(u)
+            mx = max(bfs2(u) for u in group)
+            if mx == -1:
+                return -1
+            result += mx
+        return result
