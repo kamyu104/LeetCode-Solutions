53403d67020244f4ccad0c19a982e1097232e258
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 11:10:35 +0800

    Update count-palindromic-subsequences.cpp

--- C++/count-palindromic-subsequences.cpp
+++ C++/count-palindromic-subsequences.cpp
@@ -1,8 +1,42 @@
+// Time:  O(10^(l/2) * n), l = 5
+// Space: O(10^(l/2) * n)
+
+// prefix sum
+class Solution {
+public:
+    int countPalindromes(string s) {
+        static const int MOD = 1e9 + 7;
+        vector<int> cnt(10);
+        vector<vector<vector<int64_t>>> left(1, vector<vector<int64_t>>(10, vector<int64_t>(10)));
+        for (int k = 0; k < size(s); ++k) {
+            left.emplace_back(left.back());
+            for (int i = 0; i < 10; ++i) {
+                left[k + 1][s[k] - '0'][i] += cnt[i];
+            }
+            ++cnt[s[k] - '0'];
+        }
+        cnt.assign(10, 0);
+        vector<vector<int64_t>> right(10, vector<int64_t>(10));
+        int result = 0;
+        for (int k = size(s) - 1; k >= 0; --k) {
+            for (int i = 0; i < 10; ++i) {
+                for (int j = 0; j < 10; ++j) {
+                    result = (result + left[k][i][j] * right[i][j]) % MOD;
+                }
+            }
+            for (int i = 0; i < 10; ++i) {
+                right[s[k] - '0'][i] += cnt[i];
+            }
+             ++cnt[s[k] - '0'];
+        }
+        return result;
+    }
+};
+
 // Time:  O(10^(l/2) * n * l), l = 5
 // Space: O(l)
-
 // dp
-class Solution {
+class Solution2 {
 public:
     int countPalindromes(string s) {
         static const int MOD = 1e9 + 7;
