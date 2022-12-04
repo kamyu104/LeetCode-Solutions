86d2e035aff0f547f1d9fe1286f5cdcacc295e86
kamyu <kamyu104@users.noreply.github.com>
2022-12-04 13:53:34 +0800

    Create remove-nodes-from-linked-list.cpp

---
+++ C++/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(n)
+
+// mono stack
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        vector<ListNode *> stk;
+        while (head) {
+            while (!empty(stk) && stk.back()->val < head->val) {
+                stk.pop_back();
+            }
+            if (!empty(stk)) {
+                stk.back()->next = head;
+            }
+            stk.emplace_back(head);
+            tie(head->next, head) = make_pair(nullptr, head->next);
+        }
+        return stk[0];
+    }
+};
