d2e75a5fcd26bdbeeec40eb821723ef92dcfc58f
kamyu <kamyu104@users.noreply.github.com>
2022-12-09 22:05:04 +0800

    Update course-schedule-ii.py

--- Python/course-schedule-ii.py
+++ Python/course-schedule-ii.py
@@ -1,11 +1,114 @@
 # Time:  O(|V| + |E|)
 # Space: O(|E|)
+import collections
 
+
+# khan's algorithm
+class Solution(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        adj = collections.defaultdict(list)
+        adj = collections.defaultdict(list)
+        in_degree = collections.Counter()
+        for u, v in prerequisites:
+            in_degree[u] += 1
+            adj[v].append(u)
+        result = []
+        q = [u for u in xrange(numCourses) if in_degree[u] == 0]
+        while q:
+            new_q = []
+            for u in q:
+                result.append(u)
+                for v in adj[u]:
+                    in_degree[v] -= 1
+                    if in_degree[v] == 0:
+                        new_q.append(v)
+            q = new_q
+        return result if len(result) == numCourses else []
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+import collections
+
+
+# dfs solution
+class Solution2(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        WHITE, GRAY, BLACK = range(3)
+        def dfs(u):
+            if lookup[u] != WHITE:
+                return lookup[u] == BLACK
+            lookup[u] = GRAY
+            if any(not dfs(v) for v in adj[u]):
+                return False
+            lookup[u] = BLACK
+            result.append(u)  # should be postorder
+            return True
+
+        result = []
+        adj = collections.defaultdict(list)
+        for u, v in prerequisites:
+            adj[v].append(u)
+        lookup = collections.defaultdict(lambda:WHITE)
+        for u in xrange(numCourses):
+            if not dfs(u):
+                return []
+        result.reverse()
+        return result
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
+import collections
+
+
+# dfs solution
+class Solution3(object):
+    def findOrder(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: List[int]
+        """
+        WHITE, GRAY, BLACK = range(3)
+        def dfs(u):
+            if lookup[u] != WHITE:
+                return lookup[u] == BLACK
+            lookup[u] = GRAY
+            if any(not dfs(v) for v in adj[u]):
+                return False
+            lookup[u] = BLACK
+            result.append(u)  # should be postorder
+            return True
+
+        adj = collections.defaultdict(list)
+        for u, v in prerequisites:
+            adj[u].append(v)
+        result = []
+        lookup = collections.defaultdict(lambda:WHITE)
+        for u in xrange(numCourses):
+            if not dfs(u):
+                return []
+        return result
+
+
+# Time:  O(|V| + |E|)
+# Space: O(|E|)
 import collections
 
 
 # bfs solution
-class Solution(object):
+class Solution4(object):
     def findOrder(self, numCourses, prerequisites):
         """
         :type numCourses: int
@@ -34,7 +137,7 @@ class Solution(object):
 # Time:  O(|V| + |E|)
 # Space: O(|E|)
 # dfs solution
-class Solution2(object):
+class Solution5(object):
     def findOrder(self, numCourses, prerequisites):
         """
         :type numCourses: int
