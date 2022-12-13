f0e7c0ff7ca7d3a3fee5975fb9ed64586ac87825
kamyu <kamyu104@users.noreply.github.com>
2022-12-13 21:36:56 +0800

    Update minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py

--- Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ Python/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -12,20 +12,20 @@ class Solution(object):
         def a_star(grid, b, t):
             f, dh = 0, 1
             closer, detour = [b], []
-            lookup = [[False]*len(grid[0]) for _ in xrange(len(grid))]
+            lookup = set()
             while closer or detour:
                 if not closer:
                     f += dh
                     closer, detour = detour, closer
                 b = closer.pop()
+                if b in lookup:
+                    continue
+                lookup.add(b)
                 if b == t:
                     return f
-                if lookup[b[0]][b[1]]:
-                    continue
-                lookup[b[0]][b[1]] = True
                 for nd, (dr, dc) in enumerate(directions, 1):
                     nb = (b[0]+dr, b[1]+dc)
-                    if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and not lookup[nb[0]][nb[1]]):
+                    if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and nb not in lookup):
                         continue
                     (closer if nd == grid[b[0]][b[1]] else detour).append(nb)
             return -1
@@ -48,17 +48,17 @@ class Solution2(object):
         directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         b, t = (0, 0), (len(grid)-1, len(grid[0])-1)
         dq = collections.deque([(b, 0)])
-        lookup = [[False]*len(grid[0]) for _ in xrange(len(grid))]
+        lookup = set()
         while dq:
             b, d = dq.popleft()
+            if b in lookup:
+                continue
+            lookup.add(b)
             if b == t:
                 return d
-            if lookup[b[0]][b[1]]:
-                    continue
-            lookup[b[0]][b[1]] = True
             for nd, (dr, dc) in enumerate(directions, 1):
                 nb = (b[0]+dr, b[1]+dc)
-                if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and not lookup[nb[0]][nb[1]]):
+                if not (0 <= nb[0] < len(grid) and 0 <= nb[1] < len(grid[0]) and nb not in lookup):
                     continue
                 if nd == grid[b[0]][b[1]]:
                     dq.appendleft((nb, d))
