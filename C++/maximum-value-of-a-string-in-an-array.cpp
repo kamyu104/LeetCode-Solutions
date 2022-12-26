73852afbe5b3ab885df52cee12f485ec79e9b2d9
kamyu <kamyu104@users.noreply.github.com>
2022-12-27 01:38:35 +0800

    Update maximum-value-of-a-string-in-an-array.cpp

--- C++/maximum-value-of-a-string-in-an-array.cpp
+++ C++/maximum-value-of-a-string-in-an-array.cpp
@@ -7,7 +7,7 @@ public:
     int maximumValue(vector<string>& strs) {
         int result = 0;
         for (const auto& s : strs) {
-            result = max(result, all_of(cbegin(s), cend(s), ::isdigit) 
+            result = max(result, all_of(cbegin(s), cend(s), ::isdigit)
                                  ? stoi(s)
                                  : static_cast<int>(size(s)));
         }
