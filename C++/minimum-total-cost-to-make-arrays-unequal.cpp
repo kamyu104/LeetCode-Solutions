c6db1e09c0f2b356918f15e57411df49e5dff912
kamyu <kamyu104@users.noreply.github.com>
2022-12-27 05:56:04 +0800

    Create minimum-total-cost-to-make-arrays-unequal.cpp

---
+++ C++/minimum-total-cost-to-make-arrays-unequal.cpp
@@ -0,0 +1,42 @@
+// Time:  O(n)
+// Space: O(n)
+
+// greedy
+class Solution {
+public:
+    long long minimumTotalCost(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> cnt;
+        int64_t result = 0;
+        for (int i = 0; i < size(nums1); ++i) {
+            if (nums1[i] != nums2[i]) {
+                continue;
+            }
+            ++cnt[nums1[i]];
+            result += i;
+        }
+        if (empty(cnt)) {
+            return 0;
+        }
+        const int majority = max_element(cbegin(cnt), cend(cnt),
+                                         [](const auto& a, const auto& b) {
+                                                return a.second < b.second;
+                                         })->first;
+        int remain = cnt[majority] - (accumulate(cbegin(cnt), cend(cnt), 0,
+                                                 [](const auto& total, const auto& x) {
+                                                     return total + x.second;
+                                                 }) - cnt[majority]);
+        if (remain <= 0) {
+            return result;
+        }
+        for (int i = 0; i < size(nums1); ++i) {
+            if (nums1[i] == nums2[i] || nums1[i] == majority || nums2[i] == majority) {
+                continue;
+            }
+            result += i;
+            if (--remain == 0) {
+                return result;
+            }
+        }
+        return -1;
+    }
+};
