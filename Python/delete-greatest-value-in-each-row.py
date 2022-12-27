6bd79454d5519db89982745363eeae380a3065c1
kamyu <kamyu104@users.noreply.github.com>
2022-12-27 20:34:53 +0800

    Create delete-greatest-value-in-each-row.py

---
+++ Python/delete-greatest-value-in-each-row.py
@@ -0,0 +1,13 @@
+# Time:  O(m * n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def deleteGreatestValue(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        for row in grid:
+            row.sort()
+        return sum(max(grid[i][j] for i in xrange(len(grid))) for j in xrange(len(grid[0])))
