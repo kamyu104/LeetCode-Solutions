d05a70825ebb42ceeb30f4e5f5da3f94eafe749c
kamyu <kamyu104@users.noreply.github.com>
2022-12-27 20:34:04 +0800

    Create delete-greatest-value-in-each-row.cpp

---
+++ C++/delete-greatest-value-in-each-row.cpp
@@ -0,0 +1,21 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int deleteGreatestValue(vector<vector<int>>& grid) {
+        for (auto& row : grid) {
+            sort(begin(row), end(row));
+        }
+        int result = 0;
+        for (int j = 0; j < size(grid[0]); ++j) {
+            int mx = 0;
+            for (int i = 0; i < size(grid); ++i) {
+                mx = max(mx, grid[i][j]);
+            }
+            result += mx;
+        }
+        return result;
+    }
+};
