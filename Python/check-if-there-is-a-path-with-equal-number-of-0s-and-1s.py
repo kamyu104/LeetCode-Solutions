811b3554ca93c22136c537673e0a422c8ac1892c
kamyu <kamyu104@users.noreply.github.com>
2022-12-28 14:33:08 +0800

    Create check-if-there-is-a-path-with-equal-number-of-0s-and-1s.py

---
+++ Python/check-if-there-is-a-path-with-equal-number-of-0s-and-1s.py
@@ -0,0 +1,21 @@
+# Time:  O(m * n)
+# Space: O(n)
+
+# dp
+class Solution(object):
+    def isThereAPath(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        if (len(grid)+len(grid[0])-1)%2 != 0:
+            return False
+        dp_left = [[float("inf")]*(len(grid[0])+1) for _ in xrange(2)]
+        dp_left[0][1] = 0
+        dp_right = [[float("-inf")]*(len(grid[0])+1) for _ in xrange(2)]
+        dp_right[0][1] = 0
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                dp_left[(i+1)%2][j+1] = min(dp_left[i%2][j+1], dp_left[(i+1)%2][j])+grid[i][j]
+                dp_right[(i+1)%2][j+1] = max(dp_right[i%2][j+1], dp_right[(i+1)%2][j])+grid[i][j]
+        return dp_left[len(grid)%2][-1] <= (len(grid)+len(grid[0])-1)//2 <= dp_right[len(grid)%2][-1]
