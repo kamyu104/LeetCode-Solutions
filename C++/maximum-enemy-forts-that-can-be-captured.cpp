e930f361f56b86be0eade8fe30aeb1fdb4b2f8a2
kamyu <kamyu104@users.noreply.github.com>
2022-12-29 00:52:04 +0800

    Create maximum-enemy-forts-that-can-be-captured.cpp

---
+++ C++/maximum-enemy-forts-that-can-be-captured.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array, two pointers
+class Solution {
+public:
+    int captureForts(vector<int>& forts) {
+        int result = 0, left = 0;
+        for (int right = 0; right < size(forts); ++right) {
+            if (!forts[right]) {
+                continue;
+            }
+            if (forts[right] == -forts[left]) {
+                result = max(result, right - left - 1);
+            }
+            left = right;
+        }
+        return result;
+    }
+};
