5677a9b2e52b83605e395657daca8a6fc550823d
kamyu <kamyu104@users.noreply.github.com>
2022-12-28 14:48:37 +0800

    Update check-if-there-is-a-path-with-equal-number-of-0s-and-1s.cpp

--- C++/check-if-there-is-a-path-with-equal-number-of-0s-and-1s.cpp
+++ C++/check-if-there-is-a-path-with-equal-number-of-0s-and-1s.cpp
@@ -9,9 +9,9 @@ public:
             return false;
         }
         vector<vector<int>> dp_left(2, vector<int>(size(grid[0]) + 1, numeric_limits<int>::max()));
-        dp_left[0][1] = 0;
+        dp_left[0][1] = dp_left[1][0] = 0;
         vector<vector<int>> dp_right(2, vector<int>(size(grid[0]) + 1, numeric_limits<int>::min()));
-        dp_right[0][1] = 0;
+        dp_right[0][1] = dp_right[1][0] = 0;
         for (int i = 0; i < size(grid); ++i) {
             for (int j = 0; j < size(grid[0]); ++j) {
                 dp_left[(i + 1) % 2][j + 1] = min(dp_left[i % 2][j + 1], dp_left[(i + 1) % 2][j]) + grid[i][j];
