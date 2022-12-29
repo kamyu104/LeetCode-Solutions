6952bfec8b4c2fa2eb88e65d0617d4db5b5102c8
kamyu <kamyu104@users.noreply.github.com>
2022-12-30 01:55:33 +0800

    Update course-schedule.cpp

--- C++/course-schedule.cpp
+++ C++/course-schedule.cpp
@@ -1,14 +1,15 @@
 // Time:  O(|V| + |E|)
 // Space: O(|E|)
 
-// dfs solution
+// Kahn’s algorithm (bfs solution)
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, unordered_set<int>> in_degree, out_degree;
+        unordered_map<int, vector<int>> adj;
+        unordered_map<int, int> in_degree;
         for (const auto& p : prerequisites) {
-            in_degree[p[0]].emplace(p[1]);
-            out_degree[p[1]].emplace(p[0]);
+            ++in_degree[p[0]];
+            adj[p[1]].emplace_back(p[0]);
         }
         queue<int> q;
         for (int i = 0; i < numCourses; ++i) {
@@ -16,31 +17,31 @@ public:
                 q.emplace(i);
             }
         }
+        vector<int> result;
         while (!q.empty()) {
             const auto node = q.front(); q.pop();
-            for (const auto& i : out_degree[node]) {
-                in_degree[i].erase(node);
-                if (in_degree[i].empty()) {
+            result.emplace_back(node);
+            for (const auto& i : adj[node]) {
+                if (!--in_degree[i]) {
                     q.emplace(i);
-                    in_degree.erase(i);
                 }
             }
-            out_degree.erase(node);
         }
-        return in_degree.empty() && out_degree.empty();
+        return size(result) == numCourses;
     }
 };
 
 // Time:  O(|V| + |E|)
 // Space: O(|E|)
-// bfs solution
+// dfs solution
 class Solution2 {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, unordered_set<int>> in_degree, out_degree;
+        unordered_map<int, vector<int>> adj;
+        unordered_map<int, int> in_degree;
         for (const auto& p : prerequisites) {
-            in_degree[p[0]].emplace(p[1]);
-            out_degree[p[1]].emplace(p[0]);
+            ++in_degree[p[0]];
+            adj[p[1]].emplace_back(p[0]);
         }
         vector<int> stk;
         for (int i = 0; i < numCourses; ++i) {
@@ -48,17 +49,16 @@ public:
                 stk.emplace_back(i);
             }
         }
+        vector<int> result;
         while (!stk.empty()) {
             const auto node = stk.back(); stk.pop_back();
-            for (const auto& i : out_degree[node]) {
-                in_degree[i].erase(node);
-                if (in_degree[i].empty()) {
+            result.emplace_back(node);
+            for (const auto& i : adj[node]) {
+                if (!--in_degree[i]) {
                     stk.emplace_back(i);
-                    in_degree.erase(i);
                 }
             }
-            out_degree.erase(node);
         }
-        return in_degree.empty() && out_degree.empty();
+        return size(result) == numCourses;
     }
 };
