1d2fbcb2000012c73f1cc878a9ce45ecafbf379e
kamyu <kamyu104@users.noreply.github.com>
2022-12-30 01:58:30 +0800

    Update course-schedule.py

--- Python/course-schedule.py
+++ Python/course-schedule.py
@@ -33,6 +33,9 @@ class Solution(object):
 
 # Time:  O(|V| + |E|)
 # Space: O(|E|)
+import collections
+
+
 # dfs solution
 class Solution2(object):
     def canFinish(self, numCourses, prerequisites):
