3b4018cc42b6afd6ffb510d050a14d47abe77c37
kamyu <kamyu104@users.noreply.github.com>
2022-12-30 01:30:48 +0800

    Update course-schedule-ii.cpp

--- C++/course-schedule-ii.cpp
+++ C++/course-schedule-ii.cpp
@@ -1,7 +1,7 @@
 // Time:  O(|V| + |E|)
 // Space: O(|E|)
 
-// dfs solution
+// bfs solution
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
@@ -33,15 +33,15 @@ public:
 
 // Time:  O(|V| + |E|)
 // Space: O(|E|)
-// bfs solution
+// dfs solution
 class Solution2 {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        unordered_map<int, unordered_set<int>> adj;
+        unordered_map<int, vector<int>> adj;
         unordered_map<int, int> in_degree;
         for (const auto& p : prerequisites) {
             ++in_degree[p[0]];
-            adj[p[1]].emplace(p[0]);
+            adj[p[1]].emplace_back(p[0]);
         }
         vector<int> stk;
         for (int i = 0; i < numCourses; ++i) {
