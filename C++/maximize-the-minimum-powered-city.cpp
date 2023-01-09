16500ac83d094a8ef98675dee3392227e523aff2
kamyu <kamyu104@users.noreply.github.com>
2023-01-10 00:56:41 +0800

    Create maximize-the-minimum-powered-city.cpp

---
+++ C++/maximize-the-minimum-powered-city.cpp
@@ -0,0 +1,62 @@
+// Time:  O(nlogk)
+// Space: O(n)
+
+// binary search, sliding window, greedy
+class Solution {
+public:
+    long long maxPower(vector<int>& stations, int r, int k) {
+        const auto& min_power = [&]() {
+            int64_t mn = numeric_limits<int64_t>::max();
+            int64_t curr = accumulate(cbegin(stations), cbegin(stations) + r, 0ll);
+            for (int i = 0; i < size(stations); ++i) {
+                if (i + r < size(stations)) {
+                    curr += stations[i + r];
+                }
+                if (i >= r + 1) {
+                    curr -= stations[i - (r + 1)];
+                }
+                mn = min(mn, curr);
+            }
+            return mn;
+        };
+
+        const int64_t mn = min_power();
+        const auto& check = [&](int64_t target) {
+            vector<int> arr(cbegin(stations), cend(stations));
+            int64_t curr = accumulate(cbegin(arr), cbegin(arr) + r, 0ll);
+            int cnt = k;
+            for (int i = 0; i < size(arr); ++i) {
+                if (i + r < size(arr)) {
+                    curr += arr[i + r];
+                }
+                if (i >= r + 1) {
+                    curr -= arr[i - (r + 1)];
+                }
+                if (curr >= target) {
+                    continue;
+                }
+                const auto d = target - curr;
+                if (d > cnt) {
+                    return false;
+                }
+                cnt -= d;
+                curr += d;
+                if (i + r < size(arr)) {
+                    arr[i + r] += d;
+                }
+            }
+            return true;
+        };
+
+        int64_t left = mn, right = mn + k;
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (!check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+};
