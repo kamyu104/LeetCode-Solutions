7b95f4bcac12d159f0b6544f5e9ff31c55c76d12
kamyu <kamyu104@users.noreply.github.com>
2023-01-09 23:06:10 +0800

    Update categorize-box-according-to-criteria.cpp

--- C++/categorize-box-according-to-criteria.cpp
+++ C++/categorize-box-according-to-criteria.cpp
@@ -5,8 +5,8 @@
 class Solution {
 public:
     string categorizeBox(int length, int width, int height, int mass) {
-        bool bulky = (length >= 10000  || width >= 10000 || height >= 10000) ||
-                     1ll * length * width * height >= 1000000000;
+        bool bulky = (length >= 10'000  || width >= 10'000 || height >= 10'000) ||
+                     1ll * length * width * height >= 1'000'000'000;
         bool heavy = mass >= 100;
         if (bulky && heavy) {
             return "Both";
@@ -28,8 +28,8 @@ class Solution2 {
 public:
     string categorizeBox(int length, int width, int height, int mass) {
         static const vector<string> CATEGORIES = {"Neither", "Heavy", "Bulky", "Both"};
-        const int i = 2 * ((length >= 10000  || width >= 10000 || height >= 10000) ||
-                      1ll * length * width * height >= 1000000000) + (mass >= 100);
+        const int i = 2 * ((length >= 10'000  || width >= 10'000 || height >= 10'000) ||
+                     1ll * length * width * height >= 1'000'000'000) + (mass >= 100);
         return CATEGORIES[i];
     }
 };
