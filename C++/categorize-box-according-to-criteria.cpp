1370cecb578cd38947474e660a547014073e881c
kamyu <kamyu104@users.noreply.github.com>
2023-01-09 23:07:02 +0800

    Update categorize-box-according-to-criteria.cpp

--- C++/categorize-box-according-to-criteria.cpp
+++ C++/categorize-box-according-to-criteria.cpp
@@ -29,7 +29,7 @@ public:
     string categorizeBox(int length, int width, int height, int mass) {
         static const vector<string> CATEGORIES = {"Neither", "Heavy", "Bulky", "Both"};
         const int i = 2 * ((length >= 10'000  || width >= 10'000 || height >= 10'000) ||
-                     1ll * length * width * height >= 1'000'000'000) + (mass >= 100);
+                      1ll * length * width * height >= 1'000'000'000) + (mass >= 100);
         return CATEGORIES[i];
     }
 };
