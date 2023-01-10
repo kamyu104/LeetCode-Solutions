33999b19d8968c08fe74d00533d8cb502a9439b0
kamyu <kamyu104@users.noreply.github.com>
2023-01-10 14:46:54 +0800

    Update time-to-cross-a-bridge.py

--- Python/time-to-cross-a-bridge.py
+++ Python/time-to-cross-a-bridge.py
@@ -15,26 +15,26 @@ class Solution(object):
         """
         left_bridge, right_ware, right_bridge, left_ware = [(-(time[i][0]+time[i][2]), -i) for i in xrange(k)], [], [], []
         heapq.heapify(left_bridge)
-        result = 0
+        curr = 0
         while n:
-            while left_ware and left_ware[0][0] <= result:
+            while left_ware and left_ware[0][0] <= curr:
                 _, i = heapq.heappop(left_ware)
                 heapq.heappush(left_bridge, (-(time[i][0]+time[i][2]), -i))
-            while right_ware and right_ware[0][0] <= result:
+            while right_ware and right_ware[0][0] <= curr:
                 _, i = heapq.heappop(right_ware)
                 heapq.heappush(right_bridge, (-(time[i][0]+time[i][2]), -i))
             if right_bridge:
                 _, i = heapq.heappop(right_bridge)
                 i = -i
-                result += time[i][2]
-                heapq.heappush(left_ware, (result+time[i][3], i))
+                curr += time[i][2]
+                heapq.heappush(left_ware, (curr+time[i][3], i))
                 n -= 1
             elif left_bridge and n-len(right_ware):
                 _, i = heapq.heappop(left_bridge)
                 i = -i
-                result += time[i][0]
-                heapq.heappush(right_ware, (result+time[i][1], i))
+                curr += time[i][0]
+                heapq.heappush(right_ware, (curr+time[i][1], i))
             else:
-                result = min(left_ware[0][0] if left_ware else float("inf"),
-                             right_ware[0][0] if right_ware else float("inf"))
-        return result
+                curr = min(left_ware[0][0] if left_ware else float("inf"),
+                           right_ware[0][0] if right_ware else float("inf"))
+        return curr
