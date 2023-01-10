bfc2a33c906037253b8a6777ba8360d1eb90d175
kamyu <kamyu104@users.noreply.github.com>
2023-01-10 14:46:58 +0800

    Update time-to-cross-a-bridge.cpp

--- C++/time-to-cross-a-bridge.cpp
+++ C++/time-to-cross-a-bridge.cpp
@@ -11,30 +11,30 @@ public:
         }
         priority_queue<pair<int, int>> left_bridge(cbegin(workers), cend(workers)), right_bridge;
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> left_ware, right_ware;
-        int result = 0;
+        int curr = 0;
         while (n) {
-            while (!empty(left_ware) && left_ware.top().first <= result) {
+            while (!empty(left_ware) && left_ware.top().first <= curr) {
                 const auto [_, i] = left_ware.top(); left_ware.pop();
                 left_bridge.emplace(time[i][0] + time[i][2], i);
             }
-            while (!empty(right_ware) && right_ware.top().first <= result) {
+            while (!empty(right_ware) && right_ware.top().first <= curr) {
                 const auto [_, i] = right_ware.top(); right_ware.pop();
                 right_bridge.emplace(time[i][0] + time[i][2], i);
             }
             if (!empty(right_bridge)) {
                 const auto [_, i] = right_bridge.top(); right_bridge.pop();
-                result += time[i][2];
-                left_ware.emplace(result + time[i][3], i);
+                curr += time[i][2];
+                left_ware.emplace(curr + time[i][3], i);
                 --n;
             } else if (!empty(left_bridge) && n - size(right_ware)) {
                 const auto [_, i] = left_bridge.top(); left_bridge.pop();
-                result += time[i][0];
-                right_ware.emplace(result + time[i][1], i);
+                curr += time[i][0];
+                right_ware.emplace(curr + time[i][1], i);
             } else {
-                result = min(!empty(left_ware) ? left_ware.top().first : numeric_limits<int>::max(),
-                             !empty(right_ware) ? right_ware.top().first : numeric_limits<int>::max());
+                curr = min(!empty(left_ware) ? left_ware.top().first : numeric_limits<int>::max(),
+                           !empty(right_ware) ? right_ware.top().first : numeric_limits<int>::max());
             }
         }
-        return result;
+        return curr;
     }
 };
