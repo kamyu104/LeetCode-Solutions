757c9e41ae564bab6ae6388e2b4819241fd8e4e8
kamyu <kamyu104@users.noreply.github.com>
2023-01-14 12:24:24 +0800

    Create time-taken-to-cross-the-door.cpp

---
+++ C++/time-taken-to-cross-the-door.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(n)
+
+// queue, simulation
+class Solution {
+public:
+    vector<int> timeTaken(vector<int>& arrival, vector<int>& state) {
+        enum {ENTERING, EXITING};
+        vector<int> result(size(arrival));
+        int curr = 0, direction = EXITING;
+        vector<queue<int>> q(2);
+        const auto& go_until = [&](int t) {
+            while (curr <= t && any_of(cbegin(q), cend(q), [](const auto& x) { return !empty(x); })) {
+                if (empty(q[direction])) {
+                    direction ^= 1;
+                }
+                result[q[direction].front()] = curr++;
+                q[direction].pop();
+            }
+        };
+
+        for (int i = 0; i < size(arrival); ++i) {
+            go_until(arrival[i] - 1);
+            q[state[i]].emplace(i);
+            if (!(arrival[i] <= curr)) {
+                curr = arrival[i], direction = EXITING;
+            }
+        }
+        go_until(numeric_limits<int>::max());
+        return result;
+    }
+};
