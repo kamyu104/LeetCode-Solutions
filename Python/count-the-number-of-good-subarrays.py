6f291cd92ea3d5655744216b4f49071b03e3769f
kamyu <kamyu104@users.noreply.github.com>
2023-01-15 18:25:07 +0800

    Create count-the-number-of-good-subarrays.py

---
+++ Python/count-the-number-of-good-subarrays.py
@@ -0,0 +1,25 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# two pointers, sliding window
+class Solution(object):
+    def countGood(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = curr = left = 0
+        cnt = collections.Counter()
+        for right in xrange(len(nums)):
+            curr += cnt[nums[right]]
+            cnt[nums[right]] += 1
+            while not curr < k:
+                cnt[nums[left]] -= 1
+                curr -= cnt[nums[left]]
+                left += 1
+            result += left
+        return result
