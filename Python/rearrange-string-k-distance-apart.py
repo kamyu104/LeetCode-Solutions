7040f91804482ba715a23c6b616d66af6d116657
kamyu <kamyu104@users.noreply.github.com>
2023-01-16 00:25:43 +0800

    Update rearrange-string-k-distance-apart.py

--- Python/rearrange-string-k-distance-apart.py
+++ Python/rearrange-string-k-distance-apart.py
@@ -28,33 +28,22 @@ class Solution(object):
 # Time:  O(n)
 # Space: O(n)
 class Solution2(object):
-    def rearrangeString(self, str, k):
+    def rearrangeString(self, s, k):
         """
         :type str: str
         :type k: int
         :rtype: str
         """
-        cnts = [0] * 26
-        for c in str:
-            cnts[ord(c) - ord('a')] += 1
-
-        sorted_cnts = []
-        for i in xrange(26):
-            sorted_cnts.append((cnts[i], chr(i + ord('a'))))
-        sorted_cnts.sort(reverse=True)
-
-        max_cnt = sorted_cnts[0][0]
-        blocks = [[] for _ in xrange(max_cnt)]
+        cnts = collections.Counter(s)
+        bucket_cnt = max(cnts.itervalues())
+        blocks = [[] for _ in xrange(bucket_cnt)]
         i = 0
-        for cnt in sorted_cnts:
-            for _ in xrange(cnt[0]):
-                blocks[i].append(cnt[1])
-                i = (i + 1) % max(cnt[0], max_cnt - 1)
-
-        for i in xrange(max_cnt-1):
-            if len(blocks[i]) < k:
-                return ""
-
+        for c in sorted(cnts.keys(), key=lambda x: cnts[x], reverse=True):
+            for _ in xrange(cnts[c]):
+                blocks[i].append(c)
+                i = (i+1) % max(cnts[c], bucket_cnt-1)
+        if any(len(blocks[i]) < k for i in xrange(bucket_cnt-1)):
+            return ""
         return "".join(map(lambda x : "".join(x), blocks))
 
 
