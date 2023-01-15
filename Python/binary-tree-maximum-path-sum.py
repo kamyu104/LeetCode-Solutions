d92b5e436e50ef08e2b222178b895adc77cfe954
kamyu <kamyu104@users.noreply.github.com>
2023-01-15 22:25:10 +0800

    Update binary-tree-maximum-path-sum.py

--- Python/binary-tree-maximum-path-sum.py
+++ Python/binary-tree-maximum-path-sum.py
@@ -46,7 +46,7 @@ class Solution2(object):
                 return (float("-inf"), 0)
             max_left, curr_left = dfs(node.left)
             max_right, curr_right = dfs(node.right)
-            return (max(max_left, max_right, node.val+max(curr_left, 0)+max(curr_right,0)),
+            return (max(max_left, max_right, node.val+max(curr_left, 0)+max(curr_right, 0)),
                     node.val+max(curr_left, curr_right, 0))
         
         return dfs(root)[0]
