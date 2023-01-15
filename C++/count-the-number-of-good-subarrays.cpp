b02d4034b365ee4ab5f049cb389de8ce9d3853b8
kamyu <kamyu104@users.noreply.github.com>
2023-01-15 18:24:12 +0800

    Create count-the-number-of-good-subarrays.cpp

---
+++ C++/count-the-number-of-good-subarrays.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(n)
+
+// two pointers, sliding window
+class Solution {
+public:
+    long long countGood(vector<int>& nums, int k) {
+        int64_t result = 0, curr = 0;
+        unordered_map<int, int> cnt;
+        for (int right = 0, left = 0; right < size(nums); ++right) {
+            curr += cnt[nums[right]]++;
+            while (!(curr < k)) {
+                curr -= --cnt[nums[left++]];
+            }
+            result += left;
+        }
+        return result;
+    }
+};
