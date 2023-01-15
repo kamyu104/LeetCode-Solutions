19674d509872f27691eee9f4153d4059d3902cc6
kamyu <kamyu104@users.noreply.github.com>
2023-01-15 20:17:54 +0800

    Update difference-between-maximum-and-minimum-price-sum.py

--- Python/difference-between-maximum-and-minimum-price-sum.py
+++ Python/difference-between-maximum-and-minimum-price-sum.py
@@ -72,3 +72,105 @@ class Solution2(object):
             adj[v].append(u)
         dfs(0, -1)
         return result[0]
+
+
+# Time:  O(n)
+# Space: O(n)
+# iterative dfs, tree dp
+class Solution3(object):
+    def maxOutput(self, n, edges, price):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type price: List[int]
+        :rtype: int
+        """
+        def iter_dfs():
+            dp = [price[u] for u in xrange(n)]  # max_sum
+            stk = [(1, 0, -1)]
+            while stk:
+                step, u, p = stk.pop()
+                if step == 1:
+                    stk.append((2, u, p))
+                    for v in adj[u]:
+                        if v == p:
+                            continue
+                        stk.append((1, v, u))
+                elif step == 2:
+                    for v in adj[u]:
+                        if v == p:
+                            continue
+                        dp[u] = max(dp[u], dp[v]+price[u])
+            return dp
+        
+        def iter_dfs2():
+            result = 0
+            stk = [(0, -1, 0)]
+            while stk:
+                u, p, curr = stk.pop()
+                result = max(result, curr, dp[u]-price[u])
+                top2 = [[curr, p], [0, -1]]
+                for v in adj[u]:
+                    if v == p:
+                        continue
+                    curr = [dp[v], v]
+                    for i in xrange(len(top2)):
+                        if curr > top2[i]:
+                            top2[i], curr = curr, top2[i]
+                for v in adj[u]:
+                    if v == p:
+                        continue
+                    stk.append((v, u, (top2[0][0] if top2[0][1] != v else top2[1][0])+price[u]))
+            return result
+    
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        dp = iter_dfs()
+        return iter_dfs2()
+
+
+# Time:  O(n)
+# Space: O(n)
+# dfs, tree dp
+class Solution4(object):
+    def maxOutput(self, n, edges, price):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type price: List[int]
+        :rtype: int
+        """
+        def dfs(u, p):
+            dp[u] = price[u]
+            for v in adj[u]:
+                if v == p:
+                    continue
+                dp[u] = max(dp[u], dfs(v, u)+price[u])
+            return dp[u]
+        
+        def dfs2(u, p, curr):
+            result[0] = max(result[0], curr, dp[u]-price[u])
+            top2 = [[curr, p], [0, -1]]
+            for v in adj[u]:
+                if v == p:
+                    continue
+                curr = [dp[v], v]
+                for i in xrange(len(top2)):
+                    if curr > top2[i]:
+                        top2[i], curr = curr, top2[i]
+            for v in adj[u]:
+                if v == p:
+                    continue
+                dfs2(v, u, (top2[0][0] if top2[0][1] != v else top2[1][0])+price[u])
+    
+        result = [0]
+        dp = [0]*n  # max_sum
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        dfs(0, -1)
+        dfs2(0, -1, 0)
+        return result[0]
