5dd2f02deb0428ffb4459cd279fe9e4e4d4448f5
kamyu <kamyu104@users.noreply.github.com>
2023-01-30 18:26:49 +0800

    Create maximum-price-to-fill-a-bag.cpp

---
+++ C++/maximum-price-to-fill-a-bag.cpp
@@ -0,0 +1,19 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// greedy, sort
+class Solution {
+public:
+    double maxPrice(vector<vector<int>>& items, int capacity) {
+        sort(begin(items), end(items), [](const auto& a, const auto& b) {
+            return a[0] * b[1] > a[1] * b[0];
+        });
+        double result = 0;
+        for (const auto& item : items) {
+            const auto cnt = min(item[1], capacity);
+            capacity -= cnt;
+            result += (static_cast<double>(item[0]) / item[1]) * cnt;
+        }
+        return capacity == 0 ? result : -1;
+    }
+};
