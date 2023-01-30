94b5627941fa0f3fcc939c8e3d34386094e81536
kamyu <kamyu104@users.noreply.github.com>
2023-01-30 21:55:48 +0800

    Create count-increasing-quadruplets.py

---
+++ Python/count-increasing-quadruplets.py
@@ -0,0 +1,70 @@
+# Time:  O(n^2)
+# Space: O(n)
+
+# dp
+class Solution(object):
+    def countQuadruplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = [0]*len(nums)  # dp[j]: # of nums[i] < nums[k] < nums[j] in the first k nums
+        result = 0
+        for k in xrange(len(nums)):
+            cnt = 0
+            for j in xrange(k):
+                if nums[j] < nums[k]:
+                    cnt += 1
+                    result += dp[j]
+                elif nums[j] > nums[k]:
+                    dp[j] += cnt
+        return result
+
+    
+# Time:  O(n^2)
+# Space: O(n^2)
+# prefix sum
+class Solution2(object):
+    def countQuadruplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        right = [[0]*(len(nums)+1) for _ in xrange(len(nums))]
+        for j in xrange(len(nums)):
+            for i in reversed(xrange(j+1, len(nums))):
+                right[j][i] = right[j][i+1] + int(nums[i] > nums[j])
+        result = 0
+        for k in xrange(len(nums)):
+            left = 0
+            for j in xrange(k):
+                if nums[k] < nums[j]:
+                    result += left*right[j][k+1]
+                else:
+                    left += 1
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(n^2)
+# prefix sum
+class Solution3(object):
+    def countQuadruplets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = [[0]*(len(nums)+1) for _ in xrange(len(nums))]
+        for j in xrange(len(nums)):
+            for i in xrange(j):
+                left[j][i+1] = left[j][i] + int(nums[i] < nums[j])
+        right = [[0]*(len(nums)+1) for _ in xrange(len(nums))]
+        for j in xrange(len(nums)):
+            for i in reversed(xrange(j+1, len(nums))):
+                right[j][i] = right[j][i+1] + int(nums[i] > nums[j])
+        result = 0
+        for k in xrange(len(nums)):
+            for j in xrange(k):
+                if nums[k] < nums[j]:
+                    result += left[k][j]*right[j][k+1]
+        return result
