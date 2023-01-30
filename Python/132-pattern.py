3af9fe6d9a0781a1a98f495f08dda2faa703877d
kamyu <kamyu104@users.noreply.github.com>
2023-01-31 02:12:36 +0800

    Update 132-pattern.py

--- Python/132-pattern.py
+++ Python/132-pattern.py
@@ -1,7 +1,6 @@
 # Time:  O(n)
 # Space: O(n)
 
-
 class Solution(object):
     def find132pattern(self, nums):
         """
@@ -19,3 +18,22 @@ class Solution(object):
             st.append(nums[i])
         return False
 
+
+# Time:  O(n^2)
+# Space: O(1)
+class Solution_TLE(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        result = 0
+        for k in xrange(len(nums)):
+            valid = False
+            for j in xrange(k):
+                if nums[j] < nums[k]:
+                    valid = True
+                elif nums[j] > nums[k]:
+                    if valid:
+                        return True
+        return False
