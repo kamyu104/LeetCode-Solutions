b11c0e810b289e81e9302f752b4de23269fc748c
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 07:41:34 +0800

    Create house-robber-iv.py

---
+++ Python/house-robber-iv.py
@@ -0,0 +1,60 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# binary search
+class Solution(object):
+    def minCapability(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def check(x):
+            cnt = i = 0
+            while i < len(nums):
+                if nums[i] <= x:
+                    cnt += 1
+                    i += 2
+                else:
+                    i += 1
+            return cnt >= k
+
+        sorted_nums = sorted(set(nums))
+        left, right = 0, len(sorted_nums)-1
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(sorted_nums[mid]):
+                right = mid-1
+            else:
+                left = mid+1
+        return sorted_nums[left]
+
+
+# Time:  O(nlogr)
+# Space: O(1)
+# binary search
+class Solution2(object):
+    def minCapability(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def check(x):
+            cnt = i = 0
+            while i < len(nums):
+                if nums[i] <= x:
+                    cnt += 1
+                    i += 2
+                else:
+                    i += 1
+            return cnt >= k
+    
+        left, right = min(nums), max(nums)
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
