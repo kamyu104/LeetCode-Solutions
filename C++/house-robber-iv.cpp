420da7e9b22a5b657d5fed01a4e1c86d87668fad
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 07:41:03 +0800

    Create house-robber-iv.cpp

---
+++ C++/house-robber-iv.cpp
@@ -0,0 +1,63 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// binary search
+class Solution {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        const auto& check = [&](const auto& x) {
+            int cnt = 0;
+            for (int i = 0; i < size(nums); ++i) {
+                if (nums[i] <= x) {
+                    ++cnt;
+                    ++i;
+                }
+            }
+            return cnt >= k;
+        };
+        unordered_set<int> nums_set(cbegin(nums), cend(nums));
+        vector<int> sorted_nums(cbegin(nums_set), cend(nums_set));
+        sort(begin(sorted_nums), end(sorted_nums));
+        int left = 0, right = size(sorted_nums) - 1;
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (check(sorted_nums[mid])) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return sorted_nums[left];
+    }
+};
+
+// Time:  O(nlogr)
+// Space: O(1)
+// binary search
+class Solution2 {
+public:
+    int minCapability(vector<int>& nums, int k) {
+        const auto& check = [&](const auto& x) {
+            int cnt = 0;
+            for (int i = 0; i < size(nums); ++i) {
+                if (nums[i] <= x) {
+                    ++cnt;
+                    ++i;
+                }
+            }
+            return cnt >= k;
+        };
+
+        int left = *min_element(cbegin(nums), cend(nums));
+        int right = *max_element(cbegin(nums), cend(nums));
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
