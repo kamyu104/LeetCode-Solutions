883e8e1c88fd3a753c66f8687c5438e642c303a1
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 03:19:41 +0800

    Create separate-the-digits-in-an-array.py

---
+++ Python/separate-the-digits-in-an-array.py
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def separateDigits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        result = []
+        for x in reversed(nums):
+            while x:
+                result.append(x%10)
+                x //= 10
+        result.reverse()
+        return result
+
+
+# Time:  O(n)
+# Space: O(logr), r = max(nums)
+# array
+class Solution2(object):
+    def separateDigits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return [int(c) for x in nums for c in str(x)]
