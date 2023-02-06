b35a2bd932a710f4159f29c39f25d646564dd59f
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 03:19:08 +0800

    Create separate-the-digits-in-an-array.cpp

---
+++ C++/separate-the-digits-in-an-array.cpp
@@ -0,0 +1,33 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> result;
+        for (int i = size(nums) - 1; i >= 0; --i) {
+            for (int x = nums[i]; x; x /= 10) {
+                result.emplace_back(x % 10);
+            }
+        }
+        reverse(begin(result), end(result));
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+// array
+class Solution2 {
+public:
+    vector<int> separateDigits(vector<int>& nums) {
+        vector<int> result;
+        for (const auto& x : nums) {
+            for (const auto& c : to_string(x)) {
+                result.emplace_back(c - '0');
+            }
+        }
+        return result;
+    }
+};
