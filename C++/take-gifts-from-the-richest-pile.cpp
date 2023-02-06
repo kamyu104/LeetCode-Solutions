c6c72c7de86f662365ded817de2064818e495530
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 07:04:25 +0800

    Create take-gifts-from-the-richest-pile.cpp

---
+++ C++/take-gifts-from-the-richest-pile.cpp
@@ -0,0 +1,17 @@
+// Time:  O(klogn)
+// Space: O(1)
+
+// heap
+class Solution {
+public:
+    long long pickGifts(vector<int>& gifts, int k) {
+        make_heap(begin(gifts), end(gifts));
+        while (--k >= 0) {
+            const int x = gifts.front();
+            pop_heap(begin(gifts), end(gifts)); gifts.pop_back();
+            gifts.emplace_back(sqrt(x));
+            push_heap(begin(gifts), end(gifts));
+        }
+        return accumulate(cbegin(gifts), cend(gifts), 0ll);
+    }
+};
