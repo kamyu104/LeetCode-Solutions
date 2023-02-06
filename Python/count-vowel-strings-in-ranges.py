d192ef61dab315c5b8c10d3a092b96ad906eabe6
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 07:22:40 +0800

    Create count-vowel-strings-in-ranges.py

---
+++ Python/count-vowel-strings-in-ranges.py
@@ -0,0 +1,19 @@
+# Time:  O(n + q)
+# Space: O(n)
+
+# prefix sum
+class Solution(object):
+    def vowelStrings(self, words, queries):
+        """
+        :type words: List[str]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        VOWELS = {'a', 'e', 'i', 'o', 'u'}
+        prefix = [0]*(len(words)+1)
+        for i, w in enumerate(words):
+            prefix[i+1] = prefix[i]+int(w[0] in VOWELS and w[-1] in VOWELS)
+        result = []
+        for l, r in queries:
+            result.append(prefix[r+1]-prefix[l])
+        return result
