f01b2e6d4241079afa4c8a50980e552b47b4f241
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 10:09:37 +0800

    Update maximum-number-of-integers-to-choose-from-a-range-i.cpp

--- C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, long long maxSum) {
-        int k = min(static_cast<int>(((-1 + sqrt(1 + 8 * maxSum)) / 2)), n);
+        int k = min(static_cast<int>(((-1 + sqrt(1 + 8 * maxSum)) / 2)), n); // k = argmax((k+1)*k//2 <= maxSum)
         int64_t total = static_cast<int64_t>(k + 1) * k / 2;
         int result = k;
         unordered_set<int> lookup(cbegin(banned), cend(banned));
