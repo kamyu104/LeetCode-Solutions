d0545853384f9b9df0ddc6b02b6f75743e834a68
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 09:50:12 +0800

    Update maximum-number-of-integers-to-choose-from-a-range-i.cpp

--- C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -28,10 +28,40 @@ public:
     }
 };
 
+// Time:  O(min(sqrt(m), n) + b)
+// Space: O(b)
+// math
+class Solution2 {
+public:
+    int maxCount(vector<int>& banned, int n, long long maxSum) {
+        int k = min(static_cast<int>(((-1 + sqrt(1 + 8 * maxSum)) / 2)), n);
+        int64_t total = static_cast<int64_t>(k + 1) * k / 2;
+        int result = k;
+        unordered_set<int> lookup(cbegin(banned), cend(banned));
+        for (const auto& x : lookup) {
+            if (x <= k) {
+                total -= x;
+                --result;
+            }
+        }
+        for (int i = k + 1; i <= n; ++i) {
+            if (lookup.count(i)) {
+                continue;
+            }
+            if (total + i > maxSum) {
+                break;
+            }
+            total += i;
+            ++result;
+        }
+        return result;
+    }
+};
+
 // Time:  O(n)
 // Space: O(b)
 // greedy
-class Solution2 {
+class Solution3 {
 public:
     int maxCount(vector<int>& banned, int n, int maxSum) {
         unordered_set<int> lookup(cbegin(banned), cend(banned));
