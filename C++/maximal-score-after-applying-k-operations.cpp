727a91f70e9d28569a3536ccb4adbef6ee18467f
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 09:02:10 +0800

    Update maximal-score-after-applying-k-operations.cpp

--- C++/maximal-score-after-applying-k-operations.cpp
+++ C++/maximal-score-after-applying-k-operations.cpp
@@ -11,6 +11,33 @@ public:
 
         int64_t result = 0;
         make_heap(begin(nums), end(nums));
+        while (k-- && !empty(nums)) {
+            int x = nums.front();
+            result += x;
+            pop_heap(begin(nums), end(nums)); nums.pop_back();
+            const auto nx = ceil_divide(x, 3);
+            if (!nx) {
+                continue;
+            }
+            nums.emplace_back(nx);
+            push_heap(begin(nums), end(nums));
+        }
+        return result;
+    }
+};
+
+// Time:  O(n + klogn)
+// Space: O(1)
+// heap
+class Solution2 {
+public:
+    long long maxKelements(vector<int>& nums, int k) {
+        const auto& ceil_divide = [](const auto& a, const auto& b) {
+            return (a + b - 1) / b;
+        };
+
+        int64_t result = 0;
+        make_heap(begin(nums), end(nums));
         while (--k >= 0) {
             int x = nums.front();
             result += x;
@@ -21,3 +48,4 @@ public:
         return result;
     }
 };
+
