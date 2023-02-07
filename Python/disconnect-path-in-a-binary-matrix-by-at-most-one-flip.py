38e884c8d489167f1736a2e36274a69ba3b1b761
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 10:32:42 +0800

    Update disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py

--- Python/disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
+++ Python/disconnect-path-in-a-binary-matrix-by-at-most-one-flip.py
@@ -29,8 +29,35 @@ class Solution(object):
 
 # Time:  O(m * n)
 # Space: O(m + n)
-# dfs
+
+# iterative dfs
 class Solution2(object):
+    def isPossibleToCutPath(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        def iter_dfs():
+            stk = [(0, 0)]
+            while stk:
+                i, j = stk.pop()
+                if not (0 <= i < len(grid) and 0 <= j < len(grid[0]) and grid[i][j]):
+                    continue
+                if (i, j) == (len(grid)-1, len(grid[0])-1):
+                    return True
+                if (i, j) != (0, 0):
+                    grid[i][j] = 0
+                stk.append((i, j+1))
+                stk.append((i+1, j))  
+            return False
+
+        return not iter_dfs() or not iter_dfs()
+
+
+# Time:  O(m * n)
+# Space: O(m + n)
+# dfs
+class Solution3(object):
     def isPossibleToCutPath(self, grid):
         """
         :type grid: List[List[int]]
