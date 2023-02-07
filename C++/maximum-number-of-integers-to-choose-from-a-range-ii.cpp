27c0220b4c8f40627ee2bbe16115ae818ee2fbad
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 08:35:54 +0800

    Update maximum-number-of-integers-to-choose-from-a-range-ii.cpp

--- C++/maximum-number-of-integers-to-choose-from-a-range-ii.cpp
+++ C++/maximum-number-of-integers-to-choose-from-a-range-ii.cpp
@@ -5,13 +5,15 @@
 class Solution {
 public:
     int maxCount(vector<int>& banned, int n, long long maxSum) {
-        sort(begin(banned), end(banned));
+        unordered_set<int> banned_set(cbegin(banned), cend(banned));
+        vector<int> sorted_banned(cbegin(banned_set), cend(banned_set));
+        sort(begin(sorted_banned), end(sorted_banned));
         vector<int64_t> prefix(size(banned) + 1);
         for (int i = 0; i < size(banned); ++i) {
-            prefix[i + 1] = prefix[i] + banned[i];
+            prefix[i + 1] = prefix[i] + sorted_banned[i];
         }
         const auto& check = [&](const int64_t x) {
-            return (x + 1) * x / 2 - prefix[distance(cbegin(banned), upper_bound(cbegin(banned), cend(banned), x))] <= maxSum;
+            return (x + 1) * x / 2 - prefix[distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), x))] <= maxSum;
         };
         int left = 1, right = n;
         while (left <= right) {
@@ -22,6 +24,6 @@ public:
                 left = mid + 1;
             }
         } 
-        return right - distance(cbegin(banned), upper_bound(cbegin(banned), cend(banned), right));
+        return right - distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), right));
     }
 };
