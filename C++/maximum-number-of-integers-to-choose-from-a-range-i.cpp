9f5f7cdd18fd31e0a074f3fa87be01ada6506603
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 09:52:48 +0800

    Update maximum-number-of-integers-to-choose-from-a-range-i.cpp

--- C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
+++ C++/maximum-number-of-integers-to-choose-from-a-range-i.cpp
@@ -1,37 +1,8 @@
-// Time:  O(blogb + logn * logb)
+// Time:  O(b)
 // Space: O(b)
 
-// binary search, prefix sum
-class Solution {
-public:
-    int maxCount(vector<int>& banned, int n, long long maxSum) {
-        unordered_set<int> banned_set(cbegin(banned), cend(banned));
-        vector<int> sorted_banned(cbegin(banned_set), cend(banned_set));
-        sort(begin(sorted_banned), end(sorted_banned));
-        vector<int64_t> prefix(size(sorted_banned) + 1);
-        for (int i = 0; i < size(sorted_banned); ++i) {
-            prefix[i + 1] = prefix[i] + sorted_banned[i];
-        }
-        const auto& check = [&](const int64_t x) {
-            return (x + 1) * x / 2 - prefix[distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), x))] <= maxSum;
-        };
-        int left = 1, right = n;
-        while (left <= right) {
-            const auto mid = left + (right - left) / 2;
-            if (!check(mid)) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        } 
-        return right - distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), right));
-    }
-};
-
-// Time:  O(min(sqrt(m), n) + b)
-// Space: O(b)
 // math
-class Solution2 {
+class Solution {
 public:
     int maxCount(vector<int>& banned, int n, long long maxSum) {
         int k = min(static_cast<int>(((-1 + sqrt(1 + 8 * maxSum)) / 2)), n);
@@ -55,6 +26,35 @@ public:
             ++result;
         }
         return result;
+    }
+};
+
+// Time:  O(blogb + logn * logb)
+// Space: O(b)
+// binary search, prefix sum
+class Solution2 {
+public:
+    int maxCount(vector<int>& banned, int n, long long maxSum) {
+        unordered_set<int> banned_set(cbegin(banned), cend(banned));
+        vector<int> sorted_banned(cbegin(banned_set), cend(banned_set));
+        sort(begin(sorted_banned), end(sorted_banned));
+        vector<int64_t> prefix(size(sorted_banned) + 1);
+        for (int i = 0; i < size(sorted_banned); ++i) {
+            prefix[i + 1] = prefix[i] + sorted_banned[i];
+        }
+        const auto& check = [&](const int64_t x) {
+            return (x + 1) * x / 2 - prefix[distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), x))] <= maxSum;
+        };
+        int left = 1, right = n;
+        while (left <= right) {
+            const auto mid = left + (right - left) / 2;
+            if (!check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } 
+        return right - distance(cbegin(sorted_banned), upper_bound(cbegin(sorted_banned), cend(sorted_banned), right));
     }
 };
 
