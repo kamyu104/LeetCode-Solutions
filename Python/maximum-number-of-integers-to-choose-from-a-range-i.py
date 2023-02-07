7698aa438ece49ddc64fd2920951f481bae0d1e4
kamyu <kamyu104@users.noreply.github.com>
2023-02-07 08:34:11 +0800

    Update maximum-number-of-integers-to-choose-from-a-range-i.py

--- Python/maximum-number-of-integers-to-choose-from-a-range-i.py
+++ Python/maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -28,3 +28,26 @@ class Solution(object):
             else:
                 left = mid+1
         return right-bisect.bisect_right(sorted_banned, right)
+
+ 
+# Time:  O(n)
+# Space: O(b)
+# greedy
+class Solution2(object):
+    def maxCount(self, banned, n, maxSum):
+        """
+        :type banned: List[int]
+        :type n: int
+        :type maxSum: int
+        :rtype: int
+        """
+        lookup = set(banned)
+        result = total = 0
+        for i in xrange(1, n+1):
+            if i in lookup:
+                continue
+            if total+i > maxSum:
+                break
+            total += i
+            result += 1
+        return result
