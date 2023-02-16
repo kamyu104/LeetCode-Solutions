31842ecb7ed7fd4bb4d919f1d9537512487073ad
kamyu <kamyu104@users.noreply.github.com>
2023-02-17 01:23:29 +0800

    Create substring-xor-queries.py

---
+++ Python/substring-xor-queries.py
@@ -0,0 +1,24 @@
+# Time:  O(n * logr + q), r = max(a^b for a, b in queries)
+# Space: O(n * logr)
+
+# hash table
+class Solution(object):
+    def substringXorQueries(self, s, queries):
+        """
+        :type s: str
+        :type queries: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        mx = max(a^b for a, b in queries)
+        lookup = {}
+        for i in xrange(len(s)):
+            curr = 0
+            for j in xrange(i, len(s)):
+                curr = (curr<<1)+int(s[j])
+                if curr > mx:
+                    break
+                if curr not in lookup:
+                    lookup[curr] = [i, j]
+                if s[i] == '0':
+                    break
+        return [lookup[a^b] if a^b in lookup else [-1, -1] for a, b in queries]
