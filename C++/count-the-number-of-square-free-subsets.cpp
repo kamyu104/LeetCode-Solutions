ea2bb18658bd592179720df3d209eee767861990
kamyu <kamyu104@users.noreply.github.com>
2023-02-21 19:47:56 +0800

    Update count-the-number-of-square-free-subsets.cpp

--- C++/count-the-number-of-square-free-subsets.cpp
+++ C++/count-the-number-of-square-free-subsets.cpp
@@ -1,50 +1,126 @@
 // Time:  O(n + m * 2^p)
 // Space: O(m * 2^p)
 
-static const int MAX_NUM = 30;
-vector<int> linear_sieve_of_eratosthenes(int n) {  // Time: O(n), Space: O(n)
-    vector<int> spf(n + 1, -1);
-    vector<int> primes;
-    for (int i = 2; i <= n; ++i) {
-        if (spf[i] == -1) {
-            spf[i] = i;
-            primes.emplace_back(i);
-        }
-        for (const auto& p : primes) {
-            if (i * p > n || p > spf[i]) {
-                break;
-            }
-            spf[i * p] = p;
-        }
-    }
-    return primes;
-}
-
-const auto& PRIMES = linear_sieve_of_eratosthenes(MAX_NUM);
-vector<int> init_masks() {
-    vector<int> masks(MAX_NUM + 1);
-    for (int x = 0; x <= MAX_NUM; ++x) {
-        for (int i = 0, y = x; i < size(PRIMES); ++i) {
-            if (y % PRIMES[i]) {
-                continue;
-            }
-            if (y % (PRIMES[i] * PRIMES[i]) == 0) {
-                masks[x] = 0;
-                break;
-            }
-            masks[x] |= (1 << i);
-            y /= PRIMES[i];
-        }
-    }
-    return masks;
-}
-const auto& MASKS = init_masks();
-
-// number theory, combinatorics, backtracking, memoization, bitmasks
+// number theory, combinatorics, bitmasks, dp
 class Solution {
 public:
     int squareFreeSubsets(vector<int>& nums) {
+        const int MAX_NUM = *max_element(cbegin(nums), cend(nums));
+        const auto& linear_sieve_of_eratosthenes = [](int n) {  // Time: O(n), Space: O(n)
+            vector<int> spf(n + 1, -1);
+            vector<int> primes;
+            for (int i = 2; i <= n; ++i) {
+                if (spf[i] == -1) {
+                    spf[i] = i;
+                    primes.emplace_back(i);
+                }
+                for (const auto& p : primes) {
+                    if (i * p > n || p > spf[i]) {
+                        break;
+                    }
+                    spf[i * p] = p;
+                }
+            }
+            return primes;
+        };
+
+        const auto& PRIMES = linear_sieve_of_eratosthenes(MAX_NUM);
+        vector<int> MASKS(MAX_NUM + 1);
+        for (int x = 0; x <= MAX_NUM; ++x) {
+            for (int i = 0, y = x; i < size(PRIMES); ++i) {
+                if (y % PRIMES[i]) {
+                    continue;
+                }
+                if (y % (PRIMES[i] * PRIMES[i]) == 0) {
+                    MASKS[x] = 0;
+                    break;
+                }
+                MASKS[x] |= (1 << i);
+                y /= PRIMES[i];
+            }
+        }
         static const int MOD = 1e9 + 7;
+
+        const auto& powmod = [&](int a, int b) {
+            a %= MOD;
+            int64_t result = 1;
+            while (b) {
+                if (b & 1) {
+                    result = result * a % MOD;
+                }
+                a = int64_t(a) * a % MOD;
+                b >>= 1;
+            }
+            return result;
+        };
+
+        unordered_map<int, int> cnt;
+        for (const auto& x : nums) {
+            ++cnt[x];
+        }
+        vector<int> arr;
+        for (const auto& [k, _] : cnt) {
+            if (k != 1) {
+                arr.emplace_back(k);
+            }
+        }
+        vector<int> dp(1 << size(PRIMES), 1);
+        for (const auto& x : arr) {
+            if (!MASKS[x]) {
+                continue;
+            }
+            for (int mask = 0; mask < size(dp); ++mask) {
+                if ((MASKS[x] & mask) == 0) {
+                    dp[mask] = (dp[mask] + static_cast<int64_t>(cnt[x]) * dp[mask | MASKS[x]]) % MOD;
+                }
+            }
+        }
+        return cnt.count(1) ? ((dp[0] * powmod(2, cnt[1]) - 1) + MOD) % MOD: ((dp[0] - 1) + MOD) % MOD;
+    }
+};
+
+// Time:  O(n + m * 2^p)
+// Space: O(m * 2^p)
+// number theory, combinatorics, backtracking, memoization, bitmasks
+class Solution2 {
+public:
+    int squareFreeSubsets(vector<int>& nums) {
+        const int MAX_NUM = *max_element(cbegin(nums), cend(nums));
+        const auto& linear_sieve_of_eratosthenes = [](int n) {  // Time: O(n), Space: O(n)
+            vector<int> spf(n + 1, -1);
+            vector<int> primes;
+            for (int i = 2; i <= n; ++i) {
+                if (spf[i] == -1) {
+                    spf[i] = i;
+                    primes.emplace_back(i);
+                }
+                for (const auto& p : primes) {
+                    if (i * p > n || p > spf[i]) {
+                        break;
+                    }
+                    spf[i * p] = p;
+                }
+            }
+            return primes;
+        };
+
+        const auto& PRIMES = linear_sieve_of_eratosthenes(MAX_NUM);
+        vector<int> MASKS(MAX_NUM + 1);
+        for (int x = 0; x <= MAX_NUM; ++x) {
+            for (int i = 0, y = x; i < size(PRIMES); ++i) {
+                if (y % PRIMES[i]) {
+                    continue;
+                }
+                if (y % (PRIMES[i] * PRIMES[i]) == 0) {
+                    MASKS[x] = 0;
+                    break;
+                }
+                MASKS[x] |= (1 << i);
+                y /= PRIMES[i];
+            }
+        }
+        static const int MOD = 1e9 + 7;
+
         const auto& powmod = [&](int a, int b) {
             a %= MOD;
             int64_t result = 1;
