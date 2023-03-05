e4dd0c31dc707a2a5b97954c3b38477c90d08137
kamyu <kamyu104@users.noreply.github.com>
2023-03-06 00:20:28 +0800

    Create split-with-minimum-sum.py

---
+++ Python/split-with-minimum-sum.py
@@ -0,0 +1,12 @@
+# Time:  O(mlogm), m = logn
+# Space: O(m)
+
+# sort, greedy
+class Solution(object):
+    def splitNum(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        sorted_num = "".join(sorted(str(num)))
+        return int(sorted_num[::2])+int(sorted_num[1::2])
