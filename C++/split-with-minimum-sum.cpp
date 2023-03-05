06747a048dba0c3ba40a6e57a0dafd5b7d83f777
kamyu <kamyu104@users.noreply.github.com>
2023-03-06 00:19:20 +0800

    Create split-with-minimum-sum.cpp

---
+++ C++/split-with-minimum-sum.cpp
@@ -0,0 +1,22 @@
+// Time:  O(mlogm), m = logn
+// Space: O(m)
+
+// sort, greedy
+class Solution {
+public:
+    int splitNum(int num) {
+        vector<int> sorted_num;
+        for (; num; num /= 10)  {
+            sorted_num.emplace_back(num % 10);
+        }
+        sort(begin(sorted_num), end(sorted_num));
+        int result = 0;
+        for (int i = 0; i < size(sorted_num); ++i) {
+            if (i % 2 == size(sorted_num) % 2) {
+                result *= 10;
+            }
+            result += sorted_num[i];
+        }
+        return result;
+    }
+};
