821013a0404948be8b91c28760bcd32911387b79
kamyu <kamyu104@users.noreply.github.com>
2023-03-06 20:51:12 +0800

    Update count-number-of-possible-root-nodes.py

--- Python/count-number-of-possible-root-nodes.py
+++ Python/count-number-of-possible-root-nodes.py
@@ -4,8 +4,60 @@
 import collections
 
 
-# dfs
+# iterative dfs
 class Solution(object):
+    def rootCount(self, edges, guesses, k):
+        """
+        :type edges: List[List[int]]
+        :type guesses: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def iter_dfs():
+            result = 0
+            stk = [(0, -1)]
+            while stk:
+                u, p = stk.pop()
+                result += int((p, u) in lookup)
+                for v in adj[u]:
+                    if v == p:
+                        continue
+                    stk.append((v, u))
+            return result
+        
+        def iter_dfs2(curr):
+            result = 0
+            stk = [(0, -1, curr)]
+            while stk:
+                u, p, curr = stk.pop()
+                if (p, u) in lookup:
+                    curr -= 1
+                if (u, p) in lookup:
+                    curr += 1
+                result += int(curr >= k)
+                for v in adj[u]:
+                    if v == p:
+                        continue
+                    stk.append((v, u, curr))
+            return result
+
+        adj = collections.defaultdict(list)
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        lookup = {(u, v) for u, v in guesses}
+        curr = iter_dfs()
+        return iter_dfs2(curr)
+
+
+# Time:  O(n) 
+# Space: O(n)
+
+import collections
+
+
+# dfs
+class Solution2(object):
     def rootCount(self, edges, guesses, k):
         """
         :type edges: List[List[int]]
@@ -48,7 +100,7 @@ import collections
 
 
 # memoization
-class Solution2(object):
+class Solution3(object):
     def rootCount(self, edges, guesses, k):
         """
         :type edges: List[List[int]]
