5f5812bf1cdc9857d66e36dd2bc0b67840b43be2
kamyu <kamyu104@users.noreply.github.com>
2023-03-20 02:52:18 +0800

    Create maximize-greatness-of-an-array.py

---
+++ Python/maximize-greatness-of-an-array.py
@@ -0,0 +1,28 @@
+# Time:  O(n)
+# Space: O(n)
+
+# freq table, contructive algorithms
+class Solution(object):
+    def maximizeGreatness(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return len(nums)-max(collections.Counter(nums).itervalues())
+  
+
+# Time:  O(nlogn)
+# Space: O(1)
+# sort, greedy, two pointers
+class Solution(object):
+    def maximizeGreatness(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        left = 0
+        for right in xrange(len(nums)):
+          if nums[right] > nums[left]:
+            left += 1
+        return left
