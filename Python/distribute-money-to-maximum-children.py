3e112985ed44f80b561c1f2506fa284d808c525b
kamyu <kamyu104@users.noreply.github.com>
2023-03-19 21:48:29 +0800

    Update distribute-money-to-maximum-children.py

--- Python/distribute-money-to-maximum-children.py
+++ Python/distribute-money-to-maximum-children.py
@@ -13,6 +13,23 @@ class Solution(object):
             return -1
         money -= children*1
         q, r = divmod(money, 7)
+        return min(q, children) - int(q > children or (q == children and r != 0) or (q == children-1 and r == 3))
+
+    
+# Time:  O(1)
+# Space: O(1)
+# greedy
+class Solution2(object):
+    def distMoney(self, money, children):
+        """
+        :type money: int
+        :type children: int
+        :rtype: int
+        """
+        if money < children*1:
+            return -1
+        money -= children*1
+        q, r = divmod(money, 7)
         if q > children:
             return children-1
         if q == children:
