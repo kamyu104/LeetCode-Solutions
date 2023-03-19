d65d9e0a64fce1790af2db6046825267edbeb205
kamyu <kamyu104@users.noreply.github.com>
2023-03-20 04:40:23 +0800

    Create minimum-time-to-repair-cars.py

---
+++ Python/minimum-time-to-repair-cars.py
@@ -0,0 +1,51 @@
+# Time:  O(mx * log(mn * c^2)) = O(mx * max(logc, logmn)) = O(mx * logc), r = len(ranks), c = cars, mx = max(ranks), mn = min(ranks)
+# Space: O(1)
+
+import collections
+
+
+# binary search
+class Solution(object):
+    def repairCars(self, ranks, cars):
+        """
+        :type ranks: List[int]
+        :type cars: int
+        :rtype: int
+        """
+        def check(x):
+            return sum(int((x//k)**0.5)*v for k, v in cnt.iteritems()) >= cars
+
+        cnt = collections.Counter(ranks)
+        left, right = 1, min(cnt.iterkeys())*cars**2
+        while left <= right:
+            mid = left+(right-left)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
+
+
+# Time:  O(c * logmx), c = cars, mx = max(ranks)
+# Space: O(mx)
+import collections
+import heapq
+
+# heap, simulation
+class Solution(object):
+    def repairCars(self, ranks, cars):
+        """
+        :type ranks: List[int]
+        :type cars: int
+        :rtype: int
+        """
+        cnt = collections.Counter(ranks)
+        min_heap = [(r*1**2, 1) for r in cnt.iterkeys()]
+        heapq.heapify(min_heap)
+        while cars > 0:
+            t, k = heapq.heappop(min_heap)
+            r = t//k**2
+            cars -= cnt[r]
+            k += 1
+            heapq.heappush(min_heap, (r*k**2, k))
+        return t
