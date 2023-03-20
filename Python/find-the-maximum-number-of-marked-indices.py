fcc280b333c0c30655edbfad4a6971ed9e2f92b2
kamyu <kamyu104@users.noreply.github.com>
2023-03-20 19:49:13 +0800

    Update find-the-maximum-number-of-marked-indices.py

--- Python/find-the-maximum-number-of-marked-indices.py
+++ Python/find-the-maximum-number-of-marked-indices.py
@@ -9,9 +9,25 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()
-        result = 0
         right = len(nums)-1
         for left in reversed(xrange(len(nums)//2)):
             if 2*nums[left] <= nums[right]:
                 right -= 1
         return ((len(nums)-1)-right)*2
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+# sort, greedy, two pointers
+class Solution2(object):
+    def maxNumOfMarkedIndices(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        right = len(nums)-1
+        for left in reversed(xrange(len(nums))):
+            if 2*nums[left] <= nums[right]:
+                right -= 1
+        return min(((len(nums)-1)-right), len(nums)//2)*2
