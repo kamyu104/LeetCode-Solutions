3b7b0e196f048f326bb7d20082e84092e75cf7d7
kamyu <kamyu104@users.noreply.github.com>
2023-03-20 08:22:12 +0800

    Update check-knight-tour-configuration.py

--- Python/check-knight-tour-configuration.py
+++ Python/check-knight-tour-configuration.py
@@ -15,3 +15,16 @@ class Solution(object):
             for j in xrange(len(grid[0])):
                 lookup[grid[i][j]] = (i, j)
         return all(sorted([abs(lookup[i+1][0]-lookup[i][0]), abs(lookup[i+1][1]-lookup[i][1])]) == [1, 2] for i in xrange(len(lookup)-1))
+
+    
+# Time:  O(m * n)
+# Space: O(m * n)
+# hash table, simulation
+class Solution2(object):
+    def checkValidGrid(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: bool
+        """
+        lookup = {grid[i][j]:(i, j) for i in xrange(len(grid)) for j in xrange(len(grid[0]))}
+        return grid[0][0] == 0 and all(sorted([abs(lookup[i+1][0]-lookup[i][0]), abs(lookup[i+1][1]-lookup[i][1])]) == [1, 2] for i in xrange(len(lookup)-1))
