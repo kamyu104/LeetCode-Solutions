bb091482de5724c9d227f60014724c26ce2e8361
kamyu <kamyu104@users.noreply.github.com>
2023-03-20 19:50:31 +0800

    Update find-the-maximum-number-of-marked-indices.cpp

--- C++/find-the-maximum-number-of-marked-indices.cpp
+++ C++/find-the-maximum-number-of-marked-indices.cpp
@@ -15,3 +15,20 @@ public:
         return ((size(nums) - 1) - right) * 2;
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(1)
+// sort, greedy, two pointers
+class Solution2 {
+public:
+    int maxNumOfMarkedIndices(vector<int>& nums) {
+        sort(begin(nums), end(nums));
+        int right = size(nums) - 1;
+        for (int left = size(nums) - 1; left >= 0; --left) {
+            if (2 * nums[left] <= nums[right]) {
+                --right;
+            }
+        }
+        return min(((size(nums) - 1) - right), size(nums) / 2) * 2;
+    }
+};
