8c57ab8de1a8eff74d12ff98a01bcfb5a4558ee4
kamyu <kamyu104@users.noreply.github.com>
2023-03-26 22:59:46 +0800

    Create make-the-prefix-sum-non-negative.py

---
+++ Python/make-the-prefix-sum-non-negative.py
@@ -0,0 +1,22 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+import heapq
+
+
+# greedy, heap
+class Solution(object):
+    def makePrefSumNonNegative(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = prefix = 0
+        min_heap = []
+        for x in nums:
+            heapq.heappush(min_heap, x)
+            prefix += x
+            if prefix < 0:
+                prefix -= heapq.heappop(min_heap)
+                result += 1
+        return result
