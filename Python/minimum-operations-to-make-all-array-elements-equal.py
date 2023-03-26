76269dd751482faec896016c4ee4de87ecc23d2a
kamyu <kamyu104@users.noreply.github.com>
2023-03-27 00:36:50 +0800

    Create minimum-operations-to-make-all-array-elements-equal.py

---
+++ Python/minimum-operations-to-make-all-array-elements-equal.py
@@ -0,0 +1,20 @@
+# Time:  O(nlogn + qlogn)
+# Space: O(n)
+
+# binary search, prefix sum
+class Solution(object):
+    def minOperations(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[int]
+        :rtype: List[int]
+        """
+        nums.sort()
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]+nums[i]
+        result = [0]*len(queries)
+        for i, q in enumerate(queries):
+            j = bisect.bisect_left(nums, q)
+            result[i] = (q*j-prefix[j])+((prefix[-1]-prefix[j])-q*(len(nums)-j))
+        return result
