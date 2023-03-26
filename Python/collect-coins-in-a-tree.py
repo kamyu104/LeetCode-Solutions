c921f4563cff1274410babb95e127ed368be22ba
kamyu <kamyu104@users.noreply.github.com>
2023-03-27 02:06:45 +0800

    Create collect-coins-in-a-tree.py

---
+++ Python/collect-coins-in-a-tree.py
@@ -0,0 +1,34 @@
+# Time:  O(n)
+# Space: O(n)
+
+# graph
+class Solution(object):
+    def collectTheCoins(self, coins, edges):
+        """
+        :type coins: List[int]
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        DISTANCE = 2
+
+        adj = [set() for _ in xrange(len(coins))]
+        for u, v in edges:
+            adj[u].add(v)
+            adj[v].add(u)
+        n = len(coins)
+        for u in xrange(len(coins)):
+            while len(adj[u]) == 1 and not coins[u]:
+                v = adj[u].pop()
+                adj[v].remove(u)
+                n -= 1
+                u = v
+        for _ in xrange(DISTANCE):
+            q = [u for u in xrange(len(coins)) if len(adj[u]) == 1]
+            for u in q:
+                if not adj[u]:
+                    assert(n == 1)
+                    break
+                v = adj[u].pop()
+                adj[v].remove(u)
+                n -= 1
+        return (n-1)*2
