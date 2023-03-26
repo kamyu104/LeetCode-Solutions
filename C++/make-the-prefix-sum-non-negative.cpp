6b4caef74fc08446e9dd275c2705d97277da6d4d
kamyu <kamyu104@users.noreply.github.com>
2023-03-26 22:59:09 +0800

    Create make-the-prefix-sum-non-negative.cpp

---
+++ C++/make-the-prefix-sum-non-negative.cpp
@@ -0,0 +1,21 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// greedy, heap
+class Solution {
+public:
+    int makePrefSumNonNegative(vector<int>& nums) {
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        int result = 0;
+        int64_t prefix = 0;
+        for (const auto& x: nums) {
+            min_heap.emplace(x);
+            prefix += x;
+            if (prefix < 0) {
+                prefix -= min_heap.top(); min_heap.pop();
+                ++result;
+            }
+        }
+        return result;
+    }
+};
