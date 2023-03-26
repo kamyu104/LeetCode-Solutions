ee701f9e40dbf55e76d21d671b5e983f84649ecc
kamyu <kamyu104@users.noreply.github.com>
2023-03-27 02:05:54 +0800

    Create collect-coins-in-a-tree.cpp

---
+++ C++/collect-coins-in-a-tree.cpp
@@ -0,0 +1,46 @@
+// Time:  O(n)
+// Space: O(n)
+
+// graph
+class Solution {
+public:
+    int collectTheCoins(vector<int>& coins, vector<vector<int>>& edges) {
+        static const int DISTANCE = 2;
+
+        vector<unordered_set<int>> adj(size(coins));
+        for (const auto& e : edges) {
+            adj[e[0]].emplace(e[1]);
+            adj[e[1]].emplace(e[0]);
+        }
+        int n = size(coins);
+        for (int i = 0; i < size(coins); ++i) {
+            int u = i;
+            while (size(adj[u]) == 1 && !coins[u]) {
+                const int v = *cbegin(adj[u]);
+                adj[u].erase(v);
+                adj[v].erase(u);
+                --n;
+                u = v;
+            }
+        }
+        for (int _ = 0; _ < DISTANCE; ++_) {
+            vector<int> q;
+            for (int u = 0; u < size(coins); ++u) {
+                if (size(adj[u]) == 1) {
+                    q.emplace_back(u);
+                }
+            }
+            for (const auto& u : q) {
+                if (empty(adj[u])) {
+                    assert(n == 1);
+                    break;
+                }
+                const int v = *cbegin(adj[u]);
+                adj[u].erase(v);
+                adj[v].erase(u);
+                --n;
+            }
+        }
+        return (n - 1) * 2;
+    }
+};
