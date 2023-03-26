565b072f10ae79d5c418af73f74ecce72cefd520
kamyu <kamyu104@users.noreply.github.com>
2023-03-27 00:37:27 +0800

    Update minimum-operations-to-make-all-array-elements-equal.cpp

--- C++/minimum-operations-to-make-all-array-elements-equal.cpp
+++ C++/minimum-operations-to-make-all-array-elements-equal.cpp
@@ -12,7 +12,7 @@ public:
         }
         vector<long long> result(size(queries));
         for (int i = 0; i < size(queries); ++i) {
-            long long q = queries[i];
+            const long long q = queries[i];
             const int j = distance(cbegin(nums), lower_bound(cbegin(nums), cend(nums), q));
             result[i] = (q * j - prefix[j]) + ((prefix.back() - prefix[j]) - q * (size(nums) - j));
         }
