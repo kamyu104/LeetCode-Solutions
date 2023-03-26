0f9f95e15d743616fee6f07472e97aa9fdccbd71
kamyu <kamyu104@users.noreply.github.com>
2023-03-26 23:13:13 +0800

    Create k-items-with-the-maximum-sum.py

---
+++ Python/k-items-with-the-maximum-sum.py
@@ -0,0 +1,14 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def kItemsWithMaximumSum(self, numOnes, numZeros, numNegOnes, k):
+        """
+        :type numOnes: int
+        :type numZeros: int
+        :type numNegOnes: int
+        :type k: int
+        :rtype: int
+        """
+        return min(numOnes, k)-max(k-numOnes-numZeros, 0)
