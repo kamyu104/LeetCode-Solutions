0a81bbf301773f03f9431ebb227008a078008a93
kamyu <kamyu104@users.noreply.github.com>
2023-04-03 11:48:18 +0800

    Update minimum-reverse-operations.py

--- Python/minimum-reverse-operations.py
+++ Python/minimum-reverse-operations.py
@@ -1,6 +1,37 @@
 # Time:  O(n * alpha(n)) = O(n)
 # Space: O(n)
 
+class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
+    def __init__(self, n):
+        self.set = range(n)
+        self.rank = [0]*n
+        self.max = range(n)  # added
+
+    def find_set(self, x):
+        stk = []
+        while self.set[x] != x:  # path compression
+            stk.append(x)
+            x = self.set[x]
+        while stk:
+            self.set[stk.pop()] = x
+        return x
+
+    def union_set(self, x, y):
+        x, y = self.find_set(x), self.find_set(y)
+        if x == y:
+            return False
+        if self.rank[x] > self.rank[y]:  # union by rank
+            x, y = y, x
+        self.set[x] = self.set[y]
+        if self.rank[x] == self.rank[y]:
+            self.rank[y] += 1
+        self.max[y] = max(self.max[x], self.max[y])  # added
+        return True
+
+    def max_set(self, x):  # added
+        return self.max[self.find_set(x)]
+
+
 # bfs, union find
 class Solution(object):
     def minReverseOperations(self, n, p, banned, k):
@@ -11,36 +42,6 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
-            def __init__(self, n):
-                self.set = range(n)
-                self.rank = [0]*n
-                self.max = range(n)  # added
-
-            def find_set(self, x):
-                stk = []
-                while self.set[x] != x:  # path compression
-                    stk.append(x)
-                    x = self.set[x]
-                while stk:
-                    self.set[stk.pop()] = x
-                return x
-
-            def union_set(self, x, y):
-                x, y = self.find_set(x), self.find_set(y)
-                if x == y:
-                    return False
-                if self.rank[x] > self.rank[y]:  # union by rank
-                    x, y = y, x
-                self.set[x] = self.set[y]
-                if self.rank[x] == self.rank[y]:
-                    self.rank[y] += 1
-                self.max[y] = max(self.max[x], self.max[y])  # added
-                return True
-    
-            def max_set(self, x):  # added
-                return self.max[self.find_set(x)]
-
         lookup = [False]*n
         for i in banned:
             lookup[i] = True
