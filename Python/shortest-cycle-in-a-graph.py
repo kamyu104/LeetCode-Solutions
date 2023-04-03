191693973dd17404d386786aa60b25a376eee9ab
kamyu <kamyu104@users.noreply.github.com>
2023-04-03 21:23:03 +0800

    Update shortest-cycle-in-a-graph.py

--- Python/shortest-cycle-in-a-graph.py
+++ Python/shortest-cycle-in-a-graph.py
@@ -1,8 +1,83 @@
-# Time:  O(n^2)
+# Time:  O(n + e)
 # Space: O(n + e)
+
+import collections
+
 
 # bfs
 class Solution(object):
+    def findShortestCycle(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        INF = float("inf")
+        def iter_dfs():
+            result = set(xrange(n))
+            stk = [node for node in xrange(n) if in_degree[node] <= 1]
+            while stk:
+                u = stk.pop()
+                result.remove(u)
+                for v in adj[u]:
+                    in_degree[v] -= 1
+                    if in_degree[v] == 1:
+                        stk.append(v)
+            return result
+    
+        def bfs(u, dist, parent):
+            result = INF
+            dist[u] = 0
+            parent[u] = -1
+            q = [u]
+            while q:
+                new_q = []
+                for u in q:
+                    for v in adj[u]:
+                        if v in dist:
+                            if v != parent[u]:  # v != p
+                                l = 1
+                                nu, nv = u, v
+                                if dist[nu] < dist[nv]:
+                                    l += 1
+                                    nv = parent[nv]
+                                elif dist[nu] > dist[nv]:
+                                    l += 1
+                                    nu = parent[nu]
+                                while nu != nv:
+                                    l += 2
+                                    nv = parent[nv]
+                                    nu = parent[nu]
+                                result = min(result, l)
+                            continue
+                        dist[v] = dist[u]+1
+                        parent[v] = u
+                        new_q.append(v)                        
+                    q = new_q
+            return result
+    
+        adj = [[] for _ in xrange(n)]
+        in_degree = collections.Counter()
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            in_degree[u] += 1
+            in_degree[v] += 1
+        in_cycle = iter_dfs()
+        result = INF
+        dist, parent = {}, {}
+        for u in in_cycle:
+            if u in dist:
+                continue
+            l = bfs(u, dist, parent)
+            result = min(result, l)
+        return result if result != INF else -1
+
+
+# Time:  O(n^2)
+# Space: O(n + e)
+# bfs
+class Solution2(object):
     def findShortestCycle(self, n, edges):
         """
         :type n: int
