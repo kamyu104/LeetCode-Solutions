e2d1428ef92ba3b5c4a60115da1a1a98906c3c30
kamyu <kamyu104@users.noreply.github.com>
2023-04-09 20:21:44 +0800

    Update beautiful-pairs.py

--- Python/beautiful-pairs.py
+++ Python/beautiful-pairs.py
@@ -14,7 +14,7 @@ class Solution(object):
         :rtype: List[int]
         """
         INF = float("inf")
-        MAX_NEIGHBOR_COUNT = 9
+        MAX_NEIGHBOR_COUNT = 8
         def manhattan_distance(a, b):
             return abs(a[0]-b[0])+abs(a[1]-b[1])
     
