e98433b4b5bb24a47b2a246210db196fc6a8b103
kamyu <kamyu104@users.noreply.github.com>
2023-04-10 02:41:00 +0800

    Update beautiful-pairs.py

--- Python/beautiful-pairs.py
+++ Python/beautiful-pairs.py
@@ -1,12 +1,72 @@
-# Time:  O(nlogn)
+# Time:  O(n) on average
 # Space: O(n)
 
+import random
+import itertools
+import math
+
+
+# random algorithms, variant of closest pair
+# reference: https://github.com/jilljenn/tryalgo/blob/master/tryalgo/closest_points.py
+random.seed(0)
+class Solution(object):
+    def beautifulPair(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        INF = float("inf")
+        def dist(a, b):
+            x, y = a[-1], b[-1]
+            if x > y:
+                x, y = y, x
+            return [abs(a[0]-b[0])+abs(a[1]-b[1]), x, y]
+
+        def cell(point, size):
+            x, y, _ = point
+            return math.floor(x/size), math.floor(y/size)
+
+        def improve():
+            lookup = {}
+            for p in points:
+                i, j = map(int, cell(p, result[0]/2.0))
+                for ni in xrange(i-2, (i+2)+1):
+                    for nj in xrange(j-2, (j+2)+1):
+                        if (ni, nj) not in lookup:
+                            continue
+                        d = dist(p, lookup[ni, nj])
+                        if d < result:
+                            result[:] = d
+                            return True
+                lookup[i, j] = p
+            return False
+
+        points = [(i, j, idx) for idx, (i, j) in enumerate(itertools.izip(nums1, nums2))]
+        result = [INF]*3
+        lookup = {}
+        for i in reversed(xrange(len(points))):
+            if points[i][:2] in lookup:
+                result = [0, i, lookup[points[i][:2]]]
+            lookup[points[i][:2]] = i
+        if result[0] == 0:
+            return result[1:]
+        random.shuffle(points)
+        p, q = points[0], points[1]
+        result = dist(p, q)
+        while improve():
+            pass
+        return result[1:]
+
+
+# Time:  O(nlogn)
+# Space: O(n)
 import itertools
 
 
 # divide and conquer, merge sort, variant of closest pair
 # reference: https://www.baeldung.com/cs/minimal-manhattan-distance
-class Solution(object):
+class Solution2(object):
     def beautifulPair(self, nums1, nums2):
         """
         :type nums1: List[int]
@@ -75,7 +135,7 @@ import itertools
 
 # divide and conquer, merge sort, variant of closest pair
 # reference: https://www.baeldung.com/cs/minimal-manhattan-distance
-class Solution2(object):
+class Solution3(object):
     def beautifulPair(self, nums1, nums2):
         """
         :type nums1: List[int]
