e4747d90461b4b128387c1778c2af397815d4d9f
kamyu <kamyu104@users.noreply.github.com>
2023-04-10 14:59:07 +0800

    Update beautiful-pairs.cpp

--- C++/beautiful-pairs.cpp
+++ C++/beautiful-pairs.cpp
@@ -324,9 +324,9 @@ public:
                 result = min(result, dist(i, j));
             }
             st1.update(y_to_idx[points[i][1]], vector<int>{points[i][0] + points[i][1], -i});
-            j = -st2.query(y_to_idx[points[i][1]], size(y_to_idx) - 1)[1];   // min((xi-xj)+(yj-yi)) for j in range(y_to_idx[points[i][1], len(y_to_idx)) = (xi-yi)-max((xj-yj) for j in range(y_to_idx[points[i][1], len(y_to_idx))
+            j = -st2.query(y_to_idx[points[i][1]] + 1, size(y_to_idx) - 1)[1];   // min((xi-xj)+(yj-yi)) for j in range(y_to_idx[points[i][1]+1, len(y_to_idx)) = (xi-yi)-max((xj-yj) for j in range(y_to_idx[points[i][1]+1, len(y_to_idx))
             if (j != INF) {
-                assert(points[j][1] >= points[i][1]);
+                assert(points[j][1] > points[i][1]);
                 result = min(result, dist(i, j));
             }
             st2.update(y_to_idx[points[i][1]], vector<int>{points[i][0] - points[i][1], -i});
