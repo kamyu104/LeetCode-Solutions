bc1c8ef110afd47c7f48c5a800d264af8b27a8d7
kamyu <kamyu104@users.noreply.github.com>
2023-04-10 14:45:15 +0800

    Update beautiful-pairs.cpp

--- C++/beautiful-pairs.cpp
+++ C++/beautiful-pairs.cpp
@@ -258,3 +258,124 @@ public:
         return {result[1], result[2]};
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(n)
+// segment tree
+class Solution4 {
+private:
+    template<typename T>
+    struct VectorHash {
+        size_t operator()(const std::vector<T>& v) const {
+            size_t seed = 0;
+            for (const auto& i : v) {
+                seed ^= std::hash<T>{}(i)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
+            }
+            return seed;
+        }
+    };
+
+public:
+    vector<int> beautifulPair(vector<int>& nums1, vector<int>& nums2) {
+        static const int INF = numeric_limits<int>::max();
+
+        vector<vector<int>> points;
+        for (int i = 0; i < size(nums1); ++i) {
+            points.push_back({nums1[i], nums2[i]});
+        }
+        vector<int> result(3, INF);
+        unordered_map<vector<int>, int, VectorHash<int>> lookup;
+        for (int i = size(points) - 1; i >= 0; --i) {
+            if (lookup.count(points[i])) {
+                result = min(result, vector<int>{0, i, lookup[points[i]]});
+            }
+            lookup[points[i]] = i;
+        }
+        if (result[0] == 0) {
+            return {result[1], result[2]};
+        }
+        vector<int> order(size(points));
+        iota(begin(order), end(order), 0);
+        sort(begin(order), end(order), [&](const auto& a, const auto& b) {
+            return points[a][0] < points[b][0];
+        });
+        const auto& dist = [&](auto a, auto b) {
+            if (a > b) {
+                swap(a, b);
+            }
+            return vector<int>{abs(points[a][0] - points[b][0]) + abs(points[a][1] - points[b][1]), a, b};
+        };
+
+        unordered_set<int> y_set;
+        for (const auto& p : points) {
+            y_set.emplace(p[1]);
+        }
+        vector<int> sorted_y(cbegin(y_set), cend(y_set));
+        sort(begin(sorted_y), end(sorted_y));
+        unordered_map<int, int> y_to_idx;
+        for (int i = 0; i < size(sorted_y); ++i) {  // coordinate compression
+            y_to_idx[sorted_y[i]] = i;
+        }
+        SegmentTree st1(size(y_to_idx)), st2(size(y_to_idx));
+        for (const auto& i : order) {
+            int j = -st1.query(0, y_to_idx[points[i][1]])[1];  // min((xi-xj)+(yi-yj)) for j in range(i+1) = (xi+yi)-max((xj+yj) for j in range(i+1))
+            if (j != INF) {
+                assert(points[j][1] <= points[i][1]);
+                result = min(result, dist(i, j));
+            }
+            st1.update(y_to_idx[points[i][1]], vector<int>{points[i][0] + points[i][1], -i});
+            j = -st2.query(y_to_idx[points[i][1]], size(y_to_idx) - 1)[1];   // min((xi-xj)+(yj-yi)) for j in range(i, len(y_to_idx)) = (xi-yi)-max((xj-yj) for j in range(i, len(y_to_idx))
+            if (j != INF) {
+                assert(points[j][1] >= points[i][1]);
+                result = min(result, dist(i, j));
+            }
+            st2.update(y_to_idx[points[i][1]], vector<int>{points[i][0] - points[i][1], -i});
+        }
+        return {result[1], result[2]};
+    }
+
+private:
+    class SegmentTree {
+    private:
+        static const int INF = numeric_limits<int>::max();
+      
+    public:
+        explicit SegmentTree(int N)
+          : tree(N > 1 ? 1 << (__lg(N - 1) + 2) : 2, vector<int>{-INF, -INF}),
+            base(N > 1 ? 1 << (__lg(N - 1) + 1) : 1) {
+
+        }
+
+        void update(int i, const vector<int>& h) {
+            int x = base + i;
+            tree[x] = h;
+            while (x > 1) {
+                x /= 2;
+                tree[x] = max(tree[x * 2], tree[x * 2 + 1]);
+            }
+        }
+
+        vector<int> query(int L, int R) {
+            vector<int> result = {-INF, -INF};
+            if (L > R) {
+                return result;
+            }
+            L += base;
+            R += base;
+            for (; L <= R; L /= 2, R /= 2) {
+                if (L & 1) {
+                    result = max(result, tree[L]);
+                    ++L;
+                }
+                if ((R & 1) == 0) {
+                    result = max(tree[R], result);
+                    --R;
+                }
+            }
+            return result;
+        }
+
+        vector<vector<int>> tree;
+        int base;
+    };
+};
