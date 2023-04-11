e462423c8157798a66eb7244ac5158b2bcd44ae9
kamyu <kamyu104@users.noreply.github.com>
2023-04-11 16:04:18 +0800

    Create prime-in-diagonal.cpp

---
+++ C++/prime-in-diagonal.cpp
@@ -0,0 +1,41 @@
+// Time:  precompute: O(p)
+//        runtime: O(n)
+// Space: O(p)
+
+// number theory
+vector<int> linear_sieve_of_eratosthenes(int n) {  // Time: O(n), Space: O(n)
+    vector<int> spf(n + 1, -1);
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        if (spf[i] == -1) {
+            spf[i] = i;
+            primes.emplace_back(i);
+        }
+        for (const auto& p : primes) {
+            if (i * p > n || p > spf[i]) {
+                break;
+            }
+            spf[i * p] = p;
+        }
+    }
+    return primes;  // len(primes) = O(n/(logn-1)), reference: https://math.stackexchange.com/questions/264544/how-to-find-number-of-prime-numbers-up-to-to-n
+}
+
+const int MAX_N = 4 * 1e6;
+const auto& PRIMES = linear_sieve_of_eratosthenes(MAX_N);
+const unordered_set<int> PRIMES_SET(cbegin(PRIMES), cend(PRIMES));
+class Solution {
+public:
+    int diagonalPrime(vector<vector<int>>& nums) {
+        int result = 0;
+        for (int i = 0; i < size(nums); ++i) {
+            if (PRIMES_SET.count(nums[i][i])) {
+                result = max(result, nums[i][i]);
+            }
+            if (PRIMES_SET.count(nums[i][size(nums) - 1 - i])) {
+                result = max(result, nums[i][size(nums) - 1 - i ]);
+            }
+        }
+        return result;
+    }
+};
