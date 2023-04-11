1cf4c7a576045378261c192c487a05a413d5c679
kamyu <kamyu104@users.noreply.github.com>
2023-04-11 21:27:56 +0800

    Update minimum-number-of-visited-cells-in-a-grid.py

--- Python/minimum-number-of-visited-cells-in-a-grid.py
+++ Python/minimum-number-of-visited-cells-in-a-grid.py
@@ -41,8 +41,8 @@ class Solution(object):
         """
         m, n = len(grid), len(grid[0])
         target = (m-1, n-1)
-        uf1 = [UnionFind(n+1) for _ in xrange(m+1)]
-        uf2 = [UnionFind(m+1) for _ in xrange(n+1)]
+        uf1 = [UnionFind(n+1) for _ in xrange(m)]
+        uf2 = [UnionFind(m+1) for _ in xrange(n)]
         d = 1
         q = [(0, 0)]
         uf1[0].union_set(0, 1)
