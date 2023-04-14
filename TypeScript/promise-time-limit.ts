d6641d8a64ed40e52650af678179d59e853bd27f
kamyu <kamyu104@users.noreply.github.com>
2023-04-15 02:47:56 +0800

    Update promise-time-limit.ts

--- TypeScript/promise-time-limit.ts
+++ TypeScript/promise-time-limit.ts
@@ -4,7 +4,7 @@
 type Fn = (...params: any[]) => Promise<any>;
 
 function timeLimit(fn: Fn, t: number): Fn {
-	return async function(...args) {
+    return async function(...args) {
         return Promise.race([
             fn.apply(this, args),
             new Promise((_, reject) => setTimeout(() => reject('Time Limit Exceeded'), t)),
