4814155bc110aa44f7d1fafa0142c8ae7fc065d0
kamyu <kamyu104@users.noreply.github.com>
2023-04-15 02:35:30 +0800

    Update function-composition.ts

--- TypeScript/function-composition.ts
+++ TypeScript/function-composition.ts
@@ -4,5 +4,5 @@
 type F = (x: number) => number;
 
 function compose(functions: F[]): F {
-	return (x: number) => functions.reduceRight((total: number, f: F) => f(total), x);
+    return (x: number) => functions.reduceRight((total: number, f: F) => f(total), x);
 };
