95fa56230944c224970e99c7122e4cea8e1a33e8
kamyu <kamyu104@users.noreply.github.com>
2023-04-14 20:35:00 +0800

    Create count-the-number-of-k-free-subsets.py

---
+++ Python/count-the-number-of-k-free-subsets.py
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+import operator
+
+
+# combinatorics, dp
+class Solution(object):
+    def countTheNumOfKFreeSubsets(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def count(x):
+            y = x
+            while y-k in cnt:
+                y -= k
+            dp = [1, 0]  # dp[0]: count without i, dp[1]: count with i
+            for i in xrange(y, x+1, k):
+                dp = [dp[0]+dp[1], dp[0]*((1<<cnt[i])-1)]
+            return sum(dp)
+
+        cnt = collections.Counter(nums)
+        return reduce(operator.mul, (count(i) for i in cnt.iterkeys() if i+k not in cnt))
