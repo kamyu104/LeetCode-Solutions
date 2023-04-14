23d184f20200514adc4717212becf1385abda5c8
kamyu <kamyu104@users.noreply.github.com>
2023-04-15 02:34:23 +0800

    Update debounce.ts

--- TypeScript/debounce.ts
+++ TypeScript/debounce.ts
@@ -4,7 +4,7 @@
 type F = (...p: any[]) => any
 
 function debounce(fn: F, t: number): F {
-    let timeout = null
+    let timeout = null;
     return (...args) => {
         clearTimeout(timeout);
         timeout = setTimeout(fn, t, ...args);
