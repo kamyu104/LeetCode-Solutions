cbcb5a3d248b6b7fc6d01f2bd3650ec3dc4fb145
kamyu <kamyu104@users.noreply.github.com>
2023-04-25 01:16:17 +0800

    Create design-graph-with-shortest-path-calculator.py

---
+++ Python/design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,51 @@
+# Time:  ctor:    O(|V| + |E|)
+#        addEdge: O(1)
+#        shortestPath: O((|E| + |V|) * log|V|) = O(|E| * log|V|)
+# Space: O(|E| + |V|) = O(|E|)
+
+import heapq
+
+
+# dijkstra's algorithm
+class Graph(object):
+
+    def __init__(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        """
+        self.__adj = [[] for _ in xrange(n)]
+        for edge in edges:
+            self.addEdge(edge)
+
+    def addEdge(self, edge):
+        """
+        :type edge: List[int]
+        :rtype: None
+        """
+        u, v, w = edge
+        self.__adj[u].append((v, w))
+
+    def shortestPath(self, node1, node2):
+        """
+        :type node1: int
+        :type node2: int
+        :rtype: int
+        """
+        def dijkstra(start, target):
+            best = [float("inf")]*len(adj)
+            best[start] = 0
+            min_heap = [(best[start], start)]
+            while min_heap:
+                curr, u = heapq.heappop(min_heap)
+                if best[u] < curr:
+                    continue
+                for v, w in adj[u]:                
+                    if not (curr+w < best[v]):
+                        continue
+                    best[v] = curr+w
+                    heapq.heappush(min_heap, (best[v], v))
+            return best[target] if best[target] != float("inf") else -1
+
+        adj = self.__adj
+        return dijkstra(node1, node2)
