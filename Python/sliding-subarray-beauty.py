ec5abeb31ebafa9bfb162452b892e41d854b8cf4
kamyu <kamyu104@users.noreply.github.com>
2023-04-25 23:34:14 +0800

    Create sliding-subarray-beauty.py

---
+++ Python/sliding-subarray-beauty.py
@@ -0,0 +1,24 @@
+# Time:  O(nlogk)
+# Space: O(k)
+
+from sortedcontainers import SortedList
+
+
+# sorted list
+class Solution(object):
+    def getSubarrayBeauty(self, nums, k, x):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type x: int
+        :rtype: List[int]
+        """
+        result = []
+        sl = SortedList()
+        for i, v in enumerate(nums): 
+            if i-k >= 0:
+                sl.remove(nums[i-k])
+            sl.add(v)
+            if i-k+1 >= 0:
+                result.append(min(sl[x-1], 0))
+        return result 
