02553e10adbc4e1c55cecf3719267c97592f3e30
kamyu <kamyu104@users.noreply.github.com>
2023-04-25 23:44:39 +0800

    Create minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp

---
+++ C++/minimum-number-of-operations-to-make-all-array-elements-equal-to-1.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+// math, number theory, constructive algorithms
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        static const int INF = numeric_limits<int>::max();
+
+        const int cnt = count(cbegin(nums), cend(nums), 1);
+        if (cnt) {
+            return size(nums) - cnt;
+        }
+        int result = INF;
+        for (int i = 0; i < size(nums); ++i) {
+            int g = nums[i];
+            for (int j = i + 1; j < size(nums); ++j) {
+                g = gcd(g, nums[j]);
+                if (g == 1) {
+                    result = min(result, j - i);
+                    break;
+                }
+            }
+        }
+        return result != INF ? result + (size(nums) - 1) : -1;
+    }
+};
