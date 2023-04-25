4f47178015b84499ea637e6177f6387d8db0ce53
kamyu <kamyu104@users.noreply.github.com>
2023-04-25 23:02:32 +0800

    Create calculate-delayed-arrival-time.cpp

---
+++ C++/calculate-delayed-arrival-time.cpp
@@ -0,0 +1,10 @@
+// Time:  O(1)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        return (arrivalTime + delayedTime) % 24;
+    }
+};
