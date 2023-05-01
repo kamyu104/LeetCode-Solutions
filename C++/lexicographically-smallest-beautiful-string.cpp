5a48a064a8b7fdbaeb58a6f2d5e554a981ba74a0
kamyu <kamyu104@users.noreply.github.com>
2023-05-01 14:20:18 +0800

    Create lexicographically-smallest-beautiful-string.cpp

---
+++ C++/lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,30 @@
+// Time:  O(n)
+// Space: O(n)
+
+// greedy
+class Solution {
+public:
+    string smallestBeautifulString(string s, int k) {
+        const auto& check = [&](int i) {
+            return (i - 1 < 0 || s[i - 1] != s[i]) && (i - 2 < 0 || s[i - 2] != s[i]);
+        };
+
+        int i = size(s) - 1;
+        for (; i >= 0; --i) {
+            for (++s[i]; !check(i); ++s[i]);
+            if (s[i] - 'a' < k) {
+                break;
+            }
+        }
+        if (i < 0) {
+            return "";
+        }
+        for (int j = i + 1; j < size(s); ++j) {
+            s[j] = 'a';
+            while (!check(j)) {
+                ++s[j];
+            }
+        }
+        return s;
+    }
+};
