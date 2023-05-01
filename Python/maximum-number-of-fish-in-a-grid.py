88c68efb5f88a62ce24f8fead36d3d246ec01ae4
kamyu <kamyu104@users.noreply.github.com>
2023-05-01 14:43:04 +0800

    Update maximum-number-of-fish-in-a-grid.py

--- Python/maximum-number-of-fish-in-a-grid.py
+++ Python/maximum-number-of-fish-in-a-grid.py
@@ -1,43 +1,8 @@
 # Time:  O(m * n)
-# Space: O(1)
+# Space: O(m + n)
 
-# dfs
-class Solution(object):
-    def findMaxFish(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        def dfs(i, j):
-            result = grid[i][j]
-            stk = [(i, j)]
-            grid[i][j] = 0
-            while stk:
-                i, j = stk.pop()
-                for di, dj in reversed(DIRECTIONS):
-                    ni, nj = i+di, j+dj
-                    if not (0 <= ni < len(grid) and
-                            0 <= nj < len(grid[0]) and
-                            grid[ni][nj]):
-                        continue
-                    result += grid[ni][nj]
-                    grid[ni][nj] = 0
-                    stk.append((ni, nj))
-            return result
-
-        result = 0
-        for i in xrange(len(grid)):
-            for j in xrange(len(grid[0])):
-                if grid[i][j]:
-                    result = max(result, dfs(i, j))
-        return result
-
-
-# Time:  O(m * n)
-# Space: O(1)
 # bfs
-class Solution2(object):
+class Solution(object):
     def findMaxFish(self, grid):
         """
         :type grid: List[List[int]]
@@ -68,4 +33,39 @@ class Solution2(object):
             for j in xrange(len(grid[0])):
                 if grid[i][j]:
                     result = max(result, bfs(i, j))
+        return result
+
+
+# Time:  O(m * n)
+# Space: O(m * n)
+# dfs
+class Solution2(object):
+    def findMaxFish(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        def dfs(i, j):
+            result = grid[i][j]
+            stk = [(i, j)]
+            grid[i][j] = 0
+            while stk:
+                i, j = stk.pop()
+                for di, dj in reversed(DIRECTIONS):
+                    ni, nj = i+di, j+dj
+                    if not (0 <= ni < len(grid) and
+                            0 <= nj < len(grid[0]) and
+                            grid[ni][nj]):
+                        continue
+                    result += grid[ni][nj]
+                    grid[ni][nj] = 0
+                    stk.append((ni, nj))
+            return result
+
+        result = 0
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                if grid[i][j]:
+                    result = max(result, dfs(i, j))
         return result
