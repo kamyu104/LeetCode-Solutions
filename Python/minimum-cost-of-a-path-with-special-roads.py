e2fd111b8ab0072861ecdeb5923142fe228f27a2
kamyu <kamyu104@users.noreply.github.com>
2023-05-01 13:32:35 +0800

    Update minimum-cost-of-a-path-with-special-roads.py

--- Python/minimum-cost-of-a-path-with-special-roads.py
+++ Python/minimum-cost-of-a-path-with-special-roads.py
@@ -32,3 +32,41 @@ class Solution(object):
             for (x2, y2), c in adj[x1, y1].iteritems():
                 if  (x2, y2) not in lookup or lookup[x2, y2] > d+c:
                     lookup[x2, y2] = d+c
+
+
+# Time:  O(n^2 * logn)
+# Space: O(n + e)
+import collections
+import heapq
+# dijkstra's algorithm
+class Solution2(object):
+    def minimumCost(self, start, target, specialRoads):
+        """
+        :type start: List[int]
+        :type target: List[int]
+        :type specialRoads: List[List[int]]
+        :rtype: int
+        """
+        start, target = tuple(start), tuple(target)
+        adj = collections.defaultdict(list, {target:[]})
+        for x1, y1, x2, y2, c in specialRoads:
+            adj[x1, y1].append((x2, y2, c))
+        min_heap = [(0, start)]
+        lookup = {start:0}
+        while min_heap:
+            d, (x1, y1) = heapq.heappop(min_heap)
+            if d > lookup[x1, y1]:
+                continue
+            if (x1, y1) == target:
+                return d
+            for x2, y2, c in adj[x1, y1]:
+                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+c):
+                    continue
+                lookup[x2, y2] = d+c
+                heapq.heappush(min_heap, (lookup[x2, y2], (x2, y2)))
+            for x2, y2 in adj.iterkeys():
+                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
+                    continue
+                lookup[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
+                heapq.heappush(min_heap, (lookup[x2, y2], (x2, y2)))
+        return -1
