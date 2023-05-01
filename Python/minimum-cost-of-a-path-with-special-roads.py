39c61e6fb012fa7e3fd1a46ffca7341d028c918f
kamyu <kamyu104@users.noreply.github.com>
2023-05-01 15:18:55 +0800

    Update minimum-cost-of-a-path-with-special-roads.py

--- Python/minimum-cost-of-a-path-with-special-roads.py
+++ Python/minimum-cost-of-a-path-with-special-roads.py
@@ -15,26 +15,23 @@ class Solution(object):
         """
         start, target = tuple(start), tuple(target)
         adj = collections.defaultdict(list, {target:[]})
-        nodes = {start, target}
         for x1, y1, x2, y2, c in specialRoads:
             adj[x1, y1].append((x2, y2, c))
-            nodes.add((x1, y1))
-            nodes.add((x2, y2))
-        min_heap = [(0, start)]
-        lookup = {start:0}
-        while nodes:
-            d, x1, y1 = min((lookup[x1, y1], x1, y1) for x1, y1 in nodes if (x1, y1) in lookup)
-            nodes.remove((x1, y1))
+        dist = {start:0}
+        lookup = set()
+        while len(lookup) != len(dist):
+            d, x1, y1 = min((dist[x1, y1], x1, y1) for x1, y1 in dist.iterkeys() if (x1, y1) not in lookup)
+            lookup.add((x1, y1))
             if (x1, y1) == target:
                 return d
             for x2, y2, c in adj[x1, y1]:
-                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+c):
+                if not ((x2, y2) not in dist or dist[x2, y2] > d+c):
                     continue
-                lookup[x2, y2] = d+c
+                dist[x2, y2] = d+c
             for x2, y2 in adj.iterkeys():
-                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
+                if not ((x2, y2) not in dist or dist[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
                     continue
-                lookup[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
+                dist[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
 
 
 # Time:  O(n^2 * logn)
@@ -55,21 +52,21 @@ class Solution2(object):
         for x1, y1, x2, y2, c in specialRoads:
             adj[x1, y1].append((x2, y2, c))
         min_heap = [(0, start)]
-        lookup = {start:0}
+        dist = {start:0}
         while min_heap:
             d, (x1, y1) = heapq.heappop(min_heap)
-            if d > lookup[x1, y1]:
+            if d > dist[x1, y1]:
                 continue
             if (x1, y1) == target:
                 return d
             for x2, y2, c in adj[x1, y1]:
-                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+c):
+                if not ((x2, y2) not in dist or dist[x2, y2] > d+c):
                     continue
-                lookup[x2, y2] = d+c
-                heapq.heappush(min_heap, (lookup[x2, y2], (x2, y2)))
+                dist[x2, y2] = d+c
+                heapq.heappush(min_heap, (dist[x2, y2], (x2, y2)))
             for x2, y2 in adj.iterkeys():
-                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
+                if not ((x2, y2) not in dist or dist[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
                     continue
-                lookup[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
-                heapq.heappush(min_heap, (lookup[x2, y2], (x2, y2)))
+                dist[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
+                heapq.heappush(min_heap, (dist[x2, y2], (x2, y2)))
         return -1
