b52baa5cca7c4cc4475f75ca9c445ea2e571300f
kamyu <kamyu104@users.noreply.github.com>
2023-05-01 14:58:02 +0800

    Update minimum-cost-of-a-path-with-special-roads.py

--- Python/minimum-cost-of-a-path-with-special-roads.py
+++ Python/minimum-cost-of-a-path-with-special-roads.py
@@ -14,24 +14,27 @@ class Solution(object):
         :rtype: int
         """
         start, target = tuple(start), tuple(target)
-        adj = collections.defaultdict(lambda: collections.defaultdict(lambda: float("inf")))
+        adj = collections.defaultdict(list, {target:[]})
         nodes = {start, target}
         for x1, y1, x2, y2, c in specialRoads:
-            adj[x1, y1][x2, y2] = min(adj[x1, y1][x2, y2], c)
+            adj[x1, y1].append((x2, y2, c))
             nodes.add((x1, y1))
             nodes.add((x2, y2))
-        for x1, y1 in nodes:
-            for x2, y2 in nodes:
-                adj[x1, y1][x2, y2] = min(adj[x1, y1][x2, y2], abs(x2-x1)+abs(y2-y1))
+        min_heap = [(0, start)]
         lookup = {start:0}
         while nodes:
             d, x1, y1 = min((lookup[x1, y1], x1, y1) for x1, y1 in nodes if (x1, y1) in lookup)
             nodes.remove((x1, y1))
             if (x1, y1) == target:
                 return d
-            for (x2, y2), c in adj[x1, y1].iteritems():
-                if  (x2, y2) not in lookup or lookup[x2, y2] > d+c:
-                    lookup[x2, y2] = d+c
+            for x2, y2, c in adj[x1, y1]:
+                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+c):
+                    continue
+                lookup[x2, y2] = d+c
+            for x2, y2 in adj.iterkeys():
+                if not ((x2, y2) not in lookup or lookup[x2, y2] > d+abs(x2-x1)+abs(y2-y1)):
+                    continue
+                lookup[x2, y2] = d+abs(x2-x1)+abs(y2-y1)
 
 
 # Time:  O(n^2 * logn)
