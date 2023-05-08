afcf4eaa389ababddda1ecf51421e26408bdb899
kamyu <kamyu104@users.noreply.github.com>
2023-05-08 19:24:06 +0800

    Update number-of-adjacent-elements-with-the-same-color.cpp

--- C++/number-of-adjacent-elements-with-the-same-color.cpp
+++ C++/number-of-adjacent-elements-with-the-same-color.cpp
@@ -6,25 +6,25 @@ class Solution {
 public:
     vector<int> colorTheArray(int n, vector<vector<int>>& queries) {
         vector<int> nums(n);
-        const auto& update = [&](int i, int d) {
+        const auto& update = [&](int i) {
             if (!nums[i]) {
                 return 0;
             }
             int cnt = 0;
             if (i - 1 >= 0 && nums[i - 1] == nums[i]) {
-                cnt += d;
+                ++cnt;
             }
             if (i + 1 < n && nums[i + 1] == nums[i]) {
-                cnt += d;
+                ++cnt;
             }
             return cnt;
         };
 
         vector<int> result(size(queries));
         for (int idx = 0, curr = 0; idx < size(queries); ++idx) {
-            curr += update(queries[idx][0], -1);
+            curr -= update(queries[idx][0]);
             nums[queries[idx][0]] = queries[idx][1];
-            curr += update(queries[idx][0], 1);
+            curr += update(queries[idx][0]);
             result[idx] = curr;
         }
         return result;
