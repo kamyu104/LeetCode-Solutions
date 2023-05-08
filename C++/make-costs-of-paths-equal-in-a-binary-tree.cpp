8f91cd4a149f4b01f99598e2b42b6a0d335e7118
kamyu <kamyu104@users.noreply.github.com>
2023-05-08 19:58:17 +0800

    Update make-costs-of-paths-equal-in-a-binary-tree.cpp

--- C++/make-costs-of-paths-equal-in-a-binary-tree.cpp
+++ C++/make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -6,9 +6,9 @@ class Solution {
 public:
     int minIncrements(int n, vector<int>& cost) {
         int result = 0;
-        for (; n != 1; n -= 2) {
-            result += abs(cost[n - 2] - cost[n - 1]);
-            cost[(n - 2) / 2] += max(cost[n - 2], cost[n - 1]);
+        for (int i = (n - 1) / 2 - 1; i >= 0; --i) {
+            result += abs(cost[2 * i + 1] - cost[2 * i + 2]);
+            cost[i] += max(cost[2 * i + 1], cost[2 * i + 2]);
         }
         return result;
     }
