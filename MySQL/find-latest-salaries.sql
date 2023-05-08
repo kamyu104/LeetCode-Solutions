b85d354747cdae9f6b5dbc81017866105e8911ff
kamyu <kamyu104@users.noreply.github.com>
2023-05-08 17:27:46 +0800

    Create find-latest-salaries.sql

---
+++ MySQL/find-latest-salaries.sql
@@ -0,0 +1,12 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT DISTINCT 
+    emp_id,
+    firstname,
+    lastname,
+    MAX(salary) AS salary,
+    department_id   
+FROM Salary
+GROUP BY emp_id
+ORDER BY emp_id;
