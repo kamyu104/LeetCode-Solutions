462997a961825787d53a6462e23c6d77b21f8f49
kamyu <kamyu104@users.noreply.github.com>
2023-05-16 00:36:30 +0800

    Update maximum-number-of-moves-in-a-grid.cpp

--- C++/maximum-number-of-moves-in-a-grid.cpp
+++ C++/maximum-number-of-moves-in-a-grid.cpp
@@ -5,6 +5,38 @@
 class Solution {
 public:
     int maxMoves(vector<vector<int>>& grid) {
+        vector<bool> dp(size(grid), true);
+        for (int c = 0; c < size(grid[0]) - 1; ++c) {
+            vector<bool> new_dp(size(grid));
+            for (int r = 0; r < size(grid); ++r) {
+                if (!dp[r]) {
+                    continue;
+                }
+                if (grid[r][c] < grid[r][c + 1]) {
+                    new_dp[r] = true;
+                }
+                if (r - 1 >= 0 && grid[r][c] < grid[r - 1][c + 1]) {
+                    new_dp[r - 1] = true;
+                }
+                if (r + 1 < size(grid) && grid[r][c] < grid[r + 1][c + 1]) {
+                    new_dp[r + 1] = true;
+                }
+            }
+            dp = move(new_dp);
+            if (!accumulate(cbegin(dp), cend(dp), 0)) {
+                return c;
+            }
+        }
+        return size(grid[0]) - 1;
+    }
+};
+
+// Time:  O(m * n)
+// Space: O(m)
+// dp
+class Solution2 {
+public:
+    int maxMoves(vector<vector<int>>& grid) {
         vector<int> dp(size(grid));
         for (int c = size(grid[0]) - 2; c >= 0; --c) {
             vector<int> new_dp(size(grid));
