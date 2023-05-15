1082e799e97dbda68af683d2f06299ff7126d946
kamyu <kamyu104@users.noreply.github.com>
2023-05-16 00:54:38 +0800

    Update maximum-number-of-moves-in-a-grid.py

--- Python/maximum-number-of-moves-in-a-grid.py
+++ Python/maximum-number-of-moves-in-a-grid.py
@@ -1,26 +1,28 @@
 # Time:  O(m * n)
 # Space: O(m)
 
-# bfs
+# dp
 class Solution(object):
     def maxMoves(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
-        c = -1
-        q = set(range(len(grid)))
+        dp = [True]*len(grid)
+        result = 0
         for c in xrange(len(grid[0])-1):
-            new_q = set()
-            for r in q:
+            new_dp = [False]*len(grid)
+            for r in xrange(len(grid)):
+                if not dp[r]:
+                    continue
                 if grid[r][c] < grid[r][c+1]:
-                    new_q.add(r)
+                    new_dp[r] = True
                 if r-1 >= 0 and grid[r][c] < grid[r-1][c+1]:
-                    new_q.add(r-1)
+                    new_dp[r-1] = True
                 if r+1 < len(grid) and grid[r][c] < grid[r+1][c+1]:
-                    new_q.add(r+1)
-            q = new_q
-            if not q:
+                    new_dp[r+1] = True
+            dp = new_dp
+            if not sum(dp):
                 return c
         return len(grid[0])-1
 
@@ -46,3 +48,28 @@ class Solution2(object):
                     new_dp[r] = max(new_dp[r], dp[r+1]+1)
             dp = new_dp
         return max(dp)
+
+
+# Time:  O(m * n)
+# Space: O(m)
+# bfs
+class Solution3(object):
+    def maxMoves(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        q = set(xrange(len(grid)))
+        for c in xrange(len(grid[0])-1):
+            new_q = set()
+            for r in q:
+                if grid[r][c] < grid[r][c+1]:
+                    new_q.add(r)
+                if r-1 >= 0 and grid[r][c] < grid[r-1][c+1]:
+                    new_q.add(r-1)
+                if r+1 < len(grid) and grid[r][c] < grid[r+1][c+1]:
+                    new_q.add(r+1)
+            q = new_q
+            if not q:
+                return c
+        return len(grid[0])-1
