aeeed7d47eb92e001f18da556fdf93a47ff99b46
kamyu <kamyu104@users.noreply.github.com>
2023-05-16 00:24:32 +0800

    Update maximum-number-of-moves-in-a-grid.py

--- Python/maximum-number-of-moves-in-a-grid.py
+++ Python/maximum-number-of-moves-in-a-grid.py
@@ -8,6 +8,34 @@ class Solution(object):
         :type grid: List[List[int]]
         :rtype: int
         """
+        dp = [True]*len(grid)
+        result = 0
+        for c in xrange(len(grid[0])-1):
+            new_dp = [False]*len(grid)
+            for r in xrange(len(grid)):
+                if not dp[r]:
+                    continue
+                if grid[r][c] < grid[r][c+1]:
+                    new_dp[r] = True
+                if r-1 >= 0 and grid[r][c] < grid[r-1][c+1]:
+                    new_dp[r-1] = True
+                if r+1 < len(grid) and grid[r][c] < grid[r+1][c+1]:
+                    new_dp[r+1] = True
+            dp = new_dp
+            if not any(dp):
+                return c
+        return len(grid[0])-1
+
+
+# Time:  O(m * n)
+# Space: O(m)
+# dp
+class Solution2(object):
+    def maxMoves(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
         dp = [0]*len(grid)
         for c in reversed(xrange(len(grid[0])-1)):
             new_dp = [0]*len(grid)
