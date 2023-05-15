a40ac6168e18fcf76cb73aaee25397a0a287525b
kamyu <kamyu104@users.noreply.github.com>
2023-05-15 20:37:37 +0800

    Create find-the-losers-of-the-circular-game.py

---
+++ Python/find-the-losers-of-the-circular-game.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(n)
+
+# hash table
+class Solution(object):
+    def circularGameLosers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        lookup = [False]*n
+        idx = 0
+        for i in xrange(n):
+            if lookup[idx]:
+                break
+            lookup[idx] = True
+            idx = (idx+(i+1)*k)%n
+        return [i+1 for i in xrange(n) if not lookup[i]]
