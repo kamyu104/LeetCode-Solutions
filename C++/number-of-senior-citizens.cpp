3a20e37446b7dc73bc54ed4345b669cd4b145767
kamyu <kamyu104@users.noreply.github.com>
2023-05-15 14:18:38 +0800

    Create number-of-senior-citizens.cpp

---
+++ C++/number-of-senior-citizens.cpp
@@ -0,0 +1,12 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+        return accumulate(cbegin(details), cend(details), 0, [](const auto& total, const auto& x) {
+            return total + static_cast<int>(x.substr(11, 2) > "60");
+        });
+    }
+};
