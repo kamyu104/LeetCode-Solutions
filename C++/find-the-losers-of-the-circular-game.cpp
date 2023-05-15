3c3964aa2a842bdb1b45fc3bd632ded3bd179802
kamyu <kamyu104@users.noreply.github.com>
2023-05-15 20:37:04 +0800

    Create find-the-losers-of-the-circular-game.cpp

---
+++ C++/find-the-losers-of-the-circular-game.cpp
@@ -0,0 +1,24 @@
+// Time:  O(n)
+// Space: O(n)
+
+// hash table
+class Solution {
+public:
+    vector<int> circularGameLosers(int n, int k) {
+        vector<bool> lookup(n);
+        for (int i = 0, idx = 0; i < n; ++i) {
+            if (lookup[idx]) {
+                break;
+            }
+            lookup[idx] = true;
+            idx = (idx + (i + 1) * k) % n;
+        }
+        vector<int> result;
+        for (int i = 0; i < n; ++i) {
+            if (!lookup[i]) {
+                result.emplace_back(i + 1);
+            }
+        }
+        return result;
+    }
+};
