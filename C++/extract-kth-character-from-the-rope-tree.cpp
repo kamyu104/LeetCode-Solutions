5e755aa0d62ebe9e5c30c1fd81a024cd27ca63de
kamyu <kamyu104@users.noreply.github.com>
2023-05-22 17:05:43 +0800

    Create extract-kth-character-from-the-rope-tree.cpp

---
+++ C++/extract-kth-character-from-the-rope-tree.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(h)
+
+// binary tree
+class Solution {
+public:
+    char getKthCharacter(RopeTreeNode* root, int k) {
+        while (root->len) {
+            const int l = root->left ? max(root->left->len, static_cast<int>(size(root->left->val))) : 0;
+            if (k <= l) {
+                root = root->left;
+            } else {
+                k -= l;
+                root = root->right;
+            }
+        }
+        return root->val[k - 1];
+    }
+};
