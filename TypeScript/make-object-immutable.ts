87c087c224240df242a855a9bbcdc3f787982f91
kamyu <kamyu104@users.noreply.github.com>
2023-05-22 18:22:03 +0800

    Create make-object-immutable.ts

---
+++ TypeScript/make-object-immutable.ts
@@ -0,0 +1,31 @@
+// Time:  O(1)
+// Space: O(1)
+
+// proxy
+type Obj = Array<any> | Record<any, any>;
+
+const forbiddenMethods = new Set(["pop", "push", "shift", "unshift", "splice", "sort", "reverse"]);
+
+function makeImmutable(obj: Obj): Obj {
+    return new Proxy(obj, {
+        set(target, prop) {
+            if (Array.isArray(target) && typeof prop === 'string') {
+                if (Number(prop) !== undefined) {
+                    throw `Error Modifying Index: ${prop}`;
+                }
+            }
+            throw `Error Modifying: ${prop.toString()}`;
+        },
+        get(target, prop) {
+            if (typeof obj[prop] === 'object' && obj[prop] !== null) {
+                return makeImmutable(obj[prop]);
+            }
+            if (forbiddenMethods.has(prop.toString())) {
+                return () => {
+                    throw `Error Calling Method: ${prop.toString()}`;
+                }
+            }
+            return Reflect.get(target, prop);
+        }
+    });
+};
