94b41b5684c224afb2ba4aff08854dffa1e31002
kamyu <kamyu104@users.noreply.github.com>
2023-05-23 03:37:35 +0800

    Create modify-graph-edge-weights.cpp

---
+++ C++/modify-graph-edge-weights.cpp
@@ -0,0 +1,54 @@
+// Time:  O((|E| + |V|) * log|V|) = O(|E| * log|V|) by using binary heap,
+//        if we can further to use Fibonacci heap, it would be O(|E| + |V| * log|V|)
+// Space: O(|E| + |V|) = O(|E|)
+
+// dijkstra's algorithm
+class Solution {
+public:
+    vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
+        static const int INF = 1e9;
+
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& e : edges) {
+            adj[e[0]].emplace_back(e[1], e[2]);
+            adj[e[1]].emplace_back(e[0], e[2]);
+        }
+        const auto& dijkstra = [&](int start, int x) {
+            vector<int> best(size(adj), INF);
+            best[start] = 0;
+            priority_queue<pair<int64_t, int>, vector<pair<int64_t, int>>, greater<pair<int64_t, int>>> min_heap;
+            min_heap.emplace(0, start);
+            while (!empty(min_heap)) {
+                const auto [curr, u] = min_heap.top(); min_heap.pop();
+                if (best[u] < curr) {
+                    continue;
+                }
+                for (auto [v, w] : adj[u]) {
+                    if (w == -1) {
+                        w = x;
+                    }
+                    if (best[v] - curr <= w) {
+                        continue;
+                    }
+                    best[v] = curr + w;
+                    min_heap.emplace(curr + w, v);
+                }
+            }
+            return best;
+        };
+        const auto& left = dijkstra(source, 1);
+        if (!(left[destination] <= target)) {
+            return {};
+        }
+        const auto& right = dijkstra(destination, INF);
+        if (!(right[source] >= target)) {
+            return {};
+        }
+        for (auto& e : edges) {
+            if (e[2] == -1) {
+                e[2] = max({target - left[e[0]] - right[e[1]], target - left[e[1]] - right[e[0]], 1});
+            }
+        }
+        return edges;
+    }
+};
