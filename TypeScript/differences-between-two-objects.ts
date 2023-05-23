40a7d7c96c6f52164b9f8a725fdfff1eb3d38306
kamyu <kamyu104@users.noreply.github.com>
2023-05-23 11:34:37 +0800

    Update differences-between-two-objects.ts

--- TypeScript/differences-between-two-objects.ts
+++ TypeScript/differences-between-two-objects.ts
@@ -4,17 +4,18 @@
 // dfs
 function objDiff(obj1: any, obj2: any): any {
     let result = {};
-    for (let key in obj1) {
-        if (!(key in obj2)) {
+    for (const [k, o1] of Object.entries(obj1)) {
+        if (!(k in obj2)) {
             continue;
         }
-        if (typeof obj1[key] === 'object' && typeof obj2[key] === 'object' && Array.isArray(obj1[key]) === Array.isArray(obj2[key])) {
-            result[key] = objDiff(obj1[key], obj2[key]);
-            if (Object.keys(result[key]).length === 0) {
-                delete result[key];
+        const o2 = obj2[k];
+        if (typeof o1 === 'object' && typeof o2 === 'object' && Array.isArray(o1) === Array.isArray(o2)) {
+            result[k] = objDiff(o1, o2);
+            if (Object.keys(result[k]).length === 0) {
+                delete result[k];
             }
-        } else if (obj1[key] !== obj2[key]) {
-            result[key] = [obj1[key], obj2[key]];
+        } else if (o1 !== o2) {
+            result[k] = [o1, o2];
         }
     }
     return result;
