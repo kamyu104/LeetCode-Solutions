dcefd770c1eae5fc5716b4ad0d47f13e5d4290ae
kamyu <kamyu104@users.noreply.github.com>
2023-06-02 00:06:34 +0800

    Create minimum-operations-to-make-numbers-non-positive.py

---
+++ Python/minimum-operations-to-make-numbers-non-positive.py
@@ -0,0 +1,26 @@
+# Time:  O(nlogr)
+# Space: O(1)
+
+# binary search
+class Solution(object):
+    def minOperations(self, nums, x, y):
+        """
+        :type nums: List[int]
+        :type x: int
+        :type y: int
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//b
+
+        def check(total):
+            return sum(ceil_divide(max(v-min(ceil_divide(v, y), total)*y, 0), x-y) for v in nums) <= total
+    
+        left, right = 1, ceil_divide(max(nums), y)
+        while left <= right:
+            mid = left+(right-left)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return left
