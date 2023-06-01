aa4a3c79b01d98f7d01f23b935764b16e037aca9
kamyu <kamyu104@users.noreply.github.com>
2023-06-02 01:18:29 +0800

    Create buy-two-chocolates.py

---
+++ Python/buy-two-chocolates.py
@@ -0,0 +1,14 @@
+# Time:  O(n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def buyChoco(self, prices, money):
+        """
+        :type prices: List[int]
+        :type money: int
+        :rtype: int
+        """
+        i = min(xrange(len(prices)), key=lambda x: prices[x])
+        j = min((j for j in xrange(len(prices)) if j != i), key=lambda x: prices[x])
+        return money-(prices[i]+prices[j]) if prices[i]+prices[j] <= money else money
