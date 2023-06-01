077f89a30a9581a1889e150781e0345185e54bbf
kamyu <kamyu104@users.noreply.github.com>
2023-06-02 01:17:53 +0800

    Create buy-two-chocolates.cpp

---
+++ C++/buy-two-chocolates.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        const auto& min_idx = [&](int exclude) {
+            int j = -1;
+            for (int i = 0; i < size(prices); ++i) {
+                if (i != exclude && (j == -1 || prices[j] > prices[i])) {
+                    j = i;
+                }
+            }
+            return j;
+        };
+    
+        const auto& i = min_idx(-1);
+        const auto& j = min_idx(i);
+        return prices[i] + prices[j] <= money ? money - (prices[i] + prices[j]) : money;
+    }
+};
