caf43cb73060d3518a9fd6e722396c15aa476224
kamyu <kamyu104@users.noreply.github.com>
2023-06-02 00:06:04 +0800

    Create minimum-operations-to-make-numbers-non-positive.cpp

---
+++ C++/minimum-operations-to-make-numbers-non-positive.cpp
@@ -0,0 +1,29 @@
+// Time:  O(nlogr)
+// Space: O(1)
+
+// binary search
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x, int y) {
+        const auto& ceil_divide = [](int a, int b) {
+            return (a + b - 1) / b;
+        };
+
+        const auto& check = [&](int total) {
+            return accumulate(cbegin(nums), cend(nums), 0ll, [&](const auto& accu, const auto& v) {
+                return accu + ceil_divide(max(v - min(ceil_divide(v, y), total) * y, 0), x - y);
+            }) <= total;
+        };
+
+        int left = 1, right = ceil_divide(*max_element(cbegin(nums), cend(nums)), y);
+        while (left <= right) {
+            const int mid = left + (right - left) / 2;
+            if (check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
