7820ee8d194ef05b710bbe60445b82aecefb31d4
kamyu <kamyu104@users.noreply.github.com>
2023-06-03 03:19:21 +0800

    Create find-shortest-path-with-k-hops.cpp

---
+++ C++/find-shortest-path-with-k-hops.cpp
@@ -0,0 +1,44 @@
+// Time:  O(n * k + (k * e) * log(n * k))
+// Space: O(n * k + e)
+
+// dijkstra's algorithm
+class Solution {
+public:
+    int shortestPathWithHops(int n, vector<vector<int>>& edges, int s, int d, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (const auto& e : edges) {
+            adj[e[0]].emplace_back(e[1], e[2]);
+            adj[e[1]].emplace_back(e[0], e[2]);
+        }
+        const auto& modified_dijkstra = [&]() {
+            static const int INF = numeric_limits<int>::max();
+
+            vector<vector<int>> best(size(adj), vector<int>(k + 1, INF));
+            best[s][0] = 0;
+            priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> min_heap;
+            min_heap.emplace(best[s][0], s, 0);
+            while (!empty(min_heap)) {
+                const auto [curr, u, cnt] = min_heap.top(); min_heap.pop();
+                if (curr > best[u][cnt]) {
+                    continue;
+                }
+                if (u == d) {
+                    return curr;
+                }
+                for (const auto& [v, w] : adj[u]) {
+                    if (w < best[v][cnt] - curr) {
+                        best[v][cnt] = curr + w;
+                        min_heap.emplace(best[v][cnt], v, cnt);
+                    }
+                    if (cnt + 1 <= k && curr < best[v][cnt + 1]) {
+                        best[v][cnt + 1] = curr;
+                        min_heap.emplace(best[v][cnt + 1], v, cnt + 1);
+                    }
+                }
+            }
+            return -1;
+        };
+
+        return modified_dijkstra();
+    }
+};
