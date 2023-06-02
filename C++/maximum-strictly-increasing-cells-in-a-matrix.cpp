ece9a027d93e8e271a5524e51bcc2a38a7191f77
kamyu <kamyu104@users.noreply.github.com>
2023-06-03 02:04:02 +0800

    Create maximum-strictly-increasing-cells-in-a-matrix.cpp

---
+++ C++/maximum-strictly-increasing-cells-in-a-matrix.cpp
@@ -0,0 +1,27 @@
+// Time:  O(m * n * log(m * n))
+// Space: O(m * n)
+
+// dp
+class Solution {
+public:
+    int maxIncreasingCells(vector<vector<int>>& mat) {
+        map<int, vector<pair<int, int>>> lookup;
+        for (int i = 0; i < size(mat); ++i) {
+            for (int j = 0; j < size(mat[0]); ++j) {
+                lookup[mat[i][j]].emplace_back(i, j);
+            }
+        }
+        vector<vector<int>> dp(size(mat), vector<int>(size(mat[0])));
+        vector<int> row(size(mat)), col(size(mat[0]));
+        for (const auto& [_, pairs] : lookup) {
+            for (const auto& [i, j] : pairs) {
+                dp[i][j] = max(row[i], col[j]) + 1;
+            }
+            for (const auto& [i, j] : pairs) {
+                row[i] = max(row[i], dp[i][j]);
+                col[j] = max(col[j], dp[i][j]);
+            }
+        }
+        return *max_element(cbegin(row), cend(row));
+    }
+};
