2d4e5d12fbbfe41ea3a98f2cedb586340075fb8e
kamyu <kamyu104@users.noreply.github.com>
2023-06-02 19:54:19 +0800

    Create maximum-strength-of-a-group.cpp

---
+++ C++/maximum-strength-of-a-group.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    long long maxStrength(vector<int>& nums) {
+        if (all_of(cbegin(nums), cend(nums), [](const auto& x) { return x <= 0; }) &&
+            accumulate(cbegin(nums), cend(nums), 0ll, [](const auto& accu, const auto& x) { return accu + static_cast<int>(x < 0); }) <= 1) {
+            return *max_element(cbegin(nums), cend(nums));
+        }
+        const int64_t result = accumulate(cbegin(nums), cend(nums), 1ll, [](const auto& accu, const auto& x) { return x != 0 ? accu * x : accu; });
+        int mx = numeric_limits<int>::min();
+        for (const auto& x : nums) {
+            if (x < 0) {
+                mx = max(mx, x);
+            }
+        }
+        return result > 0 ? result : result / mx;
+    }
+};
