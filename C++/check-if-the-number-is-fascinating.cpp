b47b1fc8383fe848cbdc98bf6291110a5500eeac
kamyu <kamyu104@users.noreply.github.com>
2023-06-20 01:33:18 +0800

    Create check-if-the-number-is-fascinating.cpp

---
+++ C++/check-if-the-number-is-fascinating.cpp
@@ -0,0 +1,22 @@
+// Time:  O(logn)
+// Space: O(1)
+
+// string, bitmask
+class Solution {
+public:
+    bool isFascinating(int n) {
+        int lookup = 0;
+        const auto& check = [&](int x) {
+            for (; x ; x /= 10) {
+                const int d = x % 10;
+                if (d == 0 || lookup & (1 << d)) {
+                    return false;
+                }
+                lookup |= 1 << d;
+            }
+            return true;
+        };
+
+        return check(n) && check(2 * n) && check(3 * n);
+    }
+};
