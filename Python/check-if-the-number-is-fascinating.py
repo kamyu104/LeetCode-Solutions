d2f5f9edbb58b327837875f966ecc32265d50f1f
kamyu <kamyu104@users.noreply.github.com>
2023-06-20 01:33:58 +0800

    Create check-if-the-number-is-fascinating.py

---
+++ Python/check-if-the-number-is-fascinating.py
@@ -0,0 +1,33 @@
+# Time:  O(logn)
+# Space: O(1)
+
+# string, bitmask
+class Solution(object):
+    def isFascinating(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        lookup = [0]
+        def check(x):
+            while x:
+                x, d = divmod(x, 10)
+                if d == 0 or lookup[0]&(1<<d):
+                    return False
+                lookup[0] |= (1<<d)
+            return True
+    
+        return check(n) and check(2*n) and check(3*n)
+
+
+# Time:  O(logn)
+# Space: O(logn)
+# string
+class Solution2(object):
+    def isFascinating(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        s = str(n)+str(2*n)+str(3*n)
+        return '0' not in s and len(s) == 9 and len(set(s)) == 9
