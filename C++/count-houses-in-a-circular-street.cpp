8d5a3a4efdd75998c25771f785705e682805910b
kamyu <kamyu104@users.noreply.github.com>
2023-06-20 01:45:47 +0800

    Create count-houses-in-a-circular-street.cpp

---
+++ C++/count-houses-in-a-circular-street.cpp
@@ -0,0 +1,24 @@
+// Time:  O(k)
+// Space: O(1)
+
+// constructive algorithms
+class Solution {
+public:
+    int houseCount(Street* street, int k) {
+        for (int _ = 0; _ < k; ++_) {
+            if (street->isDoorOpen()) {
+                street->closeDoor();
+            }
+            street->moveRight();
+        }
+        int result = 0;
+        for (; result < k; ++result) {
+            if (street->isDoorOpen()) {
+                break;
+            }
+            street->openDoor();
+            street->moveRight();
+        }
+        return result;
+    }
+};
