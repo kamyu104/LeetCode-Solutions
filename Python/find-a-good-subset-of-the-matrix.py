434696560e9af7a11ac78df52d0aeef9945dc989
kamyu <kamyu104@users.noreply.github.com>
2023-06-20 07:48:56 +0800

    Update find-a-good-subset-of-the-matrix.py

--- Python/find-a-good-subset-of-the-matrix.py
+++ Python/find-a-good-subset-of-the-matrix.py
@@ -1,7 +1,7 @@
 # Time:  O(m * 2^n)
 # Space: O(2^n)
 
-# bitmasks, constructive algorithms, greedy, submask enumeration
+# bitmasks, constructive algorithms, greedy
 class Solution(object):
     def goodSubsetofBinaryMatrix(self, grid):
         """
@@ -13,34 +13,8 @@ class Solution(object):
             mask = reduce(lambda mask, j: mask|(grid[i][j]<<j), xrange(len(grid[0])), 0)
             if not mask:
                 return [i]
-            mask2 = ((1<<len(grid[0]))-1)^mask
-            submask = mask2  # submask enumeration
-            while submask > 0:
-                if submask in lookup:
-                    return [lookup[submask], i]
-                submask = (submask-1)&mask2                
-            lookup[mask] = i
-        return []
-
-
-# Time:  O(m * 2^n)
-# Space: O(2^n)
-# bitmasks, constructive algorithms, greedy
-class Solution2(object):
-    def goodSubsetofBinaryMatrix(self, grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: List[int]
-        """
-        lookup = {}
-        for i in xrange(len(grid)):
-            mask = reduce(lambda mask, j: mask|(grid[i][j]<<j), xrange(len(grid[0])), 0)
-            if not mask:
-                return [i]
-            for mask2 in xrange(1, 1<<len(grid[0])):
-                if mask&mask2:
-                    continue
-                if mask2 in lookup:
-                    return [lookup[mask2], i]
+            for mask2, j in lookup.iteritems():
+                if mask&mask2 == 0:
+                    return [j, i]
             lookup[mask] = i
         return []
