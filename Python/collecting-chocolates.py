a69a5f1fbbc94727447fa0c3233199cf1d5fcefe
kamyu <kamyu104@users.noreply.github.com>
2023-06-20 14:39:14 +0800

    Update collecting-chocolates.py

--- Python/collecting-chocolates.py
+++ Python/collecting-chocolates.py
@@ -36,10 +36,50 @@ class Solution(object):
         return min(accumulate(accumulate(diff2)))
 
 
+# Time:  O(nlogn)
+# Space: O(n)
+import collections
+
+
+# binary search, mono deque
+class Solution2(object):
+    def minCost(self, nums, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        def cost(k):
+            w = k+1
+            result = x*k
+            dq = collections.deque()
+            for i in xrange(len(nums)+w-1):
+                if dq and i-dq[0] == w:
+                    dq.popleft()
+                while dq and nums[dq[-1]%len(nums)] >= nums[i%len(nums)]:
+                    dq.pop()
+                dq.append(i)
+                if i >= w-1:
+                    result += nums[dq[0]%len(nums)]
+            return result
+
+        def check(x):
+            return cost(x) <= cost(x+1)
+
+        left, right = 0, len(nums)
+        while left <= right:
+            mid = left + (right-left)//2
+            if check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return cost(left)
+
+
 # Time:  O(n^2)
 # Space: O(n)
 # brute force
-class Solution2(object):
+class Solution3(object):
     def minCost(self, nums, x):
         """
         :type nums: List[int]
