9e7871523ea2739fa503ab28e65f9157bd979297
kamyu <kamyu104@users.noreply.github.com>
2023-06-21 15:31:13 +0800

    Create maximum-sum-queries.cpp

---
+++ C++/maximum-sum-queries.cpp
@@ -0,0 +1,35 @@
+// Time:  O(nlogn + mlogm + mlogn)
+// Space: O(n + m)
+
+// sort, mono stack, binary search
+class Solution {
+public:
+    vector<int> maximumSumQueries(vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& queries) {
+        vector<pair<int, int>> pairs;
+        for (int i = 0; i < size(nums1); ++i) {
+            pairs.emplace_back(nums1[i], nums2[i]);
+        }
+        sort(begin(pairs), end(pairs));
+        vector<tuple<int, int, int>> sorted_queries;
+        for (int i = 0; i < size(queries); ++i) {
+            sorted_queries.emplace_back(queries[i][0], queries[i][1], i);
+        }
+        sort(begin(sorted_queries), end(sorted_queries), greater<tuple<int, int, int>>());
+        vector<int> result(size(queries));
+        vector<pair<int, int>> stk;
+        for (const auto& [x, y, i] : sorted_queries) {
+            while (!empty(pairs) && pairs.back().first >= x) {
+                const auto [a, b] = pairs.back(); pairs.pop_back();
+                while (!empty(stk) && stk.back().second <= a + b) {
+                    stk.pop_back();
+                }
+                if (empty(stk) || stk.back().first < b) {
+                    stk.emplace_back(b, a + b);
+                }
+            }
+            const auto it = lower_bound(cbegin(stk), cend(stk), pair(y, 0));
+            result[i] = it != cend(stk) ? it->second : -1;
+        }
+        return result;
+    }
+};
