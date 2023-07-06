ba04a5c043c21bb8d408b2c263bb144a2dd603cc
kamyu <kamyu104@users.noreply.github.com>
2023-07-06 22:47:43 +0800

    Update count-substrings-without-repeating-character.cpp

--- C++/count-substrings-without-repeating-character.cpp
+++ C++/count-substrings-without-repeating-character.cpp
@@ -6,6 +6,25 @@ class Solution {
 public:
     int numberOfSpecialSubstrings(string s) {
         int result = 0, left = 0;
+        vector<int> lookup(26, -1);
+        for (int right = 0; right < size(s); ++right) {
+            if (lookup[s[right] - 'a'] >= left) {
+                left = lookup[s[right] - 'a'] + 1;
+            }
+            lookup[s[right] - 'a'] = right;
+            result += (right - left + 1);
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+// two pointers, sliding window
+class Solution2 {
+public:
+    int numberOfSpecialSubstrings(string s) {
+        int result = 0, left = 0;
         vector<bool> lookup(26);
         for (int right = 0; right < size(s); ++right) {
             while (lookup[s[right] - 'a']) {
