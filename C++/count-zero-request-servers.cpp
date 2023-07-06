74d21e46891e873ce4f7085c347df9068bdd4dd7
kamyu <kamyu104@users.noreply.github.com>
2023-07-07 02:45:15 +0800

    Update count-zero-request-servers.cpp

--- C++/count-zero-request-servers.cpp
+++ C++/count-zero-request-servers.cpp
@@ -1,8 +1,45 @@
 // Time:  O(nlogn + mlogm)
 // Space: O(n + m)
 
-// sort, line sweep
+// sort, two pointers
 class Solution {
+public:
+    vector<int> countServers(int n, vector<vector<int>>& logs, int x, vector<int>& queries) {
+        sort(begin(logs), end(logs), [](const auto& a, const auto& b) {
+            return a[1] < b[1];
+        });
+
+        vector<pair<int, int>> events;
+        for (int i = 0; i < size(queries); ++i) {
+            events.emplace_back(queries[i], i);
+        }
+        sort(begin(events), end(events));
+
+        vector<int> result(size(queries));
+        vector<int> cnt(n);
+        int curr = 0, left = 0, right = 0;
+        for (const auto& [t, i] : events) {
+            while (right < size(logs) && logs[right][1] <= t) {
+                if (cnt[logs[right++][0] - 1]++ == 0) {
+                    ++curr;
+                }
+            }
+            while (left < right && logs[left][1] < t - x) {
+                if (--cnt[logs[left++][0] - 1] == 0) {
+                    --curr;
+                }
+            }
+            result[i] = n - curr;
+        }
+        return result;
+    }
+};
+
+
+// Time:  O(nlogn + mlogm)
+// Space: O(n + m)
+// sort, line sweep
+class Solution2 {
 public:
     vector<int> countServers(int n, vector<vector<int>>& logs, int x, vector<int>& queries) {
         vector<tuple<int, int, int>> events;
