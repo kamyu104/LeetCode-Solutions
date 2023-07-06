5681b67cce4463af5b68fd6345a9e3ba6e80ef2e
kamyu <kamyu104@users.noreply.github.com>
2023-07-06 22:41:37 +0800

    Update count-substrings-without-repeating-character.py

--- Python/count-substrings-without-repeating-character.py
+++ Python/count-substrings-without-repeating-character.py
@@ -9,6 +9,25 @@ class Solution(object):
         :rtype: int
         """
         result = left = 0
+        lookup = [-1]*26
+        for right in xrange(len(s)):
+            if lookup[ord(s[right])-ord('a')] >= left:
+                left = lookup[ord(s[right])-ord('a')]+1
+            lookup[ord(s[right])-ord('a')] = right
+            result += (right-left+1)
+        return result
+
+
+# Time:  O(n)
+# Space: O(1)
+# two pointers, sliding window
+class Solution2(object):
+    def numberOfSpecialSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = left = 0
         lookup = [False]*26
         for right in xrange(len(s)):
             while lookup[ord(s[right])-ord('a')]:
