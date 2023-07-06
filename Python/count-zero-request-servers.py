3a19f706f6299d438cd4c5af72d03eb5dbed1d5c
kamyu <kamyu104@users.noreply.github.com>
2023-07-07 02:46:40 +0800

    Update count-zero-request-servers.py

--- Python/count-zero-request-servers.py
+++ Python/count-zero-request-servers.py
@@ -1,8 +1,39 @@
 # Time:  O(nlogn + mlogm)
 # Space: O(n + m)
 
-# sort, line sweep
+# sort, two pointers
 class Solution(object):
+    def countServers(self, n, logs, x, queries):
+        """
+        :type n: int
+        :type logs: List[List[int]]
+        :type x: int
+        :type queries: List[int]
+        :rtype: List[int]
+        """
+        logs.sort(key=lambda x:x[1])
+        result = [0]*len(queries)
+        cnt = [0]*n
+        curr = left = right = 0
+        for t, i in sorted((t, i) for i, t in enumerate(queries)):
+            while right < len(logs) and logs[right][1] <= t:
+                if cnt[logs[right][0]-1] == 0:
+                    curr += 1
+                cnt[logs[right][0]-1] += 1
+                right += 1
+            while left < right and logs[left][1] < t-x:
+                cnt[logs[left][0]-1] -= 1
+                if cnt[logs[left][0]-1] == 0:
+                    curr -= 1
+                left += 1
+            result[i] = n-curr
+        return result
+
+
+# Time:  O(nlogn + mlogm)
+# Space: O(n + m)
+# sort, line sweep
+class Solution2(object):
     def countServers(self, n, logs, x, queries):
         """
         :type n: int
@@ -17,12 +48,12 @@ class Solution(object):
             events.append((t+x+1, -1, sid-1))
         events.append((float("inf"), 0, 0))
         events.sort()
-      
+
         events2 = []
         for i, t in enumerate(queries):
             events2.append((t, i))
         events2.sort(reverse=True)
-      
+
         result = [0]*len(queries)
         cnt = [0]*n
         curr = 0
