9968cefcbecc04fff5091e4b02f9bb802d973b20
kamyu <kamyu104@users.noreply.github.com>
2023-07-07 19:22:38 +0800

    Create minimum-operations-to-make-the-integer-zero.py

---
+++ Python/minimum-operations-to-make-the-integer-zero.py
@@ -0,0 +1,24 @@
+# Time:  O(1)
+# Space: O(1)
+
+# linear search
+class Solution(object):
+    def makeTheIntegerZero(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        def popcount(x):
+            result = 0
+            while x:
+                x &= (x-1)
+                result += 1
+            return result
+
+        for i in xrange(1, 60+1):
+            if num1-i*num2 < 0:
+                break
+            if popcount(num1-i*num2) <= i <= num1-i*num2:
+                return i
+        return -1
