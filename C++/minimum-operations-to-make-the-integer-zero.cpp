290d4ad94d9727a5582adbee628250534a7d3643
kamyu <kamyu104@users.noreply.github.com>
2023-07-07 19:22:05 +0800

    Create minimum-operations-to-make-the-integer-zero.cpp

---
+++ C++/minimum-operations-to-make-the-integer-zero.cpp
@@ -0,0 +1,18 @@
+// Time:  O(1)
+// Space: O(1)
+
+// linear search
+class Solution {
+public:
+    int makeTheIntegerZero(int num1, int num2) {
+        for (int64_t i = 1; i <= 60; ++i) {
+            if (num1 - i * num2 < 0) {
+                break;
+            }
+            if (__builtin_popcountll(num1 - i * num2) <= i && i <= num1 - i * num2) {
+                return i;
+            }
+        }
+        return -1;
+    }
+};
