4286c26e8aa5efdb619036c84589c362220ffaf3
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 00:41:17 +0800

    Create count-houses-in-a-circular-street-ii.py

---
+++ Python/count-houses-in-a-circular-street-ii.py
@@ -0,0 +1,31 @@
+# Time:  O(n)
+# Space: O(1)
+
+# Definition for a street.
+class Street:
+    def closeDoor(self):
+        pass
+    def isDoorOpen(self):
+        pass
+    def moveRight(self):
+        pass
+
+
+# constructive algorithms
+class Solution(object):
+    def houseCount(self, street, k):
+        """
+        :type street: Street
+        :type k: int
+        :rtype: int
+        """
+        while not street.isDoorOpen():
+            street.moveRight()
+        street.moveRight()
+        result = 0
+        for i in xrange(1, k+1):
+            if street.isDoorOpen():
+                street.closeDoor()
+                result = i
+            street.moveRight()
+        return result
