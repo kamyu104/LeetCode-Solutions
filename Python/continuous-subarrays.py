b8e297189a5766b8aeefff78cb05d3c76ceff0c5
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 21:06:38 +0800

    Update continuous-subarrays.py

--- Python/continuous-subarrays.py
+++ Python/continuous-subarrays.py
@@ -28,3 +28,36 @@ class Solution(object):
                 left = max(left, min(mn[0], mx[0]))
             result += right-left+1
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+from sortedcontainers import SortedDict
+
+
+# ordered dict, two pointers
+class Solution2(object):
+    def continuousSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = left = 0;
+        lookup = SortedDict()
+        for right in xrange(len(nums)):
+            lookup[nums[right]] = right
+            to_del = []
+            for x, i in lookup.items():
+                if nums[right]-x <= 2:
+                    break
+                left = max(left, i+1)
+                to_del.append(x)
+            for x, i in reversed(lookup.items()):
+                if x-nums[right] <= 2:
+                    break
+                left = max(left, i+1)
+                to_del.append(x) 
+            for x in to_del:
+                del lookup[x]
+            result += right-left+1;
+        return result;
