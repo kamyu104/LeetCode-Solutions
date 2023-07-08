24a57df64eaf28e2473ba5761db3d072bed6f056
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 16:46:26 +0800

    Create query-batching.ts

---
+++ TypeScript/query-batching.ts
@@ -0,0 +1,37 @@
+// Time:  O(1)
+// Space: O(1)
+
+class QueryBatcher {
+    #queryMultiple : (keys: string[]) => Promise<string[]>
+    #t : number
+    #last : number
+    #pending : any[]
+
+    constructor(queryMultiple: (keys: string[]) => Promise<string[]>, t: number) {
+        this.#queryMultiple = queryMultiple;
+        this.#t = t;
+        this.#last = 0;
+        this.#pending = [];
+    }
+
+    async getValue(key: string): Promise<string> {
+        return new Promise((resolve) => {
+            const curr = Date.now();
+            const remain = Math.max((this.#t + this.#last) - curr, 0);
+            this.#last = curr + remain;
+            this.#pending.push({key, resolve});
+            if (this.#pending.length === 1) {
+                setTimeout(() => this.processPending(), remain);
+            }
+        });
+    }
+
+    async processPending() {
+        this.#last = Date.now();
+        const pending = this.#pending;
+        this.#pending = [];
+
+        const result = await this.#queryMultiple(pending.map((obj) => obj.key));
+        pending.map((obj, i) => obj.resolve(result[i]));
+    }
+};
