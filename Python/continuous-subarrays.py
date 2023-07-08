de1458bc7ee2e3cafc15453a06b40ea1fad27d47
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 21:34:15 +0800

    Update continuous-subarrays.py

--- Python/continuous-subarrays.py
+++ Python/continuous-subarrays.py
@@ -11,19 +11,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        result = 1
-        left = 0
-        mn, mx = nums[0]-2, nums[0]+2
-        for right in xrange(1, len(nums)):
+        result = left = 0
+        mn, mx = float("inf"), float("-inf")
+        for right in xrange(len(nums)):
             if mn <= nums[right] <= mx:
                 mn, mx = max(mn, nums[right]-2), min(mx, nums[right]+2)
             else:
                 mn, mx = nums[right]-2, nums[right]+2
                 for left in reversed(xrange(right)):
                     if not mn <= nums[left] <= mx:
+                        left += 1
                         break
                     mn, mx = max(mn, nums[left]-2), min(mx, nums[left]+2)
-                left += 1
             result += right-left+1
         return result
     
