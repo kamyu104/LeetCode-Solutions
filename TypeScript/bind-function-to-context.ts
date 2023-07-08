924e9d53901f14735741bf5249a933acf203bac3
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 14:36:02 +0800

    Create bind-function-to-context.ts

---
+++ TypeScript/bind-function-to-context.ts
@@ -0,0 +1,21 @@
+// Time:  O(1)
+// Space: O(1)
+
+// symbol
+type Fn = (...args) => any
+
+declare global {
+    interface Function {
+      bindPolyfill(obj: Record<any, any>): Fn;
+    }
+}
+
+Function.prototype.bindPolyfill = function(obj : any) {
+    const symbol = Symbol();
+    obj[symbol] = this;
+    return (...args) => {
+      const result = obj[symbol](...args);
+      delete obj[symbol];
+      return result;
+    }
+}
