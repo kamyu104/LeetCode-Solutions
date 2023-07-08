22457ff92fc193b3228761aef988a60499bb3e55
kamyu <kamyu104@users.noreply.github.com>
2023-07-09 00:22:44 +0800

    Update longest-even-odd-subarray-with-threshold.py

--- Python/longest-even-odd-subarray-with-threshold.py
+++ Python/longest-even-odd-subarray-with-threshold.py
@@ -1,1 +1,22 @@
+# Time:  O(n)
+# Space: O(1)
 
+# array
+class Solution(object):
+    def longestAlternatingSubarray(self, nums, threshold):
+        """
+        :type nums: List[int]
+        :type threshold: int
+        :rtype: int
+        """
+        result = l = 0
+        for x in nums:
+            if x > threshold:
+                l = 0
+                continue
+            if l%2 == x%2:
+                l += 1
+            else:
+                l = int(x%2 == 0)
+            result = max(result, l)
+        return result
