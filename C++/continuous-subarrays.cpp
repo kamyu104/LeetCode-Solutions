61d79a6605636fbfacfcff6a9391e26bd08ec9b9
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 21:47:25 +0800

    Update continuous-subarrays.cpp

--- C++/continuous-subarrays.cpp
+++ C++/continuous-subarrays.cpp
@@ -1,8 +1,39 @@
 // Time:  O(n)
-// Space: O(n)
+// Space: O(1)
 
-// mono deque, two pointers
+// two pointers
 class Solution {
+public:
+    long long continuousSubarrays(vector<int>& nums) {
+        int64_t result = 0;
+        int mn = numeric_limits<int>::max();
+        int mx = numeric_limits<int>::min();
+        for (int right = 0, left = 0; right < size(nums); ++right) {
+            if (mn <= nums[right] && nums[right] <= mx) {
+                mn = max(mn, nums[right] - 2);
+                mx = min(mx, nums[right] + 2);
+            } else {
+                mn = nums[right] - 2;
+                mx = nums[right] + 2;
+                for (left = right - 1; left >= 0; --left) {
+                    if (!(mn <= nums[left] && nums[left] <= mx)) {
+                        break;
+                    }
+                    mn = max(mn, nums[left] - 2);
+                    mx = min(mx, nums[left] + 2);
+                }
+                ++left;
+            }
+            result += right - left + 1;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// mono deque, two pointers
+class Solution2 {
 public:
     long long continuousSubarrays(vector<int>& nums) {
         deque<int> mn, mx; 
@@ -33,7 +64,7 @@ public:
 // Time:  O(nlogn)
 // Space: O(n)
 // bst, two pointers
-class Solution2 {
+class Solution3 {
 public:
     long long continuousSubarrays(vector<int>& nums) {
         int64_t result = 0;
