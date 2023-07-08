3e14658a62aaa7cbae11fa9676402bf7ed2e92b4
kamyu <kamyu104@users.noreply.github.com>
2023-07-08 15:20:39 +0800

    Create deep-merge-of-two-objects.ts

---
+++ TypeScript/deep-merge-of-two-objects.ts
@@ -0,0 +1,24 @@
+// Time:  O(n + m)
+// Space: O(h)
+
+// dfs
+function deepMerge(obj1: any, obj2: any): any {
+    if (obj1 === null ||
+        obj2 === null ||
+        typeof obj1 !== 'object' ||
+        typeof obj1 !== typeof obj2 ||
+        Array.isArray(obj1) !== Array.isArray(obj2)) {
+        return obj2;
+    }
+
+    var result = Array.isArray(obj1) ? [] : {};
+    Object.keys(obj1).forEach(key => {
+        if (obj2[key] === undefined) {
+            result[key] = obj1[key]; 
+        } else {
+            result[key] = deepMerge(obj1[key], obj2[key]);
+        }
+    });
+    Object.keys(obj2).forEach(key => !obj1[key] ? result[key] = obj2[key] : null);
+    return result;
+};
