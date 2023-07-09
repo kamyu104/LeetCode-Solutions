3279bcaf7c052e9929516378a795e38a6e2c2311
kamyu <kamyu104@users.noreply.github.com>
2023-07-10 00:51:56 +0800

    Create relocate-marbles.py

---
+++ Python/relocate-marbles.py
@@ -0,0 +1,17 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# hash table, sort
+class Solution(object):
+    def relocateMarbles(self, nums, moveFrom, moveTo):
+        """
+        :type nums: List[int]
+        :type moveFrom: List[int]
+        :type moveTo: List[int]
+        :rtype: List[int]
+        """
+        lookup = set(nums)
+        for a, b in itertools.izip(moveFrom, moveTo):
+            lookup.remove(a)
+            lookup.add(b)
+        return sorted(lookup)
