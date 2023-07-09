842595dfdd8e193e605670efb888f759490234a2
kamyu <kamyu104@users.noreply.github.com>
2023-07-10 00:37:13 +0800

    Create longest-alternating-subarray.py

---
+++ Python/longest-alternating-subarray.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def alternatingSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result, l = -1, -1
+        for i in xrange(len(nums)-1):
+            if l != -1 and nums[i-1] == nums[i+1]:
+                l += 1
+            else:
+                l = 2 if nums[i+1]-nums[i] == 1 else -1
+            result = max(result, l)
+        return result
