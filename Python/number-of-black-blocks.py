8d461dc5df5a8191e64b3cbb69ef264c792d2527
kamyu <kamyu104@users.noreply.github.com>
2023-07-10 10:37:16 +0800

    Create number-of-black-blocks.py

---
+++ Python/number-of-black-blocks.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def countBlackBlocks(self, m, n, coordinates):
+        """
+        :type m: int
+        :type n: int
+        :type coordinates: List[List[int]]
+        :rtype: List[int]
+        """
+        L = 2
+
+        cnt = collections.Counter()
+        for x, y in coordinates:
+            for nx in xrange(max(x-(L-1), 0), min(x+1, m-(L-1))):
+                for ny in xrange(max(y-(L-1), 0), min(y+1, n-(L-1))):
+                    cnt[nx, ny] += 1
+        result = [0]*(L**2+1)
+        for c in cnt.itervalues():
+            result[c] += 1
+        result[0] = (m-(L-1))*(n-(L-1))-sum(result[i] for i in xrange(1, len(result)))
+        return result
