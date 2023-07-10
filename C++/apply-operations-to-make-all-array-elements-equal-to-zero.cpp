9abe8a0adba85c94d6922cd748cac0d139f52bc4
kamyu <kamyu104@users.noreply.github.com>
2023-07-10 12:01:51 +0800

    Create apply-operations-to-make-all-array-elements-equal-to-zero.cpp

---
+++ C++/apply-operations-to-make-all-array-elements-equal-to-zero.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+// sliding window
+class Solution {
+public:
+    bool checkArray(vector<int>& nums, int k) {
+        int curr = 0;
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] - curr < 0) {
+                return false;
+            }
+            nums[i] -= curr;
+            curr += nums[i];
+            if (i - (k - 1) >= 0) {
+                curr -= nums[i - (k - 1)];
+            }
+        }
+        return curr == 0;
+    }
+};
