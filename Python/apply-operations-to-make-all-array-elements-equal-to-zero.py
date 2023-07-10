b99222d142dac6766eb1431aeee0b8f12246c176
kamyu <kamyu104@users.noreply.github.com>
2023-07-10 12:02:28 +0800

    Create apply-operations-to-make-all-array-elements-equal-to-zero.py

---
+++ Python/apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(1)
+
+# sliding window
+class Solution(object):
+    def checkArray(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: bool
+        """
+        curr = 0
+        for i, x in enumerate(nums):
+            if x-curr < 0:
+                return False
+            nums[i] -= curr
+            curr += nums[i]
+            if i-(k-1) >= 0:
+                curr -= nums[i-(k-1)]
+        return curr == 0
