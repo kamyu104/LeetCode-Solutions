889ac5f925eab519eb676c6741f5a8162891f172
kamyu <kamyu104@users.noreply.github.com>
2023-07-17 20:35:40 +0800

    Create minimum-index-of-a-valid-split.py

---
+++ Python/minimum-index-of-a-valid-split.py
@@ -0,0 +1,30 @@
+# Time:  O(n)
+# Space: O(1)
+
+# boyer-moore algorithm, linear search
+class Solution(object):
+    def minimumIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def boyer_moore_majority_vote():
+            m, cnt = nums[0], 1
+            for i in xrange(1, len(nums)):
+                if m == nums[i]:
+                    cnt += 1
+                else:
+                    cnt -= 1
+                    if cnt == 0:
+                        m = nums[i]
+                        cnt = 1
+            return m
+        
+        m = boyer_moore_majority_vote()
+        total, cnt = nums.count(m), 0
+        for i, x in enumerate(nums):
+            if x == m:
+                cnt += 1
+            if cnt*2 > i+1 and (total-cnt)*2 > len(nums)-(i+1):
+                return i
+        return -1
