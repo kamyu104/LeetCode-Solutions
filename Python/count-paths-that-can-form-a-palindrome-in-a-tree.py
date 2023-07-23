ff70ff19385f2ee5ae5218453c821007a65d0b16
kamyu <kamyu104@users.noreply.github.com>
2023-07-24 02:58:19 +0800

    Create count-paths-that-can-form-a-palindrome-in-a-tree.py

---
+++ Python/count-paths-that-can-form-a-palindrome-in-a-tree.py
@@ -0,0 +1,63 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# iterative dfs
+class Solution(object):
+    def countPalindromePaths(self, parent, s):
+        """
+        :type parent: List[int]
+        :type s: str
+        :rtype: int
+        """
+        def iter_dfs():
+            result = 0
+            cnt = collections.defaultdict(int)
+            cnt[0] = 1
+            stk = [(0, 0)]
+            while stk:
+                u, mask = stk.pop()
+                if u:
+                    mask ^= 1<<(ord(s[u])-ord('a'))
+                    result += cnt[mask]+sum(cnt[mask^(1<<i)] for i in xrange(26))
+                    cnt[mask] += 1
+                for v in reversed(adj[u]):
+                    stk.append((v, mask))
+            return result
+
+        adj = [[] for _ in xrange(len(parent))]
+        for u, p in enumerate(parent):
+            if p != -1:
+                adj[p].append(u)
+        return iter_dfs()
+
+
+# Time:  O(n)
+# Space: O(n)
+import collections
+
+# dfs
+class Solution2(object):
+    def countPalindromePaths(self, parent, s):
+        """
+        :type parent: List[int]
+        :type s: str
+        :rtype: int
+        """
+        def dfs(u, mask, cnt):
+            result = 0
+            if u:
+                mask ^= 1<<(ord(s[u])-ord('a'))
+                result += cnt[mask]+sum(cnt[mask^(1<<i)] for i in xrange(26))
+                cnt[mask] += 1
+            return result+sum(dfs(v, mask, cnt) for v in adj[u])
+
+        adj = [[] for _ in xrange(len(parent))]
+        for u, p in enumerate(parent):
+            if p != -1:
+                adj[p].append(u)
+        cnt = collections.defaultdict(int)
+        cnt[0] = 1
+        return dfs(0, 0, cnt)
