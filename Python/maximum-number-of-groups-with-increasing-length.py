e15c636acd69601caafb0e77d24e3b3ab96cb6aa
kamyu <kamyu104@users.noreply.github.com>
2023-07-24 04:18:52 +0800

    Update maximum-number-of-groups-with-increasing-length.py

--- Python/maximum-number-of-groups-with-increasing-length.py
+++ Python/maximum-number-of-groups-with-increasing-length.py
@@ -54,3 +54,32 @@ class Solution2(object):
                 curr -= result+1
                 result += 1
         return result
+
+    
+# Time:  O(nlogn)
+# Space: O(n)
+# constructive algorithms, sort, binary search, greedy
+class Solution3(object):
+    def maxIncreasingGroups(self, usageLimits):
+        """
+        :type usageLimits: List[int]
+        :rtype: int
+        """
+        def check(l):
+            curr = 0
+            for i in xrange(l):
+                curr += usageLimits[~i]-(l-i)
+                curr = min(curr, 0)
+            for i in xrange(len(usageLimits)-l):
+                curr += usageLimits[i]
+            return curr >= 0
+
+        usageLimits.sort()
+        left, right = 1, len(usageLimits)
+        while left <= right:
+            mid = left + (right-left)//2
+            if not check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return right
