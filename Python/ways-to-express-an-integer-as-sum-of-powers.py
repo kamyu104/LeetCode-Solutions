47711e484b8bae3e1de5612299d41dc66221940d
kamyu <kamyu104@users.noreply.github.com>
2023-07-23 23:57:42 +0800

    Create ways-to-express-an-integer-as-sum-of-powers.py

---
+++ Python/ways-to-express-an-integer-as-sum-of-powers.py
@@ -0,0 +1,22 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# dp
+class Solution(object):
+    def numberOfWays(self, n, x):
+        """
+        :type n: int
+        :type x: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+
+        dp = [0]*(n+1)
+        dp[0] = 1
+        for i in xrange(1, n+1):
+            i_pow_x = i**x
+            if i_pow_x > n:
+                break
+            for j in reversed(xrange(i_pow_x, n+1)):
+                dp[j] = (dp[j]+dp[j-i_pow_x])%MOD
+        return dp[-1]
