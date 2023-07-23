c0ee04d002e47a1af35d6e720bdee5cdbc7c7c0f
kamyu <kamyu104@users.noreply.github.com>
2023-07-23 23:41:15 +0800

    Create visit-array-positions-to-maximize-score.cpp

---
+++ C++/visit-array-positions-to-maximize-score.cpp
@@ -0,0 +1,18 @@
+// Time:  O(n)
+// Space: O(1)
+
+// dp
+class Solution {
+public:
+    long long maxScore(vector<int>& nums, int x) {
+        static const int64_t MIN = numeric_limits<int64_t>::min();
+        vector<int64_t> dp(2, MIN);
+        dp[nums[0] % 2] = nums[0];
+        for (int i = 1; i < size(nums); ++i) {
+            dp[nums[i] % 2] = dp[(nums[i] + 1) % 2] != MIN
+                              ? max(dp[nums[i] % 2], dp[(nums[i] + 1) % 2] - x) + nums[i]
+                              : dp[nums[i] % 2] + nums[i];
+        }
+        return max(dp[0], dp[1]);
+    }
+};
