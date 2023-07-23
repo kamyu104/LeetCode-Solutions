36f57b84d0b8afdf93678cd5e721e86a4089cad5
kamyu <kamyu104@users.noreply.github.com>
2023-07-23 23:57:12 +0800

    Create ways-to-express-an-integer-as-sum-of-powers.cpp

---
+++ C++/ways-to-express-an-integer-as-sum-of-powers.cpp
@@ -0,0 +1,23 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// dp
+class Solution {
+public:
+    int numberOfWays(int n, int x) {
+        static const int MOD = 1e9 + 7;
+
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        for (int i = 1; i <= n; ++i) {
+            const int i_pow_x = pow(i, x);
+            if (i_pow_x > n) {
+                break;
+            }
+            for (int j = n; j >= i_pow_x; --j) {
+                dp[j] = (dp[j] + dp[j - i_pow_x]) % MOD;
+            }
+        }
+        return dp.back();
+    }
+};
