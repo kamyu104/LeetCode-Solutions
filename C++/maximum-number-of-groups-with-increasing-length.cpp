717b614c815dc775b24a8c131ff8f833a47887db
kamyu <kamyu104@users.noreply.github.com>
2023-07-24 01:54:17 +0800

    Create maximum-number-of-groups-with-increasing-length.cpp

---
+++ C++/maximum-number-of-groups-with-increasing-length.cpp
@@ -0,0 +1,20 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// sort, greedy
+class Solution {
+public:
+    int maxIncreasingGroups(vector<int>& usageLimits) {
+        sort(begin(usageLimits), end(usageLimits));
+        int result = 0;
+        int64_t curr = 0;
+        for (const auto& x : usageLimits) {
+            curr += x;
+            if (curr >= result + 1) {
+                curr -= result + 1;
+                ++result;
+            }
+        }
+        return result;
+    }
+};
