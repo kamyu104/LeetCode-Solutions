00d1c81a1e8d7b8c6415fbad1889f5f6c3e15273
kamyu <kamyu104@users.noreply.github.com>
2023-08-01 06:35:18 +0800

    Update status-of-flight-tickets.sql

--- MySQL/status-of-flight-tickets.sql
+++ MySQL/status-of-flight-tickets.sql
@@ -2,9 +2,9 @@
 # Space: O(n + m)
 
 WITH rank_cte AS (
-SELECT passenger_id, capacity,
-       RANK() OVER (PARTITION BY a.flight_id ORDER BY booking_time) AS rnk
-FROM Passengers a LEFT JOIN Flights b ON a.flight_id = b.flight_id
+    SELECT passenger_id, capacity,
+           RANK() OVER (PARTITION BY a.flight_id ORDER BY booking_time) AS rnk
+    FROM Passengers a LEFT JOIN Flights b ON a.flight_id = b.flight_id
 )
 
 SELECT passenger_id,
