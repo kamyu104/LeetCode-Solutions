52bfe00fd7ca22f98282b13265238c7b9c4802f4
kamyu <kamyu104@users.noreply.github.com>
2023-08-01 02:43:57 +0800

    Create repeat-string.ts

---
+++ TypeScript/repeat-string.ts
@@ -0,0 +1,25 @@
+// Time:  O(n * t)
+// Space: O(n * t)
+
+declare global {
+    interface String {
+        replicate(times: number): string;
+    }
+}
+
+String.prototype.replicate = function(times: number) {
+    let result = "";
+    for (let curr = this; times !== 0; times >>= 1) {
+        if (times & 1) {
+            result += curr;
+        }
+        curr += curr;
+    }
+    return result;
+}
+
+// Time:  O(n * t)
+// Space: O(n * t)
+String.prototype.replicate = function(times: number) {
+    return new Array(times).fill(this).join('');
+}
