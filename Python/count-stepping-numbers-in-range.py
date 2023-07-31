db6775cd75c1eb4f797ea286c7f5229df98aa7b7
kamyu <kamyu104@users.noreply.github.com>
2023-08-01 06:13:47 +0800

    Create count-stepping-numbers-in-range.py

---
+++ Python/count-stepping-numbers-in-range.py
@@ -0,0 +1,32 @@
+# Time:  O(n)
+# Space: O(1)
+
+# dp
+class Solution(object):
+    def countSteppingNumbers(self, low, high):
+        """
+        :type low: str
+        :type high: str
+        :rtype: int
+        """
+        MOD = 10**9+7
+        def f(s):
+            dp = [[0]*10 for _ in xrange(2)]
+            for j in xrange(1, ord(s[0])-ord('0')+1):
+                dp[0][j] = 1
+            prefix = True
+            for i in xrange(1, len(s)):
+                for j in xrange(10):
+                    dp[i%2][j] = int(j != 0)
+                    if j-1 >= 0:
+                        dp[i%2][j] = (dp[i%2][j]+(dp[(i-1)%2][j-1]-int(prefix and (ord(s[i-1])-ord('0')) == j-1 and j > (ord(s[i])-ord('0')))))%MOD
+                    if j+1 < 10:
+                        dp[i%2][j] = (dp[i%2][j]+(dp[(i-1)%2][j+1]-int(prefix and (ord(s[i-1])-ord('0')) == j+1 and j > (ord(s[i])-ord('0')))))%MOD
+                if abs(ord(s[i])-ord(s[i-1])) != 1:
+                    prefix = False
+            return reduce(lambda x, y: (x+y)%MOD, dp[(len(s)-1)%2])
+
+        def check(a):
+            return all(abs(ord(a[i+1])-ord(a[i])) == 1 for i in xrange(len(a)-1))
+
+        return (f(high)-f(low)+check(low))%MOD
