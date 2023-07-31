0ca1e6e38a4a75d3099f3abccd01b5b8a8c2e1f5
kamyu <kamyu104@users.noreply.github.com>
2023-08-01 02:06:31 +0800

    Update create-object-from-two-arrays.ts

--- TypeScript/create-object-from-two-arrays.ts
+++ TypeScript/create-object-from-two-arrays.ts
@@ -2,11 +2,11 @@
 // Space: O(1)
 
 function createObject(keysArr: any[], valuesArr: any[]): Record<string, any> {
-    let obj: Record<string, any> = {};
+    let result: Record<string, any> = {};
     keysArr.forEach((key, index) => {
-        if (obj[key] === undefined) {
-            obj[key] = valuesArr[index];
+        if (result[key] === undefined) {
+            result[key] = valuesArr[index];
         }
     });
-    return obj;
+    return result;
 };
