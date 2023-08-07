076644ca6cae73671e9468d86851c370339d8e5e
kamyu <kamyu104@users.noreply.github.com>
2023-08-08 02:44:30 +0800

    Create account-balance-after-rounded-purchase.cpp

---
+++ C++/account-balance-after-rounded-purchase.cpp
@@ -0,0 +1,10 @@
+// Time:  O(1)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int accountBalanceAfterPurchase(int purchaseAmount) {
+        return 100 - (purchaseAmount + 5) / 10 * 10;
+    }
+};
