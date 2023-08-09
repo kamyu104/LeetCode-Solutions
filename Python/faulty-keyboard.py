43f383b4898da102854abc24290784e3722a7b72
kamyu <kamyu104@users.noreply.github.com>
2023-08-09 19:04:40 +0800

    Create faulty-keyboard.py

---
+++ Python/faulty-keyboard.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(n)
+
+# deque
+class Solution(object):
+    def finalString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        dq = collections.deque()
+        parity = 0
+        for x in s:
+            if x == 'i':
+                parity ^= 1
+            else:
+                dq.appendleft(x) if parity else dq.append(x)
+        if parity:
+            dq.reverse()
+        return "".join(dq)
