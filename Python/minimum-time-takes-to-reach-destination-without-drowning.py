64a85f62ee997d6dbf5187be8228aff712b761b7
kamyu <kamyu104@users.noreply.github.com>
2023-08-10 22:52:43 +0800

    Update minimum-time-takes-to-reach-destination-without-drowning.py

--- Python/minimum-time-takes-to-reach-destination-without-drowning.py
+++ Python/minimum-time-takes-to-reach-destination-without-drowning.py
@@ -9,7 +9,7 @@ class Solution(object):
         :rtype: int
         """
         DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        lookup = [[0 if land[i][j] != "*" else -1 for j in xrange(len(land[0]))] for i in xrange(len(land))]
+        lookup = [[-1 if land[i][j] == "*" else 0 for j in xrange(len(land[0]))] for i in xrange(len(land))]
         q = [(i, j, -1) for i in xrange(len(land)) for j in xrange(len(land[0])) if land[i][j] == "*"]
         q.append(next((i, j, 0) for i in xrange(len(land)) for j in xrange(len(land[0])) if land[i][j] == "S"))
         while q:
@@ -41,26 +41,28 @@ class Solution2(object):
         :rtype: int
         """
         DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
-        lookup = [[0 if land[i][j] != "*" else -1 for j in xrange(len(land[0]))] for i in xrange(len(land))]
+        lookup1 = [[0 if land[i][j] == "*" else -1 for j in xrange(len(land[0]))] for i in xrange(len(land))]
+        lookup2 = [[-1]*len(land[0]) for _ in xrange(len(land))]
         q1 = [(i, j) for i in xrange(len(land)) for j in xrange(len(land[0])) if land[i][j] == "*"]
-        q2 = [next((i, j, 0) for i in xrange(len(land)) for j in xrange(len(land[0])) if land[i][j] == "S")]
+        q2 = [next((i, j) for i in xrange(len(land)) for j in xrange(len(land[0])) if land[i][j] == "S")]
+        lookup2[q2[0][0]][q2[0][1]] = 0
         while q1 or q2:
             new_q1, new_q2 = [], []
             for i, j in q1:
                 for di, dj in DIRECTIONS:
                     ni, nj = i+di, j+dj
-                    if not (0 <= ni < len(land) and 0 <= nj < len(land[0]) and land[ni][nj] != "X" and land[ni][nj] != "D" and lookup[ni][nj] != -1):
+                    if not (0 <= ni < len(land) and 0 <= nj < len(land[0]) and land[ni][nj] != "X" and land[ni][nj] != "D" and lookup1[ni][nj] == -1):
                         continue
-                    lookup[ni][nj] = -1
+                    lookup1[ni][nj] = 0
                     new_q1.append((ni, nj))
-            for i, j, d in q2:
+            for i, j in q2:
                 if land[i][j] == "D":
-                    return d
+                    return lookup2[i][j]
                 for di, dj in DIRECTIONS:
                     ni, nj = i+di, j+dj
-                    if not (0 <= ni < len(land) and 0 <= nj < len(land[0]) and land[ni][nj] != "X" and lookup[ni][nj] == 0):
+                    if not (0 <= ni < len(land) and 0 <= nj < len(land[0]) and land[ni][nj] != "X" and lookup2[ni][nj] == lookup1[ni][nj] == -1):
                         continue
-                    lookup[ni][nj] = 1
-                    new_q2.append((ni, nj, d+1))
+                    lookup2[ni][nj] = lookup2[i][j]+1
+                    new_q2.append((ni, nj))
             q1, q2 = new_q1, new_q2
         return -1
