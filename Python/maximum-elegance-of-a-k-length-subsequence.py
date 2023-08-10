b090a577b8fe50b7f7800cf25e33f4939fcc86a1
kamyu <kamyu104@users.noreply.github.com>
2023-08-11 04:05:12 +0800

    Update maximum-elegance-of-a-k-length-subsequence.py

--- Python/maximum-elegance-of-a-k-length-subsequence.py
+++ Python/maximum-elegance-of-a-k-length-subsequence.py
@@ -1,7 +1,7 @@
 # Time:  O(nlogk)
 # Space: O(k)
 
-import collections
+import heapq
 from sortedcontainers import SortedList
 
 
@@ -43,10 +43,75 @@ class Solution(object):
         return result
 
 
+# Time:  O(n + klogk)
+# Space: O(n)
+import random
+import collections
+
+# quick select, sort, greedy
+class Solution2(object):
+    def findMaximumElegance(self, items, k):
+        """
+        :type items: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def nth_element(nums, n, left=0, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+            
+            right = len(nums)-1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+    
+        def nlargest(k, nums):
+            nth_element(nums, k-1, compare=lambda a, b: a > b)
+            return sorted(nums[:k], reverse=True)
+    
+        curr = 0
+        lookup = set()
+        stk = []
+        for p, c in nlargest(k, items):
+            if c in lookup:
+                stk.append(p)
+            curr += p
+            lookup.add(c)
+        lookup2 = collections.defaultdict(int)
+        for p, c in items:
+            if c in lookup:
+                continue
+            lookup2[c] = max(lookup2[c], p)
+        l = len(lookup)
+        result = curr+l**2
+        for p in nlargest(len(stk), lookup2.values()):
+            curr += p-stk.pop()
+            l += 1
+            result = max(result, curr+l**2) 
+        return result
+
+
 # Time:  O(nlogn)
 # Space: O(k)
 # sort, greedy
-class Solution2(object):
+class Solution3(object):
     def findMaximumElegance(self, items, k):
         """
         :type items: List[List[int]]
