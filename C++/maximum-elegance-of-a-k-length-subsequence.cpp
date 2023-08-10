c565b4b5d7fa578f7bc29f783868d90631fd5a35
kamyu <kamyu104@users.noreply.github.com>
2023-08-11 00:59:45 +0800

    Update maximum-elegance-of-a-k-length-subsequence.cpp

--- C++/maximum-elegance-of-a-k-length-subsequence.cpp
+++ C++/maximum-elegance-of-a-k-length-subsequence.cpp
@@ -59,21 +59,26 @@ public:
         sort(rbegin(items), rend(items));
         vector<bool> lookup(size(items));
         vector<int> stk;
-        int64_t result = 0, curr = 0, l = 0;
-        for (int i = 0; i < size(items); ++i) {
-            if (i < k) {
-                if (lookup[items[i][1] - 1]) {
-                    stk.emplace_back(items[i][0]);
-                }
-                curr += items[i][0];
-            } else {
-                if (!lookup[items[i][1] - 1]) {
-                    if (empty(stk)) {
-                        break;
-                    }
-                    curr += items[i][0] - stk.back(); stk.pop_back();
-                }
+        int64_t curr = 0, l = 0;
+        for (int i = 0; i < k; ++i) {
+            if (lookup[items[i][1] - 1]) {
+                stk.emplace_back(items[i][0]);
             }
+            curr += items[i][0];
+            if (!lookup[items[i][1] - 1]) {
+                lookup[items[i][1] - 1] = true;
+                ++l;
+            }
+        }
+        int64_t result = curr + l * l;
+        for (int i = k; i < size(items); ++i) {
+            if (lookup[items[i][1] - 1]) {
+                continue;
+            }
+            if (empty(stk)) {
+                break;
+            }
+            curr += items[i][0] - stk.back(); stk.pop_back();
             if (!lookup[items[i][1] - 1]) {
                 lookup[items[i][1] - 1] = true;
                 ++l;
