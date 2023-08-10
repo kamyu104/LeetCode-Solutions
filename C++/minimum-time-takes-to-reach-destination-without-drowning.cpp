202828bad6fcb28b0672c56a700d2c965c3e7fb6
kamyu <kamyu104@users.noreply.github.com>
2023-08-10 22:57:01 +0800

    Update minimum-time-takes-to-reach-destination-without-drowning.cpp

--- C++/minimum-time-takes-to-reach-destination-without-drowning.cpp
+++ C++/minimum-time-takes-to-reach-destination-without-drowning.cpp
@@ -53,43 +53,45 @@ class Solution2 {
 public:
     int minimumSeconds(vector<vector<string>>& land) {
         static const vector<pair<int, int>> DIRECTIONS {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        vector<vector<int>> lookup(size(land), vector<int>(size(land[0])));
+        vector<vector<int>> lookup1(size(land), vector<int>(size(land[0]), -1));
+        vector<vector<int>> lookup2(size(land), vector<int>(size(land[0]), -1));
         vector<pair<int, int>> q1;
-        vector<tuple<int, int, int>> q2;
+        vector<pair<int, int>> q2;
         for (int i = 0; i < size(land); ++i) {
             for (int j = 0; j < size(land[0]); ++j) {
                 if (land[i][j] == "*") {
                     q1.emplace_back(i, j);
-                    lookup[i][j] = -1;
+                    lookup2[i][j] = 0;
                 } else if (land[i][j] == "S") {
-                    q2.emplace_back(i, j, 0);
+                    q2.emplace_back(i, j);
+                    lookup2[i][j] = 0;
                 }
             }
         }
         while (!empty(q1) || !empty(q2)) {
             vector<pair<int, int>> new_q1;
-            vector<tuple<int, int, int>> new_q2;
+            vector<pair<int, int>> new_q2;
             for (const auto& [i, j] : q1) {
                 for (const auto& [di, dj] : DIRECTIONS) {
                     const int ni = i + di, nj = j + dj;
-                    if (!(0 <= ni && ni < size(land) && 0 <= nj && nj < size(land[0]) && land[ni][nj] != "X" && land[ni][nj] != "D" && lookup[ni][nj] != -1)) {
+                    if (!(0 <= ni && ni < size(land) && 0 <= nj && nj < size(land[0]) && land[ni][nj] != "X" && land[ni][nj] != "D" && lookup1[ni][nj] == -1)) {
                         continue;
                     }
-                    lookup[ni][nj] = -1;
+                    lookup1[ni][nj] = 0;
                     new_q1.emplace_back(ni, nj);
                 }
             }
-            for (const auto& [i, j, d] : q2) {
+            for (const auto& [i, j] : q2) {
                 if (land[i][j] == "D") {
-                    return d;
+                    return lookup2[i][j];
                 }
                 for (const auto& [di, dj] : DIRECTIONS) {
                     const int ni = i + di, nj = j + dj;
-                    if (!(0 <= ni && ni < size(land) && 0 <= nj && nj < size(land[0]) && land[ni][nj] != "X" && lookup[ni][nj] == 0)) {
+                    if (!(0 <= ni && ni < size(land) && 0 <= nj && nj < size(land[0]) && land[ni][nj] != "X" && lookup2[ni][nj] == -1 && lookup1[ni][nj] == -1)) {
                         continue;
                     }
-                    lookup[ni][nj] = 1;
-                    new_q2.emplace_back(ni, nj, d + 1);
+                    lookup2[ni][nj] = lookup2[i][j] + 1;
+                    new_q2.emplace_back(ni, nj);
                 }
             }
             q1 = move(new_q1);
