bd0d0fddec690995cb81b38051a2dbb401774e2c
kamyu <kamyu104@users.noreply.github.com>
2023-08-16 17:48:25 +0800

    Create minimum-absolute-difference-between-elements-with-constraint.cpp

---
+++ C++/minimum-absolute-difference-between-elements-with-constraint.cpp
@@ -0,0 +1,22 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// bst, binary search
+class Solution {
+public:
+    int minAbsoluteDifference(vector<int>& nums, int x) {
+        int result = numeric_limits<int>::max();
+        set<int> bst;
+        for (int i = x; i < size(nums); ++i) {
+            bst.emplace(nums[i - x]);
+            const auto it = bst.upper_bound(nums[i]);
+            if (it != begin(bst)) {
+                result = min(result, nums[i] - *prev(it));
+            } 
+            if (it != end(bst)) {
+                result = min(result, *it - nums[i]);
+            }
+        }
+        return result;
+    }
+};
