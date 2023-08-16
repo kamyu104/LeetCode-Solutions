b40c0c236c374262acb64dd7085d0aaa00e85795
kamyu <kamyu104@users.noreply.github.com>
2023-08-16 17:48:49 +0800

    Create minimum-absolute-difference-between-elements-with-constraint.py

---
+++ Python/minimum-absolute-difference-between-elements-with-constraint.py
@@ -0,0 +1,24 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+from sortedcontainers import SortedList
+
+
+# sorted list, binary search
+class Solution(object):
+    def minAbsoluteDifference(self, nums, x):
+        """
+        :type nums: List[int]
+        :type x: int
+        :rtype: int
+        """
+        result = float("inf")
+        sl = SortedList()
+        for i in xrange(x, len(nums)):
+            sl.add(nums[i-x])
+            j = sl.bisect_right(nums[i])
+            if j >= 1:
+                result = min(result, nums[i]-sl[j-1])
+            if j < len(sl):
+                result = min(result, sl[j]-nums[i])
+        return result
