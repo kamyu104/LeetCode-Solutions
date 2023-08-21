cf2b862b8e87b8a14a2c987cffc3dfe5ba9ea4b9
kamyu <kamyu104@users.noreply.github.com>
2023-08-21 21:28:12 +0800

    Update minimum-relative-loss-after-buying-chocolates.cpp

--- C++/minimum-relative-loss-after-buying-chocolates.cpp
+++ C++/minimum-relative-loss-after-buying-chocolates.cpp
@@ -7,19 +7,19 @@ public:
     vector<long long> minimumRelativeLosses(vector<int>& prices, vector<vector<int>>& queries) {
         const auto& binary_search = [&](int k, int m) {
             const auto& check = [&](int x) {
-                return k - (prices[size(prices) - x] - k) <= prices[((m - x) + 1) - 1] - 0;
+                return k - (prices[size(prices) - (m - x)] - k) <= prices[(x + 1) - 1] - 0;
             };
 
-            int left = 1, right = m;
+            int left = 0, right = m - 1;
             while (left <= right) {
                 const auto& mid = left + (right - left) / 2;
-                if (!check(mid)) {
+                if (check(mid)) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
             }
-            return right;
+            return left;
         };
 
         sort(begin(prices), end(prices));
@@ -31,8 +31,8 @@ public:
         for (int i = 0; i < size(queries); ++i) {
             const auto& k = queries[i][0], &m = queries[i][1];
             const long long cnt = binary_search(k, m);
-            const auto& a = prefix[size(prices)] - prefix[size(prices) - cnt] - cnt * k;
-            const auto& b = prefix[m - cnt] + cnt * k;
+            const auto& a = prefix[size(prices)] - prefix[size(prices) - (m - cnt)] - (m - cnt) * k;
+            const auto& b = prefix[cnt] + (m - cnt) * k;
             result[i] = b - a;
         }
         return result;
