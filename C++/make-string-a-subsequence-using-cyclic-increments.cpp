798a040106f4e44b1864bea09a6109dedcacebba
kamyu <kamyu104@users.noreply.github.com>
2023-08-23 02:00:48 +0800

    Create make-string-a-subsequence-using-cyclic-increments.cpp

---
+++ C++/make-string-a-subsequence-using-cyclic-increments.cpp
@@ -0,0 +1,19 @@
+// Time:  O(n)
+// Space: O(1)
+
+// two pointers
+class Solution {
+public:
+    bool canMakeSubsequence(string str1, string str2) {
+        int i = 0;
+        for (const auto& c : str1) {
+            if (((str2[i] - c) % 26 + 26) % 26 > 1) {
+                continue;
+            }
+            if (++i == size(str2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
