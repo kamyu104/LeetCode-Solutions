852a62f931aba6e4927502f75c177f1ec7b2ea1d
kamyu <kamyu104@users.noreply.github.com>
2023-08-23 01:32:42 +0800

    Create count-pairs-whose-sum-is-less-than-target.cpp

---
+++ C++/count-pairs-whose-sum-is-less-than-target.cpp
@@ -0,0 +1,21 @@
+// Time:  O(nlogn)
+// Space: O(n)
+
+// sort, two pointers
+class Solution {
+public:
+    int countPairs(vector<int>& nums, int target) {
+        sort(begin(nums), end(nums));
+        int result = 0;
+        int left = 0, right = size(nums) - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] < target) {
+                result += right - left;
+                ++left;
+            } else {
+                --right;
+            }
+        }
+        return result;
+    }
+};
