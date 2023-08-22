799fada55d71bbc391b39bb6a5f3942bee4caa1a
kamyu <kamyu104@users.noreply.github.com>
2023-08-23 01:33:07 +0800

    Create count-pairs-whose-sum-is-less-than-target.py

---
+++ Python/count-pairs-whose-sum-is-less-than-target.py
@@ -0,0 +1,21 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# sort, two pointers
+class Solution(object):
+    def countPairs(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        result = 0
+        left, right = 0, len(nums)-1
+        while left < right:
+            if nums[left]+nums[right] < target:
+                result += right-left
+                left += 1
+            else:
+                right -= 1
+        return result
