38d050404944431cafab3d17c368814ec0db9624
kamyu <kamyu104@users.noreply.github.com>
2023-08-24 15:02:51 +0800

    Update number-of-beautiful-integers-in-the-range.py

--- Python/number-of-beautiful-integers-in-the-range.py
+++ Python/number-of-beautiful-integers-in-the-range.py
@@ -11,16 +11,16 @@ class Solution(object):
         :rtype: int
         """
         def f(x):
-            arr = map(int, str(x))
-            lookup = [[[[[-1]*k for _ in xrange(2*len(arr)+1)] for _ in xrange(2)] for _ in xrange(2)] for _ in xrange(len(arr))]
+            digits = map(int, str(x))
+            lookup = [[[[[-1]*k for _ in xrange(2*len(digits)+1)] for _ in xrange(2)] for _ in xrange(2)] for _ in xrange(len(digits))]
             def memoization(i, zero, tight, diff, total):
-                if i == len(arr):
+                if i == len(digits):
                     return zero == diff == total == 0
                 if lookup[i][zero][tight][diff][total] == -1:
                     result = 0
-                    for d in xrange((arr[i] if tight else 9)+1):
+                    for d in xrange((digits[i] if tight else 9)+1):
                         new_zero = int(zero and d == 0)
-                        new_tight = int(tight and d == arr[i])
+                        new_tight = int(tight and d == digits[i])
                         new_diff = diff+((1 if d%2 == 0 else -1) if not new_zero else 0)
                         new_total = (total*10+d)%k
                         result += memoization(i+1, new_zero, new_tight, new_diff, new_total)
@@ -44,18 +44,18 @@ class Solution2(object):
         :rtype: int
         """
         def f(x):
-            arr = map(int, str(x))
-            dp = [[[[0]*k for _ in xrange(2*len(arr)+1)] for _ in xrange(2)] for _ in xrange(2)]
+            digits = map(int, str(x))
+            dp = [[[[0]*k for _ in xrange(2*len(digits)+1)] for _ in xrange(2)] for _ in xrange(2)]
             for tight in xrange(2):
                 dp[0][tight][0][0] = 1
-            for i in reversed(xrange(len(arr))):
-                new_dp = [[[[0]*k for _ in xrange(2*len(arr)+1)] for _ in xrange(2)] for _ in xrange(2)]
+            for i in reversed(xrange(len(digits))):
+                new_dp = [[[[0]*k for _ in xrange(2*len(digits)+1)] for _ in xrange(2)] for _ in xrange(2)]
                 for zero in xrange(2):
                     for tight in xrange(2):
-                        for d in xrange((arr[i] if tight else 9)+1):
+                        for d in xrange((digits[i] if tight else 9)+1):
                             new_zero = int(zero and d == 0)
-                            new_tight = int(tight and d == arr[i])
-                            for diff in xrange(-len(arr), len(arr)+1):
+                            new_tight = int(tight and d == digits[i])
+                            for diff in xrange(-len(digits), len(digits)+1):
                                 new_diff = diff+((1 if d%2 == 0 else -1) if not new_zero else 0)
                                 for total in xrange(k):
                                     new_total = (total*10+d)%k
