f58e90d3f067c4b1d51f5044bddc3d644e7f0d7e
kamyu <kamyu104@users.noreply.github.com>
2023-08-25 17:47:25 +0800

    Update maximize-the-profit-as-the-salesman.py

--- Python/maximize-the-profit-as-the-salesman.py
+++ Python/maximize-the-profit-as-the-salesman.py
@@ -14,7 +14,7 @@ class Solution(object):
             lookup[e].append([s, g])
         dp = [0]*(n+1)
         for e in xrange(n):
-            dp[e+1] = dp[e]
+            dp[e+1] = dp[(e-1)+1]
             for s, g in lookup[e]:
                 dp[e+1] = max(dp[e+1], dp[(s-1)+1]+g)
         return dp[-1]
