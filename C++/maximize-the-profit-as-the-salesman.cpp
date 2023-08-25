7054a5959109bc054fc91af90010b9ac30bd78fd
kamyu <kamyu104@users.noreply.github.com>
2023-08-25 17:45:47 +0800

    Create maximize-the-profit-as-the-salesman.cpp

---
+++ C++/maximize-the-profit-as-the-salesman.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n + m), m = len(offers)
+// Space: O(n + m)
+
+// dp
+class Solution {
+public:
+    int maximizeTheProfit(int n, vector<vector<int>>& offers) {
+        vector<vector<pair<int, int>>> lookup(n);
+        for (const auto& x : offers) {
+            lookup[x[1]].emplace_back(x[0], x[2]);
+        }
+        vector<int> dp(n + 1);
+        for (int e = 0; e < n; ++e) {
+            dp[e + 1] = dp[e];
+            for (const auto& [s, g] : lookup[e]) {
+                dp[e + 1] = max(dp[e + 1], dp[(s - 1) + 1] + g);
+            }
+        }
+        return dp.back();
+    }
+};
