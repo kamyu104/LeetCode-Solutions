7283cc6951da3a094bd2cecacb67ac3e1cc7410f
kamyu <kamyu104@users.noreply.github.com>
2023-08-25 17:46:27 +0800

    Create maximize-the-profit-as-the-salesman.py

---
+++ Python/maximize-the-profit-as-the-salesman.py
@@ -0,0 +1,20 @@
+# Time:  O(n + m), m = len(offers)
+# Space: O(n + m)
+
+# dp
+class Solution(object):
+    def maximizeTheProfit(self, n, offers):
+        """
+        :type n: int
+        :type offers: List[List[int]]
+        :rtype: int
+        """
+        lookup = [[] for _ in xrange(n)]
+        for s, e, g in offers:
+            lookup[e].append([s, g])
+        dp = [0]*(n+1)
+        for e in xrange(n):
+            dp[e+1] = dp[e]
+            for s, g in lookup[e]:
+                dp[e+1] = max(dp[e+1], dp[(s-1)+1]+g)
+        return dp[-1]
