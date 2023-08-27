7547f8f739fd3a4a79cae397ae856ef2319b2b59
kamyu <kamyu104@users.noreply.github.com>
2023-08-28 03:29:55 +0800

    Update minimum-operations-to-form-subsequence-with-target-sum.cpp

--- C++/minimum-operations-to-form-subsequence-with-target-sum.cpp
+++ C++/minimum-operations-to-form-subsequence-with-target-sum.cpp
@@ -1,9 +1,47 @@
-// Time:  O(nlogn)
-// Space: O(1)
+// Time:  O(n)
+// Space: O(logn)
 
 // codeforces, https://codeforces.com/problemset/problem/1303/D
 // sort, greedy
 class Solution {
+public:
+    int minOperations(vector<int>& nums, int target) {
+        const auto& floor_log2_x = [](int x) {
+            return 8 * sizeof(int) - __builtin_clz(x) - 1;
+        };
+
+        int64_t total = accumulate(begin(nums), end(nums), 0ll);
+        if (total < target) {
+            return -1;
+        }
+        vector<int> cnt(floor_log2_x(*max_element(cbegin(nums), cend(nums))) + 1);
+        for (const auto& x : nums) {
+            ++cnt[floor_log2_x(x)];
+        }
+        int result = 0;
+        for (int i = size(cnt) - 1; i >= 0; --i) {
+            const int x = 1 << i;
+            for (int _ = 0; _ < cnt[i]; ++_) { 
+                if (x <= target) {
+                    target -= x;
+                    total -= x;
+                } else if (total - x >= target) {
+                    total -= x;
+                } else {
+                    cnt[i - 1] += 2;
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+// codeforces, https://codeforces.com/problemset/problem/1303/D
+// sort, greedy
+class Solution2 {
 public:
     int minOperations(vector<int>& nums, int target) {
         int64_t total = accumulate(begin(nums), end(nums), 0ll);
@@ -33,7 +71,7 @@ public:
 // Space: O(n)
 // codeforces, https://codeforces.com/problemset/problem/1303/D
 // heap, greedy
-class Solution2 {
+class Solution3 {
 public:
     int minOperations(vector<int>& nums, int target) {
         int64_t total = accumulate(begin(nums), end(nums), 0ll);
@@ -63,7 +101,7 @@ public:
 // Space: O(logr)
 // codeforces, https://codeforces.com/problemset/problem/1303/D
 // bitmasks, greedy
-class Solution3 {
+class Solution4 {
 public:
     int minOperations(vector<int>& nums, int target) {
         const auto& floor_log2_x = [](int x) {
