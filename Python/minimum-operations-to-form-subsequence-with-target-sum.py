991fb83929003905c6c305769652a042c57bcd29
kamyu <kamyu104@users.noreply.github.com>
2023-08-28 01:19:49 +0800

    Update minimum-operations-to-form-subsequence-with-target-sum.py

--- Python/minimum-operations-to-form-subsequence-with-target-sum.py
+++ Python/minimum-operations-to-form-subsequence-with-target-sum.py
@@ -1,9 +1,46 @@
-# Time:  O(nlogr)
-# Space: O(logr)
+# Time:  O(nlogn)
+# Space: O(n)
+
+import heapq
+
 
 # codeforces, https://codeforces.com/problemset/problem/1303/D
-# bitmasks, greedy
+# heap, greedy
 class Solution(object):
+    def minOperations(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        total = sum(nums)
+        if total < target:
+            return -1
+
+        result = 0
+        max_heap = [-x for x in nums]
+        heapq.heapify(max_heap)
+        while heapq:
+            if not target:
+                break
+            x = -heapq.heappop(max_heap)
+            if x <= target:
+                target -= x
+                total -= x
+            elif total-x >= target:
+                total -= x
+            else:
+                heapq.heappush(max_heap, -x//2)
+                heapq.heappush(max_heap, -x//2)
+                result += 1
+        return result
+
+
+# Time:  O(nlogr)
+# Space: O(logr)
+# codeforces, https://codeforces.com/problemset/problem/1303/D
+# bitmasks, greedy
+class Solution2(object):
     def minOperations(self, nums, target):
         """
         :type nums: List[int]
