395a9192c29866569573c54a69c484a1a0195bde
kamyu <kamyu104@users.noreply.github.com>
2023-08-28 17:06:31 +0800

    Update maximize-value-of-function-in-a-ball-passing-game.cpp

--- C++/maximize-value-of-function-in-a-ball-passing-game.cpp
+++ C++/maximize-value-of-function-in-a-ball-passing-game.cpp
@@ -1,8 +1,103 @@
+// Time:  O(n)
+// Space: O(n)
+
+// graph, prefix sum, two pointers, sliding window
+class Solution {
+public:
+    long long getMaxFunctionValue(vector<int>& receiver, long long k) {
+        const auto& find_cycles = [](const auto& adj) {
+            vector<pair<int, int>> result;
+            vector<bool> lookup(size(adj));
+            for (int i = 0; i < size(adj); ++i) {
+                unordered_map<int, int> cnt;
+                int u = i;
+                while (!lookup[u]) {
+                    lookup[u] = true;
+                    cnt[u] = size(cnt);
+                    u = adj[u];
+                }
+                if (cnt.count(u)) {
+                    result.emplace_back(u, size(cnt) - cnt[u]);
+                }
+            }
+            return result;
+        };
+        
+        const auto& cycles = find_cycles(receiver);
+        vector<pair<int, int>> lookup(size(receiver), {-1, -1});
+        vector<vector<int64_t>> prefixes(size(cycles), vector<int64_t>(1));
+        const auto& find_prefixes = [&](const auto& cycles) {
+            for (int idx = 0; idx < size(cycles); ++idx) {
+                auto [u, l] = cycles[idx];
+                for (int i = 0; i < l; ++i) {
+                    lookup[u] = {idx, i};
+                    prefixes[idx].emplace_back(prefixes[idx][i] + u);
+                    u = receiver[u];
+                }
+            }
+        };
+        
+        const auto& get_sum = [](const auto& prefix, int64_t i, int64_t cnt) {
+            const int64_t l = size(prefix) - 1;
+            const int64_t q = cnt / l;
+            const int64_t r = cnt % l;
+            return (q * prefix.back() +
+                    (prefix[min(i + r, l)] - prefix[i]) +
+                    (prefix[max((i + r) - l, static_cast<int64_t>(0))] - prefix[0]));
+        };
+        
+        const auto& start_inside_cycle = [&]() {
+            int64_t result = 0;
+            for (auto [u, l] : cycles) {
+                for (int _ = 0; _ < l; ++_) {
+                    const auto& [idx, i] = lookup[u];
+                    result = max(result, get_sum(prefixes[idx], i, k + 1));
+                    u = receiver[u];
+                }
+            }
+            return result;
+        };
+    
+        const auto& start_outside_cycle = [&]() {
+            int64_t result = 0;
+            vector<int> degree(size(receiver));
+            for (const auto& x : receiver) {
+                ++degree[x];
+            }
+            for (int u = 0; u < size(receiver); ++u) {
+                if (degree[u]) {
+                    continue;
+                }
+                int64_t curr = 0;
+                deque<int> dq;
+                int v = u;
+                while (lookup[v].first == -1) {
+                    curr += v;
+                    dq.emplace_back(v);
+                    if (size(dq) == k + 1) {
+                        result = max(result, curr);
+                        curr -= dq.front(); dq.pop_front();
+                    }
+                    v = receiver[v];
+                }
+                const auto& [idx, i] = lookup[v];
+                while (!empty(dq)) {
+                    result = max(result, curr + get_sum(prefixes[idx], i, (k + 1) - size(dq)));
+                    curr -= dq.front(); dq.pop_front();
+                }
+            }
+            return result;
+        };
+              
+        find_prefixes(cycles);
+        return max(start_inside_cycle(), start_outside_cycle());
+    }
+};
+
 // Time:  O(nlogk)
 // Space: O(nlogk)
-
 // binary lifting
-class Solution {
+class Solution2 {
 public:
     long long getMaxFunctionValue(vector<int>& receiver, long long k) {
         const auto& bit_length = [](const auto& x) {
