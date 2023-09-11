f034b4aa840b1b3a6645fa36c770a8290dd526ce
kamyu <kamyu104@users.noreply.github.com>
2023-09-12 02:08:51 +0800

    Update check-if-strings-can-be-made-equal-with-operations-i.cpp

--- C++/check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ C++/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -5,16 +5,16 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-        vector<vector<int>> lookup1(2, vector<int>(26)), lookup2(2, vector<int>(26));
+        vector<vector<int>> cnt1(2, vector<int>(26)), cnt2(2, vector<int>(26));
         for (int i = 0; i < 2; ++i) {
             for (int j = i; j < size(s1); j += 2) {
-                ++lookup1[i][s1[j] - 'a'];
+                ++cnt1[i][s1[j] - 'a'];
             }
             for (int j = i; j < size(s1); j += 2) {
-                ++lookup2[i][s2[j] - 'a'];
+                ++cnt2[i][s2[j] - 'a'];
             }
         }
-        return lookup1 == lookup2;
+        return cnt1 == cnt2;
     }
 };
 
