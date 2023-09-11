7e73e2805b098c21682bd2537f0b5de56e98358f
kamyu <kamyu104@users.noreply.github.com>
2023-09-11 18:35:40 +0800

    Update maximum-coins-heroes-can-collect.py

--- Python/maximum-coins-heroes-can-collect.py
+++ Python/maximum-coins-heroes-can-collect.py
@@ -10,18 +10,18 @@ class Solution(object):
         :type coins: List[int]
         :rtype: List[int]
         """
-        idx1 = range(len(heroes))
-        idx1.sort(key=lambda x: heroes[x])
-        idx2 = range(len(monsters))
-        idx2.sort(key=lambda x: monsters[x])
-        result = [0]*len(idx1)
+        idxs1 = range(len(heroes))
+        idxs1.sort(key=lambda x: heroes[x])
+        idxs2 = range(len(monsters))
+        idxs2.sort(key=lambda x: monsters[x])
+        result = [0]*len(idxs1)
         i = curr = 0
-        for idx in idx1:
-            for i in xrange(i, len(idx2)):
-                if monsters[idx2[i]] > heroes[idx]:
+        for idx in idxs1:
+            for i in xrange(i, len(idxs2)):
+                if monsters[idxs2[i]] > heroes[idx]:
                     break
-                curr += coins[idx2[i]]
+                curr += coins[idxs2[i]]
             else:
-                i = len(idx2)
+                i = len(idxs2)
             result[idx] = curr
         return result
