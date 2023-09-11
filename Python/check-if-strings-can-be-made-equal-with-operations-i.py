caca25c47a5ca14b1c418060103af8fa6eec7e7e
kamyu <kamyu104@users.noreply.github.com>
2023-09-11 18:57:42 +0800

    Update check-if-strings-can-be-made-equal-with-operations-i.py

--- Python/check-if-strings-can-be-made-equal-with-operations-i.py
+++ Python/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -12,7 +12,7 @@ class Solution(object):
         :type s2: str
         :rtype: bool
         """
-        return  all(collections.Counter(s1[j] for j in xrange(i, len(s1), 2)) == collections.Counter(s2[j] for j in xrange(i, len(s2), 2)) for i in xrange(2))
+        return all(collections.Counter(s1[j] for j in xrange(i, len(s1), 2)) == collections.Counter(s2[j] for j in xrange(i, len(s2), 2)) for i in xrange(2))
 
 
 # Time:  O(1)
