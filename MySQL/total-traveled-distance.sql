23cc5d44c0d8b156e5b49ab33b604cce08a3c84e
kamyu <kamyu104@users.noreply.github.com>
2023-09-11 17:33:05 +0800

    Create total-traveled-distance.sql

---
+++ MySQL/total-traveled-distance.sql
@@ -0,0 +1,10 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT u.user_id,
+       u.name,
+       IFNULL(SUM(distance), 0) AS `traveled distance`
+FROM Users u
+LEFT JOIN Rides r ON u.user_id = r.user_id
+GROUP BY 1
+ORDER BY 1;
