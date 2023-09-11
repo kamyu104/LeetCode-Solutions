a04b7b5bb90f4a4f6371b0e485f4ed507d0648de
kamyu <kamyu104@users.noreply.github.com>
2023-09-11 18:57:05 +0800

    Create check-if-strings-can-be-made-equal-with-operations-i.py

---
+++ Python/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -0,0 +1,26 @@
+# Time:  O(1)
+# Space: O(1)
+
+# freq table
+class Solution(object):
+    def canBeEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return  all(collections.Counter(s1[j] for j in xrange(i, len(s1), 2)) == collections.Counter(s2[j] for j in xrange(i, len(s2), 2)) for i in xrange(2))
+
+
+# Time:  O(1)
+# Space: O(1)
+# brute forces
+class Solution2(object):
+    def canBeEqual(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        return (((s1[0] == s2[0] and s1[2] == s2[2]) or (s1[0] == s2[2] and s1[2] == s2[0])) and
+                ((s1[1] == s2[1] and s1[3] == s2[3]) or (s1[1] == s2[3] and s1[3] == s2[1])))
