f61fd35915b83e144c78f47d899d88981806148e
kamyu <kamyu104@users.noreply.github.com>
2023-09-15 02:25:41 +0800

    Update bitwise-or-of-all-subsequence-sums.cpp

--- C++/bitwise-or-of-all-subsequence-sums.cpp
+++ C++/bitwise-or-of-all-subsequence-sums.cpp
@@ -13,3 +13,23 @@ public:
         return result;
     }
 };
+
+// Time:  O(nlogn)
+// Space: O(1)
+// bit manipulation
+class Solution2 {
+public:
+    long long subsequenceSumOr(vector<int>& nums) {
+        uint64_t result = 0, cnt = 0;
+        for (int i = 0; i < 64; ++i) {
+            cnt >>= 1;
+            for (const auto& x : nums) {
+                cnt += (static_cast<int64_t>(x) >> i) & 1;
+            }
+            if (cnt) {
+                result |= 1ull << i;
+            }
+        }
+        return result;
+    }
+};
