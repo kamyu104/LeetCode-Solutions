a05148398ce3ad5ed01a3247563cf3a602eae86b
kamyu <kamyu104@users.noreply.github.com>
2023-09-15 02:14:12 +0800

    Update bitwise-or-of-all-subsequence-sums.py

--- Python/bitwise-or-of-all-subsequence-sums.py
+++ Python/bitwise-or-of-all-subsequence-sums.py
@@ -13,3 +13,22 @@ class Solution(object):
             prefix += x
             result |= x|prefix
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+# bit manipulation
+class Solution2(object):
+    def subsequenceSumOr(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = cnt = 0
+        for i in xrange(max(nums).bit_length()):
+            cnt >>= 1
+            for x in nums:
+                cnt += (x>>i)&1
+            if cnt:
+                result |= 1<<i
+        return result
