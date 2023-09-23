acc016f041146a7f0fbcdc887708140ddf56cc36
kamyu <kamyu104@users.noreply.github.com>
2023-09-24 02:20:00 +0800

    Update minimum-right-shifts-to-sort-the-array.py

--- Python/minimum-right-shifts-to-sort-the-array.py
+++ Python/minimum-right-shifts-to-sort-the-array.py
@@ -8,8 +8,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if len(nums) == 1:
-            return 0
-        i = next((i for i in xrange(len(nums)) if nums[i] > nums[(i+1)%len(nums)]), len(nums))
-        j = next((j for j in xrange(i+1, len(nums)) if nums[j%len(nums)] > nums[(j+1)%len(nums)]), len(nums))
+        i = next((i for i in xrange(len(nums)) if not nums[i] < nums[(i+1)%len(nums)]), len(nums))
+        j = next((j for j in xrange(i+1, len(nums)) if not nums[j%len(nums)] < nums[(j+1)%len(nums)]), len(nums))
         return len(nums)-(i+1) if j == len(nums) else -1
