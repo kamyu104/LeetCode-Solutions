6b15597e199e3771e960e09c31bd864dfdf55a08
kamyu <kamyu104@users.noreply.github.com>
2023-09-24 02:19:15 +0800

    Update minimum-right-shifts-to-sort-the-array.cpp

--- C++/minimum-right-shifts-to-sort-the-array.cpp
+++ C++/minimum-right-shifts-to-sort-the-array.cpp
@@ -5,18 +5,15 @@
 class Solution {
 public:
     int minimumRightShifts(vector<int>& nums) {
-        if (size(nums) == 1) {
-            return 0;
-        }
         int i = 0;
         for (; i < size(nums); ++i) {
-            if (nums[i % size(nums)] > nums[(i + 1) % size(nums)]) {
+            if (!(nums[i % size(nums)] < nums[(i + 1) % size(nums)])) {
                 break;
             }
         }
         int j = i + 1;
         for (; j < size(nums); ++j) {
-            if (nums[j % size(nums)] > nums[(j + 1) % size(nums)]) {
+            if (!(nums[j % size(nums)] < nums[(j + 1) % size(nums)])) {
                 break;
             }
         }
