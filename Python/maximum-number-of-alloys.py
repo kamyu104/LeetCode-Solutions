9004f59b04a21711dc408f5c863b68cdabb4e8a2
kamyu <kamyu104@users.noreply.github.com>
2023-09-25 13:28:56 +0800

    Update maximum-number-of-alloys.py

--- Python/maximum-number-of-alloys.py
+++ Python/maximum-number-of-alloys.py
@@ -1,8 +1,50 @@
-# Time:  O(k * n * logr), r = min(stock)+budget
-# Space: O(1)
+# Time:  O(k * nlogn)
+# Space: O(n)
 
-# binary search
+# math
 class Solution(object):
+    def maxNumberOfAlloys(self, n, k, budget, composition, stock, cost):
+        """
+        :type n: int
+        :type k: int
+        :type budget: int
+        :type composition: List[List[int]]
+        :type stock: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        def count(machine, budget):
+            def cnt(x):
+                return stock[x]//machine[x]
+    
+            idxs = range(n)
+            idxs.sort(key=cnt)
+            result = cnt(idxs[0])
+            curr = delta = remain = 0
+            for i in xrange(n):
+                delta += cost[idxs[i]]*machine[idxs[i]]
+                remain += cost[idxs[i]]*(stock[idxs[i]]%machine[idxs[i]])
+                if i+1 != n and cnt(idxs[i+1])-cnt(idxs[i]) == 0:
+                    continue
+                if curr+delta > budget+remain:
+                    break
+                curr += delta
+                budget += remain
+                if i+1 == n or budget-curr*(cnt(idxs[i+1])-cnt(idxs[i])) <= 0:
+                    result += budget//curr
+                    break
+                budget -= curr*(cnt(idxs[i+1])-cnt(idxs[i]))
+                result += cnt(idxs[i+1])-cnt(idxs[i])
+                remain = delta = 0
+            return result
+
+        return max(count(machine, budget) for machine in composition)
+
+
+# Time:  O(k * n * logr), remain = min(stock)+budget
+# Space: O(1)
+# binary search
+class Solution2(object):
     def maxNumberOfAlloys(self, n, k, budget, composition, stock, cost):
         """
         :type n: int
