ccaca2ba711e2a77d32fdceaf171ab020693324e
kamyu <kamyu104@users.noreply.github.com>
2023-09-28 10:54:23 +0800

    Create the-wording-game.cpp

---
+++ C++/the-wording-game.cpp
@@ -0,0 +1,33 @@
+// Time:  O(n + m)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    bool canAliceWin(vector<string>& a, vector<string>& b) {
+        const auto& is_closely_greater = [](const auto& a, const auto& b) {
+            return a[0] - b[0] <= 1 && a > b;
+        };
+
+        bool result = true;
+        int i = 0, j = -1;
+        while (true) {
+            for (++j; j < size(b); ++j) {
+                if (!is_closely_greater(b[j], a[i])) {
+                    continue;
+                }
+                while (j + 1 < size(b) && b[j][0] == b[j + 1][0] && is_closely_greater(b[j + 1], a[i])) {
+                    ++j;
+                }
+                break;
+            }
+            if (j == size(b)) {
+                break;
+            }
+            swap(a, b);
+            swap(i, j);
+            result = !result;
+        }
+        return result;
+    }
+};
