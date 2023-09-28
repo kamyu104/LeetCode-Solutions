364221490569fcd4d2ca4c4a35388940d9af8d6c
kamyu <kamyu104@users.noreply.github.com>
2023-09-28 11:16:42 +0800

    Update the-wording-game.py

--- Python/the-wording-game.py
+++ Python/the-wording-game.py
@@ -16,9 +16,9 @@ class Solution(object):
         i, j = 0, -1
         while True:
             j = next((j for j in xrange(j+1, len(b)) if is_closely_greater(b[j], a[i])), len(b))
-            while j+1 < len(b) and b[j+1][0] == b[j][0]:
-                j += 1
             if j == len(b):
                 break
+            while j+1 < len(b) and b[j+1][0] == b[j][0]:
+                j += 1
             a, b, i, j, result = b, a, j, i, not result
         return result
