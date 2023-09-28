f6d633958555d8772b51b8e3ee942a05d27de241
kamyu <kamyu104@users.noreply.github.com>
2023-09-29 00:44:23 +0800

    Update count-valid-paths-in-a-tree.cpp

--- C++/count-valid-paths-in-a-tree.cpp
+++ C++/count-valid-paths-in-a-tree.cpp
@@ -134,3 +134,99 @@ public:
         return result;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+// number theory, union find
+class Solution3 {
+public:
+    long long countPaths(int n, vector<vector<int>>& edges) {
+        const auto& linear_sieve_of_eratosthenes = [](int n) {  // Time: O(n), Space: O(n)
+            vector<int> spf(n + 1, -1);
+            vector<int> primes;
+            for (int i = 2; i <= n; ++i) {
+                if (spf[i] == -1) {
+                    spf[i] = i;
+                    primes.emplace_back(i);
+                }
+                for (const auto& p : primes) {
+                    if (i * p > n || p > spf[i]) {
+                        break;
+                    }
+                    spf[i * p] = p;
+                }
+            }
+            return spf;
+        };
+
+        const auto& spf = linear_sieve_of_eratosthenes(n);
+        const auto& is_prime = [&](int x) {
+            return spf[x] == x;
+        };
+    
+        UnionFind uf(n);
+        for (const auto& e : edges) {
+            const int u = e[0] - 1, v = e[1] - 1;
+            if (!is_prime(u + 1) && !is_prime(v + 1)) {
+                uf.union_set(u, v);
+            }
+        }
+        int64_t result = 0;
+        vector<int64_t> cnt(n, 1);
+        for (const auto& e : edges) {
+            int u = e[0] - 1, v = e[1] - 1;
+            if (is_prime(u + 1) == is_prime(v + 1)) {
+                continue;
+            }
+            if (!is_prime(u + 1)) {
+                swap(u, v);
+            }
+            result += cnt[u] * uf.total(v);
+            cnt[u] += uf.total(v);
+        }
+        return result;
+    }
+
+private:
+    class UnionFind {
+    public:
+        UnionFind(int n)
+         : set_(n)
+         , rank_(n)
+         , size_(n, 1)  {
+            iota(set_.begin(), set_.end(), 0);
+        }
+
+        int find_set(int x) {
+           if (set_[x] != x) {
+               set_[x] = find_set(set_[x]);  // Path compression.
+           }
+           return set_[x];
+        }
+
+        bool union_set(int x, int y) {
+            x = find_set(x), y = find_set(y);
+            if (x == y) {
+                return false;
+            }
+            if (rank_[x] > rank_[y]) {
+                swap(x, y);
+            }
+            set_[x] = y;  // Union by rank.
+            if (rank_[x] == rank_[y]) {
+                ++rank_[y];
+            }
+            size_[y] += size_[x];
+            return true;
+        }
+
+        int64_t total(int x) {
+            return size_[find_set(x)];
+        }
+
+    private:
+        vector<int> set_;
+        vector<int> rank_;
+        vector<int64_t> size_;
+    };
+};
