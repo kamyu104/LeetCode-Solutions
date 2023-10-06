65e740811a4ef70fee7f5f840f536ffd85d01850
kamyu <kamyu104@users.noreply.github.com>
2023-10-06 20:18:30 +0800

    Create maximum-number-of-k-divisible-components.py

---
+++ Python/maximum-number-of-k-divisible-components.py
@@ -0,0 +1,33 @@
+# Time:  O(n)
+# Space: O(n)
+
+# bfs, greedy
+class Solution(object):
+    def maxKDivisibleComponents(self, n, edges, values, k):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type values: List[int]
+        :type k: int
+        :rtype: int
+        """
+        adj = [[] for _ in xrange(n)]
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+        result = 0
+        dp = [x%k for x in values]
+        cnt = [len(adj[u]) for u in xrange(len(adj))]
+        q = [u for u in xrange(n) if cnt[u] == 1]
+        while q:
+            new_q = []
+            for u in q:
+                if not dp[u]:
+                    result += 1
+                for v in adj[u]:
+                    dp[v] = (dp[v]+dp[u])%k
+                    cnt[v] -= 1
+                    if cnt[v] == 1:
+                        new_q.append(v)
+            q = new_q
+        return max(result, 1)
