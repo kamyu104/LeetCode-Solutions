c4a58d24ff34d16eeb17443c5ba0b62322e5d0ca
kamyu <kamyu104@users.noreply.github.com>
2023-10-07 10:02:11 +0800

    Update count-visited-nodes-in-a-directed-graph.py

--- Python/count-visited-nodes-in-a-directed-graph.py
+++ Python/count-visited-nodes-in-a-directed-graph.py
@@ -1,7 +1,7 @@
 # Time:  O(n)
 # Space: O(n)
 
-# graph, prefix sum, two pointers, sliding window
+# graph, stack
 class Solution(object):
     def countVisitedNodes(self, edges):
         """
@@ -11,22 +11,22 @@ class Solution(object):
         def find_cycles(adj):
             result = [0]*len(adj)
             lookup = [0]*len(adj)
-            lookup2 = [0]*len(adj)  # added
+            stk = []  # added
             idx = 0
             for u in xrange(len(adj)):
                 prev = idx
                 while not lookup[u]:
                     idx += 1
                     lookup[u] = idx
-                    lookup2[idx-1] = u  # added
+                    stk.append(u)  # added
                     u = adj[u]
                 if lookup[u] > prev:
                     l = idx-lookup[u]+1
                     for _ in xrange(l):  # added
-                        result[u] = l
-                        u = adj[u]
-                for i in reversed(xrange(prev, lookup[u]-1 if lookup[u] > prev else idx)):  # added
-                    result[lookup2[i]] = result[adj[lookup2[i]]]+1
+                        result[stk.pop()] = l
+                while stk:  # added
+                    result[stk[-1]] = result[adj[stk[-1]]]+1
+                    stk.pop()
             return result
         
         return find_cycles(edges)
