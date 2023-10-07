23a416b2f6445087ceb43785d18ef3cbab725d8b
kamyu <kamyu104@users.noreply.github.com>
2023-10-07 10:04:53 +0800

    Update count-visited-nodes-in-a-directed-graph.cpp

--- C++/count-visited-nodes-in-a-directed-graph.cpp
+++ C++/count-visited-nodes-in-a-directed-graph.cpp
@@ -1,29 +1,31 @@
 // Time:  O(n)
 // Space: O(n)
 
-// graph, prefix sum, two pointers, sliding window
+// graph, stack
 class Solution {
 public:
     vector<int> countVisitedNodes(vector<int>& edges) {
         const auto& find_cycles = [](const auto& adj) {
             vector<int> result(size(adj));
             vector<int> lookup(size(adj));
-            vector<int> lookup2(size(adj));  // added
+            vector<int> stk;  // added
             for (int i = 0, idx = 0; i < size(adj); ++i) {
                 int u = i, prev = idx;
                 while (!lookup[u]) {
                     lookup[u] = ++idx;
-                    lookup2[idx - 1] = u;  // added
+                    stk.emplace_back(u);  // added
                     u = adj[u];
                 }
                 if (lookup[u] > prev) {
                     const int l = idx - lookup[u] + 1;
-                    for (int _ = 0; _ < l; ++_, u = adj[u]) {  // added
-                        result[u] = l;
+                    for (int _ = 0; _ < l; ++_) {  // added
+                        result[stk.back()] = l;
+                        stk.pop_back();
                     }
                 }
-                for (int j = lookup[u] > prev ? (lookup[u] - 1) - 1 : idx - 1; j >= prev; --j)  {  // added
-                    result[lookup2[j]] = result[adj[lookup2[j]]] + 1;
+                while (!empty(stk))  {  // added
+                    result[stk.back()] = result[adj[stk.back()]] + 1;
+                    stk.pop_back();
                 }
             }
             return result;
