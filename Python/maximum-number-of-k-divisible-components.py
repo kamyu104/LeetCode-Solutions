5907c1d55e85ed6a25bb910ccb21fcf922513117
kamyu <kamyu104@users.noreply.github.com>
2023-10-07 12:43:39 +0800

    Update maximum-number-of-k-divisible-components.py

--- Python/maximum-number-of-k-divisible-components.py
+++ Python/maximum-number-of-k-divisible-components.py
@@ -11,23 +11,26 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        def bfs():
+            result = 0
+            dp = [x%k for x in values]
+            cnt = [len(adj[u]) for u in xrange(len(adj))]
+            q = [u for u in xrange(n) if cnt[u] == 1]
+            while q:
+                new_q = []
+                for u in q:
+                    if not dp[u]:
+                        result += 1
+                    for v in adj[u]:
+                        dp[v] = (dp[v]+dp[u])%k
+                        cnt[v] -= 1
+                        if cnt[v] == 1:
+                            new_q.append(v)
+                q = new_q
+            return max(result, 1)
+
         adj = [[] for _ in xrange(n)]
         for u, v in edges:
             adj[u].append(v)
             adj[v].append(u)
-        result = 0
-        dp = [x%k for x in values]
-        cnt = [len(adj[u]) for u in xrange(len(adj))]
-        q = [u for u in xrange(n) if cnt[u] == 1]
-        while q:
-            new_q = []
-            for u in q:
-                if not dp[u]:
-                    result += 1
-                for v in adj[u]:
-                    dp[v] = (dp[v]+dp[u])%k
-                    cnt[v] -= 1
-                    if cnt[v] == 1:
-                        new_q.append(v)
-            q = new_q
-        return max(result, 1)
+        return bfs()
