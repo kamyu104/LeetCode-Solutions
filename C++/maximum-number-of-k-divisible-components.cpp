ce0823ee0ed2ea613fdbe3d103eb26a63133d00e
kamyu <kamyu104@users.noreply.github.com>
2023-10-07 12:44:58 +0800

    Update maximum-number-of-k-divisible-components.cpp

--- C++/maximum-number-of-k-divisible-components.cpp
+++ C++/maximum-number-of-k-divisible-components.cpp
@@ -10,36 +10,40 @@ public:
             adj[e[0]].emplace_back(e[1]);
             adj[e[1]].emplace_back(e[0]);
         }
-        vector<int> cnt(n);
-        for (int u = 0; u < n; ++u) {
-            cnt[u] = size(adj[u]);
-        }
-        int result = 0;
-        vector<int> dp(size(values));
-        transform(cbegin(values), cend(values), begin(dp), [&](const auto& x) {
-            return x % k;
-        });
-        vector<int> q;
-        for (int u = 0; u < n; ++u) {
-            if (cnt[u] == 1) {
-                q.emplace_back(u);
+        const auto& bfs = [&]() {
+            vector<int> cnt(n);
+            for (int u = 0; u < n; ++u) {
+                cnt[u] = size(adj[u]);
             }
-        }
-        while (!empty(q)) {
-            vector<int> new_q;
-            for (const auto& u : q) {
-                if (!dp[u]) {
-                    ++result;
+            int result = 0;
+            vector<int> dp(size(values));
+            transform(cbegin(values), cend(values), begin(dp), [&](const auto& x) {
+                return x % k;
+            });
+            vector<int> q;
+            for (int u = 0; u < n; ++u) {
+                if (cnt[u] == 1) {
+                    q.emplace_back(u);
                 }
-                for (const auto& v : adj[u]) {
-                    dp[v] = (dp[v] + dp[u]) % k;
-                    if (--cnt[v] == 1) {
-                        new_q.emplace_back(v);
+            }
+            while (!empty(q)) {
+                vector<int> new_q;
+                for (const auto& u : q) {
+                    if (!dp[u]) {
+                        ++result;
+                    }
+                    for (const auto& v : adj[u]) {
+                        dp[v] = (dp[v] + dp[u]) % k;
+                        if (--cnt[v] == 1) {
+                            new_q.emplace_back(v);
+                        }
                     }
                 }
+                q = move(new_q);
             }
-            q = move(new_q);
-        }
-        return max(result, 1);
+            return max(result, 1);
+        };
+
+        return bfs();
     }
 };
