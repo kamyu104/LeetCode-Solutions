60581ca836b983862ba402ad5a8235d0c9d75b89
kamyu <kamyu104@users.noreply.github.com>
2023-10-08 19:15:29 +0800

    Update greatest-common-divisor-traversal.py

--- Python/greatest-common-divisor-traversal.py
+++ Python/greatest-common-divisor-traversal.py
@@ -1,5 +1,5 @@
 # Time:  precompute: O(sqrt(r)), r = max(nums)
-#        runtime:    O(n * (logr + pi(sqrt(r))) = O(n * (logr + sqrt(r)/log(sqrt(r))), pi(n) = number of primes in a range [1, n] = O(n/logn) by prime number theorem, see https://en.wikipedia.org/wiki/Prime_number_theorem
+#        runtime:    O(n * (logr + pi(sqrt(r))) = O(n * (logr + sqrt(r)/log(sqrt(r)))), pi(n) = number of primes in a range [1, n] = O(n/logn) by prime number theorem, see https://en.wikipedia.org/wiki/Prime_number_theorem
 # Space: O(sqrt(r) + nlogr)
 
 # linear sieve of eratosthenes, number theory, bfs
