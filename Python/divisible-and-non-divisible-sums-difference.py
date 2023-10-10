2b957cdafa1a6407a23db6889f97f9fbdff8857e
kamyu <kamyu104@users.noreply.github.com>
2023-10-10 20:36:05 +0800

    Update divisible-and-non-divisible-sums-difference.py

--- Python/divisible-and-non-divisible-sums-difference.py
+++ Python/divisible-and-non-divisible-sums-difference.py
@@ -9,4 +9,20 @@ class Solution(object):
         :type m: int
         :rtype: int
         """
+        def arithmetic_progression_sum(a, d, l):
+            return (a+(a+(l-1)*d))*l//2
+    
+        return arithmetic_progression_sum(1, 1, n) - 2*arithmetic_progression_sum(m, m, n//m)
+
+
+# Time:  O(1)
+# Space: O(1)
+# math
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
         return (n+1)*n//2 - 2*(((n//m+1)*(n//m)//2)*m)
