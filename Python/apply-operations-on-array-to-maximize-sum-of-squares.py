38934ff1da307608ceb27277e0a011320e7745b3
kamyu <kamyu104@users.noreply.github.com>
2023-10-10 20:17:03 +0800

    Create apply-operations-on-array-to-maximize-sum-of-squares.py

---
+++ Python/apply-operations-on-array-to-maximize-sum-of-squares.py
@@ -0,0 +1,19 @@
+# Time:  O(nlogr), r = max(nums)
+# Space: O(logr)
+
+# greedy, freq table
+class Solution(object):
+    def maxSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        l = max(nums).bit_length()
+        cnt = [0]*l
+        for i in xrange(l):
+            for x in nums:
+                if x&(1<<i):
+                    cnt[i] += 1
+        return reduce(lambda x, y: (x+y)%MOD, (sum(1<<i for i in xrange(l) if cnt[i] >= j)**2 for j in xrange(1, k+1)))
