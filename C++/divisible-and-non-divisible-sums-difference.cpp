0a37621ce86b5eb1bf70ea1971d5a5f128171476
kamyu <kamyu104@users.noreply.github.com>
2023-10-10 20:37:57 +0800

    Update divisible-and-non-divisible-sums-difference.cpp

--- C++/divisible-and-non-divisible-sums-difference.cpp
+++ C++/divisible-and-non-divisible-sums-difference.cpp
@@ -5,6 +5,21 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
+        const auto& arithmetic_progression_sum  = [](const auto& a, const auto& d, const auto& l) {
+            return (a + (a + (l - 1) * d)) * l / 2;
+        };
+
+        return arithmetic_progression_sum(1, 1, n) - 2 * arithmetic_progression_sum(m, m, n / m);
+    }
+};
+
+
+// Time:  O(1)
+// Space: O(1)
+// math
+class Solution2 {
+public:
+    int differenceOfSums(int n, int m) {
         return (n + 1) * n / 2 - 2 * (((n / m + 1) * (n / m) / 2) * m);
     }
 };
