f33fb1a6beb92541f9eb441e81c39436f85e5429
kamyu <kamyu104@users.noreply.github.com>
2023-10-12 14:28:39 +0800

    Update majority-element.cpp

--- C++/majority-element.cpp
+++ C++/majority-element.cpp
@@ -4,17 +4,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int result = 0, cnt = 0;
-        for (const auto& x : nums) {
-            if (cnt == 0) {
-                result = x;
+        const auto& boyer_moore_majority_vote = [&]() {
+            int result = 0, cnt = 0;
+            for (const auto& x : nums) {
+                if (cnt == 0) {
+                    result = x;
+                }
+                if (x == result) {
+                    ++cnt;
+                } else {
+                    --cnt;
+                }
             }
-            if (x == result) {
-                ++cnt;
-            } else {
-                --cnt;
-            }
-        }
-        return result; 
+            return result; 
+        };
+
+        return boyer_moore_majority_vote();
     }
 };
