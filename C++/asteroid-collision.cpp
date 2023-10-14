25ab565f55c7b5b7e1e70eac6e59f74dae2535a6
kamyu <kamyu104@users.noreply.github.com>
2023-10-15 02:42:11 +0800

    Update asteroid-collision.cpp

--- C++/asteroid-collision.cpp
+++ C++/asteroid-collision.cpp
@@ -5,20 +5,45 @@ class Solution {
 public:
     vector<int> asteroidCollision(vector<int>& asteroids) {
         vector<int> result;
-        for (const auto& asteroid : asteroids) {
+        for (const auto& x : asteroids) {
+            if (x > 0) {
+                result.emplace_back(x);
+            }
+            while (!empty(result) && 0 < result.back() && result.back() < -x) {
+                result.pop_back();
+            }
+            if (!empty(result) && 0 < result.back()) {
+                if (result.back() == -x) {
+                    result.pop_back();
+                }
+                continue;
+            }
+            result.emplace_back(x);
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        vector<int> result;
+        for (const auto& x : asteroids) {
             bool is_exploded = false;
-            while (!result.empty() && asteroid < 0 && 0 < result.back()) {
-                if (result.back() < -asteroid) {
+            while (!empty(result) && x < 0 && 0 < result.back()) {
+                if (result.back() < -x) {
                     result.pop_back();
                     continue;
-                } else if (result.back() == -asteroid) {
+                } else if (result.back() == -x) {
                     result.pop_back();
                 }
                 is_exploded = true;
                 break;
             }
             if (!is_exploded) {
-                result.emplace_back(asteroid);
+                result.emplace_back(x);
             }
         }
         return result;
