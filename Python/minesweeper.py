1900c10d0549d328ffe1d003e016f423c9d12559
kamyu <kamyu104@users.noreply.github.com>
2023-10-15 00:12:58 +0800

    Update minesweeper.py

--- Python/minesweeper.py
+++ Python/minesweeper.py
@@ -1,9 +1,6 @@
 # Time:  O(m * n)
 # Space: O(m + n)
 
-import collections
-
-
 class Solution(object):
     def updateBoard(self, board, click):
         """
@@ -11,38 +8,36 @@ class Solution(object):
         :type click: List[int]
         :rtype: List[List[str]]
         """
-        q = collections.deque([click])
+        q = [click]
         while q:
-            row, col = q.popleft()
-            if board[row][col] == 'M':
-                board[row][col] = 'X'
-            else:
-                count = 0
-                for i in xrange(-1, 2):
-                    for j in xrange(-1, 2):
-                        if i == 0 and j == 0:
+            new_q = []
+            for r, c in q:
+                if board[r][c] == 'M':
+                    board[r][c] = 'X'
+                    continue
+                cnt = 0
+                for dr in range(-1, 2):
+                    for dc in range(-1, 2):
+                        if dr == dc == 0:
                             continue
-                        r, c = row + i, col + j
-                        if not (0 <= r < len(board)) or not (0 <= c < len(board[r])):
+                        nr, nc = r+dr, c+dc
+                        if not (0 <= nr < len(board) and 0 <= nc < len(board[r]) and board[nr][nc] == 'M'):
                             continue
-                        if board[r][c] == 'M' or board[r][c] == 'X':
-                            count += 1
-
-                if count:
-                    board[row][col] = chr(count + ord('0'))
-                else:
-                    board[row][col] = 'B'
-                    for i in xrange(-1, 2):
-                        for j in xrange(-1, 2):
-                            if i == 0 and j == 0:
-                                continue
-                            r, c = row + i, col + j
-                            if not (0 <= r < len(board)) or not (0 <= c < len(board[r])):
-                                continue
-                            if board[r][c] == 'E':
-                                q.append((r, c))
-                                board[r][c] = ' '
-
+                        cnt += 1
+                if cnt:
+                    board[r][c] = chr(cnt + ord('0'))
+                    continue
+                board[r][c] = 'B'
+                for dr in range(-1, 2):
+                    for dc in range(-1, 2):
+                        if dr == dc == 0:
+                            continue
+                        nr, nc = r+dr, c+dc
+                        if not (0 <= nr < len(board) and 0 <= nc < len(board[r]) and board[nr][nc] == 'E'):
+                            continue
+                        board[nr][nc] = ' '
+                        new_q.append((nr, nc))
+            q = new_q
         return board
 
 
