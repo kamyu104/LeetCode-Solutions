96805d92ffd49600599ecd6858d3f4dc90b473fc
kamyu <kamyu104@users.noreply.github.com>
2023-10-15 01:07:07 +0800

    Update minesweeper.cpp

--- C++/minesweeper.cpp
+++ C++/minesweeper.cpp
@@ -1,108 +1,98 @@
 // Time:  O(m * n)
 // Space: O(m + n)
 
+// dfs
 class Solution {
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click)  {
-        queue<vector<int>> q;
-        q.emplace(click);
-        while (!q.empty()) {
-            int row = q.front()[0], col = q.front()[1];
-            q.pop();
-            if (board[row][col] == 'M') {
-                board[row][col] = 'X';
-            } else {
-                int count = 0;
-                for (int i = -1; i < 2; ++i) {
-                    for (int j = -1; j < 2; ++j) {
-                        if (i == 0 && j == 0) {
-                            continue;
-                        }
-                        int r = row + i, c = col + j;
-                        if (r < 0 || r >= board.size() || c < 0 || c < 0 || c >= board[r].size()) {
-                            continue;
-                        }
-                        if (board[r][c] == 'M' || board[r][c] == 'X') {
-                            ++count;
-                        }
+        vector<pair<int, int>> stk;
+        stk.emplace_back(click[0], click[1]);
+        while (!empty(stk)) {
+            const auto [r, c] = stk.back(); stk.pop_back();
+            if (board[r][c] == 'M') {
+                board[r][c] = 'X';
+                continue;
+            }
+            int cnt = 0;
+            vector<pair<int, int>> adj;
+            for (int dr = -1; dr < 2; ++dr) {
+                for (int dc = -1; dc < 2; ++dc) {
+                    if (dr == 0 && dc == 0) {
+                        continue;
                     }
-                }
-                
-                if (count > 0) {
-                    board[row][col] = count + '0';
-                } else {
-                    board[row][col] = 'B';
-                    for (int i = -1; i < 2; ++i) {
-                        for (int j = -1; j < 2; ++j) {
-                            if (i == 0 && j == 0) {
-                                continue;
-                            }
-                            int r = row + i, c = col + j;
-                            if (r < 0 || r >= board.size() || c < 0 || c < 0 || c >= board[r].size()) {
-                                continue;
-                            }
-                            if (board[r][c] == 'E') {
-                                vector<int> next_click = {r, c};
-                                q.emplace(next_click);
-                                board[r][c] = 'B';
-                            }
-                        }
+                    int nr = r + dr, nc = c + dc;
+                    if (!(0 <= nr && nr < size(board) && 0 <= nc && nc < size(board[0]))) {
+                        continue;
+                    }
+                    if (board[nr][nc] == 'M') {
+                        ++cnt;
+                    }
+                    if (board[nr][nc] == 'E') {
+                        adj.emplace_back(nr, nc);
                     }
                 }
             }
+            if (cnt) {
+                board[r][c] = cnt + '0';
+                continue;
+            }
+            board[r][c] = 'B';
+            for (const auto& [nr, nc] : adj) {
+                board[nr][nc] = ' ';
+                stk.emplace_back(nr, nc);
+            }
         }
-        
         return board;
     }
 };
 
+
 // Time:  O(m * n)
-// Space: O(m * n)
+// Space: O(m + n)
+// bfs
 class Solution2 {
 public:
     vector<vector<char>> updateBoard(vector<vector<char>>& board, vector<int>& click)  {
-        int row = click[0], col = click[1];
-        if (board[row][col] == 'M') {
-            board[row][col] = 'X';
-        } else {
-            int count = 0;
-            for (int i = -1; i < 2; ++i) {
-                for (int j = -1; j < 2; ++j) {
-                    if (i == 0 && j == 0) {
-                        continue;
-                    }
-                    int r = row + i, c = col + j;
-                    if (r < 0 || r >= board.size() || c < 0 || c < 0 || c >= board[r].size()) {
-                        continue;
-                    }
-                    if (board[r][c] == 'M' || board[r][c] == 'X') {
-                        ++count;
-                    }
+        vector<pair<int, int>> q;
+        q.emplace_back(click[0], click[1]);
+        while (!empty(q)) {
+            vector<pair<int, int>> new_q;
+            for (const auto& [r, c] : q) {
+                if (board[r][c] == 'M') {
+                    board[r][c] = 'X';
+                    continue;
                 }
-            }
-            
-            if (count > 0) {
-                board[row][col] = count + '0';
-            } else {
-                board[row][col] = 'B';
-                for (int i = -1; i < 2; ++i) {
-                    for (int j = -1; j < 2; ++j) {
-                        if (i == 0 && j == 0) {
+                int cnt = 0;
+                vector<pair<int, int>> adj;
+                for (int dr = -1; dr < 2; ++dr) {
+                    for (int dc = -1; dc < 2; ++dc) {
+                        if (dr == 0 && dc == 0) {
                             continue;
                         }
-                        int r = row + i, c = col + j;
-                        if (r < 0 || r >= board.size() || c < 0 || c < 0 || c >= board[r].size()) {
+                        int nr = r + dr, nc = c + dc;
+                        if (!(0 <= nr && nr < size(board) && 0 <= nc && nc < size(board[0]))) {
                             continue;
                         }
-                        if (board[r][c] == 'E') {
-                            vector<int> next_click = {r, c};
-                            updateBoard(board, next_click);
+                        if (board[nr][nc] == 'M') {
+                            ++cnt;
+                        }
+                        if (board[nr][nc] == 'E') {
+                            adj.emplace_back(nr, nc);
                         }
                     }
                 }
+                if (cnt) {
+                    board[r][c] = cnt + '0';
+                    continue;
+                }
+                board[r][c] = 'B';
+                for (const auto& [nr, nc] : adj) {
+                    board[nr][nc] = ' ';
+                    new_q.emplace_back(nr, nc);
+                }
             }
+            q = move(new_q);
         }
-        
         return board;
     }
 };
