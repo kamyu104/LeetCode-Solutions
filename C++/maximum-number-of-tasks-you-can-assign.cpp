c5f72ca247771434abb312a153859b0ce17fb5be
kamyu <kamyu104@users.noreply.github.com>
2023-10-15 15:55:55 +0800

    Update maximum-number-of-tasks-you-can-assign.cpp

--- C++/maximum-number-of-tasks-you-can-assign.cpp
+++ C++/maximum-number-of-tasks-you-can-assign.cpp
@@ -4,6 +4,51 @@
 class Solution {
 public:
     int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
+        sort(begin(tasks), end(tasks));
+        sort(begin(workers), end(workers));
+        int left = 1, right = min(size(tasks), size(workers));
+        while (left <= right) {
+            const auto& mid = left + (right - left) / 2;
+            if (!check(tasks, workers, pills, strength, mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return right;
+    }
+
+private:
+    bool check(const vector<int>& tasks, const vector<int>& workers,
+               int pills, int strength,
+               int x) {
+
+        multiset<int> t(cbegin(tasks), cbegin(tasks) + x);
+        for (int i = size(workers) - x; i < size(workers); ++i) {  // enumerate from the weakest worker to the strongest worker, greedily assign him to the hardest task which he can do
+            auto it = t.upper_bound(workers[i]);
+            if (it != begin(t)) {
+                t.erase(prev(it));
+                continue;
+            }
+            if (pills) {
+                it = t.upper_bound(workers[i] + strength);
+                if (it != begin((t))) {
+                    t.erase(prev(it));
+                    --pills;
+                    continue;
+                }
+            }
+            return false;
+        }
+        return true;
+    }
+};
+
+// Time:  O(n * (logn)^2)
+// Space: O(n)
+class Solution2 {
+public:
+    int maxTaskAssign(vector<int>& tasks, vector<int>& workers, int pills, int strength) {
         sort(rbegin(tasks), rend(tasks));
         sort(begin(workers), end(workers));
         int left = 1, right = min(size(tasks), size(workers));
@@ -24,7 +69,7 @@ private:
                int x) {
 
         multiset<int> w(cbegin(workers) + (size(workers) - x), cend(workers));
-        for (int i = size(tasks) - x; i < size(tasks); ++i) {
+        for (int i = size(tasks) - x; i < size(tasks); ++i) {  // enumerate from the hardest task to the easiest task, greedily assign it to the weakest worker whom it can be done by
             auto it = w.lower_bound(tasks[i]);
             if (it != end(w)) {
                 w.erase(it);
