f82f53650662cab1b23a4372f456972fde5b2da7
kamyu <kamyu104@users.noreply.github.com>
2023-10-16 00:26:40 +0800

    Create last-visited-integers.py

---
+++ Python/last-visited-integers.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(n)
+
+# stack
+class Solution(object):
+    def lastVisitedIntegers(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[int]
+        """
+        KEY = "prev"
+        result, stk = [], []
+        i = -1
+        for x in words:
+            if x == PREV:
+                result.append(stk[i] if i >= 0 else -1)
+                i -= 1
+                continue
+            stk.append(int(x))
+            i = len(stk)-1
+        return result
