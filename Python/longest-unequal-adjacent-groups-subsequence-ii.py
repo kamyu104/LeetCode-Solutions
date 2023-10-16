a78aad52a91ae6866224e22da1dbe5bd22e18dfa
kamyu <kamyu104@users.noreply.github.com>
2023-10-16 21:13:58 +0800

    Update longest-unequal-adjacent-groups-subsequence-ii.py

--- Python/longest-unequal-adjacent-groups-subsequence-ii.py
+++ Python/longest-unequal-adjacent-groups-subsequence-ii.py
@@ -13,15 +13,13 @@ class Solution(object):
         :type groups: List[int]
         :rtype: List[str]
         """
-        def check(i, j):
-            return (groups[i] != groups[j] and
-                    len(words[i]) == len(words[j]) and
-                    sum(a != b for a, b in itertools.izip(words[i], words[j])) == 1)
+        def check(s1, s2):
+            return len(s1) == len(s2) and sum(a != b for a, b in itertools.izip(s1, s2)) == 1
 
         dp = [[1, -1] for _ in xrange(n)]
         for i in reversed(xrange(n)):
             for j in xrange(i+1, n):
-                if check(j, i):
+                if groups[i] != groups[j] and check(words[j], words[i]):
                     dp[i] = max(dp[i], [dp[j][0]+1, j])
         result = []
         i = max(xrange(n), key=lambda x: dp[x])
@@ -45,15 +43,13 @@ class Solution2(object):
         :type groups: List[int]
         :rtype: List[str]
         """
-        def check(i, j):
-            return (groups[i] != groups[j] and
-                    len(words[i]) == len(words[j]) and
-                    sum(a != b for a, b in itertools.izip(words[i], words[j])) == 1)
+        def check(s1, s2):
+            return len(s1) == len(s2) and sum(a != b for a, b in itertools.izip(s1, s2)) == 1
 
         dp = [[1, -1] for _ in xrange(n)]
         for i in xrange(n):
             for j in xrange(i):
-                if check(j, i):
+                if groups[i] != groups[j] and check(words[j], words[i]):
                     dp[i] = max(dp[i], [dp[j][0]+1, j])
         result = []
         i = max(xrange(n), key=lambda x: dp[x])
@@ -78,15 +74,13 @@ class Solution3(object):
         :type groups: List[int]
         :rtype: List[str]
         """
-        def check(i, j):
-            return (groups[i] != groups[j] and
-                    len(words[i]) == len(words[j]) and
-                    sum(a != b for a, b in itertools.izip(words[i], words[j])) == 1)
+        def check(s1, s2):
+            return len(s1) == len(s2) and sum(a != b for a, b in itertools.izip(s1, s2)) == 1
 
         dp = [[] for _ in xrange(n)]
         for i in xrange(n):
             for j in xrange(i):
-                if check(j, i) and len(dp[j]) > len(dp[i]):
+                if groups[i] != groups[j] and check(words[j], words[i]) and len(dp[j]) > len(dp[i]):
                     dp[i] = dp[j]
             dp[i] = dp[i]+[i]
         return map(lambda x: words[x], max(dp, key=lambda x: len(x)))
