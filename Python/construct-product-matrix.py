09d5ef0d0d19ec83aa250dfe9a7422ffc835709c
kamyu <kamyu104@users.noreply.github.com>
2023-10-17 01:31:21 +0800

    Update construct-product-matrix.py

--- Python/construct-product-matrix.py
+++ Python/construct-product-matrix.py
@@ -13,13 +13,11 @@ class Solution(object):
         for i in reversed(xrange(len(grid))):
             for j in reversed(xrange(len(grid[0]))):
                 right[i*len(grid[0])+j] = (right[(i*len(grid[0])+j)+1]*grid[i][j])%MOD
-        result = [[0]*len(grid[0]) for _ in xrange(len(grid))]
         left = 1
         for i in xrange(len(grid)):
             for j in xrange(len(grid[0])):
-                result[i][j] = (left*right[(i*len(grid[0])+j)+1])%MOD
-                left = (left*grid[i][j])%MOD
-        return result
+                grid[i][j], left = (left*right[(i*len(grid[0])+j)+1])%MOD, (left*grid[i][j])%MOD
+        return grid
 
 
 # Time:  O(m * n)
@@ -40,4 +38,7 @@ class Solution2(object):
         for i in reversed(xrange(len(grid))):
             for j in reversed(xrange(len(grid[0]))):
                 right[i*len(grid[0])+j] = (right[(i*len(grid[0])+j)+1]*grid[i][j])%MOD
-        return [[(left[i*len(grid[0])+j]*right[(i*len(grid[0])+j)+1])%MOD for j in xrange(len(grid[0]))] for i in xrange(len(grid))]
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                grid[i][j] = (left[i*len(grid[0])+j]*right[(i*len(grid[0])+j)+1])%MOD
+        return grid
