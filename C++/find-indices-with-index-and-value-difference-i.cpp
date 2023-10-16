cab8263177c20f888a9d710d93c59bd704ae79f2
kamyu <kamyu104@users.noreply.github.com>
2023-10-17 00:13:40 +0800

    Create find-indices-with-index-and-value-difference-i.cpp

---
+++ C++/find-indices-with-index-and-value-difference-i.cpp
@@ -0,0 +1,26 @@
+// Time:  O(n)
+// Space: O(1)
+
+// prefix sum
+class Solution {
+public:
+    vector<int> findIndices(vector<int>& nums, int indexDifference, int valueDifference) {
+        for (int i = 0, mx_i = 0, mn_i = 0; i + indexDifference < size(nums); ++i) {
+            if (nums[i] > nums[mx_i]) {
+                mx_i = i;
+            } else if (nums[i] < nums[mn_i]) {
+                mn_i = i;
+            }
+            // we don't need to add abs for the difference since
+            // - if nums[mx_i]-nums[i+indexDifference] < 0, then checking nums[i+indexDifference]-nums[mn_i] >= -(nums[mx_i]-nums[i+indexDifference]) > 0 can cover the case
+            // - if nums[i+indexDifference]-nums[mn_i] < 0, then checking nums[mx_i]-nums[i+indexDifference] >= -(nums[i+indexDifference]-nums[mn_i]) > 0 can cover the case
+            if (nums[mx_i] - nums[i + indexDifference] >= valueDifference) {
+                return {mx_i, i + indexDifference};
+            }
+            if (nums[i + indexDifference] - nums[mn_i] >= valueDifference) {
+                return {mn_i, i + indexDifference};
+            }
+        }
+        return {-1, -1};
+    }
+};
