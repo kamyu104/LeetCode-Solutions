c5845f5ea5f04f31ce2a848af74cb71d944ee3cb
kamyu <kamyu104@users.noreply.github.com>
2023-10-24 15:03:12 +0800

    Update maximum-profitable-triplets-with-increasing-prices-i.cpp

--- C++/maximum-profitable-triplets-with-increasing-prices-i.cpp
+++ C++/maximum-profitable-triplets-with-increasing-prices-i.cpp
@@ -6,34 +6,34 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, vector<int>& profits) {
         static const int NEG_INF = numeric_limits<int>::min();
-        vector<int> left(size(prices), NEG_INF);
+        vector<int> right(size(prices), NEG_INF);
         set<pair<int, int>> bst;
-        for (int i = 0; i < size(prices); ++i) {
-            const auto it = bst.lower_bound(pair(prices[i], 0));
+        for (int i = size(prices) - 1; i >= 0; --i) {
+            const auto it = bst.lower_bound(pair(-prices[i], 0));
             if (it != begin(bst)) {
-                left[i] = prev(it)->second;
+                right[i] = prev(it)->second;
             }
             if (!(it == begin(bst) || prev(it)->second < profits[i])) {
                 continue;
             }
-            const auto [jt, _] = bst.emplace(prices[i], profits[i]);
+            const auto [jt, _] = bst.emplace(-prices[i], profits[i]);
             while (next(jt) != end(bst) && next(jt)->second <= jt->second) {
                 bst.erase(next(jt));
             }
         }
         int result = NEG_INF;
         bst.clear();
-        for (int i = size(prices) - 1; i >= 0; --i) {
-            const auto it = bst.lower_bound(pair(-prices[i], 0));
+        for (int i = 0; i < size(prices); ++i) {
+            const auto it = bst.lower_bound(pair(prices[i], 0));
             if (it != begin(bst)) {
-                if (left[i] != NEG_INF && prev(it)->second != NEG_INF) {
-                    result = max(result, left[i] + profits[i] + prev(it)->second);
+                if (prev(it)->second != NEG_INF && right[i] != NEG_INF) {
+                    result = max(result, prev(it)->second + profits[i] + right[i]);
                 }
             }
             if (!(it == begin(bst) || prev(it)->second < profits[i])) {
                 continue;
             }
-            const auto [jt, _] = bst.emplace(-prices[i], profits[i]);
+            const auto [jt, _] = bst.emplace(prices[i], profits[i]);
             while (next(jt) != end(bst) && next(jt)->second <= jt->second) {
                 bst.erase(next(jt));
             }
