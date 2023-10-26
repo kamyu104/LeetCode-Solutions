a44b9acd9dcfff7e48f08df3b1a0dce1cc22cdcc
kamyu <kamyu104@users.noreply.github.com>
2023-10-26 16:31:09 +0800

    Update minimum-changes-to-make-k-semi-palindromes.cpp

--- C++/minimum-changes-to-make-k-semi-palindromes.cpp
+++ C++/minimum-changes-to-make-k-semi-palindromes.cpp
@@ -1,63 +1,18 @@
-// Time:  O(n * nlogn * n + n^2 * k) = O(n^3 * logn + n^2 * k)
-// Space: O(nlogn + n * k)
+// Time: O(n * nlogn + n^3 + n^2 * k) = O(n^3)
+// Space: O(n * nlogn) = O(n^2 * logn)
 
 // number theory, dp
 class Solution {
 public:
     int minimumChanges(string s, int k) {
         vector<vector<int>> divisors(size(s) + 1);
-        for (int i = 1; i < size(divisors); ++i) {
-            for (int j = i + i; j < size(divisors); j += i) {
-                divisors[j].emplace_back(i);
-            }
-        }
-
-        const auto& dist = [&](int left, int right, int d) {
-            int result = 0;
-            for (int i = 0; i < (right - left + 1) / 2; ++i) {
-                if (s[left + i] != s[right - ((i / d + 1) * d - 1) + (i % d)]) {
-                    ++result;
-                }
-            }
-            return result;
-        };
-
-        const auto& min_dist = [&](int left, int right) {
-            int result = size(s);
-            for (const auto& d : divisors[right - left + 1]) {
-                result = min(result, dist(left, right, d));
-            }
-            return result;
-        };
-            
-        vector<vector<int>> dp(size(s) + 1, vector<int>(k + 1, size(s)));
-        dp[0][0] = 0;
-        for (int i = 0; i < size(s); ++i) {
-            for (int j = 0; j < i; ++j) {
-                const int c = min_dist(j, i);
-                for (int l = 0; l < k; ++l) {
-                    dp[i + 1][l + 1] = min(dp[i + 1][l + 1], dp[j][l] + c);
-                }
-            }
-        }
-        return dp[size(s)][k];
-    }
-};
-
-// Time:  O(n * nlogn * n + n^2 * k) = O(n^3 * logn + n^2 * k)
-// Space: O(n * nlogn + n * k) = O(n^2 * logn + n * k)
-// number theory, dp
-class Solution2 {
-public:
-    int minimumChanges(string s, int k) {
-        vector<vector<int>> divisors(size(s) + 1);
-        for (int i = 1; i < size(divisors); ++i) {
+        for (int i = 1; i < size(divisors); ++i) {  // Time: O(nlogn), Space: O(nlogn)
             for (int j = i; j < size(divisors); j += i) {
                 divisors[j].emplace_back(i);
             }
         }    
         vector<vector<unordered_map<int, int>>> dp(size(s), vector<unordered_map<int, int>>(size(s)));
-        for (int l = 1; l <= size(s); ++l) {
+        for (int l = 1; l <= size(s); ++l) {  // Time: O(n * nlogn + n^3), Space: O(n * nlogn)
             for (int left = 0; left + l - 1 < size(s); ++left) {
                 const int right = left + l - 1;
                 for (const auto& d : divisors[l]) {
@@ -73,7 +28,7 @@ public:
         }
         vector<vector<int>> dp2(size(s) + 1, vector<int>(k + 1, size(s)));
         dp2[0][0] = 0;
-        for (int i = 0; i < size(s); ++i) {
+        for (int i = 0; i < size(s); ++i) {  // Time: O(n^2 * logn + n^2 * k), Space: O(n * k)
             for (int j = 0; j < i; ++j) {
                 int c = size(s);
                 for (const auto& d : divisors[i - j + 1]) {
@@ -87,5 +42,50 @@ public:
             }
         }
         return dp2[size(s)][k];
+    }
+};
+
+// Time:  O(n * nlogn  * n + n^2 * k) = O(n^3 * logn)
+// Space: O(nlogn + n * k)
+// number theory, dp
+class Solution2 {
+public:
+    int minimumChanges(string s, int k) {
+        vector<vector<int>> divisors(size(s) + 1);
+        for (int i = 1; i < size(divisors); ++i) {  // Time: O(nlogn), Space: O(nlogn)
+            for (int j = i + i; j < size(divisors); j += i) {
+                divisors[j].emplace_back(i);
+            }
+        }
+
+        const auto& dist = [&](int left, int right, int d) {
+            int result = 0;
+            for (int i = 0; i < (right - left + 1) / 2; ++i) {
+                if (s[left + i] != s[right - ((i / d + 1) * d - 1) + (i % d)]) {
+                    ++result;
+                }
+            }
+            return result;
+        };
+
+        const auto& min_dist = [&](int left, int right) {  // Time: O(nlogn)
+            int result = size(s);
+            for (const auto& d : divisors[right - left + 1]) {
+                result = min(result, dist(left, right, d));
+            }
+            return result;
+        };
+            
+        vector<vector<int>> dp(size(s) + 1, vector<int>(k + 1, size(s)));
+        dp[0][0] = 0;
+        for (int i = 0; i < size(s); ++i) {  // Time: O(n^2 * nlogn + n^2 * k), Space: O(n * k)
+            for (int j = 0; j < i; ++j) {
+                const int c = min_dist(j, i);
+                for (int l = 0; l < k; ++l) {
+                    dp[i + 1][l + 1] = min(dp[i + 1][l + 1], dp[j][l] + c);
+                }
+            }
+        }
+        return dp[size(s)][k];
     }
 };
