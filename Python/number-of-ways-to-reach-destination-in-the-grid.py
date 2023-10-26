a4daa24ac794d617403fa3537f0bdbdbc82433bd
kamyu <kamyu104@users.noreply.github.com>
2023-10-26 18:56:53 +0800

    Update number-of-ways-to-reach-destination-in-the-grid.py

--- Python/number-of-ways-to-reach-destination-in-the-grid.py
+++ Python/number-of-ways-to-reach-destination-in-the-grid.py
@@ -66,6 +66,6 @@ class Solution2(object):
             col_same = 1
         else:
             no_same = 1
-        for i in xrange(k):
+        for _ in xrange(k):
             both_same, row_same, col_same, no_same = (row_same+col_same)%MOD, (both_same*(m-1)+row_same*(m-2)+no_same)%MOD, (both_same*(n-1)+col_same*(n-2)+no_same)%MOD, (row_same*(n-1)+col_same*(m-1)+no_same*((n-2)+(m-2)))%MOD
         return both_same
