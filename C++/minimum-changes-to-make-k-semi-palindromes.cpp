0d14472861aaa21051dc79c0d6cbeab37f941b58
kamyu <kamyu104@users.noreply.github.com>
2023-10-26 15:08:34 +0800

    Update minimum-changes-to-make-k-semi-palindromes.cpp

--- C++/minimum-changes-to-make-k-semi-palindromes.cpp
+++ C++/minimum-changes-to-make-k-semi-palindromes.cpp
@@ -1,8 +1,53 @@
 // Time:  O(n * nlogn * n + n^2 * k) = O(n^3 * logn + n^2 * k)
-// Space: O(n * nlogn + n * k) = O(n^2 * logn + n * k)
+// Space: O(nlogn + n * k)
 
 // number theory, dp
 class Solution {
+public:
+    int minimumChanges(string s, int k) {
+        vector<vector<int>> divisors(size(s) + 1);
+        for (int i = 1; i < size(divisors); ++i) {
+            for (int j = i + i; j < size(divisors); j += i) {
+                divisors[j].emplace_back(i);
+            }
+        }
+
+        const auto& dist = [&](int left, int right, int d) {
+            int result = 0;
+            for (int i = 0; i < (right - left + 1) / 2; ++i) {
+                if (s[left + i] != s[right - ((i / d + 1) * d - 1) + (i % d)]) {
+                    ++result;
+                }
+            }
+            return result;
+        };
+
+        const auto& min_dist = [&](int left, int right) {
+            int result = size(s);
+            for (const auto& d : divisors[right - left + 1]) {
+                result = min(result, dist(left, right, d));
+            }
+            return result;
+        };
+            
+        vector<vector<int>> dp(size(s) + 1, vector<int>(k + 1, size(s)));
+        dp[0][0] = 0;
+        for (int i = 0; i < size(s); ++i) {
+            for (int j = 0; j < i; ++j) {
+                const int c = min_dist(j, i);
+                for (int l = 0; l < k; ++l) {
+                    dp[i + 1][l + 1] = min(dp[i + 1][l + 1], dp[j][l] + c);
+                }
+            }
+        }
+        return dp[size(s)][k];
+    }
+};
+
+// Time:  O(n * nlogn * n + n^2 * k) = O(n^3 * logn + n^2 * k)
+// Space: O(n * nlogn + n * k) = O(n^2 * logn + n * k)
+// number theory, dp
+class Solution2 {
 public:
     int minimumChanges(string s, int k) {
         vector<vector<int>> divisors(size(s) + 1);
