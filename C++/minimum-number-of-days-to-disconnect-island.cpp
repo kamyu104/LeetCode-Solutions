91d97bab157df59fe3311626fc95c4550f5fc5fd
kamyu <kamyu104@users.noreply.github.com>
2023-10-27 14:32:40 +0800

    Update minimum-number-of-days-to-disconnect-island.cpp

--- C++/minimum-number-of-days-to-disconnect-island.cpp
+++ C++/minimum-number-of-days-to-disconnect-island.cpp
@@ -1,8 +1,134 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+// template: https://github.com/kamyu104/GoogleCodeJam-Farewell-Rounds/blob/main/Round%20B/railroad_maintenance.py3
+// Reference: https://en.wikipedia.org/wiki/Biconnected_component#Algorithms
+vector<int> iter_get_articulation_points(const auto& graph, int v) {  // modified
+    int index_counter = 0;
+    vector<int> index(size(graph), -1);
+    vector<int> lowlinks(size(graph));
+    vector<int> children_count(size(graph));
+    vector<bool> is_cut(size(graph));
+    vector<int> cutpoints;
+    const auto& iter_dfs = [&](int v, int p) {
+        vector<vector<int>> stk = {{1, v, p}};
+        while (!empty(stk)) {
+            const auto args = stk.back(); stk.pop_back();
+            if (args[0] == 1) {
+                const int v = args[1], p = args[2];
+                index[v] = index_counter;
+                lowlinks[v] = index_counter++;
+                stk.push_back({4, v, p});
+                for (const auto& w : graph[v]) {
+                    if (w == p) {
+                        continue;
+                    }
+                    stk.push_back({2, w, v});
+                }
+            } else if (args[0] == 2) {
+                const int w = args[1], v = args[2];
+                if (index[w] == -1) {
+                    ++children_count[v];
+                    stk.push_back({3, w, v});
+                    stk.push_back({1, w, v});
+                } else {
+                    lowlinks[v] = min(lowlinks[v], index[w]);
+                }
+            } else if (args[0] == 3) {
+                const int w = args[1], v = args[2];
+                if (lowlinks[w] >= index[v]) {
+                    is_cut[v] = true;
+                }
+                lowlinks[v] = min(lowlinks[v], lowlinks[w]);
+            } else if (args[0] == 4) {
+                const int v = args[1], p = args[2];
+                if ((p != -1 && is_cut[v]) || (p == -1 && children_count[v] >= 2)) {
+                    cutpoints.emplace_back(v);
+                }
+            }
+        }
+    };
+    
+    iter_dfs(v, -1);  // modified
+    return cutpoints;
+}
+
+
+// flood fill, tarjan's algorithm, articulation points
+class Solution {
+public:
+    int minDays(vector<vector<int>>& grid) {
+        static const vector<pair<int, int>> DIRECTIONS{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        const int R = size(grid), C = size(grid[0]);
+        if (count_islands(grid) != 1) {
+            return 0;
+        }
+        vector<vector<int>> adj(R * C);
+        int cnt = 0, idx = -1;
+        for (int i = 0; i < R; ++i) {
+            for (int j = 0; j < C; ++j) {
+                if (grid[i][j] == 0) {
+                    continue;
+                }
+                ++cnt;
+                if (idx == -1) {
+                    idx = i * C + j;
+                }
+                for (const auto& [di, dj] : DIRECTIONS) {
+                    const auto& [ni, nj] = pair(i + di, j + dj);
+                    if (0 <= ni && ni < R && 0 <= nj && nj < C && grid[ni][nj] == grid[i][j]) {
+                        adj[i * C + j].emplace_back(ni * C + nj);
+                    }
+                }
+                
+            }
+        }
+        return cnt == 1 || !empty(iter_get_articulation_points(adj, idx)) ? 1 : 2;
+    }
+
+private:
+    int count_islands(const vector<vector<int>>& grid) {
+        vector<vector<int>> lookup(grid.size(), vector<int>(grid[0].size()));
+        int islands_cnt = 0;
+        for (int i = 0; i < grid.size(); ++i) {
+            for (int j = 0; j < grid[0].size(); ++j) {
+                if (grid[i][j] == 0 || lookup[i][j]) {
+                    continue;
+                }
+                ++islands_cnt;
+                floodfill(grid, i, j, &lookup);
+            }
+        }
+        return islands_cnt;
+    }
+
+    void floodfill(const vector<vector<int>>& grid, int i, int j, vector<vector<int>> *lookup) {
+        static const vector<pair<int, int>> DIRECTIONS{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+        vector<pair<int, int>> stk = {{i, j}};
+        (*lookup)[i][j] = 1;
+        while (!stk.empty()) {
+            auto [i, j] = stk.back(); stk.pop_back();
+            for (const auto& [di, dj] : DIRECTIONS) {
+                const auto& [ni, nj] = pair(i + di, j + dj);
+                if (!(0 <= ni && ni < grid.size() &&
+                      0 <= nj && nj < grid[0].size() &&
+                      grid[ni][nj] &&
+                      !(*lookup)[ni][nj])) {
+                    continue;
+                }
+                (*lookup)[ni][nj] = 1;
+                stk.emplace_back(ni, nj);
+            }
+        }
+    }
+};
+
 // Time:  O((m * n) * log(m * n))
 // Space: O((m * n) * log(m * n))
-
 // flood fill, persistent union find
-class Solution {
+class Solution2 {
 public:
     int minDays(vector<vector<int>>& grid) {
         static const vector<pair<int, int>> DIRECTIONS{{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
@@ -168,7 +294,7 @@ private:
 // Time:  O(m^2 * n^2)
 // Space: O(m * n)
 // flood fill
-class Solution2 {
+class Solution3 {
 public:
     int minDays(vector<vector<int>>& grid) {
         if (count_islands(grid) != 1) {
