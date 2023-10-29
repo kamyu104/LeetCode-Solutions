bd11d88aa707c9a60420fdc62f9001e9c7d6ade7
kamyu <kamyu104@users.noreply.github.com>
2023-10-29 19:53:01 +0800

    Update subarrays-distinct-element-sum-of-squares-ii.cpp

--- C++/subarrays-distinct-element-sum-of-squares-ii.cpp
+++ C++/subarrays-distinct-element-sum-of-squares-ii.cpp
@@ -7,6 +7,122 @@ public:
     int sumCounts(vector<int>& nums) {
         static const int MOD = 1e9 + 7;
 
+        int result = 0, accu = 0;
+        SegmentTree st(size(nums));
+        unordered_map<int, int> lookup;
+        for (int i = 0; i < size(nums); ++i) {
+            const int j = lookup.count(nums[i]) ? lookup[nums[i]] : -1;
+            // sum(count(k, i)^2 for k in range(i+1)) - sum(count(k, i-1)^2 for k in range(i))
+            // = sum(2*count(k, i-1)+1 for k in range(j+1, i+1))
+            // = (i-j) + sum(2*count(k, i-1) for k in range(j+1, i+1))
+            accu = (accu + ((i - j) + 2ll * st.query(j + 1, i))) % MOD;
+            result = (result + accu) % MOD;
+            st.update(j + 1, i, 1);  // count(k, i) = count(k, i-1)+(1 if k >= j+1 else 0) for k in range(i+1)
+            lookup[nums[i]] = i;
+        }
+        return result;
+    }
+
+private:
+    class SegmentTree {
+    private:
+        static const int MOD = 1e9 + 7;
+
+    public:
+        explicit SegmentTree(
+            int N)
+         :  base_(N > 1 ? 1 << (__lg(N - 1) + 1) : 1),
+            lazy_(base_),
+            tree_(N > 1 ? 1 << (__lg(N - 1) + 2) : 2),
+            count_(size(tree_), 1) {
+
+            for (int i = base_ - 1; i >= 1; --i) { // added
+                count_[i] = count_[i << 1] + count_[(i << 1) + 1];
+            }
+        }
+
+        void update(int L, int R, const int val) {
+            L += base_;
+            R += base_;
+            // push(L);  // enable if range assignment
+            // push(R);  // enable if range assignment
+            int L0 = L, R0 = R;
+            for (; L <= R; L >>= 1, R >>= 1) {
+                if ((L & 1) == 1) {
+                    apply(L++, val);
+                }
+                if ((R & 1) == 0) {
+                    apply(R--, val);
+                }
+            }
+            pull(L0);
+            pull(R0);
+        }
+
+        int query(int L, int R) {
+            if (L > R) {
+                return 0;
+            }
+            L += base_;
+            R += base_;
+            push(L);
+            push(R);
+            int left = 0, right = 0;
+            for (; L <= R; L >>= 1, R >>= 1) {
+                if ((L & 1) == 1) {
+                    left = (tree_[L++] + left) % MOD;
+                }
+                if ((R & 1) == 0) {
+                    right = (right + tree_[R--]) % MOD;
+                }
+            }
+            return (left + right) % MOD;
+        }
+
+    private:
+        void apply(int x, const int val) {
+            tree_[x] = (tree_[x] + val * count_[x]) % MOD;  // modified
+            if (x < base_) {
+                lazy_[x] = (lazy_[x] + val) % MOD;
+            }
+        }
+
+        void pull(int x) {
+            while (x > 1) {
+                x >>= 1;
+                tree_[x] = (tree_[x << 1] + tree_[(x << 1) + 1]) % MOD;
+                if (lazy_[x]) {
+                    tree_[x] = (tree_[x] + lazy_[x] * count_[x]) % MOD;  // modified
+                }
+            }
+        }
+
+        void push(int x) {
+            for (int h = __lg(base_) - 1; h > 0; --h) {
+                int y = x >> h;
+                if (lazy_[y]) {
+                    apply(y << 1, lazy_[y]);
+                    apply((y << 1) + 1, lazy_[y]);
+                    lazy_[y] = 0;
+                }
+            }
+        }
+
+        int base_;
+        vector<int> tree_;
+        vector<int> lazy_;
+        vector<int> count_;  // added
+    };
+};
+
+// Time:  O(nlogn)
+// Space: O(n)
+// dp, segment tree, math
+class Solution2 {
+public:
+    int sumCounts(vector<int>& nums) {
+        static const int MOD = 1e9 + 7;
+
         const auto& sum = [&] (const auto& x, const auto& y) {
             return (x + y) % MOD;
         };
