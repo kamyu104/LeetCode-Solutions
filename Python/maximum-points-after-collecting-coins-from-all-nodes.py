7cc9fd05a3e4b41d0b63d5634e9f5a8a4fec690a
kamyu <kamyu104@users.noreply.github.com>
2023-10-30 20:06:05 +0800

    Update maximum-points-after-collecting-coins-from-all-nodes.py

--- Python/maximum-points-after-collecting-coins-from-all-nodes.py
+++ Python/maximum-points-after-collecting-coins-from-all-nodes.py
@@ -10,14 +10,15 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        NEG_INF = float("-inf")
         def dfs(u, p, base):
             if base >= max_base:
                 return 0
             if (lookup[u]&base):  # we prefer the first way to the second way, so the visited state cannot improve the current chosen ways
-                return float("-inf")
+                return NEG_INF
             lookup[u] |= base
             return max(((coins[u]//base)-k)+sum(dfs(v, u, base) for v in adj[u] if v != p),
-                        (coins[u]//(base<<1))+sum(dfs(v, u, base<<1) for v in adj[u] if v != p) if (coins[u]//base)-k < coins[u]//(base*2) else 0)  #  if (coins[u]//base)-k >= coins[u]//(base*2), the first way is always better
+                        (coins[u]//(base<<1))+sum(dfs(v, u, base<<1) for v in adj[u] if v != p) if (coins[u]//base)-k < coins[u]//(base*2) else NEG_INF)  #  if (coins[u]//base)-k >= coins[u]//(base*2), the first way is always better
 
         adj = [[] for _ in xrange(len(coins))]
         for u, v in edges:
