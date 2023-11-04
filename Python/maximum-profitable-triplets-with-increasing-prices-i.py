e68eb97f8133aa31272c24d0b14fb00b409124c1
kamyu <kamyu104@users.noreply.github.com>
2023-11-05 02:36:01 +0800

    Update maximum-profitable-triplets-with-increasing-prices-i.py

--- Python/maximum-profitable-triplets-with-increasing-prices-i.py
+++ Python/maximum-profitable-triplets-with-increasing-prices-i.py
@@ -1,49 +1,6 @@
 # Time:  O(nlogn)
 # Space: O(n)
 
-import itertools
-
-
-# prefix sum, bit, fenwick tree
-class Solution(object):
-    def maxProfit(self, prices, profits):
-        """
-        :type prices: List[int]
-        :type profits: List[int]
-        :rtype: int
-        """
-        NEG_INF = float("-inf")
-        class BIT(object):  # 0-indexed.
-            def __init__(self, n, val=0, fn=lambda x, y: x+y):
-                self.__bit = [val]*(n+1)  # Extra one for dummy node.
-                self.__fn = fn
-
-            def update(self, i, val):
-                i += 1  # Extra one for dummy node.
-                while i < len(self.__bit):
-                    self.__bit[i] = self.__fn(self.__bit[i], val)
-                    i += (i & -i)
-
-            def query(self, i):
-                i += 1  # Extra one for dummy node.
-                ret = self.__bit[0]
-                while i > 0:
-                    ret = self.__fn(ret, self.__bit[i])
-                    i -= (i & -i)
-                return ret
-
-        price_to_idx = {x:i for i, x in enumerate(sorted(set(prices)))}
-        bit1, bit2 = BIT(len(price_to_idx), val=NEG_INF, fn=max), BIT(len(price_to_idx), val=NEG_INF, fn=max)
-        result = NEG_INF
-        for price, profit in itertools.izip(prices, profits):
-            result = max(result, bit2.query(price_to_idx[price]-1)+profit)
-            bit1.update(price_to_idx[price], profit)
-            bit2.update(price_to_idx[price], bit1.query(price_to_idx[price]-1)+profit)
-        return result if result != NEG_INF else -1
-
-
-# Time:  O(nlogn)
-# Space: O(n)
 from sortedcontainers import SortedList
 
 
@@ -130,6 +87,50 @@ class Solution3(object):
             while j+1 < len(sl) and sl[j+1][1] <= sl[j][1]:
                 del sl[j+1]
         result = max(left[i]+profits[i]+right[i] for i in xrange(len(profits)))
+        return result if result != NEG_INF else -1
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import itertools
+
+
+# prefix sum, bit, fenwick tree
+class Solution3(object):
+    def maxProfit(self, prices, profits):
+        """
+        :type prices: List[int]
+        :type profits: List[int]
+        :rtype: int
+        """
+        NEG_INF = float("-inf")
+        class BIT(object):  # 0-indexed.
+            def __init__(self, n, default=0, fn=lambda x, y: x+y):
+                self.__bit = [NEG_INF]*(n+1)  # Extra one for dummy node.
+                self.__default = default
+                self.__fn = fn
+
+            def update(self, i, val):
+                i += 1  # Extra one for dummy node.
+                while i < len(self.__bit):
+                    self.__bit[i] = self.__fn(self.__bit[i], val)
+                    i += (i & -i)
+
+            def query(self, i):
+                i += 1  # Extra one for dummy node.
+                ret = self.__default
+                while i > 0:
+                    ret = self.__fn(ret, self.__bit[i])
+                    i -= (i & -i)
+                return ret
+
+        price_to_idx = {x:i for i, x in enumerate(sorted(set(prices)))}
+        bit1, bit2 = BIT(len(price_to_idx), default=NEG_INF, fn=max), BIT(len(price_to_idx), default=NEG_INF, fn=max)
+        result = NEG_INF
+        for price, profit in itertools.izip(prices, profits):
+            result = max(result, bit2.query(price_to_idx[price]-1)+profit)
+            bit1.update(price_to_idx[price], profit)
+            bit2.update(price_to_idx[price], bit1.query(price_to_idx[price]-1)+profit)
         return result if result != NEG_INF else -1
 
 
