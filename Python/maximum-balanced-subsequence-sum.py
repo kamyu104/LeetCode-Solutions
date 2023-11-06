de40c63ca6e978749af05df70d153cac54651be7
kamyu <kamyu104@users.noreply.github.com>
2023-11-06 11:48:13 +0800

    Update maximum-balanced-subsequence-sum.py

--- Python/maximum-balanced-subsequence-sum.py
+++ Python/maximum-balanced-subsequence-sum.py
@@ -75,3 +75,62 @@ class Solution2(object):
             result = max(result, v)
             bit.update(val_to_idx[x-i], v)
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# segment tree
+class Solution3(object):
+    def maxBalancedSubsequenceSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        NEG_INF = float("-inf")
+        # Range Maximum Query
+        class SegmentTree(object):
+            def __init__(self, N,
+                         build_fn=lambda _: None,
+                         query_fn=lambda x, y: max(x, y),
+                         update_fn=lambda x, y: max(x, y)):
+                self.tree = [None]*(2*2**((N-1).bit_length()))
+                self.base = len(self.tree)//2
+                self.query_fn = query_fn
+                self.update_fn = update_fn
+                for i in xrange(self.base, self.base+N):
+                    self.tree[i] = build_fn(i-self.base)
+                for i in reversed(xrange(1, self.base)):
+                    self.tree[i] = query_fn(self.tree[2*i], self.tree[2*i+1])
+
+            def update(self, i, h):
+                x = self.base+i
+                self.tree[x] = self.update_fn(self.tree[x], h)
+                while x > 1:
+                    x //= 2
+                    self.tree[x] = self.query_fn(self.tree[x*2], self.tree[x*2+1])
+
+            def query(self, L, R):
+                if L > R:
+                    return None
+                L += self.base
+                R += self.base
+                left = right = None
+                while L <= R:
+                    if L & 1:
+                        left = self.query_fn(left, self.tree[L])
+                        L += 1
+                    if R & 1 == 0:
+                        right = self.query_fn(self.tree[R], right)
+                        R -= 1
+                    L //= 2
+                    R //= 2
+                return self.query_fn(left, right)
+
+        val_to_idx = {x:i for i, x in enumerate(sorted({x-i for i, x in enumerate(nums)}))}
+        result = NEG_INF
+        st = SegmentTree(len(val_to_idx))
+        for i, x in enumerate(nums):
+            v = max(st.query(0, val_to_idx[x-i]), 0)+x
+            result = max(result, v)
+            st.update(val_to_idx[x-i], v)
+        return result
