f69f61f29bf078a9b5034340dcc098f2be96eab4
kamyu <kamyu104@users.noreply.github.com>
2023-11-10 11:53:16 +0800

    Create distribute-candies-among-children-iii.cpp

---
+++ C++/distribute-candies-among-children-iii.cpp
@@ -0,0 +1,35 @@
+// Time:  O(1)
+// Space: O(1)
+
+// stars and bars, combinatorics, inclusion-exclusion principle 
+class Solution {
+public:
+    long long distributeCandies(int n, int limit) {
+        const auto& nCr = [](int n, int r) {  // Time: O(n), Space: O(1)
+            if (!(0 <= r && r <= n)) {
+                return static_cast<int64_t>(0);
+            }
+            if (n - r < r) {
+                r = n - r;
+            }
+            int64_t result = 1;
+            for (int k = 1; k <= r; ++k) {
+                result *= n - k + 1;
+                result /= k;
+            }
+            return result;
+        };
+        
+        const auto& nHr = [&](int n, int r) {
+            return nCr(n + (r - 1), r - 1);
+        };
+    
+        static const int R = 3;
+    
+        int64_t result = 0;
+        for (int r = 0; r <= R; ++r) {
+            result += (r % 2 ? -1 : 1) * nCr(R, r) * nHr(n - r * (limit + 1), R);
+        }
+        return result;
+    }
+};
