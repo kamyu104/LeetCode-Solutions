4cb6307a28ed915ebd6e3550b99fc80aee55fdaf
kamyu <kamyu104@users.noreply.github.com>
2023-11-10 11:49:57 +0800

    Update distribute-candies-among-children-iii.py

--- Python/distribute-candies-among-children-iii.py
+++ Python/distribute-candies-among-children-iii.py
@@ -24,4 +24,4 @@ class Solution(object):
             return nCr(n+(r-1), r-1)
     
         R = 3
-        return sum((-1 if r%2 else 1) * nCr(R, r) * nHr(n-r*(limit+1), R)for r in xrange(R+1))
+        return sum((-1 if r%2 else 1) * nCr(R, r) * nHr(n-r*(limit+1), R) for r in xrange(R+1))
