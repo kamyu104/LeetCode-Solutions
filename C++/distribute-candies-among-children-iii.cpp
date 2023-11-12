5879670602fe84bcfc62e915cdb19eb024d7f839
kamyu <kamyu104@users.noreply.github.com>
2023-11-13 01:11:41 +0800

    Update distribute-candies-among-children-iii.cpp

--- C++/distribute-candies-among-children-iii.cpp
+++ C++/distribute-candies-among-children-iii.cpp
@@ -1,7 +1,7 @@
 // Time:  O(1)
 // Space: O(1)
 
-// stars and bars, combinatorics, inclusion-exclusion principle 
+// stars and bars, combinatorics, principle of inclusion and exclusion
 class Solution {
 public:
     long long distributeCandies(int n, int limit) {
