0524d19b03aaa2e40e508232121e7a4efc252f8f
kamyu <kamyu104@users.noreply.github.com>
2023-11-13 13:17:42 +0800

    Update maximum-xor-of-two-numbers-in-an-array.cpp

--- C++/maximum-xor-of-two-numbers-in-an-array.cpp
+++ C++/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -1,7 +1,72 @@
-// Time:  O(nlogk), k is max(nums)
-// Space: O(n)
+// Time:  O(nlogr), r = max(nums)
+// Space: O(t)
 
 class Solution {
+private:
+    class Trie {
+    public:
+        Trie(int bit_length)
+         : bit_length_(bit_length)
+         , nodes_() {
+             new_node();
+         }
+
+        void insert(int num) {
+            int curr = 0;
+            for (int i = bit_length_ - 1; i >= 0; --i) {
+                int x = num >> i;
+                if (nodes_[curr][x & 1] == -1) {
+                    nodes_[curr][x & 1] = new_node();
+                }
+                curr = nodes_[curr][x & 1];
+            }
+        }
+
+        int query(int num) {
+            int result = 0, curr = 0;
+            for (int i = bit_length_ - 1; i >= 0; --i) {
+                result <<= 1;
+                const int x = num >> i;
+                if (nodes_[curr][1 ^ (x & 1)] != -1) {
+                    result |= 1;
+                    curr = nodes_[curr][1 ^ (x & 1)];
+                } else {
+                    curr = nodes_[curr][x & 1];
+                }
+            }
+            return result;
+        }
+
+    private:
+        int new_node() {
+            nodes_.push_back(array<int, 2>{-1, -1});
+            return size(nodes_) - 1;
+        }
+
+        const int bit_length_;
+        vector<array<int, 2>> nodes_;
+    };
+
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie(bit_length(*max_element(cbegin(nums), cend(nums))));
+        int result = 0;
+        for (const auto& num : nums) {
+            trie.insert(num);
+            result = max(result, trie.query(num));
+        }
+        return result;
+    }
+
+private:
+    int bit_length(int x) {
+        return x != 0 ? 32 - __builtin_clz(x) : 1;
+    }
+};
+
+// Time:  O(nlogr), r = max(nums)
+// Space: O(n)
+class Solution2 {
 public:
     int findMaximumXOR(vector<int>& nums) {
         int result = 0;
@@ -20,69 +85,6 @@ public:
             }
         }
 
-        return result;
-    }
-
-private:
-    int bit_length(int x) {
-        return x != 0 ? 32 - __builtin_clz(x) : 1;
-    }
-};
-
-
-// Time:  O(nlogk), k is max(nums)
-// Space: O(nlogk)
-class Solution2 {
-private:
-    class Trie {
-    public:
-        Trie(int bit_length)
-         : bit_length_(bit_length)
-         , nodes_(1) {}
-
-        void insert(int num) {
-            int idx = 0;
-            for (int i = bit_length_ - 1; i >= 0; --i) {
-                int curr = (num >> i) & 1;
-                if (!nodes_[idx][curr]) {
-                    nodes_.emplace_back();
-                    nodes_[idx][curr] = size(nodes_) - 1;
-                }
-                idx = nodes_[idx][curr];
-            }
-        }
-
-        int query(int num) {
-            if (size(nodes_) == 1) {
-                return -1;
-            }
-            int result = 0, idx = 0;
-            for (int i = bit_length_ - 1; i >= 0; --i) {
-                result <<= 1;
-                int curr = (num >> i) & 1;
-                if (nodes_[idx][1 ^ curr]) {
-                    idx = nodes_[idx][1 ^ curr];
-                    result |= 1;
-                } else {
-                    idx = nodes_[idx][curr];
-                }
-            }
-            return result;
-        }
-
-    private:
-        const int bit_length_;
-        vector<array<int, 2>> nodes_;
-    };
-
-public:
-    int findMaximumXOR(vector<int>& nums) {
-        Trie trie(bit_length(*max_element(cbegin(nums), cend(nums))));
-        int result = 0;
-        for (const auto& num : nums) {
-            trie.insert(num);
-            result = max(result, trie.query(num));
-        }
         return result;
     }
 
