3ec1d36917745d43b35ab01e20d93bd299646568
kamyu <kamyu104@users.noreply.github.com>
2023-11-13 09:49:51 +0800

    Update minimum-operations-to-maximize-last-elements-in-arrays.py

--- Python/minimum-operations-to-maximize-last-elements-in-arrays.py
+++ Python/minimum-operations-to-maximize-last-elements-in-arrays.py
@@ -10,8 +10,8 @@ class Solution(object):
         :rtype: int
         """
         INF = float("inf")
-        def f(nums1, num2, mx1, mx2):
+        def count(nums1, num2, mx1, mx2):
             return sum(1 if nums2[i] <= mx1 and nums1[i] <= mx2 else INF for i in xrange(len(nums1)-1) if not (nums1[i] <= mx1 and nums2[i] <= mx2))
 
-        result = min(f(nums1, nums2, nums1[-1], nums2[-1]), f(nums1, nums2, nums2[-1], nums1[-1])+1)
+        result = min(count(nums1, nums2, nums1[-1], nums2[-1]), count(nums1, nums2, nums2[-1], nums1[-1])+1)
         return result if result != INF else -1
