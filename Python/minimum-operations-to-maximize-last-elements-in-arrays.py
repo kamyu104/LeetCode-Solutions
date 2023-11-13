151fe32aa3d55fd2af335ef1c478529181a3f1bf
kamyu <kamyu104@users.noreply.github.com>
2023-11-13 10:11:02 +0800

    Update minimum-operations-to-maximize-last-elements-in-arrays.py

--- Python/minimum-operations-to-maximize-last-elements-in-arrays.py
+++ Python/minimum-operations-to-maximize-last-elements-in-arrays.py
@@ -1,8 +1,35 @@
 # Time:  O(n)
 # Space: O(1)
 
+import itertools
+
+
 # simulation
 class Solution(object):
+    def minOperations(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        cnt = [0]*2
+        for x, y in itertools.izip(nums1, nums2):
+            if not (min(x, y) <= min(nums1[-1], nums2[-1]) and max(x, y) <= max(nums1[-1], nums2[-1])):
+                return -1
+            if not (x <= nums1[-1] and y <= nums2[-1]):
+                cnt[0] += 1
+            if not (x <= nums2[-1] and y <= nums1[-1]):
+                cnt[1] += 1
+        return min(cnt)
+
+
+# Time:  O(n)
+# Space: O(1)
+import itertools
+
+
+# simulation
+class Solution2(object):
     def minOperations(self, nums1, nums2):
         """
         :type nums1: List[int]
