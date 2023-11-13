60512c69a43ef02b144e30ad6783c9fb0cd8d7e9
kamyu <kamyu104@users.noreply.github.com>
2023-11-13 11:13:10 +0800

    Update maximum-strong-pair-xor-ii.py

--- Python/maximum-strong-pair-xor-ii.py
+++ Python/maximum-strong-pair-xor-ii.py
@@ -1,8 +1,62 @@
-# Time:  O(nlogr), r = max(nums)
+# Time:  O(nlogn + nlogr) = O(nlogr), r = max(nums)
 # Space: O(t)
 
-# bit manipulation, greedy, trie, dp
+# bit manipulation, greedy, trie, sort, two pointers
 class Solution(object):
+    def maximumStrongPairXor(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        class Trie(object):
+            def __init__(self, bit_length):
+                self.__nodes = []
+                self.__cnts = []
+                self.__new_node()
+                self.__bit_length = bit_length
+            
+            def __new_node(self):
+                self.__nodes.append([-1]*2)
+                self.__cnts.append(0)
+                return len(self.__nodes)-1
+
+            def update(self, num, d):
+                curr = 0
+                for i in reversed(xrange(self.__bit_length)):
+                    x = num>>i
+                    if self.__nodes[curr][x&1] == -1:
+                        self.__nodes[curr][x&1] = self.__new_node()
+                    curr = self.__nodes[curr][x&1]
+                    self.__cnts[curr] += d
+                        
+            def query(self, num):
+                result = curr = 0
+                for i in reversed(xrange(self.__bit_length)):
+                    result <<= 1
+                    x = num>>i
+                    if self.__nodes[curr][1^(x&1)] != -1 and self.__cnts[self.__nodes[curr][1^(x&1)]]:
+                        curr = self.__nodes[curr][1^(x&1)]
+                        result |= 1
+                    else:
+                        curr = self.__nodes[curr][x&1]
+                return result
+    
+        nums.sort()
+        trie = Trie(nums[-1].bit_length())
+        result = j = 0
+        for i, num in enumerate(nums):
+            trie.update(num, +1)
+            while not (nums[i] <= 2*nums[j]) :
+                trie.update(nums[j], -1)
+                j += 1
+            result = max(result, trie.query(num))
+        return result
+
+
+# Time:  O(nlogr), r = max(nums)
+# Space: O(t)
+# bit manipulation, greedy, trie, dp
+class Solution2(object):
     def maximumStrongPairXor(self, nums):
         """
         :type nums: List[int]
@@ -56,60 +110,6 @@ class Solution(object):
         result = 0
         for num in nums:
             trie.insert(num)
-            result = max(result, trie.query(num))
-        return result
-
-
-# Time:  O(nlogn + nlogr) = O(nlogr), r = max(nums)
-# Space: O(t)
-# bit manipulation, greedy, trie, sort, two pointers
-class Solution2(object):
-    def maximumStrongPairXor(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        class Trie(object):
-            def __init__(self, bit_length):
-                self.__nodes = []
-                self.__cnts = []
-                self.__new_node()
-                self.__bit_length = bit_length
-            
-            def __new_node(self):
-                self.__nodes.append([-1]*2)
-                self.__cnts.append(0)
-                return len(self.__nodes)-1
-
-            def update(self, num, d):
-                curr = 0
-                for i in reversed(xrange(self.__bit_length)):
-                    x = num>>i
-                    if self.__nodes[curr][x&1] == -1:
-                        self.__nodes[curr][x&1] = self.__new_node()
-                    curr = self.__nodes[curr][x&1]
-                    self.__cnts[curr] += d
-                        
-            def query(self, num):
-                result = curr = 0
-                for i in reversed(xrange(self.__bit_length)):
-                    result <<= 1
-                    x = num>>i
-                    if self.__nodes[curr][1^(x&1)] != -1 and self.__cnts[self.__nodes[curr][1^(x&1)]]:
-                        curr = self.__nodes[curr][1^(x&1)]
-                        result |= 1
-                    else:
-                        curr = self.__nodes[curr][x&1]
-                return result
-    
-        nums.sort()
-        trie = Trie(nums[-1].bit_length())
-        result = j = 0
-        for i, num in enumerate(nums):
-            trie.update(num, +1)
-            while not (nums[i] <= 2*nums[j]) :
-                trie.update(nums[j], -1)
-                j += 1
             result = max(result, trie.query(num))
         return result
 
