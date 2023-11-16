44f3f14dfb9d0f3c7c5ab3ddbfa8a3e5eabef15a
kamyu <kamyu104@users.noreply.github.com>
2023-11-16 12:42:01 +0800

    Create number-of-equal-numbers-blocks.py

---
+++ Python/number-of-equal-numbers-blocks.py
@@ -0,0 +1,35 @@
+# Time:  O(klogn), k = len(set(nums))
+# Space: O(1)
+
+# Definition for BigArray.
+class BigArray:
+    def at(self, index):
+        pass
+    def size(self):
+        pass
+
+
+# binary search
+class Solution(object):
+    def countBlocks(self, nums):
+        """
+        :type nums: BigArray
+        :rtype: int
+        """
+        def binary_search_right(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if not check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return right
+
+        n = nums.size()
+        result = 0
+        left, right = 0, n-1
+        while left != n:
+            target = nums.at(left)
+            left = binary_search_right(left, right, lambda x: nums.at(x) == target)+1
+            result += 1
+        return result
