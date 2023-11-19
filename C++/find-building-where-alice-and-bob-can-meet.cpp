eb077985007a82e83a44a6c1ea5d7650155b9118
kamyu <kamyu104@users.noreply.github.com>
2023-11-19 19:53:43 +0800

    Create find-building-where-alice-and-bob-can-meet.cpp

---
+++ C++/find-building-where-alice-and-bob-can-meet.cpp
@@ -0,0 +1,82 @@
+// Time:  O(n + qlogq)
+// Space: O(n + q)
+
+// heap
+class Solution {
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        vector<int> result(size(queries), -1);
+        vector<vector<pair<int, int>>> qs(size(heights));
+        for (int i = 0; i < size(queries); ++i) {
+            int a = queries[i][0], b = queries[i][1];
+            if (a > b) {
+                swap(a, b);
+            }
+            if (a == b || heights[a] < heights[b]) {
+                result[i] = b;
+            } else {
+                qs[b].emplace_back(heights[a], i);
+            }
+        }
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+        for (int i = 0; i < size(heights); ++i) {
+            for (const auto& q : qs[i]) {
+                min_heap.emplace(q);
+            }
+            while (!empty(min_heap) && min_heap.top().first < heights[i]) {
+                const auto [_, j] = min_heap.top(); min_heap.pop();
+                result[j] = i;
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n + qlogn)
+// Space: O(n + q)
+// mono stack, binary search
+class Solution2 {
+public:
+    vector<int> leftmostBuildingQueries(vector<int>& heights, vector<vector<int>>& queries) {
+        vector<int> result(size(queries), -1);
+        vector<vector<pair<int, int>>> qs(size(heights));
+        for (int i = 0; i < size(queries); ++i) {
+            int a = queries[i][0], b = queries[i][1];
+            if (a > b) {
+                swap(a, b);
+            }
+            if (a == b || heights[a] < heights[b]) {
+                result[i] = b;
+            } else {
+                qs[b].emplace_back(heights[a], i);
+            }
+        }
+        vector<pair<int, int>> stk;
+        const auto& binary_search_right = [&](int left, int right, const auto& check) {
+            while (left <= right) {
+                const int mid = left + (right - left) / 2;
+                if (!check(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return right;
+        };
+        for (int b = size(heights) - 1; b >= 0; --b) {
+            while (!empty(stk) && stk.back().first <= heights[b]) {
+                stk.pop_back();
+            }
+            stk.emplace_back(heights[b], b);
+            for (const auto& [ha, i] : qs[b]) {
+                const int j = binary_search_right(0, size(stk) - 1, [&, ha=ha](int x) {
+                    return stk[x].first > ha;
+                });
+                if (j >= 0) {
+                    result[i] = stk[j].second;
+                }
+            }
+        }
+        return result;
+    }
+};
