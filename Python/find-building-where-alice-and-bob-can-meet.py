07d598a3f8a2ba34b1c5a2ec7659ca0c1f67f900
kamyu <kamyu104@users.noreply.github.com>
2023-11-20 02:19:14 +0800

    Update find-building-where-alice-and-bob-can-meet.py

--- Python/find-building-where-alice-and-bob-can-meet.py
+++ Python/find-building-where-alice-and-bob-can-meet.py
@@ -1,80 +1,8 @@
-# Time:  O(n + qlogq)
-# Space: O(n + q)
-
-import heapq
-
-
-# offline solution, heap
-class Solution(object):
-    def leftmostBuildingQueries(self, heights, queries):
-        """
-        :type heights: List[int]
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        result = [-1]*len(queries)
-        qs = [[] for _ in xrange(len(heights))]
-        for i, (a, b) in enumerate(queries):
-            if a > b:
-                a, b = b, a
-            if a == b or heights[a] < heights[b]:
-                result[i] = b
-            else:
-                qs[b].append((heights[a], i))
-        min_heap = []
-        for i, h in enumerate(heights):
-            for q in qs[i]:
-                heapq.heappush(min_heap, q)
-            while min_heap and min_heap[0][0] < h:
-                _, j = heapq.heappop(min_heap)
-                result[j] = i
-        return result
-
-
-# Time:  O(n + qlogn)
-# Space: O(n + q)
-# offline solution, mono stack, binary search
-class Solution2(object):
-    def leftmostBuildingQueries(self, heights, queries):
-        """
-        :type heights: List[int]
-        :type queries: List[List[int]]
-        :rtype: List[int]
-        """
-        def binary_search_right(left, right, check):
-            while left <= right:
-                mid = left + (right-left)//2
-                if not check(mid):
-                    right = mid-1
-                else:
-                    left = mid+1
-            return right
-
-        result = [-1]*len(queries)
-        qs = [[] for _ in xrange(len(heights))]
-        for i, (a, b) in enumerate(queries):
-            if a > b:
-                a, b = b, a
-            if a == b or heights[a] < heights[b]:
-                result[i] = b
-            else:
-                qs[b].append((heights[a], i))
-        stk = []
-        for b in reversed(xrange(len(heights))):
-            while stk and stk[-1][0] <= heights[b]:
-                stk.pop()
-            stk.append((heights[b], b))
-            for ha, i in qs[b]:
-                j = binary_search_right(0, len(stk)-1, lambda x: stk[x][0] > ha)
-                if j >= 0:
-                    result[i] = stk[j][1]
-        return result
-
-
 # Time:  O(n + qlogn)
 # Space: O(n)
+
 # online solution, segment tree, binary search
-class Solution3(object):
+class Solution(object):
     def leftmostBuildingQueries(self, heights, queries):
         """
         :type heights: List[int]
@@ -126,4 +54,76 @@ class Solution3(object):
                 continue
             i = st.binary_search(b+1, len(heights)-1, 0, len(heights)-1, 1, heights[a])
             result.append(i if i != INF else -1)
+        return result
+
+
+# Time:  O(n + qlogq)
+# Space: O(n + q)
+import heapq
+
+
+# offline solution, heap
+class Solution2(object):
+    def leftmostBuildingQueries(self, heights, queries):
+        """
+        :type heights: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        result = [-1]*len(queries)
+        qs = [[] for _ in xrange(len(heights))]
+        for i, (a, b) in enumerate(queries):
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                result[i] = b
+            else:
+                qs[b].append((heights[a], i))
+        min_heap = []
+        for i, h in enumerate(heights):
+            for q in qs[i]:
+                heapq.heappush(min_heap, q)
+            while min_heap and min_heap[0][0] < h:
+                _, j = heapq.heappop(min_heap)
+                result[j] = i
+        return result
+
+
+# Time:  O(n + qlogn)
+# Space: O(n + q)
+# offline solution, mono stack, binary search
+class Solution3(object):
+    def leftmostBuildingQueries(self, heights, queries):
+        """
+        :type heights: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        def binary_search_right(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if not check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return right
+
+        result = [-1]*len(queries)
+        qs = [[] for _ in xrange(len(heights))]
+        for i, (a, b) in enumerate(queries):
+            if a > b:
+                a, b = b, a
+            if a == b or heights[a] < heights[b]:
+                result[i] = b
+            else:
+                qs[b].append((heights[a], i))
+        stk = []
+        for b in reversed(xrange(len(heights))):
+            while stk and stk[-1][0] <= heights[b]:
+                stk.pop()
+            stk.append((heights[b], b))
+            for ha, i in qs[b]:
+                j = binary_search_right(0, len(stk)-1, lambda x: stk[x][0] > ha)
+                if j >= 0:
+                    result[i] = stk[j][1]
         return result
