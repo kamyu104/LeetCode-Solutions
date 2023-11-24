212a67d88cb2f5ee98c45f644955e422ff044858
kamyu <kamyu104@users.noreply.github.com>
2023-11-24 13:09:30 +0800

    Update maximum-gcd-sum-of-a-subarray.py

--- Python/maximum-gcd-sum-of-a-subarray.py
+++ Python/maximum-gcd-sum-of-a-subarray.py
@@ -14,24 +14,21 @@ class Solution(object):
                 a, b = b, a%b
             return a
 
-        result = prev = 0
-        prefix = {0:0}
+        result = prefix = 0
         dp = []
         for right, x in enumerate(nums):
-            dp.append((right, x))
+            dp.append((right, x, prefix))
+            prefix += x
             new_dp = []
-            for left, g in dp:  # Time:  O(logr)
+            for left, g, p in dp:  # Time:  O(logr)
                 ng = gcd(g, x)  # Total Time: O(nlogr)
                 if not new_dp or new_dp[-1][1] != ng:
-                    new_dp.append((left, ng))  # left and ng are both non-decreasing
+                    new_dp.append((left, ng, p))  # left and ng are both non-decreasing
             dp = new_dp
-            prefix = {left:prefix[left] for left, _ in dp}
-            prefix[right+1] = prev+x
-            prev = prefix[right+1]
-            for left, g in dp:
+            for left, g, p in dp:
                 if right-left+1 < k:
                     break
-                result = max(result, (prefix[right+1]-prefix[left])*g)
+                result = max(result, (prefix-p)*g)
         return result
 
 
