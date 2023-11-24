d69f6501aa5bab03f1b1c1492c3d4eeac4948227
kamyu <kamyu104@users.noreply.github.com>
2023-11-24 14:29:37 +0800

    Update maximum-gcd-sum-of-a-subarray.py

--- Python/maximum-gcd-sum-of-a-subarray.py
+++ Python/maximum-gcd-sum-of-a-subarray.py
@@ -23,7 +23,7 @@ class Solution(object):
             for left, g, p in dp:  # Time:  O(logr)
                 ng = gcd(g, x)  # Total Time: O(nlogr)
                 if not new_dp or new_dp[-1][1] != ng:
-                    new_dp.append((left, ng, p))  # left and ng are both non-decreasing
+                    new_dp.append((left, ng, p))  # left and ng are both strictly increasing
             dp = new_dp
             for left, g, p in dp:
                 if right-left+1 < k:
@@ -58,7 +58,7 @@ class Solution2(object):
             for left, g in dp:  # Time:  O(logr)
                 ng = gcd(g, x)  # Total Time: O(nlogr)
                 if not new_dp or new_dp[-1][1] != ng:
-                    new_dp.append((left, ng))  # left and ng are both non-decreasing
+                    new_dp.append((left, ng))  # left and ng are both strictly increasing
             dp = new_dp
             for left, g in dp:
                 if right-left+1 < k:
