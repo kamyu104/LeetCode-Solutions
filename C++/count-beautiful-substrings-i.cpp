0f612267090a32ee9185acf404a4edf3ac5dde86
kamyu <kamyu104@users.noreply.github.com>
2023-11-27 00:17:25 +0800

    Update count-beautiful-substrings-i.cpp

--- C++/count-beautiful-substrings-i.cpp
+++ C++/count-beautiful-substrings-i.cpp
@@ -1,7 +1,7 @@
 // Time:  O(n + sqrt(k))
 // Space: O(n)
 
-// prefix sum, freq table
+// number theory, prefix sum, freq table
 class Solution {
 public:
     int beautifulSubstrings(string s, int k) {
