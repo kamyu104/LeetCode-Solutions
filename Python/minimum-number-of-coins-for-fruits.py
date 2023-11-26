fb7d4f42ee1eab0bcc34460319690d7cabb40368
kamyu <kamyu104@users.noreply.github.com>
2023-11-26 17:21:34 +0800

    Create minimum-number-of-coins-for-fruits.py

---
+++ Python/minimum-number-of-coins-for-fruits.py
@@ -0,0 +1,79 @@
+# Time:  O(n)
+# Space: O(n)
+
+# dp, mono deque
+import collections
+
+
+class Solution(object):
+    def minimumCoins(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        dp = [float("inf")]*len(prices)
+        dp[0] = prices[0]
+        j = 1
+        dq = collections.deque()
+        for i in xrange(1, len(prices)):
+            while dq and dp[dq[-1]-1]+prices[dq[-1]] >= dp[i-1]+prices[i]:
+                dq.pop()
+            dq.append(i)
+            if i == 1:
+                dp[1] = prices[0]
+                continue
+            while j+(j+1) < i:
+                j += 1
+                if dq[0] < j:
+                    assert(len(dq) != 0)
+                    dq.popleft()
+            dp[i] = dp[dq[0]-1]+prices[dq[0]]
+        return dp[-1]
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# dp, sorted list
+from sortedcontainers import SortedList
+
+
+class Solution2(object):
+    def minimumCoins(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        dp = [float("inf")]*len(prices)
+        dp[0] = prices[0]
+        j = 1
+        sl = SortedList()
+        for i in xrange(1, len(prices)):
+            sl.add((dp[i-1]+prices[i], i))
+            if i == 1:
+                dp[1] = prices[0]
+                continue
+            while j+(j+1) < i:
+                sl.remove(((dp[j-1]+prices[j], j)))
+                j += 1
+            dp[i] = sl[0][0]
+        return dp[-1]
+
+
+# Time:  O(n^2)
+# Space: O(n)
+# dp
+class Solution3(object):
+    def minimumCoins(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        dp = [float("inf")]*len(prices)
+        dp[0] = prices[0]
+        for i in xrange(1, len(prices)):
+            if i == 1:
+                dp[1] = prices[0]
+                continue
+            for j in xrange(max(i//2, 1), i+1):
+                dp[i] = min(dp[i], dp[j-1]+prices[j])
+        return dp[-1]
