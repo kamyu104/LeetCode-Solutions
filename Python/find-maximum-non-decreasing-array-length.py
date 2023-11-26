e0c9ebad9fdf2df0d93147ee9a843f1f36e25cff
kamyu <kamyu104@users.noreply.github.com>
2023-11-26 20:48:55 +0800

    Update find-maximum-non-decreasing-array-length.py

--- Python/find-maximum-non-decreasing-array-length.py
+++ Python/find-maximum-non-decreasing-array-length.py
@@ -22,13 +22,38 @@ class Solution(object):
         return dp
 
 
+# Time:  O(n)
+# Space: O(n)
+import collections
+
+
+# greedy, prefix sum, mono deque
+class Solution2(object):
+    def findMaximumLength(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        dp = prefix = prev_prefix = prev_dp = 0
+        dq = collections.deque()
+        for right in xrange(len(nums)):
+            prefix += nums[right]
+            while dq and dq[0][0] <= prefix:
+                _, prev_prefix, prev_dp = dq.popleft()
+            last, dp = prefix-prev_prefix, prev_dp+1
+            while dq and dq[-1][0] >= last+prefix:
+                dq.pop()
+            dq.append((last+prefix, prefix, dp))
+        return dp
+
+
 # Time:  O(nlogn)
 # Space: O(n)
 import bisect
 
 
 # dp, greedy, prefix sum, mono stack, binary search
-class Solution2(object):
+class Solution3(object):
     def findMaximumLength(self, nums):
         """
         :type nums: List[int]
