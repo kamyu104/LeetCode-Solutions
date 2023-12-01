181175236ff70cc291a1282bff1dc0bcdba2a105
kamyu <kamyu104@users.noreply.github.com>
2023-12-01 21:28:13 +0800

    Create number-of-divisible-substrings.py

---
+++ Python/number-of-divisible-substrings.py
@@ -0,0 +1,22 @@
+# Time:  O(d * n)
+# Space: O(n)
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def countDivisibleSubstrings(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        result = 0
+        for d in xrange(1, 10):
+            prefix = 0
+            cnt = collections.Counter([0+d*(-1+1)])
+            for i, x in enumerate(word):
+                prefix += (ord(x)-ord('a')+1)//3+1
+                result += cnt[prefix-d*(i+1)]
+                cnt[prefix-d*(i+1)] += 1
+        return result
