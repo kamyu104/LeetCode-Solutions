10b1e7f91f6c6e45a1e5f98d4932c3b69c766d2a
kamyu <kamyu104@users.noreply.github.com>
2023-12-01 21:27:37 +0800

    Create number-of-divisible-substrings.cpp

---
+++ C++/number-of-divisible-substrings.cpp
@@ -0,0 +1,19 @@
+// Time:  O(d * n)
+// Space: O(n)
+
+// freq table
+class Solution {
+public:
+    int countDivisibleSubstrings(string word) {
+        int result = 0;
+        for (int d = 1; d <= 9; ++d) {
+            int prefix = 0;
+            unordered_map<int, int> cnt = {{0 + d * (-1 + 1), 1}};
+            for (int i = 0; i < size(word); ++i) {
+                prefix += (word[i] - 'a' + 1) / 3 + 1;
+                result += cnt[prefix - d * (i + 1)]++;
+            }
+        }
+        return result;
+    }
+};
