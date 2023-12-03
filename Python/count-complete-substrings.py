51d345b668cee63dd963212ec34a0346e605ff5a
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 22:57:04 +0800

    Update count-complete-substrings.py

--- Python/count-complete-substrings.py
+++ Python/count-complete-substrings.py
@@ -1,7 +1,8 @@
 # Time:  O(26 + d * n), d = len(set(word))
 # Space: O(26)
 
-# freq table, two pointers, sliding window
+
+# freq table, two pointers
 class Solution(object):
     def countCompleteSubstrings(self, word, k):
         """
@@ -17,22 +18,17 @@ class Solution(object):
                 cnt[ord(word[right])-ord('a')] += 1
                 curr = cnt[ord(word[right])-ord('a')]
                 valid += 1 if curr == k else -1 if curr == k+1 else 0
-                if right-1 >= 0 and abs(ord(word[right])-ord(word[right-1])) > 2:
-                    while left < right:
-                        curr = cnt[ord(word[left])-ord('a')]
-                        valid -= 1 if curr == k else -1 if curr == k+1 else 0
-                        cnt[ord(word[left])-ord('a')] -= 1
-                        left += 1
-                elif right-left+1 == c*k+1:
+                if right-left+1 == c*k+1:
                     curr = cnt[ord(word[left])-ord('a')]
                     valid -= 1 if curr == k else -1 if curr == k+1 else 0
                     cnt[ord(word[left])-ord('a')] -= 1
                     left += 1
                 if valid == c:
                     result += 1
-            while left < len(word):
-                curr = cnt[ord(word[left])-ord('a')]
-                valid -= 1 if curr == k else -1 if curr == k+1 else 0
-                cnt[ord(word[left])-ord('a')] -= 1
-                left += 1
+                if right+1 == len(word) or abs(ord(word[right+1])-ord(word[right])) > 2:
+                    while left < right+1:
+                        curr = cnt[ord(word[left])-ord('a')]
+                        valid -= 1 if curr == k else -1 if curr == k+1 else 0
+                        cnt[ord(word[left])-ord('a')] -= 1
+                        left += 1
         return result
