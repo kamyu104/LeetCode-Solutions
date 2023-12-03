949a48fe7d93e78c2a0844bbfc9b167b53ea91e2
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 18:39:51 +0800

    Update patching-array.py

--- Python/patching-array.py
+++ Python/patching-array.py
@@ -8,6 +8,31 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        result = reachable = 0
+        for x in nums:
+            while not reachable >= x-1:
+                result += 1
+                reachable += reachable+1
+                if reachable >= n:
+                    return result
+            reachable += x
+            if reachable >= n:
+                return result
+        while not reachable >= n:
+            result += 1
+            reachable += reachable+1
+        return result
+
+
+# Time:  O(s + logn), s is the number of elements in the array
+# Space: O(1)
+class Solution2(object):
+    def minPatches(self, nums, n):
+        """
+        :type nums: List[int]
+        :type n: int
+        :rtype: int
+        """
         patch, miss, i = 0, 1, 0
         while miss <= n:
             if i < len(nums) and nums[i] <= miss:
