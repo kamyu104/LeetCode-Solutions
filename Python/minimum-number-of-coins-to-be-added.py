80a812173f5cd250c3040b2de4e66a8e82cfcd99
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 19:06:01 +0800

    Update minimum-number-of-coins-to-be-added.py

--- Python/minimum-number-of-coins-to-be-added.py
+++ Python/minimum-number-of-coins-to-be-added.py
@@ -13,6 +13,32 @@ class Solution(object):
         coins.sort()
         result = reachable = 0
         for x in coins:
+            # if x > target:
+            #     break
+            while not reachable >= x-1:
+                result += 1
+                reachable += reachable+1
+            reachable += x
+        while not reachable >= target:
+            result += 1
+            reachable += reachable+1
+        return result
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+# lc0330
+# sort, greedy
+class Solution2(object):
+    def minimumAddedCoins(self, coins, target):
+        """
+        :type coins: List[int]
+        :type target: int
+        :rtype: int
+        """
+        coins.sort()
+        result = reachable = 0
+        for x in coins:
             while not reachable >= x-1:
                 result += 1
                 reachable += reachable+1
