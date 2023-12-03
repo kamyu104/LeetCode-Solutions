8522c4b8ae55197e6c4522e300d7aa61c3bd5755
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 18:39:05 +0800

    Update patching-array.cpp

--- C++/patching-array.cpp
+++ C++/patching-array.cpp
@@ -4,6 +4,35 @@
 class Solution {
 public:
     int minPatches(vector<int>& nums, int n) {
+        int result = 0, reachable = 0;
+        for (const auto& x : nums) {
+            while (!(reachable >= x - 1)) {
+                ++result;
+                if (reachable >= n - (reachable + 1)) {
+                    return result;
+                }
+                reachable += reachable + 1;
+            }
+            if (reachable >= n - x) {
+                return result;
+            }
+            reachable += x;
+        }
+        for (; !(reachable >= n); reachable += reachable + 1) {
+            ++result;
+            if (reachable >= n - (reachable + 1)) {
+                return result;
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(s + logn), s is the number of elements in the array
+// Space: O(1)
+class Solution2 {
+public:
+    int minPatches(vector<int>& nums, int n) {
         int patch = 0;
         for (uint64_t miss = 1, i = 0; miss <= n;) {
             if (i < nums.size() && nums[i] <= miss) {
