77e0ec62ae7c6549ad2f3ae98ebea17561a82871
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 18:53:20 +0800

    Create minimum-number-of-coins-to-be-added.cpp

---
+++ C++/minimum-number-of-coins-to-be-added.cpp
@@ -0,0 +1,30 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// lc0330
+// sort, greedy
+class Solution {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        sort(begin(coins), end(coins));
+        int result = 0, reachable = 0;
+        for (const auto& x : coins) {
+            while (!(reachable >= x - 1)) {
+                ++result;
+                if (reachable >= target - (reachable + 1)) {
+                    return result;
+                }
+                reachable += reachable + 1;
+            }
+            if (reachable >= target - x) {
+                return result;
+            }
+            reachable += x;
+        }
+        ++result;
+        for (; !(reachable >= target - (reachable + 1)); reachable += reachable + 1) {
+            ++result;
+        }
+        return result;
+    }
+};
