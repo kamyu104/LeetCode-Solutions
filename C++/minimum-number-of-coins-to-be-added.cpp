413b48a944e8b920f1d6edde212fb91e6b82c06e
kamyu <kamyu104@users.noreply.github.com>
2023-12-03 19:08:28 +0800

    Update minimum-number-of-coins-to-be-added.cpp

--- C++/minimum-number-of-coins-to-be-added.cpp
+++ C++/minimum-number-of-coins-to-be-added.cpp
@@ -9,6 +9,36 @@ public:
         sort(begin(coins), end(coins));
         int result = 0, reachable = 0;
         for (const auto& x : coins) {
+            // if (x > target) {
+            //     break;
+            // }
+            while (!(reachable >= x - 1)) {
+                ++result;
+                reachable += reachable + 1;
+            }
+            if (reachable >= target - x) {
+                return result;
+            }
+            reachable += x;
+        }
+        ++result;
+        for (; !(reachable >= target - (reachable + 1)); reachable += reachable + 1) {
+            ++result;
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+// lc0330
+// sort, greedy
+class Solution2 {
+public:
+    int minimumAddedCoins(vector<int>& coins, int target) {
+        sort(begin(coins), end(coins));
+        int result = 0, reachable = 0;
+        for (const auto& x : coins) {
             while (!(reachable >= x - 1)) {
                 ++result;
                 if (reachable >= target - (reachable + 1)) {
