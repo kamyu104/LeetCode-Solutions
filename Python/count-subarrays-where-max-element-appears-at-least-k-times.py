338c8d11100ad5e9d6322ce8c2bc39277a960fa0
kamyu <kamyu104@users.noreply.github.com>
2023-12-12 00:06:33 +0800

    Update count-subarrays-where-max-element-appears-at-least-k-times.py

--- Python/count-subarrays-where-max-element-appears-at-least-k-times.py
+++ Python/count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -18,3 +18,25 @@ class Solution(object):
                 left += 1
             result += left
         return result
+
+
+# Time:  O(n)
+# Space: O(1)
+# two pointers, sliding window
+class Solution2(object):
+    def countSubarrays(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        mx = max(nums)
+        result = (len(nums)+1)*len(nums)//2
+        left = cnt = 0
+        for right in xrange(len(nums)):
+            cnt += int(nums[right] == mx)
+            while cnt == k:
+                cnt -= int(nums[left] == mx)
+                left += 1
+            result -= right-left+1
+        return result
