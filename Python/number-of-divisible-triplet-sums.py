b7d058feab4c805997f502503eb93ca28ba9e9f7
kamyu <kamyu104@users.noreply.github.com>
2023-12-17 02:11:02 +0800

    Create number-of-divisible-triplet-sums.py

---
+++ Python/number-of-divisible-triplet-sums.py
@@ -0,0 +1,45 @@
+# Time:  O(n^2)
+# Space: O(n)
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def divisibleTripletCount(self, nums, d):
+        """
+        :type nums: List[int]
+        :type d: int
+        :rtype: int
+        """
+        result = 0
+        cnt = collections.Counter()
+        for i in xrange(len(nums)):
+            for j in xrange(i+1, len(nums)):
+                if (nums[i]+nums[j])%d in cnt:
+                    result += cnt[(nums[i]+nums[j])%d]
+            cnt[-nums[i]%d] += 1
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(n^2)
+import collections
+
+
+# freq table
+class Solution2(object):
+    def divisibleTripletCount(self, nums, d):
+        """
+        :type nums: List[int]
+        :type d: int
+        :rtype: int
+        """
+        result = 0
+        cnt = collections.Counter()
+        for i in xrange(len(nums)):
+            if nums[i]%d in cnt:
+                result += cnt[-ums[i]%d]
+            for j in xrange(i):
+                cnt[-(nums[i]+nums[j])%d] += 1
+        return result
