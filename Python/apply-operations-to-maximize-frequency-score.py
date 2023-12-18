9ba90ec2b366ba8435c8ae5633f2c364c0ac3507
kamyu <kamyu104@users.noreply.github.com>
2023-12-18 15:18:16 +0800

    Update apply-operations-to-maximize-frequency-score.py

--- Python/apply-operations-to-maximize-frequency-score.py
+++ Python/apply-operations-to-maximize-frequency-score.py
@@ -12,8 +12,12 @@ class Solution(object):
         nums.sort()
         result = left = curr = 0
         for right in xrange(len(nums)):
+            # -+  => -0+
+            # -0+ => --++
             curr += nums[right]-nums[(left+right)//2]
             if not curr <= k:
+                # --++ => -0+
+                # -0+  => -+
                 curr -= nums[((left+1)+right)//2]-nums[left]
                 left += 1
         return right-left+1
@@ -32,8 +36,12 @@ class Solution2(object):
         nums.sort()
         result = left = curr = 0
         for right in xrange(len(nums)):
+            # -+  => -0+
+            # -0+ => --++
             curr += nums[right]-nums[(left+right)//2]
             while not curr <= k:
+                # --++ => -0+
+                # -0+  => -+
                 curr -= nums[((left+1)+right)//2]-nums[left]
                 left += 1
             result = max(result, right-left+1)
