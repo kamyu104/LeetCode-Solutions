7aada4f0196da0b83d4835322cbc69baa668f311
kamyu <kamyu104@users.noreply.github.com>
2023-12-18 12:57:45 +0800

    Update apply-operations-to-maximize-frequency-score.py

--- Python/apply-operations-to-maximize-frequency-score.py
+++ Python/apply-operations-to-maximize-frequency-score.py
@@ -38,3 +38,30 @@ class Solution2(object):
                 left += 1
             result = max(result, right-left+1)
         return result
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+# sort, prefix sum, binary search
+class Solution3(object):
+    def maxFrequencyScore(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def check(l):
+            return any((prefix[i+l]-prefix[i+(l+1)//2])-(prefix[i+l//2]-prefix[i]) <= k for i in xrange(len(nums)-l+1))
+
+        nums.sort()
+        prefix = [0]*(len(nums)+1)
+        for i, x in enumerate(nums):
+            prefix[i+1] = prefix[i]+x
+        left, right = 1, len(nums)
+        while left <= right:
+            mid = left+(right-left)//2
+            if  not check(mid):
+                right = mid-1
+            else:
+                left = mid+1
+        return right
