2dd0944dbdc429cacec0ac084c7e5a0e5584c860
kamyu <kamyu104@users.noreply.github.com>
2023-12-25 05:55:34 +0800

    Update minimum-cost-to-convert-string-ii.py

--- Python/minimum-cost-to-convert-string-ii.py
+++ Python/minimum-cost-to-convert-string-ii.py
@@ -56,13 +56,72 @@ class Solution(object):
         return dp[len(source)%len(dp)] if dp[len(source)%len(dp)] != INF else -1
 
 
+# Time:  O(o * l + k^3 + n * c * l), o = len(original), l = max(len(x) for x in original), k = len(lookups), c = len({len(x) for x in original})
+# Space: O(o * l + k^2 + c + l)
+import collections
+import itertools
+
+
+# hash table, Floyd-Warshall algorithm, dp
+class Solution2(object):
+    def minimumCost(self, source, target, original, changed, cost):
+        """
+        :type source: str
+        :type target: str
+        :type original: List[str]
+        :type changed: List[str]
+        :type cost: List[int]
+        :rtype: int
+        """
+        INF = float("inf")
+        def floydWarshall(dist):
+            for k in dist.iterkeys():
+                for i in dist.iterkeys():
+                    for j in dist.iterkeys():
+                        dist[i][j] = min(dist[i][j], dist[i][k]+dist[k][j])
+        
+        lookup = {}
+        buckets = collections.defaultdict(set)
+        for x in itertools.chain(original, changed):
+            l = len(x)
+            if x in lookup:
+                continue
+            lookup[x] = len(lookup)
+            buckets[len(x)].add(lookup[x])            
+        dists = {l:{u:{v:0 if u == v else INF for v in lookup} for u in lookup} for l, lookup in buckets.iteritems()}
+        for i in xrange(len(original)):
+            l = len(original[i])
+            dist = dists[l]
+            u, v = lookup[original[i]], lookup[changed[i]]
+            dist[u][v] = min(dist[u][v], cost[i])
+        for dist in dists.itervalues():
+            floydWarshall(dist)
+        candidates = {len(x) for x in original}
+        dp = [INF]*(max(len(x) for x in original)+1)
+        dp[0] = 0
+        for i in xrange(len(source)):
+            if dp[i%len(dp)] == INF:
+                continue
+            if source[i] == target[i]:
+                dp[(i+1)%len(dp)] = min(dp[(i+1)%len(dp)], dp[i%len(dp)])
+            for l in candidates:
+                if i+l > len(source):
+                    continue
+                dist = dists[l]
+                u, v = source[i:i+l], target[i:i+l]
+                if u in lookup and v in lookup:
+                    dp[(i+l)%len(dp)] = min(dp[(i+l)%len(dp)], dp[i%len(dp)]+dist[lookup[u]][lookup[v]])
+            dp[i%len(dp)] = INF
+        return dp[len(source)%len(dp)] if dp[len(source)%len(dp)] != INF else -1
+
+
 # Time:  O(o * l + k^3 + n * l), o = len(original), l = max(len(x) for x in original), k = trie.k
 # Space: O(t + k^2 + l)
 import itertools
 
 
 # trie, Floyd-Warshall algorithm, dp
-class Solution2(object):
+class Solution3(object):
     def minimumCost(self, source, target, original, changed, cost):
         """
         :type source: str
