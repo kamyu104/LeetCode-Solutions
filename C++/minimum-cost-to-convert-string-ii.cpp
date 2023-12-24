954b54180dd7821d11fb3e01beeeb1188637f85c
kamyu <kamyu104@users.noreply.github.com>
2023-12-25 06:03:37 +0800

    Update minimum-cost-to-convert-string-ii.cpp

--- C++/minimum-cost-to-convert-string-ii.cpp
+++ C++/minimum-cost-to-convert-string-ii.cpp
@@ -85,10 +85,96 @@ public:
     }
 };
 
+// Time:  O(o * l + k^3 + n * c * l), o = len(original), l = max(len(x) for x in original), k = len(lookup), c = len({len(x) for x in original})
+// Space: O(o * l + k^2 + c + l)
+// hash table, Floyd-Warshall algorithm, dp
+class Solution2 {
+public:
+    long long minimumCost(string source, string target, vector<string>& original, vector<string>& changed, vector<int>& cost) {
+        static const auto INF = numeric_limits<int64_t>::max();
+        const auto& floydWarshall = [](auto& dist) {
+            for (const auto& [k, _] : dist) {
+                for (const auto& [i, _] : dist) {
+                    for (const auto& [j, _] : dist) {
+                        if (dist[i][k] != INF && dist[k][j] != INF) {
+                            dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                        }
+                    }
+                }
+            }
+        };
+
+        unordered_map<string, int> lookup;
+        unordered_map<int, vector<int>> buckets;
+        for (const auto& x : original) {
+            if (lookup.count(x)) {
+                continue;
+            }
+            lookup[x] = size(lookup);
+            buckets[size(x)].emplace_back(lookup[x]);
+        }
+        for (const auto& x : changed) {
+            if (lookup.count(x)) {
+                continue;
+            }
+            lookup[x] = size(lookup);
+            buckets[size(x)].emplace_back(lookup[x]);
+        }
+        unordered_map<int, unordered_map<int, unordered_map<int, int64_t>>> dists;
+        for (const auto& [l, lookup] : buckets) {
+            auto& dist = dists[l];
+            for (const auto& u : lookup) {
+                for (const auto& v : lookup) {
+                    dist[u][v] = u == v ? 0 : INF;
+                }
+            }
+        }
+        for (int i = 0; i < size(original); ++i) {
+            const int l = size(original[i]);
+            auto& dist = dists[l];
+            const int u = lookup[original[i]];
+            const int v = lookup[changed[i]];
+            dist[u][v] = min(dist[u][v], static_cast<int64_t>(cost[i]));
+        }
+        for (auto& [_, dist] : dists) {
+            floydWarshall(dist);
+        }
+        unordered_set<int> candidates;
+        int l = 0;
+        for (const auto& x : original) {
+            candidates.emplace(size(x));
+            l = max(l, static_cast<int>(size(x)));
+        }
+        vector<int64_t> dp(l + 1, INF);
+        dp[0] = 0;
+        for (int i = 0; i < size(source); ++i) {
+            if (dp[i % size(dp)] == INF) {
+                continue;
+            }
+            if (source[i] == target[i]) {
+                dp[(i + 1) % size(dp)] = min(dp[(i + 1) % size(dp)], dp[i % size(dp)]);
+            }
+            for (const auto& l : candidates) {
+                if (i + l > size(source)) {
+                    continue;
+                }
+                auto& dist = dists[l];
+                const auto& u = source.substr(i, l);
+                const auto& v = target.substr(i, l);
+                if (lookup.count(u) && lookup.count(v) && dist[lookup[u]][lookup[v]] != INF) {
+                    dp[(i + l) % size(dp)] = min(dp[(i + l) % size(dp)], dp[i % size(dp)] + dist[lookup[u]][lookup[v]]);
+                }
+            }
+            dp[i % size(dp)] = INF;
+        }
+        return dp[size(source) % size(dp)] != INF ? dp[size(source) % size(dp)] : -1;
+    }
+};
+
 // Time:  O(o * l + k^3 + n * l), o = len(original), l = max(len(x) for x in original), k = trie.k
 // Space: O(t + k^2 + l)
 // trie, Floyd-Warshall algorithm, dp
-class Solution2 {
+class Solution3 {
 private:
     class Trie {
     public:
