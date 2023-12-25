e7c357fa842293d6511dc0937a7088bf5802f8b8
kamyu <kamyu104@users.noreply.github.com>
2023-12-25 20:14:45 +0800

    Update minimum-cost-to-convert-string-i.py

--- Python/minimum-cost-to-convert-string-i.py
+++ Python/minimum-cost-to-convert-string-i.py
@@ -1,8 +1,54 @@
-# Time:  O(o + 26^3 + n)
-# Space: O(26^2)
+# Time:  O(o + k * eloge + n), k = len(set(original)), e is the number of edges reachable from a given node u
+# Space: O(o + k * v), v is the number of nodes reachable from a given node u
 
+import collections
+import heapq
+
+# dijkstra's algorithm, memoization
+class Solution2(object):
+    def minimumCost(self, source, target, original, changed, cost):
+        """
+        :type source: str
+        :type target: str
+        :type original: List[str]
+        :type changed: List[str]
+        :type cost: List[int]
+        :rtype: int
+        """
+        INF = float("inf")
+        def dijkstra(start):
+            best = collections.defaultdict(lambda: INF)
+            best[start] = 0
+            min_heap = [(0, start)]
+            while min_heap:
+                curr, u = heapq.heappop(min_heap)
+                if curr > best[u]:
+                    continue
+                for v, w in dist[u].iteritems():     
+                    if curr+w >= best[v]:
+                        continue
+                    best[v] = curr+w
+                    heapq.heappush(min_heap, (best[v], v))
+            return best
+        
+        def memoization(u, v):
+            if u not in lookup:
+                lookup[u] = dijkstra(u)
+            return lookup[u][v]
+
+        dist = collections.defaultdict(lambda: collections.defaultdict(lambda: INF))
+        for i in xrange(len(original)):
+            u, v = ord(original[i])-ord('a'), ord(changed[i])-ord('a')
+            dist[u][v] = min(dist[u][v], cost[i])
+        lookup = {}
+        result = sum(memoization(ord(source[i])-ord('a'), ord(target[i])-ord('a')) for i in xrange(len(source)))
+        return result if result != INF else -1
+
+
+# Time:  O(o + 26^3 + n)
+# Space: O(o + 26^2)
 # Floyd-Warshall algorithm 
-class Solution(object):
+class Solution2(object):
     def minimumCost(self, source, target, original, changed, cost):
         """
         :type source: str
