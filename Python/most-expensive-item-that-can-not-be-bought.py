ed97242802294832dc0b11a8794ffaa9efdd1882
kamyu <kamyu104@users.noreply.github.com>
2023-12-29 05:08:31 +0800

    Update most-expensive-item-that-can-not-be-bought.py

--- Python/most-expensive-item-that-can-not-be-bought.py
+++ Python/most-expensive-item-that-can-not-be-bought.py
@@ -13,3 +13,23 @@ class Solution(object):
         # - https://en.wikipedia.org/wiki/Coin_problem
         # - https://mikebeneschan.medium.com/the-chicken-mcnugget-theorem-explained-2daca6fbbe1e
         return primeOne*primeTwo-primeOne-primeTwo
+
+
+# Time:  O(p1 * p2)
+# Space: O(p1 + p2)
+# dp
+class Solution2(object):
+    def mostExpensiveItem(self, primeOne, primeTwo):
+        """
+        :type primeOne: int
+        :type primeTwo: int
+        :rtype: int
+        """
+        dp = [False]*max(primeOne, primeTwo)
+        dp[0] = True
+        result = 1
+        for i in xrange(1, primeOne*primeTwo+1):
+            dp[i%len(dp)] = dp[(i-primeOne)%len(dp)] or dp[(i-primeTwo)%len(dp)]
+            if not dp[i%len(dp)]:
+                result = i
+        return result
