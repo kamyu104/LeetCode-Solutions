927192e75bba106433d4c25c1b687d40670a3596
kamyu <kamyu104@users.noreply.github.com>
2023-12-31 22:32:12 +0800

    Update palindrome-rearrangement-queries.py

--- Python/palindrome-rearrangement-queries.py
+++ Python/palindrome-rearrangement-queries.py
@@ -10,17 +10,18 @@ class Solution(object):
         :rtype: List[bool]
         """
         def check(left1, right1, left2, right2):
+            def same(left, right):
+                return all(prefixs1[right+1][i]-prefixs1[left][i] == prefixs2[right+1][i]-prefixs2[left][i] for i in xrange(26))
+
             min_left, max_left = min(left1, left2), max(left1, left2)
             min_right, max_right = min(right1, right2), max(right1, right2)
             if not (prefix[min_left]-prefix[0] == prefix[-1]-prefix[max_right+1] == 0):
                 return False
             if min_right < max_left:  # non-overlapped
-                return (prefix[max_left]-prefix[min_right+1] == 0 and
-                        all(prefixs1[min_right+1][i]-prefixs1[min_left][i] == prefixs2[min_right+1][i]-prefixs2[min_left][i] for i in xrange(26)) and
-                        all(prefixs1[max_right+1][i]-prefixs1[max_left][i] == prefixs2[max_right+1][i]-prefixs2[max_left][i] for i in xrange(26)))
+                return prefix[max_left]-prefix[min_right+1] == 0 and same(min_left, min_right) and same(max_left, max_right)
             # overlapped
             if (left1 == min_left) == (right1 == max_right):  # inside another
-                return all(prefixs1[max_right+1][i]-prefixs1[min_left][i] == prefixs2[max_right+1][i]-prefixs2[min_left][i] for i in xrange(26))
+                return same(min_left, max_right)
             # not inside another
             p1, p2 = (prefixs1, prefixs2) if min_left == left1 else (prefixs2, prefixs1)
             diff1 = [(p2[max_right+1][i]-p2[max_left][i])-(p1[max_right+1][i]-p1[min_right+1][i]) for i in xrange(26)]
