0bf38b503a40c68893d5da62188f3a609be24430
kamyu <kamyu104@users.noreply.github.com>
2023-12-31 16:46:36 +0800

    Update find-longest-special-substring-that-occurs-thrice-ii.cpp

--- C++/find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ C++/find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -12,14 +12,14 @@ public:
             if (i + 1 != size(s) && s[i + 1] == s[i]) {
                 continue;
             }
-            const int x = s[i] - 'a';
-            for (int j = 0; j < size(lookup[x]); ++j) {
-                if (lookup[x][j] < cnt) {
-                    swap(cnt, lookup[x][j]);
+            auto& curr = lookup[s[i] - 'a'];
+            for (int j = 0; j < size(curr); ++j) {
+                if (curr[j] < cnt) {
+                    swap(cnt, curr[j]);
                 }
             }
             cnt = 0;
-            result = max({result, lookup[x][0] - 2, min(lookup[x][0] - 1, lookup[x][1]), lookup[x][2]});
+            result = max({result, curr[0] - 2, min(curr[0] - 1, curr[1]), curr[2]});
         }
         return result ? result : -1;
     }
