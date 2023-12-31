bcc512e5612667fda88a8a34a9622164eb9b249a
kamyu <kamyu104@users.noreply.github.com>
2023-12-31 16:44:19 +0800

    Create find-longest-special-substring-that-occurs-thrice-ii.py

---
+++ Python/find-longest-special-substring-that-occurs-thrice-ii.py
@@ -0,0 +1,24 @@
+# Time:  O(n * 3)
+# Space: O(26 * 3)
+
+# string, hash table
+class Solution(object):
+    def maximumLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        lookup = [[0]*3 for _ in xrange(26)]
+        result = cnt = 0
+        for i, c in enumerate(s):
+            cnt += 1
+            if i+1 != len(s) and s[i+1] == s[i]:
+                continue
+            x = ord(c)-ord('a')
+            curr = lookup[x]
+            for j in xrange(len(lookup[x])):
+                if curr[j] < cnt:
+                    cnt, curr[j] = curr[j], cnt
+            cnt = 0
+            result = max(result, max(curr[0]-2, min(curr[0]-1, curr[1]), curr[2]))
+        return result if result else -1
