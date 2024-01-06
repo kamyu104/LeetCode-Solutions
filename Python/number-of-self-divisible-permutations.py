9b777aabccc5b5c916327c490afce00f180b7f45
kamyu <kamyu104@users.noreply.github.com>
2024-01-06 21:18:48 +0800

    Create number-of-self-divisible-permutations.py

---
+++ Python/number-of-self-divisible-permutations.py
@@ -0,0 +1,25 @@
+# Time:  O(n * 2^n)
+# Space: O(2^n)
+
+# dp
+class Solution(object):
+    def selfDivisiblePermutationCount(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a%b
+            return a
+
+        dp = [0]*(1<<n)
+        dp[0] = 1
+        for i in xrange(n):
+            new_dp = [0]*(1<<n)
+            for mask in xrange(1<<n):
+                for j in xrange(n):
+                    if mask&(1<<j) and gcd(j+1, i+1) == 1:
+                        new_dp[mask] += dp[mask^(1<<j)]
+            dp = new_dp
+        return dp[-1]
