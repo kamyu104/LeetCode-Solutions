ea82117d72b1e9c64383ee3f223b37ed85b09d76
kamyu <kamyu104@users.noreply.github.com>
2024-01-06 21:49:39 +0800

    Update number-of-self-divisible-permutations.py

--- Python/number-of-self-divisible-permutations.py
+++ Python/number-of-self-divisible-permutations.py
@@ -1,13 +1,16 @@
-# Time:  O(n^2 * 2^n * logn)
+# Time:  O(n * 2^n * logn)
 # Space: O(2^n)
 
-# dp
+# bitmasks, dp
 class Solution(object):
     def selfDivisiblePermutationCount(self, n):
         """
         :type n: int
         :rtype: int
         """
+        def popcount(x):
+            return bin(x).count('1')
+
         def gcd(a, b):
             while b:
                 a, b = b, a%b
@@ -15,11 +18,9 @@ class Solution(object):
 
         dp = [0]*(1<<n)
         dp[0] = 1
-        for i in xrange(n):
-            new_dp = [0]*(1<<n)
-            for mask in xrange(1<<n):
-                for j in xrange(n):
-                    if mask&(1<<j) and gcd(j+1, i+1) == 1:
-                        new_dp[mask] += dp[mask^(1<<j)]
-            dp = new_dp
+        for mask in xrange(1<<n):
+            i = popcount(mask)
+            for j in xrange(n):
+                if mask&(1<<j) == 0 and gcd(j+1, i+1) == 1:
+                    dp[mask|(1<<j)] += dp[mask]
         return dp[-1]
