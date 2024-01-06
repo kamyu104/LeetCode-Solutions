b2111e9f8ac833c5fc5863d741c6d5ae0e2d34a4
kamyu <kamyu104@users.noreply.github.com>
2024-01-06 22:32:44 +0800

    Update number-of-self-divisible-permutations.cpp

--- C++/number-of-self-divisible-permutations.cpp
+++ C++/number-of-self-divisible-permutations.cpp
@@ -1,27 +1,22 @@
 // Time:  O(n^2 * logn + n * 2^n) = O(n * 2^n)
 // Space: O(n^2 + 2^n) = O(2^n)
 
-// bitmasks, dp, memoization
+// bitmasks, dp
 class Solution {
 public:
     int selfDivisiblePermutationCount(int n) {
-        vector<vector<int>> lookup(n, vector<int>(n));
-        const auto& memoization = [&](int a, int b) {
-            if (a > b) {
-                swap(a, b);
+        vector<vector<bool>> lookup(n, vector<bool>(n));
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                lookup[i][j] = lookup[j][i] = gcd(i + 1, j + 1) == 1;
             }
-            if (!lookup[a - 1][b - 1]) {
-                lookup[a - 1][b - 1] = gcd(a, b);
-            }
-            return lookup[a - 1][b - 1];
-        };
-        
+        }
         vector<int> dp(1 << n);
         dp[0] = 1;
         for (int mask = 0; mask < (1 << n); ++mask) {
             const int i = __builtin_popcount(mask);
             for (int j = 0; j < n; ++j) {
-                if ((mask & (1 << j)) == 0 && memoization(j + 1, i + 1) == 1) {
+                if ((mask & (1 << j)) == 0 && lookup[i][j]) {
                     dp[mask | (1 << j)] += dp[mask];
                 }
             }
