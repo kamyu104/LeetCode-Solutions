28b71d8c98753bbf5beab2e9c7cff8e078292c79
kamyu <kamyu104@users.noreply.github.com>
2024-01-06 22:04:29 +0800

    Update number-of-self-divisible-permutations.cpp

--- C++/number-of-self-divisible-permutations.cpp
+++ C++/number-of-self-divisible-permutations.cpp
@@ -1,16 +1,24 @@
-// Time:  O(n * 2^n * logn)
-// Space: O(2^n)
+// Time:  O(n^2 * logn + n * 2^n) = O(n * 2^n)
+// Space: O(n^2 + 2^n) = O(2^n)
 
-// bitmasks, dp
+// bitmasks, dp, memoization
 class Solution {
 public:
     int selfDivisiblePermutationCount(int n) {
+        vector<vector<int>> lookup(n, vector<int>(n));
+        const auto& memoization = [&](int a, int b) {
+            if (!lookup[a - 1][b - 1]) {
+                lookup[a - 1][b - 1] = gcd(a, b);
+            }
+            return lookup[a - 1][b - 1];
+        };
+        
         vector<int> dp(1 << n);
         dp[0] = 1;
         for (int mask = 0; mask < (1 << n); ++mask) {
             const int i = __builtin_popcount(mask);
             for (int j = 0; j < n; ++j) {
-                if ((mask & (1 << j)) == 0 && gcd(j + 1, i + 1) == 1) {
+                if ((mask & (1 << j)) == 0 && memoization(j + 1, i + 1) == 1) {
                     dp[mask | (1 << j)] += dp[mask];
                 }
             }
