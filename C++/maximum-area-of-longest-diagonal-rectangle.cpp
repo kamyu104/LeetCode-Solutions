cc603f2ab7ffb0d8cf1ba57fcaa5ce6d53d9da67
kamyu <kamyu104@users.noreply.github.com>
2024-01-07 17:50:34 +0800

    Create maximum-area-of-longest-diagonal-rectangle.cpp

---
+++ C++/maximum-area-of-longest-diagonal-rectangle.cpp
@@ -0,0 +1,14 @@
+// Time:  O(n)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
+        pair<int, int> result = {0, 0};
+        for (const auto& d : dimensions) {
+            result = max(result, {d[0] * d[0] + d[1] * d[1], d[0] * d[1]});
+        }
+        return result.second;
+    }
+};
