706561e21922c95cb80126f4aa6bb34cc71c1a9e
kamyu <kamyu104@users.noreply.github.com>
2024-01-08 05:12:09 +0800

    Update minimize-max-distance-to-gas-station.cpp

--- C++/minimize-max-distance-to-gas-station.cpp
+++ C++/minimize-max-distance-to-gas-station.cpp
@@ -4,25 +4,23 @@
 class Solution {
 public:
     double minmaxGasDist(vector<int>& stations, int K) {
-        double left = 0.0;
-        double right = 1e8;
+        const auto& check = [&](double x) {
+            int total = 0;
+            for (int i = 0; i + 1 < stations.size(); ++i) {
+                total += ceil((stations[i + 1] - stations[i]) / x) - 1;
+            }
+            return total <= K;
+        };
+
+        double left = 0.0, right = stations.back() - stations.front();
         while (right - left > 1e-6) {
             const auto mid = left + (right - left) / 2.0;
-            if (possible(stations, K, mid)) {
+            if (check(mid)) {
                 right = mid;
             } else {
                 left = mid;
             }
         }
         return left;
-    }
-    
-private:
-    bool possible(const vector<int>& stations, int K, double guess) {
-        int sum = 0;
-        for (int i = 0; i + 1 < stations.size(); ++i) {
-            sum += int((stations[i + 1] - stations[i]) / guess);
-        }
-        return sum <= K;
     }
 };
