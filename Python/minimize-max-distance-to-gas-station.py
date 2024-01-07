3bc48fb0ab17993db8581e56d0c71ad246c2b726
kamyu <kamyu104@users.noreply.github.com>
2024-01-08 05:13:45 +0800

    Update minimize-max-distance-to-gas-station.py

--- Python/minimize-max-distance-to-gas-station.py
+++ Python/minimize-max-distance-to-gas-station.py
@@ -1,6 +1,9 @@
 # Time:  O(nlogr)
 # Space: O(1)
 
+import math
+
+
 class Solution(object):
     def minmaxGasDist(self, stations, K):
         """
@@ -8,14 +11,13 @@ class Solution(object):
         :type K: int
         :rtype: float
         """
-        def possible(stations, K, guess):
-            return sum(int((stations[i+1]-stations[i]) / guess)
-                       for i in xrange(len(stations)-1)) <= K
+        def check(x):
+            return sum(int(math.ceil((stations[i+1]-stations[i])/x))-1 for i in xrange(len(stations)-1)) <= K
 
-        left, right = 0, 10**8
+        left, right = 0, stations[-1]-stations[0]
         while right-left > 1e-6:
             mid = left + (right-left)/2.0
-            if possible(mid):
+            if check(mid):
                 right = mid
             else:
                 left = mid
