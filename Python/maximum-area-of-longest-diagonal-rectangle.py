e4fec78b2fcc9add9a0cd71b71327d155c2f4158
kamyu <kamyu104@users.noreply.github.com>
2024-01-07 17:52:00 +0800

    Create maximum-area-of-longest-diagonal-rectangle.py

---
+++ Python/maximum-area-of-longest-diagonal-rectangle.py
@@ -0,0 +1,11 @@
+# Time:  O(n)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def areaOfMaxDiagonal(self, dimensions):
+        """
+        :type dimensions: List[List[int]]
+        :rtype: int
+        """
+        return max((l**2+w**2, l*w) for l, w in dimensions)[1]
