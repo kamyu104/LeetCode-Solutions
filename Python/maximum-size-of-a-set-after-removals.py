afb9cb25e7ebfb6de0d35acae8753316f1962656
kamyu <kamyu104@users.noreply.github.com>
2024-01-08 00:19:16 +0800

    Update maximum-size-of-a-set-after-removals.py

--- Python/maximum-size-of-a-set-after-removals.py
+++ Python/maximum-size-of-a-set-after-removals.py
@@ -11,7 +11,8 @@ class Solution(object):
         """
         lookup1, lookup2 = set(nums1), set(nums2)
         n, c = len(nums1), len(lookup1&lookup2)
-        return min(min(len(lookup1)-c, n//2)+min(len(lookup2)-c, n//2)+c, n)
+        d1, d2 = min(len(lookup1)-c, n//2), min(len(lookup2)-c, n//2)
+        return min(n, d1+d2+c)
 
 
 # Time:  O(n)
@@ -26,6 +27,6 @@ class Solution2(object):
         """
         lookup1, lookup2 = set(nums1), set(nums2)
         n, c = len(nums1), len(lookup1&lookup2)
-        r1 = max(n//2-(len(lookup1)-c), 0)
-        r2 = max(n//2-(len(lookup2)-c), 0)
-        return min(len(lookup1)-c, n//2)+min(len(lookup2)-c, n//2)+min(r1+r2, c)
+        d1, d2 = min(len(lookup1)-c, n//2), min(len(lookup2)-c, n//2)
+        r1, r2 = n//2-d1, n//2-d2
+        return d1+d2+min(r1+r2, c)  # = min(d1+d2+r1+r2, d1+d2+c) = min(d1+d2+(n//2-d1)+(n//2-d2), d1+d2+c) = min(n, d1+d2+c)
