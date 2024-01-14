506ddb653d7ee91cfbaf226066b19766b44bae6c
kamyu <kamyu104@users.noreply.github.com>
2024-01-15 07:53:59 +0800

    Create find-beautiful-indices-in-the-given-array-ii.cpp

---
+++ C++/find-beautiful-indices-in-the-given-array-ii.cpp
@@ -0,0 +1,106 @@
+// Time:  O(n)
+// Space: O(n)
+
+// kmp, two pointers
+class Solution {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        const auto& KMP = [&](const string& text, const string& pattern) {
+            const auto& getPrefix = [&](const string& pattern) {
+                vector<int> prefix(pattern.length(), -1);
+                int j = -1;
+                for (int i = 1; i < pattern.length(); ++i) {
+                    while (j > -1 && pattern[j + 1] != pattern[i]) {
+                        j = prefix[j];
+                    }
+                    if (pattern[j + 1] == pattern[i]) {
+                        ++j;
+                    }
+                    prefix[i] = j;
+                }
+                return prefix;
+            };
+
+            vector<int> result;
+            const vector<int> prefix = getPrefix(pattern);
+            int j = -1;
+            for (int i = 0; i < text.length(); ++i) {
+                while (j > -1 && pattern[j + 1] != text[i]) {
+                    j = prefix[j];
+                }
+                if (pattern[j + 1] == text[i]) {
+                    ++j;
+                }
+                if (j == pattern.length() - 1) {
+                    result.emplace_back(i - j);
+                    j = prefix[j];
+                }
+            }
+            return result;
+        };
+
+        vector<int> result;
+        const auto& lookup = KMP(s, b);
+        int j = 0;
+        for (const auto& i : KMP(s, a)) {
+            for (; j < size(lookup) && lookup[j] < i - k; ++j);
+            if (j < size(lookup) && lookup[j] <= i + k) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n + xlogy), x = len(KMP(s, a)), y = len(KMP(s, b))
+// Space: O(n)
+// kmp, binary search
+class Solution2 {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        const auto& KMP = [&](const string& text, const string& pattern) {
+            const auto& getPrefix = [&](const string& pattern) {
+                vector<int> prefix(pattern.length(), -1);
+                int j = -1;
+                for (int i = 1; i < pattern.length(); ++i) {
+                    while (j > -1 && pattern[j + 1] != pattern[i]) {
+                        j = prefix[j];
+                    }
+                    if (pattern[j + 1] == pattern[i]) {
+                        ++j;
+                    }
+                    prefix[i] = j;
+                }
+                return prefix;
+            };
+
+            vector<int> result;
+            const vector<int> prefix = getPrefix(pattern);
+            int j = -1;
+            for (int i = 0; i < text.length(); ++i) {
+                while (j > -1 && pattern[j + 1] != text[i]) {
+                    j = prefix[j];
+                }
+                if (pattern[j + 1] == text[i]) {
+                    ++j;
+                }
+                if (j == pattern.length() - 1) {
+                    result.emplace_back(i - j);
+                    j = prefix[j];
+                }
+            }
+            return result;
+        };
+
+        vector<int> result;
+        const auto& lookup = KMP(s, b);
+        int j = 0;
+        for (const auto& i : KMP(s, a)) {
+            const int j = distance(cbegin(lookup), lower_bound(cbegin(lookup), cend(lookup), i - k));
+            if (j < size(lookup) && lookup[j] <= i + k) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
