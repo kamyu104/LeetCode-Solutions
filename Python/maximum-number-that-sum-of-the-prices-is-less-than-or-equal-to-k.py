11e3e6c11f41b8997275297f5600f7c773a784cc
kamyu <kamyu104@users.noreply.github.com>
2024-01-15 12:11:28 +0800

    Update maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py

--- Python/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py
+++ Python/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py
@@ -101,7 +101,7 @@ class Solution3(object):
         return result-1
 
 
-# Time:  O(k * ((logk) / x))
+# Time:  O((logk) * ((logk) / x))
 # Space: O(1)
 # bit manipulation, binary search, combinatorics
 class Solution4(object):
