411da6f00e4321cc9ec6f63eb7a20bd6146342f5
kamyu <kamyu104@users.noreply.github.com>
2024-01-15 12:02:56 +0800

    Update maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py

--- Python/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py
+++ Python/maximum-number-that-sum-of-the-prices-is-less-than-or-equal-to-k.py
@@ -74,10 +74,37 @@ class Solution2(object):
         return result-1
 
 
+# Time:  O((logk)^2)
+# Space: O(1)
+# bit manipulation, combinatorics
+class Solution3(object):
+    def findMaximumNumber(self, k, x):
+        """
+        :type k: int
+        :type x: int
+        :rtype: int
+        """
+        def floor_log2(x):
+            return x.bit_length()-1
+
+        result = prefix_cnt = 0
+        while k >= prefix_cnt:
+            # l = result.bit_length()
+            # assert(prefix_cnt == sum(c == '1' and (l-i)%x == 0 for i, c in enumerate(bin(result)[2:])))
+            cnt, i = prefix_cnt, 0
+            while (cnt<<1)+(1<<i if (i+1)%x == 0 else 0) <= k:
+                cnt = (cnt<<1)+(1<<i if (i+1)%x == 0 else 0)
+                i += 1
+            k -= cnt
+            result += 1<<i
+            prefix_cnt += int((i+1)%x == 0)
+        return result-1
+
+
 # Time:  O(k * ((logk) / x))
 # Space: O(1)
 # bit manipulation, binary search, combinatorics
-class Solution3(object):
+class Solution4(object):
     def findMaximumNumber(self, k, x):
         """
         :type k: int
