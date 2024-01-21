8134efd4f3814b3164add63bfbb5a2b0b42763de
kamyu <kamyu104@users.noreply.github.com>
2024-01-21 11:09:39 +0800

    Update find-if-array-can-be-sorted.cpp

--- C++/find-if-array-can-be-sorted.cpp
+++ C++/find-if-array-can-be-sorted.cpp
@@ -1,8 +1,28 @@
-// Time:  O(nlogn)
+// Time:  O(n)
 // Space: O(1)
 
 // sort
 class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+        for (int right = 0, left = 0, mx = 0; right < size(nums); ++right) {
+            if (right + 1 != size(nums) && __builtin_popcount(nums[right + 1]) == __builtin_popcount(nums[right])) {
+                continue;
+            }
+            if (mx > *min_element(cbegin(nums) + left, cbegin(nums) + right + 1)) {
+                return false;
+            }
+            mx = *max_element(cbegin(nums) + left, cbegin(nums) + right + 1);
+            left = right + 1;
+        }
+        return true;
+    }
+};
+
+// Time:  O(nlogn)
+// Space: O(1)
+// sort
+class Solution2 {
 public:
     bool canSortArray(vector<int>& nums) {
         for (int right = 0, left = 0; right < size(nums); ++right) {
