3a54276a742cb6d90313c26ab3a57472c5e19326
kamyu <kamyu104@users.noreply.github.com>
2024-01-21 12:35:14 +0800

    Create divide-an-array-into-subarrays-with-minimum-cost-ii.py

---
+++ Python/divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -0,0 +1,24 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+from sortedcontainers import SortedList
+
+
+# sorted list
+class Solution(object):
+    def minimumCost(self, nums, k, dist):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type dist: int
+        :rtype: int
+        """
+        sl = SortedList(nums[1:1+dist])
+        curr, mn = sum(sl[:k-2]), float("inf")
+        for i in xrange(1+dist, len(nums)):
+            curr += min(nums[i], sl[k-2] if k-2 < len(sl) else float("inf"))
+            mn = min(mn, curr)
+            sl.add(nums[i])
+            curr -= min(nums[i-dist], sl[k-2] if k-2 < len(sl) else float("inf"))
+            sl.remove(nums[i-dist])
+        return nums[0]+mn
