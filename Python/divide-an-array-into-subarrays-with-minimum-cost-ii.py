5356f2bc1933599290a5e5e08ad06dd585c5191c
kamyu <kamyu104@users.noreply.github.com>
2024-01-22 04:45:21 +0800

    Update divide-an-array-into-subarrays-with-minimum-cost-ii.py

--- Python/divide-an-array-into-subarrays-with-minimum-cost-ii.py
+++ Python/divide-an-array-into-subarrays-with-minimum-cost-ii.py
@@ -1,70 +1,11 @@
 # Time:  O(nlogd)
 # Space: O(d)
 
-from sortedcontainers import SortedList
-
-
-# sliding window, sorted list
-class Solution(object):
-    def minimumCost(self, nums, k, dist):
-        """
-        :type nums: List[int]
-        :type k: int
-        :type dist: int
-        :rtype: int
-        """
-        sl = SortedList(nums[1:1+(1+dist)])
-        mn = curr = sum(sl[:k-1])
-        for i in xrange(1+(1+dist), len(nums)):
-            sl.add(nums[i])
-            curr += min(nums[i]-sl[k-1], 0)
-            curr -= min(nums[i-(1+dist)]-sl[k-1], 0)
-            sl.remove(nums[i-(1+dist)])
-            mn = min(mn, curr)
-        return nums[0]+mn
-
-
-# Time:  O(nlogd)
-# Space: O(d)
-from sortedcontainers import SortedList
-
-
-# sliding window, sorted list
-class Solution2(object):
-    def minimumCost(self, nums, k, dist):
-        """
-        :type nums: List[int]
-        :type k: int
-        :type dist: int
-        :rtype: int
-        """
-        sl1, sl2 = SortedList(), SortedList()
-        mn, curr = float("inf"), 0
-        for i in xrange(1, len(nums)):
-            sl1.add(nums[i])
-            curr += nums[i]
-            if len(sl1) > k-1:
-                curr -= sl1[-1]
-                sl2.add(sl1.pop())
-            if len(sl1)+len(sl2) > 1+dist:
-                if sl2[0] <= nums[i-(1+dist)]:
-                    sl2.remove(nums[i-(1+dist)])
-                else:
-                    sl1.remove(nums[i-(1+dist)])
-                    curr -= nums[i-(1+dist)]-sl2[0]
-                    sl1.add(sl2.pop(0))
-            if len(sl1) == k-1:
-                mn = min(mn, curr)
-        return nums[0]+mn
-
-
-# Time:  O(nlogd)
-# Space: O(d)
 import heapq
 
 
 # sliding window, heap
-class Solution3(object):
+class Solution(object):
     def minimumCost(self, nums, k, dist):
         """
         :type nums: List[int]
@@ -108,7 +49,7 @@ import collections
 
 
 # sliding window, heap
-class Solution4(object):
+class Solution2(object):
     def minimumCost(self, nums, k, dist):
         """
         :type nums: List[int]
@@ -149,4 +90,63 @@ class Solution4(object):
                     heapq.heappush(max_heap, -heapq.heappop(min_heap))
             if len(max_heap)-total1 == k-1:
                 mn = min(mn, curr)
+        return nums[0]+mn
+
+
+# Time:  O(nlogd)
+# Space: O(d)
+from sortedcontainers import SortedList
+
+
+# sliding window, sorted list
+class Solution3(object):
+    def minimumCost(self, nums, k, dist):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type dist: int
+        :rtype: int
+        """
+        sl1, sl2 = SortedList(), SortedList()
+        mn, curr = float("inf"), 0
+        for i in xrange(1, len(nums)):
+            sl1.add(nums[i])
+            curr += nums[i]
+            if len(sl1) > k-1:
+                curr -= sl1[-1]
+                sl2.add(sl1.pop())
+            if len(sl1)+len(sl2) > 1+dist:
+                if sl2[0] <= nums[i-(1+dist)]:
+                    sl2.remove(nums[i-(1+dist)])
+                else:
+                    sl1.remove(nums[i-(1+dist)])
+                    curr -= nums[i-(1+dist)]-sl2[0]
+                    sl1.add(sl2.pop(0))
+            if len(sl1) == k-1:
+                mn = min(mn, curr)
+        return nums[0]+mn
+
+
+# Time:  O(nlogd)
+# Space: O(d)
+from sortedcontainers import SortedList
+
+
+# sliding window, sorted list
+class Solution4(object):
+    def minimumCost(self, nums, k, dist):
+        """
+        :type nums: List[int]
+        :type k: int
+        :type dist: int
+        :rtype: int
+        """
+        sl = SortedList(nums[1:1+(1+dist)])
+        mn = curr = sum(sl[:k-1])
+        for i in xrange(1+(1+dist), len(nums)):
+            sl.add(nums[i])
+            curr += min(nums[i]-sl[k-1], 0)
+            curr -= min(nums[i-(1+dist)]-sl[k-1], 0)
+            sl.remove(nums[i-(1+dist)])
+            mn = min(mn, curr)
         return nums[0]+mn
