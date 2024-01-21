c609dd6d289bc5eb379b26c550ada4d897486276
kamyu <kamyu104@users.noreply.github.com>
2024-01-21 18:57:23 +0800

    Update count-the-number-of-houses-at-a-certain-distance-ii.py

--- Python/count-the-number-of-houses-at-a-certain-distance-ii.py
+++ Python/count-the-number-of-houses-at-a-certain-distance-ii.py
@@ -16,8 +16,8 @@ class Solution(object):
         diff = [0]*n
         for i in xrange(n):
             diff[0] += 1+1                                         # i -> two routes begin
-            diff[min(abs(i-x), abs(i-y)+1)] += 1                   # i -> x -> y, fork one route at x
-            diff[min(abs(i-y), abs(i-x)+1)] += 1                   # i -> y -> x, fork one route at y
+            diff[min(abs(i-x), abs(i-y)+1)] += 1                   # i -> x -> y, fork one route at x to y
+            diff[min(abs(i-y), abs(i-x)+1)] += 1                   # i -> y -> x, fork one route at y to x
             diff[min(abs(i-0), abs(i-y)+1+abs(x-0))] -= 1          # i -> 0, one route ends
             diff[min(abs(i-(n-1)), abs(i-x)+1+abs(y-(n-1)))] -= 1  # i -> n-1, one route ends
             diff[max(x-i, 0)+max(i-y, 0)+((y-x)+0)//2] -= 1        # i -> x -> ((y-x)+0)//2 <- x, one route ends
