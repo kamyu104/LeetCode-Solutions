1039fcde715183d92b3899fcd7e25a6568182e53
kamyu <kamyu104@users.noreply.github.com>
2024-01-22 05:57:25 +0800

    Update minimum-number-of-pushes-to-type-word-i.cpp

--- C++/minimum-number-of-pushes-to-type-word-i.cpp
+++ C++/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,8 +1,27 @@
+// Time:  O(4)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    int minimumPushes(string word) {
+        const auto& ceil_divide = [](int a, int b) {
+            return (a + b - 1) / b;
+        };
+
+        int result = 0;
+        const int l = ceil_divide(size(word), (9 - 2 + 1));
+        for (int i = 1; i <= l; ++i) {
+            result += i * min(static_cast<int>(size(word)) - (i - 1) * (9 - 2 + 1), (9 - 2 + 1));
+        }
+        return result;
+    }
+};
+
 // Time:  O(26)
 // Space: O(26)
-
 // freq table, greedy
-class Solution {
+class Solution2 {
 public:
     int minimumPushes(string word) {
         vector<int> cnt(26);
