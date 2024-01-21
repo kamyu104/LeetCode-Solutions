d0048e9aed575140755ce2969b8445c941e54bb5
kamyu <kamyu104@users.noreply.github.com>
2024-01-22 04:29:18 +0800

    Update divide-an-array-into-subarrays-with-minimum-cost-ii.cpp

--- C++/divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ C++/divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -77,6 +77,52 @@ public:
     long long minimumCost(vector<int>& nums, int k, int dist) {
         static const int64_t INF = numeric_limits<int64_t>::max();
 
+        priority_queue<pair<int, int>> max_heap;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> min_heap;
+        int total1 = 0, total2 = 0;
+        int64_t curr = 0, mn = INF;
+        for (int i = 1; i < size(nums); ++i) {
+            max_heap.emplace(nums[i], -i);
+            curr += nums[i];
+            if (size(max_heap) - total1 > k - 1) {
+                while (-max_heap.top().second < i - (1 + dist)) {
+                    max_heap.pop();
+                    --total1;
+                }
+                const auto [x, idx] = max_heap.top(); max_heap.pop();
+                curr -= x;
+                min_heap.emplace(x, idx);
+            }
+            if ((size(max_heap) - total1) + (size(min_heap) - total2) > 1 + dist) {
+                while (-min_heap.top().second < i - (1 + dist)) {
+                    min_heap.pop();
+                    --total2;
+                }
+                if (min_heap.top() <= pair(nums[i - (1 + dist)], -(i - (1 + dist)))) {
+                    ++total2;
+                } else {
+                    ++total1;
+                    curr -= nums[i - (1 + dist)] - min_heap.top().first;
+                    max_heap.emplace(min_heap.top()); min_heap.pop();
+                }
+            }
+            if (size(max_heap) - total1 == k - 1) {
+                mn = min(mn, curr);
+            }
+        }
+        return nums[0] + mn;
+    }
+};
+
+// Time:  O(nlogd)
+// Space: O(d)
+// even faster but much more verbose
+// sliding window, heap
+class Solution4 {
+public:
+    long long minimumCost(vector<int>& nums, int k, int dist) {
+        static const int64_t INF = numeric_limits<int64_t>::max();
+
         priority_queue<int> max_heap;
         priority_queue<int, vector<int>, greater<int>> min_heap;
         unordered_map<int, int> cnt1, cnt2;
@@ -93,10 +139,10 @@ public:
                     }
                     --total1;
                 }
-                const int x = max_heap.top();
+                const int x = max_heap.top(); max_heap.pop();
                 curr -= x;
                 min_heap.emplace(x);
-                max_heap.pop();
+                
             }
             if ((size(max_heap) - total1) + (size(min_heap) - total2) > 1 + dist) {
                 while (cnt2.count(min_heap.top())) {
