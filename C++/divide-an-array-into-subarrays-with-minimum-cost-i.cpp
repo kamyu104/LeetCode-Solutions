f99962f4bbcd36b1fa86f8029a6acd49c676b103
kamyu <kamyu104@users.noreply.github.com>
2024-01-22 05:05:54 +0800

    Update divide-an-array-into-subarrays-with-minimum-cost-i.cpp

--- C++/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
+++ C++/divide-an-array-into-subarrays-with-minimum-cost-i.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int minimumCost(vector<int>& nums) {
-        nth_element(begin(nums) + 1, begin(nums) + 3, end(nums));
+        nth_element(begin(nums) + 1, (begin(nums) + 1) + (2 - 1), end(nums));
         return nums[0] + nums[1] + nums[2];
     }
 };
