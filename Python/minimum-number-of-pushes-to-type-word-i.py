ab2f8684022afc78d900a936216c5d0315d74f35
kamyu <kamyu104@users.noreply.github.com>
2024-01-21 14:15:39 +0800

    Create minimum-number-of-pushes-to-type-word-i.py

---
+++ Python/minimum-number-of-pushes-to-type-word-i.py
@@ -0,0 +1,12 @@
+# Time:  O(26)
+# Space: O(26)
+
+# freq table, greedy
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        cnt = collections.Counter(word)
+        return sum(x*(i//(9-2+1)+1) for i, x in enumerate(sorted(cnt.itervalues(), reverse=True)))
