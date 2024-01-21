79be2a515dcade46e1152064f8689e8f7e81839d
kamyu <kamyu104@users.noreply.github.com>
2024-01-21 18:57:02 +0800

    Update count-the-number-of-houses-at-a-certain-distance-ii.cpp

--- C++/count-the-number-of-houses-at-a-certain-distance-ii.cpp
+++ C++/count-the-number-of-houses-at-a-certain-distance-ii.cpp
@@ -12,8 +12,8 @@ public:
         vector<long long> diff(n);
         for (int i = 0; i < n; ++i) {
             diff[0] += 1 + 1;                                                  // i -> two routes begin
-            ++diff[min(abs(i - x), abs(i - y) + 1)];                           // i -> x -> y, fork one route at x
-            ++diff[min(abs(i - y), abs(i - x) + 1)];                           // i -> y -> x, fork one route at y
+            ++diff[min(abs(i - x), abs(i - y) + 1)];                           // i -> x -> y, fork one route at x to y
+            ++diff[min(abs(i - y), abs(i - x) + 1)];                           // i -> y -> x, fork one route at y to x
             --diff[min(abs(i - 0), abs(i - y) + 1 + abs(x - 0))];              // i -> 0, one route ends
             --diff[min(abs(i - (n - 1)), abs(i - x) + 1 + abs(y - (n - 1)))];  // i -> n-1, one route ends
             --diff[max(x - i, 0) + max(i - y, 0) + ((y - x) + 0) / 2];         // i -> x -> ((y-x)+0)//2 <- x, one route ends
