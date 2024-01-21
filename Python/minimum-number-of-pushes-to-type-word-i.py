b2bea983e7ea71c1fd774f638ce6eeb86fbbedf5
kamyu <kamyu104@users.noreply.github.com>
2024-01-22 05:44:11 +0800

    Update minimum-number-of-pushes-to-type-word-i.py

--- Python/minimum-number-of-pushes-to-type-word-i.py
+++ Python/minimum-number-of-pushes-to-type-word-i.py
@@ -1,11 +1,31 @@
+# Time:  O(4)
+# Space: O(1)
+
+# greedy
+class Solution(object):
+    def minimumPushes(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//b
+
+        result, cnt = 0, len(word)
+        for i in xrange(1, 1+ceil_divide(cnt, (9-2+1))):
+            g = min(cnt, (9-2+1))
+            cnt -= g
+            result += i*g
+        return result
+
+
 # Time:  O(26)
 # Space: O(26)
-
 import collections
 
 
 # freq table, greedy
-class Solution(object):
+class Solution2(object):
     def minimumPushes(self, word):
         """
         :type word: str
