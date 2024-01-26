a3e3b032fbb963ee23965d09c24e599f63babdba
kamyu <kamyu104@users.noreply.github.com>
2024-01-26 17:03:38 +0800

    Update maximum-number-of-removal-queries-that-can-be-processed-i.py

--- Python/maximum-number-of-removal-queries-that-can-be-processed-i.py
+++ Python/maximum-number-of-removal-queries-that-can-be-processed-i.py
@@ -9,13 +9,14 @@ class Solution(object):
         :type queries: List[int]
         :rtype: int
         """
-        dp = [[float("-inf")]*len(nums) for _ in xrange(len(nums))]
+        NEG_INF = float("-inf")
+        dp = [[NEG_INF]*len(nums) for _ in xrange(len(nums))]
         dp[0][-1] = 0
         for l in reversed(xrange(1, len(nums))):
             for i in xrange(len(nums)-(l-1)):
                 j = i+(l-1)
-                left = dp[i-1][j] if i-1 >= 0 else 0
-                right = dp[i][j+1] if j+1 < len(nums) else 0
+                left = dp[i-1][j] if i-1 >= 0 else NEG_INF
+                right = dp[i][j+1] if j+1 < len(nums) else NEG_INF
                 dp[i][j] = max(left, right)
                 if i-1 >= 0 and nums[i-1] >= queries[left]:
                     dp[i][j] = max(dp[i][j], left+1)
