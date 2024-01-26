96970ca2f155800c71122253bb3da13bd2dcc555
kamyu <kamyu104@users.noreply.github.com>
2024-01-26 17:02:09 +0800

    Update maximum-number-of-removal-queries-that-can-be-processed-i.cpp

--- C++/maximum-number-of-removal-queries-that-can-be-processed-i.cpp
+++ C++/maximum-number-of-removal-queries-that-can-be-processed-i.cpp
@@ -5,12 +5,14 @@
 class Solution {
 public:
     int maximumProcessableQueries(vector<int>& nums, vector<int>& queries) {
-        vector<vector<int>> dp(size(nums), vector<int>(size(nums)));
+        static const int NEG_INF = numeric_limits<int>::min();
+        vector<vector<int>> dp(size(nums), vector<int>(size(nums), NEG_INF));
+        dp[0].back() = 0;
         for (int l = size(nums) - 1; l >= 1; --l) {
             for (int i = 0; i + (l - 1) < size(nums); ++i) {
                 const int j = i + (l - 1);
-                const int left = i - 1 >= 0 ? dp[i - 1][j] : 0;
-                const int right = j + 1 < size(nums) ? dp[i][j + 1] : 0;
+                const int left = i - 1 >= 0 ? dp[i - 1][j] : NEG_INF;
+                const int right = j + 1 < size(nums) ? dp[i][j + 1] : NEG_INF;
                 dp[i][j] = max(left, right);
                 if (i - 1 >= 0 && nums[i - 1] >= queries[left]) {
                     dp[i][j] = max(dp[i][j], left + 1);
