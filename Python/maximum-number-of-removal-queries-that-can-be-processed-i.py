60d7ec6479970e24aab5c3943f7ac827a12fd82c
kamyu <kamyu104@users.noreply.github.com>
2024-01-26 17:16:34 +0800

    Update maximum-number-of-removal-queries-that-can-be-processed-i.py

--- Python/maximum-number-of-removal-queries-that-can-be-processed-i.py
+++ Python/maximum-number-of-removal-queries-that-can-be-processed-i.py
@@ -15,13 +15,10 @@ class Solution(object):
         for l in reversed(xrange(1, len(nums))):
             for i in xrange(len(nums)-(l-1)):
                 j = i+(l-1)
-                left = dp[i-1][j] if i-1 >= 0 else NEG_INF
-                right = dp[i][j+1] if j+1 < len(nums) else NEG_INF
-                dp[i][j] = max(left, right)
-                if i-1 >= 0 and nums[i-1] >= queries[left]:
-                    dp[i][j] = max(dp[i][j], left+1)
-                if j+1 < len(nums) and nums[j+1] >= queries[right]:
-                    dp[i][j] = max(dp[i][j], right+1)
+                if i-1 >= 0:
+                    dp[i][j] = max(dp[i][j], dp[i-1][j]+int(nums[i-1] >= queries[dp[i-1][j]]))
+                if j+1 < len(nums):
+                    dp[i][j] = max(dp[i][j], dp[i][j+1]+int(nums[j+1] >= queries[dp[i][j+1]]))
                 if dp[i][j] == len(queries):
                     return len(queries)
         return max(dp[i][i]+int(nums[i] >= queries[dp[i][i]])for i in xrange(len(nums)))
