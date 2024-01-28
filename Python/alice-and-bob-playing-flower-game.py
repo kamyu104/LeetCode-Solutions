69ce5bfa66fb7856be33d614dc036eba26deb7aa
kamyu <kamyu104@users.noreply.github.com>
2024-01-28 22:04:46 +0800

    Create alice-and-bob-playing-flower-game.py

---
+++ Python/alice-and-bob-playing-flower-game.py
@@ -0,0 +1,12 @@
+# Time:  O(1)
+# Space: O(1)
+
+# combinatorics
+class Solution(object):
+    def flowerGame(self, n, m):
+        """
+        :type n: int
+        :type m: int
+        :rtype: int
+        """
+        return (n*m)//2
