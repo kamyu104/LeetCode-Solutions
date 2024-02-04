0e8e594026995bdd63798c5003fad924aed8baaf
kamyu <kamyu104@users.noreply.github.com>
2024-02-05 02:30:21 +0800

    Update find-the-number-of-ways-to-place-people-ii.py

--- Python/find-the-number-of-ways-to-place-people-ii.py
+++ Python/find-the-number-of-ways-to-place-people-ii.py
@@ -1,58 +1,37 @@
-// Time:  O(n^2)
-// Space: O(1)
+# Time:  O(n^2)
+# Space: O(1)
 
-// sort, array
-class Solution {
-public:
-    int numberOfPairs(vector<vector<int>>& points) {
-        sort(begin(points), end(points), [](const auto& a, const auto& b) {
-            return a[0] != b[0] ? a[0] < b[0] : a[1] > b[1];
-        });
+# sort, array
+class Solution(object):
+    def numberOfPairs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort(key=lambda x: (x[0], -x[1]))
+        result = 0
+        for i in xrange(len(points)):
+            y = float("-inf")
+            for j in xrange(i+1, len(points)):
+                if points[i][1] < points[j][1]:
+                    continue
+                if points[j][1] > y:
+                    y = points[j][1]
+                    result += 1
+        return result
 
-        int result = 0;
-        for (int i = 0; i < size(points); ++i) {
-            for (int j = i + 1, y = numeric_limits<int>::min(); j < size(points); ++j) {
-                if (points[i][1] < points[j][1]) {
-                    continue;
-                }
-                if (points[j][1] > y) {
-                    y = points[j][1];
-                    ++result;
-                }
-            }
-        }
-        return result;
-    }
-};
 
-// Time:  O(n^3)
-// Space: O(1)
-// sort, array
-class Solution2 {
-public:
-    int numberOfPairs(vector<vector<int>>& points) {
-        sort(begin(points), end(points), [](const auto& a, const auto& b) {
-            return a[0] != b[0] ? a[0] < b[0] : a[1] > b[1];
-        });
-
-        int result = 0;
-        for (int i = 0; i < size(points); ++i) {
-            for (int j = i + 1, y = numeric_limits<int>::min(); j < size(points); ++j) {
-                if (points[i][1] < points[j][1]) {
-                    continue;
-                }
-                bool valid = true;
-                for (int k = i + 1; k < j; ++k) {
-                    if (points[i][1] >= points[k][1] && points[k][1] >= points[j][1]) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (valid) {
-                    ++result;
-                }
-            }
-        }
-        return result;
-    }
-};
+# Time:  O(n^3)
+# Space: O(1)
+# sort, array
+class Solution2(object):
+    def numberOfPairs(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        points.sort(key=lambda x: (x[0], -x[1]))
+        return sum(all(not points[i][1] >= points[k][1] >= points[j][1] for k in xrange(i+1, j))
+                   for i in xrange(len(points))
+                   for j in xrange(i+1, len(points)) if points[i][1] >= points[j][1])
+ 
