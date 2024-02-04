07d1c0d3008ce31296dde9cfa65a3ff03ad1d390
kamyu <kamyu104@users.noreply.github.com>
2024-02-05 01:35:57 +0800

    Create find-the-grid-of-region-average.cpp

---
+++ C++/find-the-grid-of-region-average.cpp
@@ -0,0 +1,37 @@
+// Time:  O(n)
+// Space: O(n)
+
+// z-function
+class Solution {
+public:
+    int minimumTimeToInitialState(string word, int k) {
+        const auto& ceil_divide = [](int a, int b) {
+            return (a + b - 1) / b;
+        };
+
+        // Template: https://cp-algorithms.com/string/z-function.html
+        const auto& z_function = [](const string& s) {  // Time: O(n), Space: O(n)
+            vector<int> z(size(s));
+            for (int i = 1, l = 0, r = 0; i < size(z); ++i) {
+                if (i <= r) {
+                    z[i] = min(r - i + 1, z[i - l]);
+                }
+                while (i + z[i] < size(z) && s[z[i]] == s[i + z[i]]) {
+                    ++z[i];
+                }
+                if (i + z[i] - 1 > r) {
+                    l = i, r = i + z[i] - 1;
+                }
+            }
+            return z;
+        };
+
+        const auto& z = z_function(word);
+        for (int i = k; i < size(word); i += k) {
+            if (z[i] == size(word) - i) {
+                return i / k;
+            }
+        }
+        return ceil_divide(size(word), k);
+    }
+};
