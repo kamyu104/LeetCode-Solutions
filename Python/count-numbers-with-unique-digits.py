9bb11cdda5b73bcf408b0404667016f48ee28abd
kamyu <kamyu104@users.noreply.github.com>
2024-02-11 06:59:33 +0800

    Update count-numbers-with-unique-digits.py

--- Python/count-numbers-with-unique-digits.py
+++ Python/count-numbers-with-unique-digits.py
@@ -9,9 +9,25 @@ class Solution(object):
         """
         if n == 0:
             return 1
-        count, fk = 10, 9
-        for k in xrange(2, n+1):
-            fk *= 10 - (k-1)
-            count += fk
-        return count
+        result = cnt = 1
+        for k in xrange(n-1):
+            cnt *= 9-k
+            result += cnt
+        return 1+9*result
 
+
+# Time:  O(n)
+# Space: O(n)
+class Solution2(object):
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        fact = [1]*2
+        def nPr(n, k):
+            while len(fact) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(fact))
+            return fact[n]//fact[n-k]
+
+        return 1+9*sum(nPr(9, i) for i in xrange(n))
