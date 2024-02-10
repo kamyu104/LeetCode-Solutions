35756d54b4a4da81d0346f65562c6f3c536aa7d6
kamyu <kamyu104@users.noreply.github.com>
2024-02-11 07:19:11 +0800

    Update count-numbers-with-unique-digits-ii.cpp

--- C++/count-numbers-with-unique-digits-ii.cpp
+++ C++/count-numbers-with-unique-digits-ii.cpp
@@ -5,6 +5,52 @@
 class Solution {
 public:
     int numberCount(int a, int b) {
+        const auto& count2 = [&](int n) {
+            if (n == 0) {
+                return 0;
+            }
+            int result = 1;
+            for (int i = 0, cnt = 1; i < n - 1; ++i) {
+                cnt *= 9 - i;
+                result += cnt;
+            }
+            return 9 * result;
+        };
+
+        const auto& count = [&](int x) {
+            vector<int> digits;
+            for (; x; x /= 10) {
+                digits.emplace_back(x % 10);
+            }
+            reverse(begin(digits), end(digits));
+            int result = count2(size(digits) - 1);
+            int lookup = 0;
+            int cnt = 1;
+            for (int i = 0; i < size(digits) - 1; ++i) {
+                cnt *= 9 - i;
+            }
+            for (int i = 0; i < size(digits); ++i) {
+                const int mask = lookup & (((1 << digits[i]) - 1) - (i == 0 ? 1 : 0));
+                result += ((digits[i] - (i == 0 ? 1 : 0)) - __builtin_popcount(mask)) * cnt;
+                cnt /= 9 - i;
+                if (lookup & (1 << digits[i])) {
+                    break;
+                }
+                lookup |= 1 << digits[i];
+            }
+            return result;
+        };
+
+        return count(b + 1) - count(a);
+    }
+};
+
+// Time:  O(logb)
+// Space: O(logb)
+// hash table, bitmask, combinatorics
+class Solution2 {
+public:
+    int numberCount(int a, int b) {
         vector<int> fact(2, 1);
         const auto& nPr = [&](int n, int k) {
             while (size(fact) <= n) {  // lazy initialization
@@ -43,7 +89,7 @@ public:
 // Time:  O(blogb)
 // Space: O(1)
 // hash table, bitmask
-class Solution2 {
+class Solution3 {
 public:
     int numberCount(int a, int b) {
         const auto& check = [](int x) {
