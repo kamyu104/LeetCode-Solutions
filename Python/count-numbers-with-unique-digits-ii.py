6374095217ba5b9bfef9f757282b9f6c67ce4f62
kamyu <kamyu104@users.noreply.github.com>
2024-02-11 06:27:13 +0800

    Update count-numbers-with-unique-digits-ii.py

--- Python/count-numbers-with-unique-digits-ii.py
+++ Python/count-numbers-with-unique-digits-ii.py
@@ -1,8 +1,38 @@
+# Time:  O(logb)
+# Space: O(logb)
+
+# combinatorics
+class Solution(object):
+    def numberCount(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        fact = [1]*2
+        def nPr(n, k):
+            while len(fact) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(fact))
+            return fact[n]//fact[n-k]
+
+        def count(x):
+            digits = map(int, str(x))
+            result = 9*sum(nPr(9, i) for i in xrange(len(digits)-1))
+            lookup = 0
+            for i, d in enumerate(digits):
+                result += sum(lookup&(1<<j) == 0 for j in xrange(int(i == 0), d))*nPr(10-(i+1), len(digits)-(i+1))
+                if lookup&(1<<d):
+                    break
+                lookup |= 1<<d
+            return result
+
+        return count(b+1)-count(a)
+
+
 # Time:  O(blogb)
 # Space: O(1)
-
 # hash table
-class Solution(object):
+class Solution2(object):
     def numberCount(self, a, b):
         """
         :type a: int
@@ -24,7 +54,7 @@ class Solution(object):
 # Time:  O(blogb)
 # Space: O(logb)
 # hash table
-class Solution2(object):
+class Solution3(object):
     def numberCount(self, a, b):
         """
         :type a: int
