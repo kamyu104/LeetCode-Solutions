ac9d1ad4c57b43faa7f184224a99ed219b0c50db
kamyu <kamyu104@users.noreply.github.com>
2024-02-11 07:03:57 +0800

    Update count-numbers-with-unique-digits.cpp

--- C++/count-numbers-with-unique-digits.cpp
+++ C++/count-numbers-with-unique-digits.cpp
@@ -7,12 +7,33 @@ public:
         if (n == 0) {
             return 1;
         }
-        int count = 10;  // f(1) = 10
-        for (int k = 2, fk = 9; k <= n; ++k) {
-            // f(k) = f(k - 1) * (10 - (k - 1)) 
-            fk *= 10 - (k - 1);
-            count += fk;
+        int result = 1;
+        for (int i = 0, cnt = 1; i < n - 1; ++i) {
+            cnt *= 9 - i;
+            result += cnt;
         }
-        return count;  // sum(f(k), k=1~n)
+        return 1 + 9 * result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+class Solution2 {
+public:
+    int countNumbersWithUniqueDigits(int n) {
+        vector<int> fact(2, 1);
+        const auto& nPr = [&](int n, int k) {
+            while (size(fact) <= n) {  // lazy initialization
+                fact.emplace_back(fact.back() * size(fact));
+            }
+            return fact[n] / fact[n - k];
+        };
+
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            result += nPr(9, i);
+        }
+        result *= 9;
+        return ++result;
     }
 };
