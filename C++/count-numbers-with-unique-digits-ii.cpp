3b0d1d902d03b2a7f7734b3bbbb14b0e0fdce7a7
kamyu <kamyu104@users.noreply.github.com>
2024-02-11 06:42:13 +0800

    Update count-numbers-with-unique-digits-ii.cpp

--- C++/count-numbers-with-unique-digits-ii.cpp
+++ C++/count-numbers-with-unique-digits-ii.cpp
@@ -1,8 +1,49 @@
+// Time:  O(logb)
+// Space: O(logb)
+
+// hash table, bitmask, combinatorics
+class Solution {
+public:
+    int numberCount(int a, int b) {
+        vector<int> fact(2, 1);
+        const auto& nPr = [&](int n, int k) {
+            while (size(fact) <= n) {  // lazy initialization
+                fact.emplace_back(fact.back() * size(fact));
+            }
+            return fact[n] / fact[n - k];
+        };
+
+        const auto& count = [&](int x) {
+            vector<int> digits;
+            for (; x; x /= 10) {
+                digits.emplace_back(x % 10);
+            }
+            reverse(begin(digits), end(digits));
+            int result = 0;
+            for (int i = 0; i < size(digits) - 1; ++i) {
+                result += nPr(9, i);
+            }
+            result *= 9;
+            int lookup = 0;
+            for (int i = 0; i < size(digits); ++i) {
+                const int mask = lookup & (((1 << digits[i]) - 1) - (i == 0 ? 1 : 0));
+                result += ((digits[i] - (i == 0 ? 1 : 0)) - __builtin_popcount(mask)) * nPr(10 - (i + 1), size(digits) - (i + 1));
+                if (lookup & (1 << digits[i])) {
+                    break;
+                }
+                lookup |= 1 << digits[i];
+            }
+            return result;
+        };
+
+        return count(b + 1) - count(a);
+    }
+};
+
 // Time:  O(blogb)
 // Space: O(1)
-
-// hash table
-class Solution {
+// hash table, bitmask
+class Solution2 {
 public:
     int numberCount(int a, int b) {
         const auto& check = [](int x) {
