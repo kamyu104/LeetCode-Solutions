cb69ae1be44218e5a7566d50bc2840a0ca04518e
kamyu <kamyu104@users.noreply.github.com>
2024-02-13 08:28:01 +0800

    Create number-of-subarrays-that-match-a-pattern-ii.cpp

---
+++ C++/number-of-subarrays-that-match-a-pattern-ii.cpp
@@ -0,0 +1,53 @@
+// Time:  O(n)
+// Space: O(m)
+
+// kmp
+class Solution {
+public:
+    int countMatchingSubarrays(vector<int>& nums, vector<int>& pattern) {
+        const auto& KMP = [](const auto& text, const auto& pattern) {
+            const auto& getPrefix = [](const auto& pattern) {
+                vector<int> prefix(size(pattern), -1);
+                int j = -1;
+                for (int i = 1; i < size(pattern); ++i) {
+                    while (j != -1 && pattern[j + 1] != pattern[i]) {
+                        j = prefix[j];
+                    }
+                    if (pattern[j + 1] == pattern[i]) {
+                        ++j;
+                    }
+                    prefix[i] = j;
+                }
+                return prefix;
+            };
+
+            int result = 0;
+            const vector<int> prefix = getPrefix(pattern);
+            int j = -1;
+            for (int i = 0; i < size(text); ++i) {
+                while (j > -1 && pattern[j + 1] != text[i]) {
+                    j = prefix[j];
+                }
+                if (pattern[j + 1] == text[i]) {
+                    ++j;
+                }
+                if (j == size(pattern) - 1) {
+                    ++result;
+                    j = prefix[j];
+                }
+            }
+            return result;
+        };
+    
+        for (int i = 0; i + 1 < size(nums); ++i) {
+            if (nums[i + 1] > nums[i]) {
+                nums[i] = 1;
+            } else if (nums[i + 1] < nums[i]) {
+                nums[i] = -1;
+            } else {
+                nums[i] = 0;
+            }
+        }
+        return KMP(nums, pattern);
+    }
+};
