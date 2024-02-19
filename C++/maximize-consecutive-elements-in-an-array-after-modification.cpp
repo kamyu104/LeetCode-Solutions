c56292c2c0822cfe77698e99c8d58e343cdd29e3
kamyu <kamyu104@users.noreply.github.com>
2024-02-20 07:44:07 +0800

    Update maximize-consecutive-elements-in-an-array-after-modification.cpp

--- C++/maximize-consecutive-elements-in-an-array-after-modification.cpp
+++ C++/maximize-consecutive-elements-in-an-array-after-modification.cpp
@@ -7,7 +7,7 @@ public:
     int maxSelectedElements(vector<int>& nums) {
         sort(begin(nums), end(nums));
         int result = 1;
-        vector<int> dp(2, 1);
+        vector<int> dp(2, 1);  // dp[i]: the maximum length of a sequence ending with x+i, where x is the last visited value
         for (int i = 1; i < size(nums); ++i) {
             if (nums[i] == nums[i - 1]) {
                 dp[1] = dp[0] + 1;
