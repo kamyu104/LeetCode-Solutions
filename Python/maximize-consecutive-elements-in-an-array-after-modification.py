1eaf7902ca441dcbf56b94962ec05bc5b85e13c3
kamyu <kamyu104@users.noreply.github.com>
2024-02-21 17:53:40 +0800

    Update maximize-consecutive-elements-in-an-array-after-modification.py

--- Python/maximize-consecutive-elements-in-an-array-after-modification.py
+++ Python/maximize-consecutive-elements-in-an-array-after-modification.py
@@ -40,6 +40,35 @@ class Solution2(object):
         """
         nums.sort()
         dp = collections.defaultdict(int)
+        dp[nums[0]] = dp[nums[0]+1] = 1
+        for i in xrange(1, len(nums)):
+            if nums[i] == nums[i-1]:
+                dp[nums[i]+1] = dp[nums[i]]+1
+            elif nums[i] == nums[i-1]+1:
+                dp[nums[i]+1] = dp[nums[i]]+1
+                dp[nums[i]] = dp[nums[i]-1]+1
+            elif nums[i] == nums[i-1]+2:
+                dp[nums[i]] = dp[nums[i]-1]+1
+                dp[nums[i]+1] = 1
+            else:
+                dp[nums[i]+1] = dp[nums[i]] = 1
+        return max(dp.itervalues())
+
+
+# Time:  O(nlogn)
+# Space: O(n)
+import collections
+
+
+# sort, dp
+class Solution3(object):
+    def maxSelectedElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        nums.sort()
+        dp = collections.defaultdict(int)
         for x in nums:
             dp[x+1] = dp[x]+1
             dp[x] = dp[x-1]+1
