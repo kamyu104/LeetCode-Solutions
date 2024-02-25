313024efce109e9978554f2058ee0e1b73ca0a42
kamyu <kamyu104@users.noreply.github.com>
2024-02-25 21:20:53 +0800

    Create earliest-second-to-mark-indices-ii.cpp

---
+++ C++/earliest-second-to-mark-indices-ii.cpp
@@ -0,0 +1,51 @@
+// Time:  O((m + nlogn) * logm)
+// Space: O(n)
+
+// binary search, greedy, heap
+class Solution {
+public:
+    int earliestSecondToMarkIndices(vector<int>& nums, vector<int>& changeIndices) {
+        vector<int> lookup(size(nums), -1);
+        for (int i = size(changeIndices) - 1; i >= 0; --i) {
+            if (nums[changeIndices[i] - 1]) {
+                lookup[changeIndices[i] - 1] = i;
+            }
+        }
+        const int64_t total = accumulate(cbegin(nums), cend(nums), 0ll) + size(nums);
+        const auto& check = [&](int t) {
+            priority_queue<int, vector<int>, greater<int>> min_heap;
+            int64_t total2 = 0, cnt = 0;
+            for (int i = t - 1; i >= 0; --i) {
+                if (i != lookup[changeIndices[i] - 1]) {
+                    ++cnt;
+                    continue;
+                }
+                min_heap.emplace(nums[changeIndices[i] - 1]);
+                total2 += nums[changeIndices[i] - 1];
+                if (cnt) {
+                    --cnt;
+                } else {
+                    ++cnt;
+                    total2 -= min_heap.top();
+                    min_heap.pop();
+                }
+            }
+            return total - (total2 + (size(min_heap))) <= cnt;
+        };
+    
+        int64_t cnt = 0;
+        for (int i = 0; i < size(nums); ++i) {
+            cnt += lookup[i] != -1 ? 1 : nums[i];
+        }
+        int64_t left = cnt + size(nums), right = size(changeIndices);
+        while (left <= right) {
+            const int mid = left + (right - left) / 2;
+            if (check(mid)) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left <= size(changeIndices) ? left : -1;
+    }
+};
