6486a03ed1042ffaee617b36336f12125a83f261
kamyu <kamyu104@users.noreply.github.com>
2024-03-04 18:54:50 +0800

    Create maximize-items.sql

---
+++ MySQL/maximize-items.sql
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(n)
+
+WITH prime_cte AS (
+    SELECT item_type,
+           FLOOR(500000 / SUM(square_footage)) * COUNT(*) AS item_count,
+           FLOOR(500000 / SUM(square_footage)) * SUM(square_footage) AS total
+    FROM Inventory
+    WHERE item_type = 'prime_eligible'
+), not_prime_cte AS (
+    SELECT item_type,
+           FLOOR((500000 - (SELECT total FROM prime_cte)) / SUM(square_footage)) * COUNT(*) AS item_count
+    FROM Inventory
+    WHERE item_type = 'not_prime'
+)
+
+(
+    SELECT item_type, item_count
+    FROM prime_cte
+)
+UNION ALL
+(
+    SELECT item_type,  item_count
+    FROM not_prime_cte
+)
+ORDER BY item_count DESC;
