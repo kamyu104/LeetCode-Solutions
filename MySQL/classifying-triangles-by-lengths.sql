4f2329e848996ceffa54a0af7928dde53ac91874
kamyu <kamyu104@users.noreply.github.com>
2024-03-04 18:55:44 +0800

    Create classifying-triangles-by-lengths.sql

---
+++ MySQL/classifying-triangles-by-lengths.sql
@@ -0,0 +1,9 @@
+# Time:  O(n)
+# Space: O(n)
+
+SELECT CASE WHEN A + B <= C OR A + C <= B OR B + C <= A THEN "Not A Triangle"
+            WHEN A = B AND A = C THEN "Equilateral"
+            WHEN A = B OR A = C OR B = C THEN "Isosceles"
+            ELSE "Scalene"
+       END AS triangle_type
+FROM Triangles;
