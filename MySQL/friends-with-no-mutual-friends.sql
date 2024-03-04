bbce8156ea24d17f2240725bcf0e6c322f2b4467
kamyu <kamyu104@users.noreply.github.com>
2024-03-04 23:09:13 +0800

    Create friends-with-no-mutual-friends.sql

---
+++ MySQL/friends-with-no-mutual-friends.sql
@@ -0,0 +1,25 @@
+# Time:  O(n^2)
+# Space: O(n^2)
+
+WITH pairs_cte AS (
+    SELECT user_id1 AS user_id,
+           user_id2 AS friend_id
+    FROM Friends
+    UNION ALL 
+    SELECT user_id2 AS user_id,
+           user_id1 AS friend_id
+    FROM Friends
+), shared_cte AS (
+    SELECT p1.user_id AS user_id1,
+           p2.user_id AS user_id2
+    FROM pairs_cte p1
+    INNER JOIN pairs_cte p2
+    ON p1.friend_id = p2.friend_id
+)
+
+SELECT f.user_id1, f.user_id2
+FROM Friends f
+LEFT JOIN shared_cte s
+ON f.user_id1 = s.user_id1 AND f.user_id2 = s.user_id2
+WHERE s.user_id1 IS NULL
+ORDER BY 1, 2;
