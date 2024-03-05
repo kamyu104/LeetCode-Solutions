8266f6559b7bfc21e0eb47ccff73ac04b0020d55
kamyu <kamyu104@users.noreply.github.com>
2024-03-05 16:12:46 +0800

    Create count-submatrices-with-top-left-element-and-sum-less-than-k.cpp

---
+++ C++/count-submatrices-with-top-left-element-and-sum-less-than-k.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n^2)
+// Space: O(1)
+
+// prefix sum
+class Solution {
+public:
+    int countSubmatrices(vector<vector<int>>& grid, int k) {
+        int result = 0;
+        for (int i = 0; i < size(grid); ++i) {
+            for (int j = 0; j < size(grid[0]); ++j) {
+                if (i - 1 >= 0) {
+                    grid[i][j] += grid[i - 1][j];
+                }
+                if (j - 1 >= 0) {
+                    grid[i][j] += grid[i][j - 1];
+                }
+                if (i - 1 >= 0 && j - 1 >= 0) {
+                    grid[i][j] -= grid[i - 1][j - 1];
+                }
+                if (grid[i][j] <= k) {
+                    ++result;
+                }
+            }
+        }
+        return result;
+    }
+};
