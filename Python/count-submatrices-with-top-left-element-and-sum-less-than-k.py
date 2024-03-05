0fe5d9bb15ccc1a23e9e82be5d56fad237676863
kamyu <kamyu104@users.noreply.github.com>
2024-03-05 16:13:15 +0800

    Create count-submatrices-with-top-left-element-and-sum-less-than-k.py

---
+++ Python/count-submatrices-with-top-left-element-and-sum-less-than-k.py
@@ -0,0 +1,23 @@
+# Time:  O(n^2)
+# Space: O(1)
+
+# prefix sum
+class Solution(object):
+    def countSubmatrices(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                if i-1 >= 0:
+                    grid[i][j] += grid[i-1][j]
+                if j-1 >= 0:
+                    grid[i][j] += grid[i][j-1]
+                if i-1 >= 0 and j-1 >= 0:
+                    grid[i][j] -= grid[i-1][j-1]
+                if grid[i][j] <= k:
+                    result += 1
+        return result
