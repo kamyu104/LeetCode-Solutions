61f00080423a649e0e8604d59e61a5c41e237aea
kamyu <kamyu104@users.noreply.github.com>
2024-03-10 14:10:37 +0800

    Update maximum-strength-of-k-disjoint-subarrays.cpp

--- C++/maximum-strength-of-k-disjoint-subarrays.cpp
+++ C++/maximum-strength-of-k-disjoint-subarrays.cpp
@@ -1,4 +1,4 @@
-// Time:  O(n^2)
+// Time:  O(n * k)
 // Space: O(n)
 
 // dp
@@ -22,8 +22,8 @@ public:
     }
 };
 
-// Time:  O(n^2)
-// Space: O(n^2)
+// Time:  O(n * k)
+// Space: O(n * k)
 // dp
 class Solution2 {
 public:
