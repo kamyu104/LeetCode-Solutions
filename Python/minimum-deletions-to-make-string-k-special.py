07e46d9ffd39ce7e638a3961fd915e9fa1e53659
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 05:38:13 +0800

    Update minimum-deletions-to-make-string-k-special.py

--- Python/minimum-deletions-to-make-string-k-special.py
+++ Python/minimum-deletions-to-make-string-k-special.py
@@ -47,10 +47,40 @@ class Solution(object):
         return result
 
 
+# Time:  O(n + 26 * log(26))
+# Space: O(26)
+# freq table, sort, two pointers
+class Solution2(object):
+    def minimumDeletions(self, word, k):
+        """
+        :type word: str
+        :type k: int
+        :rtype: int
+        """
+        cnt = [0]*26
+        for x in word:
+            cnt[ord(x)-ord('a')] += 1
+        arr = sorted(x for x in cnt if x)
+        result = float("inf")
+        right = prefix = 0
+        suffix = len(word)
+        prev = -1
+        for left in xrange(len(arr)):
+            if left+1 < len(arr) and arr[left+1] == arr[left]:
+                continue
+            while right < len(arr) and arr[right] <= arr[left]+k:
+                suffix -= arr[right]
+                right += 1
+            result = min(result, prefix+(suffix-(arr[left]+k)*(len(arr)-right)))
+            prefix += arr[left]*(left-prev)
+            prev = left
+        return result
+
+
 # Time:  O(n + 26^2)
 # Space: O(26)
 # freq table
-class Solution2(object):
+class Solution3(object):
     def minimumDeletions(self, word, k):
         """
         :type word: str
