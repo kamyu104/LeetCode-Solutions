18d1011af36b7214f4e2aa3945fff0b92b5c8ab1
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 04:31:53 +0800

    Create existence-of-a-substring-in-a-string-and-its-reverse.cpp

---
+++ C++/existence-of-a-substring-in-a-string-and-its-reverse.cpp
@@ -0,0 +1,38 @@
+// Time:  O(n)
+// Space: O(n)
+
+// hash table
+class Solution {
+public:
+    bool isSubstringPresent(string s) {
+        vector<vector<bool>> lookup(26, vector<bool>(26));
+        for (int i = 0; i + 1 < size(s); ++i) {
+            lookup[s[i] - 'a'][s[i + 1] - 'a'] = true;
+        }
+        for (int i = 0; i + 1 < size(s); ++i) {
+            if (lookup[s[i + 1] - 'a'][s[i] - 'a']) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// hash table
+class Solution2 {
+public:
+    bool isSubstringPresent(string s) {
+        unordered_map<char, unordered_set<char>> lookup;
+        for (int i = 0; i + 1 < size(s); ++i) {
+            lookup[s[i]].emplace(s[i + 1]);
+        }
+        for (int i = 0; i + 1 < size(s); ++i) {
+            if (lookup[s[i + 1]].count(s[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
