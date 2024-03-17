d8da58f06ad824ddfc0e998597300ae4831a696e
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 01:20:40 +0800

    Create replace-question-marks-in-string-to-minimize-its-value.py

---
+++ Python/replace-question-marks-in-string-to-minimize-its-value.py
@@ -0,0 +1,117 @@
+# Time:  O(n + 26 * log(26))
+# Space: O(26)
+
+# counting sort, prefix sum, binary search
+class Solution(object):
+    def minimizeStringValue(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def counting_sort(cnt):
+            for i in xrange(len(cnt)):
+                for _ in xrange(cnt[i]):
+                    yield i
+        
+        def fill(cnt):
+            result = [0]*26
+            a = [(x, i) for i, x in enumerate(cnt)]
+            a.sort()
+            prefix = [0]*26
+            for i in xrange(len(a)-1):
+                prefix[i+1] = prefix[i]+(a[i+1][0]-a[i][0])*(i+1)
+            total = s.count('?')
+            i = bisect.bisect_right(prefix, total)-1
+            q, r = divmod(total-prefix[i], i+1)
+            for j in xrange(i+1):
+                result[a[j][1]] = (a[i][0]-a[j][0])+q
+            cnt2 = [0]*26
+            for j in xrange(i+1):
+                cnt2[a[j][1]] += 1
+            it = counting_sort(cnt2)
+            for _ in xrange(r):
+                result[next(it)] += 1
+            return result
+    
+        cnt = [0]*26
+        for x in s:
+            if x == '?':
+                continue
+            cnt[ord(x)-ord('a')] += 1
+        it = counting_sort(fill(cnt))
+        result = list(s)
+        for i in xrange(len(result)):
+            if result[i] != '?':
+                continue
+            result[i] = chr(ord('a')+next(it))
+        return "".join(result)
+
+
+# Time:  O(n + n * log(26))
+# Space: O(26)
+import heapq
+
+
+# counting sort, heap
+class Solution2(object):
+    def minimizeStringValue(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def counting_sort():
+            for i in xrange(len(cnt2)):
+                for _ in xrange(cnt2[i]):
+                    yield i
+
+        cnt = [0]*26
+        for x in s:
+            if x == '?':
+                continue
+            cnt[ord(x)-ord('a')] += 1
+        min_heap = [(x, i) for i, x in enumerate(cnt)]
+        heapq.heapify(min_heap)
+        cnt2 = [0]*26
+        for _ in xrange(s.count('?')):
+            cnt, i = heapq.heappop(min_heap)
+            heapq.heappush(min_heap, (cnt+1, i))
+            cnt2[i] += 1
+        it = counting_sort()
+        result = list(s)
+        for i in xrange(len(result)):
+            if result[i] != '?':
+                continue
+            result[i] = chr(ord('a')+next(it))
+        return "".join(result)
+
+
+# Time:  O(n + n * 26)
+# Space: O(26)
+# counting sort
+class Solution3(object):
+    def minimizeStringValue(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        def counting_sort():
+            for i in xrange(len(cnt2)):
+                for _ in xrange(cnt2[i]):
+                    yield i
+    
+        cnt = [0]*26
+        for x in s:
+            if x == '?':
+                continue
+            cnt[ord(x)-ord('a')] += 1
+        cnt2 = [0]*26
+        for _ in xrange(s.count('?')):
+            i = min(xrange(len(cnt)), key=lambda x: cnt[x]+cnt2[x])
+            cnt2[i] += 1
+        it = counting_sort()
+        result = list(s)
+        for i in xrange(len(result)):
+            if result[i] != '?':
+                continue
+            result[i] = chr(ord('a')+next(it))
+        return "".join(result)
