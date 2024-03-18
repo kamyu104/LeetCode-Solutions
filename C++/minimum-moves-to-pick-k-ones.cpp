0a164fd98e9722f141a8695812bcd1ef7aec3a74
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 08:57:48 +0800

    Update minimum-moves-to-pick-k-ones.cpp

--- C++/minimum-moves-to-pick-k-ones.cpp
+++ C++/minimum-moves-to-pick-k-ones.cpp
@@ -19,9 +19,9 @@ public:
         const int bound = min({cnt + 3, k, static_cast<int>(size(idxs))});
         int64_t result = numeric_limits<int64_t>::max();
         for (int l = cnt; l <= bound; ++l) {
+            const auto cnt1 = (k - l) * 2;
             for (int i = 0; i + l < size(prefix); ++i) {
                 const auto cnt2 = (prefix[(i + l - 1) + 1] - prefix[(i + l - 1) - (l / 2 - 1)]) - (prefix[(i + (l / 2 - 1)) + 1] - prefix[i]);
-                const auto cnt1 = (k - l) * 2;
                 result = min(result, cnt2 + cnt1);
             }
         }
