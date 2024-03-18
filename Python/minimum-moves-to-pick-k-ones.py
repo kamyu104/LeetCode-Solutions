5d93b2ee2f44ece863172eef399a212c1604c908
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 08:55:34 +0800

    Update minimum-moves-to-pick-k-ones.py

--- Python/minimum-moves-to-pick-k-ones.py
+++ Python/minimum-moves-to-pick-k-ones.py
@@ -15,9 +15,10 @@ class Solution(object):
         for i in xrange(len(idxs)):
             prefix[i+1] = prefix[i]+idxs[i]
         result = float("inf")
-        for l in xrange(max(k-maxChanges, 0), min(max(k-maxChanges, 0)+3, k, len(idxs))+1):
+        cnt = max(k-maxChanges, 0)
+        for l in xrange(cnt, min(cnt+3, k, len(idxs))+1):
+            cnt1 = (k-l)*2
             for i in xrange(len(idxs)-l+1):
                 cnt2 = (prefix[(i+l-1)+1]-prefix[(i+l-1)-(l//2-1)])-(prefix[(i+(l//2-1))+1]-prefix[i])
-                cnt1 = (k-l)*2
                 result = min(result, cnt2+cnt1)
         return result
