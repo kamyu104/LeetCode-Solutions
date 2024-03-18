fce3b617d02f6b5e809833fab58eabb95a7a4f82
kamyu <kamyu104@users.noreply.github.com>
2024-03-18 08:12:48 +0800

    Create find-trending-hashtags.sql

---
+++ MySQL/find-trending-hashtags.sql
@@ -0,0 +1,10 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT CONCAT("#", SUBSTRING_INDEX(SUBSTRING_INDEX(tweet, "#", -1), " ", 1)) AS hashtag,
+       COUNT(*) AS hashtag_count
+FROM Tweets
+WHERE tweet_date BETWEEN "2024-02-01" AND "2024-02-29"
+GROUP BY 1
+ORDER BY 2 DESC, 1 DESC
+LIMIT 3;
