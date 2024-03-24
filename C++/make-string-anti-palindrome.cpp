7ef59d149a71af795a26e139d4c31f5d70e3892c
kamyu <kamyu104@users.noreply.github.com>
2024-03-25 00:58:07 +0800

    Create make-string-anti-palindrome.cpp

---
+++ C++/make-string-anti-palindrome.cpp
@@ -0,0 +1,67 @@
+// Time:  O(n)
+// Space: O(26)
+
+// counting sort, greedy, two pointers
+class Solution {
+public:
+    string makeAntiPalindrome(string s) {
+        vector<int> cnt(26);
+        for (const auto& x : s) {
+            ++cnt[x - 'a'];
+        }
+        if (ranges::max(cnt) > size(s) / 2) {
+            return "-1";
+        }
+        string result;
+        for (int i = 0; i < size(cnt); ++i) {
+            for (int _ = 0; _ < cnt[i]; ++_) {
+                result.push_back('a' + i);
+            }
+        }
+        int left = size(s) / 2;
+        int right = left + 1;
+        for (; right < size(s) && result[right] == result[left]; ++right);
+        for (; result[left] == result[(size(s) - 1) - left]; ++left, ++right) {
+            swap(result[left], result[right]);
+        }
+        return result;
+    }
+};
+
+// Time:  O(26 * n)
+// Space: O(26)
+// freq table, greedy
+class Solution2 {
+public:
+    string makeAntiPalindrome(string s) {
+        vector<int> cnt(26);
+        for (const auto& x : s) {
+            ++cnt[x - 'a'];
+        }
+        if (ranges::max(cnt) > size(s) / 2) {
+            return "-1";
+        }
+        string result(size(s), 0);
+        for (int i = 0; i < size(s) / 2; ++i) {
+            int j = 0;
+            for (; j < size(cnt); ++j) {
+                if (cnt[j]) {
+                    break;
+                }
+            }
+            --cnt[j];
+            result[i] = 'a' + j;
+        }
+        for (int i = size(s) / 2; i < size(s); ++i) {
+            int j = 0;
+            for (; j < size(cnt); ++j) {
+                if (cnt[j] && result[(size(s) - 1) - i] != 'a' + j) {
+                    break;
+                }
+            }
+            --cnt[j];
+            result[i] = 'a' + j;
+        }
+        return result;
+    }
+};
