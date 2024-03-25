5ef95ec04116cd4473c450d32b5f202c86315b51
kamyu <kamyu104@users.noreply.github.com>
2024-03-25 23:14:04 +0800

    Update find-bursty-behavior.sql

--- MySQL/find-bursty-behavior.sql
+++ MySQL/find-bursty-behavior.sql
@@ -2,13 +2,7 @@
 # Space: O(n)
 
 # window function
-WITH avg_weekly_posts_cte AS (
-    SELECT user_id, COUNT(*) / 4.0 AS avg_weekly_posts
-    FROM Posts
-    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
-    GROUP BY 1
-    ORDER BY NULL
-), posts_in_7_days_cte AS (
+WITH posts_in_7_days_cte AS (
     SELECT user_id, post_date, COUNT(*) OVER w AS sevenday_posts
     FROM Posts
     WINDOW w AS (PARTITION BY user_id 
@@ -18,6 +12,12 @@ WITH avg_weekly_posts_cte AS (
 ), max_posts_in_7_days_cte AS (
     SELECT user_id, MAX(sevenday_posts) AS max_7day_posts
     FROM posts_in_7_days_cte
+    GROUP BY 1
+    ORDER BY NULL
+), avg_weekly_posts_cte AS (
+    SELECT user_id, COUNT(*) / 4.0 AS avg_weekly_posts
+    FROM Posts
+    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
     GROUP BY 1
     ORDER BY NULL
 )
@@ -32,13 +32,7 @@ ORDER BY 1;
 # Time:  O(nlogn)
 # Space: O(n)
 # window function
-WITH avg_weekly_posts_cte AS (
-    SELECT user_id, COUNT(*) / 4.0 AS avg_weekly_posts
-    FROM Posts
-    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
-    GROUP BY 1
-    ORDER BY NULL
-), posts_in_7_days_cte AS (
+WITH posts_in_7_days_cte AS (
     SELECT user_id, post_date, COUNT(*) OVER w AS sevenday_posts
     FROM Posts
     WINDOW w AS (PARTITION BY user_id 
@@ -48,6 +42,12 @@ WITH avg_weekly_posts_cte AS (
 ), max_posts_in_7_days_cte AS (
     SELECT user_id, MAX(sevenday_posts) AS max_7day_posts
     FROM posts_in_7_days_cte
+    GROUP BY 1
+    ORDER BY NULL
+), avg_weekly_posts_cte AS (
+    SELECT user_id, COUNT(*) / 4.0 AS avg_weekly_posts
+    FROM Posts
+    WHERE post_date BETWEEN '2024-02-01' AND '2024-02-28'
     GROUP BY 1
     ORDER BY NULL
 )
