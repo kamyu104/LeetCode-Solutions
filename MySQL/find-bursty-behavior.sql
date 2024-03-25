176be0f1a6da6cad33b077fb4546fe3e705fb7fa
kamyu <kamyu104@users.noreply.github.com>
2024-03-25 23:11:31 +0800

    Update find-bursty-behavior.sql

--- MySQL/find-bursty-behavior.sql
+++ MySQL/find-bursty-behavior.sql
@@ -15,20 +15,15 @@ WITH avg_weekly_posts_cte AS (
                  ORDER BY post_date
                  RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW)
     ORDER BY NULL
-), posts_in_7_days_aggr_cte AS (
-    SELECT user_id, MAX(sevenday_posts) AS sevenday_posts
-    FROM posts_in_7_days_cte
-    GROUP BY user_id, post_date
-    ORDER BY NULL
-), max_posts_in_7_days_aggr_cte AS (
+), max_posts_in_7_days_cte AS (
     SELECT user_id, MAX(sevenday_posts) AS max_7day_posts
-    FROM posts_in_7_days_aggr_cte
+    FROM posts_in_7_days_cte
     GROUP BY 1
     ORDER BY NULL
 )
 
 SELECT user_id, max_7day_posts, avg_weekly_posts
-FROM max_posts_in_7_days_aggr_cte
+FROM max_posts_in_7_days_cte
 INNER JOIN avg_weekly_posts_cte USING (user_id)
 WHERE max_7day_posts >= 2 * avg_weekly_posts
 ORDER BY 1;
@@ -50,20 +45,15 @@ WITH avg_weekly_posts_cte AS (
                  ORDER BY post_date
                  RANGE BETWEEN CURRENT ROW AND INTERVAL 6 DAY FOLLOWING)
     ORDER BY NULL
-), posts_in_7_days_aggr_cte AS (
-    SELECT user_id, MAX(sevenday_posts) AS sevenday_posts
-    FROM posts_in_7_days_cte
-    GROUP BY user_id, post_date
-    ORDER BY NULL
-), max_posts_in_7_days_aggr_cte AS (
+), max_posts_in_7_days_cte AS (
     SELECT user_id, MAX(sevenday_posts) AS max_7day_posts
-    FROM posts_in_7_days_aggr_cte
+    FROM posts_in_7_days_cte
     GROUP BY 1
     ORDER BY NULL
 )
 
 SELECT user_id, max_7day_posts, avg_weekly_posts
-FROM max_posts_in_7_days_aggr_cte
+FROM max_posts_in_7_days_cte
 INNER JOIN avg_weekly_posts_cte USING (user_id)
 WHERE max_7day_posts >= 2 * avg_weekly_posts
 ORDER BY 1;
