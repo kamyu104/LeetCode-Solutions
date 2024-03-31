a0e7b3dfef2a128a1c0930438d9ebd09f0b7b0d8
kamyu <kamyu104@users.noreply.github.com>
2024-03-31 20:54:03 +0800

    Update guess-the-number-using-bitwise-questions-ii.cpp

--- C++/guess-the-number-using-bitwise-questions-ii.cpp
+++ C++/guess-the-number-using-bitwise-questions-ii.cpp
@@ -8,6 +8,26 @@ public:
         static const int BIT_COUNT = 30;
 
         int result = 0;
+        for (int i = 0, prev = commonBits(0); i < BIT_COUNT; ++i) {
+            int curr = commonBits(1 << i);
+            if (curr - prev == 1) {
+                result |= 1 << i;
+            }
+            prev = curr;
+        }
+        return result;
+    }
+};
+
+// Time:  O(logr)
+// Space: O(1)
+// bit manipulation
+class Solution2 {
+public:
+    int findNumber() {
+        static const int BIT_COUNT = 30;
+
+        int result = 0;
         for (int i = 0; i < BIT_COUNT; ++i) {
             if (commonBits(1 << i) - commonBits(1 << i) == 1) {
                 result |= 1 << i;
