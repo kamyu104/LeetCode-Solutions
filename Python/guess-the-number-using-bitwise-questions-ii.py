5f99398a50ffb82904a0ee98ceb6b0726d8b4ca0
kamyu <kamyu104@users.noreply.github.com>
2024-03-31 20:52:53 +0800

    Update guess-the-number-using-bitwise-questions-ii.py

--- Python/guess-the-number-using-bitwise-questions-ii.py
+++ Python/guess-the-number-using-bitwise-questions-ii.py
@@ -8,4 +8,23 @@ class Solution(object):
         :rtype: int
         """
         BIT_COUNT = 30
+        result = 0
+        prev = commonBits(0)
+        for i in xrange(BIT_COUNT):
+            curr = commonBits(1<<i)
+            if curr-prev == 1:
+                result |= 1<<i
+            prev = curr
+        return result
+
+
+# Time:  O(logr)
+# Space: O(1)
+# bit manipulation
+class Solution2(object):
+    def findNumber(self):
+        """
+        :rtype: int
+        """
+        BIT_COUNT = 30
         return reduce(lambda accu, i: accu|(1<<i if commonBits(1<<i)-commonBits(1<<i) == 1 else 0), xrange(BIT_COUNT), 0)
