e5ce05f45fc875858c77de52cfd1dcb9f5b5059c
kamyu <kamyu104@users.noreply.github.com>
2024-04-07 23:00:38 +0800

    Create lexicographically-smallest-string-after-operations-with-constraint.py

---
+++ Python/lexicographically-smallest-string-after-operations-with-constraint.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(1)
+
+# greedy
+class Solution(object):
+    def getSmallestString(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        result = map(lambda x: ord(x)-ord('a'), s)
+        for i in xrange(len(result)):
+            d = min(result[i], -result[i]%26)
+            result[i] = 0 if d <= k else result[i]-k
+            k -= min(d, k)
+            if k == 0:
+                break
+        return "".join(map(lambda x: chr(x+ord('a')), result))
