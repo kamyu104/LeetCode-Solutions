b22656881b73c88d15fed5332fbf4abe7a705923
kamyu <kamyu104@users.noreply.github.com>
2024-04-08 02:11:53 +0800

    Update find-longest-self-contained-substring.py

--- Python/find-longest-self-contained-substring.py
+++ Python/find-longest-self-contained-substring.py
@@ -113,3 +113,34 @@ class Solution3(object):
                 if valid == l:
                     result = max(result, right-left+1)
         return result
+
+
+# Time:  O(26^2 * n)
+# Space: O(26)
+# hash table, brute force
+class Solution5(object):
+    def maxSubstringLength(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        def check(l, r):
+            return all(l <= left[ord(s[i])-ord('a')] and right[ord(s[i])-ord('a')] <= r for i in xrange(l, r+1))
+
+        left = [-1]*26
+        right = [-1]*26
+        for i, x in enumerate(s):
+            x = ord(x)-ord('a')
+            if left[x] == -1:
+                left[x] = i
+            right[x] = i
+        result = -1
+        for l in left:
+            if l == -1:
+                continue
+            for r in right:
+                if r == -1:
+                    continue
+                if l <= r and result < r-l+1 != len(s) and check(l, r):
+                    result = r-l+1
+        return result
