cc655905b37453a619dc08a89f6822456ee21ce1
kamyu <kamyu104@users.noreply.github.com>
2024-04-15 00:53:17 +0800

    Create find-the-number-of-subarrays-where-boundary-elements-are-maximum.py

---
+++ Python/find-the-number-of-subarrays-where-boundary-elements-are-maximum.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(n)
+
+# mono stack
+class Solution(object):
+    def numberOfSubarrays(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 0
+        stk = []
+        for x in nums:
+            while stk and stk[-1][0] < x:
+                stk.pop()
+            if not stk or stk[-1][0] != x:
+                stk.append([x, 0])
+            stk[-1][1] += 1
+            result += stk[-1][1]
+        return result
