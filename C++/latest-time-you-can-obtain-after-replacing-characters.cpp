3d21ec6463b0a91424517650d51e9deeacf83341
kamyu <kamyu104@users.noreply.github.com>
2024-04-15 01:11:06 +0800

    Create latest-time-you-can-obtain-after-replacing-characters.cpp

---
+++ C++/latest-time-you-can-obtain-after-replacing-characters.cpp
@@ -0,0 +1,22 @@
+// Time:  O(1)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    string findLatestTime(string s) {
+        if (s[0] == '?') {
+            s[0] = s[1] == '?' || s[1] <= '1' ? '1' : '0';
+        }
+        if (s[1] == '?') {
+            s[1] = s[0] == '1' ? '1' : '9';
+        }
+        if (s[3] == '?') {
+            s[3] = '5';
+        }
+        if (s[4] == '?') {
+            s[4] = '9';
+        }
+        return s;
+    }
+};
