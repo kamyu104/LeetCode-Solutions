add1d8f558dec158659557e2a650dff557f5c721
kamyu <kamyu104@users.noreply.github.com>
2024-04-15 00:52:28 +0800

    Create find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp

---
+++ C++/find-the-number-of-subarrays-where-boundary-elements-are-maximum.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(n)
+
+// mono stack
+class Solution {
+public:
+    long long numberOfSubarrays(vector<int>& nums) {
+        int64_t result = 0;
+        vector<pair<int, int>> stk;
+        for (const auto& x : nums) {
+            while (!empty(stk) && stk.back().first < x) {
+                stk.pop_back();
+            }
+            if (empty(stk) || stk.back().first != x) {
+                stk.emplace_back(x, 0);
+            }
+            result += ++stk.back().second;
+        }
+        return result;
+    }
+};
