464b38befce9ef7aaa3112f5dc9394b8e0202148
kamyu <kamyu104@users.noreply.github.com>
2024-04-15 18:36:23 +0800

    Update friday-purchases-i.sql

--- MySQL/friday-purchases-i.sql
+++ MySQL/friday-purchases-i.sql
@@ -2,7 +2,7 @@
 # Space: O(n)
 
 SELECT WEEK(purchase_date) - WEEK('2023-11-01') + 1 AS week_of_month,
-	     purchase_date,
+       purchase_date,
        SUM(amount_spend) AS total_amount
 FROM Purchases
 WHERE DAYOFWEEK(purchase_date) = 6
