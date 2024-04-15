b7c0d9836384baacedf98b20a304be11e70bfc5d
kamyu <kamyu104@users.noreply.github.com>
2024-04-15 18:36:57 +0800

    Create friday-purchase-iii.sql

---
+++ MySQL/friday-purchase-iii.sql
@@ -0,0 +1,30 @@
+# Time:  O(n)
+# Space: O(n)
+
+WITH weeks_cte AS (
+   SELECT 1 AS week_of_month
+   UNION ALL
+   SELECT 2
+   UNION ALL
+   SELECT 3
+   UNION ALL
+   SELECT 4
+),
+memberships_cte AS (
+   SELECT 'Premium' AS membership
+   UNION ALL
+   SELECT 'VIP'
+), friday_purchases_cte AS (
+    SELECT WEEK(purchase_date) - WEEK('2023-11-01') + 1 AS week_of_month,
+           membership,
+           SUM(amount_spend) AS total_amount
+    FROM Purchases INNER JOIN Users USING(user_id)
+    WHERE DAYOFWEEK(purchase_date) = 6
+    GROUP BY 1, 2
+    ORDER BY NULL
+)
+
+SELECT week_of_month, membership, IFNULL(total_amount, 0) as total_amount
+FROM weeks_cte CROSS JOIN memberships_cte
+LEFT JOIN friday_purchases_cte USING(week_of_month, membership)
+ORDER BY 1, 2;
