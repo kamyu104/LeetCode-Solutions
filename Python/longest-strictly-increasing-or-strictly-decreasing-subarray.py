9ccd3e548ac03679b9d85195fef068420530948d
kamyu <kamyu104@users.noreply.github.com>
2024-04-18 10:41:16 +0800

    Update longest-strictly-increasing-or-strictly-decreasing-subarray.py

--- Python/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ Python/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -8,6 +8,29 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        result = cnt1 = cnt2 = 1
+        for i in xrange(1, len(nums)):
+            if nums[i-1] < nums[i]:
+                cnt1 += 1
+                cnt2 = 1
+            elif nums[i-1] > nums[i]:
+                cnt2 += 1
+                cnt1 = 1
+            else:
+                cnt1 = cnt2 = 1
+            result = max(result, cnt1, cnt2)
+        return result
+
+
+# Time:  O(n)
+# Space: O(1)
+# array
+class Solution2(object):
+    def longestMonotonicSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         def f(compare):
             result = l = 0
             for i in xrange(len(nums)):
