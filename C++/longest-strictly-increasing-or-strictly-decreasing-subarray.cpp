3f0f53f3bf3a8c0bab1c7169e998db40ebea9ff3
kamyu <kamyu104@users.noreply.github.com>
2024-04-18 10:39:42 +0800

    Update longest-strictly-increasing-or-strictly-decreasing-subarray.cpp

--- C++/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ C++/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -5,6 +5,29 @@
 class Solution {
 public:
     int longestMonotonicSubarray(vector<int>& nums) {
+        int result = 1, cnt1 = 1, cnt2 = 1;
+        for (int i = 1; i < size(nums); ++i) {
+            if (nums[i - 1] < nums[i]) {
+                ++cnt1;
+                cnt2 = 1;
+            } else if (nums[i - 1] > nums[i]) {
+                ++cnt2;
+                cnt1 = 1;
+            } else {
+                cnt1 = cnt2 = 1;
+            }
+            result = max({result, cnt1, cnt2});
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+// array
+class Solution2 {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
         const auto& f = [&](const auto& compare) {
             int result = 0;
             for (int i = 0, l = 0; i < size(nums); ++i) {
