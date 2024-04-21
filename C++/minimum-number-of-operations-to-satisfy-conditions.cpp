243c78e92a48ad501bba31b4e45d337c54c9566e
kamyu <kamyu104@users.noreply.github.com>
2024-04-22 04:51:33 +0800

    Update minimum-number-of-operations-to-satisfy-conditions.cpp

--- C++/minimum-number-of-operations-to-satisfy-conditions.cpp
+++ C++/minimum-number-of-operations-to-satisfy-conditions.cpp
@@ -1,8 +1,48 @@
-// Time:  O(n * (m + 100))
+// Time:  O(n * (m + 10))
 // Space: O(10)
 
 // dp
 class Solution {
+public:
+    int minimumOperations(vector<vector<int>>& grid) {
+        static const int INF = numeric_limits<int>::max();
+        static const int MAX_VALUE = 9;
+
+        const auto& top = [](const auto& dp, int exclude) {
+            int j = -1;
+            for (int i = 0; i <= MAX_VALUE; ++i) {
+                if (i == exclude) {
+                    continue;
+                }
+                if (j == -1 || dp[i] < dp[j]) {
+                    j = i;
+                }
+            }
+            return j;
+        };
+    
+        vector<int> dp(MAX_VALUE + 1);
+        for (int j = 0; j < size(grid[0]); ++j) {
+            vector<int> cnt(MAX_VALUE + 1);
+            for (int i = 0; i < size(grid); ++i) {
+                ++cnt[grid[i][j]];
+            }
+            vector<int> new_dp(MAX_VALUE + 1, INF);
+            for (int i = 0; i <= MAX_VALUE; ++i) {
+                const int top1 = top(dp, -1);
+                const int top2 = top(dp, top1);
+                new_dp[i] = min(new_dp[i], (i != top1 ? dp[top1] : dp[top2]) + (static_cast<int>(size(grid)) - cnt[i]));
+            }
+            dp = move(new_dp);
+        }
+        return ranges::min(dp);
+    }
+};
+
+// Time:  O(n * (m + 100))
+// Space: O(10)
+// dp
+class Solution2 {
 public:
     int minimumOperations(vector<vector<int>>& grid) {
         static const int INF = numeric_limits<int>::max();
