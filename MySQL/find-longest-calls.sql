43fad39398827800bdfcee4739e7de2822d1fdf8
kamyu <kamyu104@users.noreply.github.com>
2024-04-28 19:40:20 +0800

    Create find-longest-calls.sql

---
+++ MySQL/find-longest-calls.sql
@@ -0,0 +1,18 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# window function
+WITH rank_cte AS (
+    SELECT contact_id AS id, type,
+    ROW_NUMBER() OVER (PARTITION BY type ORDER BY duration DESC) AS row_num,
+    TIME_FORMAT(SEC_TO_TIME(duration), '%H:%i:%s') AS duration_formatted
+    FROM Calls
+), top3_cte AS (
+    SELECT *
+    FROM rank_cte
+    WHERE row_num <= 3
+)
+
+SELECT first_name, type, duration_formatted
+FROM top3_cte INNER JOIN contacts ct USING (id)
+ORDER BY 2, 3 DESC, 1 DESC;
