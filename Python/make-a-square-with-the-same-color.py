41f6d652bedc9d926f5373cbfc78b78dc40491a9
kamyu <kamyu104@users.noreply.github.com>
2024-04-28 21:53:50 +0800

    Create make-a-square-with-the-same-color.py

---
+++ Python/make-a-square-with-the-same-color.py
@@ -0,0 +1,13 @@
+# Time:  O((n - w + 1)^2 * w^2)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def canMakeSquare(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: bool
+        """
+        N, W = 3, 2
+        return any(max(collections.Counter(grid[i+h][j+w] for h in xrange(W) for w in xrange(W)).itervalues()) >= W**2-1
+                   for i in xrange(N-W+1) for j in xrange(N-W+1))
