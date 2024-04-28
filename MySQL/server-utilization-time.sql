487c7f06fe8eb6121b399e24007dbea5b62736ce
kamyu <kamyu104@users.noreply.github.com>
2024-04-28 21:05:37 +0800

    Create server-utilization-time.sql

---
+++ MySQL/server-utilization-time.sql
@@ -0,0 +1,14 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# window function
+WITH diff_cte AS (
+  SELECT TIMESTAMPDIFF(SECOND, status_time, (LEAD(status_time) OVER w)) AS diff,
+         session_status
+  FROM servers
+  WINDOW w AS (PARTITION BY server_id ORDER BY status_time, session_status)
+)
+
+SELECT FLOOR(SUM(diff) / 86400) AS total_uptime_days
+FROM diff_cte 
+WHERE session_status = 'start';
