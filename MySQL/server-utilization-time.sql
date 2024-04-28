b6cd6a5c5e9fa9513f7b03707750a0899145204f
kamyu <kamyu104@users.noreply.github.com>
2024-04-29 04:17:40 +0800

    Update server-utilization-time.sql

--- MySQL/server-utilization-time.sql
+++ MySQL/server-utilization-time.sql
@@ -6,7 +6,7 @@ WITH diff_cte AS (
   SELECT TIMESTAMPDIFF(SECOND, status_time, (LEAD(status_time) OVER w)) AS diff,
          session_status
   FROM servers
-  WINDOW w AS (PARTITION BY server_id ORDER BY status_time, session_status)
+  WINDOW w AS (PARTITION BY server_id ORDER BY status_time)
 )
 
 SELECT FLOOR(SUM(diff) / 86400) AS total_uptime_days
