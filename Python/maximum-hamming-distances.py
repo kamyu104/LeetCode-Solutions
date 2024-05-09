f0134961fd7569dec73a0c8dc0cb0294eecd967b
kamyu <kamyu104@users.noreply.github.com>
2024-05-09 09:36:13 +0800

    Update maximum-hamming-distances.py

--- Python/maximum-hamming-distances.py
+++ Python/maximum-hamming-distances.py
@@ -1,8 +1,29 @@
 # Time:  O(m * 2^m)
 # Space: O(2^m)
 
-# bitmasks, bfs
+# bitmasks, dp
 class Solution(object):
+    def maxHammingDistances(self, nums, m):
+        """
+        :type nums: List[int]
+        :type m: int
+        :rtype: List[int]
+        """
+        dp = [float("-inf")]*(1<<m)
+        for x in nums:
+            dp[x] = 0
+        for i in xrange(m):
+            new_dp = dp[:]
+            for mask in xrange(1<<m):
+                new_dp[mask] = max(new_dp[mask], dp[mask^(1<<i)]+1)
+            dp = new_dp
+        return [dp[x] for x in nums]
+
+
+# Time:  O(m * 2^m)
+# Space: O(2^m)
+# bitmasks, bfs
+class Solution2(object):
     def maxHammingDistances(self, nums, m):
         """
         :type nums: List[int]
