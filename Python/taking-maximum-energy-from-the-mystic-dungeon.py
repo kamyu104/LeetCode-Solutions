ccef633bc938c23b5aec8fed3424b378e0bd8160
kamyu <kamyu104@users.noreply.github.com>
2024-05-14 03:45:03 +0800

    Update taking-maximum-energy-from-the-mystic-dungeon.py

--- Python/taking-maximum-energy-from-the-mystic-dungeon.py
+++ Python/taking-maximum-energy-from-the-mystic-dungeon.py
@@ -12,7 +12,7 @@ class Solution(object):
         result = float("-inf")
         for i in xrange(k):
             curr = 0
-            for j in xrange(len(energy)-1-i, -1, -k):
+            for j in reversed(xrange(((len(energy)-i)-1)%k, len(energy)-i, k)):  # xrange(len(energy)-1-i, -1, -k)
                 curr += energy[j]
                 result = max(result, curr)
         return result
