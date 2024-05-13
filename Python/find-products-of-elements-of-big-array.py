c7253683a538145e5931eb5074795614abf22d31
kamyu <kamyu104@users.noreply.github.com>
2024-05-14 06:50:05 +0800

    Update find-products-of-elements-of-big-array.py

--- Python/find-products-of-elements-of-big-array.py
+++ Python/find-products-of-elements-of-big-array.py
@@ -19,29 +19,32 @@ class Solution(object):
     
         def f(x):
             def count1(x):
-                result = 0
-                for i in xrange(x.bit_length()):
+                result = i = 0
+                while 1<<i <= x:
                     mask = (1<<(i+1))-1
                     result += ((x&~mask)>>1)+max((x&mask)-(1<<i)+1, 0)
+                    i += 1
                 return result
  
             def count2(x):
-                result = 0
-                for i in xrange(x.bit_length()):
+                result = i = 0
+                while 1<<i <= x:
                     mask = (1<<(i+1))-1
                     result += (((x&~mask)>>1)+max((x&mask)-(1<<i)+1, 0))*i
+                    i += 1
                 return result
             
             y = binary_search(1, x-1, lambda i: count1(i) >= x)
             result = count2(y-1)
             x -= count1(y-1)
-            for i in xrange(y.bit_length()):
-                if y&(1<<i) == 0:
-                    continue
-                result += i
-                x -= 1
-                if x == 0:
-                    break
+            i = 0
+            while 1<<i <= y:
+                if y&(1<<i):
+                    result += i
+                    x -= 1
+                    if x == 0:
+                        break
+                i += 1
             return result
         
         return [pow(2, f(right+1)-f(left), mod) for left, right, mod in queries]
