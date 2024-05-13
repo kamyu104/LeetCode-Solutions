088a9c8e13c7d34839c9b52f3552663b6a7d3461
kamyu <kamyu104@users.noreply.github.com>
2024-05-14 03:22:11 +0800

    Update taking-maximum-energy-from-the-mystic-dungeon.py

--- Python/taking-maximum-energy-from-the-mystic-dungeon.py
+++ Python/taking-maximum-energy-from-the-mystic-dungeon.py
@@ -1,8 +1,26 @@
 # Time:  O(n)
 # Space: O(k)
 
-# greedy, kadane's algorithm
+# dp
 class Solution(object):
+    def maximumEnergy(self, energy, k):
+        """
+        :type energy: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = float("-inf")
+        dp = [0]*k
+        for i in reversed(xrange(len(energy))):
+            dp[i%k] += energy[i]
+            result = max(result, dp[i%k])
+        return result
+
+
+# Time:  O(n)
+# Space: O(k)
+# greedy, kadane's algorithm
+class Solution2(object):
     def maximumEnergy(self, energy, k):
         """
         :type energy: List[int]
