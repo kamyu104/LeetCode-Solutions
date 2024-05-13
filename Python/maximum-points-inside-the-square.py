3594fec7581c328279d98ab570ac17d6c44144d7
kamyu <kamyu104@users.noreply.github.com>
2024-05-13 22:00:09 +0800

    Create maximum-points-inside-the-square.py

---
+++ Python/maximum-points-inside-the-square.py
@@ -0,0 +1,24 @@
+# Time:  O(26 + n)
+# Space: O(26)
+
+import itertools
+
+
+# hash table
+class Solution(object):
+    def maxPointsInsideSquare(self, points, s):
+        """
+        :type points: List[List[int]]
+        :type s: str
+        :rtype: int
+        """
+        INF = float("inf")
+        lookup = [INF for _ in xrange(26)]
+        d = INF
+        for c, (x, y) in itertools.izip(s, points):
+            k = ord(c)-ord('a')
+            mn2 = max(abs(x), abs(y))
+            if mn2 < lookup[k]:
+                mn2, lookup[k] = lookup[k], mn2
+            d = min(d, mn2)
+        return sum(mn1 < d for mn1 in lookup)
