b78a4649373080e82e4821e72ddc379c88476df5
kamyu <kamyu104@users.noreply.github.com>
2024-05-15 15:25:41 +0800

    Update maximum-hamming-distances.cpp

--- C++/maximum-hamming-distances.cpp
+++ C++/maximum-hamming-distances.cpp
@@ -32,35 +32,32 @@ public:
 class Solution2 {
 public:
     vector<int> maxHammingDistances(vector<int>& nums, int m) {
-        vector<vector<int>> group(1 << m);
-        for (int i = 0; i < size(nums); ++i) {
-            group[((1 << m) - 1) ^ nums[i]].emplace_back(i);
-        }
-        vector<int> result(size(nums));
         vector<int> q;
-        vector<int> lookup(1 << m);
+        vector<int> dist(1 << m, -1);
         for (const auto& x : nums) {
-            if (lookup[x]) {
+            if (dist[x] != -1) {
                 continue;
             }
-            lookup[x] = true;
+            dist[x] = 0;
             q.emplace_back(x);
         }
-        for (int d = 0; !empty(q); ++d) {
+        for (int d = 0; !empty(q);) {
+            ++d;
             vector<int> new_q;
             for (const auto& u : q) {
-                for (const auto& i : group[u]) {
-                    result[i] = m - d;
-                }
                 for (int i = 0; i < m; ++i) {
-                    if (lookup[u ^ (1 << i)]) {
+                    if (dist[u ^ (1 << i)] != -1) {
                         continue;
                     }
-                    lookup[u ^ (1 << i)] = true;
+                    dist[u ^ (1 << i)] = d;
                     new_q.emplace_back(u ^ (1 << i));
                 }
             }
             q = move(new_q);
+        }
+        vector<int> result(size(nums));
+        for (int i = 0; i < size(nums); ++i) {
+            result[i] = m - dist[((1 << m) - 1) ^ nums[i]];
         }
         return result;
     }
