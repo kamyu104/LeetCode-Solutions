af679daf397e92cb03fc0ade5e4839c8d814a652
kamyu <kamyu104@users.noreply.github.com>
2024-05-19 15:18:15 +0800

    Create sum-of-digit-differences-of-all-pairs.py

---
+++ Python/sum-of-digit-differences-of-all-pairs.py
@@ -0,0 +1,20 @@
+# Time:  O(nlogr)
+# Space: O(10 * n)
+
+# prefix sum
+class Solution(object):
+    def sumDigitDifferences(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        base, l = 1, 0
+        while base <= nums[0]:
+            base *= 10
+            l += 1
+        cnts = [[0]*10 for _ in xrange(l)]
+        for x in nums:
+            for i in xrange(l):
+                cnts[i][x%10] += 1
+                x //= 10
+        return sum(c*(len(nums)-c) for cnt in cnts for c in cnt)//2
