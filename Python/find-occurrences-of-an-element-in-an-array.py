78cd2783b6a0a64ca4a3c6dd4fa2fa7a661fc315
kamyu <kamyu104@users.noreply.github.com>
2024-05-27 12:30:06 +0800

    Create find-occurrences-of-an-element-in-an-array.py

---
+++ Python/find-occurrences-of-an-element-in-an-array.py
@@ -0,0 +1,14 @@
+# Time:  O(n)
+# Space: O(n)
+
+# array
+class Solution(object):
+    def occurrencesOfElement(self, nums, queries, x):
+        """
+        :type nums: List[int]
+        :type queries: List[int]
+        :type x: int
+        :rtype: List[int]
+        """
+        lookup = [i for i, y in enumerate(nums) if y == x]
+        return [lookup[q-1] if q-1 < len(lookup) else -1 for q in queries]
