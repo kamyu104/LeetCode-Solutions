3e4de97bd0af21ba0a0129c6d09d82a4ae8f6879
kamyu <kamyu104@users.noreply.github.com>
2024-05-27 12:29:35 +0800

    Create find-occurrences-of-an-element-in-an-array.cpp

---
+++ C++/find-occurrences-of-an-element-in-an-array.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(n)
+
+// array
+class Solution {
+public:
+    vector<int> occurrencesOfElement(vector<int>& nums, vector<int>& queries, int x) {
+        vector<int> lookup;
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] != x) {
+                continue;
+            }
+            lookup.emplace_back(i);
+        }
+        vector<int> result(size(queries));
+        for (int i = 0; i < size(queries); ++i) {
+            result[i] = queries[i] - 1 < size(lookup) ? lookup[queries[i] - 1] : -1;
+        }
+        return result;
+    }
+};
