0ddf16e15fe125b0106e88e3115d2ff214bc81ed
kamyu <kamyu104@users.noreply.github.com>
2024-05-27 14:15:49 +0800

    Create find-the-number-of-distinct-colors-among-the-balls.cpp

---
+++ C++/find-the-number-of-distinct-colors-among-the-balls.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n)
+// Space: O(n)
+
+// freq table
+class Solution {
+public:
+    vector<int> queryResults(int limit, vector<vector<int>>& queries) {
+        vector<int> result(size(queries));
+        unordered_map<int, int> lookup, cnt;
+        for (int i = 0; i < size(queries); ++i) {
+            const int x = queries[i][0], y = queries[i][1];
+            if (lookup.count(x)) {
+                if (--cnt[lookup[x]] == 0) {
+                    cnt.erase(lookup[x]);
+                }
+            }
+            lookup[x] = y;
+            ++cnt[lookup[x]];
+            result[i] = size(cnt);
+        }
+        return result;
+    }
+};
