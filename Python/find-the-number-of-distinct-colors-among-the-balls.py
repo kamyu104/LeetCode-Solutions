320bda035b989c244a1cd87515e7013575809f58
kamyu <kamyu104@users.noreply.github.com>
2024-05-27 14:16:25 +0800

    Create find-the-number-of-distinct-colors-among-the-balls.py

---
+++ Python/find-the-number-of-distinct-colors-among-the-balls.py
@@ -0,0 +1,26 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# freq table
+class Solution(object):
+    def queryResults(self, limit, queries):
+        """
+        :type limit: int
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        result = [0]*len(queries)
+        lookup = {}
+        cnt = collections.Counter()
+        for i, (x, y) in enumerate(queries):
+            if x in lookup:
+                cnt[lookup[x]] -= 1
+                if not cnt[lookup[x]]:
+                    del cnt[lookup[x]]
+            lookup[x] = y
+            cnt[lookup[x]] += 1
+            result[i] = len(cnt)
+        return result
