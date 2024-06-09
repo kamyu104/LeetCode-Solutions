01c5602de8a15fa6e0d6e7a2c1c665bf41e38537
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 20:50:48 +0800

    Update maximum-total-reward-using-operations-i.py

--- Python/maximum-total-reward-using-operations-i.py
+++ Python/maximum-total-reward-using-operations-i.py
@@ -8,6 +8,24 @@ class Solution(object):
         :type rewardValues: List[int]
         :rtype: int
         """
+        mx = max(rewardValues)
+        dp = 1
+        mask = (1<<mx)-1
+        for v in sorted(set(rewardValues)):
+            x = dp&((1<<v)-1)
+            dp |= (x<<v)&mask
+        return mx+(dp.bit_length()-1)
+
+
+# Time: O(nlogn + r^2), r = max(rewardValues)
+# Space: O(r)
+# sort, dp, bitset
+class Solution2(object):
+    def maxTotalReward(self, rewardValues):
+        """
+        :type rewardValues: List[int]
+        :rtype: int
+        """
         dp = 1
         for v in sorted(set(rewardValues)):
             x = dp&((1<<v)-1)
@@ -18,7 +36,25 @@ class Solution(object):
 # Time: O(nlogn + r^2), r = max(rewardValues)
 # Space: O(r)
 # sort, dp
-class Solution2(object):
+class Solution3(object):
+    def maxTotalReward(self, rewardValues):
+        """
+        :type rewardValues: List[int]
+        :rtype: int
+        """
+        mx = max(rewardValues)
+        dp = [False]*((max(rewardValues)*2-1)+1)
+        dp[0] = True
+        for v in sorted(set(rewardValues)):
+            for x in xrange(min(v, mx-v)):
+                dp[x+v] |= dp[x]
+        return mx+next(x for x in reversed(xrange(len(dp))) if dp[x])
+
+
+# Time: O(nlogn + r^2), r = max(rewardValues)
+# Space: O(r)
+# sort, dp
+class Solution4(object):
     def maxTotalReward(self, rewardValues):
         """
         :type rewardValues: List[int]
