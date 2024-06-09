6b78761264c3afeee23c04e4fd0c1a0189cafd81
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 19:53:11 +0800

    Update find-the-child-who-has-the-ball-after-k-seconds.cpp

--- C++/find-the-child-who-has-the-ball-after-k-seconds.cpp
+++ C++/find-the-child-who-has-the-ball-after-k-seconds.cpp
@@ -9,3 +9,14 @@ public:
         return (q & 1) == 0 ? r : (n - 1) - r;
     }
 };
+
+// Time:  O(1)
+// Space: O(1)
+// math
+class Solution2 {
+public:
+    int numberOfChild(int n, int k) {
+        const int r = k % (2 * (n - 1));
+        return r <= n - 1 ? r : (n - 1) - (r - (n - 1));
+    }
+};
