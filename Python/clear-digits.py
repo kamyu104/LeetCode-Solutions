5d7335e4e86c5826342f2947f27cd579cf637ec2
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 21:40:23 +0800

    Update clear-digits.py

--- Python/clear-digits.py
+++ Python/clear-digits.py
@@ -1,8 +1,30 @@
 # Time:  O(n)
 # Space: O(1)
 
-# stack
+# two pointers
 class Solution(object):
+    def clearDigits(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        s = list(s)
+        j = 0
+        for i, x in enumerate(s):
+            if x.isdigit():
+                j -= 1
+                continue
+            s[j] = x
+            j += 1
+        while len(s) > j:
+            s.pop()
+        return "".join(s)
+
+
+# Time:  O(n)
+# Space: O(1)
+# stack
+class Solution2(object):
     def clearDigits(self, s):
         """
         :type s: str
