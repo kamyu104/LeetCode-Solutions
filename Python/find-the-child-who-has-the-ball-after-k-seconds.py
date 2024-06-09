08ad89fb6db4fd2d984d60bb0000a32e6aee54ee
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 19:52:09 +0800

    Update find-the-child-who-has-the-ball-after-k-seconds.py

--- Python/find-the-child-who-has-the-ball-after-k-seconds.py
+++ Python/find-the-child-who-has-the-ball-after-k-seconds.py
@@ -11,3 +11,17 @@ class Solution(object):
         """
         q, r = divmod(k, n-1)
         return r if q&1 == 0 else (n-1)-r
+
+
+# Time:  O(1)
+# Space: O(1)
+# math
+class Solution2(object):
+    def numberOfChild(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        r = k%(2*(n-1))
+        return r if r <= n-1 else (n-1)-(r-(n-1))
