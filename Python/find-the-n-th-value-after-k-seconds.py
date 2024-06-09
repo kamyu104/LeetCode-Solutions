a2c714a0fdf7bdea67b9368dcba918902a90c25e
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 20:06:00 +0800

    Create find-the-n-th-value-after-k-seconds.py

---
+++ Python/find-the-n-th-value-after-k-seconds.py
@@ -0,0 +1,23 @@
+# Time:  O(n + k)
+# Space: O(n + k)
+
+# combinatorics
+class Solution(object):
+    def valueAfterKSeconds(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        fact, inv, inv_fact = [[1]*2 for _ in xrange(3)]
+        def nCr(n, k):
+            if not (0 <= k <= n):
+                return 0
+            while len(inv) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(inv) % MOD)
+                inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+                inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
+            return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
+
+        return nCr(n+k-1, k)
