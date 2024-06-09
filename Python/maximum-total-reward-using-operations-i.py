b6c2310c6a69350a6821caa7ef0662248d26049b
kamyu <kamyu104@users.noreply.github.com>
2024-06-09 20:39:27 +0800

    Create maximum-total-reward-using-operations-i.py

---
+++ Python/maximum-total-reward-using-operations-i.py
@@ -0,0 +1,32 @@
+# Time: O(nlogn + r^2), r = max(rewardValues)
+# Space: O(r)
+
+# sort, dp, bitset
+class Solution(object):
+    def maxTotalReward(self, rewardValues):
+        """
+        :type rewardValues: List[int]
+        :rtype: int
+        """
+        dp = 1
+        for v in sorted(set(rewardValues)):
+            x = dp&((1<<v)-1)
+            dp |= x<<v
+        return dp.bit_length()-1
+
+
+# Time: O(nlogn + r^2), r = max(rewardValues)
+# Space: O(r)
+# sort, dp
+class Solution2(object):
+    def maxTotalReward(self, rewardValues):
+        """
+        :type rewardValues: List[int]
+        :rtype: int
+        """
+        dp = [False]*((max(rewardValues)*2-1)+1)
+        dp[0] = True
+        for v in sorted(set(rewardValues)):
+            for x in xrange(v):
+                dp[x+v] |= dp[x]
+        return next(x for x in reversed(xrange(len(dp))) if dp[x])
