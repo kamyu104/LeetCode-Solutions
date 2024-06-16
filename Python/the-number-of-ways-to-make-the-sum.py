234882041496cf2ce33aecc5d8e7dda69b7ca46f
kamyu <kamyu104@users.noreply.github.com>
2024-06-17 02:40:44 +0800

    Update the-number-of-ways-to-make-the-sum.py

--- Python/the-number-of-ways-to-make-the-sum.py
+++ Python/the-number-of-ways-to-make-the-sum.py
@@ -24,7 +24,9 @@ class Solution2(object):
         """
         MOD = 10**9+7
         def count_1_2_6(n):
-            return ((n//2+1)*((n//6)+1)-3*((n//6)+0)*((n//6)-0+1)//2)%MOD
+            # sum((n-6*i)//2+1 for i in xrange((n//6)+1)
+            # = sum(((n//2)+1)-3*i for i in xrange((n//6)+1)
+            return ((n//2+1)*((n//6)-0+1)-3*((n//6)+0)*((n//6)-0+1)//2)%MOD
 
         return reduce(lambda x, y: (x+count_1_2_6(n-4*y))%MOD, (i for i in xrange(min(n//4, 2)+1)), 0)
 
