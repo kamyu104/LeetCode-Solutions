42344c42d61817bd91b4f89ca69c063e56d72cf9
kamyu <kamyu104@users.noreply.github.com>
2024-06-17 02:57:54 +0800

    Update the-number-of-ways-to-make-the-sum.py

--- Python/the-number-of-ways-to-make-the-sum.py
+++ Python/the-number-of-ways-to-make-the-sum.py
@@ -26,7 +26,7 @@ class Solution2(object):
         def count_1_2_6(n):
             # sum((n-6*i)//2+1 for i in xrange((n//6)+1))
             # = sum(((n//2)+1)-3*i for i in xrange((n//6)+1))
-            return ((n//2+1)*((n//6)-0+1)-3*((n//6)+0)*((n//6)-0+1)//2)%MOD
+            return (n//2+1)*((n//6)-0+1)-3*((n//6)+0)*((n//6)-0+1)//2
 
         return reduce(lambda x, y: (x+count_1_2_6(n-4*y))%MOD, (i for i in xrange(min(n//4, 2)+1)), 0)
 
@@ -45,7 +45,7 @@ class Solution3(object):
             return n//2+1
     
         def count_1_2_6(n):
-            return reduce(lambda x, y: (x+count_1_2(n-6*y))%MOD, (i for i in xrange((n//6)+1)), 0)
+            return sum(count_1_2(n-6*i) for i in xrange((n//6)+1))
 
         return reduce(lambda x, y: (x+count_1_2_6(n-4*y))%MOD, (i for i in xrange(min(n//4, 2)+1)), 0)
 
@@ -64,5 +64,5 @@ class Solution4(object):
         dp[0] = 1
         for i in (1, 2, 6):
             for j in xrange(i, n+1):
-                dp[j] = (dp[j]+dp[j-i])%MOD
+                dp[j] += dp[j-i]
         return reduce(lambda x, y: (x+dp[n-4*y])%MOD, (i for i in xrange(min(n//4, 2)+1)), 0)
