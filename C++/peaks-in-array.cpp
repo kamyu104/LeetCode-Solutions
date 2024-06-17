5719125ba234d8ad83128eca8943dfab6db05120
kamyu <kamyu104@users.noreply.github.com>
2024-06-17 23:19:27 +0800

    Update peaks-in-array.cpp

--- C++/peaks-in-array.cpp
+++ C++/peaks-in-array.cpp
@@ -1,8 +1,86 @@
-// Time:  O(nlogn + qlogn)
+// Time:  O(n + qlogn)
 // Space: O(n)
 
 // bit, fenwick tree
 class Solution {
+public:
+    vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) {
+        const auto& check = [&](int i) {
+            return nums[i - 1] < nums[i] && nums[i] > nums[i + 1];
+        };
+
+        vector<int> arr(size(nums));
+        for (int i = 0; i < size(nums); ++i) {
+            if (1 <= i && i <= size(nums) - 2 && check(i)) {
+                arr[i] = 1;
+            }
+        }
+        BIT bit(arr);
+
+        const auto& update = [&](int x, int d) {
+            for (int i = max(x - 1, 1); i <= min(x + 1, static_cast<int>(size(nums)) - 2); ++i) {
+                if (check(i)) {
+                    bit.add(i, d);
+                }
+            }
+        };
+
+        vector<int> result;
+        for (const auto& q : queries) {
+            const int t = q[0], x = q[1], y = q[2];
+            if (t == 1) {
+                result.emplace_back(y - 1 >= x + 1 ? bit.query(y - 1) - bit.query((x + 1) - 1) : 0);
+                continue;
+            }
+            update(x, -1);
+            nums[x] = y;
+            update(x, +1);
+        }
+        return result;
+    }
+
+private:
+    class BIT {
+    public:
+        BIT(const vector<int>& nums) : bit_(size(nums) + 1) {  // 0-indexed
+            for (int i = 1; i < bit_.size(); ++i) {
+                bit_[i] = nums[i - 1] + bit_[i - 1];
+            }
+            for (int i = bit_.size() - 1; i >= 1; --i) {
+                int last_i = i - lower_bit(i);
+                bit_[i] -= bit_[last_i];
+            }
+        }
+        
+        void add(int i, int val) {
+            ++i;
+            for (; i < size(bit_); i += lower_bit(i)) {
+                bit_[i] += val;
+            }
+        }
+
+        int query(int i) const {
+            ++i;
+            int total = 0;
+            for (; i > 0; i -= lower_bit(i)) {
+                total += bit_[i];
+            }
+            return total;
+        }
+    
+    private:
+        int lower_bit(int i) const {
+            return i & -i;
+        }
+        
+        vector<int> bit_;
+    };
+};
+
+// Time:  O(nlogn + qlogn)
+// Space: O(n)
+// bit, fenwick tree
+class Solution2 {
 public:
     vector<int> countOfPeaks(vector<int>& nums, vector<vector<int>>& queries) {
         const auto& check = [&](int i) {
