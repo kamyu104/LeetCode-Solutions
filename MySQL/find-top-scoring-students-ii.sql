eb259dbbb5c5808af768f661973ef3116204ec23
kamyu <kamyu104@users.noreply.github.com>
2024-06-19 21:18:35 +0800

    Update find-top-scoring-students-ii.sql

--- MySQL/find-top-scoring-students-ii.sql
+++ MySQL/find-top-scoring-students-ii.sql
@@ -18,14 +18,19 @@ WITH best_enrollments_cte AS (
     WHERE mandatory = 'yes'
     GROUP BY 1
     ORDER BY NULL
-), major_elective_grade_a_or_b_count_cte AS (
+), valid_mandatory_cte AS (
+    SELECT c1.student_id
+    FROM major_mandatory_grade_a_count_cte c1 INNER JOIN major_mandatory_count_cte c2 ON c1.major = c2.major
+    WHERE c1.cnt = c2.cnt
+    ORDER BY NULL
+), valid_elective_cte AS (
     SELECT s.student_id, s.major, COUNT(*) AS cnt
     FROM best_enrollments_cte e INNER JOIN students s ON e.student_id = s.student_id INNER JOIN courses c ON e.course_id = c.course_id
     WHERE s.major = c.major AND c.mandatory = 'no' and e.grade <= 'B'
     GROUP BY 1
     HAVING cnt >= 2
     ORDER BY NULL
-), gpa_cte AS (
+), valid_gpa_cte AS (
     SELECT e.student_id, AVG(e.gpa) AS gpa
     FROM enrollments e
     GROUP BY 1
@@ -34,9 +39,7 @@ WITH best_enrollments_cte AS (
 )
 
 SELECT c1.student_id
-FROM major_mandatory_grade_a_count_cte c1
-     INNER JOIN major_mandatory_count_cte c2 ON c1.major = c2.major
-     INNER JOIN major_elective_grade_a_or_b_count_cte c3 ON c1.student_id = c3.student_id
-     INNER JOIN gpa_cte c4 ON c1.student_id = c4.student_id
-WHERE c1.cnt = c2.cnt
+FROM valid_mandatory_cte c1
+     INNER JOIN valid_elective_cte c2 ON c1.student_id = c2.student_id
+     INNER JOIN valid_gpa_cte c3 ON c1.student_id = c3.student_id
 ORDER BY 1;
