38acae34089ca04efeaca00a221b93551bc381c6
kamyu <kamyu104@users.noreply.github.com>
2024-06-19 19:35:26 +0800

    Create find-top-scoring-students-ii.sql

---
+++ MySQL/find-top-scoring-students-ii.sql
@@ -0,0 +1,40 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+WITH best_enrollments_cte AS (
+    SELECT student_id, course_id, MIN(grade) AS grade
+    FROM enrollments
+    GROUP BY 1, 2
+    ORDER BY NULL
+), major_mandatory_grade_a_count_cte AS (
+    SELECT s.student_id, s.major, COUNT(*) AS cnt
+    FROM best_enrollments_cte e INNER JOIN students s ON e.student_id = s.student_id INNER JOIN courses c ON e.course_id = c.course_id
+    WHERE mandatory = 'yes' AND s.major = c.major AND e.grade = 'A'
+    GROUP BY 1
+    ORDER BY NULL
+), major_mandatory_count_cte AS (
+    SELECT major, COUNT(*) AS cnt
+    FROM courses
+    WHERE mandatory = 'yes'
+    GROUP BY 1
+    ORDER BY NULL
+), major_elective_grade_a_or_b_count_cte AS (
+    SELECT s.student_id, s.major, COUNT(*) AS cnt
+    FROM best_enrollments_cte e INNER JOIN students s ON e.student_id = s.student_id INNER JOIN courses c ON e.course_id = c.course_id
+    WHERE s.major = c.major AND c.mandatory = 'no' and e.grade <= 'B'
+    GROUP BY 1
+    ORDER BY NULL
+), gpa_cte AS (
+    SELECT e.student_id, AVG(e.gpa) AS gpa
+    FROM enrollments e
+    GROUP BY e.student_id
+    ORDER BY NULL
+)
+
+SELECT c1.student_id
+FROM major_mandatory_grade_a_count_cte c1
+INNER JOIN major_mandatory_count_cte c2 ON c1.major = c2.major
+INNER JOIN major_elective_grade_a_or_b_count_cte c3 ON c1.student_id = c3.student_id
+INNER JOIN gpa_cte c4 ON c1.student_id = c4.student_id
+WHERE c1.cnt = c2.cnt AND c3.cnt >= 2 AND c4.gpa >= 2.5
+ORDER BY 1;
