922ac9c4f33ab542f2ffb6fdba01adcb3131ed87
kamyu <kamyu104@users.noreply.github.com>
2024-06-19 21:14:23 +0800

    Update find-top-scoring-students-ii.sql

--- MySQL/find-top-scoring-students-ii.sql
+++ MySQL/find-top-scoring-students-ii.sql
@@ -23,11 +23,13 @@ WITH best_enrollments_cte AS (
     FROM best_enrollments_cte e INNER JOIN students s ON e.student_id = s.student_id INNER JOIN courses c ON e.course_id = c.course_id
     WHERE s.major = c.major AND c.mandatory = 'no' and e.grade <= 'B'
     GROUP BY 1
+    HAVING cnt >= 2
     ORDER BY NULL
 ), gpa_cte AS (
     SELECT e.student_id, AVG(e.gpa) AS gpa
     FROM enrollments e
     GROUP BY 1
+    HAVING gpa >= 2.5
     ORDER BY NULL
 )
 
@@ -36,5 +38,5 @@ FROM major_mandatory_grade_a_count_cte c1
      INNER JOIN major_mandatory_count_cte c2 ON c1.major = c2.major
      INNER JOIN major_elective_grade_a_or_b_count_cte c3 ON c1.student_id = c3.student_id
      INNER JOIN gpa_cte c4 ON c1.student_id = c4.student_id
-WHERE c1.cnt = c2.cnt AND c3.cnt >= 2 AND c4.gpa >= 2.5
+WHERE c1.cnt = c2.cnt
 ORDER BY 1;
