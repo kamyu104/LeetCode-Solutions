d0ad6fe5a841be1abf904d19ae720aa4865a8541
kamyu <kamyu104@users.noreply.github.com>
2024-06-22 14:36:38 +0800

    Update longest-strictly-increasing-or-strictly-decreasing-subarray.cpp

--- C++/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
+++ C++/longest-strictly-increasing-or-strictly-decreasing-subarray.cpp
@@ -9,14 +9,9 @@ public:
             return x < y ? -1 : (x > y ? 1 : 0);
         };
 
-        int result = 1;
-        for (int i = 1, cnt = 1, curr = 0, prev = 0; i < size(nums); ++i, prev = curr) {
-            curr = cmp(nums[i - 1], nums[i]);
-            if (curr == 0) {
-                cnt = 1;
-                continue;
-            }
-            cnt = curr == prev ? cnt + 1 : 2;
+        int result = size(nums) == 1 || cmp(nums[0], nums[1]) == 0 ? 1 : 2;
+        for (int i = 2, cnt = result; i < size(nums); ++i) {
+            cnt = cmp(nums[i - 1], nums[i]) == 0 ? 1 : cmp(nums[i - 2], nums[i - 1]) == cmp(nums[i - 1], nums[i]) ? cnt + 1 : 2;
             result = max(result, cnt);
         }
         return result;
