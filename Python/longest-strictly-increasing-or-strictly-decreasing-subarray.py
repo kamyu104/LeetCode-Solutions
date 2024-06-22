b55cc640f37e3a5a2dd3e60b5acf155dfececdbb
kamyu <kamyu104@users.noreply.github.com>
2024-06-22 14:33:55 +0800

    Update longest-strictly-increasing-or-strictly-decreasing-subarray.py

--- Python/longest-strictly-increasing-or-strictly-decreasing-subarray.py
+++ Python/longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -8,16 +8,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        result = cnt = 1
-        curr = prev = 0
-        for i in xrange(1, len(nums)):
-            curr = cmp(nums[i-1], nums[i])
-            if curr == 0:
-                cnt = 1
-            else:
-                cnt = cnt+1 if curr == prev else 2
-                result = max(result, cnt)
-            prev = curr
+        result = cnt = 1 if len(nums) == 1 or cmp(nums[0], nums[1]) == 0 else 2
+        for i in xrange(2, len(nums)):
+            cnt = 1 if cmp(nums[i-1], nums[i]) == 0 else cnt+1 if cmp(nums[i-2], nums[i-1]) == cmp(nums[i-1], nums[i]) else 2
+            result = max(result, cnt)
         return result
 
 
