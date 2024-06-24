fb6398db097b5130d8db9ecfeefc42abc05969a7
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 05:12:24 +0800

    Update k-inverse-pairs-array.cpp

--- C++/k-inverse-pairs-array.cpp
+++ C++/k-inverse-pairs-array.cpp
@@ -1,23 +1,67 @@
 // Time:  O(n * k)
 // Space: O(k)
 
+// dp, combinatorics, sliding window, two pointers
 class Solution {
 public:
     int kInversePairs(int n, int k) {
-        static const int M = 1000000007;
-        vector<vector<int>> dp(2, vector<int>(k + 1));
-        dp[0][0] = 1;
-        for (int i = 1; i <= n; ++i) {
-            dp[i % 2] = vector<int>(k + 1);
-            dp[i % 2][0] = 1;
-            for (int j = 1; j <= k; ++j) {
-                dp[i % 2][j] = (dp[i % 2][j - 1] + dp[(i - 1) % 2][j]) % M;
-                if (j - i >= 0) {
-                    dp[i % 2][j] = (dp[i % 2][j] - dp[(i - 1) % 2][j - i] + M) % M; 
+        static const int MOD = 1e9 + 7;
+
+        const auto& addmod = [&](const auto& a, const auto& b) {
+            return (a + b) % MOD;
+        };
+        const auto& submod = [&](const auto& a, const auto& b) {
+            return ((a - b) % MOD + MOD) % MOD;
+        };
+
+        vector<int> dp = {1};
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_dp(min(static_cast<int>(size(dp)) + ((i + 1) - 1), k + 1));
+            for (int j = 0; j < size(new_dp); ++j) {
+                new_dp[j] = j < size(dp) ? dp[j] : 0;
+                if (j - 1 >= 0) {
+                    new_dp[j] = addmod(new_dp[j], new_dp[j - 1]);
+                }
+                if (j - (i + 1) >= 0) {
+                    new_dp[j] = submod(new_dp[j], dp[j - (i + 1)]);
                 }
             }
+            dp = move(new_dp);
         }
-        return dp[n % 2][k];
+        return k < size(dp) ? dp[k] : 0;
     }
 };
 
+// Time:  O(n * k)
+// Space: O(k)
+// dp, combinatorics, sliding window, two pointers
+class Solution2 {
+public:
+    int kInversePairs(int n, int k) {
+        static const int MOD = 1e9 + 7;
+
+        const auto& addmod = [&](const auto& a, const auto& b) {
+            return (a + b) % MOD;
+        };
+        const auto& submod = [&](const auto& a, const auto& b) {
+            return ((a - b) % MOD + MOD) % MOD;
+        };
+
+        vector<int> dp(k + 1);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_dp(size(dp));
+            for (int j = 0; j < size(dp); ++j) {
+                new_dp[j] = dp[j];
+                if (j - 1 >= 0) {
+                    new_dp[j] = addmod(new_dp[j], new_dp[j - 1]);
+                }
+                if (j - (i + 1) >= 0) {
+                    new_dp[j] = submod(new_dp[j], dp[j - (i + 1)]);
+                }
+            }
+            dp = move(new_dp);
+        }
+        return dp[k];
+    }
+};
