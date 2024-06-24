998d0f2d7a396b672c97dd76ae5aedf3a5bb63ce
kamyu <kamyu104@users.noreply.github.com>
2024-06-24 23:57:23 +0800

    Update find-the-minimum-area-to-cover-all-ones-ii.py

--- Python/find-the-minimum-area-to-cover-all-ones-ii.py
+++ Python/find-the-minimum-area-to-cover-all-ones-ii.py
@@ -1,15 +1,175 @@
-# Time:  O((n^2 + m^2 + 4 * n * m) * n * m) = O(max(n, m)^3 * min(n, m))
+# Time:  O(max(n, m)^2 * log(max(n, m)))
 # Space: O(1)
 
-# brute force
+# prefix sum, binary search
 class Solution(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
+        def binary_search(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return left
+        
+        def binary_search_right(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if not check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return right
+
         def minimumArea(min_i, max_i, min_j, max_j):
-            min_r, max_r, min_c, max_c = len(grid), -1, len(grid[0]), -1
+            def count(x1, y1, x2, y2):
+                if not (x1 <= x2 and y1 <= y2):
+                    return 0
+                cnt = grid[x2][y2]
+                if x1-1 >= 0:
+                    cnt -= grid[x1-1][y2]
+                if y1-1 >= 0:
+                    cnt -= grid[x2][y1-1]
+                if x1-1 >= 0 and y1-1 >= 0:
+                    cnt += grid[x1-1][y1-1]
+                return cnt
+    
+            min_r = binary_search(min_i, max_i, lambda i: count(min_i, min_j, i, max_j))
+            max_r = binary_search_right(min_i, max_i, lambda i: count(i, min_j, max_i, max_j))
+            min_c = binary_search(min_j, max_j, lambda j: count(min_i, min_j, max_i, j))
+            max_c = binary_search_right(min_j, max_j, lambda j: count(min_i, j, max_i, max_j))
+            return (max_r-min_r+1)*(max_c-min_c+1)
+    
+        for i in xrange(len(grid)):
+            for j in xrange(len(grid[0])):
+                if i-1 >= 0:
+                    grid[i][j] += grid[i-1][j]
+                if j-1 >= 0:
+                    grid[i][j] += grid[i][j-1]
+                if i-1 >= 0 and j-1 >= 0:
+                    grid[i][j] -= grid[i-1][j-1]
+        result = float("inf")
+        for i in xrange(len(grid)):
+            a = minimumArea(0, i, 0, len(grid[0])-1)
+            for j in xrange(len(grid[0])):
+                b = minimumArea(i+1, len(grid)-1, 0, j)
+                c = minimumArea(i+1, len(grid)-1, j+1, len(grid[0])-1)
+                result = min(result, a+b+c)
+            for j in xrange(i+1, len(grid)):
+                b = minimumArea(i+1, j, 0, len(grid[0])-1)
+                c = minimumArea(j+1, len(grid)-1, 0, len(grid[0])-1)
+                result = min(result, a+b+c)
+            a = minimumArea(i+1, len(grid)-1, 0, len(grid[0])-1)
+            for j in xrange(len(grid[0])):
+                b = minimumArea(0, i, 0, j)
+                c = minimumArea(0, i, j+1, len(grid[0])-1)
+                result = min(result, a+b+c)
+        for j in xrange(len(grid[0])):
+            a = minimumArea(0, len(grid)-1, 0, j)
+            for i in xrange(len(grid)):
+                b = minimumArea(0, i, j+1, len(grid[0])-1)
+                c = minimumArea(i+1, len(grid)-1, j+1, len(grid[0])-1)
+                result = min(result, a+b+c)
+            for i in xrange(j+1, len(grid[0])):
+                b = minimumArea(0, len(grid)-1, j+1, i)
+                c = minimumArea(0, len(grid)-1, i+1, len(grid[0])-1)
+                result = min(result, a+b+c)
+            a = minimumArea(0, len(grid)-1, j+1, len(grid[0])-1)
+            for i in xrange(len(grid)):
+                b = minimumArea(0, i, 0, j)
+                c = minimumArea(i+1, len(grid)-1, 0, j)
+                result = min(result, a+b+c)
+        return result
+
+
+# Time:  O(max(n, m)^2 * log(max(n, m)))
+# Space: O(n * m)
+# prefix sum, binary search
+class Solution2(object):
+    def minimumSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def binary_search(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return left
+        
+        def binary_search_right(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if not check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return right
+
+        def minimumArea(min_i, max_i, min_j, max_j):
+            def count(x1, y1, x2, y2):
+                if not (x1 <= x2 and y1 <= y2):
+                    return 0
+                cnt = prefix[x2][y2]
+                if x1-1 >= 0:
+                    cnt -= prefix[x1-1][y2]
+                if y1-1 >= 0:
+                    cnt -= prefix[x2][y1-1]
+                if x1-1 >= 0 and y1-1 >= 0:
+                    cnt += prefix[x1-1][y1-1]
+                return cnt
+    
+            min_r = binary_search(min_i, max_i, lambda i: count(min_i, min_j, i, max_j))
+            max_r = binary_search_right(min_i, max_i, lambda i: count(i, min_j, max_i, max_j))
+            min_c = binary_search(min_j, max_j, lambda j: count(min_i, min_j, max_i, j))
+            max_c = binary_search_right(min_j, max_j, lambda j: count(min_i, j, max_i, max_j))
+            return (max_r-min_r+1)*(max_c-min_c+1)
+    
+        result = float("inf")
+        for _ in xrange(4):
+            prefix = [[0]*len(grid[0]) for _ in xrange(len(grid))]
+            for i in xrange(len(grid)):
+                for j in xrange(len(grid[0])):
+                    prefix[i][j] = grid[i][j]
+                    if i-1 >= 0:
+                        prefix[i][j] += prefix[i-1][j]
+                    if j-1 >= 0:
+                        prefix[i][j] += prefix[i][j-1]
+                    if i-1 >= 0 and j-1 >= 0:
+                        prefix[i][j] -= prefix[i-1][j-1]
+            for i in xrange(len(grid)):
+                a = minimumArea(0, i, 0, len(grid[0])-1)
+                for j in xrange(len(grid[0])):
+                    b = minimumArea(i+1, len(grid)-1, 0, j)
+                    c = minimumArea(i+1, len(grid)-1, j+1, len(grid[0])-1)
+                    result = min(result, a+b+c)
+                for j in xrange(i+1, len(grid)):
+                    b = minimumArea(i+1, j, 0, len(grid[0])-1)
+                    c = minimumArea(j+1, len(grid)-1, 0, len(grid[0])-1)
+                    result = min(result, a+b+c)
+            grid = zip(*grid[::-1])
+        return result
+
+
+# Time:  O((n^2 + m^2 + 4 * n * m) * n * m) = O(max(n, m)^3 * min(n, m))
+# Space: O(1)
+# brute force
+class Solution3(object):
+    def minimumSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def minimumArea(min_i, max_i, min_j, max_j):
+            min_r, max_r, min_c, max_c = max_i+1, min_i-1, max_j+1, min_j-1
             for i in xrange(min_i, max_i+1):
                 for j in xrange(min_j, max_j+1):
                     if grid[i][j] == 0:
@@ -54,14 +214,14 @@ class Solution(object):
 # Time:  O((n^2 + m^2 + 4 * n * m) * n * m) = O(max(n, m)^3 * min(n, m))
 # Space: O(n * m)
 # brute force
-class Solution2(object):
+class Solution4(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: int
         """
         def minimumArea(min_i, max_i, min_j, max_j):
-            min_r, max_r, min_c, max_c = len(grid), -1, len(grid[0]), -1
+            min_r, max_r, min_c, max_c = max_i+1, min_i-1, max_j+1, min_j-1
             for i in xrange(min_i, max_i+1):
                 for j in xrange(min_j, max_j+1):
                     if grid[i][j] == 0:
