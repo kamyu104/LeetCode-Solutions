c4e1972037ac8af4022a090a6cafd7c88dab7b1e
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 01:46:09 +0800

    Update count-the-number-of-inversions.py

--- Python/count-the-number-of-inversions.py
+++ Python/count-the-number-of-inversions.py
@@ -20,12 +20,12 @@ class Solution(object):
             if lookup[i] != -1:  # optimized
                 new_dp[lookup[i]] = reduce(lambda total, i: (total+dp[i])%MOD, xrange(max(lookup[i]-i, 0), lookup[i]+1), 0)
             else:            
-                curr = 0
                 for j in xrange(len(dp)):
-                    curr = (curr+dp[j])%MOD
+                    new_dp[j] = dp[j]
+                    if j-1 >= 0:
+                        new_dp[j] = (new_dp[j]+new_dp[j-1])%MOD
                     if j-(i+1) >= 0:
-                        curr = (curr-dp[j-(i+1)])%MOD
-                    new_dp[j] = curr
+                        new_dp[j] = (new_dp[j]-dp[j-(i+1)])%MOD
             dp = new_dp
         return dp[-1]
 
