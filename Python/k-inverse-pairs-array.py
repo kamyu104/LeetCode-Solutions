39bcd60d99a7f0496b36a6b1b1ce4e0436d58205
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 05:09:05 +0800

    Update k-inverse-pairs-array.py

--- Python/k-inverse-pairs-array.py
+++ Python/k-inverse-pairs-array.py
@@ -1,6 +1,7 @@
 # Time:  O(n * k)
 # Space: O(k)
 
+# dp, combinatorics, sliding window, two pointers
 class Solution(object):
     def kInversePairs(self, n, k):
         """
@@ -8,15 +9,41 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        M = 1000000007
-        dp = [[0]*(k+1) for _ in xrange(2)]
-        dp[0][0] = 1
-        for i in xrange(1, n+1):
-            dp[i%2] = [0]*(k+1)
-            dp[i%2][0] = 1
-            for j in xrange(1, k+1):
-                dp[i%2][j] = (dp[i%2][j-1] + dp[(i-1)%2][j]) % M
-                if j-i >= 0:
-                    dp[i%2][j] = (dp[i%2][j] - dp[(i-1)%2][j-i]) % M
-        return dp[n%2][k]
+        MOD = 10**9+7
+        dp = [1]
+        for i in xrange(n):
+            new_dp = [0]*min(len(dp)+((i+1)-1), k+1)
+            for j in xrange(len(new_dp)):
+                new_dp[j] = dp[j] if j < len(dp) else 0
+                if j-1 >= 0:
+                    new_dp[j] = (new_dp[j]+new_dp[j-1])%MOD
+                if j-(i+1) >= 0:
+                    new_dp[j] = (new_dp[j]-dp[j-(i+1)])%MOD
+            dp = new_dp
+        return dp[k] if k < len(dp) else 0
+
+
+# Time:  O(n * k)
+# Space: O(k)
+# dp, combinatorics, sliding window, two pointers
+class Solution2(object):
+    def kInversePairs(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        dp = [0]*(k+1)
+        dp[0] = 1
+        for i in xrange(n):
+            new_dp = [0]*len(dp)
+            for j in xrange(len(dp)):
+                new_dp[j] = dp[j]
+                if j-1 >= 0:
+                    new_dp[j] = (new_dp[j]+new_dp[j-1])%MOD
+                if j-(i+1) >= 0:
+                    new_dp[j] = (new_dp[j]-dp[j-(i+1)])%MOD
+            dp = new_dp
+        return dp[k]
 
