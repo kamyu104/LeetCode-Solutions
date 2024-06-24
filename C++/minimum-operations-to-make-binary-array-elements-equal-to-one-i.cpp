a939cb520d2e623ba47891c55e4b659702483762
kamyu <kamyu104@users.noreply.github.com>
2024-06-24 16:51:05 +0800

    Create minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp

---
+++ C++/minimum-operations-to-make-binary-array-elements-equal-to-one-i.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0; i + 2 < size(nums); ++i) {
+            if (nums[i]) {
+                continue;
+            }
+            nums[i] ^= 1;
+            nums[i + 1] ^= 1;
+            nums[i + 2] ^= 1;
+            ++result;
+        }
+        return nums[size(nums) - 2] == nums[size(nums) - 1] && nums[size(nums) - 1] == 1 ? result : -1;
+    }
+};
