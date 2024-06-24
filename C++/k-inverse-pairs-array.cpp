21ccf84cb74c45e758ece4120b74bda6442e530a
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 05:19:52 +0800

    Update k-inverse-pairs-array.cpp

--- C++/k-inverse-pairs-array.cpp
+++ C++/k-inverse-pairs-array.cpp
@@ -65,3 +65,35 @@ public:
         return dp[k];
     }
 };
+
+// Time:  O(n * k)
+// Space: O(k)
+// dp, combinatorics, sliding window, two pointers
+class Solution3 {
+public:
+    int kInversePairs(int n, int k) {
+        static const int MOD = 1e9 + 7;
+
+        const auto& addmod = [&](const auto& a, const auto& b) {
+            return (a + b) % MOD;
+        };
+        const auto& submod = [&](const auto& a, const auto& b) {
+            return ((a - b) % MOD + MOD) % MOD;
+        };
+
+        vector<int> dp(k + 1);
+        dp[0] = 1;
+        for (int i = 0; i < n; ++i) {
+            vector<int> new_dp(size(dp));
+            for (int j = 0, curr = 0; j < size(dp); ++j) {
+                curr = addmod(curr, dp[j]);
+                if (j - (i + 1) >= 0) {
+                    curr = submod(curr, dp[j - (i + 1)]);
+                }
+                new_dp[j] = curr;
+            }
+            dp = move(new_dp);
+        }
+        return dp[k];
+    }
+};
