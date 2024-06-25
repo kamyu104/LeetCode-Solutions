120fb6191bf5e01db439c5e9e552ec30ff8dadfb
kamyu <kamyu104@users.noreply.github.com>
2024-06-26 02:12:32 +0800

    Update find-the-minimum-area-to-cover-all-ones-ii.py

--- Python/find-the-minimum-area-to-cover-all-ones-ii.py
+++ Python/find-the-minimum-area-to-cover-all-ones-ii.py
@@ -26,52 +26,42 @@ class Solution(object):
                     dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
             return dp
         
-        def count2():
-            def horizon():
-                dp = [[0]*len(grid) for _ in xrange(len(grid))]
-                for i in xrange(len(grid)):
-                    l, r, u, d = len(grid[0]), -1, len(grid), -1
-                    for j in xrange(i, len(grid)):
-                        if right[j] != -1:
-                            l = min(l, left[j])
-                            r = max(r, right[j])
-                            u = min(u, j)
-                            d = max(d, j)
-                        dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
-                return dp
-            
-            def vertical():
-                dp = [[0]*len(grid[0]) for _ in xrange(len(grid[0]))]
-                for i in xrange(len(grid[0])):
-                    l, r, u, d = len(grid[0]), -1, len(grid), -1
-                    for j in xrange(i, len(grid[0])):
-                        if down[j] != -1:
-                            l = min(l, j)
-                            r = max(r, j)
-                            u = min(u, up[j])
-                            d = max(d, down[j])
-                        dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
-                return dp
+        def count2(is_vertical):
+            def get_n():
+                return len(grid) if not is_vertical else len(grid[0])
 
-            left = [len(grid[0]) for _ in xrange(len(grid))]
-            right = [-1 for _ in xrange(len(grid))]
-            up = [len(grid)]*len(grid[0])
-            down = [-1]*len(grid[0])
-            for i in xrange(len(grid)):
-                for j in xrange(len(grid[0])):
-                    if grid[i][j] == 0:
+            def get_m():
+                return len(grid[0]) if not is_vertical else len(grid)
+
+            def get(i, j):
+                return grid[i][j] if not is_vertical else grid[j][i]
+    
+            left = [get_m() for _ in xrange(get_n())]
+            right = [-1 for _ in xrange(get_n())]
+            for i in xrange(get_n()):
+                for j in xrange(get_m()):
+                    if get(i, j) == 0:
                         continue
                     left[i] = min(left[i], j)
                     right[i] = max(right[i], j)
-                    up[j] = min(up[j], i)
-                    down[j] = max(down[j], i)
-            return horizon(), vertical()
+            dp = [[0]*get_n() for _ in xrange(get_n())]
+            for i in xrange(len(dp)):
+                l, r, u, d = get_m(), -1, get_n(), -1
+                for j in xrange(i, len(dp[0])):
+                    if right[j] != -1:
+                        l = min(l, left[j])
+                        r = max(r, right[j])
+                        u = min(u, j)
+                        d = max(d, j)
+                    dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
+            return dp
 
         up_left = count(lambda x: x, lambda x: x)
         up_right = count(lambda x: x, reversed)
         down_left = count(reversed, lambda x: x)
         down_right = count(reversed, reversed)
-        horizon, vertical = count2()
+        horizon = count2(False)
+        vertical = count2(True)
         result = float("inf")
         for i in xrange(len(grid)-1):
             for j in xrange(len(grid[0])-1):
