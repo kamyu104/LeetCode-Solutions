cb191ea6c14c04511c358004999ca447198bd335
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 18:57:03 +0800

    Update find-the-minimum-area-to-cover-all-ones-ii.py

--- Python/find-the-minimum-area-to-cover-all-ones-ii.py
+++ Python/find-the-minimum-area-to-cover-all-ones-ii.py
@@ -1,8 +1,98 @@
+# Time:  O(max(n, m)^2)
+# Space: O(max(n, m)^2)
+
+# dp
+class Solution(object):
+    def minimumSum(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def count(dir1, dir2):
+            dp = [[0]*len(grid[0]) for _ in xrange(len(grid))]
+            up = [len(grid)]*len(grid[0])
+            down = [-1]*len(grid[0])
+            for i in dir1(xrange(len(grid))):
+                l, r, u, d = len(grid[0]), -1, len(grid), -1
+                for j in dir2(xrange(len(grid[0]))):
+                    if grid[i][j]:
+                        up[j] = min(up[j], i)
+                        down[j] = max(down[j], i)
+                    u = min(u, up[j])
+                    d = max(d, down[j])
+                    if down[j] >= 0:
+                        l = min(l, j)
+                        r = max(r, j)
+                    dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
+            return dp
+        
+        def count2():
+            def horizon():
+                dp = [[0]*len(grid) for _ in xrange(len(grid))]
+                for i in xrange(len(grid)):
+                    l, r, u, d = len(grid[0]), -1, len(grid), -1
+                    for j in xrange(i, len(grid)):
+                        if right[j] != -1:
+                            l = min(l, left[j])
+                            r = max(r, right[j])
+                            u = min(u, j)
+                            d = max(d, j)
+                        dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
+                return dp
+            
+            def vertical():
+                dp = [[0]*len(grid[0]) for _ in xrange(len(grid[0]))]
+                for i in xrange(len(grid[0])):
+                    l, r, u, d = len(grid[0]), -1, len(grid), -1
+                    for j in xrange(i, len(grid[0])):
+                        if down[j] != -1:
+                            l = min(l, j)
+                            r = max(r, j)
+                            u = min(u, up[j])
+                            d = max(d, down[j])
+                        dp[i][j] = (r-l+1)*(d-u+1) if r >= 0 else 0
+                return dp
+
+            left = [len(grid[0]) for _ in xrange(len(grid))]
+            right = [-1 for _ in xrange(len(grid))]
+            up = [len(grid)]*len(grid[0])
+            down = [-1]*len(grid[0])
+            for i in xrange(len(grid)):
+                for j in xrange(len(grid[0])):
+                    if grid[i][j] == 0:
+                        continue
+                    left[i] = min(left[i], j)
+                    right[i] = max(right[i], j)
+                    up[j] = min(up[j], i)
+                    down[j] = max(down[j], i)
+            return horizon(), vertical()
+
+        up_left = count(lambda x: x, lambda x: x)
+        up_right = count(lambda x: x, reversed)
+        down_left = count(reversed, lambda x: x)
+        down_right = count(reversed, reversed)
+        horizon, vertical = count2()
+        result = float("inf")
+        for i in xrange(len(grid)):
+            for j in xrange(i+1, len(grid)-1):
+                result = min(result, horizon[0][i]+horizon[i+1][j]+horizon[j+1][len(grid)-1])
+        for i in xrange(len(grid[0])):
+            for j in xrange(i+1, len(grid[0])-1):
+                result = min(result, vertical[0][i]+vertical[i+1][j]+vertical[j+1][len(grid[0])-1])
+        for i in xrange(len(grid)-1):
+            for j in xrange(len(grid[0])-1):
+                result = min(result,
+                             up_left[i][j]+up_right[i][j+1]+horizon[i+1][len(grid)-1],
+                             horizon[0][i]+down_left[i+1][j]+down_right[i+1][j+1],
+                             up_left[i][j]+down_left[i+1][j]+vertical[j+1][len(grid[0])-1],
+                             vertical[0][j]+up_right[i][j+1]+down_right[i+1][j+1])                    
+        return result
+
+
 # Time:  O(n * m * log(max(n, m)) + max(n, m)^2)
 # Space: O(n * m * log(max(n, m)))
-
 # sparse table
-class Solution(object):
+class Solution2(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
@@ -105,7 +195,7 @@ class Solution(object):
 # Time:  O(n * m * log(max(n, m)) + max(n, m)^2)
 # Space: O(n * m * log(max(n, m)))
 # sparse table
-class Solution2(object):
+class Solution3(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
@@ -190,7 +280,7 @@ class Solution2(object):
 # Time:  O(max(n, m)^2 * log(max(n, m)))
 # Space: O(1)
 # prefix sum, binary search
-class Solution3(object):
+class Solution4(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
@@ -278,7 +368,7 @@ class Solution3(object):
 # Time:  O(max(n, m)^2 * log(max(n, m)))
 # Space: O(n * m)
 # prefix sum, binary search
-class Solution4(object):
+class Solution5(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
@@ -350,7 +440,7 @@ class Solution4(object):
 # Time:  O((n^2 + m^2 + 4 * n * m) * n * m) = O(max(n, m)^3 * min(n, m))
 # Space: O(1)
 # brute force
-class Solution5(object):
+class Solution6(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
@@ -404,7 +494,7 @@ class Solution5(object):
 # Time:  O((n^2 + m^2 + 4 * n * m) * n * m) = O(max(n, m)^3 * min(n, m))
 # Space: O(n * m)
 # brute force
-class Solution6(object):
+class Solution7(object):
     def minimumSum(self, grid):
         """
         :type grid: List[List[int]]
