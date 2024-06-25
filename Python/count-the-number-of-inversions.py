a6bfb5087b2f498ee6274240df281127dcbb17f4
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 08:58:48 +0800

    Update count-the-number-of-inversions.py

--- Python/count-the-number-of-inversions.py
+++ Python/count-the-number-of-inversions.py
@@ -90,6 +90,33 @@ class Solution3(object):
         return dp[-1]
 
 
+# Time:  O(n^2 * k), k = max(cnt for _, cnt in requirements)
+# Space: O(n + k)
+# knapsack dp, combinatorics
+class Solution(object):
+    def numberOfPermutations(self, n, requirements):
+        """
+        :type n: int
+        :type requirements: List[List[int]]
+        :rtype: int
+        """
+        MOD = 10**9+7
+        lookup = [-1]*n
+        for i, c in requirements:
+            lookup[i] = c
+        dp = [0]*(lookup[-1]+1)
+        dp[0] = 1
+        for i in xrange(n):
+            new_dp = [0 ]*len(dp)
+            for j in xrange(len(dp)):
+                for k in xrange(min(i+1, j+1)):
+                    new_dp[j] = (new_dp[j]+dp[j-k])%MOD
+                if lookup[i] != -1 and lookup[i] != j:
+                    new_dp[j] = 0
+            dp = new_dp
+        return dp[-1]%MOD
+
+
 class Solution_ConstructPermutation(object):
     def numberOfPermutations(self, n, requirements):
         """
@@ -102,7 +129,7 @@ class Solution_ConstructPermutation(object):
         for i, c in requirements:
             lookup[i] = c
         dp = [[] for _ in xrange(lookup[-1]+1)]
-        dp[0] = [[]]
+        dp[0].append([])
         for i in xrange(n):
             new_dp = [[] for _ in xrange(len(dp))]
             for j in xrange(len(dp)):
