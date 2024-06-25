b962dddf7161431708c2a8ee408f89903bcbc8f9
kamyu <kamyu104@users.noreply.github.com>
2024-06-25 09:06:32 +0800

    Update k-inverse-pairs-array.py

--- Python/k-inverse-pairs-array.py
+++ Python/k-inverse-pairs-array.py
@@ -72,3 +72,53 @@ class Solution3(object):
             dp = new_dp
         return dp[-1]
 
+
+# Time:  O(n^2 * k), k = max(cnt for _, cnt in requirements)
+# Space: O(n + k)
+# knapsack dp, combinatorics
+class Solution_TLE(object):
+    def kInversePairs(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        dp = [0]*(k+1)
+        dp[0] = 1
+        for i in xrange(n):
+            new_dp = [0]*len(dp)
+            for j in xrange(len(dp)):
+                for k in xrange(min(i+1, j+1)):
+                    new_dp[j] = (new_dp[j]+dp[j-k])%MOD
+            dp = new_dp
+        return dp[-1]%MOD
+
+
+class Solution_ConstructPermutation(object):
+    def kInversePairs(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        dp = [[] for _ in xrange(k+1)]
+        dp[0].append([])
+        for i in xrange(n):
+            new_dp = [[] for _ in xrange(len(dp))]
+            for j in xrange(len(dp)):
+                for k in xrange(min(i+1, j+1)):
+                    for p in dp[j-k]:
+                        # p = [0, 2, 1], i = 3
+                        # k = 0, [0, 2, 1]+[3]
+                        # k = 1, [0, 3, 1]+[2]
+                        # k = 2, [0, 3, 2]+[1]
+                        # k = 3, [1, 3, 2]+[0]
+                        new_dp[j].append([x+int(x >= i-k) for x in p]+[i-k])
+            dp = new_dp
+        for p in dp[-1]:
+            assert(sum(int(p[j] > p[i]) for i in xrange(n) for j in xrange(i)) == len(dp)-1)
+        return len(dp[-1])%MOD
+
+        return len(dp[-1])%MOD
