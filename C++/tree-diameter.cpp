1bc3c20e95bda242697a3754bc501b9c83e0c819
kamyu <kamyu104@users.noreply.github.com>
2024-06-30 18:05:23 +0800

    Update tree-diameter.cpp

--- C++/tree-diameter.cpp
+++ C++/tree-diameter.cpp
@@ -1,44 +1,111 @@
 // Time:  O(|V| + |E|)
 // Space: O(|E|)
 
+// iterative dfs
 class Solution {
-private:
-	template <typename T>
-    struct PairHash {
-        size_t operator()(const pair<T, T>& p) const {
-            size_t seed = 0;
-            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            return seed;
-        }
-    };
-
 public:
     int treeDiameter(vector<vector<int>>& edges) {
-        unordered_map<int, unordered_set<int>> graph;
-        int length = 0;
-        for (const auto& edge : edges) {
-            graph[edge[0]].emplace(edge[1]);
-            graph[edge[1]].emplace(edge[0]);
+        vector<vector<int>> adj(size(edges) + 1);
+        for (const auto& e : edges) {
+            adj[e[0]].emplace_back(e[1]);
+            adj[e[1]].emplace_back(e[0]);
         }
-        unordered_set<pair<int, int>, PairHash<int>> curr_level;
-        for (const auto& [u, neighbors] : graph) {
-            if (neighbors.size() == 1) {
-                curr_level.emplace(-1, u);
-            }
-        }
-        while (!curr_level.empty()) {
-            unordered_set<pair<int, int>, PairHash<int>> new_level;
-            for (const auto& [prev, u] : curr_level) {
-                for (const auto& v : graph[u]) {
-                    if (v != prev) {
-                        new_level.emplace(u, v);
+        int result = 0;
+        const auto& iter_dfs = [&]() {
+            int result = 0;
+            using RET = int;
+            RET ret{};
+            vector<tuple<int, int, int, shared_ptr<RET>, RET *>> stk = {{1, 0, -1, nullptr, &ret}};
+            while (!empty(stk)) {
+                const auto [step, u, p, ret2, ret] = stk.back(); stk.pop_back();
+                if (step == 1) {
+                    for (const auto& v : adj[u]) {
+                        if (v == p) {
+                            continue;
+                        }
+                        const auto& ret2 = make_shared<RET>();
+                        stk.emplace_back(2, -2, -2, ret2, ret);
+                        stk.emplace_back(1, v, u, nullptr, ret2.get());
                     }
+                } else if (step == 2) {
+                    result = max(result, *ret + (*ret2 + 1));
+                    *ret = max(*ret, *ret2 + 1);
                 }
             }
-            curr_level = move(new_level);
-            ++length;
+            return result;
+        };
+
+        return iter_dfs();
+    }
+};
+
+// Time:  O(|V| + |E|)
+// Space: O(|E|)
+// dfs
+class Solution2 {
+public:
+    int treeDiameter(vector<vector<int>>& edges) {
+        vector<vector<int>> adj(size(edges) + 1);
+        for (const auto& e : edges) {
+            adj[e[0]].emplace_back(e[1]);
+            adj[e[1]].emplace_back(e[0]);
         }
-        return max(length - 1, 0);
+        int result = 0;
+        const function<int (int, int)> dfs = [&](int u, int p) {
+            int mx = 0;
+            for (const auto& v : adj[u]) {
+                if (v == p) {
+                    continue;
+                }
+                const int curr = dfs(v, u);
+                result = max(result, mx + (curr + 1));
+                mx = max(mx, curr + 1);
+            }
+            return mx;
+        };
+
+        dfs(0, -1);
+        return result;
+    }
+};
+
+// Time:  O(|V| + |E|)
+// Space: O(|E|)
+// bfs
+class Solution3 {
+public:
+    int treeDiameter(vector<vector<int>>& edges) {
+        vector<vector<int>> adj(size(edges) + 1);
+        for (const auto& e : edges) {
+            adj[e[0]].emplace_back(e[1]);
+            adj[e[1]].emplace_back(e[0]);
+        }
+        int result = 0;
+        const auto& bfs = [&](int root) {
+            int d = -1, new_root = -1;
+            vector<bool> lookup(size(adj));
+            lookup[root] = true;
+            vector<int> q = {root};
+            while (!empty(q)) {
+                ++d;
+                new_root = q[0];
+                vector<int> new_q;
+                for (const auto& u : q) {
+                    for (const auto& v : adj[u]) {
+                        if (lookup[v]) {
+                            continue;
+                        }
+                        lookup[v] = true;
+                        new_q.emplace_back(v);
+                    }
+                }
+                q = move(new_q);
+            }
+            return pair(d, new_root);
+        };
+
+        const auto& [_, root] = bfs(0);
+        const auto& [d, __] = bfs(root);
+        return d;
     }
 };
