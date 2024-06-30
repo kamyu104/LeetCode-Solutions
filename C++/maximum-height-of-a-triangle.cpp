23b5158d9e4b1635a459edbb4b65be09a02eec01
kamyu <kamyu104@users.noreply.github.com>
2024-06-30 17:38:10 +0800

    Update maximum-height-of-a-triangle.cpp

--- C++/maximum-height-of-a-triangle.cpp
+++ C++/maximum-height-of-a-triangle.cpp
@@ -5,19 +5,19 @@
 class Solution {
 public:
     int maxHeightOfTriangle(int red, int blue) {
-        // odd level:
-        // (1+h)*((1+h)//2)//2 <= x
-        // => h <= int(2*x**0.5)-1
-        // even level:
-        // (2+h)*(h//2)//2 <= y
-        // => h <= int((4*y+1)**0.5)-1
-        int result = 0;
-        for (const auto& [x, y] : vector<pair<int, int>>{{red, blue}, {blue, red}}) {
+        const auto& f = [](int x, int y) {
+            // odd level:
+            // (1+h)*((1+h)//2)//2 <= x
+            // => h <= int(2*x**0.5)-1
+            // even level:
+            // (2+h)*(h//2)//2 <= y
+            // => h <= int((4*y+1)**0.5)-1
             const int a = static_cast<int>(2 * sqrt(x)) - 1;
             const int b = static_cast<int>(sqrt(4 * y + 1)) - 1;
-            result = max(result, min(a, b) + (a != b ? 1 : 0));
-        }
-        return result;
+            return min(a, b) + (a != b ? 1 : 0);
+        };
+    
+        return max(f(red, blue), f(blue, red));
     }
 };
 
