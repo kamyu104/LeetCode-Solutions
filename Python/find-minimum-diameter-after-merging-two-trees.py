7cd87767a581599f5b1d4b39b4911519e4010ce8
kamyu <kamyu104@users.noreply.github.com>
2024-06-30 17:42:25 +0800

    Create find-minimum-diameter-after-merging-two-trees.py

---
+++ Python/find-minimum-diameter-after-merging-two-trees.py
@@ -0,0 +1,124 @@
+# Time:  O(n + m)
+# Space: O(n + m)
+
+# iterative dfs, tree diameter
+class Solution(object):
+    def minimumDiameterAfterMerge(self, edges1, edges2):
+        """
+        :type edges1: List[List[int]]
+        :type edges2: List[List[int]]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//2
+    
+        def tree_diameter(edges):
+            def iter_dfs():
+                result = 0
+                stk = [(1, (0, -1, [0]))]
+                while stk:
+                    step, args = stk.pop()
+                    if step == 1:
+                        u, p, ret = args
+                        for v in reversed(adj[u]):
+                            if v == p:
+                                continue
+                            ret2 = [0]
+                            stk.append((2, (ret2, ret)))
+                            stk.append((1, (v, u, ret2)))
+                    elif step == 2:
+                        ret2, ret = args
+                        result = max(result, ret[0]+(ret2[0]+1))
+                        ret[0] = max(ret[0], ret2[0]+1)
+                return result
+            
+            adj = [[] for _ in range(len(edges)+1)]
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
+            return iter_dfs()
+        
+        d1 = tree_diameter(edges1)
+        d2 = tree_diameter(edges2)
+        return max(ceil_divide(d1, 2)+1+ceil_divide(d2, 2), d1, d2)
+        
+
+# Time:  O(n + m)
+# Space: O(n + m)
+# dfs, tree diameter
+class Solution2(object):
+    def minimumDiameterAfterMerge(self, edges1, edges2):
+        """
+        :type edges1: List[List[int]]
+        :type edges2: List[List[int]]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//2
+    
+        def tree_diameter(edges):
+            def dfs(u, p):
+                mx = 0
+                for v in adj[u]:
+                    if v == p:
+                        continue
+                    curr = dfs(v, u)
+                    result[0] = max(result[0], mx+(curr+1))
+                    mx = max(mx, curr+1)
+                return mx
+            
+            adj = [[] for _ in range(len(edges)+1)]
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
+            result = [0]
+            dfs(0, -1)
+            return result[0]
+        
+        d1 = tree_diameter(edges1)
+        d2 = tree_diameter(edges2)
+        return max(ceil_divide(d1, 2)+1+ceil_divide(d2, 2), d1, d2)
+
+
+# Time:  O(n + m)
+# Space: O(n + m)
+# bfs, tree diameter
+class Solution3(object):
+    def minimumDiameterAfterMerge(self, edges1, edges2):
+        """
+        :type edges1: List[List[int]]
+        :type edges2: List[List[int]]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//2
+    
+        def tree_diameter(edges):
+            def bfs(root):
+                d = new_root = -1
+                lookup = [False]*len(adj)
+                lookup[root] = True
+                q = [root]
+                while q:
+                    d, new_root = d+1, q[0]
+                    new_q = []
+                    for u in q:
+                        for v in adj[u]:
+                            if lookup[v]:
+                                continue
+                            lookup[v] = True
+                            new_q.append(v)
+                    q = new_q
+                return d, new_root
+            
+            adj = [[] for _ in range(len(edges)+1)]
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
+            _, root = bfs(0)
+            d, _ = bfs(root)
+            return d
+        
+        d1 = tree_diameter(edges1)
+        d2 = tree_diameter(edges2)
+        return max(ceil_divide(d1, 2)+1+ceil_divide(d2, 2), d1, d2)
