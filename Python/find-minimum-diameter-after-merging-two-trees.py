3acf8788d82da8eecafe473e67d71bde8406c36a
kamyu <kamyu104@users.noreply.github.com>
2024-07-02 00:57:06 +0800

    Update find-minimum-diameter-after-merging-two-trees.py

--- Python/find-minimum-diameter-after-merging-two-trees.py
+++ Python/find-minimum-diameter-after-merging-two-trees.py
@@ -94,6 +94,54 @@ class Solution3(object):
             return (a+b-1)//2
     
         def tree_diameter(edges):
+            def bfs():
+                result = 0
+                dp = [0]*len(adj)
+                degree = map(len, adj)
+                q = [u for u in xrange(len(degree)) if degree[u] == 1]
+                while q:
+                    new_q = []
+                    for u in q:
+                        if degree[u] == 0:
+                            continue
+                        degree[u] -= 1
+                        for v in adj[u]:
+                            if degree[v] == 0:
+                                continue
+                            result = max(result, dp[v]+(dp[u]+1))
+                            dp[v] = max(dp[v], (dp[u]+1))
+                            degree[v] -= 1
+                            if degree[v] == 1:
+                                new_q.append(v)
+                    q = new_q
+                return result
+            
+            adj = [[] for _ in range(len(edges)+1)]
+            for u, v in edges:
+                adj[u].append(v)
+                adj[v].append(u)
+            return bfs()
+
+        d1 = tree_diameter(edges1)
+        d2 = tree_diameter(edges2)
+        return max(ceil_divide(d1, 2)+1+ceil_divide(d2, 2), d1, d2)
+
+
+
+# Time:  O(n + m)
+# Space: O(n + m)
+# bfs, tree diameter
+class Solution4(object):
+    def minimumDiameterAfterMerge(self, edges1, edges2):
+        """
+        :type edges1: List[List[int]]
+        :type edges2: List[List[int]]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//2
+    
+        def tree_diameter(edges):
             def bfs(root):
                 d = new_root = -1
                 lookup = [False]*len(adj)
