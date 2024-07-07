6613f6eb8e855152d5efbc65f1260bd3f0381ecc
kamyu <kamyu104@users.noreply.github.com>
2024-07-08 06:08:43 +0800

    Update construct-string-with-minimum-cost.py

--- Python/construct-string-with-minimum-cost.py
+++ Python/construct-string-with-minimum-cost.py
@@ -14,6 +14,16 @@ class Solution(object):
         :rtype: int
         """
         INF = float("inf")
+        def query(i):
+            curr = trie
+            for j in xrange(i, len(target)):
+                x = target[j]
+                if x not in curr:
+                    break
+                curr = curr[x]
+                if "_end" in curr:
+                    dp[j+1] = min(dp[j+1], dp[i]+curr["_end"])
+
         _trie = lambda: collections.defaultdict(_trie)
         trie = _trie()
         for w, c in itertools.izip(words, costs):
@@ -26,14 +36,8 @@ class Solution(object):
         for i in xrange(len(target)):
             if dp[i] == INF:
                 continue
-            curr = trie
-            for j in xrange(i, len(target)):
-                x = target[j]
-                if x not in curr:
-                    break
-                curr = curr[x]
-                if "_end" in curr:
-                    dp[j+1] = min(dp[j+1], dp[i]+curr["_end"])
+            query(i)
+                
         return dp[-1] if dp[-1] != INF else -1
 
 
@@ -54,24 +58,34 @@ class Solution2(object):
         INF = float("inf")
         class Trie(object):
             def __init__(self):
-                self.nodes = []
-                self.mns = []
+                self.__nodes = []
+                self.__mns = []
                 self.__new_node()
             
             def __new_node(self):
-                self.nodes.append([-1]*26)
-                self.mns.append(INF)
-                return len(self.nodes)-1
+                self.__nodes.append([-1]*26)
+                self.__mns.append(INF)
+                return len(self.__nodes)-1
 
             def add(self, w, c):
                 curr = 0
                 for x in w:
                     x = ord(x)-ord('a')
-                    if self.nodes[curr][x] == -1:
-                        self.nodes[curr][x] = self.__new_node()
-                    curr = self.nodes[curr][x]
-                self.mns[curr] = min(self.mns[curr], c)
-
+                    if self.__nodes[curr][x] == -1:
+                        self.__nodes[curr][x] = self.__new_node()
+                    curr = self.__nodes[curr][x]
+                self.__mns[curr] = min(self.__mns[curr], c)
+            
+            def query(self, i):
+                curr = 0
+                for j in xrange(i, len(target)):
+                    x = ord(target[j])-ord('a')
+                    if self.__nodes[curr][x] == -1:
+                        break
+                    curr = self.__nodes[curr][x]
+                    if self.__mns[curr] != INF:
+                        dp[j+1] = min(dp[j+1], dp[i]+self.__mns[curr])
+    
         trie = Trie()
         for w, c in itertools.izip(words, costs):
             trie.add(w, c)
@@ -80,12 +94,5 @@ class Solution2(object):
         for i in xrange(len(target)):
             if dp[i] == INF:
                 continue
-            curr = 0
-            for j in xrange(i, len(target)):
-                x = ord(target[j])-ord('a')
-                if trie.nodes[curr][x] == -1:
-                    break
-                curr = trie.nodes[curr][x]
-                if trie.mns[curr] != INF:
-                    dp[j+1] = min(dp[j+1], dp[i]+trie.mns[curr])
+            trie.query(i)
         return dp[-1] if dp[-1] != INF else -1
