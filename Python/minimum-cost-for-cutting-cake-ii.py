9a05fa8c6efd23e23f27511f30fe2b338111d08a
kamyu <kamyu104@users.noreply.github.com>
2024-07-14 16:07:58 +0800

    Update minimum-cost-for-cutting-cake-ii.py

--- Python/minimum-cost-for-cutting-cake-ii.py
+++ Python/minimum-cost-for-cutting-cake-ii.py
@@ -24,3 +24,28 @@ class Solution(object):
                 cnt_h += 1
         return result
 
+
+# Time:  O(mlogm + nlogn)
+# Space: O(1)
+# sort, greedy
+class Solution2(object):
+    def minimumCost(self, m, n, horizontalCut, verticalCut):
+        """
+        :type m: int
+        :type n: int
+        :type horizontalCut: List[int]
+        :type verticalCut: List[int]
+        :rtype: int
+        """
+        horizontalCut.sort(reverse=True)
+        verticalCut.sort(reverse=True)
+        result = i = j = 0
+        while i < len(horizontalCut) or j < len(verticalCut):
+            if j == len(verticalCut) or (i < len(horizontalCut) and horizontalCut[i] > verticalCut[j]):
+                result += horizontalCut[i]*(j+1)
+                i += 1
+            else:
+                result += verticalCut[j]*(i+1)
+                j += 1
+        return result
+
