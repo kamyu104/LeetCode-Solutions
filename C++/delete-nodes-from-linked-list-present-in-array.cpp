9be65bf4e3d3fe452ad57464734f832198b039e0
kamyu <kamyu104@users.noreply.github.com>
2024-07-14 14:32:38 +0800

    Create delete-nodes-from-linked-list-present-in-array.cpp

---
+++ C++/delete-nodes-from-linked-list-present-in-array.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+// linked list
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+        unordered_set<int> lookup(cbegin(nums), cend(nums));
+        ListNode dummy(0, head);
+        for (auto curr = &dummy; curr->next;) {
+            if (!lookup.count(curr->next->val)) {
+                curr = curr->next;
+            } else {
+                auto to_del = curr->next;
+                curr->next = curr->next->next;
+                delete to_del;
+            }
+        }
+        return dummy.next;
+    }
+};
