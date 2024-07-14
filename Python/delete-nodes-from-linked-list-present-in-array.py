894192385ea91964a9d70a19041f5f5609d8b77e
kamyu <kamyu104@users.noreply.github.com>
2024-07-14 14:33:12 +0800

    Create delete-nodes-from-linked-list-present-in-array.py

---
+++ Python/delete-nodes-from-linked-list-present-in-array.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(1)
+
+# linked list
+class Solution(object):
+    def modifiedList(self, nums, head):
+        """
+        :type nums: List[int]
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        lookup = set(nums)
+        curr = dummy = ListNode(0, head)
+        while curr.next:
+            if curr.next.val not in lookup:
+                curr = curr.next
+            else:
+                curr.next = curr.next.next
+        return dummy.next
