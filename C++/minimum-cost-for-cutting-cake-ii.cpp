5b7fce69d5dc53d758597d15991079292b320bab
kamyu <kamyu104@users.noreply.github.com>
2024-07-14 15:32:36 +0800

    Update minimum-cost-for-cutting-cake-ii.cpp

--- C++/minimum-cost-for-cutting-cake-ii.cpp
+++ C++/minimum-cost-for-cutting-cake-ii.cpp
@@ -8,13 +8,13 @@ public:
         sort(begin(horizontalCut), end(horizontalCut));
         sort(begin(verticalCut), end(verticalCut));
         int64_t result = 0;
-        for (int cnt1 = 1, cnt2 = 1; !empty(horizontalCut) || !empty(verticalCut); ) {
+        for (int cnt_v = 1, cnt_h = 1; !empty(horizontalCut) || !empty(verticalCut); ) {
             if (empty(verticalCut) || (!empty(horizontalCut) && horizontalCut.back() > verticalCut.back())) {
-                result += horizontalCut.back() * cnt1; horizontalCut.pop_back();
-                ++cnt2;
+                result += horizontalCut.back() * cnt_v; horizontalCut.pop_back();
+                ++cnt_h;
             } else {
-                result += verticalCut.back() * cnt2; verticalCut.pop_back();
-                ++cnt1;
+                result += verticalCut.back() * cnt_h; verticalCut.pop_back();
+                ++cnt_v;
             }
         }
         return result;
