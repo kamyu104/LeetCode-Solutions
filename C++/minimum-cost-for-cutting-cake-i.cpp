5ffbb6c109589b6b4d59dea53beb3648092bda04
kamyu <kamyu104@users.noreply.github.com>
2024-07-14 16:11:24 +0800

    Update minimum-cost-for-cutting-cake-i.cpp

--- C++/minimum-cost-for-cutting-cake-i.cpp
+++ C++/minimum-cost-for-cutting-cake-i.cpp
@@ -7,7 +7,7 @@ public:
     int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
         sort(begin(horizontalCut), end(horizontalCut));
         sort(begin(verticalCut), end(verticalCut));
-        int64_t result = 0;
+        int result = 0;
         for (int cnt_h = 1, cnt_v = 1; !empty(horizontalCut) || !empty(verticalCut); ) {
             if (empty(verticalCut) || (!empty(horizontalCut) && horizontalCut.back() > verticalCut.back())) {
                 result += horizontalCut.back() * cnt_h; horizontalCut.pop_back();
@@ -21,10 +21,30 @@ public:
     }
 };
 
+// Time:  O(mlogm + nlogn)
+// Space: O(1)
+// sort, greedy
+class Solution2 {
+public:
+    int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
+        sort(begin(horizontalCut), end(horizontalCut), greater<int>());
+        sort(begin(verticalCut), end(verticalCut), greater<int>());
+        int result = 0;
+        for (int i = 0, j = 0; i < size(horizontalCut) || j < size(verticalCut); ) {
+            if (j == size(verticalCut) || (i < size(horizontalCut) && horizontalCut[i] > verticalCut[j])) {
+                result += horizontalCut[i++] * (j + 1);
+            } else {
+                result += verticalCut[j++] * (i + 1);
+            }
+        }
+        return result;
+    }
+};
+
 // Time:  O((m + n) * m^2 * n^2)
 // Space: O(m^2 * n^2)
 // memoization
-class Solution2 {
+class Solution3 {
 public:
     int minimumCost(int m, int n, vector<int>& horizontalCut, vector<int>& verticalCut) {
         vector<vector<vector<vector<int>>>> lookup(m, vector<vector<vector<int>>>(n, vector<vector<int>>(m, vector<int>(n, -1))));
