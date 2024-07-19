a90fc6e1f6ba39f5cfafb1db00021b3fa12088da
kamyu <kamyu104@users.noreply.github.com>
2024-07-19 22:11:45 +0800

    Update divisor-game.cpp

--- C++/divisor-game.cpp
+++ C++/divisor-game.cpp
@@ -16,10 +16,46 @@ public:
     }
 };
 
-// Time:  O(n^3/2)
+// Time:  O(nlogn)
+// Space: O(nlogn)
+// memoization, number theory
+class Solution2 {
+public:
+    bool divisorGame(int n) {
+        const auto& factors = [](int n) {
+            vector<vector<int>> result(n + 1);
+            for (int i = 1; i <= n; ++i) {
+                for (int j = i; j <= n; j += i) {
+                    result[j].emplace_back(i);
+                }
+            }
+            return result;
+        };
+
+        const auto& FACTORS = factors(n);
+        vector<int> lookup(n + 1, -1);
+        const function<int (int)> memoization = [&](int n) {
+            if (lookup[n] == -1) {
+                int result = 0;
+                for (const auto& i : FACTORS[n]) {
+                    if (i != n && !memoization(n - i)) {
+                        result = 1;
+                        break;
+                    }
+                }
+                lookup[n] = result;
+            }
+            return lookup[n];
+        };
+
+        return memoization(n);
+    }
+};
+
+// Time:  O(n^(3/2))
 // Space: O(n)
 // memoization
-class Solution2 {
+class Solution3 {
 public:
     bool divisorGame(int n) {
         vector<int> lookup(n + 1, -1);
@@ -39,6 +75,34 @@ public:
                         continue;
                     }
                     if (j != n && !memoization(n - j)) {
+                        result = 1;
+                        break;
+                    }
+                }
+                lookup[n] = result;
+            }
+            return lookup[n];
+        };
+
+        return memoization(n);
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+// memoization
+class Solution4 {
+public:
+    bool divisorGame(int n) {
+        vector<int> lookup(n + 1, -1);
+        const function<int (int)> memoization = [&](int n) {
+            if (lookup[n] == -1) {
+                int result = 0;
+                for (auto i = 1; i <= n; ++i) {
+                    if (n % i) {
+                        continue;
+                    }
+                    if (i != n && !memoization(n - i)) {
                         result = 1;
                         break;
                     }
