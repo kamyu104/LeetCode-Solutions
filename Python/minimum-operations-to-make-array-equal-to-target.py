72cb0d18308526745749b779b1b7cec672881ac0
kamyu <kamyu104@users.noreply.github.com>
2024-07-21 19:18:43 +0800

    Create minimum-operations-to-make-array-equal-to-target.py

---
+++ Python/minimum-operations-to-make-array-equal-to-target.py
@@ -0,0 +1,14 @@
+# Time:  O(n)
+# Space: O(1)
+
+# greedy
+class Solution(object):
+    def minimumOperations(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: List[int]
+        :rtype: int
+        """
+        for i in xrange(len(target)):
+            target[i] -= nums[i]
+        return sum(max((target[i] if i < len(target) else 0)-(target[i-1] if i-1 >= 0 else 0), 0) for i in xrange(len(target)+1))
