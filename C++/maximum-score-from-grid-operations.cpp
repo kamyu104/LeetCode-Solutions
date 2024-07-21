d3d0087a18a0ac89fb6e3237ceec64f85f513097
kamyu <kamyu104@users.noreply.github.com>
2024-07-21 17:47:06 +0800

    Update maximum-score-from-grid-operations.cpp

--- C++/maximum-score-from-grid-operations.cpp
+++ C++/maximum-score-from-grid-operations.cpp
@@ -17,40 +17,6 @@ public:
             }
             vector<vector<int64_t>> new_dp(2, vector<int64_t>(size(grid) + 1));
             for (int i = 0; i <= size(grid); ++i) {
-                for (int k = 0; k < i + 1; ++k) {
-                    new_dp[0][i] = max({new_dp[0][i], (prefix[i] - prefix[k]) + dp[0][k], dp[1][k]});
-                }
-                for (int k = i + 1; k <= size(grid); ++k) {
-                    new_dp[1][i] = max(new_dp[1][i], dp[1][k] + (new_prefix[k] - new_prefix[i]));
-                }
-                new_dp[1][i] = max(new_dp[1][i], new_dp[0][i]);
-            }
-            dp = move(new_dp);
-            prefix = move(new_prefix);
-        }
-        return ranges::max(dp[1]);
-    }
-};
-
-
-// Time:  O(n^3)
-// Space: O(n)
-// prefix sum, dp
-class Solution2 {
-public:
-    long long maximumScore(vector<vector<int>>& grid) {
-        vector<int64_t> prefix(size(grid)+ 1);
-        for (int i = 0; i < size(grid); ++i) {
-            prefix[i + 1] = prefix[i] + grid[i][0];
-        }
-        vector<vector<int64_t>> dp(2, vector<int64_t>(size(grid) + 1));
-        for (int j = 1; j < size(grid[0]); ++j) {
-            vector<int64_t> new_prefix(size(grid)+ 1);
-            for (int i = 0; i < size(grid); ++i) {
-                new_prefix[i + 1] = new_prefix[i] + grid[i][j];
-            }
-            vector<vector<int64_t>> new_dp(2, vector<int64_t>(size(grid) + 1));
-            for (int i = 0; i <= size(grid); ++i) {
                 for (int k = 0; k <= size(grid); ++k) {
                     new_dp[0][i] = max({new_dp[0][i], max(prefix[i] - prefix[k], static_cast<int64_t>(0)) + dp[0][k], dp[1][k]});
                     new_dp[1][i] = max(new_dp[1][i], dp[1][k] + max(new_prefix[k] - new_prefix[i], static_cast<int64_t>(0)));
