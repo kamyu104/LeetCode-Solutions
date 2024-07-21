30beff981d0532339c91cc49359fab1aea57fc1f
kamyu <kamyu104@users.noreply.github.com>
2024-07-21 19:18:09 +0800

    Create minimum-operations-to-make-array-equal-to-target.cpp

---
+++ C++/minimum-operations-to-make-array-equal-to-target.cpp
@@ -0,0 +1,17 @@
+// Time:  O(n)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    long long minimumOperations(vector<int>& nums, vector<int>& target) {
+        for (int i = 0; i < size(target); ++i) {
+            target[i] -= nums[i];
+        }
+        int64_t result = 0;
+        for (int i = 0; i <= size(target); ++i) {
+            result += max((i < size(target) ? target[i] : 0) - (i - 1 >= 0 ? target[i - 1] : 0), 0);
+        }
+        return result;
+    }
+};
