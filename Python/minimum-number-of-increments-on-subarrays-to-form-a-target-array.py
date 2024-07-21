5bedbf932f4f7a40aa7edfb40848febcfad4455c
kamyu <kamyu104@users.noreply.github.com>
2024-07-21 19:14:32 +0800

    Update minimum-number-of-increments-on-subarrays-to-form-a-target-array.py

--- Python/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ Python/minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -7,18 +7,4 @@ class Solution(object):
         :type target: List[int]
         :rtype: int
         """
-        return target[0]+sum(max(target[i]-target[i-1], 0) for i in xrange(1, len(target)))
-
-
-# Time:  O(n)
-# Space: O(n)
-import itertools
-
-
-class Solution2(object):
-    def minNumberOperations(self, target):
-        """
-        :type target: List[int]
-        :rtype: int
-        """
-        return sum(max(b-a, 0) for b, a in itertools.izip(target, [0]+target))
+        return sum(max((target[i] if i < len(target) else 0)-(target[i-1] if i-1 >= 0 else 0), 0) for i in xrange(len(target)+1))
