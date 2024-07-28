cb837d32e789917b6687eba34c198372b7837429
kamyu <kamyu104@users.noreply.github.com>
2024-07-28 22:59:41 +0800

    Update count-the-number-of-substrings-with-dominant-ones.cpp

--- C++/count-the-number-of-substrings-with-dominant-ones.cpp
+++ C++/count-the-number-of-substrings-with-dominant-ones.cpp
@@ -32,9 +32,36 @@ public:
 };
 
 // Time:  O(n * sqrt(n)) = O(n^(3/2))
-// Space: O(1)
+// Space: O(n)
 // two pointers, sliding window
 class Solution2 {
+public:
+    int numberOfSubstrings(string s) {
+        int result = 0;
+        vector<int> idxs = {-1};
+        for (int i = 0; i < size(s); ++i) {
+            if (s[i] == '0') {
+                idxs.emplace_back(i);
+            }
+        }
+        idxs.emplace_back(size(s));
+        for (int i = 0, curr = 1; i < size(s); ++i) {
+            if (idxs[curr] == i) {
+                ++curr;
+            }
+            for (int c = 0, cnt = 0; c * c <= min(static_cast<int>(size(s)), (curr - 1) * (curr - 1)); ++c) {
+                cnt += (c ? idxs[curr - c] - 1 : i) - idxs[curr - c - 1];
+                result += max(min((c ? idxs[curr - c] : i) - idxs[curr - c - 1], cnt - c * c + 1), 0);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * sqrt(n)) = O(n^(3/2))
+// Space: O(1)
+// two pointers, sliding window
+class Solution3 {
 public:
     int numberOfSubstrings(string s) {
         int result = 0;
