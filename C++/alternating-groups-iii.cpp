b85c5c328a79fd4d63271be3d5c2c367cc23dc18
kamyu <kamyu104@users.noreply.github.com>
2024-08-06 21:04:26 +0800

    Create alternating-groups-iii.cpp

---
+++ C++/alternating-groups-iii.cpp
@@ -0,0 +1,92 @@
+// Time:  O(nlogn + qlogn)
+// Space: O(n)
+
+// sorted list, bit, fenwick tree
+class Solution {
+private:
+    class BIT {
+    public:
+        BIT(int n) : bit_(n + 1) {  // 0-indexed
+        }
+        
+        void add(int i, int val) {
+            ++i;
+            for (; i < size(bit_); i += lower_bit(i)) {
+                bit_[i] += val;
+            }
+        }
+
+        int query(int i) const {
+            ++i;
+            int total = 0;
+            for (; i > 0; i -= lower_bit(i)) {
+                total += bit_[i];
+            }
+            return total;
+        }
+    
+    private:
+        int lower_bit(int i) const {
+            return i & -i;
+        }
+        
+        vector<int> bit_;
+    };
+
+public:
+    vector<int> numberOfAlternatingGroups(vector<int>& colors, vector<vector<int>>& queries) {
+        const int n = size(colors);
+        set<int> bst;
+        BIT bit1(n + 1), bit2(n + 1);
+        const auto& update = [&](int i, int d) {
+            if (d == +1) {
+                bst.emplace(i);
+            }
+            auto curr = bst.find(i);
+            auto prv = prev(curr != begin(bst) ? curr : end(bst));
+            auto nxt = next(curr);
+            if (nxt == end(bst)) {
+                nxt = begin(bst);
+            }
+            if (size(bst) != 1) {
+                int l = (size(bst) != 2) ? ((*nxt) - (*prv) + n) % n : n;
+                bit1.add(l, d * -1);
+                bit2.add(l, d * -l);
+                l = ((*curr) - (*prv) + n) % n;
+                bit1.add(l, d * +1);
+                bit2.add(l, d * +l);
+                l = ((*nxt) - (*curr) + n) % n;
+                bit1.add(l, d * +1);
+                bit2.add(l, d * +l);
+            }
+            if (d == -1) {
+                bst.erase(curr);
+            }
+        };
+
+        vector<int> result;
+        bit1.add(n, +1);
+        bit2.add(n, +n);
+        for (int i = 0; i < n; ++i) {
+            if (colors[i] == colors[(i + 1) % n]) {
+                update(i, +1);
+            }
+        }
+        for (const auto &q : queries) {
+            if (q[0] == 1) {
+                const int l = q[1];
+                result.emplace_back(!empty(bst) ? (bit2.query(n) - bit2.query(l - 1)) - 
+                                                  (l - 1) * (bit1.query(n) - bit1.query(l - 1)) : n);
+                continue;
+            }
+            const int i = q[1];
+            if (colors[i] == q[2]) {
+                continue;
+            }
+            colors[i] = q[2];
+            update((i - 1 + n) % n, colors[i] == colors[(i - 1 + n) % n] ? +1 : -1);
+            update(i, colors[i] == colors[(i + 1) % n] ? +1 : -1);
+        }
+        return result;
+    }
+};
