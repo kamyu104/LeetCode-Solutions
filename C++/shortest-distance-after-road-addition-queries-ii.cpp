1c187d9b6df4c6dd5caec14d29d21f3f148424d5
kamyu <kamyu104@users.noreply.github.com>
2024-08-06 21:59:17 +0800

    Update shortest-distance-after-road-addition-queries-ii.cpp

--- C++/shortest-distance-after-road-addition-queries-ii.cpp
+++ C++/shortest-distance-after-road-addition-queries-ii.cpp
@@ -10,9 +10,8 @@ public:
         iota(begin(arr), end(arr), 0);
         set<int> bst(cbegin(arr), cend(arr));
         for (const auto& q : queries) {
-            auto it = bst.upper_bound(q[0]);
             auto jt = bst.lower_bound(q[1]);
-            for (; it != jt; it = bst.erase(it));
+            for (auto it = bst.upper_bound(q[0]); it != jt; it = bst.erase(it));
             result.emplace_back(size(bst) - 1);
         }
         return result;
