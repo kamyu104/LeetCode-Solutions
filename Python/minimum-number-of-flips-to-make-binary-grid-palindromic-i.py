8642244e461d20633740d7aa78ea4f898b6454d9
kamyu <kamyu104@users.noreply.github.com>
2024-08-06 18:03:13 +0800

    Create minimum-number-of-flips-to-make-binary-grid-palindromic-i.py

---
+++ Python/minimum-number-of-flips-to-make-binary-grid-palindromic-i.py
@@ -0,0 +1,16 @@
+# Time:  O(m * n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def minFlips(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        def count(m, n, get):
+            return sum(get(i, j) != get(i, ~j) for i in xrange(m) for j in xrange(n//2))
+
+        m, n = len(grid), len(grid[0])
+        return min(count(m, n, lambda i, j: grid[i][j]),
+                   count(n, m, lambda i, j: grid[j][i]))
