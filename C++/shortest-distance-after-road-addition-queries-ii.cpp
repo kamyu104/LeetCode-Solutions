2e9e6b45e1badab1cacf39d2c96045044c20c6cc
kamyu <kamyu104@users.noreply.github.com>
2024-08-07 03:11:08 +0800

    Update shortest-distance-after-road-addition-queries-ii.cpp

--- C++/shortest-distance-after-road-addition-queries-ii.cpp
+++ C++/shortest-distance-after-road-addition-queries-ii.cpp
@@ -6,9 +6,10 @@ class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
         vector<int> result;
-        vector<int> arr(n);
-        iota(begin(arr), end(arr), 0);
-        set<int> bst(cbegin(arr), cend(arr));
+        set<int> bst;
+        for (int i = 0; i < n; ++i) {
+            bst.emplace(i);
+        }
         for (const auto& q : queries) {
             auto jt = bst.lower_bound(q[1]);
             for (auto it = bst.upper_bound(q[0]); it != jt; it = bst.erase(it));
