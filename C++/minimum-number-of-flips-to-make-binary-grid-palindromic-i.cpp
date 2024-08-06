28286157697e378296f7bcdbf11662aa01b74f75
kamyu <kamyu104@users.noreply.github.com>
2024-08-06 18:02:13 +0800

    Create minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp

---
+++ C++/minimum-number-of-flips-to-make-binary-grid-palindromic-i.cpp
@@ -0,0 +1,23 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int minFlips(vector<vector<int>>& grid) {
+        const auto& count = [](int m, int n, const auto& get) {
+            int result = 0;
+            for (int i = 0; i < m; ++i) {
+                for (int j = 0; j < n / 2; ++j) {
+                    if (get(i, j) != get(i, n - 1 - j)) {
+                        ++result;
+                    }
+                }
+            }
+            return result;
+        };
+
+        return min(count(size(grid), size(grid[0]), [&](int i, int j) { return grid[i][j]; }),
+                   count(size(grid[0]), size(grid), [&](int i, int j) { return grid[j][i]; }));
+    }
+};
