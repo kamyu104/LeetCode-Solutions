f07818c1e87459c762b4e272eeb0a2947d33318a
kamyu <kamyu104@users.noreply.github.com>
2024-08-12 01:52:14 +0800

    Update find-the-count-of-monotonic-pairs-ii.py

--- Python/find-the-count-of-monotonic-pairs-ii.py
+++ Python/find-the-count-of-monotonic-pairs-ii.py
@@ -20,6 +20,10 @@ class Solution(object):
             return nCr(n+r-1, r)
 
         MOD = 10**9+7
+        # arr1 = [0+x1, arr1[0]+max(nums[1]-nums[0], 0)+x2, ..., arr[n-2]+max(nums[n-1]-nums[n-2])+xn]
+        # => sum(max(nums[i]-nums[i-1], 0) for i in xrange(1, len(nums)))+(x1+x2+...xn) <= nums[-1]
+        # x1+x2+...xn <= nums[-1]-sum(max(nums[i]-nums[i-1], 0) for i in xrange(1, len(nums))) = cnt >= 0
+        # the answer is the number of solutions s.t. x1+x2+...xn <= cnt
         cnt = nums[-1]-sum(max(nums[i]-nums[i-1], 0) for i in xrange(1, len(nums)))
         return nHr(len(nums)+1, cnt) if cnt >= 0 else 0
     
