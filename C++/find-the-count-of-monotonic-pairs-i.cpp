71829fea1c68a50fe8715c378ddd66cef0efbb2e
kamyu <kamyu104@users.noreply.github.com>
2024-08-12 01:55:01 +0800

    Update find-the-count-of-monotonic-pairs-i.cpp

--- C++/find-the-count-of-monotonic-pairs-i.cpp
+++ C++/find-the-count-of-monotonic-pairs-i.cpp
@@ -5,6 +5,10 @@
 class Solution {
 public:
     int countOfPairs(vector<int>& nums) {
+        // arr1 = [0+x1, arr1[0]+max(nums[1]-nums[0], 0)+x2, ..., arr[n-2]+max(nums[n-1]-nums[n-2])+xn]
+        // => sum(max(nums[i]-nums[i-1], 0) for i in xrange(1, len(nums)))+(x1+x2+...+xn) <= nums[-1]
+        // x1+x2+...+xn <= nums[-1]-sum(max(nums[i]-nums[i-1], 0) for i in xrange(1, len(nums))) = cnt
+        // the answer is the number of solutions s.t. x1+x2+...xn <= cnt, where cnt >= 0
         int cnt = nums.back();
         for (int i = 1; i < size(nums); ++i) {
             cnt -= max(nums[i] - nums[i - 1], 0);
