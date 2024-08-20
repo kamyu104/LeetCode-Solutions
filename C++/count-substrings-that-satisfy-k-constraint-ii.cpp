e58b0a3f0dbfcba332f6e02c8afc90c6fbfb32ab
kamyu <kamyu104@users.noreply.github.com>
2024-08-21 02:01:59 +0800

    Create count-substrings-that-satisfy-k-constraint-ii.cpp

---
+++ C++/count-substrings-that-satisfy-k-constraint-ii.cpp
@@ -0,0 +1,36 @@
+// Time:  O(n)
+// Space: O(n)
+
+// two pointers, sliding window, prefix sum
+class Solution {
+public:
+    vector<long long> countKConstraintSubstrings(string s, int k, vector<vector<int>>& queries) {
+        const auto& count = [](int64_t l) {
+            return (l + 1) * l / 2;
+        };
+
+        vector<int64_t> prefix(size(s) + 1);
+        vector<int> lookup(size(s), -1);
+        for (int right = 0, left = 0, cnt = 0; right < size(s); ++right) {
+            cnt += s[right] == '1' ? 1 : 0;
+            while (!(cnt <= k || (right - left + 1) - cnt <= k)) {
+                cnt -= s[left++] == '1' ? 1 : 0;
+            }
+            prefix[right + 1] = prefix[right] + (right - left + 1);
+            lookup[left] = right;
+        }
+        assert(lookup[0] != -1);
+        for (int i = 0; i + 1 < size(s); ++i) {
+            if (lookup[i + 1] == -1) {
+                lookup[i + 1] = lookup[i];
+            }
+        }
+        vector<long long> result(size(queries));
+        for (int i = 0; i < size(queries); ++i) {
+            const int left = queries[i][0], right = queries[i][1];
+            const int new_right = min(lookup[left], right);
+            result[i] = count(new_right - left + 1) + (prefix[right + 1] - prefix[new_right + 1]);
+        }
+        return result;
+    }
+};
