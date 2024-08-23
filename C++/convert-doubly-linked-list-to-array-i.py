9dabe2f882e43c2bc60f50de723d439f9a183dfe
kamyu <kamyu104@users.noreply.github.com>
2024-08-23 23:59:34 +0800

    Create convert-doubly-linked-list-to-array-i.py

---
+++ C++/convert-doubly-linked-list-to-array-i.py
@@ -0,0 +1,14 @@
+// Time:  O(n)
+// Space: O(1)
+
+// linked list
+class Solution {
+public:
+	vector<int> toArray(Node *head){
+        vector<int> result;
+        for (; head; head = head->next) {
+            result.emplace_back(head->val);
+        }
+        return result;
+    }
+};
