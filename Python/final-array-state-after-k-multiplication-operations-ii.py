44e5b2b1b18cf84a81d3734e647792a2fca0b9a8
kamyu <kamyu104@users.noreply.github.com>
2024-08-26 09:07:49 +0800

    Update final-array-state-after-k-multiplication-operations-ii.py

--- Python/final-array-state-after-k-multiplication-operations-ii.py
+++ Python/final-array-state-after-k-multiplication-operations-ii.py
@@ -30,10 +30,8 @@ class Solution(object):
         def check(target):
             result = 0
             for x, i in vals:
-                if x >= target:
-                    break
                 c = count(x, target)
-                if c == 0:
+                if c <= 0:
                     break
                 result += c
             return result <= k
@@ -43,10 +41,8 @@ class Solution(object):
         vals = sorted([log(x)/log(multiplier), i] for i, x in enumerate(nums))
         target = binary_search_right(1, int(vals[-1][0])+1, check)
         for idx, (x, i) in enumerate(vals):
-            if x >= target:
-                break
             c = count(x, target)
-            if c == 0:
+            if c <= 0:
                 break
             k -= c
             nums[i] *= pow(multiplier, c)
