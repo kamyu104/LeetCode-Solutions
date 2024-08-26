3db085e11ec06cbe79dd02b17d1247213ae70ac9
kamyu <kamyu104@users.noreply.github.com>
2024-08-26 22:59:40 +0800

    Update count-almost-equal-pairs-ii.py

--- Python/count-almost-equal-pairs-ii.py
+++ Python/count-almost-equal-pairs-ii.py
@@ -47,7 +47,7 @@ class Solution(object):
 import collections
 
 
-# freq table, combinatorics
+# freq table, combinatorics, bfs
 class Solution2(object):
     def countPairs(self, nums):
         """
