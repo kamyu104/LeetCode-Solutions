276472b4f31d15dcecbc0c9a1cda05ada010ca45
kamyu <kamyu104@users.noreply.github.com>
2024-08-26 17:22:45 +0800

    Update count-almost-equal-pairs-ii.cpp

--- C++/count-almost-equal-pairs-ii.cpp
+++ C++/count-almost-equal-pairs-ii.cpp
@@ -112,3 +112,57 @@ public:
         return result;
     }
 };
+
+// Time:  O(n * l^(2 * k))
+// Space: O(n + l^(2 * k)) = O(n)
+// freq table, combinatorics
+class Solution3 {
+public:
+    int countPairs(vector<int>& nums) {
+        static const int L = 7;
+        static const int K = 2;
+        vector<int> POW10(L);
+        POW10[0] = 1;
+        for (int i = 0; i + 1 < L; ++i) {
+            POW10[i+1] = POW10[i] * 10;
+        }
+        const function<void (int, int, unordered_set<int>&)> at_most = [&](int k, int x, unordered_set<int>& result) {
+            result.emplace(x);
+            if (k == 0) {
+                return;
+            }
+            --k;
+            for (int i = 0; i < L; ++i) {
+                const int a = x / POW10[i] % 10;
+                for (int j = i + 1; j < L; ++j) {
+                    const int b = x / POW10[j] % 10;
+                    if (a == b) {
+                        continue;
+                    }
+                    x += -a * (POW10[i] - POW10[j]) + b * (POW10[i] - POW10[j]);
+                    at_most(k, x, result);
+                    x -= -a * (POW10[i] - POW10[j]) + b * (POW10[i] - POW10[j]);
+                }
+            }
+        };
+
+        int result = 0;
+        unordered_map<int, int> cnt1;
+        for (const auto& x : nums) {
+            ++cnt1[x];
+        }
+        unordered_map<int, int> cnt2;
+        for (const auto& [x, v] : cnt1) {
+            result += cnt2[x] * v + v * (v - 1) / 2;
+            unordered_set<int> lookup;
+            at_most(K, x, lookup);
+            for (const auto& x : lookup) {
+                if (!cnt1.count(x)) {
+                    continue;
+                }
+                cnt2[x] += v;
+            }
+        }
+        return result;
+    }
+};
