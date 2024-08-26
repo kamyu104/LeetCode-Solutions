6127bdd1bd7a54e686464b1c08f2fd2c9266de71
kamyu <kamyu104@users.noreply.github.com>
2024-08-26 12:08:18 +0800

    Create final-array-state-after-k-multiplication-operations-ii.cpp

---
+++ C++/final-array-state-after-k-multiplication-operations-ii.cpp
@@ -0,0 +1,213 @@
+// Time:  O(n + (n + logr) + nlog(logr) + nlogn) = O(nlogn), assumed log(x) takes O(1) time
+// Space: O(n)
+
+// sort, two pointers, sliding window, fast exponentiation
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        static const double EPS = 1e-15;
+        static const int MOD = 1e9 + 7;
+        const auto& powmod = [&](int a, int b) {
+            a %= MOD;
+            int64_t result = 1;
+            while (b) {
+                if (b & 1) {
+                    result = result * a % MOD;
+                }
+                a = int64_t(a) * a % MOD;
+                b >>= 1;
+            }
+            return result;
+        };
+
+        const auto& count = [](const auto& x, int target) {
+            return static_cast<int>(target - x + EPS);
+        };
+
+        if (multiplier == 1) {
+            return nums;
+        }
+        using P = pair<double, int>;
+        vector<P> vals;
+        for (int i = 0; i < size(nums); ++i) {
+            vals.emplace_back(log(nums[i]) / log(multiplier), i);
+        }
+        sort(begin(vals), end(vals));
+        int right = 1;
+        for (int left = 0; right <= static_cast<int>(vals.back().first) + 1; ++right) {
+            for (; left < size(vals) && count(vals[left].first, right) >= 1; ++left);
+            if (k - left < 0) {
+                --right;
+                break;
+            }
+            k -= left;
+        }
+        if (right == static_cast<int>(vals.back().first) + 2) {
+            --right;
+        }
+        vector<int64_t> nums2(cbegin(nums), cend(nums));
+        for (int idx = 0; idx < size(vals); ++idx) {
+            const auto& [x, i] = vals[idx];
+            const int c = count(x, right);
+            if (c <= 0) {
+                break;
+            }
+            nums2[i] *= pow(multiplier, c);
+        }
+        using P2 = pair<int64_t, int>;
+        vector<P2> vals2;
+        for (int i = 0; i < size(nums2); ++i) {
+            vals2.emplace_back(nums2[i], i);
+        }
+        sort(begin(vals2), end(vals2));
+        const int q =  k / size(nums), r = k % size(nums);
+        const int m = powmod(multiplier, q);
+        vector<int> result(size(nums));
+        for (int idx = 0; idx < size(vals2); ++idx) {
+            const auto& [x, i] = vals2[idx];
+            result[i] = ((x % MOD * m) % MOD * (idx < r ? multiplier : 1)) % MOD;
+        }
+        return result;
+    }
+};
+
+// Time:  O(n + min(n, k) * log(logr) + nlog(logr) + nlogn) = O(nlogr), assumed log(x) takes O(1) time
+// Space: O(n)
+// binary search, sort, fast exponentiation
+class Solution2 {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        static const double EPS = 1e-15;
+        static const int MOD = 1e9 + 7;
+        const auto& powmod = [&](int a, int b) {
+            a %= MOD;
+            int64_t result = 1;
+            while (b) {
+                if (b & 1) {
+                    result = result * a % MOD;
+                }
+                a = int64_t(a) * a % MOD;
+                b >>= 1;
+            }
+            return result;
+        };
+
+        const auto& binary_search_right = [](auto left, auto right, const auto& check) {
+            while (left <= right) {
+                const auto mid = left + (right - left) / 2;
+                if (!check(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return right;
+        };
+
+        const auto& count = [](const auto& x, int target) {
+            return static_cast<int>(target - x + EPS);
+        };
+
+        if (multiplier == 1) {
+            return nums;
+        }
+        using P = pair<double, int>;
+        vector<P> vals;
+        for (int i = 0; i < size(nums); ++i) {
+            vals.emplace_back(log(nums[i]) / log(multiplier), i);
+        }
+        sort(begin(vals), end(vals));
+        const auto& check = [&](const auto& target) {
+            int result = 0;
+            for (const auto& [x, i] : vals) {
+                const int c = count(x, target);
+                if (c <= 0) {
+                    break;
+                }
+                result += c;
+            }
+            return result <= k;
+        };
+
+        const int target = binary_search_right(1, static_cast<int>(vals.back().first) + 1, check);
+        vector<int64_t> nums2(cbegin(nums), cend(nums));
+        for (int idx = 0; idx < size(vals); ++idx) {
+            const auto& [x, i] = vals[idx];
+            const int c = count(x, target);
+            if (c <= 0) {
+                break;
+            }
+            k -= c;
+            nums2[i] *= pow(multiplier, c);
+        }
+        using P2 = pair<int64_t, int>;
+        vector<P2> vals2;
+        for (int i = 0; i < size(nums2); ++i) {
+            vals2.emplace_back(nums2[i], i);
+        }
+        sort(begin(vals2), end(vals2));
+        const int q =  k / size(nums), r = k % size(nums);
+        const int m = powmod(multiplier, q);
+        vector<int> result(size(nums));
+        for (int idx = 0; idx < size(vals2); ++idx) {
+            const auto& [x, i] = vals2[idx];
+            result[i] = ((x % MOD * m) % MOD * (idx < r ? multiplier : 1)) % MOD;
+        }
+        return result;
+    }
+};
+
+// Time:  O(min(nlogr, k) * logn + nlogn) = O(nlogn * logr)
+// Space: O(n)
+// heap, sort, fast exponentiation
+class Solution3 {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+        static const int MOD = 1e9 + 7;
+        const auto& powmod = [&](int a, int b) {
+            a %= MOD;
+            int64_t result = 1;
+            while (b) {
+                if (b & 1) {
+                    result = result * a % MOD;
+                }
+                a = int64_t(a) * a % MOD;
+                b >>= 1;
+            }
+            return result;
+        };
+
+        if (multiplier == 1) {
+            return nums;
+        }
+        using P = pair<int64_t, int>;
+        vector<P> vals;
+        for (int i = 0; i < size(nums); ++i) {
+            vals.emplace_back(nums[i], i);
+        }
+        priority_queue<P, vector<P>, greater<P>> min_heap(cbegin(vals), cend(vals));
+        const int mx = ranges::max(nums);
+        vector<int64_t> nums2(cbegin(nums), cend(nums));
+        for (; k; --k) {
+            const auto [x, i] = min_heap.top(); min_heap.pop();
+            if (x >= mx) {
+                break;
+            }
+            nums2[i] *= multiplier;
+            min_heap.emplace(nums2[i], i);
+        }
+        vals.clear();
+        for (int i = 0; i < size(nums2); ++i) {
+            vals.emplace_back(nums2[i], i);
+        }
+        sort(begin(vals), end(vals));
+        const int q =  k / size(nums), r = k % size(nums);
+        const int m = powmod(multiplier, q);
+        vector<int> result(size(nums));
+        for (int idx = 0; idx < size(vals); ++idx) {
+            const auto& [x, i] = vals[idx];
+            result[i] = ((x % MOD * m) % MOD * (idx < r ? multiplier : 1)) % MOD;
+        }
+        return result;
+    }
+};
