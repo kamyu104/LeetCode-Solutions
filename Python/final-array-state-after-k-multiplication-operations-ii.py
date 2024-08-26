4aa50833e29f6cd585aaee09811eca3593e38a61
kamyu <kamyu104@users.noreply.github.com>
2024-08-26 10:41:21 +0800

    Update final-array-state-after-k-multiplication-operations-ii.py

--- Python/final-array-state-after-k-multiplication-operations-ii.py
+++ Python/final-array-state-after-k-multiplication-operations-ii.py
@@ -1,4 +1,4 @@
-# Time:  O(nlogr + n + logr + nlogn) = O(nlogr)
+# Time:  O(n + (n + logr) + nlogn) = O(nlogn), assumed log(x) takes O(1) time
 # Space: O(n)
 
 import math
@@ -42,7 +42,7 @@ class Solution(object):
         return result
 
 
-# Time:  O(nlogr + min(n, k) * log(logr) + nlogn) = O(nlogr)
+# Time:  O(n + min(n, k) * log(logr) + nlogn) = O(nlogr), assumed log(x) takes O(1) time
 # Space: O(n)
 import math
 
