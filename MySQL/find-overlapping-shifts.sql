ccded3ab185c33dab067c30db213ee1fbc57494b
kamyu <kamyu104@users.noreply.github.com>
2024-09-02 08:17:26 +0800

    Update find-overlapping-shifts.sql

--- MySQL/find-overlapping-shifts.sql
+++ MySQL/find-overlapping-shifts.sql
@@ -22,18 +22,14 @@ WITH events_cte AS (
            CASE WHEN @event_count = 0 THEN event_time
                 ELSE @start_time END AS end_time
     FROM events_cte, (SELECT @event_count := 0, @start_time := 0) init
-), result_cte AS (
-    SELECT employee_id,
-       SUM(event_count) AS overlapping_shifts
-    FROM line_sweep_cte
-    WHERE event_type = -1
-    GROUP BY 1
-    ORDER BY NULL
 )
 
-SELECT *
-FROM result_cte
-WHERE overlapping_shifts != 0
+SELECT employee_id,
+    SUM(event_count) AS overlapping_shifts
+FROM line_sweep_cte
+WHERE event_type = -1
+GROUP BY 1
+HAVING overlapping_shifts != 0
 ORDER BY 1;
 
 # Time:  O(n^2)
