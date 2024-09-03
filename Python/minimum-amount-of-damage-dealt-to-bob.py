8dbd9d582ca9a5cd259e382dd70f11e9994bc847
kamyu <kamyu104@users.noreply.github.com>
2024-09-03 14:42:54 +0800

    Create minimum-amount-of-damage-dealt-to-bob.py

---
+++ Python/minimum-amount-of-damage-dealt-to-bob.py
@@ -0,0 +1,22 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+# sort, greedy
+class Solution(object):
+    def minDamage(self, power, damage, health):
+        """
+        :type power: int
+        :type damage: List[int]
+        :type health: List[int]
+        :rtype: int
+        """
+        def ceil_divide(a, b):
+            return (a+b-1)//b
+        
+        idxs = range(len(health))
+        idxs.sort(key=lambda i: 1.0*ceil_divide(health[i], power)/damage[i])
+        result = t = 0
+        for i in idxs:
+            t += ceil_divide(health[i], power)
+            result += t*damage[i]
+        return result
