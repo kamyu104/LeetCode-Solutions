688cc3d45fbed682a42b578d86155ce9a9638a2b
kamyu <kamyu104@users.noreply.github.com>
2024-09-03 14:03:52 +0800

    Create find-the-count-of-good-integers.py

---
+++ Python/find-the-count-of-good-integers.py
@@ -0,0 +1,48 @@
+# Time:  O(10^(n/2))
+# Space: O(n + nHr(10, n/2))
+
+# combinatorics
+class Solution(object):
+    def countGoodIntegers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def reverse(x, n):
+            if n%2:
+                x //= 10
+            result = 0
+            while x:
+                result = result*10+x%10
+                x //= 10
+            return result
+
+        def palindrome(x, n):
+            return x*(10**(n//2))+reverse(x, n)
+
+        def count(x):
+            cnt = [0]*10
+            while x:
+                cnt[x%10] += 1
+                x //= 10
+            return cnt
+
+        fact = [1]*(n+1)
+        for i in xrange(len(fact)-1):
+            fact[i+1] = fact[i]*(i+1)
+        l = (n+1)//2
+        result = 0
+        lookup = set()
+        for d in xrange(10**(l-1), 10**l):
+            x = palindrome(d, n)
+            cnt = count(x)
+            key = tuple(enumerate(cnt))
+            if x%k or key in lookup:
+                continue
+            lookup.add(key)
+            total = (n-cnt[0])*fact[n-1]
+            for c in cnt:
+                total //= fact[c]
+            result += total
+        return result
