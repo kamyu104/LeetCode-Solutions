3e089bd0a125c1d5be68f2ec899054e1db5574d6
kamyu <kamyu104@users.noreply.github.com>
2024-09-08 23:05:36 +0800

    Update maximum-number-of-moves-to-kill-all-pawns.cpp

--- C++/maximum-number-of-moves-to-kill-all-pawns.cpp
+++ C++/maximum-number-of-moves-to-kill-all-pawns.cpp
@@ -44,17 +44,17 @@ public:
         }
         dp.back().assign(size(positions), 0);
         for (int mask = (1 << size(positions)) - 1; mask >= 1; --mask) {
-            const auto& turn = __builtin_popcount(mask) & 1;
+            const auto& turn = (__builtin_popcount(mask) & 1) ^ 1;
             for (int i = 0; i < size(positions); ++i) {
-                if (mask & (1 << i)) {
+                if ((mask & (1 << i)) == 0) {
                     continue;
                 }
                 for (int j = 0; j < size(positions); ++j) {
                     if (j == i || (mask & (1 << j)) == 0) {
                         continue;
                     }
-                    dp[mask][j] = turn ? min(dp[mask][j], dp[mask ^ (1 << i)][i] + dist[i][j])
-                                       : max(dp[mask][j], dp[mask ^ (1 << i)][i] + dist[i][j]);
+                    dp[mask ^ (1 << i)][j] = turn ? min(dp[mask ^ (1 << i)][j], dp[mask][i] + dist[i][j])
+                                                  : max(dp[mask ^ (1 << i)][j], dp[mask][i] + dist[i][j]);
                 }
             }
         }
