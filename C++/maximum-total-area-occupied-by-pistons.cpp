a02edf7850012ab950a8aa670c406b5df9a21b7a
kamyu <kamyu104@users.noreply.github.com>
2024-09-08 10:24:28 +0800

    Update maximum-total-area-occupied-by-pistons.cpp

--- C++/maximum-total-area-occupied-by-pistons.cpp
+++ C++/maximum-total-area-occupied-by-pistons.cpp
@@ -1,27 +1,27 @@
 // Time:  O(h)
 // Space: O(h)
 
-// line sweep, difference array
+// line sweep, derence array
 class Solution {
 public:
     long long maxArea(int height, vector<int>& positions, string directions) {
-        vector<int> events(2 * height + 1);
+        vector<int> diff(2 * height + 1);
         for (int i = 0; i < size(positions); ++i) {
             if (directions[i] == 'U') {
-                --events[height - positions[i]];
-                ++events[(height - positions[i]) + height];
+                --diff[height - positions[i]];
+                ++diff[(height - positions[i]) + height];
             } else {
-                ++events[positions[i]];
-                --events[positions[i] + height];
+                ++diff[positions[i]];
+                --diff[positions[i] + height];
             }
         }
         int64_t total = accumulate(cbegin(positions), cend(positions), 0ll);
         int64_t result = total;
         int cnt = count(cbegin(directions), cend(directions), 'U');
-        for (int t = 1; t < size(events); ++t) {
+        for (int t = 1; t < size(diff); ++t) {
             total += -(static_cast<int32_t>(size(directions)) - cnt) + cnt;
             result = max(result, total);
-            cnt += events[t];
+            cnt += diff[t];
         }
         return result;
     }
@@ -29,30 +29,30 @@ public:
 
 // Time:  O(nlogn)
 // Space: O(n)
-// sort, line sweep, difference array
+// sort, line sweep, derence array
 class Solution2 {
 public:
     long long maxArea(int height, vector<int>& positions, string directions) {
-        unordered_map<int, int> events;
+        unordered_map<int, int> diff;
         for (int i = 0; i < size(positions); ++i) {
             if (directions[i] == 'U') {
-                --events[height - positions[i]];
-                ++events[(height - positions[i]) + height];
+                --diff[height - positions[i]];
+                ++diff[(height - positions[i]) + height];
             } else {
-                ++events[positions[i]];
-                --events[positions[i] + height];
+                ++diff[positions[i]];
+                --diff[positions[i] + height];
             }
         }
         int64_t total = accumulate(cbegin(positions), cend(positions), 0ll);
         int64_t result = total;
         int cnt = count(cbegin(directions), cend(directions), 'U');
         int prev = 0;
-        vector<pair<int, int>> sorted_events(cbegin(events), cend(events));
-        sort(begin(sorted_events), end(sorted_events));
-        for (const auto& [t, diff] : sorted_events) {
+        vector<pair<int, int>> sorted_diff(cbegin(diff), cend(diff));
+        sort(begin(sorted_diff), end(sorted_diff));
+        for (const auto& [t, d] : sorted_diff) {
             total += static_cast<int64_t>(t - prev) * (-(static_cast<int32_t>(size(directions)) - cnt) + cnt);
             result = max(result, total);
-            cnt += diff;
+            cnt += d;
             prev = t;
         }
         return result;
