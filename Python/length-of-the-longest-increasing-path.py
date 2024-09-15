34a3840c0b321a7135054c7dc1874807976e8944
kamyu <kamyu104@users.noreply.github.com>
2024-09-16 00:06:14 +0800

    Create length-of-the-longest-increasing-path.py

---
+++ Python/length-of-the-longest-increasing-path.py
@@ -0,0 +1,33 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+import bisect
+
+
+# binary search, longest increasing subsequence, lis
+class Solution(object):
+    def maxPathLength(self, coordinates, k):
+        """
+        :type coordinates: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def longest_increasing_subsequence(arr):
+            result = []
+            for x in arr:
+                i = bisect.bisect_left(result, x)
+                if i == len(result):
+                    result.append(x)
+                else:
+                    result[i] = x
+            return len(result)
+
+        target = coordinates[k]
+        coordinates.sort(key=lambda x: (x[0], -x[1]))
+        left, right = [], []
+        for x, y in coordinates:
+            if x < target[0] and y < target[1]:
+                left.append(y)
+            elif x > target[0] and y > target[1]:
+                right.append(y)
+        return longest_increasing_subsequence(left)+1+longest_increasing_subsequence(right)
