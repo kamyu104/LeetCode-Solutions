aff76a6420cff7a28aede31f2a9386ab85f4bff4
kamyu <kamyu104@users.noreply.github.com>
2024-09-15 19:54:12 +0800

    Create find-a-safe-walk-through-a-grid.cpp

---
+++ C++/find-a-safe-walk-through-a-grid.cpp
@@ -0,0 +1,41 @@
+// Time:  O(m * n)
+// Space: O(m * n)
+
+// 0-1 bfs solution
+class Solution {
+public:
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+        static const vector<pair<int, int>> directions = {{0, 1}, {0, -1},
+                                                          {1, 0}, {-1, 0}};
+        const pair<int, int> b = {0, 0}, t = {size(grid) - 1, size(grid[0]) - 1};
+        if (0 + grid[0][0] == health) {
+            return false;
+        }
+        deque<pair<pair<int, int>, int>> dq = {{b, grid[0][0]}};
+        unordered_set<int> lookup;
+        while (!empty(dq)) {
+            const auto [b, d] = dq.front(); dq.pop_front();
+            if (b == t) {
+                return true;
+            }
+            if (lookup.count(b.first * size(grid[0]) + b.second)) {
+                continue;
+            }
+            lookup.emplace(b.first * size(grid[0]) + b.second);
+            for (const auto& [dr, dc] : directions) {
+                const auto& nb = make_pair(b.first + dr, b.second + dc);
+                if (!(0 <= nb.first && nb.first < size(grid) &&
+                      0 <= nb.second && nb.second < size(grid[0]) &&
+                      !lookup.count(nb.first * size(grid[0]) + nb.second))) {
+                    continue;
+                }
+                if (!grid[nb.first][nb.second]) {
+                    dq.emplace_front(nb, d);
+                } else if (d + 1 < health) {
+                    dq.emplace_back(nb, d + 1);
+                }
+            }
+        }
+        return false;
+    }
+};
