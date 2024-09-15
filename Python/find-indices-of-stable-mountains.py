70aa35988485de274ff29fa4d108f8b17d5606a3
kamyu <kamyu104@users.noreply.github.com>
2024-09-15 19:03:18 +0800

    Create find-indices-of-stable-mountains.py

---
+++ Python/find-indices-of-stable-mountains.py
@@ -0,0 +1,12 @@
+# Time:  O(n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def stableMountains(self, height, threshold):
+        """
+        :type height: List[int]
+        :type threshold: int
+        :rtype: List[int]
+        """
+        return[i for i in xrange(1, len(height)) if height[i-1] > threshold]
