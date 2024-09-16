0a85c122361ef1ede6ffe223f4ab1e0c4a23ed27
kamyu <kamyu104@users.noreply.github.com>
2024-09-16 15:22:27 +0800

    Update minimum-number-of-valid-strings-to-form-target-i.py

--- Python/minimum-number-of-valid-strings-to-form-target-i.py
+++ Python/minimum-number-of-valid-strings-to-form-target-i.py
@@ -162,3 +162,55 @@ class Solution3(object):
                 return -1
             dp[i+1] = dp[(i-lookup[i])+1]+1
         return dp[-1]
+
+
+# Time:  O(w * l + n * l)
+# Space: O(n + t), t is the total size of trie
+# trie, dp
+class Solution4(object):
+    def minValidStrings(self, words, target):
+        """
+        :type words: List[str]
+        :type target: str
+        :rtype: int
+        """
+        class Trie(object):
+            def __init__(self):
+                self.__nodes = []
+                self.__new_node()
+            
+            def __new_node(self):
+                self.__nodes.append([-1]*26)
+                return len(self.__nodes)-1
+
+            def add(self, w):
+                curr = 0
+                for c in w:
+                    x = ord(c)-ord('a')
+                    if self.__nodes[curr][x] == -1:
+                        self.__nodes[curr][x] = self.__new_node()
+                    curr = self.__nodes[curr][x]
+            
+            def query(self, target, i):
+                curr = 0
+                for l in xrange(len(target)-i):
+                    x = ord(target[i+l])-ord('a')
+                    if self.__nodes[curr][x] == -1:
+                        return l
+                    curr = self.__nodes[curr][x]
+                return len(target)-i
+
+        trie = Trie()
+        for w in words:
+            trie.add(w)
+        lookup = [0]*len(target)
+        for i in xrange(len(target)):
+            l = trie.query(target, i)
+            for nl in xrange(1, l+1):
+                lookup[i+nl-1] = max(lookup[i+nl-1], nl)
+        dp = [0]*(len(target)+1)
+        for i in xrange(len(target)):
+            if not lookup[i]:
+                return -1
+            dp[i+1] = dp[(i-lookup[i])+1]+1
+        return dp[-1]
