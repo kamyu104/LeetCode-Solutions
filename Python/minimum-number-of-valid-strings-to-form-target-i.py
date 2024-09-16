f89e2e3e3a2e7a5b2da107a2ed653edad4c174a0
kamyu <kamyu104@users.noreply.github.com>
2024-09-16 12:16:58 +0800

    Update minimum-number-of-valid-strings-to-form-target-i.py

--- Python/minimum-number-of-valid-strings-to-form-target-i.py
+++ Python/minimum-number-of-valid-strings-to-form-target-i.py
@@ -1,7 +1,7 @@
 # Time:  O(n + w * l)
 # Space: O(n + w * l)
 
-# rolling hash, prefix sum, hash table, greedy
+# rolling hash, two pointers, sliding window, dp
 class Solution(object):
     def minValidStrings(self, words, target):
         """
@@ -10,28 +10,109 @@ class Solution(object):
         :rtype: int
         """
         MOD, P = 10**9+7, 131
-        def hash(l, r):
-            return (prefix[r+1]-prefix[l]*power[r-l+1])%MOD            
-        
         power = [1]
-        prefix = [0]
-        for x in target:
+        for _ in xrange(len(target)):
             power.append(power[-1]*P%MOD)
-            prefix.append((prefix[-1]*P+(ord(x)-ord('a')+1))%MOD)
         lookup = set()
         for w in words:
             h = 0
             for x in w:
                 h = (h*P+(ord(x)-ord('a')+1))%MOD
                 lookup.add(h)
-        result = r = 0
-        left = right = -1
-        while r != right:
-            left, right = right+1, r
-            result += 1
-            for l in xrange(left, right+1):
-                while hash(l, r) in lookup:
-                    if r == len(target)-1:
-                        return result
-                    r += 1
-        return -1
+        dp = [0]*(len(target)+1)
+        left = h = 0
+        for right in xrange(len(target)):
+            h = (h*P+(ord(target[right])-ord('a')+1))%MOD
+            while right-left+1 >= 1 and h not in lookup:
+                h = (h-(ord(target[left])-ord('a')+1)*power[(right-left+1)-1])%MOD
+                left += 1
+            if right-left+1 == 0:
+                return -1
+            dp[right+1] = dp[(right-(right-left+1))+1]+1
+        return dp[-1]
+
+
+
+# Time:  O(n + w * l)
+# Space: O(t), t is the total size of ac automata trie
+import collections
+
+
+class AhoNode(object):
+    def __init__(self):
+        self.children = collections.defaultdict(AhoNode)
+        # self.indices = []
+        self.suffix = None
+        # self.output = None
+        self.length = 0  # added
+
+
+class AhoTrie(object):
+
+    def step(self, letter):
+        while self.__node and letter not in self.__node.children:
+            self.__node = self.__node.suffix
+        self.__node = self.__node.children[letter] if self.__node else self.__root
+        return self.__get_ac_node_outputs(self.__node)
+    
+    def __init__(self, patterns):
+        self.__root = self.__create_ac_trie(patterns)
+        self.__node = self.__create_ac_suffix_and_output_links(self.__root)
+    
+    def __create_ac_trie(self, patterns):  # Time:  O(n), Space: O(t)
+        root = AhoNode()
+        for i, pattern in enumerate(patterns):
+            node = root
+            for l, c in enumerate(pattern, 1):  # modified
+                node = node.children[c]
+                node.length = l  # added
+            # node.indices.append(i)
+        return root
+
+    def __create_ac_suffix_and_output_links(self, root):  # Time:  O(n), Space: O(t)
+        queue = collections.deque()
+        for node in root.children.itervalues():
+            queue.append(node)
+            node.suffix = root
+
+        while queue:
+            node = queue.popleft()
+            for c, child in node.children.iteritems():
+                queue.append(child)
+                suffix = node.suffix
+                while suffix and c not in suffix.children:
+                    suffix = suffix.suffix
+                child.suffix = suffix.children[c] if suffix else root
+                # child.output = child.suffix if child.suffix.indices else child.suffix.output
+                
+        return root
+
+    def __get_ac_node_outputs(self, node):  # Time:  O(z)
+        return node.length  # modified
+        # result = []
+        # for i in node.indices:
+        #     result.append(i)
+        # output = node.output
+        # while output:
+        #     for i in output.indices:
+        #         result.append(i)
+        #     output = output.output
+        # return result
+
+
+# ac autometa trie
+class Solution2(object):
+    def minValidStrings(self, words, target):
+        """
+        :type words: List[str]
+        :type target: str
+        :rtype: int
+        """
+        trie = AhoTrie(words)
+        dp = [0]*(len(target)+1)
+        for i in xrange(len(target)):
+            l = trie.step(target[i])
+            if not l:
+                return -1
+            dp[i+1] = dp[(i-l)+1]+1
+        return dp[-1]
