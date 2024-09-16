a3dd32928d0f42d4dc36929d77394cea5db94765
kamyu <kamyu104@users.noreply.github.com>
2024-09-16 12:36:33 +0800

    Update minimum-number-of-valid-strings-to-form-target-i.cpp

--- C++/minimum-number-of-valid-strings-to-form-target-i.cpp
+++ C++/minimum-number-of-valid-strings-to-form-target-i.cpp
@@ -1,20 +1,14 @@
 // Time:  O(n + w * l)
 // Space: O(n + w * l)
 
-// rolling hash, prefix sum, hash table, greedy
+// rolling hash, two pointers, sliding window, dp
 class Solution {
 public:
     int minValidStrings(vector<string>& words, string target) {
         static const int MOD = 1e9 + 7, P = 131;
         vector<int64_t> power = {1};
-        vector<int64_t> prefix = {0};
-        const auto& hash = [&](int l, int r) {
-            return ((prefix[r+1] - prefix[l] * power[r - l + 1]) % MOD + MOD) % MOD;
-        };
-
-        for (const auto& x : target) {
+        for (int _ = 0; _ < size(target); ++_) {
             power.emplace_back(power.back() * P % MOD);
-            prefix.emplace_back((prefix.back() * P + (x - 'a' + 1)) % MOD);
         }
         unordered_set<int> lookup;
         for (const auto& w : words) {
@@ -24,20 +18,118 @@ public:
                 lookup.emplace(h);
             }
         }
-        int result = 0;
-        int left = -1, right = -1, r = 0;
-        while (r != right) {
-            left = right + 1;
-            right = r;
-            ++result;
-            for (int l = left; l <= right; ++l) {
-                while (lookup.count(hash(l, r))) {
-                    if (++r == size(target)) {
-                        return result;
-                    }
+        vector<int> dp(size(target) + 1);
+        for (int64_t right = 0, left = 0, h = 0; right < size(target); ++right) {
+            h = (h * P + (target[right] - 'a' + 1)) % MOD;
+            for (; right - left + 1 >= 1 && !lookup.count(h); ++left) {
+                h = ((h - (target[left] - 'a' + 1) * power[(right - left + 1) - 1]) % MOD + MOD) % MOD;
+            }
+            if (right - left + 1 == 0) {
+                return -1;
+            }
+            dp[right + 1] = dp[(right - (right - left + 1)) + 1] + 1;
+        }
+        return dp.back();
+    }
+};
+
+// Time:  O(n + w * l)
+// Space: O(t), t is the total size of ac automata trie
+struct AhoNode {
+    vector<AhoNode *> children;
+    AhoNode *suffix;
+    int length;
+    AhoNode() :
+        children(26, nullptr),
+        suffix(nullptr),
+        length(0) {}
+};
+
+class AhoTrie {
+public:
+    AhoTrie(const vector<string>& patterns) : root_(createACTrie(patterns)) {
+        node_ = createACSuffixAndOutputLinks(root_);
+    }
+
+    int step(char letter) {
+        while (node_ && !node_->children[letter - 'a']) {
+            node_ = node_->suffix;
+        }
+        node_ = node_ ? node_->children[letter - 'a'] : root_;
+        return getACNodeOutputs(node_);
+    }
+    
+    void reset() {
+        node_ = root_;
+    }
+
+private:
+    AhoNode *createACTrie(const vector<string>& patterns) {  // Time:  O(n), Space: O(t)
+        auto root = new AhoNode();
+        for (int i = 0; i < patterns.size(); ++i) {
+            auto node = root;
+            for (int l = 1; l <= size(patterns[i]); ++l) {
+                const auto c = patterns[i][l - 1];
+                if (!node->children[c - 'a']) {
+                    node->children[c - 'a'] = new AhoNode();
                 }
+                node = node->children[c - 'a'];
+                node->length = l;
             }
         }
-        return -1;
+        return root;
+    }
+    
+    AhoNode *createACSuffixAndOutputLinks(AhoNode *root) {  // Time:  O(n), Space: O(t)
+        queue<AhoNode *> q;
+        for (auto node : root->children) {
+            if (!node) {
+                continue;
+            }
+            q.emplace(node);
+            node->suffix = root;
+        }
+                
+        while (!q.empty()) {
+            auto node = q.front(); q.pop();
+            for (int c = 0; c < node->children.size(); ++c) {
+                if (!node->children[c]) {
+                    continue;
+                }
+                auto child = node->children[c];
+                q.emplace(child);
+                auto suffix = node->suffix;
+                while (suffix && !suffix->children[c]) {
+                    suffix = suffix->suffix;
+                }
+                child->suffix = suffix ? suffix->children[c] : root;
+            }
+        }
+        
+        return root;
+    }
+    
+    int getACNodeOutputs(AhoNode *node) {  // modified
+        return node->length;
+    }
+    
+    AhoNode * const root_;
+    AhoNode *node_;
+};
+
+// ac autometa trie
+class Solution2 {
+public:
+    int minValidStrings(vector<string>& words, string target) {
+        auto trie = AhoTrie(words);
+        vector<int> dp(size(target) + 1);
+        for (int i = 0; i < size(target); ++i) {
+            const int l = trie.step(target[i]);
+            if (!l) {
+                return -1;
+            }
+            dp[i + 1] = dp[(i - l) + 1] + 1;
+        }
+        return dp.back();
     }
 };
