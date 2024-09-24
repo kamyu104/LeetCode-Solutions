96387cd6224312a374e4f8637f8474369391e452
kamyu <kamyu104@users.noreply.github.com>
2024-09-24 14:52:31 +0800

    Update report-spam-message.cpp

--- C++/report-spam-message.cpp
+++ C++/report-spam-message.cpp
@@ -5,16 +5,18 @@
 class Solution {
 public:
     bool reportSpam(vector<string>& message, vector<string>& bannedWords) {
+        static const int THRESHOLD = 2;
+
         unordered_set<string> lookup(cbegin(bannedWords), cend(bannedWords));
         int cnt = 0;
         for (const auto& m : message) {
             if (!lookup.count(m)) {
                 continue;
             }
-            if (++cnt == 2) {
+            if (++cnt == THRESHOLD) {
                 break;
             }
         }
-        return cnt == 2;
+        return cnt == THRESHOLD;
     }
 };
