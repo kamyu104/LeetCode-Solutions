89aa31872769254fbc856a0591be67bf2853e5ce
kamyu <kamyu104@users.noreply.github.com>
2024-09-30 01:36:19 +0800

    Create find-the-k-th-character-in-string-game-ii.cpp

---
+++ C++/find-the-k-th-character-in-string-game-ii.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+// bitmasks
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& operations) {
+        const auto& bit_length = [](int64_t x) {
+            return (x ? std::__lg(x) : -1) + 1;
+        };
+
+        int result = 0;
+        --k;
+        for (int i = 0; i < min(static_cast<int64_t>(size(operations)), bit_length(k)); ++i) {
+            if (k & (1ll << i)) {
+                result = (result + operations[i]) % 26;
+            }
+        }
+        return 'a' + result;
+    }
+};
