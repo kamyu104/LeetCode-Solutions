478efacc72263020de2911c7da89a21d8b033b18
kamyu <kamyu104@users.noreply.github.com>
2024-09-30 01:19:11 +0800

    Create find-the-k-th-character-in-string-game-i.py

---
+++ Python/find-the-k-th-character-in-string-game-i.py
@@ -0,0 +1,14 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def kthCharacter(self, k):
+        """
+        :type k: int
+        :rtype: str
+        """
+        def popcount(x):
+            return bin(x)[2:].count('1')
+
+        return chr(ord('a')+popcount(k-1))
