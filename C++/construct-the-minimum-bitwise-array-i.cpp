624a03e3fd2df2a961b57b09e376f9deee4a6223
kamyu <kamyu104@users.noreply.github.com>
2024-10-14 02:35:37 +0800

    Update construct-the-minimum-bitwise-array-i.cpp

--- C++/construct-the-minimum-bitwise-array-i.cpp
+++ C++/construct-the-minimum-bitwise-array-i.cpp
@@ -7,7 +7,7 @@ public:
     vector<int> minBitwiseArray(vector<int>& nums) {
         vector<int> result(size(nums), -1);
         for (int i = 0; i < size(nums); ++i) {
-            if (nums[i] % 2) {
+            if (nums[i] & 1) {
                 result[i] = nums[i] - (((nums[i] + 1) & (~nums[i])) >> 1);
             }
         }
