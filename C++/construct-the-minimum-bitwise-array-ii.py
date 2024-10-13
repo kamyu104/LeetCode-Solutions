eee214898d0de2fe49eef6b50af1054c41823b89
kamyu <kamyu104@users.noreply.github.com>
2024-10-14 02:33:36 +0800

    Create construct-the-minimum-bitwise-array-ii.py

---
+++ C++/construct-the-minimum-bitwise-array-ii.py
@@ -0,0 +1,16 @@
+// Time:  O(n)
+// Space: O(1)
+
+// bit manipulation
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> result(size(nums), -1);
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] % 2) {
+                result[i] = nums[i] - (((nums[i] + 1) & (~nums[i])) >> 1);
+            }
+        }
+        return result;
+    }
+};
