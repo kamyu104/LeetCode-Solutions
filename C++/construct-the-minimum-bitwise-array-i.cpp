77b593d04b5d6c670d2294bf79d7bfe7dab17f29
kamyu <kamyu104@users.noreply.github.com>
2024-10-14 02:33:08 +0800

    Create construct-the-minimum-bitwise-array-i.cpp

---
+++ C++/construct-the-minimum-bitwise-array-i.cpp
@@ -0,0 +1,35 @@
+// Time:  O(n)
+// Space: O(1)
+
+// bit manipulation
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> result(size(nums), -1);
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] % 2) {
+                result[i] = nums[i] - (((nums[i] + 1) & (~nums[i])) >> 1);
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n * r)
+// Space: O(1)
+// brute force
+class Solution2 {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        vector<int> result(size(nums), -1);
+        for (int i = 0; i < size(nums); ++i) {
+            for (int j = 0; j < nums[i]; ++j) {
+                if ((j | (j + 1)) == nums[i]) {
+                    result[i] = j;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
