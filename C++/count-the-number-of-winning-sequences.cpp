7b590e42af0898e4a00b343e5840498a1f0df66b
kamyu <kamyu104@users.noreply.github.com>
2024-10-15 02:07:11 +0800

    Create count-the-number-of-winning-sequences.cpp

---
+++ C++/count-the-number-of-winning-sequences.cpp
@@ -0,0 +1,42 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+// dp
+class Solution {
+public:
+    int countWinningSequences(string s) {
+        static const int MOD = 1e9 + 7;
+        static const unordered_map<char, int> lookup = {{'F', 0}, {'W', 1}, {'E', 2}};
+
+        vector<unordered_map<int, int>> dp(3);
+        for (int i = 0; i < size(s); ++i) {
+            vector<unordered_map<int, int>> new_dp(3);
+            const auto x = lookup.at(s[i]);
+            for (int j = 0; j < 3; ++j) {
+                const int diff = ((j - x + 1) % 3 + 3) % 3 - 1;
+                if (i == 0) {
+                    new_dp[j][diff] = 1;
+                    continue;
+                }
+                for (int k = 0; k < 3; ++k) {
+                    if (k == j) {
+                        continue;
+                    }
+                    for (const auto& [v, c] : dp[k]) {
+                        new_dp[j][v + diff] = (new_dp[j][v + diff] + c) % MOD;
+                    }
+                }
+            }
+            dp = move(new_dp);
+        }
+        int result = 0;
+        for (int j = 0; j < 3; ++j) {
+            for (const auto& [v, c] : dp[j]) {
+                if (v >= 1) {
+                    result = (result + c) % MOD;
+                }
+            }
+        }
+        return result;
+    }
+};
