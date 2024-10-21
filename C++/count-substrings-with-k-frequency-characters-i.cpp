493cb618069e37207ef7ece826347c40716a41f0
kamyu <kamyu104@users.noreply.github.com>
2024-10-21 18:12:43 +0800

    Create count-substrings-with-k-frequency-characters-i.cpp

---
+++ C++/count-substrings-with-k-frequency-characters-i.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n + 26)
+// Space: O(26)
+
+// freq table, two pointers, sliding window
+class Solution {
+public:
+    int numberOfSubstrings(string s, int k) {
+        const auto& count = [&]() {
+            int result = 0;
+            vector<int> cnt(26);
+            for (int right = 0, left = 0; right < size(s); ++right) {
+                ++cnt[s[right] - 'a'];
+                for (; cnt[s[right] - 'a'] >= k; ++left) {
+                    --cnt[s[left] - 'a'];
+                }
+                result += right - left + 1;
+            }
+            return result;
+        };
+
+        return (size(s) + 1) * size(s) / 2 - count();
+    }
+};
