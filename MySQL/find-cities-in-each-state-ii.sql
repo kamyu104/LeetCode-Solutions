3677c3c7fad5aae1483fe2ae91d24c5ee779f1ac
kamyu <kamyu104@users.noreply.github.com>
2024-10-21 19:01:58 +0800

    Create find-cities-in-each-state-ii.sql

---
+++ MySQL/find-cities-in-each-state-ii.sql
@@ -0,0 +1,10 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+SELECT state,
+       GROUP_CONCAT(city ORDER BY city ASC separator ', ') AS cities,
+       SUM(IF(LEFT(city, 1) = LEFT(state, 1), 1, 0)) AS matching_letter_count
+FROM cities
+GROUP BY 1
+HAVING COUNT(city) >= 3 AND matching_letter_count >= 1
+ORDER BY 3 DESC, 1 ASC;
