f4c9162ae299d5e8016c709e7164532c27987e02
kamyu <kamyu104@users.noreply.github.com>
2024-11-01 00:17:14 +0800

    Create find-the-number-of-k-even-arrays.py

---
+++ Python/find-the-number-of-k-even-arrays.py
@@ -0,0 +1,41 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# stars and bars, combinatorics
+class Solution(object):
+    def countOfArrays(self, n, m, k):
+        """
+        :type n: int
+        :type m: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        fact, inv, inv_fact = [[1]*2 for _ in xrange(3)]
+        def nCr(n, k):
+            while len(inv) <= n:  # lazy initialization
+                fact.append(fact[-1]*len(inv) % MOD)
+                inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+                inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
+            return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
+
+        def nHr(n, k):
+            return nCr(n+k-1, k)
+
+        lookup = collections.defaultdict(list)
+        def pow(a, b):
+            while len(lookup[a]) <= b:
+                lookup[a].append((lookup[a][-1]*a)%MOD if lookup[a] else 1)
+            return lookup[a][b]
+
+        MOD = 10**9+7
+        odd, even = (m+1)//2, m//2
+        result = 0
+        if k == 0:
+            result += pow(odd, n)
+        for x in xrange(1, (n+1-k)//2+1):  # since (n-(k+x))-((x+1)-2) >= 0, so x <= (n+1-k)/2
+            result = (result+(nHr(x, (k+x)-x)*nHr(x+1, (n-(k+x))-((x+1)-2))*pow(even, k+x)*pow(odd, n-(k+x))%MOD))%MOD
+        return result
