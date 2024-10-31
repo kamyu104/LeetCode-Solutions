7109952851033ca789cb4077aa5fdfd1876ca389
kamyu <kamyu104@users.noreply.github.com>
2024-11-01 04:19:12 +0800

    Update find-the-number-of-k-even-arrays.py

--- Python/find-the-number-of-k-even-arrays.py
+++ Python/find-the-number-of-k-even-arrays.py
@@ -39,3 +39,24 @@ class Solution(object):
         for x in xrange(1, (n+1-k)//2+1):  # since (n-(k+x))-((x+1)-2) >= 0, so x <= (n+1-k)/2
             result = (result+(nHr(x, (k+x)-x)*nHr(x+1, (n-(k+x))-((x+1)-2))*pow(even, k+x)*pow(odd, n-(k+x))%MOD))%MOD
         return result
+
+
+# Time:  O(n * k)
+# Space: O(k)
+# dp
+class Solution2(object):
+    def countOfArrays(self, n, m, k):
+        """
+        :type n: int
+        :type m: int
+        :type k: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        odd, even = (m+1)//2, m//2
+        dp = [[0]*(k+1) for _ in xrange(2)]
+        dp[0][0], dp[1][0] = even, odd
+        for _ in xrange(n-1):
+            for i in reversed(xrange(k+1)):
+                dp[0][i], dp[1][i] = (((dp[0][i-1] if i-1 >= 0 else 0)+dp[1][i])*even)%MOD, ((dp[0][i]+dp[1][i])*odd)%MOD
+        return (dp[0][k]+dp[1][k])%MOD
