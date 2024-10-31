1a0e736fb1c967184e4447a4c8d489833a5cdf17
kamyu <kamyu104@users.noreply.github.com>
2024-10-31 18:20:56 +0800

    Update total-characters-in-string-after-transformations-i.cpp

--- C++/total-characters-in-string-after-transformations-i.cpp
+++ C++/total-characters-in-string-after-transformations-i.cpp
@@ -8,8 +8,8 @@ public:
         static const int MOD = 1e9 + 7;
 
         vector<int> dp(26, 1);
-        char c = ranges::max(s);
-        for (int i = 26; i <= c - 'a' + t; ++i) {
+        const auto mx = ranges::max(s);
+        for (int i = 26; i <= (mx - 'a') + t; ++i) {
             dp[i % 26] = (dp[(i - 26) % 26] + dp[((i - 26) + 1) % 26]) % MOD;
         }
         int result = 0;
