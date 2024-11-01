5a9ae7a8b69d4dddc7609c7ab92db978d2a4e8d0
kamyu <kamyu104@users.noreply.github.com>
2024-11-01 20:59:03 +0800

    Update total-characters-in-string-after-transformations-i.cpp

--- C++/total-characters-in-string-after-transformations-i.cpp
+++ C++/total-characters-in-string-after-transformations-i.cpp
@@ -28,10 +28,31 @@ public:
     }
 };
 
+// Time:  O(n + t + 26)
+// Space: O(26)
+// dp
+class Solution2 {
+public:
+    int lengthAfterTransformations(string s, int t) {
+        static const int MOD = 1e9 + 7;
+
+        vector<int> dp(26, 1);
+        const auto mx = ranges::max(s);
+        for (int i = 26; i <= (mx - 'a') + t; ++i) {
+            dp[i % 26] = (dp[(i - 26) % 26] + dp[((i - 26) + 1) % 26]) % MOD;
+        }
+        int result = 0;
+        for (const auto& x : s) {
+            result = (result + dp[((x - 'a') + t) % 26]) % MOD;
+        }
+        return result;
+    }
+};
+
 // Time:  O(n + 26^3 * logt)
 // Space: O(26^2)
 // matrix fast exponentiation
-class Solution2 {
+class Solution3 {
 public:
     int lengthAfterTransformations(string s, int t) {
         vector<int> cnt(26);
@@ -85,27 +106,6 @@ private:
     }
 
     static const int MOD = 1e9 + 7;
-};
-
-// Time:  O(n + t + 26)
-// Space: O(26)
-// dp
-class Solution3 {
-public:
-    int lengthAfterTransformations(string s, int t) {
-        static const int MOD = 1e9 + 7;
-
-        vector<int> dp(26, 1);
-        const auto mx = ranges::max(s);
-        for (int i = 26; i <= (mx - 'a') + t; ++i) {
-            dp[i % 26] = (dp[(i - 26) % 26] + dp[((i - 26) + 1) % 26]) % MOD;
-        }
-        int result = 0;
-        for (const auto& x : s) {
-            result = (result + dp[((x - 'a') + t) % 26]) % MOD;
-        }
-        return result;
-    }
 };
 
 // Time:  O(n + t * 26)
