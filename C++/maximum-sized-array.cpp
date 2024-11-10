93f130b5d9c810a8a8de124e8a4d49c74d568546
kamyu <kamyu104@users.noreply.github.com>
2024-11-11 03:45:38 +0800

    Update maximum-sized-array.cpp

--- C++/maximum-sized-array.cpp
+++ C++/maximum-sized-array.cpp
@@ -1,4 +1,4 @@
-// Time:  precompute: O(max_s^(2/5))
+// Time:  precompute: O(max_s^(1/5) * log(max_s))
 //        runtime:    O(log(max_s))
 // Space: O(max_s^(1/5))
 
@@ -6,14 +6,18 @@
 int i = 1;
 int64_t area = 0;
 vector<int64_t> vol = {0};
-
 class Solution {
 public:
     int maxSizedArray(long long s) {
+        const auto& bit_length = [](int x) {
+            return (x ? std::__lg(x) : -1) + 1;
+        };
+
         for (; vol.back() <= s; ++i) {
+            const int l = bit_length(i - 1);
             int line = 0;
-            for (int j = 0; j < i; ++j) {
-                line += (i - 1) | j;
+            for (int bit = 0; bit < l; ++bit) {
+                line += ((i - 1) & (1 << bit) ? i : ((i - 1) >> (bit + 1)) * (1 << bit)) * (1 << bit);
             }
             area += 2 * line - ((i - 1) | (i - 1));
             vol.emplace_back(((0 + (i - 1)) * i / 2) * area);
