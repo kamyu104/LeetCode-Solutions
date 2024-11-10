633ad27bfa04e4fa7cdd8e3559ae35100d6b6b75
kamyu <kamyu104@users.noreply.github.com>
2024-11-11 01:58:15 +0800

    Create maximum-sized-array.py

---
+++ Python/maximum-sized-array.py
@@ -0,0 +1,23 @@
+# Time:  precompute: O(max_s^(1/5))
+#        runtime:    O(log(max_s))
+# Space: O(max_s^(1/5))
+
+import bisect 
+
+
+# precompute, binary search
+MAX_S = 10**15
+i = 1
+area = 0
+vol = [0]
+while vol[-1] <= MAX_S:
+    area += 2*sum((i-1)|j for j in xrange(i))-((i-1)|(i-1))
+    vol.append(((0+(i-1))*i//2)*area)
+    i += 1
+class Solution(object):
+    def maxSizedArray(self, s):
+        """
+        :type s: int
+        :rtype: int
+        """
+        return bisect.bisect_right(vol, s)-1
