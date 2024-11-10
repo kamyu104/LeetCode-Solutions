9c32ef6e80c5cde97df9586693d3632e872d5279
kamyu <kamyu104@users.noreply.github.com>
2024-11-11 03:58:04 +0800

    Update maximum-sized-array.py

--- Python/maximum-sized-array.py
+++ Python/maximum-sized-array.py
@@ -6,18 +6,18 @@ import bisect
 
 
 # precompute, binary search
-MAX_S = 10**15  
 i = 1
 area = 0
 vol = [0]
-while vol[-1] <= MAX_S:
-    area += 2*sum((i if (i-1)&(1<<bit) else ((i-1)>>(bit+1))*(1<<bit))*(1<<bit) for bit in xrange((i-1).bit_length()))-((i-1)|(i-1))
-    vol.append(((0+(i-1))*i//2)*area)
-    i += 1
 class Solution(object):
     def maxSizedArray(self, s):
         """
         :type s: int
         :rtype: int
         """
+        global i, area
+        while vol[-1] <= s:
+            area += 2*sum((i if (i-1)&(1<<bit) else ((i-1)>>(bit+1))*(1<<bit))*(1<<bit) for bit in xrange((i-1).bit_length()))-((i-1)|(i-1))
+            vol.append(((0+(i-1))*i//2)*area)
+            i += 1
         return bisect.bisect_right(vol, s)-1
