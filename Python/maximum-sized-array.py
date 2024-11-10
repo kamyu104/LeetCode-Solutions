c1625344557c04c8851c4f3de7113878498fb6a8
kamyu <kamyu104@users.noreply.github.com>
2024-11-11 04:23:09 +0800

    Update maximum-sized-array.py

--- Python/maximum-sized-array.py
+++ Python/maximum-sized-array.py
@@ -16,7 +16,7 @@ class Solution(object):
         """
         global i, area
         while vol[-1] <= s:
-            area += 2*sum((i if (i-1)&(1<<bit) else ((i-1)>>(bit+1))*(1<<bit))*(1<<bit) for bit in xrange((i-1).bit_length()))-((i-1)|(i-1))
+            area += 2*((i-1)*i+sum((((i-1)>>(bit+1))*(1<<bit))*(1<<bit) for bit in xrange((i-1).bit_length()) if not (i-1)&(1<<bit)))-((i-1)|(i-1))
             vol.append(((0+(i-1))*i//2)*area)
             i += 1
         return bisect.bisect_right(vol, s)-1
