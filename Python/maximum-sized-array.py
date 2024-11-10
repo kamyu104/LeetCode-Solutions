bb191ccb4657c4c7eda100b8146f7953b42faed8
kamyu <kamyu104@users.noreply.github.com>
2024-11-11 03:40:49 +0800

    Update maximum-sized-array.py

--- Python/maximum-sized-array.py
+++ Python/maximum-sized-array.py
@@ -1,4 +1,4 @@
-# Time:  precompute: O(max_s^(2/5))
+# Time:  precompute: O(max_s^(1/5) * log(max_s))
 #        runtime:    O(log(max_s))
 # Space: O(max_s^(1/5))
 
@@ -6,12 +6,12 @@ import bisect
 
 
 # precompute, binary search
-MAX_S = 10**15
+MAX_S = 10**15  
 i = 1
 area = 0
 vol = [0]
 while vol[-1] <= MAX_S:
-    area += 2*sum((i-1)|j for j in xrange(i))-((i-1)|(i-1))
+    area += 2*sum((i if (i-1)&(1<<bit) else ((i-1)>>(bit+1))*(1<<bit))*(1<<bit) for bit in xrange((i-1).bit_length()))-((i-1)|(i-1))
     vol.append(((0+(i-1))*i//2)*area)
     i += 1
 class Solution(object):
