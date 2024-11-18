1a2d81c73087cf5d25e8f8de9837258a2a71835e
kamyu <kamyu104@users.noreply.github.com>
2024-11-18 16:31:32 +0800

    Create zero-array-transformation-i.cpp

---
+++ C++/zero-array-transformation-i.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(n)
+
+// line sweep
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> events(size(nums) + 1);
+        for (const auto& q : queries) {
+            ++events[q[0]];
+            --events[q[1] + 1];
+        }
+        for (int i = 0, curr = 0; i < size(nums); ++i) {
+            curr += events[i];
+            if (nums[i] > curr) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
