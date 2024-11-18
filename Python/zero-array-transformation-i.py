36a7805453fefa22ab3c63603994351562c75e03
kamyu <kamyu104@users.noreply.github.com>
2024-11-18 16:32:00 +0800

    Create zero-array-transformation-i.py

---
+++ Python/zero-array-transformation-i.py
@@ -0,0 +1,21 @@
+# Time:  O(n)
+# Space: O(n)
+
+# line sweep
+class Solution(object):
+    def isZeroArray(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[List[int]]
+        :rtype: bool
+        """
+        events = [0]*(len(nums)+1)
+        for l, r in queries:
+            events[l] += 1
+            events[r+1] -= 1
+        curr = 0
+        for i in xrange(len(nums)):
+            curr += events[i]
+            if nums[i] > curr:
+                return False
+        return True
