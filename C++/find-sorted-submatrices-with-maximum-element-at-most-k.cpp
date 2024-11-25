7bc589be4263597c89c8e208ef9a14cf78569f38
kamyu <kamyu104@users.noreply.github.com>
2024-11-25 20:17:24 +0800

    Create find-sorted-submatrices-with-maximum-element-at-most-k.cpp

---
+++ C++/find-sorted-submatrices-with-maximum-element-at-most-k.cpp
@@ -0,0 +1,39 @@
+// Time:  O(m * n)
+// Space: O(m)
+
+// mono stack, dp
+class Solution {
+public:
+    long long countSubmatrices(vector<vector<int>>& grid, int k) {
+        const auto& count = [](const auto& heights) {
+            vector<int> dp(size(heights));
+            vector<int> stk;
+            for (int i = 0; i < size(heights); ++i) {
+                while (!empty(stk) && heights[stk.back()] >= heights[i]) {
+                    stk.pop_back();
+                }
+                dp[i] = !empty(stk)
+                    ? dp[stk.back()] + heights[i] * (i - stk.back())
+                    : heights[i] * (i - (-1));
+                stk.emplace_back(i);
+            }
+            return accumulate(cbegin(dp), cend(dp), 0);
+        };
+    
+        int64_t result = 0;
+        vector<int> heights(size(grid));
+        for (int j = size(grid[0]) - 1; j >= 0; --j) {
+            for (int i = 0; i < size(grid); ++i) {
+                if (grid[i][j] > k) {
+                    heights[i] = 0;
+                } else if (j + 1 < size(grid[0]) && grid[i][j] >= grid[i][j + 1]) {
+                    ++heights[i];
+                } else {
+                    heights[i] = 1;
+                }
+            }
+            result += count(heights);
+        }
+        return result;
+    }
+};
