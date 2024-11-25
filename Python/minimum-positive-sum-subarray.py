f75bff88e2bf0197f135548b00f2f59b34f0bddc
kamyu <kamyu104@users.noreply.github.com>
2024-11-26 02:41:07 +0800

    Create minimum-positive-sum-subarray.py

---
+++ Python/minimum-positive-sum-subarray.py
@@ -0,0 +1,30 @@
+# Time:  O(nlogn)
+# Space: O(n)
+
+from sortedcontainers import SortedList
+
+
+# prefix sum, two pointers, sliding window, bst, binary search
+class Solution(object):
+    def minimumSumSubarray(self, nums, l, r):
+        """
+        :type nums: List[int]
+        :type l: int
+        :type r: int
+        :rtype: int
+        """
+        INF = float("inf")
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]+nums[i]
+        result = INF
+        sl = SortedList()
+        for i in xrange(len(nums)):
+            if i-l+1 >= 0:
+                sl.add(prefix[i-l+1])
+            if i-r >= 0:
+                sl.remove(prefix[i-r])
+            idx = sl.bisect_left(prefix[i+1])-1
+            if idx >= 0:
+                result = min(result, prefix[i+1]-sl[idx])
+        return result if result != INF else -1
