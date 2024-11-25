55449898d603559e1d2e016a153cf33e97983a6e
kamyu <kamyu104@users.noreply.github.com>
2024-11-25 20:18:03 +0800

    Create find-sorted-submatrices-with-maximum-element-at-most-k.py

---
+++ Python/find-sorted-submatrices-with-maximum-element-at-most-k.py
@@ -0,0 +1,27 @@
+# Time:  O(m * n)
+# Space: O(m)
+
+# mono stack, dp
+class Solution(object):
+    def countSubmatrices(self, grid, k):
+        """
+        :type grid: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def count(heights):
+            dp, stk = [0]*len(heights), []
+            for i in xrange(len(heights)):
+                while stk and heights[stk[-1]] >= heights[i]:
+                    stk.pop()
+                dp[i] = dp[stk[-1]] + heights[i]*(i-stk[-1]) if stk else heights[i]*(i-(-1))
+                stk.append(i)
+            return sum(dp)
+
+        result = 0
+        heights = [0]*len(grid)
+        for j in reversed(range(len(grid[0]))):
+            for i in xrange(len(grid)):
+                heights[i] = 0 if grid[i][j] > k else heights[i]+1 if j+1 < len(grid[0]) and grid[i][j] >= grid[i][j+1] else 1
+            result += count(heights)
+        return result
