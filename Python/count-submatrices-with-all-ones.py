add1800ab201acafae010a92e8da0062aad16ea9
kamyu <kamyu104@users.noreply.github.com>
2024-11-26 03:43:22 +0800

    Update count-submatrices-with-all-ones.py

--- Python/count-submatrices-with-all-ones.py
+++ Python/count-submatrices-with-all-ones.py
@@ -1,7 +1,38 @@
 # Time:  O(m * n)
 # Space: O(n)
 
+# mono stack
 class Solution(object):
+    def numSubmat(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: int
+        """
+        def count(heights):
+            result = curr = 0
+            stk = []
+            for i in xrange(len(heights)):
+                while stk and heights[stk[-1]] >= heights[i]:
+                    j = stk.pop()
+                    curr -= (heights[j]-heights[i])*(j-(stk[-1] if stk else -1))
+                stk.append(i)
+                curr += heights[i]
+                result += curr
+            return result
+
+        result = 0
+        heights = [0]*len(mat[0])
+        for i in xrange(len(mat)):
+            for j in xrange(len(mat[0])):
+                heights[j] = heights[j]+1 if mat[i][j] == 1 else 0
+            result += count(heights)
+        return result
+
+
+# Time:  O(m * n)
+# Space: O(n)
+# mono stack, dp
+class Solution2(object):
     def numSubmat(self, mat):
         """
         :type mat: List[List[int]]
