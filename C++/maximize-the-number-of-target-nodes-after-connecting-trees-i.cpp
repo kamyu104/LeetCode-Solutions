d58a6be8dcd58f6d314bc42c0e857d3bfa85db6e
kamyu <kamyu104@users.noreply.github.com>
2024-12-03 21:43:31 +0800

    Update maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp

--- C++/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
+++ C++/maximize-the-number-of-target-nodes-after-connecting-trees-i.cpp
@@ -1,7 +1,7 @@
 // Time:  O((n + m) * k)
 // Space: O((n + m) * k)
 
-// tree dp
+// dfs, tree dp
 class Solution {
 public:
     vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2, int k) {
