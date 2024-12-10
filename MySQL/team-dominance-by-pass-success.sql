5f23929004714bf39c4acd92b38743efa87b1e02
kamyu <kamyu104@users.noreply.github.com>
2024-12-11 04:48:49 +0800

    Create team-dominance-by-pass-success.sql

---
+++ MySQL/team-dominance-by-pass-success.sql
@@ -0,0 +1,11 @@
+# Time:  O(p + t)
+# Space: O(p + t)
+
+SELECT t1.team_name,
+       IF(time_stamp <= '45:00', 1, 2) AS half_number,
+       SUM(IF(t1.team_name = t2.team_name, 1, -1)) AS dominance
+FROM Passes p
+     LEFT JOIN Teams t1 ON p.pass_from = t1.player_id
+     LEFT JOIN Teams t2 ON p.pass_to = t2.player_id
+GROUP BY 1, 2
+ORDER BY 1, 2;
