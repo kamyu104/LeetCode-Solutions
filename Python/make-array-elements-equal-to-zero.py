318c1393ae83d4f81b9e301a3a579b54de683991
kamyu <kamyu104@users.noreply.github.com>
2024-12-13 19:08:17 +0800

    Update make-array-elements-equal-to-zero.py

--- Python/make-array-elements-equal-to-zero.py
+++ Python/make-array-elements-equal-to-zero.py
@@ -1,8 +1,27 @@
 # Time:  O(n)
-# Space: O(n)
+# Space: O(1)
 
 # prefix sum, CodeChef Starters 146 - Bouncing Ball (https://www.codechef.com/problems/BOUNCE_BALL)
 class Solution(object):
+    def countValidSelections(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        total = sum(nums)
+        result = curr = 0
+        for i, x in enumerate(nums):
+            if not x:
+                result += max(2-abs(curr-(total-curr)), 0)
+            else:
+                curr += x
+        return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# prefix sum, CodeChef Starters 146 - Bouncing Ball (https://www.codechef.com/problems/BOUNCE_BALL)
+class Solution2(object):
     def countValidSelections(self, nums):
         """
         :type nums: List[int]
