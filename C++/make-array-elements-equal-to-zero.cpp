48ae30be60f0cf664ad1db654ed97398860df99d
kamyu <kamyu104@users.noreply.github.com>
2024-12-13 19:06:08 +0800

    Update make-array-elements-equal-to-zero.cpp

--- C++/make-array-elements-equal-to-zero.cpp
+++ C++/make-array-elements-equal-to-zero.cpp
@@ -5,6 +5,24 @@
 class Solution {
 public:
     int countValidSelections(vector<int>& nums) {
+        int result = 0;
+        for (int i = 0, curr = 0, total = accumulate(cbegin(nums), cend(nums), 0); i < size(nums); ++i) {
+            if (nums[i] == 0) {
+                result += max(2 - abs(curr - (total - curr)), 0);
+            } else {
+                curr += nums[i];
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(n)
+// prefix sum, CodeChef Starters 146 - Bouncing Ball (https://www.codechef.com/problems/BOUNCE_BALL)
+class Solution2 {
+public:
+    int countValidSelections(vector<int>& nums) {
         vector<int> prefix(size(nums) + 1);
         for (int i = 0; i < size(nums); ++i) {
             prefix[i + 1] = prefix[i] + nums[i];
