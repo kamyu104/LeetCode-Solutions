687c9eaf9b708f5474952896b31f53bed1b5a4cb
kamyu <kamyu104@users.noreply.github.com>
2024-12-19 16:47:27 +0800

    Update maximize-amount-after-two-days-of-conversions.cpp

--- C++/maximize-amount-after-two-days-of-conversions.cpp
+++ C++/maximize-amount-after-two-days-of-conversions.cpp
@@ -21,3 +21,53 @@ public:
         return dist[initialCurrency];
     }
 };
+
+// Time:  O(n^2)
+// Space: O(n)
+// bfs
+class Solution2 {
+public:
+    double maxAmount(string initialCurrency, vector<vector<string>>& pairs1, vector<double>& rates1, vector<vector<string>>& pairs2, vector<double>& rates2) {
+        const auto& find_adj = [](const auto& pairs, const auto& rates) {
+            unordered_map<string, vector<pair<string, double>>> adj;
+            for (int i = 0; i < size(pairs); ++i) {
+                for (int i = 0; i < size(pairs); ++i) {
+                    adj[pairs[i][0]].emplace_back(pairs[i][1], rates[i]);
+                    adj[pairs[i][1]].emplace_back(pairs[i][0], 1 / rates[i]);
+                }
+            }
+            return adj;
+        };
+
+        const auto& bfs = [](auto& dist, const auto& adj) {
+            vector<string> q;
+            for (const auto& [k, _] : adj) {
+                q.emplace_back(k);
+            }
+            while (!empty(q)) {
+                vector<string> new_q;
+                for (const auto& u : q) {
+                    if (empty(adj.at(u))) {
+                        continue;
+                    }
+                    for (const auto& [v, w] : adj.at(u)) {
+                        if (!(w * dist[u] > dist[v])) {
+                            continue;
+                        }
+                        dist[v] = w * dist[u];
+                        new_q.emplace_back(v);
+                    }
+                }
+                q = move(new_q);
+            }
+        };
+        
+        unordered_map<string, double> dist;
+        dist[initialCurrency] = 1.0;
+        const auto& adj1 = find_adj(pairs1, rates1);
+        bfs(dist, adj1);  // Time: O(n)
+        const auto& adj2 = find_adj(pairs2, rates2);
+        bfs(dist, adj2);  // Time: O(n^2)
+        return dist[initialCurrency];
+    }
+};
