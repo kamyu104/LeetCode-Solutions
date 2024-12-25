a905955d272b53a9e9e0a24d20bd8101c63fed6d
kamyu <kamyu104@users.noreply.github.com>
2024-12-25 18:36:16 +0800

    Create check-if-grid-can-be-cut-into-sections.py

---
+++ Python/check-if-grid-can-be-cut-into-sections.py
@@ -0,0 +1,24 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+# sort
+class Solution(object):
+    def checkValidCuts(self, n, rectangles):
+        """
+        :type n: int
+        :type rectangles: List[List[int]]
+        :rtype: bool
+        """
+        def check(idx):
+            cnt = 0
+            curr = rectangles[0][idx+2]
+            for x in rectangles:
+                cnt += int(curr <= x[idx])
+                curr = max(curr, x[idx+2])
+            return cnt >= 2
+    
+        for idx in xrange(2):
+            rectangles.sort(key=lambda x: x[idx])
+            if check(idx):
+                return True
+        return False
