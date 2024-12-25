b2330df1ff193b79c24ac005af59417187bc499f
kamyu <kamyu104@users.noreply.github.com>
2024-12-25 18:35:39 +0800

    Create check-if-grid-can-be-cut-into-sections.cpp

---
+++ C++/check-if-grid-can-be-cut-into-sections.cpp
@@ -0,0 +1,28 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// sort
+class Solution {
+public:
+    bool checkValidCuts(int n, vector<vector<int>>& rectangles) {
+        const auto& check = [&](int idx) {
+            int cnt = 0;
+            int curr = rectangles[0][idx + 2];
+            for (const auto& x : rectangles) {
+                cnt += curr <= x[idx] ? 1 : 0;
+                curr = max(curr, x[idx + 2]);
+            }
+            return cnt >= 2;
+        };
+
+        for (int idx = 0; idx < 2; ++idx) {
+            sort(begin(rectangles), end(rectangles), [&](const auto& a, const auto& b) {
+                return a[idx] < b[idx];
+            });
+            if (check(idx)) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
