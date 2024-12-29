b17d92f89cd4642ea3ea3fc404a89acd62e36378
kamyu <kamyu104@users.noreply.github.com>
2024-12-30 00:56:18 +0800

    Update find-circular-gift-exchange-chains.sql

--- MySQL/find-circular-gift-exchange-chains.sql
+++ MySQL/find-circular-gift-exchange-chains.sql
@@ -16,7 +16,7 @@ WITH RECURSIVE cycles_cte AS (
     FROM SecretSanta s
     INNER JOIN cycles_cte c ON s.giver_id = c.receiver_id AND s.giver_id != c.start
 ),
-cycle_length_value_cte AS (
+cycle_start_length_value_triples_cte AS (
     SELECT start, 
            COUNT(*) AS chain_length, 
            SUM(gift_value) AS total_gift_value
@@ -24,13 +24,13 @@ cycle_length_value_cte AS (
     GROUP BY 1
     ORDER BY NULL
 ),
-unique_cycle_length_value_cte AS (
+unique_cycle_length_value_pairs_cte AS (
     SELECT chain_length, total_gift_value
-    FROM cycle_length_value_cte
+    FROM cycle_start_length_value_triples_cte
     GROUP BY 1, 2
     ORDER BY NULL
 )
 SELECT RANK() OVER (ORDER BY chain_length DESC, total_gift_value DESC) AS chain_id,
        chain_length,
        total_gift_value
-FROM unique_cycle_length_value_cte;
+FROM unique_cycle_length_value_pairs_cte;
