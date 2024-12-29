addcb24f9ec555c68d2e715c509cbf923ada7f79
kamyu <kamyu104@users.noreply.github.com>
2024-12-29 23:21:30 +0800

    Update minimum-operations-to-make-columns-strictly-increasing.cpp

--- C++/minimum-operations-to-make-columns-strictly-increasing.cpp
+++ C++/minimum-operations-to-make-columns-strictly-increasing.cpp
@@ -6,11 +6,13 @@ class Solution {
 public:
     int minimumOperations(vector<vector<int>>& grid) {
         int result = 0;
-        for (int i = 0; i + 1 < size(grid); ++i) {
-            for (int j = 0; j < size(grid[0]); ++j) {
-                const int diff = max((grid[i][j] + 1) - grid[i + 1][j], 0);
-                grid[i + 1][j] += diff;
-                result += diff;
+        for (int j = 0; j < size(grid[0]); ++j) {
+            for (int i = 0; i + 1 < size(grid); ++i) {
+                if (grid[i][j] + 1 <= grid[i + 1][j]) {
+                    continue;
+                }
+                result += (grid[i][j] + 1) - grid[i + 1][j];
+                grid[i + 1][j] = grid[i][j] + 1;
             }
         }
         return result;
