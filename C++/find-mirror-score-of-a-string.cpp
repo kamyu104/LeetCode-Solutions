a5f2decf8462479c2d6ec2062a16683306334f18
kamyu <kamyu104@users.noreply.github.com>
2025-01-07 00:53:35 +0800

    Create find-mirror-score-of-a-string.cpp

---
+++ C++/find-mirror-score-of-a-string.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n + 26)
+// Space: O(n + 26)
+
+// hash table, stack
+class Solution {
+public:
+    long long calculateScore(string s) {
+        int64_t result = 0;
+        vector<vector<int>> lookup(26);
+        for (int i = 0; i < size(s); ++i) {
+            const int x = s[i] - 'a';
+            if (!empty(lookup[25 - x])) {
+                result += i - lookup[25 - x].back(); lookup[25 - x].pop_back();
+            } else {
+                lookup[x].emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
