5a8075fdad77c0c441a122bbd53684d1ebb77332
kamyu <kamyu104@users.noreply.github.com>
2025-01-06 23:37:34 +0800

    Create maximize-subarray-sum-after-removing-all-occurrences-of-one-element.cpp

---
+++ C++/maximize-subarray-sum-after-removing-all-occurrences-of-one-element.cpp
@@ -0,0 +1,20 @@
+// Time:  O(n)
+// Space: O(n)
+
+// greedy, kadane's algorithm
+class Solution {
+public:
+    long long maxSubarraySum(vector<int>& nums) {
+        int64_t result = numeric_limits<int64_t>::min();
+        int64_t curr = 0, mn = 0, mn0 = 0;
+        unordered_map<int, int64_t> mn1;        
+        for (const auto& x: nums) {
+            curr += x;
+            result = max(result, curr - mn);
+            mn1[x] = min(mn1[x], mn0) + x;
+            mn0 = min(mn0, curr);
+            mn = min({mn, mn1[x], mn0});
+        }
+        return result;
+    }
+};
