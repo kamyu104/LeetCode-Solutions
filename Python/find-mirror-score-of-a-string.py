7612f08c30f0abf75becf854b9ff3c4400034681
kamyu <kamyu104@users.noreply.github.com>
2025-01-07 00:54:13 +0800

    Create find-mirror-score-of-a-string.py

---
+++ Python/find-mirror-score-of-a-string.py
@@ -0,0 +1,19 @@
+# Time:  O(n + 26)
+# Space: O(n + 26)
+
+# hash table, stack
+class Solution(object):
+    def calculateScore(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        lookup = [[] for _ in xrange(26)]
+        for i, x in enumerate(s):
+            x = ord(x)-ord('a')
+            if lookup[25-x]:
+                result += i-lookup[25-x].pop()
+            else:
+                lookup[x].append(i)
+        return result
