4d1f30469dcb9c54f9568f4d0e1aaf6c13f11fb7
kamyu <kamyu104@users.noreply.github.com>
2025-01-06 22:14:45 +0800

    Create longest-subsequence-with-decreasing-adjacent-difference.cpp

---
+++ C++/longest-subsequence-with-decreasing-adjacent-difference.cpp
@@ -0,0 +1,24 @@
+// Time:  O(r^2 + n * r), r = max(nums)
+// Space: O(r^2)
+
+// dp
+class Solution {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int result = 2;
+        const int mx = ranges::max(nums);
+        vector<vector<int>> dp(mx, vector<int>(mx));
+        for (int i = nums.size() - 1; i >= 0; --i){
+            const int x = nums[i] - 1;
+            for (int nx = 0; nx < size(dp[x]); ++nx){
+                const int d = abs(nx - x);
+                dp[x][d] = max(dp[x][d], dp[nx][d] + 1);
+            }
+            for (int d = 1; d < size(dp[x]); ++d){
+                dp[x][d] = max(dp[x][d], dp[x][d - 1]);
+            }
+            result = max(result, dp[x].back());
+        }
+        return result;
+    }
+};
