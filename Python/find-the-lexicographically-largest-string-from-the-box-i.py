a1eb7c9083af44d260530870661bef6175f67134
kamyu <kamyu104@users.noreply.github.com>
2025-01-07 15:21:14 +0800

    Update find-the-lexicographically-largest-string-from-the-box-i.py

--- Python/find-the-lexicographically-largest-string-from-the-box-i.py
+++ Python/find-the-lexicographically-largest-string-from-the-box-i.py
@@ -11,19 +11,20 @@ class Solution(object):
         """
         if numFriends == 1:
             return word
-        idx, l = 0, 1
+        idx = l = 0
         for i in xrange(1, len(word)):
-            if word[i] == word[idx+(l-1)]:
+            if word[i] == word[idx+l]:
                 l += 1
-            elif word[i] < word[idx+(l-1)]:
-                l = 1
-            elif word[i] > word[idx+(l-1)]:
-                if word[i-(l-1)] >= word[i]:
-                    idx = i-(l-1)
+            elif word[i] < word[idx+l]:
+                l = 0
+            elif word[i] > word[idx+l]:
+                if word[i-l] >= word[i]:
+                    idx = i-l
                 else:
                     idx = i
-                l = 1
+                l = 0
         return word[idx:len(word)-max((numFriends-1)-idx, 0)]
+
 
 
 # Time:  O(n * m)
