355067d5b1c9ea1c776a1a8c3d15fb388d94cc62
kamyu <kamyu104@users.noreply.github.com>
2025-01-07 20:07:37 +0800

    Update maximize-subarray-sum-after-removing-all-occurrences-of-one-element.py

--- Python/maximize-subarray-sum-after-removing-all-occurrences-of-one-element.py
+++ Python/maximize-subarray-sum-after-removing-all-occurrences-of-one-element.py
@@ -21,3 +21,29 @@ class Solution(object):
             mn0 = min(mn0, curr)
             mn = min(mn, mn1[x], mn0)
         return result
+
+
+# Time:  O(n)
+# Space: O(n)
+import collections
+
+
+# hash table, greedy, kadane's algorithm
+class Solution2(object):
+    def maxSubarraySum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = float("-inf")
+        curr = mn = mn0 = 0
+        mn1 = collections.defaultdict(int)
+        for x in nums:
+            curr += x
+            result = max(result, curr-mn)
+            if x < 0:
+                mn1[x] = min(mn1[x], mn0)+x
+                mn = min(mn, mn1[x])
+            mn0 = min(mn0, curr)
+            mn = min(mn, mn0)
+        return result
