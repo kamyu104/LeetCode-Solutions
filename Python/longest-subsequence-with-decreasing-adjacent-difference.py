2926439cdfdeb2f1e6c6edc6557b8854db442118
kamyu <kamyu104@users.noreply.github.com>
2025-01-08 14:31:31 +0800

    Update longest-subsequence-with-decreasing-adjacent-difference.py

--- Python/longest-subsequence-with-decreasing-adjacent-difference.py
+++ Python/longest-subsequence-with-decreasing-adjacent-difference.py
@@ -11,6 +11,29 @@ class Solution(object):
         result = 2
         mx = max(nums)
         dp = [[0]*mx for _ in xrange(mx)]
+        for x in nums:
+            x -= 1
+            for nx in xrange(len(dp[x])):
+                d = abs(nx-x)
+                dp[x][d] = max(dp[x][d], dp[nx][d]+1)
+            for d in reversed(xrange(len(dp[x])-1)):
+                dp[x][d] = max(dp[x][d], dp[x][d+1])
+            result = max(result, dp[x][0])
+        return result
+
+
+# Time:  O(r^2 + n * r), r = max(nums)
+# Space: O(r^2)
+# dp
+class Solution2(object):
+    def longestSubsequence(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = 2
+        mx = max(nums)
+        dp = [[0]*mx for _ in xrange(mx)]
         for x in reversed(nums):
             x -= 1
             for nx in xrange(len(dp[x])):
