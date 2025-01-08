ced067f8f21f2f0753a67428a68e71a740277d0e
kamyu <kamyu104@users.noreply.github.com>
2025-01-08 14:33:04 +0800

    Update longest-subsequence-with-decreasing-adjacent-difference.cpp

--- C++/longest-subsequence-with-decreasing-adjacent-difference.cpp
+++ C++/longest-subsequence-with-decreasing-adjacent-difference.cpp
@@ -8,6 +8,30 @@ public:
         int result = 2;
         const int mx = ranges::max(nums);
         vector<vector<int>> dp(mx, vector<int>(mx));
+        for (int i = 0; i < size(nums); ++i){
+            const int x = nums[i] - 1;
+            for (int nx = 0; nx < size(dp[x]); ++nx){
+                const int d = abs(nx - x);
+                dp[x][d] = max(dp[x][d], dp[nx][d] + 1);
+            }
+            for (int d = size(dp[x]) - 2; d >= 0; --d){
+                dp[x][d] = max(dp[x][d], dp[x][d + 1]);
+            }
+            result = max(result, dp[x][0]);
+        }
+        return result;
+    }
+};
+
+// Time:  O(r^2 + n * r), r = max(nums)
+// Space: O(r^2)
+// dp
+class Solution2 {
+public:
+    int longestSubsequence(vector<int>& nums) {
+        int result = 2;
+        const int mx = ranges::max(nums);
+        vector<vector<int>> dp(mx, vector<int>(mx));
         for (int i = size(nums) - 1; i >= 0; --i){
             const int x = nums[i] - 1;
             for (int nx = 0; nx < size(dp[x]); ++nx){
