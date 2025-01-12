c5884fcac602c5ec102c80339abb8a431024f06b
kamyu <kamyu104@users.noreply.github.com>
2025-01-12 23:59:39 +0800

    Create minimize-the-maximum-edge-weight-of-graph.py

---
+++ Python/minimize-the-maximum-edge-weight-of-graph.py
@@ -0,0 +1,85 @@
+# Time:  O(nlogn + e)
+# Space: O(n + e)
+
+import collections
+import heapq
+
+
+# dijkstra's algorithm
+class Solution(object):
+    def minMaxWeight(self, n, edges, threshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type threshold: int
+        :rtype: int
+        """
+        def dijkstra():
+            best = [float("inf")]*len(adj)
+            best[0] = 0
+            min_heap = [(best[0], 0)]
+            while min_heap:
+                curr, u = heapq.heappop(min_heap)
+                if curr != best[u]:
+                    continue
+                for v, w in adj[u].iteritems():
+                    if not (max(curr, w) < best[v]):
+                        continue
+                    best[v] = max(curr, w)
+                    heapq.heappush(min_heap, (best[v], v))
+            result = max(best)
+            return result if result != float("inf") else -1
+
+        adj = [collections.defaultdict(lambda: float("inf")) for _ in xrange(n)]
+        for i, j, w in edges:
+            adj[j][i] = min(adj[j][i], w)
+        return dijkstra()
+
+
+# Time:  O(nlogw + e)
+# Space: O(n + e)
+import collections
+
+
+# binary search, bfs
+class Solution2(object):
+    def minMaxWeight(self, n, edges, threshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type threshold: int
+        :rtype: int
+        """
+        def binary_search(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return left
+
+        def check(x):
+            cnt = len(adj)
+            lookup = [False]*len(adj)
+            lookup[0] = True
+            cnt -= 1
+            q = [0]
+            while q:
+                new_q = []
+                for u in q:
+                    for v, w in adj[u].iteritems():
+                        if w > x or lookup[v]:
+                            continue
+                        lookup[v] = True
+                        cnt -= 1
+                        new_q.append(v)
+                q = new_q
+            return cnt == 0
+    
+        adj = [collections.defaultdict(lambda: float("inf")) for _ in xrange(n)]
+        for i, j, w in edges:
+            adj[j][i] = min(adj[j][i], w)
+        left, right = min(w for _, _, w in edges), max(w for _, _, w in edges)
+        result = binary_search(left, right, check)
+        return result if result <= right else -1
