4e59af074a52276e9444d57849697a75d091c751
kamyu <kamyu104@users.noreply.github.com>
2025-01-13 01:02:31 +0800

    Update minimize-the-maximum-edge-weight-of-graph.py

--- Python/minimize-the-maximum-edge-weight-of-graph.py
+++ Python/minimize-the-maximum-edge-weight-of-graph.py
@@ -36,13 +36,49 @@ class Solution(object):
         return dijkstra()
 
 
+# Time:  O(nlogn + e)
+# Space: O(n + e)
+import collections
+import heapq
+
+
+# prim's algorithm
+class Solution2(object):
+    def minMaxWeight(self, n, edges, threshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type threshold: int
+        :rtype: int
+        """
+        def prim():
+            best = [float("inf")]*len(adj)
+            min_heap = [(0, 0)]
+            while min_heap:
+                curr, u = heapq.heappop(min_heap)
+                if best[u] != float("inf"):
+                    continue
+                best[u] = curr
+                for v, w in adj[u].iteritems():
+                    if best[v] != float("inf"):
+                        continue
+                    heapq.heappush(min_heap, (w, v))
+            result = max(best)
+            return result if result != float("inf") else -1
+
+        adj = [collections.defaultdict(lambda: float("inf")) for _ in xrange(n)]
+        for i, j, w in edges:
+            adj[j][i] = min(adj[j][i], w)
+        return prim()
+
+
 # Time:  O(nlogw + e)
 # Space: O(n + e)
 import collections
 
 
 # binary search, bfs
-class Solution2(object):
+class Solution3(object):
     def minMaxWeight(self, n, edges, threshold):
         """
         :type n: int
