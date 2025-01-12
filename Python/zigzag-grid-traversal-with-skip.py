786e953061cb53817d701b9c005258e43ae50b5c
kamyu <kamyu104@users.noreply.github.com>
2025-01-12 21:32:20 +0800

    Create zigzag-grid-traversal-with-skip.py

---
+++ Python/zigzag-grid-traversal-with-skip.py
@@ -0,0 +1,17 @@
+# Time:  O(n * m)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def zigzagTraversal(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: List[int]
+        """
+        result = []
+        for i in xrange(len(grid)):
+            if i%2 == 0:
+                result.extend(grid[i][j] for j in xrange(0, len(grid[0]), 2))
+            else:
+                result.extend(grid[i][j] for j in xrange((len(grid[0])-1)-len(grid[0])%2, -1, -2))
+        return result
