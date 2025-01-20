dc260512fe07112fc7ddff1a9293d09ef38d922b
kamyu <kamyu104@users.noreply.github.com>
2025-01-21 05:47:16 +0800

    Update maximum-and-minimum-sums-of-at-most-size-k-subarrays.py

--- Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
+++ Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
@@ -16,6 +16,40 @@ class Solution(object):
             result = total = 0
             dq = collections.deque()
             for right in xrange(len(nums)):
+                if right-k >= 0:
+                    total -= nums[dq[0]]
+                    if dq[0] == right-k:
+                        dq.popleft()
+                while dq and not check(nums[dq[-1]], nums[right]):
+                    i = dq.pop()
+                    cnt = i-(dq[-1]+1 if dq else max(right-k+1, 0))+1
+                    total -= cnt*nums[i]
+                cnt = right-(dq[-1]+1 if dq else max(right-k+1, 0))+1
+                dq.append(right)
+                total += cnt*nums[right]
+                result += total
+            return result
+    
+        return count(lambda a, b: a < b)+count(lambda a, b: a > b)
+
+
+# Time:  O(n)
+# Space: O(k)
+import collections
+
+
+# two pointers, sliding window, mono deque
+class Solution2(object):
+    def minMaxSubarraySum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def count(check):
+            result = total = 0
+            dq = collections.deque()
+            for right in xrange(len(nums)):
                 if dq:
                     if dq[0][0] == right-k:
                         total -= nums[dq.popleft()[0]]
