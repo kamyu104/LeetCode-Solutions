b6774541dcbd77c14660ec875a5be9f1815008c9
kamyu <kamyu104@users.noreply.github.com>
2025-01-20 23:59:43 +0800

    Update manhattan-distances-of-all-arrangements-of-pieces.py

--- Python/manhattan-distances-of-all-arrangements-of-pieces.py
+++ Python/manhattan-distances-of-all-arrangements-of-pieces.py
@@ -3,6 +3,16 @@
 # Space: O(max(m * n))
 
 # combinatorics
+MOD = 10**9+7
+FACT, INV, INV_FACT = [[1]*2 for _ in xrange(3)]
+def nCr(n, k):
+    while len(INV) <= n:  # lazy initialization
+        FACT.append(FACT[-1]*len(INV) % MOD)
+        INV.append(INV[MOD%len(INV)]*(MOD-MOD//len(INV)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
+        INV_FACT.append(INV_FACT[-1]*INV[-1] % MOD)
+    return (FACT[n]*INV_FACT[n-k] % MOD) * INV_FACT[k] % MOD
+
+
 class Solution(object):
     def distanceSum(self, m, n, k):
         """
@@ -11,15 +21,6 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        MOD = 10**9+7
-        fact, inv, inv_fact = [[1]*2 for _ in xrange(3)]
-        def nCr(n, k):
-            while len(inv) <= n:  # lazy initialization
-                fact.append(fact[-1]*len(inv) % MOD)
-                inv.append(inv[MOD%len(inv)]*(MOD-MOD//len(inv)) % MOD)  # https://cp-algorithms.com/algebra/module-inverse.html
-                inv_fact.append(inv_fact[-1]*inv[-1] % MOD)
-            return (fact[n]*inv_fact[n-k] % MOD) * inv_fact[k] % MOD
-
         def sum_n(n):
             return (n+1)*n//2
 
