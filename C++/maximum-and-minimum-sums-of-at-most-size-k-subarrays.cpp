f6cb8587d8b2f41047066707a8234566dcf0c80d
kamyu <kamyu104@users.noreply.github.com>
2025-01-21 03:33:19 +0800

    Create maximum-and-minimum-sums-of-at-most-size-k-subarrays.cpp

---
+++ C++/maximum-and-minimum-sums-of-at-most-size-k-subarrays.cpp
@@ -0,0 +1,36 @@
+// Time:  O(n)
+// Space: O(k)
+
+// two pointers, sliding window, mono deque
+class Solution {
+public:
+    long long minMaxSubarraySum(vector<int>& nums, int k) {
+        const auto& count = [&](const auto& check) {
+            int64_t result = 0, total = 0;
+            deque<pair<int, int>> dq;
+            for (int right = 0; right < size(nums); ++right) {
+                while (!empty(dq) && dq[0].first <= right - k) {
+                    total -= nums[dq[0].first];
+                    dq.pop_front();
+                }
+                if (!empty(dq) && dq[0].second == right - k) {
+                    total -= nums[dq[0].first];
+                    ++dq[0].second;
+                }
+                int left = right;
+                while (!empty(dq) && !check(nums[dq.back().first], nums[right])) {
+                    total -= static_cast<int64_t>(dq.back().first - dq.back().second + 1) * nums[dq.back().first];
+                    left = dq.back().second;
+                    dq.pop_back();
+                }
+                dq.emplace_back(right, left);
+                total += static_cast<int64_t>(right - left + 1) * nums[right];
+                result += total;
+            }
+            return result;
+        };
+    
+        return count([](const auto& a, const auto& b) { return a < b; }) + 
+               count([](const auto& a, const auto& b) { return a > b; });
+    }
+};
