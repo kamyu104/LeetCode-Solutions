39eaf6d5718aabddd37c13b1a8428438724af167
kamyu <kamyu104@users.noreply.github.com>
2025-01-21 03:43:54 +0800

    Update maximum-and-minimum-sums-of-at-most-size-k-subarrays.py

--- Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
+++ Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
@@ -16,15 +16,15 @@ class Solution(object):
             result = total = 0
             dq = collections.deque()
             for right in xrange(len(nums)):
-                while dq and dq[0][0] <= right-k:
+                if dq and dq[0][0] == right-k:
                     total -= nums[dq.popleft()[0]]
                 if dq and dq[0][1] == right-k:
                     total -= nums[dq[0][0]]
                     dq[0][1] += 1
                 left = right
                 while dq and not check(nums[dq[-1][0]], nums[right]):
-                    total -= (dq[-1][0]-dq[-1][1]+1)*nums[dq[-1][0]]
-                    left = dq.pop()[1]
+                    i, left = dq.pop()
+                    total -= (i-left+1)*nums[i]
                 dq.append([right, left])
                 total += (right-left+1)*nums[right]
                 result += total
