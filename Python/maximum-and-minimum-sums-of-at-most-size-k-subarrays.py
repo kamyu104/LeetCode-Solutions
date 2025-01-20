f648e7d73acb411986ce887a1da419436c32351e
kamyu <kamyu104@users.noreply.github.com>
2025-01-21 05:58:40 +0800

    Update maximum-and-minimum-sums-of-at-most-size-k-subarrays.py

--- Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
+++ Python/maximum-and-minimum-sums-of-at-most-size-k-subarrays.py
@@ -57,11 +57,13 @@ class Solution2(object):
                 dq.append([right, left])
                 total += (right-left+1)*nums[right]
                 result += total
-                if dq[0][0] == right-(k-1):
-                    total -= nums[dq.popleft()[0]]
-                elif dq[0][1] == right-(k-1):
+                if right-(k-1) >= 0:
                     total -= nums[dq[0][0]]
-                    dq[0][1] += 1
+                    if dq[0][0] == right-(k-1):
+                        dq.popleft()
+                    else:
+                        assert(dq[0][1] == right-(k-1))
+                        dq[0][1] += 1
             return result
     
         return count(lambda a, b: a < b)+count(lambda a, b: a > b)
