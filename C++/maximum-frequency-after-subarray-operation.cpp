56aa4d73728047810d917837618f74ed5b41b4f5
kamyu <kamyu104@users.noreply.github.com>
2025-01-27 06:08:47 +0800

    Update maximum-frequency-after-subarray-operation.cpp

--- C++/maximum-frequency-after-subarray-operation.cpp
+++ C++/maximum-frequency-after-subarray-operation.cpp
@@ -1,14 +1,14 @@
 // Time:  O(n)
 // Space: O(n)
 
-// freq table
+// freq table, dp
 class Solution {
 public:
     int maxFrequency(vector<int>& nums, int k) {
         int result = 0, cnt_k = 0;
         unordered_map<int, int> cnt;
         for (const auto& x : nums) {
-            cnt[x] = max(cnt[x] + 1, cnt_k + 1);
+            cnt[x] = max(cnt[x], cnt_k) + 1;
             if (x == k) {
                 ++cnt_k;
                 ++result;
