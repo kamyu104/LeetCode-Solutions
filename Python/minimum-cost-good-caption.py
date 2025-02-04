4b1737f358cb8e449b13c835dc451292af4fe0d5
kamyu <kamyu104@users.noreply.github.com>
2025-02-04 19:37:08 +0800

    Update minimum-cost-good-caption.py

--- Python/minimum-cost-good-caption.py
+++ Python/minimum-cost-good-caption.py
@@ -14,15 +14,16 @@ class Solution(object):
             return ""
         dp = [[[0]*2 for _ in xrange(26)] for _ in xrange(n-L+1)]
         mn = [[0]*2 for _ in xrange(n-L+1)]
+        cap = map(lambda x: ord(x)-ord('a'), caption)
         for i in reversed(xrange(n-L+1)):
             for j in xrange(26):
                 if i == n-L:
-                    dp[i][j][:] = [sum(abs((ord(caption[k])-ord('a'))-j) for k in xrange(i, i+L)), L]
+                    dp[i][j][:] = [sum(abs(cap[k]-j) for k in xrange(i, i+L)), L]
                     continue
-                dp[i][j][:] = [dp[i+1][j][0]+abs((ord(caption[i])-ord('a'))-j), 1]
+                dp[i][j][:] = [dp[i+1][j][0]+abs(cap[i]-j), 1]
                 if i+L < n-2:
                     curr, c = mn[i+L]
-                    curr += sum(abs((ord(caption[k])-ord('a'))-j) for k in xrange(i, i+L))
+                    curr += sum(abs(cap[k]-j) for k in xrange(i, i+L))
                     if curr < dp[i][j][0] or (curr == dp[i][j][0] and c < j):
                         dp[i][j][:] = [curr, L]
             mn[i] = min([dp[i][j][0], j] for j in xrange(26))
