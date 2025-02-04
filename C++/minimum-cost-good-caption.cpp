6fe13c6548ce563631fbac0b82e998cc37dfc813
kamyu <kamyu104@users.noreply.github.com>
2025-02-04 19:37:31 +0800

    Update minimum-cost-good-caption.cpp

--- C++/minimum-cost-good-caption.cpp
+++ C++/minimum-cost-good-caption.cpp
@@ -12,7 +12,7 @@ public:
             return "";
         }
         vector<vector<pair<int, int>>> dp(n - L + 1, vector<pair<int, int>>(26));
-        vector<pair<int, int>> mx(n - L + 1);
+        vector<pair<int, int>> mn(n - L + 1);
         for (int i = n - L; i >= 0; --i) {
             for (int j = 0; j < 26; ++j) {
                 if (i == n - L) {
@@ -24,7 +24,7 @@ public:
                 }
                 dp[i][j] = pair(dp[i + 1][j].first + abs((caption[i] - 'a') - j), 1);
                 if (i + L < n - 2) {
-                    auto [curr, c] = mx[i + L];
+                    auto [curr, c] = mn[i + L];
                     for (int k = i; k < i + L; ++k) {
                         curr += abs((caption[k] - 'a') - j);
                     }
@@ -33,15 +33,15 @@ public:
                     }
                 }
             }
-            mx[i] = pair(dp[i][0].first, 0);
+            mn[i] = pair(dp[i][0].first, 0);
             for (int j = 1; j < 26; ++j) {
-                mx[i] = min(mx[i], pair(dp[i][j].first, j));
+                mn[i] = min(mn[i], pair(dp[i][j].first, j));
             }
         }
         string result;
-        for (int i = 0, j = mx[0].second, l = 1; i != n; i += l) {
+        for (int i = 0, j = mn[0].second, l = 1; i != n; i += l) {
             if (l == L) {
-                j = mx[i].second;
+                j = mn[i].second;
             }
             l = dp[i][j].second;
             result.append(l, 'a' + j);
