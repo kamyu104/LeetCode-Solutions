8621e05a3f62abd26d09c31ed79c0ed0b95d5cb1
kamyu <kamyu104@users.noreply.github.com>
2025-02-04 17:40:17 +0800

    Create minimum-cost-good-caption.py

---
+++ Python/minimum-cost-good-caption.py
@@ -0,0 +1,37 @@
+# Time:  O(26 * n)
+# Space: O(26 * n)
+
+# dp, backtracing
+class Solution(object):
+    def minCostGoodCaption(self, caption):
+        """
+        :type caption: str
+        :rtype: str
+        """
+        L = 3
+        n = len(caption)
+        if n < L:
+            return ""
+        dp = [[[0]*2 for _ in xrange(26)] for _ in xrange(n-L+1)]
+        mx = [[0]*2 for _ in xrange(n-L+1)]
+        for i in reversed(xrange(n-L+1)):
+            for j in xrange(26):
+                if i == n-L:
+                    dp[i][j][:] = [sum(abs((ord(caption[k])-ord('a'))-j) for k in xrange(i, i+L)), L]
+                    continue
+                dp[i][j][:] = [dp[i+1][j][0]+abs((ord(caption[i])-ord('a'))-j), 1]
+                if i+L < n-2:
+                    curr, c = mx[i+L]
+                    curr += sum(abs((ord(caption[k])-ord('a'))-j) for k in xrange(i, i+L))
+                    if curr < dp[i][j][0] or (curr == dp[i][j][0] and c < j):
+                        dp[i][j][:] = [curr, L]
+            mx[i] = min([dp[i][j][0], j] for j in xrange(26))
+        result = []
+        i, j, l = 0, mx[0][1], 1
+        while i != n:
+            if l == L:
+                j = mx[i][1]
+            l = dp[i][j][1]
+            result.append(chr(ord('a')+j)*l)
+            i += l
+        return "".join(result)
