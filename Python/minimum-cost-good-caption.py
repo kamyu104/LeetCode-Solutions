304103ed530ac624a1b86d1931d62a7b574494a8
kamyu <kamyu104@users.noreply.github.com>
2025-02-04 19:33:15 +0800

    Update minimum-cost-good-caption.py

--- Python/minimum-cost-good-caption.py
+++ Python/minimum-cost-good-caption.py
@@ -13,7 +13,7 @@ class Solution(object):
         if n < L:
             return ""
         dp = [[[0]*2 for _ in xrange(26)] for _ in xrange(n-L+1)]
-        mx = [[0]*2 for _ in xrange(n-L+1)]
+        mn = [[0]*2 for _ in xrange(n-L+1)]
         for i in reversed(xrange(n-L+1)):
             for j in xrange(26):
                 if i == n-L:
@@ -21,16 +21,16 @@ class Solution(object):
                     continue
                 dp[i][j][:] = [dp[i+1][j][0]+abs((ord(caption[i])-ord('a'))-j), 1]
                 if i+L < n-2:
-                    curr, c = mx[i+L]
+                    curr, c = mn[i+L]
                     curr += sum(abs((ord(caption[k])-ord('a'))-j) for k in xrange(i, i+L))
                     if curr < dp[i][j][0] or (curr == dp[i][j][0] and c < j):
                         dp[i][j][:] = [curr, L]
-            mx[i] = min([dp[i][j][0], j] for j in xrange(26))
+            mn[i] = min([dp[i][j][0], j] for j in xrange(26))
         result = []
-        i, j, l = 0, mx[0][1], 1
+        i, j, l = 0, mn[0][1], 1
         while i != n:
             if l == L:
-                j = mx[i][1]
+                j = mn[i][1]
             l = dp[i][j][1]
             result.append(chr(ord('a')+j)*l)
             i += l
