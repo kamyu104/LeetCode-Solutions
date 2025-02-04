dd88b53a7f1d0af4335e01ae39fd78fb116d0194
kamyu <kamyu104@users.noreply.github.com>
2025-02-04 15:00:15 +0800

    Update reschedule-meetings-for-maximum-free-time-i.cpp

--- C++/reschedule-meetings-for-maximum-free-time-i.cpp
+++ C++/reschedule-meetings-for-maximum-free-time-i.cpp
@@ -5,12 +5,14 @@
 class Solution {
 public:
     int maxFreeTime(int eventTime, int k, vector<int>& startTime, vector<int>& endTime) {
+        startTime.emplace_back(eventTime);
+        endTime.emplace(begin(endTime), 0);
         int result = 0;
-        for (int i = 0, curr = 0; i < size(startTime) + 1; ++i) {
-            curr += (i != size(startTime) ? startTime[i] : eventTime) - (i - 1 >= 0 ? endTime[i - 1] : 0);
+        for (int i = 0, curr = 0; i < size(startTime); ++i) {
+            curr += startTime[i] - endTime[i];
             result = max(result, curr);
             if (i - k >= 0) {
-                curr -= startTime[i - k] - (i - k - 1 >= 0 ? endTime[i - k - 1] : 0);
+                curr -= startTime[i - k] - endTime[i - k];
             }
         }
         return result;
