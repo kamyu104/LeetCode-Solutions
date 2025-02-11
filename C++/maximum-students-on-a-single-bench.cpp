5ce1c0ad4e855e2af25692ddd6c689424ab6dcb1
kamyu <kamyu104@users.noreply.github.com>
2025-02-11 15:56:50 +0800

    Update maximum-students-on-a-single-bench.cpp

--- C++/maximum-students-on-a-single-bench.cpp
+++ C++/maximum-students-on-a-single-bench.cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int maxStudentsOnBench(vector<vector<int>>& students) {
-        unordered_map<int, set<int>> lookup;
+        unordered_map<int, unordered_set<int>> lookup;
         for (const auto& x : students) {
             lookup[x[1]].emplace(x[0]);
         }
