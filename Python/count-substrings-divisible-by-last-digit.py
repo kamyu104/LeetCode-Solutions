c399fd2477b00dac91ecb6beefcced832dfe1bf1
kamyu <kamyu104@users.noreply.github.com>
2025-02-11 14:22:27 +0800

    Update count-substrings-divisible-by-last-digit.py

--- Python/count-substrings-divisible-by-last-digit.py
+++ Python/count-substrings-divisible-by-last-digit.py
@@ -59,10 +59,54 @@ class Solution(object):
         return result
 
 
+# Time:  O(d * n)
+# Space: O(d)
+# case works, math, freq table
+class Solution2(object):
+    def countSubstrings(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        # digit 1, 2, 5
+        for i in xrange(len(s)):
+            if s[i] in ('1', '2', '5'):
+                result += i+1
+        # digit 4
+        for i in xrange(len(s)):
+            if s[i] == '4':
+                result += 1
+                if i-1 >= 0 and int(s[i-1:i+1])%4 == 0:
+                    result += i
+        # digit 8
+        for i in xrange(len(s)):
+            if s[i] == '8':
+                result += 1
+                if i-1 >= 0 and int(s[i-1:i+1])%8 == 0:
+                    result += 1
+                if i-2 >= 0 and int(s[i-2:i+1])%8 == 0:
+                    result += i-1
+        for d in (3, 6, 7, 9):
+            base = 1
+            remain = 0
+            cnt = [0]*d
+            c = 0
+            for i in xrange(len(s)):
+                remain = (remain+base*(ord(s[~i])-ord('0')))%d
+                c += cnt[remain]
+                if s[~i] == str(d):
+                    c += d != 8
+                    cnt[remain] += 1
+                base = (base*10)%d
+            result += c
+        return result
+
+
 # Time:  O(d^2 * n)
 # Space: O(d^2)
 # dp
-class Solution2(object):
+class Solution3(object):
     def countSubstrings(self, s):
         """
         :type s: str
