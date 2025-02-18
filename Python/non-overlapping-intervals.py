3bac47d38915f44f146bfde7dc3c6a3058a3f0c3
kamyu <kamyu104@users.noreply.github.com>
2025-02-18 23:48:28 +0800

    Update non-overlapping-intervals.py

--- Python/non-overlapping-intervals.py
+++ Python/non-overlapping-intervals.py
@@ -4,14 +4,32 @@
 class Solution(object):
     def eraseOverlapIntervals(self, intervals):
         """
-        :type intervals: List[Interval]
+        :type intervals: List[List[int]]
         :rtype: int
         """
-        intervals.sort(key=lambda interval: interval.start)
+        intervals.sort(key=lambda interval: interval[1])
+        result, right = 0, float("-inf")
+        for l, r in intervals:
+            if l < right:
+                result += 1
+            else:
+                right = r
+        return result
+
+
+# Time:  O(nlogn)
+# Space: O(1)
+class Solution2(object):
+    def eraseOverlapIntervals(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: int
+        """
+        intervals.sort(key=lambda interval: interval[0])
         result, prev = 0, 0
         for i in xrange(1, len(intervals)):
-            if intervals[i].start < intervals[prev].end:
-                if intervals[i].end < intervals[prev].end:
+            if intervals[i][0] < intervals[prev][1]:
+                if intervals[i][1] < intervals[prev][1]:
                     prev = i
                 result += 1
             else:
