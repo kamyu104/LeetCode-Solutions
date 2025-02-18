290dc8933b29d8f08455f6e12174f9caabe53abb
kamyu <kamyu104@users.noreply.github.com>
2025-02-18 23:51:47 +0800

    Update non-overlapping-intervals.cpp

--- C++/non-overlapping-intervals.cpp
+++ C++/non-overlapping-intervals.cpp
@@ -1,25 +1,32 @@
 // Time:  O(nlogn)
 // Space: O(1)
 
-/**
- * Definition for an interval.
- * struct Interval {
- *     int start;
- *     int end;
- *     Interval() : start(0), end(0) {}
- *     Interval(int s, int e) : start(s), end(e) {}
- * };
- */
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<Interval>& intervals) {
-        sort(intervals.begin(), intervals.end(),
-             [](const Interval& a, const Interval& b) { return a.start < b.start; });
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) { return a[1] < b[1]; });
+        int result = 0, right = numeric_limits<int>::min();
+        for (const auto& x : intervals) {
+            if (x[0] < right) {
+                ++result;
+            } else {
+                right = x[1];
+            }
+        }
+        return result;
+    }
+};
 
+// Time:  O(nlogn)
+// Space: O(1)
+class Solution2 {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) { return a[0] < b[0]; });
         int result = 0, prev = 0;
         for (int i = 1; i < intervals.size(); ++i) {
-            if (intervals[i].start < intervals[prev].end) {
-                if (intervals[i].end < intervals[prev].end) {
+            if (intervals[i][0] < intervals[prev][1]) {
+                if (intervals[i][1] < intervals[prev][1]) {
                     prev = i;
                 }
                 ++result;
