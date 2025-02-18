b82598a29b0af348ffef73a6731cbb489ab4c612
kamyu <kamyu104@users.noreply.github.com>
2025-02-18 15:19:38 +0800

    Create shortest-matching-substring.py

---
+++ Python/shortest-matching-substring.py
@@ -0,0 +1,47 @@
+# Time:  O(n + m)
+# Space: O(n + m)
+
+# kmp, two pointers (three pointers)
+class Solution(object):
+    def shortestMatchingSubstring(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: int
+        """
+        INF = float("inf")
+        def getPrefix(pattern):
+            prefix = [-1]*len(pattern)
+            j = -1
+            for i in xrange(1, len(pattern)):
+                while j+1 > 0 and pattern[j+1] != pattern[i]:
+                    j = prefix[j]
+                if pattern[j+1] == pattern[i]:
+                    j += 1
+                prefix[i] = j
+            return prefix
+
+        a, b, c = p.split('*')
+        n = len(s)
+        la, lb, lc = len(a), len(b), len(c)
+        prefix1 = getPrefix(a+'#'+s)
+        prefix2 = getPrefix(b+'#'+s)
+        prefix3 = getPrefix(c+'#'+s)
+        result = INF
+        i = j = k = 0
+        while i+lb+lc < n:
+            while i < n and prefix1[la+1+i]+1 != la:
+                i += 1
+            if i >= n:
+                break
+            while j < i+lb or (j < n and prefix2[lb+1+j]+1 != lb):
+                j += 1
+            if j >= n:
+                break
+            while k < j+lc or (k < n and prefix3[lc+1+k]+1 != lc):
+                k += 1
+            if k >= n:
+                break
+            result = min(result, k-(i-la))
+            i += 1
+        return result if result != INF else -1
