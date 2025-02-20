8f8a577d387a71de494b00dbe37a5c73688f8080
kamyu <kamyu104@users.noreply.github.com>
2025-02-20 15:50:28 +0800

    Update length-of-longest-v-shaped-diagonal-segment.cpp

--- C++/length-of-longest-v-shaped-diagonal-segment.cpp
+++ C++/length-of-longest-v-shaped-diagonal-segment.cpp
@@ -80,3 +80,49 @@ public:
         return result;
     }
 };
+
+// Time:  O(n * m)
+// Space: O(n * m)
+// memoization
+class Solution2 {
+public:
+    int lenOfVDiagonal(vector<vector<int>>& grid) {
+        const int n = size(grid);
+        const int m = size(grid[0]);
+        vector<pair<int, int>> directions = {{1, 1}, {1, -1}, {-1, -1}, {-1, 1}};
+        // be careful with the order, going from smaller dimensions to larger dimensions
+        vector<vector<vector<vector<vector<int>>>>> lookup(2, vector<vector<vector<vector<int>>>>(3, vector<vector<vector<int>>>(4, vector<vector<int>>(n, vector<int>(m)))));
+        const function<int (int, int, int, int, int)> memoization = [&](int i, int j, int x, int d, int k) {
+            if (!(0 <= i && i < n && 0 <= j && j < m)) {
+                return 0;
+            }
+            if (grid[i][j] != x) {
+                return 0;
+            }
+            if (lookup[k][x][d][i][j] == 0) {
+                const int ni = i + directions[d].first;
+                const int nj = j + directions[d].second;
+                const int nx = x == 2 ? 0 : 2;
+                int result = memoization(ni, nj, nx, d, k) + 1;
+                if (k != 1) {
+                    const int nd = (d + 1) % 4;
+                    result = max(result, memoization(ni, nj, nx, nd, k + 1) + 1);
+                }
+                lookup[k][x][d][i][j] = result;
+            }
+            return lookup[k][x][d][i][j];
+        };
+        
+        int result = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (grid[i][j] == 1) {
+                    for (int d = 0; d < 4; ++d) {
+                        result = max(result, memoization(i, j, 1, d, 0));
+                    }
+                }
+            }
+        }
+        return result;
+    }
+};
