91022b5c3bc061f4bbfa9a2a8a4d2a2e20db8cf3
kamyu <kamyu104@users.noreply.github.com>
2025-02-25 02:38:24 +0800

    Update best-time-to-buy-and-sell-stock-iii.py

--- Python/best-time-to-buy-and-sell-stock-iii.py
+++ Python/best-time-to-buy-and-sell-stock-iii.py
@@ -10,10 +10,10 @@ class Solution(object):
         hold1, hold2 = float("-inf"), float("-inf")
         release1, release2 = 0, 0
         for i in prices:
-            release1 = max(release1, hold1 + i)
             hold1 = max(hold1, -i)
-            release2 = max(release2, hold2 + i)
+            release1 = max(release1, hold1 + i)
             hold2 = max(hold2, release1 - i)
+            release2 = max(release2, hold2 + i)
         return release2
 
 
