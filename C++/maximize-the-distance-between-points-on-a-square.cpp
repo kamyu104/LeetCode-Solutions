ce18cb033c82529e58fb46d79fd0cdff548f1477
kamyu <kamyu104@users.noreply.github.com>
2025-02-25 22:45:21 +0800

    Update maximize-the-distance-between-points-on-a-square.cpp

--- C++/maximize-the-distance-between-points-on-a-square.cpp
+++ C++/maximize-the-distance-between-points-on-a-square.cpp
@@ -17,6 +17,67 @@ public:
             return right;
         };
       
+        vector<int64_t> p;
+        for (const auto& x : points) {
+            if (x[0] == 0) {
+                p.emplace_back(0ll * side + x[1]);
+            } else if(x[1] == side) {
+                p.emplace_back(1ll * side + x[0]);
+            } else if(x[0] == side) {
+                p.emplace_back(2ll * side + (side - x[1]));
+            } else {
+                p.emplace_back(3ll * side + (side - x[0]));
+            }
+        }
+        sort(begin(p), end(p));
+        
+        const auto& check = [&](int d) {
+            vector<tuple<int, int, int>> intervals = {{0, 0, 1}};
+            for (int right = 1, i = 0; right < size(p); ++right) {
+                int left = right, cnt = 1;
+                for (; i < size(intervals); ++i) {
+                    const auto& [l, r, c] = intervals[i];
+                    if (p[right] - p[r] < d) {
+                        break;
+                    }
+                    if ((p[l] + 4ll * side) - p[right] >= d) {
+                        if (c + 1 >= cnt) {
+                            cnt = c + 1;
+                            left = l;
+                        }
+                    }
+                }
+                intervals.emplace_back(left, right, cnt);
+            }
+            int mx = 0;
+            for (const auto& [l, r, c] : intervals) {
+                mx = max(mx, c);
+            }
+            return mx >= k;
+        };
+
+        return binary_search_right(1, 4ll * side / k, check);
+    }
+};
+
+// Time:  O(nlogn + nlogs), s = side
+// Space: O(n)
+// sort, binary search, greedy, two pointers, sliding window
+class Solution2 {
+public:
+    int maxDistance(int side, vector<vector<int>>& points, int k) {
+        const auto& binary_search_right = [](auto left, auto right, const auto& check) {
+            while (left <= right) {
+                const auto mid = left + (right - left) / 2;
+                if (!check(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return right;
+        };
+      
         vector<vector<pair<int, int>>> p(4);
         for (const auto& x : points) {
             if (x[0] == 0) {
@@ -72,7 +133,7 @@ public:
 // Time:  O(nlogn + n * (k * logn) * logs), s = side
 // Space: O(n)
 // sort, binary search, greedy
-class Solution2 {
+class Solution3 {
 public:
     int maxDistance(int side, vector<vector<int>>& points, int k) {
         const auto& binary_search_right = [](auto left, auto right, const auto& check) {
@@ -123,7 +184,7 @@ public:
 // Time:  O(nlogn + (n * k * logn) * logs), s = side
 // Space: O(n)
 // sort, binary search, greedy
-class Solution3 {
+class Solution4 {
 public:
     int maxDistance(int side, vector<vector<int>>& points, int k) {
         const auto& binary_search_right = [](auto left, auto right, const auto& check) {
