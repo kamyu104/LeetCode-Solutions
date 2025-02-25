3206de7cb260662c67979b943fd737a2f42cf4da
kamyu <kamyu104@users.noreply.github.com>
2025-02-25 22:41:46 +0800

    Update maximize-the-distance-between-points-on-a-square.py

--- Python/maximize-the-distance-between-points-on-a-square.py
+++ Python/maximize-the-distance-between-points-on-a-square.py
@@ -22,6 +22,57 @@ class Solution(object):
         def check(d):
             intervals = [(0, 0, 1)]
             i = 0
+            for right in xrange(1, len(p)):
+                left, cnt = right, 1
+                while i < len(intervals):
+                    l, r, c = intervals[i]
+                    if p[right]-p[r] < d:
+                        break
+                    if (p[l]+4*side)-p[right] >= d:
+                        if c+1 >= cnt:
+                            cnt = c+1
+                            left = l
+                    i += 1
+                intervals.append((left, right, cnt))
+            return max(x[2] for x in intervals) >= k
+
+        p = []
+        for x, y in points:
+            if x == 0:
+                p.append(0*side+y)
+            elif y == side:
+                p.append(1*side+x)
+            elif x == side:
+                p.append(2*side+(side-y))
+            else:
+                p.append(3*side+(side-x))
+        p.sort()
+        return binary_search_right(1, 4*side//k, check)
+
+
+# Time:  O(nlogn + nlogs), s = side
+# Space: O(n)
+# sort, binary search, greedy, two pointers, sliding window
+class Solution2(object):
+    def maxDistance(self, side, points, k):
+        """
+        :type side: int
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        def binary_search_right(left, right, check):
+            while left <= right:
+                mid = left + (right-left)//2
+                if not check(mid):
+                    right = mid-1
+                else:
+                    left = mid+1
+            return right
+
+        def check(d):
+            intervals = [(0, 0, 1)]
+            i = 0
             for right in xrange(1, len(sorted_points)):
                 left, cnt = right, 1
                 while i < len(intervals):
@@ -60,7 +111,7 @@ import bisect
 
 
 # sort, binary search, greedy
-class Solution2(object):
+class Solution3(object):
     def maxDistance(self, side, points, k):
         """
         :type side: int
@@ -110,7 +161,7 @@ import bisect
 
 
 # sort, binary search, greedy
-class Solution3(object):
+class Solution4(object):
     def maxDistance(self, side, points, k):
         """
         :type side: int
