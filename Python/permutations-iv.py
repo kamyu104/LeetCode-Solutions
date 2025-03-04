356be10d7d74ba18a5138b9b3dca0590f8eec47f
kamyu <kamyu104@users.noreply.github.com>
2025-03-04 17:57:26 +0800

    Create permutations-iv.py

---
+++ Python/permutations-iv.py
@@ -0,0 +1,29 @@
+# Time:  O(n^2)
+# Space: O(n)
+
+# combinatorics
+class Solution(object):
+    def permute(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        result = []
+        fact = [1]*(((n-1)+1)//2+1)
+        for i in xrange(len(fact)-1):
+            fact[i+1] = fact[i]*(i+1)
+        lookup = [False]*n
+        for i in xrange(n):
+            cnt = fact[(n-1-i)//2]*fact[((n-1-i)+1)//2]
+            for j in xrange(n):
+                if not (not lookup[j] and ((i == 0 and n%2 == 0) or (j+1)%2 == (1 if not result else (result[-1]%2)^1))):
+                    continue
+                if k <= cnt:
+                    break
+                k -= cnt
+            else:
+                return []
+            lookup[j] = True
+            result.append(j+1)
+        return result
