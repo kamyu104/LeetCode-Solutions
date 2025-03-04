262e4198c43531a0396ad8f81619b0fb7697f56d
kamyu <kamyu104@users.noreply.github.com>
2025-03-04 19:19:57 +0800

    Create find-the-largest-almost-missing-integer.cpp

---
+++ C++/find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,32 @@
+// Time:  O(n)
+// Space: O(n)
+
+// array
+class Solution {
+public:
+    int largestInteger(vector<int>& nums, int k) {
+        if (k == size(nums)) {
+            return ranges::max(nums);
+        }
+        unordered_map<int, int> cnt;
+        for (const auto& x : nums) {
+            ++cnt[x];
+        }
+        int result = -1;
+        if (k == 1) {
+            for (const auto& [x, v] : cnt) {
+                if (v == 1) {
+                    result = max(result, x);
+                }
+            }
+            return result;
+        }
+        if (cnt[nums[0]] == 1) {
+            result = max(result, nums[0]);
+        }
+        if (cnt[nums.back()] == 1) {
+            result = max(result, nums.back());
+        }
+        return result;
+    }
+};
