860b04e260289bd86731d63dcd1bc290cecf5bb2
kamyu <kamyu104@users.noreply.github.com>
2025-03-04 18:15:17 +0800

    Update permutations-iv.cpp

--- C++/permutations-iv.cpp
+++ C++/permutations-iv.cpp
@@ -5,6 +5,39 @@
 class Solution {
 public:
     vector<int> permute(int n, long long k) {
+        vector<int> result;
+        vector<int64_t> cnt(n + 1, 1);
+        for (int i = 1; i + 1 < size(cnt); ++i) {
+            cnt[i + 1] = min(cnt[i] * ((i + 2) / 2), static_cast<int64_t>(k));
+        }
+        vector<bool> lookup(n);
+        for (int i = 0; i < n; ++i) {
+            int j = 0;
+            for (; j < n; ++j) {
+                if (!(!lookup[j] && ((i == 0 && n % 2 == 0) || ((j + 1) % 2 == (!empty(result) ? (result.back() % 2) ^ 1 : 1))))) {
+                    continue;
+                }
+                if (k <= cnt[n - 1 - i]) {
+                    break;
+                }
+                k -= cnt[n - 1 - i];
+            }
+            if (j == n) {
+                return {};
+            }
+            lookup[j] = true;
+            result.emplace_back(j + 1);
+        }
+        return result;
+    }
+};
+
+// Time:  O(n^2)
+// Space: O(n)
+// combinatorics
+class Solution2 {
+public:
+    vector<int> permute(int n, long long k) {
         typedef __int128 int128_t;
 
         vector<int> result;
