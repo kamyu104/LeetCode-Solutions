2233ae576b46ddcfd2420305097f0d266b5daedf
kamyu <kamyu104@users.noreply.github.com>
2025-03-04 18:16:53 +0800

    Update permutations-iv.py

--- Python/permutations-iv.py
+++ Python/permutations-iv.py
@@ -10,6 +10,35 @@ class Solution(object):
         :rtype: List[int]
         """
         result = []
+        cnt = [1]*n
+        for i in xrange(len(cnt)-1):
+            cnt[i+1] = cnt[i]*((i+2)//2)
+        lookup = [False]*n
+        for i in xrange(n):
+            for j in xrange(n):
+                if not (not lookup[j] and ((i == 0 and n%2 == 0) or (j+1)%2 == (1 if not result else (result[-1]%2)^1))):
+                    continue
+                if k <= cnt[n-1-i]:
+                    break
+                k -= cnt[n-1-i]
+            else:
+                return []
+            lookup[j] = True
+            result.append(j+1)
+        return result
+
+
+# Time:  O(n^2)
+# Space: O(n)
+# combinatorics
+class Solution2(object):
+    def permute(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        result = []
         fact = [1]*(((n-1)+1)//2+1)
         for i in xrange(len(fact)-1):
             fact[i+1] = fact[i]*(i+1)
