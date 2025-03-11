135e7f8fb91da865813712f2fadf1f250a8ef694
kamyu <kamyu104@users.noreply.github.com>
2025-03-11 18:28:51 +0800

    Create maximize-subarrays-after-removing-one-conflicting-pair.cpp

---
+++ C++/maximize-subarrays-after-removing-one-conflicting-pair.cpp
@@ -0,0 +1,34 @@
+// Time:  O(n)
+// Space: O(n)
+
+// greedy
+class Solution {
+public:
+    long long maxSubarrays(int n, vector<vector<int>>& conflictingPairs) {
+        vector<vector<int64_t>> right(n);
+        for (const auto& p : conflictingPairs) {
+            int l = p[0] - 1, r = p[1] - 1;
+            if (l > r) {
+                swap(l, r);
+            }
+            right[r].emplace_back(l);
+        }
+        int64_t result = 0;
+        vector<int64_t> top2(2, -1);
+        vector<int64_t> cnt(n);
+        for (int r = 0; r < n; ++r) {
+            for (auto l : right[r]) {
+                for (int i = 0; i < size(top2); ++i) {
+                    if (l > top2[i]) {
+                        swap(l, top2[i]);
+                    }
+                }
+            }
+            result += r - top2[0];
+            if (top2[0] != -1) {
+                cnt[top2[0]] += top2[0] - top2[1];
+            }
+        }
+        return result + ranges::max(cnt);
+    }
+};
