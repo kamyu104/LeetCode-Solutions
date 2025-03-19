8b0387a5b70b2d38ff64e3487dbfa6a6fa1a61e8
kamyu <kamyu104@users.noreply.github.com>
2025-03-20 02:20:21 +0800

    Create maximum-unique-subarray-sum-after-deletion.cpp

---
+++ C++/maximum-unique-subarray-sum-after-deletion.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(n)
+
+// hash set
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        const int mx = ranges::max(nums);
+        if (mx < 0) {
+            return mx;
+        }
+        unordered_set<int> lookup(cbegin(nums), cend(nums));
+        int result = 0;
+        for (const auto& x : lookup) {
+            if (x > 0) {
+                result += x;
+            }
+        }
+        return result;
+    }
+};
