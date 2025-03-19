b95751cf90ee99b54c9b8af24d45f3cf15fb6b40
kamyu <kamyu104@users.noreply.github.com>
2025-03-20 02:21:03 +0800

    Create maximum-unique-subarray-sum-after-deletion.py

---
+++ Python/maximum-unique-subarray-sum-after-deletion.py
@@ -0,0 +1,12 @@
+# Time:  O(n)
+# Space: O(n)
+
+# hash set
+class Solution(object):
+    def maxSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        mx = max(nums)
+        return mx if mx < 0 else sum(x for x in set(nums) if x >= 0)
