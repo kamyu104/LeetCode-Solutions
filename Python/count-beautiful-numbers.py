de5edb555f13b9aabcb7f5b99581d1e7f63a5a26
kamyu <kamyu104@users.noreply.github.com>
2025-03-25 03:09:25 +0800

    Create count-beautiful-numbers.py

---
+++ Python/count-beautiful-numbers.py
@@ -0,0 +1,33 @@
+# Time:  O(logr * 2 * 10 * states)
+# Space: O(states) ~= O(2026)
+
+import collections
+
+
+# dp
+class Solution(object):
+    def beautifulNumbers(self, l, r):
+        """
+        :type l: int
+        :type r: int
+        :rtype: int
+        """
+        def count(x):
+            s = map(lambda x: ord(x)-ord('0'), str(x))
+            dp = [collections.defaultdict(int) for _ in xrange(2)]
+            dp[1][1, 0] = 1
+            for c in s:
+                new_dp = [collections.defaultdict(int) for _ in xrange(2)]
+                for b in xrange(2):
+                    for (mul, total), cnt in dp[b].iteritems():
+                        for x in xrange((c if b else 9)+1):
+                            new_dp[b and x == c][mul*(1 if total == 0 == x else x), total+x] += cnt
+                dp = new_dp
+            result = 0
+            for b in xrange(2):
+                for (mul, total), cnt in dp[b].iteritems():
+                    if total and mul%total == 0:
+                        result += cnt
+            return result
+
+        return count(r)-count(l-1)
