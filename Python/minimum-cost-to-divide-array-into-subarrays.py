c86bd63986aba07496653da7c4d32039f1c65aa4
kamyu <kamyu104@users.noreply.github.com>
2025-03-31 18:11:29 +0800

    Create minimum-cost-to-divide-array-into-subarrays.py

---
+++ Python/minimum-cost-to-divide-array-into-subarrays.py
@@ -0,0 +1,24 @@
+# Time:  O(n^2)
+# Space: O(n)
+
+# dp
+class Solution(object):
+    def minimumCost(self, nums, cost, k):
+        """
+        :type nums: List[int]
+        :type cost: List[int]
+        :type k: int
+        :rtype: int
+        """
+        prefix1 = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix1[i+1] = prefix1[i]+nums[i]
+        prefix2 = [0]*(len(cost)+1)
+        for i in xrange(len(nums)):
+            prefix2[i+1] = prefix2[i]+cost[i]
+        dp = [float("inf")]*(len(nums)+1)
+        dp[-1] = 0
+        for i in reversed(xrange(len(nums))):
+            for j in xrange(i, len(nums)):
+                dp[i] = min(dp[i], prefix1[j+1]*(prefix2[j+1]-prefix2[i])+dp[j+1]+(k*(prefix2[-1]-prefix2[i])))
+        return dp[0]
