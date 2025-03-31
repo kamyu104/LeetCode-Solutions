d47001338a1a6e9acb24196f7f69efb5a4413af8
kamyu <kamyu104@users.noreply.github.com>
2025-03-31 18:10:54 +0800

    Create minimum-cost-to-divide-array-into-subarrays.cpp

---
+++ C++/minimum-cost-to-divide-array-into-subarrays.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n^2)
+// Space: O(n)
+
+// dp
+class Solution {
+public:
+    long long minimumCost(vector<int>& nums, vector<int>& cost, int k) {
+        vector<int64_t> prefix1(size(nums) + 1);
+        vector<int64_t> prefix2(size(nums) + 1);
+        for (int i = 0; i < size(nums); ++i) {
+            prefix1[i + 1] = prefix1[i] + nums[i];
+            prefix2[i + 1] = prefix2[i] + cost[i];
+        }
+        vector<int64_t> dp(size(nums) + 1, numeric_limits<int64_t>::max());
+        dp.back() = 0;
+        for (int i = size(nums) - 1; i >= 0; --i) {
+            for (int j = i; j < size(nums); ++j) {
+                dp[i] = min(dp[i], prefix1[j + 1] * (prefix2[j + 1] - prefix2[i]) + dp[j + 1] + (k * (prefix2.back() - prefix2[i])));
+            }
+        }
+        return dp[0];
+    }
+};
