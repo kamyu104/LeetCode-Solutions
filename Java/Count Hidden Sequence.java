5e4c852cd8174e09303f46618889ab233231b656
Dipanita45 <132455672+Dipanita45@users.noreply.github.com>
2025-04-21 20:32:58 +0530

    u

--- Java/Count Hidden Sequence.java
+++ Java/Count Hidden Sequence.java
@@ -1,10 +1,11 @@
-public class Solution {
+public  class Solution {
     public int numberOfArrays(int[] differences, int lower, int upper) {
         int n = differences.length;
         int prefix = 0;
         int min = 0;
         int max = 0;
         int diff = upper-lower;
+        
         for(int i = 0;i<n;i++){
             prefix = prefix + differences[i];
             min = Math.min(min,prefix);
