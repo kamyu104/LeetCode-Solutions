ed5ec79bb529bae18236e9c44989c702249e8d5e
kamyu <kamyu104@users.noreply.github.com>
2025-05-09 02:10:28 +0800

    Create merge-operations-for-minimum-travel-time.cpp

---
+++ C++/merge-operations-for-minimum-travel-time.cpp
@@ -0,0 +1,33 @@
+// Time:  O((n - k) * k^3)
+// Space: O(k^2)
+
+// prefix sum, dp
+class Solution {
+public:
+    int minTravelTime(int l, int n, int k, vector<int>& position, vector<int>& time) {
+        static const int INF = numeric_limits<int>::max();
+        vector<int> prefix(n + 1);
+        for (int i = 0; i < n; ++i) {
+            prefix[i + 1] = prefix[i] + time[i];
+        }
+        unordered_map<int, unordered_map<int, int>> dp;
+        dp[0][time[0]] = 0;
+        for (int w = 2; w <= n - k; ++w) {
+            unordered_map<int, unordered_map<int, int>> new_dp;
+            for (int i = w - 1; i < (w - 1) + (k + 1); ++i) {
+                for (int j = w - 2; j < i; ++j) {
+                    for (const auto& [t, c] : dp[j]) {
+                        const int nt = prefix[i + 1] - prefix[j + 1];
+                        new_dp[i][nt] = min(new_dp[i].count(nt) ? new_dp[i][nt] : INF, (position[i] - position[j]) * t + c);
+                    }
+                }
+            }
+            dp = move(new_dp);
+        }
+        int result = INF;
+        for (const auto& [_, c] : dp[n - 1]) {
+            result = min(result, c);
+        }
+        return result;
+    }
+};
