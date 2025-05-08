ae0d5d4ba43498bc0d58db086d83f04e287c03db
kamyu <kamyu104@users.noreply.github.com>
2025-05-09 02:12:24 +0800

    Create merge-operations-for-minimum-travel-time.py

---
+++ Python/merge-operations-for-minimum-travel-time.py
@@ -0,0 +1,30 @@
+# Time:  O((n - k) * k^3)
+# Space: O(k^2)
+
+import collections
+
+
+# prefix sum, dp
+class Solution(object):
+    def minTravelTime(self, l, n, k, position, time):
+        """
+        :type l: int
+        :type n: int
+        :type k: int
+        :type position: List[int]
+        :type time: List[int]
+        :rtype: int
+        """
+        prefix = [0]*(n+1)
+        for i in xrange(n):
+            prefix[i+1] = prefix[i]+time[i]
+        dp = collections.defaultdict(lambda: collections.defaultdict(lambda:float("inf")))
+        dp[0][time[0]] = 0
+        for w in xrange(2, (n-k)+1):
+            new_dp = collections.defaultdict(lambda: collections.defaultdict(lambda:float("inf")))
+            for i in xrange(w-1, (w-1)+(k+1)):
+                for j in xrange(w-2, i):
+                    for t, c in dp[j].iteritems():
+                        new_dp[i][prefix[i+1]-prefix[j+1]] = min(new_dp[i][prefix[i+1]-prefix[j+1]], (position[i]-position[j])*t+c)
+            dp = new_dp
+        return min(dp[n-1].itervalues())
