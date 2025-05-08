40296929426af3c83ee847960c2cc55a4f958773
kamyu <kamyu104@users.noreply.github.com>
2025-05-08 11:15:48 +0800

    Create maximum-product-of-two-digits.py

---
+++ Python/maximum-product-of-two-digits.py
@@ -0,0 +1,29 @@
+# Time:  O(d + 10)
+# Space: O(10)
+
+# freq table
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        MAX_COUNT = 2
+        def count(n):
+            cnt = [0]*10
+            while n:
+                n, r = divmod(n, 10)
+                cnt[r] += 1
+            return cnt
+
+        cnt = count(n)
+        result = 1
+        c = MAX_COUNT
+        for d in reversed(xrange(9+1)):
+            if c == 0:
+                break
+            while cnt[d] and c:
+                cnt[d] -= 1
+                c -= 1
+                result *= d
+        return result
