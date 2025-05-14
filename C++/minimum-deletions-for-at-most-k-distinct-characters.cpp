954e568e92d13f7084eefe9d32e411dba942a1e8
kamyu <kamyu104@users.noreply.github.com>
2025-05-14 17:08:08 +0800

    Create minimum-deletions-for-at-most-k-distinct-characters.cpp

---
+++ C++/minimum-deletions-for-at-most-k-distinct-characters.cpp
@@ -0,0 +1,27 @@
+// Time:  O(n + 26)
+// Space: O(n + 26)
+
+// freq table, greedy
+class Solution {
+public:
+    int minDeletion(string s, int k) {
+        vector<int> cnt(26);
+        for (const auto& x : s) {
+            ++cnt[x - 'a'];
+        }
+        vector<int> cnt2(ranges::max(cnt) + 1);
+        for (const auto& x : cnt) {
+            ++cnt2[x];
+        }
+        int result = 0;
+        for (int i = 0, total = 26 - k; i < size(cnt2); ++i) {
+            const int c = min(cnt2[i], total);
+            result += i * c;
+            total -= c;
+            if (total == 0) {
+                break;
+            }
+        }
+        return result;
+    }
+};
