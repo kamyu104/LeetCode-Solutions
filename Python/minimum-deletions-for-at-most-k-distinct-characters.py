55c2062c76be2ea42d8d54d64d1b0eb83d73e48a
kamyu <kamyu104@users.noreply.github.com>
2025-05-14 17:09:19 +0800

    Create minimum-deletions-for-at-most-k-distinct-characters.py

---
+++ Python/minimum-deletions-for-at-most-k-distinct-characters.py
@@ -0,0 +1,26 @@
+# Time:  O(n + 26)
+# Space: O(n + 26)
+
+# freq table, greedy
+class Solution(object):
+    def minDeletion(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+        """
+        cnt = [0]*26
+        for x in s:
+            cnt[ord(x)-ord('a')] += 1
+        cnt2 = [0]*(max(cnt)+1)
+        for x in cnt:
+            cnt2[x] += 1
+        result = 0
+        total = 26-k
+        for i, x in enumerate(cnt2):
+            c = min(total, x)
+            result += i*c
+            total -= c
+            if total == 0:
+                break
+        return result
