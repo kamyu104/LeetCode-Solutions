914d9f2e6c2eedc9764ca6ff4a2648b58fa0f848
kamyu <kamyu104@users.noreply.github.com>
2025-05-20 14:06:03 +0800

    Create minimum-swaps-to-sort-by-digit-sum.py

---
+++ Python/minimum-swaps-to-sort-by-digit-sum.py
@@ -0,0 +1,32 @@
+# Time:  O(nlogr)
+# Space: O(n)
+
+# sort
+class Solution(object):
+    def minSwaps(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def total(x):
+            result = 0
+            while x:
+                result += x%10
+                x //= 10
+            return result
+
+        idxs = range(len(nums))
+        idxs.sort(key=lambda i: (total(nums[i]), nums[i]))
+        i_to_idx = [-1]*len(idxs)
+        for idx, x in enumerate(idxs):
+            i_to_idx[x] = idx
+        result = 0
+        lookup = [False]*len(nums)
+        for i in xrange(len(nums)):
+            l = 0
+            while not lookup[i]:
+                lookup[i] = True
+                l += 1
+                i = i_to_idx[i]
+            result += max(l-1, 0)
+        return result
