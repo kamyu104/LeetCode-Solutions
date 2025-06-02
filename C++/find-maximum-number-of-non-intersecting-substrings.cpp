8ef0294c94e1ccab113277bb5411518eaaca5f0a
kamyu <kamyu104@users.noreply.github.com>
2025-06-02 23:09:43 +0800

    Create find-maximum-number-of-non-intersecting-substrings.cpp

---
+++ C++/find-maximum-number-of-non-intersecting-substrings.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n)
+// Space: O(26)
+
+// greedy
+class Solution {
+public:
+    int maxSubstrings(string word) {
+        static const int L = 4;
+
+        int result = 0;
+        unordered_map<char, int> lookup;
+        for (int i = 0; i < size(word); ++i) {
+            if (!lookup.count(word[i])) {
+                lookup[word[i]] = i;
+            } else if (i - lookup[word[i]] + 1 >= L) {
+                ++result;
+                lookup.clear();
+            }
+        }
+        return result;
+    }
+};
