69b2b27b944c86d6d6f69e05863a1de181ff6254
kamyu <kamyu104@users.noreply.github.com>
2025-06-02 23:10:25 +0800

    Create find-maximum-number-of-non-intersecting-substrings.py

---
+++ Python/find-maximum-number-of-non-intersecting-substrings.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(26)
+
+# greedy
+class Solution(object):
+    def maxSubstrings(self, word):
+        """
+        :type word: str
+        :rtype: int
+        """
+        L = 4
+        result = 0
+        lookup = {}
+        for i, c in enumerate(word):
+            if c not in lookup:
+                lookup[c] = i
+            elif i-lookup[c]+1 >= L:
+                result += 1
+                lookup.clear()
+        return result
