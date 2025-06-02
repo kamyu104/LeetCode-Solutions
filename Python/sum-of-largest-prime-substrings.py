dbff0e177c1a488f6ea9395cd691a0359a521df4
kamyu <kamyu104@users.noreply.github.com>
2025-06-03 02:20:12 +0800

    Update sum-of-largest-prime-substrings.py

--- Python/sum-of-largest-prime-substrings.py
+++ Python/sum-of-largest-prime-substrings.py
@@ -1,13 +1,43 @@
-# Time:  O(n^2 * sqrt(r) + n^2 * logn)
+# Time:  O(n^2 * sqrt(r))
 # Space: O(n^2)
 
-# number theory, sort
+import random
+
+
+# number theory, quick select
 class Solution(object):
     def sumOfLargestPrimes(self, s):
         """
         :type s: str
         :rtype: int
         """
+        COUNT = 3
+        def nth_element(nums, n, left=0, compare=lambda a, b: a < b):
+            def tri_partition(nums, left, right, target, compare):
+                mid = left
+                while mid <= right:
+                    if nums[mid] == target:
+                        mid += 1
+                    elif compare(nums[mid], target):
+                        nums[left], nums[mid] = nums[mid], nums[left]
+                        left += 1
+                        mid += 1
+                    else:
+                        nums[mid], nums[right] = nums[right], nums[mid]
+                        right -= 1
+                return left, right
+            
+            right = len(nums)-1
+            while left <= right:
+                pivot_idx = random.randint(left, right)
+                pivot_left, pivot_right = tri_partition(nums, left, right, nums[pivot_idx], compare)
+                if pivot_left <= n <= pivot_right:
+                    return
+                elif pivot_left > n:
+                    right = pivot_left-1
+                else:  # pivot_right < n.
+                    left = pivot_right+1
+
         def is_prime(n):
             if n == 1:
                 return False
@@ -22,11 +52,14 @@ class Solution(object):
                     return False
             return True
     
-        primes = set()
+        primes_set = set()
         for i in xrange(len(s)):
             curr = 0
             for j in xrange(i, len(s)):
                 curr = curr*10+int(s[j])
                 if is_prime(curr):
-                    primes.add(curr)
-        return sum(sorted(primes)[-3:])
+                    primes_set.add(curr)
+        primes = list(primes_set)
+        d = min(len(primes), COUNT)
+        nth_element(primes, d, compare=lambda a, b: a > b)
+        return sum(primes[i] for i in xrange(d))
