847a8dd02a6d86a2e68e159c8d572d2d606fd08f
kamyu <kamyu104@users.noreply.github.com>
2025-06-04 18:04:39 +0800

    Create lexicographically-smallest-string-after-adjacent-removals.cpp

---
+++ C++/lexicographically-smallest-string-after-adjacent-removals.cpp
@@ -0,0 +1,37 @@
+// Time:  O(n^3)
+// Space: O(n^2)
+
+// dp2
+class Solution {
+public:
+    string lexicographicallySmallestString(string s) {
+        vector<vector<bool>> dp(size(s), vector<bool>(size(s)));
+        for (int l = 2; l <= size(s); l += 2) {
+            for (int i = 0; i + (l - 1) < size(s); ++i) {
+                const int j = i + (l - 1);
+                if ((abs(s[i] - s[j]) == 1 || abs(s[i] - s[j]) == 25)) {
+                    if (j == i + 1 || dp[i + 1][j - 1]) {
+                        dp[i][j] = true;
+                        continue;
+                    }
+                }
+                for (int k = i + 1; k < j; k += 2) {
+                    if (dp[i][k] && dp[k + 1][j]) {
+                        dp[i][j] = true;
+                        break;
+                    }
+                }
+            }
+        }
+        vector<string> dp2(size(s) + 1);
+        for (int i = size(s) - 1; i >= 0; --i) {
+            dp2[i] = s[i] + dp2[i + 1];
+            for (int j = i + 1; j < size(s); j += 2) {
+                if (dp[i][j]) {
+                    dp2[i] = min(dp2[i], dp2[j + 1]);  
+                }
+            }
+        }
+        return dp2[0];
+    }
+};
