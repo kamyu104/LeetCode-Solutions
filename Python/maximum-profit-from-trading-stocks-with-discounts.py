cf38178b83f8550eac2cc4843e32792e83a572af
kamyu <kamyu104@users.noreply.github.com>
2025-06-12 04:20:57 +0800

    Update maximum-profit-from-trading-stocks-with-discounts.py

--- Python/maximum-profit-from-trading-stocks-with-discounts.py
+++ Python/maximum-profit-from-trading-stocks-with-discounts.py
@@ -16,46 +16,46 @@ class Solution(object):
         :rtype: int
         """
         def iter_dfs():
-            ret = [collections.defaultdict(int) for _ in xrange(2)]
+            ret = []
             stk = [(1, (0, ret))]
             while stk:
                 step, args = stk.pop()
                 if step == 1:
                     u, ret = args
-                    dp = [collections.defaultdict(int) for _ in xrange(2)]
-                    dp[0][0] = dp[1][0] = 0
-                    stk.append((4, (u, dp, ret)))
-                    stk.append((2, (u, dp, 0)))
+                    ret[:] = [collections.defaultdict(int) for _ in xrange(2)]
+                    ret[0][0] = ret[1][0] = 0
+                    stk.append((4, (u, ret)))
+                    stk.append((2, (u, 0, ret)))
                 elif step == 2:
-                    u, dp, i = args
+                    u, i, ret = args
                     if i == len(adj[u]):
                         continue
                     v = adj[u][i]
-                    stk.append((2, (u, dp, i+1)))
-                    new_ret = [collections.defaultdict(int) for _ in xrange(2)]
-                    stk.append((3, (new_ret, dp)))
+                    stk.append((2, (u, i+1, ret)))
+                    new_ret = []
+                    stk.append((3, (new_ret, ret)))
                     stk.append((1, (v, new_ret)))
                 elif step == 3:
-                    new_ret, dp = args
-                    new_dp = [collections.defaultdict(int) for _ in xrange(2)]
+                    new_ret, ret = args
                     for i in xrange(2):
-                        for j1, v1 in dp[i].iteritems():
+                        for j1, v1 in ret[i].items():
                             for j2, v2 in new_ret[i].iteritems():
                                 if j1+j2 <= budget:
-                                    new_dp[i][j1+j2] = max(new_dp[i][j1+j2], v1+v2)
-                    dp[:] = new_dp
+                                    ret[i][j1+j2] = max(ret[i][j1+j2], v1+v2)
                 elif step == 4:
-                    u, dp, ret = args
+                    u, ret = args
+                    new_ret = [collections.defaultdict(int) for _ in xrange(2)]
                     for i in xrange(2):
-                        for j, v in dp[0].iteritems():
-                            ret[i][j] = max(ret[i][j], v)
+                        for j, v in ret[0].iteritems():
+                            new_ret[i][j] = max(new_ret[i][j], v)
                         cost = present[u]>>i
                         if cost > budget:
                             continue
                         profit = future[u]-cost
-                        for j, v in dp[1].iteritems():
+                        for j, v in ret[1].iteritems():
                             if j+cost <= budget:
-                                ret[i][j+cost] = max(ret[i][j+cost], v+profit)
+                                new_ret[i][j+cost] = max(new_ret[i][j+cost], v+profit)
+                    ret[:] = new_ret
             return max(ret[0].itervalues())
 
         adj = [[] for _ in xrange(n)]
@@ -87,11 +87,10 @@ class Solution2(object):
                 res = dfs(v)
                 new_dp = [collections.defaultdict(int) for _ in xrange(2)]
                 for i in xrange(2):
-                    for j1, v1 in dp[i].iteritems():
+                    for j1, v1 in dp[i].items():
                         for j2, v2 in res[i].iteritems():
                             if j1+j2 <= budget:
-                                new_dp[i][j1+j2] = max(new_dp[i][j1+j2], v1+v2)
-                dp = new_dp
+                                dp[i][j1+j2] = max(dp[i][j1+j2], v1+v2)
             result = [collections.defaultdict(int) for _ in xrange(2)]
             for i in xrange(2):
                 for j, v in dp[0].iteritems():
