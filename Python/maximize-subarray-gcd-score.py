3fa37510b71449c8c90c8b28acd08d415585f8e9
kamyu <kamyu104@users.noreply.github.com>
2025-06-11 22:20:15 +0800

    Create maximize-subarray-gcd-score.py

---
+++ Python/maximize-subarray-gcd-score.py
@@ -0,0 +1,76 @@
+# Time:  O(n * (logr)^2 * logn)
+# Space: O((logr)^2 + n)
+
+# suffix-gcd states, dp, binary search
+class Solution(object):
+    def maxGCDScore(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a%b
+            return a
+    
+        lookup = [0]*len(nums)
+        for i in xrange(len(nums)):
+            while nums[i]&1 == 0:
+                nums[i] >>= 1
+                lookup[i] += 1
+        lookup2 = [[] for _ in xrange(max(lookup)+1)]
+        for i, e in enumerate(lookup):
+            lookup2[e].append(i)
+        result = 0
+        dp = {}
+        for i, x in enumerate(nums):
+            new_dp = {}
+            new_dp[x, lookup[i]] = [i]*2
+            for (g, e), v in dp.iteritems():  # |g| = O(logr), |e| = O(logr)
+                ng = gcd(g, x)
+                ne = min(e, lookup[i])
+                if (ng, ne) not in new_dp:
+                    new_dp[ng, ne] = [float("inf")]*2
+                new_dp[ng, ne][0] = min(new_dp[ng, ne][0], v[0])
+                left = bisect_left(lookup2[ne], v[0])  # Time: O(logn)
+                right = bisect_right(lookup2[ne], i)-1  # Time: O(logn)
+                new_dp[ng, ne][1] = min(new_dp[ng, ne][1], v[0] if right-left+1 <= k else lookup2[ne][right-k]+1)
+            dp = new_dp
+            for (g, e), v in dp.iteritems():  # |g| = O(logr), |e| = O(logr)
+                result = max(result, g*(i-v[0]+1)<<e, g*(i-v[1]+1)<<(e+1))
+        return result
+
+
+# Time:  O(n^2 + n * logr)
+# Space: O(1)
+# brute force
+class Solution2(object):
+    def maxGCDScore(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a%b
+            return a
+
+        result = 0
+        for i in xrange(len(nums)):
+            mn = float("inf")
+            g = cnt = 0
+            for j in xrange(i, len(nums)):
+                g = gcd(g, nums[j])
+                last_bit = nums[j]&-nums[j]
+                if last_bit < mn:
+                    mn = last_bit
+                    cnt = 0
+                if last_bit == mn:
+                    cnt += 1
+                result = max(result, g*(j-i+1)*(2 if cnt <= k else 1))
+                if g*(len(nums)-i)*2 <= result:
+                    break
+        return result
+
