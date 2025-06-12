eef9a9df0401b69aa56d7327a23d1871a9edf827
kamyu <kamyu104@users.noreply.github.com>
2025-06-13 02:35:04 +0800

    Create count-the-number-of-computer-unlocking-permutations.py

---
+++ Python/count-the-number-of-computer-unlocking-permutations.py
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(1)
+
+# combinatorics
+class Solution(object):
+    def countPermutations(self, complexity):
+        """
+        :type complexity: List[int]
+        :rtype: int
+        """
+        MOD = 10**9+7
+        def factorial(n):
+            return reduce(lambda accu, x: (accu*x)%MOD, xrange(1, n+1), 1)
+
+        mn = min(complexity)
+        return factorial(len(complexity)-1) if all(complexity[0] < complexity[i] for i in xrange(1, len(complexity))) else 0
