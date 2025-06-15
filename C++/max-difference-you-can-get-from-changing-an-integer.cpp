7265fbcba1cef9172fecf2e9663216d7344cd26b
kamyu <kamyu104@users.noreply.github.com>
2025-06-15 23:33:45 +0800

    Update max-difference-you-can-get-from-changing-an-integer.cpp

--- C++/max-difference-you-can-get-from-changing-an-integer.cpp
+++ C++/max-difference-you-can-get-from-changing-an-integer.cpp
@@ -1,7 +1,51 @@
 // Time:  O(logn)
-// Space: O(logn)
+// Space: O(1)
 
+// greedy
 class Solution {
+public:
+    int maxDiff(int num) {
+        const auto& find = [](int num, const auto& check) {
+            int result = 0;
+            for (; num; num /= 10) {
+                const auto& d = num % 10;
+                if (check(d)) {
+                    result = d;
+                }
+            }
+            return result;
+        };
+
+        const auto& reverse = [](int num, int l) {
+            int result = 0;
+            for (; num || l > 0; num /= 10, --l) {
+                result = result * 10 + num % 10;
+            };
+            return result;
+        };
+
+        const auto& replace = [&](int num, int x, int y) {
+            int result = 0, l = 0;
+            for (; num; num /= 10, ++l) {
+                auto d = num % 10;
+                if (d == x) {
+                    d = y;
+                }
+                result = result * 10 + d;
+            };
+            return reverse(result, l);
+        };
+
+        const auto& b = find(num, [](int x) { return x < 9; });
+        const auto& a = find(num, [](int x) { return x > 1; });
+        return replace(num, b, 9) - replace(num, a, reverse(num, 0) % 10 != 1 ? 1 : 0);
+    }
+};
+
+// Time:  O(logn)
+// Space: O(logn)
+// greedy
+class Solution2 {
 public:
     int maxDiff(int num) {
         const auto& digits = to_string(num);
