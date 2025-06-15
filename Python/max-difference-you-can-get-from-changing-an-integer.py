adc8b5fec8d540080ce504e81cedbdbcec699d3a
kamyu <kamyu104@users.noreply.github.com>
2025-06-15 22:14:19 +0800

    Update max-difference-you-can-get-from-changing-an-integer.py

--- Python/max-difference-you-can-get-from-changing-an-integer.py
+++ Python/max-difference-you-can-get-from-changing-an-integer.py
@@ -8,6 +8,6 @@ class Solution(object):
         :rtype: int
         """
         digits = str(num)
-        b = next((x for x in digits if x != '9'), '0')
+        b = next((x for x in digits if x < '9'), '0')
         a = next((x for x in digits if x > '1'), '0')
         return int(digits.replace(b, '9'))-int(digits.replace(a, '1' if digits[0] != '1' else '0'))
