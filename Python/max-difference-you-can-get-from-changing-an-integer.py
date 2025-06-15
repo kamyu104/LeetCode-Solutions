e5430913650e9e18e4088b9df373d80256cfeac0
kamyu <kamyu104@users.noreply.github.com>
2025-06-15 23:27:40 +0800

    Update max-difference-you-can-get-from-changing-an-integer.py

--- Python/max-difference-you-can-get-from-changing-an-integer.py
+++ Python/max-difference-you-can-get-from-changing-an-integer.py
@@ -1,7 +1,49 @@
 # Time:  O(logn)
-# Space: O(logn)
+# Space: O(1)
 
+# greedy
 class Solution(object):
+    def maxDiff(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        def find(num, check):
+            result = 0
+            while num:
+                num, d = divmod(num, 10)
+                if check(d):
+                    result = d
+            return result
+
+        def reverse(num, l):
+            result = 0
+            while num or l > 0:
+                num, d = divmod(num, 10)
+                result = result*10+d
+                l -= 1
+            return result
+
+        def replace(num, x, y):
+            result = l = 0
+            while num:
+                num, d = divmod(num, 10)
+                if d == x:
+                    d = y
+                result = result*10+d
+                l += 1
+            return reverse(result, l)
+    
+        digits = str(num)
+        b = find(num, lambda x: x < 9)
+        a = find(num, lambda x: x > 1)
+        return replace(num, b, 9)-replace(num, a, 1 if reverse(num, 0)%10 != 1 else 0)
+
+
+# Time:  O(logn)
+# Space: O(logn)
+# greedy
+class Solution2(object):
     def maxDiff(self, num):
         """
         :type num: int
