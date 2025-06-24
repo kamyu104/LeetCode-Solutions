5f3ed9075bfef66d6051f128d5c1109defaad13d
kamyu <kamyu104@users.noreply.github.com>
2025-06-25 02:25:45 +0800

    Update kth-smallest-path-xor-sum.py

--- Python/kth-smallest-path-xor-sum.py
+++ Python/kth-smallest-path-xor-sum.py
@@ -13,30 +13,32 @@ class Solution(object):
         :type queries: List[List[int]]
         :rtype: List[int]
         """
+        def small_to_large_merge(sl1, sl2):  # Total Time: O(n * (logn)^2)
+            if len(sl1) < len(sl2):
+                sl1, sl2 = sl2, sl1
+            for x in sl2:  # each node is merged at most O(logn) times
+                if x not in sl1:
+                    sl1.add(x)  # each add costs O(logn)
+            return sl1
+
         def iter_dfs():
             sl = [SortedList() for _ in xrange(len(adj))]
-            idxs = range(len(adj))
             result = [-1]*len(queries)
             stk = [(1, (0, 0))]
             while stk:
                 step, (u, curr) = stk.pop()
                 if step == 1:
                     curr ^= vals[u]
-                    sl[idxs[u]].add(curr)
+                    sl[u].add(curr)
                     stk.append((2, (u, curr)))
                     for v in reversed(adj[u]):
                         stk.append((1, (v, curr)))
                 elif step == 2:
                     for v in adj[u]:
-                        if len(sl[idxs[u]]) < len(sl[idxs[v]]):
-                            idxs[u], idxs[v] = idxs[v], idxs[u]                
-                    for v in adj[u]:
-                        for x in sl[idxs[v]]:  # each node is merged at most O(logn) times
-                            if x not in sl[idxs[u]]:
-                                sl[idxs[u]].add(x)  # each add costs O(logn)
+                        sl[u] = small_to_large_merge(sl[u], sl[v])
                     for i in lookup[u]:  # Total Time: O(qlogn)
-                        if queries[i][1]-1 < len(sl[idxs[u]]):
-                            result[i] = sl[idxs[u]][queries[i][1]-1]
+                        if queries[i][1]-1 < len(sl[u]):
+                            result[i] = sl[u][queries[i][1]-1]
             return result
 
         adj = [[] for _ in xrange(len(par))]
@@ -73,19 +75,13 @@ class Solution2(object):
 
         def dfs(u, curr):
             curr ^= vals[u]
-            sl[idxs[u]].add(curr)
+            sl = SortedList([curr])
             for v in adj[u]:
-                dfs(v, curr)
-            for v in adj[u]:
-                if len(sl[idxs[u]]) < len(sl[idxs[v]]):
-                    idxs[u], idxs[v] = idxs[v], idxs[u]                
-            for v in adj[u]:
-                for x in sl[idxs[v]]:  # each node is merged at most O(logn) times
-                    if x not in sl[idxs[u]]:
-                        sl[idxs[u]].add(x)  # each add costs O(logn)
+                sl = small_to_large_merge(sl, dfs(v, curr))
             for i in lookup[u]:  # Total Time: O(qlogn)
-                if queries[i][1]-1 < len(sl[idxs[u]]):
-                    result[i] = sl[idxs[u]][queries[i][1]-1]
+                if queries[i][1]-1 < len(sl):
+                    result[i] = sl[queries[i][1]-1]
+            return sl
 
         adj = [[] for _ in xrange(len(par))]
         for u, p in enumerate(par):
@@ -94,8 +90,6 @@ class Solution2(object):
         lookup = [[] for _ in xrange(len(adj))]
         for i, (u, _) in enumerate(queries):
             lookup[u].append(i)
-        sl = [SortedList() for _ in xrange(len(adj))]
-        idxs = range(len(adj))
         result = [-1]*len(queries)
         dfs(0, 0)
         return result
