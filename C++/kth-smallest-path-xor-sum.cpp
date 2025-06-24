1eb12d16dfc25394a27a30a1ae03227fb12773e0
kamyu <kamyu104@users.noreply.github.com>
2025-06-25 04:27:04 +0800

    Update kth-smallest-path-xor-sum.cpp

--- C++/kth-smallest-path-xor-sum.cpp
+++ C++/kth-smallest-path-xor-sum.cpp
@@ -83,32 +83,31 @@ public:
         for (int i = 0; i < size(queries); ++i) {
             lookup[queries[i][0]].emplace_back(i);
         }
-        using ordered_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
-        vector<ordered_set> os(size(adj));
-        vector<int> idxs(size(adj));
-        iota(begin(idxs), end(idxs), 0);
-        const auto& small_to_large_merge = [&](auto& i, auto& j) {  // Total Time: O(n * (logn)^2)
-            if (size(os[i]) < size(os[j])) {
-                swap(i, j);  // swapping indices is much faster than swapping ordered sets
+        const auto& small_to_large_merge = [&](auto& ptr1, auto& ptr2) {  // Total Time: O(n * (logn)^2)
+            if (size(*ptr1) < size(*ptr2)) {
+                swap(ptr1, ptr2);  // swapping ptrs is much faster than swapping ordered sets
             }
-            for (const auto& x : os[j]) {  // each node is merged at most O(logn) times
-                os[i].insert(x);  // each add costs O(logn)
+            for (const auto& x : *ptr2) {  // each node is merged at most O(logn) times
+                ptr1->insert(x);  // each add costs O(logn)
             }
         };
 
         vector<int> result(size(queries), -1);
-        const function<void (int, int)> dfs = [&](int u, int curr) {
+        using ordered_set = tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update>;
+        const function<unique_ptr<ordered_set> (int, int)> dfs = [&](int u, int curr) {
             curr ^= vals[u];
-            os[idxs[u]].insert(curr);
+            auto ptr = make_unique<ordered_set>();
+            ptr->insert(curr);
             for (const auto& v : adj[u]) {
-                dfs(v, curr);
-                small_to_large_merge(idxs[u], idxs[v]);
+                auto new_ptr = dfs(v, curr);
+                small_to_large_merge(ptr, new_ptr);
             }
             for (const auto& i : lookup[u]) {  // Total Time: O(qlogn)
-                if (queries[i][1] - 1 < size(os[idxs[u]])) {
-                    result[i] = *(os[idxs[u]].find_by_order(queries[i][1] - 1));
+                if (queries[i][1] - 1 < size(*ptr)) {
+                    result[i] = *(ptr->find_by_order(queries[i][1] - 1));
                 }
             }
+            return ptr;
         };
 
         dfs(0, 0);
