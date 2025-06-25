fad5e17caf0786c3c1e5c92fd40acbc327dfeff2
kamyu <kamyu104@users.noreply.github.com>
2025-06-26 02:23:26 +0800

    Update count-prime-gap-balanced-subarrays.cpp

--- C++/count-prime-gap-balanced-subarrays.cpp
+++ C++/count-prime-gap-balanced-subarrays.cpp
@@ -2,7 +2,7 @@
 //        runtime:    O(n)
 // Space: O(r)
 
-// number theory, binary search
+// number theory, mono deque, two pointers, sliding window
 vector<int> linear_sieve_of_eratosthenes(int n) {  // Time: O(n), Space: O(n)
     vector<int> spf(n + 1, -1);
     vector<int> primes;
