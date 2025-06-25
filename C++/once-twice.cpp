c8aafcfce69faadb1df533eb6914ef6812a6b85c
kamyu <kamyu104@users.noreply.github.com>
2025-06-26 02:28:24 +0800

    Update once-twice.cpp

--- C++/once-twice.cpp
+++ C++/once-twice.cpp
@@ -5,7 +5,8 @@
 class Solution {
 public:
     vector<int> onceTwice(vector<int>& nums) {
-        vector<int> dp = {~0, 0, 0};
+        vector<int> dp(3);
+        dp[0] = ~0;
         for (const auto& x : nums) {
             vector<int> new_dp(dp);
             for (int i = 0; i < 3; ++i) {
@@ -13,7 +14,8 @@ public:
             }
             dp = move(new_dp);
         }
-        vector<int> dp2 = {~0, 0, 0};
+        vector<int> dp2(3);
+        dp2[0] = ~0;
         for (const auto& x : nums) {
             if ((~x & dp[1]) || (x & dp[2])) {
                 continue;
