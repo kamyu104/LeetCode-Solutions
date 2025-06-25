62cbef8d4164dcd96968ad1e10da01e4a406fd33
kamyu <kamyu104@users.noreply.github.com>
2025-06-25 17:30:58 +0800

    Create check-if-any-element-has-prime-frequency.cpp

---
+++ C++/check-if-any-element-has-prime-frequency.cpp
@@ -0,0 +1,37 @@
+// Time:  precompute: O(MAX_N)
+//        runtime: O(n)
+// Space: O(MAX_N)
+
+// number theory
+vector<int> linear_sieve_of_eratosthenes(int n) {  // Time: O(n), Space: O(n)
+    vector<int> spf(n + 1, -1);
+    vector<int> primes;
+    for (int i = 2; i <= n; ++i) {
+        if (spf[i] == -1) {
+            spf[i] = i;
+            primes.emplace_back(i);
+        }
+        for (const auto& p : primes) {
+            if (i * p > n || p > spf[i]) {
+                break;
+            }
+            spf[i * p] = p;
+        }
+    }
+    return spf;
+}
+
+const int MAX_N = 100;
+const auto& SPF = linear_sieve_of_eratosthenes(MAX_N);
+class Solution {
+public:
+    bool checkPrimeFrequency(vector<int>& nums) {
+        unordered_map<int, int> cnt;
+        for (const auto& x : nums) {
+            ++cnt[x];
+        }
+        return any_of(cbegin(cnt), cend(cnt), [](const auto& x) {
+            return SPF[x.second] == x.second;
+        });
+    }
+};
