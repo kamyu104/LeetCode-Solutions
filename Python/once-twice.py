64ed619ea13712e20cdd21e37755dac15c82eb4b
kamyu <kamyu104@users.noreply.github.com>
2025-06-26 01:56:58 +0800

    Create once-twice.py

---
+++ Python/once-twice.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(1)
+
+# dp, bitmasks
+class Solution(object):
+    def onceTwice(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        dp = [~0, 0, 0]
+        for x in nums:
+            dp = [(x&dp[i-1])|(~x&dp[i]) for i in xrange(3)]
+        dp2 = [~0, 0, 0]
+        for x in nums:
+            if ~x&dp[1] or x&dp[2]:
+                continue
+            dp2 = [(x&dp2[i-1])|(~x&dp2[i]) for i in xrange(3)]
+        return [dp2[1], (dp2[1]^dp[1])|dp[2]]
