c16f11f6ef91b5297fedd5692d3558ebf85497ae
kamyu <kamyu104@users.noreply.github.com>
2025-06-26 01:56:12 +0800

    Create once-twice.cpp

---
+++ C++/once-twice.cpp
@@ -0,0 +1,29 @@
+// Time:  O(n)
+// Space: O(1)
+
+// dp, bitmasks
+class Solution {
+public:
+    vector<int> onceTwice(vector<int>& nums) {
+        vector<int> dp = {~0, 0, 0};
+        for (const auto& x : nums) {
+            vector<int> new_dp(dp);
+            for (int i = 0; i < 3; ++i) {
+                new_dp[i] = (x & dp[((i - 1) % 3 + 3) % 3]) | (~x & dp[i]);
+            }
+            dp = move(new_dp);
+        }
+        vector<int> dp2 = {~0, 0, 0};
+        for (const auto& x : nums) {
+            if ((~x & dp[1]) || (x & dp[2])) {
+                continue;
+            }
+            vector<int> new_dp2(dp2);
+            for (int i = 0; i < 3; ++i) {
+                new_dp2[i] = (x & dp2[((i - 1) % 3 + 3) % 3]) | (~x & dp2[i]);
+            }
+            dp2 = move(new_dp2);
+        }
+        return {dp2[1], (dp2[1] ^ dp[1]) | dp[2]};
+    }
+};
