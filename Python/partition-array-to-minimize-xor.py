10b3c68d9989b18ef2e0756a3304664ea9ccaa91
kamyu <kamyu104@users.noreply.github.com>
2025-07-01 10:17:59 +0800

    Create partition-array-to-minimize-xor.py

---
+++ Python/partition-array-to-minimize-xor.py
@@ -0,0 +1,51 @@
+# Time:  O(n^2 * k)
+# Space: O(n)
+
+# dp
+class Solution(object):
+    def minXor(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        INF = float("inf")
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]^nums[i]
+        dp = prefix[:]
+        dp[0] = INF
+        for l in xrange(2, k+1):
+            for i in reversed(xrange(l-1, len(dp))):
+                mn = INF
+                for j in xrange(l-1, i):
+                    v = prefix[i]^prefix[j]
+                    mx = dp[j] if dp[j] > v else v
+                    if mx < mn:
+                        mn = mx
+                dp[i] = mn
+        return dp[-1]
+
+
+# Time:  O(n^2 * k)
+# Space: O(n)
+# dp
+class Solution2(object):
+    def minXor(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        INF = float("inf")
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]^nums[i]
+        dp = [INF]*(len(nums)+1)
+        dp[0] = 0
+        for l in xrange(1, k+1):
+            for i in reversed(xrange(l-1, len(dp))):
+                dp[i] = INF
+                for j in xrange(l-1, i):
+                    dp[i] = min(dp[i], max(dp[j], prefix[i]^prefix[j]))
+        return dp[-1]
