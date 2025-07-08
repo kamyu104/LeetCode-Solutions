f27f89fa6a85faf63f26c1693cc36231b8d9df99
kamyu <kamyu104@users.noreply.github.com>
2025-07-09 03:52:25 +0800

    Create minimum-time-to-reach-destination-in-directed-graph.py

---
+++ Python/minimum-time-to-reach-destination-in-directed-graph.py
@@ -0,0 +1,37 @@
+# Time:  O(n + elogn)
+# Space: O(n + e)
+
+import collections
+
+
+# dijkstra's algorithm
+class Solution(object):
+    def minTime(self, n, edges):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :rtype: int
+        """
+        def dijkstra():
+            best = [float("inf")]*len(adj)
+            best[0] = 0
+            min_heap = [(best[0], 0)]
+            while min_heap:
+                curr, u = heapq.heappop(min_heap)
+                if curr != best[u]:
+                    continue
+                if u == len(adj)-1:
+                    return curr
+                for v, s, e in adj[u]:
+                    if curr > e:
+                        continue
+                    if not (best[v] > max(curr, s)+1):
+                        continue
+                    best[v] = max(curr, s)+1
+                    heapq.heappush(min_heap, (best[v], v))
+            return -1
+
+        adj = [[] for _ in xrange(n)]
+        for u, v, s, e in edges:
+            adj[u].append((v, s, e))
+        return dijkstra()
