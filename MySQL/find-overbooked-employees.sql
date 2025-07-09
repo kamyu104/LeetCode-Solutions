f5d8c35f4dd3c359e927e86d47fa128fcefbd7eb
kamyu <kamyu104@users.noreply.github.com>
2025-07-10 01:51:09 +0800

    Create find-overbooked-employees.sql

---
+++ MySQL/find-overbooked-employees.sql
@@ -0,0 +1,20 @@
+# Time:  O(m + nlogn)
+# Space: O(n)
+
+WITH overbooked_employees_cte AS (
+    SELECT employee_id
+    FROM meetings
+    GROUP BY employee_id, YEAR(meeting_date), WEEKOFYEAR(meeting_date)
+    HAVING SUM(duration_hours) > 20
+    ORDER BY NULL
+)
+
+SELECT e.employee_id,
+       e.employee_name,
+       e.department,
+       COUNT(*) AS meeting_heavy_weeks
+FROM overbooked_employees_cte AS o
+INNER JOIN employees AS e ON o.employee_id = e.employee_id
+GROUP BY 1, 2, 3
+HAVING meeting_heavy_weeks >= 2
+ORDER BY 4 DESC, 2;
