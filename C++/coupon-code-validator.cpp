9f6ec313702401fd2827c0c9459a97d97f4e1034
kamyu <kamyu104@users.noreply.github.com>
2025-07-09 18:44:30 +0800

    Create coupon-code-validator.cpp

---
+++ C++/coupon-code-validator.cpp
@@ -0,0 +1,26 @@
+// Time:  O(l * nlogn)
+// Space: O(1)
+
+// hash table, sort
+class Solution {
+public:
+    vector<string> validateCoupons(vector<string>& code, vector<string>& businessLine, vector<bool>& isActive) {
+        static const unordered_map<string, int> LOOKUP = {{"electronics", 0}, {"grocery", 1}, {"pharmacy", 2}, {"restaurant", 3}};
+
+        vector<pair<int, string>> sorted_codes;
+        for (int i = 0; i < size(code); ++i) {
+            if (isActive[i] && !empty(code[i]) && LOOKUP.count(businessLine[i]) && all_of(cbegin(code[i]), cend(code[i]), [](const auto& x) {
+                return isalnum(x) || x == '_';
+            })) {
+                sorted_codes.emplace_back(LOOKUP.at(businessLine[i]), code[i]);
+            }
+        }
+        sort(begin(sorted_codes), end(sorted_codes));
+        vector<string> result;
+        result.reserve(size(sorted_codes));
+        for (const auto& [_, c] : sorted_codes) {
+            result.emplace_back(c);
+        }
+        return result;
+    }
+};
