8795057da14e97efb5ce1edb65a4a987323a920e
kamyu <kamyu104@users.noreply.github.com>
2025-07-14 14:21:24 +0800

    Create process-string-with-special-operations-i.cpp

---
+++ C++/process-string-with-special-operations-i.cpp
@@ -0,0 +1,40 @@
+// Time:  O(n), n = len(result)
+// Space: O(n)
+
+// deque, simulation
+class Solution {
+public:
+    string processStr(string s) {
+        deque<char> dq;
+        bool right = true;
+        for (const auto& x : s) {
+            if (x == '*') {
+                if (empty(dq)) {
+                    continue;
+                }
+                if (right) {
+                    dq.pop_back();
+                } else {
+                    dq.pop_front();
+                }
+            } else if (x == '#') {
+                const int l = size(dq);
+                for (int i = 0; i < l; ++i) {
+                    dq.emplace_back(dq[i]);
+                }
+            } else if (x == '%') {
+                right = !right;
+            } else {
+                if (right) {
+                    dq.emplace_back(x);
+                } else {
+                    dq.emplace_front(x);
+                }
+            }
+        }
+        if (!right) {
+            reverse(begin(dq), end(dq));
+        }
+        return string(cbegin(dq), cend(dq));
+    }
+};
