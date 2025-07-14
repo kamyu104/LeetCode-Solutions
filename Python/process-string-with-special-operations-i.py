0586f315d7eb50e9746861b11f57759e05933856
kamyu <kamyu104@users.noreply.github.com>
2025-07-14 14:24:48 +0800

    Create process-string-with-special-operations-i.py

---
+++ Python/process-string-with-special-operations-i.py
@@ -0,0 +1,35 @@
+# Time:  O(n), n = len(result)
+# Space: O(n)
+
+import collections
+
+
+# deque, simulation
+class Solution(object):
+    def processStr(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        result = collections.deque()
+        right = True
+        for x in s:
+            if x == '*':
+                if not result:
+                    continue
+                if right:
+                    result.pop()
+                else:
+                    result.popleft()
+            elif x == '#':
+                result.extend(result)
+            elif x == '%':
+                right = not right
+            else:
+                if right:
+                    result.append(x)
+                else:
+                    result.appendleft(x)
+        if not right:
+            result.reverse()
+        return "".join(result)
