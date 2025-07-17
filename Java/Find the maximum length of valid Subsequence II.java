d9550d3a040522f6bbe71c94d74fff1f423694ff
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-07-17 08:36:50 +0530

    Create Find the maximum length of valid Subsequence II.java

---
+++ Java/Find the maximum length of valid Subsequence II.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int maximumLength(int[] nums, int k) {
+        int[][] dp = new int[k][k];
+        int res = 0;
+        for (int num : nums) {
+            num %= k;
+            for (int prev = 0; prev < k; prev++) {
+                dp[prev][num] = dp[num][prev] + 1;
+                res = Math.max(res, dp[prev][num]);
+            }
+        }
+        return res;
+    }
+}
