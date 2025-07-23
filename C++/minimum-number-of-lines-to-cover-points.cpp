f6a6df1639c34af6feaef6fb0f5ae41b6c2a4655
kamyu <kamyu104@users.noreply.github.com>
2025-07-24 05:27:25 +0800

    Update minimum-number-of-lines-to-cover-points.cpp

--- C++/minimum-number-of-lines-to-cover-points.cpp
+++ C++/minimum-number-of-lines-to-cover-points.cpp
@@ -4,26 +4,15 @@
 // math, hash table, bitmasks
 class Solution {
 private:
-    template <typename A, typename B, typename C>
     struct TupleHash {
-        size_t operator()(const tuple<A, B, C>& p) const {
-            size_t seed = 0;
-            A a; B b; C c;
-            tie(a, b, c) = p;
-            seed ^= std::hash<A>{}(a) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<B>{}(b) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<C>{}(c) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            return seed;
-        }
-    };
-
-    template <typename T>
-    struct PairHash {
-        size_t operator()(const pair<T, T>& p) const {
-            size_t seed = 0;
-            seed ^= std::hash<T>{}(p.first)  + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            seed ^= std::hash<T>{}(p.second) + 0x9e3779b9 + (seed<<6) + (seed>>2);
-            return seed;
+        template <typename... T>
+        std::size_t operator()(const std::tuple<T...>& t) const {
+            return apply([](const auto&... args) {
+                std::size_t seed = 0;
+                ((seed ^= std::hash<std::decay_t<decltype(args)>>{}(args) + 
+                        0x9e3779b9 + (seed << 6) + (seed >> 2)), ...);
+                return seed;
+            }, t);
         }
     };
 
@@ -32,7 +21,7 @@ public:
         auto ceil_divide = [](int a, int b) {
             return (a + b - 1) / b;
         };
-        unordered_map<tuple<int, int, int>, unordered_set<pair<int, int>, PairHash<int>>, TupleHash<int, int, int>> lookup;
+        unordered_map<tuple<int, int, int>, unordered_set<tuple<int, int>, TupleHash>, TupleHash> lookup;
         for (int i = 0; i < size(points); ++i) {
             const int x1 = points[i][0], y1 = points[i][1];
             for (int j = i + 1; j < size(points); ++j) {
@@ -44,6 +33,14 @@ public:
                 int c = x1 * (y2 - y1) - y1 * (x2 - x1);
                 const int g = gcd(gcd(a, b), c);
                 a /= g, b /= g, c /= g;
+                for (const auto& x : {a, b, c}) {
+                    if (x) {
+                        if (x < 0) {
+                            a = -a, b = -b, c = -c;
+                        }
+                        break;
+                    }
+                }
                 lookup[{a, b, c}].emplace(x1, y1);
                 lookup[{a, b, c}].emplace(x2, y2);
             }
@@ -58,7 +55,7 @@ public:
         int result = numeric_limits<int>::max();
         const int mask_upper_bound = 1 << size(lines);
         for (int mask = 0; mask < mask_upper_bound; ++mask) {
-            unordered_set<pair<int, int>, PairHash<int>> covered;
+            unordered_set<tuple<int, int>, TupleHash> covered;
             for (int i = 0, bit = 1; bit <= mask; bit <<= 1, ++i) {
                 if (mask & bit) {
                     for (const auto& x : lookup[lines[i]]) {
