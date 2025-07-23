42911d3a6c61dc0df60bcd1f8a8ba6f694656677
kamyu <kamyu104@users.noreply.github.com>
2025-07-24 02:32:40 +0800

    Create number-of-integers-with-popcount-depth-equal-to-k-ii.py

---
+++ Python/number-of-integers-with-popcount-depth-equal-to-k-ii.py
@@ -0,0 +1,38 @@
+# Time:  O(n^2 * logr), r = max(max(abs(x), abs(y)) for x, y in points)
+# Space: O(n^2)
+
+# math
+class Solution(object):
+    def countTrapezoids(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        def gcd(a, b):
+            while b:
+                a, b = b, a%b
+            return a
+
+        lookup_slope = collections.defaultdict(int)
+        lookup_line = collections.defaultdict(int)
+        lookup_slope_length = collections.defaultdict(int)
+        lookup_line_length = collections.defaultdict(int)
+        result = same = 0
+        for i in range(len(points)):
+            x1, y1 = points[i]
+            for j in xrange(i):
+                x2, y2 = points[j]
+                dx, dy = x2-x1, y2-y1
+                g = abs(gcd(dx, dy))
+                a, b = dx//g, dy//g
+                if a < 0 or (a == 0 and b < 0):
+                    a, b = -a, -b
+                c = b*x1-a*y1
+                result += lookup_slope[a, b]-lookup_line[a, b, c]
+                lookup_slope[a, b] += 1
+                lookup_line[a, b, c] += 1
+                l = dx**2+dy**2
+                same += lookup_slope_length[a, b, l]-lookup_line_length[a, b, c, l]
+                lookup_slope_length[a, b, l] += 1
+                lookup_line_length[a, b, c, l] += 1
+        return result-same//2
