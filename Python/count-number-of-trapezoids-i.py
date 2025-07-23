9939ac8bfb9d0882eac36a31a7864c4e7bba1211
kamyu <kamyu104@users.noreply.github.com>
2025-07-23 20:44:50 +0800

    Create count-number-of-trapezoids-i.py

---
+++ Python/count-number-of-trapezoids-i.py
@@ -0,0 +1,20 @@
+# Time:  O(n)
+# Space: O(n)
+
+# freq table, combinatorics
+class Solution(object):
+    def countTrapezoids(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        MOD = 10**9+7
+        cnt = collections.defaultdict(int)
+        for _, y in points:
+            cnt[y] += 1
+        result = total = 0
+        for c in cnt.itervalues():
+            curr = (c*(c-1)//2)%MOD
+            result = (result+(total*curr)%MOD)%MOD
+            total = (total+curr)%MOD
+        return result
