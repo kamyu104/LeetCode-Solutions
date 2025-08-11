5b9a15366713fca585c82f9cb4951d0897316e6f
kamyu <kamyu104@users.noreply.github.com>
2025-08-11 18:13:16 +0800

    Create maximum-k-to-sort-a-permutation.py

---
+++ Python/maximum-k-to-sort-a-permutation.py
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(1)
+
+# sort, constructive algorithms
+class Solution(object):
+    def sortPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        result = -1
+        for i in xrange(len(nums)):
+            if nums[i] == i:
+                continue
+            result = result&i if result != -1 else i
+        return result if result != -1 else 0
