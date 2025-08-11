8aaff6f02cacee79333b4520a7ff4cc5bc9419ae
kamyu <kamyu104@users.noreply.github.com>
2025-08-11 17:23:32 +0800

    Create flip-square-submatrix-vertically.py

---
+++ Python/flip-square-submatrix-vertically.py
@@ -0,0 +1,17 @@
+# Time:  O(m * n)
+# Space: O(1)
+
+# array
+class Solution(object):
+    def reverseSubmatrix(self, grid, x, y, k):
+        """
+        :type grid: List[List[int]]
+        :type x: int
+        :type y: int
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        for i in xrange(k//2):
+            for j in xrange(k):
+                grid[x+i][y+j], grid[x+(k-1-i)][y+j] = grid[x+(k-1-i)][y+j], grid[x+i][y+j]
+        return grid
