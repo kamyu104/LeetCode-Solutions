d2cb5024f53aa12d1a86683d9cd3e56c70c31cf1
kamyu <kamyu104@users.noreply.github.com>
2025-08-16 04:15:33 +0800

    Update find-users-with-valid-e-mails.sql

--- MySQL/find-users-with-valid-e-mails.sql
+++ MySQL/find-users-with-valid-e-mails.sql
@@ -3,4 +3,4 @@
 
 SELECT * 
 FROM   users AS u 
-WHERE REGEXP_LIKE(u.mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode.com$', 'c');
+WHERE REGEXP_LIKE(u.mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$', 'c');
