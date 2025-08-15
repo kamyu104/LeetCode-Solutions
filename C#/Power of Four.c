99aba8b043fe55773af7688ee70538ef335336b0
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-08-15 11:58:15 +0530

    Update Power of Four.c

--- C#/Power of Four.c
+++ C#/Power of Four.c
@@ -5,7 +5,7 @@ bool isPowerOfFour(int n) {
         }
     for(int i=0;i<=16;i++)
         {
-        if(pow(4,i)==n)
+        if((int)pow(4,i)==n)
             {
             return 1;
             }
