7e4ecc3e26b11b3d6525b00d0101ce7d15afaa6a
kamyu <kamyu104@users.noreply.github.com>
2025-08-20 16:06:39 +0800

    Create number-of-perfect-pairs.cpp

---
+++ C++/number-of-perfect-pairs.cpp
@@ -0,0 +1,19 @@
+// Time:  O(nlogn)
+// Space: O(1)
+
+// sort, two pointers
+class Solution {
+public:
+    long long perfectPairs(vector<int>& nums) {
+        for (int i = 0; i < size(nums); ++i) {
+            nums[i] = abs(nums[i]);
+        }
+        sort(begin(nums), end(nums));
+        int64_t result = 0;
+        for (int right = 0, left = 0; right < size(nums); ++right) {
+            for (; !(nums[right] - nums[left] <= nums[left]); ++left);
+            result += (right - left + 1) - 1;
+        }
+        return result;
+    }
+};
