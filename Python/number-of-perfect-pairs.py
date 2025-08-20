617e8760b34392d83738c4bb348a2ef6788de6dd
kamyu <kamyu104@users.noreply.github.com>
2025-08-20 16:07:08 +0800

    Create number-of-perfect-pairs.py

---
+++ Python/number-of-perfect-pairs.py
@@ -0,0 +1,19 @@
+# Time:  O(nlogn)
+# Space: O(1)
+
+# sort, two pointers
+class Solution(object):
+    def perfectPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in xrange(len(nums)):
+            nums[i] = abs(nums[i])
+        nums.sort()
+        result = left = 0
+        for right in xrange(len(nums)):
+            while not (nums[right]-nums[left] <= nums[left]):
+                left += 1
+            result += (right-left+1)-1
+        return result
