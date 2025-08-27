595e651275127b769af95adb82e3e18c6d170fb5
kamyu <kamyu104@users.noreply.github.com>
2025-08-27 23:16:44 +0800

    Create gcd-of-odd-and-even-sums.cpp

---
+++ C++/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,11 @@
+// Time:  O(1)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // gcd((1+(2n-1))*n/2, (2+2n)*n/2) = gcd(n*n, n*(n+1)) = n * gcd(n, n+1) = n
+        return n;
+    }
+};
