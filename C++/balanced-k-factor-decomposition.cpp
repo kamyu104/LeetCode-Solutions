32f6f8b6717f66c47cfd804a8b47d7e117a7200c
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 16:44:29 +0800

    Refactor backtracking and add second solution class
    Refactor backtracking function to simplify parameters and improve readability. Added a second solution class with similar functionality.

--- C++/balanced-k-factor-decomposition.cpp
+++ C++/balanced-k-factor-decomposition.cpp
@@ -6,7 +6,8 @@ class Solution {
 public:
     vector<int> minDifference(int n, int k) {
         vector<int> result, curr;
-        function<void (int, int)> backtracking = [&](int remain, int start) {
+        function<void (int)> backtracking = [&](int remain) {
+            const int start = !empty(curr) ? curr.back() : 1;
             if (size(curr) == k - 1) {
                 if (remain >= start) {
                     curr.emplace_back(remain);
@@ -24,7 +25,7 @@ public:
                 const int j = remain / i;
                 if (i >= start) {
                     curr.emplace_back(i);
-                    backtracking(j, i);
+                    backtracking(j);
                     curr.pop_back();
                 }
                 if (j == i) {
@@ -32,13 +33,51 @@ public:
                 }
                 if (j >= start) {
                     curr.emplace_back(j);
-                    backtracking(i, j);
+                    backtracking(i);
                     curr.pop_back();
                 }
             }
         };
 
-        backtracking(n, 1);
+        backtracking(n);
+        return result;
+    }
+};
+
+// Time:  O(k * sqrt(n)^(k - 1))
+// Space: O(k)
+// backtracking, number theory
+class Solution2 {
+public:
+    vector<int> minDifference(int n, int k) {
+        vector<int> result, curr;
+        function<void (int)> backtracking = [&](int remain) {
+            if (size(curr) == k - 1) {
+                curr.emplace_back(remain);
+                if (empty(result) || ranges::max(result) - ranges::min(result) > ranges::max(curr) - ranges::min(curr)) {
+                    result = curr;
+                }
+                curr.pop_back();
+                return;
+            }
+            for (int i = 1; i * i <= remain; ++i) {
+                if (remain % i) {
+                    continue;
+                }
+                const int j = remain / i;
+                curr.emplace_back(i);
+                backtracking(j);
+                curr.pop_back();
+                if (j == i) {
+                    continue;
+                }
+                curr.emplace_back(j);
+                backtracking(i);
+                curr.pop_back();
+            }
+        };
+
+        backtracking(n);
         return result;
     }
 };
