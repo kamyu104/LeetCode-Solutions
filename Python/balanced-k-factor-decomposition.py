9bd3f08e1e0a88651058cf9aa7b8b493f4e3a763
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 16:32:41 +0800

    Create balanced-k-factor-decomposition.py

---
+++ Python/balanced-k-factor-decomposition.py
@@ -0,0 +1,38 @@
+# Time:  O(sqrt(n)^(k - 1))
+# Space: O(k)
+
+# backtracking, number theory
+class Solution(object):
+    def minDifference(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        def factors(n):
+            for i in xrange(1, n+1):
+                if i*i > n:
+                    break
+                if n%i:
+                    continue
+                yield i
+                if n//i != i:
+                    yield n//i
+
+        def backtracking(remain, start):
+            if len(curr) == k-1 and remain >= start:
+                curr.append(remain)
+                if not result or result[-1]-result[0] > curr[-1]-curr[0]:
+                    result[:] = curr
+                curr.pop()
+                return
+            for i in factors(remain):
+                if i < start:
+                    continue
+                curr.append(i)
+                backtracking(remain//i, i)
+                curr.pop()
+                    
+        result, curr = [], []
+        backtracking(n, 1)
+        return result
