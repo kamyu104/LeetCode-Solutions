3791d52b62f294f408e31f30283acca9a860382d
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 16:37:39 +0800

    Implement minDifference method in Solution2 class

--- Python/balanced-k-factor-decomposition.py
+++ Python/balanced-k-factor-decomposition.py
@@ -36,3 +36,39 @@ class Solution(object):
         result, curr = [], []
         backtracking(n, 1)
         return result
+
+# Time:  O(k * sqrt(n)^(k - 1))
+# Space: O(k)
+# backtracking, number theory
+class Solution2(object):
+    def minDifference(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        def factors(n):
+            for i in xrange(1, n+1):
+                if i*i > n:
+                    break
+                if n%i:
+                    continue
+                yield i
+                if n//i != i:
+                    yield n//i
+
+        def backtracking(remain):
+            if len(curr) == k-1:
+                curr.append(remain)
+                if not result or max(result)-min(result) > max(curr)-min(curr):
+                    result[:] = curr
+                curr.pop()
+                return
+            for i in factors(remain):
+                curr.append(i)
+                backtracking(remain//i)
+                curr.pop()
+                    
+        result, curr = [], []
+        backtracking(n)
+        return result
