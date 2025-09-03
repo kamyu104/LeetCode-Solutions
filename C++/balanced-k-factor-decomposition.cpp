71d248cd9be31ab3bee5e13bd9ba0a0cb001a65b
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 22:47:30 +0800

    Refactor balanced-k-factor-decomposition with new logic
    Updated time and space complexity comments, added factorization logic, and introduced a new solution class.

--- C++/balanced-k-factor-decomposition.cpp
+++ C++/balanced-k-factor-decomposition.cpp
@@ -1,8 +1,53 @@
-// Time:  O(k * (n^(1/2) * n^(1/4) * n^(1/8) * n^(1/6) + n^(1/2) * n^(1/4) * n^(1/8) + n^(1/2) * n^(1/4) + n^(1/2))) <= O(k^2 * n)
-// Space: O(k)
+// Time:  precompute: O(rlogr)
+//        runtime:    O(k * (logn)^(k - 1))
+// Space: O(rlogr)
 
 // backtracking, number theory
+const auto& factors = [](int n) {
+    vector<vector<int>> result(n + 1);
+    for (int i = 1; i <= n; ++i) {
+        for (int j = i; j <= n; j += i) {
+            result[j].emplace_back(i);
+        }
+    }
+    return result;
+};
+
+const int MAX_N = 1e5;
+const auto& FACTORS = factors(MAX_N);
 class Solution {
+public:
+    vector<int> minDifference(int n, int k) {
+        vector<int> result, curr;
+        function<void (int)> backtracking = [&](int remain) {
+            const int start = !empty(curr) ? curr.back() : 1;
+            if (size(curr) == k - 1) {
+                if (remain >= start) {
+                    curr.emplace_back(remain);
+                    if (empty(result) || result.back() - result[0] > curr.back() - curr[0]) {
+                        result = curr;
+                    }
+                    curr.pop_back();
+                }
+                return;
+            }
+            const auto& factors = FACTORS[remain];
+            for (auto it = lower_bound(cbegin(factors), cend(factors), start); it != cend(factors); ++it) {
+                curr.emplace_back(*it);
+                backtracking(remain / *it);
+                curr.pop_back();
+            }
+        };
+
+        backtracking(n);
+        return result;
+    }
+};
+
+// Time:  O(k * (n^(1/2) * n^(1/4) * n^(1/8) * n^(1/6) + n^(1/2) * n^(1/4) * n^(1/8) + n^(1/2) * n^(1/4) + n^(1/2))) <= O(k^2 * n)
+// Space: O(k)
+// backtracking, number theory
+class Solution2 {
 public:
     vector<int> minDifference(int n, int k) {
         vector<int> result, curr;
@@ -47,7 +92,7 @@ public:
 // Time:  O(2^(k-1) * k * n)
 // Space: O(k)
 // backtracking, number theory
-class Solution2 {
+class Solution3 {
 public:
     vector<int> minDifference(int n, int k) {
         vector<int> result, curr;
