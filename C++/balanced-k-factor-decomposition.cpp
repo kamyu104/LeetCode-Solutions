2c50ac6ff623dcdedf9df82cf99d91889df7cab2
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 16:31:43 +0800

    Create balanced-k-factor-decomposition.cpp

---
+++ C++/balanced-k-factor-decomposition.cpp
@@ -0,0 +1,44 @@
+// Time:  O(sqrt(n)^(k - 1))
+// Space: O(k)
+
+// backtracking, number theory
+class Solution {
+public:
+    vector<int> minDifference(int n, int k) {
+        vector<int> result, curr;
+        function<void (int, int)> backtracking = [&](int remain, int start) {
+            if (size(curr) == k - 1) {
+                if (remain >= start) {
+                    curr.emplace_back(remain);
+                    if (empty(result) || result.back() - result[0] > curr.back() - curr[0]) {
+                        result = curr;
+                    }
+                    curr.pop_back();
+                }
+                return;
+            }
+            for (int i = 1; i * i <= remain; ++i) {
+                if (remain % i) {
+                    continue;
+                }
+                const int j = remain / i;
+                if (i >= start) {
+                    curr.emplace_back(i);
+                    backtracking(j, i);
+                    curr.pop_back();
+                }
+                if (j == i) {
+                    continue;
+                }
+                if (j >= start) {
+                    curr.emplace_back(j);
+                    backtracking(i, j);
+                    curr.pop_back();
+                }
+            }
+        };
+
+        backtracking(n, 1);
+        return result;
+    }
+};
