49ad9c921e969db7532d45c183a8b28d82923933
kamyu <kamyu104@users.noreply.github.com>
2025-09-03 22:43:13 +0800

    Update balanced-k-factor-decomposition.py

--- Python/balanced-k-factor-decomposition.py
+++ Python/balanced-k-factor-decomposition.py
@@ -1,8 +1,51 @@
-# Time:  O(k * (n^(1/2) * n^(1/4) * n^(1/8) * n^(1/6) + n^(1/2) * n^(1/4) * n^(1/8) + n^(1/2) * n^(1/4) + n^(1/2))) <= O(k^2 * n)
-# Space: O(k)
+# Time:  precompute: O(rlogr)
+#        runtime:    O(k * (logn)^(k - 1))
+# Space: O(rlogr)
+
+import bisect
+
 
 # backtracking, number theory
+def factors(n):
+    result = [[] for _ in xrange(n+1)]
+    for i in xrange(1, n+1):
+        for j in range(i, n+1, i):
+            result[j].append(i)
+    return result
+
+
+MAX_N = 10**5
+FACTORS = factors(MAX_N)
 class Solution(object):
+    def minDifference(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[int]
+        """
+        def backtracking(remain):
+            start = curr[-1] if curr else 1
+            if len(curr) == k-1 and remain >= start:
+                curr.append(remain)
+                if not result or result[-1]-result[0] > curr[-1]-curr[0]:
+                    result[:] = curr
+                curr.pop()
+                return
+            factors = FACTORS[remain]
+            for i in xrange(bisect.bisect_left(factors, start), len(factors)):
+                curr.append(factors[i])
+                backtracking(remain//factors[i])
+                curr.pop()
+                    
+        result, curr = [], []
+        backtracking(n)
+        return result    
+
+
+# Time:  O(k * (n^(1/2) * n^(1/4) * n^(1/8) * n^(1/6) + n^(1/2) * n^(1/4) * n^(1/8) + n^(1/2) * n^(1/4) + n^(1/2))) <= O(k^2 * n)
+# Space: O(k)
+# backtracking, number theory
+class Solution2(object):
     def minDifference(self, n, k):
         """
         :type n: int
@@ -42,7 +85,7 @@ class Solution(object):
 # Time:  O(2^(k-1) * k * n)
 # Space: O(k)
 # backtracking, number theory
-class Solution2(object):
+class Solution3(object):
     def minDifference(self, n, k):
         """
         :type n: int
