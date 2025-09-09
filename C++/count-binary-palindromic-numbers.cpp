94abb8f88b658c5e473b6704ffce975b2a919496
kamyu <kamyu104@users.noreply.github.com>
2025-09-10 00:27:08 +0800

    Update count-binary-palindromic-numbers.cpp

--- C++/count-binary-palindromic-numbers.cpp
+++ C++/count-binary-palindromic-numbers.cpp
@@ -1,8 +1,38 @@
 // Time:  O(logn)
-// Space: O(logn)
+// Space: O(1)
 
-// math, combinatorics
+// bitmasks, combinatorics
 class Solution {
+public:
+    int countBinaryPalindromes(long long n) {
+        const auto& length = [&](int64_t n) {
+            int result = 0;
+            for (; n; n >>= 1) {
+                ++result;
+            }
+            return result;
+        };
+
+        const auto& reverse = [](int64_t n, int l) {
+            int64_t result = 0;
+            for (int i = 0; i < l; ++i) {
+                if (n & (1 << i)) {
+                    result |= 1 << ((l - 1) - i);
+                }
+            }
+            return result;
+        };
+
+        const auto& l = length(n) / 2;
+        const auto& p = ((n >> l) << l) | reverse(n >> (length(n) - l), l);
+        return ((1 << l) - 1) + (n >> l) + (p <= n ? 1 : 0);
+    }
+};
+
+// Time:  O(logn)
+// Space: O(logn)
+// bitmasks, combinatorics
+class Solution2 {
 public:
     int countBinaryPalindromes(long long n) {
         const auto& to_binary = [&](int64_t n) {
@@ -15,11 +45,11 @@ public:
         };
 
         const auto& s = to_binary(n);
-        const int l = size(s);
-        string p = s.substr(0, (l + 1) / 2);
-        for (int i = 0; i < l / 2; ++i) {
-            p.push_back(s[(l / 2 - 1) - i]);
+        const int l = size(s) / 2;
+        string p = s.substr(0, size(s) - l);
+        for (int i = 0; i < l; ++i) {
+            p.push_back(s[(l - 1) - i]);
         }
-        return ((1 << (l / 2)) - 1) + (n >> (l / 2)) + (p <= s ? 1 : 0);
+        return ((1 << l) - 1) + (n >> l) + (p <= s ? 1 : 0);
     }
 };
