2684eaf4fa25b7e0b6d211b7a8ceff44bd399560
kamyu <kamyu104@users.noreply.github.com>
2025-09-10 00:28:29 +0800

    Optimize space complexity and logic in palindrome count
    Refactor countBinaryPalindromes methods to improve space complexity and update bitmasking logic.

--- Python/count-binary-palindromic-numbers.py
+++ Python/count-binary-palindromic-numbers.py
@@ -1,13 +1,40 @@
 # Time:  O(logn)
-# Space: O(logn)
+# Space: O(1)
 
-# math, combinatorics
+# bitmasks, combinatorics
 class Solution(object):
     def countBinaryPalindromes(self, n):
         """
         :type n: int
         :rtype: int
         """
+        def length(n):
+            result = 0
+            while n:
+                result += 1
+                n >>= 1
+            return result
+
+        def reverse(n, l):
+            result = 0
+            for i in xrange(l):
+                if n&(1<<i):
+                    result |= 1<<((l-1)-i)
+            return result
+    
+        l = length(n)//2
+        return ((1<<l)-1)+(n>>l)+int(((n>>l)<<l)|reverse(n>>(length(n)-l), l) <= n)
+
+
+# Time:  O(logn)
+# Space: O(logn)
+# bitmasks, combinatorics
+class Solution2(object):
+    def countBinaryPalindromes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
         s = map(int, bin(n)[2:])
-        l = len(s)
-        return ((1<<(l//2))-1)+(n>>(l//2))+int(s[:(l+1)//2]+s[:l//2][::-1] <= s)
+        l = len(s)//2
+        return ((1<<l)-1)+(n>>l)+int(s[:len(s)-l]+s[:l][::-1] <= s)
