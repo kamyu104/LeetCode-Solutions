43946dc7a0eed8e1900c76f54f4d752813d2883f
kamyu <kamyu104@users.noreply.github.com>
2025-10-01 17:48:32 +0800

    Update climbing-stairs-ii.cpp

--- C++/climbing-stairs-ii.cpp
+++ C++/climbing-stairs-ii.cpp
@@ -10,8 +10,8 @@ public:
         int a = INF, b = INF, c = 0;
         for (int i = 0; i < n; ++i) {
             tie(a, b, c) = tuple(b, c, costs[i] + min({i - 2 >= 0 ? a + 3 * 3 : INF,
-                                       i - 1 >= 0 ? b + 2 * 2 : INF,
-                                       c + 1 * 1}));
+                                                       i - 1 >= 0 ? b + 2 * 2 : INF,
+                                                       c + 1 * 1}));
         }
         return c;
     }
