1d85aba086421884e4b1fddf232e66cc480a138c
kamyu <kamyu104@users.noreply.github.com>
2025-10-01 17:00:29 +0800

    Add solution for number of zigzag arrays problem
    Implement zigzag arrays calculation using dynamic programming.

---
+++ Python/number-of-zigzag-arrays-i.py
@@ -0,0 +1,21 @@
+# Time:  O(n * (r - l))
+# Space: O(r - l)
+
+# prefix sum, dp
+class Solution(object):
+    def zigZagArrays(self, n, l, r):
+        """
+        :type n: int
+        :type l: int
+        :type r: int
+        :rtype: int
+        """
+        MOD = 10**9+7
+        r -= l
+        dp = [1]*(r+1)
+        for _ in xrange(n-1):
+            prefix = 0
+            for i in xrange(len(dp)):
+                dp[i], prefix = prefix, (prefix+dp[i])%MOD
+            dp.reverse()
+        return (reduce(lambda total, x: (total+x)%MOD, dp, 0)*2)%MOD
