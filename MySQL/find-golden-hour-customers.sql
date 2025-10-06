3e5592044ce13c995168c276648c63d9189fb6fb
kamyu <kamyu104@users.noreply.github.com>
2025-10-06 19:02:19 +0800

    Update find-golden-hour-customers.sql

--- MySQL/find-golden-hour-customers.sql
+++ MySQL/find-golden-hour-customers.sql
@@ -3,12 +3,12 @@
 
 SELECT customer_id,
        COUNT(*) AS total_orders,
-       ROUND(SUM(IF((HOUR(order_timestamp) BETWEEN 11 AND 13) OR (HOUR(order_timestamp) BETWEEN 18 AND 20), 1, 0)) / COUNT(order_id), 2) * 100 AS peak_hour_percentage,
+       ROUND(SUM(IF((HOUR(order_timestamp) BETWEEN 11 AND 13) OR (HOUR(order_timestamp) BETWEEN 18 AND 20), 1, 0)) / COUNT(*), 2) * 100 AS peak_hour_percentage,
        ROUND(AVG(order_rating), 2) AS average_rating
 FROM restaurant_orders
 GROUP BY customer_id
 HAVING total_orders >= 3
    AND peak_hour_percentage >= 60.0
    AND average_rating >= 4.0
-   AND SUM(IF(order_rating IS NOT NULL, 1, 0)) * 2 > COUNT(order_id)
+   AND SUM(IF(order_rating IS NOT NULL, 1, 0)) * 2 > total_orders
 ORDER BY 4 DESC, 1 DESC;
