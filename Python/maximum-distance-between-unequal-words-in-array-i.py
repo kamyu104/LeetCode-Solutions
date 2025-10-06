1909e4149cde64810589e8eed8c8bc7e4064a6f4
kamyu <kamyu104@users.noreply.github.com>
2025-10-06 18:09:24 +0800

    Simplify maximum distance calculation logic
    Refactor logic to calculate maximum distance between unequal words in the array.

--- Python/maximum-distance-between-unequal-words-in-array-i.py
+++ Python/maximum-distance-between-unequal-words-in-array-i.py
@@ -8,8 +8,7 @@ class Solution(object):
         :type words: List[str]
         :rtype: int
         """
-        if words[0] != words[-1]:
-            return (len(words)-1)-0+1
-        i = next((i for i in reversed(xrange(len(words))) if words[i] != words[0]), -1)
-        j = next((j for j in xrange(len(words)) if words[j] != words[-1]), len(words))
-        return max(i-0+1, (len(words)-1)-j+1)
+        for i in xrange(len(words)//2+1):
+            if words[~i] != words[0] or words[i] != words[-1]:
+                return len(words)-i
+        return 0
