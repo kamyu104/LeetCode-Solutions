f8bd8aeda3f0d29c7aaba57c5661d0b91fd4ec18
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-10-07 13:02:45 +0530

    Create Avoid Flood in the city.java

---
+++ Java/Avoid Flood in the city.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    public int[] avoidFlood(int[] rains) {
+        int[] ans = new int[rains.length];
+        Arrays.fill(ans, 1);
+        TreeSet<Integer> st = new TreeSet<Integer>();
+        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
+        for (int i = 0; i < rains.length; ++i) {
+            if (rains[i] == 0) {
+                st.add(i);
+            } else {
+                ans[i] = -1;
+                if (mp.containsKey(rains[i])) {
+                    Integer it = st.ceiling(mp.get(rains[i]));
+                    if (it == null) {
+                        return new int[0];
+                    }
+                    ans[it] = rains[i];
+                    st.remove(it);
+                }
+                mp.put(rains[i], i);
+            }
+        }
+        return ans;
+    }
+}
