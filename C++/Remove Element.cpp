00190623936145c18edcc264ac0b135a3dce646c
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-10-12 14:39:03 +0530

    Create Remove Element.cpp

---
+++ C++/Remove Element.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int index = 0;
+        for(int i = 0; i< nums.size(); i++){
+            if(nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index;
+    }
+};
