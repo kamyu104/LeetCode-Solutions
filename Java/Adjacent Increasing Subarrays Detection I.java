b58551d17591bf6024dd88af477eab2c8bec5fc2
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-10-14 18:40:16 +0530

    Create Adjacent Increasing Subarrays Detection I.java

---
+++ Java/Adjacent Increasing Subarrays Detection I.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+        int n = nums.size();
+        int cnt = 1;
+        int precnt = 0;
+        int ans = 0;
+
+        for (int i = 1; i < n; ++i) {
+            if (nums.get(i) > nums.get(i - 1)) {
+                ++cnt;
+            } else {
+                precnt = cnt;
+                cnt = 1;
+            }
+            ans = Math.max(ans, Math.min(precnt, cnt));
+            ans = Math.max(ans, cnt / 2);
+        }
+
+        return ans >= k;
+    }
+}
