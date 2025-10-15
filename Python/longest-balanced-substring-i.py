a57eabfdb9ecc8fdeb681c6f684eef923eb11800
kamyu <kamyu104@users.noreply.github.com>
2025-10-15 20:28:16 +0800

    Update longest-balanced-substring-i.py

--- Python/longest-balanced-substring-i.py
+++ Python/longest-balanced-substring-i.py
@@ -1,4 +1,4 @@
-# Time:  O(a * n^2), a = len(set(s))
+# Time:  O(n * (a + n))), a = len(set(s))
 # Space: O(a)
 
 import collections
@@ -14,14 +14,16 @@ class Solution(object):
         result = 0
         for i in xrange(len(s)):
             cnt = collections.defaultdict(int)
+            mx = 0
             for j in xrange(i, len(s)):
                 cnt[s[j]] += 1
-                if (j-i+1)%len(cnt) == 0 and (j-i+1)//len(cnt) == max(cnt.itervalues()):
+                mx = max(mx, cnt[s[j]])
+                if (j-i+1)%len(cnt) == 0 and (j-i+1)//len(cnt) == mx:
                     result = max(result, j-i+1)
         return result
 
 
-# Time:  O(26 * n^2)
+# Time:  O(n * (26 + n))
 # Space: O(26)
 # freq table
 class Solution2(object):
@@ -33,11 +35,12 @@ class Solution2(object):
         result = 0
         for i in xrange(len(s)):
             cnt = [0]*26
-            unique = 0
+            mx = unique = 0
             for j in xrange(i, len(s)):
                 if cnt[ord(s[j])-ord('a')] == 0:
                     unique += 1
                 cnt[ord(s[j])-ord('a')] += 1
-                if (j-i+1)%unique == 0 and (j-i+1)//unique == max(cnt):
+                mx = max(mx, cnt[ord(s[j])-ord('a')])
+                if (j-i+1)%unique == 0 and (j-i+1)//unique == mx:
                     result = max(result, j-i+1)
         return result
