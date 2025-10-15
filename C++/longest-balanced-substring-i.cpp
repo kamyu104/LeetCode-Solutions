7c8e3c38d14005e6ea41694232ca31440be832b2
kamyu <kamyu104@users.noreply.github.com>
2025-10-15 20:30:58 +0800

    Update longest-balanced-substring-i.cpp

--- C++/longest-balanced-substring-i.cpp
+++ C++/longest-balanced-substring-i.cpp
@@ -1,4 +1,4 @@
-// Time:  O(26 * n^2)
+// Time:  O(n * (26 + n))
 // Space: O(26)
 
 // freq table
@@ -8,12 +8,13 @@ public:
         int result = 0;
         for (int i = 0; i < size(s); ++i) {
             vector<int> cnt(26);
-            int unique = 0;
+            int mx = 0, unique = 0;
             for (int j = i; j < size(s); ++j) {
                 if (++cnt[s[j] - 'a'] == 1) {
                     ++unique;
                 }
-                if ((j - i + 1) % unique == 0 && (j - i + 1) / unique == ranges::max(cnt)) {
+                mx = max(mx, cnt[s[j] - 'a']);
+                if ((j - i + 1) % unique == 0 && (j - i + 1) / unique == mx) {
                     result = max(result, j - i + 1);
                 }
             }
@@ -22,7 +23,7 @@ public:
     }
 };
 
-// Time:  O(a * n^2), a = len(set(s))
+// Time:  O(n * (a + n)), a = len(set(s))
 // Space: O(a)
 // freq table
 class Solution2 {
@@ -31,11 +32,10 @@ public:
         int result = 0;
         for (int i = 0; i < size(s); ++i) {
             unordered_map<char, int> cnt;
+            int mx = 0;
             for (int j = i; j < size(s); ++j) {
-                ++cnt[s[j]];
-                if ((j - i + 1) % size(cnt) == 0 && (j - i + 1) / size(cnt) == max_element(cbegin(cnt), cend(cnt), [](const auto& a, const auto& b) {
-                    return a.second < b.second;
-                })->second) {
+                mx = max(mx, ++cnt[s[j]]);
+                if ((j - i + 1) % size(cnt) == 0 && (j - i + 1) / size(cnt) == mx) {
                     result = max(result, j - i + 1);
                 }
             }
