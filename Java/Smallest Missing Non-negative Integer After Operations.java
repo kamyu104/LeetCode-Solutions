ab52be8b1d6588c89f191148dd917c70f78cd7c9
Dipanita Mondal <132455672+Dipanita45@users.noreply.github.com>
2025-10-16 08:30:14 +0530

    Create Smallest Missing Non-negative Integer After Operations.java

---
+++ Java/Smallest Missing Non-negative Integer After Operations.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    public int findSmallestInteger(int[] nums, int value) {
+        int[] mp = new int[value];
+        for (int x : nums) {
+            int v = ((x % value) + value) % value;
+            mp[v]++;
+        }
+        int mex = 0;
+        while (mp[mex % value] > 0) {
+            mp[mex % value]--;
+            mex++;
+        }
+        return mex;
+    }
+}
