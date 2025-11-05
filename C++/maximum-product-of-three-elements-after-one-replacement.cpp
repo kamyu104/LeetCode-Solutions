59c3f016c82844dd923c1d3090fa3c65db3a8348
kamyu <kamyu104@users.noreply.github.com>
2025-11-05 16:37:25 +0800

    Create maximum-product-of-three-elements-after-one-replacement.cpp

---
+++ C++/maximum-product-of-three-elements-after-one-replacement.cpp
@@ -0,0 +1,21 @@
+// Time:  O(n)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    long long maxProduct(vector<int>& nums) {
+        static const int L = 2;
+
+        vector<int> top(L);
+        for (auto x : nums) {
+            x = abs(x);
+            for (auto& y: top) {
+                if (x > y) {
+                    swap(x, y);
+                }
+            }
+        }
+        return 100000ll * top[0] * top[1];
+    }
+};
