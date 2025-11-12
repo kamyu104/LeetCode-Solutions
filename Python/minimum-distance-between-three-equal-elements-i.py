99969f2a40968844416aeb7b584cf5263e7d4661
kamyu <kamyu104@users.noreply.github.com>
2025-11-12 18:28:06 +0800

    Add minimum distance function for equal elements
    Implement minimum distance calculation for three equal elements using a hash table and sliding window approach.

---
+++ Python/minimum-distance-between-three-equal-elements-i.py
@@ -0,0 +1,19 @@
+# Time:  O(n)
+# Space: O(n)
+
+# hash table, sliding window
+class Solution(object):
+    def minimumDistance(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        INF = float("inf")
+        result = INF
+        lookup = collections.defaultdict(list)
+        for i, x in enumerate(nums):
+            lookup[x].append(i)
+            if len(lookup[x]) < 3:
+                continue
+            result = min(result, 2*(i-lookup[x][-3]))  # k, j, i = lookup[x][:-3], (i-j)+(j-k)+(i-k) = 2*(i-k)
+        return result if result != INF else -1
