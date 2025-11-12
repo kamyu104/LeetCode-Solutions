1e20393296cc86a9ec8f90c12c37d01104fdfec3
kamyu <kamyu104@users.noreply.github.com>
2025-11-12 17:58:08 +0800

    Add solution for longest non-decreasing subarray
    Implement a solution to find the longest non-decreasing subarray after replacing at most one element.

---
+++ Python/longest-non-decreasing-subarray-after-replacing-at-most-one-element.py
@@ -0,0 +1,23 @@
+# Time:  O(n)
+# Space: O(n)
+
+# prefix sum
+class Solution(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = [1]*len(nums)
+        for i in xrange(len(nums)-1):
+            if nums[i] <= nums[i+1]:
+                left[i+1] = left[i]+1
+        right = [1]*len(nums)
+        for i in reversed(xrange(len(nums)-1)):
+            if nums[i] <= nums[i+1]:
+                right[i] = right[i+1]+1
+        result = min(max(left)+1, len(nums))
+        for i in xrange(1, len(nums)-1):
+            if nums[i-1] <= nums[i+1]:
+                result = max(result, left[i-1]+1+right[i+1])
+        return result
