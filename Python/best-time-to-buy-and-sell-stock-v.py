da8955f494a1c419645ff59c6c8a640a27b73599
kamyu <kamyu104@users.noreply.github.com>
2025-11-12 22:23:11 +0800

    Update best-time-to-buy-and-sell-stock-v.py

--- Python/best-time-to-buy-and-sell-stock-v.py
+++ Python/best-time-to-buy-and-sell-stock-v.py
@@ -9,6 +9,34 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        dp = [float("-inf")]*(len(prices)+1)
+        dp[0] = 0
+        mn, mx = float("inf"), float("-inf")
+        for i in xrange(len(prices)):
+            mn, mx = min(mn, prices[i]), max(mx, prices[i])
+            dp[i+1] = mx-mn
+        result = dp[-1]
+        for i in xrange(1, k):
+            x, y = float("-inf"), float("-inf")
+            new_dp = [float("-inf")]*(len(prices)+1)
+            for j in xrange(i, len(prices)):
+                x, y = max(x, dp[j]-prices[j]), max(y, dp[j]+prices[j])
+                new_dp[j+1] = max(new_dp[j], x+prices[j], y-prices[j])
+            dp = new_dp
+            result = max(result, dp[-1])
+        return result
+
+
+# Time:  O(n * k)
+# Space: O(k)
+# dp
+class Solution2(object):
+    def maximumProfit(self, prices, k):
+        """
+        :type prices: List[int]
+        :type k: int
+        :rtype: int
+        """
         bought = [float("-inf")]*k
         sold = [float("-inf")]*k
         result = [float("-inf")]*(k+1)
