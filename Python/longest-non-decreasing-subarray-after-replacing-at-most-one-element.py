b2ffa77db1ce689df82ea9f24adab9ef045acf2f
kamyu <kamyu104@users.noreply.github.com>
2025-11-13 00:11:41 +0800

    Update longest-non-decreasing-subarray-after-replacing-at-most-one-element.py

--- Python/longest-non-decreasing-subarray-after-replacing-at-most-one-element.py
+++ Python/longest-non-decreasing-subarray-after-replacing-at-most-one-element.py
@@ -8,6 +8,31 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        right = [1]*len(nums)
+        for i in reversed(xrange(len(nums)-1)):
+            if nums[i] <= nums[i+1]:
+                right[i] = right[i+1]+1
+        result = min(max(right)+1, len(nums))
+        left = 1
+        for i in xrange(1, len(nums)-1):
+            if nums[i-1] <= nums[i+1]:
+                result = max(result, left+1+right[i+1])
+            if nums[i-1] <= nums[i]:
+                left += 1
+            else:
+                left = 1
+        return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# prefix sum
+class Solution2(object):
+    def longestSubarray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
         left = [1]*len(nums)
         for i in xrange(len(nums)-1):
             if nums[i] <= nums[i+1]:
