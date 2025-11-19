cf381fb9edf10b1525b7ad4d5f30529ca7973731
kamyu <kamyu104@users.noreply.github.com>
2025-11-20 02:25:52 +0800

    Add countStableSubarrays method in Solution class
    Implement countStableSubarrays method to count stable subarrays based on given queries.

---
+++ Python/count-stable-subarrays.py
@@ -0,0 +1,26 @@
+# Time:  O(n + q)
+# Space: O(n)
+
+# prefix sum
+class Solution(object):
+    def countStableSubarrays(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        def count(n):
+            return (n+1)*n//2
+
+        right = range(len(nums))
+        for i in reversed(xrange(len(nums)-1)):
+            if nums[i] <= nums[i+1]:
+                right[i] = right[i+1]
+        prefix = [0]*(len(nums)+1)
+        curr = 0
+        for i in xrange(len(nums)):
+            if i-1 >= 0 and nums[i-1] > nums[i]:
+                curr = 0
+            curr += 1
+            prefix[i+1] = prefix[i]+curr
+        return [count(min(right[l], r)-l+1)+prefix[r+1]-prefix[min(right[l], r)+1] for l, r in queries]
