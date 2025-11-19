868c745d358ab4aa47148ae80ad5c211c3230ef5
kamyu <kamyu104@users.noreply.github.com>
2025-11-20 03:59:01 +0800

    Update maximize-expression-of-three-elements.py

--- Python/maximize-expression-of-three-elements.py
+++ Python/maximize-expression-of-three-elements.py
@@ -8,11 +8,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        i = max(xrange(len(nums)), key=lambda x:nums[x])
+        i = max(xrange(len(nums)), key=lambda x: nums[x])
         nums[i], nums[-1] = nums[-1], nums[i]
-        j = max(xrange(len(nums)-1), key=lambda x:nums[x])
+        j = max(xrange(len(nums)-1), key=lambda x: nums[x])
         nums[j], nums[-2] = nums[-2], nums[j]
-        k = min(xrange(len(nums)-2), key=lambda x:nums[x])
+        k = min(xrange(len(nums)-2), key=lambda x: nums[x])
         nums[k], nums[0] = nums[0], nums[k]
         return nums[-1]+nums[-2]-nums[0]
     
