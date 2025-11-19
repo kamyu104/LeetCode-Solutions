a2703bc0163db4df5a95ba0de7d68cdc2b8ec16a
kamyu <kamyu104@users.noreply.github.com>
2025-11-20 03:18:44 +0800

    Create evaluate-valid-expressions.cpp

---
+++ C++/evaluate-valid-expressions.cpp
@@ -0,0 +1,49 @@
+// Time:  O(n)
+// Space: O(n)
+
+// stack
+class Solution {
+public:
+    long long evaluateExpression(string expression) {
+        static const unordered_map<string, function<int64_t (int64_t, int64_t)>> LOOKUP = {
+            {"add", [](int64_t a, int64_t b) { return a + b; }},
+            {"sub", [](int64_t a, int64_t b) { return a - b; }},
+            {"mul", [](int64_t a, int64_t b) { return a * b; }},
+            {"div", [](int64_t a, int64_t b) { return a / b; }}
+        };
+    
+        static const string SYMBOLS = "(,)";
+
+        int64_t result = 0;
+        string curr;
+        vector<string> ops;
+        vector<vector<int64_t>> params;
+        for (const auto& x : expression) {
+            if (SYMBOLS.find(x) == string::npos) {
+                curr.push_back(x);
+                continue;
+            }
+            if (x == '(') {
+                ops.emplace_back(curr);
+                curr.clear();
+                params.emplace_back();
+                continue;
+            }
+            if (!empty(curr)) {
+                result = stoll(curr);
+                curr.clear();
+            }
+            params.back().emplace_back(result);
+            if (x == ')') {
+                const auto op = ops.back(); ops.pop_back();
+                const auto param = params.back(); params.pop_back();
+                result = LOOKUP.at(op)(param[0], param[1]);
+            }
+        }
+        if (!empty(curr)) {
+            result = stoll(curr);
+            curr.clear();
+        }
+        return result;
+    }
+};
