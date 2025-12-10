40c0e4e26d190b18f007c4312d6bab5a70dee372
kamyu <kamyu104@users.noreply.github.com>
2025-12-10 10:03:11 +0800

    Create sort-integers-by-binary-reflection.cpp

---
+++ C++/sort-integers-by-binary-reflection.cpp
@@ -0,0 +1,29 @@
+// Time:  O(nlogr + nlogn)
+// Space: O(n)
+
+// sort, bitmasks
+class Solution {
+public:
+    vector<int> sortByReflection(vector<int>& nums) {
+        const auto& reverse = [](int x) {
+            int result = 0;
+            for (; x; x >>= 1) {
+                result = (result << 1) + (x & 1);
+            }
+            return result;
+        };
+
+        vector<pair<int, int>> pairs;
+        pairs.reserve(size(nums));
+        for (const auto& x : nums) {
+            pairs.emplace_back(reverse(x), x);
+        }
+        ranges::sort(pairs);
+        vector<int> result;
+        result.reserve(size(nums));
+        for (const auto& [_, x] : pairs) {
+            result.emplace_back(x);
+        }
+        return result;
+    }
+};
