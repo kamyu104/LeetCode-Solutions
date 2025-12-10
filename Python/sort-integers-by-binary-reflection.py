d61350ecd2129640f4fcb4cdf4555e748aa69622
kamyu <kamyu104@users.noreply.github.com>
2025-12-10 10:03:57 +0800

    Add sorting by binary reflection method
    Implement sorting of integers by their binary reflection.

---
+++ Python/sort-integers-by-binary-reflection.py
@@ -0,0 +1,18 @@
+# Time:  O(nlogr + nlogn)
+# Space: O(n)
+
+# sort, bitmasks
+class Solution(object):
+    def sortByReflection(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        def reverse(x):
+            result = 0
+            while x:
+                result = (result<<1)+(x&1)
+                x >>= 1
+            return result
+
+        return [x for _, x in sorted((reverse(x), x) for x in nums)]
