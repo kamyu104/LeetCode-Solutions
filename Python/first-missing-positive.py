016a1c1b0d395e343b0c566933a7b84cabb22953
kamyu <kamyu104@users.noreply.github.com>
2025-12-13 08:57:53 +0800

    Update first-missing-positive.py

--- Python/first-missing-positive.py
+++ Python/first-missing-positive.py
@@ -2,18 +2,13 @@
 # Space: O(1)
 
 class Solution(object):
-    # @param A, a list of integers
-    # @return an integer
-    def firstMissingPositive(self, A):
-        i = 0
-        while i < len(A):
-            if A[i] > 0 and A[i] - 1 < len(A) and A[i] != A[A[i]-1]:
-                A[A[i]-1], A[i] = A[i], A[A[i]-1]
-            else:
-                i += 1
-
-        for i, integer in enumerate(A):
-            if integer != i + 1:
-                return i + 1
-        return len(A) + 1
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in xrange(len(nums)):
+            while 0 <= nums[i]-1 <= len(nums)-1 and nums[nums[i]-1] != nums[i]:
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+        return next(((i+1) for i, x in enumearte(nums if x != i+1), len(nums)+1)
 
