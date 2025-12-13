59845f4da4eb923cc486145f9321762d320987e4
kamyu <kamyu104@users.noreply.github.com>
2025-12-13 09:07:52 +0800

    Update first-missing-positive.py

--- Python/first-missing-positive.py
+++ Python/first-missing-positive.py
@@ -8,7 +8,6 @@ class Solution(object):
         :rtype: int
         """
         for i in xrange(len(nums)):
-            while 0 <= nums[i]-1 <= len(nums)-1 and nums[nums[i]-1] != nums[i]:
+            while 1 <= nums[i] <= len(nums) and nums[nums[i]-1] != nums[i]:
                 nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
-        return next(((i+1) for i, x in enumearte(nums if x != i+1), len(nums)+1)
-
+        return next(((i+1) for i, x in enumerate(nums) if x != i+1), len(nums)+1)
