cf95a97387bac52b66e4da59a7ddb46b58f207d2
kamyu <kamyu104@users.noreply.github.com>
2025-12-13 09:06:20 +0800

    Update first-missing-positive.cpp

--- C++/first-missing-positive.cpp
+++ C++/first-missing-positive.cpp
@@ -4,22 +4,16 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-        int i = 0;
-        bucketSort(&nums);
-        for (; i < nums.size() && nums[i] == i + 1; ++i);
-        return i + 1;
-    }
-
-private:
-    void bucketSort(vector<int> *nums) {
-        int i = 0;
-        while (i < nums->size()) {
-            if ((*nums)[i] > 0 && (*nums)[i] <= nums->size() &&
-                (*nums)[i] != (*nums)[(*nums)[i] - 1]) {
-                swap((*nums)[i], (*nums)[(*nums)[i] - 1]);
-            } else {
-                ++i;
+        for (int i = 0; i < size(nums); ++i) {
+            while (1 <= nums[i] && nums[i] <= size(nums) && nums[nums[i] - 1] != nums[i]) {
+                swap(nums[nums[i] - 1], nums[i]);
             }
         }
+        for (int i = 0; i < size(nums); ++i) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        return size(nums) + 1;
     }
 };
