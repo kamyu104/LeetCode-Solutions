1276f53245597b03feb8aab288deedf791966f08
kamyu <kamyu104@users.noreply.github.com>
2025-12-29 12:35:41 +0800

    Create number-of-balanced-integers-in-a-range.cpp

---
+++ C++/number-of-balanced-integers-in-a-range.cpp
@@ -0,0 +1,116 @@
+// Time:  O(d^2)
+// Space: O(d^2)
+
+// memoization
+class Solution {
+public:
+    long long countBalanced(long long low, long long high) {
+      const auto& count = [&](int64_t n) {
+        vector<int> digits;
+        for (; n; n /= 10) {
+            digits.emplace_back(n % 10);
+        }
+        ranges::reverse(digits);
+        vector<vector<int64_t>> memo(size(digits),
+            vector<int64_t>(size(digits) * 9 + 1, -1));
+        const int shift = size(digits) / 2 * 9;
+        const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
+            if (i == size(digits)) {
+                return curr == shift;
+            }
+            if (!tight && memo[i][curr] != -1) {
+                return memo[i][curr];
+            }
+            const auto& bound = tight ? digits[i] : 9;
+            int64_t result = 0;
+            for (int d = 0; d <= bound; ++d) {
+                result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
+            }
+            if (!tight) {
+                memo[i][curr] = result;
+            }
+            return result;
+        };
+        
+        return memoization(0, shift, true);
+      };
+
+      return count(high) - count(low - 1);
+    }
+};
+
+// Time:  O(d^2)
+// Space: O(d^2)
+// memoization
+class Solution2 {
+public:
+    long long countBalanced(long long low, long long high) {
+      const auto& count = [&](int64_t n) {
+        vector<int> digits;
+        for (; n; n /= 10) {
+            digits.emplace_back(n % 10);
+        }
+        ranges::reverse(digits);
+        vector<vector<vector<int64_t>>> memo(size(digits),
+            vector<vector<int64_t>>(size(digits) * 9 + 1,
+                vector<int64_t>(2, -1)));
+        const int shift = size(digits) / 2 * 9;
+        const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
+            if (i == size(digits)) {
+                return curr == shift;
+            }
+            if (memo[i][curr][tight] == -1) {
+                const auto& bound = tight ? digits[i] : 9;
+                int64_t result = 0;
+                for (int d = 0; d <= bound; ++d) {
+                    result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
+                }
+                memo[i][curr][tight] = result;
+            }
+            return memo[i][curr][tight];
+        };
+        
+        return memoization(0, shift, true);
+      };
+
+      return count(high) - count(low - 1);
+    }
+};
+
+// Time:  O(d^2)
+// Space: O(d^2)
+// dp
+class Solution3 {
+public:
+    long long countBalanced(long long low, long long high) {
+        const auto& count = [&](int64_t n) {
+            vector<int> digits;
+            for (; n; n /= 10) {
+                digits.emplace_back(n % 10);
+            }
+            ranges::reverse(digits);
+            const auto& max_sum = size(digits) * 9;
+            const auto& shift = size(digits) / 2 * 9;
+            vector<vector<vector<int64_t>>> dp(size(digits) + 1, 
+                vector<vector<int64_t>>(max_sum + 1, 
+                    vector<int64_t>(2)));
+            dp[0][shift][1] = 1;
+            for (int i = 0; i < size(digits); ++i) {
+                for (int curr = 0; curr <= max_sum; ++curr) {
+                    for (int tight = 0; tight <= 1; ++tight) {
+                        if (dp[i][curr][tight] == 0) {
+                            continue;
+                        }                        
+                        for (int d = 0, bound = tight ? digits[i] : 9; d <= bound; ++d) {
+                            dp[i + 1][(i & 1) ? curr - d : curr + d][tight && (d == bound) ? 1 : 0] += dp[i][curr][tight];
+                        }
+                    }
+                }
+            }
+            
+            return dp[size(digits)][shift][0] + dp[size(digits)][shift][1];
+        };
+        
+        return count(high) - count(low - 1);
+    }
+};
