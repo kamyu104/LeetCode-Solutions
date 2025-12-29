484e39cb77a5a94bcb274d98e28c0f4d524e82b4
kamyu <kamyu104@users.noreply.github.com>
2025-12-29 13:59:11 +0800

    Update number-of-balanced-integers-in-a-range.cpp

--- C++/number-of-balanced-integers-in-a-range.cpp
+++ C++/number-of-balanced-integers-in-a-range.cpp
@@ -1,86 +1,8 @@
 // Time:  O((logn)^2)
-// Space: O((logn)^2)
-
-// memoization
-class Solution {
-public:
-    long long countBalanced(long long low, long long high) {
-      const auto& count = [&](int64_t n) {
-        vector<int> digits;
-        for (; n; n /= 10) {
-            digits.emplace_back(n % 10);
-        }
-        ranges::reverse(digits);
-        vector<vector<int64_t>> memo(size(digits),
-            vector<int64_t>(size(digits) * 9 + 1, -1));
-        const int shift = size(digits) / 2 * 9;
-        const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
-            if (i == size(digits)) {
-                return curr == shift;
-            }
-            if (!tight && memo[i][curr] != -1) {
-                return memo[i][curr];
-            }
-            const auto& bound = tight ? digits[i] : 9;
-            int64_t result = 0;
-            for (int d = 0; d <= bound; ++d) {
-                result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
-            }
-            if (!tight) {
-                memo[i][curr] = result;
-            }
-            return result;
-        };
-        
-        return memoization(0, shift, true);
-      };
-
-      return count(high) - count(low - 1);
-    }
-};
-
-// Time:  O((logn)^2)
-// Space: O((logn)^2)
-// memoization
-class Solution2 {
-public:
-    long long countBalanced(long long low, long long high) {
-      const auto& count = [&](int64_t n) {
-        vector<int> digits;
-        for (; n; n /= 10) {
-            digits.emplace_back(n % 10);
-        }
-        ranges::reverse(digits);
-        vector<vector<vector<int64_t>>> memo(size(digits),
-            vector<vector<int64_t>>(size(digits) * 9 + 1,
-                vector<int64_t>(2, -1)));
-        const int shift = size(digits) / 2 * 9;
-        const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
-            if (i == size(digits)) {
-                return curr == shift;
-            }
-            if (memo[i][curr][tight] == -1) {
-                const auto& bound = tight ? digits[i] : 9;
-                int64_t result = 0;
-                for (int d = 0; d <= bound; ++d) {
-                    result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
-                }
-                memo[i][curr][tight] = result;
-            }
-            return memo[i][curr][tight];
-        };
-        
-        return memoization(0, shift, true);
-      };
-
-      return count(high) - count(low - 1);
-    }
-};
-
-// Time:  O((logn)^2)
 // Space: O(logn)
+
 // dp
-class Solution3 {
+class Solution {
 public:
     long long countBalanced(long long low, long long high) {
         const auto& count = [&](int64_t n) {
@@ -112,5 +34,83 @@ public:
         };
         
         return count(high) - count(low - 1);
+    }
+};
+
+// Time:  O((logn)^2)
+// Space: O((logn)^2)
+// memoization
+class Solution2 {
+public:
+    long long countBalanced(long long low, long long high) {
+        const auto& count = [&](int64_t n) {
+            vector<int> digits;
+            for (; n; n /= 10) {
+                digits.emplace_back(n % 10);
+            }
+            ranges::reverse(digits);
+            vector<vector<int64_t>> memo(size(digits),
+            vector<int64_t>(size(digits) * 9 + 1, -1));
+            const int shift = size(digits) / 2 * 9;
+            const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
+                if (i == size(digits)) {
+                    return curr == shift;
+                }
+                if (!tight && memo[i][curr] != -1) {
+                    return memo[i][curr];
+                }
+                const auto& bound = tight ? digits[i] : 9;
+                int64_t result = 0;
+                for (int d = 0; d <= bound; ++d) {
+                    result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
+                }
+                if (!tight) {
+                    memo[i][curr] = result;
+                }
+                return result;
+            };
+            
+            return memoization(0, shift, true);
+        };
+
+        return count(high) - count(low - 1);
+    }
+};
+
+// Time:  O((logn)^2)
+// Space: O((logn)^2)
+// memoization
+class Solution3 {
+public:
+    long long countBalanced(long long low, long long high) {
+      const auto& count = [&](int64_t n) {
+        vector<int> digits;
+        for (; n; n /= 10) {
+            digits.emplace_back(n % 10);
+        }
+        ranges::reverse(digits);
+        vector<vector<vector<int64_t>>> memo(size(digits),
+            vector<vector<int64_t>>(size(digits) * 9 + 1,
+                vector<int64_t>(2, -1)));
+        const int shift = size(digits) / 2 * 9;
+        const auto memoization = [&](this auto&& memoization, int i, int curr, bool tight) -> int64_t {
+            if (i == size(digits)) {
+                return curr == shift;
+            }
+            if (memo[i][curr][tight] == -1) {
+                const auto& bound = tight ? digits[i] : 9;
+                int64_t result = 0;
+                for (int d = 0; d <= bound; ++d) {
+                    result += memoization(i + 1, (i & 1) ? curr - d : curr + d, tight && d == bound);
+                }
+                memo[i][curr][tight] = result;
+            }
+            return memo[i][curr][tight];
+        };
+        
+        return memoization(0, shift, true);
+      };
+
+      return count(high) - count(low - 1);
     }
 };
