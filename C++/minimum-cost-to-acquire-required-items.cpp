8aec9fbe12fc563bcc4b577874db8b4f620de0cd
kamyu <kamyu104@users.noreply.github.com>
2025-12-29 12:24:44 +0800

    Create minimum-cost-to-acquire-required-items.cpp

---
+++ C++/minimum-cost-to-acquire-required-items.cpp
@@ -0,0 +1,14 @@
+// Time:  O(1)
+// Space: O(1)
+
+// math
+class Solution {
+public:
+    long long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
+        const int64_t mn = min(need1, need2);
+        const int64_t mx = max(need1, need2);
+        return min({static_cast<int64_t>(need1) * cost1 + static_cast<int64_t>(need2) * cost2,
+                    mn * costBoth + (need1 - mn) * cost1 + (need2 - mn) * cost2,
+                    mx * costBoth});
+    }
+};
