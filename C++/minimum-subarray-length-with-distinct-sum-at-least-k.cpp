6c439bdf4cdfd5377d03e91250887a1f90c9e634
kamyu <kamyu104@users.noreply.github.com>
2026-01-06 17:31:07 +0800

    Create minimum-subarray-length-with-distinct-sum-at-least-k.cpp

---
+++ C++/minimum-subarray-length-with-distinct-sum-at-least-k.cpp
@@ -0,0 +1,25 @@
+// Time:  O(n)
+// Space: O(1)
+
+// freq table, two pointers
+class Solution {
+public:
+    int minLength(vector<int>& nums, int k) {
+        static const int INF = numeric_limits<int>::max();
+
+        int result = INF;
+        unordered_map<int, int> cnt;
+        for (int right = 0, left = 0, curr = 0; right < size(nums); ++right) {
+            if (++cnt[nums[right]] == 1) {
+                curr += nums[right];
+            }
+            for (; curr >= k; ++left) {
+                result = min(result, right - left + 1);
+                if (cnt[nums[left]]-- == 1) {
+                    curr -= nums[left];
+                }
+            }
+        }
+        return result != INF ? result : -1;
+    }
+};
