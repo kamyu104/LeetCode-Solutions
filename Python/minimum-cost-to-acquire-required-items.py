fea7a67ec5fe2ae325c3d8f0a318e7cb6ae6fa02
kamyu <kamyu104@users.noreply.github.com>
2026-01-07 01:42:50 +0800

    Refactor minimumCost calculation for clarity

--- Python/minimum-cost-to-acquire-required-items.py
+++ Python/minimum-cost-to-acquire-required-items.py
@@ -14,4 +14,22 @@ class Solution(object):
         """
         mn = min(need1, need2)
         mx = max(need1, need2)
+        return mn*min(cost1+cost2, costBoth)+min((need1-mn)*cost1+(need2-mn)*cost2, (mx-mn)*costBoth)
+
+
+# Time:  O(1)
+# Space: O(1)
+# math
+class Solution2(object):
+    def minimumCost(self, cost1, cost2, costBoth, need1, need2):
+        """
+        :type cost1: int
+        :type cost2: int
+        :type costBoth: int
+        :type need1: int
+        :type need2: int
+        :rtype: int
+        """
+        mn = min(need1, need2)
+        mx = max(need1, need2)
         return min(need1*cost1+need2*cost2, mn*costBoth+(need1-mn)*cost1+(need2-mn)*cost2, mx*costBoth)
