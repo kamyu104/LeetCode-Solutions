16e8d6ba681b1d06f2ca5239d1b247ce1010a6d3
kamyu <kamyu104@users.noreply.github.com>
2026-01-07 00:50:38 +0800

    Create word-squares-ii.py

---
+++ Python/word-squares-ii.py
@@ -0,0 +1,56 @@
+# Time:  O(n^4)
+# Space: O(n)
+
+import collections
+
+
+# brute force, hash table
+class Solution(object):
+    def wordSquares(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[str]]
+        """
+        words.sort()
+        lookup = collections.defaultdict(list)
+        for i, w in enumerate(words):
+            lookup[w[0]].append(i)
+            lookup[w[0], w[3]].append(i)
+        result = []
+        for i in xrange(len(words)):
+            for j in lookup[words[i][0]]:
+                if j == i:
+                    continue
+                for k in lookup[words[i][3]]:
+                    if k in (i, j):
+                        continue
+                    for l in lookup[words[j][3], words[k][3]]:
+                        if l in (i, j, k):
+                            continue
+                        result.append([words[i], words[j], words[k], words[l]])
+        return result
+
+
+# Time:  O(n^4)
+# Space: O(1)
+# brute force
+class Solution2(object):
+    def wordSquares(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[List[str]]
+        """
+        words.sort()
+        result = []
+        for i in xrange(len(words)):
+            for j in xrange(len(words)):
+                if j == i or words[j][0] != words[i][0]:
+                    continue
+                for k in xrange(len(words)):
+                    if k in (i, j) or words[k][0] != words[i][3]:
+                        continue
+                    for l in xrange(len(words)):
+                        if l in (i, j, k) or words[l][0] != words[j][3] or words[l][3] != words[k][3]:
+                            continue
+                        result.append([words[i], words[j], words[k], words[l]])
+        return result
