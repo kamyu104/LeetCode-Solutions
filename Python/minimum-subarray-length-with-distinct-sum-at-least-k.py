e889c7a81de3dd02f2187808d37ceaf3a4c94883
kamyu <kamyu104@users.noreply.github.com>
2026-01-06 17:31:49 +0800

    Add solution for minimum subarray length problem
    Implement solution to find minimum subarray length with distinct sum at least k.

---
+++ Python/minimum-subarray-length-with-distinct-sum-at-least-k.py
@@ -0,0 +1,29 @@
+# Time:  O(n)
+# Space: O(1)
+
+import collections
+
+
+# freq table, two pointers
+class Solution(object):
+    def minLength(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        INF = float("inf")
+        cnt = collections.defaultdict(int)
+        result = INF
+        left = curr = 0
+        for right in xrange(len(nums)):
+            cnt[nums[right]] += 1
+            if cnt[nums[right]] == 1:
+                curr += nums[right]
+            while curr >= k:
+                result = min(result, right-left+1)
+                if cnt[nums[left]] == 1:
+                    curr -= nums[left]
+                cnt[nums[left]] -= 1
+                left += 1
+        return result if result is not INF else -1
