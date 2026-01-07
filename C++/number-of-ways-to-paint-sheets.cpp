4a9b5231940bbb8ba973af63e2df6e13ed3026b0
kamyu <kamyu104@users.noreply.github.com>
2026-01-07 15:20:25 +0800

    Create number-of-ways-to-paint-sheets.cpp

---
+++ C++/number-of-ways-to-paint-sheets.cpp
@@ -0,0 +1,31 @@
+// Time:  O(mlogm)
+// Space: O(m)
+
+// prefix sum, two pointers
+class Solution {
+public:
+    int numberOfWays(int n, vector<int>& limit) {
+        static const int MOD = 1e9 + 7;
+
+        ranges::sort(limit);
+        vector<int64_t> suffix(size(limit) + 1);        
+        for(int i = size(limit) - 1; i >= 0; i--) {
+            limit[i] = min(limit[i], n - 1);
+            suffix[i] = suffix[i + 1] + limit[i];
+        }
+        int64_t result = 0;
+        for (int i = size(limit) - 1, j = 0; i >= 0; --i) {
+            for (; j < size(limit); ++j) {
+                if (limit[i] + limit[j] >= n) {
+                    break;
+                }
+            }
+            int64_t cnt = (limit[i] - n + 1) * (size(limit) - j)  + suffix[j];
+            if (i >= j) {
+                cnt -= limit[i] + limit[i] - n + 1;
+            }
+            result = (result + cnt) % MOD;
+        }
+        return result;
+    }
+};
