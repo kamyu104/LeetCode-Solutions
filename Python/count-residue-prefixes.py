0d0b561147d42bfb43f920da92e84a8461255e56
kamyu <kamyu104@users.noreply.github.com>
2026-01-12 20:01:07 +0800

    Refactor variable names for clarity in function

---
+++ Python/count-residue-prefixes.py
@@ -0,0 +1,41 @@
+# Time:  O(n + 26)
+# Space: O(26)
+
+# freq table
+class Solution(object):
+    def residuePrefixes(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = distinct = 0
+        lookup = [False]*26
+        for i, x in enumerate(s):
+            if not lookup[ord(x)-ord('a')]:
+                distinct += 1
+                if distinct >= 3:
+                    break
+            lookup[ord(x)-ord('a')] = True
+            if distinct == (i+1)%3:
+                result += 1
+        return result
+
+
+# Time:  O(n)
+# Space: O(3)
+# freq table
+class Solution2(object):
+    def residuePrefixes(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        result = 0
+        lookup = set()
+        for i, x in enumerate(s):
+            lookup.add(x)
+            if len(lookup) >= 3:
+                break
+            if len(lookup) == (i+1)%3:
+                result += 1
+        return result
