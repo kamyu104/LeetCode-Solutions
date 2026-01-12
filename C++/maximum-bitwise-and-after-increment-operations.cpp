52fc80d5b9f97b76a56f67d503facb1bcc261d38
kamyu <kamyu104@users.noreply.github.com>
2026-01-13 02:28:34 +0800

    Implement maximum AND function with two approaches

---
+++ C++/maximum-bitwise-and-after-increment-operations.cpp
@@ -0,0 +1,50 @@
+// Time:  O(nlogr)
+// Space: O(n)
+
+// bitmasks, greedy, quick select
+class Solution {
+public:
+    int maximumAND(vector<int>& nums, int k, int m) {
+        int result = 0;
+        for (int i = bit_width(static_cast<uint32_t>(ranges::max(nums) + k)) - 1; i >= 0; --i) {
+            const int target = result | (1 << i);
+            vector<int> costs;
+            costs.reserve(size(nums));
+            for (const auto& x : nums) {
+                const auto& l = bit_width(static_cast<uint32_t>(target & ~x));
+                const auto& mask = (1ll << l) - 1;
+                costs.emplace_back((target & mask) - (x & mask));
+            }
+            ranges::nth_element(costs, begin(costs) + (m - 1));
+            if (accumulate(cbegin(costs), cbegin(costs) + m, 0ll) <= k) {
+                result |= 1 << i;
+            }
+        }
+        return result;
+    }
+};
+
+// Time:  O(nlogn * logr)
+// Space: O(n)
+// bitmasks, greedy, sort
+class Solution2 {
+public:
+    int maximumAND(vector<int>& nums, int k, int m) {
+        int result = 0;
+        for (int i = bit_width(static_cast<uint32_t>(ranges::max(nums) + k)) - 1; i >= 0; --i) {
+            const int target = result | (1 << i);
+            vector<int> costs;
+            costs.reserve(size(nums));
+            for (const auto& x : nums) {
+                const auto& l = bit_width(static_cast<uint32_t>(target & ~x));
+                const auto& mask = (1ll << l) - 1;
+                costs.emplace_back((target & mask) - (x & mask));
+            }
+            ranges::sort(costs);
+            if (accumulate(cbegin(costs), cbegin(costs) + m, 0ll) <= k) {
+                result |= 1 << i;
+            }
+        }
+        return result;
+    }
+};
