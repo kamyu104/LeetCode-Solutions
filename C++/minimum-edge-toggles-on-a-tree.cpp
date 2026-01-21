78ad1890909bcaf691ab427edeb5548aa5d980a1
kamyu <kamyu104@users.noreply.github.com>
2026-01-22 05:31:51 +0800

    Update minimum-edge-toggles-on-a-tree.cpp

--- C++/minimum-edge-toggles-on-a-tree.cpp
+++ C++/minimum-edge-toggles-on-a-tree.cpp
@@ -53,3 +53,70 @@ public:
         return result;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+// greedy, topological sort
+class Solution2 {
+public:
+    vector<int> minimumFlips(int n, vector<vector<int>>& edges, string start, string target) {
+        vector<int> diff(n);
+        for (int i = 0; i < n; ++i) {
+            if (start[i] != target[i]) {
+                diff[i] = 1;
+            }
+        }
+        if (accumulate(cbegin(diff), cend(diff), 0) % 2) {
+            return {-1};
+        }
+        vector<int> degree(n);
+        vector<vector<pair<int, int>>> adj(n);
+        const auto& topological_sort = [&]() {
+            vector<bool> lookup(size(adj));
+            vector<int> q;
+            for (int u = 0; u < size(adj); ++u) {
+                if (size(adj[u]) == 1) {
+                    q.emplace_back(u);
+                }
+            }
+            while (!empty(q)) {
+                vector<int> new_q;
+                for (const auto& u : q) {
+                    for (const auto& [v, idx] : adj[u]) {
+                        if (degree[v] == 0) {
+                            continue;
+                        }
+                        if (diff[u]) {
+                            diff[u] ^= 1;
+                            diff[v] ^= 1;
+                            lookup[idx] = true;
+                        }
+                        --degree[u];
+                        --degree[v];
+                        if (degree[v] == 1) {
+                            new_q.emplace_back(v);
+                        }
+                    }
+                }
+                q = move(new_q);
+            }
+            return lookup;
+        };
+
+        for (int idx = 0; idx < size(edges); ++idx) {
+            const auto& u = edges[idx][0], &v = edges[idx][1];
+            ++degree[u];
+            ++degree[v];
+            adj[u].emplace_back(v, idx);
+            adj[v].emplace_back(u, idx);
+        }
+        const auto& lookup = topological_sort();
+        vector<int> result;
+        for (int i = 0; i < size(lookup); ++i) {
+            if (lookup[i]) {
+                result.emplace_back(i);
+            }
+        }
+        return result;
+    }
+};
