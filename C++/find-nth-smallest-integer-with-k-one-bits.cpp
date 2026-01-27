21d0c8068247217dbc7d1d77e317b85623aba342
kamyu <kamyu104@users.noreply.github.com>
2026-01-27 17:37:11 +0800

    Create find-nth-smallest-integer-with-k-one-bits.cpp

---
+++ C++/find-nth-smallest-integer-with-k-one-bits.cpp
@@ -0,0 +1,35 @@
+// Time:  ctor:    O(r^2)
+//        runtime: O(k)
+// Space: O(r^2)
+
+// dp, combinatorics
+const auto& init = [](int k) {
+    vector<vector<int64_t>> dp(k + 1, vector<int64_t>(k + 1));
+    for (int i = 0; i < size(dp); ++i) {
+        dp[i][0] = 1;
+        for (int j = 1; j <= i; ++j) {
+            dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+        }
+    }
+    return dp;
+};
+
+static const int MAX_K = 50;
+const auto& DP = init(MAX_K);
+class Solution {
+public:
+    long long nthSmallest(long long n, int k) {
+        int64_t result = 0;
+        for (int i = MAX_K - 1; i >= 0; --i) {
+            if (n <= DP[i][k]) {
+                continue;
+            }
+            n -= DP[i][k];
+            result |= 1ll << i;
+            if (--k == 0) {
+                break;
+            }
+        }
+        return result;
+    }
+};
