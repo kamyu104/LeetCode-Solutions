a8ef0ba6c01f24a64523eead388f799e05dcdbcb
kamyu <kamyu104@users.noreply.github.com>
2026-02-04 22:32:34 +0800

    Add ride-sharing system implementation
    Implement a ride-sharing system with methods to add riders and drivers, match them, and cancel riders.

---
+++ Python/design-ride-sharing-system.py
@@ -0,0 +1,55 @@
+# Time:   ctor:                 O(1)
+#         addRider:             O(1)
+#         addDriver:            O(1)
+#         matchDriverWithRider: O(1)
+#         cancelRider:          O(1)
+# Space: O(n)
+
+import collections
+
+
+# ordered dict
+class RideSharingSystem(object):
+
+    def __init__(self):
+        self.__riders = collections.OrderedDict()
+        self.__drivers = collections.OrderedDict()
+        
+
+    def addRider(self, riderId):
+        """
+        :type riderId: int
+        :rtype: None
+        """
+        self.__riders[riderId] = True
+        
+
+    def addDriver(self, driverId):
+        """
+        :type driverId: int
+        :rtype: None
+        """
+        self.__drivers[driverId] = True
+        
+
+    def matchDriverWithRider(self):
+        """
+        :rtype: List[int]
+        """
+        if not self.__riders or not self.__drivers:
+            return [-1, -1]
+        r = next(iter(self.__riders))
+        d = next(iter(self.__drivers))
+        del self.__riders[r]
+        del self.__drivers[d]
+        return [d, r]
+    
+
+    def cancelRider(self, riderId):
+        """
+        :type riderId: int
+        :rtype: None
+        """
+        if riderId in self.__riders:
+            del self.__riders[riderId]
+        
