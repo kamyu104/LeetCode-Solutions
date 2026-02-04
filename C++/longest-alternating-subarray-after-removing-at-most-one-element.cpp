eea7ad8a9f35306e075d32441f34dbaeec018e68
kamyu <kamyu104@users.noreply.github.com>
2026-02-04 23:56:36 +0800

    Update longest-alternating-subarray-after-removing-at-most-one-element.cpp

--- C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
+++ C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
@@ -5,14 +5,14 @@
 class Solution {
 public:
     int longestAlternating(vector<int>& nums) {
-        int result = 1, up1 = 1, up = 1, down1 = 1, down = 1;
+        int result = 1, up1 = 1, up0 = 1, down1 = 1, down0 = 1;
         for (int i = 0; i + 1 < size(nums); ++i) {
             if (nums[i] < nums[i + 1]) {
-                tie(up1, up, down1, down) = tuple(down1 + 1, down + 1, down, 1);
+                tie(up1, up0, down1, down0) = tuple(down1 + 1, down0 + 1, down0, 1);
             } else if (nums[i] > nums[i + 1]) {
-                tie(up1, up, down1, down) = tuple(up, 1, up1 + 1, up + 1);
+                tie(up1, up0, down1, down0) = tuple(up0, 1, up1 + 1, up0 + 1);
             } else {
-                tie(up1, up, down1, down) = tuple(up, 1, down, 1);
+                tie(up1, up0, down1, down0) = tuple(up0, 1, down0, 1);
             }
             result = max({result, up1, down1});
         }
