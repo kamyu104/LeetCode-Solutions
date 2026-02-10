ae9af1476f39f81bab27c1e3db82ef500a7d6fdf
kamyu <kamyu104@users.noreply.github.com>
2026-02-10 14:38:20 +0800

    Update longest-alternating-subarray-after-removing-at-most-one-element.cpp

--- C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
+++ C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
@@ -19,3 +19,52 @@ public:
         return result;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+// prefix sum
+class Solution2 {
+public:
+    int longestAlternating(vector<int>& nums) {
+        const auto& cmp = [](int a, int b) {
+            return (a > b) ? 1 : (a < b) ? -1 : 0;
+        };
+        
+
+        vector<int> left(size(nums), 1);
+        for (int i = 1; i < size(nums); ++i) {
+            const auto& diff = cmp(nums[i - 1], nums[i]);
+            if (diff == 0) {
+                continue;
+            }
+            if (i - 2 >= 0 && cmp(nums[i - 2], nums[i - 1]) == -diff) {
+                left[i] = left[i - 1] + 1;
+            } else {
+                left[i] = 2;
+            }
+        }
+        vector<int> right(size(nums), 1);
+        for (int i = size(nums) - 2; i >= 0; --i) {
+            const auto& diff = cmp(nums[i], nums[i + 1]);
+            if (diff == 0) {
+                continue;
+            }
+            if (i + 2 < size(nums) && cmp(nums[i + 1], nums[i + 2]) == -diff) {
+                right[i] = right[i + 1] + 1;
+            } else {
+                right[i] = 2;
+            }
+        }
+        int result = ranges::max(left);
+        for (int i = 1; i + 1 < size(nums); ++i) {
+            const auto& diff = cmp(nums[i - 1], nums[i + 1]);
+            if (diff == 0) {
+                continue;
+            }
+            const auto& l = (i - 2 >= 0 && cmp(nums[i - 2], nums[i - 1]) == -diff) ? left[i - 1] : 1;
+            const auto& r = (i + 2 < size(nums) && cmp(nums[i + 1], nums[i + 2]) == -diff) ? right[i + 1] : 1;
+            result = max(result, l + r);
+        }
+        return result;
+    }
+};
