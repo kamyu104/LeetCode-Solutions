a5a92f472605eda5d1bb85fa7b571984a185a54b
kamyu <kamyu104@users.noreply.github.com>
2026-03-04 00:43:00 +0800

    Add mergeCharacters method to merge close characters
    Implement mergeCharacters method to merge close characters in a string based on a given integer k.

---
+++ Python/merge-close-characters.py
@@ -0,0 +1,21 @@
+# Time:  O(n + 26)
+# Space: O(26)
+
+# simulation, two pointers
+class Solution(object):
+    def mergeCharacters(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        result = []
+        cnt = [0]*26
+        for x in s:
+            if cnt[ord(x)-ord('a')]:
+                continue
+            cnt[ord(x)-ord('a')] += 1
+            result.append(x)
+            if len(result) >= k+1:
+                cnt[ord(result[-(k+1)])-ord('a')] -= 1
+        return "".join(result)
