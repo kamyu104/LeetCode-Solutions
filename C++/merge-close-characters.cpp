e680113019f6b8d7114d58ebe7041114a888ebd2
kamyu <kamyu104@users.noreply.github.com>
2026-03-04 00:42:32 +0800

    Create merge-close-characters.cpp

---
+++ C++/merge-close-characters.cpp
@@ -0,0 +1,22 @@
+// Time:  O(n + 26)
+// Space: O(26)
+
+// simulation, two pointers
+class Solution {
+public:
+    string mergeCharacters(string s, int k) {
+        string result;
+        vector<int> cnt(26);
+        for (const auto& x : s) {
+            if (cnt[x - 'a']) {
+                continue;
+            }
+            ++cnt[x - 'a'];
+            result.push_back(x);
+            if (size(result) >= k + 1) {
+                --cnt[result[size(result) - (k + 1)] - 'a'];
+            }
+        }
+        return result;
+    }
+};
