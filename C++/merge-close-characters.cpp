34444787f566d7482b9cd0b300784adfb440935c
kamyu <kamyu104@users.noreply.github.com>
2026-03-04 19:52:08 +0800

    Update merge-close-characters.cpp

--- C++/merge-close-characters.cpp
+++ C++/merge-close-characters.cpp
@@ -1,8 +1,27 @@
 // Time:  O(n + 26)
 // Space: O(26)
 
-// simulation, freq table, two pointers
+// simulation, hash table
 class Solution {
+public:
+    string mergeCharacters(string s, int k) {
+        string result;
+        vector<int> lookup(26, -1);
+        for (const auto& x : s) {
+            if (lookup[x - 'a'] != -1 && size(result) - lookup[x - 'a'] <= k) {
+                continue;
+            }
+            lookup[x - 'a'] = size(result);
+            result.push_back(x);
+        }
+        return result;
+    }
+};
+
+// Time:  O(n + 26)
+// Space: O(26)
+// simulation, freq table, two pointers
+class Solution2 {
 public:
     string mergeCharacters(string s, int k) {
         string result;
