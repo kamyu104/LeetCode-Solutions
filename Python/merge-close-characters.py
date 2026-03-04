53544d323d11e719a430d85b4cda0bf845a23c85
kamyu <kamyu104@users.noreply.github.com>
2026-03-04 19:50:34 +0800

    Update merge-close-characters.py

--- Python/merge-close-characters.py
+++ Python/merge-close-characters.py
@@ -1,8 +1,28 @@
 # Time:  O(n + 26)
 # Space: O(26)
 
-# simulation, freq table, two pointers
+# simulation, hash table
 class Solution(object):
+    def mergeCharacters(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: str
+        """
+        result = []
+        lookup = [-1]*26
+        for x in s:
+            if lookup[ord(x)-ord('a')] != -1 and len(result)-lookup[ord(x)-ord('a')] <= k:
+                continue
+            lookup[ord(x)-ord('a')] = len(result)
+            result.append(x)
+        return "".join(result)
+
+
+# Time:  O(n + 26)
+# Space: O(26)
+# simulation, freq table, two pointers
+class Solution2(object):
     def mergeCharacters(self, s, k):
         """
         :type s: str
