5f52dcd3bbf8f00bd49d7f3e4bb97fae1a1cca52
kamyu <kamyu104@users.noreply.github.com>
2026-03-10 00:18:35 +0800

    Add Solution2 class for minimum cost partitioning
    Implement a new solution class with minCost method to calculate minimum cost for partitioning a binary string.

--- Python/minimum-cost-to-partition-a-binary-string.py
+++ Python/minimum-cost-to-partition-a-binary-string.py
@@ -22,3 +22,33 @@ class Solution(object):
         for i in xrange(len(s)):
             prefix[i+1] = prefix[i]+(1 if s[i] == '1' else 0)
         return divide_and_conquer(0, len(s)-1)
+
+
+# Time:  O(n)
+# Space: O(n)
+# queue
+class Solution2(object):
+    def minCost(self, s, encCost, flatCost):
+        """
+        :type s: str
+        :type encCost: int
+        :type flatCost: int
+        :rtype: int
+        """
+        l = len(s)
+        while l%2 == 0:
+            l //= 2
+        result = 0
+        q = []
+        for left in xrange(0, len(s), l):
+            x = sum(s[i] == '1' for i in xrange(left, left+l))
+            q.append((l*x*encCost if x else flatCost, x))
+        while len(q) != 1:
+            new_q = []
+            l *= 2
+            for i in xrange(0, len(q), 2):
+                v = q[i][0]+q[i+1][0]
+                x = q[i][1]+q[i+1][1]
+                new_q.append((min(l*x*encCost if x else flatCost, v), x))
+            q = new_q
+        return q[0][0]
