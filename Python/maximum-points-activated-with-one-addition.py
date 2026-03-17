fd760d6c35cd40c8969df541f769b2a34aeeb364
kamyu <kamyu104@users.noreply.github.com>
2026-03-18 00:28:37 +0800

    Add union-find solution for maximum points calculation
    Implement a union-find algorithm to calculate maximum points activated with one addition.

---
+++ Python/maximum-points-activated-with-one-addition.py
@@ -0,0 +1,57 @@
+# Time:  O(n)
+# Space: O(n)
+
+# union find
+class Solution(object):
+    def maxActivated(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        class UnionFind(object):  # Time: O(n * alpha(n)), Space: O(n)
+            def __init__(self, n):
+                self.set = range(n)
+                self.rank = [0]*n
+                self.size = [1]*n
+
+            def find_set(self, x):
+                stk = []
+                while self.set[x] != x:  # path compression
+                    stk.append(x)
+                    x = self.set[x]
+                while stk:
+                    self.set[stk.pop()] = x
+                return x
+
+            def union_set(self, x, y):
+                x, y = self.find_set(x), self.find_set(y)
+                if x == y:
+                    return False
+                if self.rank[x] > self.rank[y]:  # union by rank
+                    x, y = y, x
+                elif self.rank[x] == self.rank[y]:
+                    self.rank[y] += 1
+                self.set[x] = self.set[y]
+                self.size[y] += self.size[x]
+                return True
+            
+            def total(self, x):
+                return self.size[self.find_set(x)]
+        
+        uf = UnionFind(len(points))
+        lookup = [{} for _ in xrange(2)]
+        for i, p in enumerate(points):
+            for j in xrange(len(lookup)):
+                if p[j] in lookup[j]:
+                    uf.union_set(i, lookup[j][p[j]])
+                else:
+                    lookup[j][p[j]] = i
+        top = [0]*2
+        for i in xrange(len(points)):
+            if uf.find_set(i) != i:
+                continue
+            s = uf.total(i)
+            for j in xrange(len(top)):
+                if s > top[j]:
+                    top[j], s = s,top[j]
+        return sum(top)+1
