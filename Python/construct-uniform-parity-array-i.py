a2095eeff4ec0d58a509f5be8a0199d7368d8a41
kamyu <kamyu104@users.noreply.github.com>
2026-03-25 19:49:19 +0800

    Implement uniformArray method in Solution class
    Added a class Solution with a method uniformArray that checks if the input list is uniform.

---
+++ Python/construct-uniform-parity-array-i.py
@@ -0,0 +1,11 @@
+# Time:  O(1)
+# Space: O(1)
+
+# constructive algorithms
+class Solution(object):
+    def uniformArray(self, nums1):
+        """
+        :type nums1: List[int]
+        :rtype: bool
+        """
+        return True
