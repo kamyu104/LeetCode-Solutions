4d5650e9cf649849771340b1c4aea275d50ca824
kamyu <kamyu104@users.noreply.github.com>
2026-03-30 15:56:16 +0800

    Refactor variables in longestAlternating function

--- C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
+++ C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
@@ -5,7 +5,8 @@
 class Solution {
 public:
     int longestAlternating(vector<int>& nums) {
-        int result = 1, up1 = 0, up0 = 1, down1 = 0, down0 = 1;
+        int result = 1, up0 = 1, down0 = 1;
+        int up1 = 0, down1 = 0;
         for (int i = 0; i + 1 < size(nums); ++i) {
             if (nums[i] < nums[i + 1]) {
                 tie(up1, up0, down1, down0) = tuple(down1 + 1, down0 + 1, down0, 1);
