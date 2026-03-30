7bca38d501fda84e88e1fab7c271a7c0b4fe1f54
kamyu <kamyu104@users.noreply.github.com>
2026-03-30 16:06:25 +0800

    Update longest-alternating-subarray-after-removing-at-most-one-element.cpp

--- C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
+++ C++/longest-alternating-subarray-after-removing-at-most-one-element.cpp
@@ -5,6 +5,27 @@
 class Solution {
 public:
     int longestAlternating(vector<int>& nums) {
+        int result = 1, up0 = 1, down0 = 1, up1 = 1, down1 = 1;
+        for (int i = 0; i + 1 < size(nums); ++i) {
+            if (nums[i] < nums[i + 1]) {
+                tie(up1, up0, down1, down0) = tuple(down1 + 1, down0 + 1, down0, 1);
+            } else if (nums[i] > nums[i + 1]) {
+                tie(up1, up0, down1, down0) = tuple(up0, 1, up1 + 1, up0 + 1);
+            } else {
+                tie(up1, up0, down1, down0) = tuple(up0, 1, down0, 1);
+            }
+            result = max({result, up1, down1});
+        }
+        return result;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+// dp
+class Solution2 {
+public:
+    int longestAlternating(vector<int>& nums) {
         int result = 1, up0 = 1, down0 = 1;
         int up1 = 0, down1 = 0;
         for (int i = 0; i + 1 < size(nums); ++i) {
@@ -24,7 +45,7 @@ public:
 // Time:  O(n)
 // Space: O(n)
 // prefix sum
-class Solution2 {
+class Solution3 {
 public:
     int longestAlternating(vector<int>& nums) {
         const auto& cmp = [](int a, int b) {
