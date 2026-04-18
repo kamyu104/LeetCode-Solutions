05548afdf4b636b75de1afe1daaaec3941434909
kamyu <kamyu104@users.noreply.github.com>
2026-04-19 02:36:12 +0800

    Update longest-balanced-substring-after-one-swap.py

--- Python/longest-balanced-substring-after-one-swap.py
+++ Python/longest-balanced-substring-after-one-swap.py
@@ -11,18 +11,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        arr = map(int, s)
-        cnt0 = s.count(0)
+        cnt0 = s.count('0')
         cnt1 = len(s)-cnt0
         if cnt0 < cnt1:
             cnt0, cnt1 = cnt1, cnt0
-            for i in xrange(len(arr)):
-                arr[i] ^= 1
+            s = "".join('0' if x == '1' else '1' for x in s)
         result = bal = 0
         lookup = collections.defaultdict(list)
         lookup[0].append(-1)
         for i, x in enumerate(s):
-            bal += +1 if x == 1 else -1
+            bal += +1 if x == '1' else -1
             if len(lookup[bal]) != 2:
                 lookup[bal].append(i)
             result = max(result, i-lookup[bal][0])
