cf26b3f9f134ffc1ead4a1e7cb757b8c580c64a0
kamyu <kamyu104@users.noreply.github.com>
2026-04-18 19:16:30 +0800

    Create zigzag-level-sum-of-binary-tree.cpp

---
+++ C++/zigzag-level-sum-of-binary-tree.cpp
@@ -0,0 +1,36 @@
+// Time:  O(n)
+// Space: O(w)
+
+// bfs
+class Solution {
+public:
+    vector<long long> zigzagLevelSum(TreeNode* root) {
+        vector<long long> result;
+        vector<TreeNode *> q = {root};
+        for (int parity = 0; !empty(q); parity ^= 1) {
+            vector<TreeNode *> new_q;
+            int64_t total = 0;
+            bool stop = false;
+            for (int i = size(q) - 1; i >= 0; --i) {
+                const auto& node = q[i];
+                auto left = node->left, right = node->right;
+                if (parity) {
+                    swap(left, right);
+                }
+                if (left) {
+                    new_q.emplace_back(left);
+                }
+                if (right) {
+                    new_q.emplace_back(right);
+                }
+                stop = stop || !left;
+                if (!stop) {
+                    total += node->val;
+                }
+            }
+            result.emplace_back(total);
+            q = move(new_q);
+        }
+        return result;
+    }
+};
