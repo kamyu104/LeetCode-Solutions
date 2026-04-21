c870bf6d9d24fee1d8f8eb043aa4268d16d6cded
kamyu <kamyu104@users.noreply.github.com>
2026-04-21 18:38:31 +0800

    Update count-good-integers-on-a-grid-path.cpp

--- C++/count-good-integers-on-a-grid-path.cpp
+++ C++/count-good-integers-on-a-grid-path.cpp
@@ -64,3 +64,63 @@ public:
         return count(r) - count(l - 1);
     }
 };
+
+// Time:  O(16 * 2 * 10 * 10)
+// Space: O(16 + 2 * 10)
+// dp
+class Solution2 {
+public:
+    long long countGoodIntegersOnPath(long long l, long long r, string directions) {
+        static const int L = 16;
+
+        vector<bool> lookup(L);
+        const auto& count = [&](int64_t n) {
+            vector<int> digits(L);
+            for (int i = L - 1; i >= 0; --i) {
+                digits[i] = n % 10;
+                n /= 10;
+            }
+            vector<vector<int64_t>> dp(2, vector<int64_t>(10));
+            dp[1][0] = 1;
+            for (int i = 0; i < L; ++i) {
+                vector<vector<int64_t>> new_dp(2, vector<int64_t>(10));
+                for (int t = 0; t < 2; ++t) {
+                    const auto& bound = (t == 1) ? digits[i] : 9;
+                    for (int k = 0; k < 10; ++k) {
+                        if (!dp[t][k]) {
+                            continue;
+                        }
+                        for (int d = 0; d <= bound; ++d) {
+                            int nk = k;
+                            if (lookup[i]) {
+                                if (d < k) {
+                                    continue;
+                                }
+                                nk = d;
+                            }
+                            new_dp[t == 1 && d == digits[i]][nk] += dp[t][k];
+                        }
+                    }
+                }
+                dp = move(new_dp);
+            }
+            int64_t result = 0;
+            for (const auto& row : dp) {
+                result += accumulate(cbegin(row), cend(row), 0LL);
+            }
+            return result;
+        };
+
+        int i = 0, j = 0;
+        lookup[i * 4 + j] = true;
+        for (const auto& x : directions) {
+            if (x == 'D') {
+                ++i;
+            } else {
+                ++j;
+            }
+            lookup[i * 4 + j] = true;
+        }
+        return count(r) - count(l - 1);
+    }
+};
