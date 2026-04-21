b567c67a7b8e69cc8b3bb180da2dd7f9365a51d9
kamyu <kamyu104@users.noreply.github.com>
2026-04-22 00:39:50 +0800

    Update multi-source-flood-fill.py

--- Python/multi-source-flood-fill.py
+++ Python/multi-source-flood-fill.py
@@ -32,3 +32,34 @@ class Solution(object):
                 result[nr][nc] = -result[nr][nc]
             q = new_q
         return result
+
+
+# Time:  O(slogs + n * m) = O((n * m) * log(n * m))
+# Space: O(n * m)
+# sort, bfs, flood fill
+class Solution2(object):
+    def colorGrid(self, n, m, sources):
+        """
+        :type n: int
+        :type m: int
+        :type sources: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
+        sources.sort(key=lambda x: -x[2])
+        result = [[0]*m for _ in xrange(n)]
+        q = []
+        for r, c, color in sources:
+            result[r][c] = color
+            q.append((r, c))
+        while q:
+            new_q = []
+            for r, c in q:
+                for dr, dc in DIRECTIONS:
+                    nr, nc = r+dr, c+dc
+                    if not (0 <= nr < n and 0 <= nc < m and result[nr][nc] == 0):
+                        continue
+                    result[nr][nc] = result[r][c]
+                    new_q.append((nr, nc))
+            q = new_q
+        return result
