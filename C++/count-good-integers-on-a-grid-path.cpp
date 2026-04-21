a44af2b3988946c321862edb658e9fd144f492b2
kamyu <kamyu104@users.noreply.github.com>
2026-04-22 02:17:57 +0800

    Refactor Solution3 to Solution4 for counting integers

--- C++/count-good-integers-on-a-grid-path.cpp
+++ C++/count-good-integers-on-a-grid-path.cpp
@@ -125,10 +125,68 @@ public:
     }
 };
 
+// Time:  O(16 * 10 * 10)
+// Space: O(16 * 10)
+// memoization
+class Solution3 {
+public:
+    long long countGoodIntegersOnPath(long long l, long long r, string directions) {
+        static const int L = 16;
+
+        const auto& count = [&](int64_t n) {
+            vector<bool> lookup(L);
+            vector<int> digits(L);
+            vector<vector<int64_t>> memo(L, vector<int64_t>(10, -1));
+            const auto memoization = [&](this auto&& memoization, int i, bool t, int k) -> int64_t {
+                if (i == 16) {
+                    return 1;
+                }
+                if (!t && memo[i][k] != -1) {
+                    return memo[i][k];
+                }
+                int64_t result = 0;
+                const auto& bound = t ? digits[i] : 9;
+                for (int d = 0; d <= bound; ++d) {
+                    int nk = k;
+                    if (lookup[i]) {
+                        if (d < k) {
+                            continue;
+                        }
+                        nk = d;
+                    }
+                    result += memoization(i + 1, t && (d == bound), nk);
+                }
+                if (!t) {
+                    memo[i][k] = result;
+                }
+                return result;
+            };
+            
+            for (int i = L - 1; i >= 0; --i) {
+                digits[i] = n % 10;
+                n /= 10;
+            }
+            int i = 0, j = 0;
+            lookup[i * 4 + j] = true;
+            for (const auto& x : directions) {
+                if (x == 'D') {
+                    ++i;
+                } else {
+                    ++j;
+                }
+                lookup[i * 4 + j] = true;
+            }
+            return memoization(0, true, 0);
+        };
+
+        return count(r) - count(l - 1);
+    }
+};
+
 // Time:  O(16 * 2 * 10 * 10)
 // Space: O(16 * 2 * 10)
 // memoization
-class Solution3 {
+class Solution4 {
 public:
     long long countGoodIntegersOnPath(long long l, long long r, string directions) {
         static const int L = 16;
