3af5516610245c5eccf53367963380ea5c982b00
kamyu <kamyu104@users.noreply.github.com>
2026-04-22 01:02:42 +0800

    Update count-good-integers-on-a-grid-path.py

--- Python/count-good-integers-on-a-grid-path.py
+++ Python/count-good-integers-on-a-grid-path.py
@@ -45,3 +45,50 @@ class Solution(object):
                 j += 1
             lookup[i*4+j] = True
         return count(r)-count(l-1)
+
+
+# Time:  O(16 * 2 * 10 * 10)
+# Space: O(16 * 2 * 10)
+# memoization
+class Solution2(object):
+    def countGoodIntegersOnPath(self, l, r, directions):
+        """
+        :type l: int
+        :type r: int
+        :type directions: str
+        :rtype: int
+        """
+        L = 16
+        def count(n):
+            def memoization(i, t, k):
+                if i == L:
+                    return 1
+                if memo[i][t][k] == -1:
+                    memo[i][t][k] = 0
+                    bound = digits[i] if t else 9
+                    for d in xrange(bound+1):
+                        nk = k
+                        if lookup[i]:
+                            if d < k:
+                                continue
+                            nk = d   
+                        memo[i][t][k] += memoization(i+1, t and d == bound, nk)
+                return memo[i][t][k]
+
+            digits = [0]*L
+            for i in reversed(xrange(len(digits))):
+                digits[i] = n%10
+                n //= 10
+            memo = [[[-1]*10 for _ in xrange(2)] for _ in xrange(L)]
+            return memoization(0, True, 0)
+
+        i = j = 0
+        lookup = [False]*L
+        lookup[i*4+j] = True
+        for x in directions:
+            if x == 'D':
+                i += 1
+            else:
+                j += 1
+            lookup[i*4+j] = True
+        return count(r)-count(l-1)
