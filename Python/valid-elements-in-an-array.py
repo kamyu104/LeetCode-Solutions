c28eacaeedbaa1d56db9472dffbba3c47ad3d3bb
kamyu <kamyu104@users.noreply.github.com>
2026-05-02 00:54:32 +0800

    Add findValidElements method to Solution class
    Implement a method to find valid elements in an array based on prefix sums.

---
+++ Python/valid-elements-in-an-array.py
@@ -0,0 +1,23 @@
+# Time:  O(n)
+# Space: O(n)
+
+# prefix sum
+class Solution(object):
+    def findValidElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        right = [True]*len(nums)
+        mx = 0
+        for i in reversed(xrange(len(nums))):
+            right[i] = mx < nums[i]
+            mx = max(mx, nums[i])
+        result = []
+        mx = 0
+        for i in xrange(len(nums)):
+            left = mx < nums[i]
+            mx = max(mx, nums[i])
+            if left or right[i]:
+                result.append(nums[i])
+        return result
