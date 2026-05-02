b8cb63c222d9917b3577c6afda38648b0aeacbd4
kamyu <kamyu104@users.noreply.github.com>
2026-05-02 15:50:23 +0800

    Update valid-elements-in-an-array.py

--- Python/valid-elements-in-an-array.py
+++ Python/valid-elements-in-an-array.py
@@ -21,3 +21,25 @@ class Solution(object):
             if left or right[i]:
                 result.append(nums[i])
         return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# prefix sum
+class Solution2(object):
+    def findValidElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = [True]*len(nums)
+        mx = 0
+        for i in xrange(len(nums)):
+            left[i] = mx < nums[i]
+            mx = max(mx, nums[i])
+        right = [True]*len(nums)
+        mx = 0
+        for i in reversed(xrange(len(nums))):
+            right[i] = mx < nums[i]
+            mx = max(mx, nums[i])
+        return [nums[i] for i in xrange(len(nums)) if left[i] or right[i]]
