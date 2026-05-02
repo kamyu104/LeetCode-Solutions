3a872b87c97a11375872950b7d54a790c3bb1e27
kamyu <kamyu104@users.noreply.github.com>
2026-05-02 15:51:50 +0800

    Update valid-elements-in-an-array.cpp

--- C++/valid-elements-in-an-array.cpp
+++ C++/valid-elements-in-an-array.cpp
@@ -24,3 +24,31 @@ public:
         return result;
     }
 };
+
+// Time:  O(n)
+// Space: O(n)
+// prefix sum
+class Solution2 {
+public:
+    vector<int> findValidElements(vector<int>& nums) {
+        vector<bool> left(size(nums));
+        int mx = 0;
+        for (int i = 0; i < size(nums); ++i) {
+            left[i] = nums[i] > mx;
+            mx = max(mx, nums[i]);
+        }
+        vector<bool> right(size(nums));
+        mx = 0;
+        for (int i = size(nums) - 1; i >= 0; --i) {
+            right[i] = nums[i] > mx;
+            mx = max(mx, nums[i]);
+        }
+        vector<int> result;
+        for (int i = 0; i < size(nums); ++i) {
+            if (left[i] || right[i]) {
+                result.emplace_back(nums[i]);
+            }
+        }
+        return result;
+    }
+};
