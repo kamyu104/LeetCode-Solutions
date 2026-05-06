5341dfd0a7e5310c23b188d99c934679793b489b
kamyu <kamyu104@users.noreply.github.com>
2026-05-06 10:51:48 +0800

    Create minimum-cost-to-move-between-indices.py

---
+++ Python/minimum-cost-to-move-between-indices.py
@@ -0,0 +1,18 @@
+# Time:  O(n)
+# Space: O(n)
+
+# prefix sum
+class Solution(object):
+    def minCost(self, nums, queries):
+        """
+        :type nums: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        prefix = [0]*((len(nums)-1)+1)
+        for i in xrange(len(prefix)-1):
+            prefix[i+1] = prefix[i]+(1 if i-1 == -1 or nums[i+1]-nums[i] < nums[i]-nums[i-1] else nums[i+1]-nums[i])
+        suffix = [0]*((len(nums)-1)+1)
+        for i in reversed(xrange(1, len(suffix))):
+            suffix[i-1] = suffix[i]+(1 if i+1 == len(nums) or nums[i]-nums[i-1] <= nums[i+1]-nums[i] else nums[i]-nums[i-1])
+        return [prefix[right]-prefix[left] if left < right else suffix[right]-suffix[left] for left, right in queries]
