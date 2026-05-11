2a2c1fc8b7971720bae1df1441f105f49cff5872
kamyu <kamyu104@users.noreply.github.com>
2026-05-11 19:59:45 +0800

    Update minimum-generations-to-target-point.cpp

--- C++/minimum-generations-to-target-point.cpp
+++ C++/minimum-generations-to-target-point.cpp
@@ -9,6 +9,44 @@ public:
             return p[0] * 7 * 7 + p[1] * 7 + p[2];
         };
 
+        vector<bool> lookup(7 * 7 * 7);
+        for (const auto& p : points) {
+            if (lookup[encode(p)]) {
+                continue;
+            }
+            lookup[encode(p)] = true;
+        }
+        for (int i = 0, k = 0; i != size(points); ++k) {
+            if (lookup[encode(target)]) {
+                return k;
+            }
+            vector<vector<int>> new_q;
+            const auto& total = size(points);
+            for (; i < total; ++i) {
+                for (int j = 0; j < i; ++j) {
+                    const auto& p = vector<int>{(points[i][0] + points[j][0]) / 2, (points[i][1] + points[j][1]) / 2, (points[i][2] + points[j][2]) / 2};
+                    if (lookup[encode(p)]) {
+                        continue;
+                    }
+                    lookup[encode(p)] = true;
+                    points.emplace_back(p);
+                }
+            }
+        }
+        return -1;
+    }
+};
+
+// Time:  O(7^6)
+// Space: O(7^3)
+// bfs
+class Solution2 {
+public:
+    int minGenerations(vector<vector<int>>& points, vector<int>& target) {
+        const auto& encode = [](const auto& p) {
+            return p[0] * 7 * 7 + p[1] * 7 + p[2];
+        };
+
         vector<vector<int>> q;
         vector<bool> lookup(7 * 7 * 7);
         for (const auto& p : points) {
