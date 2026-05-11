be675b993eaae44f4e473ab629220338b490c6b8
kamyu <kamyu104@users.noreply.github.com>
2026-05-11 20:27:35 +0800

    Update minimum-generations-to-target-point.py

--- Python/minimum-generations-to-target-point.py
+++ Python/minimum-generations-to-target-point.py
@@ -13,6 +13,44 @@ class Solution(object):
             return p[0]*7*7+p[1]*7+p[2]
     
         lookup = [False]*(7**3)
+        k = total = 0
+        for p in points:
+            if lookup[encode(p)]:
+                continue
+            if p == target:
+                return k
+            lookup[encode(p)] = True
+        i = 0
+        while i < len(points):
+            if i == total:
+                total = len(points)
+                k += 1
+            for j in xrange(i):
+                p = [(points[i][0]+points[j][0])//2, (points[i][1]+points[j][1])//2, (points[i][2]+points[j][2])//2]
+                if lookup[encode(p)]:
+                    continue
+                if p == target:
+                    return k
+                lookup[encode(p)] = True
+                points.append(p)
+            i += 1
+        return -1
+
+
+# Time:  O(7^6)
+# Space: O(7^3)
+# bfs
+class Solution2(object):
+    def minGenerations(self, points, target):
+        """
+        :type points: List[List[int]]
+        :type target: List[int]
+        :rtype: int
+        """
+        def encode(p):
+            return p[0]*7*7+p[1]*7+p[2]
+    
+        lookup = [False]*(7**3)
         for p in points:
             if lookup[encode(p)]:
                 continue
@@ -37,7 +75,7 @@ class Solution(object):
 # Time:  O(7^6)
 # Space: O(7^3)
 # bfs
-class Solution2(object):
+class Solution3(object):
     def minGenerations(self, points, target):
         """
         :type points: List[List[int]]
