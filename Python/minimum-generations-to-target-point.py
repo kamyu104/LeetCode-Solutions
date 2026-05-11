695dc9fc02c13de648fbe588c4f1c0058f7693d5
kamyu <kamyu104@users.noreply.github.com>
2026-05-11 19:45:59 +0800

    Create minimum-generations-to-target-point.py

---
+++ Python/minimum-generations-to-target-point.py
@@ -0,0 +1,37 @@
+# Time:  O(7^6)
+# Space: O(7^3)
+
+# bfs
+class Solution(object):
+    def minGenerations(self, points, target):
+        """
+        :type points: List[List[int]]
+        :type target: List[int]
+        :rtype: int
+        """
+        def encode(p):
+            return p[0]*7*7+p[1]*7+p[2]
+    
+        q = []
+        lookup = [False]*(7**3)
+        for p in points:
+            if lookup[encode(p)]:
+                continue
+            lookup[encode(p)] = True
+            q.append(p)
+        k = 0
+        while q:
+            if lookup[encode(target)]:
+                return k
+            new_q = []
+            for i in xrange(len(points)-len(q), len(points)):
+                for j in xrange(i):
+                    p = [(points[i][0]+points[j][0])//2, (points[i][1]+points[j][1])//2, (points[i][2]+points[j][2])//2]
+                    if lookup[encode(p)]:
+                        continue
+                    lookup[encode(p)] = True
+                    new_q.append(p)
+            points.extend(new_q)
+            q = new_q
+            k += 1
+        return -1
