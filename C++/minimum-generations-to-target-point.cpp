f982d5700b8d07d3153d8f847b0621d7a0dbb2c9
kamyu <kamyu104@users.noreply.github.com>
2026-05-11 20:24:01 +0800

    Refactor minGenerations function and add Solution3 class

--- C++/minimum-generations-to-target-point.cpp
+++ C++/minimum-generations-to-target-point.cpp
@@ -14,6 +14,47 @@ public:
             if (lookup[encode(p)]) {
                 continue;
             }
+            if (p == target) {
+                return 0;
+            }
+            lookup[encode(p)] = true;
+        }
+        for (int i = 0, k = 1, total = size(points); i < size(points); ++i) {
+            if (i == total) {
+                total = size(points);
+                ++k;
+            }
+            for (int j = 0; j < i; ++j) {
+                const auto& p = vector<int>{(points[i][0] + points[j][0]) / 2, (points[i][1] + points[j][1]) / 2, (points[i][2] + points[j][2]) / 2};
+                if (lookup[encode(p)]) {
+                    continue;
+                }
+                if (p == target) {
+                    return k;
+                }
+                lookup[encode(p)] = true;
+                points.emplace_back(p);
+            }
+        }
+        return -1;
+    }
+};
+
+// Time:  O(7^6)
+// Space: O(7^3)
+// bfs
+class Solution2 {
+public:
+    int minGenerations(vector<vector<int>>& points, vector<int>& target) {
+        const auto& encode = [](const auto& p) {
+            return p[0] * 7 * 7 + p[1] * 7 + p[2];
+        };
+
+        vector<bool> lookup(7 * 7 * 7);
+        for (const auto& p : points) {
+            if (lookup[encode(p)]) {
+                continue;
+            }
             lookup[encode(p)] = true;
         }
         for (int i = 0, k = 0; i < size(points); ++k) {
@@ -39,7 +80,7 @@ public:
 // Time:  O(7^6)
 // Space: O(7^3)
 // bfs
-class Solution2 {
+class Solution3 {
 public:
     int minGenerations(vector<vector<int>>& points, vector<int>& target) {
         const auto& encode = [](const auto& p) {
