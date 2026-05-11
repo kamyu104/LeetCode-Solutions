804c5562a4087225542c44aca75d9375caf0f65d
kamyu <kamyu104@users.noreply.github.com>
2026-05-11 19:45:27 +0800

    Create minimum-generations-to-target-point.cpp

---
+++ C++/minimum-generations-to-target-point.cpp
@@ -0,0 +1,41 @@
+// Time:  O(7^6)
+// Space: O(7^3)
+
+// bfs
+class Solution {
+public:
+    int minGenerations(vector<vector<int>>& points, vector<int>& target) {
+        const auto& encode = [](const auto& p) {
+            return p[0] * 7 * 7 + p[1] * 7 + p[2];
+        };
+
+        vector<vector<int>> q;
+        vector<bool> lookup(7 * 7 * 7);
+        for (const auto& p : points) {
+            if (lookup[encode(p)]) {
+                continue;
+            }
+            lookup[encode(p)] = true;
+            q.emplace_back(p);
+        }
+        for (int k = 0; !empty(q); ++k) {
+            if (lookup[encode(target)]) {
+                return k;
+            }
+            vector<vector<int>> new_q;
+            for (int i = size(points) - size(q); i < size(points); ++i) {
+                for (int j = 0; j < i; ++j) {
+                    const auto& p = vector<int>{(points[i][0] + points[j][0]) / 2, (points[i][1] + points[j][1]) / 2, (points[i][2] + points[j][2]) / 2};
+                    if (lookup[encode(p)]) {
+                        continue;
+                    }
+                    lookup[encode(p)] = true;
+                    new_q.emplace_back(p);
+                }
+            }
+            ranges::copy(new_q, std::back_inserter(points));
+            q = move(new_q);
+        }
+        return -1;
+    }
+};
