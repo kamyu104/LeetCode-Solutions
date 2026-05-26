5ceb933437c0dd6a9e48fd98dae7c64159d56226
kamyu <kamyu104@users.noreply.github.com>
2026-05-26 18:39:36 +0800

    Create minimum-swaps-to-move-zeros-to-end.cpp

---
+++ C++/minimum-swaps-to-move-zeros-to-end.cpp
@@ -0,0 +1,16 @@
+// Time:  O(n)
+// Space: O(1)
+
+// array
+class Solution {
+public:
+    int minimumSwaps(vector<int>& nums) {
+        int result = 0;
+        for (int i = size(nums) - ranges::count(nums, 0); i < size(nums); ++i) {
+            if (nums[i] != 0) {
+                ++result;
+            }
+        }
+        return result;
+    }
+};
