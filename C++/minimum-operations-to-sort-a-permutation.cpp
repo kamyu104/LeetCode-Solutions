0f46849a35f10cce96f76b95e971d86481f0a120
kamyu <kamyu104@users.noreply.github.com>
2026-05-27 21:13:07 +0800

    Update minimum-operations-to-sort-a-permutation.cpp

--- C++/minimum-operations-to-sort-a-permutation.cpp
+++ C++/minimum-operations-to-sort-a-permutation.cpp
@@ -17,7 +17,8 @@ public:
         const auto& idx = distance(cbegin(nums), find(cbegin(nums), cend(nums), 0));
         if (check(idx, +1)) {
             return min(idx, 1 + (static_cast<int>(size(nums)) - idx) + 1);
-        } else if (check(idx, -1)) {
+        }
+        if (check(idx, -1)) {
             return min((idx + 1) + 1, 1 + (static_cast<int>(size(nums)) - (idx + 1)));
         }
         return -1;
@@ -45,7 +46,8 @@ public:
         const auto& idx = distance(cbegin(nums), find(cbegin(nums), cend(nums), 0));
         if (check([](const auto& a, const auto& b) { return a <= b; })) {
             return min(idx, 1 + (static_cast<int>(size(nums)) - idx) + 1);
-        } else if (check([](const auto& a, const auto& b) { return a >= b; })) {
+        }
+        if (check([](const auto& a, const auto& b) { return a >= b; })) {
             return min((idx + 1) + 1, 1 + (static_cast<int>(size(nums)) - (idx + 1)));
         }
         return -1;
