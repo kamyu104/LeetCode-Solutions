5f846cdbc7202243ffc6e13419dff3e478c41e4f
kamyu <kamyu104@users.noreply.github.com>
2026-05-27 20:34:47 +0800

    Update minimum-operations-to-sort-a-permutation.py

--- Python/minimum-operations-to-sort-a-permutation.py
+++ Python/minimum-operations-to-sort-a-permutation.py
@@ -17,3 +17,26 @@ class Solution(object):
         elif check(idx, -1):
             return min((idx+1)+1, 1+(len(nums)-(idx+1)))
         return -1
+
+
+# Time:  O(n)
+# Space: O(1)
+# array
+class Solution2(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def asc():
+            return sum(nums[i] > nums[(i+1)%len(nums)] for i in xrange(len(nums))) <= 1
+
+        def desc():
+            return sum(nums[i] < nums[(i+1)%len(nums)] for i in xrange(len(nums))) <= 1
+
+        idx = nums.index(0)
+        if asc():
+            return min(idx, 1+(len(nums)-idx)+1)
+        elif desc():
+            return min((idx+1)+1, 1+(len(nums)-(idx+1)))
+        return -1
