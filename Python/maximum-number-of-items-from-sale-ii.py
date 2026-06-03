b3a04e40e641650430ea8866d0755ced8eb6ad74
kamyu <kamyu104@users.noreply.github.com>
2026-06-03 17:10:32 +0800

    Create maximum-number-of-items-from-sale-ii.py

---
+++ Python/maximum-number-of-items-from-sale-ii.py
@@ -0,0 +1,39 @@
+# Time:  O(rlogr + nlogn), r = max(f for f, _ in items)
+# Space: O(r + n)
+
+import collections
+
+
+# freq table, sort, greedy
+class Solution(object):
+    def maximumSaleItems(self, items, budget):
+        """
+        :type items: List[List[int]]
+        :type budget: int
+        :rtype: int
+        """
+        NEG_INF = float("-inf")
+        cnt = [0]*(max(f for f, _ in items)+1)
+        for f, _ in items:
+            cnt[f] += 1
+        total = [0]*len(cnt)
+        for i in xrange(1, len(total)):
+            if not cnt[i]:
+                continue
+            for j in xrange(i, len(total), i):
+                total[i] += cnt[j]
+        min_p = min(p for _, p in items)
+        group = collections.defaultdict(int)
+        for f, price in items:
+            if price >= 2*min_p:
+                continue
+            group[price] += total[f]-1
+        result = 0
+        for p in sorted(group):
+            c = min(group[p], budget//p)
+            result += 2*c
+            budget -= c*p
+            if not budget:
+                break
+        result += budget//min_p
+        return result
