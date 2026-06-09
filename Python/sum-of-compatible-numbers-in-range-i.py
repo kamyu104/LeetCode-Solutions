0bab9a698704d72f4eb63b704b75fada3f2a0c0a
kamyu <kamyu104@users.noreply.github.com>
2026-06-09 19:21:39 +0800

    Create sum-of-compatible-numbers-in-range-i.py

---
+++ Python/sum-of-compatible-numbers-in-range-i.py
@@ -0,0 +1,50 @@
+# Time:  O(log(n + k))
+# Space: O(log(n + k))
+
+# combinatorics
+class Solution(object):
+    def sumOfGoodIntegers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        def count(x):
+            if x <= 0:
+                return 0
+            l = x.bit_length()
+            cnt = [0]*(l+1)
+            cnt[0] = 1
+            total = [0]*(l+1)
+            for i in xrange(l):
+                cnt[i+1] = cnt[i]
+                total[i+1] = total[i]
+                if n&(1<<i):
+                    continue
+                cnt[i+1] = cnt[i]*2
+                total[i+1] = total[i]*2+(1<<i)*cnt[i]
+            result = prefix = 0
+            for i in reversed(xrange(l)):
+                if not x&(1<<i):
+                    continue
+                result += prefix*cnt[i]+total[i]
+                if n&(1<<i):
+                    return result
+                prefix |= 1<<i
+            result += prefix
+            return result
+
+        return count(n+k)-count((n-k)-1)
+
+
+# Time:  O(k)
+# Space: O(k)
+# simulation
+class Solution2(object):
+    def sumOfGoodIntegers(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: int
+        """
+        return sum(i for i in xrange(max(n-k, 1), (n+k)+1) if n&i == 0)
