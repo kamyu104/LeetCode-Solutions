6fce3879b28f3920cf9f486aae4c660d2e473014
kamyu <kamyu104@users.noreply.github.com>
2026-06-09 20:59:43 +0800

    Create valid-binary-strings-with-cost-limit.py

---
+++ Python/valid-binary-strings-with-cost-limit.py
@@ -0,0 +1,39 @@
+# Time:  O(n * 2^n)
+# Space: O(n)
+
+# backtracking
+class Solution(object):
+    def generateValidStrings(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[str]
+        """
+        def backtracking(cost):
+            if len(curr) == n:
+                result.append("".join(curr))
+                return
+            curr.append('0')
+            backtracking(cost)
+            curr.pop()
+            if (not curr or curr[-1] == '0') and cost+len(curr) <= k:
+                curr.append('1')
+                backtracking(cost+(len(curr)-1))
+                curr.pop()
+
+        result, curr = [], []
+        backtracking(0)
+        return result
+
+
+# Time:  O(n * 2^n)
+# Space: O(n)
+# bitmasks
+class Solution2(object):
+    def generateValidStrings(self, n, k):
+        """
+        :type n: int
+        :type k: int
+        :rtype: List[str]
+        """
+        return ["".join('1' if mask&(1<<i) else '0' for i in xrange(n)) for mask in xrange(1<<n) if mask&(mask>>1) == 0 and sum(i for i in xrange(n) if mask&(1<<i)) <= k]
