82fd503cfa87fff0af5224820f22c6767a1452da
kamyu <kamyu104@users.noreply.github.com>
2026-06-10 18:32:13 +0800

    Create minimum-cost-to-split-into-ones-ii.py

---
+++ Python/minimum-cost-to-split-into-ones-ii.py
@@ -0,0 +1,11 @@
+# Time:  O(1)
+# Space: O(1)
+
+# math
+class Solution(object):
+    def minCost(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        return n*(n-1)//2
