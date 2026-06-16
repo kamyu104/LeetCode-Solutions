5718fd5083c9a2e1e2a1b4dc95f9fffee8d0d9e6
kamyu <kamyu104@users.noreply.github.com>
2026-06-17 01:08:52 +0800

    Add solution to maximize sum of device ratings
    Implement a greedy algorithm to maximize the sum of device ratings by selecting the top two ratings from each unit and adjusting the total based on the minimum ratings.

---
+++ C++/maximize-sum-of-device-ratings.cpp
@@ -0,0 +1,36 @@
+// Time:  O(m * n)
+// Space: O(1)
+
+// greedy
+class Solution {
+public:
+    long long maxRatings(vector<vector<int>>& units) {
+        const auto& top2 = [](const auto& nums) {  // Time: O(k * n)
+            int mn1 = numeric_limits<int>::max(), mn2 = numeric_limits<int>::max();
+            for (auto x : nums) {
+                if (x < mn1) {
+                    mn2 = mn1;
+                    mn1 = x;
+                } else if (x < mn2) {
+                    mn2 = x;
+                }
+            }
+            return pair(mn1, mn2);
+        };
+
+        if (size(units[0]) == 1) {
+            return accumulate(cbegin(units), cend(units), 0LL, [](const auto& accu, const auto& x) {
+                return accu + x[0];
+            });
+        }
+        int64_t total = 0;
+        int mn1 = numeric_limits<int>::max(), mn2 = numeric_limits<int>::max();
+        for (const auto& x : units) {
+            const auto& [m1, m2] = top2(x);
+            total += m2;
+            mn1 = min(mn1, m1);
+            mn2 = min(mn2, m2);
+        }
+        return total - mn2 + mn1;
+    }
+};
