35469d33945678f0784549252473dd1bbb9ec6ee
kamyu <kamyu104@users.noreply.github.com>
2026-06-17 03:44:47 +0800

    Create maximum-subarray-sum-after-at-most-k-swaps.py

---
+++ Python/maximum-subarray-sum-after-at-most-k-swaps.py
@@ -0,0 +1,100 @@
+# Time:  O(n^2 * logk)
+# Space: O(n)
+
+import heapq
+
+
+# prefix sum, heap, dp
+class Solution(object):
+    def maxSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def update(heap, total, x):
+            if k == 0:
+                return total
+            heapq.heappush(heap, x)
+            total += x
+            if len(heap) == k+1:
+                total -= heapq.heappop(heap)
+            return total
+
+        cnt = sum(x < 0 for x in nums)
+        if cnt <= k:
+            return sum(x for x in nums if x >= 0) if len(nums)-cnt else max(nums)
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]+nums[i]
+        result = float("-inf")
+        dp = [0]*len(nums)
+        for i in xrange(len(nums)):
+            max_heap = [] 
+            total1 = 0
+            for j in xrange(i, len(nums)):
+                if nums[j] >= 0:
+                    dp[j] = -total1
+                    continue
+                dp[j] = 0
+                total1 = update(max_heap, total1, -nums[j])
+            min_heap = []
+            total2 = 0
+            for j in xrange(i):
+                if nums[j] >= 0:
+                    total2 = update(min_heap, total2, nums[j])
+            for j in reversed(xrange(i, len(nums))):
+                result = max(result, (prefix[j+1]-prefix[i])-dp[j]+total2)
+                if nums[j] >= 0:
+                    total2 = update(min_heap, total2, nums[j])
+                    result = max(result, total2)
+        return result
+
+
+# Time:  O(n^2 * logk)
+# Space: O(n)
+import heapq
+
+
+# prefix sum, heap, dp
+class Solution2(object):
+    def maxSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        def update(heap, total, x):
+            if k == 0:
+                return total
+            heapq.heappush(heap, x)
+            total += x
+            if len(heap) == k+1:
+                total -= heapq.heappop(heap)
+            return total
+
+        prefix = [0]*(len(nums)+1)
+        for i in xrange(len(nums)):
+            prefix[i+1] = prefix[i]+nums[i]
+        result = float("-inf")
+        dp = [0]*len(nums)
+        for i in xrange(len(nums)):
+            max_heap = [] 
+            total1 = 0
+            for j in xrange(i, len(nums)):
+                if nums[j] >= 0:
+                    dp[j] = -total1
+                    continue
+                dp[j] = 0
+                total1 = update(max_heap, total1, -nums[j])
+            min_heap = []
+            total2 = 0
+            for j in xrange(i):
+                if nums[j] >= 0:
+                    total2 = update(min_heap, total2, nums[j])
+            for j in reversed(xrange(i, len(nums))):
+                result = max(result, (prefix[j+1]-prefix[i])-dp[j]+total2)
+                if nums[j] >= 0:
+                    total2 = update(min_heap, total2, nums[j])
+                    result = max(result, total2)
+        return result
