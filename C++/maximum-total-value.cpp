9e37dc08f085693251a90ecbed466e9376806116
kamyu <kamyu104@users.noreply.github.com>
2026-07-01 15:04:57 +0800

    Create maximum-total-value.cpp

---
+++ C++/maximum-total-value.cpp
@@ -0,0 +1,50 @@
+// Time:  O(nlogr)
+// Space: O(1)
+
+// binary search
+class Solution {
+public:
+    int maxTotalValue(vector<int>& value, vector<int>& decay, int m) {
+        static const int MOD = 1e9 + 7;
+        const auto& binary_search = [](int left, int right, const auto& check) {
+            while (left <= right) {
+                const auto& mid = left + (right - left) / 2;
+                if (check(mid)) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            return left;
+        };
+
+        const auto& total = [&](int x) {
+            int result = 0;
+            int64_t cnt = 0;
+            for (int i = 0; i < size(value); ++i) {
+                if (value[i] - x < 0) {
+                    continue;
+                }
+                const int64_t t = (value[i] - x) / decay[i] + 1;
+                result = t % 2 == 0
+                    ? (result + (value[i] + (value[i] - decay[i] * (t - 1))) * (t / 2)) % MOD
+                    : (result + (value[i] - decay[i] * ((t - 1) / 2)) * t) % MOD;
+                cnt += t;
+            }
+            return pair(result, cnt);
+        };
+
+        const auto& check = [&](int x) {
+            return total(x).second <= m;
+        };
+
+        auto [result, cnt] = total(1);
+        if (cnt <= m) {
+            return result;
+        }
+        const auto& x = binary_search(2, ranges::max(value), check);
+        tie(result, cnt) = total(x);
+        result = (result + (m - cnt) * (x - 1)) % MOD;
+        return result;
+    }
+};
