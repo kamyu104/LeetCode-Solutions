3b7a1013d31840a1f0b64ec9c426a22ee3e498b7
kamyu <kamyu104@users.noreply.github.com>
2026-07-11 17:02:30 +0800

    Create minimum-operations-to-transform-binary-string.py

---
+++ Python/minimum-operations-to-transform-binary-string.py
@@ -0,0 +1,27 @@
+# Time:  O(n)
+# Space: O(1)
+
+# greedy
+class Solution(object):
+    def minOperations(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        if s1 == "1" and s2 == "0":
+            return -1
+        s1 = list(s1)
+        s2 = list(s2)
+        result = 0
+        for i in xrange(len(s1)):
+            if s1[i] == s2[i]:
+                continue
+            if s1[i] == '0':
+                result += 1
+            elif i+1 < len(s1):
+                result += 1 if s1[i+1] == '1' else 2
+                s1[i+1] = '0'
+            else:
+                result += 2
+        return result
