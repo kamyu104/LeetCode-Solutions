85380615751bc636c7cf641cb2ecbf4a249ed584
kamyu <kamyu104@users.noreply.github.com>
2026-07-11 17:06:41 +0800

    Update minimum-operations-to-transform-binary-string.py

--- Python/minimum-operations-to-transform-binary-string.py
+++ Python/minimum-operations-to-transform-binary-string.py
@@ -11,8 +11,36 @@ class Solution(object):
         """
         if s1 == "1" and s2 == "0":
             return -1
+        result = 0
+        flipped = False
+        for i in xrange(len(s1)):
+            curr = '0' if flipped else s1[i]
+            flipped = False
+            if curr == s2[i]:
+                continue
+            if curr == '0':
+                result += 1
+            elif i+1 < len(s1):
+                result += 1 if s1[i+1] == '1' else 2
+                flipped = True
+            else:
+                result += 2
+        return result
+
+
+# Time:  O(n)
+# Space: O(n)
+# greedy
+class Solution2(object):
+    def minOperations(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: int
+        """
+        if s1 == "1" and s2 == "0":
+            return -1
         s1 = list(s1)
-        s2 = list(s2)
         result = 0
         for i in xrange(len(s1)):
             if s1[i] == s2[i]:
