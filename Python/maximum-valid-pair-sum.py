393fb38bddffea7d285d2405079292c281cfebae
kamyu <kamyu104@users.noreply.github.com>
2026-07-11 16:29:25 +0800

    Create maximum-valid-pair-sum.py

---
+++ Python/maximum-valid-pair-sum.py
@@ -0,0 +1,16 @@
+# Time:  O(n)
+# Space: O(1)
+
+# prefix sum
+class Solution(object):
+    def maxValidPairSum(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = prefix = float("-inf")
+        for i in xrange(len(nums)-k):
+            prefix = max(prefix, nums[i])
+            result = max(result, prefix+nums[i+k])
+        return result
