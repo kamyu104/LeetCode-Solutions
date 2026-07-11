47009c0b8543322888e76c8dbf207966eb940093
kamyu <kamyu104@users.noreply.github.com>
2026-07-12 04:40:32 +0800

    Update maximum-valid-pair-sum.cpp

--- C++/maximum-valid-pair-sum.cpp
+++ C++/maximum-valid-pair-sum.cpp
@@ -5,9 +5,7 @@
 class Solution {
 public:
     int maxValidPairSum(vector<int>& nums, int k) {
-        static const int NEG_INF = numeric_limits<int>::min();
-
-        int result = NEG_INF, prefix = NEG_INF;
+        int result = 0, prefix = 0;
         for (int i = 0; i + k < size(nums); ++i) {
             prefix = max(prefix, nums[i]);
             result = max(result, prefix + nums[i + k]);
