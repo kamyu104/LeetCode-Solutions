c58a170cf5c4a7262e41e048034a02c0dc529415
kamyu <kamyu104@users.noreply.github.com>
2026-08-06 04:29:01 +0800

    Update minimum-moves-to-balance-circular-array-ii.cpp

--- C++/minimum-moves-to-balance-circular-array-ii.cpp
+++ C++/minimum-moves-to-balance-circular-array-ii.cpp
@@ -39,12 +39,12 @@ public:
 
 // Time:  O(F * E * logV) = O(n^2 * logn), V = O(n), E = O(n), and each augmentation saturates a supply or demand edge, so there are only O(n) augmentations
 // Space: O(V + E) = O(n)
-// min cut max flow
+// min-cost max-flow
+#include <bits/stdc++.h>
+
 // Time: O(F * E * logV
 // Space: O(V + E)
 // Template: https://github.com/kth-competitive-programming/kactl/blob/main/content/graph/MinCostMaxFlow.h
-#include <bits/stdc++.h>
-
 const long long INF = numeric_limits<long long>::max();
 
 struct MCMF {
