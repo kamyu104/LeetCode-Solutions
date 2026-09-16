978d966b78c764dea052f1786ce9f99f74709331
kamyu <kamyu104@users.noreply.github.com>
2026-09-16 17:20:35 +0800

    Fix index calculation in dynamic programming array

--- C++/minimum-days-to-score-exactly-n-points.cpp
+++ C++/minimum-days-to-score-exactly-n-points.cpp
@@ -14,7 +14,7 @@ public:
         vector<int> dp(n + 1, INF);
         dp[0] = -1;
         for (int d = 1; count(d) <= n; ++d) {
-            for (int i = 0; i <= min(count(d), n - count(d)); ++i) {
+            for (int i = 0; i <= min<int>(count(d), size(dp) - 1 - count(d)); ++i) {
                 dp[i + count(d)] = min(dp[i + count(d)], dp[i] + 1 + d);
             }
         }
