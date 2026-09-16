abece858f33ee894f0c38c601ac7b7b9a0845b65
kamyu <kamyu104@users.noreply.github.com>
2026-09-16 17:17:12 +0800

    Add solution for minimum days to score n points
    Implement dynamic programming solution to find minimum days to score exactly n points.

---
+++ C++/minimum-days-to-score-exactly-n-points.cpp
@@ -0,0 +1,23 @@
+// Time:  O(n * sqrt(n))
+// Space: O(n)
+
+// dp
+class Solution {
+public:
+    int minDays(int n) {
+        static const auto& INF = numeric_limits<int>::max();
+
+        const auto& count = [](int n) {
+            return (n + 1) * n / 2;
+        };
+
+        vector<int> dp(n + 1, INF);
+        dp[0] = -1;
+        for (int d = 1; count(d) <= n; ++d) {
+            for (int i = 0; i <= min(count(d), n - count(d)); ++i) {
+                dp[i + count(d)] = min(dp[i + count(d)], dp[i] + 1 + d);
+            }
+        }
+        return dp[n];
+    }
+};
