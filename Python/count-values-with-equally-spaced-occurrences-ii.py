d3aa703cd9a59bfe9b82bf781089e90572868c3a
kamyu <kamyu104@users.noreply.github.com>
2026-09-16 12:04:17 +0800

    Add countSpecialIntegers method to Solution class
    Implement a solution to count special integers with equally spaced occurrences.

---
+++ Python/count-values-with-equally-spaced-occurrences-ii.py
@@ -0,0 +1,17 @@
+# Time:  O(n)
+# Space: O(n)
+
+import collections
+
+
+# hash table
+class Solution(object):
+    def countSpecialIntegers(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        lookup = collections.defaultdict(list)
+        for i, x in enumerate(nums):
+            lookup[x].append(i)
+        return sum(len(idxs) >= 3 and all(idxs[i+1]-idxs[i] == idxs[-1]-idxs[-2] for i in xrange(len(idxs)-2)) for idxs in lookup.itervalues())
