3931eae2f259128da93e94edbc227c7b0d2f65b8
kamyu <kamyu104@users.noreply.github.com>
2026-09-22 13:37:32 +0800

    Refactor to bi-bfs for minimum operations calculation

--- Python/minimum-operations-to-make-xor-of-all-elements-zero.py
+++ Python/minimum-operations-to-make-xor-of-all-elements-zero.py
@@ -1,8 +1,51 @@
 # Time:  O(n + r * d), r = max(nums), d = len(set(nums))
 # Space: O(r + d)
 
-# bfs
+# bi-bfs
 class Solution(object):
+    def minOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        def bi_bfs(start, target):
+            left, right = {start}, {target}
+            lookup = set()
+            steps = 0
+            while left:
+                if len(left) > len(right):
+                    left, right = right, left
+                for x in left:
+                    lookup.add(x)
+                new_left = set()
+                for x in left:
+                    if x in right:
+                        return steps
+                    for v in nums_set:
+                        nx = x^v
+                        if nx in lookup:
+                            continue
+                        new_left.add(nx)
+                left = new_left
+                steps += 1
+            return -1
+
+        total = 0
+        for x in nums:
+            total ^= x
+        if total == 0:
+            return 0
+        nums_set = set(nums)
+        if len(nums_set) == 1:
+            return -1
+        steps = bi_bfs(0, total)
+        return steps if steps != len(nums) else -1
+
+
+# Time:  O(n + r * d), r = max(nums), d = len(set(nums))
+# Space: O(r + d)
+# bfs
+class Solution2(object):
     def minOperations(self, nums):
         """
         :type nums: List[int]
